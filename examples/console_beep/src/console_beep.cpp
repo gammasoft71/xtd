12bddc7d1c79229d41a61883be53c7c90fa79450
Gammasoft71 <gammasoft71@gmail.com>
2019-02-06 11:14:04 +0100

    add console_beep example

---
+++ examples/console_beep/src/console_beep.cpp
@@ -0,0 +1,16 @@
+#include <console/console.hpp>
+
+using namespace gammasoft;
+
+// The main entry point for the application.
+int main(int argc, char* argv[]) {
+  int x = 0;
+  //
+  if ((argc == 2) && (x = atoi(argv[1])) && ((x >= 1) && (x <= 9))) {
+    for (int i = 1; i <= x; i++) {
+      console::write_line("Beep number %d.", i);
+      console::beep();
+    }
+  } else
+    console::write_line("Usage: Enter the number of times (between 1 and 9) to beep.");
+}
