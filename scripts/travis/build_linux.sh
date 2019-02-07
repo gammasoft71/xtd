cbe168fe272338618adceb1bc801edd6a7826b03
Gammasoft71 <gammasoft71@gmail.com>
2019-02-07 16:29:42 +0100

    remove pushd

--- scripts/travis/build_linux.sh
+++ scripts/travis/build_linux.sh
@@ -2,14 +2,14 @@
 
 # generate and build console
 mkdir -p build
-pushd build
+cd build
 cmake .. -DCMAKE_BUILD_TYPE=Debug
 cmake --build . -- -j8
 if [ $? -ne 0 ]; then exit -1; fi
-popd
+cd ..
 
 # run registered unit tests
-pushd build
+#cd build
 #ctest --output-on-failure --build-config Debug
 #if [ $? -ne 0 ]; then exit -1; fi
-popd
+#cd ..
