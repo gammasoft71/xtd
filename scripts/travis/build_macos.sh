7756700dbcae398182e33e255b0cd63712e9c30b
Gammasoft71 <gammasoft71@gmail.com>
2019-02-07 16:09:07 +0100

    add travis scripts

---
+++ scripts/travis/build_macos.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+# generate and build Switch
+pushd build
+cmake .. -DCMAKE_BUILD_TYPE=Debug
+cmake --build . -- -j8
+if [ $? -ne 0 ]; then exit -1; fi
+popd
+
+# run registered unit tests
+pushd build
+#ctest --output-on-failure --build-config Debug
+#if [ $? -ne 0 ]; then exit -1; fi
+popd
