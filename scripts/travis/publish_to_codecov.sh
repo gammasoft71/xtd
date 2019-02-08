f163379398534cc12a117b3099e67ac632a29652
Gammasoft71 <gammasoft71@gmail.com>
2019-02-08 15:02:23 +0100

    review add codecov

--- scripts/travis/publish_to_codecov.sh
+++ scripts/travis/publish_to_codecov.sh
@@ -4,4 +4,4 @@ set -ev
 
 # publish to codecov
 export CODECOV_TOKEN="8dfddc0a-d627-4211-b69b-f82557d3145a"
-bash <(curl -s https://codecov.io/bash)
+bash <(curl -s https://codecov.io/bash) || echo "Codecov did not collect coverage reports"
