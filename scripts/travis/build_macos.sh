356a13b4af11582c6e470811cc09bbb67080f598
Gammasoft71 <gammasoft71@gmail.com>
2019-02-10 20:57:17 +0100

    run coverage on linux

--- scripts/travis/build_macos.sh
+++ scripts/travis/build_macos.sh
@@ -4,14 +4,14 @@ mkdir -p build/examples
 pushd build
 
 # generate and build lib
-cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=~/local
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=~/local ..
 if [ $? -ne 0 ]; then exit -1; fi
 cmake --build . --target install -- -j $(nproc)
 if [ $? -ne 0 ]; then exit -1; fi
 
 # generate and build examples
 pushd examples
-cmake ../../examples -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=~/local
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=~/local ../../examples
 if [ $? -ne 0 ]; then exit -1; fi
 cmake --build . -- -j $(nproc)
 if [ $? -ne 0 ]; then exit -1; fi
