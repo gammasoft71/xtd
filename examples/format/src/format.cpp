bbae6b7343a3ccac0b0170429c70e4c5cdb5c5f7
Gammasoft71 <gammasoft71@gmail.com>
2019-02-12 23:45:00 +0100

    add split and format examples

---
+++ examples/format/src/format.cpp
@@ -0,0 +1,15 @@
+#include <xtd/strings>
+#include <iostream>
+
+using namespace std;
+using namespace string_literals;
+using namespace xtd;
+
+// The main entry point for the application.
+int main() {
+  cout << strings::format("%10s %s %03d %c", "string", "literal"s, 42, 'a') << endl;
+}
+
+// This code produces the following output with colors:
+//
+//     string literal 042 a
