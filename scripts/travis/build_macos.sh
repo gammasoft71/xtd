65c8f1e8ed28dbed282cc997245001b1b3c0d609
Gammasoft71 <gammasoft71@gmail.com>
2019-02-13 23:05:11 +0100

    first commit

---
+++ scripts/travis/build_macos.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+mkdir -p build/examples
+pushd build
+
+# generate and build lib
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=~/local ..
+if [ $? -ne 0 ]; then exit -1; fi
+cmake --build . --target install -- -j $(nproc)
+if [ $? -ne 0 ]; then exit -1; fi
+
+# generate and build examples
+pushd examples
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=~/local ../../examples
+if [ $? -ne 0 ]; then exit -1; fi
+cmake --build . -- -j $(nproc)
+if [ $? -ne 0 ]; then exit -1; fi
+popd
+
+# run registered unit tests
+ctest -j $(nproc) --output-on-failure --build-config Debug
+if [ $? -ne 0 ]; then exit -1; fi
+
+popd
