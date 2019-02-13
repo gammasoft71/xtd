298fb6570d051ac06db011be9cd663a8cee30c0a
Gammasoft71 <gammasoft71@gmail.com>
2019-02-14 00:49:11 +0100

    rename to unit_test

---
+++ xtd.tunit/include/xtd/unit_test.hpp
@@ -0,0 +1,19 @@
+/// @file
+/// @brief Contains xtd::unit_test class.
+#pragma once
+
+#include "registered_method.hpp" /// @todo remove include
+
+/// @brief The xtd namespace contains all fundamental classes to access console.
+namespace xtd {
+  /// @brief The TUnit namespace contains a unit test framework.
+  namespace tunit {
+    /// @brief The template class.
+    class unit_test {
+    public:
+      /// @cond
+      unit_test() = delete;
+      /// @endcond
+    };
+  }
+}
