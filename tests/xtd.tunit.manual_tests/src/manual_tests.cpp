9b81722bb8ad959ba12a1b6bacae9962a06b3d72
Gammasoft71 <gammasoft71@gmail.com>
2019-02-14 20:53:35 +0100

    remove cout

--- tests/xtd.tunit.manual_tests/src/manual_tests.cpp
+++ tests/xtd.tunit.manual_tests/src/manual_tests.cpp
@@ -10,39 +10,31 @@ namespace unit_tests {
   public:
     // This is the method that is called 1 time before test class run.
     static void class_initialize_(class_initialize) {
-      cout << line_info_ << endl;
     }
     
     // This is the method that is called 1 time after test class run.
     static void class_cleanup_(class_cleanup) {
-      cout << line_info_ << endl;
     }
     
     // This is the method that is called before any tests in a fixture are run.
     static void test_initialize_(test_initialize) {
-      cout << line_info_ << endl;
     }
     
     // This is the method that is called after any tests in a fixture are run.
     static void test_cleanup_(test_cleanup) {
-      cout << line_info_ << endl;
     }
     
     void test_method_(test_case1) {
-      cout << line_info_ << endl;
     }
     
     void test_method_(test_case2) {
-      cout << line_info_ << endl;
     }
     
     void ignore_test_method_(test_case3) {
-      cout << line_info_ << endl;
     }
   };
 }
 
 int main(int argc, char* argv[]) {
-  cout << line_info_ << endl;
   return unit_test(argv, argc).run();
 }
