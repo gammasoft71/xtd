eec57d9890aee9773f76bbb2648335886ea5da23
Gammasoft71 <gammasoft71@gmail.com>
2019-02-14 11:30:09 +0100

    Create test_class_attribute.hpp

---
+++ xtd.tunit/include/xtd/test_class_attribute.hpp
@@ -0,0 +1,18 @@
+/// @file
+/// @brief Contains xtd::tunit::test_state enum class.
+#pragma once
+#include "unit_test.hpp"
+#include <memory>
+#include <string>
+
+/// @brief The xtd namespace contains all fundamental classes to access console.
+namespace xtd {
+  /// @brief The tunit namespace contains a unit test framework.
+  namespace tunit {
+    template <typename TestClass>
+    struct test_class_attribute {
+    public:
+      test_class_attribute(const std::string& name) {xtd::tunit::unit_test::add({name, std::make_unique<TestClass>()});}
+    };
+  }
+}
