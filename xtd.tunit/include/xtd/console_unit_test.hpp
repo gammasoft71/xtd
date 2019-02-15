9a2358670930775391cb4db2965a3b7046a908cc
Gammasoft71 <gammasoft71@gmail.com>
2019-02-15 09:52:37 +0100

    add ostream_unit_test class

--- xtd.tunit/include/xtd/console_unit_test.hpp
+++ xtd.tunit/include/xtd/console_unit_test.hpp
@@ -2,6 +2,7 @@
 /// @brief Contains xtd::console_unit_test class.
 #pragma once
 #include "unit_test.hpp"
+#include "ostream_unit_test.hpp"
 #include <iostream>
 
 /// @brief The xtd namespace contains all fundamental classes to access console.
@@ -9,31 +10,11 @@ namespace xtd {
   /// @brief The TUnit namespace contains a unit test framework.
   namespace tunit {
     /// @brief The console_unit_test class.
-    class console_unit_test : public unit_test {
+    class console_unit_test : public ostream_unit_test {
     public:
-      console_unit_test() = default;
+      console_unit_test() : ostream_unit_test(std::cout) {}
       
-      console_unit_test(char* argv[], int argc) : unit_test(argv, argc) {}
-      
-      void on_class_end(const xtd::tunit::class_event_args& e) const override {
-        this->unit_test::on_unit_test_end(e);
-        std::cout << "  End " << e.test_class().test_count() << " test" << (e.test_class().test_count() < 2 ? "" : "s") << " from " << e.test_class().name() << " (" << std::chrono::duration_cast<std::chrono::milliseconds>(e.test_class().elapsed_time()).count() << " ms total) " << std::endl;
-      }
-      
-      void on_class_start(const xtd::tunit::class_event_args& e) const override {
-        std::cout << "  Start " << e.test_class().test_count() << " test" << (e.test_class().test_count() < 2 ? "" : "s") << " from " << e.test_class().name() << std::endl;
-        this->unit_test::on_unit_test_start(e);
-      }
-      
-      void on_unit_test_end(const xtd::event_args& e) const override {
-        this->unit_test::on_unit_test_end(e);
-        std::cout << "End " << this->test_count() << " test" << (this->test_count() < 2 ? "" : "s") << " from " << this->test_cases_count() << " test case" << (this->test_cases_count() < 2 ? "" : "s") << " ran. (" << std::chrono::duration_cast<std::chrono::milliseconds>(this->elapsed_time()).count() << " ms total)" << std::endl;
-      }
-      
-      void on_unit_test_start(const xtd::event_args& e) const override {
-        std::cout << "Start " << this->test_count() << " test" << (this->test_count() < 2 ? "" : "s") << " from " << this->test_cases_count() << " test case" << (this->test_cases_count() < 2 ? "" : "s") << std::endl;
-        this->unit_test::on_unit_test_start(e);
-      }
+      console_unit_test(char* argv[], int argc) : ostream_unit_test(std::cout, argv, argc) {}
     };
   }
 }
---
+++ xtd.tunit/include/xtd/ostream_unit_test.hpp
@@ -0,0 +1,42 @@
+/// @file
+/// @brief Contains xtd::ostream_unit_test class.
+#pragma once
+#include "unit_test.hpp"
+#include <ostream>
+
+/// @brief The xtd namespace contains all fundamental classes to access console.
+namespace xtd {
+  /// @brief The TUnit namespace contains a unit test framework.
+  namespace tunit {
+    /// @brief The ostream_unit_test class.
+    class ostream_unit_test : public unit_test {
+    public:
+      ostream_unit_test(std::ostream& os) noexcept : os_(os) {}
+      
+      ostream_unit_test(std::ostream& os, char* argv[], int argc) noexcept : os_(os), unit_test(argv, argc) {}
+      
+      void on_class_end(const xtd::tunit::class_event_args& e) const override {
+        this->unit_test::on_unit_test_end(e);
+        this->os_ << "  End " << e.test_class().test_count() << " test" << (e.test_class().test_count() < 2 ? "" : "s") << " from " << e.test_class().name() << " (" << std::chrono::duration_cast<std::chrono::milliseconds>(e.test_class().elapsed_time()).count() << " ms total) " << std::endl;
+      }
+      
+      void on_class_start(const xtd::tunit::class_event_args& e) const override {
+        this->os_ << "  Start " << e.test_class().test_count() << " test" << (e.test_class().test_count() < 2 ? "" : "s") << " from " << e.test_class().name() << std::endl;
+        this->unit_test::on_unit_test_start(e);
+      }
+      
+      void on_unit_test_end(const xtd::event_args& e) const override {
+        this->unit_test::on_unit_test_end(e);
+        this->os_ << "End " << this->test_count() << " test" << (this->test_count() < 2 ? "" : "s") << " from " << this->test_cases_count() << " test case" << (this->test_cases_count() < 2 ? "" : "s") << " ran. (" << std::chrono::duration_cast<std::chrono::milliseconds>(this->elapsed_time()).count() << " ms total)" << std::endl;
+      }
+      
+      void on_unit_test_start(const xtd::event_args& e) const override {
+        this->os_ << "Start " << this->test_count() << " test" << (this->test_count() < 2 ? "" : "s") << " from " << this->test_cases_count() << " test case" << (this->test_cases_count() < 2 ? "" : "s") << std::endl;
+        this->unit_test::on_unit_test_start(e);
+      }
+      
+    private:
+      std::ostream& os_;
+    };
+  }
+}
