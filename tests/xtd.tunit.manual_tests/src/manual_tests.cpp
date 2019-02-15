a856acf3d53973d6acc5b8b6aa4c8e336e632baa
Gammasoft71 <gammasoft71@gmail.com>
2019-02-16 00:03:11 +0100

    add docs and add default ostream operator <<

--- tests/xtd.tunit.manual_tests/src/manual_tests.cpp
+++ tests/xtd.tunit.manual_tests/src/manual_tests.cpp
@@ -1,48 +1,100 @@
 #include <xtd/tunit>
-#include <iostream>
-#include <stdexcept>
-#include <thread>
+#include <string>
+#include <tuple>
+#include <regex>
 
 using namespace std;
+using namespace std::string_literals;
 using namespace xtd;
 using namespace xtd::tunit;
 
+class version final {
+public:
+  version() = default;
+  version(int major, int minor) : major_(major), minor_(minor) {}
+  version(int major, int minor, int build) : major_(major), minor_(minor), build_(build) {}
+  version(int major, int minor, int build, int revision) : major_(major), minor_(minor), build_(build), revision_(revision) {}
+  version(const version&) = default;
+  version& operator=(const version&) = default;
+  
+  bool operator==(const version& v) const {return this->major_ == v.major_ && this->minor_ == v.minor_ && this->build_ == v.build_ && this->revision_ == v.revision_;}
+  bool operator!=(const version& v) const {return this->operator==(v);}
+  
+  int build() const {return this->build_;}
+  int major() const {return this->major_;}
+  int16_t major_revision() const {return (int16_t)((this->revision_ & 0xFFFF0000) >> 16);}
+  int minor() const {return this->minor_;}
+  int16_t mainor_revision() const {return (int16_t)(this->revision_ & 0x0000FFFF);}
+  int revision() const {return this->revision_;}
+  
+  static version parse(const std::string& ver) {
+    std::regex rgx("\\.");
+    std::vector<std::string> versions;
+    for (std::sregex_token_iterator it(ver.begin(), ver.end(), rgx, -1), end; it != end; ++it)
+      versions.push_back(*it);
+    
+    switch (versions.size()) {
+      case 2: return version(std::stoi(versions[0]), std::stoi(versions[1]));
+      case 3: return version(std::stoi(versions[0]), std::stoi(versions[1]), std::stoi(versions[2]));
+      case 4: return version(std::stoi(versions[0]), std::stoi(versions[1]), std::stoi(versions[2]), std::stoi(versions[3]));
+    }
+    throw std::invalid_argument("bad format");
+  }
+  
+  static bool try_parse(const std::string& ver, version& result) {
+    try {
+      result = parse(ver);
+      return true;
+    } catch (...) {
+      return false;
+    }
+  }
+  
+  std::string to_string() const {
+    return this->to_string(2 + (this->build_ != -1 ? 1 : 0) + (this->revision_ != -1 ? 1 : 0));;
+  }
+  
+  std::string to_string(size_t field_count) const {
+    if (field_count > 4 || (field_count >= 3 && this->build_ == -1) || (field_count == 4 && this->revision_ == -1))
+      throw std::invalid_argument("field_count invalid");
+    std::stringstream result;
+    if (field_count >= 1)
+      result << std::to_string(this->major_);
+    if (field_count >= 2)
+      result << "." << std::to_string(this->minor_);
+    if (field_count >= 3)
+      result << "." << std::to_string(this->build_);
+    if (field_count == 4)
+      result << "." << std::to_string(this->revision_);
+    return result.str();
+  }
+  
+  friend std::ostream& operator <<(std::ostream& os, const version& ver) {
+    return os << ver.to_string();
+  }
+  
+private:
+  int major_ = 0;
+  int minor_ = 0;
+  int build_ = -1;
+  int revision_ = -1;
+};
+
 namespace unit_tests {
-  // The class test must be declared with test_class_ helper.
   class test_class_(version_test) {
   public:
     void test_method_(test_case1) {
-      string s = "Hello, World!";
-      assert::are_equal("Hello, World!", s);
+      version v(1, 2);
+      assert::are_equal(version {1, 1}, v);
     }
-    
+
     void test_method_(test_case2) {
-      bool b = true;
-      assert::are_equal(true, b);
-    }
-    
-    void ignore_test_method_(test_case3) {
-      int i = 42;
-      assert::are_equal(42, i);
+      auto t = std::make_tuple("a"s, 2);
+      assert::are_equal(std::tuple<std::string, int>("a"s, 1), t);
     }
   };
-
-  // The class test must be declared with test_class_ helper.
-  class test_class_(uri_test) {
-  public:
-    void test_method_(test_case1) {
-      bool b = true;
-      assert::are_equal_(false, b);
-    }
-  };
-}
-
-void dump() {
-  throw std::invalid_argument("");
 }
 
 int main(int argc, char* argv[]) {
-  console_unit_test unit_test(argv, argc);
-  settings::default_settings().also_run_ignored_tests(true);
-  return unit_test.run();
+  return console_unit_test(argv, argc).run();
 }
--- xtd.tunit/include/xtd/assert.hpp
+++ xtd.tunit/include/xtd/assert.hpp
@@ -5,29 +5,83 @@
 #include "line_info.hpp"
 #include <string>
 #include <sstream>
+#include <iomanip>
+#include <iostream>
 
-#define assert_ assert
+/// @cond
+#ifdef assert
 #undef assert
+#endif
+
+template <typename Char, typename CharTraits, typename Type>
+std::basic_ostream<Char, CharTraits>& operator<<(std::basic_ostream<Char, CharTraits>& os, const Type& value) {
+  size_t size = sizeof(value) > 32 ? 32 : sizeof(value);
+  os << size << "-byte object <";
+  for (size_t index = 0; index != size; index++)
+    os <<  (index != 0 ? (index % 2 == 0 ? " " : "-") : "") << std::hex << std::setiosflags(std::ios_base::uppercase) << std::setw(2) << std::setfill('0') << static_cast<int>(reinterpret_cast<const unsigned char*>(&value)[index]) << std::resetiosflags(std::ios_base::dec) << std::dec;
+  return os << (size < sizeof(value) ? "-..." : "") << ">";
+}
+/// @endcond
 
 /// @brief The xtd namespace contains all fundamental classes to access console.
 namespace xtd {
   /// @brief The tunit namespace contains a unit test framework.
   namespace tunit {
+    /// @brief The assert class contains a collection of static methods that implement the most common assertions used in xtd::tUnit.
+    /// @par Examples
+    /// This example show how to used some methods :
+    /// @include assert.cpp
     class assert final {
     public:
       /// @cond
       assert() = delete;
       /// @endcond
       
+      /// @brief Verifies that two type are equal. If they are not, then a xtd::tunit::assertion_error excpetion is thrown.
+      /// @param expected the expected value.
+      /// @param actual the actual value.
+      /// @par Examples
+      /// @code
+      /// xtd::tunit::assert::are_aqual(24, int(24)); // test ok
+      /// xtd::tunit::assert::are_equal(23, int(24)); // test throws an assertion_error exception.
+      /// @endcode
       template<typename TExpected, typename TActual>
       static void are_equal(const TExpected& expected, const TActual& actual) {are_equal(expected, actual, "", line_info());}
       
+      /// @brief Verifies that two type are equal. If they are not, then a xtd::tunit::assertion_error excpetion is thrown.
+      /// @param expected the expected value.
+      /// @param actual the actual value.
+      /// @param line_info Contains information about current file and current line.
+      /// @par Examples
+      /// @code
+      /// xtd::tunit::assert::are_aqual(24, int(24), line_info_); // test ok
+      /// xtd::tunit::assert::are_equal(23, int(24), line_info_); // test throws an assertion_error exception.
+      /// @endcode
       template<typename TExpected, typename TActual>
       static void are_equal(const TExpected& expected, const TActual& actual, const xtd::tunit::line_info& line_info) {are_equal(expected, actual, "", line_info);}
       
+      /// @brief Verifies that two type are equal. If they are not, then a xtd::tunit::assertion_error excpetion is thrown.
+      /// @param expected the expected value.
+      /// @param actual the actual value.
+      /// @param message A message to display if the assertion fails. This message can be seen in the unit test results.
+      /// @par Examples
+      /// @code
+      /// xtd::tunit::assert::are_aqual(24, int(24), "User message..."); // test ok
+      /// xtd::tunit::assert::are_equal(23, int(24), "User message..."); // test throws an assertion_error exception.
+      /// @endcode
       template<typename TExpected, typename TActual>
       static void are_equal(const TExpected& expected, const TActual& actual, const std::string& message) {are_equal(expected, actual, message, line_info());}
       
+      /// @brief Verifies that two type are equal. If they are not, then a xtd::tunit::assertion_error excpetion is thrown.
+      /// @param expected the expected value.
+      /// @param actual the actual value.
+      /// @param message A message to display if the assertion fails. This message can be seen in the unit test results.
+      /// @param line_info Contains information about current file and current line.
+      /// @par Examples
+      /// @code
+      /// xtd::tunit::assert::are_aqual(24, int(24), "User message...", line_info_); // test ok
+      /// xtd::tunit::assert::are_equal(23, int(24), "User message...", line_info_); // test throws an assertion_error exception.
+      /// @endcode
       template<typename TExpected, typename TActual>
       static void are_equal(const TExpected& expected, const TActual& actual, const std::string& message, const xtd::tunit::line_info& line_info) {
         if (actual == expected)
