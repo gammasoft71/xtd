5df04918a5d9570e23ad977218bb4b4bba624ab4
Gammasoft71 <gammasoft71@gmail.com>
2019-02-17 23:05:16 +0100

    add docs

--- xtd.tunit/include/xtd/class_cleanup_attribute.hpp
+++ xtd.tunit/include/xtd/class_cleanup_attribute.hpp
@@ -7,13 +7,23 @@
 namespace xtd {
   /// @brief The tunit namespace contains a unit test framework.
   namespace tunit {
+    /// @brief This attribute is use to add cleaup class method to class cleanup attribute.
     struct class_cleanup_attribute {
     public:
+      /// @brief Crrates new instance of class_cleanup_attribute attribute.
+      /// @param name Name of attribute
+      /// @param test_class xtd::tunit::class_test containing clean_up method.
+      /// @param method Cleanup class method.
       template<typename TestClass>
       class_cleanup_attribute(const std::string& name, TestClass& test_class, void (*method)()) noexcept :  class_cleanup_attribute(name, test_class, method, xtd::tunit::line_info()) {}
       
+      /// @brief Crrates new instance of class_cleanup_attribute attribute.
+      /// @param name Name of attribute
+      /// @param test_class xtd::tunit::class_test containing clean_up method.
+      /// @param method Cleanup class method.
+      /// @param line_info Contains information about current file and current line.
       template<typename TestClass>
-      class_cleanup_attribute(const std::string& name, TestClass& test_class, void (*method)(), const xtd::tunit::line_info& caller) noexcept {test_class.add_class_cleanup({name, method, caller});}
+      class_cleanup_attribute(const std::string& name, TestClass& test_class, void (*method)(), const xtd::tunit::line_info& line_info) noexcept {test_class.add_class_cleanup({name, method, line_info});}
     };
   }
 }
