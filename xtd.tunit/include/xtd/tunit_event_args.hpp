55005232d7c78d00aa7d2973f8b0fdb891f00486
Gammasoft71 <gammasoft71@gmail.com>
2019-02-17 22:49:25 +0100

    Update tunit_event_args.hpp

--- xtd.tunit/include/xtd/tunit_event_args.hpp
+++ xtd.tunit/include/xtd/tunit_event_args.hpp
@@ -6,9 +6,19 @@
 namespace xtd {
   /// @brief The tunit namespace contains a unit test framework.
   namespace tunit {
+    /// @brief tunit_event_args is the base class for classes containing event data.
     class tunit_event_args {
     public:
+      /// @brief Create a new instance of class tunit_event_args
       tunit_event_args() = default;
+      
+      /// @cond
+      tunit_event_args(const tunit_event_args&) = default;
+      tunit_event_args& operator=(const tunit_event_args&) = default;
+      virtual ~tunit_event_args() = default;
+      /// @endcond
+
+      /// @brief Represents an event with no event data.
       static tunit_event_args empty() { return tunit_event_args(); }
     };
   }
