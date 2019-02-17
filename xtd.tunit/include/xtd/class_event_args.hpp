c78c4f8b41f83428b6baa5e81cea6863dd04f41d
Gammasoft71 <gammasoft71@gmail.com>
2019-02-17 22:59:10 +0100

    add docs

--- xtd.tunit/include/xtd/class_event_args.hpp
+++ xtd.tunit/include/xtd/class_event_args.hpp
@@ -8,10 +8,15 @@
 namespace xtd {
   /// @brief The tunit namespace contains a unit test framework.
   namespace tunit {
+    /// @brief Provides data for the xtd::tunit::class_test events.
     class class_event_args : public xtd::tunit::tunit_event_args {
     public:
-      explicit class_event_args(const xtd::tunit::test_class& test_class) : tc_(test_class) {}
+      /// @brief Create a new instance of class class_event_args.
+      /// @param test_class Current class_test.
+     explicit class_event_args(const xtd::tunit::test_class& test_class) : tc_(test_class) {}
 
+      /// @brief Gets current class test.
+      /// @return Current class test.
       const xtd::tunit::test_class& test_class() const noexcept {return this->tc_;}
 
     private:
--- xtd.tunit/include/xtd/test_event_args.hpp
+++ xtd.tunit/include/xtd/test_event_args.hpp
@@ -9,11 +9,20 @@
 namespace xtd {
   /// @brief The tunit namespace contains a unit test framework.
   namespace tunit {
+    /// @brief Provides data for the xtd::tunit::test events.
     class test_event_args : public xtd::tunit::tunit_event_args {
     public:
+      /// @brief Create a new instance of class class_event_args.
+      /// @param test Current testt.
+      /// @param test_class Current test_class.
       explicit test_event_args(const xtd::tunit::test& test, const xtd::tunit::test_class& test_class) : t_(test), tc_(test_class) {}
 
+      /// @brief Gets current test.
+      /// @return Current test.
       const xtd::tunit::test& test() const noexcept {return this->t_;}
+
+      /// @brief Gets current class test.
+      /// @return Current class test.
       const xtd::tunit::test_class& test_class() const noexcept {return this->tc_;}
 
     private:
