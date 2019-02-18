3c6610b073219961fc828e872c54497a16fdf225
Gammasoft71 <gammasoft71@gmail.com>
2019-02-18 15:50:27 +0100

    add one_test_two_test_cases_and_one_ignore_test_case test

--- tests/xtd.tunit.unit_tests/src/one_test_one_test_case.cpp
+++ tests/xtd.tunit.unit_tests/src/one_test_one_test_case.cpp
@@ -12,21 +12,23 @@ namespace unit_tests {
 void test_(one_test_one_test_case, test_output) {
   std::stringstream ss;
   xtd::tunit::settings::default_settings().filter_tests("one_test_one_test_case.*");
+  xtd::tunit::settings::default_settings().show_duration(false);
   xtd::tunit::ostream_unit_test(ss, argv, argc).run();
   assert_value_("Start 1 test from 1 test case\n"
                 "  Start 1 test from one_test_one_test_case\n"
-                "    PASSED test_case1 (0 ms total)\n"
-                "  End 1 test from one_test_one_test_case (0 ms total)\n"
+                "    PASSED test_case1\n"
+                "  End 1 test from one_test_one_test_case\n"
                 "\n"
                 "  Summary :\n"
                 "    PASSED 1 tests.\n"
-                "End 1 test from 1 test case ran. (0 ms total)\n"
+                "End 1 test from 1 test case ran.\n"
                 "\n", ss.str());
 }
 
 void test_(one_test_one_test_case, test_result) {
   std::stringstream ss;
   xtd::tunit::settings::default_settings().filter_tests("one_test_one_test_case.*");
+  xtd::tunit::settings::default_settings().show_duration(false);
   int result = xtd::tunit::ostream_unit_test(ss).run();
   assert_value_(0, result);
 }
--- tests/xtd.tunit.unit_tests/src/one_test_two_test_cases_and_one_ignore_test_case.cpp
+++ tests/xtd.tunit.unit_tests/src/one_test_two_test_cases_and_one_ignore_test_case.cpp
@@ -3,35 +3,39 @@
 
 
 namespace unit_tests {
-  class test_class_(one_test_two_test_cases) {
+  class test_class_(one_test_two_test_cases_and_one_ignore_test_case) {
   public:
     void test_method_(test_case1) {
     }
     void test_method_(test_case2) {
     }
+    void ignore_test_method_(test_case3) {
+    }
   };
 }
 
-void test_(one_test_two_test_cases, test_output) {
+void test_(one_test_two_test_cases_and_one_ignore_test_case, test_output) {
   std::stringstream ss;
-  xtd::tunit::settings::default_settings().filter_tests("one_test_two_test_cases.*");
+  xtd::tunit::settings::default_settings().filter_tests("one_test_two_test_cases_and_one_ignore_test_case.*");
   xtd::tunit::settings::default_settings().show_duration(false);
   xtd::tunit::ostream_unit_test(ss, argv, argc).run();
   assert_value_("Start 2 tests from 1 test case\n"
-                "  Start 2 tests from one_test_two_test_cases\n"
+                "  Start 2 tests from one_test_two_test_cases_and_one_ignore_test_case\n"
                 "    PASSED test_case1\n"
                 "    PASSED test_case2\n"
-                "  End 2 tests from one_test_two_test_cases\n"
+                "  End 2 tests from one_test_two_test_cases_and_one_ignore_test_case\n"
                 "\n"
                 "  Summary :\n"
                 "    PASSED 2 tests.\n"
                 "End 2 tests from 1 test case ran.\n"
+                "\n"
+                "  You have 1 ignored test\n"
                 "\n", ss.str());
 }
 
-void test_(one_test_two_test_cases, test_result) {
+void test_(one_test_two_test_cases_and_one_ignore_test_case, test_result) {
   std::stringstream ss;
-  xtd::tunit::settings::default_settings().filter_tests("one_test_two_test_cases.*");
+  xtd::tunit::settings::default_settings().filter_tests("one_test_two_test_cases_and_one_ignore_test_case.*");
   xtd::tunit::settings::default_settings().show_duration(false);
   int result = xtd::tunit::ostream_unit_test(ss).run();
   assert_value_(0, result);
