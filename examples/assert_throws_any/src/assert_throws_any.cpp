588d6c29cf7f38655052c42ea948729cffc84901
Gammasoft71 <gammasoft71@gmail.com>
2019-02-19 13:33:08 +0100

    add assert_throws_any example

---
+++ examples/assert_throws_any/src/assert_throws_any.cpp
@@ -0,0 +1,46 @@
+#include <xtd/tunit>
+#include <vector>
+
+using namespace std;
+using namespace xtd::tunit;
+
+namespace unit_tests {
+  class test_class_(test) {
+  public:
+    void test_method_(test_case_succeed) {
+      std::vector v = {1, 2, 3, 4};
+      xtd::tunit::assert::throws_any_([&] {v.at(5);});
+    }
+    
+    void test_method_(test_case_failed) {
+      std::vector v = {1, 2, 3, 4};
+      xtd::tunit::assert::throws_any_([&] {v.at(2);});
+    }
+  };
+}
+
+// The main entry point for the application.
+int main(int argc, char* argv[]) {
+  return console_unit_test(argv, argc).run();
+}
+
+// This code produces the following output:
+//
+// Start 2 tests from 1 test case
+//   Start 2 tests from test
+//     PASSED test_case_succeed (0 ms total)
+//     FAILED test_case_failed (4 ms total)
+//
+// Expected: <std::out_of_range>
+// But was:  <nothing>
+// error: !---OMITTED---!/xtd/xtd.tunit/examples/assert_does_not_throw/src/assert_throws_any.cpp:17
+//
+//   End 2 tests from test (4 ms total)
+//
+//   Summary :
+//     PASSED 1 test.
+// *** FAILED 1 test, listed below:
+// *** FAILED test.test_case_failed
+//
+//     FAILED 1 test.
+// End 2 tests from 1 test case ran. (4 ms total)
