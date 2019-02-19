94c28b25543da28f0d755f7ee6e9250590f2682c
Gammasoft71 <gammasoft71@gmail.com>
2019-02-19 08:36:48 +0100

    add test_assert_contains_collection tests

---
+++ tests/xtd.tunit.unit_tests/src/test_assert_contains_collection_failed.cpp
@@ -0,0 +1,48 @@
+#include "assert_unit_tests.hpp"
+#include <xtd/tunit>
+#include <vector>
+
+namespace unit_tests {
+  class test_class_(test_assert_contains_collection_failed) {
+  public:
+    void test_method_(test_case_failed) {
+      std::vector v = {0, 1, 2, 3};
+      xtd::tunit::assert::contains(4, v, xtd::tunit::line_info {"test_assert_contains_collection_failed.cpp", 10});
+    }
+  };
+}
+
+void test_(test_assert_contains_collection_failed, test_output) {
+  std::stringstream ss;
+  xtd::tunit::settings::default_settings().exit_status(0);
+  xtd::tunit::settings::default_settings().filter_tests("test_assert_contains_collection_failed.*");
+  xtd::tunit::settings::default_settings().show_duration(false);
+  xtd::tunit::ostream_unit_test(ss, argv, argc).run();
+  assert_value_("Start 1 test from 1 test case\n"
+                "  Start 1 test from test_assert_contains_collection_failed\n"
+                "    FAILED test_case_failed\n"
+                "\n"
+                "Expected: collection containing 4\n"
+                "But was:  < 0, 1, 2, 3 >\n"
+                "error: test_assert_contains_collection_failed.cpp:10\n"
+                "\n"
+                "  End 1 test from test_assert_contains_collection_failed\n"
+                "\n"
+                "  Summary :\n"
+                "    PASSED 0 test.\n"
+                "*** FAILED 1 test, listed below:\n"
+                "*** FAILED test_assert_contains_collection_failed.test_case_failed\n"
+                "\n"
+                "    FAILED 1 test.\n"
+                "End 1 test from 1 test case ran.\n"
+                "\n", ss.str());
+}
+
+void test_(test_assert_contains_collection_failed, test_result) {
+  std::stringstream ss;
+  xtd::tunit::settings::default_settings().exit_status(0);
+  xtd::tunit::settings::default_settings().filter_tests("test_assert_contains_collection_failed.*");
+  xtd::tunit::settings::default_settings().show_duration(false);
+  int result = xtd::tunit::ostream_unit_test(ss).run();
+  assert_value_(1, result);
+}
