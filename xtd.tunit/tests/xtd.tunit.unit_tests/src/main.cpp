97b8126a705c27ce2da158f57c4f8593e0946f6a
Gammasoft71 <gammasoft71@gmail.com>
2019-02-19 22:27:14 +0100

    remove xtd.tunit

--- xtd.tunit/tests/xtd.tunit.unit_tests/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "assert_unit_tests.hpp"
-#include <xtd/tunit>
-
-std::vector<assert_unit_tests::register_assert_unit_test> assert_unit_tests::register_assert_unit_test::assert_unit_tests;
-
-int main(int argc, char* argv[]) {
-  return assert_unit_tests::register_assert_unit_test::run_all_tests(argc, argv);
-}
-
