aa6981950744f5feefe4c0e4e1940d2e267edfd0
Gammasoft71 <gammasoft71@gmail.com>
2019-02-19 12:26:31 +0100

    use helper

--- examples/assert_are_same/src/assert_are_same.cpp
+++ examples/assert_are_same/src/assert_are_same.cpp
@@ -8,13 +8,13 @@ namespace unit_tests {
     void test_method_(test_case_succeed) {
       int a = 24;
       int& b = a;
-      xtd::tunit::assert::are_same(b, a);
+      xtd::tunit::assert::are_same_(b, a);
     }
 
     void test_method_(test_case_failed) {
       int a = 24;
       int b =  24;
-      xtd::tunit::assert::are_same(b, a);
+      xtd::tunit::assert::are_same_(b, a);
     }
   };
 }
@@ -33,15 +33,15 @@ int main(int argc, char* argv[]) {
 //
 // Expected: same as 24
 // But was:  24
-// error: !---OMITTED---!/xtd/xtd.tunit/examples/assert_are_not_equal/src/assert_are_same.cpp:14
+// error: !---OMITTED---!/xtd/xtd.tunit/examples/assert_are_not_equal/src/assert_are_same.cpp:17
 //
 //   End 2 tests from test (0 ms total)
 //
 //   Summary :
-//     PASSED 1 tests.
+//     PASSED 1 test.
 // *** FAILED 1 test, listed below:
 // *** FAILED test.test_case_failed
 //
-//     FAILED 1 tests.
+//     FAILED 1 test.
 // End 2 tests from 1 test case ran. (0 ms total)
 
