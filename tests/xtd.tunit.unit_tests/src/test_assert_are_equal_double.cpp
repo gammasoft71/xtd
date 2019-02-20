036f72670ecfa579d7da10f7ec38da7680856b8c
Gammasoft71 <gammasoft71@gmail.com>
2019-02-20 17:15:01 +0100

    add test_assert_are_equal_double

---
+++ tests/xtd.tunit.unit_tests/src/test_assert_are_equal_double.cpp
@@ -0,0 +1,38 @@
+#include "assert_unit_tests.hpp"
+#include <xtd/tunit>
+
+namespace unit_tests {
+  class test_class_(test_assert_are_equal_double) {
+  public:
+    void test_method_(test_case_succeed) {
+      double d = 0.2;
+      xtd::tunit::assert::are_equal(0.2, d, xtd::tunit::line_info {"test_assert_are_equal_double.cpp", 9});
+    }
+  };
+}
+
+void test_(test_assert_are_equal_double, test_output) {
+  std::stringstream ss;
+  xtd::tunit::settings::default_settings().exit_status(0);
+  xtd::tunit::settings::default_settings().filter_tests("test_assert_are_equal_double.*");
+  xtd::tunit::settings::default_settings().show_duration(false);
+  xtd::tunit::ostream_unit_test(ss, argv, argc).run();
+  assert_value_("Start 1 test from 1 test case\n"
+                "  Start 1 test from test_assert_are_equal_double\n"
+                "    PASSED test_case_succeed\n"
+                "  End 1 test from test_assert_are_equal_double\n"
+                "\n"
+                "  Summary :\n"
+                "    PASSED 1 test.\n"
+                "End 1 test from 1 test case ran.\n"
+                "\n", ss.str());
+}
+
+void test_(test_assert_are_equal_double, test_result) {
+  std::stringstream ss;
+  xtd::tunit::settings::default_settings().exit_status(0);
+  xtd::tunit::settings::default_settings().filter_tests("test_assert_are_equal_double.*");
+  xtd::tunit::settings::default_settings().show_duration(false);
+  int result = xtd::tunit::ostream_unit_test(ss).run();
+  assert_value_(0, result);
+}
