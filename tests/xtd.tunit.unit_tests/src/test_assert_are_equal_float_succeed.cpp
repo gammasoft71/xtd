b16deccd329adcc9fd7e2de233e02d8ff3bf9ff5
Gammasoft71 <gammasoft71@gmail.com>
2019-02-20 17:20:31 +0100

    rename unit test

--- tests/xtd.tunit.unit_tests/src/test_assert_are_equal_float_succeed.cpp
+++ tests/xtd.tunit.unit_tests/src/test_assert_are_equal_float_succeed.cpp
@@ -2,25 +2,25 @@
 #include <xtd/tunit>
 
 namespace unit_tests {
-  class test_class_(test_assert_are_equal_float) {
+  class test_class_(test_assert_are_equal_float_succeed) {
   public:
     void test_method_(test_case_succeed) {
       float f = 0.2;
-      xtd::tunit::assert::are_equal(0.2f, f, xtd::tunit::line_info {"test_assert_are_equal_float.cpp", 9});
+      xtd::tunit::assert::are_equal(0.2f, f, xtd::tunit::line_info {"test_assert_are_equal_float_succeed.cpp", 9});
     }
   };
 }
 
-void test_(test_assert_are_equal_float, test_output) {
+void test_(test_assert_are_equal_float_succeed, test_output) {
   std::stringstream ss;
   xtd::tunit::settings::default_settings().exit_status(0);
-  xtd::tunit::settings::default_settings().filter_tests("test_assert_are_equal_float.*");
+  xtd::tunit::settings::default_settings().filter_tests("test_assert_are_equal_float_succeed.*");
   xtd::tunit::settings::default_settings().show_duration(false);
   xtd::tunit::ostream_unit_test(ss, argv, argc).run();
   assert_value_("Start 1 test from 1 test case\n"
-                "  Start 1 test from test_assert_are_equal_float\n"
+                "  Start 1 test from test_assert_are_equal_float_succeed\n"
                 "    PASSED test_case_succeed\n"
-                "  End 1 test from test_assert_are_equal_float\n"
+                "  End 1 test from test_assert_are_equal_float_succeed\n"
                 "\n"
                 "  Summary :\n"
                 "    PASSED 1 test.\n"
@@ -28,10 +28,10 @@ void test_(test_assert_are_equal_float, test_output) {
                 "\n", ss.str());
 }
 
-void test_(test_assert_are_equal_float, test_result) {
+void test_(test_assert_are_equal_float_succeed, test_result) {
   std::stringstream ss;
   xtd::tunit::settings::default_settings().exit_status(0);
-  xtd::tunit::settings::default_settings().filter_tests("test_assert_are_equal_float.*");
+  xtd::tunit::settings::default_settings().filter_tests("test_assert_are_equal_float_succeed.*");
   xtd::tunit::settings::default_settings().show_duration(false);
   int result = xtd::tunit::ostream_unit_test(ss).run();
   assert_value_(0, result);
