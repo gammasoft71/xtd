5a62730559ff08281d8c0d91fdefb0bb42ef07f6
Gammasoft71 <gammasoft71@gmail.com>
2019-02-20 17:15:26 +0100

    test_assert_are_equal_double_NaN

---
+++ tests/xtd.tunit.unit_tests/src/test_assert_are_equal_double_NaN.cpp
@@ -0,0 +1,38 @@
+#include "assert_unit_tests.hpp"
+#include <xtd/tunit>
+
+namespace unit_tests {
+  class test_class_(test_assert_are_equal_double_NaN) {
+  public:
+    void test_method_(test_case_succeed) {
+      double d = std::numeric_limits<double>::quiet_NaN();
+      xtd::tunit::assert::are_equal(std::numeric_limits<double>::quiet_NaN(), d, xtd::tunit::line_info {"test_assert_are_equal_double_NaN.cpp", 9});
+    }
+  };
+}
+
+void test_(test_assert_are_equal_double_NaN, test_output) {
+  std::stringstream ss;
+  xtd::tunit::settings::default_settings().exit_status(0);
+  xtd::tunit::settings::default_settings().filter_tests("test_assert_are_equal_double_NaN.*");
+  xtd::tunit::settings::default_settings().show_duration(false);
+  xtd::tunit::ostream_unit_test(ss, argv, argc).run();
+  assert_value_("Start 1 test from 1 test case\n"
+                "  Start 1 test from test_assert_are_equal_double_NaN\n"
+                "    PASSED test_case_succeed\n"
+                "  End 1 test from test_assert_are_equal_double_NaN\n"
+                "\n"
+                "  Summary :\n"
+                "    PASSED 1 test.\n"
+                "End 1 test from 1 test case ran.\n"
+                "\n", ss.str());
+}
+
+void test_(test_assert_are_equal_double_NaN, test_result) {
+  std::stringstream ss;
+  xtd::tunit::settings::default_settings().exit_status(0);
+  xtd::tunit::settings::default_settings().filter_tests("test_assert_are_equal_double_NaN.*");
+  xtd::tunit::settings::default_settings().show_duration(false);
+  int result = xtd::tunit::ostream_unit_test(ss).run();
+  assert_value_(0, result);
+}
