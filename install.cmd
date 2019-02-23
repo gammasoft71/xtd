c751636b931e864cdded9fdf4948ad4450335fe4
Gammasoft71 <gammasoft71@gmail.com>
2019-02-23 15:51:34 +0100

    add install cmd

---
+++ install.cmd
@@ -0,0 +1,2 @@
+@set xtd_tunit_version=0.1.0
+@call scripts\install\install.cmd %*
---
+++ scripts/install/install.cmd
@@ -0,0 +1,17 @@
+@echo off
+echo Install xtd.tunit libraries version %xtd_tunit_version%, copyright Gammasoft, 2019
+echo.
+
+:: generate, build and install
+mkdir build\examples
+cd build
+cmake .. %*
+cmake --build . --target install --config Debug
+cmake --build . --target install --config Release
+cd ..
+
+:: generate and launch examples
+cd examples\build
+cmake ../../../examples/build %*
+start xtd.tunit.examples.sln
+cd ..
