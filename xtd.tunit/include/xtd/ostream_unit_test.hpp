2780fd72661b85bafb9798f029480f38e59635e8
Gammasoft71 <gammasoft71@gmail.com>
2019-02-23 23:56:23 +0100

    add comments

--- xtd.tunit/include/xtd/ostream_unit_test.hpp
+++ xtd.tunit/include/xtd/ostream_unit_test.hpp
@@ -8,11 +8,20 @@
 namespace xtd {
   /// @brief The tunit namespace contains a unit test library.
   namespace tunit {
-    /// @brief The ostream_unit_test class.
+    /// @brief The ostream_unit_test class is a specialisation of event_listener class for writing events in std::ostream.
+    /// @remarks This class is use by console_unit_test.
+    /// @remarks you can use this class as you want as for example communicate between network.
+    /// @par Examples
+    /// The following examples show how to use ostream_event_listener.
+    /// @include ostream_unit_test.cpp
     class ostream_event_listener : public xtd::tunit::event_listener {
     public:
+      /// @brief Creates new instance ostream event lister with ostream specified.
+      /// @param os sotream where evnets are writting.
       ostream_event_listener(std::ostream& os) noexcept : os_(os) {}
 
+      /// @brief Occurs when class is ended.
+      /// @param class_event_args Contains class event parameters.
       void on_class_end(const xtd::tunit::class_event_args& e) const override {
         this->event_listener::on_class_end(e);
         this->os_ << "  End " << e.test_class().test_count() << " test" << (e.test_class().test_count() < 2 ? "" : "s") << " from " << e.test_class().name();
@@ -21,11 +30,15 @@ namespace xtd {
         this->os_ << std::endl;
       }
       
+      /// @brief Occurs when class is started.
+      /// @param class_event_args Contains class_event parameters.
       void on_class_start(const xtd::tunit::class_event_args& e) const override {
         this->os_ << "  Start " << e.test_class().test_count() << " test" << (e.test_class().test_count() < 2 ? "" : "s") << " from " << e.test_class().name() << std::endl;
         this->event_listener::on_class_start(e);
       }
       
+      /// @brief Occurs when test is failed.
+      /// @param test_event_args Contains test event parameters.
       void on_test_failed(const xtd::tunit::test_event_args& e) const override {
         this->event_listener::on_test_failed(e);
         //__console_foreground_color(__console_color::red);
@@ -47,6 +60,8 @@ namespace xtd {
         this->os_ << std::endl;
       }
       
+      /// @brief Occurs when test is succeed.
+      /// @param test_event_args Contains test event parameters.
       void on_test_succeed(const xtd::tunit::test_event_args& e) const override {
         this->event_listener::on_test_succeed(e);
         //__console_foreground_color(__console_color::green);
@@ -58,6 +73,8 @@ namespace xtd {
         this->os_ << std::endl;
       }
       
+      /// @brief Occurs when unit test is ended.
+      /// @param tunit_event_args Contains unit test event parameters.
       void on_unit_test_end(const xtd::tunit::tunit_event_args& e) const override {
         this->event_listener::on_unit_test_end(e);
         this->os_ << std::endl;
@@ -95,6 +112,8 @@ namespace xtd {
         this->os_ << std::endl;
       }
       
+      /// @brief Occurs when unit test cleanup is started.
+      /// @param tunit_event_args Contains unit test event parameters.
       void on_unit_test_start(const xtd::tunit::tunit_event_args& e) const override {
         this->os_ << "Start " << e.unit_test().test_count() << " test" << (e.unit_test().test_count() < 2 ? "" : "s") << " from " << e.unit_test().test_cases_count() << " test case" << (e.unit_test().test_cases_count() < 2 ? "" : "s") << std::endl;
         this->event_listener::on_unit_test_start(e);
