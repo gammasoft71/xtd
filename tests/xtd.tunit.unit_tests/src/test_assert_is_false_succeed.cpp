f253c4738c8cca28b52e9ce3bad49ea7ed69c999
Gammasoft71 <gammasoft71@gmail.com>
2019-02-24 23:54:23 +0100

    fix line

--- tests/xtd.tunit.unit_tests/src/test_assert_is_false_succeed.cpp
+++ tests/xtd.tunit.unit_tests/src/test_assert_is_false_succeed.cpp
@@ -6,7 +6,7 @@ namespace unit_tests {
   public:
     void test_method_(test_case_succeed) {
       bool b = false;
-      xtd::tunit::assert::is_false(b, xtd::tunit::line_info {"test_assert_is_false_string_succeed.cpp", 10});
+      xtd::tunit::assert::is_false(b, xtd::tunit::line_info {"test_assert_is_false_string_succeed.cpp", 9});
     }
   };
 }
