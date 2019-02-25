ba4d7d1d27e3f2686cc7ca324d0cfcdd168501d6
Gammasoft71 <gammasoft71@gmail.com>
2019-02-25 11:10:06 +0100

    add examples

---
+++ examples/class_with_insert_stream_operator/src/class_with_insert_stream_operator.cpp
@@ -0,0 +1,65 @@
+#include <xtd/tunit>
+
+using namespace xtd::tunit;
+
+namespace unit_tests {
+  class class_with_insert_stream_operator {
+  public:
+    class_with_insert_stream_operator() = default;
+    explicit class_with_insert_stream_operator(int value) : value_(value) {}
+    
+    int value() const noexcept {return this->value_;}
+
+    bool operator==(const class_with_insert_stream_operator& c) const noexcept {return this->value_ == c.value_;}
+    bool operator!=(const class_with_insert_stream_operator& c) const noexcept {return !this->operator==(c);}
+
+    friend std::ostream& operator<<(std::ostream& os, const class_with_insert_stream_operator& c) {
+      return os << "(value = " << c.value_ << ")";
+    }
+    
+  private:
+    int value_ = 0;
+  };
+
+  class test_class_(test) {
+  public:
+    void test_method_(test_case_succeed) {
+      class_with_insert_stream_operator c1(42);
+      class_with_insert_stream_operator c2(42);
+      assert::are_equal_(c1, c2);
+    }
+
+    void test_method_(test_case_failed) {
+      class_with_insert_stream_operator c1(42);
+      class_with_insert_stream_operator c2(24);
+      assert::are_equal_(c1, c2);
+    }
+  };
+}
+
+// The main entry point for the application.
+int main(int argc, char* argv[]) {
+  return console_unit_test(argv, argc).run();
+}
+
+// This code produces the following output:
+//
+/// tart 2 tests from 1 test case
+//   Start 2 tests from test
+//     PASSED test_case_succeed (0 ms total)
+//     FAILED test_case_failed (0 ms total)
+//
+// Expected: (value = 42)
+// But was:  (value = 24)
+// error: !---OMITTED---!/xtd.tunit/examples/assert_are_equal/src/class_with_insert_stream_operator.cpp:35
+//
+//   End 2 tests from test (0 ms total)
+//
+//   Summary :
+//     PASSED 1 test.
+// *** FAILED 1 test, listed below:
+// *** FAILED test.test_case_failed
+//
+//     FAILED 1 test.
+// End 2 tests from 1 test case ran. (0 ms total)
+
---
+++ examples/class_without_insert_stream_operator/src/class_without_insert_stream_operator.cpp
@@ -0,0 +1,63 @@
+#include <xtd/tunit>
+
+using namespace xtd::tunit;
+
+namespace unit_tests {
+  class class_without_insert_stream_operator {
+  public:
+  public:
+    class_without_insert_stream_operator() = default;
+    explicit class_without_insert_stream_operator(int value) : value_(value) {}
+    
+    int value() const noexcept {return this->value_;}
+    
+    bool operator==(const class_without_insert_stream_operator& c) const noexcept {return this->value_ == c.value_;}
+    bool operator!=(const class_without_insert_stream_operator& c) const noexcept {return !this->operator==(c);}
+
+  private:
+    int value_ = 0;
+  };
+  
+  class test_class_(test) {
+  public:
+    void test_method_(test_case_succeed) {
+      class_without_insert_stream_operator c1(42);
+      class_without_insert_stream_operator c2(42);
+      assert::are_equal_(c1, c2);
+
+    }
+
+    void test_method_(test_case_failed) {
+      class_without_insert_stream_operator c1(42);
+      class_without_insert_stream_operator c2(24);
+      assert::are_equal_(c1, c2);
+    }
+  };
+}
+
+// The main entry point for the application.
+int main(int argc, char* argv[]) {
+  return console_unit_test(argv, argc).run();
+}
+
+// This code produces the following output:
+//
+/// tart 2 tests from 1 test case
+//   Start 2 tests from test
+//     PASSED test_case_succeed (0 ms total)
+//     FAILED test_case_failed (0 ms total)
+//
+// Expected: 4-byte object <2A-00 00-00>
+// But was:  4-byte object <18-00 00-00>
+// error: !---OMITTED---!/xtd.tunit/examples/assert_are_equal/src/class_without_insert_stream_operator.cpp:33
+//
+//   End 2 tests from test (0 ms total)
+//
+//   Summary :
+//     PASSED 1 test.
+// *** FAILED 1 test, listed below:
+// *** FAILED test.test_case_failed
+//
+//     FAILED 1 test.
+// End 2 tests from 1 test case ran. (0 ms total)
+
