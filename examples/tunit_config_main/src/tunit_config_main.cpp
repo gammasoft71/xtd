92a31555853552480ffdc490a092353eb13b244c
Gammasoft71 <gammasoft71@gmail.com>
2019-02-25 14:24:56 +0100

    add tunit_config_main example

---
+++ examples/tunit_config_main/src/tunit_config_main.cpp
@@ -0,0 +1,30 @@
+#define TUNIT_CONFIG_MAIN
+#include <xtd/tunit>
+#include <string>
+
+using namespace xtd::tunit;
+
+namespace unit_tests {
+  // The class test must be declared with test_class_ helper.
+  class test_class_(test) {
+  public:
+    void test_method_(test_case1) {
+      std::string s = "Hello";
+      assert::is_true(s == "Hello");
+    }
+  };
+}
+
+// This code can produce the following output:
+//
+// Start 2 tests from 1 test case
+//   Start 2 tests from test
+//     PASSED test_case1 (0 ms total)
+//     PASSED test_case2 (0 ms total)
+//   End 2 tests from test (0 ms total)
+//
+//   Summary :
+//     PASSED 2 tests.
+// End 2 tests from 1 test case ran. (0 ms total)
+//
+// You have 1 ignored test
