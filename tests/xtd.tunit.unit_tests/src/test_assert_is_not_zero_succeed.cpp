1c18d03d477b717f38a5c634e746919691b339d9
Gammasoft71 <gammasoft71@gmail.com>
2019-02-26 11:52:25 +0100

    add test_assert_is_not_zero_succeed test

---
+++ tests/xtd.tunit.unit_tests/src/test_assert_is_not_zero_succeed.cpp
@@ -0,0 +1,38 @@
+#include "assert_unit_tests.hpp"
+#include <xtd/tunit>
+
+namespace unit_tests {
+  class test_class_(test_assert_is_not_zero_succeed) {
+  public:
+    void test_method_(test_case_succeed) {
+      int i = 42;
+      xtd::tunit::assert::is_not_zero(i, xtd::tunit::line_info {"test_assert_is_not_zero_succeed.cpp", 9});
+    }
+  };
+}
+
+void test_(test_assert_is_not_zero_succeed, test_output) {
+  std::stringstream ss;
+  xtd::tunit::settings::default_settings().exit_status(0);
+  xtd::tunit::settings::default_settings().filter_tests("test_assert_is_not_zero_succeed.*");
+  xtd::tunit::settings::default_settings().show_duration(false);
+  xtd::tunit::ostream_unit_test(ss, argv, argc).run();
+  assert_value_("Start 1 test from 1 test case\n"
+                "  Start 1 test from test_assert_is_not_zero_succeed\n"
+                "    PASSED test_case_succeed\n"
+                "  End 1 test from test_assert_is_not_zero_succeed\n"
+                "\n"
+                "  Summary :\n"
+                "    PASSED 1 test.\n"
+                "End 1 test from 1 test case ran.\n"
+                "\n", ss.str());
+}
+
+void test_(test_assert_is_not_zero_succeed, test_result) {
+  std::stringstream ss;
+  xtd::tunit::settings::default_settings().exit_status(0);
+  xtd::tunit::settings::default_settings().filter_tests("test_assert_is_not_zero_succeed.*");
+  xtd::tunit::settings::default_settings().show_duration(false);
+  int result = xtd::tunit::ostream_unit_test(ss).run();
+  assert_value_(0, result);
+}
