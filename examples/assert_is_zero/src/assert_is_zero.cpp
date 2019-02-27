6c81874e7531652f16fac09b764189af119f9aa4
Gammasoft71 <gammasoft71@gmail.com>
2019-02-28 00:43:00 +0100

    review examples

--- examples/assert_is_zero/src/assert_is_zero.cpp
+++ examples/assert_is_zero/src/assert_is_zero.cpp
@@ -10,12 +10,12 @@ namespace unit_tests {
   public:
     void test_method_(test_case_succeed) {
       int i = 0;
-      assert::is_zero_(i);
+      assert::is_zero(i);
     }
 
     void test_method_(test_case_failed) {
       int i = 42;
-      assert::is_zero_(i);
+      assert::is_zero(i);
     }
   };
 }
@@ -27,22 +27,15 @@ int main(int argc, char* argv[]) {
 
 // This code produces the following output:
 //
-/// tart 2 tests from 1 test case
-//   Start 2 tests from test
-//     SUCCEED test_case_succeed (0 ms total)
-//     FAILED  test_case_failed (0 ms total)
+// Start 2 tests from 1 test case
+// Run tests:
+//   SUCCEED test.test_case_succeed (0 ms total)
+//   FAILED  test.test_case_failed (0 ms total)
+//     Expected: zero
+//     But was:  42
+//     Stack Trace: in |---OMITTED---|/assert_is_zero.cpp:16
 //
-// Expected: zero
-// But was:  42
-// error: !---OMITTED---!/xtd.tunit/examples/assert_are_equal/src/assert_is_zero.cpp:18
-//
-//   End 2 tests from test (0 ms total)
-//
-//   Summary :
-//     SUCCEED 1 test.
-//     FAILED  1 test, listed below:
-//     FAILED  test.test_case_failed
-//
-//     FAILED  1 test.
+// Test results:
+//   SUCCEED 1 test.
+//   FAILED  1 test.
 // End 2 tests from 1 test case ran. (0 ms total)
-
--- examples/assert_succeed/src/assert_succeed.cpp
+++ examples/assert_succeed/src/assert_succeed.cpp
@@ -19,10 +19,9 @@ int main(int argc, char* argv[]) {
 // This code produces the following output:
 //
 // Start 1 test from 1 test case
-//   Start 1 test from test
-//     SUCCEED test_case_failed (0 ms total)
-//   End 1 test from test (0 ms total)
+// Run tests:
+//   SUCCEED test.test_case_succeed (0 ms total)
 //
-//   Summary :
-//     SUCCEED 1 test.
+// Test results:
+//  SUCCEED 1 test.
 // End 1 test from 1 test case ran. (0 ms total)
