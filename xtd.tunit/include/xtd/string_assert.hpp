8edeee9f4670ed3c92f90ba3046e4c5d2316f930
Gammasoft71 <gammasoft71@gmail.com>
2019-03-01 21:27:29 +0100

    add string contains assertions

--- xtd.tunit/include/xtd/string_assert.hpp
+++ xtd.tunit/include/xtd/string_assert.hpp
@@ -973,6 +973,198 @@ namespace xtd {
         else
           assert::fail("string containing " + assert::to_string(item), "< " + __join__items(string) + " >", message, line_info);
       }
+      
+      /// @brief Asserts that collection contains an item.
+      /// @param item object to verify.
+      /// @param collection that contains object.
+      /// @exception xtd::tunit::assertion_error If bad assertion.
+      /// @par Examples
+      /// @code
+      /// std::u16string s = u"value";
+      /// xtd::tunit::string_assert::contains(u'a', s); // test ok.
+      /// xtd::tunit::string_assert::contains(u'z', s); // test throws an assertion_error exception.
+      /// @endcode
+      static void contains(char16_t item, const std::u16string& string) {contains(item, string, "", line_info());}
+      
+      /// @brief Asserts that collection contains an item.
+      /// @param item object to verify.
+      /// @param collection that contains object.
+      /// @param line_info Contains information about current file and current line.
+      /// @exception xtd::tunit::assertion_error If bad assertion.
+      /// @par Examples
+      /// @code
+      /// std::u16string s = u"value";
+      /// xtd::tunit::string_assert::contains(u'a', s, line_info_); // test ok.
+      /// xtd::tunit::string_assert::contains(u'z', s, line_info_); // test throws an assertion_error exception.
+      /// @endcode
+      static void contains(char16_t item, const std::u16string& string, const xtd::tunit::line_info& line_info) {contains(item, string, "", line_info);}
+      
+      /// @brief Asserts that collection contains an item.
+      /// @param item object to verify.
+      /// @param collection that contains object.
+      /// @param message A user message to display if the assertion fails. This message can be seen in the unit test results.
+      /// @exception xtd::tunit::assertion_error If bad assertion.
+      /// @par Examples
+      /// @code
+      /// std::u16string s = u"value";
+      /// xtd::tunit::string_assert::contains(u'a', s, "User message..."); // test ok.
+      /// xtd::tunit::string_assert::contains(u'z', s, "User message..."); // test throws an assertion_error exception.
+      /// @endcode
+      static void contains(char16_t item, const std::u16string& string, const std::string& message) {contains(item, string, message, line_info());}
+      
+      /// @brief Asserts that collection contains an item.
+      /// @param item object to verify.
+      /// @param collection that contains object.
+      /// @param message A user message to display if the assertion fails. This message can be seen in the unit test results.
+      /// @param line_info Contains information about current file and current line.
+      /// @exception xtd::tunit::assertion_error If bad assertion.
+      /// @par Examples
+      /// @code
+      /// std::u16string s = u"value";
+      /// xtd::tunit::string_assert::contains(u'a', s, "User message...", line_info_); // test ok.
+      /// xtd::tunit::string_assert::contains(u'z', s, "User message...", line_info_); // test throws an assertion_error exception.
+      /// @endcode
+      static void contains(char16_t item, const std::u16string& string, const std::string& message, const xtd::tunit::line_info& line_info) {
+        auto result = std::find(string.begin(), string.end(), item);
+        if (result != string.end())
+          assert::succeed(message, line_info);
+        else
+          assert::fail("string containing " + assert::to_string(item), "< " + __join__items(string) + " >", message, line_info);
+      }
+      
+      /// @brief Asserts that collection contains an item.
+      /// @param item object to verify.
+      /// @param collection that contains object.
+      /// @exception xtd::tunit::assertion_error If bad assertion.
+      /// @par Examples
+      /// @code
+      /// std::u32string s = U"value";
+      /// xtd::tunit::string_assert::contains(U'a', s); // test ok.
+      /// xtd::tunit::string_assert::contains(U'z', s); // test throws an assertion_error exception.
+      /// @endcode
+      static void contains(char32_t item, const std::u32string& string) {contains(item, string, "", line_info());}
+      
+      /// @brief Asserts that collection contains an item.
+      /// @param item object to verify.
+      /// @param collection that contains object.
+      /// @param line_info Contains information about current file and current line.
+      /// @exception xtd::tunit::assertion_error If bad assertion.
+      /// @par Examples
+      /// @code
+      /// std::u32string s = U"value";
+      /// xtd::tunit::string_assert::contains(U'a', s, line_info_); // test ok.
+      /// xtd::tunit::string_assert::contains(U'z', s, line_info_); // test throws an assertion_error exception.
+      /// @endcode
+      static void contains(char32_t item, const std::u32string& string, const xtd::tunit::line_info& line_info) {contains(item, string, "", line_info);}
+      
+      /// @brief Asserts that collection contains an item.
+      /// @param item object to verify.
+      /// @param collection that contains object.
+      /// @param message A user message to display if the assertion fails. This message can be seen in the unit test results.
+      /// @exception xtd::tunit::assertion_error If bad assertion.
+      /// @par Examples
+      /// @code
+      /// std::u32string s = U"value";
+      /// xtd::tunit::string_assert::contains(U'a', s, "User message..."); // test ok.
+      /// xtd::tunit::string_assert::contains(U'z', s, "User message..."); // test throws an assertion_error exception.
+      /// @endcode
+      static void contains(char16_t item, const std::u32string& string, const std::string& message) {contains(item, string, message, line_info());}
+      
+      /// @brief Asserts that collection contains an item.
+      /// @param item object to verify.
+      /// @param collection that contains object.
+      /// @param message A user message to display if the assertion fails. This message can be seen in the unit test results.
+      /// @param line_info Contains information about current file and current line.
+      /// @exception xtd::tunit::assertion_error If bad assertion.
+      /// @par Examples
+      /// @code
+      /// std::u32string s = U"value";
+      /// xtd::tunit::string_assert::contains(U'a', s, "User message...", line_info_); // test ok.
+      /// xtd::tunit::string_assert::contains(U'z', s, "User message...", line_info_); // test throws an assertion_error exception.
+      /// @endcode
+      static void contains(char32_t item, const std::u32string& string, const std::string& message, const xtd::tunit::line_info& line_info) {
+        auto result = std::find(string.begin(), string.end(), item);
+        if (result != string.end())
+          assert::succeed(message, line_info);
+        else
+          assert::fail("string containing " + assert::to_string(item), "< " + __join__items(string) + " >", message, line_info);
+      }
+      
+      /// @brief Asserts that collection contains an item.
+      /// @param item object to verify.
+      /// @param collection that contains object.
+      /// @exception xtd::tunit::assertion_error If bad assertion.
+      /// @par Examples
+      /// @code
+      /// std::wstring s = L"value";
+      /// xtd::tunit::string_assert::contains(L'a', s); // test ok.
+      /// xtd::tunit::string_assert::contains(L'z', s); // test throws an assertion_error exception.
+      /// @endcode
+      static void contains(wchar_t item, const std::wstring& string) {contains(item, string, "", line_info());}
+      
+      /// @brief Asserts that collection contains an item.
+      /// @param item object to verify.
+      /// @param collection that contains object.
+      /// @param line_info Contains information about current file and current line.
+      /// @exception xtd::tunit::assertion_error If bad assertion.
+      /// @par Examples
+      /// @code
+      /// std::wstring s = L"value";
+      /// xtd::tunit::string_assert::contains(L'a', s, line_info_); // test ok.
+      /// xtd::tunit::string_assert::contains(L'z', s, line_info_); // test throws an assertion_error exception.
+      /// @endcode
+      static void contains(wchar_t item, const std::wstring& string, const xtd::tunit::line_info& line_info) {contains(item, string, "", line_info);}
+      
+      /// @brief Asserts that collection contains an item.
+      /// @param item object to verify.
+      /// @param collection that contains object.
+      /// @param message A user message to display if the assertion fails. This message can be seen in the unit test results.
+      /// @exception xtd::tunit::assertion_error If bad assertion.
+      /// @par Examples
+      /// @code
+      /// std::wstring s = L"value";
+      /// xtd::tunit::string_assert::contains(L'a', s, "User message..."); // test ok.
+      /// xtd::tunit::string_assert::contains(L'z', s, "User message..."); // test throws an assertion_error exception.
+      /// @endcode
+      static void contains(wchar_t item, const std::wstring& string, const std::string& message) {contains(item, string, message, line_info());}
+      
+      /// @brief Asserts that collection contains an item.
+      /// @param item object to verify.
+      /// @param collection that contains object.
+      /// @param message A user message to display if the assertion fails. This message can be seen in the unit test results.
+      /// @param line_info Contains information about current file and current line.
+      /// @exception xtd::tunit::assertion_error If bad assertion.
+      /// @par Examples
+      /// @code
+      /// std::wstring s = L"value";
+      /// xtd::tunit::string_assert::contains(L'a', s, "User message...", line_info_); // test ok.
+      /// xtd::tunit::string_assert::contains(L'z', s, "User message...", line_info_); // test throws an assertion_error exception.
+      /// @endcode
+      static void contains(wchar_t item, const std::wstring& string, const std::string& message, const xtd::tunit::line_info& line_info) {
+        auto result = std::find(string.begin(), string.end(), item);
+        if (result != string.end())
+          assert::succeed(message, line_info);
+        else
+          assert::fail("string containing " + assert::to_string(item), "< " + __join__items(string) + " >", message, line_info);
+      }
+
+      /// @cond
+      static void contains(char item, const char* string) {contains(item, string, "", line_info());}
+      static void contains(char item, const char* string, const xtd::tunit::line_info& line_info) {contains(item, string, "", line_info);}
+      static void contains(char item, const char* string, const std::string& message) {contains(item, string, message, line_info());}
+      static void contains(char item, const char* string, const std::string& message, const xtd::tunit::line_info& line_info) {contains(item, std::string(string), message, line_info);}
+
+      static void contains(char16_t item, const char16_t* string) {contains(item, string, "", line_info());}
+      static void contains(char16_t item, const char16_t* string, const xtd::tunit::line_info& line_info) {contains(item, string, "", line_info);}
+      static void contains(char16_t item, const char16_t* string, const std::string& message) {contains(item, string, message, line_info());}
+      static void contains(char16_t item, const char16_t* string, const std::string& message, const xtd::tunit::line_info& line_info) {contains(item, std::u16string(string), message, line_info);}
+
+      static void contains(char32_t item, const char32_t* string) {contains(item, string, "", line_info());}
+      static void contains(char32_t item, const char32_t* string, const xtd::tunit::line_info& line_info) {contains(item, string, "", line_info);}
+      static void contains(char32_t item, const char32_t* string, const std::string& message) {contains(item, string, message, line_info());}
+      static void contains(char32_t item, const char32_t* string, const std::string& message, const xtd::tunit::line_info& line_info) {contains(item, std::u32string(string), message, line_info);}
+      /// @endcond
+
 
     private:
       static std::string to_lower(const std::string& value) {
