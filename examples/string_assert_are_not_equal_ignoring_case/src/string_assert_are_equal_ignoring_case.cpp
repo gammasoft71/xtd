cb93fec3062a57772a21b46f3a91438b08431e76
Gammasoft71 <gammasoft71@gmail.com>
2019-03-01 20:09:16 +0100

    add string_assert_are_not_equal_ignoring_case example

---
+++ examples/string_assert_are_not_equal_ignoring_case/src/string_assert_are_equal_ignoring_case.cpp
@@ -0,0 +1,41 @@
+#include <xtd/tunit>
+#include <string>
+
+using namespace std;
+using namespace std::string_literals;
+using namespace xtd::tunit;
+
+namespace unit_tests {
+  class test_class_(test) {
+  public:
+    void test_method_(test_case_succeed) {
+      auto s = u"VALUE"s;
+      string_assert::are_not_equal_ingoring_case(u"key", s);
+    }
+    
+    void test_method_(test_case_failed) {
+      auto s = U"VALUE"s;
+      string_assert::are_not_equal_ingoring_case(U"value", s);
+    }
+  };
+}
+
+// The main entry point for the application.
+int main(int argc, char* argv[]) {
+  return console_unit_test(argv, argc).run();
+}
+
+// This code produces the following output:
+//
+// Start 2 tests from 1 test case
+// Run tests:
+//   SUCCEED test.test_case_succeed (0 ms total)
+//   FAILED  test.test_case_failed (0 ms total)
+//     Expected: not "value"
+//     But was:  "VALUE"
+//     Stack Trace: in |---OMITTED---|/string_assert_are_equal_ignoring_case.cpp:16
+//
+// Test results:
+//   SUCCEED 1 test.
+//   FAILED  1 test.
+// End 2 tests from 1 test case ran. (0 ms total)
