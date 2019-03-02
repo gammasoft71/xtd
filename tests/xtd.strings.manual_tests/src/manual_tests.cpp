9091e04bc65a02ecb7c5f40808eecc8d51908c30
Gammasoft71 <gammasoft71@gmail.com>
2019-03-02 23:15:55 +0100

    add pad_right method

--- tests/xtd.strings.manual_tests/src/manual_tests.cpp
+++ tests/xtd.strings.manual_tests/src/manual_tests.cpp
@@ -19,6 +19,7 @@ int main() {
   cout << strings::contains("Hello, Wolrd !", "Helli") << endl;
   cout << strings::join(", ", {"black", "yellow", "red"}, 0, 22) << endl;
   cout << strings::pad_left("padded", 10) << endl;
+  cout << strings::pad_right("padded", 10, '+') << endl;
 }
 
 // This code produces the following output with colors:
