7e7c16af2ff632cd0b9b30ea1aacfa7175773adb
Gammasoft71 <gammasoft71@gmail.com>
2019-03-02 22:12:35 +0100

    add concat and contains methods

--- tests/xtd.strings.manual_tests/src/manual_tests.cpp
+++ tests/xtd.strings.manual_tests/src/manual_tests.cpp
@@ -1,16 +1,22 @@
 #include <xtd/strings>
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 using namespace string_literals;
 using namespace xtd;
+
+inline std::string const& to_string(std::string const& s) { return s; }
 
 // The main entry point for the application.
 int main() {
   cout << strings::format("%s, %s!", "Hello", "World"s) << endl;
   std::string s = "Hello, World!";
   cout << strings::starts_width(s, "HELLO", true) << endl;
+  int i = 2;
+  cout << strings::concat<char>("item1 ", i, " ", 3.5) << endl;
+  cout << strings::contains("Hello, Wolrd !", "Helli") << endl;
 }
 
 // This code produces the following output with colors:
