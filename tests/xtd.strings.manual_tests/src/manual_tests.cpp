fc646d3f171fb65522d0aca503f82b314882b6f9
Gammasoft71 <gammasoft71@gmail.com>
2019-03-03 22:06:05 +0100

    immutable_basic_string implementation

--- tests/xtd.strings.manual_tests/src/manual_tests.cpp
+++ tests/xtd.strings.manual_tests/src/manual_tests.cpp
@@ -11,21 +11,11 @@ inline std::string const& to_string(std::string const& s) { return s; }
 
 // The main entry point for the application.
 int main() {
-  cout << strings::format("%s, %s!", "Hello", "World"s) << endl;
-  std::string s = "Hello, World!";
-  cout << strings::starts_width(s, "HELLO", true) << endl;
-  int i = 2;
-  cout << strings::concat("item1 ", i, " ", 3.5) << endl;
-  cout << strings::contains("Hello, Wolrd !", "Helli") << endl;
-  cout << strings::join(", ", {"black", "yellow", "red"}, 0, 22) << endl;
-  cout << strings::pad_left("padded", 10) << endl;
-  cout << strings::pad_right("padded", 10, '+') << endl;
-  cout << strings::replace("abc defabc def", "abc", "1234") << endl;
-  cout << strings::replace("abc defabc def", 'e', 'z') << endl;
-  cout << strings::trim_start("    toto") << endl;
-  
   istring str = "My immutable string"_is;
-  cout << str << endl;
+  istring s2 = "Hello, ";
+  istring s3 = "World!";
+  s2 += s3;
+  cout << s2 << endl;
   cout << strings::replace(str, 'm', 'z') << endl;
 }
 
