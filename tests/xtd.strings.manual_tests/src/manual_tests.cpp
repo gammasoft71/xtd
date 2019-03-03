7435c15666f3cfb618c945d908fba8476b9e7053
Gammasoft71 <gammasoft71@gmail.com>
2019-03-03 22:19:39 +0100

    fix build

--- tests/xtd.strings.manual_tests/src/manual_tests.cpp
+++ tests/xtd.strings.manual_tests/src/manual_tests.cpp
@@ -12,11 +12,13 @@ inline std::string const& to_string(std::string const& s) { return s; }
 // The main entry point for the application.
 int main() {
   istring str = "My immutable string"_is;
+  istring str2 = "My immutable string"_is;
   istring s2 = "Hello, ";
   istring s3 = "World!";
   s2 += s3;
   cout << s2 << endl;
   cout << strings::replace(str, 'm', 'z') << endl;
+  //cout << str == str2 << endl;
 }
 
 // This code produces the following output with colors:
