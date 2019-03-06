27a878d9ccd1ca7bbde12caf854e0780271b31e3
Gammasoft71 <gammasoft71@gmail.com>
2019-03-06 22:57:38 +0100

    move __is_windows_os in cpp file

--- xtd.io/src/path_win32.cpp
+++ xtd.io/src/path_win32.cpp
@@ -4,9 +4,14 @@
 #include <string>
 #include <Windows.h>
 
-std::string __get_current_dirirectory() {
+std::string __get_current_dirirectory() noexcept {
   char path[MAX_PATH + 1];
   return _getcwd(path, MAX_PATH) ? path : "";
 }
+
+bool __is_windows_os() noexcept {
+  return true;
+}
+
 
 #endif
