c25c8f26cdfca57a22e1b32684c8aead503130f9
Gammasoft71 <gammasoft71@gmail.com>
2019-03-06 22:34:47 +0100

    add some unit tests

--- tests/xtd.io.unit_tests/src/path.cpp
+++ tests/xtd.io.unit_tests/src/path.cpp
@@ -11,5 +11,45 @@ namespace unit_tests {
     void test_method_(alt_directory_separator_char) {
       assert::are_equal('/', path::alt_directory_separator_char());
     }
+
+    void test_method_(change_extension) {
+      auto file_name = "file.txt";
+      assert::are_equal("file.bak", path::change_extension(file_name, ".bak"));
+    }
+
+    void test_method_(change_extension_without_point) {
+      auto file_name = "file.txt";
+      assert::are_equal("file.bak", path::change_extension(file_name, "bak"));
+    }
+
+    void test_method_(change_extension_with_strange_extension) {
+      auto file_name = "file.txt";
+      assert::are_equal("file.backup.bak", path::change_extension(file_name, ".backup.bak"));
+    }
+
+    void test_method_(change_extension_source_without_extension) {
+      auto file_name = "file";
+      assert::are_equal("file.bak", path::change_extension(file_name, ".bak"));
+    }
+
+    void test_method_(change_extension_with_empty_extension) {
+      auto file_name = "file.com";
+      assert::are_equal("file", path::change_extension(file_name, ""));
+    }
+
+    void test_method_(change_extension_with_only_point_extension) {
+      auto file_name = "file.com";
+      assert::are_equal("file.", path::change_extension(file_name, "."));
+    }
+
+    void test_method_(change_extension_without_source) {
+      auto file_name = ".com";
+      assert::are_equal(".bak", path::change_extension(file_name, ".bak"));
+    }
+
+    void test_method_(change_extension_wit_empty_source) {
+      auto file_name = "";
+      assert::are_equal(".bak", path::change_extension(file_name, ".bak"));
+    }
   };
 }
