88b98d42ca0c4eada0d4c2d03621a2561990cbaa
Gammasoft71 <gammasoft71@gmail.com>
2019-03-06 14:24:29 +0100

    add scripts

---
+++ scripts/travis/build_linux.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+mkdir -p build/examples
+cd build
+
+# generate and build lib
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=g++-7 -DENABLE_TESTS=ON -DCMAKE_INSTALL_PREFIX=~/local ..
+if [ $? -ne 0 ]; then exit -1; fi
+cmake --build . --target install -- -j $(nproc)
+if [ $? -ne 0 ]; then exit -1; fi
+
+# generate and build examples
+cd examples
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=g++-7 -DCMAKE_INSTALL_PREFIX=~/local ../../examples
+if [ $? -ne 0 ]; then exit -1; fi
+cmake --build . -- -j $(nproc)
+if [ $? -ne 0 ]; then exit -1; fi
+cd ..
+
+# run registered unit tests
+ctest -j $(nproc) --output-on-failure --build-config Debug
+if [ $? -ne 0 ]; then exit -1; fi
+
+cd ..
