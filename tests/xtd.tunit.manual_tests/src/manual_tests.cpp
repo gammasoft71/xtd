cd485eed50888b56a1d26fbd3b26989138deaab1
Gammasoft71 <gammasoft71@gmail.com>
2019-03-07 09:59:20 +0100

    add repeat tests

--- tests/xtd.tunit.manual_tests/src/manual_tests.cpp
+++ tests/xtd.tunit.manual_tests/src/manual_tests.cpp
@@ -189,7 +189,7 @@ namespace unit_tests {
 
 int main(int argc, char* argv[]) {
   settings::default_settings().output_xml(std::string(argv[0]) + ".xml");
-  settings::default_settings().repeat_tests(-1);
+  settings::default_settings().repeat_tests(2);
   //settings::default_settings().list_tests(true);
   //settings::default_settings().also_run_ignored_tests(true);
   return console_unit_test(argv, argc).run();
