d841f0cf9283a2be2bd54c58c20648cb16b949a5
Gammasoft71 <gammasoft71@gmail.com>
2019-03-11 10:56:07 +0100

    add examples

--- examples/hello_world_console/src/hello_world_console.cpp
+++ examples/hello_world_console/src/hello_world_console.cpp
@@ -1,10 +1,21 @@
 #include <xtd/xtd>
 
+using namespace std;
 using namespace xtd;
 
 // The main entry point for the application.
 int main() {
-  console::write_line("%s", "Hello, World!");
+  const char* s1 = "Hello,";
+  string s2 = "World!";
+  
+  cout << foreground_color(console_color::green);
+  cout << format("%s%7s", s1, s2);
+  cout << reset_color() << endl;
+  
+  console::background_color(console_color::blue);
+  console::foreground_color(console_color::white);
+  console::write_line("%s%7s", s1, s2);
+  console::reset_color();
 }
 
 // This code produces the following output :
