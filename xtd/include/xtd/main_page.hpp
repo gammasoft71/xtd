4ff4525ccd019666bfedd47c24098d952efe6e05
Gammasoft71 <gammasoft71@gmail.com>
2019-03-11 11:54:17 +0100

    Update main_page.hpp

--- xtd/include/xtd/main_page.hpp
+++ xtd/include/xtd/main_page.hpp
@@ -4,12 +4,12 @@
 /// @mainpage xtd. - Reference Guide Documentation
 ///
 /// @section FeaturesSection Features
-///   * <a href="https://github.com/gammasoft71/xtd.console">xtd.console</a>
-///   * <a href="https://github.com/gammasoft71/xtd.delegates">xtd.delegates</a>
-///   * <a href="https://github.com/gammasoft71/xtd.io">xtd.io</a>
-///   * <a href="https://github.com/gammasoft71/xtd.properties">xtd.properties</a>
-///   * <a href="https://github.com/gammasoft71/xtd.strings">xtd.strings</a>
-///   * <a href="https://github.com/gammasoft71/xtd.tunit">xtd.tunit</a>
+///   * <a href="https://github.com/gammasoft71/xtd.console">xtd.console</a> Modern c++17 library to manage console application on Windows, macOS and Linux.
+///   * <a href="https://github.com/gammasoft71/xtd.delegates">xtd.delegates</a> Modern c ++17 library  containing delegate which is a data structure that refers to a static method or to a class instance and an instance method of that class.
+///   * <a href="https://github.com/gammasoft71/xtd.io">xtd.io</a> Modern c++17 library, that allow reading and writing to files and data streams, and that provide basic file and directory support, for Windows, macOS, Linux, iOS and android.
+///   * <a href="https://github.com/gammasoft71/xtd.properties">xtd.properties</a> another c#-like property accessor for C++11 and above.
+///   * <a href="https://github.com/gammasoft71/xtd.strings">xtd.strings</a> Modern c++17 library to add string utilities on Windows, macOS and Linux.
+///   * <a href="https://github.com/gammasoft71/xtd.tunit">xtd.tunit</a> Modern c++17 unit testing framework on Windows, macOS, Linux, iOS and android.
 ///
 /// For more information see <a href="https://gammasoft71.wixsite.com/gammasoft">Gammasoft website</a> (or <a href="https://github.com/gammasoft71/xtd/blob/master/docs/home.md">markdown documentations</a>) and <a href="https://codedocs.xyz/gammasoft71/xtd/">Reference Guide</a>).
 ///
