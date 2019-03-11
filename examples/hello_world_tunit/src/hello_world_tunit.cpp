25aecab1c6e70f75f62ae2fa7570333da16e3df2
Gammasoft71 <gammasoft71@gmail.com>
2019-03-11 10:56:07 +0100

    add examples

--- examples/hello_world_tunit/src/hello_world_tunit.cpp
+++ examples/hello_world_tunit/src/hello_world_tunit.cpp
@@ -9,7 +9,7 @@ namespace unit_tests {
   public:
     void test_method_(create_string_from_literal) {
       string s = "Hello, World!";
-      asssert::are_equal("Hello, World!", s);
+      assert::are_equal("Hello, World!", s);
     }
     
     void test_method_(create_string_from_chars) {
@@ -21,17 +21,16 @@ namespace unit_tests {
 
 // The main entry point for the application.
 int main(int argc, char* argv[]) {
-  return unit_test(argv, argc).run();
+  return console_unit_test(argv, argc).run();
 }
 
 // This code produces the following output:
 //
-// Start 2 tests from 1 test case
-//   Start 2 tests from hello_world_test
-//     PASSED hello_world_test.create_string_from_literal (0 ms)
-//     PASSED hello_world_test.create_string_from_chars (0 ms)
-//   End 2 tests from hello_world_test (3 ms total)
+// SStart 2 tests from 1 test case
+// Run tests:
+//   SUCCEED hello_world_test.create_string_from_literal (0 ms total)
+//   SUCCEED hello_world_test.create_string_from_chars (0 ms total)
 //
-//   Summary :
-//     PASSED 2 tests.
-// End 2 tests from 1 test case ran. (4 ms total)
+// Test results:
+//   SUCCEED 2 tests.
+// End 2 tests from 1 test case ran. (0 ms total)
