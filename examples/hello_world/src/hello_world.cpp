3dad4d1c0b7a13a2805f32310fd0bbd2d8af1858
Gammasoft71 <gammasoft71@gmail.com>
2019-03-12 09:29:00 +0100

    update submodules

--- examples/hello_world/src/hello_world.cpp
+++ examples/hello_world/src/hello_world.cpp
@@ -1,14 +1,31 @@
 #include <xtd/console>
 
+using namespace std;
 using namespace xtd;
 
 // The main entry point for the application.
 int main() {
-  console::foreground_color(console_color::blue);
-  console::write_line("%s", "Hello, World!");
+  const char* s1 = "Hello,";
+  string s2 = "World!";
+  
+  console::out << background_color(console_color::cyan);
+  cout << foreground_color(console_color::yellow);
+  cout << format("%s%7s", s1, s2);
+  cout << reset_color() << endl;
+  
+  console::out << background_color(console_color::white);
+  console::out << foreground_color(console_color::black);
+  console::out << format("%s%7s", s1, s2);
+  console::out << reset_color() << endl;
+  
+  console::background_color(console_color::blue);
+  console::foreground_color(console_color::white);
+  console::write_line("%s%7s", s1, s2);
   console::reset_color();
 }
 
-// This code produces the following output in blue :
+// This code produces the following output with colors :
 //
+// Hello, World!
+// Hello, World!
 // Hello, World!
