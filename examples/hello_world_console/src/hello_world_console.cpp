761c1605f8094422b5014bb3e7f29703db9cc3d0
Gammasoft71 <gammasoft71@gmail.com>
2019-03-12 09:28:34 +0100

    change examples

--- examples/hello_world_console/src/hello_world_console.cpp
+++ examples/hello_world_console/src/hello_world_console.cpp
@@ -9,9 +9,15 @@ int main() {
   const char* s1 = "Hello,";
   string s2 = "World!";
   
-  cout << foreground_color(console_color::green);
+  console::out << background_color(console_color::cyan);
+  cout << foreground_color(console_color::yellow);
   cout << format("%s%7s", s1, s2);
   cout << reset_color() << endl;
+  
+  console::out << background_color(console_color::white);
+  console::out << foreground_color(console_color::black);
+  console::out << format("%s%7s", s1, s2);
+  console::out << reset_color() << endl;
   
   console::background_color(console_color::blue);
   console::foreground_color(console_color::white);
@@ -21,5 +27,6 @@ int main() {
 
 // This code produces the following output with colors :
 //
+// Hello, World!
 // Hello, World!
 // Hello, World!
