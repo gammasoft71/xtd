73a0b20dfe773e69eabcfef8b64e76131759d70e
Gammasoft71 <gammasoft71@gmail.com>
2019-03-24 21:08:32 +0100

    add formats

--- examples/format/src/format.cpp
+++ examples/format/src/format.cpp
@@ -7,7 +7,7 @@ using namespace xtd;
 
 // The main entry point for the application.
 int main() {
-  cout << strings::format("%10s %s %03d %c", "string", "literal"s, 42, 'a') << endl;
+  cout << strings::formatf("%10s %s %03d %c", "string", "literal"s, 42, 'a') << endl;
 }
 
 // This code produces the following output:
--- xtd.strings/include/xtd/to_string.hpp
+++ xtd.strings/include/xtd/to_string.hpp
@@ -8,52 +8,117 @@
 #include "string_split_options.hpp"
 
 #include <algorithm>
+#include <cmath>
 #include <iomanip>
 #include <locale>
 #include <sstream>
 #include "strings.hpp"
 
 template<typename Char, typename Value>
-std::basic_string<Char> __format_to_string(const std::basic_string<Char>& fmt, Value value) {return xtd::strings::formatf(xtd::strings::concat<Char, Char, std::basic_string<Char>>(static_cast<Char>('%'), fmt), value);}
+std::basic_string<Char> __default_format(Value value) {throw std::invalid_argument("Invalid format numeric type");}
+
+template<typename Char>
+std::basic_string<Char> __default_format(int value) {return std::basic_string<Char>({static_cast<Char>('%'), static_cast<Char>('d')});}
+template<typename Char>
+std::basic_string<Char> __default_format(unsigned int value) {return std::basic_string<Char>({static_cast<Char>('%'), static_cast<Char>('u')});}
+template<typename Char>
+std::basic_string<Char> __default_format(long value) {return std::basic_string<Char>({static_cast<Char>('%'), static_cast<Char>('l'), static_cast<Char>('d')});}
+template<typename Char>
+std::basic_string<Char> __default_format(unsigned long value) {return std::basic_string<Char>({static_cast<Char>('%'), static_cast<Char>('l'), static_cast<Char>('u')});}
+template<typename Char>
+std::basic_string<Char> __default_format(long long value) {return std::basic_string<Char>({static_cast<Char>('%'), static_cast<Char>('l'), static_cast<Char>('l'), static_cast<Char>('d')});}
+template<typename Char>
+std::basic_string<Char> __default_format(unsigned long long value) {return std::basic_string<Char>({static_cast<Char>('%'), static_cast<Char>('l'), static_cast<Char>('l'), static_cast<Char>('u')});}
+template<typename Char>
+std::basic_string<Char> __default_format(float value) {return std::basic_string<Char>({static_cast<Char>('%'), static_cast<Char>('.'), static_cast<Char>('2'), static_cast<Char>('f')});}
+template<typename Char>
+std::basic_string<Char> __default_format(double value) {return std::basic_string<Char>({static_cast<Char>('%'), static_cast<Char>('.'), static_cast<Char>('2'), static_cast<Char>('f')});}
+template<typename Char>
+std::basic_string<Char> __default_format(long double value) {return std::basic_string<Char>({static_cast<Char>('%'), static_cast<Char>('.'), static_cast<Char>('2'), static_cast<Char>('L'), static_cast<Char>('f')});}
+
+template<typename Char, typename Value>
+std::basic_string<Char> __numeric_formater(const std::basic_string<Char>& fmt, Value value) {
+  throw std::invalid_argument("Invalid format expression");
+}
+
+template<typename Char>
+std::basic_string<Char> __put_money(long double value, size_t precision) {
+  std::basic_stringstream<Char> ss;
+  ss.imbue(std::locale(std::locale().name() == "" || std::locale().name() == "C" ? "en_US.UTF-8" : std::locale().name().c_str()));
+  ss << std::showbase << std::setprecision(precision) << std::put_money(value*100);
+  return ss.str();
+}
+
+template<typename Char>
+std::basic_string<Char> __numeric_formater(const std::basic_string<Char>& fmt, int value) {
+  if (fmt.empty()) return xtd::strings::formatf(std::basic_string<Char>({static_cast<Char>('%'), static_cast<Char>('d')}), value);
+  
+  size_t precision = 0;
+  if (fmt.size() > 1) precision = std::stoi(fmt.substr(1));
+  if ((fmt[0] == 'd' || fmt[0] == 'D') && precision > 0 && value < 0) precision += 1;
+  
+  switch (fmt[0]) {
+    case static_cast<Char>('c'):
+    case static_cast<Char>('C'): return __put_money<Char>(static_cast<long double>(value), precision == 0 ? 2 : precision);
+    case static_cast<Char>('d'):
+    case static_cast<Char>('D'): return xtd::strings::formatf(std::basic_string<Char>({static_cast<Char>('%'), static_cast<Char>('0'), static_cast<Char>('*')}) + std::basic_string<Char>({static_cast<Char>('d')}), precision, value);
+    case static_cast<Char>('e'): return xtd::strings::formatf(std::basic_string<Char>({static_cast<Char>('%'), static_cast<Char>('.'), static_cast<Char>('*')}) + std::basic_string<Char>({static_cast<Char>('e')}), precision == 0 ? 6 : precision, static_cast<double>(value));
+    case static_cast<Char>('E'): return xtd::strings::formatf(std::basic_string<Char>({static_cast<Char>('%'), static_cast<Char>('.'), static_cast<Char>('*')}) + std::basic_string<Char>({static_cast<Char>('E')}), precision == 0 ? 6 : precision, static_cast<double>(value));
+    case static_cast<Char>('f'): return xtd::strings::formatf(std::basic_string<Char>({static_cast<Char>('%'), static_cast<Char>('.'), static_cast<Char>('*')}) + std::basic_string<Char>({static_cast<Char>('f')}), precision == 0 ? 2 : precision, static_cast<double>(value));
+    case static_cast<Char>('F'): return xtd::strings::formatf(std::basic_string<Char>({static_cast<Char>('%'), static_cast<Char>('.'), static_cast<Char>('*')}) + std::basic_string<Char>({static_cast<Char>('F')}), precision == 0 ? 2 : precision, static_cast<double>(value));
+    case static_cast<Char>('x'): return xtd::strings::formatf(std::basic_string<Char>({static_cast<Char>('%'), static_cast<Char>('0'), static_cast<Char>('*')}) + std::basic_string<Char>({static_cast<Char>('x')}), precision, value);
+    case static_cast<Char>('X'): return xtd::strings::formatf(std::basic_string<Char>({static_cast<Char>('%'), static_cast<Char>('0'), static_cast<Char>('*')}) + std::basic_string<Char>({static_cast<Char>('X')}), precision, value);
+  }
+  throw std::invalid_argument("Invalid format expression");
+}
+
+template<typename Char, typename Value>
+std::basic_string<Char> __string_formater(const std::basic_string<Char>& fmt, Value value) {
+  int precision = 0;
+  if (fmt[0] == static_cast<Char>(',') && fmt.size() > 1) precision = std::stoi(fmt.substr(1));
+  if (precision > 0) return xtd::strings::formatf(std::basic_string<Char>{static_cast<Char>('%'), static_cast<Char>('s')}, xtd::strings::pad_left(value, precision));
+  if (precision < 0) return xtd::strings::formatf(std::basic_string<Char>{static_cast<Char>('%'), static_cast<Char>('s')}, xtd::strings::pad_right(value, std::abs(precision)));
+  return xtd::strings::formatf(std::basic_string<Char>{static_cast<Char>('%'), static_cast<Char>('s')}, value);
+}
 
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
-  inline std::string to_string(int value, const std::string& fmt) {return __format_to_string(fmt, value);}
-  inline std::string to_string(int value) {return to_string(value, "d");}
-
-  inline std::string to_string(unsigned int value, const std::string& fmt) {return __format_to_string(fmt, value);}
-  inline std::string to_string(unsigned int value) {return to_string(value, "u");}
-
+  inline std::string to_string(int value, const std::string& fmt) {return __numeric_formater(fmt, value);}
+  inline std::string to_string(int value) {return to_string(value, "");}
+  
+  inline std::string to_string(unsigned int value, const std::string& fmt) {return __numeric_formater(fmt, value);}
+  inline std::string to_string(unsigned int value) {return to_string(value, "");}
+  
   inline std::string to_string(long value, const std::string& fmt) {return strings::formatf(fmt, value);}
-  inline std::string to_string(long value) {return to_string(value, "ld");}
-
-  inline std::string to_string(unsigned long value, const std::string& fmt) {return __format_to_string(fmt, value);}
-  inline std::string to_string(unsigned long value) {return to_string(value, "lu");}
-
+  inline std::string to_string(long value) {return to_string(value, "");}
+  
+  inline std::string to_string(unsigned long value, const std::string& fmt) {return __numeric_formater(fmt, value);}
+  inline std::string to_string(unsigned long value) {return to_string(value, "");}
+  
   inline std::string to_string(long long value, const std::string& fmt) {return strings::formatf(fmt, value);}
-  inline std::string to_string(long long value) {return to_string(value, "lld");}
-
-  inline std::string to_string(unsigned long long value, const std::string& fmt) {return __format_to_string(fmt, value);}
-  inline std::string to_string(unsigned long long value) {return to_string(value, "llu");}
-
-  inline std::string to_string(float value, const std::string& fmt) {return __format_to_string(fmt, value);}
-  inline std::string to_string(float value) {return to_string(value, ".2f");}
-
-  inline std::string to_string(double value, const std::string& fmt) {return __format_to_string(fmt, value);}
+  inline std::string to_string(long long value) {return to_string(value, "");}
+  
+  inline std::string to_string(unsigned long long value, const std::string& fmt) {return __numeric_formater(fmt, value);}
+  inline std::string to_string(unsigned long long value) {return to_string(value, "");}
+  
+  inline std::string to_string(float value, const std::string& fmt) {return __numeric_formater(fmt, value);}
+  inline std::string to_string(float value) {return to_string(value, "");}
+  
+  inline std::string to_string(double value, const std::string& fmt) {return __numeric_formater(fmt, value);}
   inline std::string to_string(double value) {return to_string(value, ".2f");}
-
-  inline std::string to_string(long double value, const std::string& fmt) {return __format_to_string(fmt, value);}
-  inline std::string to_string(long double value) {return to_string(value, ".2f");}
-
-  inline std::string to_string(const std::string& value, const std::string& fmt) {return __format_to_string(fmt, value);}
+  
+  inline std::string to_string(long double value, const std::string& fmt) {return __numeric_formater(fmt, value);}
+  inline std::string to_string(long double value) {return to_string(value, "");}
+  
+  inline std::string to_string(const std::string& value, const std::string& fmt) {return __string_formater(fmt, value);}
   inline std::string to_string(const std::string& value) {return to_string(value, "");}
-
-  inline std::string to_string(const xtd::istring& value, const std::string& fmt) {return __format_to_string(fmt, value);}
-  inline std::string to_string(const xtd::istring& value) {return to_string(value, "s");}
-
-  inline std::string to_string(const char* value, const std::string& fmt) {return __format_to_string(fmt, value);}
-  inline std::string to_string(const char* value) {return to_string(value, "s");}
-
+  
+  inline std::string to_string(const xtd::istring& value, const std::string& fmt) {return __string_formater(fmt, value);}
+  inline std::string to_string(const xtd::istring& value) {return to_string(value, "");}
+  
+  inline std::string to_string(const char* value, const std::string& fmt) {return __string_formater(fmt, value);}
+  inline std::string to_string(const char* value) {return to_string(value, "");}
+  
   template<typename Value>
   inline std::string to_string(Value value) {
     std::stringstream ss;
@@ -62,66 +127,66 @@ namespace xtd {
   }
   template<typename Value>
   inline std::string to_string(const Value& value, const std::string& fmt) {throw std::invalid_argument("to_string speciailisation not found");}
-
-  template<typename Char>
-  inline std::basic_string<Char> to_string(int value, const std::basic_string<Char>& fmt) {return __format_to_string(fmt, value);}
-  template<typename Char>
-  inline std::basic_string<Char> to_string(int value) {return to_string<Char>(value, std::basic_string<Char>({static_cast<Char>('d')}));}
   
   template<typename Char>
-  inline std::basic_string<Char> to_string(unsigned int value, const std::basic_string<Char>& fmt) {return __format_to_string(fmt, value);}
+  inline std::basic_string<Char> to_string(int value, const std::basic_string<Char>& fmt) {return __numeric_formater(fmt, value);}
   template<typename Char>
-  inline std::basic_string<Char> to_string(unsigned int value) {return to_string<Char>(value, std::basic_string<Char>({static_cast<Char>('u')}));}
+  inline std::basic_string<Char> to_string(int value) {return to_string<Char>(value, xtd::strings::empty<Char>());}
   
   template<typename Char>
-  inline std::basic_string<Char> to_string(long value, const std::basic_string<Char>& fmt) {return strings::formatf(fmt, value);}
+  inline std::basic_string<Char> to_string(unsigned int value, const std::basic_string<Char>& fmt) {return __numeric_formater(fmt, value);}
   template<typename Char>
-  inline std::basic_string<Char> to_string(long value) {return to_string<Char>(value, std::basic_string<Char>({static_cast<Char>('l'), static_cast<Char>('d')}));}
+  inline std::basic_string<Char> to_string(unsigned int value) {return to_string<Char>(value, xtd::strings::empty<Char>());}
   
   template<typename Char>
-  inline std::basic_string<Char> to_string(unsigned long value, const std::basic_string<Char>& fmt) {return __format_to_string(fmt, value);}
+  inline std::basic_string<Char> to_string(long value, const std::basic_string<Char>& fmt) {return __numeric_formater(fmt, value);}
   template<typename Char>
-  inline std::basic_string<Char> to_string(unsigned long value) {return to_string<Char>(value, std::basic_string<Char>({static_cast<Char>('l'), static_cast<Char>('u')}));}
+  inline std::basic_string<Char> to_string(long value) {return to_string<Char>(value, xtd::strings::empty<Char>());}
   
   template<typename Char>
-  inline std::basic_string<Char> to_string(long long value, const std::basic_string<Char>& fmt) {return strings::formatf(fmt, value);}
+  inline std::basic_string<Char> to_string(unsigned long value, const std::basic_string<Char>& fmt) {return __numeric_formater(fmt, value);}
   template<typename Char>
-  inline std::basic_string<Char> to_string(long long value) {return to_string<Char>(value, std::basic_string<Char>({static_cast<Char>('l'), static_cast<Char>('l'), static_cast<Char>('d')}));}
+  inline std::basic_string<Char> to_string(unsigned long value) {return to_string<Char>(value, xtd::strings::empty<Char>());}
   
   template<typename Char>
-  inline std::basic_string<Char> to_string(unsigned long long value, const std::basic_string<Char>& fmt) {return __format_to_string(fmt, value);}
+  inline std::basic_string<Char> to_string(long long value, const std::basic_string<Char>& fmt) {return __numeric_formater(fmt, value);}
   template<typename Char>
-  inline std::basic_string<Char> to_string(unsigned long long value) {return to_string<Char>(value, std::basic_string<Char>({static_cast<Char>('l'), static_cast<Char>('l'), static_cast<Char>('u')}));}
+  inline std::basic_string<Char> to_string(long long value) {return to_string<Char>(value, xtd::strings::empty<Char>());}
   
   template<typename Char>
-  inline std::basic_string<Char> to_string(float value, const std::basic_string<Char>& fmt) {return __format_to_string(fmt, value);}
+  inline std::basic_string<Char> to_string(unsigned long long value, const std::basic_string<Char>& fmt) {return __numeric_formater(fmt, value);}
   template<typename Char>
-  inline std::basic_string<Char> to_string(float value) {return to_string<Char>(value, std::basic_string<Char>({static_cast<Char>('.'), static_cast<Char>('2'), static_cast<Char>('f')}));}
+  inline std::basic_string<Char> to_string(unsigned long long value) {return to_string<Char>(value, xtd::strings::empty<Char>());}
   
   template<typename Char>
-  inline std::basic_string<Char> to_string(double value, const std::basic_string<Char>& fmt) {return __format_to_string(fmt, value);}
+  inline std::basic_string<Char> to_string(float value, const std::basic_string<Char>& fmt) {return __numeric_formater(fmt, value);}
   template<typename Char>
-  inline std::basic_string<Char> to_string(double value) {return to_string<Char>(value, std::basic_string<Char>({static_cast<Char>('.'), static_cast<Char>('2'), static_cast<Char>('f')}));}
+  inline std::basic_string<Char> to_string(float value) {return to_string<Char>(value, xtd::strings::empty<Char>());}
   
   template<typename Char>
-  inline std::basic_string<Char> to_string(long double value, const std::basic_string<Char>& fmt) {return __format_to_string(fmt, value);}
+  inline std::basic_string<Char> to_string(double value, const std::basic_string<Char>& fmt) {return __numeric_formater(fmt, value);}
   template<typename Char>
-  inline std::basic_string<Char> to_string(long double value) {return to_string<Char>(value, std::basic_string<Char>({static_cast<Char>('.'), static_cast<Char>('2'), static_cast<Char>('f')}));}
+  inline std::basic_string<Char> to_string(double value) {return to_string<Char>(value, xtd::strings::empty<Char>());}
   
   template<typename Char>
-  inline std::basic_string<Char> to_string(const std::basic_string<Char>& value, const std::basic_string<Char>& fmt) {return __format_to_string(fmt, value);}
+  inline std::basic_string<Char> to_string(long double value, const std::basic_string<Char>& fmt) {return __numeric_formater(fmt, value);}
+  template<typename Char>
+  inline std::basic_string<Char> to_string(long double value) {return to_string<Char>(value, xtd::strings::empty<Char>());}
+  
+  template<typename Char>
+  inline std::basic_string<Char> to_string(const std::basic_string<Char>& value, const std::basic_string<Char>& fmt) {return __string_formater(fmt, value);}
   template<typename Char>
   inline std::basic_string<Char> to_string(const std::basic_string<Char>& value) {return to_string<Char>(value, xtd::strings::empty<Char>());}
   
   template<typename Char>
-  inline std::basic_string<Char> to_string(const xtd::immutable_basic_string<Char>& value, const std::basic_string<Char>& fmt) {return __format_to_string(fmt, value);}
+  inline std::basic_string<Char> to_string(const xtd::immutable_basic_string<Char>& value, const std::basic_string<Char>& fmt) {return __string_formater(fmt, value);}
   template<typename Char>
-  inline std::basic_string<Char> to_string(const xtd::immutable_basic_string<Char>& value) {return to_string<Char>(value, std::basic_string<Char>({static_cast<Char>('s')}));}
+  inline std::basic_string<Char> to_string(const xtd::immutable_basic_string<Char>& value) {return to_string<Char>(value, xtd::strings::empty<Char>());}
   
   template<typename Char>
-  inline std::basic_string<Char> to_string(const char* value, const std::basic_string<Char>& fmt) {return __format_to_string(fmt, value);}
+  inline std::basic_string<Char> to_string(const Char* value, const std::basic_string<Char>& fmt) {return __string_formater(fmt, value);}
   template<typename Char>
-  inline std::basic_string<Char> to_string(const char* value) {return to_string<Char>(value, std::basic_string<Char>({static_cast<Char>('s')}));}
+  inline std::basic_string<Char> to_string(const Char* value) {return to_string<Char>(value, xtd::strings::empty<Char>());}
   
   template<typename Char, typename Value>
   inline std::basic_string<Char> to_string(Value value) {
@@ -132,3 +197,4 @@ namespace xtd {
   template<typename Char, typename Value>
   inline std::basic_string<Char> to_string(const Value& value, const std::basic_string<Char>& fmt) {throw std::invalid_argument("to_string speciailisation not found");}
 }
+
