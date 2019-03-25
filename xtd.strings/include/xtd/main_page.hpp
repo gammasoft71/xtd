bca4aa331907207166dec6ef1583238acc4c5000
Gammasoft71 <gammasoft71@gmail.com>
2019-03-25 01:12:00 +0100

    add docs

--- xtd.strings/include/xtd/main_page.hpp
+++ xtd.strings/include/xtd/main_page.hpp
@@ -29,7 +29,7 @@
 ///
 /// // The main entry point for the application.
 /// int main() {
-///   cout << strings::format("%s, %s!", "Hello", "World"s) << endl;
+///   cout << strings::format("{}, {}!", "Hello", "World"s) << endl;
 /// }
 /// @endcode
 ///
