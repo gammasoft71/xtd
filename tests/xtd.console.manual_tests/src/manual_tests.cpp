535dd2fa5a25502ad73cfbfe55b37eac742cb46c
Gammasoft71 <gammasoft71@gmail.com>
2019-03-25 16:49:44 +0100

    fix build

--- tests/xtd.console.manual_tests/src/manual_tests.cpp
+++ tests/xtd.console.manual_tests/src/manual_tests.cpp
@@ -5,5 +5,6 @@ using namespace xtd;
 
 // The main entry point for the application.
 int main() {
+  console::write_line("{0:D} 0x{0:X} 0{0:O} 0b{0:B}", 42);
   cout << format("{} {}", 42, "test") << endl;
 }
