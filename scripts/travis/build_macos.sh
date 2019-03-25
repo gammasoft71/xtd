e70a2c2f21ae723149039fd2f09f5da98cdd7a33
Gammasoft71 <gammasoft71@gmail.com>
2019-03-25 18:54:49 +0100

    Update build_macos.sh

--- scripts/travis/build_macos.sh
+++ scripts/travis/build_macos.sh
@@ -6,14 +6,14 @@ pushd build
 # generate and build lib
 cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=~/local ..
 if [ $? -ne 0 ]; then exit -1; fi
-cmake --build . --target install -- -j $(nproc)
+cmake --build . --target install -- -j 8
 if [ $? -ne 0 ]; then exit -1; fi
 
 # generate and build examples
 pushd examples
 cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=~/local ../../examples
 if [ $? -ne 0 ]; then exit -1; fi
-cmake --build . -- -j $(nproc)
+cmake --build . -- -j 8
 if [ $? -ne 0 ]; then exit -1; fi
 popd
 
