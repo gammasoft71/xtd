12730a05a2104d6b850604a96e162ca60e9ffa56
Gammasoft71 <gammasoft71@gmail.com>
2019-03-26 22:47:01 +0100

    add format string unit tests

--- tests/xtd.strings.unit_tests/src/boolean_format.cpp
+++ tests/xtd.strings.unit_tests/src/boolean_format.cpp
@@ -12,19 +12,19 @@ namespace unit_tests {
     void test_method_(string_format_boolean_false_with_default_argument) {
       assert::are_equal("false", strings::format("{0}", false));
     }
-
+    
     void test_method_(string_format_boolean_true_with_default_argument) {
       assert::are_equal("true", strings::format("{0}", true));
     }
-
+    
     void test_method_(string_format_boolean_false_with_left_alignment) {
       assert::are_equal("     false", strings::format("{0,10}", false));
     }
-
+    
     void test_method_(string_format_boolean_true_with_left_alignment) {
       assert::are_equal("      true", strings::format("{0,10}", true));
     }
-
+    
     void test_method_(string_format_boolean_false_with_right_alignment) {
       assert::are_equal("false     ", strings::format("{0, -10}", false));
     }
