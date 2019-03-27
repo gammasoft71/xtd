fa6af1bd7b985651ba29c20eefa69cb2ef1c3434
Gammasoft71 <gammasoft71@gmail.com>
2019-03-27 21:55:13 +0100

    add docs

---
+++ xtd.strings/include/xtd/format_page.hpp
@@ -0,0 +1,48 @@
+/// @file
+/// @brief Contains doygen format page.
+
+/// @page FormatPage Format Documentation
+///
+/// format : {[id][,precision | :format]}
+///
+/// @section IdSection id
+///
+/// Represent argument identifier. You can specified it or not. If not, it's in automatic mode.
+/// If you specify identier, you can use it
+///
+/// Example with specifying argument identifier :
+/// @code
+/// cout << string::format("{0}, {1}, {3}", 42, 'c', "word") << endl;
+/// cout << strings::format("The {0} we {1} {1} {2} {1}.", "man", "saw", 'a') << endl; // repeated identifier
+/// @endcode
+///
+/// Result
+/// @code
+/// 42, c, word
+/// The man saw saw a saw.
+/// @endcode
+///
+/// Example without specifying argument identifier :
+/// @code
+/// cout << string::format("{} {} {}", 1, "two", 3.0) << endl;
+/// cout << strings::format("{}, {}!", "Hello", "World") << endl;
+/// @endcode
+///
+/// Result
+/// @code
+/// 1 two 3
+/// Hello, World!
+/// @endcode
+///
+/// @section PrecisionSection precision
+///
+/// * ,precision : Begin with , and contains an integer value positive or negative
+///                ** positivie value : pad left
+///                ** negative value : pad rigth
+/// @par Example
+///
+/// @code
+/// strings.format({0,10}
+/// @endcode
+///
+/// @section FormatSEction format
