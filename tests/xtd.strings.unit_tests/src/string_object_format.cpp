96d15b90b046284239c21cdd251310488c3b37b8
Gammasoft71 <gammasoft71@gmail.com>
2019-04-01 14:02:46 +0200

    replace my_object by point

--- tests/xtd.strings.unit_tests/src/string_object_format.cpp
+++ tests/xtd.strings.unit_tests/src/string_object_format.cpp
@@ -6,69 +6,66 @@ using namespace std::string_literals;
 using namespace xtd;
 using namespace xtd::tunit;
 
-class my_object {
+class point {
 public:
-  my_object() = default;
-  my_object(const my_object&) = default;
-  my_object(int a, int b) : value_a_(a), value_b_(b) {}
-  
-  int value_a() const {return this->value_a_;}
-  int value_b() const {return this->value_b_;}
-  
-  template<typename Char>
-  friend std::basic_ostream<Char>& operator<<(std::basic_ostream<Char>& os, const my_object& value) {
-    os << Char('[');
-    os << value.value_a();
-    os << Char(' ');
-    os << value.value_b();
-    os << Char(']');
-    return os;
-  }
-  
-private:
-  int value_a_ = 0;
-  int value_b_ = 0;
+  int x = 0;
+  int y = 0;
 };
+
+template<typename Char>
+std::basic_ostream<Char>& operator<<(std::basic_ostream<Char>& os, const point& p) {
+  return os << xtd::to_string(p, strings::empty<Char>());
+}
 
 namespace xtd {
   template<>
-  std::string to_string(const my_object& value, const std::string& fmt, const std::locale& loc) {
+  std::string to_string(const point& p, const std::string& fmt, const std::locale& loc) {
+    if (fmt.empty()) return xtd::strings::format("({}, {})", p.x, p.y);
     if (fmt.size() > 1) throw std::invalid_argument("Format can contains only one character.");
-    std::stringstream ss;
-    ss << "[";
-    ss << value.value_a();
-    ss << fmt[0];
-    ss << value.value_b();
-    ss << "]";
-    return ss.str();
+
+    switch (fmt[0]) {
+      case 'l': return xtd::strings::format("(x={}, y={})", p.x, p.y);
+      case 'L': return xtd::strings::format("(X={}, Y={})", p.x, p.y);
+      case 'g':
+      case 'G': return xtd::strings::format("({}, {})", p.x, p.y);
+      default: throw std::invalid_argument("Format invalid argument.");
+    }
   }
 }
 
 namespace unit_tests {
   class test_class_(test_string_object_format) {
   public:
-    void test_method_(format_my_object_with_default_argument) {
-      assert::are_equal("[42 24]", xtd::strings::format("{0}", my_object(42, 24)));
+    void test_method_(format_point_with_default_argument) {
+      assert::are_equal("(42, 24)", xtd::strings::format("{0}", point {42, 24 }));
     }
     
-    void test_method_(format_my_object_with_left_alignment) {
-      assert::are_equal("   [42 24]", strings::format("{0,10}", my_object(42, 24)));
+    void test_method_(format_point_with_left_alignment) {
+      assert::are_equal("  (42, 24)", strings::format("{0,10}", point {42, 24 }));
     }
     
-    void test_method_(format_my_object_with_right_alignment) {
-      assert::are_equal("[42 24]   ", strings::format("{0, -10}", my_object(42, 24)));
+    void test_method_(format_point_with_right_alignment) {
+      assert::are_equal("(42, 24)  ", strings::format("{0, -10}", point {42, 24 }));
     }
     
-    void test_method_(format_my_object_with_zero_alignment) {
-      assert::are_equal("[42 24]", strings::format("{0,0}", my_object(42, 24)));
+    void test_method_(format_point_with_zero_alignment) {
+      assert::are_equal("(42, 24)", strings::format("{0,0}", point {42, 24 }));
     }
     
-    void test_method_(format_my_object_with_one_character_argument) {
-      assert::are_equal("[42,24]", xtd::strings::format("{0:,}", my_object(42, 24)));
+    void test_method_(format_point_with_general_argument) {
+      assert::are_equal("(42, 24)", xtd::strings::format("{0:G}", point {42, 24 }));
     }
     
-    void test_method_(format_my_object_with_invalid_argument) {
-      assert::throws<std::invalid_argument>([]{xtd::strings::format("{0:, }", my_object(42, 24));});
+    void test_method_(format_point_with_label_argument) {
+      assert::are_equal("(x=42, y=24)", xtd::strings::format("{0:l}", point {42, 24 }));
+    }
+    
+    void test_method_(format_point_with_label_uppercase_argument) {
+      assert::are_equal("(X=42, Y=24)", xtd::strings::format("{0:L}", point {42, 24 }));
+    }
+
+    void test_method_(format_point_with_invalid_argument) {
+      assert::throws<std::invalid_argument>([]{xtd::strings::format("{0:, }", point {42, 24 });});
     }
   };
 }
