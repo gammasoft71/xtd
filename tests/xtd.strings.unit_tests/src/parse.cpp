431e335d4292e4aad07fe2f574f99bdf63285308
Gammasoft71 <gammasoft71@gmail.com>
2019-04-03 10:46:06 +0200

    parse implementation

--- tests/xtd.strings.unit_tests/src/parse.cpp
+++ tests/xtd.strings.unit_tests/src/parse.cpp
@@ -12,21 +12,5 @@ namespace unit_tests {
     void test_method_(parse_string) {
       assert::are_equal("string", xtd::parse<string>("string"));
     }
-
-    void test_method_(parse_int) {
-      assert::are_equal(42, xtd::parse<int>("42"));
-    }
-
-    void test_method_(parse_int_binary) {
-      assert::are_equal(42, xtd::parse<int>("0b101010", number_styles::binary_number));
-    }
-
-    void test_method_(parse_int_octal) {
-      assert::are_equal(42, xtd::parse<int>("052", number_styles::octal_number));
-    }
-
-    void test_method_(parse_int_hexa) {
-      assert::are_equal(42, xtd::parse<int>("0x2A", number_styles::hex_number));
-    }
   };
 }
