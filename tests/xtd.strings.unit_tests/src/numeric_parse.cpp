c31bceceaa9a91a0b58fe00f59f961e1213f5698
Gammasoft71 <gammasoft71@gmail.com>
2019-04-03 16:51:07 +0200

    add unit tests

--- tests/xtd.strings.unit_tests/src/numeric_parse.cpp
+++ tests/xtd.strings.unit_tests/src/numeric_parse.cpp
@@ -439,6 +439,106 @@ namespace unit_tests {
     void test_method_(parse_styles_number_invalid) {
       assert::throws<std::invalid_argument>([]{xtd::parse<Value>("z42", number_styles::number);});
     }
+    
+    void test_method_(parse_styles_fixed_point) {
+      assert::are_equal(static_cast<Value>(42), xtd::parse<Value>("42", number_styles::fixed_point));
+    }
+    
+    void test_method_(parse_styles_fixed_point_with_leading_white_spaces) {
+      assert::are_equal(static_cast<Value>(42), xtd::parse<Value>(" \f\n\r\t\v42", number_styles::fixed_point));
+    }
+    
+    void test_method_(parse_styles_fixed_point_with_trailing_white_spaces) {
+      assert::are_equal(static_cast<Value>(42), xtd::parse<Value>("42 \f\n\r\t\v", number_styles::fixed_point));
+    }
+    
+    void test_method_(parse_styles_fixed_point_with_leading_plus_sign) {
+      assert::are_equal(static_cast<Value>(42), xtd::parse<Value>("+42", number_styles::fixed_point));
+    }
+    
+    void test_method_(parse_styles_fixed_point_with_two_leading_plus_sign) {
+      assert::throws<std::invalid_argument>([]{xtd::parse<Value>("++42", number_styles::fixed_point);});
+    }
+    
+    void test_method_(parse_styles_fixed_point_with_leading_minus_sign) {
+      if (std::is_signed<Value>::value)
+        assert::are_equal(static_cast<Value>(-42), xtd::parse<Value>("-42", number_styles::fixed_point));
+      else
+        assert::throws<std::invalid_argument>([]{xtd::parse<Value>("-42", number_styles::fixed_point);});
+    }
+    
+    void test_method_(parse_styles_fixed_point_with_two_leading_minus_sign) {
+      assert::throws<std::invalid_argument>([]{xtd::parse<Value>("--42", number_styles::fixed_point);});
+    }
+    
+    void test_method_(parse_styles_fixed_point_with_trailing_plus_sign) {
+      assert::throws<std::invalid_argument>([]{xtd::parse<Value>("42+", number_styles::fixed_point);});
+    }
+    
+    void test_method_(parse_styles_fixed_point_with_two_trailing_plus_sign) {
+      assert::throws<std::invalid_argument>([]{xtd::parse<Value>("42++", number_styles::fixed_point);});
+    }
+    
+    void test_method_(parse_styles_fixed_point_with_trailing_minus_sign) {
+      if (std::is_signed<Value>::value)
+        assert::throws<std::invalid_argument>([]{xtd::parse<Value>("42-", number_styles::fixed_point);});
+      else
+        assert::throws<std::invalid_argument>([]{xtd::parse<Value>("42-", number_styles::fixed_point);});
+    }
+    
+    void test_method_(parse_styles_fixed_point_with_two_trailing_minus_sign) {
+      assert::throws<std::invalid_argument>([]{xtd::parse<Value>("42--", number_styles::fixed_point);});
+    }
+    
+    void test_method_(parse_styles_fixed_point_with_parentheses) {
+      assert::throws<std::invalid_argument>([]{xtd::parse<Value>("(42)", number_styles::fixed_point);});
+    }
+    
+    void test_method_(parse_styles_fixed_point_with_two_parentheses) {
+      assert::throws<std::invalid_argument>([]{xtd::parse<Value>("((42))", number_styles::fixed_point);});
+    }
+    
+    void test_method_(parse_styles_fixed_point_with_decimal_point) {
+      assert::are_equal(static_cast<Value>(42), xtd::parse<Value>("42.0", number_styles::fixed_point));
+    }
+    
+    void test_method_(parse_styles_fixed_point_with_two_decimal_point) {
+      assert::throws<std::invalid_argument>([]{xtd::parse<Value>("4.2.0", number_styles::fixed_point);});
+    }
+    
+    void test_method_(parse_styles_fixed_point_with_thousands) {
+      if (!std::is_same<Value, int8_t>::value && !std::is_same<Value, char>::value && !std::is_same<Value, unsigned char>::value)
+        assert::throws<std::invalid_argument>([]{xtd::parse<Value>("1,234", number_styles::fixed_point);});
+    }
+    
+    void test_method_(parse_styles_fixed_point_with_two_thousands) {
+      if (!std::is_same<Value, int8_t>::value && !std::is_same<Value, char>::value && !std::is_same<Value, unsigned char>::value)
+        assert::throws<std::invalid_argument>([]{xtd::parse<Value>("1,,234", number_styles::fixed_point);});
+    }
+    
+    void test_method_(parse_styles_fixed_point_with_exponent) {
+      assert::are_equal(static_cast<Value>(40), xtd::parse<Value>("4E+01", number_styles::fixed_point));
+    }
+    
+    void test_method_(parse_styles_fixed_point_with_currency_symbol) {
+      assert::throws<std::invalid_argument>([]{xtd::parse<Value>("$42", number_styles::fixed_point);});
+    }
+    
+    void test_method_(parse_styles_fixed_point_with_binary_specifier) {
+      assert::throws<std::invalid_argument>([]{xtd::parse<Value>("0b42", number_styles::fixed_point);});
+    }
+    
+    void test_method_(parse_styles_fixed_point_with_octal_specifier) {
+      assert::are_equal(static_cast<Value>(42), xtd::parse<Value>("042", number_styles::fixed_point));
+    }
+    
+    void test_method_(parse_styles_fixed_point_with_hexa_specifier) {
+      assert::throws<std::invalid_argument>([]{xtd::parse<Value>("0x42", number_styles::fixed_point);});
+    }
+    
+    void test_method_(parse_styles_fixed_point_invalid) {
+      assert::throws<std::invalid_argument>([]{xtd::parse<Value>("z42", number_styles::fixed_point);});
+    }
 
     void test_method_(parse_with_number_styles_binary) {
       assert::are_equal(static_cast<Value>(42), xtd::parse<Value>("0b101010", number_styles::binary_number));
