63c14f953bf4647a8bf4d1b39fe0de65faf7c988
Gammasoft71 <gammasoft71@gmail.com>
2019-04-03 22:01:11 +0200

    fix currency

--- tests/xtd.strings.manual_tests/src/manual_tests.cpp
+++ tests/xtd.strings.manual_tests/src/manual_tests.cpp
@@ -12,9 +12,25 @@ using namespace xtd::string_literals;
 
 // The main entry point for the application.
 int main() {
+  cout << to_string(325, "C", std::locale("en_US.UTF-8")) << endl;
   std::locale::global(std::locale("en_US.UTF-8"));
-  cout << std::stold("4.2.1") << endl;
-  cout << xtd::parse<int>("4.2.1", xtd::number_styles::number) << endl;
+  cout << to_string(325, "C") << endl;
+  cout << strings::format("{:C}", 325) << endl;
+  
+  cout << to_string(325, "C", std::locale("fr_FR.UTF-8")) << endl;
+  std::locale::global(std::locale("fr_FR.UTF-8"));
+  cout << to_string(325, "C") << endl;
+  cout << strings::format("{:C}", 325) << endl;
+  
+  cout << to_string(325, "C", std::locale("ja_JP.UTF-8")) << endl;
+  std::locale::global(std::locale("ja_JP.UTF-8"));
+  cout << to_string(325, "C") << endl;
+  cout << strings::format("{:C}", 325) << endl;
+  
+  cout << to_string(325, "C", std::locale("fi_FI.UTF-8")) << endl;
+  std::locale::global(std::locale("fi_FI.UTF-8"));
+  cout << to_string(325, "C") << endl;
+  cout << strings::format("{:C}", 325) << endl;
 }
 
 // This code produces the following output:
