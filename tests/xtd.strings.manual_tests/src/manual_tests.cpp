7ad947778e91d8f043ea1f79e17a9d556e052720
Gammasoft71 <gammasoft71@gmail.com>
2019-04-13 11:31:35 +0200

    fix character format

--- tests/xtd.strings.manual_tests/src/manual_tests.cpp
+++ tests/xtd.strings.manual_tests/src/manual_tests.cpp
@@ -9,30 +9,8 @@ using namespace std::chrono_literals;
 int main() {
   //cout << "Hello, World!" << endl;
 
-  cout << xtd::strings::format(" : {}", 2h + 3min + 32s) << endl;
-  cout << xtd::strings::format("c: {:c}", 2h + 3min + 32s) << endl;
-  cout << xtd::strings::format("f: {:f}", 2h + 3min + 32s) << endl;
-  cout << xtd::strings::format("F: {:F}", 2h + 3min + 32s) << endl;
-  cout << xtd::strings::format("g: {:g}", 2h + 3min + 32s) << endl;
-  cout << xtd::strings::format("G: {:G}", 2h + 3min + 32s) << endl;
-
-  cout << endl;
-
-  wcout << xtd::strings::format(L" : {}", 26h + 3min + 32s + 24ms + 500ns) << endl;
-  wcout << xtd::strings::format(L"c: {:c}", 26h + 3min + 32s + 24ms + 500ns) << endl;
-  wcout << xtd::strings::format(L"f: {:f}", 26h + 3min + 32s + 24ms + 500ns) << endl;
-  wcout << xtd::strings::format(L"F: {:F}", 26h + 3min + 32s + 24ms + 500ns) << endl;
-  wcout << xtd::strings::format(L"g: {:g}", 26h + 3min + 32s + 24ms + 500ns) << endl;
-  wcout << xtd::strings::format(L"G: {:G}", 26h + 3min + 32s + 24ms + 500ns) << endl;
-
-  cout << endl;
-  
-  wcout << xtd::strings::format(L" : {}", 1045623.6442ms) << endl;
-  wcout << xtd::strings::format(L"c: {:c}", 1045623.6442ms) << endl;
-  wcout << xtd::strings::format(L"f: {:f}", 1045623.6442ms) << endl;
-  wcout << xtd::strings::format(L"F: {:F}", 1045623.6442ms) << endl;
-  wcout << xtd::strings::format(L"g: {:g}", 1045623.6442ms) << endl;
-  wcout << xtd::strings::format(L"G: {:G}", 1045623.6442ms) << endl;
+  char32_t a = 'a';
+  cout << xtd::strings::format("'{}'", a) << endl;
 }
 
 // This code produces the following output:
