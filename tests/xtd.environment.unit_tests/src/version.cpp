69432f97f7e5a41d037675185d0df4d052c3fdb7
Gammasoft71 <gammasoft71@gmail.com>
2019-04-21 23:15:49 +0200

    add unit tests

--- tests/xtd.environment.unit_tests/src/version.cpp
+++ tests/xtd.environment.unit_tests/src/version.cpp
@@ -1,5 +1,6 @@
 #include <xtd/version.hpp>
 #include <xtd/tunit>
+#include <sstream>
 
 using namespace std;
 using namespace xtd;
@@ -109,6 +110,84 @@ namespace unit_tests {
 
     void test_method_(parse_version_with_bad_value) {
       assert::throws_(invalid_argument, [] {version::parse("one");});
+    }
+
+    void test_method_(try_parse_version_with_good_string_value) {
+      version vers;
+      assert::is_true_(version::try_parse("1.2.3.4", vers));
+      assert::are_equal_(version(1, 2, 3, 4), vers);
+    }
+
+    void test_method_(try_parse_version_with_bad_string_value) {
+      version vers;
+      assert::is_false_(version::try_parse("one", vers));
+      assert::are_equal_(version(), vers);
+    }
+
+    void test_method_(to_string_with_empty_version) {
+      assert::are_equal_("0.0", version().to_string());
+    }
+
+    void test_method_(to_string_with_major_and_minor) {
+      assert::are_equal_("1.2", version(1, 2).to_string());
+    }
+
+    void test_method_(to_string_with_major_minor_and_build) {
+      assert::are_equal_("1.2.3", version(1, 2, 3).to_string());
+    }
+
+    void test_method_(to_string_with_major_minor_build_and_revision) {
+      assert::are_equal_("1.2.3.4", version(1, 2, 3, 4).to_string());
+    }
+
+    void test_method_(to_string_field_0_with_empty_version) {
+      assert::are_equal_("", version().to_string(0));
+    }
+    
+    void test_method_(to_string_field_0_with_full_version) {
+      assert::are_equal_("", version(1, 2, 3, 4).to_string(0));
+    }
+
+    void test_method_(to_string_field_1_with_empty_version) {
+      assert::are_equal_("0", version().to_string(1));
+    }
+    
+    void test_method_(to_string_field_1_with_full_version) {
+      assert::are_equal_("1", version(1, 2, 3, 4).to_string(1));
+    }
+
+    void test_method_(to_string_field_2_with_empty_version) {
+      assert::are_equal_("0.0", version().to_string(2));
+    }
+    
+    void test_method_(to_string_field_2_with_full_version) {
+      assert::are_equal_("1.2", version(1, 2, 3, 4).to_string(2));
+    }
+
+    void test_method_(to_string_field_3_with_empty_version) {
+      assert::throws_(invalid_argument, [] {version().to_string(3);});
+    }
+    
+    void test_method_(to_string_field_3_with_full_version) {
+      assert::are_equal_("1.2.3", version(1, 2, 3, 4).to_string(3));
+    }
+
+    void test_method_(to_string_field_4_with_empty_version) {
+      assert::throws_(invalid_argument, [] {version().to_string(4);});
+    }
+    
+    void test_method_(to_string_field_4_with_full_version) {
+      assert::are_equal_("1.2.3.4", version(1, 2, 3, 4).to_string(4));
+    }
+
+    void test_method_(to_string_field_greater_than_4) {
+      assert::throws_(invalid_argument, [] {version().to_string(5);});
+    }
+
+    void test_method_(ostream_operator) {
+      stringstream ss;
+      ss << version(1, 2, 3, 4);
+      assert::are_equal_("1.2.3.4", ss.str());
     }
   };
 }
