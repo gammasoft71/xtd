c6aa2a3913e1d343da7447b15efd6411e03c0ee7
Gammasoft71 <gammasoft71@gmail.com>
2019-05-09 22:55:34 +0200

    add docs

--- tests/xtd.io.manual_tests/src/manual_tests.cpp
+++ tests/xtd.io.manual_tests/src/manual_tests.cpp
@@ -6,10 +6,6 @@ using namespace std;
 using namespace xtd::io;
 
 int main(int argc, char* argv[]) {
-  string file_name = path::combine(path::get_temp_path(), "file.txt");
-  
-  file::write_all_text(file_name, "Hello, World!");
-  cout << file::read_all_text(file_name) << endl;
-  
-  file::remove(file_name);
+  cout << xtd::io::path::get_file_name("c:\\temp\\File.ext") << endl;
+  cout << xtd::io::path::get_file_name("/temp/File.ext") << endl;
 }
