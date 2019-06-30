53045cba68516222c1f27e5c007853e67b678bc2
Gammasoft71 <gammasoft71@gmail.com>
2019-06-30 22:01:37 +0200

    Update point.cpp

--- tests/xtd.drawing.unit_tests/src/point.cpp
+++ tests/xtd.drawing.unit_tests/src/point.cpp
@@ -23,5 +23,63 @@ namespace unit_tests {
       assert::are_equal(1, p.x());
       assert::are_equal(2, p.y());
     }
+    
+    void test_method_(create_point_with_copy) {
+      point p = {1, 2};
+      assert::are_equal(point(1, 2), p);
+      assert::are_not_equal(point::empty, p);
+      assert::are_equal(1, p.x());
+      assert::are_equal(2, p.y());
+    }
+    
+    void test_method_(create_point_and_assign_it) {
+      point p;
+      p = {1, 2};
+      assert::are_equal(point(1, 2), p);
+      assert::are_not_equal(point::empty, p);
+      assert::are_equal(1, p.x());
+      assert::are_equal(2, p.y());
+    }
+    
+    void test_method_(is_empty) {
+      assert::is_true(point().is_empty());
+      assert::is_false(point(1, 2).is_empty());
+    }
+    
+    void test_method_(offset_point) {
+      point p(1, 2);
+      p.offset({3, 4});
+      assert::are_equal(point(4, 6), p);
+    }
+    
+    void test_method_(offset_x_y) {
+      point p(1, 2);
+      p.offset(3, 4);
+      assert::are_equal(point(4, 6), p);
+    }
+    
+    void test_method_(set_x) {
+      point p;
+      p.x(3);
+      assert::are_equal(point(3, 0), p);
+    }
+    
+    void test_method_(set_y) {
+      point p;
+      p.y(4);
+      assert::are_equal(point(0, 4), p);
+    }
+    
+    void test_method_(set_x_y) {
+      point p;
+      p.x(3);
+      p.y(4);
+      assert::are_equal(point(3, 4), p);
+    }
+    
+    void test_method_(to_string) {
+      point p(1, 2);
+      assert::are_equal("{x=1, y=2}", p.to_string());
+    }
   };
 }
