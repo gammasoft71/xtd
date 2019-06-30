62cd297d52e8ece2c818501ca284147fbe14590e
Gammasoft71 <gammasoft71@gmail.com>
2019-06-30 23:47:10 +0200

    add xtd.delegate, xtd.drawing and xtd.string  submodules

--- xtd.forms/include/xtd/control.h
+++ xtd.forms/include/xtd/control.h
@@ -1,17 +1,25 @@
 #pragma once
 #include <string>
+#include <xtd/event_args.hpp>
+#include <xtd/event_handler.hpp>
+#include <xtd/point.h>
+#include <xtd/size.h>
 
 namespace xtd {
   namespace forms {
     class control {
     public:
       static const control null;
+      
       control() {}
       
       virtual intptr_t handle() const {return this->handle_;}
+      
+      virtual xtd::drawing::point location() const {return this->location_;}
+      virtual void location(const xtd::drawing::point& location);
 
       virtual const control& parent() const {return *this->parent_;}
-      virtual void parent(const control&);
+      virtual void parent(const control& parent);
 
       virtual const std::string& text() const {return this->text_;}
       virtual void text(const std::string& text);
@@ -23,12 +31,28 @@ namespace xtd {
 
       virtual void create_handle();
       
-      virtual void show() const;
+      virtual void on_location_changed(const xtd::event_args& e);
+
+      virtual void on_parent_changed(const xtd::event_args& e);
       
+      virtual void on_text_changed(const xtd::event_args& e);
+      
+      virtual void on_visible_changed(const xtd::event_args& e);
+      
+      virtual void show();
+      
+      xtd::event_handler<control> location_changed;
+      xtd::event_handler<control> parent_changed;
+      xtd::event_handler<control> text_changed;
+      xtd::event_handler<control> visible_changed;
+
     protected:
       intptr_t handle_ = 0;
+      xtd::drawing::point location_;
       control* parent_ = const_cast<control*>(&control::null);
       std::string text_;
+      xtd::drawing::size size_;
+      bool visible_ = true;
     };
   }
 }
--- xtd.forms/src/control.cpp
+++ xtd.forms/src/control.cpp
@@ -3,19 +3,25 @@
 
 const xtd::forms::control xtd::forms::control::null;
 
+
+void xtd::forms::control::location(const xtd::drawing::point& location) {
+  if (this->location_ != location) {
+    this->location_ = location;
+    this->on_location_changed(xtd::event_args::empty());
+  }
+}
+
 void xtd::forms::control::parent(const xtd::forms::control& parent) {
   if (this->parent_ != &parent) {
     this->parent_ = const_cast<xtd::forms::control*>(&parent);
-    this->create_control();
-    //for (xtd::forms::control* control : this->controls_)
-    //  control->create_control();
+    this->on_parent_changed(xtd::event_args::empty());
   }
 }
 
 void xtd::forms::control::text(const std::string& text) {
   if (this->text_ != text) {
-    native::control_api::text(this->handle_, text);
     this->text_ = text;
+    this->on_text_changed(xtd::event_args::empty());
   }
 }
 
@@ -23,7 +29,29 @@ void xtd::forms::control::create_handle() {
   this->handle_ = native::control_api::create(this->parent_->handle());
 }
 
-void xtd::forms::control::show() const {
-  native::control_api::visible(this->handle_, true);
+void xtd::forms::control::on_location_changed(const xtd::event_args &e) {
+  this->location_changed(*this, e);
+}
+
+void xtd::forms::control::on_parent_changed(const xtd::event_args &e) {
+  this->create_control();
+  //for (xtd::forms::control* control : this->controls_)
+  //  control->create_control();
+  this->parent_changed(*this, e);
+}
+
+void xtd::forms::control::on_text_changed(const xtd::event_args &e) {
+  native::control_api::text(this->handle_, this->text_);
+  this->text_changed(*this, e);
+}
+
+void xtd::forms::control::on_visible_changed(const xtd::event_args &e) {
+  native::control_api::visible(this->handle_, this->visible_);
+  this->visible_changed(*this, e);
+}
+
+void xtd::forms::control::show() {
+  this->visible_ = true;
+  this->on_visible_changed(xtd::event_args::empty());
 }
 
