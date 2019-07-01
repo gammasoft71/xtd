57c17c5027eb065169f3e7e08b8286c2840123b8
Gammasoft71 <gammasoft71@gmail.com>
2019-07-01 23:06:34 +0200

    Update form_api_universal.cpp

--- xtd.forms/src/native/form_api_universal.cpp
+++ xtd.forms/src/native/form_api_universal.cpp
@@ -1,9 +1,21 @@
 #include "form_api.hpp"
 #include <wx/app.h>
 #include <wx/frame.h>
+#include <wx/panel.h>
+
+class Form : public wxFrame {
+public:
+  Form(wxWindow *parent, wxWindowID id, const wxString& title, const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxDefaultSize, long style = wxDEFAULT_FRAME_STYLE,  const wxString& name = wxFrameNameStr) : wxFrame(parent, id, title, pos, size, style, name) {
+    this->SetBackgroundColour(this->panel->GetBackgroundColour());
+    this->panel->Hide();
+  }
+  
+private:
+  wxPanel* panel = new wxPanel(this);
+};
 
 intptr_t native::form_api::create(const xtd::drawing::size& size) {
-  return (intptr_t) new wxFrame(nullptr, wxID_ANY, wxEmptyString, wxDefaultPosition, wxSize(size.width(), size.height()));
+  return (intptr_t) new Form(nullptr, wxID_ANY, wxEmptyString, wxDefaultPosition, wxSize(size.width(), size.height()));
 }
 
 void native::form_api::initialize_application() {
