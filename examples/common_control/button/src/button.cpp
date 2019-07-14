41b98e554b94b959a6874e6c442d1c4129bd6ffc
Gammasoft71 <gammasoft71@gmail.com>
2019-07-14 16:08:34 +0200

    Update button.cpp

--- examples/common_control/button/src/button.cpp
+++ examples/common_control/button/src/button.cpp
@@ -10,7 +10,7 @@ namespace examples {
       this->button1.parent(*this);
       this->button1.text("button1");
       this->button1.location({50, 50});
-      this->button1.click += [&] (const control& sender, const event_args& e) {
+      this->button1.click += [&](const control& sender, const event_args& e) {
         this->label1.text(strings::format("button1 clicked {} times", ++this->button1_clicked));
       };
       
@@ -18,7 +18,7 @@ namespace examples {
       this->button2.text("button2");
       this->button2.location({50, 100});
       this->button2.size({200, 75});
-      this->button2.click += [&] (const control& sender, const event_args& e) {
+      this->button2.click += [&](const control& sender, const event_args& e) {
         this->label2.text(strings::format("button2 clicked {} times", ++this->button2_clicked));
       };
       
