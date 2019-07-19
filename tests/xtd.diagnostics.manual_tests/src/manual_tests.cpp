d75e867149b91a15a474f14094e02e97b3aba38a
Gammasoft71 <gammasoft71@gmail.com>
2019-07-19 23:21:53 +0200

    Update manual_tests.cpp

--- tests/xtd.diagnostics.manual_tests/src/manual_tests.cpp
+++ tests/xtd.diagnostics.manual_tests/src/manual_tests.cpp
@@ -2,17 +2,12 @@
 
 using namespace std;
 using namespace xtd;
-using namespace xtd::diagnostics;
 
 // The main entry point for the application.
 int main() {
-  cdebug << "This is  message" << std::endl;
-  debug::indent();
-  cdebug << strings::format("Second message with int={}", 42) << std::endl;
-  debug::unindent();
-  cdebug << strings::format("Other with bool={}", true) << std::endl;
+  cdebug << "Hello, World!" << endl;
 }
 
-// This code produces the following output with colors:
+// This code produces the following output on debug screen:
 //
 // Hello, World!
