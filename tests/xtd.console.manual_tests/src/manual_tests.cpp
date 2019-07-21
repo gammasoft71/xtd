f4371abb8fefc8e2c18bf2f89dbb63ca7cc814f1
Gammasoft71 <gammasoft71@gmail.com>
2019-07-21 19:56:17 +0200

    update xtd.strings lib

--- tests/xtd.console.manual_tests/src/manual_tests.cpp
+++ tests/xtd.console.manual_tests/src/manual_tests.cpp
@@ -2,24 +2,27 @@
 #include <xtd/xtd.console>
 
 using namespace std;
+using namespace xtd;
 using namespace std::string_literals;
 
 // The main entry point for the application.
 int main() {
   //cout << xtd::strings::format("{}, {}!", "Hello", "World"s) << endl;
-  xtd::console::output_code_page(65001);
+  console::output_code_page(65001);
+  xtd::console::write_line("\u0627\u0644\u0643\u0644\u0645\u0629 \u0627\u0644\u0639\u0631\u0628\u064A\u0629");
+
   
-  xtd::console::cancel_key_press += [&](xtd::console_cancel_event_args& e) {
+  console::cancel_key_press += [&](console_cancel_event_args& e) {
     xtd::console::write_line("Event handler called");
     e.cancel(true);
   };
   
   //xtd::console::treat_control_c_as_input(true);
-  xtd::console::write_line(xtd::console::treat_control_c_as_input());
-  xtd::console::write_line("Press escape key to quit");
-  xtd::console_key_info key;
-  while (key.key() != xtd::console_key::escape) {
-    key = xtd::console::read_key(true);
-    xtd::console::write_line("key_press [key={}, key_char='{}', modifiers={}]", key.key(), key.key_char(), key.modifiers());
+  console::write_line(console::treat_control_c_as_input());
+  console::write_line("Press escape key to quit");
+  console_key_info key;
+  while (key.key() != console_key::escape) {
+    key = console::read_key(true);
+    console::write_line("key_press [key={}, key_char='{}', modifiers={}]", key.key(), key.key_char(), key.modifiers());
   }
 }
--- xtd.console/include/xtd/console_special_key.hpp
+++ xtd.console/include/xtd/console_special_key.hpp
@@ -22,5 +22,6 @@ namespace xtd {
 
   /// @cond
   inline std::ostream& operator<<(std::ostream& os, console_special_key value) {return os << to_string(value, {{console_special_key::control_break, "control_break"}, {console_special_key::control_c, "control_c"}, {console_special_key::control_backslash, "control_backslash"}, {console_special_key::control_z, "control_z"}});}
+  inline std::wostream& operator<<(std::wostream& os, console_special_key value) {return os << to_string(value, {{console_special_key::control_break, L"control_break"}, {console_special_key::control_c, L"control_c"}, {console_special_key::control_backslash, L"control_backslash"}, {console_special_key::control_z, L"control_z"}});}
   /// @endcond
 }
