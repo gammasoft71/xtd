b9f560d72b05ef2caa8f2f1c92bc9731b9614039
Gammasoft71 <gammasoft71@gmail.com>
2019-07-21 23:13:27 +0200

    update xtd.strings lib

--- xtd.environment/include/xtd/environment_variable_target.hpp
+++ xtd.environment/include/xtd/environment_variable_target.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <xtd/xtd.strings>
 
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
@@ -7,8 +8,11 @@ namespace xtd {
     /// @brief The environment variable is stored or retrieved from the environment block associated with the current process.
     process = 0,
     /// @brief The environment variable is stored or retrieved from the HKEY_CURRENT_USER\Environment key in the Windows operating system registry.
-    user,
+    user = 1,
     /// @brief The environment variable is stored or retrieved from the HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment key in the Windows operating system registry.
     machine = 2,
   };
+  
+  inline std::ostream& operator<<(std::ostream& os, environment_variable_target value) {return os << to_string(value, {{environment_variable_target::process, "process"}, {environment_variable_target::user, "user"}, {environment_variable_target::machine, "machine"}});}
+  inline std::wostream& operator<<(std::wostream& os, environment_variable_target value) {return os << to_string(value, {{environment_variable_target::process, L"process"}, {environment_variable_target::user, L"user"}, {environment_variable_target::machine, L"machine"}});}
 }
