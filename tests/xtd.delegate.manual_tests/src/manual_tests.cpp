0f590c55f5d5eda5ce658736348762a3e7e60e2b
Gammasoft71 <gammasoft71@gmail.com>
2019-07-22 13:47:15 +0200

    update event class

--- tests/xtd.delegate.manual_tests/src/manual_tests.cpp
+++ tests/xtd.delegate.manual_tests/src/manual_tests.cpp
@@ -6,29 +6,43 @@ using namespace std;
 using namespace xtd;
 
 struct control {
-  virtual ~control() = default;
-  
   const string& text() const {return this->text_;}
-  void text(const string& text) {this->text_ = text;}
+  void text(const string& text) {
+    if (this->text_ != text) {
+      this->text_ = text;
+      this->on_text_changed(event_args::empty);
+    }
+  }
+
+  event<control, event_handler<control>> text_changed;
+  
+  void on_text_changed(const event_args& e) {this->text_changed(*this, e);}
   
 private:
   string text_;
 };
 
 struct button : public control {
+  event<button, event_handler<control>> click;
   
-  event_handler<control> click;
+  void click_button() {this->on_click(event_args::empty);}
+  
+  virtual void on_click(const event_args& e) {this->click(*this, e);}
 };
 
 // The main entry point for the application.
 int main() {
   button button1;
-  button1.text("button1");
   
-  button1.click += [&](const control& sender, const event_args& e)  {
-    cout << "button (" << sender.text() << ") clicked!" << endl;
+  button1.text_changed += [&](const control& sender, const event_args& e)  {
+    cout << "text_changed [text=" << sender.text() << "]" << endl;
   };
   
+  button1.click += [&](const control& sender, const event_args& e)  {
+    cout << "click" << endl;
+  };
+
+  button1.text("button1");
   // click simulation
-  button1.click(button1, event_args::empty);
+  button1.click_button();
 }
--- xtd.delegates/include/xtd/event.hpp
+++ xtd.delegates/include/xtd/event.hpp
@@ -3,17 +3,33 @@
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
   template<typename object_t, typename handler_t>
-  class event : public handler_t {
-  private:
+  class event : protected handler_t {
     friend object_t;
-    template<typename... arguments_t>
-    void invoke(arguments_t&&... arguments) const {
-      this->handler_t::invoke(arguments...);
+  public:
+    handler_t& operator+=(const handler_t& handler) noexcept {
+      return this->handler_t::operator+=(handler);
     }
     
-    template<typename... arguments_t>
-    void operator()(arguments_t&&... arguments) const {
-      this->invoke(arguments...);
+    handler_t& operator +=(const typename handler_t::function_t& function) noexcept {
+      return this->handler_t::operator+=(function);
+    }
+    
+    template<typename fn_t>
+    handler_t& operator +=(fn_t function) noexcept {
+      return this->handler_t::operator+=(function);
+    }
+    
+    handler_t& operator -=(const handler_t& delegate) noexcept {
+      return this->handler_t::operator-=(delegate);
+    }
+    
+    handler_t& operator -=(const typename handler_t::function_t& function) noexcept {
+      return this->handler_t::operator-=(function);
+    }
+    
+    template<typename fn_t>
+    handler_t& operator -=(fn_t function) noexcept {
+      return this->handler_t::operator-=(function);
     }
   };
 }
