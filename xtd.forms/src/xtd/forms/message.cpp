94ca29e29130185ff3e3bd7d0aca62b5cd8271f0
Gammasoft71 <gammasoft71@gmail.com>
2019-07-25 00:05:44 +0200

    add using namespace

--- xtd.forms/src/xtd/forms/message.cpp
+++ xtd.forms/src/xtd/forms/message.cpp
@@ -2,8 +2,11 @@
 #include "../include/xtd/forms/message.hpp"
 #include "../../native/control_api.hpp"
 
-int xtd::forms::message::counter_ = 0;
+using namespace std;
+using namespace xtd::forms;
 
-std::string xtd::forms::message::to_string() const {
-  return xtd::strings::format("msg=0x{:X4} ({}), hwnd=0x{:X}, wparam=0x{:X}, lparam=0x{:X}, result=0x{:X}", this->msg_, native::control_api::message_to_string(this->msg_), this->hwnd_, this->wparam_, this->lparam_, this->result_);
+int message::counter_ = 0;
+
+string message::to_string() const {
+  return strings::format("msg=0x{:X4} ({}), hwnd=0x{:X}, wparam=0x{:X}, lparam=0x{:X}, result=0x{:X}", this->msg_, native::control_api::message_to_string(this->msg_), this->hwnd_, this->wparam_, this->lparam_, this->result_);
 }
