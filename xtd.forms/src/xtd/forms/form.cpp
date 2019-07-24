cefafa1986f0d85cd483c52a95883d372f7c41f3
Gammasoft71 <gammasoft71@gmail.com>
2019-07-25 00:03:12 +0200

    add using namespace

--- xtd.forms/src/xtd/forms/form.cpp
+++ xtd.forms/src/xtd/forms/form.cpp
@@ -4,18 +4,20 @@
 #include "../../native/control_api.hpp"
 #include "../../native/form_api.hpp"
 
-xtd::forms::form::form() {
+using namespace xtd::forms;
+
+form::form() {
   create_control();
 }
 
-void xtd::forms::form::create_handle() {
+void form::create_handle() {
   this->handle_ = native::form_api::create(this->default_size());
   this->visible_ = false;
   this->location_ = native::control_api::location(this->handle_);
   this->control::create_handle();
 }
 
-void xtd::forms::form::parent(const xtd::forms::control& parent) {
+void form::parent(const control& parent) {
   throw std::invalid_argument("Top-level control cannot be added to a control.");
 }
 
