34f7fc176ba3c8c5a2fa9be4d0750ccb1d19f12d
Gammasoft71 <gammasoft71@gmail.com>
2019-07-26 13:50:34 +0200

    Update delegate.hpp

--- xtd.delegates/include/xtd/delegate.hpp
+++ xtd.delegates/include/xtd/delegate.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <functional>
+#include <stdexcept>
 #include <vector>
 
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
