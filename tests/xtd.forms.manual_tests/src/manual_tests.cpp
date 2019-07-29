d0e0091acc2eb237aa824315046b80110e869e4c
Gammasoft71 <gammasoft71@gmail.com>
2019-07-29 16:30:00 +0200

    update xtd.drawing lib

--- tests/xtd.forms.manual_tests/src/manual_tests.cpp
+++ tests/xtd.forms.manual_tests/src/manual_tests.cpp
@@ -1,15 +1,12 @@
 #include <xtd/xtd.forms>
 
+using namespace std;
 using namespace xtd;
 using namespace xtd::drawing;
 using namespace xtd::forms;
 
 // The main entry point for the application.
 int main() {
-  form form;
-  //form.back_color(color::from_argb(64, 255, 0, 0));
-  form.text(strings::format("{}", color::spring_green.name()));
-  form.back_color(color::spring_green);
-  
+  form form;  
   application::run(form);
 }
