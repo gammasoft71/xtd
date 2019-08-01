91e8840c53994f2a590b96edc1992320dc5ab43d
Gammasoft71 <gammasoft71@gmail.com>
2019-08-01 11:44:18 +0200

    Update check_box.cpp

--- examples/common_controls/check_box/src/check_box.cpp
+++ examples/common_controls/check_box/src/check_box.cpp
@@ -11,27 +11,27 @@ namespace examples {
 
       this->check_box1.parent(*this);
       this->check_box1.auto_check(false);
-      this->check_box1.location({10, 10});
+      this->check_box1.location({30, 30});
       this->check_box1.text("unchecked");
 
       this->check_box2.parent(*this);
       this->check_box2.checked(true);
-      this->check_box2.location({10, 40});
+      this->check_box2.location({30, 60});
       this->check_box2.text("Checked");
 
       this->check_box3.parent(*this);
       this->check_box3.check_state(forms::check_state::indeterminate);
       this->check_box3.three_state(true);
-      this->check_box3.location({10, 70});
+      this->check_box3.location({30, 90});
       this->check_box3.text("indeterminate");
       this->check_box3.check_state_changed += [&](const control& sender, const event_args& e) {
-        this->label1.text(strings::format("check_box3.check_state() = {}", this->check_box3.check_state()));
+        this->label1.text(strings::format("check_box3 state = {}", this->check_box3.check_state()));
       };
       
       this->label1.parent(*this);
       this->label1.auto_size(true);
-      this->label1.location({10, 150});
-      this->label1.text(strings::format("check_box3.check_state() = {}", this->check_box3.check_state()));
+      this->label1.location({30, 150});
+      this->label1.text(strings::format("check_box3 state = {}", this->check_box3.check_state()));
     }
     
   private:
