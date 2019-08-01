7203cec0f770a20e043023c32b008ba71d6bb865
Gammasoft71 <gammasoft71@gmail.com>
2019-08-01 10:49:22 +0200

    add check_box example

---
+++ examples/common_controls/check_box/src/check_box.cpp
@@ -0,0 +1,48 @@
+#include <xtd/xtd.forms>
+
+using namespace xtd;
+using namespace xtd::forms;
+
+namespace examples {
+  class form1 : public form {
+  public:
+    form1() {
+      this->text("Check box example");
+
+      this->check_box1.parent(*this);
+      this->check_box1.auto_check(false);
+      this->check_box1.location({10, 10});
+      this->check_box1.text("unchecked");
+
+      this->check_box2.parent(*this);
+      this->check_box2.checked(true);
+      this->check_box2.location({10, 40});
+      this->check_box2.text("Checked");
+
+      this->check_box3.parent(*this);
+      this->check_box3.check_state(forms::check_state::indeterminate);
+      this->check_box3.three_state(true);
+      this->check_box3.location({10, 70});
+      this->check_box3.text("indeterminate");
+      this->check_box3.check_state_changed += [&](const control& sender, const event_args& e) {
+        this->label1.text(strings::format("check_box3.check_state() = {}", this->check_box3.check_state()));
+      };
+      
+      this->label1.parent(*this);
+      this->label1.auto_size(true);
+      this->label1.location({10, 150});
+      this->label1.text(strings::format("check_box3.check_state() = {}", this->check_box3.check_state()));
+    }
+    
+  private:
+    check_box check_box1;
+    check_box check_box2;
+    check_box check_box3;
+    label label1;
+  };
+}
+
+int main() {
+  application::enable_visual_styles();
+  application::run(examples::form1());
+}
