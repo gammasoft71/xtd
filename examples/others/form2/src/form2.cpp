3e1c1f4baf3e7368426e6d575e1bc9798af9daf5
Gammasoft <gammasoft71@gmail.com>
2019-08-04 23:47:01 +0200

    Update form2.cpp

--- examples/others/form2/src/form2.cpp
+++ examples/others/form2/src/form2.cpp
@@ -3,5 +3,6 @@
 int main() {
   auto form1 = xtd::forms::control::create<xtd::forms::form>("form1");
   form1->form_closing += [](const control& sender, form_closing_event_args& e) {e.cancel(message_box::show("Are you sure you want exit?", "Close Form", message_box_buttons::yes_no, message_box_icon::question) == dialog_result::no);};
+   auto button1 = xtd::forms::control::create<xtd::forms::button>(*form1, "Close", {10, 10});
   xtd::forms::application::run(*form1);
 }
