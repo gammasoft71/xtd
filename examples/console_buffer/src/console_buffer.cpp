6793fc2a16cc88d906938c439781d6f80a85670c
Gammasoft <gammasoft71@gmail.com>
2019-08-06 23:20:41 +0200

    Update console_buffer.cpp

--- examples/console_buffer/src/console_buffer.cpp
+++ examples/console_buffer/src/console_buffer.cpp
@@ -4,8 +4,8 @@ using namespace xtd;
 
 // The main entry point for the application.
 int main() {
-  console::write_line("The current buffer height is %d rows.", console::buffer_height());
-  console::write_line("The current buffer width is %d columns.", console::buffer_width());
+  console::write_line("The current buffer height is {} rows.", console::buffer_height());
+  console::write_line("The current buffer width is {} columns.", console::buffer_width());
 }
 
 // This code produces the following output:
