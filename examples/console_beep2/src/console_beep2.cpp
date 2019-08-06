6f8e983a9851d0b0ddafd77d87c036d8fe33f533
Gammasoft <gammasoft71@gmail.com>
2019-08-06 23:18:54 +0200

    Update console_beep2.cpp

--- examples/console_beep2/src/console_beep2.cpp
+++ examples/console_beep2/src/console_beep2.cpp
@@ -9,7 +9,7 @@ int main(int argc, char* argv[]) {
   //
   if ((argc == 2) && (x = atoi(argv[1])) && ((x >= 1) && (x <= 9)))
     for (int i = 1; i <= x; i++)
-      console::out << format("Beep number %d.", i) << beep() << endl;
+      console::out << format("Beep number {}.", i) << beep() << endl;
   else
     console::out << "Usage: Enter the number of times (between 1 and 9) to beep." << endl;
 }
