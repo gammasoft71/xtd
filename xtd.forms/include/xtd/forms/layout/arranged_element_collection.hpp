9d975ba9880ea7f6c2c1f6a604a2454d927d921e
Gammasoft <gammasoft71@gmail.com>
2019-08-07 11:27:21 +0200

    Add push_back and push_back_range methods

--- xtd.forms/include/xtd/forms/layout/arranged_element_collection.hpp
+++ xtd.forms/include/xtd/forms/layout/arranged_element_collection.hpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <xtd/event_args.hpp>
 #include <xtd/event_handler.hpp>
 #include <xtd/event.hpp>
 
@@ -13,11 +14,32 @@ namespace xtd {
         /// @cond
         arranged_element_collection(const arranged_element_collection&) = default;
         arranged_element_collection& operator=(const arranged_element_collection&) = default;
+        arranged_element_collection(const std::vector<type>& collection) {
+          this->push_back_range(collection);
+        }
+        arranged_element_collection(const std:initializer_list<type_t>& coll3ction) {
+          this->push_back_range(collection);
+        }
         /// @endcond
         
-        event<arranged_element_collection, delegate<void(size_t)>> item_added;
-        event<arranged_element_collection, delegate<void(size_t)>> item_removed;
+        event<arranged_element_collection, delegate<void(size_t, type_t item)>> item_added;
+        event<arranged_element_collection, delegate<void(size_t, type_t item)>> item_removed;
         
+        void push_back(size_t item) {
+          this->collection_.pudh_back(item);
+          this->item_added(this->collection_.size() - 1, item);
+        }
+        
+        void push_back_range(const std::vector<type_t>& collection) {
+          for(type_t item : collection)
+            this->push_back(item);
+        }
+
+        void push_back_range(const std::initializer_list<type_t>& collection) {
+          for(type_t item : collection)
+            this->push_back(item);
+        }
+      
       private:
         std::vector<type_t> collection_;
       };
