b966c214c5f7761d43792c0476997b590aec3297
Gammasoft <gammasoft71@gmail.com>
2019-08-08 20:54:38 +0200

    Update screen.hpp

--- xtd.forms/include/xtd/forms/screen.hpp
+++ xtd.forms/include/xtd/forms/screen.hpp
@@ -23,6 +23,7 @@ namespace xtd {
     private:
       screen() = default;
       screen(int32_t bist_per_pixel, const drawing::rectangle& bounds, const std::string& device_name, bool primary, const drawing::rectangle& working_area) : bits_per_pixel_(bits_per_pixel), bounds_(bounds), device_name_(device_name), primary_(primary), working_area_(working_area) {}
+      
       int32_t bits_per_pixel_ = 0;
       drawing::rectangle bounds_;
       std::string device_name_;
