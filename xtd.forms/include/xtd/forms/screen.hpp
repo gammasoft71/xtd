ca338597091be5ed6ea4e8e1a9ef3c3c3637f890
Gammasoft <gammasoft71@gmail.com>
2019-08-08 20:58:06 +0200

    Add handle_ member

--- xtd.forms/include/xtd/forms/screen.hpp
+++ xtd.forms/include/xtd/forms/screen.hpp
@@ -22,11 +22,12 @@ namespace xtd {
                                
     private:
       screen() = default;
-      screen(int32_t bist_per_pixel, const drawing::rectangle& bounds, const std::string& device_name, bool primary, const drawing::rectangle& working_area) : bits_per_pixel_(bits_per_pixel), bounds_(bounds), device_name_(device_name), primary_(primary), working_area_(working_area) {}
+      screen(int32_t bist_per_pixel, const drawing::rectangle& bounds, const std::string& device_name, intptr_t handle, bool primary, const drawing::rectangle& working_area) : bits_per_pixel_(bits_per_pixel), bounds_(bounds), device_name_(device_name), handle_(handle), primary_(primary), working_area_(working_area) {}
       
       int32_t bits_per_pixel_ = 0;
       drawing::rectangle bounds_;
       std::string device_name_;
+      intptr_t handle_ = 0;
       bool primary_ = false;
       drawing::rectangle working_area_;
     };                           
