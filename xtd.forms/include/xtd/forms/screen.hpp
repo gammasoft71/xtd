eb1215c122f80ffd99295fda5bab528a2913a268
Gammasoft <gammasoft71@gmail.com>
2019-08-08 20:40:37 +0200

    Add bits_per_pixel method

--- xtd.forms/include/xtd/forms/screen.hpp
+++ xtd.forms/include/xtd/forms/screen.hpp
@@ -1,9 +1,26 @@
 #pragma once
 #include <csdint>
+#include <vector>
 
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
   /// @brief The xtd::forms namespace contains classes for creating Windows-based applications that take full advantage of the rich user interface features available in the Microsoft Windows operating system, Apple macOS and Linux like Ubuntu operating system.
   namespace forms {
+    /// @brief Represents a display device or multiple display devices on a single system.
+    class Screen {
+    public:
+      /// @cond
+      screen(const Screen&) = default;
+      screen& operator=(const Screen&) = default;
+      /// @endcond
+      
+      static std::vector<screen> all_screens;
+
+      int32_t bits_per_seconds(! const {return this->bits_per_pixel;}
+                               
+  private:
+    screen() = default;
+    screen(int32_t bist_per_pixel) : bits_per_pixel_(bits_per_pixel) {}
+    int32_t bits_per_pixel_ = 0;
   }
 }
