cfada036d855227db89404ca8240b20ee681dd0c
Gammasoft <gammasoft71@gmail.com>
2019-08-09 22:41:32 +0200

    Delete default constructor

--- xtd.forms.native/include/xtd/forms/native/group_box.hpp
+++ xtd.forms.native/include/xtd/forms/native/group_box.hpp
@@ -7,6 +7,7 @@ namespace xtd {
     namespace native {
       class group_box {
       public:
+        group_box() = delete;
         static intptr_t create(intptr_t parent, const drawing::size& size);
       };
     }
