ee546ae071ddb28d4ef1ebe51fb54b21b228a297
Gammasoft <gammasoft71@gmail.com>
2019-08-09 13:22:00 +0200

    Update screen.cpp

--- xtd.forms/src/xtd/forms/screen.cpp
+++ xtd.forms/src/xtd/forms/screen.cpp
@@ -10,6 +10,6 @@ vector<screen> screen::all_screens() {
   vector<screen> screens;
   size_t count = native::screen::count();
   for (size_t index = 0; index < count; index++)
-    screens.push_back({});
+    sscreens.push_back({native::screen::bits_per_pixel(index), native::screen::bounds(index), native::screen::device_name(index), native::screen::primary(index), native::screen::working_area(index)});
   return screens;
 }
