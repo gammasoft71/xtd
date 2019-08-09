3e3f10ce64649333885c584139eb5b1ff2370216
Gammasoft <gammasoft71@gmail.com>
2019-08-09 13:12:02 +0200

    Update screen.cpp

--- xtd.forms/src/xtd/forms/screen.cpp
+++ xtd.forms/src/xtd/forms/screen.cpp
@@ -7,4 +7,9 @@ using namespace xtd::drawing;
 using namespace xtd::forms;
 
 vector<screen> screen::all_screens() {
+  vector<screen> screens;
+  size_t count = native::screen::count();
+  for (size_t index = 0; index < count; index++)
+    screens.push_back({});
+  return screens;
 }
