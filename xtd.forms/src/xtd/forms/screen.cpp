a5dec868f3b93ee4ac42fda87f83343e78f59f08
Gammasoft <gammasoft71@gmail.com>
2019-08-09 13:08:20 +0200

    Create screen.cpp

---
+++ xtd.forms/src/xtd/forms/screen.cpp
@@ -0,0 +1,10 @@
+#include "../../../include/xtd/forms/screen.hpp
+#include <xtd/forms/native/screen.hpp>
+
+using namespace std;
+using namespace xtd;
+using namespace xtd::drawing;
+using namespace xtd::forms;
+
+vector<screen> screen::all_screens() {
+}
