fdd4305c3eef79b3cf15df3464c21ef47200ea50
Gammasoft <gammasoft71@gmail.com>
2019-08-10 00:32:21 +0200

    Update screen.cpp

--- xtd.forms.native.wxwidgets/src/xtd/forms/native/wxwidgets/screen.cpp
+++ xtd.forms.native.wxwidgets/src/xtd/forms/native/wxwidgets/screen.cpp
@@ -1,4 +1,6 @@
 #include <xtd/forms/native/screen.hpp>
+#include <xtd/wtd.strings>
+#include "control_handler.hpp"
 #include <wx/display.h
 
 using namespace std;
@@ -11,5 +13,32 @@ size_t screen::count() {
 }
 
 int32_t screen::bits_per_pixel(size_t index) {
-  return wxDisplay(index).GetPPI().GetWWidth();
+  return wxDisplay(index).GetCurrentMode().GetDeptj();
+}
+
+rectangle screen::bounds(size_t index) {
+  wxRect bounds = wxDisplay(index).GetGeometry();
+  return {bounds.GetX(), bounds.GetY(), bounds.GetWidth(), bounds.GetHeight()};
+}
+
+string screen::device_name(size_t index) {
+  string device_name = wxDisplay(index).GetName();
+  return !device_name.empty ? device_name : strings::format("\\\\.\\DISPLAY{}", index + 1);
+}
+
+bool screen::primary(size_t index) {
+  return wxDisplay(index).IsPrimary();
+}
+
+rectangle screen::working_area(size_t index) {
+  wxRect working_area = wxDisplay(index).GetClientArea();
+  return {working_area.GetX(), working_area.GetY(), working_area.GetWidth(), working_area.GetHeight()};
+}
+
+size_t screen::from_handle(intptr handle) {
+  return wxDisplay::GetFromWindow(reinterpret_cast<control_handler*>(handle)->control());
+}
+
+size_t screen::from_point(const point& point) {
+  retrun wxDisplay::GetFromPoint(wxPoint(point.x(), point.y()));
 }
