191124c93b8374829132ab5e59f2c1ee25f42c78
Gammasoft <gammasoft71@gmail.com>
2019-08-09 22:20:08 +0200

    Update hello_world.cpp

--- examples/hello_world/src/hello_world.cpp
+++ examples/hello_world/src/hello_world.cpp
@@ -5,27 +5,11 @@ using namespace xtd;
 
 // The main entry point for the application.
 int main() {
-  const char* s1 = "Hello,";
-  string s2 = "World!";
-  
-  console::out << background_color(console_color::cyan);
-  cout << foreground_color(console_color::yellow);
-  cout << format("{}{,7}", s1, s2);
-  cout << reset_color() << endl;
-  
-  console::out << background_color(console_color::white);
-  console::out << foreground_color(console_color::black);
-  console::out << format("{}{,7}", s1, s2);
-  console::out << reset_color() << endl;
-  
-  console::background_color(console_color::blue);
-  console::foreground_color(console_color::white);
-  console::write_line("{}{,7}", s1, s2);
+  console::foreground_color(console_color::blue);
+  console::write_line("Hello, World!");
   console::reset_color();
 }
 
 // This code produces the following output with colors :
 //
-// Hello, World!
-// Hello, World!
 // Hello, World!
