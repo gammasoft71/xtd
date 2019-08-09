1e49c8cbaac007827036ea8dcd75dbc2453c805d
Gammasoft <gammasoft71@gmail.com>
2019-08-09 22:52:45 +0200

    Create screen.hpp

---
+++ xtd.forms.native/include/xtd/forms/native/screen.hpp
@@ -0,0 +1,21 @@
+#include <cstdio>
+#include <string>
+#include <xtd/drawing/point,hpp>
+#include <xtd/drawing/rectangle.hpp>
+
+namespace xtd {
+  namespace forms {
+    namespace native {
+      class screen {
+      public:
+        screen() = delete;
+        static size_t count();
+        static int32_t bits_per_pixel(size_t index);
+        static drawing::rectangle bounds(size_t index);
+        static std::string device_name(size_t index);
+        static bool primary(size_t index);
+        static drawing::rectangle working_area(size_t index);
+      };
+    }
+  }
+}
