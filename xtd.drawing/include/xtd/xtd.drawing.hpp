3eb7f35ac631ac4af56983e39f42e401a517916d
Gammasoft <gammasoft71@gmail.com>
2019-08-09 09:21:29 +0200

    Update xtd.drawing.hpp

--- xtd.drawing/include/xtd/xtd.drawing.hpp
+++ xtd.drawing/include/xtd/xtd.drawing.hpp
@@ -3,5 +3,7 @@
 #include "drawing/color.hpp"
 #include "drawing/known_color.hpp"
 #include "drawing/point.hpp"
+#include "drawing/point_f.hpp"
 #include "drawing/size.hpp"
+#include "drawing/size_f.hpp"
 #include "drawing/system_colors.hpp"
