6fc8d667f68f2aebb58f52f561035c01d9b7b523
Gammasoft <gammasoft71@gmail.com>
2019-08-12 14:21:24 +0200

    Update button2.cpp

--- examples/others/button2/src/button2.cpp
+++ examples/others/button2/src/button2.cpp
@@ -5,6 +5,7 @@ auto main()->int {
   xtd::forms::button button1, button2;
   xtd::forms::label label1, label2;
   xtd::forms::form form1;
+  
   button1.text("Button 1").location({50, 50});
   button2.text("Button 2").location({50, 100}).size({200, 75});
   label1.text("Button 1 clicked 0 times").location({50, 200}).width(200);      
@@ -13,5 +14,6 @@ auto main()->int {
   button2.click += [&](const auto& sender, const auto& e) {label2.text(xtd::strings::format("Button 2 clicked {} times", ++button2_clicked));};
   form1.text("Button 2 example");     
   form1 << button1 << button2 << label1 << label2;
+  
   xtd::forms::application::run(form1);
 }
