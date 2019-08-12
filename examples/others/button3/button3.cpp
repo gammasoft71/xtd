94544e6060dda39cde455d26b2b0d6da7ee9cc8e
Gammasoft <gammasoft71@gmail.com>
2019-08-12 13:53:10 +0200

    Update button3.cpp

--- examples/others/button3/button3.cpp
+++ examples/others/button3/button3.cpp
@@ -1,31 +1,17 @@
 #include <xtd/xtd.forms>
 
-using namespace xtd;
-using namespace xtd::forms;
-
-namespace examples {
-  class form1 : public form {
-  public:
-    form1() {
-      this->button1.text("Button 1").location({50, 50});
-      this->button2.text("Button 2").location({50, 100}).size({200, 75});
-      this->label1.text("Button 1 clicked 0 times").location({50, 200}).width(200);      
-      this->label2.text("Button 2 clicked 0 times").location({50, 230}).width(200);      
-      this->button1.click += [&](const control& sender, const event_args& e) {this->label1.text(strings::format("Button 1 clicked {} times", ++this->button1_clicked));};
-      this->button2.click += [&](const control& sender, const event_args& e) {this->label2.text(strings::format("Button 2 clicked {} times", ++this->button2_clicked));};
-      this->text("Button example");     
-      *this << this->button1 << this->button2 << this->label1 << this->label2;
-    }
-    
-  private:
-    button button1, button button2;
-    label label1, label2;
-    int button1_clicked = 0, button2_clicked = 0;
-  };
-}
-
-// The main entry point for the application.
 int main() {
-  application::enable_visual_styles();
-  application::run(examples::form1());
+  int button1_clicked = 0, button2_clicked = 0;
+  xtd::forms::button button1, button2;
+  xtd::forms::label label1, label2;
+  xtd::forms::form form1;
+  button1.text("Button 1").location({50, 50});
+  button2.text("Button 2").location({50, 100}).size({200, 75});
+  label1.text("Button 1 clicked 0 times").location({50, 200}).width(200);      
+  label2.text("Button 2 clicked 0 times").location({50, 230}).width(200);      
+  button1.click += [&](const xtd::forms::control& sender, const xtd::event_args& e) {label1.text(xtd::strings::format("Button 1 clicked {} times", ++button1_clicked));};
+  button2.click += [&](const xtd::forms::control& sender, const xtd::event_args& e) {label2.text(xtd::strings::format("Button 2 clicked {} times", ++button2_clicked));};
+  form1.text("Button 3 example");     
+  form1 << button1 << button2 << label1 << label2;
+  xtd::forms::application::run(form1);
 }
