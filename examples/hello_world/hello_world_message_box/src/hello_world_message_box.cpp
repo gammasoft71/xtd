be2b2ff3b4c3024317651bd07d73eea4d528cc4e
Gammasoft71 <gammasoft71@gmail.com>
2019-08-16 11:32:40 +0200

    Update hello_world_message_box.cpp

--- examples/hello_world/hello_world_message_box/src/hello_world_message_box.cpp
+++ examples/hello_world/hello_world_message_box/src/hello_world_message_box.cpp
@@ -4,17 +4,17 @@ using namespace xtd;
 using namespace xtd::forms;
 
 int main() {
-  button button1;
-  button1.text("Click me");
-  button1.location({10, 10});
-  button1.width(90);
-  button1.click += [](const control& sender, const event_args& e) {
+  button button;
+  button.text("Click me");
+  button.location({10, 10});
+  button.width(90);
+  button.click += [](const control& sender, const event_args& e) {
     message_box::show("Hello, World!");
   };
 
-  form form1;
-  form1.text("Hello World Form");
-  form1.controls().push_back(button1);
+  form form;
+  form.text("Hello World Form");
+  form.controls().push_back(button);
 
-  application::run(form1);
+  application::run(form);
 }
