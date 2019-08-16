e83577a8e5815bc99ff7bafc37d38d6b5fc932ca
Gammasoft71 <gammasoft71@gmail.com>
2019-08-16 08:18:01 +0200

    add point example

---
+++ examples/point/src/point.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <xtd/xtd.drawing>
+
+using namespace std;
+using namespace xtd;
+using namespace xtd::drawing;
+
+int main() {
+  point location = {10, 235};
+  cout << format("location = {}", location) << endl;
+
+  location.x(location.x() + 20);
+  location.y(location.y() - 15);
+  cout << format("location = {}", location) << endl;
+  
+  //location = point(10, 10) + size(20, 50);
+  //cout << format("location = {}", location) << endl;
+  
+  location.offset({70, 140});
+  cout << format("location = {}", location) << endl;
+}
