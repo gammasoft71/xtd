0d39148c2de292d338c3b7823d3116e847f1e325
gammasoft71 <gammasoft71@gmail.com>
2019-08-18 22:01:05 +0200

    Update control.cpp

--- xtd.forms/src/xtd/forms/control.cpp
+++ xtd.forms/src/xtd/forms/control.cpp
@@ -439,8 +439,8 @@ void control::get_properties() {
 
 void control::set_properties() const {
   if (this->client_size_ != drawing::size(-1, -1) && this->size_ == drawing::size(-1, -1)) native::control::client_size(this->handle_, this->client_size());
-  if (this->back_color_.has_value()) native::control::back_color(this->handle_, this->back_color());
-  if (this->fore_color_.has_value()) native::control::fore_color(this->handle_, this->fore_color());
+  if (this->back_color_.has_value() || this->back_color() != this->default_back_color()) native::control::back_color(this->handle_, this->back_color());
+  if (this->fore_color_.has_value() || this->fore_color() != this->default_fore_color()) native::control::fore_color(this->handle_, this->fore_color());
   if (this->location_ != point(-1, -1)) native::control::location(this->handle_, this->location());
   if (this->size_ != drawing::size(-1, -1)) native::control::size(this->handle_, this->size());
   native::control::text(this->handle_, this->text());
