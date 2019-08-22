8a6f8e95283b5362ee0b294f50ed3bceee171321
Gammasoft71 <gammasoft71@gmail.com>
2019-08-22 22:04:19 +0200

    Update all_git_pull_submodules.sh

--- scripts/all_git_pull_submodules.sh
+++ scripts/all_git_pull_submodules.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./scripts/gi_pull_submodules.sh
+./scripts/git_pull_submodules.sh
 
 declare -a submodules=("xtd.tunit" "xtd.properties" "xtd.delegates" "xtd.io" "xtd.strings" "xtd.diagnostics" "xtd.environment" "xtd.console" "xtd.drawing" "xtd.forms")
 
