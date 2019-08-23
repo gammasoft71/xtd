be739603c4ca7da88ea99e800413645db31935e5
Gammasoft71 <gammasoft71@gmail.com>
2019-08-23 16:14:25 +0200

    fix ctor

--- tests/xtd.delegate.unit_tests/src/delegate.cpp
+++ tests/xtd.delegate.unit_tests/src/delegate.cpp
@@ -29,17 +29,17 @@ namespace unit_tests {
     }
     
     void test_method_(create_delegate_with_static_method_and_test_is_empty) {
-      delegate<string()> d = static_method1;
+      delegate<string()> d(static_method1);
       assert::is_false(d.is_empty());
     }
     
     void test_method_(create_delegate_with_static_method_and_invoke_it) {
-      delegate<string()> d = static_method1;
+      delegate<string()> d(static_method1);
       assert::are_equal("static_method1", d.invoke());
     }
     
     void test_method_(create_delegate_with_static_method_and_invoke_it_with_functor) {
-      delegate<string()> d = static_method1;
+      delegate<string()> d(static_method1);
       assert::are_equal("static_method1", d());
     }
     
@@ -74,34 +74,34 @@ namespace unit_tests {
     
     void test_method_(create_delegate_with_functor_method_and_test_is_empty) {
       container c;
-      delegate<string()> d = c;
+      delegate<string()> d(c);
       assert::is_false(d.is_empty());
     }
     
     void test_method_(create_delegate_with_functor_method_and_invoke_it) {
       container c;
-      delegate<string()> d = c;
+      delegate<string()> d(c);
       assert::are_equal("functor_method1", d.invoke());
     }
     
     void test_method_(create_delegate_with_functor_method_and_invoke_it_with_functor) {
       container c;
-      delegate<string()> delegate = c;
+      delegate<string()> delegate(c);
       assert::are_equal("functor_method1", delegate());
     }
     
     void test_method_(create_delegate_with_lambda_expression_and_test_is_empty) {
-      delegate<string()> d = []()->string {return "lambda_expression1";};
+      delegate<string()> d([]()->string {return "lambda_expression1";});
       assert::is_false(d.is_empty());
     }
     
     void test_method_(create_delegate_with_lambda_expression_and_invoke_it) {
-      delegate<string()> d = []()->string {return "lambda_expression1";};
+      delegate<string()> d([]()->string {return "lambda_expression1";});
       assert::are_equal("lambda_expression1", d.invoke());
     }
     
     void test_method_(create_delegate_with_lambda_expression_and_invoke_it_with_functor) {
-      delegate<string()> d = []()->string {return "lambda_expression1";};
+      delegate<string()> d([]()->string {return "lambda_expression1";});
       assert::are_equal("lambda_expression1", d());
     }
   };
