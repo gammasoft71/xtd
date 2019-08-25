f4a875f3ead384bc7c5a61c7673eb2a953124864
Gammasoft71 <gammasoft71@gmail.com>
2019-08-25 23:45:40 +0200

    Update manual_tests.cpp

--- tests/xtd.forms.manual_tests/src/manual_tests.cpp
+++ tests/xtd.forms.manual_tests/src/manual_tests.cpp
@@ -9,17 +9,21 @@ using namespace xtd::forms;
 int main() {
   form form;
   form.text("Manual tests");
-  form.name("main form");
+  form.client_size({640, 480});
 
   form.show();
-  //graphics graphics = form.create_graphics();
-  //graphics.clear(color::spring_green);
-
   form.paint += [&](const control& sender, paint_event_args& e) {
-    //e.graphics().clear(color::spring_green);
-    graphics graphics = screen::create_graphics();
-    graphics.clear(color::spring_green);
-  };
+    //string_format string_format;
+    e.graphics().fill_rectangle(solid_brush(color::blue), 0, 0, 640, 480);
+    e.graphics().clear(color::light_yellow);
+    e.graphics().draw_rectangle(pen(color::light_pink, 10), e.clip_rectangle());
+    e.graphics().draw_line(pen(color::light_steel_blue, 5), 20, 60, 260, 60);
+    e.graphics().fill_rectangle(solid_brush(color::light_blue), 50, 300, 400, 50);
+    e.graphics().draw_line(pen(color::red, 1), 0, 0, 1, 1);
+    //e.graphics().draw_string("Draw string", font("Arial", 34, font_style::regular), solid_brush(color::light_green), 20, 0, string_format);
+    e.graphics().fill_rectangle(solid_brush(color::light_sea_green), 400, 70, 100, 200);
+    e.graphics().draw_arc(pen(color::black, 10), 400, 70, 100, 200, 45, 270);
+ };
   
   application::run(form);
 }
