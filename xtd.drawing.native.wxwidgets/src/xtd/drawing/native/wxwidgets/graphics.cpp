67b2e7987f27235ab97affdb0017368bdafdbae7
Gammasoft71 <gammasoft71@gmail.com>
2019-08-25 20:28:46 +0200

    add screen

--- xtd.drawing.native.wxwidgets/src/xtd/drawing/native/wxwidgets/graphics.cpp
+++ xtd.drawing.native.wxwidgets/src/xtd/drawing/native/wxwidgets/graphics.cpp
@@ -1,10 +1,12 @@
 #include <xtd/drawing/native/graphics.hpp>
+#include <wx/app.h>
 #include <wx/dc.h>
 #include <wx/dcgraph.h>
 
 using namespace xtd::drawing::native;
 
 void graphics::clear(intptr_t hdc, uint8_t a, uint8_t r, uint8_t g, uint8_t b) {
+  if (hdc == 0) return;
   wxBrush default_brush = reinterpret_cast<wxDC*>(hdc)->GetBackground();
   reinterpret_cast<wxDC*>(hdc)->SetBackground(wxBrush(wxColour(r, g, b, a)));
   reinterpret_cast<wxDC*>(hdc)->Clear();
@@ -16,5 +18,6 @@ intptr_t graphics::create() {
 }
 
 void graphics::destroy(intptr_t hdc) {
+  if (hdc == 0 || !wxTheApp->IsMainLoopRunning()) return;
   delete reinterpret_cast<wxDC*>(hdc);
 }
