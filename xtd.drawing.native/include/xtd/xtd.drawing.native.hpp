896e0d887b3732457a031f486bb6e4b6ca547638
Gammasoft71 <gammasoft71@gmail.com>
2019-08-25 16:12:41 +0200

    Update xtd.drawing.native.hpp

--- xtd.drawing.native/include/xtd/xtd.drawing.native.hpp
+++ xtd.drawing.native/include/xtd/xtd.drawing.native.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <xtd/xtd.strings>
 #include "drawing/native/brush.hpp"
+#include "drawing/native/graphics.hpp"
 #include "drawing/native/pen.hpp"
 #include "drawing/native/system_colors.hpp"
