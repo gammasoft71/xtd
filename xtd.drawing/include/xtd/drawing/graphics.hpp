0e63e38068145ca0ee47d74cd9ed3ed46a0ecd82
Gammasoft71 <gammasoft71@gmail.com>
2019-08-26 14:04:19 +0200

    grpahics implementation

--- xtd.drawing/include/xtd/drawing/graphics.hpp
+++ xtd.drawing/include/xtd/drawing/graphics.hpp
@@ -31,6 +31,22 @@ namespace xtd {
       
       void draw_arc(const pen& pen, int32_t x, int32_t y, int32_t width, int32_t height, int32_t start_angle, int32_t sweep_angle);
       
+      void draw_arc(const pen& pen, float x, float y, float width, float height, float start_angle, float sweep_angle) {draw_arc(pen, static_cast<int32_t>(x), static_cast<int32_t>(y), static_cast<int32_t>(width), static_cast<int32_t>(height), static_cast<int32_t>(start_angle), static_cast<int32_t>(sweep_angle));}
+
+      void draw_arc(const pen& pen, const rectangle& rect, float start_angle, float sweep_angle) {draw_arc(pen, rect.x(), rect.y(), rect.width(), rect.height(), static_cast<int32_t>(start_angle), static_cast<int32_t>(sweep_angle));}
+      
+      void draw_arc(const pen& pen, const rectangle_f& rect, float start_angle, float sweep_angle) {draw_arc(pen, rect.x(), rect.y(), rect.width(), rect.height(), start_angle, sweep_angle);}
+
+      void draw_bezier(const pen& pen, int32_t x1, int32_t y1, int32_t x2, int32_t y2, int32_t x3, int32_t y3, int32_t x4, int32_t y4);
+
+      void draw_ellipse(const pen& pen, int32_t x, int32_t y, int32_t width, int32_t height);
+      
+      void draw_ellipse(const pen& pen, float x, float y, float width, float height) {draw_ellipse(pen, static_cast<int32_t>(x), static_cast<int32_t>(y), static_cast<int32_t>(width), static_cast<int32_t>(height));}
+      
+      void draw_ellipse(const pen& pen, const rectangle& rect) {draw_ellipse(pen, rect.x(), rect.y(), rect.width(), rect.height());}
+      
+      void draw_ellipse(const pen& pen, const rectangle_f& rect) {draw_ellipse(pen, rect.x(), rect.y(), rect.width(), rect.height());}
+
       void draw_line(const pen& pen, int32_t x1, int32_t y1, int32_t x2, int32_t y2);
       
       void draw_line(const pen& pen, float x1, float y1, float x2, float y2) {draw_line(pen, static_cast<int32_t>(x1), static_cast<int32_t>(y1), static_cast<int32_t>(x2), static_cast<int32_t>(y2));}
@@ -47,6 +63,22 @@ namespace xtd {
       
       void draw_rectangle(const pen& pen, const rectangle_f& rect) {draw_rectangle(pen, rect.x(), rect.y(), rect.width(), rect.height());}
       
+      void fill_ellipse(const brush& brush, int32_t x, int32_t y, int32_t width, int32_t height);
+      
+      void fill_ellipse(const brush& brush, float x, float y, float width, float height) {fill_ellipse(brush, static_cast<int32_t>(x), static_cast<int32_t>(y), static_cast<int32_t>(width), static_cast<int32_t>(height));}
+      
+      void fill_ellipse(const brush& brush, const rectangle& rect) {fill_ellipse(brush, rect.x(), rect.y(), rect.width(), rect.height());}
+      
+      void fill_ellipse(const brush& brush, const rectangle_f& rect) {fill_ellipse(brush, rect.x(), rect.y(), rect.width(), rect.height());}
+      
+      void fill_pie(const brush& brush, int32_t x, int32_t y, int32_t width, int32_t height, int32_t start_angle, int32_t sweep_angle);
+      
+      void fill_pie(const brush& brush, float x, float y, float width, float height, float start_angle, float sweep_angle) {fill_pie(brush, static_cast<int32_t>(x), static_cast<int32_t>(y), static_cast<int32_t>(width), static_cast<int32_t>(height), static_cast<int32_t>(start_angle), static_cast<int32_t>(sweep_angle));}
+      
+      void fill_pie(const brush& brush, const rectangle& rect, float start_angle, float sweep_angle) {fill_pie(brush, rect.x(), rect.y(), rect.width(), rect.height(), static_cast<int32_t>(start_angle), static_cast<int32_t>(sweep_angle));}
+      
+      void fill_pie(const brush& brush, const rectangle_f& rect, float start_angle, float sweep_angle) {fill_pie(brush, rect.x(), rect.y(), rect.width(), rect.height(), start_angle, sweep_angle);}
+
       void fill_rectangle(const brush& brush, int32_t x, int32_t y, int32_t width, int32_t height);
       
       void fill_rectangle(const brush& brush, float x, float y, float width, float height) {fill_rectangle(brush, static_cast<int32_t>(x), static_cast<int32_t>(y), static_cast<int32_t>(width), static_cast<int32_t>(height));}
