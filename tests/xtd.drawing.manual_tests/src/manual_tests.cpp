263b2f9ee887d916031ab646901368a3dacb6a59
Gammasoft71 <gammasoft71@gmail.com>
2019-08-28 10:12:05 +0200

    review code

--- tests/xtd.drawing.manual_tests/src/manual_tests.cpp
+++ tests/xtd.drawing.manual_tests/src/manual_tests.cpp
@@ -6,11 +6,6 @@ using namespace xtd;
 using namespace xtd::drawing;
 
 int main() {
-  cout << format("families = {{{}}}", strings::join(", ", font_family::families())) << endl;
-  cout << format("Serif = {}", font_family::generic_serif()) << endl;
-  cout << format("Sans Serif = {}", font_family::generic_sans_serif()) << endl;
-  cout << format("Monspace = {}", font_family::generic_monospace()) << endl;
-  cout << format("arial = {}", font_family("arial")) << endl;
-  cout << format("arial.ascent = {}", font_family("arial").get_cell_ascent(font_style::regular)) << endl;
-  cout << format("arial.descent = {}", font_family("arial").get_cell_descent(font_style::regular)) << endl;
+  font font("Arial", 12, font_style::italic);
+  cout << format("font = {}", font) << endl;
 }
