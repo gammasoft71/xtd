44b49d4bc9d80a9c8416db15f15115af1642c21b
Gammasoft71 <gammasoft71@gmail.com>
2019-08-31 21:27:46 +0200

    Update manual_tests.cpp

--- tests/xtd.forms.manual_tests/src/manual_tests.cpp
+++ tests/xtd.forms.manual_tests/src/manual_tests.cpp
@@ -1,47 +1,12 @@
 #include <xtd/xtd.forms>
 
-using namespace std;
-using namespace xtd;
-using namespace xtd::drawing;
 using namespace xtd::forms;
 
 class form_test : public form {
 public:
-  drawing::size compute_label_size(const label& label) const {
-    return drawing::size::ceiling(this->create_graphics().measure_string(label.text(), label.font())) + drawing::size(2 + (label.border_style() == border_style::none ? 0 : 4), 1 + (label.border_style() == border_style::none ? 0 : 4));
-  }
-
   form_test() {
     this->text("xtd.forms.manual_tests");
-    this->font(drawing::font("Arial", 34, font_style::bold | font_style::italic));
-    this->controls().push_back(this->label1);
-    this->controls().push_back(this->label2);
-
-    this->label1.auto_size(true);
-    this->label1.location({0, 0});
-    this->label1.back_color(color::dark_green);
-    this->label1.text("Hello, World!");
-    cdebug << format("label1.size = {}", label1.size()) << endl;
-
-    
-    this->label2.location({0, 62});
-    this->label2.back_color(color::dark_green);
-    this->label2.text("Hello, World!");
-    this->label2.size(compute_label_size(this->label2));
-    cdebug << format("label2.size = {}", label2.size()) << endl;
-
-    /*
-    this->paint += [&](const control& sender, paint_event_args& e) {
-      drawing::size size = drawing::size::truncate(e.graphics().measure_string(this->label1.text(), this->label1.font()));
-      //e.graphics().draw_rectangle(pen(color::red), 0, 60, size.width() + 4, size.height() + 4);
-      e.graphics().draw_string(this->label1.text(), this->label1.font(), solid_brush(system_colors::control_text), {{2.0f, 62.0f}, size});
-      //cdebug << format("measure_string = {}", size) << endl;
-    };
-     */
   }
-private:
-  label label1;
-  label label2;
 };
 
 int main() {
