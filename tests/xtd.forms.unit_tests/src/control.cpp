3d1da0c02e137f31e61bea52a816363d3dd52f21
Gammasoft71 <gammasoft71@gmail.com>
2019-09-01 18:46:26 +0200

    Update control.cpp

--- tests/xtd.forms.unit_tests/src/control.cpp
+++ tests/xtd.forms.unit_tests/src/control.cpp
@@ -11,76 +11,102 @@ namespace unit_tests {
   class test_class_(test_control) {
   public:
     void test_method_(new_control_back_color) {
+      /*
       control control;
-      //assert::are_equal_(system_colors::control(), control.back_color());
+      assert::are_equal_(system_colors::control(), control.back_color());
+       */
     }
     
-    /*
     void test_method_(new_control_client_size) {
+      /*
       control control;
       assert::are_equal_(drawing::size(-1, -1), control.client_size());
+       */
     }
     
     void test_method_(new_control_default_back_color) {
+      /*
       control control;
-      //assert::are_equal_(system_colors::control(), control.default_back_color());
+      assert::are_equal_(system_colors::control(), control.default_back_color());
+       */
     }
     
     void test_method_(new_control_default_fore_color) {
+      /*
       control control;
-      //assert::are_equal_(system_colors::control_text(), control.default_fore_color());
+      assert::are_equal_(system_colors::control_text(), control.default_fore_color());
+       */
     }
 
     void test_method_(new_control_default_size) {
+      /*
       control control;
       assert::are_equal_(drawing::size(0, 0), control.default_size());
+       */
     }
     
     void test_method_(new_control_enabled) {
+      /*
       control control;
       assert::is_true_(control.enabled());
+       */
     }
     
     void test_method_(new_control_fore_color) {
+      /*
       control control;
-      //assert::are_equal_(system_colors::control_text(), control.fore_color());
+      assert::are_equal_(system_colors::control_text(), control.fore_color());
+       */
     }
     
     void test_method_(new_control_handle) {
+      /*
       control control;
       assert::are_equal_(0, control.native_handle());
+       */
     }
     
     void test_method_(new_control_location) {
+      /*
       control control;
       assert::are_equal_(point(-1, -1), control.location());
+       */
     }
     
     void test_method_(new_control_parent) {
+      /*
       control control;
       assert::are_same_(control::null, control.parent());
+       */
     }
     
     void test_method_(new_control_size) {
+       /*
       control control;
       assert::are_equal_(drawing::size(0, 0), control.size());
+        */
     }
     
     void test_method_(new_control_text) {
+      /*
       control control;
       assert::is_empty_(control.text());
+       */
     }
     
     void test_method_(new_control_visible) {
+      /*
       control control;
       assert::is_true_(control.visible());
+       */
     }
     
     void test_method_(create_control_without_parent) {
+      /*
       control control;
-      //assert::throws_(std::invalid_argument, [&] {control.create_control();});
+      assert::throws_(std::invalid_argument, [&] {control.create_control();});
+       */
     }
-     */
     
     void test_method_(create_control_with_parent) {
       /*
