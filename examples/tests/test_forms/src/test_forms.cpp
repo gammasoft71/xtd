d5454a8673bde36757b3d4294acb3024daa19fef
Gammasoft71 <gammasoft71@gmail.com>
2019-09-04 01:01:46 +0200

    Update test_forms.cpp

--- examples/tests/test_forms/src/test_forms.cpp
+++ examples/tests/test_forms/src/test_forms.cpp
@@ -4,15 +4,8 @@ using namespace xtd;
 using namespace xtd::forms;
 
 int main() {
-  message_box::show("Invalid argument exception: Bad index value!", "Error", message_box_buttons::ok, message_box_icon::exclamation);
-
   form form;
-  form.text("Test forms");
-  
-  button button;
-  button.parent(form).location({30, 30}).text("Click me").click += [] {
-    message_box::show("Hello, World!");
-  };
-  
+  form.text("Test forms").controls().push_back(button().parent(form).location({30, 30}).text("Click me"));
+  form.controls()[0].get().click += [] {message_box::show("Hello, World!");};
   application::run(form);
 }
