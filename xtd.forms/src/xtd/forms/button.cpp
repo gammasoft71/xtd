07f3b3f812d3570e9c3332a7d9d9d6298bb6dc82
Gammasoft71 <gammasoft71@gmail.com>
2019-09-06 22:06:44 +0200

    inherits button_base

--- xtd.forms/src/xtd/forms/button.cpp
+++ xtd.forms/src/xtd/forms/button.cpp
@@ -1,4 +1,5 @@
 #include "../../../include/xtd/forms/button.hpp"
+#include <xtd/forms/native/window_button.hpp>
 
 using namespace xtd;
 using namespace xtd::forms;
@@ -12,3 +13,15 @@ button& button::operator=(const button& value) {
   return *this;
 }
 
+forms::create_params button::create_params() const {
+  forms::create_params create_params = this->button_base::create_params();
+  
+  create_params.style(create_params.style() | BS_PUSHBUTTON);
+  
+  return create_params;
+}
+
+button& button::auto_size_mode(forms::auto_size_mode value) {
+  this->set_auto_size_mode(value);
+  return *this;
+}
--- xtd.forms/src/xtd/forms/button_base.cpp
+++ xtd.forms/src/xtd/forms/button_base.cpp
@@ -16,16 +16,10 @@ button_base& button_base::operator=(const button_base& value) {
   return *this;
 }
 
-button_base& button_base::auto_size_mode(forms::auto_size_mode value) {
-  this->set_auto_size_mode(value);
-  return *this;
-}
-
 forms::create_params button_base::create_params() const {
   forms::create_params create_params = this->control::create_params();
   
   create_params.class_name("BUTTON");
-  create_params.style(create_params.style() | BS_PUSHBUTTON);
   
   return create_params;
 }
