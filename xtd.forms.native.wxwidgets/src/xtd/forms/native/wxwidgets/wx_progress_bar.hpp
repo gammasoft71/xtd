422ce3bb9f8b6dd70797072069a0a76a2ff5d8f1
gammasoft71 <gammasoft71@gmail.com>
2019-09-11 20:31:56 +0200

    Update wx_progress_bar.hpp

--- xtd.forms.native.wxwidgets/src/xtd/forms/native/wxwidgets/wx_progress_bar.hpp
+++ xtd.forms.native.wxwidgets/src/xtd/forms/native/wxwidgets/wx_progress_bar.hpp
@@ -15,10 +15,15 @@ namespace xtd {
           this->control_handler::create<wxGauge>(reinterpret_cast<control_handler*>(create_params.parent())->control(), wxID_ANY, 100, wxPoint(create_params.x(), create_params.y()), wxSize(create_params.width(), create_params.height()), control_handler::progress_bar_to_wx_style(create_params.style(), create_params.ex_style()));
           
           this->timer_marquee.Bind(wxEVT_TIMER, [&](wxTimerEvent& event) {
-            static_cast<wxGauge*>(this->control())->Pulse();
+            if (event.GetTimer().GetId() == this->timer_marquee.GetId() && !static_cast<wxGauge*>(this->control())->IsBeingDeleted())
+              static_cast<wxGauge*>(this->control())->Pulse();
           });
         }
         
+        ~wx_progress_bar() {
+          this->timer_marquee.Stop();
+        }
+
         void marquee(bool marquee, size_t animation_speed) {
           if (marquee) this->timer_marquee.Start(animation_speed);
           else this->timer_marquee.Stop();
