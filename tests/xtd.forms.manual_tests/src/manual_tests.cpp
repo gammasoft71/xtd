bd23af29317c87f10f7a5043e104045fc61fd42d
Gammasoft71 <gammasoft71@gmail.com>
2019-09-16 00:38:48 +0200

    fix start_position

--- tests/xtd.forms.manual_tests/src/manual_tests.cpp
+++ tests/xtd.forms.manual_tests/src/manual_tests.cpp
@@ -5,10 +5,12 @@ using namespace xtd;
 using namespace xtd::forms;
 
 int main() {
-  auto form1 = control::create<form>("form1");
+  auto form1 = control::create<form>("form1", {20, 20}, {800, 450});
+  form1.start_position(form_start_position::center_screen);
   auto button1 = control::create<button>(form1, "Dialog", {10, 10});
   button1.click += [&] {
     auto dialog = control::create<form>("dialog", {200, 400});
+    dialog.start_position(form_start_position::center_parent);
     auto button_ok = control::create<button>(dialog, "OK", {10, 10});
     button_ok.dialog_result(forms::dialog_result::ok);
     auto button_cancel = control::create<button>(dialog, "Cancel", {100, 10});
