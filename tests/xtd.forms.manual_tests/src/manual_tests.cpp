fb79f90f4de56b96273c480a539a6b017f2a6d77
Gammasoft71 <gammasoft71@gmail.com>
2019-09-16 01:01:31 +0200

    create unique_ptr...

--- tests/xtd.forms.manual_tests/src/manual_tests.cpp
+++ tests/xtd.forms.manual_tests/src/manual_tests.cpp
@@ -6,20 +6,20 @@ using namespace xtd::forms;
 
 int main() {
   auto form1 = control::create<form>("form1", {20, 20}, {800, 450});
-  form1.start_position(form_start_position::center_screen);
+  form1->start_position(form_start_position::center_screen);
   
-  auto button1 = control::create<button>(form1, "Dialog", {10, 10});
-  button1.click += [&] {
+  auto button1 = control::create<button>(*form1, "Dialog", {10, 10});
+  button1->click += [&] {
     auto dialog = control::create<form>("dialog");
-    dialog.start_position(form_start_position::center_parent);
+    dialog->start_position(form_start_position::center_parent);
   
-    auto button_ok = control::create<button>(dialog, "OK", {10, 10});
-    button_ok.dialog_result(forms::dialog_result::ok);
+    auto button_ok = control::create<button>(*dialog, "OK", {10, 10});
+    button_ok->dialog_result(forms::dialog_result::ok);
     
-    auto button_cancel = control::create<button>(dialog, "Cancel", {100, 10});
-    button_cancel.dialog_result(forms::dialog_result::cancel);
+    auto button_cancel = control::create<button>(*dialog, "Cancel", {100, 10});
+    button_cancel->dialog_result(forms::dialog_result::cancel);
     
-    cdebug << format("dialog_result = {}", dialog.show_dialog()) << endl;
+    cdebug << format("dialog_result = {}", dialog->show_dialog()) << endl;
   };
-  application::run(form1);
+  application::run(*form1);
 }
