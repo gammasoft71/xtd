822a4b8a517891c227e29479619405873a9b2640
Gammasoft71 <gammasoft71@gmail.com>
2019-09-19 11:18:06 +0200

    create __get_temp_path method

--- xtd.io/src/path_win32.cpp
+++ xtd.io/src/path_win32.cpp
@@ -14,5 +14,11 @@ bool __is_windows_os() noexcept {
   return true;
 }
 
+std::string __get_temp_path() noexcept {
+  if (getenv("TMP") != nullptr)  return getenv("TMP");
+  if (getenv("TEMP") != nullptr) return getenv("TEMP");
+  if (getenv("USERPROFILE") != nullptr) return getenv("USERPROFILE");
+  return getenv("WINDIR");
+}
 
 #endif
