11237a4c7aab46bed799fcb56876cadad35884f4
Gammasoft71 <gammasoft71@gmail.com>
2019-09-19 10:23:23 +0200

    fix panel size

--- xtd.forms.native.wxwidgets/src/xtd/forms/native/wxwidgets/wx_panel.hpp
+++ xtd.forms.native.wxwidgets/src/xtd/forms/native/wxwidgets/wx_panel.hpp
@@ -12,6 +12,7 @@ namespace xtd {
         wx_panel(const forms::create_params& create_params) {
           if (!create_params.parent()) throw std::invalid_argument("control must have a parent");
           this->control_handler::create<wxPanel>(reinterpret_cast<control_handler*>(create_params.parent())->control(), wxID_ANY, wxPoint(create_params.x(), create_params.y()), wxSize(create_params.width(), create_params.height()), control_handler::panel_to_wx_style(create_params.style(), create_params.ex_style()));
+          this->control()->SetSize(create_params.width(), create_params.height());
         }
       };
     }
