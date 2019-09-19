5e219e491a5ad0b63668099bf53093c7a88d2455
Gammasoft71 <gammasoft71@gmail.com>
2019-09-19 10:23:23 +0200

    fix panel size

--- tests/xtd.forms.manual_tests/src/manual_tests.cpp
+++ tests/xtd.forms.manual_tests/src/manual_tests.cpp
@@ -8,13 +8,13 @@ int main() {
   auto form_main = control::create<form>("Form main");
   form_main->client_size({800, 450});
   
-  auto panel_left = control::create<panel>(*form_main, "panel_left", {10, 10}, {340, 420});
+  auto panel_left = control::create<panel>(*form_main, "panel_left", {10, 10}, {340, 430});
   panel_left->border_style(forms::border_style::fixed_single);
   
   auto list_box_numbers = control::create<list_box>(*panel_left, "list_box_numbers", {10, 10}, {150, 250});
   list_box_numbers->items().push_back_range({"One", "two", "Three", "Four", "Five", "Six", "Seven"});
   
-  auto panel_right = control::create<panel>(*form_main, "panel_right", {450, 10}, {330, 420});
+  auto panel_right = control::create<panel>(*form_main, "panel_right", {450, 10}, {340, 430});
   panel_right->border_style(forms::border_style::fixed_single);
   
   auto button_move = control::create<button>(*form_main, "Move", {365, 50});
