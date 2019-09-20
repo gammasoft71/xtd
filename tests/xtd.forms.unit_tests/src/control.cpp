7ed72164d7ff0e1def94c53a75c6e220b6d238a6
Gammasoft71 <gammasoft71@gmail.com>
2019-09-20 19:37:24 +0200

    add unit tests

--- tests/xtd.forms.unit_tests/src/control.cpp
+++ tests/xtd.forms.unit_tests/src/control.cpp
@@ -10,11 +10,21 @@ using namespace xtd::tunit;
 namespace unit_tests {
   class test_class_(test_control) {
   public:
+    void test_method_(new_control_auto_size) {
+      control control;
+      assert::is_false_(control.auto_size());
+    }
+
     void test_method_(new_control_back_color) {
       control control;
       assert::are_equal_(system_colors::control(), control.back_color());
     }
     
+    void test_method_(new_control_bottom) {
+      control control;
+      assert::is_zero_(control.bottom());
+    }
+
     void test_method_(new_control_client_size) {
       control control;
       assert::are_equal_(drawing::size(0, 0), control.client_size());
@@ -26,95 +36,95 @@ namespace unit_tests {
     }
     
     void test_method_(new_control_default_fore_color) {
-      /*
       control control;
       assert::are_equal_(system_colors::control_text(), control.default_fore_color());
-       */
     }
 
     void test_method_(new_control_default_size) {
-      /*
       control control;
       assert::are_equal_(drawing::size(0, 0), control.default_size());
-       */
     }
     
     void test_method_(new_control_enabled) {
-      /*
       control control;
       assert::is_true_(control.enabled());
-       */
     }
     
     void test_method_(new_control_fore_color) {
-      /*
       control control;
       assert::are_equal_(system_colors::control_text(), control.fore_color());
-       */
     }
     
     void test_method_(new_control_handle) {
-      /*
       control control;
-      assert::are_equal_(0, control.native_handle());
-       */
+      assert::is_zero_(control.handle());
     }
     
     void test_method_(new_control_location) {
-      /*
       control control;
-      assert::are_equal_(point(-1, -1), control.location());
-       */
+      assert::are_equal_(point(0, 0), control.location());
     }
     
     void test_method_(new_control_parent) {
-      /*
       control control;
-      assert::are_same_(control::null, control.parent());
-       */
+      assert::is_false_(control.parent().has_value());
     }
     
     void test_method_(new_control_size) {
-       /*
       control control;
       assert::are_equal_(drawing::size(0, 0), control.size());
-        */
     }
     
     void test_method_(new_control_text) {
-      /*
       control control;
       assert::is_empty_(control.text());
-       */
     }
     
     void test_method_(new_control_visible) {
-      /*
       control control;
       assert::is_true_(control.visible());
-       */
     }
     
     void test_method_(create_control_without_parent) {
-      /*
       control control;
       assert::throws_(std::invalid_argument, [&] {control.create_control();});
-       */
     }
     
     void test_method_(create_control_with_parent) {
-      /*
       form form;
-      form.name("form");
       control control;
-      control.name("control");
       control.parent(form);
-      form.show();
-      assert::are_equal_(drawing::size(0, 0), control.client_size());
-      assert::are_not_equal_(0, control.native_handle());
-      assert::are_equal_(xtd::drawing::point(-1, -1), control.location());
-      assert::are_equal_(xtd::drawing::size(0, 0), control.size());
-       */
+      assert::are_equal_(1, form.controls().size());
+      assert::are_equal_(control, form.controls()[0].get());
+    }
+
+    void test_method_(create_control_with_parent_back_color) {
+      form form;
+      control control;
+      control.parent(form);
+      assert::are_equal_(form.back_color(), control.back_color());
+    }
+
+    void test_method_(create_control_with_parent_fore_color) {
+      form form;
+      control control;
+      control.parent(form);
+      assert::are_equal_(form.fore_color(), control.fore_color());
+    }
+
+    void test_method_(create_control_with_parent_handle) {
+      form form;
+      control control;
+      control.parent(form);
+      assert::is_not_zero_(control.handle());
+    }
+
+    void test_method_(create_control_with_parent_parent) {
+      form form;
+      control control;
+      control.parent(form);
+      assert::is_true(control.parent().has_value());
+      assert::are_equal_(form, control.parent().value().get());
     }
   };
 }
