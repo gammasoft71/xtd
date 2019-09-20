489d77dd71459a6f0f0be8fee6d51b4f8b4575f4
Gammasoft71 <gammasoft71@gmail.com>
2019-09-20 17:52:20 +0200

    fix unit tests

--- tests/xtd.forms.unit_tests/src/main.cpp
+++ tests/xtd.forms.unit_tests/src/main.cpp
@@ -1,7 +1,12 @@
+#include <xtd/xtd.forms>
 #include <xtd/xtd.tunit>
 
+using namespace xtd::forms;
 using namespace xtd::tunit;
 
 int main() {
-  return console_unit_test().run();
+  application::initialize();
+  int result = console_unit_test().run();
+  application::cleanup();
+  return result;
 }
