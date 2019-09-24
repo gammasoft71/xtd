f8d6169cce6cad9fe2e0bb8395f544bf87e942c4
Gammasoft71 <gammasoft71@gmail.com>
2019-09-24 14:21:29 +0200

    Update manual_tests.cpp

--- tests/xtd.forms.manual_tests/src/manual_tests.cpp
+++ tests/xtd.forms.manual_tests/src/manual_tests.cpp
@@ -8,12 +8,13 @@ class form1 : public form {
 public:
   form1() {
     this->text("Cursor example");
-    this->client_size({400, 240});
+    this->client_size({420, 240});
 
     this->list_box_cursors.parent(*this);
     this->list_box_cursors.anchor(anchor_styles::top | anchor_styles::left | anchor_styles::bottom);
-    this->list_box_cursors.bounds({20, 20, 150, 200});
-    this->list_box_cursors.items().push_back_range({{"Application Starting", cursors::app_starting()}, {"Arrow", cursors::arrow()}, {"Close hand", cursors::close_hand()}, {"Contextual menu", cursors::contextual_menu()}, {"Cross", cursors::cross()}, {"Default cursor", cursors::default_cursor()}, {"Disappearing item", cursors::disappearing_item()}, {"Drag copy", cursors::drag_copy()}, {"Drag link", cursors::drag_link()}, {"Hand", cursors::hand()}, {"Help", cursors::help()}, {"Horizontal split", cursors::hsplit()}, {"I beam", cursors::ibeam()}, {"No", cursors::no()}, {"No move 2d", cursors::no_move_2d()}, {"No move horizontal", cursors::no_move_horiz()}, {"No move vertical", cursors::no_move_vert()}, {"Open hand", cursors::open_hand()}, {"Pan east", cursors::pan_east()}, {"Pan north east", cursors::pan_ne()}, {"Pan north", cursors::pan_north()}, {"Pan north west", cursors::pan_nw()}, {"Pan south", cursors::pan_south()}, {"Pan south west", cursors::pan_sw()}, {"Pan West", cursors::pan_west()}, {"Size all", cursors::size_all()}, {"Size north-east south-west", cursors::size_nesw()}, {"Size north south", cursors::size_ns()}, {"Size north-west south-east", cursors::size_nwse()}, {"Up arrow", cursors::up_arrow()}, {"Vertical I beam", cursors::vibeam()}, {"Vertical split", cursors::vsplit()}, {"Wait cursor", cursors::wait_cursor()}});
+    this->list_box_cursors.bounds({20, 20, 170, 200});
+    this->list_box_cursors.items().push_back_range({cursors::app_starting(), cursors::arrow(), cursors::close_hand(), cursors::contextual_menu(), cursors::cross(), cursors::default_cursor(), cursors::disappearing_item(), cursors::drag_copy(), cursors::drag_link(), cursors::hand(), cursors::help(), cursors::hsplit(), cursors::ibeam(), cursors::no(), cursors::no_move_2d(), cursors::no_move_horiz(), cursors::no_move_vert(), cursors::open_hand(), cursors::pan_east(), cursors::pan_ne(), cursors::pan_north(), cursors::pan_nw(), cursors::pan_south(), cursors::pan_sw(), cursors::pan_west(), cursors::size_all(), cursors::size_nesw(), cursors::size_ns(), cursors::size_nwse(), cursors::up_arrow(), cursors::vibeam(), cursors::vsplit(), cursors::wait_cursor()});
+    this->list_box_cursors.selected_index(5);
     this->list_box_cursors.selected_index_changed += [this] {
       this->panel_test_zone.cursor(any_cast<forms::cursor>(this->list_box_cursors.selected_item().tag()));
       cdebug << format("cursor = {}", this->panel_test_zone.cursor()) << endl;
@@ -23,11 +24,11 @@ public:
     this->panel_test_zone.anchor(anchor_styles::top | anchor_styles::left | anchor_styles::bottom | anchor_styles::right);
     this->panel_test_zone.border_style(border_style::fixed_3d);
     this->panel_test_zone.back_color(drawing::system_colors::window());
-    this->panel_test_zone.bounds({190, 20, 190, 160});
+    this->panel_test_zone.bounds({210, 20, 190, 160});
 
     this->button_move_up_left.parent(*this);
     this->button_move_up_left.anchor(anchor_styles::right | anchor_styles::bottom);
-    this->button_move_up_left.location({190, 200});
+    this->button_move_up_left.location({210, 200});
     this->button_move_up_left.text("To Up/Left");
     this->button_move_up_left.click += [&] {
       forms::cursor::position({0, 0});
@@ -35,7 +36,7 @@ public:
       
      this->button_show_hide.parent(*this);
      this->button_show_hide.anchor(anchor_styles::right | anchor_styles::bottom);
-     this->button_show_hide.location({305, 200});
+     this->button_show_hide.location({325, 200});
      this->button_show_hide.text("Hide");
      this->button_show_hide.click += [&] {
        forms::cursor::hide();
