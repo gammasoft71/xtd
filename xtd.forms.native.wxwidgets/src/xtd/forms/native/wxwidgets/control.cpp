963e63d828ea6ffd8a79e90e92076fa0e1a87a42
Gammasoft71 <gammasoft71@gmail.com>
2019-09-26 17:18:32 +0200

    fix client_rect

--- xtd.forms.native.wxwidgets/src/xtd/forms/native/wxwidgets/control.cpp
+++ xtd.forms.native.wxwidgets/src/xtd/forms/native/wxwidgets/control.cpp
@@ -179,7 +179,7 @@ void control::init() {
 drawing::rectangle control::client_rectangle(intptr_t control) {
   if (control == 0) return {};
   wxRect rect = reinterpret_cast<control_handler*>(control)->control()->GetClientRect();
-  return {rect.GetX(), rect.GetY(), rect.GetWidth(), rect.GetHeight()};
+  return {{rect.GetX(), rect.GetY()}, client_size(control)};
 }
 
 drawing::size control::client_size(intptr_t control) {
