af4a521c6ce17e5e3fecfefe33fdefe1dca31fe5
Gammasoft71 <gammasoft71@gmail.com>
2019-09-27 15:35:19 +0200

    Update user_control.cpp

--- examples/common_controls/user_control/src/user_control.cpp
+++ examples/common_controls/user_control/src/user_control.cpp
@@ -12,10 +12,10 @@ namespace examples {
 
       this->user_control1.parent(*this);
       this->user_control1.cursor(cursors::hand());
-      this->user_control1.back_color(color::spring_green);
+      this->user_control1.back_color(color::light_blue);
       this->user_control1.location({50, 50});
-      this->user_control1.click += [&](const control& sender, const event_args& e) {
-        this->user_control1.back_color(this->user_control1.back_color() == color::spring_green ? color::orange_red : color::spring_green);
+      this->user_control1.double_click += [&](const control& sender, const event_args& e) {
+        this->user_control1.back_color(this->user_control1.back_color() == color::light_blue ? color::light_pink : color::light_blue);
       };
     }
     
