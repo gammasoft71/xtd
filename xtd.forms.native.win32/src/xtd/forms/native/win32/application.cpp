a9be7c359ee3debc1dfc5634b018d79b4e52c90b
Gammasoft71 <gammasoft71@gmail.com>
2019-09-27 13:34:43 +0200

    add future native prots

---
+++ xtd.forms.native.win32/src/xtd/forms/native/win32/application.cpp
@@ -0,0 +1,13 @@
+#include <cstdint>
+#include <vector>
+#include <xtd/version.hpp>
+#include <xtd/forms/native/application.hpp>
+#include <xtd/forms/window_messages.hpp>
+
+using namespace std;
+using namespace xtd;
+using namespace xtd::forms::native;
+
+xtd::version __xtd_forms_native_win32_version() {
+  return {0, 0, 0};
+}
