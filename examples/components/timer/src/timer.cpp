5e53a1c1215bd3aca2df7296d12d1b9341adc2b7
Gammasoft71 <gammasoft71@gmail.com>
2019-09-30 01:31:25 +0200

    Update timer.cpp

--- examples/components/timer/src/timer.cpp
+++ examples/components/timer/src/timer.cpp
@@ -8,24 +8,24 @@ namespace examples {
   class form1 : public form {
   public:
     form1() {
-      label.parent(*this);
-      label.text("0.0");
-      label.font(drawing::font("Arial", 48, font_style::italic));
-      label.location({10, 10});
-      label.auto_size(true);
-      label.fore_color(color::dodger_blue);
+      label1.parent(*this);
+      label1.text("0.0");
+      label1.font(drawing::font("Arial", 48, font_style::italic));
+      label1.location({10, 10});
+      label1.auto_size(true);
+      label1.fore_color(color::dodger_blue);
 
-      button.parent(*this);
-      button.text("Start");
-      button.location({10, 90});
-      button.click += [this](const control& sender, const event_args& e) {
-        timer.enabled(!timer.enabled());
-        button.text(timer.enabled() ? "Stop": "Start");
+      button1.parent(*this);
+      button1.text("Start");
+      button1.location({10, 90});
+      button1.click += [this](const control& sender, const event_args& e) {
+        timer1.enabled(!timer1.enabled());
+        button1.text(timer1.enabled() ? "Stop": "Start");
       };
 
-      timer.interval(100);
-      timer.tick += [this](const forms::timer& sender, const event_args& e) {
-        label.text(strings::format("{:F1}", ++counter / 10.0));
+      timer1.interval(100);
+      timer1.tick += [this](const forms::timer& sender, const event_args& e) {
+        label1.text(strings::format("{:F1}", ++counter / 10.0));
       };
 
       text("Timer example");
@@ -33,9 +33,9 @@ namespace examples {
     }
     
   private:
-    forms::timer timer;
-    forms::label label;
-    forms::button button;
+    timer timer1;
+    label label1;
+    button button1;
     int counter = 0;
   };
 }
