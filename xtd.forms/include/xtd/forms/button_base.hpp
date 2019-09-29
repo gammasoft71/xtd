8b8cc2b90d429fb432b5730f009c92693dd3d1bd
Gammasoft71 <gammasoft71@gmail.com>
2019-09-30 00:21:52 +0200

    middle_center by default

--- xtd.forms/include/xtd/forms/button_base.hpp
+++ xtd.forms/include/xtd/forms/button_base.hpp
@@ -66,7 +66,7 @@ namespace xtd {
     protected:
       /// @cond
       bool auto_ellipsis_ = false;
-      content_alignment text_align_ = content_alignment::middle_left;
+      content_alignment text_align_ = content_alignment::middle_center;
       /// @endcond
     };
   }
