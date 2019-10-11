477802b5dbc8ee1c4c1edb34dc4eb3fffa516277
Gammasoft71 <gammasoft71@gmail.com>
2019-10-11 22:48:13 +0200

    add window_style

--- tests/xtd.forms.manual_tests/src/manual_tests.cpp
+++ tests/xtd.forms.manual_tests/src/manual_tests.cpp
@@ -9,6 +9,7 @@ public:
     text("Tab control example");
     controls().push_back(tab_control1);
     client_size({390, 270});
+    //window_state(form_window_state::minimized);
 
     tab_control1.anchor(anchor_styles::left | anchor_styles::top | anchor_styles::right | anchor_styles::bottom);
     tab_control1.location({10, 10});
@@ -23,6 +24,27 @@ public:
     
     tab_page3.parent(tab_control1);
     tab_page3.text("Tab page 3");
+    
+    button_maximize.parent(tab_page1);
+    button_maximize.location({10, 10});
+    button_maximize.text("Maximize");
+    button_maximize.click += [&] {
+      window_state(form_window_state::maximized);
+    };
+    
+    button_minimize.parent(tab_page1);
+    button_minimize.location({100, 10});
+    button_minimize.text("Minimize");
+    button_minimize.click += [&] {
+      window_state(form_window_state::minimized);
+    };
+    
+    button_normal.parent(tab_page1);
+    button_normal.location({190, 10});
+    button_normal.text("Normal");
+    button_normal.click += [&] {
+      window_state(form_window_state::normal);
+    };
   }
   
 private:
@@ -30,6 +52,9 @@ private:
   tab_page tab_page1;
   tab_page tab_page2;
   tab_page tab_page3;
+  button button_maximize;
+  button button_minimize;
+  button button_normal;
 };
 
 int main() {
