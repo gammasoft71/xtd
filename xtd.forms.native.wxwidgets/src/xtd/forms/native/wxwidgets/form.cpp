6ba11af1fb645509b1d96deba75dc3c29b677599
Gammasoft71 <gammasoft71@gmail.com>
2019-10-12 09:29:40 +0200

    native::form review

--- xtd.forms.native.wxwidgets/src/xtd/forms/native/wxwidgets/form.cpp
+++ xtd.forms.native.wxwidgets/src/xtd/forms/native/wxwidgets/form.cpp
@@ -9,29 +9,36 @@ using namespace xtd::drawing;
 using namespace xtd::forms::native;
 
 void form::activate(intptr_t form) {
+  if (form == 0) return;
   reinterpret_cast<control_handler*>(form)->control()->Raise();
 }
 
 void form::close(intptr_t form) {
+  if (form == 0) return;
   reinterpret_cast<control_handler*>(form)->control()->Close();
 }
 
 void form::maximize(intptr_t form, bool maximize) {
+  if (form == 0) return;
   static_cast<wxTopLevelWindow*>(reinterpret_cast<control_handler*>(form)->control())->Maximize(maximize);
 }
 
 void form::minimize(intptr_t form, bool minimize) {
+  if (form == 0) return;
   static_cast<wxTopLevelWindow*>(reinterpret_cast<control_handler*>(form)->control())->Iconize(minimize);
 }
 
 void form::default_control(intptr_t form, intptr_t default_control) {
+  if (form == 0 || default_control == 0) return;
   static_cast<wxTopLevelWindow*>(reinterpret_cast<control_handler*>(form)->control())->SetDefaultItem(reinterpret_cast<control_handler*>(default_control)->control());
 }
 
 int32_t form::show_dialog(intptr_t form) {
+  if (form == 0) return 0;
   return static_cast<wxDialog*>(reinterpret_cast<control_handler*>(form)->control())->ShowModal();
 }
 
 void form::end_dialog(intptr_t form, int32_t result) {
+  if (form == 0) return;
   static_cast<wxDialog*>(reinterpret_cast<control_handler*>(form)->control())->EndModal(result);
 }
