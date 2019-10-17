3b60d0c373bfac612811c835573c48c18958b2e7
Gammasoft71 <gammasoft71@gmail.com>
2019-10-17 10:01:06 +0200

    optimization

--- examples/others/game_of_life/grid.hpp
+++ examples/others/game_of_life/grid.hpp
@@ -9,8 +9,8 @@ namespace game_of_life {
   
   class grid {
   public:
-    static constexpr int columns = 500; //5000;
-    static constexpr int rows = 500; //5000;
+    static constexpr int columns = 700; // 5200
+    static constexpr int rows = 700; // 2900
 
     grid() = default;
 
@@ -18,49 +18,58 @@ namespace game_of_life {
     cell_collection& cells() {return cells_;}
 
     void clear() {cells_ = {columns, cell_row_collection {rows}};}
-
-    void evolve() {
-      cell_collection current_cells = cells_;
-
+    
+    void next() {
+      struct updated_cell {
+        int x = 0;
+        int y = 0;
+        cell state = cell::dead;
+      };
+      std::vector<updated_cell> updated_cells;
+      
       for (int y = 0; y < columns; y++) {
         for (int x = 0; x < rows; x++) {
-          int neighbors = 0;
-
-          if ((x - 1) >= 0 && current_cells[x - 1][y] == cell::alive)
-            neighbors++;
-          if ((x + 1) < rows && current_cells[x + 1][y] == cell::alive)
-            neighbors++;
-
-          if ((y - 1) >= 0) {
-            if ((x - 1) >= 0 && current_cells[x - 1][y - 1] == cell::alive)
-              neighbors++;
-            if (current_cells[x][y - 1] == cell::alive)
-              neighbors++;
-            if ((x + 1) < rows && current_cells[x + 1][y - 1] == cell::alive)
-              neighbors++;
-          }
-
-          if ((y + 1) < columns) {
-            if ((x - 1) >= 0 && current_cells[x - 1][y + 1] == cell::alive)
-              neighbors++;
-            if (current_cells[x][y + 1] == cell::alive)
-              neighbors++;
-            if ((x + 1) < rows && current_cells[x + 1][y + 1] == cell::alive)
-              neighbors++;
-          }
-
-          if (current_cells[x][y] == cell::alive && (neighbors <= 1 || neighbors >= 4)) {
-            cells_[x][y] = cell::dead;
-            on_status_changed(state_event_args(cells_[x][y], x, y));
-          } else if (current_cells[x][y] == cell::alive && (neighbors == 2 || neighbors == 3)) {
-            cells_[x][y] = cell::alive;
-            on_status_changed(state_event_args(cells_[x][y], x, y));
-          } else if (current_cells[x][y] == cell::dead && neighbors == 3) {
-            cells_[x][y] = cell::alive;
-            on_status_changed(state_event_args(cells_[x][y], x, y));
+          int neighbors = get_neighbors(x, y);
+          if (cells_[y][x] == cell::alive && (neighbors <= 1 || neighbors >= 4)) {
+            updated_cells.push_back({x, y, cell::dead});
+          } else if (cells_[y][x] == cell::dead && neighbors == 3) {
+            updated_cells.push_back({x, y, cell::alive});
           }
         }
       }
+      
+      for(auto cell : updated_cells) {
+        cells_[cell.y][cell.x] = cell.state;
+        on_status_changed(state_event_args(cells_[cell.y][cell.x], cell.x, cell.y));
+      }
+    }
+    
+    int get_neighbors(int x, int y) const {
+      int neighbors = 0;
+
+      if ((x - 1) >= 0 && cells_[y][x - 1] == cell::alive)
+        neighbors++;
+      if ((x + 1) < rows && cells_[y][x + 1] == cell::alive)
+        neighbors++;
+
+      if ((y - 1) >= 0) {
+        if ((x - 1) >= 0 && cells_[y - 1][x - 1] == cell::alive)
+          neighbors++;
+        if (cells_[y - 1][x] == cell::alive)
+          neighbors++;
+        if ((x + 1) < rows && cells_[y - 1][x + 1] == cell::alive)
+          neighbors++;
+      }
+
+      if ((y + 1) < columns) {
+        if ((x - 1) >= 0 && cells_[y + 1][x - 1] == cell::alive)
+          neighbors++;
+        if (cells_[y + 1][x] == cell::alive)
+          neighbors++;
+        if ((x + 1) < rows && cells_[y + 1][x + 1] == cell::alive)
+          neighbors++;
+      }
+      return neighbors;
     }
 
     xtd::event<grid, state_event_handler<grid>> status_changed;
