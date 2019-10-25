676e04fc09d94d76dd73e6b9fe45a70fcff0e12d
Gammasoft71 <gammasoft71@gmail.com>
2019-10-25 11:28:12 +0200

    Update git_pull_submodules.sh

--- scripts/git_pull_submodules.sh
+++ scripts/git_pull_submodules.sh
@@ -2,7 +2,7 @@
 
 git submodule update --init
 
-declare -a submodules=("xtd_delegates" "xtd_diagnostics" "xtd_drawing" "xtd_environment" "xtd_io" "xtd_strings" "xtd_tunit")
+declare -a submodules=("xtd_delegates" "xtd_diagnostics" "xtd_drawing" "xtd_core" "xtd_io" "xtd_strings" "xtd_tunit")
 for submodule in ${submodules[@]}; do
   cd src/$submodule
   git pull origin master
