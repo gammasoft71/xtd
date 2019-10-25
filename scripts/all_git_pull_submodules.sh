bf27ccf3511ffeada505e8ec1ab17554065baf50
Gammasoft71 <gammasoft71@gmail.com>
2019-10-25 11:08:03 +0200

    fix scripts

--- scripts/all_git_pull_submodules.sh
+++ scripts/all_git_pull_submodules.sh
@@ -2,10 +2,10 @@
 
 ./scripts/git_pull_submodules.sh
 
-declare -a submodules=("xtd.tunit" "xtd.properties" "xtd.delegates" "xtd.io" "xtd.strings" "xtd.diagnostics" "xtd.environment" "xtd.console" "xtd.drawing" "xtd.forms")
+declare -a submodules=("xtd_tunit" "xtd_properties" "xtd_delegates" "xtd_io" "xtd_strings" "xtd_diagnostics" "xtd_core" "xtd_console" "xtd_drawing" "xtd_forms")
 
 for submodule in ${submodules[@]}; do
-  cd $submodule
+  cd src/$submodule
   ./scripts/git_pull_submodules.sh
-  cd ..
+  cd ../..
 done
--- scripts/git_pull_submodules.sh
+++ scripts/git_pull_submodules.sh
@@ -2,10 +2,10 @@
 
 git submodule update --init
 
-declare -a submodules=("xtd.console" "xtd.delegates" "xtd.diagnostics" "xtd.drawing" "xtd.environment" "xtd.forms" "xtd.io" "xtd.properties" "xtd.strings" "xtd.tunit")
+declare -a submodules=("xtd_console" "xtd_delegates" "xtd_diagnostics" "xtd_drawing" "xtd_core" "xtd_forms" "xtd_io" "xtd_properties" "xtd_strings" "xtd_tunit")
 
 for submodule in ${submodules[@]}; do
-  cd $submodule
+  cd src/$submodule
   git pull origin master
-  cd ..
+  cd ../..
 done
