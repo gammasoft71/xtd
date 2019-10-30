da3e94db9ab6196e173ae70892b1ad967d84c13d
Gammasoft71 <gammasoft71@gmail.com>
2019-10-30 09:45:25 +0100

    restore fixed_dialog

--- tests/xtd.forms.manual_tests/src/manual_tests.cpp
+++ tests/xtd.forms.manual_tests/src/manual_tests.cpp
@@ -8,7 +8,10 @@ int main() {
   form form;
   form.text("Manual tests");
   form.client_size({300, 300});
-  
+  form.form_border_style(forms::form_border_style::fixed_dialog);
+  form.maximize_box(false);
+  form.minimize_box(false);
+
   picture_box picture_box;
   picture_box.border_style(forms::border_style::fixed_3d);
   picture_box.image(xtd::drawing::bitmap(logo_xpm));
