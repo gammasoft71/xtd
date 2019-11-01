af487dcff703650a634d33bcb905990e5fe3e3ae
Gammasoft71 <gammasoft71@gmail.com>
2019-11-01 20:31:00 +0100

    Update file_dialog.cpp

--- src/xtd_forms_native_wxwidgets/src/xtd/forms/native/wxwidgets/file_dialog.cpp
+++ src/xtd_forms_native_wxwidgets/src/xtd/forms/native/wxwidgets/file_dialog.cpp
@@ -9,7 +9,9 @@ using namespace xtd::forms::native;
 bool file_dialog::run_open_dialog(intptr_t hwnd, const ustring& default_ext, ustring& file_name, std::vector<ustring>& file_names, const ustring& filter, size_t filter_index, const ustring& initial_directory, size_t options, bool support_multi_dotted_extensions, const ustring& title) {
   long wx_style = wxFD_OPEN;
   if ((options & OFN_OVERWRITEPROMPT) == OFN_OVERWRITEPROMPT) wx_style |= wxFD_OVERWRITE_PROMPT;
+#if wxMAJOR_VERSION >= 3 && wxMINOR_VERSION >= 1
   if ((options & OFN_NODEREFERENCELINKS) == OFN_NODEREFERENCELINKS) wx_style |= wxFD_NO_FOLLOW;
+#endif
   if ((options & OFN_FILEMUSTEXIST) == OFN_FILEMUSTEXIST) wx_style |= wxFD_FILE_MUST_EXIST;
   if ((options & OFN_NOCHANGEDIR) != OFN_NOCHANGEDIR) wx_style |= wxFD_CHANGE_DIR;
   if ((options & OFN_SHOWPREVIEW) == OFN_SHOWPREVIEW) wx_style |= wxFD_PREVIEW;
