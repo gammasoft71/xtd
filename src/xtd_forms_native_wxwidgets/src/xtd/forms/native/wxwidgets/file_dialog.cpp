4c69475e9d71c443e3201a842300f29753335873
Gammasoft71 <gammasoft71@gmail.com>
2019-11-01 21:42:13 +0100

    add save_file_dialog

--- src/xtd_forms_native_wxwidgets/src/xtd/forms/native/wxwidgets/file_dialog.cpp
+++ src/xtd_forms_native_wxwidgets/src/xtd/forms/native/wxwidgets/file_dialog.cpp
@@ -33,8 +33,17 @@ bool file_dialog::run_open_dialog(intptr_t hwnd, const ustring& default_ext, ust
 
 bool file_dialog::run_save_dialog(intptr_t hwnd, const ustring& default_ext, ustring& file_name, std::vector<ustring>& file_names, const ustring& filter, size_t filter_index, const ustring& initial_directory, size_t options, bool support_multi_dotted_extensions, const ustring& title) {
   long wx_style = wxFD_SAVE;
+  if ((options & OFN_OVERWRITEPROMPT) == OFN_OVERWRITEPROMPT) wx_style |= wxFD_OVERWRITE_PROMPT;
+#if wxMAJOR_VERSION >= 3 && wxMINOR_VERSION >= 1
+  if ((options & OFN_NODEREFERENCELINKS) == OFN_NODEREFERENCELINKS) wx_style |= wxFD_NO_FOLLOW;
+#endif
+  if ((options & OFN_FILEMUSTEXIST) == OFN_FILEMUSTEXIST) wx_style |= wxFD_FILE_MUST_EXIST;
+  if ((options & OFN_NOCHANGEDIR) != OFN_NOCHANGEDIR) wx_style |= wxFD_CHANGE_DIR;
+  if ((options & OFN_SHOWPREVIEW) == OFN_SHOWPREVIEW) wx_style |= wxFD_PREVIEW;
+  //if ((options & OFN_FORCESHOWHIDDEN) == OFN_FORCESHOWHIDDEN) wx_style |= wxFD_SHOW_HIDDEN;
   wxFileDialog dialog(reinterpret_cast<wxWindow*>(hwnd), title.wstr(), initial_directory.wstr(), file_name.wstr(), filter, wx_style);
   if (dialog.ShowModal() != wxID_OK) return false;
+  file_name = dialog.GetPath();
   return true;
 }
 #endif
