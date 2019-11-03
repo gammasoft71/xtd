be18726e3d7ecac5532b47d015f9f8ab3031b756
Gammasoft71 <gammasoft71@gmail.com>
2019-11-04 00:17:37 +0100

    Update panel.cpp

--- src/xtd_forms_native_wxwidgets/src/xtd/forms/native/wxwidgets/panel.cpp
+++ src/xtd_forms_native_wxwidgets/src/xtd/forms/native/wxwidgets/panel.cpp
@@ -9,13 +9,8 @@ using namespace xtd::forms::native;
 void panel::virtual_size(intptr_t control, const drawing::size& size) {
   if (control == 0) return;
 
-#if defined(__WXOSX__)
   int32_t offset_x = -9;
-#else
-  int32_t offset_x = -9;
-#endif
   int32_t offset_y = -10;
-
   
   static_cast<wxScrolled<wxPanel>*>(reinterpret_cast<control_handler*>(control)->control())->SetVirtualSize(wxSize(size.width() + offset_x, size.height() + offset_y));
   static_cast<wxScrolled<wxPanel>*>(reinterpret_cast<control_handler*>(control)->control())->SetScrollRate(10, 10);
