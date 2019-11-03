7668d6c380166a15fc95105aede51d3a554032bd
Gammasoft71 <gammasoft71@gmail.com>
2019-11-03 20:31:59 +0100

    add rounded property

--- examples/controls/switch_button/switch_button.cpp
+++ examples/controls/switch_button/switch_button.cpp
@@ -8,9 +8,9 @@ class form1 : public form {
 public:
   form1() {
     text("Switch button example");
-
+    
     switch_button1.parent(*this);
-    switch_button1.location({10, 10});
+    switch_button1.location({30, 10});
     switch_button1.checked_changed += [this](control& sender, const event_args& e) {
       label1.text(strings::format("switch: {}", static_cast<switch_button&>(sender).checked() ? "ON" : "OFF"));
     };
@@ -18,21 +18,45 @@ public:
     switch_button2.parent(*this);
     switch_button2.auto_check(false);
     switch_button2.checked(true);
-    switch_button2.location({10, 50});
+    switch_button2.location({30, 50});
     switch_button2.click += [](control& sender, const event_args& e) {
+      //static_cast<switch_button&>(sender).checked(!static_cast<switch_button&>(sender).checked());
+    };
+    
+    switch_button3.parent(*this);
+    switch_button3.location({150, 10});
+    switch_button3.rounded(true);
+    switch_button3.checked_changed += [this](control& sender, const event_args& e) {
+      label2.text(strings::format("switch: {}", static_cast<switch_button&>(sender).checked() ? "ON" : "OFF"));
+    };
+
+    switch_button4.parent(*this);
+    switch_button4.auto_check(false);
+    switch_button4.checked(true);
+    switch_button4.location({150, 50});
+    switch_button4.rounded(true);
+    switch_button4.click += [](control& sender, const event_args& e) {
       //static_cast<switch_button&>(sender).checked(!static_cast<switch_button&>(sender).checked());
     };
 
     label1.parent(*this);
-    label1.location({10, 90});
+    label1.location({30, 90});
     label1.auto_size(true);
     label1.text("switch: OFF");
+
+    label2.parent(*this);
+    label2.location({150, 90});
+    label2.auto_size(true);
+    label2.text("switch: OFF");
   }
   
 private:
   switch_button switch_button1;
   switch_button switch_button2;
+  switch_button switch_button3;
+  switch_button switch_button4;
   label label1;
+  label label2;
 };
 
 int main() {
--- tests/xtd.forms.manual_tests/src/manual_tests.cpp
+++ tests/xtd.forms.manual_tests/src/manual_tests.cpp
@@ -1,27 +1,24 @@
 #include <xtd/xtd.forms>
 
+using namespace std;
 using namespace xtd;
 using namespace xtd::forms;
 
-int main() {
-  form form1;
-  form1.text("Manual tests");
-  form1.client_size({300, 300});
+class form1 : public form {
+public:
+  form1() {
+    text("Switch button example");
+    back_color(drawing::color::dark_blue);
 
-  tab_control tab_control1;
-  tab_control1.parent(form1);
-  tab_control1.dock(dock_style::fill);
-
-  tab_page page1;
-  page1.text("Page 1");
-  page1.auto_scroll(true);
-  page1.parent(tab_control1);
+    switch_button1.parent(*this);
+    switch_button1.location({10, 10});
+    switch_button1.rounded(true);
+  }
   
-  label label1;
-  label1.parent(page1);
-  label1.text("text");
-  label1.back_color(drawing::color::spring_green);
-  label1.location({350, 350});
-   
-  application::run(form1);
+private:
+  switch_button switch_button1;
+};
+
+int main() {
+  application::run(form1());
 }
