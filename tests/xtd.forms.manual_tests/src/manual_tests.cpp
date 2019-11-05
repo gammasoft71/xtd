1a329a8533e247d79faf9822032855d04e259789
Gammasoft71 <gammasoft71@gmail.com>
2019-11-05 10:54:17 +0100

    Update manual_tests.cpp

--- tests/xtd.forms.manual_tests/src/manual_tests.cpp
+++ tests/xtd.forms.manual_tests/src/manual_tests.cpp
@@ -7,13 +7,12 @@ using namespace xtd::forms;
 int main() {
   form form_main;
   form_main.text("Manual tests");
-  form_main.back_color(color::spring_green);
 
   combo_box combo_box1;
   combo_box1.location({10, 10});
   combo_box1.drop_down_style(combo_box_style::simple);
   combo_box1.height(120);
-  combo_box1.items().push_back_range({"Item1", "Item2", "Item3", "Item4", "Item5", "Item6", "Item7", "Item8", "Item9", "Item10"});
+  combo_box1.items().push_back_range({"Item1", "Item2", "Item3", "Item4"});
   combo_box1.selected_index(2);
   combo_box1.parent(form_main);
 
