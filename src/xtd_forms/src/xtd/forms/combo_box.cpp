32cf040bb6cfb7f7ec936116a59648c8f08223b0
Gammasoft71 <gammasoft71@gmail.com>
2019-11-05 12:26:32 +0100

    fix size

--- src/xtd_forms/src/xtd/forms/combo_box.cpp
+++ src/xtd_forms/src/xtd/forms/combo_box.cpp
@@ -14,7 +14,7 @@ combo_box::combo_box() {
   this->fore_color_ = this->default_fore_color();
   this->size_ = this->default_size();
   this->drop_down_width_ = this->default_size().width();
-  this->drop_down_height_ = static_cast<int32_t>(this->font().get_height()) * 9;
+  this->drop_down_height_ = static_cast<int32_t>(this->font().get_height()) * 9 + 5;
 
   this->items_.item_added += [&](size_t pos, const item& item) {
     native::combo_box::insert_item(this->handle_, pos, item.value());
