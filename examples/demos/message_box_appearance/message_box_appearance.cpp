f38b4374101ec064976b5f110dae53087ea9a7a6
Gammasoft71 <gammasoft71@gmail.com>
2019-11-10 12:18:06 +0100

    Update message_box_appearance.cpp

--- examples/demos/message_box_appearance/message_box_appearance.cpp
+++ examples/demos/message_box_appearance/message_box_appearance.cpp
@@ -8,7 +8,7 @@ class form1 : public form {
 public:
   form1() {
     text("Message box appearance");
-    client_size({300, 200});
+    client_size({300, 185});
     
     label_caption.text("Caption : ");
     label_caption.auto_size(true);
@@ -22,40 +22,40 @@ public:
 
     label_message.text("Message : ");
     label_message.auto_size(true);
-    label_message.location({10, 43});
+    label_message.location({10, 48});
     label_message.parent(*this);
 
     text_message.text("It's fantastic !");
-    text_message.location({90, 40});
+    text_message.location({90, 45});
     text_message.parent(*this);
     text_message.width(200);
     
     label_buttons.text("Buttons : ");
     label_buttons.auto_size(true);
-    label_buttons.location({10, 73});
+    label_buttons.location({10, 83});
     label_buttons.parent(*this);
 
     combo_box_buttons.items().push_back_range({{"ok", message_box_buttons::ok}, {"ok_cancel", message_box_buttons::ok_cancel}, {"abort_retry_ignore", message_box_buttons::abort_retry_ignore}, {"yes_no_cancel", message_box_buttons::yes_no_cancel}, {"yes_no", message_box_buttons::yes_no}, {"retry_cancel", message_box_buttons::retry_cancel}});
     combo_box_buttons.drop_down_style(combo_box_style::drop_down_list);
     combo_box_buttons.selected_index(0);
-    combo_box_buttons.location({90, 70});
+    combo_box_buttons.location({90, 80});
     combo_box_buttons.parent(*this);
     combo_box_buttons.width(200);
     
     label_icons.text("Icons : ");
     label_icons.auto_size(true);
-    label_icons.location({10, 103});
+    label_icons.location({10, 118});
     label_icons.parent(*this);
 
     combo_box_icons.items().push_back_range({{"none", message_box_icon::none}, {"hand", message_box_icon::hand}, {"stop", message_box_icon::stop}, {"error", message_box_icon::error}, {"question", message_box_icon::question}, {"exclamation", message_box_icon::exclamation}, {"warning", message_box_icon::warning}, {"asterick", message_box_icon::asterisk}, {"information", message_box_icon::information}});
     combo_box_icons.drop_down_style(combo_box_style::drop_down_list);
     combo_box_icons.selected_index(0);
-    combo_box_icons.location({90, 100});
+    combo_box_icons.location({90, 115});
     combo_box_icons.parent(*this);
     combo_box_icons.width(200);
 
     button_try_it.text("Try it");
-    button_try_it.location({10, 130});
+    button_try_it.location({10, 150});
     button_try_it.parent(*this);
     button_try_it.click += [this] {
       label_dialog_result.text("dialog_result = ");
@@ -65,7 +65,7 @@ public:
 
     label_dialog_result.text("dialog_result = ");
     label_dialog_result.auto_size(true);
-    label_dialog_result.location({90, 133});
+    label_dialog_result.location({90, 154});
     label_dialog_result.parent(*this);
   }
   
