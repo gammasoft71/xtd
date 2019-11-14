4d21337dffe83328acfd00e34511eb3b72fe38a5
Gammasoft71 <gammasoft71@gmail.com>
2019-11-14 14:45:48 +0100

    fix bitmap_button example

--- tests/xtd.forms.manual_tests/src/manual_tests.cpp
+++ tests/xtd.forms.manual_tests/src/manual_tests.cpp
@@ -1,5 +1,4 @@
 #include <xtd/xtd.forms>
-#include "Gammasoft-32x32.xpm"
 
 using namespace xtd;
 using namespace xtd::drawing;
@@ -8,15 +7,6 @@ using namespace xtd::forms;
 int main() {
   form form_main;
   form_main.text("Manual tests");
-  
-  button button1;
-  button1.parent(form_main);
-  button1.location({10, 10});
-  button1.size({100, 40});
-  button1.dock(dock_style::top);
-  button1.image(bitmap(gammasoft_32x32));
-  button1.text("Click me");
-  button1.image_align(content_alignment::middle_left);
 
   application::run(form_main);
 }
