2fc933af68b5d9e6a4bc08bd0714be625dc738ec
Gammasoft71 <gammasoft71@gmail.com>
2019-11-17 18:10:28 +0100

    Update manual_tests.cpp

--- tests/xtd.forms.manual_tests/src/manual_tests.cpp
+++ tests/xtd.forms.manual_tests/src/manual_tests.cpp
@@ -8,6 +8,14 @@ using namespace xtd::drawing;
 using namespace xtd::forms;
 
 int main() {
+  bitmap bmp(128, 128);
+  
+  for (int y = 0; y < bmp.height(); y++)
+    for (int x = 0; x < bmp.width(); x++)
+      if (bmp.get_pixel(x, y) != color::from_argb(0, 0, 0, 0))
+        std::cout <<format("x = {}, y = {}  color = {,-34}", x, y, bmp.get_pixel(x, y)) << std::endl;
+  
+  /*
   form form_main;
   form_main.text("Manual tests");
 
@@ -24,4 +32,5 @@ int main() {
   //button.image(drawing::image::from_data(gammasoft_32x32));
 
   application::run(form_main);
+   */
 }
