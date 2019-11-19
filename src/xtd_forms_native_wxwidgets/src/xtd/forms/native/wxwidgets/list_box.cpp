b92231b61d6c14e5ec9300648c8dd5d92b5fafce
Gammasoft71 <gammasoft71@gmail.com>
2019-11-19 22:28:15 +0100

    Update list_box.cpp

--- src/xtd_forms_native_wxwidgets/src/xtd/forms/native/wxwidgets/list_box.cpp
+++ src/xtd_forms_native_wxwidgets/src/xtd/forms/native/wxwidgets/list_box.cpp
@@ -58,4 +58,5 @@ vector<size_t> list_box::selected_indices(intptr_t control) {
 void list_box::update_item(intptr_t control, size_t index, const ustring& value) {
   if (control == 0) return;
   static_cast<wxListBox*>(reinterpret_cast<control_handler*>(control)->control())->SetString(index, value);
+  wxTheApp->ProcessPendingEvents();
 }
