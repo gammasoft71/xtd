37f54c019cc52640d1b0a3ac5b9afb385b42f46d
Gammasoft71 <gammasoft71@gmail.com>
2019-11-19 14:58:56 +0100

    add unit tests and fix arranged_element_collection

--- tests/xtd.forms.manual_tests/src/manual_tests.cpp
+++ tests/xtd.forms.manual_tests/src/manual_tests.cpp
@@ -1,4 +1,5 @@
 #include <xtd/xtd.forms>
+#include <xtd/forms/native/wxwidgets/wx_list_box.hpp>
 #include "address_book_new_32x32.xpm"
 #include "appointment_new_32x32.xpm"
 #include "gammasoft_32x32.xpm"
@@ -11,17 +12,34 @@ int main() {
   form form_main;
   form_main.text("Manual tests");
 
-  image_list pictures;
-  pictures.image_size({64, 64});
-  pictures.images().push_back_range({address_book_new_32x32, appointment_new_32x32, gammasoft_32x32});
-  
-  button button;
-  button.parent(form_main);
-  button.location({10, 10});
-  button.size({80, 80});
-  button.image_list(pictures);
-  button.image_index(2);
-  //button.image(drawing::image::from_data(gammasoft_32x32));
+  list_box lb;
+  lb.parent(form_main);
+  lb.dock(dock_style::fill);
 
+  cdebug << format("before create handle = 0x{:X}", lb.handle()) << std::endl;
+  form_main.show();
+  cdebug << format("after create handle = 0x{:X}", lb.handle()) << std::endl;
+
+  lb.sorted(true);
+  lb.items().push_back("A");
+  lb.selected_index(0);
+  //lb.items().insert_at(0, "Z");
+  //lb.items().insert_at(0, "B");
+  lb.items().push_back_range({"G", "F", "D"});
+  
+  cdebug << format("handle = 0x{:X}", lb.handle()) << std::endl;
+  for (size_t index = 0; index < static_cast<wxListBox*>(reinterpret_cast<xtd::forms::native::control_handler*>(lb.handle())->control())->GetCount(); index++)
+    cdebug << format("  {} - {}", index, static_cast<wxListBox*>(reinterpret_cast<xtd::forms::native::control_handler*>(lb.handle())->control())->GetString(index)) << std::endl;
+
+  lb.items().erase_at(0);
+  lb.selected_index(0);
+
+  cdebug << format("handle = 0x{:X}", lb.handle()) << std::endl;
+  for (size_t index = 0; index < static_cast<wxListBox*>(reinterpret_cast<xtd::forms::native::control_handler*>(lb.handle())->control())->GetCount(); index++)
+    cdebug << format("  {} - {}", index, static_cast<wxListBox*>(reinterpret_cast<xtd::forms::native::control_handler*>(lb.handle())->control())->GetString(index)) << std::endl;
+
+  for (auto& item : lb.items())
+    cdebug << item << std::endl;
+  
   application::run(form_main);
 }
