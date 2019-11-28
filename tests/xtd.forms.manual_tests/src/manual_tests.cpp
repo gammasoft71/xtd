de9fa01174369300c93b1d8f385191c38165489c
Gammasoft71 <gammasoft71@gmail.com>
2019-11-28 23:34:04 +0100

    Update manual_tests.cpp

--- tests/xtd.forms.manual_tests/src/manual_tests.cpp
+++ tests/xtd.forms.manual_tests/src/manual_tests.cpp
@@ -525,19 +525,17 @@ int main() {
   form_main.start_position(form_start_position::manual);
   form_main.location({300, 200});
   form_main.client_size({985, 100});
-
+  
   lcd_label label;
   label.parent(form_main);
   label.location({10, 10});
-  label.size({965, 25});
-  //label.dock(dock_style::fill);
-  label.text(" -.0123456789aAbBcCdDeEfFgGhHiIjJlLoOpPqQrRtTuUyY");
-  //label.style(lcd_style::seven_segments);
-  //label.digit_spacing(8);
+  label.dock(dock_style::fill);
+  label.text("1234567890ABCDEF -.");
+  label.style(lcd_style::seven_segments);
   //label.show_background_digit(false);
   //label.back_color(color::black);
-  label.back_color(color::from_argb(16, color::green));
-  label.fore_color(color::green);
-
+  label.back_color(color::from_argb(16, color::red));
+  label.fore_color(color::red);
+  
   application::run(form_main);
 }
