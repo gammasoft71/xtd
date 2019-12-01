103b201c17e5c18ca2a4b6a443a7357176117e32
Gammasoft71 <gammasoft71@gmail.com>
2019-12-01 21:13:19 +0100

    Update manual_tests.cpp

--- tests/xtd.console.manual_tests/src/manual_tests.cpp
+++ tests/xtd.console.manual_tests/src/manual_tests.cpp
@@ -7,5 +7,17 @@ using namespace std::string_literals;
 
 // The main entry point for the application.
 int main() {
-  cout << format("{}, {}!", "Hello", "World"s) << endl;
+  //cout << format("{}, {}!", "Hello", "World"s) << endl;
+  double denominator = 1;
+  double pi = 0;
+  int counter = 1;
+
+  while (!console::key_available()) {
+    if (counter % 2) pi += 4.0 / denominator;
+    else pi -= 4.0 / denominator;
+    console::write_line("{:D} - pi = {:F20}", counter, pi);
+    console::out.flush();
+    denominator += 2;
+    counter++;
+  }
 }
