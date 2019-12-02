c3b26f04537c533e6626611a0ab08afd032a3bfc
Gammasoft71 <gammasoft71@gmail.com>
2019-12-02 09:51:09 +0100

    Update form_main.hpp

--- examples/games/game_of_life/form_main.hpp
+++ examples/games/game_of_life/form_main.hpp
@@ -1,4 +1,3 @@
-
 #pragma once
 #include <random>
 #include <xtd/xtd.forms>
@@ -11,7 +10,7 @@ namespace game_of_life {
     form_main() {
       text("John Conway's Game of Life");
       client_size({715, 485});
-
+      
       button_run_.parent(*this);
       button_run_.text("Run");
       button_run_.location({10, 10});
@@ -19,17 +18,17 @@ namespace game_of_life {
         timer_run_.enabled(!timer_run_.enabled());
         button_run_.text(timer_run_.enabled() ? "Stop" : "Run");
       };
-
+      
       button_next_.parent(*this);
       button_next_.text("Next");
       button_next_.location({100, 10});
       button_next_.click += {*this, & form_main::next};
-
+      
       button_clear_.parent(*this);
       button_clear_.text("Clear");
       button_clear_.location({190, 10});
       button_clear_.click += {*this, & form_main::clear};
-
+      
       combo_box_figures_.parent(*this);
       combo_box_figures_.drop_down_style(xtd::forms::combo_box_style::drop_down_list);
       combo_box_figures_.items().push_back_range({{"Figure", figure_delegate(*this, &form_main::nothing)}, {"Random cells", figure_delegate(*this, &form_main::random)}, {"Blinker", figure_delegate(*this, &form_main::blinker)}, {"Toad", figure_delegate(*this, &form_main::toad)}, {"beacon", figure_delegate(*this, &form_main::beacon)}, {"galaxy", figure_delegate(*this, &form_main::galaxy)}, {"Pulsar", figure_delegate(*this, &form_main::pulsar)}, {"Penta-decathlon", figure_delegate(*this, &form_main::penta_decathlon)}, {"Glider", figure_delegate(*this, &form_main::glider)}, {"Small exploder", figure_delegate(*this, &form_main::small_exploder)}, {"Exploder", figure_delegate(*this, &form_main::exploder)}, {"Lightweight spaceship", figure_delegate(*this, &form_main::lightweight_spaceship)}, {"Middleweight spaceship", figure_delegate(*this, &form_main::middleweight_spaceship)}, {"Heavyweight spaceship", figure_delegate(*this, &form_main::heavyweight_spaceship)}, {"Tumbler", figure_delegate(*this, &form_main::tumbler)}, {"Gosper glider gun", figure_delegate(*this, &form_main::gosper_glider_gun)}});
@@ -40,7 +39,7 @@ namespace game_of_life {
         std::any_cast<figure_delegate>(combo_box_figures_.items()[combo_box_figures_.selected_index()].tag())();
         combo_box_figures_.selected_index(0);
       };
-
+      
       label_iterations_.parent(*this);
       label_iterations_.anchor(xtd::forms::anchor_styles::top | xtd::forms::anchor_styles::right);
       label_iterations_.font(xtd::drawing::font(xtd::drawing::font_family::generic_monospace(), label_iterations_.font().size()));
@@ -48,11 +47,11 @@ namespace game_of_life {
       label_iterations_.text(xtd::strings::format("Iterations : {}", iterations_));
       label_iterations_.width(150);
       label_iterations_.location({555, 15});
-
+      
       label_zoom_.parent(*this);
       label_zoom_.location({10, 45});
       label_zoom_.text(xtd::strings::format("Zoom : {}", zoom_));
-
+      
       track_bar_zoom_.parent(*this);
       track_bar_zoom_.auto_size(false);
       track_bar_zoom_.location({90, 40});
@@ -70,12 +69,12 @@ namespace game_of_life {
         label_zoom_.text(xtd::strings::format("Zoom : {}", zoom_));
       };
       track_bar_zoom_.size({200, 25});
-
+      
       label_speed_.parent(*this);
       label_speed_.anchor(xtd::forms::anchor_styles::top | xtd::forms::anchor_styles::right);
       label_speed_.location({425, 45});
       label_speed_.text(xtd::strings::format("Speed : {}", speed_));
-
+      
       track_bar_speed_.parent(*this);
       track_bar_speed_.anchor(xtd::forms::anchor_styles::top | xtd::forms::anchor_styles::right);
       track_bar_speed_.auto_size(false);
@@ -90,28 +89,29 @@ namespace game_of_life {
         label_speed_.text(xtd::strings::format("Speed : {}", speed_));
       };
       track_bar_speed_.size({200, 25});
-
+      
       panel_grid_.parent(*this);
       panel_grid_.anchor(xtd::forms::anchor_styles::top | xtd::forms::anchor_styles::left | xtd::forms::anchor_styles::bottom | xtd::forms::anchor_styles::right);
       panel_grid_.back_color(empty_color_);
       panel_grid_.border_style(xtd::forms::border_style::fixed_single);
       panel_grid_.location({10, 75});
       panel_grid_.size({695, 395});
-
+      
       panel_grid_.mouse_down += [this](control& sender, const xtd::forms::mouse_event_args& e) {
         current_state_ = grid_.cells()[offset_y_ + e.location().y() / zoom_][offset_x_ + e.location().x() / zoom_] == cell::populated ? cell::empty : cell::populated;
         grid_.cells()[offset_y_ + e.location().y() / zoom_][offset_x_ + e.location().x() / zoom_] = current_state_;
         panel_grid_.invalidate(xtd::drawing::rectangle(e.location().x() / zoom_ * zoom_, e.location().y() / zoom_ * zoom_, zoom_, zoom_), false);
       };
-
+      
       panel_grid_.mouse_move += [this](control& sender, const xtd::forms::mouse_event_args& e) {
         if (e.button() == xtd::forms::mouse_buttons::left) {
           grid_.cells()[offset_y_ + e.location().y() / zoom_][offset_x_ + e.location().x() / zoom_] = current_state_;
           panel_grid_.invalidate(xtd::drawing::rectangle(e.location().x() / zoom_ * zoom_, e.location().y() / zoom_ * zoom_, zoom_, zoom_), false);
         }
       };
-
+      
       panel_grid_.paint += [this](control& sender, xtd::forms::paint_event_args& e) {
+        e.graphics().clear(back_color());
         if ((track_bar_zoom_.value() * grid::columns) >= panel_grid_.client_size().width() && (track_bar_zoom_.value() * grid::rows) >= panel_grid_.client_size().height())
           for (int y = 0; y < panel_grid_.client_size().height(); y += zoom_)
             for (int x = 0; x < panel_grid_.client_size().width(); x += zoom_)
@@ -124,7 +124,7 @@ namespace game_of_life {
             e.graphics().fill_rectangle(xtd::drawing::solid_brush(line_color_), 0, index, panel_grid_.client_size().width(), 1);
         }
       };
-
+      
       panel_grid_.resize += [this] {
         if ((track_bar_zoom_.value() * grid::columns) < panel_grid_.client_size().width()) {
           zoom_ = panel_grid_.client_size().width() / grid::columns + 1;
@@ -135,15 +135,15 @@ namespace game_of_life {
           track_bar_zoom_.value(zoom_);
         }
       };
-
+      
       timer_run_.interval(1000 / speed_);
       timer_run_.tick += {*this, &form_main::next};
-
+      
       grid_.cell_changed += [this](grid& sender, const cell_event_args& e) {
         panel_grid_.invalidate(xtd::drawing::rectangle((e.x() - offset_x_) * zoom_, (e.y() - offset_y_) * zoom_, zoom_, zoom_), false);
       };
     }
-
+    
   private:
     void clear() {
       grid_.clear();
@@ -151,14 +151,14 @@ namespace game_of_life {
       label_iterations_.text(xtd::strings::format("Iterations : {}", iterations_));
       panel_grid_.invalidate();
     }
-
+    
     void nothing() {}
-
+    
     void next() {
       grid_.next();
       label_iterations_.text(xtd::strings::format("Iterations : {}", ++iterations_));
     }
-
+    
     void random() {
       std::random_device rand;
       int max = std::uniform_int_distribution<int>{ 50, 200 }(rand);
@@ -171,19 +171,19 @@ namespace game_of_life {
         panel_grid_.invalidate(xtd::drawing::rectangle(x * zoom_, y * zoom_, zoom_, zoom_), false);
       }
     }
-
+    
     void fill_figure(const std::vector<xtd::ustring>& figure) {
       int height = static_cast<int>(figure.size());
       int width = 0;
       for (xtd::ustring line : figure)
         if (line.length() > width) width = static_cast<int>(line.length());
-
+      
       int start_x = (panel_grid_.client_size().width() / zoom_ / 2) - (width / 2);
       int y = (panel_grid_.client_size().height() / zoom_ / 2) - (height / 2);
-
+      
       if (start_x < 5) start_x = 5;
       if (y < 5) y = 5;
-
+      
       for (xtd::ustring line : figure) {
         int x = start_x;
         for (char cell : line) {
@@ -194,7 +194,7 @@ namespace game_of_life {
         y++;
       }
     }
-
+    
     void beacon() {
       fill_figure({
         "**  ",
@@ -236,8 +236,7 @@ namespace game_of_life {
       });
     }
     void glider() {
-      fill_figure(
-      {
+      fill_figure({
         " * ",
         "  *",
         "***"
@@ -349,7 +348,7 @@ namespace game_of_life {
     int speed_ = 25;
     int offset_x_ = 200;
     int offset_y_ = 200;
-
+    
     xtd::forms::button button_run_;
     xtd::forms::button button_next_;
     xtd::forms::button button_clear_;
