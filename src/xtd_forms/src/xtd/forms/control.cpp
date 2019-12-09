0aa87838312fefae2a19f7468c36cc6bde365e62
Gammasoft71 <gammasoft71@gmail.com>
2019-12-09 21:28:19 +0100

    invoke review

--- src/xtd_forms/src/xtd/forms/control.cpp
+++ src/xtd_forms/src/xtd/forms/control.cpp
@@ -621,6 +621,17 @@ void control::update() const {
 intptr_t control::wnd_proc_(intptr_t hwnd, int32_t msg, intptr_t wparam, intptr_t lparam, intptr_t handle) {
   message message = forms::message::create(hwnd, msg, wparam, lparam, 0, handle);
   wnd_proc(message);
+  
+  if (application::message_loop()) {
+    std::lock_guard<std::mutex> lock(control::mutex_invokers_access);
+    if (control::invokers.size()) {
+      control::invokers.front().invoke(control::invokers.front().args);
+      std::this_thread::yield();
+      control::invokers.front().condition_variable_invoked->notify_one();
+      control::invokers.pop_front();
+    }
+  }
+
   return message.result();
 }
 
