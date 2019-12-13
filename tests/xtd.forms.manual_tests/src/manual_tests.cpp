a9cb49139e4cbc39ea66679f079f02da63c66498
Gammasoft71 <gammasoft71@gmail.com>
2019-12-13 14:33:52 +0100

    Update manual_tests.cpp

--- tests/xtd.forms.manual_tests/src/manual_tests.cpp
+++ tests/xtd.forms.manual_tests/src/manual_tests.cpp
@@ -13,6 +13,12 @@ int main() {
   form form_main;
   form_main.text("Manual tests");
   //form_main.menu(menu);
+  form_main.auto_scroll(true);
+  
+  button button1;
+  button1.parent(form_main);
+  button1.location({600, 10});
+  button1.text("Click");
 
   application::run(form_main);
 }
