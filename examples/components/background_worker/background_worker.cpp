6b96a0791066b2e79925495a5d64df4751213d2a
Gammasoft71 <gammasoft71@gmail.com>
2019-12-15 16:31:57 +0100

    Update background_worker.cpp

--- examples/components/background_worker/background_worker.cpp
+++ examples/components/background_worker/background_worker.cpp
@@ -11,17 +11,30 @@ namespace examples {
   public:
     form1() {
       text("Background worker example");
-      client_size({300, 105});
-      controls().push_back_range({button_run, button_cancel, progress, status});
+      //client_size({300, 105});
+      auto_size_mode(forms::auto_size_mode::grow_and_shrink);
+      auto_size(true);
+      controls().push_back_range({panel_command, progress});
       form_closed += [this] {
         button_cancel.enabled(true);
         if (worker.is_busy())
           worker.cancel_async();
       };
+      
+      panel_command.parent(*this);
+      panel_command.size({300, 80});
+      panel_command.controls().push_back_range({button_run, button_cancel, status});
+      
+      panel_progress.parent(*this);
+      panel_progress.top(80);
+      panel_progress.size({300, 155});
+      panel_progress.controls().push_back_range({progress, list_progress});
+      panel_progress.visible(false);
 
       button_run.location({10, 10});
       button_run.text("Run");
       button_run.click += [this] {
+        panel_progress.visible(true);
         button_cancel.enabled(true);
         button_run.enabled(false);
         worker.run_worker_async();
@@ -37,42 +50,50 @@ namespace examples {
         worker.cancel_async();
       };
       
-      progress.location({10, 50});
-      progress.width(280);
-      progress.set_range(0, 100);
-      
-      status.location({10, 80});
+      status.location({10, 50});
       status.auto_size(true);
-      status.text("Status :");
+      status.text("Status : not started");
 
+      progress.location({10, 10});
+      progress.width(280);
+      progress.set_range(0, 150);
+      
+      list_progress.location({10, 45});
+      list_progress.size({280, 100});
+      
       worker.worker_supports_cancellation(true);
       worker.worker_reports_progress(true);
       worker.do_work += [this](component& sender, do_work_event_args& e){
-        for (int progress = 1; progress <= 100; progress++) {
+        for (int step = 1; step <= progress.maximum(); step++) {
           if (worker.cancellation_pending()) break; // stop work...
           std::this_thread::sleep_for(100ms); // simulate work...
-          worker.report_progress(progress);
-          cdebug << format("progress {} / 100", progress) << std::endl;
+          worker.report_progress(step, strings::format("step {} / {}", step, progress.maximum()));
         }
       };
       
       worker.progress_changed += [this](component& sender, const progress_changed_event_args& e) {
         progress.value(e.progress_percentage());
+        list_progress.items().push_back(std::any_cast<std::string>(e.user_state()));
       };
       
       worker.run_worker_completed += [this](component& sender, const run_worker_completed_event_args& e){
+        panel_progress.visible(false);
         button_run.enabled(true);
         button_cancel.enabled(false);
         progress.value(0);
+        list_progress.items().clear();
         status.text(strings::format("Status : {}", e.cancel() ? "canceled" : "completed"));
       };
     }
     
   private:
+    panel panel_command;
+    panel panel_progress;
     button button_run;
     button button_cancel;
-    progress_bar progress;
     label status;
+    progress_bar progress;
+    list_box list_progress;
     background_worker worker;
   };
 }
