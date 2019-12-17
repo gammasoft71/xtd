254f3cceee8fec0b44600e2716a3b230efeef708
Gammasoft71 <gammasoft71@gmail.com>
2019-12-17 21:20:05 +0100

    add seven segment display control

---
+++ examples/other_controls/seven_segment_display/README.md
@@ -0,0 +1,35 @@
+# seven_segment_display
+
+demonstrates how to use [xtd::forms::seven_segment_display](../../../src/xtd_forms/include/xtd/forms/seven_segment_display.hpp) control.
+
+# Sources
+
+[seven_segment_display.cpp](seven_segment_display.cpp)
+
+[CMakeLists.txt](CMakeLists.txt)
+
+# Build and run
+
+Open "Command Prompt" or "Terminal". Navigate to the folder that contains the project and type the following:
+
+```shell
+xtd run
+```
+
+# Output
+
+## Windows :
+
+![Screenshot](../../../docs/pictures/examples/seven_segment_display_w.png)
+
+## macOS :
+
+![Screenshot](../../../docs/pictures/examples/seven_segment_display_m.png)
+
+![Screenshot](../../../docs/pictures/examples/seven_segment_display_md.png)
+
+## Gnome :
+
+![Screenshot](../../../docs/pictures/examples/seven_segment_display_g.png)
+
+![Screenshot](../../../docs/pictures/examples/seven_segment_display_gd.png)
---
+++ examples/other_controls/seven_segment_display/seven_segment_display.cpp
@@ -0,0 +1,39 @@
+#include <xtd/xtd.forms>
+
+using namespace xtd;
+using namespace xtd::drawing;
+using namespace xtd::forms;
+
+class form1 : public form {
+public:
+  form1() {
+    seven_segment_display1.height(250);
+    seven_segment_display1.location({(client_size().width() - seven_segment_display1.width()) / 2, (client_size().height() - seven_segment_display1.height()) / 2});
+    seven_segment_display1.parent(*this);
+    seven_segment_display1.fore_color(color::red);
+    seven_segment_display1.anchor(anchor_styles::top|anchor_styles::bottom);
+    
+    timer1.interval(300);
+    timer1.enabled(true);
+    timer1.tick += [&] {
+      seven_segment_display1.value(chaser[counter % chaser.size()]);
+      seven_segment_display1.segment_style(segment_style((counter++ / chaser.size()) % 2));
+    };
+    
+    back_color(color::average(seven_segment_display1.fore_color(), color::black, 0.20));
+    text("Seven segment display example");
+    resize += [this] {
+      seven_segment_display1.left((client_size().width() - seven_segment_display1.width()) / 2);
+    };
+  }
+  
+private:
+  int counter = 0;
+  std::vector<segments> chaser = {segments::a|segments::b|segments::c|segments::d|segments::e|segments::f|segments::g, segments::none, segments::a|segments::b|segments::c|segments::d|segments::e|segments::f|segments::g, segments::none, segments::a|segments::b|segments::c|segments::d|segments::e|segments::f|segments::g, segments::a, segments::b, segments::g, segments::e, segments::d, segments::c, segments::g, segments::f, segments::dp, segments::pc};
+  seven_segment_display seven_segment_display1;
+  timer timer1;
+};
+
+int main() {
+  application::run(form1());
+}
