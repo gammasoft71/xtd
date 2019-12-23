281ba7172403b160b742b60ec24ac774ed22dc40
Gammasoft71 <gammasoft71@gmail.com>
2019-12-23 14:02:19 +0100

    add do_layout_parent() method

--- src/xtd_forms/include/xtd/forms/control.hpp
+++ src/xtd_forms/include/xtd/forms/control.hpp
@@ -837,6 +837,7 @@ namespace xtd {
       static std::mutex mutex_invokers_access;
       static std::list<invoker> invokers;
       static std::shared_ptr<timer> timer_invoke;
+      void do_layout_parent();
       void do_layout();
       void on_parent_size_changed(const control& sender, const event_args& e);
       void do_layout_childs_with_dock_style();
