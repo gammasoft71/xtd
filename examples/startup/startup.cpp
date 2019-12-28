1515d044e7b075c15eb9c7965a34564c5fc76660
Gammasoft71 <gammasoft71@gmail.com>
2019-12-28 22:47:20 +0100

    add examples

---
+++ examples/startup/startup.cpp
@@ -0,0 +1,23 @@
+#include <xtd/xtd.core>
+#include <xtd/xtd.strings>
+#include <iostream>
+
+using namespace std;
+using namespace xtd;
+
+namespace examples {
+  class program {
+  public:
+    // The main entry point for the application.
+    static void main(const std::vector<std::string>& args) {
+      cout << format("args = {}", strings::join(", ", args)) << endl;
+      environment::exit_code(42);
+    }
+  };
+}
+
+startup_(examples::program);
+
+// This code produces the following output if one two "three four" five are entered on command line:
+//
+// args = one, two, three, four five, six
