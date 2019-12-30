9b0462cdcc2235f6254eef41a082cca1e013a1be
Gammasoft71 <gammasoft71@gmail.com>
2019-12-31 00:05:04 +0100

    Update combo_box.cpp

--- examples/common_controls/combo_box/combo_box.cpp
+++ examples/common_controls/combo_box/combo_box.cpp
@@ -14,18 +14,30 @@ namespace examples {
       combo_box1.items().push_back_range({"Item1", "Item2", "Item3", "Item4", "Item5", "Item6", "Item7", "Item8", "Item9", "Item10"});
       combo_box1.selected_index(0);
       combo_box1.parent(*this);
-      
+      combo_box1.selected_index_changed += [this] {
+        combo_box2.selected_index(combo_box1.selected_index());
+        combo_box3.selected_index(combo_box1.selected_index());
+      };
+
       combo_box2.location({10, 50});
       combo_box2.drop_down_style(combo_box_style::drop_down_list);
       combo_box2.items().push_back_range({"Item1", "Item2", "Item3", "Item4", "Item5", "Item6", "Item7", "Item8", "Item9", "Item10"});
-      combo_box2.selected_index(1);
+      combo_box2.selected_index(0);
       combo_box2.parent(*this);
-      
+      combo_box2.selected_index_changed += [this] {
+        combo_box1.selected_index(combo_box2.selected_index());
+        combo_box3.selected_index(combo_box2.selected_index());
+      };
+
       combo_box3.location({10, 90});
       combo_box3.drop_down_style(combo_box_style::simple);
       combo_box3.items().push_back_range({"Item1", "Item2", "Item3", "Item4", "Item5", "Item6", "Item7", "Item8", "Item9", "Item10"});
-      combo_box3.selected_index(2);
+      combo_box3.selected_index(0);
       combo_box3.parent(*this);
+      combo_box3.selected_index_changed += [this] {
+        combo_box1.selected_index(combo_box3.selected_index());
+        combo_box2.selected_index(combo_box3.selected_index());
+      };
     }
     
   private:
