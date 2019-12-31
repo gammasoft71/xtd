08457b1701a67e7cd94c996466166e85a93c7649
Gammasoft71 <gammasoft71@gmail.com>
2020-01-01 00:32:07 +0100

    Update message_box.cpp

--- src/xtd_forms_native_wxwidgets/src/xtd/forms/native/wxwidgets/message_box.cpp
+++ src/xtd_forms_native_wxwidgets/src/xtd/forms/native/wxwidgets/message_box.cpp
@@ -12,7 +12,6 @@ using namespace xtd::forms::native;
 
 #if defined(__WXMSW__)
 int32_t message_box::show(intptr_t control, const std::string& text, const std::string& caption, uint32_t style, bool displayHelpButton) {
-  //return MessageBoxA(control == 0 ? nullptr : reinterpret_cast<control_handler*>(control)->control()->GetHandle(), text.c_str(), caption.c_str(), style + (displayHelpButton ? 0x00004000L : 0));
   return MessageBoxW(control == 0 ? nullptr : reinterpret_cast<control_handler*>(control)->control()->GetHandle(), std::wstring_convert<std::codecvt_utf8<wchar_t>, wchar_t>().from_bytes(text.c_str()).c_str(), std::wstring_convert<std::codecvt_utf8<wchar_t>, wchar_t>().from_bytes(caption.c_str()).c_str(), style + (displayHelpButton ? 0x00004000L : 0));
 }
 #elif !defined(__WXOSX__)
