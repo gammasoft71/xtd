0c148aa7c1690aef841e5f6937c827234515831b
Gammasoft71 <gammasoft71@gmail.com>
2020-01-19 14:29:45 +0100

    add images

--- src/xtd_drawing/src/xtd/drawing/images.cpp
+++ src/xtd_drawing/src/xtd/drawing/images.cpp
@@ -4,7 +4,9 @@
 #include "resources/apply_256x256.xpm"
 #include "resources/archive_256x256.xpm"
 #include "resources/audio_disk_256x256.xpm"
+#include "resources/back_256x256.xpm"
 #include "resources/gammasoft_256x256.xpm"
+#include "resources/left_256x256.xpm"
 
 using namespace xtd;
 using namespace xtd::drawing;
@@ -14,4 +16,6 @@ const image images::add_256x256 = image::from_data(add_256x256_xpm);
 const image images::apply_256x256 = image::from_data(apply_256x256_xpm);
 const image images::archive_256x256 = image::from_data(archive_256x256_xpm);
 const image images::audio_disk_256x256 = image::from_data(audio_disk_256x256_xpm);
+const image images::back_256x256 = image::from_data(back_256x256_xpm);
 const image images::gammasoft_256x256 = image::from_data(gammasoft_256x256_xpm);
+const image images::left_256x256 = image::from_data(left_256x256_xpm);
