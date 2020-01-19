75efa94938cba88888c6d2d5464dfbbf6be0ede2
Gammasoft71 <gammasoft71@gmail.com>
2020-01-19 21:48:14 +0100

    add images

--- src/xtd_drawing/include/xtd/drawing/images.hpp
+++ src/xtd_drawing/include/xtd/drawing/images.hpp
@@ -21,6 +21,22 @@ namespace xtd {
       static const image audio_disk_256x256;
       /// @brief Gets a system-defined image that represent back image. This field is constant.
       static const image back_256x256;
+      /// @brief Gets a system-defined image that represent burn disk image. This field is constant.
+      static const image burn_disk_256x256;
+      /// @brief Gets a system-defined image that represent calendar image. This field is constant.
+      static const image calendar_256x256;
+      /// @brief Gets a system-defined image that represent cancel image. This field is constant.
+      static const image cancel_256x256;
+      /// @brief Gets a system-defined image that represent cancel 2image. This field is constant.
+      static const image cancel2_256x256;
+      /// @brief Gets a system-defined image that represent cdrom image. This field is constant.
+      static const image cdrom_256x256;
+      /// @brief Gets a system-defined image that represent copy image. This field is constant.
+      static const image copy_256x256;
+      /// @brief Gets a system-defined image that represent copy2 image. This field is constant.
+      static const image copy2_256x256;
+      /// @brief Gets a system-defined image that represent cut image. This field is constant.
+      static const image cut_256x256;
       /// @brief Gets a system-defined image that represent gammasoft image. This field is constant.
       static const image gammasoft_256x256;
       /// @brief Gets a system-defined image that represent left image. This field is constant.
