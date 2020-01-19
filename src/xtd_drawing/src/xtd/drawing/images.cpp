64095d3147b83db63d8f10fbce9dd9ce62ca327c
Gammasoft71 <gammasoft71@gmail.com>
2020-01-19 23:37:37 +0100

    add images

--- src/xtd_drawing/src/xtd/drawing/images.cpp
+++ src/xtd_drawing/src/xtd/drawing/images.cpp
@@ -15,8 +15,60 @@
 #include "resources/cut_256x256.xpm"
 #include "resources/down_256x256.xpm"
 #include "resources/down2_256x256.xpm"
+#include "resources/download_256x256.xpm"
+#include "resources/dvd_disk_256x256.xpm"
+#include "resources/earth_256x256.xpm"
+#include "resources/edit_256x256.xpm"
+#include "resources/favorite_256x256.xpm"
+#include "resources/find_256x256.xpm"
+#include "resources/flash_disk_256x256.xpm"
+#include "resources/folder_256x256.xpm"
+#include "resources/forward_256x256.xpm"
 #include "resources/gammasoft_256x256.xpm"
+#include "resources/hard_disk_256x256.xpm"
+#include "resources/help_256x256.xpm"
+#include "resources/information_256x256.xpm"
+#include "resources/key_256x256.xpm"
+#include "resources/left2_256x256.xpm"
 #include "resources/left_256x256.xpm"
+#include "resources/mail_256x256.xpm"
+#include "resources/mail_forward_256x256.xpm"
+#include "resources/mail_send_256x256.xpm"
+#include "resources/mulimedia_stop_256x256.xpm"
+#include "resources/multimedia_pause_256x256.xpm"
+#include "resources/multimedia_play_256x256.xpm"
+#include "resources/negative_256x256.xpm"
+#include "resources/new_256x256.xpm"
+#include "resources/next_256x256.xpm"
+#include "resources/open_256x256.xpm"
+#include "resources/paste_256x256.xpm"
+#include "resources/picture_256x256.xpm"
+#include "resources/positive_256x256.xpm"
+#include "resources/presentation_256x256.xpm"
+#include "resources/preview_256x256.xpm"
+#include "resources/previous_256x256.xpm"
+#include "resources/print_256x256.xpm"
+#include "resources/properties_256x256.xpm"
+#include "resources/quit_256x256.xpm"
+#include "resources/redo_256x256.xpm"
+#include "resources/refresh_256x256.xpm"
+#include "resources/remove_256x256.xpm"
+#include "resources/rename_256x256.xpm"
+#include "resources/right2_256x256.xpm"
+#include "resources/right_256x256.xpm"
+#include "resources/save_256x256.xpm"
+#include "resources/search_256x256.xpm"
+#include "resources/settings_256x256.xpm"
+#include "resources/stop_256x256.xpm"
+#include "resources/synchronize_256x256.xpm"
+#include "resources/text_document_256x256.xpm"
+#include "resources/undo_256x256.xpm"
+#include "resources/up2_256x256.xpm"
+#include "resources/up_256x256.xpm"
+#include "resources/upload_256x256.xpm"
+#include "resources/user_256x256.xpm"
+#include "resources/zoom_in_256x256.xpm"
+#include "resources/zoom_out_256x256.xpm"
 
 using namespace xtd;
 using namespace xtd::drawing;
@@ -29,13 +81,13 @@ const image images::audio_disk_256x256 = image::from_data(audio_disk_256x256_xpm
 const image images::back_256x256 = image::from_data(back_256x256_xpm);
 const image images::burn_disk_256x256 = image::from_data(burn_disk_256x256_xpm);
 const image images::calendar_256x256 = image::from_data(calendar_256x256_xpm);
-const image images::cancel_256x256 = image::from_data(cancel_256x256_xpm);
 const image images::cancel2_256x256 = image::from_data(cancel2_256x256_xpm);
+const image images::cancel_256x256 = image::from_data(cancel_256x256_xpm);
 const image images::cdrom_256x256 = image::from_data(cdrom_256x256_xpm);
-const image images::copy_256x256 = image::from_data(copy_256x256_xpm);
 const image images::copy2_256x256 = image::from_data(copy2_256x256_xpm);
+const image images::copy_256x256 = image::from_data(copy_256x256_xpm);
 const image images::cut_256x256 = image::from_data(cut_256x256_xpm);
-const image images::down_256x256 = image::from_data(down_256x256_xpm);
 const image images::down2_256x256 = image::from_data(down2_256x256_xpm);
+const image images::down_256x256 = image::from_data(down_256x256_xpm);
 const image images::gammasoft_256x256 = image::from_data(gammasoft_256x256_xpm);
 const image images::left_256x256 = image::from_data(left_256x256_xpm);
