99c2fc5e5d2d0e9da4747a41d9f9a6c2940d7d6e
Gammasoft71 <gammasoft71@gmail.com>
2020-01-19 23:37:37 +0100

    add images

--- src/xtd_drawing/include/xtd/drawing/images.hpp
+++ src/xtd_drawing/include/xtd/drawing/images.hpp
@@ -25,26 +25,130 @@ namespace xtd {
       static const image burn_disk_256x256;
       /// @brief Gets a system-defined image that represent calendar image. This field is constant.
       static const image calendar_256x256;
+      /// @brief Gets a system-defined image that represent cancel2 image. This field is constant.
+      static const image cancel2_256x256;
       /// @brief Gets a system-defined image that represent cancel image. This field is constant.
       static const image cancel_256x256;
-      /// @brief Gets a system-defined image that represent cancel 2image. This field is constant.
-      static const image cancel2_256x256;
       /// @brief Gets a system-defined image that represent cdrom image. This field is constant.
       static const image cdrom_256x256;
-      /// @brief Gets a system-defined image that represent copy image. This field is constant.
-      static const image copy_256x256;
       /// @brief Gets a system-defined image that represent copy2 image. This field is constant.
       static const image copy2_256x256;
+      /// @brief Gets a system-defined image that represent copy image. This field is constant.
+      static const image copy_256x256;
       /// @brief Gets a system-defined image that represent cut image. This field is constant.
       static const image cut_256x256;
+      /// @brief Gets a system-defined image that represent down2 image. This field is constant.
+      static const image down2_256x256;
       /// @brief Gets a system-defined image that represent down image. This field is constant.
       static const image down_256x256;
-      /// @brief Gets a system-defined image that represent down image. This field is constant.
-      static const image down2_256x256;
+      /// @brief Gets a system-defined image that represent dwonload image. This field is constant.
+      static const image download_256x256;
+      /// @brief Gets a system-defined image that represent dvd disk image. This field is constant.
+      static const image dvd_disk_256x256;
+      /// @brief Gets a system-defined image that represent earth image. This field is constant.
+      static const image earth_256x256;
+      /// @brief Gets a system-defined image that represent edit image. This field is constant.
+      static const image edit_256x256;
+      /// @brief Gets a system-defined image that represent favorite image. This field is constant.
+      static const image favorite_256x256;
+      /// @brief Gets a system-defined image that represent find image. This field is constant.
+      static const image find_256x256;
+      /// @brief Gets a system-defined image that represent flash disk image. This field is constant.
+      static const image flash_disk_256x256;
+      /// @brief Gets a system-defined image that represent folder image. This field is constant.
+      static const image folder_256x256;
+      /// @brief Gets a system-defined image that represent forward image. This field is constant.
+      static const image forward_256x256;
       /// @brief Gets a system-defined image that represent gammasoft image. This field is constant.
       static const image gammasoft_256x256;
+      /// @brief Gets a system-defined image that represent hard disk image. This field is constant.
+      static const image hard_disk_256x256;
+      /// @brief Gets a system-defined image that represent help image. This field is constant.
+      static const image help_256x256;
+      /// @brief Gets a system-defined image that represent information image. This field is constant.
+      static const image information_256x256;
+      /// @brief Gets a system-defined image that represent key image. This field is constant.
+      static const image key_256x256;
+      /// @brief Gets a system-defined image that represent left2 image. This field is constant.
+      static const image left2_256x256;
       /// @brief Gets a system-defined image that represent left image. This field is constant.
       static const image left_256x256;
+      /// @brief Gets a system-defined image that represent mail image. This field is constant.
+      static const image mail_256x256;
+      /// @brief Gets a system-defined image that represent mail forward image. This field is constant.
+      static const image mail_forward_256x256;
+      /// @brief Gets a system-defined image that represent mail send image. This field is constant.
+      static const image mail_send_256x256;
+      /// @brief Gets a system-defined image that represent multimedia stop image. This field is constant.
+      static const image mulimedia_stop_256x256;
+      /// @brief Gets a system-defined image that represent multimedia pause image. This field is constant.
+      static const image multimedia_pause_256x256;
+      /// @brief Gets a system-defined image that represent multimedia play image. This field is constant.
+      static const image multimedia_play_256x256;
+      /// @brief Gets a system-defined image that represent negative image. This field is constant.
+      static const image negative_256x256;
+      /// @brief Gets a system-defined image that represent new image. This field is constant.
+      static const image new_256x256;
+      /// @brief Gets a system-defined image that represent next image. This field is constant.
+      static const image next_256x256;
+      /// @brief Gets a system-defined image that represent open image. This field is constant.
+      static const image open_256x256;
+      /// @brief Gets a system-defined image that represent paste image. This field is constant.
+      static const image paste_256x256;
+      /// @brief Gets a system-defined image that represent picture image. This field is constant.
+      static const image picture_256x256;
+      /// @brief Gets a system-defined image that represent positive image. This field is constant.
+      static const image positive_256x256;
+      /// @brief Gets a system-defined image that represent presentation image. This field is constant.
+      static const image presentation_256x256;
+      /// @brief Gets a system-defined image that represent preview image. This field is constant.
+      static const image preview_256x256;
+      /// @brief Gets a system-defined image that represent previous image. This field is constant.
+      static const image previous_256x256;
+      /// @brief Gets a system-defined image that represent print image. This field is constant.
+      static const image print_256x256;
+      /// @brief Gets a system-defined image that represent properties image. This field is constant.
+      static const image properties_256x256;
+      /// @brief Gets a system-defined image that represent quit image. This field is constant.
+      static const image quit_256x256;
+      /// @brief Gets a system-defined image that represent redo image. This field is constant.
+      static const image redo_256x256;
+      /// @brief Gets a system-defined image that represent refresh image. This field is constant.
+      static const image refresh_256x256;
+      /// @brief Gets a system-defined image that represent remove image. This field is constant.
+      static const image remove_256x256;
+      /// @brief Gets a system-defined image that represent rename image. This field is constant.
+      static const image rename_256x256;
+      /// @brief Gets a system-defined image that represent right2 image. This field is constant.
+      static const image right2_256x256;
+      /// @brief Gets a system-defined image that represent right image. This field is constant.
+      static const image right_256x256;
+      /// @brief Gets a system-defined image that represent save image. This field is constant.
+      static const image save_256x256;
+      /// @brief Gets a system-defined image that represent search image. This field is constant.
+      static const image search_256x256;
+      /// @brief Gets a system-defined image that represent settings image. This field is constant.
+      static const image settings_256x256;
+      /// @brief Gets a system-defined image that represent stop image. This field is constant.
+      static const image stop_256x256;
+      /// @brief Gets a system-defined image that represent synchronise image. This field is constant.
+      static const image synchronize_256x256;
+      /// @brief Gets a system-defined image that represent text document image. This field is constant.
+      static const image text_document_256x256;
+      /// @brief Gets a system-defined image that represent undo image. This field is constant.
+      static const image undo_256x256;
+      /// @brief Gets a system-defined image that represent up2 image. This field is constant.
+      static const image up2_256x256;
+      /// @brief Gets a system-defined image that represent up image. This field is constant.
+      static const image up_256x256;
+      /// @brief Gets a system-defined image that represent upload image. This field is constant.
+      static const image upload_256x256;
+      /// @brief Gets a system-defined image that represent user image. This field is constant.
+      static const image user_256x256;
+      /// @brief Gets a system-defined image that represent zoom in image. This field is constant.
+      static const image zoom_in_256x256;
+      /// @brief Gets a system-defined image that represent zoom out image. This field is constant.
+      static const image zoom_out_256x256;
     };
   }
 }
