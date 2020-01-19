7c20e888dc4d5fea448a16a906c167f679c8d21d
Gammasoft71 <gammasoft71@gmail.com>
2020-01-19 21:48:14 +0100

    add images

--- src/xtd_drawing/src/xtd/drawing/images.cpp
+++ src/xtd_drawing/src/xtd/drawing/images.cpp
@@ -5,6 +5,14 @@
 #include "resources/archive_256x256.xpm"
 #include "resources/audio_disk_256x256.xpm"
 #include "resources/back_256x256.xpm"
+#include "resources/burn_disk_256x256.xpm"
+#include "resources/calendar_256x256.xpm"
+#include "resources/cancel_256x256.xpm"
+#include "resources/cancel2_256x256.xpm"
+#include "resources/cdrom_256x256.xpm"
+#include "resources/copy_256x256.xpm"
+#include "resources/copy2_256x256.xpm"
+#include "resources/cut_256x256.xpm"
 #include "resources/gammasoft_256x256.xpm"
 #include "resources/left_256x256.xpm"
 
@@ -17,5 +25,13 @@ const image images::apply_256x256 = image::from_data(apply_256x256_xpm);
 const image images::archive_256x256 = image::from_data(archive_256x256_xpm);
 const image images::audio_disk_256x256 = image::from_data(audio_disk_256x256_xpm);
 const image images::back_256x256 = image::from_data(back_256x256_xpm);
+const image images::burn_disk_256x256 = image::from_data(burn_disk_256x256_xpm);
+const image images::calendar_256x256 = image::from_data(calendar_256x256_xpm);
+const image images::cancel_256x256 = image::from_data(cancel_256x256_xpm);
+const image images::cancel2_256x256 = image::from_data(cancel2_256x256_xpm);
+const image images::cdrom_256x256 = image::from_data(cdrom_256x256_xpm);
+const image images::copy_256x256 = image::from_data(copy_256x256_xpm);
+const image images::copy2_256x256 = image::from_data(copy2_256x256_xpm);
+const image images::cut_256x256 = image::from_data(cut_256x256_xpm);
 const image images::gammasoft_256x256 = image::from_data(gammasoft_256x256_xpm);
 const image images::left_256x256 = image::from_data(left_256x256_xpm);
