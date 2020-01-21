aee6bb41214adbf6572dbd8fefe338bd572dd56b
Gammasoft71 <gammasoft71@gmail.com>
2020-01-21 23:01:38 +0100

    add images

--- src/xtd_drawing/src/xtd/drawing/images.cpp
+++ src/xtd_drawing/src/xtd/drawing/images.cpp
@@ -1,4 +1,75 @@
 #include "../../../include/xtd/drawing/images.hpp"
+#include "resources/about_128x128.xpm"
+#include "resources/add_128x128.xpm"
+#include "resources/apply_128x128.xpm"
+#include "resources/archive_128x128.xpm"
+#include "resources/audio_disk_128x128.xpm"
+#include "resources/back_128x128.xpm"
+#include "resources/burn_disk_128x128.xpm"
+#include "resources/calendar_128x128.xpm"
+#include "resources/cancel2_128x128.xpm"
+#include "resources/cancel_128x128.xpm"
+#include "resources/compact_disk_128x128.xpm"
+#include "resources/copy2_128x128.xpm"
+#include "resources/copy_128x128.xpm"
+#include "resources/cut_128x128.xpm"
+#include "resources/delete_128x128.xpm"
+#include "resources/down2_128x128.xpm"
+#include "resources/down_128x128.xpm"
+#include "resources/download_128x128.xpm"
+#include "resources/dvd_disk_128x128.xpm"
+#include "resources/earth_128x128.xpm"
+#include "resources/edit_128x128.xpm"
+#include "resources/favorite_128x128.xpm"
+#include "resources/find_128x128.xpm"
+#include "resources/flash_disk_128x128.xpm"
+#include "resources/folder_128x128.xpm"
+#include "resources/forward_128x128.xpm"
+#include "resources/gammasoft_128x128.xpm"
+#include "resources/hard_disk_128x128.xpm"
+#include "resources/help_128x128.xpm"
+#include "resources/information_128x128.xpm"
+#include "resources/key_128x128.xpm"
+#include "resources/left2_128x128.xpm"
+#include "resources/left_128x128.xpm"
+#include "resources/mail_128x128.xpm"
+#include "resources/mail_forward_128x128.xpm"
+#include "resources/mail_send_128x128.xpm"
+#include "resources/multimedia_pause_128x128.xpm"
+#include "resources/multimedia_play_128x128.xpm"
+#include "resources/multimedia_stop_128x128.xpm"
+#include "resources/negative_128x128.xpm"
+#include "resources/new_128x128.xpm"
+#include "resources/next_128x128.xpm"
+#include "resources/open_128x128.xpm"
+#include "resources/paste_128x128.xpm"
+#include "resources/picture_128x128.xpm"
+#include "resources/positive_128x128.xpm"
+#include "resources/presentation_128x128.xpm"
+#include "resources/preview_128x128.xpm"
+#include "resources/previous_128x128.xpm"
+#include "resources/print_128x128.xpm"
+#include "resources/properties_128x128.xpm"
+#include "resources/quit_128x128.xpm"
+#include "resources/redo_128x128.xpm"
+#include "resources/refresh_128x128.xpm"
+#include "resources/remove_128x128.xpm"
+#include "resources/rename_128x128.xpm"
+#include "resources/right2_128x128.xpm"
+#include "resources/right_128x128.xpm"
+#include "resources/save_128x128.xpm"
+#include "resources/search_128x128.xpm"
+#include "resources/settings_128x128.xpm"
+#include "resources/stop_128x128.xpm"
+#include "resources/synchronize_128x128.xpm"
+#include "resources/text_document_128x128.xpm"
+#include "resources/undo_128x128.xpm"
+#include "resources/up2_128x128.xpm"
+#include "resources/up_128x128.xpm"
+#include "resources/upload_128x128.xpm"
+#include "resources/user_128x128.xpm"
+#include "resources/zoom_in_128x128.xpm"
+#include "resources/zoom_out_128x128.xpm"
 #include "resources/about_256x256.xpm"
 #include "resources/add_256x256.xpm"
 #include "resources/apply_256x256.xpm"
@@ -74,6 +145,78 @@
 using namespace xtd;
 using namespace xtd::drawing;
 
+const image images::about_128x128 = image::from_data(about_128x128_xpm);
+const image images::add_128x128 = image::from_data(add_128x128_xpm);
+const image images::apply_128x128 = image::from_data(apply_128x128_xpm);
+const image images::archive_128x128 = image::from_data(archive_128x128_xpm);
+const image images::audio_disk_128x128 = image::from_data(audio_disk_128x128_xpm);
+const image images::back_128x128 = image::from_data(back_128x128_xpm);
+const image images::burn_disk_128x128 = image::from_data(burn_disk_128x128_xpm);
+const image images::calendar_128x128 = image::from_data(calendar_128x128_xpm);
+const image images::cancel2_128x128 = image::from_data(cancel2_128x128_xpm);
+const image images::cancel_128x128 = image::from_data(cancel_128x128_xpm);
+const image images::compact_disk_128x128 = image::from_data(compact_disk_128x128_xpm);
+const image images::copy2_128x128 = image::from_data(copy2_128x128_xpm);
+const image images::copy_128x128 = image::from_data(copy_128x128_xpm);
+const image images::cut_128x128 = image::from_data(cut_128x128_xpm);
+const image images::delete_128x128 = image::from_data(delete_128x128_xpm);
+const image images::down2_128x128 = image::from_data(down2_128x128_xpm);
+const image images::down_128x128 = image::from_data(down_128x128_xpm);
+const image images::download_128x128 = image::from_data(download_128x128_xpm);
+const image images::dvd_disk_128x128 = image::from_data(dvd_disk_128x128_xpm);
+const image images::earth_128x128 = image::from_data(earth_128x128_xpm);
+const image images::edit_128x128 = image::from_data(edit_128x128_xpm);
+const image images::favorite_128x128 = image::from_data(favorite_128x128_xpm);
+const image images::find_128x128 = image::from_data(find_128x128_xpm);
+const image images::flash_disk_128x128 = image::from_data(flash_disk_128x128_xpm);
+const image images::folder_128x128 = image::from_data(folder_128x128_xpm);
+const image images::forward_128x128 = image::from_data(forward_128x128_xpm);
+const image images::gammasoft_128x128 = image::from_data(gammasoft_128x128_xpm);
+const image images::hard_disk_128x128 = image::from_data(hard_disk_128x128_xpm);
+const image images::help_128x128 = image::from_data(help_128x128_xpm);
+const image images::information_128x128 = image::from_data(information_128x128_xpm);
+const image images::key_128x128 = image::from_data(key_128x128_xpm);
+const image images::left2_128x128 = image::from_data(left2_128x128_xpm);
+const image images::left_128x128 = image::from_data(left_128x128_xpm);
+const image images::mail_128x128 = image::from_data(mail_128x128_xpm);
+const image images::mail_forward_128x128 = image::from_data(mail_forward_128x128_xpm);
+const image images::mail_send_128x128 = image::from_data(mail_send_128x128_xpm);
+const image images::multimedia_pause_128x128 = image::from_data(multimedia_pause_128x128_xpm);
+const image images::multimedia_play_128x128 = image::from_data(multimedia_play_128x128_xpm);
+const image images::multimedia_stop_128x128 = image::from_data(multimedia_stop_128x128_xpm);
+const image images::negative_128x128 = image::from_data(negative_128x128_xpm);
+const image images::new_128x128 = image::from_data(new_128x128_xpm);
+const image images::next_128x128 = image::from_data(next_128x128_xpm);
+const image images::open_128x128 = image::from_data(open_128x128_xpm);
+const image images::paste_128x128 = image::from_data(paste_128x128_xpm);
+const image images::picture_128x128 = image::from_data(picture_128x128_xpm);
+const image images::positive_128x128 = image::from_data(positive_128x128_xpm);
+const image images::presentation_128x128 = image::from_data(presentation_128x128_xpm);
+const image images::preview_128x128 = image::from_data(previous_128x128_xpm);
+const image images::previous_128x128 = image::from_data(previous_128x128_xpm);
+const image images::print_128x128 = image::from_data(print_128x128_xpm);
+const image images::properties_128x128 = image::from_data(properties_128x128_xpm);
+const image images::quit_128x128 = image::from_data(quit_128x128_xpm);
+const image images::redo_128x128 = image::from_data(redo_128x128_xpm);
+const image images::refresh_128x128 = image::from_data(refresh_128x128_xpm);
+const image images::remove_128x128 = image::from_data(remove_128x128_xpm);
+const image images::rename_128x128 = image::from_data(rename_128x128_xpm);
+const image images::right2_128x128 = image::from_data(right2_128x128_xpm);
+const image images::right_128x128 = image::from_data(right_128x128_xpm);
+const image images::save_128x128 = image::from_data(save_128x128_xpm);
+const image images::search_128x128 = image::from_data(search_128x128_xpm);
+const image images::settings_128x128 = image::from_data(settings_128x128_xpm);
+const image images::stop_128x128 = image::from_data(stop_128x128_xpm);
+const image images::synchronize_128x128 = image::from_data(synchronize_128x128_xpm);
+const image images::text_document_128x128 = image::from_data(text_document_128x128_xpm);
+const image images::undo_128x128 = image::from_data(undo_128x128_xpm);
+const image images::up2_128x128 = image::from_data(up2_128x128_xpm);
+const image images::up_128x128 = image::from_data(up_128x128_xpm);
+const image images::upload_128x128 = image::from_data(upload_128x128_xpm);
+const image images::user_128x128 = image::from_data(user_128x128_xpm);
+const image images::zoom_in_128x128 = image::from_data(zoom_in_128x128_xpm);
+const image images::zoom_out_128x128 = image::from_data(zoom_out_128x128_xpm);
+
 const image images::about_256x256 = image::from_data(about_256x256_xpm);
 const image images::add_256x256 = image::from_data(add_256x256_xpm);
 const image images::apply_256x256 = image::from_data(apply_256x256_xpm);
@@ -137,7 +280,7 @@ const image images::search_256x256 = image::from_data(search_256x256_xpm);
 const image images::settings_256x256 = image::from_data(settings_256x256_xpm);
 const image images::stop_256x256 = image::from_data(stop_256x256_xpm);
 const image images::synchronize_256x256 = image::from_data(synchronize_256x256_xpm);
-const image images::text_document_256x256 = image::from_data(text_ocument_256x256_xpm);
+const image images::text_document_256x256 = image::from_data(text_document_256x256_xpm);
 const image images::undo_256x256 = image::from_data(undo_256x256_xpm);
 const image images::up2_256x256 = image::from_data(up2_256x256_xpm);
 const image images::up_256x256 = image::from_data(up_256x256_xpm);
