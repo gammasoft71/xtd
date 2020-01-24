6836f46d8692e4526904e5a2b4dbc745ec94196f
Gammasoft71 <gammasoft71@gmail.com>
2020-01-24 21:11:11 +0100

    Update control.cpp

--- src/xtd_forms/src/xtd/forms/control.cpp
+++ src/xtd_forms/src/xtd/forms/control.cpp
@@ -316,7 +316,7 @@ bool control::is_handle_created() const {
 }
 
 control::async_result_invoke control::begin_invoke(delegate<void(std::vector<std::any>)> value, const std::vector<std::any>& args) {
-  while (!xtd::forms::application::message_loop) std::this_thread::sleep_for(10ms);
+  while (!xtd::forms::application::message_loop()) std::this_thread::sleep_for(10ms);
   async_result_invoke async;
   async.async_mutex().lock();
   native::control::invoke_in_control_thread(handle_, value, args, async.async_mutex_);
