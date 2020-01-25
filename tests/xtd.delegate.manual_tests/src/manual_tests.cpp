cb6d1084809e79b31d445dd3d8287328f4eda002
Gammasoft71 <gammasoft71@gmail.com>
2020-01-25 21:05:22 +0100

    Update manual_tests.cpp

--- tests/xtd.delegate.manual_tests/src/manual_tests.cpp
+++ tests/xtd.delegate.manual_tests/src/manual_tests.cpp
@@ -5,21 +5,13 @@
 using namespace std;
 using namespace xtd;
 
-//using printer = delegate<void()>;
-using printer = function<void()>;
-
-void print_hello_world() {
-  cout << "Hello, world!" << endl;
-}
-
-printer&& create_delegate() {
-  printer item = {print_hello_world};
-  //return item;
-  //return std::forward<printer>(item);
-  return std::move(item);
-}
-
 int main() {
-  printer d1 = create_delegate();
-  d1();
+  delegate<void()> d1;
+  delegate<void()> d2 = d1;
+
+  d1 += []()  {
+    cout << "test" << endl;
+  };
+  
+  d2();
 }
