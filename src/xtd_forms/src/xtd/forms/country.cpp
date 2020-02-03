60e0c4bb8c2f410ed280668e0514d424e8c27ebb
Gammasoft71 <gammasoft71@gmail.com>
2020-02-03 22:44:42 +0100

    add country and countries classes

---
+++ src/xtd_forms/src/xtd/forms/country.cpp
@@ -0,0 +1,6 @@
+#include "../../../include/xtd/forms/country.hpp"
+
+using namespace std;
+using namespace xtd::forms;
+
+const vector<string> country::enclosed_letters {u8"\U0001F1E6", u8"\U0001F1E7", u8"\U0001F1E8", u8"\U0001F1E9", u8"\U0001F1EA", u8"\U0001F1EB", u8"\U0001F1EC", u8"\U0001F1ED", u8"\U0001F1EE", u8"\U0001F1EF", u8"\U0001F1F0", u8"\U0001F1F1", u8"\U0001F1F2", u8"\U0001F1F3", u8"\U0001F1F4", u8"\U0001F1F5", u8"\U0001F1F6", u8"\U0001F1F7", u8"\U0001F1F8", u8"\U0001F1F9", u8"\U0001F1FA", u8"\U0001F1FB", u8"\U0001F1FC", u8"\U0001F1FD", u8"\U0001F1FE", u8"\U0001F1FF"};
