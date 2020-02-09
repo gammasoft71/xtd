abb4f073f565d6f9e0128f862c6ee73b19219249
Gammasoft71 <gammasoft71@gmail.com>
2020-02-09 22:05:27 +0100

    add emoticons

--- src/xtd_forms/include/xtd/forms/emoticons.hpp
+++ src/xtd_forms/include/xtd/forms/emoticons.hpp
@@ -54,7 +54,7 @@ namespace xtd {
       static const xtd::forms::emoticon watch;
       /// @brief Gets a system-defined emoticon that has a string value of 0x0000231B. This field is constant.
       static const xtd::forms::emoticon hourglass;
-      /// @brief Gets a system-defined emoticon that has a string value of 0x00002328. This field is constant.
+      /// @brief Gets a system-defined emoticon that has a string value of {0x00002328, 0x0000FE0F}. This field is constant.
       static const xtd::forms::emoticon keyboard;
       /// @brief Gets a system-defined emoticon that has a string value of {0x000023CF, 0x0000FE0F}. This field is constant.
       static const xtd::forms::emoticon eject_symbol;
@@ -109,6 +109,173 @@ namespace xtd {
       static const xtd::forms::emoticon white_medium_small_square;
       /// @brief Gets a system-defined emoticon that has a string value of 0x000025FE. This field is constant.
       static const xtd::forms::emoticon black_medium_small_square;
+
+      /// 2600-26FF Miscellaneous Symbols
+      
+      /// @brief Gets a system-defined emoticon that has a string value of {0x00002600, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon black_sun_with_rays;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x00002601, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon cloud;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x00002602, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon umbrella;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x00002603, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon snowman;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x00002604, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon comet;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x0000260E, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon black_telephone;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x00002611, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon ballot_box_with_check;
+      /// @brief Gets a system-defined emoticon that has a string value of 0x00002614. This field is constant.
+      static const xtd::forms::emoticon umbrella_with_rain_drops;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x00002615, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon hot_beverage;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x00002618, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon shamrock;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x0000261D, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon white_up_pointing_index;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x00002620, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon skull_and_crossbones;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x00002622, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon radioactive_sign;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x00002623, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon biohazard_sign;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x00002626, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon orthodox_cross;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x0000262A, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon star_and_crescent;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x0000262E, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon peace_symbol;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x0000262F, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon yin_yang;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x00002638, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon wheel_of_dharma;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x00002639, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon white_frowning_face;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x0000263A, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon white_smiling_face;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x00002640, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon female_sign;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x00002642, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon male_sign;
+      /// @brief Gets a system-defined emoticon that has a string value of 0x00002648. This field is constant.
+      static const xtd::forms::emoticon aries;
+      /// @brief Gets a system-defined emoticon that has a string value of 0x00002649. This field is constant.
+      static const xtd::forms::emoticon taurus;
+      /// @brief Gets a system-defined emoticon that has a string value of 0x0000264A. This field is constant.
+      static const xtd::forms::emoticon gemini;
+      /// @brief Gets a system-defined emoticon that has a string value of 0x0000264B. This field is constant.
+      static const xtd::forms::emoticon cancer;
+      /// @brief Gets a system-defined emoticon that has a string value of 0x0000264C. This field is constant.
+      static const xtd::forms::emoticon leo;
+      /// @brief Gets a system-defined emoticon that has a string value of 0x0000264D. This field is constant.
+      static const xtd::forms::emoticon virgo;
+      /// @brief Gets a system-defined emoticon that has a string value of 0x0000264E. This field is constant.
+      static const xtd::forms::emoticon libra;
+      /// @brief Gets a system-defined emoticon that has a string value of 0x0000264F. This field is constant.
+      static const xtd::forms::emoticon scorpius;
+      /// @brief Gets a system-defined emoticon that has a string value of 0x00002650. This field is constant.
+      static const xtd::forms::emoticon sagittarius;
+      /// @brief Gets a system-defined emoticon that has a string value of 0x00002651. This field is constant.
+      static const xtd::forms::emoticon capricorn;
+      /// @brief Gets a system-defined emoticon that has a string value of 0x00002652. This field is constant.
+      static const xtd::forms::emoticon aquarius;
+      /// @brief Gets a system-defined emoticon that has a string value of 0x00002653. This field is constant.
+      static const xtd::forms::emoticon pisces;
+      /// @brief Gets a system-defined emoticon that has a string value of 0x0000265F. This field is constant.
+      static const xtd::forms::emoticon black_chess_pawn;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x00002660, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon black_spade_suit;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x00002663, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon black_club_suit;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x00002665, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon black_heart_suit;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x00002666, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon black_diamond_suit;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x00002668, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon hot_springs;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x0000267B, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon black_universal_recycling_symbol;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x0000267E, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon permanent_paper_sign;
+      /// @brief Gets a system-defined emoticon that has a string value of 0x0000267F. This field is constant.
+      static const xtd::forms::emoticon wheelchair_symbol;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x00002692, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon hammer_and_pick;
+      /// @brief Gets a system-defined emoticon that has a string value of 0x00002693. This field is constant.
+      static const xtd::forms::emoticon anchor;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x00002694, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon crossed_swords;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x00002695, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon staff_of_aesculapius;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x00002696, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon scales;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x00002697, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon alembic;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x00002699, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon gear;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x0000269B, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon atom_symbol;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x0000269C, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon fleur_de_lis;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x000026A0, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon warning_sign;
+      /// @brief Gets a system-defined emoticon that has a string value of 0x000026A1. This field is constant.
+      static const xtd::forms::emoticon high_voltage_sign;
+      /// @brief Gets a system-defined emoticon that has a string value of 0x000026AA. This field is constant.
+      static const xtd::forms::emoticon medium_white_circle;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x000026AB. This field is constant.
+      static const xtd::forms::emoticon medium_black_circle;
+      /// @brief Gets a system-defined emoticon that has a string value of 0x000026B0, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon coffin;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x000026B1, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon funeral_urn;
+      /// @brief Gets a system-defined emoticon that has a string value of 0x000026BD. This field is constant.
+      static const xtd::forms::emoticon soccer_ball;
+      /// @brief Gets a system-defined emoticon that has a string value of 0x000026BE. This field is constant.
+      static const xtd::forms::emoticon baseball;
+      /// @brief Gets a system-defined emoticon that has a string value of 0x000026C4. This field is constant.
+      static const xtd::forms::emoticon snowman_without_snow;
+      /// @brief Gets a system-defined emoticon that has a string value of 0x000026C5. This field is constant.
+      static const xtd::forms::emoticon sun_behind_cloud;
+      /// @brief Gets a system-defined emoticon that has a string value of 0x000026C8. This field is constant.
+      static const xtd::forms::emoticon thunder_cloud_and_rain;
+      /// @brief Gets a system-defined emoticon that has a string value of 0x000026CE. This field is constant.
+      static const xtd::forms::emoticon ophiuchus;
+      /// @brief Gets a system-defined emoticon that has a string value of 0x000026CF. This field is constant.
+      static const xtd::forms::emoticon pick;
+      /// @brief Gets a system-defined emoticon that has a string value of 0x000026D1. This field is constant.
+      static const xtd::forms::emoticon helmet_with_white_cross;
+      /// @brief Gets a system-defined emoticon that has a string value of 0x000026D3. This field is constant.
+      static const xtd::forms::emoticon chains;
+      /// @brief Gets a system-defined emoticon that has a string value of 0x000026D4. This field is constant.
+      static const xtd::forms::emoticon no_entry;
+      /// @brief Gets a system-defined emoticon that has a string value of 0x000026E9. This field is constant.
+      static const xtd::forms::emoticon shinto_shrine;
+      /// @brief Gets a system-defined emoticon that has a string value of 0x000026EA. This field is constant.
+      static const xtd::forms::emoticon church;
+      /// @brief Gets a system-defined emoticon that has a string value of 0x000026F0. This field is constant.
+      static const xtd::forms::emoticon mountain;
+      /// @brief Gets a system-defined emoticon that has a string value of 0x000026F1. This field is constant.
+      static const xtd::forms::emoticon umbrella_on_ground;
+      /// @brief Gets a system-defined emoticon that has a string value of 0x000026F2. This field is constant.
+      static const xtd::forms::emoticon fountain;
+      /// @brief Gets a system-defined emoticon that has a string value of 0x000026F3. This field is constant.
+      static const xtd::forms::emoticon flag_in_hole;
+      /// @brief Gets a system-defined emoticon that has a string value of 0x000026F4. This field is constant.
+      static const xtd::forms::emoticon ferry;
+      /// @brief Gets a system-defined emoticon that has a string value of 0x000026F5. This field is constant.
+      static const xtd::forms::emoticon sailboat;
+      /// @brief Gets a system-defined emoticon that has a string value of 0x000026F7. This field is constant.
+      static const xtd::forms::emoticon skier;
+      /// @brief Gets a system-defined emoticon that has a string value of 0x000026F8. This field is constant.
+      static const xtd::forms::emoticon ice_skate;
+      /// @brief Gets a system-defined emoticon that has a string value of 0x000026F9. This field is constant.
+      static const xtd::forms::emoticon person_with_ball;
+      /// @brief Gets a system-defined emoticon that has a string value of 0x000026FA. This field is constant.
+      static const xtd::forms::emoticon tent;
+      /// @brief Gets a system-defined emoticon that has a string value of 0x000026FD. This field is constant.
+      static const xtd::forms::emoticon fuel_pump;
 
       static const std::vector<xtd::forms::emoticon>& get_emoticons();
     };
--- src/xtd_forms/src/xtd/forms/emoticons.cpp
+++ src/xtd_forms/src/xtd/forms/emoticons.cpp
@@ -33,7 +33,7 @@ const xtd::forms::emoticon xtd::forms::emoticons::rightwards_arrow_with_hook = {
 
 const xtd::forms::emoticon xtd::forms::emoticons::watch = {"watch", 0x0000231A};
 const xtd::forms::emoticon xtd::forms::emoticons::hourglass = {"hourglass", 0x0000231B};
-const xtd::forms::emoticon xtd::forms::emoticons::keyboard = {"keyboard", 0x00002328};
+const xtd::forms::emoticon xtd::forms::emoticons::keyboard = {"keyboard", {0x00002328, 0x0000FE0F}};
 const xtd::forms::emoticon xtd::forms::emoticons::eject_symbol = {"eject symbol", {0x000023CF, 0x0000FE0F}};
 const xtd::forms::emoticon xtd::forms::emoticons::black_right_pointing_double_triangle = {"black right-pointing double triangle", 0x000023E9};
 const xtd::forms::emoticon xtd::forms::emoticons::black_left_pointing_double_triangle = {"black left-pointing double triangle", 0x000023EA};
@@ -65,6 +65,91 @@ const xtd::forms::emoticon xtd::forms::emoticons::black_medium_square = {"black 
 const xtd::forms::emoticon xtd::forms::emoticons::white_medium_small_square = {"white medium small square", 0x000025FD};
 const xtd::forms::emoticon xtd::forms::emoticons::black_medium_small_square = {"black medium small square", 0x000025FE};
 
+/// 2600-26FF Miscellaneous Symbols
+
+const xtd::forms::emoticon xtd::forms::emoticons::black_sun_with_rays = {"black sun with rays", {0x00002600, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::cloud = {"cloud", {0x00002601, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::umbrella = {"umbrella", {0x00002602, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::snowman = {"snowman", {0x00002603, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::comet = {"comet", {0x00002604, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::black_telephone = {"black telephone", {0x0000260E, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::ballot_box_with_check = {"ballot box with check", {0x00002611, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::umbrella_with_rain_drops = {"umbrella with rain drops", 0x00002614};
+const xtd::forms::emoticon xtd::forms::emoticons::hot_beverage = {"hot beverage", {0x00002615, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::shamrock = {"shamrock", {0x00002618, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::white_up_pointing_index = {"white up pointing index", {0x0000261D, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::skull_and_crossbones = {"skull and crossbones", {0x00002620, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::radioactive_sign = {"radioactive sign", {0x00002622, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::biohazard_sign = {"biohazard sign", {0x00002623, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::orthodox_cross = {"orthodox cross", {0x00002626, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::star_and_crescent = {"star and crescent", {0x0000262A, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::peace_symbol = {"peace symbol", {0x0000262E, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::yin_yang = {"yin yang", {0x0000262F, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::wheel_of_dharma = {"wheel of dharma", {0x00002638, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::white_frowning_face = {"white frowning face", {0x00002639, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::white_smiling_face = {"white smiling face", {0x0000263A, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::female_sign = {"female sign", {0x00002640, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::male_sign = {"male sign", {0x00002642, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::aries = {"aries", 0x00002648};
+const xtd::forms::emoticon xtd::forms::emoticons::taurus = {"taurus", 0x00002649};
+const xtd::forms::emoticon xtd::forms::emoticons::gemini = {"gemini", 0x0000264A};
+const xtd::forms::emoticon xtd::forms::emoticons::cancer = {"cancer", 0x0000264B};
+const xtd::forms::emoticon xtd::forms::emoticons::leo = {"leo", 0x0000264C};
+const xtd::forms::emoticon xtd::forms::emoticons::virgo = {"virgo", 0x0000264D};
+const xtd::forms::emoticon xtd::forms::emoticons::libra = {"libra", 0x0000264E};
+const xtd::forms::emoticon xtd::forms::emoticons::scorpius = {"scorpius", 0x0000264F};
+const xtd::forms::emoticon xtd::forms::emoticons::sagittarius = {"sagittarius", 0x00002650};
+const xtd::forms::emoticon xtd::forms::emoticons::capricorn = {"capricorn", 0x00002651};
+const xtd::forms::emoticon xtd::forms::emoticons::aquarius = {"aquarius", 0x00002652};
+const xtd::forms::emoticon xtd::forms::emoticons::pisces = {"pisces", 0x00002653};
+const xtd::forms::emoticon xtd::forms::emoticons::black_chess_pawn = {"black chess pawn", {0x0000265F, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::black_spade_suit = {"black spade suit", {0x00002660, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::black_club_suit = {"black club suit", {0x00002663, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::black_heart_suit = {"black heart suit", {0x00002665, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::black_diamond_suit = {"black diamond suit", {0x00002666, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::hot_springs = {"hot springs", {0x00002668, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::black_universal_recycling_symbol = {"black universal recycling symbol", {0x0000267B, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::permanent_paper_sign = {"permanent paper sign", {0x0000267E, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::wheelchair_symbol = {"wheelchair symbol", 0x0000267F};
+const xtd::forms::emoticon xtd::forms::emoticons::hammer_and_pick = {"hammer and pick", {0x00002692, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::anchor = {"anchor", 0x00002693};
+const xtd::forms::emoticon xtd::forms::emoticons::crossed_swords = {"crossed swords", {0x00002694, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::staff_of_aesculapius = {"staff of aesculapius", {0x00002695, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::scales = {"scales", {0x00002696, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::alembic = {"alembic", {0x00002697, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::gear = {"gear", {0x00002699, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::atom_symbol = {"atom symbol", {0x0000269B, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::fleur_de_lis = {"fleur-de-lis", {0x0000269C, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::warning_sign = {"warning sign", {0x000026A0, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::high_voltage_sign = {"high voltage sign", 0x000026A1};
+const xtd::forms::emoticon xtd::forms::emoticons::medium_white_circle = {"medium white circle", 0x000026AA};
+const xtd::forms::emoticon xtd::forms::emoticons::medium_black_circle = {"medium black circle", 0x000026AB};
+const xtd::forms::emoticon xtd::forms::emoticons::coffin = {"coffin", {0x000026B0, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::funeral_urn = {"funeral urn", {0x000026B1, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::soccer_ball = {"soccer ball", 0x000026BD};
+const xtd::forms::emoticon xtd::forms::emoticons::baseball = {"baseball", 0x000026BE};
+const xtd::forms::emoticon xtd::forms::emoticons::snowman_without_snow = {"snowman without snow", 0x000026C4};
+const xtd::forms::emoticon xtd::forms::emoticons::sun_behind_cloud = {"sun behind cloud", 0x000026C5};
+const xtd::forms::emoticon xtd::forms::emoticons::thunder_cloud_and_rain = {"thunder cloud and rain", 0x000026C8};
+const xtd::forms::emoticon xtd::forms::emoticons::ophiuchus = {"ophiuchus", 0x000026CE};
+const xtd::forms::emoticon xtd::forms::emoticons::pick = {"pick", 0x000026CF};
+const xtd::forms::emoticon xtd::forms::emoticons::helmet_with_white_cross = {"helmet with white cross", 0x000026D1};
+const xtd::forms::emoticon xtd::forms::emoticons::chains = {"chains", 0x000026D3};
+const xtd::forms::emoticon xtd::forms::emoticons::no_entry = {"no entry", 0x000026D4};
+const xtd::forms::emoticon xtd::forms::emoticons::shinto_shrine = {"shinto shrine", 0x000026E9};
+const xtd::forms::emoticon xtd::forms::emoticons::church = {"church", 0x000026EA};
+const xtd::forms::emoticon xtd::forms::emoticons::mountain = {"mountain", 0x000026F0};
+const xtd::forms::emoticon xtd::forms::emoticons::umbrella_on_ground = {"umbrella on ground", 0x000026F1};
+const xtd::forms::emoticon xtd::forms::emoticons::fountain = {"fountain", 0x000026F2};
+const xtd::forms::emoticon xtd::forms::emoticons::flag_in_hole = {"flag in hole", 0x000026F3};
+const xtd::forms::emoticon xtd::forms::emoticons::ferry = {"ferry", 0x000026F4};
+const xtd::forms::emoticon xtd::forms::emoticons::sailboat = {"sailboat", 0x000026F5};
+const xtd::forms::emoticon xtd::forms::emoticons::skier = {"skier", 0x000026F7};
+const xtd::forms::emoticon xtd::forms::emoticons::ice_skate = {"ice skate", 0x000026F8};
+const xtd::forms::emoticon xtd::forms::emoticons::person_with_ball = {"person with ball", 0x000026F9};
+const xtd::forms::emoticon xtd::forms::emoticons::tent = {"tent", 0x000026FA};
+const xtd::forms::emoticon xtd::forms::emoticons::fuel_pump = {"fuel pump", 0x000026FD};
+
 const std::vector<xtd::forms::emoticon>& xtd::forms::emoticons::get_emoticons() {
   static std::vector<xtd::forms::emoticon> emoticons {
     copyright_sign, registered_sign,
@@ -74,6 +159,7 @@ const std::vector<xtd::forms::emoticon>& xtd::forms::emoticons::get_emoticons() 
     watch, hourglass, keyboard, eject_symbol, black_right_pointing_double_triangle, black_left_pointing_double_triangle, black_up_pointing_double_triangle, black_down_pointing_double_triangle, black_right_pointing_double_triangle_with_vertical_bar, black_left_pointing_double_triangle_with_vertical_bar, black_right_pointing_triangle_with_double_vertical_bar, alarm_clock, stopwatch, timer_clock, hourglass_with_flowing_sand, double_vertical_bar, black_square_for_stop, black_circle_for_record,
     circled_latin_capital_letter_m,
     black_small_square, white_small_square, black_right_pointing_triangle, black_left_pointing_triangle, white_medium_square, black_medium_square, white_medium_small_square, black_medium_small_square,
+    black_sun_with_rays, cloud, umbrella, snowman, comet, black_telephone, ballot_box_with_check, umbrella_with_rain_drops, hot_beverage, shamrock, white_up_pointing_index, skull_and_crossbones, radioactive_sign, biohazard_sign, orthodox_cross, star_and_crescent, peace_symbol, yin_yang, wheel_of_dharma, white_frowning_face, white_smiling_face, female_sign, male_sign, aries, taurus, gemini, cancer, leo, virgo, libra, scorpius, sagittarius, capricorn, aquarius, pisces, black_chess_pawn, black_spade_suit, black_club_suit, black_heart_suit, black_diamond_suit, hot_springs, black_universal_recycling_symbol, permanent_paper_sign, wheelchair_symbol, hammer_and_pick, anchor, crossed_swords, staff_of_aesculapius, scales, alembic, gear, atom_symbol, fleur_de_lis, warning_sign, high_voltage_sign, medium_white_circle, medium_black_circle, coffin, funeral_urn, soccer_ball, baseball, snowman_without_snow, sun_behind_cloud, thunder_cloud_and_rain, ophiuchus, pick, helmet_with_white_cross, chains, no_entry, shinto_shrine, church, mountain, umbrella_on_ground, fountain, flag_in_hole, ferry, sailboat, skier, ice_skate, person_with_ball, tent, fuel_pump,
   };
   return emoticons;
 }
