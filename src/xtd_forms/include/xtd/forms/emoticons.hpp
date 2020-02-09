b6ae8cff8d45611f6aff0832464ac2c5446f70c9
Gammasoft71 <gammasoft71@gmail.com>
2020-02-09 20:14:32 +0100

    add emoticons

--- src/xtd_forms/include/xtd/forms/emoticons.hpp
+++ src/xtd_forms/include/xtd/forms/emoticons.hpp
@@ -14,6 +14,15 @@ namespace xtd {
       /// @brief Gets a system-defined emoticon that has a string value of {0x000000AE, 0x0000FE0F}. This field is constant.
       static const xtd::forms::emoticon registered_sign;
 
+      /// 2000-206F - General Punctuation
+      
+      /// @brief Gets a system-defined emoticon that has a string value of 0x0000200D. This field is constant.
+      static const xtd::forms::emoticon zero_width_joiner;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x0000203C, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon double_exclamation_mark;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x00002049, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon exclamation_question_mark;
+
       static const std::vector<xtd::forms::emoticon>& get_emoticons();
     };
   }
--- src/xtd_forms/src/xtd/forms/emoticons.cpp
+++ src/xtd_forms/src/xtd/forms/emoticons.cpp
@@ -4,10 +4,19 @@ using namespace xtd::forms;
 
 // 00080-00FF Latin 1 Supplement
 
-const xtd::forms::emoticon xtd::forms::emoticons::copyright_sign = {"copyright sign", {0x000000A9 , 0x0000FE0F }};
-const xtd::forms::emoticon xtd::forms::emoticons::registered_sign = {"registered sign", {0x000000AE , 0x0000FE0F }};
+const xtd::forms::emoticon xtd::forms::emoticons::copyright_sign = {"copyright sign", {0x000000A9, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::registered_sign = {"registered sign", {0x000000AE, 0x0000FE0F}};
+
+/// 2000-206F - General Punctuation
+
+const xtd::forms::emoticon xtd::forms::emoticons::zero_width_joiner = {"zero width joiner", 0x0000200D};
+const xtd::forms::emoticon xtd::forms::emoticons::double_exclamation_mark = {"double exclamation mark", {0x0000203C, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::exclamation_question_mark = {"exclamation question mark", {0x00002049, 0x0000FE0F}};
 
 const std::vector<xtd::forms::emoticon>& xtd::forms::emoticons::get_emoticons() {
-  static std::vector<xtd::forms::emoticon> emoticons {copyright_sign, registered_sign, };
+  static std::vector<xtd::forms::emoticon> emoticons {
+    copyright_sign, registered_sign,
+    double_exclamation_mark, exclamation_question_mark,
+  };
   return emoticons;
 }
