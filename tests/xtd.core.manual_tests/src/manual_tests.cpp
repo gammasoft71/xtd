f596fcaa7a14a53a4c52aad38e234974dcf46db0
Gammasoft71 <gammasoft71@gmail.com>
2020-02-09 01:21:13 +0100

    Update manual_tests.cpp

--- tests/xtd.core.manual_tests/src/manual_tests.cpp
+++ tests/xtd.core.manual_tests/src/manual_tests.cpp
@@ -6,24 +6,25 @@
 using namespace std;
 using namespace string_literals;
 using namespace xtd;
+using namespace xtd::io;
+
+string to_variable(const string& name) {
+  return strings::replace(strings::replace(strings::replace(strings::replace(strings::replace(strings::replace(strings::replace(strings::replace(strings::replace(strings::replace(strings::replace(strings::trim(strings::to_lower(name)), " ", "_"), "(", ""), ")", ""), "'", "_"), "ô", "o"), "é", "e"), "Å", "a"), ",", ""), "ç", "c"), "-", "_"), ".", "");
+}
 
 // The main entry point for the application.
 int main() {
   //cout << "Hello, World!" << endl;
-  cout << "command line = " << environment::command_line() << environment::new_line;
-  cout << "Current directory = " << environment::current_directory() << environment::new_line;
-  cout << "Current thread id = " << environment::current_thread_id() << environment::new_line;
-  cout << "Machine name = " << environment::machine_name() << environment::new_line;
-  cout << "Os version = " << environment::os_version() << environment::new_line;
-  cout << "Processor count = " << environment::processor_count() << environment::new_line;
-  cout << "System page size = " << environment::system_page_size() << environment::new_line;
-  cout << "System directory = " << environment::system_directory() << environment::new_line;
-  cout << "User domain name = " << environment::user_domain_name() << environment::new_line;
-  cout << "User name = " << environment::user_name() << environment::new_line;
-  cout << "Tick count = " << environment::tick_count() << environment::new_line;
-  environment::set_environment_variable("ARROW", "Oliver Queen");
-  cout << environment::expand_environment_variables("Arrow is '%ARROW%'.") << environment::new_line;
-  environment::exit(42);
+  auto lines = file::read_all_lines("/Users/yves/Desktop/countries.txt");
+  const vector<string> enclosed_letters {u8"\\U0001F1E6", u8"\\U0001F1E7", u8"\\U0001F1E8", u8"\\U0001F1E9", u8"\\U0001F1EA", u8"\\U0001F1EB", u8"\\U0001F1EC", u8"\\U0001F1ED", u8"\\U0001F1EE", u8"\\U0001F1EF", u8"\\U0001F1F0", u8"\\U0001F1F1", u8"\\U0001F1F2", u8"\\U0001F1F3", u8"\\U0001F1F4", u8"\\U0001F1F5", u8"\\U0001F1F6", u8"\\U0001F1F7", u8"\\U0001F1F8", u8"\\U0001F1F9", u8"\\U0001F1FA", u8"\\U0001F1FB", u8"\\U0001F1FC", u8"\\U0001F1FD", u8"\\U0001F1FE", u8"\\U0001F1FF"};
+  for(auto index = 0; index < lines.size(); index += 1) {
+    //cout << format("      static const xtd::forms::emoticon {};\n", to_variable(lines[index]));
+    //cout << format("const xtd::forms::emoticon xtd::forms::emoticons::{} = {{\"{}\", \"{}\"}};\n", to_variable(lines[index]), lines[index], strings::replace(lines[index + 1], "U+", "\\U000"));
+    //cout << format("{}, ", to_variable(lines[index]));
+    auto items = strings::split(strings::trim(lines[index]), {'\t'});
+    //cout << format("const xtd::forms::emoticon xtd::forms::emoticons::flag_of_{} = {{\"{}\", \"{}\"}};\n", to_variable(items[0]), items[0], enclosed_letters[items[1][0] - 'A'] + enclosed_letters[items[1][1] - 'A']);
+    cout << format("flag_of_{}, ", to_variable(items[0]));
+  }
 }
 
 // This code produces the following output with colors:
