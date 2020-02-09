dfe3704fcd48adc89a4c56666dd5018dc2be1e5e
Gammasoft71 <gammasoft71@gmail.com>
2020-02-09 20:35:32 +0100

    add emoticons

--- src/xtd_forms/include/xtd/forms/emoticons.hpp
+++ src/xtd_forms/include/xtd/forms/emoticons.hpp
@@ -29,6 +29,24 @@ namespace xtd {
       static const xtd::forms::emoticon trade_mark_sign;
       /// @brief Gets a system-defined emoticon that has a string value of {0x00002139, 0x0000FE0F}. This field is constant.
       static const xtd::forms::emoticon information_source;
+      /// 2190-21FF Arrows
+      
+      /// @brief Gets a system-defined emoticon that has a string value of {0x00002194, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon left_right_arrow;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x00002195, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon up_down_arrow;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x00002196, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon north_west_arrow;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x00002197, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon north_east_arrow;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x00002198, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon south_east_arrow;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x00002199, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon south_west_arrow;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x000021A9, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon leftwards_arrow_with_hook;
+      /// @brief Gets a system-defined emoticon that has a string value of {0x000021AA, 0x0000FE0F}. This field is constant.
+      static const xtd::forms::emoticon rightwards_arrow_with_hook;
 
       static const std::vector<xtd::forms::emoticon>& get_emoticons();
     };
--- src/xtd_forms/src/xtd/forms/emoticons.cpp
+++ src/xtd_forms/src/xtd/forms/emoticons.cpp
@@ -18,11 +18,23 @@ const xtd::forms::emoticon xtd::forms::emoticons::exclamation_question_mark = {"
 const xtd::forms::emoticon xtd::forms::emoticons::trade_mark_sign = {"trade mark sign", {0x00002122, 0x0000FE0F}};
 const xtd::forms::emoticon xtd::forms::emoticons::information_source = {"information source", {0x00002139, 0x0000FE0F}};
 
+/// 2190-21FF Arrows
+
+const xtd::forms::emoticon xtd::forms::emoticons::left_right_arrow = {"left right arrow", {0x00002194, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::up_down_arrow = {"up down arrow", {0x00002195, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::north_west_arrow = {"north west arrow", {0x00002196, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::north_east_arrow = {"north east arrow", {0x00002197, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::south_east_arrow = {"south east arrow", {0x00002198, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::south_west_arrow = {"south west arrow", {0x00002199, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::leftwards_arrow_with_hook = {"leftwards arrow with hook", {0x000021A9, 0x0000FE0F}};
+const xtd::forms::emoticon xtd::forms::emoticons::rightwards_arrow_with_hook = {"rightwards arrow with hook", {0x000021AA, 0x0000FE0F}};
+
 const std::vector<xtd::forms::emoticon>& xtd::forms::emoticons::get_emoticons() {
   static std::vector<xtd::forms::emoticon> emoticons {
     copyright_sign, registered_sign,
     double_exclamation_mark, exclamation_question_mark,
     trade_mark_sign, information_source,
+    left_right_arrow, up_down_arrow, north_west_arrow, north_east_arrow, south_east_arrow, south_west_arrow, leftwards_arrow_with_hook, rightwards_arrow_with_hook,
   };
   return emoticons;
 }
