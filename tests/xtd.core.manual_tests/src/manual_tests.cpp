8c6dbcb396268f0e6e0d5e421590f58f2e1a65fe
Gammasoft71 <gammasoft71@gmail.com>
2020-02-10 00:32:12 +0100

    Update manual_tests.cpp

--- tests/xtd.core.manual_tests/src/manual_tests.cpp
+++ tests/xtd.core.manual_tests/src/manual_tests.cpp
@@ -9,7 +9,7 @@ using namespace xtd;
 using namespace xtd::io;
 
 string to_variable(const string& name) {
-  return strings::replace(strings::replace(strings::replace(strings::replace(strings::replace(strings::replace(strings::replace(strings::replace(strings::replace(strings::replace(strings::replace(strings::trim(strings::to_lower(name)), " ", "_"), "(", ""), ")", ""), "'", "_"), "ô", "o"), "é", "e"), "Å", "a"), ",", ""), "ç", "c"), "-", "_"), ".", "");
+  return strings::replace(strings::replace(strings::replace(strings::replace(strings::replace(strings::replace(strings::replace(strings::replace(strings::replace(strings::replace(strings::replace(strings::replace(strings::trim(strings::to_lower(name)), " ", "_"), "(", ""), ")", ""), "'", "_"), "ô", "o"), "é", "e"), "Å", "a"), ",", ""), "ç", "c"), "-", "_"), ".", ""), ":", "");
 }
 
 string to_value(const string& value) {
@@ -85,13 +85,39 @@ void generate_flags_vector_items(const std::string& filename) {
 int main() {
   //cout << "Hello, World!" << endl;
   
-  generate_header_file("/Users/yves/Projects/xtd/src/xtd_forms/docs/Emoticons/01 - 00080-00FF Latin 1 Supplement.txt");
-  //generate_source_file("/Users/yves/Projects/xtd/src/xtd_forms/docs/Emoticons/01 - 00080-00FF Latin 1 Supplement.txt");
-  //generate_vector_items("/Users/yves/Projects/xtd/src/xtd_forms/docs/Emoticons/01 - 00080-00FF Latin 1 Supplement.txt");
+  // auto filename = "/Users/yves/Projects/xtd/src/xtd_forms/docs/Emoticons/01 - 00080-00FF Latin 1 Supplement.txt";
+  // auto filename = "/Users/yves/Projects/xtd/src/xtd_forms/docs/Emoticons/02 - 2000-206F - General Punctuation.txt";
+  // auto filename = "/Users/yves/Projects/xtd/src/xtd_forms/docs/Emoticons/03 - 2100-214F Letterlike Symbols.txt";
+  // auto filename = "/Users/yves/Projects/xtd/src/xtd_forms/docs/Emoticons/04 - 2190-21FF Arrows.txt";
+  // auto filename = "/Users/yves/Projects/xtd/src/xtd_forms/docs/Emoticons/05 - 2300-23FF Miscellaneous Technical.txt";
+  // auto filename = "/Users/yves/Projects/xtd/src/xtd_forms/docs/Emoticons/06 - 2460-24FF Enclosed Alphanumerics.txt";
+  // auto filename = "/Users/yves/Projects/xtd/src/xtd_forms/docs/Emoticons/07 - 25A0 25FF Geometric Shapes.txt";
+  // auto filename = "/Users/yves/Projects/xtd/src/xtd_forms/docs/Emoticons/08 - 2600-26FF Miscellaneous Symbols.txt";
+  // auto filename = "/Users/yves/Projects/xtd/src/xtd_forms/docs/Emoticons/09 - 2700-27BF Dingbats.txt";
+  // auto filename = "/Users/yves/Projects/xtd/src/xtd_forms/docs/Emoticons/10 - 2900-297F Supplemental Arrows-B.txt";
+  // auto filename = "/Users/yves/Projects/xtd/src/xtd_forms/docs/Emoticons/11 - 2B00-2BFF Miscellaneous Symbols And Arrows.txt";
+  // auto filename = "/Users/yves/Projects/xtd/src/xtd_forms/docs/Emoticons/12 - 3000-303F CJK Symbols and Punctuation.txt";
+  // auto filename = "/Users/yves/Projects/xtd/src/xtd_forms/docs/Emoticons/13 - 3200-32FF Enclosed CJK Letters and Months.txt";
+  // auto filename = "/Users/yves/Projects/xtd/src/xtd_forms/docs/Emoticons/14 - 1F000-1F02F Mahjong Tiles.txt";
+  // auto filename = "/Users/yves/Projects/xtd/src/xtd_forms/docs/Emoticons/15 - 1F0A0-1F0FF Playing Cards.txt";
+  // auto filename = "/Users/yves/Projects/xtd/src/xtd_forms/docs/Emoticons/16 - 1F100-1F1FF Enclosed Alphanumeric Supplement.txt";
+  // auto filename = "/Users/yves/Projects/xtd/src/xtd_forms/docs/Emoticons/17 - 1F200-1F2FF Enclosed Ideographic Supplement.txt";
+  // auto filename = "/Users/yves/Projects/xtd/src/xtd_forms/docs/Emoticons/18 - 1F300-1F5FF symbols_and_pictographs.txt";
+  // auto filename = "/Users/yves/Projects/xtd/src/xtd_forms/docs/Emoticons/19 - 1F600-1F64F Emoticons (Emoji).txt";
+  // auto filename = "/Users/yves/Projects/xtd/src/xtd_forms/docs/Emoticons/20 - 1F680-1F6FF Transport And Map Symbols.txt";
+  // auto filename = "/Users/yves/Projects/xtd/src/xtd_forms/docs/Emoticons/21 - 1F780-1F7FF Geometric Shapes Extended.txt";
+  // auto filename = "/Users/yves/Projects/xtd/src/xtd_forms/docs/Emoticons/22 - 1F900-1F9FF Supplemental Symbols and Pictographs.txt";
+  //auto filename = "/Users/yves/Projects/xtd/src/xtd_forms/docs/Emoticons/23 - 1FA70-1F7FF Symbols and Pictographs Extended-A.txt";
+  auto filename = "/Users/yves/Projects/xtd/src/xtd_forms/docs/Emoticons/24 - Emoji ZWJ Sequences.txt";
+
+  generate_header_file(filename);
+  //generate_source_file(filename);
+  //generate_vector_items(filename);
   
-  //generate_flags_header_file("/Users/yves/Projects/xtd/src/xtd_forms/docs/Emoticons/25 - Country Flags.txt");
-  //generate_flags_source_file("/Users/yves/Projects/xtd/src/xtd_forms/docs/Emoticons/25 - Country Flags.txt");
-  //generate_flags_vector_items("/Users/yves/Projects/xtd/src/xtd_forms/docs/Emoticons/25 - Country Flags.txt");
+  //auto filename = "/Users/yves/Projects/xtd/src/xtd_forms/docs/Emoticons/25 - Country Flags.txt";
+  //generate_flags_header_file(filename);
+  //generate_flags_source_file(filename);
+  //generate_flags_vector_items(filename);
   
 }
 
