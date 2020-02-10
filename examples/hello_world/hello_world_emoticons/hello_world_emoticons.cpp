f615818f5b844c83864433e60beaa640194f2465
Gammasoft71 <gammasoft71@gmail.com>
2020-02-10 12:53:28 +0100

    add hello_world_emoticons example

---
+++ examples/hello_world/hello_world_emoticons/hello_world_emoticons.cpp
@@ -0,0 +1,28 @@
+#include <xtd/xtd.forms>
+
+using namespace xtd;
+using namespace xtd::drawing;
+using namespace xtd::forms;
+
+namespace examples {
+  class form1 : public form {
+  public:
+    form1() {
+      text("\"Hello, World!\" with emoticons");
+      client_size({300, 300});
+      controls().push_back(label);
+
+      label.auto_size(true);
+      label.font(drawing::font(label.font(), 48));
+      label.location({5, 100});
+      label.text(emoticons::waving_hand_sign + ", " + emoticons::earth_globe_americas + emoticons::heavy_exclamation_mark_symbol);
+    }
+    
+  private:
+    forms::label label;
+  };
+}
+
+int main() {
+  application::run(examples::form1());
+}
