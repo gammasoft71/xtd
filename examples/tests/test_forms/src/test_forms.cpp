937cee835677bab314eb2c0fbb5c16c8629ba6a9
Gammasoft71 <gammasoft71@gmail.com>
2020-02-10 21:05:07 +0100

    Update test_forms.cpp

--- examples/tests/test_forms/src/test_forms.cpp
+++ examples/tests/test_forms/src/test_forms.cpp
@@ -6,13 +6,13 @@ using namespace xtd::forms;
 
 int main() {
   auto main_form = control::create<form>("Test Gui");
-  main_form->client_size({360, 270});
+  main_form->client_size({390, 270});
   
   auto emoticons_list_box = control::create<list_box>(*main_form, "", {10, 10}, {150, 250});
   for (const auto& emoticon : emoticons::get_emoticons())
     emoticons_list_box->items().push_back({strings::format("{} {}", emoticon, emoticon.name()), emoticon});
 
-  auto emoticon_label = control::create<label>(*main_form, "", {170, 30});
+  auto emoticon_label = control::create<label>(*main_form, "", {170, 20});
   emoticon_label->auto_size(true).font({main_form->font(), 128});
 
   emoticons_list_box->selected_value_changed += [&] {
