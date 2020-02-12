cdeace21cd02736db6c2ca4b579f1a81847b7262
Gammasoft71 <gammasoft71@gmail.com>
2020-02-12 18:30:24 +0100

    Update country.hpp

--- src/xtd_forms/include/xtd/forms/country.hpp
+++ src/xtd_forms/include/xtd/forms/country.hpp
@@ -16,7 +16,6 @@ namespace xtd {
     public:
       country() = default;
       /// @cond
-      virtual ~country() = default;
       bool operator==(const country& value) const {return name_ == value.name_ && alpha_2_code_ == value.alpha_2_code_ && alpha_3_code_ == value.alpha_3_code_ && numeric_code_ == value.numeric_code_;}
       bool operator!=(const country& value) const {return !operator==(value);}
       /// @endcond
