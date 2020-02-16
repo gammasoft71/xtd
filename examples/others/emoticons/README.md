41c26d206580ee80a37d7456c14dad314f6a2b2f
Gammasoft71 <gammasoft71@gmail.com>
2020-02-16 14:40:31 +0100

    add settings class

---
+++ examples/others/emoticons/README.md
@@ -0,0 +1,35 @@
+# emoticons
+
+demonstrates the use of [xtd::forms::emoticons](../../../src/xtd_forms/include/xtd/forms/emoticons.hpp) control.
+
+# Sources
+
+[emoticons.cpp](emoticons.cpp)
+
+[CMakeLists.txt](CMakeLists.txt)
+
+# Build and run
+
+Open "Command Prompt" or "Terminal". Navigate to the folder that contains the project and type the following:
+
+```shell
+xtd run
+```
+
+# Output
+
+## Windows :
+
+![Screenshot](../../../docs/pictures/examples/emoticons_w.png)
+
+## macOS :
+
+![Screenshot](../../../docs/pictures/examples/emoticons_m.png)
+
+![Screenshot](../../../docs/pictures/examples/emoticons_md.png)
+
+## Gnome :
+
+![Screenshot](../../../docs/pictures/examples/emoticons_g.png)
+
+![Screenshot](../../../docs/pictures/examples/emoticons_gd.png)
--- examples/tests/test_forms/src/test_forms.cpp
+++ examples/tests/test_forms/src/test_forms.cpp
@@ -1,26 +1,41 @@
 #include <xtd/xtd.forms>
 
-using namespace std;
 using namespace xtd;
 using namespace xtd::forms;
 
 int main() {
+  settings settings;
   auto main_form = control::create<form>("Test Gui");
-  main_form->client_size({390, 270});
-  
-  auto emoticons_list_box = control::create<list_box>(*main_form, "", {10, 10}, {150, 250});
-  for (const auto& emoticon : emoticons::get_emoticons())
-    emoticons_list_box->items().push_back({strings::format("{} {}", emoticon, emoticon.name()), emoticon});
+  main_form->start_position(form_start_position::manual);
 
-  auto emoticon_label = control::create<label>(*main_form, "", {170, 20});
-  emoticon_label->auto_size(true).font({main_form->font(), 128});
-
-  emoticons_list_box->selected_value_changed += [&] {
-    emoticon_label->text(any_cast<emoticon>(emoticons_list_box->selected_item().tag()).to_string());
+  auto color_picker = control::create<forms::color_picker>(*main_form, "", {10, 10}, {75, 25});
+  color_picker->color_changed += [&] {
+    main_form->back_color(color_picker->color());
   };
-  emoticons_list_box->selected_index(0);
 
-  //emoticon_label->text(emoticon("test", {0x1F486, 0x1F3FD, 0x200D, 0x2640, 0xFE0F}).to_string());
+  auto save_button = control::create<button>(*main_form, "&Save", {90, 10});
+  save_button->click += [&] {
+    settings.write("client_size", main_form->client_size());
+    settings.write("location", main_form->location());
+    settings.write("back_color", main_form->back_color());
+    settings.save();
+  };
+
+  auto reload_button = control::create<button>(*main_form, "&Reload", {170, 10});
+  reload_button->click += [&] {
+    main_form->client_size(settings.read("client_size", drawing::size {360, 80}));
+    main_form->location(settings.read("location", drawing::point {100, 50}));
+    main_form->back_color(settings.read("back_color", main_form->default_back_color()));
+    color_picker->color(main_form->back_color());
+  };
+
+  auto reset_button = control::create<button>(*main_form, "R&eset", {250, 10});
+  reset_button->click += [&] {
+    settings.reset();
+    reload_button->perform_click();
+  };
+  
+  reload_button->perform_click();
 
   application::run(*main_form);
 }
