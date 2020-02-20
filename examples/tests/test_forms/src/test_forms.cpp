ec0351b00cfad53917b430a43d11683a5c031010
Gammasoft71 <gammasoft71@gmail.com>
2020-02-20 15:03:36 +0100

    add xtd CMake helpers

--- examples/tests/test_forms/src/test_forms.cpp
+++ examples/tests/test_forms/src/test_forms.cpp
@@ -4,6 +4,9 @@ using namespace xtd;
 using namespace xtd::forms;
 
 int main() {
+  cdebug << format("install path = {}", __CMAKE_INSTALL_PREFIX__) << std::endl;
+  cdebug << format("xtd include path = {}", __XTD_INCLUDE_PATH__) << std::endl;
+  cdebug << format("xtd lib path = {}", __XTD_LIB_PATH__) << std::endl;
   auto main_form = control::create<form>("Test Gui");
   application::run(*main_form);
 }
