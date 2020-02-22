e7d390195e579adc8612c4e7dca661f7c4856a87
Gammasoft71 <gammasoft71@gmail.com>
2020-02-22 15:27:23 +0100

    add resources and settings

---
+++ examples/games/minesweeper/properties/resources.cmake
@@ -0,0 +1,6 @@
+resource(flag resources/flag.png)
+resource(mine resources/mine.png)
+resource(smiley1 resources/smiley1.png)
+resource(smiley2 resources/smiley2.png)
+resource(smiley3 resources/smiley3.png)
+resource(smiley4 resources/smiley4.png)
