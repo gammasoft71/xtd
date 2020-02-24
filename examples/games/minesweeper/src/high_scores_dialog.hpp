1c64a73f818ced495a29ba47693f069b92365019
Gammasoft71 <gammasoft71@gmail.com>
2020-02-24 22:40:08 +0100

    Update high_scores_dialog.hpp

--- examples/games/minesweeper/src/high_scores_dialog.hpp
+++ examples/games/minesweeper/src/high_scores_dialog.hpp
@@ -8,69 +8,32 @@ namespace minesweeper {
   public:
     high_scores_dialog() {
       using namespace xtd::forms;
-      start_position(form_start_position::center_parent);
-      minimize_box(false);
-      maximize_box(false);
-      close_box(false);
-      control_box(false);
-      form_border_style(xtd::forms::form_border_style::none);
-      client_size({360, 170});
+      start_position(form_start_position::center_parent).minimize_box(false).maximize_box(false).close_box(false).control_box(false).form_border_style(xtd::forms::form_border_style::none).client_size({360, 200});
 
-      beginner_level_label_.parent(*this);
-      beginner_level_label_.location({10, 25});
-      beginner_level_label_.auto_size(true);
-      beginner_level_label_.text("Beginner");
-      
-      intermediate_level_label_.parent(*this);
-      intermediate_level_label_.location({10, 55});
-      intermediate_level_label_.auto_size(true);
-      intermediate_level_label_.text("Intermediate");
-      
-      expert_level_label_.parent(*this);
-      expert_level_label_.location({10, 85});
-      expert_level_label_.auto_size(true);
-      expert_level_label_.text("Expert");
+      title_.text_align(content_alignment::middle_center).location({10, 15}).width(340).text("Fastest Mine Sweepers");
 
-      beginner_score_label_.parent(*this);
-      beginner_score_label_.location({130, 25});
-      beginner_score_label_.auto_size(true);
-      
-      intermediate_score_label_.parent(*this);
-      intermediate_score_label_.location({130, 55});
-      intermediate_score_label_.auto_size(true);
-      
-      expert_score_label_.parent(*this);
-      expert_score_label_.location({130, 85});
-      expert_score_label_.auto_size(true);
+      beginner_level_label_.location({10, 55}).auto_size(true).text("Beginner");
+      intermediate_level_label_.location({10, 85}).auto_size(true).text("Intermediate");
+      expert_level_label_.location({10, 115}).auto_size(true).text("Expert");
 
-      beginner_name_label_.parent(*this);
-      beginner_name_label_.location({250, 25});
-      beginner_name_label_.auto_size(true);
-      
-      intermediate_name_label_.parent(*this);
-      intermediate_name_label_.location({250, 55});
-      intermediate_name_label_.auto_size(true);
-      
-      expert_name_label_.parent(*this);
-      expert_name_label_.location({250, 85});
-      expert_name_label_.auto_size(true);
+      beginner_score_label_.location({130, 55}).auto_size(true);
+      intermediate_score_label_.location({130, 85}).auto_size(true);
+      expert_score_label_.location({130, 115}).auto_size(true);
 
-      reset_scores_button_.parent(*this);
-      reset_scores_button_.location({70, 120});
-      reset_scores_button_.width(100);
-      reset_scores_button_.text("&Reset Scores");
-      reset_scores_button_.click += [&] {
+      beginner_name_label_.location({250, 55}).auto_size(true);
+      intermediate_name_label_.location({250, 85}).auto_size(true);
+      expert_name_label_.location({250, 115}).auto_size(true);
+
+      reset_scores_button_.location({70, 150}).width(100).text("&Reset Scores").click += [&] {
         properties::settings::default_settings().reset();
         read_and_update_settings();
       };
 
-      ok_button_.parent(*this);
-      ok_button_.location({190, 120});
-      ok_button_.width(100);
-      ok_button_.text(xtd::forms::texts::ok);
-      ok_button_.dialog_result(dialog_result::ok);
-      accept_button(ok_button_);
+      ok_button_.dialog_result(dialog_result::ok).location({190, 150}).width(100).text(xtd::forms::texts::ok);
 
+      controls().push_back_range({title_, beginner_level_label_, intermediate_level_label_, expert_level_label_, beginner_score_label_, intermediate_score_label_, expert_score_label_, beginner_name_label_, intermediate_name_label_, expert_name_label_, reset_scores_button_, ok_button_});
+      accept_button(ok_button_);
+      
       read_and_update_settings();
     }
     
@@ -86,6 +49,7 @@ namespace minesweeper {
       expert_name_label_.text(properties::settings::default_settings().expert_high_scores_name());
     }
     
+    xtd::forms::label title_;
     xtd::forms::label beginner_level_label_;
     xtd::forms::label intermediate_level_label_;
     xtd::forms::label expert_level_label_;
