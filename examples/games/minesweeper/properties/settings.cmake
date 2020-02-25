f8b86b1c070e3c569a7d6e46aba1632c26830410
Gammasoft71 <gammasoft71@gmail.com>
2020-02-25 11:15:33 +0100

    Update settings.cmake

--- examples/games/minesweeper/properties/settings.cmake
+++ examples/games/minesweeper/properties/settings.cmake
@@ -2,8 +2,7 @@
 # =============
 
 # if "true", minesweeper use original color otherwise, minesweeper use system color.
-setting(original_color bool USER "true")
-
+setting(original_color bool USER "false")
 setting(marks bool USER "true")
 
 # minesweeper::level :
@@ -13,15 +12,19 @@ setting(marks bool USER "true")
 # "3" : minesweeper::level::custom
 setting(level int USER "0")
 
-setting(custom_height int USER "16")
-setting(custom_width int USER "30")
-setting(custom_mines int USER "99")
-
+# Beginner level settings :
 setting(beginner_high_scores_value int USER "999")
 setting(beginner_high_scores_name std::string USER "\"Anonymous\"")
 
+# Intermediate level settings :
 setting(intermediate_high_scores_value int USER "999")
 setting(intermediate_high_scores_name std::string USER "\"Anonymous\"")
 
+# Expert level settings :
 setting(expert_high_scores_value int USER "999")
 setting(expert_high_scores_name std::string USER "\"Anonymous\"")
+
+# Custom level settings :
+setting(custom_height int USER "16")
+setting(custom_width int USER "30")
+setting(custom_mines int USER "99")
