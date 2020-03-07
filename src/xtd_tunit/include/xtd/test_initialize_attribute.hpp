82f4c936fbb6e7e517c82c5b46a280c184c02a21
Gammasoft71 <gammasoft71@gmail.com>
2020-03-07 23:20:17 +0100

    Update test_initialize_attribute.hpp

--- src/xtd_tunit/include/xtd/test_initialize_attribute.hpp
+++ src/xtd_tunit/include/xtd/test_initialize_attribute.hpp
@@ -7,7 +7,7 @@
 namespace xtd {
   /// @brief The tunit namespace contains a unit test library.
   namespace tunit {
-    struct test_initialize_attribute {
+    class test_initialize_attribute {
     public:
       template<typename TestClass>
       test_initialize_attribute(const std::string& name, TestClass& test_class, void (*method)()) noexcept :  test_initialize_attribute(name, test_class, method, xtd::tunit::line_info()) {}
@@ -20,8 +20,9 @@ namespace xtd {
 
 #define test_initialize_(method_name) \
   __##method_name##_unused() = delete; \
-  struct __test_initialize_attribute : public xtd::tunit::test_initialize_attribute { \
-  template<typename test_class> __test_initialize_attribute(test_class& test) : test_initialize_attribute(#method_name, test, &test_class::method_name, {__func__, __FILE__, __LINE__}) {} \
+  class __test_initialize_attribute : public xtd::tunit::test_initialize_attribute { \
+  public:\
+    template<typename test_class> __test_initialize_attribute(test_class& test) : test_initialize_attribute(#method_name, test, &test_class::method_name, {__func__, __FILE__, __LINE__}) {} \
   } __test_initialize_attribute {*this}; \
   static void method_name()
 
