f8eaddc944809dcdd0bde2d8ac1dc75d208a00e3
Gammasoft71 <gammasoft71@gmail.com>
2020-03-07 23:20:15 +0100

    Update test_cleanup_attribute.hpp

--- src/xtd_tunit/include/xtd/test_cleanup_attribute.hpp
+++ src/xtd_tunit/include/xtd/test_cleanup_attribute.hpp
@@ -7,7 +7,7 @@
 namespace xtd {
   /// @brief The tunit namespace contains a unit test library.
   namespace tunit {
-    struct test_cleanup_attribute {
+    class test_cleanup_attribute {
     public:
       template<typename TestClass>
       test_cleanup_attribute(const std::string& name, TestClass& test_class, void (*method)()) noexcept :  test_cleanup_attribute(name, test_class, method, xtd::tunit::line_info()) {}
@@ -20,7 +20,8 @@ namespace xtd {
 
 #define test_cleanup_(method_name) \
   __##method_name##_unused() = delete; \
-  struct __test_cleanup_attribute : public xtd::tunit::test_cleanup_attribute { \
-  template<typename test_class> __test_cleanup_attribute(test_class& test) : test_cleanup_attribute(#method_name, test, &test_class::method_name, {__func__, __FILE__, __LINE__}) {} \
+  class __test_cleanup_attribute : public xtd::tunit::test_cleanup_attribute { \
+  public:\
+    template<typename test_class> __test_cleanup_attribute(test_class& test) : test_cleanup_attribute(#method_name, test, &test_class::method_name, {__func__, __FILE__, __LINE__}) {} \
   } __test_cleanup_attribute {*this}; \
   static void method_name()
