7ba6f665f3125ff81a18122d61e31f5f0a43bf7a
Gammasoft71 <gammasoft71@gmail.com>
2020-03-08 10:34:49 +0100

    Update manual_tests.cpp

--- tests/xtd.console.manual_tests/src/manual_tests.cpp
+++ tests/xtd.console.manual_tests/src/manual_tests.cpp
@@ -9,13 +9,13 @@ bool is_windows_gui_app(const std::filesystem::path& app_path) {
   auto bytes = xtd::io::file::read_all_bytes(app_path);
   // read pe format : https://docs.microsoft.com/en-us/windows/win32/debug/pe-format?redirectedfrom=MSDN#machine_type
   if (bytes[0] != 'M' || bytes[1] != 'Z') return false;
-  // ...nexts
-  console::write_line("pe offset = 0x{:X4}", bit_converter::to_int16(bytes, 0x3C));
-  return true;
+  auto subsystem_offset = bit_converter::to_uint16(bytes, 0x3C) + 92;
+
+  return bit_converter::to_uint16(bytes, subsystem_offset) == 2;
 }
 
 // The main entry point for the application.
 int main() {
-  //console::write_line("is_windows_gui_app(\"/Users/yves/Projects/console.exe\") = {}", is_windows_gui_app("/Users/yves/Projects/console.exe"));
+  console::write_line("is_windows_gui_app(\"/Users/yves/Projects/console.exe\") = {}", is_windows_gui_app("/Users/yves/Projects/console.exe"));
   console::write_line("is_windows_gui_app(\"/Users/yves/Projects/gui.exe\") = {}", is_windows_gui_app("/Users/yves/Projects/gui.exe"));
 }
