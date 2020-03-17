67958bbf800342ae313d1a4e2b286868008d9395
Gammasoft71 <gammasoft71@gmail.com>
2020-03-17 22:44:44 +0100

    Update xtd_commands.cmake

--- scripts/cmake/xtd_commands.cmake
+++ scripts/cmake/xtd_commands.cmake
@@ -1895,7 +1895,7 @@ endif()
 ################################################################################
 # Reference Guide Generator
 
-option(ADD_COMMAND_REFERENCE_GUIDE_GENERATOR "Add command Referecne Guide Generator" OFF)
+option(ADD_COMMAND_REFERENCE_GUIDE_GENERATOR "Add command Referecne Guide Generator" ON)
 option(DOWNLOAD_DOXYGEN "Download and build doxygen from Github" OFF)
 
 if (ADD_COMMAND_REFERENCE_GUIDE_GENERATOR AND NOT DOXYGEN_FOUND)  
