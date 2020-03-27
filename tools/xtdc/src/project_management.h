40962c225680216595cd0843480f324e45aeb926
Gammasoft71 <gammasoft71@gmail.com>
2020-03-27 14:14:33 +0100

    Update project_management.h

--- tools/xtdc/src/project_management.h
+++ tools/xtdc/src/project_management.h
@@ -2025,13 +2025,13 @@ namespace xtdc_command {
     }
 
     void create_objectivec_shared_library(const std::string& name, project_sdk sdk, project_language language, bool create_solution) const {
-      std::filesystem::create_directories(path_/name/"include");
-      std::filesystem::create_directories(path_/name/"src");
-      create_objectivec_shared_library_solution_cmakelists_txt(name);
-      create_objectivec_shared_library_cmakelists_txt(name);
-      create_objectivec_shared_library_header(name);
-      create_objectivec_shared_library_export(name);
-      create_objectivec_shared_library_source(name);
+      std::filesystem::create_directories(create_solution ? path_/name/"include" : path_/"include");
+      std::filesystem::create_directories(create_solution ? path_/name/"src" : path_/"src");
+      if (create_solution) create_objectivec_shared_library_solution_cmakelists_txt(name);
+      create_objectivec_shared_library_cmakelists_txt(name, create_solution ? path_/name : path_);
+      create_objectivec_shared_library_header(name, create_solution ? path_/name : path_);
+      create_objectivec_shared_library_export(name, create_solution ? path_/name : path_);
+      create_objectivec_shared_library_source(name, create_solution ? path_/name : path_);
     }
     
     void create_objectivec_shared_library_solution_cmakelists_txt(const std::string& name) const {
@@ -2045,7 +2045,7 @@ namespace xtdc_command {
       xtd::io::file::write_all_lines(path_/"CMakeLists.txt", lines);
     }
     
-    void create_objectivec_shared_library_cmakelists_txt(const std::string& name) const {
+    void create_objectivec_shared_library_cmakelists_txt(const std::string& name, const std::filesystem::path& path) const {
       std::vector<std::string> lines {
         "cmake_minimum_required(VERSION 3.8)",
         "",
@@ -2088,7 +2088,7 @@ namespace xtdc_command {
       xtd::io::file::write_all_lines(path_/name/"CMakeLists.txt", lines);
     }
     
-    void create_objectivec_shared_library_export(const std::string& name) const {
+    void create_objectivec_shared_library_export(const std::string& name, const std::filesystem::path& path) const {
       std::vector<std::string> lines {
         "#pragma once",
         xtd::strings::format("#if defined({0}_EXPORT)", xtd::strings::to_upper(name)),
@@ -2098,10 +2098,10 @@ namespace xtdc_command {
         "#endif",
       };
       
-      xtd::io::file::write_all_lines(path_/name/"include"/"Export.h", lines);
+      xtd::io::file::write_all_lines(path/"include"/"Export.h", lines);
     }
     
-    void create_objectivec_shared_library_header(const std::string& name) const {
+    void create_objectivec_shared_library_header(const std::string& name, const std::filesystem::path& path) const {
       std::vector<std::string> lines {
         "#import \"Export.h\"",
         "#import <Foundation/Foundation.h>",
@@ -2111,10 +2111,10 @@ namespace xtdc_command {
         "@end",
       };
       
-      xtd::io::file::write_all_lines(path_/name/"include"/"Class1.h", lines);
+      xtd::io::file::write_all_lines(path/"include"/"Class1.h", lines);
     }
 
-    void create_objectivec_shared_library_source(const std::string& name) const {
+    void create_objectivec_shared_library_source(const std::string& name, const std::filesystem::path& path) const {
       std::vector<std::string> lines {
         "#import \"../include/Class1.h\"",
         "",
@@ -2123,7 +2123,7 @@ namespace xtdc_command {
         "@end"
       };
       
-      xtd::io::file::write_all_lines(path_/name/"src"/"Class1.m", lines);
+      xtd::io::file::write_all_lines(path/"src"/"Class1.m", lines);
     }
 
     void create_xtd_shared_library(const std::string& name, project_sdk sdk, project_language language, bool create_solution) const {
