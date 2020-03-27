03beb1f57020e20febb4325c22c4c36e60ebd35a
Gammasoft71 <gammasoft71@gmail.com>
2020-03-27 22:50:10 +0100

    Update main_form.h

--- tools/xtdc-gui/src/main_form.h
+++ tools/xtdc-gui/src/main_form.h
@@ -1,7 +1,12 @@
 /// @file
 /// @brief Contains main_form class.
 #pragma once
+#include <xtd/forms/button.h>
+#include <xtd/forms/choice.h>
 #include <xtd/forms/form.h>
+#include <xtd/forms/label.h>
+#include <xtd/forms/list_box.h>
+#include <xtd/forms/panel.h>
 
 namespace xtdc_gui {
   /// @brief Represents the main form
@@ -12,5 +17,22 @@ namespace xtdc_gui {
 
     // The main entry point for the application.
     static void main();
+    
+  private:
+    xtd::forms::panel create_panel_;
+    xtd::forms::label create_title_label_;
+    xtd::forms::label recent_project_title_label_;
+    xtd::forms::list_box recent_projects_list_box_;
+    xtd::forms::choice language_choice_;
+    xtd::forms::choice platform_choice_;
+    xtd::forms::choice template_choice_;
+    xtd::forms::panel items_panel_;
+
+    xtd::forms::panel configure_panel_;
+    xtd::forms::label configure_title_label_;
+    xtd::forms::label project_type_title_label_;
+
+    xtd::forms::button cancel_button_;
+    xtd::forms::button create_button_;
   };
 }
