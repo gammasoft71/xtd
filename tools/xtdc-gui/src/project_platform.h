8a54810b290f2297268882b49d3c0150a14cf434
Gammasoft71 <gammasoft71@gmail.com>
2020-03-31 15:02:28 +0200

    Update project_platform.h

--- tools/xtdc-gui/src/project_platform.h
+++ tools/xtdc-gui/src/project_platform.h
@@ -3,8 +3,22 @@
 namespace xtdc_gui {
   enum class project_platform {
     windows = 0b1,
-    linux = 0b01,
-    macos = 0b001,
+    linux = 0b10,
+    macos = 0b100,
     all = windows|linux|macos,
   };
+
+  /// @cond
+  inline project_platform& operator +=(project_platform& lhs, project_platform rhs) {lhs = static_cast<project_platform>(static_cast<long long>(lhs) + static_cast<long long>(rhs)); return lhs;}
+  inline project_platform& operator -=(project_platform& lhs, project_platform rhs) {lhs = static_cast<project_platform>(static_cast<long long>(lhs) - static_cast<long long>(rhs)); return lhs;}
+  inline project_platform& operator &=(project_platform& lhs, project_platform rhs) {lhs = static_cast<project_platform>(static_cast<long long>(lhs) & static_cast<long long>(rhs)); return lhs;}
+  inline project_platform& operator |=(project_platform& lhs, project_platform rhs) {lhs = static_cast<project_platform>(static_cast<long long>(lhs) | static_cast<long long>(rhs)); return lhs;}
+  inline project_platform& operator ^=(project_platform& lhs, project_platform rhs) {lhs = static_cast<project_platform>(static_cast<long long>(lhs) ^ static_cast<long long>(rhs)); return lhs;}
+  inline project_platform operator +(project_platform lhs, project_platform rhs) {return static_cast<project_platform>(static_cast<long long>(lhs) + static_cast<long long>(rhs));}
+  inline project_platform operator -(project_platform lhs, project_platform rhs) {return static_cast<project_platform>(static_cast<long long>(lhs) - static_cast<long long>(rhs));}
+  inline project_platform operator ~(project_platform rhs) {return static_cast<project_platform>(~static_cast<long long>(rhs));}
+  inline project_platform operator &(project_platform lhs, project_platform rhs) {return static_cast<project_platform>(static_cast<long long>(lhs) & static_cast<long long>(rhs));}
+  inline project_platform operator |(project_platform lhs, project_platform rhs) {return static_cast<project_platform>(static_cast<long long>(lhs) | static_cast<long long>(rhs));}
+  inline project_platform operator ^(project_platform lhs, project_platform rhs) {return static_cast<project_platform>(static_cast<long long>(lhs) ^ static_cast<long long>(rhs));}
+  /// @endcond
 }
