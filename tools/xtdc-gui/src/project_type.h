98708f39aafe7bc79e0da6f98a199d81a20dad60
Gammasoft71 <gammasoft71@gmail.com>
2020-03-31 15:02:33 +0200

    Update project_type.h

--- tools/xtdc-gui/src/project_type.h
+++ tools/xtdc-gui/src/project_type.h
@@ -3,11 +3,25 @@
 namespace xtdc_gui {
   enum class project_type {
     gui = 0b1,
-    console = 0b01,
-    shared_library = 0b001,
-    static_library = 0b0001,
-    unit_tests_project = 0b00001,
-    solution_file = 0b000001,
+    console = 0b10,
+    shared_library = 0b100,
+    static_library = 0b1000,
+    unit_tests_project = 0b10000,
+    solution_file = 0b100000,
     all = gui|console|shared_library|static_library|unit_tests_project|solution_file,
   };
+
+  /// @cond
+  inline project_type& operator +=(project_type& lhs, project_type rhs) {lhs = static_cast<project_type>(static_cast<long long>(lhs) + static_cast<long long>(rhs)); return lhs;}
+  inline project_type& operator -=(project_type& lhs, project_type rhs) {lhs = static_cast<project_type>(static_cast<long long>(lhs) - static_cast<long long>(rhs)); return lhs;}
+  inline project_type& operator &=(project_type& lhs, project_type rhs) {lhs = static_cast<project_type>(static_cast<long long>(lhs) & static_cast<long long>(rhs)); return lhs;}
+  inline project_type& operator |=(project_type& lhs, project_type rhs) {lhs = static_cast<project_type>(static_cast<long long>(lhs) | static_cast<long long>(rhs)); return lhs;}
+  inline project_type& operator ^=(project_type& lhs, project_type rhs) {lhs = static_cast<project_type>(static_cast<long long>(lhs) ^ static_cast<long long>(rhs)); return lhs;}
+  inline project_type operator +(project_type lhs, project_type rhs) {return static_cast<project_type>(static_cast<long long>(lhs) + static_cast<long long>(rhs));}
+  inline project_type operator -(project_type lhs, project_type rhs) {return static_cast<project_type>(static_cast<long long>(lhs) - static_cast<long long>(rhs));}
+  inline project_type operator ~(project_type rhs) {return static_cast<project_type>(~static_cast<long long>(rhs));}
+  inline project_type operator &(project_type lhs, project_type rhs) {return static_cast<project_type>(static_cast<long long>(lhs) & static_cast<long long>(rhs));}
+  inline project_type operator |(project_type lhs, project_type rhs) {return static_cast<project_type>(static_cast<long long>(lhs) | static_cast<long long>(rhs));}
+  inline project_type operator ^(project_type lhs, project_type rhs) {return static_cast<project_type>(static_cast<long long>(lhs) ^ static_cast<long long>(rhs));}
+  /// @endcond
 }
