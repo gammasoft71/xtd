1af045872606ab6de3ead60a28ba28f4aa464d1b
Gammasoft71 <gammasoft71@gmail.com>
2020-04-17 11:39:02 +0200

    Update application.mm

--- src/xtd_forms_native_wxwidgets/src/xtd/forms/native/wxwidgets/application.mm
+++ src/xtd_forms_native_wxwidgets/src/xtd/forms/native/wxwidgets/application.mm
@@ -1,11 +1,11 @@
 #if defined(__APPLE__)
 #include <Quartz/Quartz.h>
 
-void __xtd_enable_dark_mode__() {
+void __xtd_osx_enable_dark_mode__() {
   [NSApp setAppearance:[NSAppearance appearanceNamed:NSAppearanceNameDarkAqua]];
 }
 
-void __xtd_enable_light_mode__() {
+void __xtd_osx_enable_light_mode__() {
   [NSApp setAppearance:[NSAppearance appearanceNamed:NSAppearanceNameAqua]];
 }
 #endif
