de7f44952e51e7705ed11dfa428a6d7c15426f67
Gammasoft71 <gammasoft71@gmail.com>
2020-04-17 21:41:03 +0200

    enable dark theme

--- src/xtd_forms_native_wxwidgets/src/xtd/forms/native/wxwidgets/about_dialog.cpp
+++ src/xtd_forms_native_wxwidgets/src/xtd/forms/native/wxwidgets/about_dialog.cpp
@@ -1,9 +1,26 @@
 #include <xtd/forms/native/about_dialog.h>
 #include "../../../../../include/xtd/forms/native/wxwidgets/control_handler.h"
+#include "../../../../../include/xtd/forms/native/wxwidgets/dark_mode.h"
 #include <wx/aboutdlg.h>
 
 using namespace xtd;
 using namespace xtd::forms::native;
+
+#if defined(__WXMSW__)
+namespace {
+  HHOOK handle_hook;
+  LRESULT CALLBACK callbackProc(INT ncode, WPARAM wparam, LPARAM lparam) {
+    if (ncode == HCBT_ACTIVATE) {
+      allow_dark_mode_for_window(static_cast<intptr_t>(wparam));
+      refresh_title_bar_theme_color(static_cast<intptr_t>(wparam));
+      UnhookWindowsHookEx(handle_hook);
+    }
+    else
+      CallNextHookEx(handle_hook, ncode, wparam, lparam);
+    return 0;
+  }
+}
+#endif
 
 bool about_dialog::run_dialog(intptr_t hwnd, const std::string& name, const std::string& description, const std::string& version, const std::string& long_version, const std::string& copyright) {
   wxAboutDialogInfo about_info;
@@ -11,6 +28,9 @@ bool about_dialog::run_dialog(intptr_t hwnd, const std::string& name, const std:
   about_info.SetDescription(description);
   about_info.SetVersion(version, long_version);
   about_info.SetCopyright(copyright);
+#if defined(__WXMSW__)
+  handle_hook = SetWindowsHookExW(WH_CBT, &callbackProc, 0, GetCurrentThreadId());
+#endif
   wxAboutBox(about_info, hwnd ? reinterpret_cast<control_handler*>(hwnd)->control() : nullptr);
   return true;
 }
--- src/xtd_forms_native_wxwidgets/src/xtd/forms/native/wxwidgets/color_dialog.cpp
+++ src/xtd_forms_native_wxwidgets/src/xtd/forms/native/wxwidgets/color_dialog.cpp
@@ -1,9 +1,26 @@
 #include <xtd/forms/native/color_dialog.h>
 #include <xtd/forms/color_dialog_flags.h>
 #include <wx/colordlg.h>
+#include "../../../../../include/xtd/forms/native/wxwidgets/dark_mode.h"
 
 using namespace xtd;
 using namespace xtd::forms::native;
+
+#if defined(__WXMSW__)
+namespace {
+  HHOOK handle_hook;
+  LRESULT CALLBACK callbackProc(INT ncode, WPARAM wparam, LPARAM lparam) {
+    if (ncode == HCBT_ACTIVATE) {
+      allow_dark_mode_for_window(static_cast<intptr_t>(wparam));
+      refresh_title_bar_theme_color(static_cast<intptr_t>(wparam));
+      UnhookWindowsHookEx(handle_hook);
+    }
+    else
+      CallNextHookEx(handle_hook, ncode, wparam, lparam);
+    return 0;
+  }
+}
+#endif
 
 bool color_dialog::run_dialog(intptr_t hwnd, drawing::color& color, std::optional<std::vector<int32_t>>& custom_colors, size_t options) {
   wxColourData color_data;
@@ -17,6 +34,9 @@ bool color_dialog::run_dialog(intptr_t hwnd, drawing::color& color, std::optiona
     for(size_t index = 0; index < max_index; ++index)
       color_data.SetCustomColour(index, wxColour(custom_colors.value()[index]));
   }
+#if defined(__WXMSW__)
+  handle_hook = SetWindowsHookExW(WH_CBT, &callbackProc, 0, GetCurrentThreadId());
+#endif
   wxColourDialog dialog(reinterpret_cast<wxWindow*>(hwnd), &color_data);
   if (dialog.ShowModal() != wxID_OK) return false;
   wxColour colour = dialog.GetColourData().GetColour();
