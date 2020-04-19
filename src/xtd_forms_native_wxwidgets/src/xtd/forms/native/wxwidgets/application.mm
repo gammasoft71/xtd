c11a08716ae488211a9324562786b4f66571f585
Gammasoft71 <gammasoft71@gmail.com>
2020-04-19 16:40:10 +0200

    Update application.mm

--- src/xtd_forms_native_wxwidgets/src/xtd/forms/native/wxwidgets/application.mm
+++ src/xtd_forms_native_wxwidgets/src/xtd/forms/native/wxwidgets/application.mm
@@ -10,8 +10,8 @@ void __xtd_osx_enable_light_mode__() {
 }
 
 bool __xtd_osx_dark_mode_enabled__() {
-  //return [[[NSAppearance currentAppearance] name] isEqualToString:(NSAppearanceNameDarkAqua)];
-  return [[[NSApp appearance] name] isEqualToString:(NSAppearanceNameDarkAqua)];
+  //return [[[NSApp appearance] name] isEqualToString:(NSAppearanceNameDarkAqua)];
+  return [[[NSAppearance currentAppearance] name] isEqualToString:(NSAppearanceNameDarkAqua)];
 }
 
 #endif
