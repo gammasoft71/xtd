4ccfcbe30c0aa42c417f8c254713f951118c84ae
Gammasoft71 <gammasoft71@gmail.com>
2020-04-26 00:46:14 +0200

    add wnd_proc example

---
+++ examples/events/wnd_proc/README.md
@@ -0,0 +1,37 @@
+# wnd_proc
+
+demonstrates the use of [wnd_proc](../src/xtd_forms/include/xtd/forms/control.hpp) method..
+
+# Sources
+
+[src/wnd_proc.cpp](src/wnd_proc.cpp)
+
+[CMakeLists.txt](CMakeLists.txt)
+
+# Build and run
+
+Open "Command Prompt" or "Terminal". Navigate to the folder that contains the project and type the following:
+
+```shell
+xtd run
+```
+
+# Output
+
+## Windows :
+
+![Screenshot](../../../docs/pictures/examples/wnd_proc_w.png)
+
+![Screenshot](../../../docs/pictures/examples/wnd_proc_wd.png)
+
+## macOS :
+
+![Screenshot](../../../docs/pictures/examples/wnd_proc_m.png)
+
+![Screenshot](../../../docs/pictures/examples/wnd_proc_md.png)
+
+## Gnome :
+
+![Screenshot](../../../docs/pictures/examples/wnd_proc_g.png)
+
+![Screenshot](../../../docs/pictures/examples/wnd_proc_gd.png)
---
+++ examples/events/wnd_proc/src/wnd_proc.cpp
@@ -0,0 +1,49 @@
+#include <xtd/xtd.forms>
+
+using namespace xtd;
+using namespace xtd::drawing;
+using namespace xtd::forms;
+
+namespace examples {
+  class form1 : public form {
+  public:
+    form1() {
+      client_size({300, 300});
+      text("form1");
+      font({font().font_family(), 18.0f, drawing::font_style::bold, drawing::graphics_unit::point, 0});
+    }
+
+  protected:
+    void on_paint(paint_event_args& e) override {
+      // Paint a string in different styles depending on whether the application is active.
+      if (app_active) {
+        e.graphics().fill_rectangle(solid_brush(system_colors::menu_highlight()), 10, 10, 280, 50);
+        e.graphics().draw_string("Application is active", font(), solid_brush(system_colors::control_text()), 10, 10);
+      } else {
+        e.graphics().fill_rectangle(solid_brush(system_colors::control()), 10, 10, 280, 50);
+        e.graphics().draw_string("Application is Inactive", font(), solid_brush(system_colors::control_text()), 10, 10);
+      }
+    }
+    
+    void wnd_proc(message& m) override {
+      // Listen for operating system messages.
+      switch (m.msg()) {
+          // The WM_ACTIVATEAPP message occurs when the application becomes the active application or becomes inactive.
+        case WM_ACTIVATEAPP:
+          // The wparam value identifies what is occurring.
+          app_active = ((static_cast<int>(m.wparam()) != 0));
+          // Invalidate to get new text painted.
+          invalidate();
+          break;
+      }
+      form::wnd_proc(m);
+    }
+
+  private:
+    bool app_active = true;
+  };
+}
+
+int main() {
+  application::run(examples::form1());
+}
