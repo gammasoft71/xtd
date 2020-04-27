6c2f284b01e9674595265cd49ee39c04b9cfdfb3
Gammasoft71 <gammasoft71@gmail.com>
2020-04-27 20:39:20 +0200

    add system_brushes and system_pens classes

--- examples/events/form_paint/src/form_paint.cpp
+++ examples/events/form_paint/src/form_paint.cpp
@@ -19,7 +19,7 @@ namespace examples {
         e.graphics().draw_line(pen(color::light_steel_blue, 5), 20, 60, 260, 60);
         e.graphics().fill_rectangle(brushes::light_blue(), 50, 300, 400, 50);
         e.graphics().draw_line(pens::red(), 0, 0, 1, 1);
-        e.graphics().draw_string("Draw string", drawing::font("Arial", 34, font_style::regular), solid_brush(color::light_green), 20.0f, 0.0f);
+        e.graphics().draw_string("Draw string", drawing::font("Arial", 34, font_style::regular), brushes::light_green(), 20.0f, 0.0f);
         e.graphics().fill_rectangle(brushes::light_sea_green(), 400, 70, 100, 200);
         e.graphics().draw_arc(pen(color::black, 10), 400, 70, 100, 200, 45, 270);
         e.graphics().draw_ellipse(pen(color::red, 10), 100, 80, 200, 200);
