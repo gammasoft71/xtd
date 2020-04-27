cd4430a670627473a15df371860137bfe8b84f55
Gammasoft71 <gammasoft71@gmail.com>
2020-04-27 14:45:25 +0200

    Update hello_world_paint.cpp

--- examples/hello_world_examples/hello_world_paint/src/hello_world_paint.cpp
+++ examples/hello_world_examples/hello_world_paint/src/hello_world_paint.cpp
@@ -1,6 +1,5 @@
 #include <xtd/xtd.forms>
 
-using namespace std;
 using namespace xtd;
 using namespace xtd::drawing;
 using namespace xtd::forms;
@@ -13,7 +12,7 @@ namespace examples {
       client_size({300, 300});
 
       paint += [&](control& sender, paint_event_args& e) {
-        e.graphics().draw_string("Hello World!", {font(), 32, font_style::bold | font_style::italic}, xtd::drawing::solid_brush(color::blue), e.clip_rectangle(), xtd::drawing::string_format().alignment(xtd::drawing::string_alignment::center).line_alignment(xtd::drawing::string_alignment::center));
+        e.graphics().draw_string("Hello World!", {font(), 32, font_style::bold | font_style::italic}, brushes::blue(), e.clip_rectangle(), string_format().alignment(string_alignment::center).line_alignment(string_alignment::center));
       };
     }
   };
