7aefa349ab46e6e9b3d22e65e77d2cb130b47d6b
Gammasoft71 <gammasoft71@gmail.com>
2020-04-27 12:08:46 +0200

    add font_changed_event example

---
+++ examples/events/font_changed_event/README.md
@@ -0,0 +1,37 @@
+# font_changed_event
+
+demonstrates the use of [idle](../../../src/xtd_forms/include/xtd/forms/application.hpp) event.
+
+# Sources
+
+[src/font_changed_event.cpp](src/font_changed_event.cpp)
+
+[CMakeLists.txt](CMakeLists.txt)
+
+# Build and run
+
+Open "Command Prompt" or "Terminal". Navigate to the folder that contains the project and type the following:
+
+```shell
+xtd run
+```
+
+# Output
+
+## Windows :
+
+![Screenshot](../../../docs/pictures/examples/font_changed_event_w.png)
+
+![Screenshot](../../../docs/pictures/examples/font_changed_event_wd.png)
+
+## macOS :
+
+![Screenshot](../../../docs/pictures/examples/font_changed_event_m.png)
+
+![Screenshot](../../../docs/pictures/examples/font_changed_event_md.png)
+
+## Gnome :
+
+![Screenshot](../../../docs/pictures/examples/font_changed_event_g.png)
+
+![Screenshot](../../../docs/pictures/examples/font_changed_event_gd.png)
---
+++ examples/events/font_changed_event/src/font_changed_event.cpp
@@ -0,0 +1,43 @@
+#include <xtd/xtd.forms>
+
+using namespace xtd;
+using namespace xtd::forms;
+
+namespace examples {
+  class form1 : public form {
+  public:
+    form1() {
+      radio_button1.location({0, 0});
+      radio_button1.name("radio_button1");
+      radio_button1.size({150, 25});
+      radio_button1.text("button1");
+      radio_button1.font_changed += {*this, &form1::radio_button1_font_changed};
+
+      radio_button2.location({0, 39});
+      radio_button2.name("radio_button2");
+      radio_button2.size({150, 17});
+      radio_button2.text("Change button1 font");
+      radio_button2.checked_changed += {*this, &form1::radio_button2_checked_changed};
+
+      client_size({292, 273});
+      controls().push_back_range({radio_button1, radio_button2});
+      name("Form1");
+    }
+    
+  private:
+    void radio_button1_font_changed(const control& sender, const event_args& e) {
+      message_box::show("The font has been chenged.");
+    }
+
+    void radio_button2_checked_changed(const control& sender, const event_args& e) {
+      radio_button1.font({radio_button1.font().font_family(), 15.0f, drawing::font_style::bold|drawing::font_style::italic, drawing::graphics_unit::point, 0});
+    }
+    
+    radio_button radio_button1;
+    radio_button radio_button2;
+  };
+}
+
+int main() {
+  application::run(examples::form1());
+}
