a9ef995edf2939eed1530b6ae0fc5e78b71ae714
Gammasoft71 <gammasoft71@gmail.com>
2020-05-01 19:00:25 +0200

    Update manual_tests.cpp

--- tests/xtd.manual_tests/src/manual_tests.cpp
+++ tests/xtd.manual_tests/src/manual_tests.cpp
@@ -6,6 +6,7 @@ using namespace xtd;
 using namespace xtd::drawing;
 using namespace xtd::forms;
 
+/*
 int main() {
   picture_box picture_box_icon;
   picture_box_icon.image(icon(error_m_icon).to_bitmap()).size_mode(picture_box_size_mode::center_image).border_style(forms::border_style::fixed_3d).location({10, 50}).size({100, 100}).back_color(system_colors::window());
@@ -26,8 +27,8 @@ int main() {
   
   application::run(form_main);
 }
+*/
 
-/*
 int main() {
   panel panel_cursor;
   panel_cursor.border_style(forms::border_style::fixed_3d).location({10, 50}).size({100, 100}).back_color(system_colors::window());
@@ -38,7 +39,7 @@ int main() {
     open_file_dialog dialog;
     dialog.filter("Image file|*.icns;*.ico;*.bmp;*.png");
     if (dialog.show_dialog() == dialog_result::ok) {
-      panel_cursor.cursor(cursor::from_bitmap(bitmap(dialog.file_name()), {0, 15}));
+      panel_cursor.cursor(cursor::from_bitmap(bitmap(dialog.file_name())));
     }
   };
   
@@ -48,4 +49,3 @@ int main() {
   
   application::run(form_main);
 }
- */
