1de0d9175fdbfd5699001920f15d4485fa3d46ee
Gammasoft71 <gammasoft71@gmail.com>
2020-05-03 16:38:34 +0200

    add cursors

--- src/xtd_forms/include/xtd/forms/properties/resources.h
+++ src/xtd_forms/include/xtd/forms/properties/resources.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <xtd/environment.h>
 #include "../control.h"
 
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
@@ -8,6 +9,19 @@ namespace xtd {
     /// @brief A strongly-typed resource class, for looking up localized strings, etc.
     class resources static_ {
     public:
+      /// @brief Looks up a localized resource of type xtd::drawing::bitmap.
+      static const xtd::drawing::bitmap& cursor_arrow() {
+        if (!xtd::io::file::exists(xtd::io::path::combine(forms_resource_path_, "cursors", xtd::strings::format("arrow{}.png", get_os_potfix)))) return xtd::drawing::bitmap::empty;
+        static xtd::drawing::bitmap bitmap(xtd::io::path::combine(forms_resource_path_, "cursors", xtd::strings::format("arrow{}.png", get_os_potfix)));
+        return bitmap;
+      }
+      
+      static const xtd::drawing::bitmap& cursor_no_move_2d() {
+        if (!xtd::io::file::exists(xtd::io::path::combine(forms_resource_path_, "cursors", xtd::strings::format("cursor_no_move_2d{}.png", get_os_potfix)))) return xtd::drawing::bitmap::empty;
+        static xtd::drawing::bitmap bitmap(xtd::io::path::combine(forms_resource_path_, "cursors", xtd::strings::format("cursor_no_move_2d{}.png", get_os_potfix)));
+        return bitmap;
+      }
+      
       /// @brief Looks up a localized resource of type xtd::drawing::bitmap.
       static const xtd::drawing::bitmap& gammasoft() {
         if (!xtd::io::file::exists(xtd::io::path::combine(forms_resource_path_, "pictures", "gammasoft.png"))) return xtd::drawing::bitmap::empty;
@@ -37,6 +51,7 @@ namespace xtd {
       }
 
     private:
+      static std::string get_os_potfix() noexcept {return xtd::environment::os_version().is_windows_platform() ? "_w" : xtd::environment::os_version().is_linux_platform() ? "_g" : xtd::environment::os_version().is_osx_platform() ? "_m" : "";}
 #if defined(__XTD_FORMS_RESOURCES_PATH__)
       static constexpr const char* forms_resource_path_ = __XTD_FORMS_RESOURCES_PATH__;
 #else
