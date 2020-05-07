54c891a1922c5280b2f4a6a08f901afb37d23b26
Gammasoft71 <gammasoft71@gmail.com>
2020-05-07 23:02:39 +0200

    update cursors

--- examples/common_controls/text_box/src/text_box.cpp
+++ examples/common_controls/text_box/src/text_box.cpp
@@ -17,6 +17,7 @@ namespace examples {
 
       text_box2.parent(*this);
       text_box2.location({10, 50});
+      text_box2.cursor(cursors::ibeam());
       text_box2.text_changed += [&] {
         text_box1.text(text_box2.text());
       };
@@ -31,5 +32,6 @@ namespace examples {
 }
 
 int main() {
+  application::enable_light_mode();
   application::run(examples::form1());
 }
