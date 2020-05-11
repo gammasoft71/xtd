872dc9f0cdf0303bc4eb5da186b00799f0adcc78
Gammasoft71 <gammasoft71@gmail.com>
2020-05-12 00:18:24 +0200

    Update wiggly.cpp

--- examples/others/wiggly/src/wiggly.cpp
+++ examples/others/wiggly/src/wiggly.cpp
@@ -10,6 +10,7 @@ namespace examples {
   public:
     wiggly() {
       back_color(system_colors::window());
+      font({font(), font().size() + 12});
       timer.interval(60);
       timer.tick += [&] {
         step++;
@@ -20,17 +21,17 @@ namespace examples {
 
   protected:
     void on_paint(paint_event_args& e) override {
-      point pos((e.clip_rectangle().size().width() - static_cast<int>(e.graphics().measure_string(text(), font()).width())) / 2, (e.clip_rectangle().size().height() - static_cast<int>(e.graphics().measure_string(text(), font()).height())) / 2);
+      static const vector sins = {0, 38, 71, 92, 100, 92, 71, 38, 0, -38, -71, -92, -100, -92, -71, -38};
+      auto pos = point {(e.clip_rectangle().size().width() - static_cast<int>(e.graphics().measure_string(text(), font()).width())) / 2, (e.clip_rectangle().size().height() - static_cast<int>(e.graphics().measure_string(text(), font()).height())) / 2};
       for (auto i = 0; i < text().length(); i++) {
-        auto index = (step + i) % sin.size();
-        e.graphics().draw_string(strings::format("{}", text()[i]), font(), solid_brush(color::from_hsb(static_cast<float>(15 - index) * 16 / 255 * 360, 1.0f, 0.75f)), point(pos.x(), pos.y() + sin[index] * (e.clip_rectangle().height() - font().height()) / 400));
+        auto index = (step + i) % sins.size();
+        e.graphics().draw_string(strings::format("{}", text()[i]), font(), solid_brush {color::from_hsb(360.0f / sins.size() * index, 1.0f, 0.75f)}, point::subtract(pos, {0, sins[index] * font().height() / 400}));
         pos.x(pos.x() + static_cast<int>(e.graphics().measure_string(strings::format("{}", text()[i]), font()).width()));
       }
     }
     
   private:
     forms::timer timer;
-    const vector<int> sin = {0, 38, 71, 92, 100, 92, 71, 38, 0, -38, -71, -92, -100, -92, -71, -38};
     int step = 0;
   };
 
@@ -52,7 +53,6 @@ namespace examples {
       
       wiggly.bounds({20, 20, 290, 60});
       wiggly.anchor(anchor_styles::top | anchor_styles::left | anchor_styles::right | anchor_styles::bottom);
-      wiggly.font(drawing::font(wiggly.font(), 22.0f));
       wiggly.text(text_box.text());
     }
     
