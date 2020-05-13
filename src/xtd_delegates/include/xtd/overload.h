51d2af66e6bfec8800377393d7ad7be51bfd30f6
Gammasoft71 <gammasoft71@gmail.com>
2020-05-13 21:51:05 +0200

    Update overload.h

--- src/xtd_delegates/include/xtd/overload.h
+++ src/xtd_delegates/include/xtd/overload.h
@@ -2,33 +2,31 @@
 
 #include <functional>
 
-/// @cond
-template <typename... args_t>
-struct __non_const_overload__ {
-  template <typename result_t, typename type_t>
-  constexpr auto operator()(result_t (type_t::*method)(args_t...)) const noexcept -> decltype(method) {return method;}
-  template <typename result_t, typename type_t>
-  static constexpr auto of(result_t (type_t::*method)(args_t...)) noexcept -> decltype(method) {return method;}
-};
-
-template <typename... args_t>
-struct __const_overload__ {
-  template <typename result_t, typename type_t>
-  constexpr auto operator()(result_t (type_t::*method)(args_t...) const) const noexcept -> decltype(method) {return method;}
-  template <typename result_t, typename type_t>
-  static constexpr auto of(result_t (type_t::*method)(args_t...) const) noexcept -> decltype(method) {return method;}
-};
-/// @endcond
-
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
   template <typename... args_t>
-  struct overload : __const_overload__<args_t...>, __non_const_overload__<args_t...> {
+  struct non_const_overload {
+    template <typename result_t, typename type_t>
+    constexpr auto operator()(result_t (type_t::*method)(args_t...)) const noexcept -> decltype(method) {return method;}
+    template <typename result_t, typename type_t>
+    static constexpr auto of(result_t (type_t::*method)(args_t...)) noexcept -> decltype(method) {return method;}
+  };
+
+  template <typename... args_t>
+  struct const_overload {
+    template <typename result_t, typename type_t>
+    constexpr auto operator()(result_t (type_t::*method)(args_t...) const) const noexcept -> decltype(method) {return method;}
+    template <typename result_t, typename type_t>
+    static constexpr auto of(result_t (type_t::*method)(args_t...) const) noexcept -> decltype(method) {return method;}
+  };
+
+  template <typename... args_t>
+  struct overload : const_overload<args_t...>, non_const_overload<args_t...> {
     /// @cond
-    using __const_overload__<args_t...>::of;
-    using __const_overload__<args_t...>::operator();
-    using __non_const_overload__<args_t...>::of;
-    using __non_const_overload__<args_t...>::operator();
+    using const_overload<args_t...>::of;
+    using const_overload<args_t...>::operator();
+    using non_const_overload<args_t...>::of;
+    using non_const_overload<args_t...>::operator();
     /// @endcond
     
     template <typename result_t>
@@ -36,4 +34,21 @@ namespace xtd {
     template <typename result_t>
     static constexpr auto of(result_t (*method)(args_t...)) noexcept -> decltype(method) {return method;}
   };
+
+  /// @cond
+  template <typename... args_t>
+  inline constexpr overload<args_t...> overload_;
+
+  template <typename... args_t>
+  inline constexpr const_overload<args_t...> const_overload_;
+
+  template <typename... args_t>
+  inline constexpr non_const_overload<args_t...> non_const_overload_;
+  /// @endcond
 }
+
+#define overload_ xtd::overload_
+
+#define const_overload_ xtd::const_overload_
+
+#define non_const_overload_ xtd::non_const_overload_
