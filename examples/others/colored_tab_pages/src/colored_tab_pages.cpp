10ea142e31adc6065a179e4cc8d1eb255c3693c2
Gammasoft71 <gammasoft71@gmail.com>
2020-05-24 19:34:12 +0200

    fix colors

--- examples/others/colored_tab_pages/src/colored_tab_pages.cpp
+++ examples/others/colored_tab_pages/src/colored_tab_pages.cpp
@@ -11,27 +11,27 @@ public:
     client_size({390, 270});
     
     tab_control_colored.anchor(anchor_styles::left | anchor_styles::top | anchor_styles::right | anchor_styles::bottom);
-    tab_control_colored << tab_page_light_pink << tab_page_light_green << tab_page_light_blue << tab_page_light_yellow;
+    tab_control_colored << tab_page_light_red << tab_page_light_green << tab_page_light_blue << tab_page_light_yellow;
     tab_control_colored.location({10, 10});
     tab_control_colored.size({370, 250});
     tab_control_colored.alignment(tab_alignment::bottom);
 
-    tab_page_light_pink.text("Pink");
-    tab_page_light_pink.back_color(drawing::color::light_pink);
+    tab_page_light_red.text("Red");
+    tab_page_light_red.back_color(drawing::color::red);
 
     tab_page_light_green.text("Green");
-    tab_page_light_green.back_color(drawing::color::light_green);
+    tab_page_light_green.back_color(drawing::color::green);
 
     tab_page_light_blue.text("Blue");
-    tab_page_light_blue.back_color(drawing::color::light_blue);
+    tab_page_light_blue.back_color(drawing::color::blue);
 
     tab_page_light_yellow.text("Yellow");
-    tab_page_light_yellow.back_color(drawing::color::light_yellow);
+    tab_page_light_yellow.back_color(drawing::color::yellow);
   }
   
 private:
   tab_control tab_control_colored;
-  tab_page tab_page_light_pink;
+  tab_page tab_page_light_red;
   tab_page tab_page_light_green;
   tab_page tab_page_light_blue;
   tab_page tab_page_light_yellow;
