4016ec5e20c4d1ccdda45edf7a0f915057740a08
Gammasoft71 <gammasoft71@gmail.com>
2020-06-13 00:09:06 +0200

    add flag and flag_squared to contry and review contry example

--- examples/others/countries/src/countries.cpp
+++ examples/others/countries/src/countries.cpp
@@ -1,6 +1,8 @@
 #include <xtd/xtd.forms>
 
+using namespace std;
 using namespace xtd;
+using namespace xtd::drawing;
 using namespace xtd::forms;
 
 namespace examples {
@@ -11,24 +13,58 @@ namespace examples {
     }
 
     main_form() {
-      text("Countries example").client_size({560, 180});
-      auto name = [](const country& c) {return strings::format("{} ({})", c.emoticon(), c.name());};
-      button_france.text(name(countries::france())).tag(countries::france()).parent(*this).location({10, 10}).size({100, 25}).click += {*this, &main_form::on_button_click};
-      button_canada.text(name(countries::canada())).tag(countries::canada()).parent(*this).location({120, 10}).size({100, 25}).click += {*this, &main_form::on_button_click};
-      button_egypt.text(name(countries::egypt())).tag(countries::egypt()).parent(*this).location({230, 10}).size({100, 25}).click += {*this, &main_form::on_button_click};
-      button_japan.text(name(countries::japan())).tag(countries::japan()).parent(*this).location({340, 10}).size({100, 25}).click += {*this, &main_form::on_button_click};
-      button_australia.text(name(countries::australia())).tag(countries::australia()).parent(*this).location({450, 10}).size({100, 25}).click += {*this, &main_form::on_button_click};
-      informations.border_style(border_style::fixed_3d).text(strings::format("Name: {0}Emoticon: {0}Alpha code: {0}Numeric code:", '\n')).parent(*this).location({10, 45}).size({540, 125});
+      text("Countries example");
+      client_size({600, 500});
+      
+      countries_list_box.parent(*this);
+      countries_list_box.location({10, 10});
+      countries_list_box.size({200, 480});
+      countries_list_box.anchor(anchor_styles::left|anchor_styles::top|anchor_styles::bottom);
+      for(auto country : countries::get_countries())
+        countries_list_box.items().push_back({country.name(), country});
+      countries_list_box.selected_value_changed += [&] {
+        country_flag_picture_box.image(bitmap(any_cast<forms::country>(countries_list_box.selected_item().tag()).flag(), {256, 192}));
+        country_flag_squared_picture_box.image(bitmap(any_cast<forms::country>(countries_list_box.selected_item().tag()).flag_squared(), {128, 128}));
+        country_emoticon_label.text(any_cast<forms::country>(countries_list_box.selected_item().tag()).emoticon());
+        country_code_label.text(strings::format("Codes : {} ({}) / {}", any_cast<forms::country>(countries_list_box.selected_item().tag()).alpha_3_code(), any_cast<forms::country>(countries_list_box.selected_item().tag()).alpha_2_code(), any_cast<forms::country>(countries_list_box.selected_item().tag()).numeric_code()));
+      };
+      countries_list_box.selected_index(0);
+      
+      country_panel.parent(*this);
+      country_panel.location({220, 10});
+      country_panel.size({370, 480});
+      country_panel.anchor(anchor_styles::left|anchor_styles::top|anchor_styles::right|anchor_styles::bottom);
+      country_panel.border_style(forms::border_style::fixed_3d);
+      country_panel.back_color(system_colors::window());
+      country_panel.auto_scroll(true);
+      
+      country_flag_picture_box.parent(country_panel);
+      country_flag_picture_box.border_style(forms::border_style::fixed_3d);
+      country_flag_picture_box.location({10, 10});
+      country_flag_picture_box.size({256, 192});
+      
+      country_flag_squared_picture_box.parent(country_panel);
+      country_flag_squared_picture_box.border_style(forms::border_style::fixed_3d);
+      country_flag_squared_picture_box.location({10, 212});
+      country_flag_squared_picture_box.size({128, 128});
+      
+      country_emoticon_label.parent(country_panel);
+      country_emoticon_label.font({country_emoticon_label.font(), 64});
+      country_emoticon_label.location({10, 350});
+      country_emoticon_label.auto_size(true);
+      
+      country_code_label.parent(country_panel);
+      country_code_label.location({10, 445});
+      country_code_label.auto_size(true);
     }
     
   private:
-    void on_button_click(control& sender, const event_args& e) {
-      auto c = std::any_cast<country>(sender.tag());
-      informations.text(strings::format("Name: {1}{0}Emoticon: {2}{0}Alpha code: {3} ({4}){0}Numeric code: {5:D3}", '\n', c.name(), c.emoticon(), c.alpha_2_code(), c.alpha_3_code(), c.numeric_code()));
-    }
-    
-    button button_france, button_canada, button_egypt, button_japan, button_australia;
-    label informations;
+    list_box countries_list_box;
+    panel country_panel;
+    picture_box country_flag_picture_box;
+    picture_box country_flag_squared_picture_box;
+    label country_emoticon_label;
+    label country_code_label;
   };
 }
 
--- src/xtd_forms/src/xtd/forms/country.cpp
+++ src/xtd_forms/src/xtd/forms/country.cpp
@@ -1,13 +1,38 @@
 #include "../../../include/xtd/forms/country.h"
 #include "../../../include/xtd/forms/countries.h"
+#include <xtd/drawing/system_images.h>
+#include <filesystem>
 
 using namespace std;
+using namespace xtd;
+using namespace std::filesystem;
+using namespace xtd::drawing;
 using namespace xtd::forms;
+
+namespace {
+  static path country_images_resource_path() {
+#if defined(__XTD_FORMS_RESOURCES_PATH__)
+    return path(__XTD_FORMS_RESOURCES_PATH__)/"share"/"xtd"/"resources"/"countries";
+#else
+    return {};
+#endif
+  }
+}
 
 const vector<string> country::enclosed_letters {u8"\U0001F1E6", u8"\U0001F1E7", u8"\U0001F1E8", u8"\U0001F1E9", u8"\U0001F1EA", u8"\U0001F1EB", u8"\U0001F1EC", u8"\U0001F1ED", u8"\U0001F1EE", u8"\U0001F1EF", u8"\U0001F1F0", u8"\U0001F1F1", u8"\U0001F1F2", u8"\U0001F1F3", u8"\U0001F1F4", u8"\U0001F1F5", u8"\U0001F1F6", u8"\U0001F1F7", u8"\U0001F1F8", u8"\U0001F1F9", u8"\U0001F1FA", u8"\U0001F1FB", u8"\U0001F1FC", u8"\U0001F1FD", u8"\U0001F1FE", u8"\U0001F1FF"};
 
 const country country::empty() {
   return country();
+}
+
+const image country::flag() const {
+  auto filepath = country_images_resource_path()/(strings::to_lower(alpha_2_code()) + ".png");
+  if (!exists(filepath)) return image::empty;
+  return bitmap(filepath.string());
+}
+
+const image country::flag_squared() const {
+  return system_images::from_name(strings::format("flag-{}", strings::to_lower(alpha_2_code())), drawing::size {1024, 1024});
 }
 
 country country::from_name(const std::string& name) {
