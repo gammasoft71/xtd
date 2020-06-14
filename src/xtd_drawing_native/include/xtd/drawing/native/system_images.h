9b16c253c50de5276b3ff809d08daf6b1f8b3dd0
Gammasoft71 <gammasoft71@gmail.com>
2020-06-14 16:40:15 +0200

    add default_theme

--- src/xtd_drawing_native/include/xtd/drawing/native/system_images.h
+++ src/xtd_drawing_native/include/xtd/drawing/native/system_images.h
@@ -9,6 +9,7 @@ namespace xtd {
     namespace native {
       class system_images static_ {
       public:
+        static std::string default_theme();
         static intptr_t from_name(const std::string& name, int32_t width, int32_t height);
       };
     }
