207e8da36395f6cdc4876e2de1f85fb1bf57db6d
Gammasoft71 <gammasoft71@gmail.com>
2020-06-14 15:40:45 +0200

    fix build

--- src/xtd_drawing_native_wxwidgets/src/xtd/drawing/native/wxwidgets/system_images.cpp
+++ src/xtd_drawing_native_wxwidgets/src/xtd/drawing/native/wxwidgets/system_images.cpp
@@ -6,8 +6,7 @@
 #include <gtk/gtk.h>
 #endif
 #include <wx/image.h>
-#include <wx/palette.h>
-#include <wx/stream.h>
+#include <wx/bitmap.h>
 
 using namespace xtd;
 using namespace xtd::drawing::native;
@@ -20,7 +19,7 @@ intptr_t system_images::from_name(const std::string& name, int32_t width, int32_
 #if defined(__WXMSW__)
   return 0;
 #elif defined(__WXGTK__)
-  if (!icon) return 0;
+  if (name.empty()) return 0;
   auto icon_theme = gtk_icon_theme_get_default();
   const char* icon_names[] = {name.c_str(), nullptr};
   GtkIconInfo* iconInfo = gtk_icon_theme_choose_icon(icon_theme, icon_names, width, GtkIconLookupFlags::GTK_ICON_LOOKUP_FORCE_SIZE);
