b734e4bf91f20838092dc12fa9b8218ca587f229
Gammasoft71 <gammasoft71@gmail.com>
2020-07-19 14:33:35 +0200

    Update track_bar2.cpp

--- examples/others/track_bar2/src/track_bar2.cpp
+++ examples/others/track_bar2/src/track_bar2.cpp
@@ -1,6 +1,5 @@
 #include <xtd/xtd.forms>
 
-using namespace xtd;
 using namespace xtd::forms;
 
 class form1 : public form {
@@ -17,10 +16,10 @@ public:
       if (muted && track_bar1.value()) muted = false;
       if (!muted || track_bar1.value()) volume = track_bar1.value();
 
-      if (track_bar1.value() == 0) button1.image(button_images::from_name("audio-volume-muted", drawing::size {64, 64}));
-      else if (track_bar1.value() < 30) button1.image(button_images::from_name("audio-volume-low", drawing::size {64, 64}));
-      else if (track_bar1.value() < 80) button1.image(button_images::from_name("audio-volume-medium", drawing::size {64, 64}));
-      else button1.image(button_images::from_name("audio-volume-high", drawing::size {64, 64}));
+      if (track_bar1.value() == 0) button1.image(button_images::from_name("audio-volume-muted", xtd::drawing::size {64, 64}));
+      else if (track_bar1.value() < 30) button1.image(button_images::from_name("audio-volume-low", xtd::drawing::size {64, 64}));
+      else if (track_bar1.value() < 80) button1.image(button_images::from_name("audio-volume-medium", xtd::drawing::size {64, 64}));
+      else button1.image(button_images::from_name("audio-volume-high", xtd::drawing::size {64, 64}));
      };
     track_bar1.value(60);
 
