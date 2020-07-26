e425697a04b2d8052c3b5883963b92e06a23632e
Gammasoft71 <gammasoft71@gmail.com>
2020-07-26 15:21:12 +0200

    update xtd_forms icons and pictures

--- examples/applications/application_icon/src/application_icon.cpp
+++ examples/applications/application_icon/src/application_icon.cpp
@@ -1,10 +1,8 @@
 #include <xtd/xtd.forms>
-#include "../properties/resources.h"
 
 using namespace xtd::forms;
 
 int main() {
   auto main_form = control::create<form>("Application icon example");
-  main_form->icon(xtd::drawing::icon::from_bitmap(application_icon::properties::resources::application_icon()));
   xtd::forms::application::run(*main_form);
 }
