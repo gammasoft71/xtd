8fe0e93f96f537c10bb30f086775008a2c52e32f
Gammasoft71 <gammasoft71@gmail.com>
2020-07-31 23:54:50 +0200

    Update split_container.cpp

--- examples/containers/split_container/src/split_container.cpp
+++ examples/containers/split_container/src/split_container.cpp
@@ -1,6 +1,6 @@
 #include <xtd/xtd.forms>
 
-using namespace xtd;
+using namespace xtd::drawing;
 using namespace xtd::forms;
 
 class form1 : public form {
@@ -10,22 +10,19 @@ public:
     client_size({300, 300});
     
     split_container_left.parent(*this);
-    split_container_left.location({10, 10});
     split_container_left.dock(dock_style::fill);
-    split_container_left.panel1().back_color(drawing::color::light_blue);
+    split_container_left.panel1().back_color(color::light_blue);
     split_container_left.splitter_distance(70);
     
     split_container_right.dock(dock_style::fill);
-    split_container_right.location({10, 10});
-    split_container_right.panel2().back_color(drawing::color::light_yellow);
+    split_container_right.panel2().back_color(color::light_yellow);
     split_container_right.parent(split_container_left.panel2());
     split_container_right.splitter_distance(160);
     
     split_container_center.dock(dock_style::fill);
     split_container_center.orientation(xtd::forms::orientation::horzontal);
-    split_container_center.location({10, 10});
-    split_container_center.panel1().back_color(drawing::color::pink);
-    split_container_center.panel2().back_color(drawing::color::light_green);
+    split_container_center.panel1().back_color(color::pink);
+    split_container_center.panel2().back_color(color::light_green);
     split_container_center.parent(split_container_right.panel1());
     split_container_center.splitter_distance(150);
   }
