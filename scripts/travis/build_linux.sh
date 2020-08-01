53d958b1f9192446fb5f1a2474efd4aebf7ddb3b
Gammasoft71 <gammasoft71@gmail.com>
2020-08-01 17:16:36 +0200

    update projects

--- scripts/travis/build_linux.sh
+++ scripts/travis/build_linux.sh
@@ -4,14 +4,14 @@ mkdir -p build/examples
 cd build
 
 # generate and build lib
-cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_TESTS=ON -DCMAKE_CXX_COMPILER=g++-7 -DENABLE_TESTS=ON -DCMAKE_INSTALL_PREFIX=~/local ..
+cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_TESTS=ON -DCMAKE_CXX_COMPILER=g++-8 -DENABLE_TESTS=ON -DCMAKE_INSTALL_PREFIX=~/local ..
 if [ $? -ne 0 ]; then exit -1; fi
 cmake --build . --target install -- -j $(nproc)
 if [ $? -ne 0 ]; then exit -1; fi
 
 # generate and build examples
 cd examples
-cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=g++-7 -DCMAKE_INSTALL_PREFIX=~/local ../../examples
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=g++-8 -DCMAKE_INSTALL_PREFIX=~/local ../../examples
 if [ $? -ne 0 ]; then exit -1; fi
 cmake --build . -- -j $(nproc)
 if [ $? -ne 0 ]; then exit -1; fi
