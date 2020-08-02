8cfb3e043455ce8371fe83caee942ce566ffc6b0
Gammasoft71 <gammasoft71@gmail.com>
2020-08-02 13:45:09 +0200

    Create build.cmd

---
+++ scripts/appveyor/build.cmd
@@ -0,0 +1,23 @@
+:: cmd
+
+:: init and update submodule
+git submodule update --init
+
+mkdir build\examples
+cd build
+
+:: generate and build lib
+cmake .. -G "%BUILD_OPTION%" -DENABLE_TESTS=ON -DCMAKE_INSTALL_PREFIX=%HOMEPATH%/local
+if %ERRORLEVEL% NEQ 0 exit 1
+cmake --build . --config Debug --target install
+if %ERRORLEVEL% NEQ 0 exit 1
+
+:: generate and build examples
+cd examples
+cmake ../../examples -G "%BUILD_OPTION%" -DCMAKE_INSTALL_PREFIX=%HOMEPATH%/local
+if %ERRORLEVEL% NEQ 0 exit 1
+cmake --build . --config Debug
+if %ERRORLEVEL% NEQ 0 exit 1
+cd ..
+
+cd ..
