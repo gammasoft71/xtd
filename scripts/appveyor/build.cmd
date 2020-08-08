da737df5caaecad19e65812c95db18b9cc6adc8d
Gammasoft71 <gammasoft71@gmail.com>
2020-08-08 11:56:40 +0200

    Update build.cmd

--- scripts/appveyor/build.cmd
+++ scripts/appveyor/build.cmd
@@ -19,14 +19,14 @@ cd build
 
 :: generate and build lib
 git submodule update --init
-cmake .. -G "%BUILD_OPTION%" -DENABLE_TESTS=ON -DCMAKE_INSTALL_PREFIX=%HOMEPATH%/local
+cmake .. -G "%BUILD_OPTION%" -DENABLE_TESTS=ON
 if %ERRORLEVEL% NEQ 0 exit 1
 cmake --build . --config Debug --target install
 if %ERRORLEVEL% NEQ 0 exit 1
 
 :: generate and build examples
 cd examples
-cmake ../../examples -G "%BUILD_OPTION%" -DCMAKE_INSTALL_PREFIX=%HOMEPATH%/local
+cmake ../../examples -G "%BUILD_OPTION%"
 if %ERRORLEVEL% NEQ 0 exit 1
 cmake --build . --config Debug
 if %ERRORLEVEL% NEQ 0 exit 1
