cc801d48414d9e3f568e00d38055956d5ffbbf0c
Gammasoft71 <gammasoft71@gmail.com>
2020-08-18 23:38:31 +0200

    Update combo_box.cpp

--- examples/common_controls/combo_box/src/combo_box.cpp
+++ examples/common_controls/combo_box/src/combo_box.cpp
@@ -8,39 +8,33 @@ namespace examples {
   class form1 : public form {
   public:
     form1() {
+      controls().push_back_range({combo_box1, combo_box2, combo_box3});
       text("Combo box example");
-      
+
       combo_box1.location({10, 10});
       combo_box1.items().push_back_range({"Item1", "Item2", "Item3", "Item4", "Item5", "Item6", "Item7", "Item8", "Item9", "Item10"});
       combo_box1.selected_index(0);
-      combo_box1.parent(*this);
-      combo_box1.selected_index_changed += [&] {
-        combo_box2.selected_index(combo_box1.selected_index());
-        combo_box3.selected_index(combo_box1.selected_index());
-      };
+      combo_box1.selected_index_changed += {*this, &form1::on_combo_box_click};
 
       combo_box2.location({10, 50});
       combo_box2.drop_down_style(combo_box_style::drop_down_list);
       combo_box2.items().push_back_range({"Item1", "Item2", "Item3", "Item4", "Item5", "Item6", "Item7", "Item8", "Item9", "Item10"});
       combo_box2.selected_index(0);
-      combo_box2.parent(*this);
-      combo_box2.selected_index_changed += [&] {
-        combo_box1.selected_index(combo_box2.selected_index());
-        combo_box3.selected_index(combo_box2.selected_index());
-      };
+      combo_box2.selected_index_changed += {*this, &form1::on_combo_box_click};
 
       combo_box3.location({10, 90});
       combo_box3.drop_down_style(combo_box_style::simple);
       combo_box3.items().push_back_range({"Item1", "Item2", "Item3", "Item4", "Item5", "Item6", "Item7", "Item8", "Item9", "Item10"});
       combo_box3.selected_index(0);
-      combo_box3.parent(*this);
-      combo_box3.selected_index_changed += [&] {
-        combo_box1.selected_index(combo_box3.selected_index());
-        combo_box2.selected_index(combo_box3.selected_index());
-      };
+      combo_box3.selected_index_changed += {*this, &form1::on_combo_box_click};
     }
     
   private:
+    void on_combo_box_click(control& sender, const xtd::event_args& e) {
+      combo_box1.selected_index(static_cast<combo_box&>(sender).selected_index());
+      combo_box2.selected_index(static_cast<combo_box&>(sender).selected_index());
+      combo_box3.selected_index(static_cast<combo_box&>(sender).selected_index());
+    }
     combo_box combo_box1;
     combo_box combo_box2;
     combo_box combo_box3;
