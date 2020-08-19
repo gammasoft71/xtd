25dc72a83b3d50e9291818116c043415a0ad8e7a
Gammasoft71 <gammasoft71@gmail.com>
2020-08-19 20:07:08 +0200

    Update wx_command_link_button.h

--- src/xtd_forms_native_wxwidgets/include/xtd/forms/native/wxwidgets/wx_command_link_button.h
+++ src/xtd_forms_native_wxwidgets/include/xtd/forms/native/wxwidgets/wx_command_link_button.h
@@ -1,7 +1,9 @@
 #pragma once
 #include <stdexcept>
+#include <xtd/environment.h>
 #include <xtd/drawing/system_images.h>
 #include <xtd/forms/create_params.h>
+#include <xtd/forms/native/application.h>
 #include <xtd/forms/native/button_styles.h>
 #include <wx/commandlinkbutton.h>
 #include "control_handler.h"
@@ -15,9 +17,8 @@ namespace xtd {
           if (!create_params.parent()) throw std::invalid_argument("control must have a parent");
           this->control_handler::create<wxCommandLinkButton>(reinterpret_cast<control_handler*>(create_params.parent())->container(), wxID_ANY, wxEmptyString, wxEmptyString, wxPoint(create_params.x(), create_params.y()), wxSize(create_params.width(), create_params.height()), style_to_wx_style(create_params.style(), create_params.ex_style()));
           static_cast<wxCommandLinkButton*>(control())->SetLabel(wxString(create_params.caption().c_str(), wxMBConvUTF8()));
-          #if !defined(__WXMSW__)
-          static_cast<wxCommandLinkButton*>(control())->SetBitmap(wxBitmap(*reinterpret_cast<wxImage*>(xtd::drawing::system_images::from_name("go-next", xtd::drawing::size(16, 16)).handle())));
-          #endif
+          if (!(xtd::environment::os_version().is_windows_platform() && application::dark_mode_enabled()))
+            static_cast<wxCommandLinkButton*>(control())->SetBitmap(wxBitmap(*reinterpret_cast<wxImage*>(xtd::drawing::system_images::from_name("go-next", xtd::drawing::size(16, 16)).handle())));
         }
         
         static long style_to_wx_style(size_t style, size_t ex_style) {
