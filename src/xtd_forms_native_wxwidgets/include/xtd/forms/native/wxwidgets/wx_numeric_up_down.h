4ac6b33784b2504496d52d995dd1317a29a736db
gammasoft71 <gammasoft71@gmail.com>
2020-08-27 23:40:38 +0200

    Update wx_numeric_up_down.h

--- src/xtd_forms_native_wxwidgets/include/xtd/forms/native/wxwidgets/wx_numeric_up_down.h
+++ src/xtd_forms_native_wxwidgets/include/xtd/forms/native/wxwidgets/wx_numeric_up_down.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <codecvt>
 #include <stdexcept>
+#include <xtd/drawing/system_colors.h>
 #include <xtd/forms/create_params.h>
 #include <xtd/forms/native/up_down_styles.h>
 #include <wx/spinctrl.h>
@@ -18,6 +19,10 @@ namespace xtd {
           if (height < 32) height = 32;
 #endif
           this->control_handler::create<wxSpinCtrlDouble>(reinterpret_cast<control_handler*>(create_params.parent())->container(), wxID_ANY, wxString(create_params.caption().c_str(), wxMBConvUTF8()), wxPoint(create_params.x(), create_params.y()), wxSize(create_params.width(), height), style_to_wx_style(create_params.style(), create_params.ex_style()));
+#if defined(__WIN32__)
+          // Workaround : with wxWidgets version <= 3.1.4 foreground text has bad color with dark mode on Windows
+          control()->GetChildren()[0]->SetForegroundColour(wxColour(xtd::drawing::system_colors::window_text().r(), xtd::drawing::system_colors::window_text().g(), xtd::drawing::system_colors::window_text().b(), xtd::drawing::system_colors::window_text().a()));
+#endif
         }
         
         static long style_to_wx_style(size_t style, size_t ex_style) {
