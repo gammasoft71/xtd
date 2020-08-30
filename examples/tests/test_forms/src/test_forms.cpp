44b80292e095cbf0bce25ea6556e6a7f7329de3d
Gammasoft71 <gammasoft71@gmail.com>
2020-08-30 13:56:52 +0200

    Update test_forms.cpp

--- examples/tests/test_forms/src/test_forms.cpp
+++ examples/tests/test_forms/src/test_forms.cpp
@@ -1,32 +1,56 @@
-#include <filesystem>
 #include <xtd/xtd.forms>
 
-using namespace std;
-using namespace std::filesystem;
-using namespace xtd;
-using namespace xtd;
-using namespace xtd::drawing;
-using namespace xtd::forms;
-
 int main() {
-  /*
-  auto main_form = control::create<form>("Test Gui");
-  auto picture = control::create<picture_box>(*main_form);
-  picture->size_mode(picture_box_size_mode::zoom).dock(dock_style::fill);
-  //picture->image(properties::resources::xtd_console());
-  picture->image(system_images::from_name("xtd", size(1024, 1024)));
-  application::run(*main_form);
-   */
-  
-  auto source_path = path("/Users/yves/Projects/xtd/src/xtd_drawing/resources/system_images/xtd");
-  auto target_path = path("/Users/yves/Projects/xtd/src/xtd_drawing/resources/system_images/windows");
-  auto sizes = {"16x16", "24x24", "32x32", "48x48", "64x64", "96x96", "128x128", "256x256", "512x512", "1024x1024"};
-  auto images = {"image-x-generic", "package-x-generic", "text-x-generic"};
-  
-  for (auto size : sizes) {
-    for (auto image : images) {
-      copy(source_path/size/strings::format("{}.png", image), target_path/size/strings::format("{}.png", image), copy_options::overwrite_existing);
+  xtd::forms::theme_color::current_theme_color(xtd::forms::theme_color("frozen", xtd::forms::theme_style::light, {[](xtd::forms::known_themed_color color)->xtd::drawing::color {
+    switch (color) {
+      case xtd::forms::known_themed_color::accent: return xtd::drawing::color::white;
+      case xtd::forms::known_themed_color::accent_text: return xtd::drawing::color::dark_gray;
+      case xtd::forms::known_themed_color::active_border: return xtd::drawing::color::dark_gray;
+      case xtd::forms::known_themed_color::active_caption: return xtd::drawing::color::alice_blue;
+      case xtd::forms::known_themed_color::active_caption_text: return xtd::drawing::color::dark_gray;
+      case xtd::forms::known_themed_color::app_workspace: return xtd::drawing::color::alice_blue;
+      case xtd::forms::known_themed_color::button_face: return xtd::drawing::color::white;
+      case xtd::forms::known_themed_color::button_highlight: return xtd::drawing::color::white;
+      case xtd::forms::known_themed_color::button_shadow: return xtd::drawing::color::light_gray;
+      case xtd::forms::known_themed_color::control: return xtd::drawing::color::alice_blue;
+      case xtd::forms::known_themed_color::control_dark: return xtd::drawing::color::dark_gray;
+      case xtd::forms::known_themed_color::control_dark_dark: return xtd::drawing::color::dark_gray;
+      case xtd::forms::known_themed_color::control_light: return xtd::drawing::color::white;
+      case xtd::forms::known_themed_color::control_light_light: return xtd::drawing::color::white;
+      case xtd::forms::known_themed_color::control_text: return xtd::drawing::color::dark_gray;
+      case xtd::forms::known_themed_color::desktop: return xtd::drawing::color::alice_blue;
+      case xtd::forms::known_themed_color::gradient_active_caption: return xtd::drawing::color::alice_blue;
+      case xtd::forms::known_themed_color::gradient_inactive_caption: return xtd::drawing::color::alice_blue;
+      case xtd::forms::known_themed_color::gray_text: return xtd::drawing::color::gray;
+      case xtd::forms::known_themed_color::highlight: return  xtd::drawing::system_colors::highlight();
+      case xtd::forms::known_themed_color::highlight_text: return  xtd::drawing::system_colors::highlight_text();
+      case xtd::forms::known_themed_color::hot_track: return  xtd::drawing::system_colors::hot_track();
+      case xtd::forms::known_themed_color::inactive_border: return xtd::drawing::color::dark_gray;
+      case xtd::forms::known_themed_color::inactive_caption: return xtd::drawing::color::alice_blue;
+      case xtd::forms::known_themed_color::inactive_caption_text: return xtd::drawing::color::dark_gray;
+      case xtd::forms::known_themed_color::info: return  xtd::drawing::system_colors::info();
+      case xtd::forms::known_themed_color::info_text: return  xtd::drawing::system_colors::info_text();
+      case xtd::forms::known_themed_color::menu: return xtd::drawing::color::alice_blue;;
+      case xtd::forms::known_themed_color::menu_text: return xtd::drawing::color::dark_gray;
+      case xtd::forms::known_themed_color::menu_bar: return xtd::drawing::color::alice_blue;;
+      case xtd::forms::known_themed_color::menu_highlight: return  xtd::drawing::system_colors::menu_highlight();
+      case xtd::forms::known_themed_color::scroll_bar: return xtd::drawing::color::alice_blue;
+      case xtd::forms::known_themed_color::text_box: return xtd::drawing::color::white;
+      case xtd::forms::known_themed_color::text_box_text: return xtd::drawing::color::dark_gray;
+      case xtd::forms::known_themed_color::window: return xtd::drawing::color::white;
+      case xtd::forms::known_themed_color::window_frame: return xtd::drawing::color::dark_gray;
+      case xtd::forms::known_themed_color::window_text: return xtd::drawing::color::dark_gray;
+      default: throw std::invalid_argument("unknown know_color");
     }
-  }
-  
+  }}));
+
+  auto main_form = xtd::forms::control::create<xtd::forms::form>("Test Gui", xtd::drawing::point(-1, -1), xtd::drawing::size(400, 300));
+  auto label = xtd::forms::control::create<xtd::forms::label>(*main_form, "This example showhow to use theme_color.\nSet current theme color to \"Frozen\" theme color.", xtd::drawing::point(10, 10));
+  label->auto_size(true);
+  auto button = xtd::forms::control::create<xtd::forms::button>(*main_form, "Click me", xtd::drawing::point(10, 50));
+  auto text_box = xtd::forms::control::create<xtd::forms::text_box>(*main_form, "Text box line", xtd::drawing::point(10, 80));
+  auto list_box = xtd::forms::control::create<xtd::forms::list_box>(*main_form, xtd::drawing::point(150, 50));
+  list_box->items().push_back_range({"Apple", "Banana", "Orange", "Peer"});
+  list_box->selection_mode(xtd::forms::selection_mode::multi_extended);
+  xtd::forms::application::run(*main_form);
 }
