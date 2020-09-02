aaa0f40d04ad9bc8cb90f8394626fa9c80f76eaf
Gammasoft71 <gammasoft71@gmail.com>
2020-09-02 22:56:49 +0200

    fix desttroy

--- src/xtd_forms_native_wxwidgets/include/xtd/forms/native/wxwidgets/control_handler.h
+++ src/xtd_forms_native_wxwidgets/include/xtd/forms/native/wxwidgets/control_handler.h
@@ -438,7 +438,7 @@ namespace xtd {
       inline bool control_wrapper<control_t>::ProcessEvent(wxEvent& event) {
         //diagnostics::debug::write_line_if(event.GetEventType() != wxEVT_UPDATE_UI && event.GetEventType() != wxEVT_IDLE, strings::format("control_wrapper<{}>::ProcessEvent {}", strings::full_class_name<control_t>(), to_string(event)));
         if (event.GetEventType() == wxEVT_DESTROY) {
-          def_process_event(event);
+          //def_process_event(event);
           return process_result_;
         }
         
