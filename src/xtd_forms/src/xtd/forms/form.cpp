9338d48aab4f9dec119a125aec5b536c0eade3a6
Gammasoft71 <gammasoft71@gmail.com>
2020-09-02 17:08:03 +0200

    Update form.cpp

--- src/xtd_forms/src/xtd/forms/form.cpp
+++ src/xtd_forms/src/xtd/forms/form.cpp
@@ -19,81 +19,81 @@ using namespace xtd::forms;
 std::optional<std::reference_wrapper<form>> form::active_form_;
 
 form::form() {
-  this->auto_size_mode_ = forms::auto_size_mode::grow_only;
-  //this->back_color_ = this->default_back_color();
-  //this->fore_color_ = this->default_fore_color();
-  this->cursor_ = this->default_cursor();
+  auto_size_mode_ = forms::auto_size_mode::grow_only;
+  //back_color_ = default_back_color();
+  //fore_color_ = default_fore_color();
+  cursor_ = default_cursor();
   icon_ = system_icons::xtd_forms_logo();
-  this->font_ = this->default_font();
-  this->size_ = this->default_size();
-  this->set_state(state::visible, false);
-  this->set_state(state::top_level, true);
-  this->create_control();
+  font_ = default_font();
+  size_ = default_size();
+  set_state(state::visible, false);
+  set_state(state::top_level, true);
+  create_control();
 }
 
 form& form::accept_button(const ibutton_control& accept_button) {
-  if (!this->accept_button_.has_value() || &this->accept_button_.value().get() != &accept_button) {
-    this->accept_button_ = const_cast<ibutton_control&>(accept_button);
+  if (!accept_button_.has_value() || &accept_button_.value().get() != &accept_button) {
+    accept_button_ = const_cast<ibutton_control&>(accept_button);
   }
   return *this;
 }
 
 form& form::accept_button(nullptr_t) {
-  this->accept_button_.reset();
+  accept_button_.reset();
   return *this;
 }
 
 form& form::cancel_button(const ibutton_control& cancel_button) {
-  if (!this->cancel_button_.has_value() || &this->cancel_button_.value().get() != &cancel_button) {
-    this->cancel_button_ = const_cast<ibutton_control&>(cancel_button);
+  if (!cancel_button_.has_value() || &cancel_button_.value().get() != &cancel_button) {
+    cancel_button_ = const_cast<ibutton_control&>(cancel_button);
   }
   return *this;
 }
 
 form& form::cancel_button(nullptr_t) {
-  this->cancel_button_.reset();
+  cancel_button_.reset();
   return *this;
 }
 
 form& form::auto_size_mode(forms::auto_size_mode value) {
-  this->set_auto_size_mode(value);
+  set_auto_size_mode(value);
   return *this;
 }
 
 form& form::close_box(bool value) {
-  if (this->close_box_ != value) {
-    this->close_box_ = value;
-    this->recreate_handle();
+  if (close_box_ != value) {
+    close_box_ = value;
+    recreate_handle();
   }
   return *this;
 }
 
 form& form::control_box(bool value) {
-  if (this->control_box_ != value) {
-    this->control_box_ = value;
-    this->recreate_handle();
+  if (control_box_ != value) {
+    control_box_ = value;
+    recreate_handle();
   }
   return *this;
 }
 
 form& form::dialog_result(forms::dialog_result value) {
-  if (this->dialog_result_ != value)
-    this->dialog_result_ = value;
+  if (dialog_result_ != value)
+    dialog_result_ = value;
   return *this;
 }
 
 form& form::form_border_style(forms::form_border_style value) {
-  if (this->form_border_style_ != value) {
-    this->form_border_style_ = value;
-    this->recreate_handle();
+  if (form_border_style_ != value) {
+    form_border_style_ = value;
+    recreate_handle();
   }
   return *this;
 }
 
 form& form::help_button(bool value) {
-  if (this->help_button_ != value) {
-    this->help_button_ = value;
-    this->recreate_handle();
+  if (help_button_ != value) {
+    help_button_ = value;
+    recreate_handle();
   }
   return *this;
 }
@@ -107,9 +107,9 @@ form& form::icon(const xtd::drawing::icon& value) {
 }
 
 form& form::maximize_box(bool value) {
-  if (this->maximize_box_ != value) {
-    this->maximize_box_ = value;
-    this->recreate_handle();
+  if (maximize_box_ != value) {
+    maximize_box_ = value;
+    recreate_handle();
   }
   return *this;
 }
@@ -131,9 +131,9 @@ form& form::menu(nullptr_t) {
 }
 
 form& form::minimize_box(bool value) {
-  if (this->minimize_box_ != value) {
-    this->minimize_box_ = value;
-    this->recreate_handle();
+  if (minimize_box_ != value) {
+    minimize_box_ = value;
+    recreate_handle();
   }
   return *this;
 }
@@ -153,56 +153,56 @@ control& form::parent(const control& parent) {
 }
 
 form& form::start_position(form_start_position start_position) {
-  if (this->start_position_ != start_position)
-    this->start_position_ = start_position;
+  if (start_position_ != start_position)
+    start_position_ = start_position;
   return *this;
 }
 
 form& form::top_level(bool top_level) {
-  if (!this->get_state(state::top_level) != top_level) {
-    this->set_state(state::top_level, top_level);
+  if (!get_state(state::top_level) != top_level) {
+    set_state(state::top_level, top_level);
   }
   return *this;
 }
 
 form& form::top_most(bool value) {
-  if (this->top_most_ != value) {
-    this->top_most_ = value;
-    this->recreate_handle();
+  if (top_most_ != value) {
+    top_most_ = value;
+    recreate_handle();
   }
   return *this;
 }
 
 control& form::visible(bool visible) {
   std::optional<forms::form_window_state> current_window_state;
-  if (!this->previous_screeen_) {
-    current_window_state = this->window_state_;
-    this->previous_screeen_ = std::make_shared<screen>(screen::from_control(*this));
-    this->recreate_handle();
+  if (!previous_screeen_) {
+    current_window_state = window_state_;
+    previous_screeen_ = std::make_shared<screen>(screen::from_control(*this));
+    recreate_handle();
   }
-  this->container_control::visible(visible);
-  if (active_form().has_value() && active_form().value().get().handle() == this->handle() && this->active_control_.has_value())
-    this->active_control_.value().get().focus();
+  container_control::visible(visible);
+  if (active_form().has_value() && active_form().value().get().handle() == handle() && active_control_.has_value())
+    active_control_.value().get().focus();
 
   if (current_window_state.has_value())
-    this->window_state(current_window_state.value());
-  this->internal_set_window_state();
+    window_state(current_window_state.value());
+  internal_set_window_state();
   if (visible) can_close_ = false;
   return *this;
 }
 
 form& form::window_state(form_window_state value) {
-  if (this->window_state_ == form_window_state::full_screen && value != form_window_state::normal) return *this;
-  if (this->window_state_ != form_window_state::normal && value == form_window_state::full_screen) return *this;
-  if (this->window_state_ != value) {
-    this->window_state_ = value;
-    this->internal_set_window_state();
+  if (window_state_ == form_window_state::full_screen && value != form_window_state::normal) return *this;
+  if (window_state_ != form_window_state::normal && value == form_window_state::full_screen) return *this;
+  if (window_state_ != value) {
+    window_state_ = value;
+    internal_set_window_state();
   }
   return *this;
 }
 
 void form::activate() {
-  native::form::activate(this->handle());
+  native::form::activate(handle());
 }
 
 void form::bring_to_front() {
@@ -217,7 +217,7 @@ void form::center_to_screen() {
 }
 
 void form::close() {
-  native::form::close(this->handle());
+  native::form::close(handle());
 }
 
 forms::dialog_result form::show_dialog() {
@@ -225,22 +225,22 @@ forms::dialog_result form::show_dialog() {
 }
 
 forms::dialog_result form::show_dialog(const iwin32_window& owner) {
-  intptr_t current_parent = this->parent_;
-  this->set_state(state::modal, true);
-  if (owner.handle() != this->handle()) this->parent_ = owner.handle();
-  this->show();
-  forms::dialog_result result = this->dialog_result_ = forms::dialog_result::none;
+  intptr_t current_parent = parent_;
+  set_state(state::modal, true);
+  if (owner.handle() != handle()) parent_ = owner.handle();
+  show();
+  forms::dialog_result result = dialog_result_ = forms::dialog_result::none;
   application::raise_enter_thread_modal(event_args::empty);
-  result = static_cast<forms::dialog_result>(native::form::show_dialog(this->handle()));
+  result = static_cast<forms::dialog_result>(native::form::show_dialog(handle()));
   application::raise_leave_thread_modal(event_args::empty);
-  this->set_state(state::modal, false);
-  this->parent_ = current_parent;
-  this->hide();
+  set_state(state::modal, false);
+  parent_ = current_parent;
+  hide();
   return result;
 }
 
 forms::create_params form::create_params() const {
-  forms::create_params create_params = this->container_control::create_params();
+  forms::create_params create_params = container_control::create_params();
   create_params.style(create_params.style() & !WS_CHILD);
   static int32_t default_location = 0;
   if (default_location == 0) {
@@ -252,7 +252,7 @@ forms::create_params form::create_params() const {
   
   create_params.style(create_params.style() | WS_CLIPCHILDREN);
   
-  switch (this->form_border_style_) {
+  switch (form_border_style_) {
     case forms::form_border_style::none: break;
     case forms::form_border_style::fixed_single: create_params.style(create_params.style() | WS_BORDER); break;
     case forms::form_border_style::sizable: create_params.style(create_params.style() | WS_BORDER | WS_THICKFRAME); break;
@@ -263,60 +263,60 @@ forms::create_params form::create_params() const {
     default: break;
   }
 
-  switch (this->window_state_) {
+  switch (window_state_) {
     case form_window_state::maximized: create_params.style(create_params.style() | WS_MAXIMIZE); break;
     case form_window_state::minimized: create_params.style(create_params.style() | WS_MINIMIZE); break;
     default: break;
   }
 
-  if (this->form_border_style_ != form_border_style::none) create_params.style(create_params.style() | WS_CAPTION);
+  if (form_border_style_ != form_border_style::none) create_params.style(create_params.style() | WS_CAPTION);
 
-  if (this->control_box_) create_params.style(create_params.style() | WS_SYSMENU);
-  if (this->maximize_box_) create_params.style(create_params.style() | WS_MAXIMIZEBOX);
-  if (this->minimize_box_) create_params.style(create_params.style() | WS_MINIMIZEBOX);
-  if (this->help_button_ && !this->maximize_box_ && !this->minimize_box_) create_params.ex_style(create_params.ex_style() | WS_EX_CONTEXTHELP);
+  if (control_box_) create_params.style(create_params.style() | WS_SYSMENU);
+  if (maximize_box_) create_params.style(create_params.style() | WS_MAXIMIZEBOX);
+  if (minimize_box_) create_params.style(create_params.style() | WS_MINIMIZEBOX);
+  if (help_button_ && !maximize_box_ && !minimize_box_) create_params.ex_style(create_params.ex_style() | WS_EX_CONTEXTHELP);
 
-  if (!this->close_box_) create_params.class_style(create_params.class_style() | CS_NOCLOSE);
+  if (!close_box_) create_params.class_style(create_params.class_style() | CS_NOCLOSE);
 
-  if (this->show_in_taskbar_) create_params.ex_style(create_params.ex_style() | WS_EX_APPWINDOW);
+  if (show_in_taskbar_) create_params.ex_style(create_params.ex_style() | WS_EX_APPWINDOW);
 
-  if (!this->show_icon_ && (this->form_border_style_ == forms::form_border_style::sizable || this->form_border_style_ == forms::form_border_style::fixed_3d || this->form_border_style_ == forms::form_border_style::fixed_single)) create_params.ex_style(create_params.ex_style() | WS_EX_DLGMODALFRAME);
+  if (!show_icon_ && (form_border_style_ == forms::form_border_style::sizable || form_border_style_ == forms::form_border_style::fixed_3d || form_border_style_ == forms::form_border_style::fixed_single)) create_params.ex_style(create_params.ex_style() | WS_EX_DLGMODALFRAME);
   
-  if (this->get_state(state::modal)) create_params.ex_style(create_params.ex_style() | WS_EX_MODALWINDOW);
+  if (get_state(state::modal)) create_params.ex_style(create_params.ex_style() | WS_EX_MODALWINDOW);
 
   if (owner_ != 0) create_params.parent(owner_);
 
   if (top_most_) create_params.ex_style(create_params.ex_style() | WS_EX_TOPMOST);
   
-  if (this->previous_screeen_) {
-    switch (this->start_position_) {
+  if (previous_screeen_) {
+    switch (start_position_) {
       case form_start_position::manual:
-        create_params.location(this->location());
-        create_params.size(this->size());
+        create_params.location(location());
+        create_params.size(size());
         if (application::open_forms().size() == 1) default_location = 40;
         break;
       case form_start_position::center_screen:
-        create_params.location({(this->previous_screeen_->working_area().width() - this->width()) / 2, (this->previous_screeen_->working_area().height() - this->height()) / 2});
-        create_params.size(this->size());
+        create_params.location({(previous_screeen_->working_area().width() - width()) / 2, (previous_screeen_->working_area().height() - height()) / 2});
+        create_params.size(size());
         break;
       case form_start_position::windows_default_location:
         create_params.location({default_location, default_location});
-        create_params.size(this->size());
+        create_params.size(size());
         break;
       case form_start_position::windows_default_bounds:
         create_params.location({default_location, default_location});
-        create_params.size({this->previous_screeen_->working_area().width() / 4 * 3, this->previous_screeen_->working_area().height() / 4 * 3});
+        create_params.size({previous_screeen_->working_area().width() / 4 * 3, previous_screeen_->working_area().height() / 4 * 3});
         break;
       case form_start_position::center_parent:
-        if (this->parent_ != 0)
-          create_params.location({this->parent().value().get().left() + (this->parent().value().get().width() - this->width()) / 2, this->parent().value().get().top() + (this->parent().value().get().height() - this->height()) / 2});
+        if (parent_ != 0)
+          create_params.location({parent().value().get().left() + (parent().value().get().width() - width()) / 2, parent().value().get().top() + (parent().value().get().height() - height()) / 2});
         else
           create_params.location({default_location, default_location});
-        create_params.size(this->size());
+        create_params.size(size());
         break;
     }
     
-    if (this->start_position_ == form_start_position::windows_default_location || this->start_position_ == form_start_position::windows_default_bounds || (this->start_position_ == form_start_position::center_parent && !this->parent_)) {
+    if (start_position_ == form_start_position::windows_default_location || start_position_ == form_start_position::windows_default_bounds || (start_position_ == form_start_position::center_parent && !parent_)) {
       default_location = default_location < 200 ? default_location + 20 : 40;
     }
   }
@@ -326,36 +326,36 @@ forms::create_params form::create_params() const {
 
 void form::wnd_proc(message &message) {
   switch (message.msg()) {
-    case WM_ACTIVATE: this->wm_activate(message); break;
-    case WM_KEYUP: this->wm_key_up(message); break;
-    case WM_CLOSE: this->wm_close(message); break;
+    case WM_ACTIVATE: wm_activate(message); break;
+    case WM_KEYUP: wm_key_up(message); break;
+    case WM_CLOSE: wm_close(message); break;
     case WM_MENUCOMMAND: if (menu_.has_value()) menu_.value().wm_click(message); break;
-    default: this->container_control::wnd_proc(message); break;
+    default: container_control::wnd_proc(message); break;
   }
 }
 
 void form::wm_activate(message &message) {
   if (message.lparam() == WA_INACTIVE && active_form_.has_value() && &active_form_.value().get() == this) {
     active_form_.reset();
-    this->on_deactivate(event_args::empty);
+    on_deactivate(event_args::empty);
   } else {
     active_form_ = *this;
-    this->on_activated(event_args::empty);
+    on_activated(event_args::empty);
   }
 }
 
 void form::wm_close(message &message) {
   form_closing_event_args event_args;
-  this->on_form_closing(event_args);
+  on_form_closing(event_args);
   if (event_args.cancel() != true) {
     can_close_ = true;
     hide();
-    if (!this->get_state(state::modal)) {
-      this->def_wnd_proc(message);
-      //this->destroy_control();
+    if (!get_state(state::modal)) {
+      def_wnd_proc(message);
+      //destroy_control();
     } else {
-      if (this->dialog_result_ == forms::dialog_result::none) this->dialog_result_ = forms::dialog_result::cancel;
-      native::form::end_dialog(this->handle(), static_cast<int32_t>(this->dialog_result_));
+      if (dialog_result_ == forms::dialog_result::none) dialog_result_ = forms::dialog_result::cancel;
+      native::form::end_dialog(handle(), static_cast<int32_t>(dialog_result_));
     }
     form_closed_event_args close_event_args;
     on_form_closed(close_event_args);
@@ -376,42 +376,42 @@ void form::wm_key_up(message& message) {
 }
 
 void form::on_handle_created(const event_args &e) {
-  this->container_control::on_handle_created(e);
+  container_control::on_handle_created(e);
   if (icon_ != drawing::icon::empty) native::form::icon(handle(), icon_.handle());
   if (menu_.has_value()) native::form::menu(handle(), menu_.value().handle());
-  if (this->accept_button_.has_value()) native::form::default_control(this->handle(), dynamic_cast<control&>(this->accept_button_.value().get()).handle());
+  if (accept_button_.has_value()) native::form::default_control(handle(), dynamic_cast<control&>(accept_button_.value().get()).handle());
   if (menu_.has_value()) native::form::menu(handle(), menu_.value().handle());
 }
 
 void form::on_handle_destroyed(const event_args &e) {
-  this->container_control::on_handle_destroyed(e);
+  container_control::on_handle_destroyed(e);
   native::form::menu(handle(), 0);
 }
 
 void form::on_layout(const event_args& e) {
-  this->scrollable_control::on_layout(e);
-  if (auto_scroll_) native::form::virtual_size(this->handle(), display_rectangle().size());
+  scrollable_control::on_layout(e);
+  if (auto_scroll_) native::form::virtual_size(handle(), display_rectangle().size());
 }
 
 void form::on_resize(const event_args& e) {
-  if (native::form::minimize(this->handle()))
-    this->window_state_ = forms::form_window_state::minimized;
-  else if (native::form::maximize(this->handle()))
-    this->window_state_ = forms::form_window_state::maximized;
-  else if (this->window_state_ != form_window_state::full_screen)
-    this->window_state_ = forms::form_window_state::normal;
-  this->container_control::on_resize(e);
+  if (native::form::minimize(handle()))
+    window_state_ = forms::form_window_state::minimized;
+  else if (native::form::maximize(handle()))
+    window_state_ = forms::form_window_state::maximized;
+  else if (window_state_ != form_window_state::full_screen)
+    window_state_ = forms::form_window_state::normal;
+  container_control::on_resize(e);
 }
 
 void form::internal_set_window_state() {
-  if (!this->previous_screeen_)
-    this->recreate_handle();
+  if (!previous_screeen_)
+    recreate_handle();
   else {
-    switch (this->window_state_) {
-      case form_window_state::normal: native::form::restore(this->handle()); break;
-      case form_window_state::maximized: native::form::maximize(this->handle(), true); break;
-      case form_window_state::minimized: native::form::minimize(this->handle(), true); break;
-      case form_window_state::full_screen: native::form::full_screen(this->handle(), true); break;
+    switch (window_state_) {
+      case form_window_state::normal: native::form::restore(handle()); break;
+      case form_window_state::maximized: native::form::maximize(handle(), true); break;
+      case form_window_state::minimized: native::form::minimize(handle(), true); break;
+      case form_window_state::full_screen: native::form::full_screen(handle(), true); break;
       default: break;
     }
   }
