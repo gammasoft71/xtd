115b22e272c977bf5315420d88874ea9b2dc7e15
Gammasoft71 <gammasoft71@gmail.com>
2020-09-03 11:54:31 +0200

    Update tab_control.cpp

--- examples/containers/tab_control/src/tab_control.cpp
+++ examples/containers/tab_control/src/tab_control.cpp
@@ -16,11 +16,29 @@ public:
     tab_page1.parent(tab_control1);
     tab_page1.text("Tab page 1");
     
+    label1.dock(dock_style::fill);
+    label1.location({10, 10});
+    label1.parent(tab_page1);
+    label1.text("label1");
+    label1.text_align(xtd::forms::content_alignment::top_left);
+
     tab_page2.parent(tab_control1);
     tab_page2.text("Tab page 2");
     
+    label2.dock(dock_style::fill);
+    label2.location({10, 10});
+    label2.parent(tab_page2);
+    label2.text_align(xtd::forms::content_alignment::middle_center);
+    label2.text("label2");
+
     tab_page3.parent(tab_control1);
     tab_page3.text("Tab page 3");
+    
+    label3.dock(dock_style::fill);
+    label3.location({10, 10});
+    label3.parent(tab_page3);
+    label3.text_align(xtd::forms::content_alignment::bottom_right);
+    label3.text("label3");
   }
   
 private:
@@ -28,6 +46,10 @@ private:
   tab_page tab_page1;
   tab_page tab_page2;
   tab_page tab_page3;
+  
+  label label1;
+  label label2;
+  label label3;
 };
 
 int main() {
