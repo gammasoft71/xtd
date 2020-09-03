cbe41b16f36fe27be7202a0fef958dd0570cc1c8
Gammasoft71 <gammasoft71@gmail.com>
2020-09-03 12:19:26 +0200

    update travis scripts

--- scripts/travis/build_linux.sh
+++ scripts/travis/build_linux.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
 
-mkdir -p build/examples
-cd build
+# clone, generate and build wxwidgets 3.1.4
+mkdir -p build/thirdparty/ && cd build/thirdparty
+git clone https://github.com/wxwidgets/wxwidgets.git -b v3.1.4 --depth 1
+cd wxwidgets
+git submodule update --init
+mkdir build_cmake && cd build_cmake
+cmake .. -G "CodeBlocks - Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DwxBUILD_SHARED=OFF
+cmake --build . -- -j8
+sudo cmake --build . --target install
+cd ../../../..
 
 # generate and build lib
+mkdir -p build && cd build
 cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_TESTS=ON -DENABLE_XTD_COMMAND_LINE=OFF -DCMAKE_CXX_COMPILER=g++-9 -DCMAKE_INSTALL_PREFIX=~/local ..
 if [ $? -ne 0 ]; then exit -1; fi
 cmake --build . --target install -- -j $(nproc)
 if [ $? -ne 0 ]; then exit -1; fi
+cd ..
 
 # generate and build examples
-cd examples
+mkdir -p build/examples && cd build/examples
 cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=g++-9 -DCMAKE_INSTALL_PREFIX=~/local ../../examples
 if [ $? -ne 0 ]; then exit -1; fi
 cmake --build . -- -j $(nproc)
 if [ $? -ne 0 ]; then exit -1; fi
-cd ..
+cd ../..
 
 # run registered unit tests
+# cd build
 # ctest -j $(nproc) --output-on-failure --build-config Debug
 # if [ $? -ne 0 ]; then exit -1; fi
-
-cd ..
+# cd ..
--- scripts/travis/build_macos.sh
+++ scripts/travis/build_macos.sh
@@ -3,25 +3,35 @@
 brew update
 brew install doxygen
 
-mkdir -p build/examples
-pushd build
+# clone, generate and build wxwidgets 3.1.4
+mkdir -p build/thirdparty/ && cd build/thirdparty
+git clone https://github.com/wxwidgets/wxwidgets.git -b v3.1.4 --depth 1
+cd wxwidgets
+git submodule update --init
+mkdir -p build_cmake && cd build_cmake
+cmake .. -G "CodeBlocks - Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DwxBUILD_SHARED=OFF
+cmake --build . -- -j8
+sudo cmake --build . --target install
+cd ../../../..
 
 # generate and build lib
-cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_TESTS=ON -DENABLE_XTD_COMMAND_LINE=OFF -DCMAKE_INSTALL_PREFIX=~/local ..
+mkdir -p build && cd build
+cmake .. -G "Xcode"
+cmake --build . --config Debug
+cmake --build . --target install --config Debug
 if [ $? -ne 0 ]; then exit -1; fi
-cmake --build . --target install -- -j 8
-if [ $? -ne 0 ]; then exit -1; fi
+cd ..
 
 # generate and build examples
-#pushd examples
+# mkdir -p build/examples && cd build/examples
 #cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=~/local ../../examples
 #if [ $? -ne 0 ]; then exit -1; fi
 #cmake --build . -- -j 8
 #if [ $? -ne 0 ]; then exit -1; fi
-#popd
+# cd ../..
 
 # run registered unit tests
+# cd build
 # ctest -j $(nproc) --output-on-failure --build-config Debug
 # if [ $? -ne 0 ]; then exit -1; fi
-
-popd
+# cd ..
