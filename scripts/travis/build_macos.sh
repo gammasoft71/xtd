641d67f3ffa076126bc4015ae1bd69b9db1a18b4
Gammasoft71 <gammasoft71@gmail.com>
2020-09-04 09:51:33 +0200

    Update build_macos.sh

--- scripts/travis/build_macos.sh
+++ scripts/travis/build_macos.sh
@@ -9,25 +9,25 @@ git clone https://github.com/wxwidgets/wxwidgets.git -b v3.1.4 --depth 1
 cd wxwidgets
 git submodule update --init
 mkdir -p build_cmake && cd build_cmake
-cmake .. -G "CodeBlocks - Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DwxBUILD_SHARED=OFF -DCMAKE_INSTALL_PREFIX=~/loca
+cmake .. -G "CodeBlocks - Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DwxBUILD_SHARED=OFF -DCMAKE_INSTALL_PREFIX=~/local
 cmake --build . -- -j8
 cmake --build . --target install
 cd ../../../..
 
 # generate and build lib
 mkdir -p build && cd build
-cmake .. -G "Xcode"
+cmake .. -G "Xcode" -DCMAKE_INSTALL_PREFIX=~/local
 cmake --build . --config Debug
-cmake --build . --target install --config Debug -DCMAKE_INSTALL_PREFIX=~/loca
+cmake --build . --target install --config Debug
 if [ $? -ne 0 ]; then exit -1; fi
 cd ..
 
 # generate and build examples
 # mkdir -p build/examples && cd build/examples
-#cmake ../../examples -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=~/local
-#if [ $? -ne 0 ]; then exit -1; fi
-#cmake --build . -- -j 8
-#if [ $? -ne 0 ]; then exit -1; fi
+# cmake ../../examples -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=~/local
+# if [ $? -ne 0 ]; then exit -1; fi
+# cmake --build . -- -j 8
+# if [ $? -ne 0 ]; then exit -1; fi
 # cd ../..
 
 # run registered unit tests
