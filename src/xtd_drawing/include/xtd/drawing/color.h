76181497ebbc3189aac3d3b53c1d45840a902fcc
Gammasoft71 <gammasoft71@gmail.com>
2020-09-12 21:34:01 +0200

    review average, dark and light

--- src/xtd_drawing/include/xtd/drawing/color.h
+++ src/xtd_drawing/include/xtd/drawing/color.h
@@ -571,14 +571,28 @@ namespace xtd {
       /// @param color1 The first color.
       /// @param color2 The second color.
       /// @param weight The weighting factor
+      /// @param average_alpha if true alpha was comptute to; otheerwise the alpha result is the alpha of color1
       /// @return The average color.
       /// @remarks The alpha, red, green and blue values are averages using the following formula:
       /// @code
-      /// color = color1 * weight + color2 * (1 - weight);
+      /// color = color1 * (1 - weight) + color2 * weight;
       /// @endcode
       /// @remarks Thus, a weight value of 1.0 will return the first color, while a value of 0.0 will return the second color.
-      static color average(const color& color1, const color& color2, double weight) {return from_argb(static_cast<uint8_t>(color1.a() * weight + color2.a() * (1 - weight)), static_cast<uint8_t>(color1.r() * weight + color2.r() * (1 - weight)), static_cast<uint8_t>(color1.g() * weight + color2.g() * (1 - weight)), static_cast<uint8_t>(color1.b() * weight + color2.b() * (1 - weight)));}
-      
+      static color average(const color& color1, const color& color2, double weight, bool average_alpha) {return from_argb(average_alpha ? static_cast<uint8_t>(color1.a() * (1 - weight) + color2.a() * weight) : static_cast<uint8_t>(color1.a()), static_cast<uint8_t>(color1.r() * (1 - weight) + color2.r() * weight), static_cast<uint8_t>(color1.g() * (1 - weight) + color2.g() * weight), static_cast<uint8_t>(color1.b() * (1 - weight) + color2.b() * weight));}
+
+      /// @brief Returns the weighted average color between the two given colors.
+      /// @param color1 The first color.
+      /// @param color2 The second color.
+      /// @param weight The weighting factor
+      /// @return The average color.
+      /// @remarks The alpha od color1 is conserved.
+      /// @remarks The red, green and blue values are averages using the following formula:
+      /// @code
+      /// color = color1 * (1 - weight) + color2 * weight;
+      /// @endcode
+      /// @remarks Thus, a weight value of 1.0 will return the second color, while a value of 0.0 will return the first color.
+      static color average(const color& color1, const color& color2, double weight) {return average(color1, color2, weight, false);}
+
       /// @brief Returns a darker version of the specified color.
       /// @param color The color to dark.
       /// @return the darker color result.
@@ -587,7 +601,7 @@ namespace xtd {
       /// @brief Returns a darker version of the specified color.
       /// @param color The color to dark.
       /// @return the darker color result.
-      static color darker(const color& color) {return color::darker(color, .67);}
+      static color darker(const color& color) {return color::darker(color, 1.0/3);}
       
       /// @brief Returns a lighter version of the specified color.
       /// @param color The color to light.
@@ -597,7 +611,7 @@ namespace xtd {
       /// @brief Returns a lighter version of the specified color.
       /// @param color The color to light.
       /// @return the lighter color result.
-      static color lighter(const color& color) {return color::lighter(color, .67);}
+      static color lighter(const color& color) {return color::lighter(color, 1.0/3);}
 
       /// @brief Creates a xtd::drawing::color class from a 32-bit ARGB value.
       /// @param argb A value specifying the 32-bit ARGB value
