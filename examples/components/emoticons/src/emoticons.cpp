52b65f6325704c16c3593fd0807779f48d46f51c
Gammasoft71 <gammasoft71@gmail.com>
2020-09-14 00:20:56 +0200

    review examples

--- examples/components/emoticons/src/emoticons.cpp
+++ examples/components/emoticons/src/emoticons.cpp
@@ -6,15 +6,15 @@ using namespace xtd::forms;
 
 int main() {
   auto main_form = control::create<form>("Emoticons example");
-  main_form->client_size({390, 270});
+  main_form->client_size({500, 400}).padding(10);
   
-  auto emoticons_list_box = control::create<list_box>(*main_form, "", {10, 10}, {150, 250});
+  auto emoticon_label = control::create<label>(*main_form);
+  emoticon_label->text_align(xtd::forms::content_alignment::middle_center).dock(dock_style::fill).font({main_form->font(), 192});
+
+  auto emoticons_list_box = control::create<list_box>(*main_form);
+  emoticons_list_box->dock(dock_style::left).width(200);
   for (const auto& emoticon : emoticons::get_emoticons())
     emoticons_list_box->items().push_back({strings::format("{} {}", emoticon, emoticon.name()), emoticon});
-  
-  auto emoticon_label = control::create<label>(*main_form, "", {170, 20});
-  emoticon_label->auto_size(true).font({main_form->font(), 128});
-  
   emoticons_list_box->selected_value_changed += [&] {
     emoticon_label->text(any_cast<emoticon>(emoticons_list_box->selected_item().tag()).to_string());
   };
---
+++ examples/others/font_families/README.md
@@ -0,0 +1,37 @@
+# font_family
+
+demonstrates the use of xtd::drawing::font_families class.
+
+# Sources
+
+[src/font_families.cpp](src/font_families.cpp)
+
+[CMakeLists.txt](CMakeLists.txt)
+
+# Build and run
+
+Open "Command Prompt" or "Terminal". Navigate to the folder that contains the project and type the following:
+
+```shell
+xtdc run
+```
+
+# Output
+
+## Windows :
+
+![Screenshot](../../../docs/pictures/examples/font_families_w.png)
+
+![Screenshot](../../../docs/pictures/examples/font_families_wd.png)
+
+## macOS :
+
+![Screenshot](../../../docs/pictures/examples/font_families_m.png)
+
+![Screenshot](../../../docs/pictures/examples/font_families_md.png)
+
+## Gnome :
+
+![Screenshot](../../../docs/pictures/examples/font_families_g.png)
+
+![Screenshot](../../../docs/pictures/examples/font_families_gd.png)
