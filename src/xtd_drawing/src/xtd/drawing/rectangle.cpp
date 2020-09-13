9275280c82aa6f15e9d87c7faf715e97703e55e3
Gammasoft71 <gammasoft71@gmail.com>
2020-09-13 16:16:22 +0200

    review rectangle and rectangle_f

--- src/xtd_drawing/src/xtd/drawing/rectangle.cpp
+++ src/xtd_drawing/src/xtd/drawing/rectangle.cpp
@@ -5,21 +5,29 @@ using namespace xtd::drawing;
 
 const rectangle rectangle::empty;
 
-rectangle rectangle::make_intersect(const rectangle& a, const rectangle& b) {
-  int x1 = std::max(a.x(), b.x());
-  int x2 = std::min(a.x() + a.width(), b.x() + b.width());
-  int y1 = std::max(a.y(), b.y());
-  int y2 = std::min(a.y() + a.height(), b.y() + b.height());
-  
-  if (x2 >= x1 && y2 >= y1) return rectangle(x1, y1, x2 - x1, y2 - y1);
-  return rectangle::empty;
+void rectangle::make_intersect(const rectangle& rect) {
+  auto x1 = std::max(x_, rect.x_);
+  auto x2 = std::min(x_ + width_, rect.x_ + rect.width_);
+  auto y1 = std::max(y_, rect.y_);
+  auto y2 = std::min(y_ + height_, rect.y_+ rect.height_);
+
+  if (x2 < x1 || y2 < y1) x_ = y_ = width_ = height_ = 0;
+  else {
+    x_ = x1;
+    y_ = y1;
+    width_ = x2 - x1;
+    height_ = y2 - y1;
+  }
 }
 
-rectangle rectangle::make_union(const rectangle& a, const rectangle& b) {
-  int32_t x1 = std::min(a.x(), b.x());
-  int32_t x2 = std::max(a.x() + a.width(), b.x() + b.width());
-  int32_t y1 = std::min(a.y(), b.y());
-  int32_t y2 = std::max(a.y() + a.height(), b.y() + b.height());
+void rectangle::make_union(const rectangle& rect) {
+  auto x1 = std::min(x_, rect.x_);
+  auto x2 = std::max(x_ + width_, rect.x_ + rect.width_);
+  auto y1 = std::min(y_, rect.y_);
+  auto y2 = std::max(y_ + height_, rect.y_ + rect.height_);
   
-  return rectangle(x1, y1, x2 - x1, y2 - y1);
+  x_ = x1;
+  y_ = y1;
+  width_ = x2 - x1;
+  height_ = y2 - y1;
 }
