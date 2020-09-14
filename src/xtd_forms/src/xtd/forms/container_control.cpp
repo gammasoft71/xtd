e49675234082a0cad7ab393bc86b0596cef5eba3
Gammasoft71 <gammasoft71@gmail.com>
2020-09-14 22:57:55 +0200

    Update container_control.cpp

--- src/xtd_forms/src/xtd/forms/container_control.cpp
+++ src/xtd_forms/src/xtd/forms/container_control.cpp
@@ -5,6 +5,8 @@ using namespace xtd::forms;
 
 void container_control::active_control(const control& active_control) {
   this->active_control_ = const_cast<control&>(active_control);
+  if (active_control_.has_value() && active_control_.value().get().can_focus())
+    active_control_.value().get().focus();
 }
 
 drawing::size container_control::measure_control() const {
