9666e93fd56b0c93cf2686e70d6061e6cb4751e8
Gammasoft71 <gammasoft71@gmail.com>
2020-09-30 13:43:59 +0200

    Update colored_forms.cpp

--- examples/others/colored_forms/src/colored_forms.cpp
+++ examples/others/colored_forms/src/colored_forms.cpp
@@ -11,13 +11,7 @@ int main() {
   vector<shared_ptr<form>> forms;
   auto form_color = known_color::alice_blue;
 
-  form form_main;
-  form_main.text("Main Form");
-  form_main.start_position(form_start_position::manual);
-  form_main.location({screen::primary_screen().working_area().width() - form_main.width() - 40, 40});
-
   button button;
-  button.parent(form_main);
   button.text("Create");
   button.location({10, 10});
   button.click += [&] {
@@ -27,8 +21,12 @@ int main() {
     form->visible(true);
     forms.push_back(form);
     form_color = form_color != known_color::yellow_green ? static_cast<known_color>(static_cast<int>(form_color) + 1) : known_color::alice_blue;
-    form_main.activate();
   };
-  
+
+  form form_main;
+  form_main.text("Main Form");
+  form_main.start_position(form_start_position::manual);
+  form_main.location({screen::primary_screen().working_area().width() - form_main.width() - 40, 40});
+  form_main.controls().push_back(button);
   application::run(form_main);
 }
