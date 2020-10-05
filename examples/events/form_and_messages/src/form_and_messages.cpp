889c5f3df59c5cb7120c11390232b12c08c61b95
Gammasoft71 <gammasoft71@gmail.com>
2020-10-06 00:58:39 +0200

    add trace_form_base

--- examples/events/form_and_messages/src/form_and_messages.cpp
+++ examples/events/form_and_messages/src/form_and_messages.cpp
@@ -1,4 +1,4 @@
-#define TRACE 1
+#define TRACE
 #include <xtd/xtd.forms>
 
 using namespace xtd;
@@ -9,7 +9,6 @@ class form1 : public form {
 public:
   form1() {
     text("Form and Messages");
-    location({100, 100});
   }
 
 protected:
@@ -17,49 +16,47 @@ protected:
     form::wnd_proc(message);
     
     switch (message.msg()) {
-      case WM_ACTIVATE: trace::write_line(strings::format("WM_ACTIVATE [activate={}]", static_cast<bool>(message.lparam()))); break;
-      case WM_ACTIVATEAPP: trace::write_line(strings::format("WM_ACTIVATEAPP [activate={}, threat={}]", static_cast<bool>(message.wparam()), message.lparam())); break;
+      case WM_ACTIVATE: trace::write_line("WM_ACTIVATE [activate={}]", static_cast<bool>(message.lparam())); break;
+      case WM_ACTIVATEAPP: trace::write_line("WM_ACTIVATEAPP [activate={}, threat={}]", static_cast<bool>(message.wparam()), message.lparam()); break;
       case WM_CANCELMODE: trace::write_line("WM_CANCELMODE"); break;
       case WM_CLOSE: trace::write_line("WM_CLOSE"); break;
-      case WM_COMMAND: trace::write_line(strings::format("WM_COMMAND [type=0x{:X8}, control={}]", message.wparam(), message.lparam())); break;
-      case WM_CREATE: trace::write_line(strings::format("WM_CREATE [CREATESTRUCT={}]", message.lparam())); break;
+      case WM_COMMAND: trace::write_line("WM_COMMAND [type=0x{:X8}, control={}]", message.wparam(), message.lparam()); break;
+      case WM_CREATE: trace::write_line("WM_CREATE [CREATESTRUCT={}]", message.lparam()); break;
       case WM_DESTROY: trace::write_line("WM_DESTROY"); break;
       case WM_ENTERSIZEMOVE: trace::write_line("WM_ENTERSIZEMOVE"); break;
       case WM_EXITSIZEMOVE: trace::write_line("WM_EXITSIZEMOVE"); break;
-      case WM_GETTEXT: trace::write_line(strings::format("WM_GETTEXT [size={}, buffer={}]", message.wparam(), message.lparam())); break;
+      case WM_GETTEXT: trace::write_line("WM_GETTEXT [size={}, buffer={}]", message.wparam(), message.lparam()); break;
       case WM_GETTEXTLENGTH: trace::write_line("WM_GETTEXTLENGTH"); break;
       case WM_KILLFOCUS: trace::write_line("WM_KILLFOCUS"); break;
-      case WM_LBUTTONDBLCLK: trace::write_line(strings::format("WM_LBUTTONDBLCLK [Buttons={}, x={}, y={}]", message.wparam(), LOWORD(message.lparam()), HIWORD(message.lparam()))); break;
-      case WM_LBUTTONDOWN: trace::write_line(strings::format("WM_LBUTTONDOWN [Buttons={}, x={}, y={}]", message.wparam(), LOWORD(message.lparam()), HIWORD(message.lparam()))); break;
-      case WM_LBUTTONUP: trace::write_line(strings::format("WM_LBUTTONUP [Buttons={}, x={}, y={}]", message.wparam(), LOWORD(message.lparam()), HIWORD(message.lparam()))); break;
-      case WM_MBUTTONDBLCLK: trace::write_line(strings::format("WM_MBUTTONDBLCLK [Buttons={}, x={}, y={}]", message.wparam(), LOWORD(message.lparam()), HIWORD(message.lparam()))); break;
-      case WM_MBUTTONDOWN: trace::write_line(strings::format("WM_MBUTTONDOWN [Buttons={}, x={}, y={}]", message.wparam(), LOWORD(message.lparam()), HIWORD(message.lparam()))); break;
-      case WM_MBUTTONUP: trace::write_line(strings::format("WM_MBUTTONUP [Buttons={}, x={}, y={}]", message.wparam(), LOWORD(message.lparam()), HIWORD(message.lparam()))); break;
-      case WM_MOVE: trace::write_line(strings::format("WM_MOVE [x={}, y={}]", LOWORD(message.lparam()), HIWORD(message.lparam()))); break;
+      case WM_LBUTTONDBLCLK: trace::write_line("WM_LBUTTONDBLCLK [Buttons={}, x={}, y={}]", message.wparam(), LOWORD(message.lparam()), HIWORD(message.lparam())); break;
+      case WM_LBUTTONDOWN: trace::write_line("WM_LBUTTONDOWN [Buttons={}, x={}, y={}]", message.wparam(), LOWORD(message.lparam()), HIWORD(message.lparam())); break;
+      case WM_LBUTTONUP: trace::write_line("WM_LBUTTONUP [Buttons={}, x={}, y={}]", message.wparam(), LOWORD(message.lparam()), HIWORD(message.lparam())); break;
+      case WM_MBUTTONDBLCLK: trace::write_line("WM_MBUTTONDBLCLK [Buttons={}, x={}, y={}]", message.wparam(), LOWORD(message.lparam()), HIWORD(message.lparam())); break;
+      case WM_MBUTTONDOWN: trace::write_line("WM_MBUTTONDOWN [Buttons={}, x={}, y={}]", message.wparam(), LOWORD(message.lparam()), HIWORD(message.lparam())); break;
+      case WM_MBUTTONUP: trace::write_line("WM_MBUTTONUP [Buttons={}, x={}, y={}]", message.wparam(), LOWORD(message.lparam()), HIWORD(message.lparam())); break;
+      case WM_MOVE: trace::write_line("WM_MOVE [x={}, y={}]", LOWORD(message.lparam()), HIWORD(message.lparam())); break;
       case WM_MOUSEENTER: trace::write_line("WM_MOUSEENTER"); break;
-      case WM_MOUSEHWHEEL: trace::write_line(strings::format("WM_MOUSEHWHEEL [Buttons={}, delta={}, x={}, y={}]", LOWORD(message.wparam()), HIWORD(message.wparam()), LOWORD(message.lparam()), HIWORD(message.lparam()))); break;
+      case WM_MOUSEHWHEEL: trace::write_line("WM_MOUSEHWHEEL [Buttons={}, delta={}, x={}, y={}]", LOWORD(message.wparam()), HIWORD(message.wparam()), LOWORD(message.lparam()), HIWORD(message.lparam())); break;
       case WM_MOUSELEAVE: trace::write_line("WM_MOUSELEAVE"); break;
-      case WM_MOUSEMOVE: trace::write_line(strings::format("WM_MOUSEMOVE [Buttons={}, x={}, y={}]", message.wparam(), LOWORD(message.lparam()), HIWORD(message.lparam()))); break;
-      case WM_MOUSEWHEEL: trace::write_line(strings::format("WM_MOUSEWHEEL [Buttons={}, delta={}, x={}, y={}]", LOWORD(message.wparam()), HIWORD(message.wparam()), LOWORD(message.lparam()), HIWORD(message.lparam()))); break;
+      case WM_MOUSEMOVE: trace::write_line("WM_MOUSEMOVE [Buttons={}, x={}, y={}]", message.wparam(), LOWORD(message.lparam()), HIWORD(message.lparam())); break;
+      case WM_MOUSEWHEEL: trace::write_line("WM_MOUSEWHEEL [Buttons={}, delta={}, x={}, y={}]", LOWORD(message.wparam()), HIWORD(message.wparam()), LOWORD(message.lparam()), HIWORD(message.lparam())); break;
       case WM_PAINT: trace::write_line("WM_PAINT"); break;
-      case WM_RBUTTONDBLCLK: trace::write_line(strings::format("WM_RBUTTONDBLCLK [Buttons={}, x={}, y={}]", message.wparam(), LOWORD(message.lparam()), HIWORD(message.lparam()))); break;
-      case WM_RBUTTONDOWN: trace::write_line(strings::format("WM_RBUTTONDOWN [Buttons={}, x={}, y={}]", message.wparam(), LOWORD(message.lparam()), HIWORD(message.lparam()))); break;
-      case WM_RBUTTONUP: trace::write_line(strings::format("WM_RBUTTONUP [Buttons={}, x={}, y={}]", message.wparam(), LOWORD(message.lparam()), HIWORD(message.lparam()))); break;
+      case WM_RBUTTONDBLCLK: trace::write_line("WM_RBUTTONDBLCLK [Buttons={}, x={}, y={}]", message.wparam(), LOWORD(message.lparam()), HIWORD(message.lparam())); break;
+      case WM_RBUTTONDOWN: trace::write_line("WM_RBUTTONDOWN [Buttons={}, x={}, y={}]", message.wparam(), LOWORD(message.lparam()), HIWORD(message.lparam())); break;
+      case WM_RBUTTONUP: trace::write_line("WM_RBUTTONUP [Buttons={}, x={}, y={}]", message.wparam(), LOWORD(message.lparam()), HIWORD(message.lparam())); break;
       case WM_SETFOCUS: trace::write_line("WM_SETFOCUS"); break;
-      case WM_SETTEXT: trace::write_line(strings::format("WM_SETTEXT [text=\"{}\"]", reinterpret_cast<char*>(message.lparam()))); break;
-      case WM_SHOWWINDOW: trace::write_line(strings::format("WM_SHOWWINDOW [show={}]", static_cast<bool>(message.wparam()))); break;
-      case WM_SIZE: trace::write_line(strings::format("WM_SIZE [type={}, width={}, heignt={}]", message.wparam(), LOWORD(message.lparam()), HIWORD(message.lparam()))); break;
-      case WM_XBUTTONDBLCLK: trace::write_line(strings::format("WM_XBUTTONDBLCLK [Buttons={}, x={}, y={}]", message.wparam(), LOWORD(message.lparam()), HIWORD(message.lparam()))); break;
-      case WM_XBUTTONDOWN: trace::write_line(strings::format("WM_XBUTTONDOWN [Buttons={}, x={}, y={}]", message.wparam(), LOWORD(message.lparam()), HIWORD(message.lparam()))); break;
-      case WM_XBUTTONUP: trace::write_line(strings::format("WM_XBUTTONUP [Buttons={}, x={}, y={}]", message.wparam(), LOWORD(message.lparam()), HIWORD(message.lparam()))); break;
-      default: trace::write_line(strings::format("[{}]", message)); break;
+      case WM_SETTEXT: trace::write_line("WM_SETTEXT [text=\"{}\"]", reinterpret_cast<char*>(message.lparam())); break;
+      case WM_SHOWWINDOW: trace::write_line("WM_SHOWWINDOW [show={}]", static_cast<bool>(message.wparam())); break;
+      case WM_SIZE: trace::write_line("WM_SIZE [type={}, width={}, heignt={}]", message.wparam(), LOWORD(message.lparam()), HIWORD(message.lparam())); break;
+      case WM_XBUTTONDBLCLK: trace::write_line("WM_XBUTTONDBLCLK [Buttons={}, x={}, y={}]", message.wparam(), LOWORD(message.lparam()), HIWORD(message.lparam())); break;
+      case WM_XBUTTONDOWN: trace::write_line("WM_XBUTTONDOWN [Buttons={}, x={}, y={}]", message.wparam(), LOWORD(message.lparam()), HIWORD(message.lparam())); break;
+      case WM_XBUTTONUP: trace::write_line("WM_XBUTTONUP [Buttons={}, x={}, y={}]", message.wparam(), LOWORD(message.lparam()), HIWORD(message.lparam())); break;
+      default: trace::write_line("[{}]", message); break;
     }
   }
-  
-private:
-  trace_form trace_form_;
 };
 
 int main() {
+  trace_form trace;
   application::run(form1());
 }
--- src/xtd_forms/include/xtd/forms/debug_form.h
+++ src/xtd_forms/include/xtd/forms/debug_form.h
@@ -1,50 +1,29 @@
 #pragma once
-#include "form.h"
+#include "trace_form_base.h"
 #include "icontrol_trace.h"
-#include "text_box.h"
 
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
   /// @brief The xtd::forms namespace contains classes for creating Windows-based applications that take full advantage of the rich user interface features available in the Microsoft Windows operating system, Apple macOS and Linux like Ubuntu operating system.
   namespace forms {
-    class debug_form : public xtd::forms::icontrol_trace, public xtd::forms::form {
+    class debug_form final : public xtd::forms::icontrol_trace, public xtd::forms::trace_form_base {
     public:
-      debug_form() {
-        xtd::diagnostics::debug::listeners().push_back(listener_);
-        dock(xtd::forms::dock_style::bottom);
-        start_position(xtd::forms::form_start_position::manual);
-        text("Debug form");
-        visible(true);
-
-        text_.dock(xtd::forms::dock_style::fill);
-        text_.multiline(true);
-        text_.parent(*this);
-        text_.read_only(true);
-        text_.word_wrap(false);
-      }
-      
+      debug_form() : trace_form_base("Debug") {xtd::diagnostics::debug::listeners().push_back(listener_);}
       /// @cond
       ~debug_form() {xtd::diagnostics::debug::listeners().erase(std::find(xtd::diagnostics::debug::listeners().begin(), xtd::diagnostics::debug::listeners().end(), listener_));}
       /// @endcond
       
-      xtd::forms::control& dock(xtd::forms::dock_style dock) override {
-        if (dock == xtd::forms::dock_style::left) bounds({xtd::forms::screen::get_working_area(handle_).left(), xtd::forms::screen::get_working_area(handle_).top(), xtd::forms::screen::get_working_area(handle_).width() / 4, xtd::forms::screen::get_working_area(handle_).height()});
-        else if (dock == xtd::forms::dock_style::top) bounds({xtd::forms::screen::get_working_area(handle_).left(), xtd::forms::screen::get_working_area(handle_).top(), xtd::forms::screen::get_working_area(handle_).width(), xtd::forms::screen::get_working_area(handle_).height() / 4});
-        else if (dock == xtd::forms::dock_style::right) bounds({xtd::forms::screen::get_working_area(handle_).left() + xtd::forms::screen::get_working_area(handle_).width() - xtd::forms::screen::get_working_area(handle_).width() / 4, xtd::forms::screen::get_working_area(handle_).top(), xtd::forms::screen::get_working_area(handle_).width() / 4, xtd::forms::screen::get_working_area(handle_).height()});
-        else if (dock == xtd::forms::dock_style::bottom) bounds({xtd::forms::screen::get_working_area(handle_).left(), xtd::forms::screen::get_working_area(handle_).top() + xtd::forms::screen::get_working_area(handle_).height() - xtd::forms::screen::get_working_area(handle_).height() / 4, xtd::forms::screen::get_working_area(handle_).width(), xtd::forms::screen::get_working_area(handle_).height() / 4});
-        else if (dock == xtd::forms::dock_style::fill) bounds({xtd::forms::screen::get_working_area(handle_).left(), xtd::forms::screen::get_working_area(handle_).top(), xtd::forms::screen::get_working_area(handle_).width(), xtd::forms::screen::get_working_area(handle_).height()});
-        return xtd::forms::control::dock(dock);
-      }
+      const xtd::diagnostics::trace_listener& trace_listener() const {return *listener_;}
+      xtd::diagnostics::trace_listener& trace_listener() {return *listener_;}
       
-      void write(const std::string& log_str) override {
+      void write(const std::string& debug) override {
   #if !defined(NDEBUG) || defined(DEBUG) || defined(TRACE)
-        text_.append_text(log_str);
+        write_trace(debug);
   #endif
       }
       void flush() override {}
       
     private:
-      xtd::forms::text_box text_;
       std::shared_ptr<xtd::diagnostics::trace_listener> listener_ = xtd::forms::control_trace_listener::create(*this);
     };
   }
