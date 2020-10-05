ea8f4b04ec020a6ff3c87d11b93075d6e9a137bd
Gammasoft71 <gammasoft71@gmail.com>
2020-10-06 01:24:47 +0200

    Update mouse_events.cpp

--- examples/events/mouse_events/src/mouse_events.cpp
+++ examples/events/mouse_events/src/mouse_events.cpp
@@ -1,8 +1,8 @@
 #define TRACE
 #include <xtd/xtd.forms>
 
+using namespace std;
 using namespace xtd;
-using namespace xtd::diagnostics;
 using namespace xtd::forms;
 
 class form1 : public form {
@@ -11,47 +11,47 @@ public:
     text("Mouse events example");
 
     click += [&] {
-      trace::write_line("click");
+      ctrace << format("click") << endl;
     };
     
     double_click += [&] {
-      trace::write_line("double_click");
+      ctrace << format("double_click") << endl;
     };
     
     mouse_click += [&](control& sender, const mouse_event_args& e) {
-      trace::write_line("mouse_click={{button={}, clicks={}, delta={}, location=[{}], modifier_keys=[{}]}}", e.button(), e.clicks(), e.delta(), e.location(), modifier_keys());
+      ctrace << format("mouse_click={{button={}, clicks={}, delta={}, location=[{}], modifier_keys=[{}]}}", e.button(), e.clicks(), e.delta(), e.location(), modifier_keys()) << endl;
     };
     
     mouse_double_click += [&](control& sender, const mouse_event_args& e) {
-      trace::write_line("mouse_double_click={{button={}, clicks={}, delta={}, location=[{}], modifier_keys=[{}]}}", e.button(), e.clicks(), e.delta(), e.location(), modifier_keys());
+      ctrace << format("mouse_double_click={{button={}, clicks={}, delta={}, location=[{}], modifier_keys=[{}]}}", e.button(), e.clicks(), e.delta(), e.location(), modifier_keys()) << endl;
     };
     
     mouse_down += [&](control& sender, const mouse_event_args& e) {
-      trace::write_line("mouse_down={{button={}, clicks={}, delta={}, location=[{}], modifier_keys=[{}]}}", e.button(), e.clicks(), e.delta(), e.location(), modifier_keys());
+      ctrace << format("mouse_down={{button={}, clicks={}, delta={}, location=[{}], modifier_keys=[{}]}}", e.button(), e.clicks(), e.delta(), e.location(), modifier_keys()) << endl;
     };
 
     mouse_enter += [&] {
-      trace::write_line("mouse_enter");
+      ctrace << format("mouse_enter") << endl;
     };
     
     mouse_horizontal_wheel += [&](control& sender, const mouse_event_args& e) {
-      trace::write_line("mouse_horizontal_wheel={{button={}, clicks={}, delta={}, location=[{}]}}", e.button(), e.clicks(), e.delta(), e.location());
+      ctrace << format("mouse_horizontal_wheel={{button={}, clicks={}, delta={}, location=[{}]}}", e.button(), e.clicks(), e.delta(), e.location()) << endl;
     };
 
     mouse_leave += [&] {
-      trace::write_line("mouse_leave");
+      ctrace << format("mouse_leave") << endl;
     };
     
     mouse_move += [&](control& sender, const mouse_event_args& e) {
-      trace::write_line("mouse_move={{button={}, clicks={}, delta={}, location=[{}], modifier_keys=[{}]}}", e.button(), e.clicks(), e.delta(), e.location(), modifier_keys());
+      ctrace << format("mouse_move={{button={}, clicks={}, delta={}, location=[{}], modifier_keys=[{}]}}", e.button(), e.clicks(), e.delta(), e.location(), modifier_keys()) << endl;
     };
     
     mouse_up += [&](control& sender, const mouse_event_args& e) {
-      trace::write_line("mouse_up={{button={}, clicks={}, delta={}, location=[{}], modifier_keys=[{}]}}", e.button(), e.clicks(), e.delta(), e.location(), modifier_keys());
+      ctrace << format("mouse_up={{button={}, clicks={}, delta={}, location=[{}], modifier_keys=[{}]}}", e.button(), e.clicks(), e.delta(), e.location(), modifier_keys()) << endl;
     };
     
     mouse_wheel += [&](control& sender, const mouse_event_args& e) {
-      trace::write_line("mouse_wheel={{button={}, clicks={}, delta={}, location=[{}]}}", e.button(), e.clicks(), e.delta(), e.location());
+      ctrace << format("mouse_wheel={{button={}, clicks={}, delta={}, location=[{}]}}", e.button(), e.clicks(), e.delta(), e.location()) << endl;
     };
   }
 };
