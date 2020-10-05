8f7d6a89ed7ad86a078cc9146323092ad4381dd4
Gammasoft71 <gammasoft71@gmail.com>
2020-10-06 01:24:45 +0200

    Update key_events.cpp

--- examples/events/key_events/src/key_events.cpp
+++ examples/events/key_events/src/key_events.cpp
@@ -1,6 +1,8 @@
 #define TRACE
 #include <xtd/xtd.forms>
 
+using namespace std;
+using namespace xtd;
 using namespace xtd::forms;
 
 class form1 : public form {
@@ -9,16 +11,16 @@ public:
     text("Key events example");
     
     key_down += [&](control& sender, key_event_args& e) {
-      xtd::diagnostics::trace::write_line("key_down={{key_code={}, key_data=[{}], value=0x{:X4}, modifiers=[{}]}}", e.key_code(), e.key_data(), e.key_value(), e.modifiers());
+      ctrace << format("key_down={{key_code={}, key_data=[{}], value=0x{:X4}, modifiers=[{}]}}", e.key_code(), e.key_data(), e.key_value(), e.modifiers()) << endl;
     };
-
+    
     key_press += [&](control& sender, key_press_event_args& e) {
-      xtd::diagnostics::trace::write_line("key_press={{key_char={}}}", e.key_char() == 0 ? "[none]" : xtd::strings::format("'{}'", e.key_char()));
+      ctrace << format("key_press={{key_char={}}}", e.key_char() == 0 ? "[none]" : xtd::strings::format("'{}'", e.key_char())) << endl;
     };
-
+    
     key_up += [&](control& sender, key_event_args& e) {
-      xtd::diagnostics::trace::write_line("key_up={{key_code={}, key_data=[{}], value=0x{:X4}, modifiers=[{}]}}", e.key_code(), e.key_data(), e.key_value(), e.modifiers());
-      if (e.modifiers() == keys::none) xtd::diagnostics::trace::write_line();
+      ctrace << format("key_up={{key_code={}, key_data=[{}], value=0x{:X4}, modifiers=[{}]}}", e.key_code(), e.key_data(), e.key_value(), e.modifiers()) << endl;
+      if (e.modifiers() == keys::none) ctrace << endl;
     };
   }
 };
