147744a52cf3094886c6e4f312f220e4ebfadf2b
Gammasoft71 <gammasoft71@gmail.com>
2020-10-05 15:39:16 +0200

    Update __character_formater.h

--- src/xtd_strings/include/xtd/internal/__character_formater.h
+++ src/xtd_strings/include/xtd/internal/__character_formater.h
@@ -14,20 +14,21 @@
 template<typename char_t, typename value_t>
 inline std::basic_string<char_t> __character_formater(const std::basic_string<char_t>& fmt, value_t value, const std::locale& loc) {
   std::basic_string<char_t> result;
-  if (value < 0x80) {
-    result.push_back(static_cast<char>(value));
-  } else  if (value < 0x800) {
-    result.push_back(static_cast<char>((value >> 6) | 0xc0));
-    result.push_back(static_cast<char>((value & 0x3f) | 0x80));
-  } else if (value < 0x10000) {
-    result.push_back(static_cast<char>((value >> 12) | 0xe0));
-    result.push_back(static_cast<char>(((value >> 6) & 0x3f) | 0x80));
-    result.push_back(static_cast<char>((value & 0x3f) | 0x80));
+  char32_t codepoint = value;
+  if (codepoint < 0x80) {
+    result.push_back(static_cast<char>(codepoint));
+  } else  if (codepoint < 0x800) {
+    result.push_back(static_cast<char>((codepoint >> 6) | 0xc0));
+    result.push_back(static_cast<char>((codepoint & 0x3f) | 0x80));
+  } else if (codepoint < 0x10000) {
+    result.push_back(static_cast<char>((codepoint >> 12) | 0xe0));
+    result.push_back(static_cast<char>(((codepoint >> 6) & 0x3f) | 0x80));
+    result.push_back(static_cast<char>((codepoint & 0x3f) | 0x80));
   } else {
-    result.push_back(static_cast<char>((value >> 18) | 0xf0));
-    result.push_back(static_cast<char>(((value >> 12) & 0x3f) | 0x80));
-    result.push_back(static_cast<char>(((value >> 6) & 0x3f) | 0x80));
-    result.push_back(static_cast<char>((value & 0x3f) | 0x80));
+    result.push_back(static_cast<char>((codepoint >> 18) | 0xf0));
+    result.push_back(static_cast<char>(((codepoint >> 12) & 0x3f) | 0x80));
+    result.push_back(static_cast<char>(((codepoint >> 6) & 0x3f) | 0x80));
+    result.push_back(static_cast<char>((codepoint & 0x3f) | 0x80));
   }
   return result;
 }
