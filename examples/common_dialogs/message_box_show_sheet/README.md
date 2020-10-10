8f3fe61e0e90a3860ba057f89cb7ec22b20a0bbd
Gammasoft71 <gammasoft71@gmail.com>
2020-10-10 17:16:12 +0200

    add message_box_show_sheet(...) methods

---
+++ examples/common_dialogs/message_box_show_sheet/README.md
@@ -0,0 +1,37 @@
+# message_box_show_sheet
+
+demonstrates the use of [xtd::forms::message_box](../../../src/xtd_forms/include/xtd/forms/message_box.hpp) dialog.
+
+# Sources
+
+[src/message_box.cpp](src/message_box_show_sheet.cpp)
+
+[CMakeLists.txt](CMakeLists.txt)
+
+# Build and run
+
+Open "Command Prompt" or "Terminal". Navigate to the folder that contains the project and type the following:
+
+```shell
+xtdc run
+```
+
+# Output
+
+## Windows :
+
+![Screenshot](../../../docs/pictures/examples/message_box_w.png)
+
+![Screenshot](../../../docs/pictures/examples/message_box_wd.png)
+
+## macOS :
+
+![Screenshot](../../../docs/pictures/examples/message_box_m.png)
+
+![Screenshot](../../../docs/pictures/examples/message_box_md.png)
+
+## Gnome :
+
+![Screenshot](../../../docs/pictures/examples/message_box_g.png)
+
+![Screenshot](../../../docs/pictures/examples/message_box_gd.png)
--- src/xtd_forms/include/xtd/forms/message_box.h
+++ src/xtd_forms/include/xtd/forms/message_box.h
@@ -7,6 +7,10 @@
 #include "message_box_options.h"
 #include <xtd/static.h>
 
+/// @cond
+struct __xtd_forms_message_box_closed_caller__;
+/// @endcond
+
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
   /// @brief The xtd::forms namespace contains classes for creating Windows-based applications that take full advantage of the rich user interface features available in the Microsoft Windows operating system, Apple macOS and Linux like Ubuntu operating system.
@@ -14,23 +18,58 @@ namespace xtd {
     /// @cond
     class control;
     /// @endcond
-    
+
+    class message_box_closed_event_args : public event_args {
+    public:
+      message_box_closed_event_args() = default;
+      message_box_closed_event_args(forms::dialog_result dialog_result) : dialog_result_(dialog_result) {};
+      
+      /// @cond
+      message_box_closed_event_args(const message_box_closed_event_args& form_closed_event_args) = default;
+      message_box_closed_event_args& operator=(const message_box_closed_event_args& form_closed_event_args) = default;
+      /// @endcond
+      
+      forms::dialog_result dialog_result() const {return this->dialog_result_;}
+      
+    private:
+      forms::dialog_result dialog_result_ = forms::dialog_result::none;
+    };
+
+    using message_box_closed_event_handler = delegate<void(const message_box_closed_event_args& e)>;
+
     class message_box static_ {
     public:
+      static event<message_box, message_box_closed_event_handler> message_box_closed;
+
+      
+      static void show_sheet(const iwin32_window& owner, const std::string& text);
+      
+      static void show_sheet(const iwin32_window& owner, const std::string& text, const std::string& caption);
+      
+      static void show_sheet(const iwin32_window& owner, const std::string& text, const std::string& caption, message_box_buttons buttons);
+      
+      static void show_sheet(const iwin32_window& owner, const std::string& text, const std::string& caption, message_box_buttons buttons, message_box_icon icon);
+      
+      static void show_sheet(const iwin32_window& owner, const std::string& text, const std::string& caption, message_box_buttons buttons, message_box_icon icon, message_box_default_button default_button);
+      
+      static void show_sheet(const iwin32_window& owner, const std::string& text, const std::string& caption, message_box_buttons buttons, message_box_icon icon, message_box_default_button default_button, message_box_options options);
+      
+      static void show_sheet(const iwin32_window& owner, const std::string& text, const std::string& caption, message_box_buttons buttons, message_box_icon icon, message_box_default_button default_button, message_box_options options, bool display_help_button);
+      
       static dialog_result show(const iwin32_window& owner, const std::string& text);
       
       static dialog_result show(const iwin32_window& owner, const std::string& text, const std::string& caption);
       
       static dialog_result show(const iwin32_window& owner, const std::string& text, const std::string& caption, message_box_buttons buttons);
-
+      
       static dialog_result show(const iwin32_window& owner, const std::string& text, const std::string& caption, message_box_buttons buttons, message_box_icon icon);
-
+      
       static dialog_result show(const iwin32_window& owner, const std::string& text, const std::string& caption, message_box_buttons buttons, message_box_icon icon, message_box_default_button default_button);
-
+      
       static dialog_result show(const iwin32_window& owner, const std::string& text, const std::string& caption, message_box_buttons buttons, message_box_icon icon, message_box_default_button default_button, message_box_options options);
-
+      
       static dialog_result show(const iwin32_window& owner, const std::string& text, const std::string& caption, message_box_buttons buttons, message_box_icon icon, message_box_default_button default_button, message_box_options options, bool display_help_button);
-
+      
       static dialog_result show(const std::string& text);
       
       static dialog_result show(const std::string& text, const std::string& caption);
@@ -44,6 +83,12 @@ namespace xtd {
       static dialog_result show(const std::string& text, const std::string& caption, message_box_buttons buttons, message_box_icon icon, message_box_default_button default_button, message_box_options options);
 
       static dialog_result show(const std::string& text, const std::string& caption, message_box_buttons buttons, message_box_icon icon, message_box_default_button default_button, message_box_options options, bool display_help_button);
+      
+    private:
+      friend struct ::__xtd_forms_message_box_closed_caller__;
+      static void call_message_box_closed(dialog_result dialog_result) {
+        message_box_closed(dialog_result);
+      }
     };
   }
 }
