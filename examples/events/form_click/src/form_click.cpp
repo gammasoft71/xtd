d1dcb471bbb54a7fd64283969bf2017e0115188c
Gammasoft71 <gammasoft71@gmail.com>
2020-10-14 20:15:52 +0200

    Update form_click.cpp

--- examples/events/form_click/src/form_click.cpp
+++ examples/events/form_click/src/form_click.cpp
@@ -6,9 +6,9 @@ using namespace xtd::forms;
 int main() {
   form form;
   form.text("Click anywhere on the form");
-  form.mouse_click += [](control& sender, const mouse_event_args& e) {
+  form.mouse_click += [&](control& sender, const mouse_event_args& e) {
     if (e.button() == mouse_buttons::left)
-      message_box::show(strings::format("The form is clicked at {}", e.location()), "form_click");
+      message_box::show(form, strings::format("The form is clicked at {}", e.location()), "form_click");
   };
   application::run(form);
 }
