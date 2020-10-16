87a8c5ec72e1e514ad6bfb9988e84ba0d50d54a2
Gammasoft71 <gammasoft71@gmail.com>
2020-10-17 00:59:05 +0200

    Update build_macos.sh

--- scripts/travis/build_macos.sh
+++ scripts/travis/build_macos.sh
@@ -5,7 +5,8 @@ brew install doxygen
 
 # generate and build lib
 git submodule update --init
-mkdir -p build & cd build
+mkdir -p build
+cd build
 cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_TESTS=ON -DCMAKE_INSTALL_PREFIX=~/local ..
 if [ $? -ne 0 ]; then exit -1; fi
 cmake --build . --target install -- -j 8
@@ -13,7 +14,8 @@ if [ $? -ne 0 ]; then exit -1; fi
 cd ..
 
 # generate and build examples
-#mkdir -p build/examples & cd build/examples
+#mkdir -p build/examples
+#cd build/examples
 #cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=~/local ../../examples
 #if [ $? -ne 0 ]; then exit -1; fi
 #cmake --build . -- -j 8
