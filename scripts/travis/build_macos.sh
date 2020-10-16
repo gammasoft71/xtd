bfc4b107e61bdefee2abb3ea2e107a7c78597db8
Gammasoft71 <gammasoft71@gmail.com>
2020-10-16 23:03:33 +0200

    fix scripts

--- scripts/travis/build_macos.sh
+++ scripts/travis/build_macos.sh
@@ -3,29 +3,25 @@
 brew update
 brew install doxygen
 
-# init and update submodule
-git submodule update --init
-
-# create build directory
-mkdir -p build/examples
-pushd build
-
 # generate and build lib
+git submodule update --init
+mkdir -p build & cd build
 cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_TESTS=ON -DCMAKE_INSTALL_PREFIX=~/local ..
 if [ $? -ne 0 ]; then exit -1; fi
 cmake --build . --target install -- -j 8
 if [ $? -ne 0 ]; then exit -1; fi
+cd ..
 
 # generate and build examples
-pushd examples
-cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=~/local ../../examples
-if [ $? -ne 0 ]; then exit -1; fi
-cmake --build . -- -j 8
-if [ $? -ne 0 ]; then exit -1; fi
-popd
+#mkdir -p build/examples & cd build/examples
+#cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=~/local ../../examples
+#if [ $? -ne 0 ]; then exit -1; fi
+#cmake --build . -- -j 8
+#if [ $? -ne 0 ]; then exit -1; fi
+#cd ../..
 
 # run registered unit tests
-ctest -j $(nproc) --output-on-failure --build-config Debug
-if [ $? -ne 0 ]; then exit -1; fi
-
-popd
+#cd build
+#ctest -j $(nproc) --output-on-failure --build-config Debug
+#if [ $? -ne 0 ]; then exit -1; fi
+#cd..
