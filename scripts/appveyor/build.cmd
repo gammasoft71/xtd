25d57a6f79c3f63e5dad795387f5a87a4ca95f12
Gammasoft71 <gammasoft71@gmail.com>
2020-10-17 17:14:59 +0200

    fix build option

--- scripts/appveyor/build.cmd
+++ scripts/appveyor/build.cmd
@@ -8,7 +8,7 @@ cd wxwidgets
 git submodule update --init
 mkdir build_cmake
 cd build_cmake
-cmake .. -G "%BUILD_OPTION%" -DwxBUILD_SHARED=OFF -DCMAKE_INSTALL_PREFIX=%HOMEPATH%/local
+cmake .. -G "Visual Studio 16 2019" -A %BUILD_OPTION% -DwxBUILD_SHARED=OFF -DCMAKE_INSTALL_PREFIX=%HOMEPATH%/local
 cmake --build . --target install --config Debug
 cd ..\..\..\..
 
@@ -16,7 +16,7 @@ cd ..\..\..\..
 mkdir build
 cd build
 git submodule update --init
-cmake .. -G "%BUILD_OPTION%" -DENABLE_TESTS=ON -DCMAKE_INSTALL_PREFIX=%HOMEPATH%/local
+cmake .. -G "Visual Studio 16 2019" -A %BUILD_OPTION% -DENABLE_TESTS=ON -DCMAKE_INSTALL_PREFIX=%HOMEPATH%/local
 if %ERRORLEVEL% NEQ 0 exit 1
 cmake --build . --config Debug --target install
 if %ERRORLEVEL% NEQ 0 exit 1
@@ -25,7 +25,7 @@ cd ..
 :: generate and build examples
 ::mkdir build\examples
 ::cd build\examples
-::cmake ../../examples -G "%BUILD_OPTION%" -DCMAKE_INSTALL_PREFIX=%HOMEPATH%/local
+::cmake ../../examples -G "Visual Studio 16 2019" -A %BUILD_OPTION% -DCMAKE_INSTALL_PREFIX=%HOMEPATH%/local
 ::if %ERRORLEVEL% NEQ 0 exit 1
 ::cmake --build . --config Debug
 ::if %ERRORLEVEL% NEQ 0 exit 1
