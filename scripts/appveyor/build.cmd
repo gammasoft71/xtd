42af34bf6aed13fd6b806dc7c032f6747167b0f1
Gammasoft71 <gammasoft71@gmail.com>
2020-10-17 13:35:35 +0200

    review appveyor scripts

--- scripts/appveyor/build.cmd
+++ scripts/appveyor/build.cmd
@@ -1,23 +1,33 @@
 :: cmd
 
-:: init and update submodule
+:: clone, generate and build wxwidgets 3.1.4
+mkdir build\thirdparty
+cd build\thirdparty
+git clone https://github.com/wxwidgets/wxwidgets.git -b v3.1.4 --depth 1
+cd wxwidgets
 git submodule update --init
-
-mkdir build\examples
-cd build
+mkdir build_cmake 
+cd build_cmake
+cmake .. -G "CodeBlocks - Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DwxBUILD_SHARED=OFF -DCMAKE_INSTALL_PREFIX=%HOMEPATH%/local
+cmake --build . -- -j8
+cmake --build . --target install
+cd ../../../..
 
 :: generate and build lib
+git submodule update --init
+mkdir build
+cd build
 cmake .. -G "%BUILD_OPTION%" -DENABLE_TESTS=ON -DCMAKE_INSTALL_PREFIX=%HOMEPATH%/local
 if %ERRORLEVEL% NEQ 0 exit 1
 cmake --build . --config Debug --target install
 if %ERRORLEVEL% NEQ 0 exit 1
+cd ..
 
 :: generate and build examples
-cd examples
-cmake ../../examples -G "%BUILD_OPTION%" -DCMAKE_INSTALL_PREFIX=%HOMEPATH%/local
-if %ERRORLEVEL% NEQ 0 exit 1
-cmake --build . --config Debug
-if %ERRORLEVEL% NEQ 0 exit 1
-cd ..
-
-cd ..
+::mkdir build\examples
+::cd examples
+::cmake ../../examples -G "%BUILD_OPTION%" -DCMAKE_INSTALL_PREFIX=%HOMEPATH%/local
+::if %ERRORLEVEL% NEQ 0 exit 1
+::cmake --build . --config Debug
+::if %ERRORLEVEL% NEQ 0 exit 1
+::cd ..\..
--- scripts/appveyor/test.cmd
+++ scripts/appveyor/test.cmd
@@ -1,7 +1,7 @@
 :: cmd
 
 :: run registered unit tests
-cd build
-ctest --output-on-failure --build-config Debug
-if %ERRORLEVEL% NEQ 0 exit 1
-cd ..
+::cd build
+::ctest --output-on-failure --build-config Debug
+::if %ERRORLEVEL% NEQ 0 exit 1
+::cd ..
