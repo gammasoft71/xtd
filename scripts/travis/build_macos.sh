bd2e4a81f4ca00658342713e59998ca80c14bb7f
Gammasoft71 <gammasoft71@gmail.com>
2020-10-18 15:17:03 +0200

    Update build_macos.sh

--- scripts/travis/build_macos.sh
+++ scripts/travis/build_macos.sh
@@ -6,20 +6,20 @@ git clone https://github.com/wxwidgets/wxwidgets.git -b v3.1.4 --depth 1
 cd wxwidgets
 git submodule update --init
 mkdir build_cmake && cd build_cmake
-cmake .. -G "CodeBlocks - Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DwxBUILD_SHARED=OFF -DCMAKE_INSTALL_PREFIX=~/local
+cmake ..  -G "Xcode" -DwxBUILD_SHARED=OFF -DCMAKE_INSTALL_PREFIX=~/local
 if [ $? -ne 0 ]; then exit -1; fi
-cmake --build . --target install -- -j $(nproc) -- -quiet
+cmake --build . --config Debug --target install -- -quiet
 if [ $? -ne 0 ]; then exit -1; fi
 cd ../../../..
 
 # generate and build lib
 git submodule update --init
 mkdir -p build && cd build
-cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_TESTS=ON -DENABLE_XTD_COMMAND_LINE=OFF -DCMAKE_CXX_COMPILER=g++-9 -DCMAKE_INSTALL_PREFIX=~/local ..
+cmake .. -G "Xcode" -DCMAKE_INSTALL_PREFIX=~/local
 if [ $? -ne 0 ]; then exit -1; fi
-cmake --build . -- -j $(nproc)
+cmake --build . --config Debug --target
 if [ $? -ne 0 ]; then exit -1; fi
-#cmake --build . --target install -- -quiet
+#cmake --build . --config Debug --target install -- -quiet
 #if [ $? -ne 0 ]; then exit -1; fi
 cd ..
 
