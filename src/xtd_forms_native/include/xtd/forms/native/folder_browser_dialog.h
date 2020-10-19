6a13f12f1702202d1a049fb3421982ef7b41e2b6
Gammasoft71 <gammasoft71@gmail.com>
2020-10-20 00:01:31 +0200

    add folder_browser show_sheet method

--- src/xtd_forms_native/include/xtd/forms/native/folder_browser_dialog.h
+++ src/xtd_forms_native/include/xtd/forms/native/folder_browser_dialog.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <cstdint>
 #include <string>
+#include <xtd/delegate.h>
 #include <xtd/environment.h>
 #include <xtd/static.h>
 
@@ -10,6 +11,7 @@ namespace xtd {
       class folder_browser_dialog static_ {
       public:
         static bool run_dialog(intptr_t hwnd, const std::string& description, environment::special_folder root_folder, std::string& selected_path, size_t options);
+        static void run_sheet(xtd::delegate<void(bool)> on_dialog_closed, intptr_t hwnd, const std::string& description, environment::special_folder root_folder, std::string& selected_path, size_t options);
       };
     }
   }
