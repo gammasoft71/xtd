a3876c4c3d8691a76e64f6afb114215e065c3446
Gammasoft71 <gammasoft71@gmail.com>
2020-10-22 16:23:02 +0200

    add docs

--- src/xtd_delegates/include/xtd/predicate.h
+++ src/xtd_delegates/include/xtd/predicate.h
@@ -4,6 +4,7 @@
 
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
+  /// @brief Represents a delagate that defines a set of criteria and determines whether the specified object meets those criteria.
   template<typename type_t>
   using predicate = delegate<bool(type_t)>;
 }
