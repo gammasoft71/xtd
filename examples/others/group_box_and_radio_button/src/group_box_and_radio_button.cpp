5a4fa2a33c6ea75da8e0da182704656a06ef0431
Gammasoft71 <gammasoft71@gmail.com>
2020-10-24 23:08:26 +0200

    Update group_box_and_radio_button.cpp

--- examples/others/group_box_and_radio_button/src/group_box_and_radio_button.cpp
+++ examples/others/group_box_and_radio_button/src/group_box_and_radio_button.cpp
@@ -18,28 +18,28 @@ public:
     group_box2.text("Group 2");
     
     radio_button1.checked(true);
-    radio_button1.location({20, 20});
+    radio_button1.location({20, 10});
     radio_button1.parent(group_box1);
     radio_button1.text("radio 1");
     
-    radio_button2.location({20, 50});
+    radio_button2.location({20, 40});
     radio_button2.parent(group_box1);
     radio_button2.text("radio 2");
 
-    radio_button3.location({20, 80});
+    radio_button3.location({20, 70});
     radio_button3.parent(group_box1);
     radio_button3.text("radio 3");
 
-    radio_button4.location({20, 20});
+    radio_button4.location({20, 10});
     radio_button4.parent(group_box2);
     radio_button4.text("radio 4");
 
     radio_button5.checked(true);
-    radio_button5.location({20, 50});
+    radio_button5.location({20, 40});
     radio_button5.parent(group_box2);
     radio_button5.text("radio 5");
 
-    radio_button6.location({20, 80});
+    radio_button6.location({20, 70});
     radio_button6.parent(group_box2);
     radio_button6.text("radio 6");
   }
