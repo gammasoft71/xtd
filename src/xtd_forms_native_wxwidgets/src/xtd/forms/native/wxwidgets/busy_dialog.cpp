5793ceff809f93789e26272b8f55f8ad42596f25
Gammasoft71 <gammasoft71@gmail.com>
2020-10-29 00:36:23 +0100

    Update busy_dialog.cpp

--- src/xtd_forms_native_wxwidgets/src/xtd/forms/native/wxwidgets/busy_dialog.cpp
+++ src/xtd_forms_native_wxwidgets/src/xtd/forms/native/wxwidgets/busy_dialog.cpp
@@ -2,14 +2,35 @@
 #include <xtd/forms/native/application.h>
 #include <xtd/forms/native/busy_dialog.h>
 #include "../../../../../include/xtd/forms/native/wxwidgets/control_handler.h"
-#include <wx/busyinfo.h>
 #include <wx/iconbndl.h>
+
+// Workaround : with wxWidgets version <= 3.1.4 wxBusyInfo does not shows hourglass cursor on macOS ==> set public m_InfoFrame...
+#if !defined(__APPLE__)
+#include <wx/busyinfo.h>
+#else
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wkeyword-macro"
+#define private public
+#include <wx/busyinfo.h>
+#undef private
+#pragma clang diagnostic pop
+#endif
 
 using namespace xtd;
 using namespace xtd::forms::native;
 
 intptr_t busy_dialog::create(intptr_t hwnd, const xtd::drawing::icon& icon, const std::string& text, const std::string& description, const xtd::drawing::color& back_color, const xtd::drawing::color& fore_color, double transparency) {
-  return reinterpret_cast<intptr_t>(new wxBusyInfo(wxBusyInfoFlags().Parent(hwnd ? reinterpret_cast<control_handler*>(hwnd)->control() : nullptr).Icon(icon.handle() ? reinterpret_cast<wxIconBundle*>(icon.handle())->GetIcon() : wxNullIcon).Title(text).Label(description).Background(wxColour(back_color.r(), back_color.g(), back_color.b(), back_color.a())).Foreground(wxColour(fore_color.r(), fore_color.g(), fore_color.b(), fore_color.a())).Transparency(255.0 * transparency)));
+  auto dialog = new wxBusyInfo(wxBusyInfoFlags().Parent(hwnd ? reinterpret_cast<control_handler*>(hwnd)->control() : nullptr).Icon(icon.handle() ? reinterpret_cast<wxIconBundle*>(icon.handle())->GetIcon() : wxNullIcon).Title(text).Label(description).Background(wxColour(back_color.r(), back_color.g(), back_color.b(), back_color.a())).Foreground(wxColour(fore_color.r(), fore_color.g(), fore_color.b(), fore_color.a())).Transparency(255.0 * transparency));
+#if defined(__APPLE__)
+  std::function<void(wxWindow*, const wxCursor*)> set_cursor = [&](wxWindow* window, const wxCursor* cursor) {
+    if (!window) return;
+    for (auto child : window->GetChildren())
+      set_cursor(child, cursor);
+    window->SetCursor(*cursor);
+  };
+  set_cursor(dialog->m_InfoFrame, wxHOURGLASS_CURSOR);
+#endif
+  return reinterpret_cast<intptr_t>(dialog);
 }
 
 void busy_dialog::destroy(intptr_t dialog) {
