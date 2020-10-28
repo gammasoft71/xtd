9de73485d23c16186822aaf9abe51ee915a24674
Gammasoft <gammasoft71@gmail.com>
2020-10-28 22:29:55 +0100

    fix warnings...

--- src/xtd_console/include/xtd/__get_err_rdbuf.h
+++ src/xtd_console/include/xtd/__get_err_rdbuf.h
@@ -5,17 +5,17 @@
 
 /// @cond
 template<class char_t>
-inline std::basic_streambuf<char_t>* __get_err_rdbuf() {return nullptr;};
+inline std::basic_streambuf<char_t>* __get_err_rdbuf() {return nullptr;}
 
 template<>
 inline std::basic_streambuf<char>* __get_err_rdbuf<char>() {
   static std::basic_streambuf<char>* rdbuf = std::cerr.rdbuf();
   return rdbuf;
-};
+}
 
 template<>
 inline std::basic_streambuf<wchar_t>* __get_err_rdbuf<wchar_t>() {
   static std::basic_streambuf<wchar_t>* rdbuf = std::wcerr.rdbuf();
   return rdbuf;
-};
+}
 /// @endcond
