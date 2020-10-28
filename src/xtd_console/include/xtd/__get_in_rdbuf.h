e80cda90375cb738d3fb077b1dd85f30dd05d5f4
Gammasoft <gammasoft71@gmail.com>
2020-10-28 22:29:55 +0100

    fix warnings...

--- src/xtd_console/include/xtd/__get_in_rdbuf.h
+++ src/xtd_console/include/xtd/__get_in_rdbuf.h
@@ -5,17 +5,17 @@
 
 /// @cond
 template<class char_t>
-inline std::basic_streambuf<char_t>* __get_in_rdbuf() {return nullptr;};
+inline std::basic_streambuf<char_t>* __get_in_rdbuf() {return nullptr;}
 
 template<>
 inline std::basic_streambuf<char>* __get_in_rdbuf<char>() {
   static std::basic_streambuf<char>* rdbuf = std::cin.rdbuf();
   return rdbuf;
-};
+}
 
 template<>
 inline std::basic_streambuf<wchar_t>* __get_in_rdbuf<wchar_t>() {
   static std::basic_streambuf<wchar_t>* rdbuf = std::wcin.rdbuf();
   return rdbuf;
-};
+}
 /// @endcond
--- src/xtd_console/include/xtd/__get_out_rdbuf.h
+++ src/xtd_console/include/xtd/__get_out_rdbuf.h
@@ -5,17 +5,17 @@
 
 /// @cond
 template<class char_t>
-inline std::basic_streambuf<char_t>* __get_out_rdbuf() {return nullptr;};
+inline std::basic_streambuf<char_t>* __get_out_rdbuf() {return nullptr;}
 
 template<>
 inline std::basic_streambuf<char>* __get_out_rdbuf<char>() {
   static std::basic_streambuf<char>* rdbuf = std::cout.rdbuf();
   return rdbuf;
-};
+}
 
 template<>
 inline std::basic_streambuf<wchar_t>* __get_out_rdbuf<wchar_t>() {
   static std::basic_streambuf<wchar_t>* rdbuf = std::wcout.rdbuf();
   return rdbuf;
-};
+}
 /// @endcond
