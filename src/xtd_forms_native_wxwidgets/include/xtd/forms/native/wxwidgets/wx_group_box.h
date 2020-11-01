e9bd46e553677b2c7a0855a6b6d8803ac9282298
Gammasoft <gammasoft71@gmail.com>
2020-11-02 00:26:12 +0100

    Update wx_group_box.h

--- src/xtd_forms_native_wxwidgets/include/xtd/forms/native/wxwidgets/wx_group_box.h
+++ src/xtd_forms_native_wxwidgets/include/xtd/forms/native/wxwidgets/wx_group_box.h
@@ -105,7 +105,7 @@ namespace xtd {
         
         static long style_to_wx_style(size_t style, size_t ex_style) {
           long wx_style = 0;
-          return wx_style | common_window_style_to_wx_style(style, ex_style);
+          return wx_style;
         }
 
         bool owner_draw_ = false;
