a92e8914f505aad13f0426590da1318c19c72b54
Gammasoft71 <gammasoft71@gmail.com>
2020-11-03 10:35:24 +0100

    Update switch_button.cpp

--- examples/common_controls/switch_button/src/switch_button.cpp
+++ examples/common_controls/switch_button/src/switch_button.cpp
@@ -9,54 +9,29 @@ public:
   form1() {
     text("Switch button example");
     
+    switch_button1.auto_check(false);
     switch_button1.parent(*this);
     switch_button1.location({50, 30});
-    switch_button1.checked_changed += [&] {
-      label1.text(strings::format("switch: {}", switch_button1.checked() ? "ON" : "OFF"));
+    switch_button1.click += [&] {
+      //switch_button1.checked(!switch_button1.checked());
     };
 
     switch_button2.parent(*this);
-    switch_button2.auto_check(false);
-    switch_button2.checked(true);
     switch_button2.location({50, 70});
-    switch_button2.click += [&] {
-      //switch_button2.checked(!switch_button2.checked());
+    switch_button2.checked_changed += [&] {
+      label1.text(strings::format("switch: {}", switch_button2.checked() ? "On" : "Off"));
     };
-    
-    switch_button3.parent(*this);
-    switch_button3.location({170, 30});
-    switch_button3.rounded(true);
-    switch_button3.checked_changed += [&] {
-      label2.text(strings::format("switch: {}", switch_button3.checked() ? "ON" : "OFF"));
-    };
-
-    switch_button4.parent(*this);
-    switch_button4.auto_check(false);
-    switch_button4.checked(true);
-    switch_button4.location({170, 70});
-    switch_button4.rounded(true);
-    switch_button4.click += [&] {
-      //switch_button4.checked(!switch_button4.checked());
-    };
+    switch_button2.checked(true);
 
     label1.parent(*this);
     label1.location({50, 110});
     label1.auto_size(true);
-    label1.text("switch: OFF");
-
-    label2.parent(*this);
-    label2.location({170, 110});
-    label2.auto_size(true);
-    label2.text("switch: OFF");
   }
   
 private:
   switch_button switch_button1;
   switch_button switch_button2;
-  switch_button switch_button3;
-  switch_button switch_button4;
   label label1;
-  label label2;
 };
 
 int main() {
