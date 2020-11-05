f905f9d41b733d47a25da007a2e73507a222cf9e
gammasoft71 <gammasoft71@gmail.com>
2020-11-05 19:59:35 +0100

    Update default_trace_listener_win32.cpp

--- src/xtd_diagnostics/src/default_trace_listener_win32.cpp
+++ src/xtd_diagnostics/src/default_trace_listener_win32.cpp
@@ -2,6 +2,8 @@
 
 #if defined(_WIN32)
 #include <windows.h>
+#undef max
+#undef min
 
 void xtd::diagnostics::default_trace_listener::write_to_output_debug(const std::string& message) {
   OutputDebugStringA(message.c_str());
