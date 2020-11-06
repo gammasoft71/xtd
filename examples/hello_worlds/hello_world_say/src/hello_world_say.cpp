4ebf4b1d11651d0331e5ae6eb4a703bc5517cc7a
Gammasoft71 <gammasoft71@gmail.com>
2020-11-06 16:45:33 +0100

    Update hello_world_say.cpp

--- examples/hello_worlds/hello_world_say/src/hello_world_say.cpp
+++ examples/hello_worlds/hello_world_say/src/hello_world_say.cpp
@@ -10,13 +10,34 @@ using namespace xtd::forms;
 class form1 : public form {
 public:
   form1() {
+    if (environment::os_version().is_windows_platform())
+      file::write_all_lines(temp_directory_path()/"say.cmd", {
+        "@echo off",
+        "echo Dim Speak >> %TEMP%\\speak.vbs",
+        "echo Set Speak=CreateObject(\"sapi.spvoice\") >> %TEMP%\\speak.vbs",
+        "echo Speak.Speak %* >> %TEMP%\\speak.vbs",
+        "%TEMP%\\speak.vbs",
+        "del %TEMP%\\speak.vbs"
+      });
+    else if (environment::os_version().is_linux_platform())
+      file::write_all_lines(temp_directory_path()/"say.cmd", {
+        "#!/bin/bash",
+        "spd-say \"$*\""
+      });
+    else if (environment::os_version().is_macos_platform())
+      file::write_all_lines(temp_directory_path()/"say.cmd", {
+        "#!/bin/bash",
+        "say \"$*\""
+      });
+    permissions(temp_directory_path()/"say.cmd", perms::owner_all);
+    
     text("Hello world (say)");
     
     button1.location({10, 10});
     button1.parent(*this);
     button1.text("Say...");
     button1.click += [&] {
-      process::start(process_start_info().file_name(temp_directory_path()/"say.cmd").arguments("\"Hello world\"").window_style(process_window_style::hidden)).wait_for_exit();
+      process::start(process_start_info().file_name(temp_directory_path()/"say.cmd").arguments("\"Hello, world!\"").window_style(process_window_style::hidden));
     };
   }
   
@@ -25,27 +46,5 @@ private:
 };
 
 int main() {
-  std::vector<std::string> lines;
-  if (environment::os_version().is_windows_platform()) lines = {
-    "@echo off",
-    "echo Dim Speak >> %TEMP%\\speak.vbs",
-    "echo Set Speak=CreateObject(\"sapi.spvoice\") >> %TEMP%\\speak.vbs",
-    "echo Speak.Speak %* >> %TEMP%\\speak.vbs",
-    "%TEMP%\\speak.vbs",
-    "del %TEMP%\\speak.vbs"
-  };
-  else if (environment::os_version().is_linux_platform()) lines = {
-    "#!/bin/bash",
-    "spd-say \"$*\""
-  };
-  else if (environment::os_version().is_macos_platform()) lines = {
-    "#!/bin/bash",
-    "say \"$*\""
-  };
-  file::write_all_lines(temp_directory_path()/"say.cmd", lines);
-  permissions(temp_directory_path()/"say.cmd", perms::owner_all);
-  
   application::run(form1());
-
-  remove(temp_directory_path()/"say.cmd");
 }
