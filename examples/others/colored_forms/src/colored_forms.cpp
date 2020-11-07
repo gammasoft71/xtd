c704b0de28196dd0a377e8b64668e81de9cee466
Gammasoft71 <gammasoft71@gmail.com>
2020-11-07 13:49:18 +0100

    Update colored_forms.cpp

--- examples/others/colored_forms/src/colored_forms.cpp
+++ examples/others/colored_forms/src/colored_forms.cpp
@@ -7,11 +7,13 @@ using namespace xtd::drawing;
 using namespace xtd::forms;
 
 int main() {
-  vector<shared_ptr<form>> forms;
-  auto form_color = known_color::alice_blue;
+  auto form_main = control::create<forms::form>("Main Form", {screen::primary_screen().working_area().width() - 340, 40});
+  form_main->start_position(form_start_position::manual);
 
-  auto button = control::create<forms::button>("Create", {10, 10});
+  auto button = control::create<forms::button>(*form_main, "Create", {10, 10});
   button->click += [&] {
+    static vector<shared_ptr<form>> forms;
+    static auto form_color = known_color::alice_blue;
     auto form = control::create<forms::form>(strings::format("{}", form_color));
     form->back_color(color::from_known_color(form_color));
     form->visible(true);
@@ -19,8 +21,5 @@ int main() {
     form_color = form_color != known_color::yellow_green ? static_cast<known_color>(static_cast<int>(form_color) + 1) : known_color::alice_blue;
   };
 
-  auto form_main = control::create<forms::form>("Main Form", {screen::primary_screen().working_area().width() - 340, 40});
-  form_main->start_position(form_start_position::manual);
-  form_main->controls().push_back(*button);
   application::run(*form_main);
 }
