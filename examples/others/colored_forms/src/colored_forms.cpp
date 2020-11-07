3c20e8221a5d62d282fb4b3279b2a06edad00c6e
Gammasoft71 <gammasoft71@gmail.com>
2020-11-07 13:40:50 +0100

    Update colored_forms.cpp

--- examples/others/colored_forms/src/colored_forms.cpp
+++ examples/others/colored_forms/src/colored_forms.cpp
@@ -1,4 +1,3 @@
-#include <memory>
 #include <vector>
 #include <xtd/xtd.forms>
 
@@ -11,22 +10,17 @@ int main() {
   vector<shared_ptr<form>> forms;
   auto form_color = known_color::alice_blue;
 
-  button button;
-  button.text("Create");
-  button.location({10, 10});
-  button.click += [&] {
-    auto form = make_shared<forms::form>();
+  auto button = control::create<forms::button>("Create", {10, 10});
+  button->click += [&] {
+    auto form = control::create<forms::form>(strings::format("{}", form_color));
     form->back_color(color::from_known_color(form_color));
-    form->text(strings::format("{}", form_color));
     form->visible(true);
-    forms.push_back(form);
+    forms.push_back(move(form));
     form_color = form_color != known_color::yellow_green ? static_cast<known_color>(static_cast<int>(form_color) + 1) : known_color::alice_blue;
   };
 
-  form form_main;
-  form_main.text("Main Form");
-  form_main.start_position(form_start_position::manual);
-  form_main.location({screen::primary_screen().working_area().width() - form_main.width() - 40, 40});
-  form_main.controls().push_back(button);
-  application::run(form_main);
+  auto form_main = control::create<forms::form>("Main Form", {screen::primary_screen().working_area().width() - 340, 40});
+  form_main->start_position(form_start_position::manual);
+  form_main->controls().push_back(*button);
+  application::run(*form_main);
 }
