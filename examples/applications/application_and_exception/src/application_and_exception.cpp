4d86253eae40eceb6d038dd69b782707df8de472
Gammasoft71 <gammasoft71@gmail.com>
2020-11-08 23:51:46 +0100

    Update application_and_exception.cpp

--- examples/applications/application_and_exception/src/application_and_exception.cpp
+++ examples/applications/application_and_exception/src/application_and_exception.cpp
@@ -18,7 +18,7 @@ public:
     generate_handled_exception_button.text("Generate handled exception");
     generate_handled_exception_button.click += [] {
       try {
-        throw invalid_argument("Not a valid argument");
+        throw system_exception("Generate handled exception");
       } catch(const exception& e) {
         // do something on exception...
       }
@@ -29,26 +29,37 @@ public:
     generate_exception_button.parent(*this);
     generate_exception_button.text("Generate exception");
     generate_exception_button.click += [] {
-      throw invalid_argument("Not a valid argument");
+      throw invalid_argument("Generate exception");
     };
-    
+
+    generate_system_exception_button.auto_size(true);
+    generate_system_exception_button.location({10, 70});
+    generate_system_exception_button.parent(*this);
+    generate_system_exception_button.text("Generate system exception");
+    generate_system_exception_button.click += [] {
+      throw system_exception("Generate system exception", caller_info_);
+    };
+
     generate_unknown_exception_button.auto_size(true);
-    generate_unknown_exception_button.location({10, 70});
+    generate_unknown_exception_button.location({10, 100});
     generate_unknown_exception_button.parent(*this);
     generate_unknown_exception_button.text("Generate unknown exception");
     generate_unknown_exception_button.click += [] {
-      throw "throw unknown type exception";
+      throw "Generate unknown exception";
     };
   }
 private:
   button generate_handled_exception_button;
   button generate_exception_button;
+  button generate_system_exception_button;
   button generate_unknown_exception_button;
 };
 
 int main() {
   try {
     application::run(main_form());
+  } catch(const system_exception& e) {
+    message_box::show(e.to_string(), strings::format("Exception {} occured", strings::class_name(e)), message_box_buttons::ok, message_box_icon::error);
   } catch(const exception& e) {
     message_box::show(strings::format("Message : {}", e.what()), strings::format("Exception {} occured", strings::class_name(e)), message_box_buttons::ok, message_box_icon::error);
   } catch(...) {
