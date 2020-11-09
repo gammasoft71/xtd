968550eb0e2ca6b3282787e9c7b6cf99e1ce2c71
Gammasoft71 <gammasoft71@gmail.com>
2020-11-09 15:43:53 +0100

    add documentations

--- src/xtd_core/include/xtd/system_exception.h
+++ src/xtd_core/include/xtd/system_exception.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <cstdint>
+#include <optional>
 #include <stdexcept>
 #include <system_error>
 #include <xtd/strings.h>
@@ -13,27 +14,108 @@ namespace xtd {
   class system_exception : public std::exception {
   public:
     /// @brief Create a new instance of class system_exception
-    /// @remarks Message is set with the default message associate to the error.
-    system_exception(const xtd::caller_info& info = xtd::caller_info::empty()) : info_(info) {}
-    explicit system_exception(const std::string& message, const xtd::caller_info& info = xtd::caller_info::empty()) : message_(message), info_(info) {}
-    explicit system_exception(const std::string& message, const std::error_code& error, std::string& help_link, const xtd::caller_info& info = xtd::caller_info::empty()) : message_(message), error_(error), help_link_(help_link), info_(info) {}
-
+    /// @param information (optional) Conatains current information about memeber name, file path and  line number in the file where the exception is occurred. Typically #caller_information_.
+    /// @remarks Message is set with the default message associate to the exception.
+    explicit system_exception(const xtd::caller_info& information = xtd::caller_info::empty()) : message_(default_message_), information_(information) {}
+    /// @brief Create a new instance of class system_exception
+    /// @param message Message string associate to the exception.
+    /// @param information (optional) Conatains current information about memeber name, file path and  line number in the file where the exception is occurred. Typically #caller_information_.
+    explicit system_exception(const std::string& message, const xtd::caller_info& information = xtd::caller_info::empty()) : message_(message), information_(information) {}
+    /// @brief Create a new instance of class system_exception
+    /// @param message Message string associate to the exception.
+    /// @param error Error code associate to the exception.
+    /// @param information (optional) Conatains current information about memeber name, file path and  line number in the file where the exception is occurred. Typically #caller_information_.
+    system_exception(const std::string& message, const std::error_code& error, const xtd::caller_info& information = xtd::caller_info::empty()) : message_(message), error_(error), information_(information) {}
+    /// @brief Create a new instance of class system_exception
+    /// @param message Message string associate to the exception.
+    /// @param help_link Help link string associate to the exception.
+    /// @param information (optional) Conatains current information about memeber name, file path and  line number in the file where the exception is occurred. Typically #caller_information_.
+    system_exception(const std::string& message, const std::string& help_link, const xtd::caller_info& information = xtd::caller_info::empty()) : message_(message), help_link_(help_link), information_(information) {}
+    /// @brief Create a new instance of class system_exception
+    /// @param message Message string associate to the exception.
+    /// @param error Error code associate to the exception.
+    /// @param help_link Help link string associate to the exception.
+    /// @param information (optional) Conatains current information about memeber name, file path and  line number in the file where the exception is occurred. Typically #caller_information_.
+    system_exception(const std::string& message, const std::error_code& error, const std::string& help_link, const xtd::caller_info& information = xtd::caller_info::empty()) : message_(message), error_(error), help_link_(help_link), information_(information) {}
+    /// @brief Create a new instance of class system_exception
+    /// @param inner_exception The exception that is the cause of the current exception.
+    /// @param information (optional) Conatains current information about memeber name, file path and  line number in the file where the exception is occurred. Typically #caller_information_.
+    /// @remarks Message is set with the default message associate to the exception.
+    explicit system_exception(const std::exception& inner_exception, const xtd::caller_info& information = xtd::caller_info::empty()) : inner_exception_(inner_exception), information_(information) {}
+    /// @brief Create a new instance of class system_exception
+    /// @param message Message string associate to the exception.
+    /// @param inner_exception The exception that is the cause of the current exception.
+    /// @param information (optional) Conatains current information about memeber name, file path and  line number in the file where the exception is occurred. Typically #caller_information_.
+    system_exception(const std::string& message, const std::exception& inner_exception, const xtd::caller_info& information = xtd::caller_info::empty()) : message_(message), inner_exception_(inner_exception), information_(information) {}
+    /// @brief Create a new instance of class system_exception
+    /// @param message Message string associate to the exception.
+    /// @param inner_exception The exception that is the cause of the current exception.
+    /// @param error Error code associate to the exception.
+    /// @param information (optional) Conatains current information about memeber name, file path and  line number in the file where the exception is occurred. Typically #caller_information_.
+    system_exception(const std::string& message, const std::exception& inner_exception, const std::error_code& error, const xtd::caller_info& information = xtd::caller_info::empty()) : message_(message), inner_exception_(inner_exception), error_(error), information_(information) {}
+    /// @brief Create a new instance of class system_exception
+    /// @param message Message string associate to the exception.
+    /// @param inner_exception The exception that is the cause of the current exception.
+    /// @param help_link Help link string associate to the exception.
+    /// @param information (optional) Conatains current information about memeber name, file path and  line number in the file where the exception is occurred. Typically #caller_information_.
+    system_exception(const std::string& message, const std::exception& inner_exception, const std::string& help_link, const xtd::caller_info& information = xtd::caller_info::empty()) : message_(message), inner_exception_(inner_exception), help_link_(help_link), information_(information) {}
+    /// @brief Create a new instance of class system_exception
+    /// @param message Message string associate to the exception.
+    /// @param inner_exception The exception that is the cause of the current exception.
+    /// @param error Error code associate to the exception.
+    /// @param help_link Help link string associate to the exception.
+    /// @param information (optional) Conatains current information about memeber name, file path and  line number in the file where the exception is occurred. Typically #caller_information_.
+    system_exception(const std::string& message, const std::exception& inner_exception, const std::error_code& error, const std::string& help_link, const xtd::caller_info& information = xtd::caller_info::empty()) : message_(message), inner_exception_(inner_exception), error_(error), help_link_(help_link), information_(information) {}
+    
     /// @cond
     system_exception(const system_exception&) = default;
     system_exception& operator=(const system_exception&) = default;
     /// @endcond
     
-    xtd::caller_info caller_info() const {return info_;}
+    /// @brief Get file path where exception occurred
+    /// @return A string represent file path where exception occurred
+    virtual const std::string& file_path() const noexcept {return information_.file_path();}
     
-    const std::string& help_link() const noexcept {return help_link_;}
+    /// @brief Gets a link to the help file associated with this exception.
+    /// @return A string represent a link to Help file associated with sxception
+    virtual const std::string& help_link() const noexcept {return help_link_;}
     
-    std::error_code error() const noexcept {return error_;}
-
-    std::string name() const {return xtd::strings::class_name(*this);}
+    /// @brief Get error associate to the exception
+    /// @return An error_code represent a Error associate to the exception
+    virtual std::error_code error() const noexcept {return error_;}
     
-    const std::string& message() const {return message_;}
+    /// @brief Gets the exception instance that caused the current exception.
+    /// @return An instance of exception that describes the error that caused the current exception. The inner_exception property returns the same value as was passed into the constructor, or a null reference if the inner exception value was not supplied to the constructor.
+    virtual std::optional<std::reference_wrapper<const std::exception>> inner_exception() const noexcept {return inner_exception_;}
     
-    std::string to_string() const {return info_ == caller_info::empty() ? xtd::strings::format("{} : {}", name(), what()) : xtd::strings::format("{} : {}\n{}", name(), message(), caller_info());}
+    /// @brief Get Line number where the exception occurred
+    /// @return the line number where exception occurred
+    virtual uint32_t line_numer() const noexcept {return information_.line_number();}
+    
+    /// @brief Get file member where exception occurred
+    /// @return A string represent member name where exception occurred
+    virtual const std::string& member_name() const noexcept {return information_.member_name();}
+    
+    /// @brief Get message associate to the exception
+    /// @return A string represent a massage associate to the exception
+    virtual const std::string& message() const noexcept {return message_;}
+    
+    /// @brief Get full class name the exception
+    /// @return A string represent a full class name of the exception
+    virtual std::string name() const noexcept {return xtd::strings::full_class_name(*this);}
+    
+    /// @brief Gets a string representation of the immediate frames on the call stack.
+    /// @return A string that describes the immediate frames of the call stack.
+    virtual std::string stack_trace() const noexcept {
+      return information_.to_trace();
+    }
+    
+    /// @brief Returns a string that represents the current exception.
+    /// @return A string that represents the current exception.
+    virtual std::string to_string() const noexcept {
+      if (stack_trace().empty()) return xtd::strings::format("{} : {}", name(), message());
+      return xtd::strings::format("{} : {}\n{}", name(), message(), stack_trace());
+    }
 
     /// @cond
     friend std::ostream& operator<<(std::ostream& os, const xtd::system_exception& e) noexcept {
@@ -41,14 +123,27 @@ namespace xtd {
     }
     /// @endcond
 
-    const char* what() const noexcept override {return message_.c_str();}
+    /// @brief Get message associate to the exception
+    /// @return A string represent a massage associate to the exception
+    const char* what() const noexcept override {return message().c_str();}
 
   private:
-    std::string message_ = "Sytem error.";
+    static constexpr const char* default_message_ = "Sytem error.";
+    std::string message_;
+    std::optional<std::reference_wrapper<const std::exception>> inner_exception_;
     std::error_code error_;
     std::string help_link_;
-    xtd::caller_info info_;
+    xtd::caller_info information_;
   };
 }
 
-#define system_exception_(...) system_exception(add_caller_info_(__VA_ARGS__))
+/// @brief Helper on system_exception to call it with caller information
+/// @remarks Is equivalent to system_exception({any argument}, caller_info_)
+/// @code
+/// void my_func() {
+///   if (invalid_info) throw stystem_exception_(); // same as : throw system_exception(caller_info_)
+///   if (invalid_value) throw stystem_exception_("Bad value"); // same as : throw system_exception("Bad value", caller_info_)
+///   ...
+/// }
+/// @endcode
+#define system_exception_(...) system_exception(add_caller_information_(__VA_ARGS__))
