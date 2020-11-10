ed0a9e93652c2ea11e6a64c9d4b066cca8219da2
Gammasoft71 <gammasoft71@gmail.com>
2020-11-10 16:00:27 +0100

    force zero variadic macro on other compiler than gcc

--- src/xtd_core/include/xtd/va_args_comma.h
+++ src/xtd_core/include/xtd/va_args_comma.h
@@ -3,12 +3,20 @@
 #pragma once
 
 #if !defined(__VA_ARGS_COMMA__)
-#  define __VA_ARGS_COMMA__(...) __GET_VA_ARGS_COMMA_ARG__( , ##__VA_ARGS__, __COMMA__, __COMMA__, __COMMA__, __COMMA__, __COMMA__, __COMMA__, __COMMA__, __COMMA__, __COMMA__, __COMMA__, __COMMA__, __COMMA__, __COMMA__, __COMMA__, __COMMA__, __COMMA__,)
+#  define __VA_ARGS_COMMA__(...) __GET_VA_ARGS_COMMA_ARG__( , __VA_ARGS__, __COMMA__, __COMMA__, __COMMA__, __COMMA__, __COMMA__, __COMMA__, __COMMA__, __COMMA__, __COMMA__, __COMMA__, __COMMA__, __COMMA__, __COMMA__, __COMMA__, __COMMA__, __COMMA__,)
 #  define __GET_VA_ARGS_COMMA_ARG__(start, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, end,...) end
 #  define __COMMA__ ,
 #endif
 
-
-// Can be used With Visual studio 2019 16.5 :
-// (add compiler options : "/Zc::preprocessor / wd5105")
+// To use with 0 argument, we need to force zero variadic macro on other compiler than gcc :
+//
+// clang : add_compile_options(-Wno-gnu-zero-variadic-macro-arguments) : Work
+// msvc : add_compile_options(/wd5105 /Zc:preprocessor) : Not work !!!!!!
+// Not working well with windows specific include... eror on some macro like _VARIANT_BOOL... with bad definition...
 // See https://docs.microsoft.com/en-us/cpp/preprocessor/preprocessor-experimental-overview?view=msvc-160
+//
+// #if !defined(__VA_ARGS_COMMA__)
+//#  define __VA_ARGS_COMMA__(...) __GET_VA_ARGS_COMMA_ARG__( , ##__VA_ARGS__, __COMMA__, __COMMA__, __COMMA__, __COMMA__, __COMMA__, __COMMA__, __COMMA__, __COMMA__, __COMMA__, __COMMA__, __COMMA__, __COMMA__, __COMMA__, __COMMA__, __COMMA__, __COMMA__,)
+//#  define __GET_VA_ARGS_COMMA_ARG__(start, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, end,...) end
+//#  define __COMMA__ ,
+//#endif
