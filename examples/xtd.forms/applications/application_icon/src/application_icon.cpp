a3e0ddba9d2df2260a8eb6fb707fe050d9a13216
Gammasoft71 <gammasoft71@gmail.com>
2020-11-11 16:13:21 +0100

    merge xtd_forms (examples)

--- examples/xtd.forms/applications/application_icon/src/application_icon.cpp
+++
@@ -1,8 +0,0 @@
-#include <xtd/xtd.forms>
-
-using namespace xtd::forms;
-
-int main() {
-  auto main_form = control::create<form>("Application icon example");
-  xtd::forms::application::run(*main_form);
-}
