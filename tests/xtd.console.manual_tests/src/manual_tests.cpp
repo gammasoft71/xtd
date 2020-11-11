3b3d308f4eb9c98c4df1d8616a7e5de4de8485aa
Gammasoft71 <gammasoft71@gmail.com>
2020-11-11 20:17:23 +0100

    move xtd.tunittt examples

--- tests/xtd.console.manual_tests/src/manual_tests.cpp
+++ tests/xtd.console.manual_tests/src/manual_tests.cpp
@@ -2,19 +2,7 @@
 
 using namespace xtd;
 
-void update_cmakefile(std::filesystem::path path) {
-  for (auto item : std::filesystem::directory_iterator(path)) {
-    if (item.is_directory()) update_cmakefile(item.path());
-    else if (item.is_regular_file() && item.path().filename() == "CMakeLists.txt") {
-      io::file::write_all_text(item.path(), strings::replace(io::file::read_all_text(item.path()), "find_package(xtd.forms REQUIRED)", "find_package(xtd REQUIRED)"));
-      io::file::write_all_text(item.path(), strings::replace(io::file::read_all_text(item.path()), "add_executable(${PROJECT_NAME} ", "add_sources("));
-      io::file::write_all_text(item.path(), strings::replace(io::file::read_all_text(item.path()), "target_link_libraries(${PROJECT_NAME} xtd.tunit)\nset_target_properties(${PROJECT_NAME} PROPERTIES FOLDER \"xtd.tunit\")", "target_type(CONSOLE_APPLICATION)"));
-    }
-  }
-}
-
 // The main entry point for the application.
 int main() {
-  //console::write_line("Hello, World!");
-  update_cmakefile(std::filesystem::path("/Users/yves/Projects/xtd/examples/xtd.forms"));
+  console::write_line("Hello, World!");
 }
