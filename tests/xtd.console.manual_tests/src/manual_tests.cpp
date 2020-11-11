d6f29bb2803ce05daac890cace7ae98160a9186b
Gammasoft71 <gammasoft71@gmail.com>
2020-11-11 20:29:31 +0100

    Update manual_tests.cpp

--- tests/xtd.console.manual_tests/src/manual_tests.cpp
+++ tests/xtd.console.manual_tests/src/manual_tests.cpp
@@ -2,17 +2,7 @@
 
 using namespace xtd;
 
-void update_cmakefile(std::filesystem::path path) {
-  for (auto item : std::filesystem::directory_iterator(path)) {
-    if (item.is_directory()) update_cmakefile(item.path());
-    else if (item.is_regular_file() && item.path().filename() == "CMakeLists.txt") {
-      io::file::write_all_text(item.path(), strings::replace(io::file::read_all_text(item.path()), "target_type(CONSOLE_APPLICATION)", "target_type(TEST_APPLICATION)"));
-    }
-  }
-}
-
 // The main entry point for the application.
 int main() {
-  //console::write_line("Hello, World!");
-  update_cmakefile(std::filesystem::path("/Users/yves/Projects/xtd/examples/xtd.tunit"));
+  console::write_line("Hello, World!");
 }
