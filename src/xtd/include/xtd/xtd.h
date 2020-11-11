bbe2fd6f81b3c77d942fdec5429188fc65b30989
Gammasoft71 <gammasoft71@gmail.com>
2020-11-11 12:59:34 +0100

    Update xtd.h

--- src/xtd/include/xtd/xtd.h
+++ src/xtd/include/xtd/xtd.h
@@ -8,19 +8,5 @@
 #include <xtd/xtd.core>
 #include <xtd/xtd.forms>
 #include <xtd/xtd.io>
-#include <xtd/xtd.properties>
 #include <xtd/xtd.strings>
 #include <xtd/xtd.tunit>
-
-/// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
-namespace xtd {
-  namespace consoles {}
-  namespace delegates {}
-  namespace diagnostics {}
-  namespace drawing {}
-  namespace forms {}
-  namespace io {}
-  namespace properties {}
-  class strings;
-  namespace tunit {}
-}
