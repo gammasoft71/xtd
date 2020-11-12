4a0f3d6bd74984faeea6a725253d10bbbc62e58b
Gammasoft71 <gammasoft71@gmail.com>
2020-11-12 19:12:18 +0100

    move *.hpp to *.h (old files)

--- tests/xtd.console.manual_tests/src/manual_tests.cpp
+++ tests/xtd.console.manual_tests/src/manual_tests.cpp
@@ -1,8 +1,17 @@
+#include <filesystem>
 #include <xtd/xtd.console>
 
 using namespace xtd;
 
+void replace_hpp_by_h(const std::filesystem::path& path) {
+  for (auto item : std::filesystem::directory_iterator(path)) {
+    if (item.is_directory()) replace_hpp_by_h(item.path());
+    else if (item.path().extension() == ".hpp") io::file::move(item.path().string(), strings::replace(item.path().string(), ".hpp", ".h"));
+  }
+}
+
 // The main entry point for the application.
 int main() {
   console::write_line("Hello, World!");
+  replace_hpp_by_h(std::filesystem::path("/Users/yves/Projects/xtd/src"));
 }
