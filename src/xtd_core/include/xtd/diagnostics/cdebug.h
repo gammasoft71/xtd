6cd936fd7522077044a4fa0a5ff85fbae883711e
Gammasoft71 <gammasoft71@gmail.com>
2020-11-13 19:55:39 +0100

    add docs

--- src/xtd_core/include/xtd/diagnostics/cdebug.h
+++ src/xtd_core/include/xtd/diagnostics/cdebug.h
@@ -9,8 +9,16 @@ inline xtd::diagnostics::debugstreambuf __cdebugstreambuf__;
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
 #if !defined(NDEBUG) || defined(DEBUG) || defined(TRACE)
+  /// @brief Provides an std::ostream for xtd::diagnostics::debug.
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core
   inline std::ostream cdebug(&__cdebugstreambuf__);
 #else
+  /// @brief Provides an std::ostream for xtd::diagnostics::debug.
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core
   inline std::ostream cdebug(nullptr);
 #endif
 }
--- src/xtd_core/include/xtd/diagnostics/console_trace_listener.h
+++ src/xtd_core/include/xtd/diagnostics/console_trace_listener.h
@@ -8,9 +8,18 @@
 namespace xtd {
   /// @brief The xtd::diagnostics namespace provides classes that allow you to interact with system processes, event logs, and performance counters.
   namespace diagnostics {
+    /// @brief Directs tracing or debugging output to either the standard output or the standard error stream.
+    /// @par Library
+    /// xtd.core
+    /// @ingroup xtd_core
     class console_trace_listener : public ostream_trace_listener {
     public:
+      /// @brief Initializes a new instance of the console_trace_listener class with trace output written to the standard output stream.
+      /// @remarks This constructor initializes a console_trace_listener object to write messages to either the std::cout or the std::cerr stream. Its Name property is initialized to an empty string ("").
       console_trace_listener() : console_trace_listener(false) {}
+      /// @brief Initializes a new instance of the console_trace_listener class with an option to write trace output to the standard output stream or the standard error stream.
+      /// @param use_error_stream true to write tracing and debugging output to the standard error stream; false to write tracing and debugging output to the standard output stream.
+      /// @remarks This constructor initializes a console_trace_listener object to write messages to either the std::cut or the std::cerr stream. Its Name property is initialized to an empty string ("").
       console_trace_listener(bool use_error_stream) : ostream_trace_listener(use_error_stream ? std::cerr : std::cout) {}
       
     private:
