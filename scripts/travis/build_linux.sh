eb96e426c1fa96c29c783c858622339d91609b15
Gammasoft71 <gammasoft71@gmail.com>
2020-11-14 20:26:00 +0100

    Update build_linux.sh

--- scripts/travis/build_linux.sh
+++ scripts/travis/build_linux.sh
@@ -15,7 +15,7 @@ cd ../../../..
 # generate and build lib
 git submodule update --init
 mkdir -p build && cd build
-cmake -DCMAKE_BUILD_TYPE=Debug -DXTD_ENABLE_TESTS=ON -DXTD_DISABLE_FOMRS_TESTS=ON -DXTD_ENABLE_XTD_COMMAND_LINE=OFF -DCMAKE_CXX_COMPILER=g++-9 -DCMAKE_INSTALL_PREFIX=~/local ..
+cmake -DCMAKE_BUILD_TYPE=Debug -DXTD_ENABLE_TESTS=ON -DXTD_DISABLE_FOMRS_TESTS=ON -DCMAKE_CXX_COMPILER=g++-9 -DCMAKE_INSTALL_PREFIX=~/local ..
 if [ $? -ne 0 ]; then exit -1; fi
 cmake --build . -- -j $(nproc)
 if [ $? -ne 0 ]; then exit -1; fi
