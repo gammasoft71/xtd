b2e412665ad5dd4207c3731c4f9c5a744485ed51
Gammasoft71 <gammasoft71@gmail.com>
2020-11-16 11:10:47 +0100

    add system_exception stack_trace

--- src/xtd.core/src/xtd/system_exception.cpp
+++ src/xtd.core/src/xtd/system_exception.cpp
@@ -1,4 +1,5 @@
 #include "../include/xtd/system_exception.h"
+#include "../include/xtd/diagnostics/stack_trace.h"
 
 using namespace xtd;
 
@@ -6,10 +7,7 @@ bool system_exception::enable_stack_trace_ = true;
 
 system_exception::system_exception(const std::string& message, const std::exception* inner_exception, const std::error_code& error, const std::string& help_link, const xtd::caller_info& information) : message_(message), error_(error), help_link_(help_link), information_(information) {
   if (inner_exception) inner_exception_ = *inner_exception;
-  if (enable_stack_trace_) {
-    // Get stack trace...
-  }
-  
+  if (enable_stack_trace_) stack_trace_ = xtd::strings::split(xtd::diagnostics::stack_trace(*this, 4, true).to_string(), {'\n'});
   if (!stack_trace_.size()) stack_trace_.push_back(information_.to_trace());
 }
 
