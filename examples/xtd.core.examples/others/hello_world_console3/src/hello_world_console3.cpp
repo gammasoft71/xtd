d21d89c8dfad4dfb88129f811ba7a456f71163b3
Gammasoft71 <gammasoft71@gmail.com>
2020-11-20 23:52:02 +0100

    review examples

---
+++ examples/xtd.core.examples/others/hello_world_console3/src/hello_world_console3.cpp
@@ -0,0 +1,12 @@
+#include <xtd/xtd.core>
+
+using namespace std;
+using namespace xtd;
+
+int main() {
+  cout << background_color(console_color::blue) << foreground_color(console_color::white) << "Hello, World!" << reset_color() << endl;
+}
+
+// This code produces the following output with colors :
+//
+// Hello, World!
