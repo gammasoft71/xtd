e78d972cf6e68ea8aeef7a73cb6471a2d0599ef0
Gammasoft71 <gammasoft71@gmail.com>
2020-11-21 17:40:37 +0100

    Update console_song.cpp

--- examples/xtd.core.examples/console/console_song/src/console_song.cpp
+++ examples/xtd.core.examples/console/console_song/src/console_song.cpp
@@ -4,42 +4,44 @@
 #include <thread>
 
 using namespace std;
+using namespace std::this_thread;
 using namespace xtd;
 
 // Define the frequencies of notes in an octave, as well as
 // silence (rest).
 enum class tone {
-  REST = 0,
-  GbelowC = 196,
-  A = 220,
-  Asharp = 233,
-  B = 247,
-  C = 262,
-  Csharp = 277,
-  D = 294,
-  Dsharp = 311,
-  E = 330,
-  F = 349,
-  Fsharp = 370,
-  G = 392,
-  Gsharp = 415,
+  rest = 0,
+  g_below_c = 196,
+  a = 220,
+  a_sharp = 233,
+  b = 247,
+  c = 262,
+  c_sharp = 277,
+  d = 294,
+  d_sharp = 311,
+  e = 330,
+  f = 349,
+  f_sharp = 370,
+  g = 392,
+  g_sharp = 415,
 };
 
 // Define the duration of a note in units of milliseconds.
 enum class duration {
-  WHOLE = 1600,
-  HALF = WHOLE / 2,
-  QUARTER = HALF / 2,
-  EIGHTH = QUARTER / 2,
-  SIXTEENTH = EIGHTH / 2,
+  none = 0,
+  whole = 1600,
+  half = whole / 2,
+  quarter = half / 2,
+  eighth = quarter / 2,
+  sixteenth = eighth / 2,
 };
 
 // Define a note as a frequency (tone) and the amount of
 // time (duration) the note plays.
 struct note final {
 private:
-  tone tone_val = static_cast<tone>(0);
-  duration dur_val = static_cast<duration>(0);
+  tone tone_val = tone::rest;
+  duration dur_val = duration::none;
   
 public:
   // Define a constructor to create a specific note.
@@ -50,38 +52,36 @@ public:
   note& operator=(const note& note) = default;
   
   // Define properties to return the note's tone and duration.
-  tone note_tone() noexcept {return tone_val;}
-  duration note_duration() noexcept {return dur_val;}
+  tone note_tone() const noexcept {return tone_val;}
+  duration note_duration() const noexcept {return dur_val;}
 };
 
 // Play the notes in a song.
 void play(const vector<note>& tune) {
   for (note n : tune) {
-    if (n.note_tone() == tone::REST)
-      this_thread::sleep_for(chrono::milliseconds(static_cast<int>(n.note_duration())));
+    if (n.note_tone() == tone::rest)
+      sleep_for(chrono::milliseconds(static_cast<int>(n.note_duration())));
     else
-      console::beep(static_cast<int>(n.note_tone()), static_cast<int>(n.note_duration()));
+      console::beep(static_cast<unsigned int>(n.note_tone()), static_cast<unsigned int>(n.note_duration()));
   }
 }
 
-// The main entry point for the application.
 int main() {
   // Declare the first few notes of the song, "Mary Had A Little Lamb".
-  vector<note> mary = {
-    note(tone::B, duration::QUARTER),
-    note(tone::A, duration::QUARTER),
-    note(tone::GbelowC, duration::QUARTER),
-    note(tone::A, duration::QUARTER),
-    note(tone::B, duration::QUARTER),
-    note(tone::B, duration::QUARTER),
-    note(tone::B, duration::HALF),
-    note(tone::A, duration::QUARTER),
-    note(tone::A, duration::QUARTER),
-    note(tone::A, duration::HALF),
-    note(tone::B, duration::QUARTER),
-    note(tone::D, duration::QUARTER),
-    note(tone::D, duration::HALF)
-  };
+  vector mary = {
+    note(tone::b, duration::quarter),
+    note(tone::a, duration::quarter),
+    note(tone::g_below_c, duration::quarter),
+    note(tone::a, duration::quarter),
+    note(tone::b, duration::quarter),
+    note(tone::b, duration::quarter),
+    note(tone::b, duration::half),
+    note(tone::a, duration::quarter),
+    note(tone::a, duration::quarter),
+    note(tone::a, duration::half),
+    note(tone::b, duration::quarter),
+    note(tone::d, duration::quarter),
+    note(tone::d, duration::half)};
   // Play the song
   play(mary);
 }
