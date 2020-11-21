ebf968484202e59ebd0b5ecd6e8137cabe04e2e7
Gammasoft71 <gammasoft71@gmail.com>
2020-11-21 21:34:13 +0100

    Update event.h

--- src/xtd.core/include/xtd/event.h
+++ src/xtd.core/include/xtd/event.h
@@ -6,32 +6,32 @@ namespace xtd {
   class event : protected handler_t {
     friend object_t;
   public:
-    bool is_empty() const noexcept { return this->handler_t::is_empty(); }
+    bool is_empty() const noexcept { return handler_t::is_empty(); }
 
     handler_t& operator+=(const handler_t& handler) noexcept {
-      return this->handler_t::operator+=(handler);
+      return handler_t::operator+=(handler);
     }
     
     handler_t& operator +=(const typename handler_t::function_t& function) noexcept {
-      return this->handler_t::operator+=(function);
+      return handler_t::operator+=(function);
     }
     
     template<typename fn_t>
     handler_t& operator +=(fn_t function) noexcept {
-      return this->handler_t::operator+=(function);
+      return handler_t::operator+=(function);
     }
     
     handler_t& operator -=(const handler_t& delegate) noexcept {
-      return this->handler_t::operator-=(delegate);
+      return handler_t::operator-=(delegate);
     }
     
     handler_t& operator -=(const typename handler_t::function_t& function) noexcept {
-      return this->handler_t::operator-=(function);
+      return handler_t::operator-=(function);
     }
     
     template<typename fn_t>
     handler_t& operator -=(fn_t function) noexcept {
-      return this->handler_t::operator-=(function);
+      return handler_t::operator-=(function);
     }
   };
 }
