20b59e075cbd56baf9472c3af839a11816da7ad4
Gammasoft71 <gammasoft71@gmail.com>
2020-11-21 16:55:30 +0100

    Update console_color.cpp

--- examples/xtd.core.examples/console/console_color/src/console_color.cpp
+++ examples/xtd.core.examples/console/console_color/src/console_color.cpp
@@ -7,7 +7,7 @@ using namespace xtd;
 
 int main() {
   // Get an array with the values of ConsoleColor enumeration members.
-  vector<console_color> colors = {console_color::black, console_color::dark_blue, console_color::dark_green, console_color::dark_cyan, console_color::dark_red, console_color::dark_magenta, console_color::dark_yellow, console_color::gray, console_color::dark_gray, console_color::blue, console_color::green, console_color::cyan, console_color::red, console_color::magenta, console_color::yellow, console_color::white};
+  vector colors = {console_color::black, console_color::dark_blue, console_color::dark_green, console_color::dark_cyan, console_color::dark_red, console_color::dark_magenta, console_color::dark_yellow, console_color::gray, console_color::dark_gray, console_color::blue, console_color::green, console_color::cyan, console_color::red, console_color::magenta, console_color::yellow, console_color::white};
   
   // Save the current background and foreground colors.
   console_color current_background = console::background_color();
