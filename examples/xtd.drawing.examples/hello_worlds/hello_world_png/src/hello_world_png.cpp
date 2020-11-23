c4aaaed0d24d3c9083117a21e30bce9c810eb5bf
Gammasoft71 <gammasoft71@gmail.com>
2020-11-23 23:03:56 +0100

    Update hello_world_png.cpp

--- examples/xtd.drawing.examples/hello_worlds/hello_world_png/src/hello_world_png.cpp
+++ examples/xtd.drawing.examples/hello_worlds/hello_world_png/src/hello_world_png.cpp
@@ -7,8 +7,8 @@ using namespace xtd::drawing;
 int main() {
   bitmap bitmap(300, 300);
   auto graphics = graphics::from_image(bitmap);
-  graphics.draw_string("Hello World!", {system_fonts::default_font(), 32, font_style::bold | font_style::italic}, solid_brush(color::dark(color::spring_green, 2.0 / 3)), {2, 2, 302, 302}, string_format().alignment(string_alignment::center).line_alignment(string_alignment::center));
-  graphics.draw_string("Hello World!", {system_fonts::default_font(), 32, font_style::bold | font_style::italic}, brushes::spring_green(), {0, 0, 300, 300}, string_format().alignment(string_alignment::center).line_alignment(string_alignment::center));
-  bitmap.save("hello_world.png", imaging::image_format::png());
+  graphics.draw_string("Hello World!", {system_fonts::default_font(), 32, font_style::bold | font_style::italic}, solid_brush(color::dark(color::spring_green, 2.0 / 3)), {2, 2, 300, 300}, string_format().alignment(string_alignment::center).line_alignment(string_alignment::center));
+  graphics.draw_string("Hello World!", {system_fonts::default_font(), 32, font_style::bold | font_style::italic}, brushes::spring_green(), {0, 0, 298, 298}, string_format().alignment(string_alignment::center).line_alignment(string_alignment::center));
+  bitmap.save("hello_world.png");
   process::start("open hello_world.png").wait_for_exit();
 }
