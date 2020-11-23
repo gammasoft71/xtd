72955084c75f469e5e62da4dec45041e8934e0b1
Gammasoft71 <gammasoft71@gmail.com>
2020-11-23 22:58:53 +0100

    Update hello_world_png.cpp

--- examples/xtd.drawing.examples/hello_worlds/hello_world_png/src/hello_world_png.cpp
+++ examples/xtd.drawing.examples/hello_worlds/hello_world_png/src/hello_world_png.cpp
@@ -5,7 +5,6 @@ using namespace xtd::diagnostics;
 using namespace xtd::drawing;
 
 int main() {
-  console::write_line(environment::current_directory());
   bitmap bitmap(300, 300);
   auto graphics = graphics::from_image(bitmap);
   graphics.draw_string("Hello World!", {system_fonts::default_font(), 32, font_style::bold | font_style::italic}, solid_brush(color::dark(color::spring_green, 2.0 / 3)), {2, 2, 302, 302}, string_format().alignment(string_alignment::center).line_alignment(string_alignment::center));
