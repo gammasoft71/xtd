1528f8bb6fbc69891537d10730a9fde52b113c80
Gammasoft71 <gammasoft71@gmail.com>
2020-11-24 14:27:28 +0100

    Update hello_world_png.cpp

--- examples/xtd.drawing.examples/hello_worlds/hello_world_png/src/hello_world_png.cpp
+++ examples/xtd.drawing.examples/hello_worlds/hello_world_png/src/hello_world_png.cpp
@@ -9,6 +9,6 @@ int main() {
   auto graphics = graphics::from_image(bitmap);
   graphics.draw_string("Hello World!", {system_fonts::default_font(), 32, font_style::bold | font_style::italic}, solid_brush(color::dark(color::spring_green, 2.0 / 3)), rectangle::offset({{0, 0}, bitmap.size()}, {2, 2}), string_format().alignment(string_alignment::center).line_alignment(string_alignment::center));
   graphics.draw_string("Hello World!", {system_fonts::default_font(), 32, font_style::bold | font_style::italic}, brushes::spring_green(), rectangle::inflate({{0, 0}, bitmap.size()}, {-2, -2}), string_format().alignment(string_alignment::center).line_alignment(string_alignment::center));
-  bitmap.save("hello_world.png");
-  process::start(strings::format("{} hello_world.png", environment::os_version().is_windows_platform() ? "start" : "open")).wait_for_exit();
+  bitmap.save("hello_world_png.png");
+  process::start(strings::format("{} hello_world_png.png", environment::os_version().is_windows_platform() ? "start" : "open")).wait_for_exit();
 }
