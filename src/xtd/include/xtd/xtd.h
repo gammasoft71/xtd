cf8cb0b001b1351089a6ed4b59f09a09aaebdd7f
Gammasoft71 <gammasoft71@gmail.com>
2020-11-25 21:44:00 +0100

    Update xtd.h

--- src/xtd/include/xtd/xtd.h
+++ src/xtd/include/xtd/xtd.h
@@ -1,7 +1,3 @@
-/// @file
-/// @brief Contains xtd include files.
 #pragma once
-#include <xtd/xtd.core>
-#include <xtd/xtd.drawing>
-#include <xtd/xtd.forms>
-#include <xtd/xtd.tunit>
+#include "xtd/xtd.core"
+#include "xtd/xtd.drawing"
