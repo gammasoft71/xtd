105dd87f8475b5d33a23d1df2c02bfd0856d51a1
Gammasoft71 <gammasoft71@gmail.com>
2020-11-26 10:05:23 +0100

    remove autogenerated files

--- src/xtd.core/include/xtd/core_export.h
+++
@@ -1,5 +0,0 @@
-/// @file
-/// @brief Contains core_export_ keyword.
-#pragma once
-
-#define core_export_
--- src/xtd.tunit/include/xtd/tunit_export.h
+++
@@ -1,5 +0,0 @@
-/// @file
-/// @brief Contains tunit_export_ keyword.
-#pragma once
-
-#define tunit_export_
