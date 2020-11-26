6a1766100753268635b6193327028ed0fe121b90
gammasoft71 <gammasoft71@gmail.com>
2020-11-26 23:38:28 +0100

    Update hello_world_console.cpp

--- examples/xtd.core.examples/hello_worlds/hello_world_console/src/hello_world_console.cpp
+++ examples/xtd.core.examples/hello_worlds/hello_world_console/src/hello_world_console.cpp
@@ -6,6 +6,7 @@ int main() {
   console::background_color(console_color::blue);
   console::foreground_color(console_color::white);
   console::write_line("Hello, World!");
+  console::reset_color();
 }
 
 // This code produces the following output with colors :
