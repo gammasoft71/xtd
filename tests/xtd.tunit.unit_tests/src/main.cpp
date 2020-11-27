86ba08814548fda42f37ef327356cc7d6f3a585e
Gammasoft71 <gammasoft71@gmail.com>
2020-11-27 21:37:53 +0100

    add internal_tests

--- tests/xtd.tunit.unit_tests/src/main.cpp
+++ tests/xtd.tunit.unit_tests/src/main.cpp
@@ -3,10 +3,9 @@
 
 std::vector<assert_unit_tests::register_assert_unit_test> assert_unit_tests::register_assert_unit_test::assert_unit_tests;
 size_t assert_unit_tests::register_assert_unit_test::ignore_test_count = 0;
-extern bool __tunit_unit_tests_mode__;
 
 int main() {
-  __tunit_unit_tests_mode__ = true;
+  xtd::tunit::test::__internal_tunit_unit_tests_mode__ = reinterpret_cast<intptr_t>("internal_tests");
   return assert_unit_tests::register_assert_unit_test::run_all_tests();
 }
 
