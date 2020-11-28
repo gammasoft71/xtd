a626d2ed67f2a31a3e0e715d43a85c114cba9670
Gammasoft71 <gammasoft71@gmail.com>
2020-11-28 20:39:56 +0100

    fix c++20

--- tests/xtd.core.manual_tests/src/manual_tests.cpp
+++ tests/xtd.core.manual_tests/src/manual_tests.cpp
@@ -2,6 +2,15 @@
 
 using namespace xtd;
 
+
 int main() {
   console::write_line("Hello, World!");
+  char c = 'c';
+  console::write_line(c);
+  std::cout << c << std::endl;
+  char8_t c8 = 'c';
+  console::write_line(c8);
+  std::cout << c8 << std::endl;
+  
+  c = c8;
 }
