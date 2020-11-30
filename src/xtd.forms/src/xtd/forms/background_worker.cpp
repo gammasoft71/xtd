cd88316877501324949808136a64ff8c93cb6f84
Gammasoft <gammasoft71@gmail.com>
2020-11-30 10:33:02 +0100

    Update background_worker.cpp

--- src/xtd.forms/src/xtd/forms/background_worker.cpp
+++ src/xtd.forms/src/xtd/forms/background_worker.cpp
@@ -23,7 +23,7 @@ void background_worker::report_progress(int32_t percent_progress) {
 void background_worker::report_progress(int32_t percent_progress, std::any user_state) {
   if (worker_reports_progress_) {
     e_ = {percent_progress, user_state};
-    invoker_->begin_invoke([=] {
+    invoker_->begin_invoke([&] {
       on_progres_changed(e_);
     });
   }
@@ -37,7 +37,7 @@ void background_worker::run_worker_async() {
     do_work_event_args e(argument_);
     on_do_work(e);
     is_busy_ = false;
-    invoker_->begin_invoke([=] {
+    invoker_->begin_invoke([&] {
       on_run_worker_completed(run_worker_completed_event_args(std::any(), std::optional<std::reference_wrapper<std::exception>>(), cancellation_pending_));
       invoker_ = nullptr;
       cancellation_pending_ = false;
