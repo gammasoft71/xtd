e785b95df4291a82e6d597fbbfb9f7089b38002f
Gammasoft71 <gammasoft71@gmail.com>
2020-12-01 16:48:11 +0100

    add system_report

--- src/xtd/src/xtd.cpp
+++
@@ -1,2 +0,0 @@
-void __xtd__unsused__() {
-}
---
+++ src/xtd/src/xtd/system_report.cpp
@@ -0,0 +1,8 @@
+#include "../../include/xtd/system_report.h"
+
+using namespace std;
+using namespace xtd;
+
+string system_report::to_string() const {
+  return "";
+}
