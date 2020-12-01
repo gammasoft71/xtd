9e80f602b0625e58c6732cdb9f5e348e720db13a
Gammasoft71 <gammasoft71@gmail.com>
2020-12-01 11:31:23 +0100

    Update up_down_button.h

--- src/xtd.forms/include/xtd/forms/up_down_button.h
+++ src/xtd.forms/include/xtd/forms/up_down_button.h
@@ -6,7 +6,7 @@
 namespace xtd {
   /// @brief The xtd::forms namespace contains classes for creating Windows-based applications that take full advantage of the rich user interface features available in the Microsoft Windows operating system, Apple macOS and Linux like Ubuntu operating system.
   namespace forms {
-    class up_down_button : public button_base {
+    class forms_export_ up_down_button : public button_base {
     public:      
       up_down_button();
 
