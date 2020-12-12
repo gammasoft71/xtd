89a977ab85325efb68044deb04d3a5e16a8d7bc0
Gammasoft71 <gammasoft71@gmail.com>
2020-12-12 23:15:03 +0100

    update guidgen-gui tool

---
+++ tools/guidgen-gui/properties/application_properties.cmake
@@ -0,0 +1,4 @@
+application_default_namespace("guidgen_gui")
+application_icon(resources/guidgen-gui)
+application_display(ON)
+application_startup("guidgen_gui::main_form" src/main_form.h)
--- tools/xtdc-gui/properties/application_properties.cmake
+++ tools/xtdc-gui/properties/application_properties.cmake
@@ -1,4 +1,4 @@
 application_default_namespace("xtdc_gui")
 application_icon(resources/xtdc-gui)
-application_display(TRUE)
+application_display(ON)
 application_startup("xtdc_gui::main_form" src/main_form.h)
