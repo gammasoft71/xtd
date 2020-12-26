8686abbb5fa19f5dc9c5d07b725283cb4f00fd4c
Gammasoft <gammasoft71@gmail.com>
2020-12-26 19:33:37 +0100

    Update busy_dialog.cpp

--- src/xtd.forms/src/xtd/forms/busy_dialog.cpp
+++ src/xtd.forms/src/xtd/forms/busy_dialog.cpp
@@ -1,6 +1,7 @@
 #include <memory>
 #include <xtd/forms/native/busy_dialog.h>
 #include <xtd/drawing/system_icons.h>
+#include "../../../include/xtd/forms/application.h"
 #include "../../../include/xtd/forms/busy_box.h"
 #include "../../../include/xtd/forms/busy_dialog.h"
 
@@ -24,12 +25,14 @@ void busy_dialog::show() {
   native_ = dialog_style_ == xtd::forms::dialog_style::system;
   if (native_)
     handle_ = native::busy_dialog::create(0, xtd::drawing::icon::from_bitmap(xtd::drawing::bitmap(icon_)), text_, description_, back_color_, fore_color_, transparency_);
+  application::yield();
 }
 
 void busy_dialog::show(const iwin32_window& owner) {
   native_ = dialog_style_ == xtd::forms::dialog_style::system;
   if (native_)
     handle_ = native::busy_dialog::create(owner.handle(), xtd::drawing::icon::from_bitmap(xtd::drawing::bitmap(icon_)), text_, description_, back_color_, fore_color_, transparency_);
+  application::yield();
 }
 
 void busy_dialog::hide() {
