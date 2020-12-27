79b4e0f8ebcc016d1f5f9b2db2ee53c0d9de5d11
Gammasoft <gammasoft71@gmail.com>
2020-12-27 15:01:20 +0100

    update translator

--- src/xtd.core/src/xtd/translator.cpp
+++ src/xtd.core/src/xtd/translator.cpp
@@ -8,64 +8,32 @@
 #include "../include/xtd/io/file.h"
 #include "../include/xtd/io/path.h"
 
-namespace {
-  std::map<std::string, xtd::collections::specialized::string_map> __xtd_language_values__;
-  
-  std::string __xtd_language__;
-  
-  void parse_file(const std::filesystem::path& file, const std::string& language) {
-    std::vector<std::string> lines = xtd::io::file::read_all_lines(file);
-    std::string key;
-    std::string value;
-    int line_count = 0;
-    for (auto line : lines) {
-      line_count++;
-      line = xtd::strings::trim(line);
-      if (line.empty()) continue;
-      if (xtd::strings::starts_with(line, "#")) continue;
-      if (key.empty() && xtd::strings::starts_with(line, "key ")) key = xtd::strings::trim(xtd::strings::remove(line, 0, 4), '"');
-      else if (!key.empty() && xtd::strings::starts_with(line, "value ")) value = xtd::strings::trim(xtd::strings::remove(line, 0, 6), '"');
-      else throw xtd::format_exception(xtd::format("file {} has an invalid format at line {}", file, line_count));
-      if (!key.empty() && !value.empty()) {
-        __xtd_language_values__[language][key] = value;
-        key = "";
-        value = "";
-      }
-    }
-  }
-  
-  void parse_directory(const std::filesystem::path& path) {
-    if (!std::filesystem::exists(path) || !std::filesystem::is_directory(path)) return;
-    for (auto locale_item : std::filesystem::directory_iterator(path)) {
-      if (locale_item.is_directory()) {
-        for (auto language_item : std::filesystem::directory_iterator(locale_item.path())) {
-          if (language_item.path().extension() == ".strings")
-            parse_file(language_item.path(), xtd::strings::to_lower(locale_item.path().filename().string()));
-        }
-      }
-    }
-  }
-}
-
 using namespace std;
 using namespace std::filesystem;
 using namespace xtd;
+using namespace xtd::collections::specialized;
 
+map<string, string_map> translator::language_values_;
+string translator::language_;
+
+void translator::add_value(const std::string& language, const std::string& key, const std::string& value) {
+  language_values_[language][key] = value;
+}
 std::string translator::language() {
   initialize(); // Must be first
-  return __xtd_language__;
+  return language_;
 }
 
 void translator::language(const std::string& language) {
   initialize(); // Must be first
-  __xtd_language__ = xtd::strings::to_lower(language);
+  language_ = xtd::strings::to_lower(language);
 }
 
 std::vector<std::string> translator::languages() {
   initialize(); // Must be first
   static std::vector<std::string> languages;
   if (languages.empty()) {
-    for (auto language_value : __xtd_language_values__)
+    for (auto language_value : language_values_)
       languages.push_back(language_value.first);
   }
   return languages;
@@ -74,7 +42,7 @@ std::vector<std::string> translator::languages() {
 std::string translator::translate(const std::string& language, const std::string& value) {
   initialize(); // Must be first
   try {
-    return __xtd_language_values__.at(language).at(value);
+    return language_values_.at(language).at(value);
   } catch (...) {
     return value;
   }
@@ -83,30 +51,58 @@ std::string translator::translate(const std::string& language, const std::string
 const char* translator::translate(const std::string& language, const char* value) {
   initialize(); // Must be first
   try {
-    return __xtd_language_values__.at(language).at(value).c_str();
+    return language_values_.at(language).at(value).c_str();
   } catch (...) {
     return value;
   }
 }
 
 void translator::parse_locale(const std::filesystem::path& locale_path) {
-  parse_directory(locale_path);
+  if (!std::filesystem::exists(locale_path) || !std::filesystem::is_directory(locale_path)) return;
+  for (auto locale_item : std::filesystem::directory_iterator(locale_path)) {
+    if (locale_item.is_directory()) {
+      for (auto language_item : std::filesystem::directory_iterator(locale_item.path())) {
+        if (language_item.path().extension() == ".strings")
+          parse_file(language_item.path(), xtd::strings::to_lower(locale_item.path().filename().string()));
+      }
+    }
+  }
+}
+
+void translator::parse_file(const std::filesystem::path& file, const std::string& language) {
+  std::vector<std::string> lines = xtd::io::file::read_all_lines(file);
+  std::string key;
+  std::string value;
+  int line_count = 0;
+  for (auto line : lines) {
+    line_count++;
+    line = xtd::strings::trim(line);
+    if (line.empty()) continue;
+    if (xtd::strings::starts_with(line, "#")) continue;
+    if (key.empty() && xtd::strings::starts_with(line, "key ")) key = xtd::strings::trim(xtd::strings::remove(line, 0, 4), '"');
+    else if (!key.empty() && xtd::strings::starts_with(line, "value ")) value = xtd::strings::trim(xtd::strings::remove(line, 0, 6), '"');
+    else throw xtd::format_exception(xtd::format("file {} has an invalid format at line {}", file, line_count));
+    if (!key.empty() && !value.empty()) {
+      add_value(language, key, value);
+      key = value = "";
+    }
+  }
 }
 
 void translator::initialize() {
   static bool initialized = false;
   if (initialized) return;
   
-  if (__xtd_language__.empty() && !std::locale().name().empty() && std::locale().name() != "C") __xtd_language__ = xtd::strings::to_lower(xtd::strings::substring(std::locale().name(), 0, 2));
-  if (__xtd_language__.empty()) __xtd_language__ = xtd::strings::to_lower(xtd::strings::substring(environment::get_environment_variable("LANG"), 0, 2));
+  if (language_.empty() && !std::locale().name().empty() && std::locale().name() != "C") language_ = xtd::strings::to_lower(xtd::strings::substring(std::locale().name(), 0, 2));
+  if (language_.empty()) language_ = xtd::strings::to_lower(xtd::strings::substring(environment::get_environment_variable("LANG"), 0, 2));
 
-  parse_directory(path(__XTD_INSTALL_PATH__)/"share"/"xtd"/"locale");
-  if (xtd::environment::os_version().is_macos_platform()) parse_directory(path(xtd::io::path::get_directory_name(xtd::environment::get_command_line_args()[0]))/".."/"Resources"/"locale");
-  else parse_directory(path(xtd::io::path::get_directory_name(xtd::environment::get_command_line_args()[0]))/"locale");
+  parse_locale(path(__XTD_INSTALL_PATH__)/"share"/"xtd"/"locale");
+  if (xtd::environment::os_version().is_macos_platform()) parse_locale(path(xtd::io::path::get_directory_name(xtd::environment::get_command_line_args()[0]))/".."/"Resources"/"locale");
+  else parse_locale(path(xtd::io::path::get_directory_name(xtd::environment::get_command_line_args()[0]))/"locale");
   /*
-  if (xtd::environment::os_version().is_macos_platform()) parse_directory(path(xtd::io::path::get_directory_name(xtd::environment::get_command_line_args()[0]))/".."/"Resources");
-  else if (xtd::environment::os_version().is_linux_platform()) parse_directory(path("usr")/"share"/"locale");
-  else parse_directory(path(xtd::io::path::get_directory_name(xtd::environment::get_command_line_args()[0])));
+  if (xtd::environment::os_version().is_macos_platform()) parse_locale(path(xtd::io::path::get_directory_name(xtd::environment::get_command_line_args()[0]))/".."/"Resources");
+  else if (xtd::environment::os_version().is_linux_platform()) parse_locale(path("usr")/"share"/"locale");
+  else parse_locale(path(xtd::io::path::get_directory_name(xtd::environment::get_command_line_args()[0])));
    */
   initialized = true;
 }
