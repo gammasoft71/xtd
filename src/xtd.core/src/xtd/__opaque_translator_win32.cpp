f94b4a508b190e9da17ef3f3add10a05fb55f52c
Gammasoft <gammasoft71@gmail.com>
2020-12-27 19:22:35 +0100

    add system_language

---
+++ src/xtd.core/src/xtd/__opaque_translator_win32.cpp
@@ -0,0 +1,11 @@
+#if defined(_WIN32)
+#include "../../include/xtd/environment.h"
+
+using namespace std;
+using namespace xtd;
+
+string __translator_get_system_language__() {
+  return strings::to_lower(strings::substring(environment::get_environment_variable("LANG"), 0, 2));
+}
+
+#endif
