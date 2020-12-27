7591966657d755d4509b29b334a73d0b13d901ae
gammasoft71 <gammasoft71@gmail.com>
2020-12-27 21:35:08 +0100

    Update __opaque_translator_win32.cpp

--- src/xtd.core/src/xtd/__opaque_translator_win32.cpp
+++ src/xtd.core/src/xtd/__opaque_translator_win32.cpp
@@ -1,11 +1,15 @@
 #if defined(_WIN32)
 #include "../../include/xtd/environment.h"
+#include <Windows.h>
 
 using namespace std;
 using namespace xtd;
 
 string __translator_get_system_language__() {
-  return strings::to_lower(strings::substring(environment::get_environment_variable("LANG"), 0, 2));
+  if (!environment::get_environment_variable("LANG").empty()) return strings::to_lower(strings::substring(environment::get_environment_variable("LANG"), 0, 2));
+  string data(256, 0);
+  GetLocaleInfoA(LOCALE_USER_DEFAULT, LOCALE_SISO639LANGNAME, data.data(), static_cast<int>(data.size()));
+  return data.c_str();
 }
 
 #endif
