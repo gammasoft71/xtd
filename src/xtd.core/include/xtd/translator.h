fada98663e8f185f22eda2cdc93caa76ec04c7ec
Gammasoft <gammasoft71@gmail.com>
2020-12-27 15:01:20 +0100

    update translator

--- src/xtd.core/include/xtd/translator.h
+++ src/xtd.core/include/xtd/translator.h
@@ -2,6 +2,7 @@
 #include <filesystem>
 #include <string>
 #include <vector>
+#include "collections/specialized/string_map.h"
 #include "static.h"
 
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
@@ -13,10 +14,18 @@ namespace xtd {
     
     static std::vector<std::string> languages();
 
+    static void add_value(const std::string& language, const std::string& key, const std::string& value);
+    
     static void parse_locale(const std::filesystem::path& locale_path);
     static void parse_locale(const std::string& locale_path) {parse_locale(std::filesystem::path(locale_path));}
     /// @cond
     static void parse_locale(const char* locale_path) {parse_locale(std::filesystem::path(locale_path));}
+    /// @endcond
+
+    static void parse_file(const std::filesystem::path& file, const std::string& language);
+    static void parse_file(const std::string& file, const std::string& language) {parse_file(std::filesystem::path(file), language);}
+    /// @cond
+    static void parse_file(const char* file, const char* language) {parse_file(std::filesystem::path(file), language);}
     /// @endcond
 
     static std::string translate(const std::string& value) {return translate(language(), value);}
@@ -26,5 +35,7 @@ namespace xtd {
 
   private:
     static void initialize();
+    static std::map<std::string, xtd::collections::specialized::string_map> language_values_;
+    static std::string language_;
   };
 }
