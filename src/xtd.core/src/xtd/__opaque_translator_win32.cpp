3778b374a18ec10d9fb65ab7c04bc4559bbd02c0
gammasoft71 <gammasoft71@gmail.com>
2020-12-27 21:41:17 +0100

    Update __opaque_translator_win32.cpp

--- src/xtd.core/src/xtd/__opaque_translator_win32.cpp
+++ src/xtd.core/src/xtd/__opaque_translator_win32.cpp
@@ -7,9 +7,9 @@ using namespace xtd;
 
 string __translator_get_system_language__() {
   if (!environment::get_environment_variable("LANG").empty()) return strings::to_lower(strings::substring(environment::get_environment_variable("LANG"), 0, 2));
-  string data(256, 0);
-  GetLocaleInfoA(LOCALE_USER_DEFAULT, LOCALE_SISO639LANGNAME, data.data(), static_cast<int>(data.size()));
-  return data.c_str();
+  char data[256] = {0};
+  GetLocaleInfoA(LOCALE_USER_DEFAULT, LOCALE_SISO639LANGNAME, data, 256);
+  return data;
 }
 
 #endif
