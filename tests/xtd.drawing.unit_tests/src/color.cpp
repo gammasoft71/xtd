caadf56c6ce79e8f73c73706292f1341cd927777
Gammasoft <gammasoft71@gmail.com>
2020-12-30 20:54:18 +0100

    fix strings internationalization

--- tests/xtd.drawing.unit_tests/src/color.cpp
+++ tests/xtd.drawing.unit_tests/src/color.cpp
@@ -1,5 +1,5 @@
 #include <xtd/drawing/color.h>
-#include <xtd/format_exception.h>
+#include <xtd/argument_exception.h>
 #include <xtd/xtd.tunit>
 
 using namespace xtd;
@@ -216,7 +216,7 @@ namespace unit_tests {
     }
     
     void test_method_(create_from_know_color_invalid) {
-      assert::throws<format_exception>([] {color::from_known_color((known_color)7654);});
+      assert::throws<argument_exception>([] {color::from_known_color((known_color)7654);});
     }
 
     void test_method_(create_from_know_color_transparent) {
