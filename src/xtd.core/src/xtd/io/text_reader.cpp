4009d8f97a65959cd4d62698d56e507817191aa1
Gammasoft <gammasoft71@gmail.com>
2021-01-04 21:13:19 +0100

    Update text_reader.cpp

--- src/xtd.core/src/xtd/io/text_reader.cpp
+++ src/xtd.core/src/xtd/io/text_reader.cpp
@@ -11,9 +11,3 @@ null_text_reader& text_reader::null() {
 synchronized_text_reader text_reader::synchronised(text_reader& reader) {
   return synchronized_text_reader(reader);
 }
-
-
-int32_t synchronized_text_reader::read() {
-  std::lock_guard<std::mutex> lock(mutex_);
-  return reader_.read();
-}
