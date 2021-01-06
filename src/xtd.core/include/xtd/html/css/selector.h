b1883a5091e257c8393f8dbc47a14e1fbefcf4c7
Gammasoft <gammasoft71@gmail.com>
2021-01-06 21:55:47 +0100

    add css_wrtiter example

--- src/xtd.core/include/xtd/html/css/selector.h
+++ src/xtd.core/include/xtd/html/css/selector.h
@@ -19,6 +19,7 @@ namespace xtd {
         const std::string& name() const {return name_;}
         void name(const std::string& name) {name_ = name;}
         const properties_t& properties() const {return properties_;}
+        void properties(const properties_t& properties) {properties_ = properties;}
         properties_t& properties() {return properties_;}
 
       private:
