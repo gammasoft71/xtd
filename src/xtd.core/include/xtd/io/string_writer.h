345387631ead0e0c9eb3f56add348e9aae9e1b05
Gammasoft <gammasoft71@gmail.com>
2021-01-06 23:22:23 +0100

    add string_writer class

---
+++ src/xtd.core/include/xtd/io/string_writer.h
@@ -0,0 +1,44 @@
+/// @file
+/// @brief Contains xtd::io::file class.
+#pragma once
+
+#include <strstream>
+#include "text_writer.h"
+
+/// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
+namespace xtd {
+  /// @brief The xtd::io namespace contains types that allow reading and writing to files and data streams, and types that provide basic file and directory support.
+  namespace io {
+    /// @brief Represents a writer that can write a sequential series of characters.
+    /// @par Library
+    /// xtd.core
+    /// @ingroup xtd_core
+    class string_writer : public xtd::io::text_writer {
+    public:
+      /// @brief Returns the underlying stream.
+      /// @return The underlying stream.
+      const std::ostream& base_stream() const {return stream_;}
+      /// @brief Returns the underlying stream.
+      /// @return The underlying stream.
+      std::ostream& base_stream() {return stream_;}
+
+      /// @brief Clears all buffers for the current writer and causes any buffered data to be written to the underlying device.
+      /// @remarks This default method does nothing, but derived classes can virtual the method to provide the appropriate functionality
+      void flush() override {
+        stream_.flush();
+      }
+ 
+      std::string to_string() const {return stream_.str();}
+      
+      /// @brief Writes the specified string value to the text stream.
+      /// @param value The value to write
+      /// @exception io::ioexception An I/O error occurs.
+      void write(const std::string& value) override {
+        stream_.write(value.c_str(), value.length());
+      }
+
+    private:
+      std::stringstream stream_;
+    };
+  }
+}
