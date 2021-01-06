79d5c0ad868d21bd06f385ba637c454c02cda526
Gammasoft <gammasoft71@gmail.com>
2021-01-06 23:12:06 +0100

    add string_reader

--- src/xtd.core/include/xtd/xtd.core.h
+++ src/xtd.core/include/xtd/xtd.core.h
@@ -155,5 +155,6 @@
 #include "io/path_too_long_exception.h"
 #include "io/stream_reader.h"
 #include "io/stream_writer.h"
+#include "io/string_reader.h"
 #include "io/text_reader.h"
 #include "io/text_writer.h"
