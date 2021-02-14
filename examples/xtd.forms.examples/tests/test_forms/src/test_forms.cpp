483eb357a26b7dc5bb2c4362c4b05d80f9eb6c8b
Gammasoft <gammasoft71@gmail.com>
2021-02-14 23:11:30 +0100

    Update test_forms.cpp

--- examples/xtd.forms.examples/tests/test_forms/src/test_forms.cpp
+++ examples/xtd.forms.examples/tests/test_forms/src/test_forms.cpp
@@ -13,17 +13,18 @@ public:
     panel_.parent(*this);
     panel_.dock(dock_style::fill);
     panel_.paint += [&](control& sender, paint_event_args& e) {
-      e.graphics().draw_rectangle(pen(color::black, 1), 10, 10, 20, 20);
-      e.graphics().draw_rectangle(pen(color::white, 1), 12, 12, 16, 16);
+      e.graphics().clear(colors::light_blue());
+      e.graphics().draw_rectangle(pens::black(), 10, 10, 20, 20);
+      e.graphics().draw_rectangle(pens::white(), 12, 12, 16, 16);
 
-      e.graphics().draw_rectangle(pen(color::black, 1), 40, 10, 20, 20);
-      e.graphics().draw_ellipse(pen(color::white, 1), 42, 12, 16, 16);
+      e.graphics().draw_rectangle(pens::black(), 40, 10, 20, 20);
+      e.graphics().draw_ellipse(pens::white(), 42, 12, 16, 16);
 
-      e.graphics().fill_rectangle(solid_brush(color::black), 10, 40, 20, 20);
-      e.graphics().fill_rectangle(solid_brush(color::white), 12, 42, 16, 16);
+      e.graphics().fill_rectangle(brushes::black(), 10, 40, 20, 20);
+      e.graphics().fill_rectangle(brushes::white(), 12, 42, 16, 16);
 
-      e.graphics().fill_rectangle(solid_brush(color::black), 40, 40, 20, 20);
-      e.graphics().fill_ellipse(solid_brush(color::white), 42, 42, 16, 16);
+      e.graphics().fill_rectangle(brushes::black(), 40, 40, 20, 20);
+      e.graphics().fill_ellipse(brushes::white(), 42, 42, 16, 16);
     };
   }
 
