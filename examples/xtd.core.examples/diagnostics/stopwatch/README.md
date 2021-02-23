40738d910a7b125c89e2197c8b98035c62b21525
Gammasoft <gammasoft71@gmail.com>
2021-02-23 22:04:48 +0100

    add stopwatch example

---
+++ examples/xtd.core.examples/diagnostics/stopwatch/README.md
@@ -0,0 +1,23 @@
+# stopwatch
+
+Shows how to use [xtd::diagnostics::stopwatch](../../../../src/xtd.core/include/xtd/diagnostics/stopwatch.h) class.
+
+## Sources
+
+[src/stopwatch.cpp](src/stopwatch.cpp)
+
+[CMakeLists.txt](CMakeLists.txt)
+
+# Build and run
+
+Open "Command Prompt" or "Terminal". Navigate to the folder that contains the project and type the following:
+
+```cmake
+xtdc run
+```
+
+# Output
+
+```
+RunTime 00:00:10.00
+```
---
+++ examples/xtd.core.examples/diagnostics/stopwatch/src/stopwatch.cpp
@@ -0,0 +1,25 @@
+#include <xtd/xtd>
+
+using namespace std;
+using namespace std::chrono;
+using namespace std::literals;
+using namespace std::this_thread;
+using namespace xtd;
+using namespace xtd::diagnostics;
+
+int main() {
+  stopwatch stopwatch;
+  stopwatch.start();
+  sleep_for(10000ms);
+  stopwatch.stop();
+  // Get the elapsed time as a duration value.
+  nanoseconds ns = stopwatch.elapsed();
+  
+  // Format and display the duration value.
+  string elapsed_time = strings::format("{0:H}:{0:M}:{0:S}.{1:D2}", ns, duration_cast<milliseconds>(ns).count() % 1000 / 10);
+  console::write_line("RunTime " + elapsed_time);
+}
+
+// This code produces the following output :
+//
+// RunTime 00:00:10.00
