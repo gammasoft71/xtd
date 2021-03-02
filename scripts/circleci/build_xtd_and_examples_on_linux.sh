a581cc04ed47521fe672d88e01e809384963d15e
Gammasoft <gammasoft71@gmail.com>
2021-03-02 21:09:05 +0100

    add circleci scripts

---
+++ scripts/circleci/build_xtd_and_examples_on_linux.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# clone, generate and build wxwidgets 3.1.4
+mkdir -p build/thirdparty/ && cd build/thirdparty
+git clone https://github.com/wxwidgets/wxwidgets.git -b v3.1.4 --depth 1
+cd wxwidgets
+git submodule update --init
+mkdir build_cmake && cd build_cmake
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=g++-9 -DwxBUILD_SHARED=OFF -DCMAKE_INSTALL_PREFIX=~/local
+if [ $? -ne 0 ]; then exit -1; fi
+cmake --build . --target install -- -j $(nproc)
+if [ $? -ne 0 ]; then exit -1; fi
+cd ../../../..
+
+# generate and build lib
+git submodule update --init
+mkdir -p build && cd build
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=g++-9 -DCMAKE_INSTALL_PREFIX=~/local
+if [ $? -ne 0 ]; then exit -1; fi
+cmake --build . -- -j $(nproc)
+if [ $? -ne 0 ]; then exit -1; fi
+cmake --build . --target install
+if [ $? -ne 0 ]; then exit -1; fi
+cd ..
+
+# generate and build examples
+mkdir -p build/examples
+cd build/examples
+cmake ../../examples -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=g++-9 -DCMAKE_INSTALL_PREFIX=~/local
+if [ $? -ne 0 ]; then exit -1; fi
+cmake --build . -- -j $(nproc)
+if [ $? -ne 0 ]; then exit -1; fi
+cd ../..
