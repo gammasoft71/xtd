4652974af69947d1a26386830de7c35270b3aab0
Gammasoft <gammasoft71@gmail.com>
2021-03-08 23:24:02 +0100

    add example

---
+++ examples/xtd.core.examples/format/format_duration/src/format_duration.cpp
@@ -0,0 +1,23 @@
+#include <xtd/xtd>
+
+using namespace std;
+using namespace std::literals;
+using namespace xtd;
+
+int main() {
+  cout << format("{}", 26h + 3min + 32s + 24ms + 500ns) << endl;
+  cout << format("{:G}", 26h + 3min + 32s + 24ms + 500ns) << endl;
+  cout << format("{:F}", 2h + 3min + 32s + 24ms + 500ns) << endl;
+  cout << format("{0:H}:{0:M}", 2h + 3min + 32s + 24ms + 500ns) << endl;
+  cout << format("{0:H}:{0:M}:{0:S}:{0:N}", 2h + 3min + 32s + 24ms + 500ns) << endl;
+  cout << format("{0:h}:{0:m}:{0:s}.{0:n}", 2h + 3min + 32s + 24ms + 500ns + 24ms + 500ns) << endl;
+}
+
+// This code produces the following output with colors :
+//
+// 1.02:03:32:024000500
+// 1.02:03:32:024000500
+// 0:02:03:32:024000500
+// 02:03
+// 02:03:32:024000500
+// 2:3:32.48001000
