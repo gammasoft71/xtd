c6e4ecbe131853caad991976e1842b8a05e32347
Gammasoft <gammasoft71@gmail.com>
2021-03-09 17:13:45 +0100

    add example

---
+++ examples/xtd.core.examples/format/format_date_time/README.md
@@ -0,0 +1,52 @@
+# format_date_time
+
+Show how to use format [xtd::format](../../../../src/xtd.core/include/xtd/format.h) class.
+
+## Sources
+
+[src/format_date_time.cpp](src/format_date_time.cpp)
+
+[CMakeLists.txt](CMakeLists.txt)
+
+# Build and run
+
+Open "Command Prompt" or "Terminal". Navigate to the folder that contains the project and type the following:
+
+```cmake
+xtdc run
+```
+
+# Output (with colors)
+
+```
+Sat Jan  2 03:04:05 2021
+01/02/2021
+1/02/2021
+03:04:05
+3:04:05
+Sat Jan  2 03:04:05 2021
+Sat Jan  2 03:04:05 2021
+Sat Jan  2 03:04:05 2021
+Sat Jan  2 03:04:05 2021
+Sat 02 Jan 21
+Saturday 2 January 2021
+02/01/21
+2/1/2021
+January 2
+January 2
+Saturday, 2 January 2021
+Saturday, 2 January 2021
+2 January 2021
+2021-01-02T03:04:05
+03:04:05
+3:04:05
+2021-01-02 03:04:05
+Saturday, 2 January 2021 3:04:05
+03:04
+3:04
+January 21
+January 2021
+CET
+CET
+```
+
---
+++ examples/xtd.core.examples/format/format_date_time/src/format_date_time.cpp
@@ -0,0 +1,74 @@
+#include <xtd/xtd>
+
+using namespace std;
+using namespace std::literals;
+using namespace xtd;
+
+int main() {
+  auto date_time = std::chrono::system_clock::now();
+  // Instead previous line you can use following lines :
+  // auto t = std::time(nullptr);
+  // auto date_time = *std::localtime(&t);
+
+  cout << format("{}", date_time) << endl;
+  cout << format("{:d}", date_time) << endl;
+  cout << format("{:D}", date_time) << endl;
+  cout << format("{:t}", date_time) << endl;
+  cout << format("{:T}", date_time) << endl;
+  cout << format("{:f}", date_time) << endl;
+  cout << format("{:F}", date_time) << endl;
+  cout << format("{:g}", date_time) << endl;
+  cout << format("{:G}", date_time) << endl;
+  cout << format("{0:h} {0:i} {0:j} {0:l}", date_time) << endl;
+  cout << format("{0:H} {0:I} {0:J} {0:L}", date_time) << endl;
+  cout << format("{0:i}/{0:k}/{0:l}", date_time) << endl;
+  cout << format("{0:I}/{0:K}/{0:L}", date_time) << endl;
+  cout << format("{:m}", date_time) << endl;
+  cout << format("{:M}", date_time) << endl;
+  cout << format("{:n}", date_time) << endl;
+  cout << format("{:n}", date_time) << endl;
+  cout << format("{:O}", date_time) << endl;
+  cout << format("{:s}", date_time) << endl;
+  cout << format("{:t}", date_time) << endl;
+  cout << format("{:T}", date_time) << endl;
+  cout << format("{:u}", date_time) << endl;
+  cout << format("{:U}", date_time) << endl;
+  cout << format("{:v}", date_time) << endl;
+  cout << format("{:V}", date_time) << endl;
+  cout << format("{:y}", date_time) << endl;
+  cout << format("{:Y}", date_time) << endl;
+  cout << format("{:z}", date_time) << endl;
+  cout << format("{:Z}", date_time) << endl;
+}
+
+// This code can be produce the following output :
+//
+// Sat Jan  2 03:04:05 2021
+// 01/02/2021
+// 1/02/2021
+// 03:04:05
+// 3:04:05
+// Sat Jan  2 03:04:05 2021
+// Sat Jan  2 03:04:05 2021
+// Sat Jan  2 03:04:05 2021
+// Sat Jan  2 03:04:05 2021
+// Sat 02 Jan 21
+// Saturday 2 January 2021
+// 02/01/21
+// 2/1/2021
+// January 2
+// January 2
+// Saturday, 2 January 2021
+// Saturday, 2 January 2021
+// 2 January 2021
+// 2021-01-02T03:04:05
+// 03:04:05
+// 3:04:05
+// 2021-01-02 03:04:05
+// Saturday, 2 January 2021 3:04:05
+// 03:04
+// 3:04
+// January 21
+// January 2021
+// CET
+// CET
