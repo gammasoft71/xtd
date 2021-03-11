38027cd97024bdf5c6db254fdbdee503ebbe1743
Gammasoft <gammasoft71@gmail.com>
2021-03-11 21:30:11 +0100

    Update format_duration.cpp

--- examples/xtd.core.examples/format/format_duration/src/format_duration.cpp
+++ examples/xtd.core.examples/format/format_duration/src/format_duration.cpp
@@ -5,19 +5,40 @@ using namespace std::literals;
 using namespace xtd;
 
 int main() {
-  cout << format("{}", 26h + 3min + 32s + 24ms + 500ns) << endl;
-  cout << format("{:G}", 26h + 3min + 32s + 24ms + 500ns) << endl;
-  cout << format("{:F}", 2h + 3min + 32s + 24ms + 500ns) << endl;
-  cout << format("{0:H}:{0:M}", 2h + 3min + 32s + 24ms + 500ns) << endl;
-  cout << format("{0:H}:{0:M}:{0:S}:{0:N}", 2h + 3min + 32s + 24ms + 500ns) << endl;
-  cout << format("{0:h}:{0:m}:{0:s}.{0:n}", 2h + 3min + 32s + 24ms + 500ns + 24ms + 500ns) << endl;
+  auto duration = 26h + 3min + 32s + 24ms + 500ns;
+  cout << format("{}", duration) << endl;
+  cout << format("{:c}", duration) << endl;
+  cout << format("{:d}", duration) << endl;
+  cout << format("{:D}", duration) << endl;
+  cout << format("{:f}", duration) << endl;
+  cout << format("{:F}", duration) << endl;
+  cout << format("{:g}", duration) << endl;
+  cout << format("{:G}", duration) << endl;
+  cout << format("{:h}", duration) << endl;
+  cout << format("{:H}", duration) << endl;
+  cout << format("{:m}", duration) << endl;
+  cout << format("{:M}", duration) << endl;
+  cout << format("{:n}", duration) << endl;
+  cout << format("{:N}", duration) << endl;
+  cout << format("{:s}", duration) << endl;
+  cout << format("{:S}", duration) << endl;
 }
 
 // This code produces the following output :
 //
 // 1.02:03:32:024000500
 // 1.02:03:32:024000500
-// 0:02:03:32:024000500
-// 02:03
-// 02:03:32:024000500
-// 2:3:32.48001000
+// 1
+// 01
+// 1:2:03:32:024000500
+// 1:02:03:32:024000500
+// 1.2:03:32:024000500
+// 1.02:03:32:024000500
+// 2
+// 02
+// 3
+// 03
+// 24000500
+// 024000500
+// 32
+// 32
