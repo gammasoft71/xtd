a21de9f4436f318f65de5e2e5b0d5d3de01ccf83
Gammasoft <gammasoft71@gmail.com>
2021-03-12 22:06:31 +0100

    Update manual_tests.cpp

--- tests/xtd.core.manual_tests/src/manual_tests.cpp
+++ tests/xtd.core.manual_tests/src/manual_tests.cpp
@@ -4,6 +4,5 @@ using namespace std;
 using namespace xtd;
 
 int main() {
-  //console::write_line("Hello, Wolrd!");
-  console::write_line("{}", initializer_list<int> {1, 2, 3});
+  console::write_line("Hello, Wolrd!");
 }
