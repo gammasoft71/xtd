d2e2a96bfc577aca48b0d82918d7dea895d9bbb6
Gammasoft <gammasoft71@gmail.com>
2021-03-16 18:59:26 +0100

    add docs

--- src/xtd.forms/include/xtd/forms/color_dialog.h
+++ src/xtd.forms/include/xtd/forms/color_dialog.h
@@ -9,7 +9,6 @@ namespace xtd {
   /// @brief The xtd::forms namespace contains classes for creating Windows-based applications that take full advantage of the rich user interface features available in the Microsoft Windows operating system, Apple macOS and Linux like Ubuntu operating system.
   namespace forms {
     /// @brief Represents a common dialog box that displays available colors along with controls that enable the user to define custom colors.
-    /// @brief Used to group collections of controls.
     /// @par Library
     /// xtd.forms
     /// @ingroup xtd_forms container_dialogs
