70b558b8743ebc24567c1ea826d3c042576a89b9
Gammasoft <gammasoft71@gmail.com>
2021-03-16 19:18:23 +0100

    Update background_worker.h

--- src/xtd.forms/include/xtd/forms/background_worker.h
+++ src/xtd.forms/include/xtd/forms/background_worker.h
@@ -20,6 +20,9 @@ namespace xtd {
     /// @remarks To set up for a background operation, add an event handler for the do_work event. Call your time-consuming operation in this event handler. To start the operation, call run_worker_async. To receive notifications of progress updates, handle the progress_changed event. To receive a notification when the operation is completed, handle the run_worker_completed event.
     /// @note You must be careful not to manipulate any user-interface objects in your do_work event handler. Instead, communicate to the user interface through the progress_changed and run_worker_completed events.
     /// @remarks If your background operation requires a parameter, call run_worker_async with your parameter. Inside the do_work event handler, you can extract the parameter from the do_work_event_args.argument property.
+    /// @par Example
+    /// The following code example demonstrate the use of background_worker component.
+    /// @include background_worker.cpp
     class forms_export_ background_worker : public component {
     public:
       /// @brief Initializes a new instance of the background_worker class.
