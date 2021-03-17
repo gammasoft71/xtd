7b369c07fdf5b9e4f77328bb5a3eb95941ee58ad
Gammasoft <gammasoft71@gmail.com>
2021-03-17 21:38:28 +0100

    Update background_worker.h

--- src/xtd.forms/include/xtd/forms/background_worker.h
+++ src/xtd.forms/include/xtd/forms/background_worker.h
@@ -33,42 +33,76 @@ namespace xtd {
       ~background_worker();
       /// @endcond
       
+      /// @brief Gets a value indicating whether the application has requested cancellation of a background operation.
+      /// @return true if the application has requested cancellation of a background operation; otherwise, false. The default is false.
       bool cancellation_pending() const {return cancellation_pending_;}
       
+      /// @brief Gets a value indicating whether the background_worker is running an asynchronous operation.
+      /// @return true, if the background_worker is running an asynchronous operation; otherwise, false.
       bool is_busy() const {return is_busy_;}
       
+      /// @brief Gets a value indicating whether the background_worker can report progress updates.Gets or sets a value indicating whether the background_worker can report progress updates.
+      /// @return true if the background_worker supports progress updates; otherwise false. The default is false.
+      /// @remarks Set the worker_reports_progress property to true if you want the background_worker to support progress updates. When this property is true, user code can call the report_progress method to raise the progress_changed event.
       bool worker_reports_progress() const {return worker_reports_progress_;}
+      /// @brief Sets a value indicating whether the background_worker can report progress updates.Gets or sets a value indicating whether the background_worker can report progress updates.
+      /// @param value true if the background_worker supports progress updates; otherwise false. The default is false.
+      /// @remarks Set the worker_reports_progress property to true if you want the background_worker to support progress updates. When this property is true, user code can call the report_progress method to raise the progress_changed event.
       void worker_reports_progress(bool value) {worker_reports_progress_ = value;}
 
+      /// @brief Gets a value indicating whether the background_worker supports asynchronous cancellation.
+      /// @return true if the background_worker supports cancellation; otherwise false. The default is false.
+      /// @remarks Set the worker_supports_cancellation property to true if you want the background_worker to support cancellation. When this property is true, you can call the cancel_async method to interrupt a background operation.
       bool worker_supports_cancellation() const {return worker_supports_cancellation_;}
+      /// @brief Gets a value indicating whether the background_worker supports asynchronous cancellation.
+      /// @param value true if the background_worker supports cancellation; otherwise false. The default is false.
+      /// @remarks Set the worker_supports_cancellation property to true if you want the background_worker to support cancellation. When this property is true, you can call the cancel_async method to interrupt a background operation.
       void worker_supports_cancellation(bool value) {worker_supports_cancellation_ = value;}
 
-      void on_do_work(do_work_event_args& e) {do_work(*this, e);}
-      
-      void on_progres_changed(const progress_changed_event_args& e) {progress_changed(*this, e);}
-      
-      void on_run_worker_completed(const run_worker_completed_event_args& e) {run_worker_completed(*this, e);}
-
+      /// @brief Requests cancellation of a pending background operation.
+      /// @remarks cancel_async submits a request to terminate the pending background operation and sets the cancellation_pending property to true.
+      /// @remarks When you call cancel_async, your worker method has an opportunity to stop its execution and exit. The worker code should periodically check thecancellation_pending property to see if it has been set to true.
       void cancel_async();
       
+      /// @brief Raises the do_work event.
+      /// @param e An event_args that contains the event data.
+      virtual void on_do_work(do_work_event_args& e) {do_work(*this, e);}
+      
+      /// @brief Raises the progress_changed event.
+      /// @param e An event_args that contains the event data.
+      virtual void on_progres_changed(const progress_changed_event_args& e) {progress_changed(*this, e);}
+      
+      /// @brief Raises the run_worker_completed event.
+      /// @param e An event_args that contains the event data.
+      virtual void on_run_worker_completed(const run_worker_completed_event_args& e) {run_worker_completed(*this, e);}
+
       /// @brief Starts execution of a background operation.
       void run_worker_async();
-
-      void report_progress(int32_t percent_progress);
-      
-      void report_progress(int32_t percent_progress, std::any user_state);
       
       /// @brief Starts execution of a background operation.
+      /// @param argument A parameter for use by the background operation to be executed in the do_work event handler.
       template<typename argument_t>
       void run_worker_async(argument_t argument) {
         argument_ = argument;
         run_worker_async();
       }
 
+      /// @brief Raises the ProgressChanged event.
+      /// @param percent_progress The percentage, from 0 to 100, of the background operation that is complete.
+      void report_progress(int32_t percent_progress);
+      
+      /// @brief Raises the ProgressChanged event.
+      /// @param percent_progress The percentage, from 0 to 100, of the background operation that is complete.
+      /// @param user_state A unique object indicating the user state. Returned as the user_state property of the progress_changed_even_args.
+      void report_progress(int32_t percent_progress, std::any user_state);
+
+      /// @brief Occurs when run_worker_async() is called.
       event<background_worker, do_work_event_handler<component&>> do_work;
-      
+
+      /// @brief Occurs when report_progress(int32_t) is called.
       event<background_worker, progress_changed_event_handler<component&>> progress_changed;
-      
+
+      /// @brief Occurs when the background operation has completed, has been canceled, or has raised an exception.
       event<background_worker, run_worker_completed_event_handler<component&>> run_worker_completed;
       
     private:
