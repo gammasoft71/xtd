ffb6415a90a39665b7ea4d8822c25d66c6b4adbe
Gammasoft <gammasoft71@gmail.com>
2021-03-19 18:56:04 +0100

    add docs

--- src/xtd.forms/include/xtd/forms/progress_bar.h
+++ src/xtd.forms/include/xtd/forms/progress_bar.h
@@ -23,6 +23,18 @@ namespace xtd {
     /// @par Example
     /// The following code example demonstrate the use of progress_bar control.
     /// @include progress_bar.cpp
+    /// @par Windows
+    /// @image html progress_bar_w.png
+    /// <br>
+    /// @image html progress_bar_wd.png
+    /// @par macOS
+    /// @image html progress_bar_m.png
+    /// <br>
+    /// @image html progress_bar_md.png
+    /// @par Gnome
+    /// @image html progress_bar_g.png
+    /// <br>
+    /// @image html progress_bar_gd.png
     class forms_export_ progress_bar : public control {
     public:
       /// @brief Initializes a new instance of the progress_bar class.
