e34d065652923578beb595cbb3bd054e2a4f947c
Gammasoft <gammasoft71@gmail.com>
2021-03-19 21:33:13 +0100

    add docs

--- src/xtd.forms/include/xtd/forms/visual_styles/check_box_state.h
+++ src/xtd.forms/include/xtd/forms/visual_styles/check_box_state.h
@@ -1,5 +1,5 @@
 /// @file
-/// @brief Contains xtd::forms::viruall_styles::check_box_state enum class.
+/// @brief Contains xtd::forms::visual_styles::check_box_state enum class.
 #pragma once
 #include <xtd/strings.h>
 
