302bcff81032ffc386e63370983060067637ed60
Gammasoft <gammasoft71@gmail.com>
2021-03-19 22:45:42 +0100

    split file

---
+++ src/xtd.forms/include/xtd/forms/layout/sorter_none.h
@@ -0,0 +1,22 @@
+/// @file
+/// @brief Contains xtd::forms::layout::sorter_none class.
+#pragma once
+
+/// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
+namespace xtd {
+  /// @brief The xtd::forms namespace contains classes for creating Windows-based applications that take full advantage of the rich user interface features available in the Microsoft Windows operating system, Apple macOS and Linux like Ubuntu operating system.
+  namespace forms {
+    /// @brief The xtd::forms::layout namespace contains classes for implementing layout behaviors in your form or control.
+    namespace layout {
+      /// @brief Represents a sorter none of objects.
+      /// @par Library
+      /// xtd.forms
+      /// @ingroup xtd_forms
+      class sorter_none {
+      public:
+        template<typename type_t>
+        void operator()(type_t first, type_t last) {}
+      };
+    }
+  }
+}
