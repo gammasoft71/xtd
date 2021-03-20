03f322b71120dac0154218e36757b7e03b15b92b
Gammasoft <gammasoft71@gmail.com>
2021-03-20 16:33:45 +0100

    add docs

--- src/xtd.forms/include/xtd/forms/font_dialog.h
+++ src/xtd.forms/include/xtd/forms/font_dialog.h
@@ -1,3 +1,5 @@
+/// @file
+/// @brief Contains xtd::forms::font_dialog dialog.
 #pragma once
 #include <xtd/drawing/color.h>
 #include <xtd/drawing/font.h>
--- src/xtd.forms/include/xtd/forms/font_picker.h
+++ src/xtd.forms/include/xtd/forms/font_picker.h
@@ -1,3 +1,5 @@
+/// @file
+/// @brief Contains xtd::forms::font_picker control.
 #pragma once
 #include "control.h"
 
