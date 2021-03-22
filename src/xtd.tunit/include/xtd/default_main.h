b3f8bf505b774cba678d92888ed1e54d2eb5281a
Gammasoft <gammasoft71@gmail.com>
2021-03-22 19:44:35 +0100

    Update default_main.h

--- src/xtd.tunit/include/xtd/default_main.h
+++ src/xtd.tunit/include/xtd/default_main.h
@@ -1,5 +1,5 @@
 /// @file
-/// @brief Contains main entry point for TUNIT_CONFIG_MAIN.
+/// @brief Contains main entry point for TUNIT_CONFIG_MAIN definition.
 /// @copyright Copyright (c) 2021 Gammasoft. All rights reserved.
 #pragma once
 #include "tunit_main.h"
