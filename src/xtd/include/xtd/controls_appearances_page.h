eb4e7ae25d86b674216d54c4d24c367a576a9f79
Gammasoft <gammasoft71@gmail.com>
2021-03-24 00:09:21 +0100

    add docs

--- src/xtd/include/xtd/controls_appearances_page.h
+++ src/xtd/include/xtd/controls_appearances_page.h
@@ -6,7 +6,9 @@
 ///
 /// Shows controls appearance:
 ///
-/// | Controls           | Mode  | Windows                             | Gtk                                 | macOS                               |
-/// |--------------------|-------|-------------------------------------|-------------------------------------|-------------------------------------|
-/// | xtd::forms::button | Light | @image html control_button_w.png    | @image html control_button_g.png    | @image html control_button_m.png    |
-/// |                    | Dark  | @image html control_button_wd.png   | @image html control_button_gd.png   | @image html control_button_md.png   |
+/// | Controls              | Mode  | Windows                              | Gtk                                  | macOS                                |
+/// |-----------------------|-------|--------------------------------------|--------------------------------------|--------------------------------------|
+/// | xtd::forms::button    | Light | @image html control_button_w.png     | @image html control_button_g.png     | @image html control_button_m.png     |
+/// | xtd::forms::button    | Dark  | @image html control_button_wd.png    | @image html control_button_gd.png    | @image html control_button_md.png    |
+/// | xtd::forms::check_box | Light | @image html control_check_box_w.png  | @image html control_check_box_g.png  | @image html control_check_box_m.png  |
+/// | xtd::forms::check_box | Dark  | @image html control_check_box_wd.png | @image html control_check_box_gd.png | @image html control_check_box_md.png |
