e1c46b727e095273e0fb4a19ff143d3b93b8e49b
Gammasoft <gammasoft71@gmail.com>
2021-03-23 23:32:49 +0100

    add docs

---
+++ src/xtd/include/xtd/controls_appearances_page.h
@@ -0,0 +1,12 @@
+/// @file
+/// @brief Contains controls appearances page.
+
+/// @page ControlsAppearancesPage Controls appearances
+/// @brief Contains controls appearances.
+///
+/// Shows xtd::forms::button appearance:
+///
+/// | Mode  | Windows                             | Gtk                                 | macOS                               |
+/// |-------|-------------------------------------|-------------------------------------|-------------------------------------|
+/// | Light | @image html control_button_w.png    | @image html control_button_g.png    | @image html control_button_m.png    |
+/// | Dark  | @image html control_button_wd.png   | @image html control_button_gd.png   | @image html control_button_md.png   |
