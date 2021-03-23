58ad994d10b422c12440a94ad33e81b9b185914f
Gammasoft <gammasoft71@gmail.com>
2021-03-23 19:53:07 +0100

    Update cursor.h

--- src/xtd.forms/include/xtd/forms/cursor.h
+++ src/xtd.forms/include/xtd/forms/cursor.h
@@ -25,6 +25,7 @@ namespace xtd {
     /// @remarks A cursor is a small picture whose location on the screen is controlled by a pointing device, such as a mouse, pen, or trackball. When the user moves the pointing device, the operating system moves the cursor accordingly.
     /// @remarks Different cursor shapes are used to inform the user what operation the mouse will have. For example, when editing or selecting text, a cursors::ibeam cursor is typically displayed. A wait cursor is commonly used to inform the user that a process is currently running. Examples of processes you might have the user wait for are opening a file, saving a file, or filling a control such as a data_grid, list_box or tree_view with a large amount of data.
     /// @remarks All controls that derive from the Control class have a Cursor property. To change the cursor displayed by the mouse pointer when it is within the bounds of the control, assign a Cursor to the Cursor property of the control. Alternatively, you can display cursors at the application level by assigning a Cursor to the Current property. For example, if the purpose of your application is to edit a text file, you might set the Current property to Cursors.WaitCursor to display a wait cursor over the application while the file loads or saves to prevent any mouse events from being processed. When the process is complete, set the Current property to Cursors.Default for the application to display the appropriate cursor over each control type.
+    /// @see xtd::forms::cursors
     class forms_export_ cursor {
     public:
       /// @brief Initializes a new instance of the Cursor class.
@@ -108,16 +109,16 @@ namespace xtd {
       static cursor none;
       
     private:
-      friend class cursors;
+      friend class xtd::forms::cursors;
       cursor(const xtd::drawing::bitmap& bitmap, const xtd::drawing::point& hot_spot);
       cursor(intptr_t handle, bool destroyable, const std::string& name);
 
       struct data {
         intptr_t handle_ = 0;
         bool destroyable_ = true;
-        drawing::point hot_spot_;
+        xtd::drawing::point hot_spot_;
         std::string name_;
-        drawing::size size_;
+        xtd::drawing::size size_;
         std::any tag_;
       };
       std::shared_ptr<data> data_ = std::make_shared<data>();
