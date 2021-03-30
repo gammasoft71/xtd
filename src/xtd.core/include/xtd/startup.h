26c33bb363af53fd3d078cb7902b1d65a90600a1
Gammasoft <gammasoft71@gmail.com>
2021-03-30 22:50:14 +0200

    fix startup

--- src/xtd.core/include/xtd/startup.h
+++ src/xtd.core/include/xtd/startup.h
@@ -2,7 +2,17 @@
 /// @brief Contains #startup_ keyword.
 /// @copyright Copyright (c) 2021 Gammasoft. All rights reserved.
 #pragma once
+
 #include "environment.h"
+#include "collections/specialized/string_vector.h"
+
+/// @cond
+#undef startup_
+/// @endcond
+
+#if defined(__CMAKE_TARGET_TYPE__) && __CMAKE_TARGET_TYPE__ == 2 // 2 == GUI_APPLICATION
+#include <xtd/system_exception.h>
+#include "xtd/forms/exception_dialog.h"
 
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
@@ -36,7 +46,72 @@ namespace xtd {
         static int run(int (*main_function)(xtd::collections::specialized::string_vector), int argc, char* argv[]) {return main_function({argv + 1, argv + argc});}\
         static int run(int (*main_function)(const xtd::collections::specialized::string_vector&), int argc, char* argv[]) {return main_function({argv + 1, argv + argc});}\
       };\
-      return startup::run(main_class::main, argc, argv);\
-    } \
+      \
+      try {\
+        return startup::run(main_class::main, argc, argv);\
+      } catch(const xtd::system_exception& e) {\
+        xtd::forms::exception_dialog dialog;\
+        dialog.exception(e);\
+        dialog.show_dialog();\
+      } catch(const std::exception& e) {\
+        xtd::forms::exception_dialog dialog;\
+        dialog.exception(e);\
+        dialog.show_dialog();\
+      } catch(...) {\
+        xtd::forms::exception_dialog dialog;\
+        dialog.show_dialog();\
+      } \
+    }\
     int __startup_force_to_end_with_semicolon__ = 0
 }
+#else
+#include "strings.h"
+#include "system_exception.h"
+#include "diagnostics/debug.h"
+
+/// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
+namespace xtd {
+  /// @brief Defines the entry point to be called when the application loads. Generally this is set either to the main form in your application or to the main procedure that should run when the application starts.
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core keywords
+  /// @param main_class The class that contains the static main method.
+  /// @par Examples
+  /// This example show a main method without arguments and without return code
+  /// @include main1.cpp
+  /// @par
+  /// This example show a main method with a return code without arguments
+  /// @include main2.cpp
+  /// @par
+  /// This example show a main method with argument and without return code
+  /// @include main3.cpp
+  /// @par
+  /// This example show a main method with argument and return code
+  /// @include main4.cpp
+  #define startup_(main_class) \
+    int main(int argc, char* argv[]) {\
+      struct startup {\
+        startup() = delete; \
+        static int run(void (*main_function)(), int argc, char* argv[]) {main_function(); return xtd::environment::exit_code();}\
+        static int run(int (*main_function)(), int argc, char* argv[]) {return main_function();}\
+        static int run(void (*main_function)(int argc, char* argv[]), int argc, char* argv[]) {main_function(argc, argv); return xtd::environment::exit_code();}\
+        static int run(void (*main_function)(xtd::collections::specialized::string_vector), int argc, char* argv[]) {main_function({argv + 1, argv + argc}); return xtd::environment::exit_code();}\
+        static int run(void (*main_function)(const xtd::collections::specialized::string_vector&), int argc, char* argv[]) {main_function({argv + 1, argv + argc}); return xtd::environment::exit_code();}\
+        static int run(int (*main_function)(int argc, char* argv[]), int argc, char* argv[]) {return main_function(argc, argv);}\
+        static int run(int (*main_function)(xtd::collections::specialized::string_vector), int argc, char* argv[]) {return main_function({argv + 1, argv + argc});}\
+        static int run(int (*main_function)(const xtd::collections::specialized::string_vector&), int argc, char* argv[]) {return main_function({argv + 1, argv + argc});}\
+      };\
+      \
+      try {\
+        return startup::run(main_class::main, argc, argv);\
+      } catch(const xtd::system_exception& e) {\
+        xtd::diagnostics::debug::write_line(e);\
+      } catch(const std::exception& e) {\
+        xtd::diagnostics::debug::write_line(xtd::strings::format("exception: {}", e.what()));\
+      } catch(...) {\
+        xtd::diagnostics::debug::write_line("Unknown exception occured");\
+      }\
+    }\
+    int __startup_force_to_end_with_semicolon__ = 0
+}
+#endif
