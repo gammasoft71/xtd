473b4c04a0945ec93cd238d6e15a0eb7cd0105c5
Gammasoft <gammasoft71@gmail.com>
2021-03-30 13:13:43 +0200

    add texts property

--- examples/xtd.forms.examples/controls/command_link_button/src/command_link_button.cpp
+++ examples/xtd.forms.examples/controls/command_link_button/src/command_link_button.cpp
@@ -11,7 +11,9 @@ namespace examples {
       client_size({300, 300});
       
       command_link_button1.parent(*this);
-      command_link_button1.text("Command link\nwidth supplementary text");
+      command_link_button1.texts("Command link", "width supplementary text");
+      // same as
+      //command_link_button1.text("Command link\nwidth supplementary text");
       command_link_button1.location({30, 30});
       command_link_button1.size({240, 60});
       command_link_button1.click += [&] {
