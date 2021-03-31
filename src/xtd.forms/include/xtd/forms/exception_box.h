1030e5826deda2a371f71d31be2188d8e885d0fa
Gammasoft <gammasoft71@gmail.com>
2021-03-31 20:26:38 +0200

    Update exception_box.h

--- src/xtd.forms/include/xtd/forms/exception_box.h
+++ src/xtd.forms/include/xtd/forms/exception_box.h
@@ -1,5 +1,5 @@
 /// @file
-/// @brief Contains xtd::forms::message_box dialog.
+/// @brief Contains xtd::forms::exception_box dialog.
 /// @copyright Copyright (c) 2021 Gammasoft. All rights reserved.
 #pragma once
 #include "exception_dialog.h"
