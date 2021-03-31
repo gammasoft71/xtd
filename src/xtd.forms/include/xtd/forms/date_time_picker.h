2b5099373357dcc9e390a9ba8505f150fd2cf6f7
Gammasoft <gammasoft71@gmail.com>
2021-03-31 22:11:14 +0200

    add docs

--- src/xtd.forms/include/xtd/forms/date_time_picker.h
+++ src/xtd.forms/include/xtd/forms/date_time_picker.h
@@ -34,16 +34,37 @@ namespace xtd {
       /// @brief Initializes a new instance of the date_time_picker class.
       date_time_picker();
 
+      /// @brief Gets the format of the date and time displayed in the control.
+      /// @return One of the date_time_picker_format values. The default is long_format.
+      /// @remarks This property determines the date/time format the date is displayed in. The date/time format is based on the user's regional settings in their operating system.
       virtual date_time_picker_format format() const {return format_;}
+      /// @brief Sets the format of the date and time displayed in the control.
+      /// @param format One of the date_time_picker_format values. The default is long_format.
+      /// @return This instance of date_time_picker.
+      /// @remarks This property determines the date/time format the date is displayed in. The date/time format is based on the user's regional settings in their operating system.
       virtual control& format(date_time_picker_format format);
       
+      /// @brief Gets the maximum date and time that can be selected in the control.
+      /// @return The maximum date and time that can be selected in the control.
       virtual std::chrono::system_clock::time_point max_date() const {return max_date_;}
+      /// @brief Sets the maximum date and time that can be selected in the control.
+      /// @param max_date The maximum date and time that can be selected in the control.
+      /// @return This instance of date_time_picker.
       virtual control& max_date(std::chrono::system_clock::time_point max_date);
+      /// @brief Sets the maximum date and time that can be selected in the control.
+      /// @param max_date The maximum date and time that can be selected in the control.
+      /// @return This instance of date_time_picker.
       virtual control& max_date(time_t max_date) {return this->max_date(std::chrono::system_clock::from_time_t(max_date));}
+      /// @brief Sets the maximum date and time that can be selected in the control.
+      /// @param max_date The maximum date and time that can be selected in the control.
+      /// @return This instance of date_time_picker.
       virtual control& max_date(const std::tm& max_date) {
         std::tm internal_max_date = max_date;
         return this->max_date(mktime(&internal_max_date));
       }
+      /// @brief Sets the maximum date and time that can be selected in the control.
+      /// @param max_date The maximum date and time that can be selected in the control.
+      /// @return This instance of date_time_picker.
       virtual control& max_date(int32_t year, int32_t month, int32_t day) {
         std::tm internal_max_date = {};
         internal_max_date.tm_year = year - 1900;
@@ -51,6 +72,9 @@ namespace xtd {
         internal_max_date.tm_mday = day;
         return max_date(mktime(&internal_max_date));
       }
+      /// @brief Sets the maximum date and time that can be selected in the control.
+      /// @param max_date The maximum date and time that can be selected in the control.
+      /// @return This instance of date_time_picker.
       virtual control& max_date(int32_t year, int32_t month, int32_t day, int32_t hour, int32_t minute, int32_t second) {
         std::tm internal_max_date = {};
         internal_max_date.tm_year = year - 1900;
@@ -62,13 +86,27 @@ namespace xtd {
         return max_date(mktime(&internal_max_date));
       }
 
+      /// @brief Gets the minimum date and time that can be selected in the control.
+      /// @return The minimum date and time that can be selected in the control.
       virtual std::chrono::system_clock::time_point min_date() const {return max_date_;}
+      /// @brief Sets the minimum date and time that can be selected in the control.
+      /// @param max_date The minimum date and time that can be selected in the control.
+      /// @return This instance of date_time_picker.
       virtual control& min_date(std::chrono::system_clock::time_point min_date);
+      /// @brief Sets the minimum date and time that can be selected in the control.
+      /// @param max_date The minimum date and time that can be selected in the control.
+      /// @return This instance of date_time_picker.
       virtual control& min_date(time_t min_date) {return this->min_date(std::chrono::system_clock::from_time_t(min_date));}
+      /// @brief Sets the minimum date and time that can be selected in the control.
+      /// @param max_date The minimum date and time that can be selected in the control.
+      /// @return This instance of date_time_picker.
       virtual control& min_date(const std::tm& min_date) {
         std::tm internal_min_date = min_date;
         return this->min_date(mktime(&internal_min_date));
       }
+      /// @brief Sets the minimum date and time that can be selected in the control.
+      /// @param max_date The minimum date and time that can be selected in the control.
+      /// @return This instance of date_time_picker.
       virtual control& min_date(int32_t year, int32_t month, int32_t day) {
         std::tm internal_min_date = {};
         internal_min_date.tm_year = year - 1900;
@@ -76,6 +114,9 @@ namespace xtd {
         internal_min_date.tm_mday = day;
         return min_date(mktime(&internal_min_date));
       }
+      /// @brief Sets the minimum date and time that can be selected in the control.
+      /// @param max_date The minimum date and time that can be selected in the control.
+      /// @return This instance of date_time_picker.
       virtual control& min_date(int32_t year, int32_t month, int32_t day, int32_t hour, int32_t minute, int32_t second) {
         std::tm internal_min_date = {};
         internal_min_date.tm_year = year - 1900;
@@ -87,13 +128,23 @@ namespace xtd {
         return min_date(mktime(&internal_min_date));
       }
 
+      /// @brief Gets the date/time value assigned to the control.
+      /// @return The date and time value assign to the control.
       virtual std::chrono::system_clock::time_point value() const {return value_;}
+      /// @brief Sets the date/time value assigned to the control.
+      /// @param value The date and time value assign to the control.
       virtual control& value(std::chrono::system_clock::time_point value);
+      /// @brief Sets the date/time value assigned to the control.
+      /// @param value The date and time value assign to the control.
       virtual control& value(time_t value) {return this->value(std::chrono::system_clock::from_time_t(value));}
+      /// @brief Sets the date/time value assigned to the control.
+      /// @param value The date and time value assign to the control.
       virtual control& value(const std::tm& value) {
         std::tm internal_value = value;
         return this->value(mktime(&internal_value));
       }
+      /// @brief Sets the date/time value assigned to the control.
+      /// @param value The date and time value assign to the control.
       virtual control& value(int32_t year, int32_t month, int32_t day) {
         std::tm internal_value = {};
         internal_value.tm_year = year - 1900;
@@ -101,6 +152,8 @@ namespace xtd {
         internal_value.tm_mday = day;
         return value(mktime(&internal_value));
       }
+      /// @brief Sets the date/time value assigned to the control.
+      /// @param value The date and time value assign to the control.
       virtual control& value(int32_t year, int32_t month, int32_t day, int32_t hour, int32_t minute, int32_t second) {
         std::tm internal_value = {};
         internal_value.tm_year = year - 1900;
@@ -114,6 +167,7 @@ namespace xtd {
 
       drawing::size default_size() const override {return {100, 25};}
       
+      /// @brief Occurs when the value of the value property changes.
       event<date_time_picker, event_handler<control&>> value_changed;
 
     protected:
@@ -123,13 +177,12 @@ namespace xtd {
       
       drawing::color default_fore_color() const override {return xtd::forms::theme_colors::current_theme().window_text();}
       
+      /// @brief Raises the value_changed event.
+      /// @param e An event_args that contains the event data.
       virtual void on_value_changed(const event_args& e);
       
       void on_handle_created(const event_args& e) override;
       
-      /// @brief Processes Windows messages.
-      /// @param m The Windows Message to process.
-      /// @remarks All messages are sent to the wnd_proc method after getting filtered through the pre_process_message method.
       void wnd_proc(message& message) override;
       
     private:
