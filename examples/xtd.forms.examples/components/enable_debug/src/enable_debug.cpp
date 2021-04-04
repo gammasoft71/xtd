b8b5afb0998c1ba9d287291078bf8ff7ae12fc85
Gammasoft <gammasoft71@gmail.com>
2021-04-04 18:32:36 +0200

    add docs

--- examples/xtd.forms.examples/components/enable_debug/src/enable_debug.cpp
+++ examples/xtd.forms.examples/components/enable_debug/src/enable_debug.cpp
@@ -7,21 +7,29 @@ class form1 : public form {
 public:
   form1() {
     text("Enable debug example");
+    name("form1");
 
     button1.auto_repeat(true);
     button1.auto_size(true);
     button1.location({10, 10});
     button1.parent(*this);
     button1.text("click-me");
+    button1.name("button1");
+
+    text_box1.location({10, 50});
+    text_box1.parent(*this);
+    text_box1.text("Text box");
+    text_box1.name("text_box1");
   }
   
 private:
   button button1;
+  text_box text_box1;
 };
 
 int main() {
   debug_form debug_form;
   enable_debug::trace_switch().level(xtd::diagnostics::trace_level::verbose);
-  enable_debug::set(enable_debug::all, true);
+  enable_debug::set(enable_debug::key_events|enable_debug::mouse_events, true);
   application::run(form1());
 }
--- src/xtd.forms/include/xtd/forms/enable_debug.h
+++ src/xtd.forms/include/xtd/forms/enable_debug.h
@@ -11,30 +11,64 @@
 namespace xtd {
   /// @brief The xtd::forms namespace contains classes for creating Windows-based applications that take full advantage of the rich user interface features available in the Microsoft Windows operating system, Apple macOS and Linux like Ubuntu operating system.
   namespace forms {
+    /// @brief Represent enable debug class.
+    /// @par Library
+    /// xtd.forms
+    /// @ingroup xtd_forms debug
+    /// @remarks You can activate or deactive one, some or all debug trace categories.
+    /// @par Examples
+    /// The following code example demonstrate the use of enable_debug class.
+    /// @include enable_debug.cpp
+    /// @par Windows
+    /// @image html enable_debug_w.png
+    /// <br>
+    /// @image html enable_debug_wd.png
+    /// @par macOS
+    /// @image html enable_debug_m.png
+    /// <br>
+    /// @image html enable_debug_md.png
+    /// @par Gnome
+    /// @image html enable_debug_g.png
+    /// <br>
+    /// @image html enable_debug_gd.png
     class forms_export_ enable_debug {
     public:
+      /// @cond
       enable_debug() = default;
       enable_debug(const enable_debug&) = default;
       enable_debug& operator=(const enable_debug&) = default;
 
-      static const enable_debug none;
-      static const enable_debug events;
-      static const enable_debug key_events;
-      static const enable_debug mouse_events;
-      static const enable_debug layout;
-      static const enable_debug workaround;
-      static const enable_debug all;
-
-      static bool get(const enable_debug& flag) {return (values_.value_ & flag.value_) == flag.value_;}
-      static void set(const enable_debug& flag, bool value) {values_.value_ = value ? (values_.value_ | flag.value_) : (values_.value_ & ~flag.value_);}
-      
-      static xtd::diagnostics::trace_switch& trace_switch() {return trace_switch_;}
-
       bool operator==(const enable_debug& value) const {return value_ == value.value_;}
       bool operator!=(const enable_debug& value) const  {return !operator==(value);}
-
       enable_debug operator|(const enable_debug& value) const {return enable_debug(value_ | value.value_);}
       enable_debug operator+(const enable_debug& value) const {return enable_debug(value_ + value.value_);}
+      /// @endcond
+
+      /// @brief Represent none debug.
+      static const enable_debug none;
+      /// @brief Represent events debug.
+      static const enable_debug events;
+      /// @brief Represent key events debug.
+      static const enable_debug key_events;
+      /// @brief Represent mouse events debug.
+      static const enable_debug mouse_events;
+      /// @brief Represent layout debug.
+      static const enable_debug layout;
+      /// @brief Represent workaround debug.
+      static const enable_debug workaround;
+      /// @brief Represent all debug.
+      static const enable_debug all;
+
+      /// @brief Gets enable_debug flags status.
+      /// @return true if enable_debug flags is on; otherwise false.
+      static bool get(const enable_debug& flags) {return (values_.value_ & flags.value_) == flags.value_;}
+      /// @brief Sets enable_debug flags status.
+      /// @param on true if enable_debug flags is on; otherwise false.
+      static void set(const enable_debug& flags, bool on) {values_.value_ = on ? (values_.value_ | flags.value_) : (values_.value_ & ~flags.value_);}
+      
+      /// @brief Gets a trace switch to configure debug traces.
+      /// @return a xtd::diagnostics::trace_switch to configure debug traces.
+      static xtd::diagnostics::trace_switch& trace_switch() {return trace_switch_;}
 
     private:
       explicit enable_debug(uint64_t value) : value_(value) {}
