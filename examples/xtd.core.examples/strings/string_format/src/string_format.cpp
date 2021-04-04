433e903f84568d7d858bf484875043e36b13f6e0
Gammasoft <gammasoft71@gmail.com>
2021-04-04 22:46:26 +0200

    fix build

--- examples/xtd.core.examples/strings/string_format/src/string_format.cpp
+++ examples/xtd.core.examples/strings/string_format/src/string_format.cpp
@@ -2,9 +2,8 @@
 #include <iostream>
 
 using namespace std;
-using namespace std::string_literals;
+using namespace std::literals;
 using namespace xtd;
-using namespace xtd::string_literals;
 
 int main() {
   cout << strings::format("{0,10} {1} {2:D3} {3}", "string", "literal"_is, 42, 'a') << endl;
