aafe14e92ce2a9ea42ee9d76c563cde46c684673
Gammasoft <gammasoft71@gmail.com>
2021-04-08 22:26:58 +0200

    Update mouse_events.cpp

--- examples/xtd.forms.examples/events/mouse_events/src/mouse_events.cpp
+++ examples/xtd.forms.examples/events/mouse_events/src/mouse_events.cpp
@@ -2,7 +2,6 @@
 #include <xtd/xtd>
 
 using namespace xtd;
-using namespace xtd::diagnostics;
 using namespace xtd::forms;
 
 class form1 : public form {
@@ -11,52 +10,52 @@ public:
     text("Mouse events example");
 
     click += [&] {
-      trace::write_line("click");
+      xtd::diagnostics::trace::write_line("click");
     };
     
     double_click += [&] {
-      trace::write_line("double_click");
+      xtd::diagnostics::trace::write_line("double_click");
     };
     
     mouse_click += [&](control& sender, const mouse_event_args& e) {
-      trace::write_line("mouse_click={{button={}, clicks={}, delta={}, location=[{}], modifier_keys=[{}]}}", e.button(), e.clicks(), e.delta(), e.location(), modifier_keys());
+      xtd::diagnostics::trace::write_line("mouse_click={{button={}, clicks={}, delta={}, location=[{}], modifier_keys=[{}]}}", e.button(), e.clicks(), e.delta(), e.location(), modifier_keys());
     };
     
     mouse_double_click += [&](control& sender, const mouse_event_args& e) {
-      trace::write_line("mouse_double_click={{button={}, clicks={}, delta={}, location=[{}], modifier_keys=[{}]}}", e.button(), e.clicks(), e.delta(), e.location(), modifier_keys());
+      xtd::diagnostics::trace::write_line("mouse_double_click={{button={}, clicks={}, delta={}, location=[{}], modifier_keys=[{}]}}", e.button(), e.clicks(), e.delta(), e.location(), modifier_keys());
     };
     
     mouse_down += [&](control& sender, const mouse_event_args& e) {
-      trace::write_line("mouse_down={{button={}, clicks={}, delta={}, location=[{}], modifier_keys=[{}]}}", e.button(), e.clicks(), e.delta(), e.location(), modifier_keys());
+      xtd::diagnostics::trace::write_line("mouse_down={{button={}, clicks={}, delta={}, location=[{}], modifier_keys=[{}]}}", e.button(), e.clicks(), e.delta(), e.location(), modifier_keys());
     };
 
     mouse_enter += [&] {
-      trace::write_line("mouse_enter");
+      xtd::diagnostics::trace::write_line("mouse_enter");
     };
     
     mouse_horizontal_wheel += [&](control& sender, const mouse_event_args& e) {
-      trace::write_line("mouse_horizontal_wheel={{button={}, clicks={}, delta={}, location=[{}], modifier_keys=[{}]}}", e.button(), e.clicks(), e.delta(), e.location(), modifier_keys());
+      xtd::diagnostics::trace::write_line("mouse_horizontal_wheel={{button={}, clicks={}, delta={}, location=[{}], modifier_keys=[{}]}}", e.button(), e.clicks(), e.delta(), e.location(), modifier_keys());
     };
 
     mouse_leave += [&] {
-      trace::write_line("mouse_leave");
+      xtd::diagnostics::trace::write_line("mouse_leave");
     };
     
     mouse_move += [&](control& sender, const mouse_event_args& e) {
-      trace::write_line("mouse_move={{button={}, clicks={}, delta={}, location=[{}], modifier_keys=[{}]}}", e.button(), e.clicks(), e.delta(), e.location(), modifier_keys());
+      xtd::diagnostics::trace::write_line("mouse_move={{button={}, clicks={}, delta={}, location=[{}], modifier_keys=[{}]}}", e.button(), e.clicks(), e.delta(), e.location(), modifier_keys());
     };
     
     mouse_up += [&](control& sender, const mouse_event_args& e) {
-      trace::write_line("mouse_up={{button={}, clicks={}, delta={}, location=[{}], modifier_keys=[{}]}}", e.button(), e.clicks(), e.delta(), e.location(), modifier_keys());
+      xtd::diagnostics::trace::write_line("mouse_up={{button={}, clicks={}, delta={}, location=[{}], modifier_keys=[{}]}}", e.button(), e.clicks(), e.delta(), e.location(), modifier_keys());
     };
     
     mouse_wheel += [&](control& sender, const mouse_event_args& e) {
-      trace::write_line("mouse_wheel={{button={}, clicks={}, delta={}, location=[{}], modifier_keys=[{}]}}", e.button(), e.clicks(), e.delta(), e.location(), modifier_keys());
+      xtd::diagnostics::trace::write_line("mouse_wheel={{button={}, clicks={}, delta={}, location=[{}], modifier_keys=[{}]}}", e.button(), e.clicks(), e.delta(), e.location(), modifier_keys());
     };
   }
 };
 
 int main() {
-  trace_form tform;
+  trace_form trace_form;
   application::run(form1());
 }
