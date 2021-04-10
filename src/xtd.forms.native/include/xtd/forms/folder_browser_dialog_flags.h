6f3426911865b374dead51e8772927614566342b
Gammasoft <gammasoft71@gmail.com>
2021-04-10 21:14:32 +0200

    add docs

--- src/xtd.forms.native/include/xtd/forms/folder_browser_dialog_flags.h
+++ src/xtd.forms.native/include/xtd/forms/folder_browser_dialog_flags.h
@@ -1,5 +1,5 @@
 /// @file
-/// @brief Contains arranged foleder browser flags constants.
+/// @brief Contains arranged foleder browser dialog flags constants.
 /// @copyright Copyright (c) 2021 Gammasoft. All rights reserved.
 #pragma once
 #include <cstddef>
