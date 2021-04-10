8dcd5656fc02673a3980cb542efd37348b810286
Gammasoft <gammasoft71@gmail.com>
2021-04-10 20:31:38 +0200

    add docs

--- src/xtd.forms.native/include/xtd/forms/native/about_dialog.h
+++ src/xtd.forms.native/include/xtd/forms/native/about_dialog.h
@@ -34,6 +34,7 @@ namespace xtd {
         /// @param translators A translators_collection that specifies the translators collection.
         /// @param designers A designers_collection that specifies the gesigners collection.
         /// @param license A string that specifies the license text to display.
+        /// @warning Internal use only
         static void show(intptr_t hwnd, const xtd::drawing::icon& icon, const std::string& name, const std::string& description, const std::string& version, const std::string& long_version, const std::string& copyright, const std::string& website, const std::string& website_label, const std::vector<std::string>& creators, const std::vector<std::string>& designers, const std::vector<std::string>& doc_writers, const std::vector<std::string>& translators, const std::string& license);
       };
     }
