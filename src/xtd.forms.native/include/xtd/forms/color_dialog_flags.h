bbfc75c8707d1ac0081174f73ea5bd8433090165
Gammasoft <gammasoft71@gmail.com>
2021-04-11 00:19:22 +0200

    add docs

--- src/xtd.forms.native/include/xtd/forms/color_dialog_flags.h
+++ src/xtd.forms.native/include/xtd/forms/color_dialog_flags.h
@@ -1,5 +1,5 @@
 /// @file
-/// @brief Contains arranged color dialog flags constants.
+/// @brief Contains color dialog flags constants.
 /// @copyright Copyright (c) 2021 Gammasoft. All rights reserved.
 #pragma once
 #include <cstddef>
