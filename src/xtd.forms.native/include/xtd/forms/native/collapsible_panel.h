075c991f20c71e546f3319d47daccf9ff6714982
Gammasoft <gammasoft71@gmail.com>
2021-04-11 00:01:50 +0200

    add docs

--- src/xtd.forms.native/include/xtd/forms/native/collapsible_panel.h
+++ src/xtd.forms.native/include/xtd/forms/native/collapsible_panel.h
@@ -1,14 +1,32 @@
+/// @file
+/// @brief Contains xtd::forms::native::collapsible_panel API.
+/// @copyright Copyright (c) 2021 Gammasoft. All rights reserved.
 #pragma once
 #include "../create_params.h"
 #include <xtd/static.h>
 #include <xtd/forms_native_export.h>
 
+/// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
+  /// @brief The xtd::forms namespace contains classes for creating Windows-based applications that take full advantage of the rich user interface features available in the Microsoft Windows operating system, Apple macOS and Linux like Ubuntu operating system.
   namespace forms {
+    /// @brief The xtd::forms::native namespace contains internal native API definitions to access underlying toolkits used by xtd.forms library.
+    /// @warning Internal use only
     namespace native {
+      /// @brief Contains collapsible panel native API.
+      /// @par Library
+      /// xtd.forms.native
+      /// @ingroup xtd_forms_native
+      /// @warning Internal use only
       class forms_native_export_ collapsible_panel final static_ {
       public:
+        /// @brief Gets a value indicating whether the collapsible_panel is in the expanded.
+        /// @param control Collapsible panel window handle.
+        /// @return true if collapsible_panel is expanded; otherwise false.
         static bool expanded(intptr_t handle);
+        /// @brief Sets a value indicating whether the collapsible_panel is in the expanded.
+        /// @param control Collapsible panel window handle.
+        /// @param expanded true if collapsible_panel is expanded; otherwise false.
         static void expanded(intptr_t handle, bool expanded);
       };
     }
