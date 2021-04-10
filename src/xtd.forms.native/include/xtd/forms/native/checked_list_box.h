6951754ba3bda5f6ed85f9a6243171314738ecc4
Gammasoft <gammasoft71@gmail.com>
2021-04-10 23:35:52 +0200

    add docs

--- src/xtd.forms.native/include/xtd/forms/native/checked_list_box.h
+++ src/xtd.forms.native/include/xtd/forms/native/checked_list_box.h
@@ -7,8 +7,12 @@
 #include <xtd/static.h>
 #include <xtd/forms_native_export.h>
 
+/// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
+  /// @brief The xtd::forms namespace contains classes for creating Windows-based applications that take full advantage of the rich user interface features available in the Microsoft Windows operating system, Apple macOS and Linux like Ubuntu operating system.
   namespace forms {
+    /// @brief The xtd::forms::native namespace contains internal native API definitions to access underlying toolkits used by xtd.forms library.
+    /// @warning Internal use only
     namespace native {
       /// @brief Contains checked list box native API.
       /// @par Library
