9d52d7ce394929baa4101b2eb3c404caab8aef9f
Gammasoft <gammasoft71@gmail.com>
2021-04-10 21:14:32 +0200

    add docs

--- src/xtd.forms.native/include/xtd/forms/native/about_dialog.h
+++ src/xtd.forms.native/include/xtd/forms/native/about_dialog.h
@@ -9,8 +9,12 @@
 #include <xtd/drawing/icon.h>
 #include <xtd/forms_native_export.h>
 
+/// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
+  /// @brief The xtd::forms namespace contains classes for creating Windows-based applications that take full advantage of the rich user interface features available in the Microsoft Windows operating system, Apple macOS and Linux like Ubuntu operating system.
   namespace forms {
+    /// @brief The xtd::forms::native namespace contains internal native API definitions to access underlying toolkits used by xtd.forms library.
+    /// @warning Internal use only
     namespace native {
       /// @brief Contains about dialog native API.
       /// @par Library
