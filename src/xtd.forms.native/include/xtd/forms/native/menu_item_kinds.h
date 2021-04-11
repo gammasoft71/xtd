dac9ecd2d990a23d1b9e8626941317ef1d51ac53
Gammasoft <gammasoft71@gmail.com>
2021-04-11 22:33:52 +0200

    add docs

--- src/xtd.forms.native/include/xtd/forms/native/menu_item_kinds.h
+++ src/xtd.forms.native/include/xtd/forms/native/menu_item_kinds.h
@@ -1,5 +1,5 @@
 /// @file
-/// @brief Contains menu item kind constants.
+/// @brief Contains menu item kinds constants.
 /// @copyright Copyright (c) 2021 Gammasoft. All rights reserved.
 #pragma once
 #include <cstddef>
--- src/xtd.forms.native/include/xtd/forms/native/message_keys.h
+++ src/xtd.forms.native/include/xtd/forms/native/message_keys.h
@@ -1,12 +1,50 @@
+/// @file
+/// @brief Contains message keys constants.
+/// @copyright Copyright (c) 2021 Gammasoft. All rights reserved.
 #pragma once
 #include <cstddef>
 
 #ifndef MK_LBUTTON
+/// @brief The left mouse button is down.
+/// @par Library
+/// xtd.forms.native
+/// @ingroup xtd_forms_native
+/// @warning Internal use only
 constexpr size_t MK_LBUTTON = 0x0001;
+/// @brief The right mouse button is down.
+/// @par Library
+/// xtd.forms.native
+/// @ingroup xtd_forms_native
+/// @warning Internal use only
 constexpr size_t MK_RBUTTON = 0x0002;
+/// @brief The SHIFT key is down.
+/// @par Library
+/// xtd.forms.native
+/// @ingroup xtd_forms_native
+/// @warning Internal use only
 constexpr size_t MK_SHIFT = 0x0004;
+/// @brief The CTRL key is down.
+/// @par Library
+/// xtd.forms.native
+/// @ingroup xtd_forms_native
+/// @warning Internal use only
 constexpr size_t MK_CONTROL = 0x0008;
+/// @brief The middle mouse button is down.
+/// @par Library
+/// xtd.forms.native
+/// @ingroup xtd_forms_native
+/// @warning Internal use only
 constexpr size_t MK_MBUTTON = 0x0010;
+/// @brief The first X button is down.
+/// @par Library
+/// xtd.forms.native
+/// @ingroup xtd_forms_native
+/// @warning Internal use only
 constexpr size_t MK_XBUTTON1 = 0x0020;
+/// @brief The second X button is down.
+/// @par Library
+/// xtd.forms.native
+/// @ingroup xtd_forms_native
+/// @warning Internal use only
 constexpr size_t MK_XBUTTON2 = 0x0040;
 #endif
