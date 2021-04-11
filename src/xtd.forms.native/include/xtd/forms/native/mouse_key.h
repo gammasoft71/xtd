bbff596f5639aaef6eb3811b6f11bf2c29d39840
Gammasoft <gammasoft71@gmail.com>
2021-04-11 23:18:43 +0200

    add docs

--- src/xtd.forms.native/include/xtd/forms/native/mouse_key.h
+++ src/xtd.forms.native/include/xtd/forms/native/mouse_key.h
@@ -1,5 +1,5 @@
 /// @file
-/// @brief Contains moouse key constants.
+/// @brief Contains mouse key constants.
 /// @copyright Copyright (c) 2021 Gammasoft. All rights reserved.
 #pragma once
 #include <cstddef>
