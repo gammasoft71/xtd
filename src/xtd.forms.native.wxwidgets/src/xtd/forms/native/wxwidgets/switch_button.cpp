f1b80d57664ebcd63005c2c46257b0f785374dad
Gammasoft <gammasoft71@gmail.com>
2021-04-12 22:53:48 +0200

    add switch_button

---
+++ src/xtd.forms.native.wxwidgets/src/xtd/forms/native/wxwidgets/switch_button.cpp
@@ -0,0 +1,11 @@
+#include <xtd/forms/native/switch_button.h>
+#include "../../../../../include/xtd/forms/native/wxwidgets/wx_switch_button.h"
+
+using namespace xtd;
+using namespace xtd::drawing;
+using namespace xtd::forms::native;
+
+void switch_button::checked(intptr_t control, bool checked) {
+  if (control == 0 || reinterpret_cast<wx_switch_button*>(control)->owner_draw_) return;
+  static_cast<wxToggleButton*>(reinterpret_cast<wx_switch_button*>(control)->control())->SetValue(checked);
+}
