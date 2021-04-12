c2ca4e13df65b3dbf561ec6a6cfed252b51a4eb9
Gammasoft <gammasoft71@gmail.com>
2021-04-12 21:06:56 +0200

    add docs

--- src/xtd.forms.native/include/xtd/forms/native/tab_control.h
+++ src/xtd.forms.native/include/xtd/forms/native/tab_control.h
@@ -22,13 +22,47 @@ namespace xtd {
       /// @warning Internal use only
       class forms_native_export_ tab_control final static_ {
       public:
+        /// @brief Maintains performance while items are added to the tab_control one at a tab_page by preventing the control from drawing until the end_update() method is called.
+        /// @param control Tab control window handle.
+        /// @warning Internal use only
+        static void begin_update(intptr_t control);
+        /// @brief Deletes specified tab page index.
+        /// @param control Tab control window handle.
+        /// @param index The item index to delete.
+        /// @warning Internal use only
         static void delete_item(intptr_t control, size_t index);
+        /// @brief Resumes painting the tab_control control after painting is suspended by the begin_update method.
+        /// @param control Tab control window handle.
+        /// @warning Internal use only
+        static void end_update(intptr_t control);
+        /// @brief Sets image list.
+        /// @param control Tab control window handle.
+        /// @param image_list The image list handle.
+        /// @warning Internal use only
         static void image_list(intptr_t control, intptr_t image_list);
+        /// @brief Inserts at specified tab page index a specified tab page handle.
+        /// @param control Tab control window handle.
+        /// @param index The tab page index to insert.
+        /// @param page The tab page handle.
+        /// @warning Internal use only
         static void insert_item(intptr_t control, size_t index, intptr_t page);
         static void page_image_index(intptr_t control, size_t index, size_t image_index);
         static void page_text(intptr_t control, size_t index, const std::string& text);
+        /// @brief Gets selected tab page index.
+        /// @param control Tab control window handle.
+        /// @return The selected tab page index.
+        /// @warning Internal use only
         static size_t selected_index(intptr_t control);
+        /// @brief Sets selected tab page with specified index
+        /// @param control Tab control window handle.
+        /// @param index The selected tab page index.
+        /// @warning Internal use only
         static void selected_index(intptr_t control, size_t index);
+        /// @brief Updates a specified tab page index with specified tab page handle.
+        /// @param control Tab control window handle.
+        /// @param index The tab page index to update.
+        /// @param page The tab page handle.
+        /// @warning Internal use only
         static void update_item(intptr_t control, size_t index, intptr_t page);
       };
     }
