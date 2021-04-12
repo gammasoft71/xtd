26a9ee61dda32daa22f7abe9ad2a375be4b6d9a4
Gammasoft <gammasoft71@gmail.com>
2021-04-12 21:31:10 +0200

    move text and image_index from tab_control to tab_page

--- src/xtd.forms.native.wxwidgets/src/xtd/forms/native/wxwidgets/tab_page.cpp
+++ src/xtd.forms.native.wxwidgets/src/xtd/forms/native/wxwidgets/tab_page.cpp
@@ -4,3 +4,17 @@
 using namespace xtd;
 using namespace xtd::drawing;
 using namespace xtd::forms::native;
+
+void tab_page::image_index(intptr_t control, size_t image_index) {
+  if (control == 0) return;
+  auto page = static_cast<wxNotebookPage*>(reinterpret_cast<control_handler*>(control)->control());
+  auto note_book = static_cast<wxNotebookBase*>(page->GetParent());
+  if (note_book) note_book->SetPageImage(note_book->FindPage(page), image_index);
+}
+
+void tab_page::text(intptr_t control, const std::string& text) {
+  if (control == 0) return;
+  auto page = static_cast<wxNotebookPage*>(reinterpret_cast<control_handler*>(control)->control());
+  auto note_book = static_cast<wxNotebookBase*>(page->GetParent());
+  if (note_book) note_book->SetPageText(note_book->FindPage(page), text);
+}
--- src/xtd.forms.native/include/xtd/forms/native/tab_page.h
+++ src/xtd.forms.native/include/xtd/forms/native/tab_page.h
@@ -6,9 +6,10 @@
 namespace xtd {
   namespace forms {
     namespace native {
-      // class forms_native_export_ tab_page final static_ {
-      class tab_page final static_ {
+      class forms_native_export_ tab_page final static_ {
       public:
+        static void image_index(intptr_t control, size_t image_index);
+        static void text(intptr_t control, const std::string& text);
       };
     }
   }
