24fe38cf0fd887289e4f54709aba9ac5b12241f7
Gammasoft <gammasoft71@gmail.com>
2021-04-12 22:53:48 +0200

    add switch_button

--- src/xtd.forms.native/include/xtd/forms/native/switch_button.h
+++ src/xtd.forms.native/include/xtd/forms/native/switch_button.h
@@ -1,5 +1,5 @@
 /// @file
-/// @brief Contains xtd::forms::native::radio_button API.
+/// @brief Contains xtd::forms::native::switch_button API.
 /// @copyright Copyright (c) 2021 Gammasoft. All rights reserved.
 #pragma once
 #include "../create_params.h"
@@ -13,16 +13,16 @@ namespace xtd {
     /// @brief The xtd::forms::native namespace contains internal native API definitions to access underlying toolkits used by xtd.forms library.
     /// @warning Internal use only
     namespace native {
-      /// @brief Contains radio button native API.
+      /// @brief Contains Switch button native API.
       /// @par Library
       /// xtd.forms.native
       /// @ingroup xtd_forms_native
       /// @warning Internal use only
-      class forms_native_export_ radio_button final static_ {
+      class forms_native_export_ switch_button final static_ {
       public:
         /// @brief Sets checked.
-        /// @param control Radio button window handle.
-        /// @param checked true if the radio button is checked; otherwise, false.
+        /// @param control Switch button window handle.
+        /// @param checked true if the switch button is checked; otherwise, false.
         /// @warning Internal use only
         static void checked(intptr_t control, bool checked);
       };
