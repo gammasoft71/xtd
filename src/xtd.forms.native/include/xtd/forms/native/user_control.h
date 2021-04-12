501974a22b25c5cf93928fd66f35459a836a5dd1
Gammasoft <gammasoft71@gmail.com>
2021-04-13 00:02:12 +0200

    add docs

--- src/xtd.forms.native/include/xtd/forms/native/user_control.h
+++ src/xtd.forms.native/include/xtd/forms/native/user_control.h
@@ -1,13 +1,29 @@
+/// @file
+/// @brief Contains xtd::forms::native::user_control API.
+/// @copyright Copyright (c) 2021 Gammasoft. All rights reserved.#pragma once
 #pragma once
 #include "../create_params.h"
 #include <xtd/static.h>
 #include <xtd/forms_native_export.h>
 
+/// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
+  /// @brief The xtd::forms namespace contains classes for creating Windows-based applications that take full advantage of the rich user interface features available in the Microsoft Windows operating system, Apple macOS and Linux like Ubuntu operating system.
   namespace forms {
+    /// @brief The xtd::forms::native namespace contains internal native API definitions to access underlying toolkits used by xtd.forms library.
+    /// @warning Internal use only
     namespace native {
+      /// @brief Contains user control native API.
+      /// @par Library
+      /// xtd.forms.native
+      /// @ingroup xtd_forms_native
+      /// @warning Internal use only
       class forms_native_export_ user_control final static_ {
       public:
+        /// @brief Set virtual size
+        /// @param control User control window handle.
+        /// @param size A virtual xtd::drawing::size.
+        /// @warning Internal use only
         static void virtual_size(intptr_t control, const drawing::size& size);
       };
     }
