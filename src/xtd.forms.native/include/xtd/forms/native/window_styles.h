dad65d98bab3a77df9046a88fd2024b8a91070e0
Gammasoft <gammasoft71@gmail.com>
2021-04-13 20:36:49 +0200

    add docs

--- src/xtd.forms.native/include/xtd/forms/native/window_styles.h
+++ src/xtd.forms.native/include/xtd/forms/native/window_styles.h
@@ -4,76 +4,181 @@
 #pragma once
 #include <cstddef>
 
+// https://docs.microsoft.com/en-us/cpp/mfc/reference/styles-used-by-mfc
+
 #ifndef WS_OVERLAPPED
+/// @brief Creates an overlapped window. An overlapped window usually has a caption and a border.
+/// @par Library
+/// xtd.forms.native
+/// @ingroup xtd_forms_native ws
+/// @warning Internal use only
 constexpr size_t WS_OVERLAPPED = 0x00000000;
+/// @brief Creates a window that has a Maximize button.
+/// @par Library
+/// xtd.forms.native
+/// @ingroup xtd_forms_native ws
+/// @warning Internal use only
 constexpr size_t WS_MAXIMIZEBOX = 0x00010000;
+/// @brief Creates a window that has a Minimize button.
+/// @par Library
+/// xtd.forms.native
+/// @ingroup xtd_forms_native ws
+/// @warning Internal use only
 constexpr size_t WS_MINIMIZEBOX = 0x00020000;
+/// @brief Creates a window with a thick frame that can be used to size the window.
+/// @par Library
+/// xtd.forms.native
+/// @ingroup xtd_forms_native ws
+/// @warning Internal use only
 constexpr size_t WS_THICKFRAME = 0x00040000;
+/// @brief Creates a window that has a Control-menu box in its title bar. Used only for windows with title bars.
+/// @par Library
+/// xtd.forms.native
+/// @ingroup xtd_forms_native ws
+/// @warning Internal use only
 constexpr size_t WS_SYSMENU = 0x00080000;
+/// @brief Creates a window that has a horizontal scroll bar.
+/// @par Library
+/// xtd.forms.native
+/// @ingroup xtd_forms_native ws
+/// @warning Internal use only
 constexpr size_t WS_HSCROLL = 0x00100000;
+/// @brief Creates a window that has a vertical scroll bar.
+/// @par Library
+/// xtd.forms.native
+/// @ingroup xtd_forms_native ws
+/// @warning Internal use only
 constexpr size_t WS_VSCROLL = 0x00200000;
+/// @brief Creates a window with a double border but no title.
+/// @par Library
+/// xtd.forms.native
+/// @ingroup xtd_forms_native ws
+/// @warning Internal use only
 constexpr size_t WS_DLGFRAME = 0x00400000;
+/// @brief Creates a window that has a border.
+/// @par Library
+/// xtd.forms.native
+/// @ingroup xtd_forms_native ws
+/// @warning Internal use only
 constexpr size_t WS_BORDER = 0x00800000;
+/// @brief Creates a window that has a title bar (implies the WS_BORDER style). Cannot be used with the WS_DLGFRAME style.
+/// @par Library
+/// xtd.forms.native
+/// @ingroup xtd_forms_native ws
+/// @warning Internal use only
+/// @brief
+/// @par Library
+/// xtd.forms.native
+/// @ingroup xtd_forms_native ws
+/// @warning Internal use only
 constexpr size_t WS_CAPTION = 0x00C00000; // WS_BORDER|WS_DLGFRAME
+/// @brief Creates a window of maximum size.
+/// @par Library
+/// xtd.forms.native
+/// @ingroup xtd_forms_native ws
+/// @warning Internal use only
 constexpr size_t WS_MAXIMIZE = 0x01000000;
+/// @brief Excludes the area occupied by child windows when you draw within the parent window. Used when you create the parent window.
+/// @par Library
+/// xtd.forms.native
+/// @ingroup xtd_forms_native ws
+/// @warning Internal use only
 constexpr size_t WS_CLIPCHILDREN = 0x02000000;
+/// @brief Clips child windows relative to each other; that is, when a particular child window receives a paint message, the WS_CLIPSIBLINGS style clips all other overlapped child windows out of the region of the child window to be updated. (If WS_CLIPSIBLINGS is not given and child windows overlap, when you draw within the client area of a child window, it is possible to draw within the client area of a neighboring child window.) For use with the WS_CHILD style only.
+/// @par Library
+/// xtd.forms.native
+/// @ingroup xtd_forms_native ws
+/// @warning Internal use only
 constexpr size_t WS_CLIPSIBLINGS = 0x04000000;
+/// @brief Creates a window that is initially disabled.
+/// @par Library
+/// xtd.forms.native
+/// @ingroup xtd_forms_native ws
+/// @warning Internal use only
 constexpr size_t WS_DISABLED = 0x08000000;
+/// @brief Creates a window that is initially visible.
+/// @par Library
+/// xtd.forms.native
+/// @ingroup xtd_forms_native ws
+/// @warning Internal use only
 constexpr size_t WS_VISIBLE = 0x10000000;
+/// @brief Creates a window that is initially minimized. For use with the WS_OVERLAPPED style only.
+/// @par Library
+/// xtd.forms.native
+/// @ingroup xtd_forms_native ws
+/// @warning Internal use only
 constexpr size_t WS_MINIMIZE = 0x20000000;
+/// @brief Creates a child window. Cannot be used with the WS_POPUP style.
+/// @par Library
+/// xtd.forms.native
+/// @ingroup xtd_forms_native ws
+/// @warning Internal use only
 constexpr size_t WS_CHILD = 0x40000000;
+/// @brief Creates a pop-up window. Cannot be used with the WS_CHILD style.
+/// @par Library
+/// xtd.forms.native
+/// @ingroup xtd_forms_native ws
+/// @warning Internal use only
 constexpr size_t WS_POPUP = 0x80000000;
 
+/// @brief Specifies the first control of a group of controls in which the user can move from one control to the next with the arrow keys. All controls defined with the WS_GROUP style FALSE after the first control belong to the same group. The next control with the WS_GROUP style starts the next group (that is, one group ends where the next begins).
+/// @par Library
+/// xtd.forms.native
+/// @ingroup xtd_forms_native ws
+/// @warning Internal use only
 constexpr size_t WS_GROUP = 0x00020000;
+/// @brief Specifies one of any number of controls through which the user can move by using the TAB key. The TAB key moves the user to the next control specified by the WS_TABSTOP style.
+/// @par Library
+/// xtd.forms.native
+/// @ingroup xtd_forms_native ws
+/// @warning Internal use only
 constexpr size_t WS_TABSTOP = 0x00010000;
 
 // Common Window Styles
+
+/// @brief Creates an overlapped window with the WS_OVERLAPPED, WS_CAPTION, WS_SYSMENU, WS_THICKFRAME, WS_MINIMIZEBOX, and WS_MAXIMIZEBOX styles.
+/// @par Library
+/// xtd.forms.native
+/// @ingroup xtd_forms_native ws
+/// @warning Internal use only
 constexpr size_t WS_OVERLAPPEDWINDOW = WS_OVERLAPPED|WS_CAPTION|WS_SYSMENU|WS_THICKFRAME|WS_MINIMIZEBOX|WS_MAXIMIZEBOX;
+/// @brief Creates a pop-up window with the WS_BORDER, WS_POPUP, and WS_SYSMENU styles. The WS_CAPTION style must be combined with the WS_POPUPWINDOW style to make the Control menu visible.
+/// @par Library
+/// xtd.forms.native
+/// @ingroup xtd_forms_native ws
+/// @warning Internal use only
 constexpr size_t WS_POPUPWINDOW = WS_POPUP|WS_BORDER|WS_SYSMENU;
+/// @brief Same as the WS_CHILD style.
+/// @par Library
+/// xtd.forms.native
+/// @ingroup xtd_forms_native ws
+/// @warning Internal use only
 constexpr size_t WS_CHILDWINDOW = WS_CHILD;
 
 // Window Styles (next...)
+
+/// @brief Creates an overlapped window. An overlapped window has a title bar and a border. Same as the WS_OVERLAPPED style.
+/// @par Library
+/// xtd.forms.native
+/// @ingroup xtd_forms_native ws
+/// @warning Internal use only
 constexpr size_t WS_TILED = WS_OVERLAPPED;
+/// @brief Creates a window that is initially minimized. Same as the WS_MINIMIZE style.
+/// @par Library
+/// xtd.forms.native
+/// @ingroup xtd_forms_native ws
+/// @warning Internal use only
 constexpr size_t WS_ICONIC = WS_MINIMIZE;
+/// @brief Creates a window that has a sizing border. Same as the WS_THICKFRAME style.
+/// @par Library
+/// xtd.forms.native
+/// @ingroup xtd_forms_native ws
+/// @warning Internal use only
 constexpr size_t WS_SIZEBOX = WS_THICKFRAME;
+/// @brief Creates an overlapped window with the WS_OVERLAPPED, WS_CAPTION, WS_SYSMENU, WS_THICKFRAME, WS_MINIMIZEBOX, and WS_MAXIMIZEBOX styles. Same as the WS_OVERLAPPEDWINDOW style.
+/// @par Library
+/// xtd.forms.native
+/// @ingroup xtd_forms_native ws
+/// @warning Internal use only
 constexpr size_t WS_TILEDWINDOW = WS_OVERLAPPEDWINDOW;
-
-// Extended Window Styles
-constexpr size_t WS_EX_DLGMODALFRAME = 0x00000001;
-constexpr size_t WS_EX_NOPARENTNOTIFY = 0x00000004;
-constexpr size_t WS_EX_TOPMOST = 0x00000008;
-constexpr size_t WS_EX_ACCEPTFILES = 0x00000010;
-constexpr size_t WS_EX_TRANSPARENT = 0x00000020;
-constexpr size_t WS_EX_MDICHILD = 0x00000040;
-constexpr size_t WS_EX_TOOLWINDOW = 0x00000080;
-constexpr size_t WS_EX_WINDOWEDGE = 0x00000100;
-constexpr size_t WS_EX_CLIENTEDGE = 0x00000200;
-constexpr size_t WS_EX_CONTEXTHELP = 0x00000400;
-constexpr size_t WS_EX_RIGHT = 0x00001000;
-constexpr size_t WS_EX_LEFT = 0x00000000;
-constexpr size_t WS_EX_RTLREADING = 0x00002000;
-constexpr size_t WS_EX_LTRREADING = 0x00000000;
-constexpr size_t WS_EX_LEFTSCROLLBAR = 0x00004000;
-constexpr size_t WS_EX_RIGHTSCROLLBAR = 0x00000000;
-
-constexpr size_t WS_EX_CONTROLPARENT = 0x00010000;
-constexpr size_t WS_EX_STATICEDGE = 0x00020000;
-constexpr size_t WS_EX_APPWINDOW = 0x00040000;
-
-
-constexpr size_t WS_EX_OVERLAPPEDWINDOW = WS_EX_WINDOWEDGE|WS_EX_CLIENTEDGE;
-constexpr size_t WS_EX_PALETTEWINDOW = WS_EX_WINDOWEDGE|WS_EX_TOOLWINDOW|WS_EX_TOPMOST;
-
-constexpr size_t WS_EX_LAYERED = 0x00080000;
-
-constexpr size_t WS_EX_NOINHERITLAYOUT = 0x00100000; // Disable inheritence of mirroring by children
-
-constexpr size_t WS_EX_NOREDIRECTIONBITMAP = 0x00200000;
-
-constexpr size_t WS_EX_LAYOUTRTL = 0x00400000; // Right to left mirroring
-
-constexpr size_t WS_EX_COMPOSITED = 0x02000000;
-constexpr size_t WS_EX_NOACTIVATE = 0x08000000;
 #endif
-constexpr size_t WS_EX_AUTOSCROLL = 0x40000000;
-constexpr size_t WS_EX_MODALWINDOW = 0x80000000;
