21b95d4a228ebaf5e7ff0969a6b080b44b447fc4
Gammasoft <gammasoft71@gmail.com>
2021-04-13 20:38:01 +0200

    add docs

--- src/xtd.forms.native/include/xtd/forms/browser_info_flags.h
+++ src/xtd.forms.native/include/xtd/forms/browser_info_flags.h
@@ -1,5 +1,5 @@
 /// @file
-/// @brief Contains folder browser dialog flags constants.
+/// @brief Contains folder browser info flags constants.
 /// @copyright Copyright (c) 2021 Gammasoft. All rights reserved.
 #pragma once
 #include <cstddef>
