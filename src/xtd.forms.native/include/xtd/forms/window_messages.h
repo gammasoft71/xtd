e975f2db5f7fc6791e5b5f202dd7c3f018af042d
Gammasoft <gammasoft71@gmail.com>
2021-04-13 20:38:01 +0200

    add docs

--- src/xtd.forms.native/include/xtd/forms/window_messages.h
+++ src/xtd.forms.native/include/xtd/forms/window_messages.h
@@ -1,5 +1,5 @@
 /// @file
-/// @brief Contains windows message identifiers constants.
+/// @brief Contains windows messages constants.
 /// @copyright Copyright (c) 2021 Gammasoft. All rights reserved.
 #pragma once
 #include <cstddef>
