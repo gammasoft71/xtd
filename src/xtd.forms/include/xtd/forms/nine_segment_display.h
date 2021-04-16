272a224f7428fdc6e1e77b496d8cc5085d525b7f
Gammasoft <gammasoft71@gmail.com>
2021-04-16 22:42:04 +0200

    Update nine_segment_display.h

--- src/xtd.forms/include/xtd/forms/nine_segment_display.h
+++ src/xtd.forms/include/xtd/forms/nine_segment_display.h
@@ -25,33 +25,33 @@ namespace xtd {
     /// @image html nine_segment_display_g.png
     /// <br>
     /// @image html nine_segment_display_gd.png
-class nine_segment_display : public seven_segment_display {
+    class nine_segment_display : public seven_segment_display {
     public:
-  /// @brief Initialize a new instance of nine_segment_display class.
+      /// @brief Initialize a new instance of nine_segment_display class.
       nine_segment_display() = default;
-
+      
       using seven_segment_display::thickness;
-  /// @brief Sets thickness of segment.
-  /// @param value A int32_t that represent the segment tickness.
-  /// @return Current seven_segment_display.
+      /// @brief Sets thickness of segment.
+      /// @param value A int32_t that represent the segment tickness.
+      /// @return Current seven_segment_display.
       int32_t thickness() const override {return thickness_.value_or(size_.height() < 20 ? 1 : (size_.height() / 20 + ((size_.height() / 20) % 2 ? 0 : 1)));}
-
+      
     protected:
       void on_paint(paint_event_args& e) override {
         seven_segment_display::on_paint(e);
         if ((value_ & forms::segments::h) == forms::segments::h) draw_segment_h(e.graphics(), fore_color());
         if ((value_ & forms::segments::i) == forms::segments::i) draw_segment_i(e.graphics(), fore_color());
       }
- 
+      
       void draw_back_digit(drawing::graphics& graphics) override {
         seven_segment_display::draw_back_digit(graphics);
         draw_segment_h(graphics, drawing::color::average(back_color(), back_segment_color(), back_segment_transparency()));
         draw_segment_i(graphics, drawing::color::average(back_color(), back_segment_color(), back_segment_transparency()));
       }
-
-  /// @brief Draw segment h on specified graphics with specified color.
-  /// @param graphics A xtd::drawing::graphics from on_paint method.
-  /// @param color A xtd::drawing::color used to draw segemnt.
+      
+      /// @brief Draw segment h on specified graphics with specified color.
+      /// @param graphics A xtd::drawing::graphics from on_paint method.
+      /// @param color A xtd::drawing::color used to draw segemnt.
       virtual void draw_segment_h(drawing::graphics& graphics, const drawing::color& color) {
         for (int32_t offset = -thickness() / 2; offset < thickness() - thickness() / 2; offset++) {
           graphics.draw_line(drawing::pen(color), size_.width() - 3 - thickness() - abs(offset), 2 + thickness(), 2 + thickness(), size_.height() / 2 - 2 - thickness() / 2 - abs(offset));
@@ -59,9 +59,9 @@ class nine_segment_display : public seven_segment_display {
         }
       }
       
-  /// @brief Draw segment ion specified graphics with specified color.
-  /// @param graphics A xtd::drawing::graphics from on_paint method.
-  /// @param color A xtd::drawing::color used to draw segemnt.
+      /// @brief Draw segment ion specified graphics with specified color.
+      /// @param graphics A xtd::drawing::graphics from on_paint method.
+      /// @param color A xtd::drawing::color used to draw segemnt.
       virtual void draw_segment_i(drawing::graphics& graphics, const drawing::color& color) {
         for (int32_t offset = -thickness() / 2; offset < thickness() - thickness() / 2; offset++) {
           graphics.draw_line(drawing::pen(color), size_.width() - 3 - thickness() - abs(offset), size_.height() / 2 + 2 + thickness() / 2, 2 + thickness(), size_.height() - 3 - thickness() - abs(offset));
@@ -71,3 +71,4 @@ class nine_segment_display : public seven_segment_display {
     };
   }
 }
+
