253a41b09a4c8f7797c0a2bd3298a3d8a947e04b
Gammasoft <gammasoft71@gmail.com>
2021-04-20 14:23:48 +0200

    Update fill_rectangle.cpp

--- examples/xtd.forms.examples/drawing/fill_rectangle/src/fill_rectangle.cpp
+++ examples/xtd.forms.examples/drawing/fill_rectangle/src/fill_rectangle.cpp
@@ -13,32 +13,23 @@ namespace examples {
       client_size({340, 230});
     }
     
+  protected:
     void on_paint(paint_event_args& e) override {
-      auto solid_transparent_brush = solid_brush(color::transparent);
-      e.graphics().fill_rectangle(solid_transparent_brush, 10, 10, 100, 100);
-      
-      auto solid_red_brush = solid_brush(color::red);
-      e.graphics().fill_rectangle(solid_red_brush, 120, 10, 100, 100);
-
-      auto gradient_horizontal_brush = linear_gradient_brush(drawing::rectangle(230, 10, 100, 100), color::green, color::white, linear_gradient_mode::horizontal);
-      e.graphics().fill_rectangle(gradient_horizontal_brush, 230, 10, 100, 100);
-
-      auto hatch_diagonal_cross_brush = hatch_brush(hatch_style::diagonal_cross, color::blue, color::white);
-      e.graphics().fill_rectangle(hatch_diagonal_cross_brush, 10, 120, 100, 100);
-      
-      auto texture_circle_brush = texture_brush(create_circle_texture(color::yellow));
-      e.graphics().fill_rectangle(texture_circle_brush, 120, 120, 100, 100);
-
+      e.graphics().fill_rectangle(brushes::transparent(), 10, 10, 100, 100);
+      e.graphics().fill_rectangle(solid_brush(color::red), 120, 10, 100, 100);
+      e.graphics().fill_rectangle(linear_gradient_brush(rectangle(230, 10, 100, 100), color::green, color::white, linear_gradient_mode::horizontal), 230, 10, 100, 100);
+      e.graphics().fill_rectangle(hatch_brush(hatch_style::diagonal_brick, color::blue, color::white), 10, 120, 100, 100);
+      e.graphics().fill_rectangle(texture_brush(create_circle_texture(color::yellow)), 120, 120, 100, 100);
       form::on_paint(e);
     }
     
   private:
     image create_circle_texture(const color& color) {
-      auto bitmap = drawing::bitmap(16, 16);
-      auto graphics = drawing::graphics::from_image(bitmap);
-      graphics.fill_ellipse(solid_brush(color), 0, 0, bitmap.width() - 1, bitmap.height() - 1);
-      graphics.draw_ellipse(pens::black(), 0, 0, bitmap.width() - 1, bitmap.height() - 1);
-      return bitmap;
+      auto texture = bitmap(16, 16);
+      auto graphics = graphics::from_image(texture);
+      graphics.fill_ellipse(solid_brush(color), 0, 0, texture.width() - 1, texture.height() - 1);
+      graphics.draw_ellipse(pens::black(), 0, 0, texture.width() - 1, texture.height() - 1);
+      return texture;
     }
   };
 }
