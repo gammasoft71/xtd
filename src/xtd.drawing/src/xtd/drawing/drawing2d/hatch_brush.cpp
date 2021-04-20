5a0b98fa92bc0c45f18a0b8a7c163e6f24c80915
Gammasoft <gammasoft71@gmail.com>
2021-04-20 09:38:47 +0200

    add hatched brushes

--- src/xtd.drawing/src/xtd/drawing/drawing2d/hatch_brush.cpp
+++ src/xtd.drawing/src/xtd/drawing/drawing2d/hatch_brush.cpp
@@ -406,6 +406,76 @@ namespace {
         if (index % 2 == 0) graphics.draw_point(pen(fore_color, 1), x, y);
     return bitmap;
   }
+
+  image create_divot_texture(const color& fore_color, const color& back_color) {
+    static point_collection points {{2, 1}, {1, 2}, {2, 3}, {5, 5}, {6, 6}, {5, 7}};
+    auto bitmap = drawing::bitmap(8, 8);
+    auto graphics = drawing::graphics::from_image(bitmap);
+    graphics.clear(back_color);
+    for (auto point : points)
+      graphics.draw_point(pen(fore_color, 1), point);
+    return bitmap;
+  }
+
+  image create_dotted_grid_texture(const color& fore_color, const color& back_color) {
+    static point_collection points {{2, 0}, {2, 2}, {0, 4}, {2, 4}, {4, 4}, {6, 4}, {2, 6}};
+    auto bitmap = drawing::bitmap(8, 8);
+    auto graphics = drawing::graphics::from_image(bitmap);
+    graphics.clear(back_color);
+    for (auto point : points)
+      graphics.draw_point(pen(fore_color, 1), point);
+    return bitmap;
+  }
+
+  image create_dotted_diamond_texture(const color& fore_color, const color& back_color) {
+    static point_collection points {{6, 0}, {0, 2}, {4, 2}, {2, 4}, {0, 6}, {4, 6}};
+    auto bitmap = drawing::bitmap(8, 8);
+    auto graphics = drawing::graphics::from_image(bitmap);
+    graphics.clear(back_color);
+    for (auto point : points)
+      graphics.draw_point(pen(fore_color, 1), point);
+    return bitmap;
+  }
+
+  image create_shingle_texture(const color& fore_color, const color& back_color) {
+    static point_collection points {{6, 0}, {7, 0}, {0, 1}, {1, 2}, {1, 3}, {0, 4}, {1, 4}, {2, 5}, {7, 5}, {3, 6}, {6, 6}, {4, 7}, {5, 7}};
+    auto bitmap = drawing::bitmap(8, 8);
+    auto graphics = drawing::graphics::from_image(bitmap);
+    graphics.clear(back_color);
+    for (auto point : points)
+      graphics.draw_point(pen(fore_color, 1), point);
+    return bitmap;
+  }
+
+  image create_trellis_texture(const color& fore_color, const color& back_color) {
+    static point_collection points {{0, 0}, {1, 0}, {2, 0}, {3, 0}, {4, 0}, {5, 0}, {6, 0}, {7, 0}, {0, 1}, {3, 1}, {4, 1}, {7, 1}, {0, 2}, {1, 2}, {2, 2}, {3, 2}, {4, 2}, {5, 2}, {6, 2}, {7, 2}, {1, 3}, {2, 3}, {5, 3}, {6, 3}, {0, 4}, {1, 4}, {2, 4}, {3, 4}, {4, 4}, {5, 4}, {6, 4}, {7, 4}, {0, 5}, {3, 5}, {4, 5}, {7, 5}, {0, 6}, {1, 6}, {2, 6}, {3, 6}, {4, 6}, {5, 6}, {6, 6}, {7, 6}, {1, 7}, {2, 7}, {5, 7}, {6, 7}};
+    auto bitmap = drawing::bitmap(8, 8);
+    auto graphics = drawing::graphics::from_image(bitmap);
+    graphics.clear(back_color);
+    for (auto point : points)
+      graphics.draw_point(pen(fore_color, 1), point);
+    return bitmap;
+  }
+
+  image create_sphere_texture(const color& fore_color, const color& back_color) {
+    static point_collection points {{0, 0}, {1, 0}, {3, 0}, {4, 0}, {5, 0}, {7, 0}, {2, 1}, {5, 1}, {6, 1}, {2, 2}, {3, 2}, {4, 2}, {5, 2}, {6, 2}, {2, 3}, {3, 3}, {4, 3}, {5, 3}, {6, 3}, {0, 4}, {1, 4}, {3, 4}, {4, 4}, {5, 4}, {7, 4}, {1, 5}, {2, 5}, {6, 5}, {0, 6}, {1, 6}, {2, 6}, {6, 6}, {7, 6}, {0, 7}, {1, 7}, {2, 7}, {6, 7}, {7, 7}};
+    auto bitmap = drawing::bitmap(8, 8);
+    auto graphics = drawing::graphics::from_image(bitmap);
+    graphics.clear(back_color);
+    for (auto point : points)
+      graphics.draw_point(pen(fore_color, 1), point);
+    return bitmap;
+  }
+
+  image create_small_grid_texture(const color& fore_color, const color& back_color) {
+    static point_collection points {{0, 0}, {1, 0}, {2, 0}, {3, 0}, {4, 0}, {5, 0}, {6, 0}, {7, 0}, {2, 1}, {6, 1}, {2, 2}, {6, 2}, {2, 3}, {6, 3}, {0, 4}, {1, 4}, {2, 4}, {3, 4}, {4, 4}, {5, 4}, {6, 4}, {7, 4}, {2, 5}, {6, 5}, {2, 6}, {6, 6}, {2, 7}, {6, 7}};
+    auto bitmap = drawing::bitmap(8, 8);
+    auto graphics = drawing::graphics::from_image(bitmap);
+    graphics.clear(back_color);
+    for (auto point : points)
+      graphics.draw_point(pen(fore_color, 1), point);
+    return bitmap;
+  }
 }
 
 hatch_brush::hatch_brush() {
@@ -423,9 +493,13 @@ hatch_brush::hatch_brush(xtd::drawing::drawing2d::hatch_style hatch_style, const
 }
 
 void hatch_brush::recreate_handle() {
-  static std::map<xtd::drawing::drawing2d::hatch_style, delegate<image(const color&, const color&)>> textures {{hatch_style::horizontal, {create_horizontal_texture}}, {hatch_style::vertical, {create_vertical_texture}}, {hatch_style::forward_diagonal, {create_forward_diagonal_texture}}, {hatch_style::backward_diagonal, {create_backward_diagonal_texture}}, {hatch_style::cross, {create_cross_texture}}, {hatch_style::diagonal_cross, {create_diagonal_cross_texture}}, {hatch_style::percent_05, {create_percent_05_texture}}, {hatch_style::percent_10, {create_percent_10_texture}}, {hatch_style::percent_20, {create_percent_20_texture}}, {hatch_style::percent_25, {create_percent_25_texture}}, {hatch_style::percent_30, {create_percent_30_texture}}, {hatch_style::percent_40, {create_percent_40_texture}}, {hatch_style::percent_50, {create_percent_50_texture}}, {hatch_style::percent_60, {create_percent_60_texture}}, {hatch_style::percent_70, {create_percent_70_texture}}, {hatch_style::percent_75, {create_percent_75_texture}}, {hatch_style::percent_80, {create_percent_80_texture}}, {hatch_style::percent_90, {create_percent_90_texture}}, {hatch_style::light_downward_diagonal, {create_light_downward_diagonal_texture}}, {hatch_style::light_upward_diagonal, {create_light_upward_diagonal_texture}}, {hatch_style::dark_downward_diagonal, {create_dark_downward_diagonal_texture}}, {hatch_style::dark_upward_diagonal, {create_dark_upward_diagonal_texture}}, {hatch_style::wide_downward_diagonal, {create_wide_downward_diagonal_texture}}, {hatch_style::wide_upward_diagonal, {create_wide_upward_diagonal_texture}}, {hatch_style::light_horizontal, {create_light_horizontal_texture}}, {hatch_style::light_vertical, {create_light_vertical_texture}}, {hatch_style::narrow_horizontal, {create_narrow_horizontal_texture}}, {hatch_style::narrow_vertical, {create_narrow_vertical_texture}}, {hatch_style::dark_horizontal, {create_dark_horizontal_texture}}, {hatch_style::dark_vertical, {create_dark_vertical_texture}}, {hatch_style::dashed_downward_diagonal, {create_dashed_downward_diagonal_texture}}, {hatch_style::dashed_upward_diagonal, {create_dashed_upward_diagonal_texture}}, {hatch_style::dashed_horizontal, {create_dashed_horizontal_texture}}, {hatch_style::dashed_vertical, {create_dashed_vertical_texture}}, {hatch_style::small_confetti, {create_small_confetti_texture}}, {hatch_style::large_confetti, {create_large_confetti_texture}}, {hatch_style::zig_zag, {create_zig_zag_texture}}, {hatch_style::wave, {create_wave_texture}}, {hatch_style::diagonal_brick, {create_diagonal_brick_texture}}, {hatch_style::horizontal_brick, {create_horizontal_brick_texture}},
-    {hatch_style::weave, {create_weave_texture}},
-    {hatch_style::plaid, {create_plaid_texture}},
+  static std::map<xtd::drawing::drawing2d::hatch_style, delegate<image(const color&, const color&)>> textures {{hatch_style::horizontal, {create_horizontal_texture}}, {hatch_style::vertical, {create_vertical_texture}}, {hatch_style::forward_diagonal, {create_forward_diagonal_texture}}, {hatch_style::backward_diagonal, {create_backward_diagonal_texture}}, {hatch_style::cross, {create_cross_texture}}, {hatch_style::diagonal_cross, {create_diagonal_cross_texture}}, {hatch_style::percent_05, {create_percent_05_texture}}, {hatch_style::percent_10, {create_percent_10_texture}}, {hatch_style::percent_20, {create_percent_20_texture}}, {hatch_style::percent_25, {create_percent_25_texture}}, {hatch_style::percent_30, {create_percent_30_texture}}, {hatch_style::percent_40, {create_percent_40_texture}}, {hatch_style::percent_50, {create_percent_50_texture}}, {hatch_style::percent_60, {create_percent_60_texture}}, {hatch_style::percent_70, {create_percent_70_texture}}, {hatch_style::percent_75, {create_percent_75_texture}}, {hatch_style::percent_80, {create_percent_80_texture}}, {hatch_style::percent_90, {create_percent_90_texture}}, {hatch_style::light_downward_diagonal, {create_light_downward_diagonal_texture}}, {hatch_style::light_upward_diagonal, {create_light_upward_diagonal_texture}}, {hatch_style::dark_downward_diagonal, {create_dark_downward_diagonal_texture}}, {hatch_style::dark_upward_diagonal, {create_dark_upward_diagonal_texture}}, {hatch_style::wide_downward_diagonal, {create_wide_downward_diagonal_texture}}, {hatch_style::wide_upward_diagonal, {create_wide_upward_diagonal_texture}}, {hatch_style::light_horizontal, {create_light_horizontal_texture}}, {hatch_style::light_vertical, {create_light_vertical_texture}}, {hatch_style::narrow_horizontal, {create_narrow_horizontal_texture}}, {hatch_style::narrow_vertical, {create_narrow_vertical_texture}}, {hatch_style::dark_horizontal, {create_dark_horizontal_texture}}, {hatch_style::dark_vertical, {create_dark_vertical_texture}}, {hatch_style::dashed_downward_diagonal, {create_dashed_downward_diagonal_texture}}, {hatch_style::dashed_upward_diagonal, {create_dashed_upward_diagonal_texture}}, {hatch_style::dashed_horizontal, {create_dashed_horizontal_texture}}, {hatch_style::dashed_vertical, {create_dashed_vertical_texture}}, {hatch_style::small_confetti, {create_small_confetti_texture}}, {hatch_style::large_confetti, {create_large_confetti_texture}}, {hatch_style::zig_zag, {create_zig_zag_texture}}, {hatch_style::wave, {create_wave_texture}}, {hatch_style::diagonal_brick, {create_diagonal_brick_texture}}, {hatch_style::horizontal_brick, {create_horizontal_brick_texture}}, {hatch_style::weave, {create_weave_texture}}, {hatch_style::plaid, {create_plaid_texture}}, {hatch_style::divot, {create_divot_texture}},
+    {hatch_style::dotted_grid, {create_dotted_grid_texture}},
+    {hatch_style::dotted_diamond, {create_dotted_diamond_texture}},
+    {hatch_style::shingle, {create_shingle_texture}},
+    {hatch_style::trellis, {create_trellis_texture}},
+    {hatch_style::sphere, {create_sphere_texture}},
+    {hatch_style::small_grid, {create_small_grid_texture}},
   };
   auto it = textures.find(data_->hatch_style_);
   native::brush::texture(brush::data_->handle_, (it != textures.end() ? it->second(data_->fore_color_, data_->back_color_) : create_empty_texture(data_->fore_color_, data_->back_color_)).handle());
