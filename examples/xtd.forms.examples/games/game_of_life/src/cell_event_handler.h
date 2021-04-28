1efb82f79b2ea8e0b8236da87cae712a67bb7978
Gammasoft <gammasoft71@gmail.com>
2021-04-28 16:41:09 +0200

    update game of life

--- examples/xtd.forms.examples/games/game_of_life/src/cell_event_handler.h
+++
@@ -1,9 +0,0 @@
-
-#pragma once
-#include <xtd/delegate.h>
-#include "cell_event_args.h"
-
-namespace game_of_life {
-  template<typename type_t>
-  using cell_event_handler = xtd::delegate<void(type_t& sender, const cell_event_args& e)>;
-}
