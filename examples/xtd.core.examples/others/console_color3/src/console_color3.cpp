cc30ff18a6309f1f3cc50e0f0217dce4a885f025
Gammasoft <gammasoft71@gmail.com>
2021-05-07 23:07:26 +0200

    Update console_color3.cpp

--- examples/xtd.core.examples/others/console_color3/src/console_color3.cpp
+++ examples/xtd.core.examples/others/console_color3/src/console_color3.cpp
@@ -43,7 +43,7 @@ int main() {
   console::output_code_page(65001);
   
   for (size_t index = 0; index < logo.size(); ++index)
-  console::out << (index == 0 || index == logo.size() - 1 ? "       " : "     ") << background_color(console_color::white) << foreground_color(console_color::dark_blue) << logo[index] << reset_color() << endl;
+    console::out << (index == 0 || index == logo.size() - 1 ? "       " : "     ") << background_color(console_color::white) << foreground_color(console_color::dark_blue) << logo[index] << reset_color() << endl;
   
   console::out << foreground_color(console_color::dark_blue) << u8"                                    Gammasoft                                   " << endl;
   console::out << foreground_color(console_color::dark_gray) << u8" More than thirty years of passion for high technology especially in development" << endl;
