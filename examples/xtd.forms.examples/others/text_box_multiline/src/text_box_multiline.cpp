5ddf93a30fce53b88f3b6ce619663b31b962723e
Gammasoft <gammasoft71@gmail.com>
2021-05-18 13:01:24 +0200

    Update text_box_multiline.cpp

--- examples/xtd.forms.examples/others/text_box_multiline/src/text_box_multiline.cpp
+++ examples/xtd.forms.examples/others/text_box_multiline/src/text_box_multiline.cpp
@@ -7,31 +7,16 @@ namespace examples {
   public:
     form1() {
       text("Text box multiline example");
-      client_size({300, 300});
-      *this << text_box1 << text_box2;
+      client_size({640, 480});
+      controls().push_back(text_box);
 
-      text_box1.location({10, 10});
-      text_box1.multiline(true);
-      text_box1.size({280, 135});
-      text_box1.text_changed += [&] {
-        text_box2.text(text_box1.text());
-      };
-      text_box1.text("Line 1\nLine 2\nLine 3\n...");
-
-      text_box2.location({10, 155});
-      text_box2.word_wrap(false);
-      text_box2.multiline(true);
-      text_box2.size({280, 135});
-      text_box2.text_changed += [&] {
-        text_box1.text(text_box2.text());
-      };
-      
-      active_control(text_box2);
+      text_box.dock(dock_style::fill);
+      text_box.multiline(true);
+      text_box.text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.\n\nUt velit mauris, egestas sed, gravida nec, ornare ut, mi. Aenean ut orci vel massa suscipit pulvinar. Nulla sollicitudin. Fusce varius, ligula non tempus aliquam, nunc turpis ullamcorper nibh, in tempus sapien eros vitae ligula. Pellentesque rhoncus nunc et augue. Integer id felis. Curabitur aliquet pellentesque diam. Integer quis metus vitae elit lobortis egestas. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi vel erat non mauris convallis vehicula. Nulla et sapien. Integer tortor tellus, aliquam faucibus, convallis id, congue eu, quam. Mauris ullamcorper felis vitae erat. Proin feugiat, augue non elementum posuere, metus purus iaculis lectus, et tristique ligula justo vitae magna.\n\nAliquam convallis sollicitudin purus. Praesent aliquam, enim at fermentum mollis, ligula massa adipiscing nisl, ac euismod nibh nisl eu lectus. Fusce vulputate sem at sapien. Vivamus leo. Aliquam euismod libero eu enim. Nulla nec felis sed leo placerat imperdiet. Aenean suscipit nulla in justo. Suspendisse cursus rutrum augue. Nulla tincidunt tincidunt mi. Curabitur iaculis, lorem vel rhoncus faucibus, felis magna fermentum augue, et ultricies lacus lorem varius purus. Curabitur eu amet.\n");
     }
     
   private:
-    text_box text_box1;
-    text_box text_box2;
+    text_box text_box;
   };
 }
 
