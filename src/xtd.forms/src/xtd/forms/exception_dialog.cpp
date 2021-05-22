fd3aebdb925034cb91362a10116ecb51038ba1ca
Gammasoft71 <gammasoft71@gmail.com>
2021-05-22 15:00:38 +0200

    Update exception_dialog.cpp

--- src/xtd.forms/src/xtd/forms/exception_dialog.cpp
+++ src/xtd.forms/src/xtd/forms/exception_dialog.cpp
@@ -38,11 +38,11 @@ namespace {
       };
 
       panel_top_.location({0, 0});
-      panel_top_.size({600, 150});
+      panel_top_.size({550, 150});
       panel_top_.controls().push_back_range({picture_box_error_, label_exception_, button_details_, button_continue_, button_quit_});
 
       panel_bottom_.location({0, 150});
-      panel_bottom_.size({600, 200});
+      panel_bottom_.size({550, 200});
       panel_bottom_.hide();
       panel_bottom_.controls().push_back(text_box_details_);
 
@@ -51,10 +51,10 @@ namespace {
       picture_box_error_.image(xtd::drawing::system_images::from_name("dialog-error", xtd::drawing::size(64, 64)));
       
       label_exception_.location({85, 10});
-      label_exception_.size({505, 95});
+      label_exception_.size({455, 95});
       label_exception_.text_align(content_alignment::top_left);
-      auto text_message = strings::format("Unhandled exception occured in your application. If you click\nContinue, the application will ignore this error and attempt to continue.\nIf you click Quit, the application will close immediately.\n\n{}"_t, exception_ ? exception_->what() : "(Unknown exception)"_t);
-      label_exception_.text(text_message);
+      auto text_m = strings::format("Unhandled exception occured in your application. If you click\nContinue, the application will ignore this error and attempt to\ncontinue. If you click Quit, the application will close immediately.\n\n{}"_t, exception_ ? exception_->what() : "(Unknown exception)"_t);
+      label_exception_.text(text_m);
 
       button_details_.location({10, 115});
       button_details_.width(100);
@@ -64,18 +64,18 @@ namespace {
       };
 
       //button_continue_.enabled(false);
-      button_continue_.location({environment::os_version().is_macos_platform() ? 490 : 380, 115});
+      button_continue_.location({environment::os_version().is_macos_platform() ? 440 : 330, 115});
       button_continue_.width(100);
       button_continue_.text("Continue"_t);
       button_continue_.dialog_result(forms::dialog_result::ok);
       
-      button_quit_.location({environment::os_version().is_macos_platform() ? 380 : 490, 115});
+      button_quit_.location({environment::os_version().is_macos_platform() ? 330 : 440, 115});
       button_quit_.width(100);
       button_quit_.text("Quit"_t);
       button_quit_.dialog_result(forms::dialog_result::cancel);
       
       text_box_details_.location({10, 5});
-      text_box_details_.size({580, 185});
+      text_box_details_.size({530, 185});
       text_box_details_.multiline(true);
       text_box_details_.read_only(true);
       text_box_details_.word_wrap(false);
