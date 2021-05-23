4849263d2a64a598c0c5f5f3525dea4be944a3ae
Gammasoft <gammasoft71@gmail.com>
2021-05-23 10:56:44 +0200

    Update control.cpp

--- examples/xtd.forms.examples/controls/control/src/control.cpp
+++ examples/xtd.forms.examples/controls/control/src/control.cpp
@@ -12,11 +12,11 @@ namespace examples {
 
       control1.parent(*this);
       control1.cursor(cursors::hand());
-      control1.back_color(color::spring_green);
+      control1.back_color(colors::spring_green());
       control1.location({50, 50});
       control1.size({100, 50});
       control1.click += [&] {
-        control1.back_color(control1.back_color() == color::spring_green ? color::orange_red : color::spring_green);
+        control1.back_color(control1.back_color() == colors::spring_green() ? colors::orange_red() : colors::spring_green());
       };
     }
     
