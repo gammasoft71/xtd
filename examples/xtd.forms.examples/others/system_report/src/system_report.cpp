5d422db600247abe5a74851aecc0659d44f396b3
Gammasoft <gammasoft71@gmail.com>
2021-05-23 12:58:32 +0200

    Update system_report.cpp

--- examples/xtd.forms.examples/others/system_report/src/system_report.cpp
+++ examples/xtd.forms.examples/others/system_report/src/system_report.cpp
@@ -9,16 +9,37 @@ public:
     text("System reports example");
     start_position(form_start_position::center_screen);
     size({500, 700});
-    reports.dock(dock_style::fill);
-    reports.font({drawing::font_family::generic_monospace(), reports.font().size()});
-    reports.multiline(true);
-    reports.parent(*this);
-    reports.read_only(true);
-    reports.text(system_report::to_string());
+    
+    report_tab_control.dock(dock_style::fill);
+    report_tab_control.parent(*this);
+    report_tab_control.tab_pages().push_back_range({text_report_tab_page, xml_report_tab_page});
+    
+    text_report_tab_page.text("text");
+    xml_report_tab_page.text("xml");
+
+    text_reports_text_box.dock(dock_style::fill);
+    text_reports_text_box.font({drawing::font_family::generic_monospace(), text_reports_text_box.font().size()});
+    text_reports_text_box.multiline(true);
+    text_reports_text_box.parent(text_report_tab_page);
+    text_reports_text_box.read_only(true);
+    text_reports_text_box.text(system_report::to_string());
+    text_reports_text_box.word_wrap(false);
+
+    xml_reports_text_box.dock(dock_style::fill);
+    xml_reports_text_box.font({drawing::font_family::generic_monospace(), xml_reports_text_box.font().size()});
+    xml_reports_text_box.multiline(true);
+    xml_reports_text_box.parent(xml_report_tab_page);
+    xml_reports_text_box.read_only(true);
+    xml_reports_text_box.text(system_report::to_xml());
+    xml_reports_text_box.word_wrap(false);
   }
   
 private:
-  text_box reports;
+  tab_control report_tab_control;
+  tab_page text_report_tab_page;
+  tab_page xml_report_tab_page;
+  text_box text_reports_text_box;
+  text_box xml_reports_text_box;
 };
 
 auto main()->int {
