3965a846b8a5ff577b909db3ef1122a9b4908fd4
Gammasoft <gammasoft71@gmail.com>
2021-05-26 01:24:55 +0200

    Update environment_tick_count.cpp

--- examples/xtd.core.examples/environment/environment_tick_count/src/environment_tick_count.cpp
+++ examples/xtd.core.examples/environment/environment_tick_count/src/environment_tick_count.cpp
@@ -3,8 +3,8 @@
 using namespace xtd;
 
 int main() {
-  auto result = environment::tick_count();
-  console::write_line("Time since boot : {0:d} days {0:h}:{0:M}", result);
+  auto time_since_boot = environment::tick_count();
+  console::write_line("Time since boot : {0:d} days {0:h}:{0:M}", time_since_boot);
 }
 
 // This code can be produce the following output :
