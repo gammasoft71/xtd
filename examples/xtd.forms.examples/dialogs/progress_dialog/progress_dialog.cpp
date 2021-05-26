d94a8535c59e96aa53bf329130eacd30d2baa0b6
Gammasoft <gammasoft71@gmail.com>
2021-05-26 21:49:05 +0200

    Update progress_dialog.cpp

--- examples/xtd.forms.examples/dialogs/progress_dialog/progress_dialog.cpp
+++ examples/xtd.forms.examples/dialogs/progress_dialog/progress_dialog.cpp
@@ -19,7 +19,7 @@ public:
       dialog.text("Process running");
       dialog.show_skip_button(true);
       dialog.show_remaining_time(true);
-      dialog.show_dialog(*this);
+      dialog.show_sheet_dialog(*this);
       for (auto step = 1; step <= dialog.maximum(); ++step) {
         dialog.value(step);
         dialog.message(strings::format("Step {}/{} ==> {}", dialog.value(), dialog.maximum(), dialog.skipped() ? "skipped" : "done"));
@@ -38,7 +38,7 @@ public:
       dialog.marquee(true);
       dialog.show_cancel_button(true);
       dialog.show_elapsed_time(true);
-      dialog.show_dialog(*this);
+      dialog.show_sheet_dialog(*this);
       while (!dialog.cancelled()) {
         application::do_events();
         std::this_thread::sleep_for(100ms);
