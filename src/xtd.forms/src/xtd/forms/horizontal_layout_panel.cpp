d1982abaae7a638bd655dc9f9c63a10adfedb8a0
Gammasoft <gammasoft71@gmail.com>
2021-05-28 15:09:30 +0200

    add layout

--- src/xtd.forms/src/xtd/forms/horizontal_layout_panel.cpp
+++ src/xtd.forms/src/xtd/forms/horizontal_layout_panel.cpp
@@ -31,13 +31,32 @@ void horizontal_layout_panel::on_control_removed(const xtd::forms::control_event
 
 void horizontal_layout_panel::on_layout(const event_args& e) {
   panel::on_layout(e);
-  if (controls().size() == 0) return;
+  if (!parent() && controls().size() == 0) return;
+  
+  size_t auto_size_control_count = count_if(control_layout_styles_.begin(), control_layout_styles_.end(), [](auto layout_style)->bool {return layout_style.second.size_type() == size_type::auto_size;});
+  int32_t auto_size_width = client_size().width() - auto_size_control_count * padding().left() - auto_size_control_count * padding().right();
+  
+  int32_t absolute_width = 0;
+  for (auto& [control, layout_style] : control_layout_styles_)
+    if (layout_style.size_type() == size_type::absolute) absolute_width += layout_style.width();
+  auto_size_width -= absolute_width;
+  
+  float total_percent = 0;
+  for (auto& [control, layout_style] : control_layout_styles_)
+    if (layout_style.size_type() == size_type::percent) total_percent += layout_style.width();
+  int32_t percent_width = static_cast<int32_t>(auto_size_width * total_percent);
+  auto_size_width -= percent_width;
+  
   int32_t left = padding().left();
   int32_t top = padding().top();
-  int32_t width = client_size().width() / static_cast<int32_t>(controls().size()) - padding().left() - padding().right();;
+  int32_t width = 0;
   int32_t height = client_size().height() - padding().top() - padding().bottom();
-  for (size_t index = 0; index < controls().size(); ++index) {
-    controls()[index].get().set_bounds(left, top, width, height);
+  for (auto& [control, layout_style] : control_layout_styles_) {
+    if (layout_style.size_type() == size_type::absolute) width = layout_style.width();
+    else if (layout_style.size_type() == size_type::percent) width = static_cast<int32_t>(percent_width * (layout_style.width() / total_percent));
+    else if (layout_style.size_type() == size_type::auto_size) width = auto_size_width / auto_size_control_count;
+    else throw argument_exception(caller_info_);
+    control.get().set_bounds(left, top, width, height);
     left += width + padding().right() + padding().left();
   }
 }
