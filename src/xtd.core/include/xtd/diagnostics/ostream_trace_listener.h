495ab9a18c7c12d4339d406c801a2838ba39f9be
Gammasoft <gammasoft71@gmail.com>
2021-05-31 16:29:25 +0200

    add docs

--- src/xtd.core/include/xtd/diagnostics/ostream_trace_listener.h
+++ src/xtd.core/include/xtd/diagnostics/ostream_trace_listener.h
@@ -9,17 +9,30 @@
 namespace xtd {
   /// @brief The xtd::diagnostics namespace provides classes that allow you to interact with system processes, event logs, and performance counters.
   namespace diagnostics {
+    /// @brief Directs tracing or debugging output to a std::ostream, such as std::fstream, std::stream...
+    /// @par Library
+    /// xtd.core
+    /// @ingroup xtd_core diagnostics debug
     class ostream_trace_listener : public trace_listener {
     public:
+      /// @brief Initializes a new instance of the ostream_trace_listener class with a specified ostream.
+      /// @param ostream An std::ostream that represents the stream the ostream_trace_listener writes to.
+      /// @remarks This constructor initializes the name property to an empty string ("").
       ostream_trace_listener(const std::ostream& ostream) {
         this->ostream(ostream);
       };
+      /// @cond
       ~ostream_trace_listener() {flush();}
+      /// @endcond
       
+      /// @brief Gets the underlying stream.
+      /// @return A std::ostream that represents the stream the ostream_trace_listener writes to.
       virtual const std::ostream& ostream() const {return ostream_;}
+      /// @brief Sets the underlying stream.
+      /// @param ostream A std::ostream that represents the stream the ostream_trace_listener writes to.
       virtual void ostream(const std::ostream& ostream) {ostream_.rdbuf(ostream.rdbuf());}
 
-      void close() override { }
+      void close() override {}
       void flush() override {
 #if !defined(NDEBUG) || defined(DEBUG) || defined(TRACE)
         if (ostream_.good())
