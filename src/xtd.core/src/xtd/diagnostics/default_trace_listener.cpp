7a6000b7f2a17d086e9f1acc0dd75d6fecab4a16
Gammasoft <gammasoft71@gmail.com>
2021-05-31 19:25:18 +0200

    revue default_trace_listener

--- src/xtd.core/src/xtd/diagnostics/default_trace_listener.cpp
+++ src/xtd.core/src/xtd/diagnostics/default_trace_listener.cpp
@@ -2,10 +2,42 @@
 #define __XTD_CORE_NATIVE_LIBRARY__
 #include <xtd/native/debug.h>
 #undef __XTD_CORE_NATIVE_LIBRARY__
+#include <xtd/io/file.h>
 
 using namespace std;
 using namespace xtd::diagnostics;
 
-void default_trace_listener::write_to_output_debug(const string& message) {
-  native::debug::write_to_output(message);
+default_trace_listener::default_trace_listener() : trace_listener("default") {
+}
+
+default_trace_listener::~default_trace_listener() {
+  flush();
+}
+
+std::string default_trace_listener::log_file_name() const {
+  return log_file_name_;
+}
+
+void default_trace_listener::log_file_name(const std::string log_file_name) {
+  log_file_name_ = log_file_name;
+}
+
+void default_trace_listener::close() {
+}
+
+void default_trace_listener::__flush() {
+  if (!message_line_.empty()) write_line("");
+}
+
+void default_trace_listener::__write(const std::string& message) {
+  if (need_indent()) write_indent();
+  message_line_ += message;
+  if (!log_file_name_.empty()) xtd::io::file::append_all_text(log_file_name_, message);
+}
+
+void default_trace_listener::__write_line(const std::string& message) {
+  write(message + "\n");
+  native::debug::write_to_output(message_line_);
+  message_line_ = "";
+  need_indent(true);
 }
