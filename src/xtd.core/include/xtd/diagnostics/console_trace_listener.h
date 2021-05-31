116e1ae270cc4855a83ceeba97b9131dcb343e12
Gammasoft <gammasoft71@gmail.com>
2021-05-31 19:47:15 +0200

    revue console_trace_listener

--- src/xtd.core/include/xtd/diagnostics/console_trace_listener.h
+++ src/xtd.core/include/xtd/diagnostics/console_trace_listener.h
@@ -20,11 +20,11 @@ namespace xtd {
     public:
       /// @brief Initializes a new instance of the console_trace_listener class with trace output written to the standard output stream.
       /// @remarks This constructor initializes a console_trace_listener object to write messages to either the std::cout or the std::cerr stream. Its Name property is initialized to an empty string ("").
-      console_trace_listener() : console_trace_listener(false) {}
+      console_trace_listener();
       /// @brief Initializes a new instance of the console_trace_listener class with an option to write trace output to the standard output stream or the standard error stream.
       /// @param use_error_stream true to write tracing and debugging output to the standard error stream; false to write tracing and debugging output to the standard output stream.
       /// @remarks This constructor initializes a console_trace_listener object to write messages to either the std::cut or the std::cerr stream. Its Name property is initialized to an empty string ("").
-      console_trace_listener(bool use_error_stream) : ostream_trace_listener(use_error_stream ? std::cerr : std::cout) {}
+      console_trace_listener(bool use_error_stream);
       
     private:
       using ostream_trace_listener::ostream;
