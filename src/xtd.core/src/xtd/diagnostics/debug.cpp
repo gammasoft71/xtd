2ac2bd48d94c045439768bc3e0d0380a10ffd1e3
Gammasoft <gammasoft71@gmail.com>
2021-05-31 12:54:06 +0200

    revue debug

--- src/xtd.core/src/xtd/diagnostics/debug.cpp
+++ src/xtd.core/src/xtd/diagnostics/debug.cpp
@@ -11,10 +11,66 @@ using namespace xtd::diagnostics;
 extern trace_listener_collection __listeners__;
 extern char** __diagnostics_argv;
 
-bool debug::auto_flush_ = false;
-unsigned int debug::indent_level_ = 0;
-unsigned int debug::indent_size_ = 4;
 trace_listener_collection& debug::listeners_ = __listeners__;
-bool debug::use_global_lock_ = true;
 mutex debug::global_lock_;
 string debug::source_name_ = environment::get_command_line_args().size() == 0 ? "(unknown)" : environment::get_command_line_args()[0];
+
+bool debug::auto_flush() {
+  return auto_flush_;
+}
+
+void debug::auto_flush(bool auto_flush) {
+  auto_flush_ = auto_flush;
+}
+
+unsigned int debug::indent_level() {
+  return indent_level_;
+}
+
+void debug::indent_level(unsigned int indent_level) {
+  indent_level_ = indent_level;
+}
+
+unsigned int debug::indent_size() {
+  return indent_size_;
+}
+
+void debug::indent_size(unsigned int indent_size) {
+  indent_size_ = indent_size;
+}
+
+trace_listener_collection& debug::listeners() {
+  return listeners_;
+}
+
+void debug::listeners(const trace_listener_collection& listeners) {
+  listeners_ = listeners;
+}
+
+bool debug::use_global_lock() {
+  return use_global_lock_;
+}
+
+void debug::use_global_lock(bool use_global_lock) {
+  use_global_lock_ = use_global_lock;
+}
+
+void debug::indent() {
+  indent_level(indent_level() + 1);
+}
+
+void debug::trace_error(const std::string& message) {
+  trace_event(trace_event_type::error, message);
+}
+
+void debug::trace_information(const std::string& message) {
+  trace_event(trace_event_type::information, message);
+}
+
+void debug::trace_warning(const std::string& message) {
+  trace_event(trace_event_type::warning, message);
+}
+
+void debug::unindent() {
+  if (indent_level() != 0) indent_level(indent_level() - 1);
+}
