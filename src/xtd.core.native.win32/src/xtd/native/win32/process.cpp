55a1f738dbaf9042a1edf21f5155d58ab05282b9
Gammasoft <gammasoft71@gmail.com>
2021-06-03 18:48:59 +0200

    fix use shell execute on win32

--- src/xtd.core.native.win32/src/xtd/native/win32/process.cpp
+++ src/xtd.core.native.win32/src/xtd/native/win32/process.cpp
@@ -1,6 +1,7 @@
 #define __XTD_CORE_NATIVE_LIBRARY__
 #include <xtd/native/process.h>
 #include <xtd/native/process_creation_flags.h>
+#include "../../../../include/xtd/native/win32/strings.h"
 #undef __XTD_CORE_NATIVE_LIBRARY__
 #include <cstdlib>
 #include <Windows.h>
@@ -14,10 +15,13 @@ namespace {
 }
 
 intptr_t process::create(const std::string& command_line, int32_t process_creation_flags) {
+  auto line_args = native::win32::strings::split(command_line, {' '});
+  auto is_executable = line_args.size() && (native::win32::strings::ends_with(line_args[0], ".exe") || native::win32::strings::ends_with(line_args[0], ".com") || native::win32::strings::ends_with(line_args[0], ".bat") || native::win32::strings::ends_with(line_args[0], ".cmd"));
+
   STARTUPINFO startup_info {};
   startup_info.cb = sizeof(STARTUPINFO);
   PROCESS_INFORMATION process_information;
-  CreateProcessA(nullptr, const_cast<LPSTR>(((process_creation_flags & USE_SHELL_EXECUTE_PROCESS) == USE_SHELL_EXECUTE_PROCESS ? shell_execute() + command_line : command_line).c_str()), nullptr, nullptr, true, process_creation_flags, nullptr, nullptr, &startup_info, &process_information);
+  CreateProcessA(nullptr, const_cast<LPSTR>(((process_creation_flags & USE_SHELL_EXECUTE_PROCESS) == USE_SHELL_EXECUTE_PROCESS && !is_executable ? shell_execute() + command_line : command_line).c_str()), nullptr, nullptr, true, process_creation_flags, nullptr, nullptr, &startup_info, &process_information);
   return reinterpret_cast<intptr_t>(process_information.hProcess);
 }
 
