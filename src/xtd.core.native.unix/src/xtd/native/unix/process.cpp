16949cbd2535326efdd1af75ca87509a6dc5bc36
Gammasoft <gammasoft71@gmail.com>
2021-06-06 01:05:59 +0200

    add redirect stream

--- src/xtd.core.native.unix/src/xtd/native/unix/process.cpp
+++ src/xtd.core.native.unix/src/xtd/native/unix/process.cpp
@@ -15,6 +15,34 @@ using namespace std::filesystem;
 using namespace xtd::native;
 
 namespace {
+  class file_descriptor_streambuf : public std::streambuf {
+  public:
+    file_descriptor_streambuf(int fd) : fd_(fd) {}
+    ~file_descriptor_streambuf() {close(fd_);}
+    
+  protected:
+    int underflow() override {
+      if (read(fd_, &value_, 1) == 1) {
+        this->setg(&value_, &value_, &value_+1);
+        return value_;
+      }
+      return std::streambuf::underflow(); // EOF
+    }
+    
+    int overflow(int c) override {
+      value_ = static_cast<char>(c);
+      if (write(fd_, &value_, 1) != -1) {
+        this->setp(&value_, &value_);
+        return 0;
+      }
+      return std::streambuf::overflow(c); // EOF
+    }
+    
+    int fd_;
+    char value_ = EOF;
+    std::string buffer_;
+  };
+  
   std::string shell_execute() {
 #if defined(__APPLE__)
     return "open";
@@ -75,9 +103,36 @@ namespace {
 }
 
 intptr_t process::create(const string& file_name, const string& arguments, int32_t process_creation_flags, const std::string& working_directory) {
+  // Set following three lines in parameters...
+  bool redirect_standard_input = false;
+  bool redirect_standard_output = true;
+  bool redirect_standard_error = false;
+  
   auto command_line = file_name + (arguments == "" ? "" : (" " + arguments));
+  int pipe_stdin[2];
+  pipe(pipe_stdin);
+  int pipe_stdout[2];
+  pipe(pipe_stdout);
+  int pipe_stderr[2];
+  pipe(pipe_stderr);
+  
   pid_t process = fork();
   if (process == 0) {
+    if (redirect_standard_input) {
+      close(pipe_stdin[1]);
+      dup2(pipe_stdin[0], 0);
+    }
+    
+    if (redirect_standard_output) {
+      close(pipe_stdout[0]);
+      dup2(pipe_stdout[1], 1);
+    }
+    
+    if (redirect_standard_error) {
+      close(pipe_stderr[0]);
+      dup2(pipe_stderr[1], 2);
+    }
+    
     vector<string> command_line_args;
     if ((process_creation_flags & USE_SHELL_EXECUTE_PROCESS) == USE_SHELL_EXECUTE_PROCESS && is_valid_shell_execute_process(command_line, working_directory)) {
       command_line_args.push_back(shell_execute());
@@ -93,6 +148,17 @@ intptr_t process::create(const string& file_name, const string& arguments, int32
     execvp_args[execvp_args.size()-1] = nullptr;
     execvp(execvp_args[0], execvp_args.data());
     exit(errno);
+  } else {
+    if (redirect_standard_input) close(pipe_stdin[0]);
+    if (redirect_standard_output) close(pipe_stdout[1]);
+    if (redirect_standard_error) close(pipe_stderr[1]);
+    
+    file_descriptor_streambuf file_descriptor_streambuf_stdin(pipe_stdin[1]);
+    std::ostream process_stdin_stream(&file_descriptor_streambuf_stdin);
+    file_descriptor_streambuf file_descriptor_streambuf_stdout(pipe_stdout[0]);
+    std::istream process_stdout_stream(&file_descriptor_streambuf_stdout);
+    file_descriptor_streambuf file_descriptor_streambuf_stderr(pipe_stderr[0]);
+    std::istream process_stderr_stream(&file_descriptor_streambuf_stderr);
   }
   return static_cast<intptr_t>(process);
 }
