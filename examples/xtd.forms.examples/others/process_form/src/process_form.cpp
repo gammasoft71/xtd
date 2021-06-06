f2b43f2de87bb61f6f291268cb459ffb0b14a1ca
gammasoft <gammasoft71@gmail.com>
2021-06-06 19:59:55 +0200

    Update process_form.cpp

--- examples/xtd.forms.examples/others/process_form/src/process_form.cpp
+++ examples/xtd.forms.examples/others/process_form/src/process_form.cpp
@@ -62,7 +62,7 @@ public:
     open_calculator_button.image_align(content_alignment::middle_left);
     open_calculator_button.text("Launch Calculator...");
     open_calculator_button.click += [] {
-      if (environment::os_version().is_windows_platform()) process::start("calc.exe");
+      if (environment::os_version().is_windows_platform()) process::start("calc");
       else if (environment::os_version().is_macos_platform()) process::start("/System/Applications/Calculator.app");
       else if (environment::os_version().is_linux_platform()) process::start("gnome-calculator");
       else throw invalid_operation_exception("Calculator not found.");
