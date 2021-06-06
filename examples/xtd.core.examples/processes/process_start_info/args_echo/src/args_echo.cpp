fda16eec3b1698708d6727fcf17fa42b514b1826
Gammasoft <gammasoft71@gmail.com>
2021-06-06 15:42:15 +0200

    Update args_echo.cpp

--- examples/xtd.core.examples/processes/process_start_info/args_echo/src/args_echo.cpp
+++ examples/xtd.core.examples/processes/process_start_info/args_echo/src/args_echo.cpp
@@ -6,9 +6,8 @@ int main() {
   console::write_line("Received the following arguments:\n");
   
   auto args = environment::get_command_line_args();
-  for (auto i = 1u; i < args.size(); i++) {
+  for (auto i = 1u; i < args.size(); i++)
     console::write_line("[{0}] = {1}", i-1, args[i]);
-  }
   
   console::write_line();
 }
