1c1b83785f08a4653c5f37212a964c06b5db2456
Gammasoft <gammasoft71@gmail.com>
2021-06-08 13:05:39 +0200

    Update manual_tests.cpp

--- tests/xtd.core.manual_tests/src/manual_tests.cpp
+++ tests/xtd.core.manual_tests/src/manual_tests.cpp
@@ -1,40 +1,20 @@
-#include <filesystem>
+#include <codecvt>
 #include <xtd/xtd.core>
 
 using namespace xtd;
+using namespace xtd::diagnostics;
+using namespace xtd::io;
 
 class xtd_core_manual_test {
 public:
-  static size_t multi_byte_to_wide_char(const char* multi_byte_str, uint32_t nb_wide_char, wchar_t* wide_char_ctr, const char* local_str) {
-    size_t result = static_cast<size_t>(-1);
-    bool ok = true;
-
-    if (multi_byte_str) {
-      if (local_str) ok = local_str[0] ? setlocale(LC_ALL, local_str) != nullptr : setlocale(LC_ALL, "C") != nullptr;
-      if (ok) result = std::mbstowcs(wide_char_ctr, multi_byte_str, nb_wide_char);
-      if (local_str) std::locale::global(std::locale());
-    }
-    
-    return result;
-  }
-  
-  size_t wide_char_to_multi_byte(const wchar_t* wide_char_ctr, uint32_t nb_multibyte_char, char* multi_byte_str, const char* local_str) {
-    size_t result = static_cast<size_t> (-1);
-    bool ok = true;
-    
-    if (wide_char_ctr) {
-      if (local_str) ok = local_str[0] ? setlocale(LC_ALL, local_str) != nullptr : setlocale(LC_ALL, "C") != nullptr;
-      if (ok) result = std::wcstombs(multi_byte_str, wide_char_ctr, nb_multibyte_char);
-      if (local_str) std::locale::global(std::locale::global(std::locale(local_str)));
-    }
-    
-    return result;
-  }
-
   static void main() {
-    console::write_line("Hello, World!");
-    console::write_line("temp = {}", io::path::get_temp_path());
-    console::write_line("temp = {}", std::filesystem::temp_directory_path().string());
+    //console::write(stream_reader(process::start(process_start_info("xtdc", "--help").use_shell_execute(false).redirect_standard_output(true)).standard_output()).read_to_end());
+    
+    auto s = std::string("Hello, World!");
+    auto ws = std::wstring_convert<std::codecvt_utf8<wchar_t>, wchar_t>().from_bytes(s);
+    auto u16s = std::wstring_convert<std::codecvt_utf8<char16_t>, char16_t>().from_bytes(s);
+    auto u32s = std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t>().from_bytes(s);
+    wconsole::write_line(ws);
   }
 };
 
