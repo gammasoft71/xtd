19bfa446d5b793b8dbab04392487fc7a57a16642
Gammasoft <gammasoft71@gmail.com>
2021-06-08 22:30:17 +0200

    Update manual_tests.cpp

--- tests/xtd.core.manual_tests/src/manual_tests.cpp
+++ tests/xtd.core.manual_tests/src/manual_tests.cpp
@@ -1,4 +1,3 @@
-#include <codecvt>
 #include <xtd/xtd.core>
 
 using namespace xtd;
@@ -8,13 +7,8 @@ using namespace xtd::io;
 class xtd_core_manual_test {
 public:
   static void main() {
-    //console::write(stream_reader(process::start(process_start_info("xtdc", "--help").use_shell_execute(false).redirect_standard_output(true)).standard_output()).read_to_end());
-    
-    auto s = std::string("Hello, World!");
-    auto ws = std::wstring_convert<std::codecvt_utf8<wchar_t>, wchar_t>().from_bytes(s);
-    auto u16s = std::wstring_convert<std::codecvt_utf8<char16_t>, char16_t>().from_bytes(s);
-    auto u32s = std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t>().from_bytes(s);
-    wconsole::write_line(ws);
+    stream_reader reader(process::start(process_start_info("xtdc", "--help").use_shell_execute(false).redirect_standard_output(true)).standard_output());
+    console::write(reader.read_to_end());
   }
 };
 
