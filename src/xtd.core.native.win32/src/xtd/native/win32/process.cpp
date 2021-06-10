41437bea5c05a3a0982fe85b11fa32e2b8025110
Gammasoft <gammasoft71@gmail.com>
2021-06-10 16:00:53 +0200

    Update process.cpp

--- src/xtd.core.native.win32/src/xtd/native/win32/process.cpp
+++ src/xtd.core.native.win32/src/xtd/native/win32/process.cpp
@@ -115,7 +115,7 @@ process::started_process process::start(const string& file_name, const string& a
   if (redirect_standard_output) CloseHandle(pipe_stdout[1]);
   if (redirect_standard_error) CloseHandle(pipe_stderr[1]);
   
-  return make_tuple(reinterpret_cast<intptr_t>(process_information.hProcess), reinterpret_cast<int32_t>(process_information.dwProcessId), make_unique<process_ostream>(pipe_stdin[1]), make_unique<process_istream>(pipe_stdout[0]), make_unique<process_istream>(pipe_stderr[0]));
+  return make_tuple(reinterpret_cast<intptr_t>(process_information.hProcess), static_cast<int32_t>(process_information.dwProcessId), make_unique<process_ostream>(pipe_stdin[1]), make_unique<process_istream>(pipe_stdout[0]), make_unique<process_istream>(pipe_stderr[0]));
 }
 
 bool process::wait(intptr_t process, int32_t& exit_code) {
