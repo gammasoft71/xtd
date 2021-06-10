b6eaf53d40d2825627b26559a0757a462a6944cc
Gammasoft <gammasoft71@gmail.com>
2021-06-10 09:47:41 +0200

    Update manual_tests.cpp

--- tests/xtd.core.manual_tests/src/manual_tests.cpp
+++ tests/xtd.core.manual_tests/src/manual_tests.cpp
@@ -1,5 +1,7 @@
+#include <filesystem>
 #include <xtd/xtd.core>
 
+using namespace std::filesystem;
 using namespace xtd;
 using namespace xtd::diagnostics;
 using namespace xtd::io;
@@ -8,8 +10,21 @@ class xtd_core_manual_test {
 public:
   static void main() {
     console::output_code_page(65001);
-    stream_reader reader(process::start(process_start_info("xtdc", "--help").use_shell_execute(false).redirect_standard_output(true)).standard_output());
-    console::write(reader.read_to_end());
+    //stream_reader reader(process::start(process_start_info("xtdc", "--help").use_shell_execute(false).redirect_standard_output(true)).standard_output());
+    //console::write(reader.read_to_end());
+    try {
+      using_(process process) {
+        process.start_info().use_shell_execute(false);
+        process.start_info().file_name("xtdc");
+        process.start_info().arguments("--help");
+        process.start();
+        process.wait_for_exit();
+        process.start();
+        process.wait_for_exit();
+      }
+    } catch(const invalid_operation_exception& e) {
+      console::write_line(e.message());
+    }
   }
 };
 
