d1e6a5bee329fd1f138d53eb856ac0ff953965e1
Gammasoft <gammasoft71@gmail.com>
2021-06-12 00:46:44 +0200

    add docs

--- src/xtd.core.native/include/xtd/native/process.h
+++ src/xtd.core.native/include/xtd/native/process.h
@@ -40,17 +40,30 @@ namespace xtd {
       /// * [3] : The created istream for the standard output.
       /// * [4] : The created istream for the standard error.
       using started_process = std::tuple<intptr_t, int32_t, std::unique_ptr<std::ostream>, std::unique_ptr<std::istream>, std::unique_ptr<std::istream>>;
-      
+      /// @brief Gets the base priority xof the associated process.
+      /// @param priority The process handle.
+      /// @return The base priority, which is computed from the process priority class of the associated process. (see prioritty_classs.h file).
+      /// @remarks The following table shows the relationship between the base priority and process priority class values.
+      /// | Windows base_priority | macOS base_priority                        | linux base_priority                        | priority_class              |
+      /// |-----------------------|--------------------------------------------|--------------------------------------------|-----------------------------|
+      /// | 4                     | PRIO_MIN                             (-20) | PRIO_MIN                             (-20) | IDLE_PRIORITY_CLASS         |
+      /// | 6                     | PRIO_MIN + (PRIO_MAX - PRIO_MIN) / 4 (-10) | PRIO_MIN + (PRIO_MAX - PRIO_MIN) / 4 (-10) | BELOW_NORMAL_PRIORITY_CLASS |
+      /// | 8                     | PRIO_MIN + (PRIO_MAX - PRIO_MIN)/2     (0) | PRIO_MIN + (PRIO_MAX - PRIO_MIN)/2     (0) | NORMAL_PRIORITY_CLASS       |
+      /// | 10                    | PRIO_MAX - (PRIO_MAX - PRIO_MIN) / 4  (10) | PRIO_MAX - (PRIO_MAX - PRIO_MIN) / 4  (10) | ABOVE_NORMAL_PRIORITY_CLASS |
+      /// | 13                    | PRIO_MAX - (PRIO_MAX - PRIO_MIN) / 8  (15) | PRIO_MAX - (PRIO_MAX - PRIO_MIN) / 8  (15) | HIGH_PRIORITY_CLASS         |
+      /// | 24                    | PRIO_MAX                              (20) | PRIO_MAX                              (20) | REALTIME_PRIORITY_CLASS     |
       static int32_t base_priority(int32_t priority);
-
       /// @brief Kill process.
       /// @param process The process handle to kill.
       /// @return true if succeed; otherwise false.
       /// @warning Internal use only
       static bool kill(intptr_t process);
-      
+      /// @brief Sets the overall priority category for the associated process.
+      /// @param process The process handle to set priority class.
+      /// @param priority The priority category for the associated process, from which the base_priority of the process is calculated. (see prioritty_classs.h file).
+      /// @return true if succeed; otherwise false.
+      /// @warning Internal use only
       static bool priority_class(intptr_t process, int32_t priority);
-
       /// @brief Creates a process with specified file name, specified arguments, specified working directory, specified process creation flags, and specified process window style.
       /// @param file_name the name of the application file to run in process launched by the shell execute.
       /// @param arguments Command-line arguments to pass when startinhg the process.
