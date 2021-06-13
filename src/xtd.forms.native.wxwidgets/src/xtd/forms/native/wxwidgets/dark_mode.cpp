97d56b66982f953099dd0eb74baeecbc8f2d2323
gammasoft <gammasoft71@gmail.com>
2021-06-13 23:34:17 +0200

    Update dark_mode.cpp

--- src/xtd.forms.native.wxwidgets/src/xtd/forms/native/wxwidgets/dark_mode.cpp
+++ src/xtd.forms.native.wxwidgets/src/xtd/forms/native/wxwidgets/dark_mode.cpp
@@ -243,7 +243,8 @@ constexpr bool CheckBuildNumber(DWORD buildNumber) {
     buildNumber == 18362 || // 1903
     buildNumber == 18363 || // 1909
     buildNumber == 19041 || // 2004
-    buildNumber == 19042);  // 20H2
+    buildNumber == 19042 || // 20H2
+    buildNumber == 19043);  // 21H1
 }
 
 void init_dark_mode(int enableDarkMode) {
