2d8fdbd98e6ddc11d13ec67d77404bf4d9647cb0
Gammasoft <gammasoft71@gmail.com>
2021-06-13 10:45:27 +0200

    add docs

--- tests/xtd.core.manual_tests/src/manual_tests.cpp
+++ tests/xtd.core.manual_tests/src/manual_tests.cpp
@@ -13,33 +13,63 @@ using namespace xtd::io;
 class xtd_core_manual_test {
 public:
   static void main() {
-    using_(process process) {
-      process.start_info().file_name("ipconfig");
-      process.start_info().use_shell_execute(false);
-      process.start_info().redirect_standard_output(true);
-      process.start();
-      
-      // Synchronously read the standard output of the spawned process.
-      stream_reader reader(process.standard_output());
-      string output = reader.read_to_end();
-      
-      // Write the redirected output to this application's window.
-      console::write_line(output);
-      
-      process.wait_for_exit();
-    }
-    
-    console::write_line("\n\nPress any key to exit.");
-    console::read_line();
+    //system("osascript -e \"do shell script \\\"/System/Applications/TextEdit.app/Contents/MacOS/TextEdit\\\" with administrator privileges\"");
 
-    /*
     try {
       using_(process process) {
-        process.start_info().file_name("xtdc");
-        process.start_info().arguments("--help");
+        process.start_info().file_name("/Users/yves/Projects/xtd/docs/pictures/gammasoft.png");
+        process.start_info().use_shell_execute(true);
+        process.start_info().verb("cot");
+        process.start_info().arguments("");
+        //process.start_info().redirect_standard_output(true);
+        process.start();
+        //console::write(stream_reader(process.standard_output()).read_to_end());
+        process.wait_for_exit();
+      }
+    } catch(const invalid_operation_exception& e) {
+      console::write_line(e.message());
+    }
+    
+    /*
+    auto target_directory = temp_directory_path()/"xtd.core.manual_tests";
+    create_directory(target_directory);
+    file::write_all_text(target_directory/"echo_out.cmd", "echo \"$*\"");
+    permissions(target_directory/"echo_out.cmd", perms::owner_all);
+    file::write_all_text(target_directory/"echo_err.cmd", ">&2 echo \"$*\"");
+    permissions(target_directory/"echo_err.cmd", perms::owner_all);
+
+    try {
+      using_(process process) {
+        process.start_info().file_name(target_directory/"echo_err.cmd");
         process.start_info().use_shell_execute(false);
+        process.start_info().arguments("Hello_world");
+        //process.start_info().redirect_standard_output(true);
+        process.start_info().redirect_standard_error(true);
+        process.start();
+        //console::write(stream_reader(process.standard_output()).read_to_end());
+        process.wait_for_exit();
+      }
+    } catch(const invalid_operation_exception& e) {
+      console::write_line(e.message());
+    }
+     */
+
+    /*
+    if (environment::os_version().is_windows_platform()) file::write_all_text(target_directory/"wait.cmd", "@echo \"Wait %* milliseconds...\"\n@sleepfor %*");
+    else file::write_all_text(target_directory/"wait.cmd", "echo \"Wait $* milliseconds...\"\nsleepfor $*");
+    permissions(target_directory/"wait.cmd", perms::owner_all);
+
+    try {
+      using_(process process) {
+        process.start_info().file_name(target_directory/"wait.cmd");
+        process.start_info().use_shell_execute(false);
+        process.start_info().arguments("1000");
         process.start();
         process.wait_for_exit();
+        process.start_info().arguments("2000");
+        process.start();
+        //process.wait_for_exit();
+        process.start_info().arguments("3000");
         process.start();
         process.wait_for_exit();
       }
