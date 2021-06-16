e3332b5cfff34048c9a72d5847ca24f7d6132585
Gammasoft <gammasoft71@gmail.com>
2021-06-16 11:25:28 +0200

    Ad Workaround : on macOS with wxWidgets toolkit, retina display, dark mode enabled, border style is not none and multiline, the border is not show.

--- src/xtd.forms/include/xtd/forms/list_box.h
+++ src/xtd.forms/include/xtd/forms/list_box.h
@@ -139,10 +139,10 @@ namespace xtd {
       forms::create_params create_params() const override;
 
       void on_handle_created(const event_args& e) override;
-
-      void on_selected_value_changed(const event_args& e) override;
       
       void on_paint(paint_event_args& e) override;
+
+      void on_selected_value_changed(const event_args& e) override;
 
       void wnd_proc(message& message) override;
       
