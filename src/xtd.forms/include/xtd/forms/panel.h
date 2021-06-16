121fea004590563c11254d8fddf5a4fec4a349ef
Gammasoft <gammasoft71@gmail.com>
2021-06-16 10:08:05 +0200

    Add Workaround : on macOS with wxWidgets toolkit, retina display, dark mode enabled and border style is fixed 3d, the border is not show.

--- src/xtd.forms/include/xtd/forms/panel.h
+++ src/xtd.forms/include/xtd/forms/panel.h
@@ -60,6 +60,8 @@ namespace xtd {
 
       void on_layout(const event_args& e) override;
       
+      void on_paint(paint_event_args& e) override;
+      
       /// @cond
       forms::border_style border_style_ = forms::border_style::none;
       /// @endcond
