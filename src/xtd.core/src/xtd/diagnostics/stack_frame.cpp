84768bce53378db78541cd8c726c948c1ab414e8
Gammasoft <gammasoft71@gmail.com>
2021-06-18 23:48:27 +0200

    Update stack_frame.cpp

--- src/xtd.core/src/xtd/diagnostics/stack_frame.cpp
+++ src/xtd.core/src/xtd/diagnostics/stack_frame.cpp
@@ -115,6 +115,6 @@ std::vector<stack_frame> stack_frame::get_stack_frames(const std::string& str, s
 
   std::vector<stack_frame> stack_frames;
   for (size_t index = skip_frames_before_str + skip_frames; index < call_stack.stack.size() - 1; ++index)
-    stack_frames.emplace_back(stack_frame(need_file_info ? call_stack.stack[index].file : "", need_file_info ? static_cast<uint32_t>(call_stack.stack[index].line) : 0, call_stack.stack[index].function, need_file_info ? static_cast<uint32_t>(call_stack.stack[index].column) : 0, call_stack.stack[index].offset));
+    stack_frames.emplace_back(stack_frame(need_file_info ? call_stack.stack[index].file : "", need_file_info ? static_cast<uint32_t>(call_stack.stack[index].line) : 0, call_stack.stack[index].function, need_file_info ? static_cast<uint32_t>(call_stack.stack[index].column) : 0, static_cast<uint32_t>(call_stack.stack[index].offset)));
   return stack_frames;
 }
