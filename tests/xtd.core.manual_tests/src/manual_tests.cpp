7164eecfdbf56a6389da4625ce8b3205f4bf8ea1
Gammasoft <gammasoft71@gmail.com>
2021-06-19 09:59:45 +0200

    Update manual_tests.cpp

--- tests/xtd.core.manual_tests/src/manual_tests.cpp
+++ tests/xtd.core.manual_tests/src/manual_tests.cpp
@@ -1,8 +1,5 @@
 #include <filesystem>
 #include <xtd/xtd.core>
-#define __XTD_CORE_NATIVE_LIBRARY__
-#include <xtd/native/priority_class.h>
-#undef __XTD_CORE_NATIVE_LIBRARY__
 
 using namespace std;
 using namespace xtd;
@@ -11,10 +8,8 @@ using namespace xtd::diagnostics;
 class xtd_core_manual_test {
 public:
   static void main() {
-    stack_trace st;
-    stack_trace st1(stack_frame(true));
-    console::write_line("stack_frame : {}", st1);
-    console::write_line(st);
+    debug::assert(false);
+    console::write_line("Hello, Woorld!");
   }
 };
 
