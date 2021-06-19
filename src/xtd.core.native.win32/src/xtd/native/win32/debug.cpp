2f38ea589b3a0c41f9bae2c109d6c87446e35d37
Gammasoft <gammasoft71@gmail.com>
2021-06-19 09:59:19 +0200

    update assert

--- src/xtd.core.native.win32/src/xtd/native/win32/debug.cpp
+++ src/xtd.core.native.win32/src/xtd/native/win32/debug.cpp
@@ -5,6 +5,10 @@
 
 using namespace xtd::native;
 
+void debug::assert_message_box(bool condition, const std::string& message) {
+  ASSERT(condition);
+}
+
 void debug::write_to_output(const std::string& message) {
   OutputDebugStringA(message.c_str());
 }
