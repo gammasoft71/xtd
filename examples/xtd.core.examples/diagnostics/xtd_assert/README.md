c70873c1a99955763b6c354037af080cc7a0174c
Gammasoft <gammasoft71@gmail.com>
2021-06-21 15:01:38 +0200

    fix xtd_assert and add examples

---
+++ examples/xtd.core.examples/diagnostics/xtd_assert/README.md
@@ -0,0 +1,27 @@
+# xtd_assert
+
+Shows how to use [xtd_assert](../../../../src/xtd.core/include/xtd/diagnostics/xtd_assert.h) macro.
+
+## Sources
+
+[src/stack_trace.cpp](src/xtd_assert.cpp)
+
+[CMakeLists.txt](CMakeLists.txt)
+
+# Build and run
+
+Open "Command Prompt" or "Terminal". Navigate to the folder that contains the project and type the following:
+
+```cmake
+xtdc run
+```
+
+# Output
+
+```
+stack_trace:
+   at method3()
+   at method2()
+   at method1()
+   at main
+```
---
+++ examples/xtd.core.examples/diagnostics/xtd_assert/src/xtd_assert.cpp
@@ -0,0 +1,26 @@
+#include <xtd/xtd>
+
+using namespace xtd;
+
+int main() {
+  // Uncomment following line to remove assert dialog
+  //diagnostics::debug::show_assert_dialog(false);
+  
+  auto index = 0;
+  console::write_line("Start application");
+  xtd_assert(index > 0);
+  console::write_line("End application");
+}
+
+// This code produces the following output :
+//
+// > If user clicks assert dialog 'Abort' button:
+// Start application
+//
+// > If user clicks assert dialog 'Retry' button:
+// Start application
+// > Break the debugger on file xtd_assert.cpp line 9.
+//
+// > If user clicks assert dialog 'Ignore' button:
+// Start application
+// End application
