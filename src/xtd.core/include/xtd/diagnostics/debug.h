9a95a0c85741af203383346b7b2d12d9bef50fd0
Gammasoft <gammasoft71@gmail.com>
2021-06-24 11:44:19 +0200

    Update debug.h

--- src/xtd.core/include/xtd/diagnostics/debug.h
+++ src/xtd.core/include/xtd/diagnostics/debug.h
@@ -469,7 +469,7 @@ namespace xtd {
 /// @brief Checks for a condition; if the condition is false, displays a message box that shows the call stack.
 /// @par Library
 /// xtd.core
-/// @ingroup xtd_core debug keywords
+/// @ingroup xtd_core debug
 /// @param condition The conditional expression to evaluate. If the condition is true, a failure message is not sent and the message box is not displayed.
 /// @param message (optional) The message to send to the xtd::diagnostics::debug::listeners collection.
 /// @par Usage
