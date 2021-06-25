2db4d8b16941ad8f4ec8bc0d2f1199c7b02bfde5
Gammasoft <gammasoft71@gmail.com>
2021-06-26 00:21:59 +0200

    add debugger class and fix

--- src/xtd.core/include/xtd/diagnostics/debugger.h
+++ src/xtd.core/include/xtd/diagnostics/debugger.h
@@ -21,7 +21,7 @@
 namespace xtd {
   /// @brief The xtd::diagnostics namespace provides classes that allow you to interact with system processes, event logs, and performance counters.
   namespace diagnostics {
-    /// @brief Provides a set of methods and properties that help you debug the execution of your code. This class cannot be inherited.
+    /// @brief Enables communication with a debugger. This class cannot be inherited.
     /// @par Library
     /// xtd.core
     /// @ingroup xtd_core diagnostics debug
@@ -32,7 +32,6 @@ namespace xtd {
       static bool is_attached();
 
       /// @brief Signals a breakpoint to an attached debugger.
-      /// @remarks f no debugger is attached, users are asked if they want to attach a debugger. If users say yes, the debugger is started. If a debugger is attached, the debugger is signaled with a user breakpoint event, and the debugger suspends execution of the process just as if a debugger breakpoint had been hit.
       /// @par Examples
       /// The following code example demonstrates how to stop the debugger at the call to write_line.
       /// @code
@@ -41,10 +40,22 @@ namespace xtd {
       /// @endcode
       static void debug_break();
 
+      /// @brief Checks to see if logging is enabled by an attached debugger.
+      /// @return true if a debugger is attached and logging is enabled; otherwise, false.
       static bool is_logging();
 
+      /// @brief Launches and attaches a debugger to the process.
+      /// @return true if the startup is successful or if the debugger is already attached; otherwise, false.
+      /// @remarks If a debugger is already attached, nothing happens.
+      /// @remarks Debugger launch is only supported on ooperating system. On Unix and macOS operating systems, the method returns true without launching a debugger.
       static bool launch();
 
+      /// @brief Posts a message for the attached debugger.
+      /// @param level A description of the importance of the message.
+      /// @param category The category of the message.
+      /// @param message The message to show.
+      /// @remarks If there is no debugger attached, this method has no effect. The debugger might or might not report the message, depending upon its settings.
+      /// @note The category parameter is limited to 256 characters. std::strings longer than 256 characters are truncated.
       static void log(int level, std::string& category, const std::string& message);
     };
   }
@@ -64,6 +75,16 @@ namespace xtd {
 #define debug_break_() \
   __debugbreak()
 #else
+/// @brief Signals a breakpoint to an attached debugger.
+/// @par Library
+/// xtd.core
+/// @ingroup xtd_core debug
+/// @par Examples
+/// The following code example demonstrates how to stop the debugger at the call to write_line.
+/// @code
+/// debugger_break_();
+/// console::write_line("Hello, world.");
+/// @endcode
 #define debug_break_() \
   std::abort()
 #endif
