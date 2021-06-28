ee76bd3334d53c2cebbfe5e8438ff27857009c0d
Gammasoft <gammasoft71@gmail.com>
2021-06-28 23:06:47 +0200

    Update console_trace_listener.cpp

--- src/xtd.core/src/xtd/diagnostics/console_trace_listener.cpp
+++ src/xtd.core/src/xtd/diagnostics/console_trace_listener.cpp
@@ -5,7 +5,9 @@ using namespace std;
 using namespace xtd::diagnostics;
 
 console_trace_listener::console_trace_listener() : console_trace_listener(false) {
+  name("console");
 }
 
 console_trace_listener::console_trace_listener::console_trace_listener(bool use_error_stream) : ostream_trace_listener(use_error_stream ? std::cerr : std::cout) {
+  name("console");
 }
