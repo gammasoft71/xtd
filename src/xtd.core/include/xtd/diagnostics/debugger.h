6440c1cd76c38a0ca34241c4582d6f9d1f2d182f
Gammasoft <gammasoft71@gmail.com>
2021-06-28 19:39:19 +0200

    Update debugger.h

--- src/xtd.core/include/xtd/diagnostics/debugger.h
+++ src/xtd.core/include/xtd/diagnostics/debugger.h
@@ -1,5 +1,5 @@
 /// @file
-/// @brief Contains xtd::diagnostics::debug class.
+/// @brief Contains xtd::diagnostics::debugger class.
 /// @copyright Copyright (c) 2021 Gammasoft. All rights reserved.
 #pragma once
 #include <cstdint>
