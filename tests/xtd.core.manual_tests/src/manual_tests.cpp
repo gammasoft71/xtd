d47122344a148d5a587d30e961fa71f7191b0865
Gammasoft <gammasoft71@gmail.com>
2021-06-28 23:06:50 +0200

    Update manual_tests.cpp

--- tests/xtd.core.manual_tests/src/manual_tests.cpp
+++ tests/xtd.core.manual_tests/src/manual_tests.cpp
@@ -2,11 +2,34 @@
 
 using namespace std;
 using namespace xtd;
+using namespace xtd::diagnostics;
 
 class xtd_core_manual_test {
 public:
   static void main() {
     console::write_line("Start app");
+    
+    trace_listener_collection tls;
+    tls.push_back(make_shared<default_trace_listener>());
+    tls.push_back(make_shared<console_trace_listener>());
+
+    console::write_line();
+    console::write_line("foreach : ");
+    for (auto tl : tls)
+      tl->write_line("Hello, world!");
+
+    console::write_line();
+    console::write_line("index operator : ");
+    tls[0]->write_line("Hello, world!");
+    tls[1]->write_line("Hello, world!");
+
+    console::write_line();
+    console::write_line("name operator : ");
+    if (tls["default"]) tls["default"]->write_line("Hello, world!");
+    if (tls["console"]) tls["console"]->write_line("Hello, world!");
+    if (tls["Console"]) tls["Console"]->write_line("Hello, world!");
+
+    console::write_line();
     
     console::write_line("End app");
   }
