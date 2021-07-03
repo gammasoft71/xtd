732503718cae28de7c98ceaf7245529f96a98eab
Gammasoft <gammasoft71@gmail.com>
2021-07-03 13:55:17 +0200

    Update labels_and_unicode_text.cpp

--- examples/xtd.forms.examples/others/labels_and_unicode_text/src/labels_and_unicode_text.cpp
+++ examples/xtd.forms.examples/others/labels_and_unicode_text/src/labels_and_unicode_text.cpp
@@ -28,12 +28,16 @@ public:
 private:
   vector<pair<string, string>> languages {
     {"Arabic", u8"\u0627\u0644\u0643\u0644\u0645\u0629 \u0627\u0644\u0639\u0631\u0628\u064A\u0629"_s},
-    {"Trad. Chinese", u8"\u4E2D\u570B\u7684\u6F22\u5B57"_s}, {"Simpl. Chinese:", u8"\u6C49\u8BED"_s},
-    {"French", u8"Langue fran\u00E7aise"_s}, {"Greek", u8"\u0395\u03BB\u03BB\u03B7\u03BD\u03B9\u03BA\u03AE \u03B3\u03BB\u03CE\u03C3\u03C3\u03B1"_s},
-    {"Hebrew", u8"\u05DB\u05EA\u05D1 \u05E2\u05D1\u05E8\u05D9\u05EA"_s}, {"Hindi", u8"\u0939\u093f\u0928\u094d\u0926\u0940 \u092d\u093e\u0937\u093e"_s},
-    {"Icelandic", u8"\u00CDslenska"_s}, {"Japanese", u8"\u65E5\u672C\u8A9E\u306E\u3072\u3089\u304C\u306A, \u6F22\u5B57\u3068\u30AB\u30BF\u30AB\u30CA"_s},
-    {"Korean", u8"\uB300\uD55C\uBBFC\uAD6D\uC758 \uD55C\uAE00"_s}, {"Russian", u8"\u0420\u0443\u0441\u0441\u043A\u0438\u0439 \u044F\u0437\u044B\u043A"_s}
-  };
+    {"Trad. Chinese", u8"\u4E2D\u570B\u7684\u6F22\u5B57"_s},
+    {"Simpl. Chinese:", u8"\u6C49\u8BED"_s},
+    {"French", u8"Langue fran\u00E7aise"_s},
+    {"Greek", u8"\u0395\u03BB\u03BB\u03B7\u03BD\u03B9\u03BA\u03AE \u03B3\u03BB\u03CE\u03C3\u03C3\u03B1"_s},
+    {"Hebrew", u8"\u05DB\u05EA\u05D1 \u05E2\u05D1\u05E8\u05D9\u05EA"_s},
+    {"Hindi", u8"\u0939\u093f\u0928\u094d\u0926\u0940 \u092d\u093e\u0937\u093e"_s},
+    {"Icelandic", u8"\u00CDslenska"_s},
+    {"Japanese", u8"\u65E5\u672C\u8A9E\u306E\u3072\u3089\u304C\u306A, \u6F22\u5B57\u3068\u30AB\u30BF\u30AB\u30CA"_s},
+    {"Korean", u8"\uB300\uD55C\uBBFC\uAD6D\uC758 \uD55C\uAE00"_s},
+    {"Russian", u8"\u0420\u0443\u0441\u0441\u043A\u0438\u0439 \u044F\u0437\u044B\u043A"_s}};
   vector<pair<label, label>> items {languages.size()};
 };
 
