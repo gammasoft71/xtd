f7151468a872cd997a11e170971a6bc74e8e2edc
Gammasoft <gammasoft71@gmail.com>
2021-07-05 23:27:20 +0200

    Update cell.h

--- examples/xtd.forms.examples/games/minesweeper/src/cell.h
+++ examples/xtd.forms.examples/games/minesweeper/src/cell.h
@@ -2,27 +2,61 @@
 #include "cell_state.h"
 #include <xtd/xtd>
 
+/// @brief The namespace minesweeper contains all classes and enums needed for the game.
 namespace minesweeper {
-  class cell {
+  /// @brief Represent one cell the game.
+  /// @remarks the game contains 81 (9 x 9) cells and 10 mines for beginner level.
+  /// @remarks the game contains 256 (16 x 16) cells and 40 mines for intermediate level.
+  /// @remarks the game contains 480 (30 x 16 cells and 99 mines for expert and custom level.
+  class cell final {
   public:
-    bool has_mine() const;
-    void has_mine(bool value);
+    /// @brief Gets a value indicate if cell contains mine.
+    /// @return true if cell contains mine; otherwhise false.
+    bool has_mine() const noexcept;
+    /// @brief Sets a value indicate if cell contains mine.
+    /// @param value true if cell contains mine; otherwhise false.
+    void has_mine(bool value) noexcept;
     
-    bool has_neighbors() const;
+    /// @brief Gets a value indicate if cell has neighbors.
+    /// @return true if cell has neighbors; otherwhise false.
+    bool has_neighbors() const noexcept;
     
-    static int height();
+    /// @brief Gets the height of cell in pixels.
+    /// @return The height of cell.
+    /// @remarks The values is 30.
+    static int height() noexcept;
     
-    int neighbors() const;
-    void neighbors(int value);
+    /// @brief Gets the number of neighbors of the cell.
+    /// @return The number of cell's neighbors.
+    /// @remarks The value is betwen 0 and 8.
+    int neighbors() const noexcept;
+    /// @brief Sets the number of neighbors of the cell.
+    /// @param value The number neighbors of the cell.
+    /// @remarks The value must be betwen 0 and 8.
+    /// @remarks The value is provided by minesweeper::minesweeper_form and there is no check.
+    void neighbors(int value) noexcept;
     
-    static xtd::drawing::size size();
+    /// @brief Gets the size of cell in pixels.
+    /// @return The size of cell.
+    /// @remarks The values is 30 x 30.
+    static xtd::drawing::size size() noexcept;
     
-    cell_state state() const;
-    void state(cell_state value);
+    cell_state state() const noexcept;
+    void state(cell_state value) noexcept;
     
-    static int width();
+    /// @brief Gets the width of cell in pixels.
+    /// @return The width of cell.
+    /// @remarks The values is 30.
+    static int width() noexcept;
     
-    void clear();
+    /// @brief Clear all cell's values
+    /// @remarks Clear as follow:
+    /// | name      | value                 |
+    /// |-----------|-----------------------|
+    /// | has_mine  | false                 |
+    /// | neighbors | 0                     |
+    /// | state     | cell_state::unchecked |
+    void clear() noexcept;
     
   private:
     bool has_mine_ = false;
