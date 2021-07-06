0fb75d827e23da79c41e068101999a5b177401eb
Gammasoft <gammasoft71@gmail.com>
2021-07-06 14:13:08 +0200

    Update target_properties.cmake

--- examples/xtd.forms.examples/games/minesweeper/properties/target_properties.cmake
+++ examples/xtd.forms.examples/games/minesweeper/properties/target_properties.cmake
@@ -1,4 +1,4 @@
 target_default_namespace("minesweeper")
 target_icon(resources/minesweeper)
 target_name("minesweeper")
-target_startup("minesweeper::form_minesweeper" src/minesweeper.h)
+target_startup("minesweeper::minesweeper_form" src/minesweeper.h)
