a5bc112f0bddf9028cc8ef8c72fe387ed05fe6a7
Gammasoft <gammasoft71@gmail.com>
2021-07-24 12:39:47 +0200

    refactoring

--- src/xtd.core/include/xtd/io/text_reader.h
+++ src/xtd.core/include/xtd/io/text_reader.h
@@ -33,59 +33,38 @@ namespace xtd {
       static null_text_reader& null();
       
       /// @brief Closes the xtd::io::text_reader and releases any system resources associated with the text_reader
-      virtual void close() {}
+      virtual void close();
       
       /// @brief Reads the next character without changing the state of the reader or the character source. Returns the next available character without actually reading it from the input stream.
       /// @return An integer representing the next character to be read, or EOF if no more characters are available or the stream does not support seeking.
-      virtual int32_t peek() const {return EOF;}
+      virtual int32_t peek() const;
       
       /// @brief Reads the next character from the input stream and advances the character position by one character.
       /// @return The next character from the input stream, or EOF if no more characters are available.
-      virtual int32_t read() {return EOF;}
+      virtual int32_t read();
       
       /// @brief Reads a specified maximum number of characters from the current text reader and writes the data to a buffer, beginning at the specified index.
       /// @param buffer When this method returns, this parameter contains the specified character array with the values between index and (index + count -1) replaced by the characters read from the current source.
       /// @param index The position in buffer at which to begin writing.
       /// @param count The maximum number of characters to read.
       /// @return The number of characters that have been read. The number will be less than or equal to count, depending on whether all input characters have been read.
-      virtual int32_t read(char* buffer, int32_t index, int32_t count) {
-        for (auto i = 0; i < count; i++) {
-          auto current = read();
-          if (current == EOF) return i;
-          buffer[index + i] = static_cast<char>(current);
-        }
-        return count;
-      }
+      virtual int32_t read(char* buffer, int32_t index, int32_t count);
       
       /// @brief Reads a specified maximum number of characters from the current text reader and writes the data to a buffer, beginning at the specified index.
       /// @param buffer When this method returns, this parameter contains the specified character array with the values between index and (index + count -1) replaced by the characters read from the current source.
       /// @param index The position in buffer at which to begin writing.
       /// @param count The maximum number of characters to read.
       /// @return The number of characters that have been read. The number will be less than or equal to count, depending on whether all input characters have been read.
-      virtual int32_t read_block(char* buffer, int32_t index, int32_t count) {return read(buffer, index, count);}
+      virtual int32_t read_block(char* buffer, int32_t index, int32_t count);
       
       /// @brief Reads a line of characters from the current stream and returns the data as a string.
       /// @return The next line from the input stream, or the empty string if all characters have been read.
-      virtual std::string read_line() {
-        std::string line;
-        for (int32_t current = read(); current != EOF && current != '\n'; current = read()) {
-          if (current == '\r') continue;
-            line += static_cast<char>(current);
-        }
-        return line;
-      }
+      virtual std::string read_line();
       
       /// @brief Reads all characters from the current position to the end of the text_reader and returns them as one string.
       /// @return A string containing all characters from the current position to the end of the text_reader.
       /// @exception io::ioexception An I/O error occurs.
-      virtual std::string read_to_end() {
-        std::string text;
-        for (int32_t current = read(); current != EOF; current = read()) {
-          if (current == '\r') continue;
-          text += static_cast<char>(current);
-        }
-        return text;
-      }
+      virtual std::string read_to_end();
       
       /// @brief Creates a thread-safe (synchronized) wrapper around the specified text_reader object.
       /// @param reader The text_reader object to synchronize.
@@ -97,21 +76,26 @@ namespace xtd {
       text_reader() = default;
     };
     
+    /// @brief Represents a null text reader.
+    /// @par Library
+    /// xtd.core
+    /// @ingroup xtd_core io
     class null_text_reader : public text_reader {
     public:
-      int32_t read() override {return EOF;}
+      int32_t read() override;
     };
     
+    /// @brief Represents a synchronized text reader.
+    /// @par Library
+    /// xtd.core
+    /// @ingroup xtd_core io
     class synchronized_text_reader : public text_reader {
     public:
-      int32_t read() override {
-        std::lock_guard<std::mutex> lock(mutex_);
-        return reader_.read();
-      }
+      int32_t read() override;
       
     private:
       friend class text_reader;
-      explicit synchronized_text_reader(xtd::io::text_reader& reader) : reader_(reader) {}
+      explicit synchronized_text_reader(xtd::io::text_reader& reader);
       synchronized_text_reader() = delete;
       xtd::io::text_reader& reader_;
       std::mutex mutex_;
