f624f6292ef8556aee07ca57bc6a987e9d456d3e
Gammasoft <gammasoft71@gmail.com>
2021-07-24 12:39:47 +0200

    refactoring

--- src/xtd.core/src/xtd/io/text_reader.cpp
+++ src/xtd.core/src/xtd/io/text_reader.cpp
@@ -7,6 +7,60 @@ null_text_reader& text_reader::null() {
   return null_text_reader;
 }
 
+void text_reader::close() {
+}
+
+int32_t text_reader::peek() const {
+  return EOF;
+}
+
+int32_t text_reader::read() {
+  return EOF;
+}
+
+int32_t text_reader::read(char* buffer, int32_t index, int32_t count) {
+  for (auto i = 0; i < count; i++) {
+    auto current = read();
+    if (current == EOF) return i;
+    buffer[index + i] = static_cast<char>(current);
+  }
+  return count;
+}
+
+int32_t text_reader::read_block(char* buffer, int32_t index, int32_t count) {
+  return read(buffer, index, count);
+}
+
+std::string text_reader::read_line() {
+  std::string line;
+  for (int32_t current = read(); current != EOF && current != '\n'; current = read()) {
+    if (current == '\r') continue;
+    line += static_cast<char>(current);
+  }
+  return line;
+}
+
+std::string text_reader::read_to_end() {
+  std::string text;
+  for (int32_t current = read(); current != EOF; current = read()) {
+    if (current == '\r') continue;
+    text += static_cast<char>(current);
+  }
+  return text;
+}
+
 synchronized_text_reader text_reader::synchronised(text_reader& reader) {
   return synchronized_text_reader(reader);
+}
+
+int32_t null_text_reader::read() {
+  return EOF;
+}
+
+int32_t synchronized_text_reader::read() {
+  std::lock_guard<std::mutex> lock(mutex_);
+  return reader_.read();
+}
+
+synchronized_text_reader::synchronized_text_reader(xtd::io::text_reader& reader) : reader_(reader) {
 }
