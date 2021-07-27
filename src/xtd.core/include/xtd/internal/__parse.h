72696b3538817bde352bfd4f13d02d6c142342a1
Gammasoft <gammasoft71@gmail.com>
2021-07-27 14:47:04 +0200

    remove strings.h

--- src/xtd.core/include/xtd/internal/__parse.h
+++ src/xtd.core/include/xtd/internal/__parse.h
@@ -7,22 +7,29 @@
 #endif
 /// @endcond
 
+#include <locale>
 #include <string>
+#include <sstream>
 #include "__parse_exception.h"
 #include "../number_styles.h"
-#include "../strings.h"
 
 /// @cond
 template <typename char_t>
 inline std::basic_string<char_t> __parse_remove_decorations(const std::basic_string<char_t>& s, xtd::number_styles styles) {
   std::basic_string<char_t> str(s);
-  if ((styles & xtd::number_styles::allow_leading_white) == xtd::number_styles::allow_leading_white) str = xtd::strings::trim_start(str);
-  if ((styles & xtd::number_styles::allow_trailing_white) == xtd::number_styles::allow_trailing_white) str = xtd::strings::trim_end(str);
-  if ((styles & xtd::number_styles::allow_currency_symbol) == xtd::number_styles::allow_currency_symbol && xtd::strings::starts_with(str, std::use_facet<std::moneypunct<char_t>>(std::locale()).curr_symbol())) str = xtd::strings::remove(str, 0, std::use_facet<std::moneypunct<char_t>>(std::locale()).curr_symbol().size());
-  if ((styles & xtd::number_styles::allow_currency_symbol) == xtd::number_styles::allow_currency_symbol && xtd::strings::ends_with(str, std::use_facet<std::moneypunct<char_t>>(std::locale()).curr_symbol())) str = xtd::strings::substring(str, 0, str.size() - std::use_facet<std::moneypunct<char_t>>(std::locale()).curr_symbol().size());
-  if ((styles & xtd::number_styles::allow_binary_specifier) == xtd::number_styles::allow_binary_specifier && (xtd::strings::starts_with(str, "0b") || xtd::strings::starts_with(str, "0B"))) str = xtd::strings::remove(str, 0, 2);
-  if ((styles & xtd::number_styles::allow_octal_specifier) == xtd::number_styles::allow_octal_specifier && xtd::strings::starts_with(str, '0')) str = xtd::strings::remove(str, 0, 1);
-  if ((styles & xtd::number_styles::allow_hex_specifier) == xtd::number_styles::allow_hex_specifier && (xtd::strings::starts_with(str, "0x") || xtd::strings::starts_with(str, "0X"))) str = xtd::strings::remove(str, 0, 2);
+  if ((styles & xtd::number_styles::allow_leading_white) == xtd::number_styles::allow_leading_white) {
+    while(str.size() > 0 && (str[0] == 9 || str[0] == 10 || str[0] == 11 || str[0] == 12 || str[0] == 13 || str[0] == 32))
+      str.erase(0, 1);
+  }
+  if ((styles & xtd::number_styles::allow_trailing_white) == xtd::number_styles::allow_trailing_white) {
+    while(str.size() > 0 && (str[str.size() - 1] == 9 || str[str.size() - 1] == 10 || str[str.size() - 1] == 11 || str[str.size() - 1] == 12 || str[str.size() - 1] == 13 || str[str.size() - 1] == 32))
+      str.erase(str.size() - 1, 1);
+  }
+  if ((styles & xtd::number_styles::allow_currency_symbol) == xtd::number_styles::allow_currency_symbol && str.find(std::use_facet<std::moneypunct<char_t>>(std::locale()).curr_symbol()) == 0) str.erase(0, std::use_facet<std::moneypunct<char_t>>(std::locale()).curr_symbol().size());
+  if ((styles & xtd::number_styles::allow_currency_symbol) == xtd::number_styles::allow_currency_symbol && str.rfind(std::use_facet<std::moneypunct<char_t>>(std::locale()).curr_symbol()) + std::use_facet<std::moneypunct<char_t>>(std::locale()).curr_symbol().size() == str.size()) str = str.substr(0, str.size() - std::use_facet<std::moneypunct<char_t>>(std::locale()).curr_symbol().size());
+  if ((styles & xtd::number_styles::allow_binary_specifier) == xtd::number_styles::allow_binary_specifier && (str.find("0b") == 0 || str.find("0B") == 0)) str.erase(0, 2);
+  if ((styles & xtd::number_styles::allow_octal_specifier) == xtd::number_styles::allow_octal_specifier && str.find('0') == 0) str.erase(0, 1);
+  if ((styles & xtd::number_styles::allow_hex_specifier) == xtd::number_styles::allow_hex_specifier && (str.find("0x") == 0 || str.find("0X") == 0)) str.erase(0, 2);
   return str;
 }
 
@@ -30,32 +37,32 @@ template <typename char_t>
 inline int __parse_remove_signs(std::basic_string<char_t>& str, xtd::number_styles styles) {
   int sign = 0;
   
-  while ((styles & xtd::number_styles::allow_leading_sign) == xtd::number_styles::allow_leading_sign && xtd::strings::starts_with(str, "+")) {
+  while ((styles & xtd::number_styles::allow_leading_sign) == xtd::number_styles::allow_leading_sign && str.find("+") == 0) {
     if (sign != 0) __throw_parse_argument_exception("String contains more than one sign");
-    str = xtd::strings::substring(str, 1, str.size()-1);
+    str = str.substr(1, str.size()-1);
     sign += 1;
   }
   
-  while ((styles & xtd::number_styles::allow_leading_sign) == xtd::number_styles::allow_leading_sign && xtd::strings::starts_with(str, "-")) {
+  while ((styles & xtd::number_styles::allow_leading_sign) == xtd::number_styles::allow_leading_sign && str.find("-") == 0) {
     if (sign != 0) __throw_parse_argument_exception("String contains more than one sign");
-    str = xtd::strings::substring(str, 1, str.size()-1);
+    str = str.substr(1, str.size()-1);
     sign -= 1;
   }
   
-  while ((styles & xtd::number_styles::allow_trailing_sign) == xtd::number_styles::allow_trailing_sign && xtd::strings::ends_with(str, "+")) {
+  while ((styles & xtd::number_styles::allow_trailing_sign) == xtd::number_styles::allow_trailing_sign && str.rfind("+") + 1 == str.size()) {
     if (sign != 0) __throw_parse_argument_exception("String contains more than one sign");
-    str = xtd::strings::substring(str, 0, str.size()-1);
+    str = str.substr(0, str.size()-1);
     sign += 1;
   }
   
-  while ((styles & xtd::number_styles::allow_trailing_sign) == xtd::number_styles::allow_trailing_sign && xtd::strings::ends_with(str, "-")) {
+  while ((styles & xtd::number_styles::allow_trailing_sign) == xtd::number_styles::allow_trailing_sign && str.rfind("-") + 1 == str.size()) {
     if (sign != 0) __throw_parse_argument_exception("String contains more than one sign");
-    str = xtd::strings::substring(str, 0, str.size()-1);
+    str = str.substr(0, str.size()-1);
     sign -= 1;
   }
   
-  while ((styles & xtd::number_styles::allow_parentheses) == xtd::number_styles::allow_parentheses && xtd::strings::starts_with(str, "(") && xtd::strings::ends_with(str, ")")) {
-    str = xtd::strings::substring(str, 1, str.size()-2);
+  while ((styles & xtd::number_styles::allow_parentheses) == xtd::number_styles::allow_parentheses && str.find("(") == 0 && str.rfind(")") + 1 == str.size()) {
+    str = str.substr(1, str.size()-2);
     if (sign != 0) __throw_parse_argument_exception("String contains more than one sign");
     sign -= 1;
   }
@@ -65,27 +72,27 @@ inline int __parse_remove_signs(std::basic_string<char_t>& str, xtd::number_styl
 template <typename char_t>
 inline void __parse_check_valid_characters(const std::basic_string<char_t>& str, xtd::number_styles styles) {
   std::basic_string<char_t> valid_characters = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-  if ((styles & xtd::number_styles::allow_binary_specifier) == xtd::number_styles::allow_binary_specifier) valid_characters = xtd::strings::remove(valid_characters, 2);
-  if ((styles & xtd::number_styles::allow_octal_specifier) == xtd::number_styles::allow_octal_specifier) valid_characters = xtd::strings::remove(valid_characters, 7);
+  if ((styles & xtd::number_styles::allow_binary_specifier) == xtd::number_styles::allow_binary_specifier) valid_characters.erase(2);
+  if ((styles & xtd::number_styles::allow_octal_specifier) == xtd::number_styles::allow_octal_specifier) valid_characters.erase(7);
   if ((styles & xtd::number_styles::allow_hex_specifier) == xtd::number_styles::allow_hex_specifier) valid_characters += std::basic_string<char_t> {'A', 'B', 'C', 'D', 'E', 'F', 'a', 'b', 'c', 'd', 'e', 'f'};
   if ((styles & xtd::number_styles::allow_decimal_point) == xtd::number_styles::allow_decimal_point) valid_characters += std::basic_string<char_t> {std::use_facet<std::numpunct<char_t>>(std::locale()).decimal_point(), '.'};
   if ((styles & xtd::number_styles::allow_thousands) == xtd::number_styles::allow_thousands) valid_characters += std::use_facet<std::numpunct<char_t>>(std::locale()).thousands_sep();
   if ((styles & xtd::number_styles::allow_exponent) == xtd::number_styles::allow_exponent) valid_characters += std::basic_string<char_t> {'E', 'e', '+', '-'};
   
   for (auto c : str) {
-    if (xtd::strings::index_of(valid_characters, c) == std::basic_string<char_t>::npos)
+    if (valid_characters.find(c) == std::basic_string<char_t>::npos)
       __throw_parse_argument_exception("invalid character found");
   }
   
   if ((styles & xtd::number_styles::allow_decimal_point) == xtd::number_styles::allow_decimal_point) {
-    size_t index = xtd::strings::index_of(str, std::use_facet<std::numpunct<char_t>>(std::locale()).decimal_point());
-    if (index != std::basic_string<char_t>::npos && xtd::strings::index_of(str, std::use_facet<std::numpunct<char_t>>(std::locale()).decimal_point(), index + 1) != std::basic_string<char_t>::npos)
+    size_t index = str.find(std::use_facet<std::numpunct<char_t>>(std::locale()).decimal_point());
+    if (index != std::basic_string<char_t>::npos && str.find(std::use_facet<std::numpunct<char_t>>(std::locale()).decimal_point(), index + 1) != std::basic_string<char_t>::npos)
       __throw_parse_argument_exception("invalid character found");
   }
   
   if ((styles & xtd::number_styles::allow_thousands) == xtd::number_styles::allow_thousands) {
     size_t index = 1;
-    while((index = xtd::strings::index_of(str, std::use_facet<std::numpunct<char_t>>(std::locale()).thousands_sep(), index)) != std::basic_string<char_t>::npos) {
+    while((index = str.find(std::use_facet<std::numpunct<char_t>>(std::locale()).thousands_sep(), index)) != std::basic_string<char_t>::npos) {
       if (str[index - 1] == std::use_facet<std::numpunct<char_t>>(std::locale()).thousands_sep())
         __throw_parse_argument_exception("invalid character found");
       ++index;
@@ -93,7 +100,8 @@ inline void __parse_check_valid_characters(const std::basic_string<char_t>& str,
   }
   
   if ((styles & xtd::number_styles::allow_exponent) == xtd::number_styles::allow_exponent) {
-    size_t index = xtd::strings::index_of_any(str, std::vector<char_t> {'+', '-'});
+    size_t index = str.find('+');
+    if (index == std::basic_string<char_t>::npos) index = str.find('-');
     if (index != std::basic_string<char_t>::npos && str[index - 1] != 'e' && str[index - 1] != 'E')
       __throw_parse_argument_exception("invalid character found");
   }
