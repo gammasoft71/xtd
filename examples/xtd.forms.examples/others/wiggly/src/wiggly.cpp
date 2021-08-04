a3fcf1e85a0d10f530c5dd4d623fc6c8d90020d5
Gammasoft <gammasoft71@gmail.com>
2021-08-04 10:00:17 +0200

    Update wiggly.cpp

--- examples/xtd.forms.examples/others/wiggly/src/wiggly.cpp
+++ examples/xtd.forms.examples/others/wiggly/src/wiggly.cpp
@@ -24,7 +24,7 @@ namespace examples {
     void on_paint(paint_event_args& e) override {
       static const vector sins = {0, 38, 71, 92, 100, 92, 71, 38, 0, -38, -71, -92, -100, -92, -71, -38};
       auto pos = point {(e.clip_rectangle().size().width() - static_cast<int>(e.graphics().measure_string(text(), font()).width())) / 2, (e.clip_rectangle().size().height() - static_cast<int>(e.graphics().measure_string(text(), font()).height())) / 2};
-      for (auto i = 0u; i < wiggly_text.length(); i++) {
+      for (auto i = 0U; i < wiggly_text.length(); i++) {
         auto index = (step + i) % sins.size();
         e.graphics().draw_string(ustring::format("{}", wiggly_text[i]), font(), solid_brush {color::from_hsb(360.0f / sins.size() * index, 1.0f, 0.75f)}, point::subtract(pos, point(0, sins[index] * font().height() / 400)));
         pos.x(pos.x() + static_cast<int>(e.graphics().measure_string(ustring::format("{}", wiggly_text[i]), font()).width()));
