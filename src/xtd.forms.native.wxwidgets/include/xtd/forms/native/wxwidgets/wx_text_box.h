c0df229bfd0912e55f1ea1802b589d9da8cad07e
Gammasoft <gammasoft71@gmail.com>
2021-08-06 14:17:04 +0200

    using ustring

--- src/xtd.forms.native.wxwidgets/include/xtd/forms/native/wxwidgets/wx_text_box.h
+++ src/xtd.forms.native.wxwidgets/include/xtd/forms/native/wxwidgets/wx_text_box.h
@@ -10,7 +10,6 @@
 #include <xtd/drawing/system_colors.h>
 #include <xtd/forms/create_params.h>
 #include <xtd/forms/native/edit_styles.h>
-#include <xtd/strings.h>
 #include <wx/textctrl.h>
 #include "control_handler.h"
 
