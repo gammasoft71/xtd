5091bf20aa9d60c4905c8d9a54e277487427acd3
Gammasoft <gammasoft71@gmail.com>
2021-08-08 14:50:09 +0200

    Update socket_address.cpp

--- tests/xtd.core.unit_tests/src/net/socket_address.cpp
+++ tests/xtd.core.unit_tests/src/net/socket_address.cpp
@@ -101,7 +101,7 @@ namespace unit_tests {
     void test_method_(socket_address_to_string) {
       socket_address sa(address_family::inter_network);
       for (auto index = 2U; index < sa.size(); ++index)
-        sa[index] = index;
+        sa[index] = static_cast<byte_t>(index);
       assert::are_equal("inter_network:32:{2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31}", sa.to_string());
     }
   };
