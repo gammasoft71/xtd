610c2d66d059e1544e4fa329b6b6cece0e5419ad
Gammasoft <gammasoft71@gmail.com>
2021-08-11 14:23:43 +0200

    add exceptions and add unit tests

---
+++ src/xtd.core/include/xtd/net/protocol_violation_exception.h
@@ -0,0 +1,82 @@
+/// @file
+/// @brief Contains xtd::io::protocol_violation_exception exception.
+/// @copyright Copyright (c) 2021 Gammasoft. All rights reserved.
+#pragma once
+#include "../format_exception.h"
+
+/// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
+namespace xtd {
+  /// @brief The xtd::net namespace provides a simple programming interface for many of the protocols used on networks today. The xtd::net::web_request and xtd::net::web_response classes form the basis of what are called pluggable protocols, an implementation of network services that enables you to develop applications that use Internet resources without worrying about the specific details of the individual protocols.
+  namespace net {
+    /// @brief The exception that is thrown when an error is made adding a xtd::net::cookie to a xtd::net::cookie_container.
+    /// @par Library
+    /// xtd.core
+    /// @ingroup xtd_core exceptions
+    /// @par Examples
+    /// The following example demonstrates how to throw and catch an protocol_violation_exception.
+    /// @include protocol_violation_exception.cpp
+    class protocol_violation_exception : public format_exception {
+    public:
+      /// @brief Create a new instance of class protocol_violation_exception
+      /// @param information (optional) Conatains current information about memeber name, file path and  line number in the file where the exception is occurred. Typically #current_stack_frame_.
+      /// @remarks Message is set with the default message associate to the exception.
+      protocol_violation_exception(const xtd::diagnostics::stack_frame& info = xtd::diagnostics::stack_frame::empty()) : format_exception(default_message(), info) {}
+      /// @brief Create a new instance of class protocol_violation_exception
+      /// @param message Message string associate to the exception.
+      /// @param information (optional) Conatains current information about memeber name, file path and  line number in the file where the exception is occurred. Typically #current_stack_frame_.
+      explicit protocol_violation_exception(const xtd::ustring& message, const xtd::diagnostics::stack_frame& info = xtd::diagnostics::stack_frame::empty()) : format_exception(message, info) {}
+      /// @brief Create a new instance of class protocol_violation_exception
+      /// @param message Message string associate to the exception.
+      /// @param error Error code associate to the exception.
+      /// @param information (optional) Conatains current information about memeber name, file path and  line number in the file where the exception is occurred. Typically #current_stack_frame_.
+      explicit protocol_violation_exception(const xtd::ustring& message, const std::error_code& error, const xtd::diagnostics::stack_frame& info = xtd::diagnostics::stack_frame::empty()) : format_exception(message, error, info) {}
+      /// @brief Create a new instance of class protocol_violation_exception
+      /// @param message Message string associate to the exception.
+      /// @param help_link Help link string associate to the exception.
+      /// @param information (optional) Conatains current information about memeber name, file path and  line number in the file where the exception is occurred. Typically #current_stack_frame_.
+      explicit protocol_violation_exception(const xtd::ustring& message, const xtd::ustring& help_link, const xtd::diagnostics::stack_frame& info = xtd::diagnostics::stack_frame::empty()) : format_exception(message, help_link, info) {}
+      /// @brief Create a new instance of class protocol_violation_exception
+      /// @param message Message string associate to the exception.
+      /// @param error Error code associate to the exception.
+      /// @param help_link Help link string associate to the exception.
+      explicit protocol_violation_exception(const xtd::ustring& message, const std::error_code& error, const xtd::ustring& help_link, const xtd::diagnostics::stack_frame& info = xtd::diagnostics::stack_frame::empty()) : format_exception(message, error, help_link, info) {}
+      /// @brief Create a new instance of class protocol_violation_exception
+      /// @param inner_exception The exception that is the cause of the current exception.
+      /// @param information (optional) Conatains current information about memeber name, file path and  line number in the file where the exception is occurred. Typically #current_stack_frame_.
+      /// @remarks Message is set with the default message associate to the exception.
+      protocol_violation_exception(const std::exception& inner_exception, const xtd::diagnostics::stack_frame& info = xtd::diagnostics::stack_frame::empty()) : format_exception(default_message(), inner_exception, info) {}
+      /// @brief Create a new instance of class protocol_violation_exception
+      /// @param message Message string associate to the exception.
+      /// @param inner_exception The exception that is the cause of the current exception.
+      /// @param information (optional) Conatains current information about memeber name, file path and  line number in the file where the exception is occurred. Typically #current_stack_frame_.
+      explicit protocol_violation_exception(const xtd::ustring& message, const std::exception& inner_exception, const xtd::diagnostics::stack_frame& info = xtd::diagnostics::stack_frame::empty()) : format_exception(message, inner_exception, info) {}
+      /// @brief Create a new instance of class protocol_violation_exception
+      /// @param message Message string associate to the exception.
+      /// @param inner_exception The exception that is the cause of the current exception.
+      /// @param error Error code associate to the exception.
+      /// @param information (optional) Conatains current information about memeber name, file path and  line number in the file where the exception is occurred. Typically #current_stack_frame_.
+      explicit protocol_violation_exception(const xtd::ustring& message, const std::exception& inner_exception, const std::error_code& error, const xtd::diagnostics::stack_frame& info = xtd::diagnostics::stack_frame::empty()) : format_exception(message, inner_exception, error, info) {}
+      /// @brief Create a new instance of class protocol_violation_exception
+      /// @param message Message string associate to the exception.
+      /// @param inner_exception The exception that is the cause of the current exception.
+      /// @param help_link Help link string associate to the exception.
+      /// @param information (optional) Conatains current information about memeber name, file path and  line number in the file where the exception is occurred. Typically #current_stack_frame_.
+      explicit protocol_violation_exception(const xtd::ustring& message, const std::exception& inner_exception, const xtd::ustring& help_link, const xtd::diagnostics::stack_frame& info = xtd::diagnostics::stack_frame::empty()) : format_exception(message, inner_exception, help_link, info) {}
+      /// @brief Create a new instance of class protocol_violation_exception
+      /// @param message Message string associate to the exception.
+      /// @param inner_exception The exception that is the cause of the current exception.
+      /// @param error Error code associate to the exception.
+      /// @param help_link Help link string associate to the exception.
+      /// @param information (optional) Conatains current information about memeber name, file path and  line number in the file where the exception is occurred. Typically #current_stack_frame_.
+      explicit protocol_violation_exception(const xtd::ustring& message, const std::exception& inner_exception, const std::error_code& error, const xtd::ustring& help_link, const xtd::diagnostics::stack_frame& info = xtd::diagnostics::stack_frame::empty()) : format_exception(message, inner_exception, error, help_link, info) {}
+      
+      /// @cond
+      protocol_violation_exception(const protocol_violation_exception&) = default;
+      protocol_violation_exception& operator=(const protocol_violation_exception&) = default;
+      /// @endcond
+      
+    private:
+      const char* default_message() const {return "Operation is not valid due to the current state of the object."_t;}
+    };
+  }
+}
---
+++ src/xtd.core/include/xtd/net/web_exception.h
@@ -0,0 +1,82 @@
+/// @file
+/// @brief Contains xtd::io::web_exception exception.
+/// @copyright Copyright (c) 2021 Gammasoft. All rights reserved.
+#pragma once
+#include "../format_exception.h"
+
+/// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
+namespace xtd {
+  /// @brief The xtd::net namespace provides a simple programming interface for many of the protocols used on networks today. The xtd::net::web_request and xtd::net::web_response classes form the basis of what are called pluggable protocols, an implementation of network services that enables you to develop applications that use Internet resources without worrying about the specific details of the individual protocols.
+  namespace net {
+    /// @brief The exception that is thrown when an error is made adding a xtd::net::cookie to a xtd::net::cookie_container.
+    /// @par Library
+    /// xtd.core
+    /// @ingroup xtd_core exceptions
+    /// @par Examples
+    /// The following example demonstrates how to throw and catch an web_exception.
+    /// @include web_exception.cpp
+    class web_exception : public format_exception {
+    public:
+      /// @brief Create a new instance of class web_exception
+      /// @param information (optional) Conatains current information about memeber name, file path and  line number in the file where the exception is occurred. Typically #current_stack_frame_.
+      /// @remarks Message is set with the default message associate to the exception.
+      web_exception(const xtd::diagnostics::stack_frame& info = xtd::diagnostics::stack_frame::empty()) : format_exception(default_message(), info) {}
+      /// @brief Create a new instance of class web_exception
+      /// @param message Message string associate to the exception.
+      /// @param information (optional) Conatains current information about memeber name, file path and  line number in the file where the exception is occurred. Typically #current_stack_frame_.
+      explicit web_exception(const xtd::ustring& message, const xtd::diagnostics::stack_frame& info = xtd::diagnostics::stack_frame::empty()) : format_exception(message, info) {}
+      /// @brief Create a new instance of class web_exception
+      /// @param message Message string associate to the exception.
+      /// @param error Error code associate to the exception.
+      /// @param information (optional) Conatains current information about memeber name, file path and  line number in the file where the exception is occurred. Typically #current_stack_frame_.
+      explicit web_exception(const xtd::ustring& message, const std::error_code& error, const xtd::diagnostics::stack_frame& info = xtd::diagnostics::stack_frame::empty()) : format_exception(message, error, info) {}
+      /// @brief Create a new instance of class web_exception
+      /// @param message Message string associate to the exception.
+      /// @param help_link Help link string associate to the exception.
+      /// @param information (optional) Conatains current information about memeber name, file path and  line number in the file where the exception is occurred. Typically #current_stack_frame_.
+      explicit web_exception(const xtd::ustring& message, const xtd::ustring& help_link, const xtd::diagnostics::stack_frame& info = xtd::diagnostics::stack_frame::empty()) : format_exception(message, help_link, info) {}
+      /// @brief Create a new instance of class web_exception
+      /// @param message Message string associate to the exception.
+      /// @param error Error code associate to the exception.
+      /// @param help_link Help link string associate to the exception.
+      explicit web_exception(const xtd::ustring& message, const std::error_code& error, const xtd::ustring& help_link, const xtd::diagnostics::stack_frame& info = xtd::diagnostics::stack_frame::empty()) : format_exception(message, error, help_link, info) {}
+      /// @brief Create a new instance of class web_exception
+      /// @param inner_exception The exception that is the cause of the current exception.
+      /// @param information (optional) Conatains current information about memeber name, file path and  line number in the file where the exception is occurred. Typically #current_stack_frame_.
+      /// @remarks Message is set with the default message associate to the exception.
+      web_exception(const std::exception& inner_exception, const xtd::diagnostics::stack_frame& info = xtd::diagnostics::stack_frame::empty()) : format_exception(default_message(), inner_exception, info) {}
+      /// @brief Create a new instance of class web_exception
+      /// @param message Message string associate to the exception.
+      /// @param inner_exception The exception that is the cause of the current exception.
+      /// @param information (optional) Conatains current information about memeber name, file path and  line number in the file where the exception is occurred. Typically #current_stack_frame_.
+      explicit web_exception(const xtd::ustring& message, const std::exception& inner_exception, const xtd::diagnostics::stack_frame& info = xtd::diagnostics::stack_frame::empty()) : format_exception(message, inner_exception, info) {}
+      /// @brief Create a new instance of class web_exception
+      /// @param message Message string associate to the exception.
+      /// @param inner_exception The exception that is the cause of the current exception.
+      /// @param error Error code associate to the exception.
+      /// @param information (optional) Conatains current information about memeber name, file path and  line number in the file where the exception is occurred. Typically #current_stack_frame_.
+      explicit web_exception(const xtd::ustring& message, const std::exception& inner_exception, const std::error_code& error, const xtd::diagnostics::stack_frame& info = xtd::diagnostics::stack_frame::empty()) : format_exception(message, inner_exception, error, info) {}
+      /// @brief Create a new instance of class web_exception
+      /// @param message Message string associate to the exception.
+      /// @param inner_exception The exception that is the cause of the current exception.
+      /// @param help_link Help link string associate to the exception.
+      /// @param information (optional) Conatains current information about memeber name, file path and  line number in the file where the exception is occurred. Typically #current_stack_frame_.
+      explicit web_exception(const xtd::ustring& message, const std::exception& inner_exception, const xtd::ustring& help_link, const xtd::diagnostics::stack_frame& info = xtd::diagnostics::stack_frame::empty()) : format_exception(message, inner_exception, help_link, info) {}
+      /// @brief Create a new instance of class web_exception
+      /// @param message Message string associate to the exception.
+      /// @param inner_exception The exception that is the cause of the current exception.
+      /// @param error Error code associate to the exception.
+      /// @param help_link Help link string associate to the exception.
+      /// @param information (optional) Conatains current information about memeber name, file path and  line number in the file where the exception is occurred. Typically #current_stack_frame_.
+      explicit web_exception(const xtd::ustring& message, const std::exception& inner_exception, const std::error_code& error, const xtd::ustring& help_link, const xtd::diagnostics::stack_frame& info = xtd::diagnostics::stack_frame::empty()) : format_exception(message, inner_exception, error, help_link, info) {}
+      
+      /// @cond
+      web_exception(const web_exception&) = default;
+      web_exception& operator=(const web_exception&) = default;
+      /// @endcond
+      
+    private:
+      const char* default_message() const {return "Operation is not valid due to the current state of the object."_t;}
+    };
+  }
+}
