3fafa3c1c19c48607f4bc761768e2c988e275e7f
Gammasoft <gammasoft71@gmail.com>
2021-08-13 19:29:14 +0200

    Update cryptography.cpp

--- src/xtd.core.native.unix/src/xtd/native/unix/cryptography.cpp
+++ src/xtd.core.native.unix/src/xtd/native/unix/cryptography.cpp
@@ -30,13 +30,13 @@ vector<uint8_t> cryptography::machine_guid() {
   guid_str = guid_str.erase(guid_str.find("\n")).substr(guid_str.find("=") + 2);
 #else
   guid_str = create_process("{ uname -n ; cat /proc/meminfo | head -n1 ; cat /proc/cpuinfo ; } | md5sum");
-  guid_str = guid_str.erase(guid_str.find("\n"));
+  guid_str = guid_str.erase(guid_str.find("\n"), 2);
 #endif
   if (guid_str.empty())
     guid_str = "30395f0e-d6aa-4a5e-b4af-6f90a608c605"; // fallback
-  cout << "[" << guid_str << "]" << endl;
   for(auto index = 0U; guid_str[index] != 0; ++index)
     if (guid_str[index] == '-' || guid_str[index] == ' ' || guid_str[index] == '"') guid_str.erase(index, 1);
+  cout << "[" << guid_str << "]" << endl;
   for(auto index = 0U; index < guid_str.size(); index += 2)
     bytes.push_back(static_cast<uint8_t>(stoi(guid_str.substr(index, 2), 0, 16)));
   return bytes;
