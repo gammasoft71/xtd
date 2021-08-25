b01b60bea0415a3abe3afcd61bf2b30c7e9422d1
Gammasoft <gammasoft71@gmail.com>
2021-08-25 19:50:21 +0200

    Update dns_get_host_entry.cpp

--- examples/xtd.core.examples/network/dns_get_host_entry/src/dns_get_host_entry.cpp
+++ examples/xtd.core.examples/network/dns_get_host_entry/src/dns_get_host_entry.cpp
@@ -6,7 +6,7 @@ using namespace xtd::net;
 
 int main() {
   ip_host_entry host_entry = dns::get_host_entry("www.github.com");
-  console::write_line("ip host entry for : github.com");
+  console::write_line("ip host entry for : www.github.com");
   console::write_line("  - host_name : {}", host_entry.host_name());
   console::write_line("  - address list :");
   for(auto address : host_entry.address_list())
@@ -18,7 +18,7 @@ int main() {
 
 // This code produces the following output:
 //
-// ip host entry for : github.com
+// ip host entry for : www.github.com
 //   - host_name : github.com
 //   - address list :
 //     * 140.82.121.3
