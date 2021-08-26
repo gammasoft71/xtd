4f21a5a2f46414c5957fdcb993c7abb08ad2eb14
Gammasoft <gammasoft71@gmail.com>
2021-08-26 22:25:22 +0200

    review examples

--- examples/xtd.core.examples/network/socket_tcp_ip_v6/src/socket_tcp_ip_v6.cpp
+++ examples/xtd.core.examples/network/socket_tcp_ip_v6/src/socket_tcp_ip_v6.cpp
@@ -7,7 +7,7 @@ using namespace xtd::net;
 using namespace xtd::net::sockets;
 
 int main() {
-  bool terminate = false;
+  bool terminate_app = false;
   
   thread server([&] {
     socket server_socket(address_family::inter_network_v6, socket_type::stream, protocol_type::tcp);
@@ -15,7 +15,7 @@ int main() {
     server_socket.listen();
     socket new_socket = server_socket.accept();
 
-    while (!terminate) {
+    while (!terminate_app) {
       vector<byte_t> buffer(256);
       size_t number_of_byte_received = new_socket.receive(buffer);
       if (number_of_byte_received) console::write_line(ustring(buffer.begin(), buffer.begin() + number_of_byte_received));
@@ -27,7 +27,7 @@ int main() {
     client_socket.connect(ip_address::ip_v6_loopback, 9400);
     
     int counter = 1;
-    while (!terminate) {
+    while (!terminate_app) {
       ustring str = ustring::format("counter={}", counter++);
       client_socket.send(vector<byte_t>(str.begin(), str.end()));
       this_thread::sleep_for(50ms);
@@ -35,7 +35,7 @@ int main() {
   });
 
   console::read_key();
-  terminate = true;
+  terminate_app = true;
   server.join();
   client.join();
 }
