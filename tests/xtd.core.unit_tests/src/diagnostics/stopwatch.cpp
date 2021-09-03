f56ef1a073875df2d38c45abb934932351533799
Gammasoft <gammasoft71@gmail.com>
2021-09-03 12:56:17 +0200

    update unit tests

--- tests/xtd.core.unit_tests/src/diagnostics/stopwatch.cpp
+++ tests/xtd.core.unit_tests/src/diagnostics/stopwatch.cpp
@@ -14,22 +14,22 @@ namespace unit_tests {
     void test_method_(create_stopwatch) {
       stopwatch s;
       sleep_for(1ms);
-      assert::is_false_(s.is_running());
-      assert::are_equal_(0ns, s.elapsed());
-      assert::are_equal_(0, s.elapsed_milliseconds());
-      assert::are_equal_(0, s.elapsed_nanoseconds());
-      assert::are_equal_(0, s.elapsed_ticks());
+      assert::is_false(s.is_running(), csf_);
+      assert::are_equal(0ns, s.elapsed(), csf_);
+      assert::are_equal(0, s.elapsed_milliseconds(), csf_);
+      assert::are_equal(0, s.elapsed_nanoseconds(), csf_);
+      assert::are_equal(0, s.elapsed_ticks(), csf_);
     }
 
     void test_method_(start_stopwatch) {
       stopwatch s;
       s.start();
       sleep_for(1ms);
-      assert::is_true_(s.is_running());
-      assert::is_greater_or_equal_(s.elapsed(), 1000000ns);
-      assert::is_greater_or_equal_(s.elapsed_milliseconds(), 1);
-      assert::is_greater_or_equal_(s.elapsed_nanoseconds(), 1000000);
-      assert::is_greater_or_equal_(s.elapsed_ticks(), 10000);
+      assert::is_true(s.is_running(), csf_);
+      assert::is_greater_or_equal(s.elapsed(), 1000000ns, csf_);
+      assert::is_greater_or_equal(s.elapsed_milliseconds(), 1, csf_);
+      assert::is_greater_or_equal(s.elapsed_nanoseconds(), 1000000, csf_);
+      assert::is_greater_or_equal(s.elapsed_ticks(), 10000, csf_);
     }
 
     void test_method_(start_start_stopwatch) {
@@ -37,22 +37,22 @@ namespace unit_tests {
       s.start();
       sleep_for(1ms);
       s.start();
-      assert::is_true_(s.is_running());
-      assert::is_greater_or_equal_(s.elapsed(), 1000000ns);
-      assert::is_greater_or_equal_(s.elapsed_milliseconds(), 1);
-      assert::is_greater_or_equal_(s.elapsed_nanoseconds(), 1000000);
-      assert::is_greater_or_equal_(s.elapsed_ticks(), 10000);
+      assert::is_true(s.is_running(), csf_);
+      assert::is_greater_or_equal(s.elapsed(), 1000000ns, csf_);
+      assert::is_greater_or_equal(s.elapsed_milliseconds(), 1, csf_);
+      assert::is_greater_or_equal(s.elapsed_nanoseconds(), 1000000, csf_);
+      assert::is_greater_or_equal(s.elapsed_ticks(), 10000, csf_);
     }
 
     void test_method_(stop_without_start_stopwatch) {
       stopwatch s;
       s.stop();
       sleep_for(1ms);
-      assert::is_false_(s.is_running());
-      assert::are_equal_(0ns, s.elapsed());
-      assert::are_equal_(0, s.elapsed_milliseconds());
-      assert::are_equal_(0, s.elapsed_nanoseconds());
-      assert::are_equal_(0, s.elapsed_ticks());
+      assert::is_false(s.is_running(), csf_);
+      assert::are_equal(0ns, s.elapsed(), csf_);
+      assert::are_equal(0, s.elapsed_milliseconds(), csf_);
+      assert::are_equal(0, s.elapsed_nanoseconds(), csf_);
+      assert::are_equal(0, s.elapsed_ticks(), csf_);
     }
 
     void test_method_(start_stop_stopwatch) {
@@ -60,11 +60,11 @@ namespace unit_tests {
       s.start();
       sleep_for(1ms);
       s.stop();
-      assert::is_false_(s.is_running());
-      assert::is_greater_or_equal_(s.elapsed(), 1000000ns);
-      assert::is_greater_or_equal_(s.elapsed_milliseconds(), 1);
-      assert::is_greater_or_equal_(s.elapsed_nanoseconds(), 1000000);
-      assert::is_greater_or_equal_(s.elapsed_ticks(), 10000);
+      assert::is_false(s.is_running(), csf_);
+      assert::is_greater_or_equal(s.elapsed(), 1000000ns, csf_);
+      assert::is_greater_or_equal(s.elapsed_milliseconds(), 1, csf_);
+      assert::is_greater_or_equal(s.elapsed_nanoseconds(), 1000000, csf_);
+      assert::is_greater_or_equal(s.elapsed_ticks(), 10000, csf_);
     }
 
     void test_method_(start_stop_reset_stopwatch) {
@@ -73,11 +73,11 @@ namespace unit_tests {
       sleep_for(1ms);
       s.stop();
       s.reset();
-      assert::is_false_(s.is_running());
-      assert::are_equal_(0ns, s.elapsed());
-      assert::are_equal_(0, s.elapsed_milliseconds());
-      assert::are_equal_(0, s.elapsed_nanoseconds());
-      assert::are_equal_(0, s.elapsed_ticks());
+      assert::is_false(s.is_running(), csf_);
+      assert::are_equal(0ns, s.elapsed(), csf_);
+      assert::are_equal(0, s.elapsed_milliseconds(), csf_);
+      assert::are_equal(0, s.elapsed_nanoseconds(), csf_);
+      assert::are_equal(0, s.elapsed_ticks(), csf_);
     }
 
     void test_method_(start_reset_stopwatch) {
@@ -85,42 +85,42 @@ namespace unit_tests {
       s.start();
       sleep_for(1ms);
       s.reset();
-      assert::is_false_(s.is_running());
-      assert::are_equal_(0ns, s.elapsed());
-      assert::are_equal_(0, s.elapsed_milliseconds());
-      assert::are_equal_(0, s.elapsed_nanoseconds());
-      assert::are_equal_(0, s.elapsed_ticks());
+      assert::is_false(s.is_running(), csf_);
+      assert::are_equal(0ns, s.elapsed(), csf_);
+      assert::are_equal(0, s.elapsed_milliseconds(), csf_);
+      assert::are_equal(0, s.elapsed_nanoseconds(), csf_);
+      assert::are_equal(0, s.elapsed_ticks(), csf_);
     }
 
     void test_method_(reset_stopwatch) {
       stopwatch s;
       s.reset();
-      assert::is_false_(s.is_running());
-      assert::are_equal_(0ns, s.elapsed());
-      assert::are_equal_(0, s.elapsed_milliseconds());
-      assert::are_equal_(0, s.elapsed_nanoseconds());
-      assert::are_equal_(0, s.elapsed_ticks());
+      assert::is_false(s.is_running(), csf_);
+      assert::are_equal(0ns, s.elapsed(), csf_);
+      assert::are_equal(0, s.elapsed_milliseconds(), csf_);
+      assert::are_equal(0, s.elapsed_nanoseconds(), csf_);
+      assert::are_equal(0, s.elapsed_ticks(), csf_);
     }
 
     void test_method_(restart_stopwatch) {
       stopwatch s;
       s.restart();
       sleep_for(1ms);
-      assert::is_true_(s.is_running());
-      assert::is_greater_or_equal_(s.elapsed(), 1000000ns);
-      assert::is_greater_or_equal_(s.elapsed_milliseconds(), 1);
-      assert::is_greater_or_equal_(s.elapsed_nanoseconds(), 1000000);
-      assert::is_greater_or_equal_(s.elapsed_ticks(), 10000);
+      assert::is_true(s.is_running(), csf_);
+      assert::is_greater_or_equal(s.elapsed(), 1000000ns, csf_);
+      assert::is_greater_or_equal(s.elapsed_milliseconds(), 1, csf_);
+      assert::is_greater_or_equal(s.elapsed_nanoseconds(), 1000000, csf_);
+      assert::is_greater_or_equal(s.elapsed_ticks(), 10000, csf_);
     }
 
     void test_method_(start_new_stopwatch) {
       auto s = stopwatch::start_new();
       sleep_for(1ms);
-      assert::is_true_(s.is_running());
-      assert::is_greater_or_equal_(s.elapsed(), 1000000ns);
-      assert::is_greater_or_equal_(s.elapsed_milliseconds(), 1);
-      assert::is_greater_or_equal_(s.elapsed_nanoseconds(), 1000000);
-      assert::is_greater_or_equal_(s.elapsed_ticks(), 10000);
+      assert::is_true(s.is_running(), csf_);
+      assert::is_greater_or_equal(s.elapsed(), 1000000ns, csf_);
+      assert::is_greater_or_equal(s.elapsed_milliseconds(), 1, csf_);
+      assert::is_greater_or_equal(s.elapsed_nanoseconds(), 1000000, csf_);
+      assert::is_greater_or_equal(s.elapsed_ticks(), 10000, csf_);
     }
 
     void test_method_(cumulative_elapsed_stopwatch) {
@@ -129,17 +129,17 @@ namespace unit_tests {
       sleep_for(1ms);
       s.stop();
       auto elapsed_1 = s.elapsed_milliseconds();
-      assert::is_greater_or_equal_(elapsed_1, 1);
+      assert::is_greater_or_equal(elapsed_1, 1, csf_);
       s.start();
       sleep_for(1ms);
       s.stop();
       auto elapsed_2 = s.elapsed_milliseconds();
-      assert::is_greater(elapsed_2, elapsed_1);
+      assert::is_greater(elapsed_2, elapsed_1, csf_);
       s.start();
       sleep_for(1ms);
       s.stop();
       auto elapsed_3 = s.elapsed_milliseconds();
-      assert::is_greater(elapsed_3, elapsed_2);
+      assert::is_greater(elapsed_3, elapsed_2, csf_);
     }
   };
 }
--- tests/xtd.core.unit_tests/src/io/path.cpp
+++ tests/xtd.core.unit_tests/src/io/path.cpp
@@ -10,119 +10,119 @@ namespace unit_tests {
   class test_class_(test_path) {
   public:
     void test_method_(alt_directory_separator_char) {
-      assert::are_equal('/', path::alt_directory_separator_char());
+      assert::are_equal('/', path::alt_directory_separator_char(), csf_);
     }
 
     void test_method_(change_extension) {
       auto file_name = "file.txt";
-      assert::are_equal("file.bak", path::change_extension(file_name, ".bak"));
+      assert::are_equal("file.bak", path::change_extension(file_name, ".bak"), csf_);
     }
 
     void test_method_(change_extension_without_point) {
       auto file_name = "file.txt";
-      assert::are_equal("file.bak", path::change_extension(file_name, "bak"));
+      assert::are_equal("file.bak", path::change_extension(file_name, "bak"), csf_);
     }
 
     void test_method_(change_extension_with_strange_extension) {
       auto file_name = "file.txt";
-      assert::are_equal("file.backup.bak", path::change_extension(file_name, ".backup.bak"));
+      assert::are_equal("file.backup.bak", path::change_extension(file_name, ".backup.bak"), csf_);
     }
 
     void test_method_(change_extension_source_without_extension) {
       auto file_name = "file";
-      assert::are_equal("file.bak", path::change_extension(file_name, ".bak"));
+      assert::are_equal("file.bak", path::change_extension(file_name, ".bak"), csf_);
     }
 
     void test_method_(change_extension_with_empty_extension) {
       auto file_name = "file.com";
-      assert::are_equal("file", path::change_extension(file_name, ""));
+      assert::are_equal("file", path::change_extension(file_name, ""), csf_);
     }
 
     void test_method_(change_extension_with_only_point_extension) {
       auto file_name = "file.com";
-      assert::are_equal("file.", path::change_extension(file_name, "."));
+      assert::are_equal("file.", path::change_extension(file_name, "."), csf_);
     }
 
     void test_method_(change_extension_without_source) {
       auto file_name = ".com";
-      assert::are_equal(".bak", path::change_extension(file_name, ".bak"));
+      assert::are_equal(".bak", path::change_extension(file_name, ".bak"), csf_);
     }
 
     void test_method_(change_extension_wit_empty_source) {
       auto file_name = "";
-      assert::are_equal(".bak", path::change_extension(file_name, ".bak"));
+      assert::are_equal(".bak", path::change_extension(file_name, ".bak"), csf_);
     }
     
     void test_method_(combine_two_paths) {
-      assert::are_equal("path1"_s + path::directory_separator_char() + "path2"_s , path::combine("path1", "path2"));
+      assert::are_equal("path1"_s + path::directory_separator_char() + "path2"_s , path::combine("path1", "path2"), csf_);
     }
     
     void test_method_(combine_three_paths) {
-      assert::are_equal("path1"_s + path::directory_separator_char() + "path2"_s + path::directory_separator_char() + "path3"_s, path::combine("path1", "path2", "path3"));
+      assert::are_equal("path1"_s + path::directory_separator_char() + "path2"_s + path::directory_separator_char() + "path3"_s, path::combine("path1", "path2", "path3"), csf_);
     }
     
     void test_method_(combine_four_paths) {
-      assert::are_equal("path1"_s + path::directory_separator_char() + "path2"_s + path::directory_separator_char() + "path3"_s + path::directory_separator_char() + "path4"_s, path::combine("path1", "path2", "path3", "path4"));
+      assert::are_equal("path1"_s + path::directory_separator_char() + "path2"_s + path::directory_separator_char() + "path3"_s + path::directory_separator_char() + "path4"_s, path::combine("path1", "path2", "path3", "path4"), csf_);
     }
     
     void test_method_(combine_n_paths) {
-      assert::are_equal("path1"_s + path::directory_separator_char() + "path2"_s + path::directory_separator_char() + "path3"_s + path::directory_separator_char() + "path4"_s + path::directory_separator_char() + "path5"_s, path::combine({"path1", "path2", "path3", "path4", "path5"}));
+      assert::are_equal("path1"_s + path::directory_separator_char() + "path2"_s + path::directory_separator_char() + "path3"_s + path::directory_separator_char() + "path4"_s + path::directory_separator_char() + "path5"_s, path::combine({"path1", "path2", "path3", "path4", "path5"}), csf_);
     }
 
     void test_method_(directory_separator_char) {
 #if defined(_WIN32)
-      assert::are_equal('\\', path::directory_separator_char());
+      assert::are_equal('\\', path::directory_separator_char(), csf_);
 #else
-      assert::are_equal('/', path::directory_separator_char());
+      assert::are_equal('/', path::directory_separator_char(), csf_);
 #endif
     }
     
     void test_method_(get_directory_name) {
-      assert::are_equal("path1/path2", path::get_directory_name("path1/path2/file.ext"));
+      assert::are_equal("path1/path2", path::get_directory_name("path1/path2/file.ext"), csf_);
     }
 
     void test_method_(get_directory_name_without_extension) {
-      assert::are_equal("path1/path2", path::get_directory_name("path1/path2/file"));
+      assert::are_equal("path1/path2", path::get_directory_name("path1/path2/file"), csf_);
     }
     
     void test_method_(get_directory_name_without_file) {
-      assert::are_equal("path1/path2", path::get_directory_name("path1/path2/"));
+      assert::are_equal("path1/path2", path::get_directory_name("path1/path2/"), csf_);
     }
     
     void test_method_(get_directory_name_without_path) {
-      assert::are_equal("", path::get_directory_name("file.ext"));
+      assert::are_equal("", path::get_directory_name("file.ext"), csf_);
     }
     
     void test_method_(get_directory_name_without_path_and_without_extension) {
-      assert::are_equal("", path::get_directory_name("file"));
+      assert::are_equal("", path::get_directory_name("file"), csf_);
     }
     
     void test_method_(get_directory_name_with_empty_source) {
-      assert::are_equal("", path::get_directory_name(""));
+      assert::are_equal("", path::get_directory_name(""), csf_);
     }
     
     void test_method_(get_extension) {
-      assert::are_equal(".ext", path::get_extension("path1/path2/file.ext"));
+      assert::are_equal(".ext", path::get_extension("path1/path2/file.ext"), csf_);
     }
     
     void test_method_(get_extension_without_path) {
-      assert::are_equal(".ext", path::get_extension("file.ext"));
+      assert::are_equal(".ext", path::get_extension("file.ext"), csf_);
     }
 
     void test_method_(get_extension_without_extension) {
-      assert::are_equal("", path::get_extension("path1/path2/file"));
+      assert::are_equal("", path::get_extension("path1/path2/file"), csf_);
     }
     
     void test_method_(get_extension_without_file_name) {
-      assert::are_equal(".ext", path::get_extension("path1/path2/.ext"));
+      assert::are_equal(".ext", path::get_extension("path1/path2/.ext"), csf_);
     }
     
     void test_method_(get_extension_without_file) {
-      assert::are_equal("", path::get_extension("path1/path2/"));
+      assert::are_equal("", path::get_extension("path1/path2/"), csf_);
     }
     
     void test_method_(get_extension_with_empty_source) {
-      assert::are_equal("", path::get_extension(""));
+      assert::are_equal("", path::get_extension(""), csf_);
     }
   };
 }
