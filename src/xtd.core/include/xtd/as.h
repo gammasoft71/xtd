f8509d409b010462f7e54d9b07dec306f1d248db
Gammasoft <gammasoft71@gmail.com>
2021-09-08 16:37:30 +0200

    add as<byte_t>(...)

--- src/xtd.core/include/xtd/as.h
+++ src/xtd.core/include/xtd/as.h
@@ -1350,6 +1350,676 @@ namespace xtd {
   inline bool as<bool>(wchar_t* value) {
     return xtd::convert::to_boolean(value);
   }
+  
+  /// @brief Casts a type into another type.
+  /// @param value object to convert.
+  /// @return A new byte_t object converted from value.
+  /// @exception xtd::invalid_cast_exception the parameters is bad cast.
+  /// @par Namespace
+  /// xtd
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core
+  /// @par Examples
+  /// @code
+  /// std::any value = true;
+  /// byte_t result = as<byte_t>(value);
+  /// @endcode
+  template<>
+  inline byte_t as<byte_t>(std::any value) {
+    return xtd::convert::to_byte(value);
+  }
+  
+  /// @brief Casts a type into another type.
+  /// @param value object to convert.
+  /// @return A new byte_t object converted from value.
+  /// @par Namespace
+  /// xtd
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core
+  /// @par Examples
+  /// @code
+  /// bool value = true;
+  /// byte_t result = as<byte_t>(value);
+  /// @endcode
+  template<>
+  inline byte_t as<byte_t>(bool value) {
+    return xtd::convert::to_byte(value);
+  }
+  
+  /// @brief Casts a type into another type.
+  /// @param value object to convert.
+  /// @return A new byte_t object converted from value.
+  /// @par Namespace
+  /// xtd
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core
+  /// @par Examples
+  /// @code
+  /// byte_t value = 42;
+  /// byte_t result = as<byte_t>(value);
+  /// @endcode
+  template<>
+  inline byte_t as<byte_t>(byte_t value) {
+    return xtd::convert::to_byte(value);
+  }
+  
+  /// @brief Casts a type into another type.
+  /// @param value object to convert.
+  /// @return A new byte_t object converted from value.
+  /// @exception xtd::invalid_cast_exception throws always exception.
+  /// @par Namespace
+  /// xtd
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core
+  /// @par Examples
+  /// @code
+  /// char value = 'a';
+  /// byte_t result = as<byte_t>(value);
+  /// @endcode
+  template<>
+  inline byte_t as<byte_t>(char value) {
+    return xtd::convert::to_byte(value);
+  }
+  
+  /// @brief Casts a type into another type.
+  /// @param value object to convert.
+  /// @return A new byte_t object converted from value.
+  /// @exception xtd::invalid_cast_exception throws always exception.
+  /// @par Namespace
+  /// xtd
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core
+  /// @par Examples
+  /// @code
+  /// char8_t value = u8'a';
+  /// byte_t result = as<byte_t>(value);
+  /// @endcode
+  template<>
+  inline byte_t as<byte_t>(char8_t value) {
+    return xtd::convert::to_byte(value);
+  }
+  
+  /// @brief Casts a type into another type.
+  /// @param value object to convert.
+  /// @return A new byte_t object converted from value.
+  /// @exception xtd::invalid_cast_exception throws always exception.
+  /// @par Namespace
+  /// xtd
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core
+  /// @par Examples
+  /// @code
+  /// char16_t value = u'a';
+  /// byte_t result = as<byte_t>(value);
+  /// @endcode
+  template<>
+  inline byte_t as<byte_t>(char16_t value) {
+    return xtd::convert::to_byte(value);
+  }
+  
+  /// @brief Casts a type into another type.
+  /// @param value object to convert.
+  /// @return A new byte_t object converted from value.
+  /// @exception xtd::invalid_cast_exception throws always exception.
+  /// @par Namespace
+  /// xtd
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core
+  /// @par Examples
+  /// @code
+  /// char32_t value = u'a';
+  /// byte_t result = as<byte_t>(value);
+  /// @endcode
+  template<>
+  inline byte_t as<byte_t>(char32_t value) {
+    return xtd::convert::to_byte(value);
+  }
+  
+  /// @brief Casts a type into another type.
+  /// @param value object to convert.
+  /// @return A new byte_t object converted from value.
+  /// @exception xtd::invalid_cast_exception throws always exception.
+  /// @par Namespace
+  /// xtd
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core
+  /// @par Examples
+  /// @code
+  /// wchar_t value = u'a';
+  /// byte_t result = as<byte_t>(value);
+  /// @endcode
+  template<>
+  inline byte_t as<byte_t>(wchar_t value) {
+    return xtd::convert::to_byte(value);
+  }
+  
+  /// @brief Casts a type into another type.
+  /// @param value object to convert.
+  /// @return A new byte_t object converted from value.
+  /// @par Namespace
+  /// xtd
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core
+  /// @par Examples
+  /// @code
+  /// decimal_t value = 42.50l;
+  /// byte_t result = as<byte_t>(value);
+  /// @endcode
+  template<>
+  inline byte_t as<byte_t>(decimal_t value) {
+    return xtd::convert::to_byte(value);
+  }
+  
+  /// @brief Casts a type into another type.
+  /// @param value object to convert.
+  /// @return A new byte_t object converted from value.
+  /// @par Namespace
+  /// xtd
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core
+  /// @par Examples
+  /// @code
+  /// double value = 42.50;
+  /// byte_t result = as<byte_t>(value);
+  /// @endcode
+  template<>
+  inline byte_t as<byte_t>(double value) {
+    return xtd::convert::to_byte(value);
+  }
+  
+  /// @brief Casts a type into another type.
+  /// @param value object to convert.
+  /// @return A new byte_t object converted from value.
+  /// @par Namespace
+  /// xtd
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core
+  /// @par Examples
+  /// @code
+  /// float value = 42.50f;
+  /// byte_t result = as<byte_t>(value);
+  /// @endcode
+  template<>
+  inline byte_t as<byte_t>(float value) {
+    return xtd::convert::to_byte(value);
+  }
+  
+  /// @brief Casts a type into another type.
+  /// @param value object to convert.
+  /// @return A new byte_t object converted from value.
+  /// @par Namespace
+  /// xtd
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core
+  /// @par Examples
+  /// @code
+  /// int16_t value = 42;
+  /// byte_t result = as<byte_t>(value);
+  /// @endcode
+  template<>
+  inline byte_t as<byte_t>(int16_t value) {
+    return xtd::convert::to_byte(value);
+  }
+  
+  /// @brief Casts a type into another type.
+  /// @param value object to convert.
+  /// @return A new byte_t object converted from value.
+  /// @par Namespace
+  /// xtd
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core
+  /// @par Examples
+  /// @code
+  /// int32_t value = 42;
+  /// byte_t result = as<byte_t>(value);
+  /// @endcode
+  template<>
+  inline byte_t as<byte_t>(int32_t value) {
+    return xtd::convert::to_byte(value);
+  }
+  
+  /// @brief Casts a type into another type.
+  /// @param value object to convert.
+  /// @return A new byte_t object converted from value.
+  /// @par Namespace
+  /// xtd
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core
+  /// @par Examples
+  /// @code
+  /// int64_t value = 42l;
+  /// byte_t result = as<byte_t>(value);
+  /// @endcode
+  template<>
+  inline byte_t as<byte_t>(int64_t value) {
+    return xtd::convert::to_byte(value);
+  }
+  
+  /// @brief Casts a type into another type.
+  /// @param value object to convert.
+  /// @return A new byte_t object converted from value.
+  /// @par Namespace
+  /// xtd
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core
+  /// @par Examples
+  /// @code
+  /// llong_t value = 42ll;
+  /// byte_t result = as<byte_t>(value);
+  /// @endcode
+  template<>
+  inline byte_t as<byte_t>(llong_t value) {
+    return xtd::convert::to_byte(value);
+  }
+  
+  /// @brief Casts a type into another type.
+  /// @param value object to convert.
+  /// @return A new byte_t object converted from value.
+  /// @par Namespace
+  /// xtd
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core
+  /// @par Examples
+  /// @code
+  /// sbyte_t value = 42ll;
+  /// byte_t result = as<byte_t>(value);
+  /// @endcode
+  template<>
+  inline byte_t as<byte_t>(sbyte_t value) {
+    return xtd::convert::to_byte(value);
+  }
+  
+  /// @brief Casts a type into another type.
+  /// @param value object to convert.
+  /// @return A new byte_t object converted from value.
+  /// @par Namespace
+  /// xtd
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core
+  /// @par Examples
+  /// @code
+  /// uint16_t value = 42u;
+  /// byte_t result = as<byte_t>(value);
+  /// @endcode
+  template<>
+  inline byte_t as<byte_t>(uint16_t value) {
+    return xtd::convert::to_byte(value);
+  }
+  
+  /// @brief Casts a type into another type.
+  /// @param value object to convert.
+  /// @return A new byte_t object converted from value.
+  /// @par Namespace
+  /// xtd
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core
+  /// @par Examples
+  /// @code
+  /// uint32_t value = 42u;
+  /// byte_t result = as<byte_t>(value);
+  /// @endcode
+  template<>
+  inline byte_t as<byte_t>(uint32_t value) {
+    return xtd::convert::to_byte(value);
+  }
+  
+  /// @brief Casts a type into another type.
+  /// @param value object to convert.
+  /// @return A new byte_t object converted from value.
+  /// @par Namespace
+  /// xtd
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core
+  /// @par Examples
+  /// @code
+  /// uint64_t value = 42ul;
+  /// byte_t result = as<byte_t>(value);
+  /// @endcode
+  template<>
+  inline byte_t as<byte_t>(uint64_t value) {
+    return xtd::convert::to_byte(value);
+  }
+  
+  /// @brief Casts a type into another type.
+  /// @param value object to convert.
+  /// @return A new byte_t object converted from value.
+  /// @par Namespace
+  /// xtd
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core
+  /// @par Examples
+  /// @code
+  /// ullong_t value = 42ull;
+  /// byte_t result = as<byte_t>(value);
+  /// @endcode
+  template<>
+  inline byte_t as<byte_t>(ullong_t value) {
+    return xtd::convert::to_byte(value);
+  }
+  
+  /// @brief Casts a type into another type.
+  /// @param value object to convert.
+  /// @return A new byte_t object converted from value.
+  /// @exception xtd::aegument_exception value does not represent a bool.
+  /// @par Namespace
+  /// xtd
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core
+  /// @par Examples
+  /// @code
+  /// ustring value = "42";
+  /// byte_t result = as<byte_t>(value);
+  /// @endcode
+  template<>
+  inline byte_t as<byte_t>(const xtd::ustring& value) {
+    return xtd::convert::to_byte(value);
+  }
+  
+  /// @brief Casts a type into another type.
+  /// @param value object to convert.
+  /// @return A new byte_t object converted from value.
+  /// @exception xtd::aegument_exception value does not represent a bool.
+  /// @par Namespace
+  /// xtd
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core
+  /// @par Examples
+  /// @code
+  /// string value = "42";
+  /// byte_t result = as<byte_t>(value);
+  /// @endcode
+  template<>
+  inline byte_t as<byte_t>(const std::string& value) {
+    return xtd::convert::to_byte(value);
+  }
+  
+  /// @brief Casts a type into another type.
+  /// @param value object to convert.
+  /// @return A new byte_t object converted from value.
+  /// @exception xtd::aegument_exception value does not represent a bool.
+  /// @par Namespace
+  /// xtd
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core
+  /// @par Examples
+  /// @code
+  /// u8string value = u8"42";
+  /// byte_t result = as<byte_t>(value);
+  /// @endcode
+  template<>
+  inline byte_t as<byte_t>(const std::u8string& value) {
+    return xtd::convert::to_byte(value);
+  }
+  
+  /// @brief Casts a type into another type.
+  /// @param value object to convert.
+  /// @return A new byte_t object converted from value.
+  /// @exception xtd::aegument_exception value does not represent a bool.
+  /// @par Namespace
+  /// xtd
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core
+  /// @par Examples
+  /// @code
+  /// u16string value = u"42";
+  /// byte_t result = as<byte_t>(value);
+  /// @endcode
+  template<>
+  inline byte_t as<byte_t>(const std::u16string& value) {
+    return xtd::convert::to_byte(value);
+  }
+  
+  /// @brief Casts a type into another type.
+  /// @param value object to convert.
+  /// @return A new byte_t object converted from value.
+  /// @exception xtd::aegument_exception value does not represent a bool.
+  /// @par Namespace
+  /// xtd
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core
+  /// @par Examples
+  /// @code
+  /// u32string value = U"42";
+  /// byte_t result = as<byte_t>(value);
+  /// @endcode
+  template<>
+  inline byte_t as<byte_t>(const std::u32string& value) {
+    return xtd::convert::to_byte(value);
+  }
+  
+  /// @brief Casts a type into another type.
+  /// @param value object to convert.
+  /// @return A new byte_t object converted from value.
+  /// @exception xtd::aegument_exception value does not represent a bool.
+  /// @par Namespace
+  /// xtd
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core
+  /// @par Examples
+  /// @code
+  /// wstring value = L"42";
+  /// byte_t result = as<byte_t>(value);
+  /// @endcode
+  template<>
+  inline byte_t as<byte_t>(const std::wstring& value) {
+    return xtd::convert::to_byte(value);
+  }
+  
+  /// @brief Casts a type into another type.
+  /// @param value object to convert.
+  /// @return A new byte_t object converted from value.
+  /// @exception xtd::aegument_exception value does not represent a bool.
+  /// @par Namespace
+  /// xtd
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core
+  /// @par Examples
+  /// @code
+  /// const char* value = "42";
+  /// byte_t result = as<byte_t>(value);
+  /// @endcode
+  template<>
+  inline byte_t as<byte_t>(const char* value) {
+    return xtd::convert::to_byte(value);
+  }
+  
+  /// @brief Casts a type into another type.
+  /// @param value object to convert.
+  /// @return A new byte_t object converted from value.
+  /// @exception xtd::aegument_exception value does not represent a bool.
+  /// @par Namespace
+  /// xtd
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core
+  /// @par Examples
+  /// @code
+  /// char* value = "42";
+  /// byte_t result = as<byte_t>(value);
+  /// @endcode
+  template<>
+  inline byte_t as<byte_t>(char* value) {
+    return xtd::convert::to_byte(value);
+  }
+  
+  /// @brief Casts a type into another type.
+  /// @param value object to convert.
+  /// @return A new byte_t object converted from value.
+  /// @exception xtd::aegument_exception value does not represent a bool.
+  /// @par Namespace
+  /// xtd
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core
+  /// @par Examples
+  /// @code
+  /// const char8_t* value = u8"42";
+  /// byte_t result = as<byte_t>(value);
+  /// @endcode
+  template<>
+  inline byte_t as<byte_t>(const char8_t* value) {
+    return xtd::convert::to_byte(value);
+  }
+  
+  /// @brief Casts a type into another type.
+  /// @param value object to convert.
+  /// @return A new byte_t object converted from value.
+  /// @exception xtd::aegument_exception value does not represent a bool.
+  /// @par Namespace
+  /// xtd
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core
+  /// @par Examples
+  /// @code
+  /// char8_t* value = u8"42";
+  /// byte_t result = as<byte_t>(value);
+  /// @endcode
+  template<>
+  inline byte_t as<byte_t>(char8_t* value) {
+    return xtd::convert::to_byte(value);
+  }
+  
+  /// @brief Casts a type into another type.
+  /// @param value object to convert.
+  /// @return A new byte_t object converted from value.
+  /// @exception xtd::aegument_exception value does not represent a bool.
+  /// @par Namespace
+  /// xtd
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core
+  /// @par Examples
+  /// @code
+  /// const char16_t* value = u"42";
+  /// byte_t result = as<byte_t>(value);
+  /// @endcode
+  template<>
+  inline byte_t as<byte_t>(const char16_t* value) {
+    return xtd::convert::to_byte(value);
+  }
+  
+  /// @brief Casts a type into another type.
+  /// @param value object to convert.
+  /// @return A new byte_t object converted from value.
+  /// @exception xtd::aegument_exception value does not represent a bool.
+  /// @par Namespace
+  /// xtd
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core
+  /// @par Examples
+  /// @code
+  /// char16_t* value = u"42";
+  /// byte_t result = as<byte_t>(value);
+  /// @endcode
+  template<>
+  inline byte_t as<byte_t>(char16_t* value) {
+    return xtd::convert::to_byte(value);
+  }
+  
+  /// @brief Casts a type into another type.
+  /// @param value object to convert.
+  /// @return A new byte_t object converted from value.
+  /// @exception xtd::aegument_exception value does not represent a bool.
+  /// @par Namespace
+  /// xtd
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core
+  /// @par Examples
+  /// @code
+  /// const char32_t* value = U"42";
+  /// byte_t result = as<byte_t>(value);
+  /// @endcode
+  template<>
+  inline byte_t as<byte_t>(const char32_t* value) {
+    return xtd::convert::to_byte(value);
+  }
+  
+  /// @brief Casts a type into another type.
+  /// @param value object to convert.
+  /// @return A new byte_t object converted from value.
+  /// @exception xtd::aegument_exception value does not represent a bool.
+  /// @par Namespace
+  /// xtd
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core
+  /// @par Examples
+  /// @code
+  /// char32_t* value = U"42";
+  /// byte_t result = as<byte_t>(value);
+  /// @endcode
+  template<>
+  inline byte_t as<byte_t>(char32_t* value) {
+    return xtd::convert::to_byte(value);
+  }
+  
+  /// @brief Casts a type into another type.
+  /// @param value object to convert.
+  /// @return A new byte_t object converted from value.
+  /// @exception xtd::aegument_exception value does not represent a bool.
+  /// @par Namespace
+  /// xtd
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core
+  /// @par Examples
+  /// @code
+  /// const wchar_t* value = L"42";
+  /// byte_t result = as<byte_t>(value);
+  /// @endcode
+  template<>
+  inline byte_t as<byte_t>(const wchar_t* value) {
+    return xtd::convert::to_byte(value);
+  }
+  
+  /// @brief Casts a type into another type.
+  /// @param value object to convert.
+  /// @return A new byte_t object converted from value.
+  /// @exception xtd::aegument_exception value does not represent a bool.
+  /// @par Namespace
+  /// xtd
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core
+  /// @par Examples
+  /// @code
+  /// wchar_t* value = L"42";
+  /// byte_t result = as<byte_t>(value);
+  /// @endcode
+  template<>
+  inline byte_t as<byte_t>(wchar_t* value) {
+    return xtd::convert::to_byte(value);
+  }
 
   // ___________________________________________________________________________________________
   //                                                         xtd::convert_pointer specialization
