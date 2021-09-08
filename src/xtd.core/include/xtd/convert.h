00ca1c28e27e73212710f1bf4bf7b8cf05c6c88e
Gammasoft <gammasoft71@gmail.com>
2021-09-08 13:54:54 +0200

    add convert::to_char8

--- src/xtd.core/include/xtd/convert.h
+++ src/xtd.core/include/xtd/convert.h
@@ -1444,5 +1444,375 @@ namespace xtd {
     static char to_char(type_t value) {
       return static_cast<byte_t>(value);
     }
+    
+    /// @brief Convert std::any to char8.
+    /// @param value object to convert.
+    /// @return A new char8_t object converted from value.
+    /// @par Examples
+    /// @code
+    /// std::any value = 42;
+    /// char8_t result = convert::to_char8(value);
+    /// @endcode
+    static char8_t to_char8(std::any value);
+    /// @brief Convert bool to char8.
+    /// @param value object to convert.
+    /// @return A new char8_t object converted from value.
+    /// @par Examples
+    /// @code
+    /// bool value = true;
+    /// char8_t result = convert::to_char8(value);
+    /// @endcode
+    static char8_t to_char8(bool value);
+    /// @brief Convert byte_t to char8.
+    /// @param value object to convert.
+    /// @return A new char8_t object converted from value.
+    /// @par Examples
+    /// @code
+    /// byte_t value = 42;
+    /// char8_t result = convert::to_char8(value);
+    /// @endcode
+    static char8_t to_char8(byte_t value) noexcept;
+    /// @brief Convert char to char8.
+    /// @param value object to convert.
+    /// @return A new char8_t object converted from value.
+    /// @par Examples
+    /// @code
+    /// char value = 'a';
+    /// char8_t result = convert::to_char8(value);
+    /// @endcode
+    static char8_t to_char8(char value) noexcept;
+    /// @brief Convert char8_t to char8.
+    /// @param value object to convert.
+    /// @return A new char8_t object converted from value.
+    /// @par Examples
+    /// @code
+    /// char8_t value = u8'a';
+    /// char8_t result = convert::to_char8(value);
+    /// @endcode
+    static char8_t to_char8(char8_t value) noexcept;
+    /// @brief Convert char16_t to char8.
+    /// @param value object to convert.
+    /// @return A new char8_t object converted from value.
+    /// @exception xtd::overflow_exception value represents a number that is greater than std::numric_limit<byte_t>::max() or is less than 0.
+    /// @par Examples
+    /// @code
+    /// char16_t value = u'a';
+    /// char8_t result = convert::to_char8(value);
+    /// @endcode
+    static char8_t to_char8(char16_t value);
+    /// @brief Convert char32_t to char8.
+    /// @param value object to convert.
+    /// @return A new char8_t object converted from value.
+    /// @exception xtd::overflow_exception value represents a number that is greater than std::numric_limit<byte_t>::max() or is less than 0.
+    /// @par Examples
+    /// @code
+    /// char32_t value = U'a';
+    /// char8_t result = convert::to_char8(value);
+    /// @endcode
+    static char8_t to_char8(char32_t value);
+    /// @brief Convert char32_t to char8.
+    /// @param value object to convert.
+    /// @return A new char8_t object converted from value.
+    /// @exception xtd::overflow_exception value represents a number that is greater than std::numric_limit<byte_t>::max() or is less than 0.
+    /// @par Examples
+    /// @code
+    /// wchar_t value = L'a';
+    /// char8_t result = convert::to_char8(value);
+    /// @endcode
+    static char8_t to_char8(wchar_t value);
+    /// @brief Convert decimal_t to char8.
+    /// @param value object to convert.
+    /// @remarks The result is rounded.
+    /// @return A new char8_t object converted from value.
+    /// @par Examples
+    /// @code
+    /// decimal_t value = 42.50l;
+    /// char8_t result = convert::to_char8(value);
+    /// @endcode
+    static char8_t to_char8(decimal_t value);
+    /// @brief Convert double to char8.
+    /// @param value object to convert.
+    /// @return A new char8_t object converted from value.
+    /// @remarks The result is rounded.
+    /// @exception xtd::overflow_exception value represents a number that is greater than std::numric_limit<byte_t>::max() or is less than 0.
+    /// @par Examples
+    /// @code
+    /// double value = 42.50;
+    /// char8_t result = convert::to_char8(value);
+    /// @endcode
+    static char8_t to_char8(double value);
+    /// @brief Convert float to char8.
+    /// @param value object to convert.
+    /// @return A new char8_t object converted from value.
+    /// @remarks The result is rounded.
+    /// @exception xtd::overflow_exception value represents a number that is greater than std::numric_limit<byte_t>::max() or is less than 0.
+    /// @par Examples
+    /// @code
+    /// float value = 42.50f;
+    /// char8_t result = convert::to_char8(value);
+    /// @endcode
+    static char8_t to_char8(float value);
+    /// @brief Convert int16_t to char8.
+    /// @param value object to convert.
+    /// @return A new char8_t object converted from value.
+    /// @exception xtd::overflow_exception value represents a number that is greater than std::numric_limit<byte_t>::max() or is less than 0.
+    /// @par Examples
+    /// @code
+    /// int16_t value = 42;
+    /// char8_t result = convert::to_char8(value);
+    /// @endcode
+    static char8_t to_char8(int16_t value);
+    /// @brief Convert int32_t to char8.
+    /// @param value object to convert.
+    /// @return A new char8_t object converted from value.
+    /// @exception xtd::overflow_exception value represents a number that is greater than std::numric_limit<byte_t>::max() or is less than 0.
+    /// @par Examples
+    /// @code
+    /// int32_t value = 42;
+    /// char8_t result = convert::to_char8(value);
+    /// @endcode
+    static char8_t to_char8(int32_t value);
+    /// @brief Convert int64_t to char8.
+    /// @param value object to convert.
+    /// @return A new char8_t object converted from value.
+    /// @exception xtd::overflow_exception value represents a number that is greater than std::numric_limit<byte_t>::max() or is less than 0.
+    /// @par Examples
+    /// @code
+    /// int64_t value = 42l;
+    /// char8_t result = convert::to_char8(value);
+    /// @endcode
+    static char8_t to_char8(int64_t value);
+    /// @brief Convert llong_t to char8.
+    /// @param value object to convert.
+    /// @return A new char8_t object converted from value.
+    /// @exception xtd::overflow_exception value represents a number that is greater than std::numric_limit<byte_t>::max() or is less than 0.
+    /// @par Examples
+    /// @code
+    /// llong_t value = 42ll;
+    /// char8_t result = convert::to_char8(value);
+    /// @endcode
+    static char8_t to_char8(llong_t value);
+    /// @brief Convert sbyte_t to char8.
+    /// @param value object to convert.
+    /// @return A new char8_t object converted from value.
+    /// @exception xtd::overflow_exception value represents a number that is less than 0.
+    /// @par Examples
+    /// @code
+    /// sbyte_t value = 42;
+    /// char8_t result = convert::to_char8(value);
+    /// @endcode
+    static char8_t to_char8(sbyte_t value);
+    /// @brief Convert uint16_t to char8.
+    /// @param value object to convert.
+    /// @return A new char8_t object converted from value.
+    /// @exception xtd::overflow_exception value represents a number that is greater than std::numric_limit<byte_t>::max().
+    /// @par Examples
+    /// @code
+    /// uint16_t value = 42u;
+    /// char8_t result = convert::to_char8(value);
+    /// @endcode
+    static char8_t to_char8(uint16_t value);
+    /// @brief Convert uint32_t to char8.
+    /// @param value object to convert.
+    /// @return A new char8_t object converted from value.
+    /// @exception xtd::overflow_exception value represents a number that is greater than std::numric_limit<byte_t>::max().
+    /// @par Examples
+    /// @code
+    /// uint32_t value = 42u;
+    /// char8_t result = convert::to_char8(value);
+    /// @endcode
+    static char8_t to_char8(uint32_t value);
+    /// @brief Convert uint64_t to char8.
+    /// @param value object to convert.
+    /// @return A new char8_t object converted from value.
+    /// @exception xtd::overflow_exception value represents a number that is greater than std::numric_limit<byte_t>::max().
+    /// @par Examples
+    /// @code
+    /// uint64t value = 42ul;
+    /// char8_t result = convert::to_char8(value);
+    /// @endcode
+    static char8_t to_char8(uint64_t value);
+    /// @brief Convert llong_t to char8.
+    /// @param value object to convert.
+    /// @return A new char8_t object converted from value.
+    /// @exception xtd::overflow_exception value represents a number that is greater than std::numric_limit<byte_t>::max().
+    /// @par Examples
+    /// @code
+    /// ullong_t value = 42ull;
+    /// char8_t result = convert::to_char8(value);
+    /// @endcode
+    static char8_t to_char8(ullong_t value);
+    /// @brief Convert string to char8.
+    /// @param value object to convert.
+    /// @return A new char8_t object converted from value.
+    /// @exception xtd::atgument_exception value does not represent a byte.
+    /// @par Examples
+    /// @code
+    /// ustring value = "42";
+    /// char8_t result = convert::to_char8(value);
+    /// @endcode
+    static char8_t to_char8(const xtd::ustring& value);
+    /// @brief Convert string to char8.
+    /// @param value object to convert.
+    /// @return A new char8_t object converted from value.
+    /// @exception xtd::atgument_exception value does not represent a byte.
+    /// @par Examples
+    /// @code
+    /// string value = "42";
+    /// char8_t result = convert::to_char8(value);
+    /// @endcode
+    static char8_t to_char8(const std::string& value);
+    /// @brief Convert string to char8.
+    /// @param value object to convert.
+    /// @return A new char8_t object converted from value.
+    /// @exception xtd::atgument_exception value does not represent a byte.
+    /// @par Examples
+    /// @code
+    /// u8string value = u8"42";
+    /// char8_t result = convert::to_char8(value);
+    /// @endcode
+    static char8_t to_char8(const std::u8string& value);
+    /// @brief Convert string to char8.
+    /// @param value object to convert.
+    /// @return A new char8_t object converted from value.
+    /// @exception xtd::atgument_exception value does not represent a byte.
+    /// @par Examples
+    /// @code
+    /// u16string value = u"42";
+    /// char8_t result = convert::to_char8(value);
+    /// @endcode
+    static char8_t to_char8(const std::u16string& value);
+    /// @brief Convert string to char8.
+    /// @param value object to convert.
+    /// @return A new char8_t object converted from value.
+    /// @exception xtd::atgument_exception value does not represent a byte.
+    /// @par Examples
+    /// @code
+    /// u32string value = U"42";
+    /// char8_t result = convert::to_char8(value);
+    /// @endcode
+    static char8_t to_char8(const std::u32string& value);
+    /// @brief Convert string to char8.
+    /// @param value object to convert.
+    /// @return A new char8_t object converted from value.
+    /// @exception xtd::atgument_exception value does not represent a byte.
+    /// @par Examples
+    /// @code
+    /// wstring value = L"42";
+    /// char8_t result = convert::to_char8(value);
+    /// @endcode
+    static char8_t to_char8(const std::wstring& value);
+    /// @brief Convert string to char8.
+    /// @param value object to convert.
+    /// @return A new char8_t object converted from value.
+    /// @exception xtd::atgument_exception value does not represent a byte.
+    /// @par Examples
+    /// @code
+    /// const char* value = "42";
+    /// char8_t result = convert::to_char8(value);
+    /// @endcode
+    static char8_t to_char8(const char* value);
+    /// @brief Convert string to char8.
+    /// @param value object to convert.
+    /// @return A new char8_t object converted from value.
+    /// @exception xtd::atgument_exception value does not represent a byte.
+    /// @par Examples
+    /// @code
+    /// char* value = "42";
+    /// char8_t result = convert::to_char8(value);
+    /// @endcode
+    static char8_t to_char8(char* value);
+    /// @brief Convert string to char8.
+    /// @param value object to convert.
+    /// @return A new char8_t object converted from value.
+    /// @exception xtd::atgument_exception value does not represent a byte.
+    /// @par Examples
+    /// @code
+    /// const char8_t* value = u8"42";
+    /// char8_t result = convert::to_char8(value);
+    /// @endcode
+    static char8_t to_char8(const char8_t* value);
+    /// @brief Convert string to char8.
+    /// @param value object to convert.
+    /// @return A new char8_t object converted from value.
+    /// @exception xtd::atgument_exception value does not represent a byte.
+    /// @par Examples
+    /// @code
+    /// char8_t* value = u8"42";
+    /// char8_t result = convert::to_char8(value);
+    /// @endcode
+    static char8_t to_char8(char8_t* value);
+    /// @brief Convert string to char8.
+    /// @param value object to convert.
+    /// @return A new char8_t object converted from value.
+    /// @exception xtd::atgument_exception value does not represent a byte.
+    /// @par Examples
+    /// @code
+    /// const char16_t* value = u"42";
+    /// char8_t result = convert::to_char8(value);
+    /// @endcode
+    static char8_t to_char8(const char16_t* value);
+    /// @brief Convert string to char8.
+    /// @param value object to convert.
+    /// @return A new char8_t object converted from value.
+    /// @exception xtd::atgument_exception value does not represent a byte.
+    /// @par Examples
+    /// @code
+    /// char16_t* value = u"42";
+    /// char8_t result = convert::to_char8(value);
+    /// @endcode
+    static char8_t to_char8(char16_t* value);
+    /// @brief Convert string to char8.
+    /// @param value object to convert.
+    /// @return A new char8_t object converted from value.
+    /// @exception xtd::atgument_exception value does not represent a byte.
+    /// @par Examples
+    /// @code
+    /// const char32_t* value = U"42";
+    /// char8_t result = convert::to_char8(value);
+    /// @endcode
+    static char8_t to_char8(const char32_t* value);
+    /// @brief Convert string to char8.
+    /// @param value object to convert.
+    /// @return A new char8_t object converted from value.
+    /// @exception xtd::atgument_exception value does not represent a byte.
+    /// @par Examples
+    /// @code
+    /// char32_t* value = U"42";
+    /// char8_t result = convert::to_char8(value);
+    /// @endcode
+    static char8_t to_char8(char32_t* value);
+    /// @brief Convert string to char8.
+    /// @param value object to convert.
+    /// @return A new char8_t object converted from value.
+    /// @exception xtd::atgument_exception value does not represent a byte.
+    /// @par Examples
+    /// @code
+    /// const wchar_t* value = L"42";
+    /// char8_t result = convert::to_char8(value);
+    /// @endcode
+    static char8_t to_char8(const wchar_t* value);
+    /// @brief Convert string to char8.
+    /// @param value object to convert.
+    /// @return A new char8_t object converted from value.
+    /// @exception xtd::atgument_exception value does not represent a byte.
+    /// @par Examples
+    /// @code
+    /// wchar_t* value = L"42";
+    /// char8_t result = convert::to_char8(value);
+    /// @endcode
+    static char8_t to_char8(wchar_t* value);
+    /// @brief Convert type_t to char8.
+    /// @param value object to convert.
+    /// @return A new char8_t object converted from value.
+    /// @par Examples
+    /// @code
+    /// address_family value = address_family::inter_network;
+    /// char8_t result = convert::to_char8(value);
+    /// @endcode
+    template<typename type_t>
+    static char8_t to_char8(type_t value) {
+      return static_cast<byte_t>(value);
+    }
   };
 }
--- tests/xtd.core.unit_tests/src/convert.cpp
+++ tests/xtd.core.unit_tests/src/convert.cpp
@@ -818,5 +818,280 @@ namespace unit_tests {
       value = L"ab";
       assert::throws<format_exception>([&]{convert::to_char(value);}, csf_);
     }
+
+    void test_method_(to_char8_from_any) {
+      // Cannot build on macOS: any with char8_t does not link!
+      //any value = u8'a';
+      //assert::throws<invalid_cast_exception>([&]{convert::to_char8(value);}, csf_);
+    }
+    
+    void test_method_(to_char8_from_any_failed) {
+      any value = "not a char";
+      assert::throws<invalid_cast_exception>([&]{convert::to_char8(value);}, csf_);
+    }
+    
+    void test_method_(to_char8_from_bool) {
+      bool value = true;
+      assert::throws<invalid_cast_exception>([&]{convert::to_char8(value);}, csf_);
+    }
+    
+    void test_method_(to_char8_from_byte) {
+      byte_t value = 97;
+      assert::are_equal(u8'a', convert::to_char8(value), csf_);
+      value = 0;
+      assert::is_zero(convert::to_char8(value), csf_);
+    }
+    
+    void test_method_(to_char8_from_char) {
+      char value = 'a';
+      assert::are_equal(u8'a', convert::to_char8(value), csf_);
+      value = 0;
+      assert::is_zero(convert::to_char8(value), csf_);
+    }
+    
+    void test_method_(to_char8_from_char8) {
+      char8_t value = u8'a';
+      assert::are_equal(u8'a', convert::to_char8(value), csf_);
+      value = 0;
+      assert::is_zero(convert::to_char8(value), csf_);
+    }
+    
+    void test_method_(to_char8_from_char16) {
+      char16_t value = u'a';
+      assert::are_equal(u8'a', convert::to_char8(value), csf_);
+      value = 0;
+      assert::is_zero(convert::to_char8(value), csf_);
+      value = u'\u01FF';
+      assert::throws<overflow_exception>([&]{convert::to_char8(value);}, csf_);
+    }
+    
+    void test_method_(to_char8_from_char32) {
+      char32_t value = U'a';
+      assert::are_equal(u8'a', convert::to_char8(value), csf_);
+      value = 0;
+      assert::is_zero(convert::to_char8(value), csf_);
+      value = U'\U000001FF';
+      assert::throws<overflow_exception>([&]{convert::to_char8(value);}, csf_);
+    }
+    
+    void test_method_(to_char8_from_wchar) {
+      wchar_t value = L'a';
+      assert::are_equal(u8'a', convert::to_char8(value), csf_);
+      value = 0;
+      assert::is_zero(convert::to_char8(value), csf_);
+      value = L'\x01FF';
+      assert::throws<overflow_exception>([&]{convert::to_char8(value);}, csf_);
+    }
+    
+    void test_method_(to_char8_from_decimal) {
+      decimal_t value = 97.0l;
+      assert::are_equal(u8'a', convert::to_char8(value), csf_);
+      value = 97.49l;
+      assert::are_equal(u8'a', convert::to_char8(value), csf_);
+      value = 97.50l;
+      assert::are_equal(u8'b', convert::to_char8(value), csf_);
+      value = 0.0l;
+      assert::is_zero(convert::to_char8(value), csf_);
+      value = -0.1l;
+      assert::throws<overflow_exception>([&]{convert::to_char8(value);}, csf_);
+      value = 256.0l;
+      assert::throws<overflow_exception>([&]{convert::to_char8(value);}, csf_);
+    }
+    
+    void test_method_(to_char8_from_double) {
+      double value = 97.0;
+      assert::are_equal(u8'a', convert::to_char8(value), csf_);
+      value = 97.49;
+      assert::are_equal(u8'a', convert::to_char8(value), csf_);
+      value = 97.50;
+      assert::are_equal(u8'b', convert::to_char8(value), csf_);
+      value = 0.0;
+      assert::is_zero(convert::to_char8(value), csf_);
+      value = -0.1;
+      assert::throws<overflow_exception>([&]{convert::to_char8(value);}, csf_);
+      value = 256.0;
+      assert::throws<overflow_exception>([&]{convert::to_char8(value);}, csf_);
+    }
+    
+    void test_method_(to_char8_from_float) {
+      float value = 97.0f;
+      assert::are_equal(u8'a', convert::to_char8(value), csf_);
+      value = 97.49f;
+      assert::are_equal(u8'a', convert::to_char8(value), csf_);
+      value = 97.50f;
+      assert::are_equal(u8'b', convert::to_char8(value), csf_);
+      value = 0.0f;
+      assert::is_zero(convert::to_char8(value), csf_);
+      value = -0.1f;
+      assert::throws<overflow_exception>([&]{convert::to_char8(value);}, csf_);
+      value = 256.0f;
+      assert::throws<overflow_exception>([&]{convert::to_char8(value);}, csf_);
+    }
+    
+    void test_method_(to_char8_from_int16) {
+      int16_t value = 97;
+      assert::are_equal(u8'a', convert::to_char8(value), csf_);
+      value = 0;
+      assert::is_zero(convert::to_char8(value), csf_);
+      value = -1;
+      assert::throws<overflow_exception>([&]{convert::to_char8(value);}, csf_);
+      value = 256;
+      assert::throws<overflow_exception>([&]{convert::to_char8(value);}, csf_);
+    }
+    
+    void test_method_(to_char8_from_int32) {
+      int32_t value = 97;
+      assert::are_equal(u8'a', convert::to_char8(value), csf_);
+      value = 0;
+      assert::is_zero(convert::to_char8(value), csf_);
+      value = -1;
+      assert::throws<overflow_exception>([&]{convert::to_char8(value);}, csf_);
+      value = 256;
+      assert::throws<overflow_exception>([&]{convert::to_char8(value);}, csf_);
+    }
+    
+    void test_method_(to_char8_from_int64) {
+      int64_t value = 97l;
+      assert::are_equal(u8'a', convert::to_char8(value), csf_);
+      value = 0l;
+      assert::is_zero(convert::to_char8(value), csf_);
+      value = -1l;
+      assert::throws<overflow_exception>([&]{convert::to_char8(value);}, csf_);
+      value = 256l;
+      assert::throws<overflow_exception>([&]{convert::to_char8(value);}, csf_);
+    }
+    
+    void test_method_(to_char8_from_llong) {
+      llong_t value = 97ll;
+      assert::are_equal(u8'a', convert::to_char8(value), csf_);
+      value = 0ll;
+      assert::is_zero(convert::to_char8(value), csf_);
+      value = -1ll;
+      assert::throws<overflow_exception>([&]{convert::to_char8(value);}, csf_);
+      value = 256ll;
+      assert::throws<overflow_exception>([&]{convert::to_char8(value);}, csf_);
+    }
+    
+    void test_method_(to_char8_from_sbyte) {
+      sbyte_t value = 97;
+      assert::are_equal(u8'a', convert::to_char8(value), csf_);
+      value = 0;
+      assert::is_zero(convert::to_char8(value), csf_);
+      value = -1;
+      assert::throws<overflow_exception>([&]{convert::to_char8(value);}, csf_);
+    }
+    
+    void test_method_(to_char8_from_uint16) {
+      uint16_t value = 97u;
+      assert::are_equal(u8'a', convert::to_char8(value), csf_);
+      value = 0u;
+      assert::is_zero(convert::to_char8(value), csf_);
+      value = 256u;
+      assert::throws<overflow_exception>([&]{convert::to_char8(value);}, csf_);
+    }
+    
+    void test_method_(to_char8_from_uint32) {
+      uint32_t value = 97u;
+      assert::are_equal(u8'a', convert::to_char8(value), csf_);
+      value = 0u;
+      assert::is_zero(convert::to_char8(value), csf_);
+      value = 256u;
+      assert::throws<overflow_exception>([&]{convert::to_char8(value);}, csf_);
+    }
+    
+    void test_method_(to_char8_from_uint64) {
+      uint64_t value = 97lu;
+      assert::are_equal(u8'a', convert::to_char8(value), csf_);
+      value = 0lu;
+      assert::is_zero(convert::to_char8(value), csf_);
+      value = 256lu;
+      assert::throws<overflow_exception>([&]{convert::to_char8(value);}, csf_);
+    }
+    
+    void test_method_(to_char8_from_ullong) {
+      ullong_t value = 97llu;
+      assert::are_equal(u8'a', convert::to_char8(value), csf_);
+      value = 0llu;
+      assert::is_zero(convert::to_char8(value), csf_);
+      value = 256llu;
+      assert::throws<overflow_exception>([&]{convert::to_char8(value);}, csf_);
+    }
+    
+    void test_method_(to_char8_from_ustring) {
+      ustring value = "a";
+      assert::are_equal(u8'a', convert::to_char8(value), csf_);
+      value = "ab";
+      assert::throws<format_exception>([&]{convert::to_char8(value);}, csf_);
+    }
+    
+    void test_method_(to_char8_from_string) {
+      string value = "a";
+      assert::are_equal(u8'a', convert::to_char8(value), csf_);
+      value = "ab";
+      assert::throws<format_exception>([&]{convert::to_char8(value);}, csf_);
+    }
+    
+    void test_method_(to_char8_from_u8string) {
+      u8string value = u8"a";
+      assert::are_equal(u8'a', convert::to_char8(value), csf_);
+      value = u8"ab";
+      assert::throws<format_exception>([&]{convert::to_char8(value);}, csf_);
+    }
+    
+    void test_method_(to_char8_from_u16string) {
+      u16string value = u"a";
+      assert::are_equal(u8'a', convert::to_char8(value), csf_);
+      value = u"ab";
+      assert::throws<format_exception>([&]{convert::to_char8(value);}, csf_);
+    }
+    
+    void test_method_(to_char8_from_u32string) {
+      u32string value = U"a";
+      assert::are_equal(u8'a', convert::to_char8(value), csf_);
+      value = U"ab";
+      assert::throws<format_exception>([&]{convert::to_char8(value);}, csf_);
+    }
+    
+    void test_method_(to_char8_from_wstring) {
+      wstring value = L"a";
+      assert::are_equal(u8'a', convert::to_char8(value), csf_);
+      value = L"ab";
+      assert::throws<format_exception>([&]{convert::to_char8(value);}, csf_);
+    }
+    
+    void test_method_(to_char8_from_literal_char) {
+      const char* value = "a";
+      assert::are_equal(u8'a', convert::to_char8(value), csf_);
+      value = "ab";
+      assert::throws<format_exception>([&]{convert::to_char8(value);}, csf_);
+    }
+    
+    void test_method_(to_char8_from_literal_char8) {
+      const char8_t* value = u8"a";
+      assert::are_equal(u8'a', convert::to_char8(value), csf_);
+      value = u8"ab";
+      assert::throws<format_exception>([&]{convert::to_char8(value);}, csf_);
+    }
+    
+    void test_method_(to_char8_from_literal_char16) {
+      const char16_t* value = u"a";
+      assert::are_equal(u8'a', convert::to_char8(value), csf_);
+      value = u"ab";
+      assert::throws<format_exception>([&]{convert::to_char8(value);}, csf_);
+    }
+    
+    void test_method_(to_char8_from_literal_char32) {
+      const char32_t* value = U"a";
+      assert::are_equal(u8'a', convert::to_char8(value), csf_);
+      value = U"ab";
+      assert::throws<format_exception>([&]{convert::to_char8(value);}, csf_);
+    }
+    
+    void test_method_(to_char8_from_literal_wchar) {
+      const wchar_t* value = L"a";
+      assert::are_equal(u8'a', convert::to_char8(value), csf_);
+      value = L"ab";
+      assert::throws<format_exception>([&]{convert::to_char8(value);}, csf_);
+    }
   };
 }
