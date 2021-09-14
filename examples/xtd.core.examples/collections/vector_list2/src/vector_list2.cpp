89e45f8cc0a23857f4310be13b8e60c1209e672c
Gammasoft <gammasoft71@gmail.com>
2021-09-14 11:28:38 +0200

    Update vector_list2.cpp

--- examples/xtd.core.examples/collections/vector_list2/src/vector_list2.cpp
+++ examples/xtd.core.examples/collections/vector_list2/src/vector_list2.cpp
@@ -17,19 +17,22 @@ int main() {
   register_any_stringer<seconds>([](seconds value) {return ustring::format("{}", value);});
 
   // Creates and initializes a new vector_list.
-  vector_list vector_list = {true, 42, "This is a string"_s, platform_id::win32s, "Another string", 4.2f, hours(2) + minutes(25) + seconds(43)};
+  vector_list vector_list = {true, 42, "This is a string"_s, platform_id::win32s, u8"Another string"_s, 4.2f, hours(2) + minutes(25) + seconds(43)};
   
   console::write_line("vector_list = {{{}}}", ustring::join(", ", vector_list));
   console::write_line();
   
   for (const any& item : vector_list) {
-    if (is<ustring>(item) || is<const char*>(item)) console::write_line("{}", std::quoted(as<ustring>(item).to_upper()));
+    if (is<ustring>(item)) console::write_line("{}", std::quoted(as<ustring>(item).to_upper()));
     else console::write_line("{}", item);
   }
 }
 
 // This code can produces the following output :
 //
+// vector_list = {true, 42, This is a string, win32s, Another string, 4.2, 02:25:43}
+//
+// true
 // 42
 // "THIS IS A STRING"
 // "ANOTHER STRING"
