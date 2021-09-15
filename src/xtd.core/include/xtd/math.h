2f5f21618b952028ddc1d8a16aa7cc3dc8388664
Gammasoft <gammasoft71@gmail.com>
2021-09-15 11:39:39 +0200

    xtd::math implementation...

--- src/xtd.core/include/xtd/math.h
+++ src/xtd.core/include/xtd/math.h
@@ -73,5 +73,91 @@ namespace xtd {
     /// @exception xtd::overflow_exception value equals std::numeric_limit<llong_t>::lowest().
     static llong_t abs(llong_t value);
 
+    /// @brief Returns the angle whose cosine is the specified number.
+    /// @param value A number representing a cosine, where -1 <= value <= 1.
+    /// @return An angle, A, measured in radians, such that 0 <= A <= PI. -or- NaN if value < -1 or d > 1.
+    static double acos(double value);
+    
+    /// @brief Returns the angle whose sine is the specified number.
+    /// @param value A number representing a sine, where -1 <= value <= 1.
+    /// @return An angle, A, measured in radians, such that -PI/2 <= A <= PI/2. -or- NaN if value < -1 or d > 1.
+    static double asin(double value);
+    
+    /// @brief Returns the angle whose tangent is the specified number.
+    /// @param value A number representing a tangent.
+    /// @return An angle, A, measured in radians, such that -PI/2 <= ? <= PI/2. -or- NaN if value equals NaN, -PI/2 rounded to double precision (-1.5707963267949) if d equals NegativeInfinity, or PI/2 rounded to double precision (1.5707963267949) if d equals PositiveInfinity
+    static double atan(double value);
+    
+    /// @brief Returns the angle whose tangent is the specified number.
+    /// @param y The y coordinate of a point.
+    /// @param x The x coordinate of a point.
+    /// @return An angle, A, measured in radians, such that -PI <= A <= PI, and tan(?) = y / x, where (x, y) is a point in the Cartesian plane. Observe the following:
+    /// * For (x, y) in quadrant 1, 0 < A < PI/2.
+    /// * For (x, y) in quadrant 2, PI/2 < A <= PI.
+    /// * For (x, y) in quadrant 3, -PI < A < -PI/2.
+    /// * For (x, y) in quadrant 4, -PI/2 < A < 0.
+    /// * For points on the boundaries of the quadrants, the return value is the following:
+    /// * If x is 0 and y is not negative, A = 0.
+    /// * If x is 0 and y is negative, A = PI.
+    /// * If x is positive and y is 0, A = PI/2.
+    /// * If x is negative and y is 0, A = -PI/2.
+    /// @remarks The return value is the angle in the Cartesian plane formed by the x-axis, and a vector starting from the origin, (0,0), and terminating at the point, (x,y).
+    static double atan2(double y, double x);
+
+    /// @brief Produces the full product of two 32-bit numbers.
+    /// @param a The first int32 to multiply.
+    /// @param b The second int32 to multiply.
+    /// @return The int64 containing the product of the specified numbers.
+    static int64_t big_mul(int32_t a, int32_t b);
+
+    /// @brief Returns the smallest integer greater than or equal to the specified double-precision floating-point number.
+    /// @param value A double-precision floating-point number.
+    /// @return The smallest integer greater than or equal to value. If value is equal to std::numeric_limits<double>::quiet_NaN(), -std::numeric_limits<double>::infinity(), or std::numeric_limits<double>::infinity(), that value is returned.
+    static decimal ceiling(decimal value);
+    
+    /// @brief Returns the smallest integer greater than or equal to the specified double-precision floating-point number.
+    /// @param value A double-precision floating-point number.
+    /// @return The smallest integer greater than or equal to value. If value is equal to std::numeric_limits<double>::quiet_NaN(), -std::numeric_limits<double>::infinity(), or std::numeric_limits<double>::infinity(), that value is returned.
+    static double ceiling(double value);
+    
+    /// @brief Returns the cosine of the specified angle.
+    /// @param value An angle, measured in radians.
+    /// @return The cosine of value. If value is equal to std::numeric_limits<double>::quiet_NaN(), -std::numeric_limits<double>::infinity(), or std::numeric_limits<double>::infinity(), this method returns NaN.
+    static double cos(double value);
+    
+    /// @brief Returns the hyperbolic cosine of the specified angle.
+    /// @param value An angle, measured in radians.
+    /// @return The hyperbolic cosine of value. If value is equal to -std::numeric_limits<double>::infinity() or std::numeric_limits<double>::infinity(), std::numeric_limits<double>::infinity() is returned. If value is equal to std::numeric_limits<double>::quiet_NaN(), std::numeric_limits<double>::quiet_NaN() is returned.
+    static double cosh(double value);
+
+    /// @brief Convert degrees to radians.
+    /// @param degrees A double-precision floating-point number.
+    /// @return A converted degrees from radians.
+    static double degrees_to_radians(double degrees);
+
+    /// @brief Returns a value indicating whether the specified number evaluates to negative or positive infinity.
+    /// @param value A double-precision floating-point number.
+    /// @return true if value evaluates to std::numeric_limits<double>::infinity() or -std::numeric_limits<double>::infinity(); otherwise, false.
+    static bool is_infinity(double value);
+    
+    /// @brief Returns a value indicating whether the specified number evaluates to negative infinity.
+    /// @param value A double-precision floating-point number.
+    /// @return true if value evaluates to -std::numeric_limits<double>::infinity(); otherwise, false.
+    static bool is_negative_infinity(double value);
+
+    /// @brief Returns a value indicating whether the specified number evaluates to positive infinity.
+    /// @param value A double-precision floating-point number.
+    /// @return true if value evaluates to std::numeric_limits<double>::infinity(); otherwise, false.
+    static bool is_positive_infinity(double value);
+
+    /// @brief Returns a value indicating whether the specified number evaluates to not a number.
+    /// @param value A double-precision floating-point number.
+    /// @return true if value evaluates to not a number; otherwise, false.
+    static bool is_NaN(double value);
+
+    /// @brief Convert radians to degrees.
+    /// @param radians A double-precision floating-point number.
+    /// @return A converted radians from degrees.
+    static double radians_to_degrees(double radians);
   };
 }
--- tests/xtd.core.unit_tests/src/xtd/math.cpp
+++ tests/xtd.core.unit_tests/src/xtd/math.cpp
@@ -43,52 +43,168 @@ namespace unit_tests {
 
     void test_method_(abs_single) {
       assert::are_equal(numeric_limits<float>::max(), math::abs(numeric_limits<float>::max()), csf_);
-      assert::are_equal(16.354e-12f, math::abs(16.354e-12f));
-      assert::are_equal(15.098123f, math::abs(15.098123f));
-      assert::are_equal(.0f, math::abs(.0f));
-      assert::are_equal(19.069713f, math::abs(-19.069713f));
-      assert::are_equal(15.058e17f, math::abs(-15.058e17f));
+      assert::are_equal(16.354e-12f, math::abs(16.354e-12f), csf_);
+      assert::are_equal(15.098123f, math::abs(15.098123f), csf_);
+      assert::are_equal(.0f, math::abs(.0f), csf_);
+      assert::are_equal(19.069713f, math::abs(-19.069713f), csf_);
+      assert::are_equal(15.058e17f, math::abs(-15.058e17f), csf_);
       assert::are_equal(-numeric_limits<float>::lowest(), math::abs(numeric_limits<float>::lowest()), csf_);
     }
 
     void test_method_(abs_int16) {
       assert::are_equal(numeric_limits<int16_t>::max(), math::abs(numeric_limits<int16_t>::max()), csf_);
-      assert::are_equal(static_cast<int16_t>(10328), math::abs(static_cast<int16_t>(10328)));
-      assert::are_equal(static_cast<int16_t>(0), math::abs(static_cast<int16_t>(0)));
-      assert::are_equal(static_cast<int16_t>(1476), math::abs(static_cast<int16_t>(-1476)));
+      assert::are_equal(static_cast<int16_t>(10328), math::abs(static_cast<int16_t>(10328)), csf_);
+      assert::are_equal(static_cast<int16_t>(0), math::abs(static_cast<int16_t>(0)), csf_);
+      assert::are_equal(static_cast<int16_t>(1476), math::abs(static_cast<int16_t>(-1476)), csf_);
       assert::throws<overflow_exception>([] {math::abs(numeric_limits<int16_t>::lowest());}, csf_);
     }
     
     void test_method_(abs_int32) {
       assert::are_equal(numeric_limits<int32_t>::max(), math::abs(numeric_limits<int32_t>::max()), csf_);
-      assert::are_equal(16921, math::abs(16921));
-      assert::are_equal(0, math::abs(0));
-      assert::are_equal(804128, math::abs(-804128));
+      assert::are_equal(16921, math::abs(16921), csf_);
+      assert::are_equal(0, math::abs(0), csf_);
+      assert::are_equal(804128, math::abs(-804128), csf_);
       assert::throws<overflow_exception>([] {math::abs(numeric_limits<int32_t>::lowest());}, csf_);
     }
     
     void test_method_(abs_int64) {
       assert::are_equal(numeric_limits<int64_t>::max(), math::abs(numeric_limits<int64_t>::max()), csf_);
-      assert::are_equal(static_cast<int64_t>(109013), math::abs(static_cast<int64_t>(109013)));
-      assert::are_equal(static_cast<int64_t>(0), math::abs(static_cast<int64_t>(0)));
-      assert::are_equal(static_cast<int64_t>(6871982), math::abs(static_cast<int64_t>(-6871982)));
+      assert::are_equal(static_cast<int64_t>(109013), math::abs(static_cast<int64_t>(109013)), csf_);
+      assert::are_equal(static_cast<int64_t>(0), math::abs(static_cast<int64_t>(0)), csf_);
+      assert::are_equal(static_cast<int64_t>(6871982), math::abs(static_cast<int64_t>(-6871982)), csf_);
       assert::throws<overflow_exception>([] {math::abs(numeric_limits<int64_t>::lowest());}, csf_);
     }
     
     void test_method_(abs_sbyte) {
       assert::are_equal(numeric_limits<sbyte_t>::max(), math::abs(numeric_limits<sbyte_t>::max()), csf_);
-      assert::are_equal(static_cast<sbyte_t>(98), math::abs(static_cast<sbyte_t>(98)));
-      assert::are_equal(static_cast<sbyte_t>(0), math::abs(static_cast<sbyte_t>(0)));
-      assert::are_equal(static_cast<sbyte_t>(32), math::abs(static_cast<sbyte_t>(-32)));
+      assert::are_equal(static_cast<sbyte_t>(98), math::abs(static_cast<sbyte_t>(98)), csf_);
+      assert::are_equal(static_cast<sbyte_t>(0), math::abs(static_cast<sbyte_t>(0)), csf_);
+      assert::are_equal(static_cast<sbyte_t>(32), math::abs(static_cast<sbyte_t>(-32)), csf_);
       assert::throws<overflow_exception>([] {math::abs(numeric_limits<sbyte_t>::lowest());}, csf_);
     }
     
     void test_method_(abs_llong) {
       assert::are_equal(numeric_limits<llong_t>::max(), math::abs(numeric_limits<llong_t>::max()), csf_);
-      assert::are_equal(109013ll, math::abs(109013ll));
-      assert::are_equal(0ll, math::abs(0ll));
-      assert::are_equal(6871982ll, math::abs(-6871982ll));
+      assert::are_equal(109013ll, math::abs(109013ll), csf_);
+      assert::are_equal(0ll, math::abs(0ll), csf_);
+      assert::are_equal(6871982ll, math::abs(-6871982ll), csf_);
       assert::throws<overflow_exception>([] {math::abs(numeric_limits<llong_t>::lowest());}, csf_);
+    }
+
+    void test_method_(acos) {
+      assert::is_NaN(math::acos(-1.1), csf_);
+      assert::are_equal(3.14159265358979, math::acos(-1), .000000001, csf_);
+      assert::are_equal(2.0943951023932, math::acos(-.5), .000000001, csf_);
+      assert::are_equal(1.5707963267949, math::acos(.0), .000000001, csf_);
+      assert::are_equal(1.0471975511966, math::acos(.5), .000000001, csf_);
+      assert::are_equal(.0, math::acos(1), csf_);
+      assert::is_NaN(math::acos(1.1), csf_);
+    }
+    
+    void test_method_(asin) {
+      assert::is_NaN(math::asin(-1.1), csf_);
+      assert::are_equal(-1.5707963267949, math::asin(-1), .000000001, csf_);
+      assert::are_equal(-0.523598775598299, math::asin(-.5), .000000001, csf_);
+      assert::are_equal(.0, math::asin(.0), csf_);
+      assert::are_equal(0.523598775598299, math::asin(.5), .000000001, csf_);
+      assert::are_equal(1.5707963267949, math::asin(1), .000000001, csf_);
+      assert::is_NaN(math::asin(1.1), csf_);
+    }
+    
+    void test_method_(atan) {
+      assert::is_NaN(math::atan(numeric_limits<double>::quiet_NaN()), csf_);
+      assert::are_equal(-1.5707963267949, math::atan(-numeric_limits<double>::infinity()), .000000001, csf_);
+      assert::are_equal(-1.41296513650674, math::atan(math::degrees_to_radians(-360)), .000000001, csf_);
+      assert::are_equal(-1.36169168297116, math::atan(math::degrees_to_radians(-270)), .000000001, csf_);
+      assert::are_equal(-1.26262725567891, math::atan(math::degrees_to_radians(-180)), .000000001, csf_);
+      assert::are_equal(-1.00388482185389, math::atan(math::degrees_to_radians(-90)), .000000001, csf_);
+      assert::are_equal(.0, math::atan(.0), csf_);
+      assert::are_equal(1.00388482185389, math::atan(math::degrees_to_radians(90)), .000000001, csf_);
+      assert::are_equal(1.26262725567891, math::atan(math::degrees_to_radians(180)), .000000001, csf_);
+      assert::are_equal(1.36169168297116, math::atan(math::degrees_to_radians(270)), .000000001, csf_);
+      assert::are_equal(1.41296513650674, math::atan(math::degrees_to_radians(360)), .000000001, csf_);
+      assert::are_equal(1.5707963267949, math::atan(numeric_limits<double>::infinity()), .000000001, csf_);
+    }
+    
+    void test_method_(atan2) {
+      assert::are_equal(0.463647609000806, math::atan2(1, 2), .000000000000001, csf_);
+      assert::are_equal(0.839287844473872, math::atan2(127, 114), .000000000000001, csf_);
+      assert::are_equal(0, math::atan2(0, 1), csf_);
+      assert::are_equal(math::pi, math::atan2(0, -1), csf_);
+      assert::are_equal(math::pi / 2, math::atan2(1, 0), csf_);
+      assert::are_equal(-math::pi / 2, math::atan2(-1, 0), csf_);
+      assert::is_NaN(math::atan2(numeric_limits<double>::quiet_NaN(), 1), csf_);
+      assert::is_NaN(math::atan2(1, numeric_limits<double>::quiet_NaN()), csf_);
+      assert::is_NaN(math::atan2(numeric_limits<double>::infinity(), numeric_limits<double>::infinity()), csf_);
+      assert::is_NaN(math::atan2(numeric_limits<double>::infinity(), -numeric_limits<double>::infinity()), csf_);
+      assert::is_NaN(math::atan2(-numeric_limits<double>::infinity(), -numeric_limits<double>::infinity()), csf_);
+      assert::is_NaN(math::atan2(-numeric_limits<double>::infinity(), numeric_limits<double>::infinity()), csf_);
+    }
+
+    void test_method_(big_mul) {
+      assert::are_equal(4611686014132420609LL, math::big_mul(numeric_limits<int32_t>::max(), numeric_limits<int32_t>::max()), csf_);
+    }
+
+    void test_method_(ceiling) {
+      assert::are_equal(8, math::ceiling(7.03), csf_);
+      assert::are_equal(8, math::ceiling(7.64), csf_);
+      assert::are_equal(1, math::ceiling(0.12), csf_);
+      assert::are_equal(0, math::ceiling(-0.12), csf_);
+      assert::are_equal(-7, math::ceiling(-7.1), csf_);
+      assert::are_equal(-7, math::ceiling(-7.6), csf_);
+    }
+    
+    void test_method_(cos) {
+      assert::are_equal(1, math::cos(math::degrees_to_radians(0.0)), csf_);
+      assert::are_equal(6.12303176911189E-17, math::cos(math::degrees_to_radians(90.0)), .0001, csf_);
+      assert::is_NaN(math::cos(numeric_limits<double>::quiet_NaN()), csf_);
+      assert::is_NaN(math::cos(-numeric_limits<double>::infinity()), csf_);
+      assert::is_NaN(math::cos(numeric_limits<double>::infinity()), csf_);
+    }
+    
+    void test_method_(cosh) {
+      assert::are_equal(1, math::cosh(0.0), csf_);
+      assert::are_equal(10.0676619957778, math::cosh(3.0), .0000000000001, csf_);
+      assert::are_equal(11013.2329201033, math::cosh(10.0), .0000000001, csf_);
+      assert::is_true(math::is_positive_infinity(math::cosh(numeric_limits<double>::infinity())), csf_);
+      assert::is_true(math::is_positive_infinity(math::cosh(-numeric_limits<double>::infinity())), csf_);
+      assert::is_NaN(math::cosh(numeric_limits<double>::quiet_NaN()), csf_);
+    }
+
+    void test_method_(degrees_to_radians) {
+      assert::are_equal(-6.28318530717959, math::degrees_to_radians(-360.0), .00000000000001, csf_);
+      assert::are_equal(-4.71238898038469, math::degrees_to_radians(-270.0), .00000000000001, csf_);
+      assert::are_equal(-3.14159265358979, math::degrees_to_radians(-180.0), .0000000000001, csf_);
+      assert::are_equal(-1.5707963267949, math::degrees_to_radians(-90.0), .0000000000001, csf_);
+      assert::are_equal(0.0, math::degrees_to_radians(0.0), csf_);
+      assert::are_equal(1.5707963267949, math::degrees_to_radians(90.0), .0000000000001, csf_);
+      assert::are_equal(3.14159265358979, math::degrees_to_radians(180.0), .00000000000001, csf_);
+      assert::are_equal(4.71238898038469, math::degrees_to_radians(270.0), .00000000000001, csf_);
+      assert::are_equal(6.28318530717959, math::degrees_to_radians(360.0), .00000000000001, csf_);
+    }
+    
+    void test_method_(radians_to_degrees) {
+      assert::are_equal(-360.0, math::radians_to_degrees(-6.28318530717959), .000000001, csf_);
+      assert::are_equal(-270.0, math::radians_to_degrees(-4.71238898038469), .000000001, csf_);
+      assert::are_equal(-180.0, math::radians_to_degrees(-3.14159265358979), .000000001, csf_);
+      assert::are_equal(-90.0, math::radians_to_degrees(-1.5707963267949), .000000001, csf_);
+      assert::are_equal(0.0, math::radians_to_degrees(0.0), csf_);
+      assert::are_equal(90.0, math::radians_to_degrees(1.5707963267949), .000000001, csf_);
+      assert::are_equal(180.0, math::radians_to_degrees(3.14159265358979), .000000001, csf_);
+      assert::are_equal(270.0, math::radians_to_degrees(4.71238898038469), .000000001, csf_);
+      assert::are_equal(360.0, math::radians_to_degrees(6.28318530717959), .000000001, csf_);
+    }
+    
+    void test_method_(radians_to_degrees_degrees_to_radians) {
+      assert::are_equal(-360.0, math::radians_to_degrees(math::degrees_to_radians(-360.0)), csf_);
+      assert::are_equal(-270.0, math::radians_to_degrees(math::degrees_to_radians(-270.0)), csf_);
+      assert::are_equal(-180.0, math::radians_to_degrees(math::degrees_to_radians(-180.0)), csf_);
+      assert::are_equal(-90.0, math::radians_to_degrees(math::degrees_to_radians(-90.0)), csf_);
+      assert::are_equal(0.0, math::radians_to_degrees(math::degrees_to_radians(0.0)), csf_);
+      assert::are_equal(90.0, math::radians_to_degrees(math::degrees_to_radians(90.0)), csf_);
+      assert::are_equal(180.0, math::radians_to_degrees(math::degrees_to_radians(180.0)), csf_);
+      assert::are_equal(270.0, math::radians_to_degrees(math::degrees_to_radians(270.0)), csf_);
+      assert::are_equal(360.0, math::radians_to_degrees(math::degrees_to_radians(360.0)), csf_);
     }
   };
 }
