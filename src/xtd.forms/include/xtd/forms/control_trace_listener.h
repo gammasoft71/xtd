3204669cc848b6317853d25e4f0f12f7c14bf6b9
Gammasoft <gammasoft71@gmail.com>
2021-09-23 20:48:51 +0200

    Update control_trace_listener.h

--- src/xtd.forms/include/xtd/forms/control_trace_listener.h
+++ src/xtd.forms/include/xtd/forms/control_trace_listener.h
@@ -9,7 +9,7 @@
 namespace xtd {
   /// @brief The xtd::forms namespace contains classes for creating Windows-based applications that take full advantage of the rich user interface features available in the Microsoft Windows operating system, Apple macOS and Linux like Ubuntu operating system.
   namespace forms {
-    /// @brief Directs tracing or debugging output to either vers un icontrol_trace.
+    /// @brief Directs tracing or debugging output to either icontrol_trace.
     /// @par Namespace
     /// xtd::forms
     /// @par Library
