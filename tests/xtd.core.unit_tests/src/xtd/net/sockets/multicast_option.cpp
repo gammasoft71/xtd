df7f316a53f7414f41e9687f54c1c081de0ea9f7
Gammasoft <gammasoft71@gmail.com>
2021-10-02 15:50:49 +0200

    fix merge pr/12

--- tests/xtd.core.unit_tests/src/xtd/net/sockets/multicast_option.cpp
+++ tests/xtd.core.unit_tests/src/xtd/net/sockets/multicast_option.cpp
@@ -14,28 +14,28 @@ namespace unit_tests {
       multicast_option mo;
       assert::are_equal(ip_address::none, mo.group(), csf_);
       assert::is_zero(mo.interface_index(), csf_);
-      assert::are_equal(ip_address::none, mo.local_adress(), csf_);
+      assert::are_equal(ip_address::none, mo.local_address(), csf_);
     }
 
     void test_method_(constructor_with_group_and_local_address) {
       multicast_option mo(ip_address(172, 16, 12, 24), ip_address(172, 16, 12, 1));
       assert::are_equal("172.16.12.24", mo.group().to_string(), csf_);
       assert::is_zero(mo.interface_index(), csf_);
-      assert::are_equal("172.16.12.1", mo.local_adress().to_string(), csf_);
+      assert::are_equal("172.16.12.1", mo.local_address().to_string(), csf_);
     }
 
     void test_method_(constructor_with_group) {
       multicast_option mo(ip_address(172, 16, 12, 24));
       assert::are_equal("172.16.12.24", mo.group().to_string(), csf_);
       assert::is_zero(mo.interface_index(), csf_);
-      assert::are_equal(ip_address::any, mo.local_adress(), csf_);
+      assert::are_equal(ip_address::any, mo.local_address(), csf_);
    }
 
     void test_method_(constructor_with_group_and_interface_index) {
       multicast_option mo(ip_address(172, 16, 12, 24), 427);
       assert::are_equal("172.16.12.24", mo.group().to_string(), csf_);
       assert::are_equal(427U, mo.interface_index(), csf_);
-      assert::are_equal(ip_address::none, mo.local_adress(), csf_);
+      assert::are_equal(ip_address::none, mo.local_address(), csf_);
     }
     
     void test_method_(constructor_with_group_and_interface_index_greather_than_0x00FFFFFF) {
@@ -47,7 +47,7 @@ namespace unit_tests {
       mo.group(ip_address(172, 16, 12, 24));
       assert::are_equal("172.16.12.24", mo.group().to_string(), csf_);
       assert::is_zero(mo.interface_index(), csf_);
-      assert::are_equal(ip_address::none, mo.local_adress(), csf_);
+      assert::are_equal(ip_address::none, mo.local_address(), csf_);
     }
 
     void test_method_(set_interface_index) {
@@ -55,7 +55,7 @@ namespace unit_tests {
       mo.interface_index(427);
       assert::are_equal(ip_address::none, mo.group(), csf_);
       assert::are_equal(427U, mo.interface_index(), csf_);
-      assert::are_equal(ip_address::none, mo.local_adress(), csf_);
+      assert::are_equal(ip_address::none, mo.local_address(), csf_);
     }
 
     void test_method_(set_interface_index_greather_than_0x00FFFFFF) {
@@ -65,10 +65,10 @@ namespace unit_tests {
 
     void test_method_(set_local_address) {
       multicast_option mo;
-      mo.local_adress(ip_address(172, 16, 12, 1));
+      mo.local_address(ip_address(172, 16, 12, 1));
       assert::are_equal(ip_address::none, mo.group(), csf_);
       assert::is_zero(mo.interface_index(), csf_);
-      assert::are_equal("172.16.12.1", mo.local_adress().to_string(), csf_);
+      assert::are_equal("172.16.12.1", mo.local_address().to_string(), csf_);
     }
   };
 }
