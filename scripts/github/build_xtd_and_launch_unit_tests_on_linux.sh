9dede24c35b35731f855ddec12b6146c20e9be10
Gammasoft <gammasoft71@gmail.com>
2021-10-15 14:52:57 +0200

    add github build action

--- scripts/github/build_xtd_and_launch_unit_tests_on_linux.sh
+++ scripts/github/build_xtd_and_launch_unit_tests_on_linux.sh
@@ -6,7 +6,7 @@ git clone https://github.com/wxwidgets/wxwidgets.git -b v3.1.5 --depth 1
 cd wxwidgets
 git submodule update --init
 mkdir build_cmake && cd build_cmake
-cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=g++-9 -DwxBUILD_SHARED=OFF -DCMAKE_INSTALL_PREFIX=~/local
+cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_CXX_COMPILER=g++-9 -DwxBUILD_SHARED=OFF -DCMAKE_INSTALL_PREFIX=~/local
 if [ $? -ne 0 ]; then exit -1; fi
 cmake --build . --target install -- -j $(nproc)
 if [ $? -ne 0 ]; then exit -1; fi
@@ -15,7 +15,7 @@ cd ../../../..
 # generate and build lib
 git submodule update --init
 mkdir -p build && cd build
-cmake .. -DCMAKE_BUILD_TYPE=Debug -DXTD_BUILD_TESTS_XTD_CORE=ON -DXTD_BUILD_TESTS_XTD_DRAWING=ON -DXTD_BUILD_TESTS_XTD_FORMS=OFF -DXTD_BUILD_TESTS_XTD_TUNIT=ON -DCMAKE_CXX_COMPILER=g++-9 -DCMAKE_INSTALL_PREFIX=~/local
+cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DXTD_BUILD_TESTS_XTD_CORE=ON -DXTD_BUILD_TESTS_XTD_DRAWING=ON -DXTD_BUILD_TESTS_XTD_FORMS=OFF -DXTD_BUILD_TESTS_XTD_TUNIT=ON -DCMAKE_CXX_COMPILER=g++-9 -DCMAKE_INSTALL_PREFIX=~/local
 if [ $? -ne 0 ]; then exit -1; fi
 cmake --build . -- -j $(nproc)
 if [ $? -ne 0 ]; then exit -1; fi
@@ -25,6 +25,6 @@ cd ..
 
 # run registered unit tests
 cd build
-ctest -j $(nproc) --output-on-failure --build-config Debug
+ctest -j $(nproc) --output-on-failure --build-config $BUILD_TYPE
 if [ $? -ne 0 ]; then exit -1; fi
 cd ..
