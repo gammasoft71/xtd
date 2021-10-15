54a5e112d583808e5ad77afa19b60f86e6dff4e3
Gammasoft <gammasoft71@gmail.com>
2021-10-15 14:58:50 +0200

    add bat

---
+++ scripts/github/build_xtd_and_launch_unit_tests_on_windows.bat
@@ -0,0 +1,38 @@
+:: cmd
+
+:: clone, generate and build wxwidgets 3.1.5
+mkdir build\thirdparty
+cd build\thirdparty
+git clone https://github.com/wxwidgets/wxwidgets.git -b v3.1.5 --depth 1
+cd wxwidgets
+git submodule update --init
+mkdir build_cmake 
+cd build_cmake
+cmake .. -G "Visual Studio 16 2019" -A %BUILD_OPTION% -DwxBUILD_SHARED=OFF -DCMAKE_INSTALL_PREFIX=%HOMEPATH%/local
+cmake --build . --config %BUILD_TYPE% --target install
+cd ../../../..
+
+:: generate and build lib
+git submodule update --init
+mkdir build
+cd build
+cmake .. -G "Visual Studio 16 2019" -A %BUILD_OPTION% -DXTD_BUILD_TESTS_XTD_CORE=OFF -DXTD_BUILD_TESTS_XTD_DRAWING=OFF -DXTD_BUILD_TESTS_XTD_FORMS=OFF -DXTD_BUILD_TESTS_XTD_TUNIT=OFF -DCMAKE_INSTALL_PREFIX=%HOMEPATH%/local
+if %ERRORLEVEL% NEQ 0 exit 1
+cmake --build . --config %BUILD_TYPE% --target install
+if %ERRORLEVEL% NEQ 0 exit 1
+cd ..
+
+:: run registered unit tests
+cd build
+ctest --output-on-failure --build-config %BUILD_TYPE%
+if %ERRORLEVEL% NEQ 0 exit 1
+cd ..
+
+:: generate and build examples
+::mkdir build\examples
+::cd examples
+::cmake ../../examples -G "Visual Studio 16 2019" -A %BUILD_OPTION% -DCMAKE_INSTALL_PREFIX=%HOMEPATH%/local
+::if %ERRORLEVEL% NEQ 0 exit 1
+::cmake --build . --config %BUILD_TYPE%
+::if %ERRORLEVEL% NEQ 0 exit 1
+::cd ..\..
