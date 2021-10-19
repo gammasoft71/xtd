807156462e321600efa4917470ed86d5895873a9
Gammasoft <gammasoft71@gmail.com>
2021-10-19 23:44:11 +0200

    add some xtd::io classes definitions

---
+++ src/xtd.core/include/xtd/io/directory.h
@@ -0,0 +1,23 @@
+/// @file
+/// @brief Contains xtd::io::directory class.
+/// @copyright Copyright (c) 2021 Gammasoft. All rights reserved.
+#pragma once
+
+#include "../core_export.h"
+#include "../static.h"
+
+/// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
+namespace xtd {
+  /// @brief The xtd::io namespace contains types that allow reading and writing to files and data streams, and types that provide basic file and directory support.
+  namespace io {
+    /// @brief Provides static methods for the creation, copying, deletion, moving, and opening of Directories, and aids in the creation of xtd::io::directory_stream objects.
+    /// @par Namespace
+    /// xtd::io
+    /// @par Library
+    /// xtd.core
+    /// @ingroup xtd_core io
+    class core_export_ directory static_ {
+    public:
+    };
+  }
+}
---
+++ src/xtd.core/include/xtd/io/directory_info.h
@@ -0,0 +1,22 @@
+/// @file
+/// @brief Contains xtd::io::directory_info class.
+/// @copyright Copyright (c) 2021 Gammasoft. All rights reserved.
+#pragma once
+
+#include "file_system_info.h"
+
+/// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
+namespace xtd {
+  /// @brief The xtd::io namespace contains types that allow reading and writing to files and data streams, and types that provide basic file and directory support.
+  namespace io {
+    /// @brief Exposes instance methods for creating, moving, and enumerating through directories and subdirectories. This class cannot be inherited.
+    /// @par Namespace
+    /// xtd::io
+    /// @par Library
+    /// xtd.core
+    /// @ingroup xtd_core io
+    class core_export_ directory_info : public xtd::io::file_system_info {
+    public:
+    };
+  }
+}
