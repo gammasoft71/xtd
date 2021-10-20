3750da0360e13d9aa1b93f0d93df4c9adca22be0
Gammasoft <gammasoft71@gmail.com>
2021-10-20 11:12:44 +0200

    add xtd::native::drive

---
+++ src/xtd.core.native.win32/src/xtd/native/win32/drive.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#define __XTD_CORE_NATIVE_LIBRARY__
+#include <xtd/native/drive.h>
+#undef __XTD_CORE_NATIVE_LIBRARY__
+#include <string>
+#include <direct.h>
+#include <Windows.h>
+
+#undef max
+#undef min
+
+using namespace std;
+using namespace xtd::native;
