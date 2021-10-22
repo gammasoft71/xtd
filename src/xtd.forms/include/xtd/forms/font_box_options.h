ee9f80cad8aae4730aa41f79759af3d79aaf4340
BaderEddineOuaich <badereddineouaich@gmail.com>
2021-10-22 19:57:54 +0100

    Fixed spacing
    Fixed tab spacing of enum xtd::forms::font_box_options

--- src/xtd.forms/include/xtd/forms/font_box_options.h
+++ src/xtd.forms/include/xtd/forms/font_box_options.h
@@ -19,25 +19,25 @@ namespace xtd {
       /// @brief No options.
       none                 = 0b0,
       /// @brief Whether the dialog box allows graphics device interface (GDI) font simulations.
-	    allow_simulations    = 0b1,
+      allow_simulations    = 0b1,
       /// @brief Whether the dialog box allows vector font selections.
-	    allow_vector_fonts   = 0b10,
+      allow_vector_fonts   = 0b10,
       /// @brief Whether the dialog box displays both vertical and horizontal fonts or only horizontal fonts.
-	    allow_vertical_fonts = 0b100,
+      allow_vertical_fonts = 0b100,
       /// @brief Whether the user can change the character set specified in the Script combo box to display a character set other than the one currently displayed.
-	    allow_script_change  = 0b1000,
+      allow_script_change  = 0b1000,
       /// @brief Whether the dialog box allows only the selection of fixed-pitch fonts.
-	    fixed_pitch_only     = 0b10000,
+      fixed_pitch_only     = 0b10000,
       /// @brief Whether the dialog box allows selection of fonts for all non-OEM and Symbol character sets, as well as the ANSI character set.
-	    scripts_only         = 0b100000,
+      scripts_only         = 0b100000,
       /// @brief Whether the dialog box contains an Apply button.
-	    show_apply           = 0b1000000,
+      show_apply           = 0b1000000,
       /// @brief Whether the dialog box displays the color choice.
-	    show_color           = 0b10000000,
+      show_color           = 0b10000000,
       /// @brief Whether the dialog box contains controls that allow the user to specify strikethrough, underline, and text color options.
-	    show_effects         = 0b100000000,
+      show_effects         = 0b100000000,
       /// @brief Whether the dialog box displays a Help button.
-	    show_help            = 0b1000000000,
+      show_help            = 0b1000000000,
       /// @brief All flags except none.
       all                  = allow_simulations | allow_vector_fonts | allow_vertical_fonts | allow_script_change | fixed_pitch_only | scripts_only | show_apply | show_color | show_effects | show_help
     };
