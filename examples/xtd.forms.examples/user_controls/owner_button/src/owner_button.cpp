084ca4847ea7fe2c089e7a13bd20a53eaf173910
Gammasoft <gammasoft71@gmail.com>
2021-10-24 10:54:08 +0200

    Update owner_button.cpp

--- examples/xtd.forms.examples/user_controls/owner_button/src/owner_button.cpp
+++ examples/xtd.forms.examples/user_controls/owner_button/src/owner_button.cpp
@@ -8,31 +8,31 @@ using namespace xtd::forms;
 namespace examples {
   class owner_button : public button {
   public:
-    void on_paint(paint_event_args& e) {
-      auto background_color = back_color_.has_value() ? back_color_.value() : xtd::forms::theme_colors::current_theme().control();
-      auto foreground_color = fore_color_.has_value() ? fore_color_.value() : xtd::forms::theme_colors::current_theme().control_text();
-      auto button_color = control_paint::light(background_color, .05);
-      auto border_color = control_paint::light(background_color, .15);
+    void on_paint(paint_event_args& e) override {
+      auto accentuate_color = [](const color& c, float percent) {
+        if (application::dark_mode_enabled()) return control_paint::light(c, percent);
+        return control_paint::dark(c, percent);
+      };
+      auto background_color = back_color_.value_or(xtd::forms::theme_colors::current_theme().control());
+      auto foreground_color = fore_color_.value_or(xtd::forms::theme_colors::current_theme().control_text());
+      auto button_color = accentuate_color(background_color, .05);
+      auto border_color = accentuate_color(background_color, .15);
       auto text_color = foreground_color;
       
-      if (state() == xtd::forms::visual_styles::push_button_state::hot) {
-        border_color = control_paint::light(border_color, .15);
-        button_color = control_paint::light(background_color, .15);
-        text_color = control_paint::light(foreground_color, .15);
+      if (state() == xtd::forms::visual_styles::push_button_state::hot || state() == xtd::forms::visual_styles::push_button_state::default_state) {
+        border_color = accentuate_color(border_color, .15);
+        button_color = accentuate_color(background_color, .15);
+        text_color = accentuate_color(foreground_color, .15);
       } else if (state() == xtd::forms::visual_styles::push_button_state::pressed || state() == xtd::forms::visual_styles::push_button_state::checked) {
-        border_color = control_paint::light(border_color, .30);
-        button_color = control_paint::light(background_color, .30);
-        text_color = control_paint::light(foreground_color, .30);
+        border_color = accentuate_color(border_color, .30);
+        button_color = accentuate_color(background_color, .30);
+        text_color =  accentuate_color(foreground_color, .30);
       } else if (state() == xtd::forms::visual_styles::push_button_state::disabled) {
-        border_color = color::from_argb(85, 85, 55);
+        border_color = button_color = color::from_argb(85, 85, 55);
         text_color = xtd::forms::theme_colors::current_theme().gray_text();
-      } else if (state() == xtd::forms::visual_styles::push_button_state::default_state) {
-        border_color = control_paint::light(border_color, .15);
-        button_color = control_paint::light(background_color, .15);
-        text_color = control_paint::light(foreground_color, .15);
       }
       
-      e.graphics().fill_rounded_rectangle(drawing2d::linear_gradient_brush(point {e.clip_rectangle().x(), e.clip_rectangle().top()}, point {e.clip_rectangle().x(), e.clip_rectangle().bottom()}, control_paint::light(button_color, .1), control_paint::dark(button_color)), e.clip_rectangle().x(), e.clip_rectangle().y(), e.clip_rectangle().width(), e.clip_rectangle().height(), e.clip_rectangle().height() / 2);
+      e.graphics().fill_rounded_rectangle(drawing2d::linear_gradient_brush(point {e.clip_rectangle().x(), e.clip_rectangle().top()}, point {e.clip_rectangle().x(), e.clip_rectangle().bottom()}, control_paint::light(button_color, .2), control_paint::dark(button_color)), e.clip_rectangle().x(), e.clip_rectangle().y(), e.clip_rectangle().width(), e.clip_rectangle().height(), e.clip_rectangle().height() / 2);
       e.graphics().draw_rounded_rectangle(pen(border_color, 1), e.clip_rectangle().x(), e.clip_rectangle().y(), e.clip_rectangle().width() - 1, e.clip_rectangle().height() - 1, e.clip_rectangle().height() / 2);
       e.graphics().draw_string(text(), font(), solid_brush(text_color), rectangle(e.clip_rectangle().x() + 5, e.clip_rectangle().y() + 3, e.clip_rectangle().width() - 10, e.clip_rectangle().height() - 7), string_format().alignment(xtd::drawing::string_alignment::center).line_alignment(xtd::drawing::string_alignment::center));
     }
