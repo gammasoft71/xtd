40996c388a7412723482e67aefbd280bd7815713
Gammasoft <gammasoft71@gmail.com>
2021-10-24 11:04:12 +0200

    Update owner_button.cpp

--- examples/xtd.forms.examples/user_controls/owner_button/src/owner_button.cpp
+++ examples/xtd.forms.examples/user_controls/owner_button/src/owner_button.cpp
@@ -1,40 +1,38 @@
 #include <xtd/xtd>
 
-using namespace std;
 using namespace xtd;
 using namespace xtd::drawing;
 using namespace xtd::forms;
 
 namespace examples {
   class owner_button : public button {
-  public:
+  protected:
     void on_paint(paint_event_args& e) override {
       auto accentuate_color = [](const color& c, float percent) {
-        if (application::dark_mode_enabled()) return control_paint::light(c, percent);
-        return control_paint::dark(c, percent);
+        return application::dark_mode_enabled() ? control_paint::light(c, percent) : control_paint::dark(c, percent);
       };
-      auto background_color = back_color_.value_or(xtd::forms::theme_colors::current_theme().control());
-      auto foreground_color = fore_color_.value_or(xtd::forms::theme_colors::current_theme().control_text());
+      auto background_color = back_color_.value_or(theme_colors::current_theme().control());
+      auto foreground_color = fore_color_.value_or(theme_colors::current_theme().control_text());
       auto button_color = accentuate_color(background_color, .05);
       auto border_color = accentuate_color(background_color, .15);
       auto text_color = foreground_color;
       
-      if (state() == xtd::forms::visual_styles::push_button_state::hot || state() == xtd::forms::visual_styles::push_button_state::default_state) {
+      if (state() == visual_styles::push_button_state::hot || state() == visual_styles::push_button_state::default_state) {
         border_color = accentuate_color(border_color, .15);
         button_color = accentuate_color(background_color, .15);
         text_color = accentuate_color(foreground_color, .15);
-      } else if (state() == xtd::forms::visual_styles::push_button_state::pressed || state() == xtd::forms::visual_styles::push_button_state::checked) {
+      } else if (state() == visual_styles::push_button_state::pressed || state() == visual_styles::push_button_state::checked) {
         border_color = accentuate_color(border_color, .30);
         button_color = accentuate_color(background_color, .30);
         text_color =  accentuate_color(foreground_color, .30);
-      } else if (state() == xtd::forms::visual_styles::push_button_state::disabled) {
+      } else if (state() == visual_styles::push_button_state::disabled) {
         border_color = button_color = color::from_argb(85, 85, 55);
-        text_color = xtd::forms::theme_colors::current_theme().gray_text();
+        text_color = theme_colors::current_theme().gray_text();
       }
       
       e.graphics().fill_rounded_rectangle(drawing2d::linear_gradient_brush(point {e.clip_rectangle().x(), e.clip_rectangle().top()}, point {e.clip_rectangle().x(), e.clip_rectangle().bottom()}, control_paint::light(button_color, .2), control_paint::dark(button_color)), e.clip_rectangle().x(), e.clip_rectangle().y(), e.clip_rectangle().width(), e.clip_rectangle().height(), e.clip_rectangle().height() / 2);
       e.graphics().draw_rounded_rectangle(pen(border_color, 1), e.clip_rectangle().x(), e.clip_rectangle().y(), e.clip_rectangle().width() - 1, e.clip_rectangle().height() - 1, e.clip_rectangle().height() / 2);
-      e.graphics().draw_string(text(), font(), solid_brush(text_color), rectangle(e.clip_rectangle().x() + 5, e.clip_rectangle().y() + 3, e.clip_rectangle().width() - 10, e.clip_rectangle().height() - 7), string_format().alignment(xtd::drawing::string_alignment::center).line_alignment(xtd::drawing::string_alignment::center));
+      e.graphics().draw_string(text(), font(), solid_brush(text_color), rectangle(e.clip_rectangle().x() + 5, e.clip_rectangle().y() + 3, e.clip_rectangle().width() - 10, e.clip_rectangle().height() - 7), string_format().alignment(string_alignment::center).line_alignment(string_alignment::center));
     }
   };
 
