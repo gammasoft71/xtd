a328c25415c791780911a9059419eaf5f879058b
Gammasoft <gammasoft71@gmail.com>
2021-10-28 23:33:20 +0200

    Update install.sh

--- scripts/install/install.sh
+++ scripts/install/install.sh
@@ -25,7 +25,7 @@ echo "install needed packages and libraries..."
 case "$OSTYPE" in
   *"Darwin"*) brew update; brew install cmake;;
   *"Debian"* | *"elementary"* | *"LinuxMint"* | *"Ubuntu"*) sudo apt update; sudo apt install build-essential codeblocks doxygen libgsound-dev libgtk-3-dev cmake -y;;
-  *"openSUSE"*) sudo zypper update; sudo zypper in build-essential doxygen libgsound-devel gtk3-devel cmake -y;;
+  *"openSUSE"*) sudo zypper update; sudo zypper install -t pattern devel_basis; sudo zypper in doxygen gsound-devel gtk3-devel cmake -y;;
   *"CentOS"* | *"Fedora"* | *"RedHat"*) sudo yum update; sudo yum install cmake gsound-devel gtk3-devel -y;;
 esac
 
