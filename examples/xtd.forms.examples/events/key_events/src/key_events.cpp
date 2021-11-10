2b72039da725bfe1299dd39030605b1dabd4fdce
Gammasoft <gammasoft71@gmail.com>
2021-11-10 20:19:45 +0100

    use control for key events

--- examples/xtd.forms.examples/events/key_events/src/key_events.cpp
+++ examples/xtd.forms.examples/events/key_events/src/key_events.cpp
@@ -10,19 +10,25 @@ public:
   form1() {
     text("Key events example");
     
-    key_down += [&](object& sender, key_event_args& e) {
+    control1.dock(dock_style::fill);
+    control1.parent(*this);
+    
+    control1.key_down += [&](object& sender, key_event_args& e) {
       ctrace << ustring::format("key_down={{key_code={}, key_data=[{}], value=0x{:X4}, modifiers=[{}]}}", e.key_code(), e.key_data(), e.key_value(), e.modifiers()) << endl;
     };
     
-    key_press += [&](object& sender, key_press_event_args& e) {
+    control1.key_press += [&](object& sender, key_press_event_args& e) {
       ctrace << ustring::format("key_press={{key_char={}}}", e.key_char() == 0 ? "[none]" : ustring::format("'{}'", e.key_char())) << endl;
     };
     
-    key_up += [&](object& sender, key_event_args& e) {
+    control1.key_up += [&](object& sender, key_event_args& e) {
       ctrace << ustring::format("key_up={{key_code={}, key_data=[{}], value=0x{:X4}, modifiers=[{}]}}", e.key_code(), e.key_data(), e.key_value(), e.modifiers()) << endl;
       if (e.modifiers() == keys::none) ctrace << endl;
     };
   }
+  
+private:
+  control control1;
 };
 
 int main() {
