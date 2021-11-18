6b1e97fe36ce0adff4f84ef796210a4966d18598
Gammasoft <gammasoft71@gmail.com>
2021-11-18 17:25:21 +0100

    Update application.mm

--- src/xtd.forms.native.wxwidgets/src/xtd/forms/native/wxwidgets/application.mm
+++ src/xtd.forms.native.wxwidgets/src/xtd/forms/native/wxwidgets/application.mm
@@ -11,7 +11,7 @@ void __xtd_macos_enable_light_mode__() {
 }
 
 bool __xtd_macos_dark_mode_enabled__() {
-#if __MAC_OS_X_VERSION_MAX_ALLOWED <= MAC_OS_X_VERSION_10_15
+#if __ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ <= MAC_OS_X_VERSION_10_15
   return [[[NSAppearance currentAppearance] name] isEqualToString:(NSAppearanceNameDarkAqua)];
 #else
   return [[[NSAppearance currentDrawingAppearance] name] isEqualToString:(NSAppearanceNameDarkAqua)];
