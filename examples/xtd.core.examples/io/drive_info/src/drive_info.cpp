1bb0933fb48a5a0e1a62ba76f4a113f61c509679
Gammasoft <gammasoft71@gmail.com>
2021-11-25 12:42:32 +0100

    add drive_info example

---
+++ examples/xtd.core.examples/io/drive_info/src/drive_info.cpp
@@ -0,0 +1,50 @@
+#include <xtd/xtd>
+
+using namespace std;
+using namespace xtd;
+using namespace xtd::io;
+
+class program {
+public:
+  static void main() {
+    vector<drive_info> all_drives = drive_info::get_drives();
+    
+    for (drive_info d : all_drives) {
+      console::write_line("Drive {0}", d.name());
+      console::write_line("  Drive type: {0}", d.drive_type());
+      if (d.is_ready() == true) {
+        console::write_line("  Volume label: {0}", d.volume_label());
+        console::write_line("  File system: {0}", d.drive_format());
+        console::write_line("  Available space to current user:{0, 15} bytes", d.available_free_space());
+        console::write_line("  Total available space:          {0, 15} bytes", d.total_free_space());
+        console::write_line("  Total size of drive:            {0, 15} bytes", d.total_size());
+      }
+    }
+  }
+};
+
+startup_(program);
+
+// This code produces the following output :
+//
+// Drive A:\
+//   Drive type: Removable
+// Drive C:\
+//   Drive type: Fixed
+//   Volume label:
+//   File system: FAT32
+//   Available space to current user:     4770430976 bytes
+//   Total available space:               4770430976 bytes
+//   Total size of drive:                10731683840 bytes
+// Drive D:\
+//   Drive type: Fixed
+//   Volume label:
+//   File system: NTFS
+//   Available space to current user:    15114977280 bytes
+//   Total available space:              15114977280 bytes
+//   Total size of drive:                25958948864 bytes
+// Drive E:\
+//   Drive type: CDRom
+//
+// The actual output of this code will vary based on machine and the permissions
+// granted to the user executing it.
--- examples/xtd.core.examples/io/file_info/README.md
+++ examples/xtd.core.examples/io/file_info/README.md
@@ -16,9 +16,18 @@ Open "Command Prompt" or "Terminal". Navigate to the folder that contains the pr
 xtdc run
 ```
 
-# Output (with colors)
+# Output
 
 ```
-Hello, World!
+full_name = //var/folders/d5/k7mxnq214dxf3jbvvvhpbfqh0000gn/T/test_file.txt
+  attributes = normal
+  creation_time = Fri Oct 22 22:05:37 2021
+  exists = true
+  extension = .txt
+  last_access_time = Fri Oct 22 22:05:37 2021
+  last_write_time = Fri Oct 22 22:05:37 2021
+  length = 13
+  name = test_file.txt
+  to_string = /var/folders/d5/k7mxnq214dxf3jbvvvhpbfqh0000gn/T/test_file.txt
 ```
 
