d11ff4308c9e4e48c4caf033795d1eea992616db
Gammasoft <gammasoft71@gmail.com>
2021-11-29 16:30:16 +0100

    Update context_menu.cpp

--- examples/xtd.forms.examples/menus_and_toolbars/context_menu/src/context_menu.cpp
+++ examples/xtd.forms.examples/menus_and_toolbars/context_menu/src/context_menu.cpp
@@ -31,29 +31,29 @@ namespace examples {
     menu_item file_open_recent_file3_menu_item {"Path/File3", {*this, &form1::on_menu_click}};
     menu_item file_open_recent_file4_menu_item {"Path/File4", {*this, &form1::on_menu_click}};
 
-    menu_item file_new_menu_item {system_texts::new_(), {*this, &form1::on_menu_click}, menu_images::file_new(), shortcut::cmd_n};
+    menu_item file_new_menu_item {system_texts::new_(), {*this, &form1::on_menu_click}, menu_images::file_new()};
     menu_item file_separator1_menu_item {"-"};
-    menu_item file_open_menu_item {system_texts::open(), {*this, &form1::on_menu_click}, menu_images::file_open(), shortcut::cmd_o};
+    menu_item file_open_menu_item {system_texts::open(), {*this, &form1::on_menu_click}, menu_images::file_open()};
     menu_item file_open_recent_menu_item {"Open recent", {file_open_recent_file1_menu_item, file_open_recent_file2_menu_item, file_open_recent_file3_menu_item, file_open_recent_file4_menu_item}};
-    menu_item file_close_menu_item {system_texts::close(), {*this, &form1::on_menu_click}, shortcut::cmd_w};
+    menu_item file_close_menu_item {system_texts::close(), {*this, &form1::on_menu_click}};
     menu_item file_separator2_menu_item {"-"};
-    menu_item file_save_menu_item {system_texts::save(), {*this, &form1::on_menu_click}, menu_images::file_save(), shortcut::cmd_s};
+    menu_item file_save_menu_item {system_texts::save(), {*this, &form1::on_menu_click}, menu_images::file_save()};
     menu_item file_save_as_menu_item {system_texts::save_as(), {*this, &form1::on_menu_click}};
     menu_item file_separator3_menu_item {"-"};
     menu_item file_page_setup_menu_item {"Page &Seup...", {*this, &form1::on_menu_click}};
-    menu_item file_print_menu_item {system_texts::print(), {*this, &form1::on_menu_click}, menu_images::file_print(), shortcut::cmd_p};
+    menu_item file_print_menu_item {system_texts::print(), {*this, &form1::on_menu_click}, menu_images::file_print()};
     menu_item file_print_preview_menu_item {system_texts::print_preview(), {*this, &form1::on_menu_click}, menu_images::file_print_preview()};
     menu_item file_separator4_menu_item {"-"};
-    menu_item file_exit_menu_item {system_texts::exit(), {*this, &form1::on_menu_click}, menu_images::file_exit(), shortcut::alt_f4};
+    menu_item file_exit_menu_item {system_texts::exit(), {*this, &form1::on_menu_click}, menu_images::file_exit()};
 
-    menu_item edit_undo_menu_item {system_texts::undo(), {*this, &form1::on_menu_click}, menu_images::edit_undo(), shortcut::cmd_z};
-    menu_item edit_redo_menu_item {system_texts::redo(), {*this, &form1::on_menu_click}, menu_images::edit_redo(), shortcut::cmd_shift_z};
+    menu_item edit_undo_menu_item {system_texts::undo(), {*this, &form1::on_menu_click}, menu_images::edit_undo()};
+    menu_item edit_redo_menu_item {system_texts::redo(), {*this, &form1::on_menu_click}, menu_images::edit_redo()};
     menu_item edit_separator1_menu_item {"-"};
-    menu_item edit_cut_menu_item {system_texts::cut(), {*this, &form1::on_menu_click}, menu_images::edit_cut(), shortcut::cmd_x};
-    menu_item edit_copy_menu_item {system_texts::copy(), {*this, &form1::on_menu_click}, menu_images::edit_copy(), shortcut::cmd_c};
-    menu_item edit_paste_menu_item {system_texts::paste(), {*this, &form1::on_menu_click}, menu_images::edit_paste(), shortcut::cmd_v};
+    menu_item edit_cut_menu_item {system_texts::cut(), {*this, &form1::on_menu_click}, menu_images::edit_cut()};
+    menu_item edit_copy_menu_item {system_texts::copy(), {*this, &form1::on_menu_click}, menu_images::edit_copy()};
+    menu_item edit_paste_menu_item {system_texts::paste(), {*this, &form1::on_menu_click}, menu_images::edit_paste()};
     menu_item edit_separator2_menu_item {"-"};
-    menu_item edit_select_all_menu_item {system_texts::select_all(), {*this, &form1::on_menu_click}, shortcut::cmd_a};
+    menu_item edit_select_all_menu_item {system_texts::select_all(), {*this, &form1::on_menu_click}};
     menu_item edit_separator3_menu_item {"-"};
     menu_item edit_options_menu_item {system_texts::options(), {*this, &form1::on_menu_click}};
 
@@ -63,15 +63,15 @@ namespace examples {
     menu_item view_show_menu_item {"Show", {*this, &form1::on_menu_click}};
     menu_item view_hide_menu_item {"Hide", {*this, &form1::on_menu_click}};
 
-    menu_item options_value_a_menu_item {"Value A", {*this, &form1::on_menu_click}, menu_item_kind::check, true, shortcut::alt_1};
-    menu_item options_value_b_menu_item {"Value B", {*this, &form1::on_menu_click}, menu_item_kind::check, shortcut::alt_2};
-    menu_item options_value_c_menu_item {"Value C", {*this, &form1::on_menu_click}, menu_item_kind::check, true, shortcut::alt_3};
+    menu_item options_value_a_menu_item {"Value A", {*this, &form1::on_menu_click}, menu_item_kind::check, true};
+    menu_item options_value_b_menu_item {"Value B", {*this, &form1::on_menu_click}, menu_item_kind::check};
+    menu_item options_value_c_menu_item {"Value C", {*this, &form1::on_menu_click}, menu_item_kind::check, true};
     menu_item options_separator1_menu_item {"-"};
-    menu_item options_value_d_menu_item {"Value D", {*this, &form1::on_menu_click}, menu_item_kind::radio, static_cast<shortcut>(keys::alt|keys::d)};
-    menu_item options_value_e_menu_item {"Value E", {*this, &form1::on_menu_click}, menu_item_kind::radio, true, static_cast<shortcut>(keys::alt|keys::e)};
-    menu_item options_value_f_menu_item {"Value F", {*this, &form1::on_menu_click}, menu_item_kind::radio, static_cast<shortcut>(keys::alt|keys::f)};
+    menu_item options_value_d_menu_item {"Value D", {*this, &form1::on_menu_click}, menu_item_kind::radio};
+    menu_item options_value_e_menu_item {"Value E", {*this, &form1::on_menu_click}, menu_item_kind::radio, true};
+    menu_item options_value_f_menu_item {"Value F", {*this, &form1::on_menu_click}, menu_item_kind::radio};
     menu_item options_separator2_menu_item {"-"};
-    menu_item options_value_g_menu_item {"Value G", {*this, &form1::on_menu_click}, menu_item_kind::radio, static_cast<shortcut>(keys::alt|keys::shift|keys::left)};
+    menu_item options_value_g_menu_item {"Value G", {*this, &form1::on_menu_click}, menu_item_kind::radio};
     menu_item options_value_h_menu_item {"Value H", {*this, &form1::on_menu_click}, menu_item_kind::radio};
     menu_item options_value_i_menu_item {"Value I", {*this, &form1::on_menu_click}, menu_item_kind::radio};
 
