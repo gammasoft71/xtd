a1964dd26455c65c6743f40006d5140e20043c2c
Gammasoft <gammasoft71@gmail.com>
2021-12-02 10:41:07 +0100

    Update directory.cpp

--- tests/xtd.core.unit_tests/src/xtd/io/directory.cpp
+++ tests/xtd.core.unit_tests/src/xtd/io/directory.cpp
@@ -54,7 +54,7 @@ namespace unit_tests {
     }
     
     void test_method_(create_directory_with_path_too_long) {
-      assert::throws<path_too_long_exception>([]{directory::create_directory("directory"_s + ustring(250, '1'));}, csf_);
+      assert::throws<path_too_long_exception>([]{directory::create_directory("directory"_s + ustring(260, '1'));}, csf_);
     }
   };
 
