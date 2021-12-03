7c4702d4168b6f008aeb280951ec290c93cc01e9
Gammasoft <gammasoft71@gmail.com>
2021-12-03 22:59:00 +0100

    add data for members

--- src/xtd.forms/src/xtd/forms/animation.cpp
+++ src/xtd.forms/src/xtd/forms/animation.cpp
@@ -9,40 +9,44 @@ using namespace xtd::forms;
 animation::animation() {
   double_buffered(true);
   size(default_size());
-  frames_timer_.tick += {*this, &animation::on_frames_timer_tick};
+  data_->frames_timer.tick += {*this, &animation::on_frames_timer_tick};
+}
+
+drawing::size animation::default_size() const {
+  return {200, 100};
 }
 
 int64_t animation::elapsed_milliseconds() const {
-  return stopwatch_.elapsed_nanoseconds();
+  return data_->stopwatch.elapsed_nanoseconds();
 }
 
 int32_t animation::frame_counter() const {
-  return frame_counter_;
+  return data_->frame_counter;
 }
 
 int32_t animation::frames_per_second() const {
-  return frames_per_second_;
+  return data_->frames_per_second;
 }
 
 animation& animation::frames_per_second(int32_t value) {
-  if (frames_per_second_ != value) {
-    frames_per_second_ = value;
-    if (!frames_per_second_) frames_timer_.interval_milliseconds(std::numeric_limits<int32_t>::max());
-    else frames_timer_.interval_milliseconds(static_cast<int32_t>(ceil(1000.0 / frames_per_second_)));
+  if (data_->frames_per_second != value) {
+    data_->frames_per_second = value;
+    if (!data_->frames_per_second) data_->frames_timer.interval_milliseconds(std::numeric_limits<int32_t>::max());
+    else data_->frames_timer.interval_milliseconds(static_cast<int32_t>(ceil(1000.0 / data_->frames_per_second)));
   }
   return *this;
 }
 
 bool animation::is_running() const {
-  return frames_timer_.enabled();
+  return data_->frames_timer.enabled();
 }
 
 void animation::start() {
-  frames_timer_.enabled(frames_per_second_);
+  data_->frames_timer.enabled(data_->frames_per_second);
 }
 
 void animation::stop() {
-  frames_timer_.enabled(false);
+  data_->frames_timer.enabled(false);
 }
 
 void animation::on_updated(const animation_updated_event_args& e) {
@@ -50,8 +54,8 @@ void animation::on_updated(const animation_updated_event_args& e) {
 }
 
 void animation::on_frames_timer_tick(object& timer, const event_args& e) {
-  ++frame_counter_;
-  on_updated(animation_updated_event_args(frame_counter_, stopwatch_.elapsed_milliseconds()));
+  ++data_->frame_counter;
+  on_updated(animation_updated_event_args(data_->frame_counter, data_->stopwatch.elapsed_milliseconds()));
   invalidate();
-  stopwatch_ = stopwatch::start_new();
+  data_->stopwatch = stopwatch::start_new();
 }
