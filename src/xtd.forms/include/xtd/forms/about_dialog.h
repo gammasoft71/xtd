ed67beeb5ef1e29a0b2817fb1b0664e81001246f
Gammasoft <gammasoft71@gmail.com>
2021-12-03 22:47:27 +0100

    add data for members

--- src/xtd.forms/include/xtd/forms/about_dialog.h
+++ src/xtd.forms/include/xtd/forms/about_dialog.h
@@ -63,181 +63,181 @@ namespace xtd {
       
       /// @brief Gets the artists array.
       /// @return The artists array.
-      const artist_collection& artists() const {return artists_;}
+      const artist_collection& artists() const {return data_->artists;}
       /// @brief Gets the artists array.
       /// @return The artists array.
-      artist_collection& artists() {return artists_;}
+      artist_collection& artists() {return data_->artists;}
       /// @brief Sets the artists array.
       /// @param artists The artists array.
       /// @return Current about_dialog instance.
       about_dialog& artists(const artist_collection& artists) {
-        artists_ = artists;
+        data_->artists = artists;
         return *this;
       }
 
       /// @brief Gets the authors array.
       /// @return The author array.
-      const author_collection& authors() const {return authors_;}
+      const author_collection& authors() const {return data_->authors;}
       /// @brief Gets the authors array.
       /// @return The authors array.
-      author_collection& authors() {return authors_;}
+      author_collection& authors() {return data_->authors;}
       /// @brief Sets the authors array.
       /// @param authors The authors array.
       /// @return Current about_dialog instance.
       about_dialog& authors(const author_collection& authors) {
-        authors_ = authors;
+        data_->authors = authors;
         return *this;
       }
       
       /// @brief Gets the product copyright.
       /// @return The product copyright.
-      xtd::ustring copyright() const {return copyright_;}
+      xtd::ustring copyright() const {return data_->copyright;}
       /// @brief Sets the product description.
       /// @param copyright The description copyright.
       /// @return Current about_dialog instance.
       about_dialog& copyright(const xtd::ustring& copyright) {
-        copyright_ = copyright;
+        data_->copyright = copyright;
         return *this;
       }
 
       /// @brief Gets the product description.
       /// @return The product description.
-      xtd::ustring description() const {return description_;}
+      xtd::ustring description() const {return data_->description;}
       /// @brief Sets the product description.
       /// @param description The product description.
       /// @return Current about_dialog instance.
       about_dialog& description(const xtd::ustring& description) {
-        description_ = description;
+        data_->description = description;
         return *this;
       }
 
       /// @brief Gets the documentation writers array.
       /// @return The documentation writers array.
-      const documenter_collection& documenters() const {return documenters_;}
+      const documenter_collection& documenters() const {return data_->documenters;}
       /// @brief Gets the artists array.
       /// @return The artists array.
-      documenter_collection& documenters() {return documenters_;}
+      documenter_collection& documenters() {return data_->documenters;}
       /// @brief Sets the documenters array.
       /// @param documenters The documenters array.
       /// @return Current about_dialog instance.
       about_dialog& documenters(const documenter_collection& documenters) {
-        documenters_ = documenters;
+        data_->documenters = documenters;
         return *this;
       }
 
       /// @brief Gets the dialog style.
       /// @return One of the xtd::forms::dialog_style values. The default value is xtd::forms::dialog_style::standard.
-      xtd::forms::dialog_style dialog_style() const {return dialog_style_;}
+      xtd::forms::dialog_style dialog_style() const {return data_->dialog_style;}
       /// @brief Sets the dialog style.
       /// @param dialog_style One of the xtd::forms::dialog_style values. The default value is xtd::forms::dialog_style::standard.
       /// @return Current about_dialog instance.
       about_dialog& dialog_style(xtd::forms::dialog_style dialog_style) {
-        dialog_style_ = dialog_style;
+        data_->dialog_style = dialog_style;
         return *this;
       }
 
       /// @brief Gets the product icon.
       /// @return The product icon.
-      xtd::drawing::icon icon() const {return xtd::drawing::icon::from_bitmap(xtd::drawing::bitmap(icon_));}
+      xtd::drawing::icon icon() const {return xtd::drawing::icon::from_bitmap(xtd::drawing::bitmap(data_->icon));}
       /// @brief Sets the product icon.
       /// @param icon The product icon.
       /// @return Current about_dialog instance.
       about_dialog& icon(const xtd::drawing::icon& icon) {
-        icon_ = icon.to_bitmap();
+        data_->icon = icon.to_bitmap();
         return *this;
       }
       /// @brief Sets the product icon.
       /// @param image The product icon.
       /// @return Current about_dialog instance.
       about_dialog& icon(const xtd::drawing::image& image) {
-        icon_ = image;
+        data_->icon = image;
         return *this;
       }
       /// @brief Sets the product icon.
       /// @param bitmap The product icon.
       /// @return Current about_dialog instance.
       about_dialog& icon(const xtd::drawing::bitmap& bitmap) {
-        icon_ = bitmap;
+        data_->icon = bitmap;
         return *this;
       }
 
       /// @brief Gets the product license.
       /// @return The product license.
-      xtd::ustring license() const {return license_;}
+      xtd::ustring license() const {return data_->license;}
       /// @brief Sets the product license.
       /// @param name The product license.
       /// @return Current about_dialog instance.
       about_dialog& license(const xtd::ustring& license) {
-        license_ = license;
+        data_->license = license;
         return *this;
       }
 
       /// @brief Gets the product long version.
       /// @return The product long version.
-      xtd::ustring long_version() const {return long_version_;}
+      xtd::ustring long_version() const {return data_->long_version;}
       /// @brief Sets the product long version.
       /// @param version The version version.
       /// @return Current about_dialog instance.
       about_dialog& long_version(const xtd::ustring& long_version) {
-        long_version_ = long_version;
+        data_->long_version = long_version;
         return *this;
       }
 
       /// @brief Gets the product name.
       /// @return The product name.
-      xtd::ustring name() const {return name_;}
+      xtd::ustring name() const {return data_->name;}
       /// @brief Sets the product name.
       /// @param name The product name.
       /// @return Current about_dialog instance.
       about_dialog& name(const xtd::ustring& name) {
-        name_ = name;
+        data_->name = name;
         return *this;
       }
       
       /// @brief Gets the translators array.
       /// @return The translators array.
-      const translator_collection& translators() const {return translators_;}
+      const translator_collection& translators() const {return data_->translators;}
       /// @brief Gets the translators array.
       /// @return The translators array.
-      translator_collection& translators() {return translators_;}
+      translator_collection& translators() {return data_->translators;}
       /// @brief Sets the translators array.
       /// @param translators The translators array.
       /// @return Current about_dialog instance.
       about_dialog& translators(const translator_collection& translators) {
-        translators_ = translators;
+        data_->translators = translators;
         return *this;
       }
 
       /// @brief Gets the product version.
       /// @return The product version.
-      xtd::ustring version() const {return version_;}
+      xtd::ustring version() const {return data_->version;}
       /// @brief Sets the product version.
       /// @param version The version version.
       /// @return Current about_dialog instance.
       about_dialog& version(const xtd::ustring& version) {
-        version_ = version;
+        data_->version = version;
         return *this;
       }
       
       /// @brief Gets the product website.
       /// @return The website.
-      xtd::ustring website() const {return website_;}
+      xtd::ustring website() const {return data_->website;}
       /// @brief Sets the product website.
       /// @param website The  product website.
       /// @return Current about_dialog instance.
       about_dialog& website(const xtd::ustring& website) {
-        website_ = website;
+        data_->website = website;
         return *this;
       }
       
       /// @brief Gets the product website label.
       /// @return The product website label.
-      xtd::ustring website_label() const {return website_label_;}
+      xtd::ustring website_label() const {return data_->website_label;}
       /// @brief Sets the product website label.
       /// @param website_label The product website label.
       /// @return Current about_dialog instance.
       about_dialog& website_label(const xtd::ustring& website_label) {
-        website_label_ = website_label;
+        data_->website_label = website_label;
         return *this;
       }
 
@@ -250,20 +250,23 @@ namespace xtd {
       void show(const iwin32_window& owner);
 
     private:
-      xtd::forms::dialog_style dialog_style_ = xtd::forms::dialog_style::standard;
-      xtd::drawing::image icon_;
-      xtd::ustring name_;
-      xtd::ustring version_;
-      xtd::ustring long_version_;
-      xtd::ustring description_;
-      xtd::ustring copyright_;
-      xtd::ustring website_;
-      xtd::ustring website_label_;
-      author_collection authors_;
-      documenter_collection documenters_;
-      translator_collection translators_;
-      artist_collection artists_;
-      xtd::ustring license_;
+      struct data {
+        xtd::forms::dialog_style dialog_style = xtd::forms::dialog_style::standard;
+        xtd::drawing::image icon;
+        xtd::ustring name;
+        xtd::ustring version;
+        xtd::ustring long_version;
+        xtd::ustring description;
+        xtd::ustring copyright;
+        xtd::ustring website;
+        xtd::ustring website_label;
+        author_collection authors;
+        documenter_collection documenters;
+        translator_collection translators;
+        artist_collection artists;
+        xtd::ustring license;
+      };
+      std::shared_ptr<data> data_ = std::make_shared<data>();
     };
   }
 }
