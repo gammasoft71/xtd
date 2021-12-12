fae7dc97f968acff9af80e0d5d4f3bc1abc90eec
Gammasoft <gammasoft71@gmail.com>
2021-12-12 21:45:47 +0100

    fix anchor

--- examples/xtd.forms.examples/containers/panel/src/panel.cpp
+++ examples/xtd.forms.examples/containers/panel/src/panel.cpp
@@ -10,17 +10,17 @@ public:
     location({200, 200});
     client_size({640, 480});
     
-    panel1.anchor(anchor_styles::top | anchor_styles::left | anchor_styles::bottom);
     panel1.parent(*this);
     panel1.border_style(forms::border_style::fixed_single);
     panel1.location({10, 10});
     panel1.size({305, 460});
+    panel1.anchor(anchor_styles::top | anchor_styles::left | anchor_styles::bottom);
 
-    panel2.anchor(anchor_styles::top | anchor_styles::left | anchor_styles::bottom | anchor_styles::right);
     panel2.parent(*this);
     panel2.border_style(forms::border_style::fixed_3d);
     panel2.location({325, 10});
     panel2.size({305, 460});
+    panel2.anchor(anchor_styles::top | anchor_styles::left | anchor_styles::bottom | anchor_styles::right);
   }
   
 private:
