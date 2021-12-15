8e75245c452efa093a87434c810d03505a7f5c61
BaderEddineOuaich <badereddineouaich@gmail.com>
2021-12-15 13:15:49 +0100

    Added TODO file

--- tests/xtd.forms.manual_tests/src/manual_tests.cpp
+++ tests/xtd.forms.manual_tests/src/manual_tests.cpp
@@ -17,9 +17,9 @@ public:
      
     m_month_calendar.parent(*this);
     m_month_calendar.location({ 10, 10 });
-    m_month_calendar.size({ 250, 200 });
+   // m_month_calendar.size({ 250, 200 });
     m_month_calendar.value(system_clock::now());
-    m_month_calendar.range_date(system_clock::now() - months(3), system_clock::now() + months(3));
+    m_month_calendar.date_range(system_clock::now() - months(3), system_clock::now() + months(3));
     m_month_calendar.value_changed += [](object&, const event_args& e) {
       message_box::show(ustring::format("month_calendar value changed: {}", e.to_string()));
     };
