a2479eae49947e3056a81b60608138e8bfe4222a
Bader <49657842+BaderEddineOuaich@users.noreply.github.com>
2021-12-16 19:17:23 +0100

    Fixed tab spaces

--- src/xtd.forms.native.wxwidgets/src/xtd/forms/native/wxwidgets/month_calendar.cpp
+++ src/xtd.forms.native.wxwidgets/src/xtd/forms/native/wxwidgets/month_calendar.cpp
@@ -9,26 +9,26 @@ using namespace xtd::drawing;
 using namespace xtd::forms::native;
 
 void month_calendar::value(const intptr_t control, const std::chrono::system_clock::time_point date) {
-	if (!control || !wxTheApp) throw argument_exception(csf_);
-	if (!reinterpret_cast<control_handler*>(control)->control()) {
-		wxASSERT_MSG_AT(reinterpret_cast<control_handler*>(control)->control() == 0, "Control is null", __FILE__, __LINE__, __func__);
-		return;
-	}
+  if (!control || !wxTheApp) throw argument_exception(csf_);
+  if (!reinterpret_cast<control_handler*>(control)->control()) {
+    wxASSERT_MSG_AT(reinterpret_cast<control_handler*>(control)->control() == 0, "Control is null", __FILE__, __LINE__, __func__);
+    return;
+  }
 
-	if (wxCalendarCtrl* wx_calendar_ctrl = dynamic_cast<wxCalendarCtrl*>(reinterpret_cast<control_handler*>(control)->control())) {
-		wx_calendar_ctrl->SetDate(wxDateTime(std::chrono::system_clock::to_time_t(date)));
-	}
+  if (wxCalendarCtrl* wx_calendar_ctrl = dynamic_cast<wxCalendarCtrl*>(reinterpret_cast<control_handler*>(control)->control())) {
+    wx_calendar_ctrl->SetDate(wxDateTime(std::chrono::system_clock::to_time_t(date)));
+  }
 }
 
 std::chrono::system_clock::time_point month_calendar::value(const intptr_t control) {
-	if (!control || !wxTheApp) throw argument_exception(csf_);
-	if (!reinterpret_cast<control_handler*>(control)->control()) {
-		wxASSERT_MSG_AT(reinterpret_cast<control_handler*>(control)->control() == 0, "Control is null", __FILE__, __LINE__, __func__);
-		return std::chrono::system_clock::time_point();
-	}
+  if (!control || !wxTheApp) throw argument_exception(csf_);
+  if (!reinterpret_cast<control_handler*>(control)->control()) {
+    wxASSERT_MSG_AT(reinterpret_cast<control_handler*>(control)->control() == 0, "Control is null", __FILE__, __LINE__, __func__);
+    return std::chrono::system_clock::time_point();
+  }
 
-	if (wxCalendarCtrl* wx_calendar_ctrl = dynamic_cast<wxCalendarCtrl*>(reinterpret_cast<control_handler*>(control)->control())) {
-		return std::chrono::system_clock::from_time_t(wx_calendar_ctrl->GetDate().GetTicks());
-	}
-	return std::chrono::system_clock::time_point();
+  if (wxCalendarCtrl* wx_calendar_ctrl = dynamic_cast<wxCalendarCtrl*>(reinterpret_cast<control_handler*>(control)->control())) {
+    return std::chrono::system_clock::from_time_t(wx_calendar_ctrl->GetDate().GetTicks());
+  }
+  return std::chrono::system_clock::time_point();
 }
