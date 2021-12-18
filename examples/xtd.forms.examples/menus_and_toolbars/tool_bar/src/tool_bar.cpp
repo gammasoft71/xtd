5987cf4de84fe8150b586ab84a0702d17d66e1fb
Gammasoft <gammasoft71@gmail.com>
2021-12-18 22:25:50 +0100

    Update tool_bar.cpp

--- examples/xtd.forms.examples/menus_and_toolbars/tool_bar/src/tool_bar.cpp
+++ examples/xtd.forms.examples/menus_and_toolbars/tool_bar/src/tool_bar.cpp
@@ -9,13 +9,17 @@ namespace examples {
     form1() {
       text("Toolbar example");
       client_size({820, 500});
-      controls().push_back_range({list_box1, tool_bar1});
+      controls().push_back_range({list_box1, tool_bar2, tool_bar1});
       tool_bar(tool_bar1);
-
+      
       list_box1.dock(dock_style::fill);
       
       tool_bar1.image_list().images().push_back_range({tool_bar_images::file_new(), tool_bar_images::file_open(), tool_bar_images::file_save(), tool_bar_images::file_print(), tool_bar_images::edit_cut(), tool_bar_images::edit_copy(), tool_bar_images::edit_paste(), tool_bar_images::help_about()});
-      tool_bar1.items().push_back_range({new_tool_bar_button, open_tool_bar_button, save_tool_bar_button, print_tool_bar_button, tool_bar_separator1, cut_tool_bar_button, copy_tool_bar_button, paste_tool_bar_button, tool_bar_separator2, about_tool_bar_button});
+      tool_bar1.items().push_back_range({new_tool_bar_button, open_tool_bar_button, save_tool_bar_button, print_tool_bar_button, tool_bar1_separator1, cut_tool_bar_button, copy_tool_bar_button, paste_tool_bar_button, tool_bar1_separator2, about_tool_bar_button});
+      
+      tool_bar2.dock(dock_style::bottom);
+      tool_bar2.image_list().images().push_back_range({tool_bar_images::from_name("media-playback-start"), tool_bar_images::from_name("media-skip-backward"), tool_bar_images::from_name("media-playback-stop"), tool_bar_images::from_name("media-skip-forward"), tool_bar_images::from_name("media-record"), tool_bar_images::from_name("media-eject")});
+      tool_bar2.items().push_back_range({play_tool_bar_button, tool_bar2_separator1, skip_backward_tool_bar_button, stop_tool_bar_button, skip_forward_tool_bar_button, tool_bar2_separator2, record_tool_bar_button, tool_bar2_separator3, eject_tool_bar_button});
      }
     
   private:
@@ -30,12 +34,23 @@ namespace examples {
     tool_bar_button open_tool_bar_button {system_texts::open(), 1, {*this, &form1::on_tool_bar_item_click}};
     tool_bar_button save_tool_bar_button {system_texts::save(), 2, {*this, &form1::on_tool_bar_item_click}};
     tool_bar_button print_tool_bar_button {system_texts::print(), 3, {*this, &form1::on_tool_bar_item_click}};
-    tool_bar_separator tool_bar_separator1;
+    tool_bar_separator tool_bar1_separator1;
     tool_bar_button cut_tool_bar_button {system_texts::cut(), 4, {*this, &form1::on_tool_bar_item_click}};
     tool_bar_button copy_tool_bar_button {system_texts::copy(), 5, {*this, &form1::on_tool_bar_item_click}};
     tool_bar_button paste_tool_bar_button {system_texts::paste(), 6, {*this, &form1::on_tool_bar_item_click}};
-    tool_bar_separator tool_bar_separator2;
+    tool_bar_separator tool_bar1_separator2;
     tool_bar_button about_tool_bar_button {system_texts::about(), 7, {*this, &form1::on_tool_bar_item_click}};
+
+    forms::tool_bar tool_bar2;
+    tool_bar_button play_tool_bar_button {"Play", 0, {*this, &form1::on_tool_bar_item_click}};
+    tool_bar_separator tool_bar2_separator1;
+    tool_bar_button skip_backward_tool_bar_button {"Skip backward", 1, {*this, &form1::on_tool_bar_item_click}};
+    tool_bar_button stop_tool_bar_button {"Stop", 2, {*this, &form1::on_tool_bar_item_click}};
+    tool_bar_button skip_forward_tool_bar_button {"Skip forward", 3, {*this, &form1::on_tool_bar_item_click}};
+    tool_bar_separator tool_bar2_separator2;
+    tool_bar_button record_tool_bar_button {"Record", 4, {*this, &form1::on_tool_bar_item_click}};
+    tool_bar_separator tool_bar2_separator3;
+    tool_bar_button eject_tool_bar_button {"Eject", 5, {*this, &form1::on_tool_bar_item_click}};
   };
 }
 
