9f4fc2800580be8d2ef2d91ee82699f06a676b21
Gammasoft <gammasoft71@gmail.com>
2021-12-20 19:36:14 +0100

    review date_time_picker

--- src/xtd.forms/src/xtd/forms/date_time_picker.cpp
+++ src/xtd.forms/src/xtd/forms/date_time_picker.cpp
@@ -10,6 +10,19 @@
 using namespace xtd;
 using namespace xtd::forms;
 
+namespace {
+  static std::tm make_tm(int32_t year, int32_t month, int32_t day, int32_t hour, int32_t minute, int32_t second) {
+    std::tm date = {};
+    date.tm_year = year - 1900;
+    date.tm_mon = month - 1;
+    date.tm_mday = day;
+    date.tm_hour = hour;
+    date.tm_min = minute;
+    date.tm_sec = second;
+    return date;
+  }
+}
+
 control& date_time_picker::format(date_time_picker_format format) {
   if (format_ != format) {
     format_ = format;
@@ -18,33 +31,101 @@ control& date_time_picker::format(date_time_picker_format format) {
   return *this;
 }
 
-control& date_time_picker::max_date(std::chrono::system_clock::time_point max_date) {
-  if (max_date_ != max_date) {
-    max_date_ = max_date;
+std::chrono::system_clock::time_point date_time_picker::max_date() const {
+  return max_date_;
+}
+
+control& date_time_picker::max_date(std::chrono::system_clock::time_point value) {
+  if (max_date_ != value) {
+    max_date_ = value;
     if (max_date_ < min_date_) min_date_ = max_date_;
-   value(value_);
+    this->value(value_);
   }
   return *this;
 }
 
-control& date_time_picker::min_date(std::chrono::system_clock::time_point min_date) {
-  if (min_date_ != min_date) {
-    min_date_ = min_date;
+control& date_time_picker::max_date(time_t value) {
+  return max_date(std::chrono::system_clock::from_time_t(value));
+}
+
+control& date_time_picker::max_date(const std::tm& value) {
+  std::tm date = value;
+  return this->max_date(mktime(&date));
+}
+
+
+control& date_time_picker::max_date(int32_t year, int32_t month, int32_t day) {
+  return max_date(year, month, day, 0, 0, 0);
+}
+
+control& date_time_picker::max_date(int32_t year, int32_t month, int32_t day, int32_t hour, int32_t minute, int32_t second) {
+  std::tm date = make_tm(year, month, day, hour, minute, second);
+  return max_date(mktime(&date));
+}
+
+std::chrono::system_clock::time_point date_time_picker::min_date() const {
+  return min_date_;
+}
+
+control& date_time_picker::min_date(std::chrono::system_clock::time_point value) {
+  if (min_date_ != value) {
+    min_date_ = value;
     if (max_date_ < min_date_) max_date_ = min_date_;
-    value(value_);
+    this->value(value_);
   }
   return *this;
+}
+
+control& date_time_picker::min_date(time_t value) {
+  return this->min_date(std::chrono::system_clock::from_time_t(value));
+}
+
+control& date_time_picker::min_date(const std::tm& value) {
+  std::tm date = value;
+  return this->min_date(mktime(&date));
+}
+
+control& date_time_picker::min_date(int32_t year, int32_t month, int32_t day) {
+  return min_date(year, month, day, 0, 0, 0);
+}
+
+
+control& date_time_picker::min_date(int32_t year, int32_t month, int32_t day, int32_t hour, int32_t minute, int32_t second) {
+  std::tm date = make_tm(year, month, day, hour, minute, second);
+  return min_date(mktime(&date));
+}
+
+std::chrono::system_clock::time_point date_time_picker::value() const {
+  return value_;
 }
 
 control& date_time_picker::value(std::chrono::system_clock::time_point value) {
+  if (value < min_date_) value = min_date_;
+  if (value > max_date_) value = max_date_;
   if (value_ != value) {
-    if (value < min_date_) value = min_date_;
-    if (value > max_date_) value = max_date_;
     value_ = value;
     if (is_handle_created()) native::date_time_picker::value(handle(), value_);
     on_value_changed(event_args::empty);
   }
   return *this;
+}
+
+control& date_time_picker::value(time_t value) {
+  return this->value(std::chrono::system_clock::from_time_t(value));
+}
+
+control& date_time_picker::value(const std::tm& value) {
+  std::tm date = value;
+  return this->value(mktime(&date));
+}
+
+control& date_time_picker::value(int32_t year, int32_t month, int32_t day) {
+  return value(year, month, day, 0, 0, 0);
+}
+
+control& date_time_picker::value(int32_t year, int32_t month, int32_t day, int32_t hour, int32_t minute, int32_t second) {
+  std::tm date = make_tm(year, month, day, hour, minute, second);
+  return value(mktime(&date));
 }
 
 forms::create_params date_time_picker::create_params() const {
