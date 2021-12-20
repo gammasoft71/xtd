d20bdb97f94f814a4fe4e91c9391a7d22ac75f22
Gammasoft <gammasoft71@gmail.com>
2021-12-20 22:53:39 +0100

    create xtd::date_time

---
+++ src/xtd.core/include/xtd/date_time.h
@@ -0,0 +1,74 @@
+#pragma once
+#include <chrono>
+#include <ctime>
+#include "object.h"
+#include "ticks.h"
+#include "ustring.h"
+
+namespace xtd {
+  enum class date_time_kind {
+    unspecified,
+    utc,
+    local,
+  };
+  
+  enum class day_of_week {
+    sunday,
+    monday,
+    tuesday,
+    wednesday,
+    thursday,
+    friday,
+    saturday
+  };
+  
+  class core_export_ date_time : public object {
+  public:
+    /// @name Fields
+    
+    /// @{
+    static date_time max_value;
+    static date_time min_value;
+    /// @}
+    
+    /// @name Constructors
+    
+    /// @{
+    date_time() = default;
+    date_time(xtd::ticks ticks);
+    /// @}
+    
+    /// @cond
+    date_time(const date_time&) = default;
+    date_time(date_time&&) = default;
+    date_time& operator=(const date_time&) = default;
+    /// @endcond
+    
+
+    /// @name Properties
+
+    /// @{
+    date_time_kind kind() const noexcept;
+    
+    /// @}
+    
+    
+    /// @name Methods
+    
+    /// @{
+    static date_time from_time_t(std::time_t value);
+    static date_time now();
+    xtd::ustring to_string() const noexcept override;
+    xtd::ustring to_string(const ustring& format) const;
+    std::time_t to_time_t() const;
+    /// @}
+    
+  private:
+    xtd::ticks value_ {0};
+    date_time_kind kind_ {date_time_kind::unspecified};
+  };
+  
+  /// @cond
+  std::ostream& operator <<(std::ostream& os, const date_time& ver) noexcept;
+  /// @endcond
+}
