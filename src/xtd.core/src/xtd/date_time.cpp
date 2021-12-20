4d1847cf83cc316cf7dd5f41cea5bfb598389730
Gammasoft <gammasoft71@gmail.com>
2021-12-21 00:08:58 +0100

    date_time implementation

--- src/xtd.core/src/xtd/date_time.cpp
+++ src/xtd.core/src/xtd/date_time.cpp
@@ -5,13 +5,11 @@ using namespace std::chrono;
 using namespace xtd;
 
 namespace {
-  constexpr ticks ticks_to_1970 = ticks(621672202500000000LL);
-  constexpr ticks date_time_minimum_value = ticks(0);
-  constexpr ticks date_time_maximum_value = ticks(2534022971990000000LL) + ticks_to_1970;
+  constexpr ticks ticks_offset_1970 = ticks(621672202500000000LL);
 };
 
-date_time date_time::max_value {date_time_maximum_value};
-date_time date_time::min_value {date_time_minimum_value};
+date_time date_time::max_value = xtd::ticks(2534022971990000000LL) + ticks_offset_1970;
+date_time date_time::min_value = xtd::ticks(0);
 
 date_time::date_time(xtd::ticks ticks) : value_(ticks) {  
 }
@@ -20,9 +18,13 @@ date_time_kind date_time::kind() const noexcept {
   return kind_;
 }
 
+xtd::ticks date_time::ticks() const noexcept {
+  return value_;
+}
+
 date_time date_time::from_time_t(std::time_t value) {
   date_time result;
-  result.value_ = duration_cast<ticks>(chrono::seconds(value)) + ticks_to_1970;
+  result.value_ = duration_cast<xtd::ticks>(chrono::seconds(value)) + ticks_offset_1970;
   return result;
 }
 
@@ -31,15 +33,16 @@ date_time date_time::now() {
 }
 
 xtd::ustring date_time::to_string() const noexcept {
-  return to_string("{:u}");
+  return to_string("G");
 }
 
 ustring date_time::to_string(const ustring& format) const {
-  return ustring::format(format, system_clock::from_time_t(to_time_t()));
+  auto fmt = ustring::format("{{:{}}}", format);
+  return ustring::format(fmt, system_clock::from_time_t(to_time_t()));
 }
 
 std::time_t date_time::to_time_t() const {
-  return duration_cast<chrono::seconds>(value_ - ticks_to_1970).count();
+  return duration_cast<chrono::seconds>(value_ - ticks_offset_1970).count();
 }
 
 std::ostream& xtd::operator<<(std::ostream& os, const date_time& dt) noexcept {
