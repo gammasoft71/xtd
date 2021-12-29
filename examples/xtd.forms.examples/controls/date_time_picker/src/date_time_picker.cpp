3b74a03fd43f75c05ffed37b50d8e981120694cb
Gammasoft <gammasoft71@gmail.com>
2021-12-29 21:04:16 +0100

    Update date_time_picker.cpp

--- examples/xtd.forms.examples/controls/date_time_picker/src/date_time_picker.cpp
+++ examples/xtd.forms.examples/controls/date_time_picker/src/date_time_picker.cpp
@@ -10,27 +10,28 @@ public:
     text("Date time picker example");
      controls().push_back_range({picker1, label1, picker2, label2});
     
-    picker1.location({10, 10});
+    picker1.location({30, 30});
+    picker1.format(date_time_picker_format::short_format);
     picker1.value_changed += [&] {
-      label1.text(ustring::format("{:u}", picker1.value()));
+      label1.text(picker1.value().to_short_date_string());
     };
     picker1.value(date_time::now());
     picker1.max_date(picker1.value().add(months(3)));
     picker1.min_date(picker1.value().add(months(-3)));
 
-    label1.location({10, 50});
+    label1.location({30, 70});
     label1.size({380, 340});
     
-    picker2.location({10, 90});
+    picker2.location({30, 110});
     picker2.format(date_time_picker_format::time);
     picker2.value_changed += [&] {
-      label2.text(ustring::format("{:u}", picker2.value()));
+      label2.text(picker2.value().to_long_time_string());
     };
     picker2.value(date_time::now());
     picker2.max_date(picker2.value().add(hours(3)));
     picker2.min_date(picker2.value().add(hours(-3)));
 
-    label2.location({10, 130});
+    label2.location({30, 150});
     label2.size({380, 340});
   }
  
