bd6f1cd70aec187bd69f311582aa5672b256f93c
Gammasoft <gammasoft71@gmail.com>
2022-01-02 00:19:05 +0100

    Create wxMonthCalendarCtrl (wxPanel) containing wxCalendarCtrl (Allows resizing and location change which is not supported by wxGenericCalendarCtrl)

--- src/xtd.forms.native.wxwidgets/src/xtd/forms/native/wxwidgets/month_calendar.cpp
+++ src/xtd.forms.native.wxwidgets/src/xtd/forms/native/wxwidgets/month_calendar.cpp
@@ -16,7 +16,7 @@ void month_calendar::allowable_dates(const intptr_t control, date_time min_date,
     return;
   }
 
-  wxCalendarCtrl* wx_calendar_ctrl = static_cast<wxCalendarCtrl*>(reinterpret_cast<control_handler*>(control)->control());
+  wxCalendarCtrl* wx_calendar_ctrl = static_cast<wxMonthCalendar*>(reinterpret_cast<control_handler*>(control)->control())->calendarCtrl;
   wxDateTime wx_min_date_time(min_date.day(), static_cast<wxDateTime::Month>(min_date.month()-1), min_date.year());
   wxDateTime wx_max_date_time(max_date.day(), static_cast<wxDateTime::Month>(max_date.month()-1), max_date.year());
   wx_calendar_ctrl->SetDateRange(wx_min_date_time, wx_max_date_time);
@@ -29,7 +29,7 @@ void month_calendar::selection_range(const intptr_t control, date_time date_star
     return;
   }
 
-  wxCalendarCtrl* wx_calendar_ctrl = static_cast<wxCalendarCtrl*>(reinterpret_cast<control_handler*>(control)->control());
+  wxCalendarCtrl* wx_calendar_ctrl = static_cast<wxMonthCalendar*>(reinterpret_cast<control_handler*>(control)->control())->calendarCtrl;
   wxDateTime wx_date_time(date_start.day(), static_cast<wxDateTime::Month>(date_start.month()-1), date_start.year());
   wx_calendar_ctrl->SetDate(wx_date_time);
 }
@@ -41,7 +41,7 @@ pair<date_time, date_time> month_calendar::selection_range(const intptr_t contro
     return {};
   }
 
-  wxDateTime wx_date_time = static_cast<wxCalendarCtrl*>(reinterpret_cast<control_handler*>(control)->control())->GetDate();
+  wxDateTime wx_date_time = static_cast<wxMonthCalendar*>(reinterpret_cast<control_handler*>(control)->control())->calendarCtrl->GetDate();
   date_time date = date_time(wx_date_time.GetYear(), static_cast<uint32_t>(wx_date_time.GetMonth()) + 1, wx_date_time.GetDay(), 0, 0, 0, date_time_kind::unspecified);
   return {date, date};
 }
--- tests/xtd.forms.manual_tests/src/manual_tests.cpp
+++ tests/xtd.forms.manual_tests/src/manual_tests.cpp
@@ -9,32 +9,31 @@ using namespace std::chrono_literals;
 class manual_test_form : public form {
 public:
   static void main() {
-    debug_form df;
     application::run(manual_test_form());
   }
 
   manual_test_form() {
     text("Manual tests");
-    client_size({ 640, 480 });
+    client_size({400, 220});
 
     month_calendar1.parent(*this);
-    month_calendar1.location({ 10, 10 });
+    month_calendar1.location({10, 10});
+    month_calendar1.date_changed += [&](object&, const event_args& e) {
+      label1.text(ustring::format("selection range: [{:d}, {:d}]", month_calendar1.selection_start(), month_calendar1.selection_end()));
+    };
     month_calendar1.selection_range({date_time::now(), date_time::now()});
     month_calendar1.min_date(date_time::now().subtract(months(3)));
     month_calendar1.max_date(date_time::now().add(months(3)));
-    month_calendar1.date_changed += [&](object&, const event_args& e) {
-      debug::write_line(ustring::format("month_calendar date_changed event: [{:u}, {:u}]", month_calendar1.selection_start(), month_calendar1.selection_end()));
-    };
-    month_calendar1.date_selected += [&](object&, const event_args& e) {
-      debug::write_line(ustring::format("month_calendar date_selected event: [{:u}, {:u}]", month_calendar1.selection_start(), month_calendar1.selection_end()));
-    };
-    month_calendar1.click += [&](object&, const event_args& e) {
-      assert_(false);
-    };
+
+    label1.parent(*this);
+    label1.auto_size(true);
+    label1.location({10, 180});
+    label1.text(ustring::format("selection range: [{:d}, {:d}]", month_calendar1.selection_start(), month_calendar1.selection_end()));
   }
 
 private:
   month_calendar month_calendar1;
+  label label1;
 };
 
 startup_(manual_test_form);
