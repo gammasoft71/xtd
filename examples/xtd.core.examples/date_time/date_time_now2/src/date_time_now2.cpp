573d6f38c2a653eea2304e34ee59bd73069f7a66
Gammasoft <gammasoft71@gmail.com>
2022-01-02 16:34:30 +0100

    add date_time doxygen comment and examples

---
+++ examples/xtd.core.examples/date_time/date_time_now2/src/date_time_now2.cpp
@@ -0,0 +1,31 @@
+#include <xtd/xtd>
+
+using namespace std;
+using namespace xtd;
+
+class program {
+public:
+  static void main() {
+    date_time local_date = date_time::now();
+    vector<ustring> locale_names = {"en_US", "en_GB", "fr_FR", "de_DE", "ru_RU"};
+    
+    for (auto locale_name : locale_names) {
+      try {
+        locale::global(locale(locale_name + ".utf-8"));
+        console::write_line("{}: {}", locale_name, local_date.sprintf("%x %T"));
+      } catch(const exception& e) {
+        console::write_line(ustring::format("Make sure {} locale is installed on your system :\n\n{}\n", locale_name, e.what()), "Exception");
+      }
+    }
+  }
+};
+
+startup_(program);
+
+// This code can produces the following output:
+//
+// en_US: 01/02/2022 10:59:07
+// en_GB: 02/01/2022 10:59:07
+// fr_FR: 02.01.2022 10:59:07
+// de_DE: 02.01.2022 10:59:07
+// ru_RU: 02.01.2022 10:59:07
---
+++ examples/xtd.core.examples/date_time/date_time_sprintf/README.md
@@ -0,0 +1,22 @@
+# date_time_sprintf
+
+Shows how to use [xtd::date_time::sprintf](../../../../src/xtd.core/include/xtd/date_time.h) method.
+
+# Sources
+
+[src/date_time_sprintf.cpp](src/date_time_sprintf.cpp)
+
+[CMakeLists.txt](CMakeLists.txt)
+
+# Build and run
+
+Open "Command Prompt" or "Terminal". Navigate to the folder that contains the project and type the following:
+
+```cmake
+xtdc run
+```
+
+# Output
+
+```
+```
