7be1054b7997a3cffaf89daa9644c65212336e01
Gammasoft <gammasoft71@gmail.com>
2022-01-18 13:48:06 +0100

    Update border_style.h

--- src/xtd.forms/include/xtd/forms/style_sheets/border_style.h
+++ src/xtd.forms/include/xtd/forms/style_sheets/border_style.h
@@ -24,8 +24,10 @@ namespace xtd {
         solid,
       };
 
+      /// @cond
       inline std::ostream& operator<<(std::ostream& os, const border_style value) {return os << to_string(value, {{border_style::none, "none,"}, {border_style::dashed, "dashed,"}, {border_style::dot_dash, "dot_dash,"}, {border_style::dot_dot_dash, "dot_dot_dash,"}, {border_style::dotted, "dotted,"}, {border_style::double_style, "double_style,"}, {border_style::groove, "groove,"}, {border_style::inset, "inset,"}, {border_style::outset, "outset,"}, {border_style::ridge, "ridge,"}, {border_style::solid, "solid,"}});}
       inline std::wostream& operator<<(std::wostream& os, const border_style value) {return os << to_string(value, {{border_style::none, L"none,"}, {border_style::dashed, L"dashed,"}, {border_style::dot_dash, L"dot_dash,"}, {border_style::dot_dot_dash, L"dot_dot_dash,"}, {border_style::dotted, L"dotted,"}, {border_style::double_style, L"double_style,"}, {border_style::groove, L"groove,"}, {border_style::inset, L"inset,"}, {border_style::outset, L"outset,"}, {border_style::ridge, L"ridge,"}, {border_style::solid, L"solid,"}});}
+      /// @endcond
     }
   }
 }
