10314ff7979e3e4b40546e073df6063fe7633e67
Gammasoft <gammasoft71@gmail.com>
2022-01-18 19:40:28 +0100

    add documentations

--- src/xtd.forms/include/xtd/forms/style_sheets/pseudo_state.h
+++ src/xtd.forms/include/xtd/forms/style_sheets/pseudo_state.h
@@ -17,21 +17,43 @@ namespace xtd {
       /// xtd.forms
       /// @ingroup xtd_forms style_sheets
       enum class pseudo_state {
-        standard,
-        flat,
-        flat_popup,
-        unchecked,
-        checked,
-        mixed,
-        disabled,
+        /// @brief The item is in default state.
         default_state,
-        pressed,
-        hot,
+        /// @brief The item is unchecked.
+        unchecked = standard,
+        /// @brief The item is checked.
+        checked = 1,
+        /// @brief The item is mixed. For the three-state ittem.
+        mixed = 2,
+        /// @brief The irem is pressed, mouse is down on the item.
+        pressed = 3,
+
+        /// @brief The irem is flat style.
+        flat = 0b1000000,
+        /// @brief The irem is popup flat style.
+        flat_popup = 0b10000000,
+        /// @brief The item is the default (see ibuttton_control).
+        default_state = 0b100000000,
+        /// @brief The item is hot (mouse is on the item).
+        hot = 0b1000000000,
+        /// @brief The irem is disabled, generally the item is grayed.
+        disabled = 0b10000000000,
       };
       
       /// @cond
-      inline std::ostream& operator<<(std::ostream& os, const pseudo_state value) {return os << to_string(value, {{pseudo_state::standard, "standard,"}, {pseudo_state::flat, "flat,"}, {pseudo_state::flat_popup, "flat_popup,"}, {pseudo_state::unchecked, "unchecked,"}, {pseudo_state::checked, "checked,"}, {pseudo_state::mixed, "mixed,"}, {pseudo_state::disabled, "disabled,"}, {pseudo_state::default_state, "default_state,"}, {pseudo_state::pressed, "pressed,"}, {pseudo_state::hot, "hot,"}});}
-      inline std::wostream& operator<<(std::wostream& os, const pseudo_state value) {return os << to_string(value, {{pseudo_state::standard, L"standard,"}, {pseudo_state::flat, L"flat,"}, {pseudo_state::flat_popup, L"flat_popup,"}, {pseudo_state::unchecked, L"unchecked,"}, {pseudo_state::checked, L"checked,"}, {pseudo_state::mixed, L"mixed,"}, {pseudo_state::disabled, L"disabled,"}, {pseudo_state::default_state, L"default_state,"}, {pseudo_state::pressed, L"pressed,"}, {pseudo_state::hot, L"hot,"}});}
+      inline pseudo_state& operator +=(pseudo_state& lhs, pseudo_state rhs) {lhs = static_cast<pseudo_state>(static_cast<long long>(lhs) + static_cast<long long>(rhs)); return lhs;}
+      inline pseudo_state& operator -=(pseudo_state& lhs, pseudo_state rhs) {lhs = static_cast<pseudo_state>(static_cast<long long>(lhs) - static_cast<long long>(rhs)); return lhs;}
+      inline pseudo_state& operator &=(pseudo_state& lhs, pseudo_state rhs) {lhs = static_cast<pseudo_state>(static_cast<long long>(lhs) & static_cast<long long>(rhs)); return lhs;}
+      inline pseudo_state& operator |=(pseudo_state& lhs, pseudo_state rhs) {lhs = static_cast<pseudo_state>(static_cast<long long>(lhs) | static_cast<long long>(rhs)); return lhs;}
+      inline pseudo_state& operator ^=(pseudo_state& lhs, pseudo_state rhs) {lhs = static_cast<pseudo_state>(static_cast<long long>(lhs) ^ static_cast<long long>(rhs)); return lhs;}
+      inline pseudo_state operator +(pseudo_state lhs, pseudo_state rhs) {return static_cast<pseudo_state>(static_cast<long long>(lhs) + static_cast<long long>(rhs));}
+      inline pseudo_state operator -(pseudo_state lhs, pseudo_state rhs) {return static_cast<pseudo_state>(static_cast<long long>(lhs) - static_cast<long long>(rhs));}
+      inline pseudo_state operator ~(pseudo_state rhs) {return static_cast<pseudo_state>(~static_cast<long long>(rhs));}
+      inline pseudo_state operator &(pseudo_state lhs, pseudo_state rhs) {return static_cast<pseudo_state>(static_cast<long long>(lhs) & static_cast<long long>(rhs));}
+      inline pseudo_state operator |(pseudo_state lhs, pseudo_state rhs) {return static_cast<pseudo_state>(static_cast<long long>(lhs) | static_cast<long long>(rhs));}
+      inline pseudo_state operator ^(pseudo_state lhs, pseudo_state rhs) {return static_cast<pseudo_state>(static_cast<long long>(lhs) ^ static_cast<long long>(rhs));}
+      inline std::ostream& operator<<(std::ostream& os, const pseudo_state value) {return os << to_string(value, {{pseudo_state::default_state, "default_state,"}, {pseudo_state::flat, "flat,"}, {pseudo_state::flat_popup, "flat_popup,"}, {pseudo_state::unchecked, "unchecked,"}, {pseudo_state::checked, "checked,"}, {pseudo_state::mixed, "mixed,"}, {pseudo_state::disabled, "disabled,"}, {pseudo_state::default_state, "default_state,"}, {pseudo_state::pressed, "pressed,"}, {pseudo_state::hot, "hot,"}});}
+      inline std::wostream& operator<<(std::wostream& os, const pseudo_state value) {return os << to_string(value, {{pseudo_state::default_state, L"default_state,"}, {pseudo_state::flat, L"flat,"}, {pseudo_state::flat_popup, L"flat_popup,"}, {pseudo_state::unchecked, L"unchecked,"}, {pseudo_state::checked, L"checked,"}, {pseudo_state::mixed, L"mixed,"}, {pseudo_state::disabled, L"disabled,"}, {pseudo_state::default_state, L"default_state,"}, {pseudo_state::pressed, L"pressed,"}, {pseudo_state::hot, L"hot,"}});}
       /// @endcond
     }
   }
