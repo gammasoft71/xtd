15367dd2ec52c16cfa7d421a3f39b11b5174ac06
Gammasoft <gammasoft71@gmail.com>
2022-01-19 16:59:27 +0100

    add color_data and color_style

---
+++ src/xtd.forms/include/xtd/forms/style_sheets/color_style.h
@@ -0,0 +1,36 @@
+/// @file
+/// @brief Contains xtd::forms::style_sheets::color_style enum class.
+/// @copyright Copyright (c) 2022 Gammasoft. All rights reserved.
+#pragma once
+#include <xtd/ustring.h>
+
+/// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
+namespace xtd {
+  /// @brief The xtd::forms namespace contains classes for creating Windows-based applications that take full advantage of the rich user interface features available in the Microsoft Windows operating system, Apple macOS and Linux like Ubuntu operating system.
+  namespace forms {
+    /// @brief The xtd::forms::style_sheets namespace contains various properties, states, and subcontrols that make it possible to customize the look of control.
+    namespace style_sheets {
+      /// @brief The color_style enum class specifies what kind of color to display.
+      /// @par Namespace
+      /// xtd::forms::style_sheets
+      /// @par Library
+      /// xtd.forms
+      /// @ingroup xtd_forms style_sheets
+      enum class color_style {
+        /// @brief Defines solid color.
+        solid,
+        /// @brief Defines a linear_gradient color.
+        linear_gradient,
+        /// @brief Defines a radial_gradient color.
+        radial_gradient,
+        /// @brief Defines a conic_gradient color.
+        conic_gradient,
+      };
+      
+      /// @cond
+      inline std::ostream& operator<<(std::ostream& os, const color_style value) {return os << to_string(value, {{color_style::solid, "solid,"}, {color_style::linear_gradient, "linear_gradient,"}, {color_style::radial_gradient, "radial_gradient,"}, {color_style::conic_gradient, "conic_gradient,"}});}
+      inline std::wostream& operator<<(std::wostream& os, const color_style value) {return os << to_string(value, {{color_style::solid, L"solid,"}, {color_style::linear_gradient, L"linear_gradient,"}, {color_style::radial_gradient, L"radial_gradient,"}, {color_style::conic_gradient, L"conic_gradient,"}});}
+      /// @endcond
+    }
+  }
+}
