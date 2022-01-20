caadba8b588dcaf2d820e1f2702135640bee6229
Gammasoft <gammasoft71@gmail.com>
2022-01-20 23:45:33 +0100

    add parse and try_parse and + unit tests

--- tests/xtd.forms.unit_tests/src/xtd/forms/style_sheets/color_data.cpp
+++ tests/xtd.forms.unit_tests/src/xtd/forms/style_sheets/color_data.cpp
@@ -105,5 +105,74 @@ namespace unit_tests {
       collection_assert::are_equal({color::blue}, c.colors(), csf_);
       assert::are_equal(180, c.angle(), csf_);
     }
+    
+    void test_method_(try_parse_with_hash_and_no_hex) {
+      color_data c;
+      assert::is_false(color_data::try_parse("#", c), csf_);
+    }
+    
+    void test_method_(try_parse_with_hash_and_one_hex) {
+      color_data c;
+      assert::is_false(color_data::try_parse("#1", c), csf_);
+    }
+
+    void test_method_(try_parse_with_hash_and_two_hex) {
+      color_data c;
+      assert::is_false(color_data::try_parse("#12", c), csf_);
+    }
+
+    void test_method_(try_parse_with_hash_and_three_hex) {
+      color_data c;
+      assert::is_true(color_data::try_parse("#123", c));
+      assert::are_equal(color_style::solid, c.style(), csf_);
+      assert::are_equal(1U, c.colors().size(), csf_);
+      collection_assert::are_equal({color::from_argb(0xFF112233)}, c.colors(), csf_);
+      assert::are_equal(90, c.angle(), csf_);
+    }
+    
+    void test_method_(try_parse_with_hash_and_four_hex) {
+      color_data c;
+      assert::is_true(color_data::try_parse("#1234", c));
+      assert::are_equal(color_style::solid, c.style(), csf_);
+      assert::are_equal(1U, c.colors().size(), csf_);
+      collection_assert::are_equal({color::from_argb(0x11223344)}, c.colors(), csf_);
+      assert::are_equal(90, c.angle(), csf_);
+    }
+    
+    void test_method_(try_parse_with_hash_and_five_hex) {
+      color_data c;
+      assert::is_false(color_data::try_parse("#12345", c), csf_);
+    }
+
+    void test_method_(try_parse_with_hash_and_six_hex) {
+      color_data c;
+      assert::is_true(color_data::try_parse("#123456", c));
+      assert::are_equal(color_style::solid, c.style(), csf_);
+      assert::are_equal(1U, c.colors().size(), csf_);
+      collection_assert::are_equal({color::from_argb(0xFF123456)}, c.colors(), csf_);
+      assert::are_equal(90, c.angle(), csf_);
+    }
+    
+    void test_method_(try_parse_with_hash_and_seven_hex) {
+      color_data c;
+      assert::is_false(color_data::try_parse("#1234567", c), csf_);
+    }
+
+    void test_method_(try_parse_with_hash_and_eight_hex) {
+      color_data c;
+      assert::is_true(color_data::try_parse("#12345678", c));
+      assert::are_equal(color_style::solid, c.style(), csf_);
+      assert::are_equal(1U, c.colors().size(), csf_);
+      collection_assert::are_equal({color::from_argb(0x12345678)}, c.colors(), csf_);
+      assert::are_equal(90, c.angle(), csf_);
+    }
+    
+    void test_method_(try_parse_with_hash_and_nine_and_more_hex) {
+      color_data c;
+      assert::is_false(color_data::try_parse("#123456789", c), csf_);
+      assert::is_false(color_data::try_parse("#1234567890", c), csf_);
+      assert::is_false(color_data::try_parse("#12345678901", c), csf_);
+      assert::is_false(color_data::try_parse("#1234567890123456", c), csf_);
+    }
   };
 }
