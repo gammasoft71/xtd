ed959751cb52e65d9001195e926e3980862510b1
Gammasoft <gammasoft71@gmail.com>
2022-01-28 23:40:58 +0100

    review files

---
+++ src/xtd.forms/include/xtd/forms/style_sheets/border_width.h
@@ -0,0 +1,158 @@
+/// @file
+/// @brief Contains xtd::forms::style_sheets::border_width class.
+/// @copyright Copyright (c) 2022 Gammasoft. All rights reserved.
+#pragma once
+#include <xtd/ustring.h>
+#include "color_property.h"
+
+/// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
+namespace xtd {
+  /// @brief The xtd::forms namespace contains classes for creating Windows-based applications that take full advantage of the rich user interface features available in the Microsoft Windows operating system, Apple macOS and Linux like Ubuntu operating system.
+  namespace forms {
+    /// @brief The xtd::forms::style_sheets namespace contains various properties, states, and subcontrols that make it possible to customize the look of control.
+    namespace style_sheets {
+      /// @brief The width specifies the width of the borders.
+      /// @code
+      /// class forms_export_ border_width : public xtd::object
+      /// @endcode
+      /// @par Inheritance
+      /// xtd::object → xtd::forms::style_sheets::border_width
+      /// @par Namespace
+      /// xtd::forms::style_sheets
+      /// @par Library
+      /// xtd.forms
+      /// @ingroup xtd_forms style_sheets
+      class forms_export_ border_width : public object {
+      public:
+        /// @name Fields
+        
+        /// @{
+        /// @brief Provides a xtd::forms::style_sheets::border_width object with no xtd::forms::style_sheets::border_width.
+        static const border_width empty;
+        /// @}
+        
+        /// @name Constructors
+        
+        /// @{
+        /// @brief Initializes a new instance of the xtd::forms::style_sheets::border_width class.
+        border_width() = default;
+        
+        /// @brief Initializes a new instance of the xtd::forms::style_sheets::border_width class using the supplied xtd::forms::style_sheets::border_width size for all edges.
+        /// @param all The number of pixels to be used for xtd::forms::style_sheets::border_width for all edges.
+        /// @remarks This constructor sets the right, left, bottom, top and all properties to the value of the all parameter.
+        explicit border_width(int all) : all_(true), left_(all), top_(all), right_(all), bottom_(all) {}
+        
+        /// @brief Initializes a new instance of the xtd::forms::style_sheets::border_width class using a separate xtd::forms::style_sheets::border_width size for each edge.
+        /// @param left The xtd::forms::style_sheets::border_width size, in pixels, for the left edge.
+        /// @param top The xtd::forms::style_sheets::border_width size, in pixels, for the top edge.
+        /// @param right The xtd::forms::style_sheets::border_width size, in pixels, for the right edge.
+        /// @param bottom The xtd::forms::style_sheets::border_width size, in pixels, for the bottom edge.
+        /// @remarks If all of the parameter values are equal, then the all property will reflect this common value.
+        border_width(int left, int top, int right, int bottom) : all_(left == top && left == right && left == bottom), left_(left), top_(top), right_(right), bottom_(bottom) {}
+        /// @}
+        
+        /// @cond
+        border_width(const border_width&) = default;
+        border_width& operator=(const border_width&) = default;
+        bool operator==(const border_width& value) {return all_ == value.all_ && left_ == value.left_ && top_ == value.top_ && right_ == value.right_ && bottom_ == value.bottom_;}
+        bool operator!=(const border_width& value) {return !operator==(value);}
+        /// @endcond
+        
+        /// @name Peorperties
+        
+        /// @{
+        /// @brief Gets the xtd::forms::style_sheets::border_width value for all the edges.
+        /// @return The xtd::forms::style_sheets::border_width, in pixels, for all edges if the same; otherwise, -1.
+        /// @remarks When retrieving this property, if all the edges use the same xtd::forms::style_sheets::border_width value, then this common value is returned. Otherwise, -1 is returned to indicate that all the xtd::forms::style_sheets::border_width values are not equal.
+        int all() const {return all_ ? top_ : -1;}
+        /// @brief Sets the xtd::forms::style_sheets::border_width value for all the edges.
+        /// @param all The xtd::forms::style_sheets::border_width, in pixels, for all edges if the same; otherwise, -1.
+        /// @remarks When retrieving this property, if all the edges use the same xtd::forms::style_sheets::border_width value, then this common value is returned. Otherwise, -1 is returned to indicate that all the xtd::forms::style_sheets::border_width values are not equal.
+        void all(int all) {
+          if (!all_ || left_ != all) {
+            all_ = true;
+            left_ = top_ = right_ = bottom_ = all;
+          }
+        }
+        
+        /// @brief Gets the xtd::forms::style_sheets::border_width value for the bottom edge.
+        /// @return The xtd::forms::style_sheets::border_width, in pixels, for the bottom edge.
+        /// @remarks Setting this value can also alter the all property.
+        int bottom() const {return bottom_;}
+        /// @brief Sets the xtd::forms::style_sheets::border_width value for the bottom edge.
+        /// @param bottom The xtd::forms::style_sheets::border_width, in pixels, for the bottom edge.
+        /// @remarks Setting this value can also alter the all property.
+        void bottom(int bottom) {
+          if (all_ || bottom_ != bottom) {
+            all_ = false;
+            bottom_ = bottom;
+          }
+        }
+        
+        /// @brief Gets the xtd::forms::style_sheets::border_width value for the left edge.
+        /// @return The xtd::forms::style_sheets::border_width, in pixels, for the left edge.
+        /// @remarks Setting this value can also alter the all property.
+        int left() const {return left_;}
+        /// @brief Sets the xtd::forms::style_sheets::border_width value for the left edge.
+        /// @param left The xtd::forms::style_sheets::border_width, in pixels, for the left edge.
+        /// @remarks Setting this value can also alter the all property.
+        void left(int left) {
+          if (all_ || left_ != left) {
+            all_ = false;
+            left_ = left;
+          }
+        }
+        
+        /// @brief Gets the xtd::forms::style_sheets::border_width value for the right edge.
+        /// @return The xtd::forms::style_sheets::border_width, in pixels, for the right edge.
+        /// @remarks Setting this value can also alter the all property.
+        int right() const {return right_;}
+        /// @brief Sets the xtd::forms::style_sheets::border_width value for the right edge.
+        /// @param right The xtd::forms::style_sheets::border_width, in pixels, for the right edge.
+        /// @remarks Setting this value can also alter the all property.
+        void right(int right) {
+          if (all_ || right_ != right) {
+            all_ = false;
+            right_ = right;
+          }
+        }
+        
+        /// @brief Gets the xtd::forms::style_sheets::border_width value for the top edge.
+        /// @return The xtd::forms::style_sheets::border_width, in pixels, for the top edge.
+        /// @remarks Setting this value can also alter the all property.
+        int top() const {return top_;}
+        /// @brief Sets the xtd::forms::style_sheets::border_width value for the top edge.
+        /// @param top The xtd::forms::style_sheets::border_width, in pixels, for the top edge.
+        /// @remarks Setting this value can also alter the all property.
+        void top(int top) {
+          if (all_ || top_ != top) {
+            all_ = false;
+            top_ = top;
+          }
+        }
+        /// @}
+        
+        /// @name Methods
+        
+        /// @{
+        /// @brief Returns a string that represents the current xtd::forms::style_sheets::border_width.
+        /// @return A string that represents the current xtd::forms::style_sheets::border_width.
+        /// @remarks This method returns a string containing the labeled values of the xtd::forms::style_sheets::border_width for all four edges.
+        xtd::ustring to_string() const noexcept override {return xtd::ustring::format("border_width [all={}, left={}, top={}, right={}, bottom={}]", all_, left_, top_, right_, bottom_);}
+        /// @}
+        
+        /// @cond
+        bool operator==(const border_width& p) const {return all_ == p.all_ && left_ == p.left_ && top_ == p.top_ && right_ == p.right_ && bottom_ == p.bottom_;}
+        friend std::ostream& operator<<(std::ostream& os, const xtd::forms::style_sheets::border_width& border_width) noexcept {return os << border_width.to_string();}
+        /// @endcond
+        
+      private:
+        bool all_ = true;
+        int left_ = 3;
+        int top_ = 3;
+        int right_ = 3;
+        int bottom_ = 3;
+      };
+    }
+  }
+}
--- src/xtd.forms/src/xtd/forms/style_sheets/border_color.cpp
+++ src/xtd.forms/src/xtd/forms/style_sheets/border_color.cpp
@@ -3,84 +3,84 @@
 #include <xtd/drawing/system_colors.h>
 #include <xtd/drawing/drawing2d/linear_gradient_brush.h>
 #include <xtd/not_supported_exception.h>
-#include "../../../../include/xtd/forms/style_sheets/border_color_property.h"
+#include "../../../../include/xtd/forms/style_sheets/border_color.h"
 
 using namespace std;
 using namespace xtd;
 using namespace xtd::forms::style_sheets;
 
-const border_color_property border_color_property::empty {color_property()};
+const border_color border_color::empty {color_property()};
 
-border_color_property::border_color_property(const color_property& all) : all_(true), left_(all), top_(all), right_(all), bottom_(all) {
+border_color::border_color(const color_property& all) : all_(true), left_(all), top_(all), right_(all), bottom_(all) {
 }
 
-border_color_property::border_color_property(const color_property& left, const color_property& top, const color_property& right, const color_property& bottom) : all_(left == top && left == right && left == bottom), left_(left), top_(top), right_(right), bottom_(bottom) {
+border_color::border_color(const color_property& left, const color_property& top, const color_property& right, const color_property& bottom) : all_(left == top && left == right && left == bottom), left_(left), top_(top), right_(right), bottom_(bottom) {
 }
 
-const color_property& border_color_property::all() const noexcept {
+const color_property& border_color::all() const noexcept {
   return all_ ? top_ : color_property::empty;
 }
 
-void border_color_property::all(const color_property& all) noexcept {
+void border_color::all(const color_property& all) noexcept {
   if (!all_ || left_ != all) {
     all_ = true;
     left_ = top_ = right_ = bottom_ = all;
   }
 }
 
-const color_property& border_color_property::bottom() const noexcept {
+const color_property& border_color::bottom() const noexcept {
   return bottom_;
 }
 
-void border_color_property::bottom(const color_property& bottom) noexcept {
+void border_color::bottom(const color_property& bottom) noexcept {
   if (all_ || bottom_ != bottom) {
     all_ = false;
     bottom_ = bottom;
   }
 }
 
-const color_property& border_color_property::left() const noexcept {
+const color_property& border_color::left() const noexcept {
   return left_;
 }
 
-void border_color_property::left(const color_property& left) noexcept {
+void border_color::left(const color_property& left) noexcept {
   if (all_ || left_ != left) {
     all_ = false;
     left_ = left;
   }
 }
 
-const color_property& border_color_property::right() const noexcept {
+const color_property& border_color::right() const noexcept {
   return right_;
 }
 
-void border_color_property::right(const color_property& right) noexcept {
+void border_color::right(const color_property& right) noexcept {
   if (all_ || right_ != right) {
     all_ = false;
     right_ = right;
   }
 }
 
-const color_property& border_color_property::top() const noexcept {
+const color_property& border_color::top() const noexcept {
   return top_;
 }
 
-void border_color_property::top(const color_property& top) noexcept {
+void border_color::top(const color_property& top) noexcept {
   if (all_ || top_ != top) {
     all_ = false;
     top_ = top;
   }
 }
 
-bool border_color_property::equals(const object& other) const noexcept {
-  return is<border_color_property>(other) ? equals(static_cast<const border_color_property&>(other)) : false;
+bool border_color::equals(const object& other) const noexcept {
+  return is<border_color>(other) ? equals(static_cast<const border_color&>(other)) : false;
 }
 
-bool border_color_property::equals(const border_color_property& other) const noexcept {
+bool border_color::equals(const border_color& other) const noexcept {
   return all_ == other.all_ && left_ == other.left_ && top_ == other.top_ && right_ == other.right_ && bottom_ == other.bottom_;;
 }
 
-bool border_color_property::from_css(const xtd::ustring& css_text, border_color_property& result) noexcept {
+bool border_color::from_css(const xtd::ustring& css_text, border_color& result) noexcept {
   vector<ustring> colors = split_colors(css_text.trim());
   if (colors.size() < 1 || colors.size() > 4) return false;
   color_property color;
@@ -101,13 +101,13 @@ bool border_color_property::from_css(const xtd::ustring& css_text, border_color_
   return true;
 }
 
-border_color_property border_color_property::from_css(const xtd::ustring& css_text, const border_color_property& default_value) noexcept {
-  border_color_property result;
+border_color border_color::from_css(const xtd::ustring& css_text, const border_color& default_value) noexcept {
+  border_color result;
   if (from_css(css_text, result)) return result;
   return default_value;
 }
 
-vector<ustring> border_color_property::split_colors(const ustring& text) {
+vector<ustring> border_color::split_colors(const ustring& text) {
   static vector<ustring> color_keywords = {"rgb(", "rgba(", "argb(", "hsl(", "hsla(", "ahsl(", "hsv(", "hsva(", "ahsv(", "system-color("};
   auto string_starts_with_any = [](const ustring& text, const vector<ustring>& values)->ustring {
     for (auto value : values)
