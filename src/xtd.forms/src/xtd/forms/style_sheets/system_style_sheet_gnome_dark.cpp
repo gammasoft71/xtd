0d9c127c6ec61ec08883d8be9be1eaca2de78de7
Gammasoft <gammasoft71@gmail.com>
2022-01-30 19:56:23 +0100

    add current_style_sheet

--- src/xtd.forms/src/xtd/forms/style_sheets/system_style_sheet_gnome_dark.cpp
+++ src/xtd.forms/src/xtd/forms/style_sheets/system_style_sheet_gnome_dark.cpp
@@ -1,13 +1,60 @@
 #include "../../../../include/xtd/forms/style_sheets/style_sheet.h"
+#include <xtd/drawing/system_colors.h>
 
 using namespace std;
 using namespace xtd;
 using namespace xtd::drawing;
+using namespace xtd::forms;
 using namespace xtd::forms::style_sheets;
 using namespace xtd::web::css;
 
+namespace {
+  style_sheets::system_colors get_system_colors() {
+    style_sheets::system_colors colors;
+    colors.accent(drawing::system_colors::accent());
+    colors.accent_text(drawing::system_colors::accent_text());
+    colors.active_border(drawing::system_colors::active_border());
+    colors.active_caption(drawing::system_colors::active_caption());
+    colors.active_caption_text(drawing::system_colors::active_caption_text());
+    colors.app_workspace(drawing::system_colors::app_workspace());
+    colors.button_face(drawing::system_colors::button_face());
+    colors.button_highlight(drawing::system_colors::button_highlight());
+    colors.button_shadow(drawing::system_colors::button_shadow());
+    colors.control(drawing::system_colors::control());
+    colors.control_dark(drawing::system_colors::control_dark());
+    colors.control_dark_dark(drawing::system_colors::control_dark_dark());
+    colors.control_light(drawing::system_colors::control_light());
+    colors.control_light_light(drawing::system_colors::control_light_light());
+    colors.control_text(drawing::system_colors::control_text());
+    colors.desktop(drawing::system_colors::desktop());
+    colors.gradient_active_caption(drawing::system_colors::gradient_active_caption());
+    colors.gradient_inactive_caption(drawing::system_colors::gradient_inactive_caption());
+    colors.gray_text(drawing::system_colors::gray_text());
+    colors.highlight(drawing::system_colors::highlight());
+    colors.highlight_text(drawing::system_colors::highlight_text());
+    colors.hot_track(drawing::system_colors::hot_track());
+    colors.inactive_border(drawing::system_colors::inactive_border());
+    colors.inactive_caption(drawing::system_colors::inactive_caption());
+    colors.inactive_caption_text(drawing::system_colors::inactive_caption_text());
+    colors.info(drawing::system_colors::info());
+    colors.info_text(drawing::system_colors::info_text());
+    colors.menu(drawing::system_colors::menu());
+    colors.menu_bar(drawing::system_colors::menu_bar());
+    colors.menu_highlight(drawing::system_colors::menu_highlight());
+    colors.menu_text(drawing::system_colors::menu_text());
+    colors.scroll_bar(drawing::system_colors::scroll_bar());
+    colors.text_box(drawing::system_colors::text_box());
+    colors.text_box_text(drawing::system_colors::text_box_text());
+    colors.window(drawing::system_colors::window());
+    colors.window_frame(drawing::system_colors::window_frame());
+    colors.window_text(drawing::system_colors::window_text());
+    return colors;
+  }
+}
+
 style_sheet style_sheet::system_style_sheet_gnome_dark() noexcept {
   style_sheet ss;
-  
+  ss.theme_ = style_sheets::theme("GNOME (dark)", "Contains GNOME (dark) theme.", "Gammasoft", uri("https://gammasoft71.wixsite.com/gammasoft"));
+  ss.system_colors_ = get_system_colors();
   return ss;
 }
