44bf44224b5cc1330002959358f3d90c4f8228f0
Gammasoft <gammasoft71@gmail.com>
2022-02-09 13:29:01 +0100

    update linear_gradient_brush

--- src/xtd.drawing/include/xtd/drawing/drawing2d/linear_gradient_brush.h
+++ src/xtd.drawing/include/xtd/drawing/drawing2d/linear_gradient_brush.h
@@ -77,12 +77,58 @@ namespace xtd {
         /// @param color1 A xtd::drawing::color structure that represents the starting color of the linear gradient.
         /// @param color2 A xtd::drawing::color structure that represents the ending color of the linear gradient.
         linear_gradient_brush(const xtd::drawing::rectangle_f& rect, const xtd::drawing::color& color1, const xtd::drawing::color& color2);
+        /// @brief Initializes a new instance of the linear_gradient_brush class with the specified points and colors.
+        /// @param point1 A xtd::drawing::point structure that represents the starting point of the linear gradient.
+        /// @param point2 A xtd::drawing::point structure that represents the endpoint of the linear gradient.
+        /// @param color1 A xtd::drawing::color structure that represents the starting color of the linear gradient.
+        /// @param color2 A xtd::drawing::color structure that represents the ending color of the linear gradient.
+        linear_gradient_brush(const xtd::drawing::point& point1, const xtd::drawing::point& point2, const std::vector<xtd::drawing::color>& linear_colors);
+        /// @brief Initializes a new instance of the linear_gradient_brush class with the specified points and colors.
+        /// @param point1 A xtd::drawing::point_f structure that represents the starting point of the linear gradient.
+        /// @param point2 A xtd::drawing::point_f structure that represents the endpoint of the linear gradient.
+        /// @param color1 A xtd::drawing::color structure that represents the starting color of the linear gradient.
+        /// @param color2 A xtd::drawing::color structure that represents the ending color of the linear gradient.
+        linear_gradient_brush(const xtd::drawing::point_f& point1, const xtd::drawing::point_f& point2, const std::vector<xtd::drawing::color>& linear_colors);
+        /// @brief Initializes a new instance of the linear_gradient_brush class with the specified rectangle, colors, and orientation.
+        /// @param rect A xtd::drawing::rectangle structure that specifies the bounds of the linear gradient.
+        /// @param color1 A xtd::drawing::color structure that represents the starting color of the linear gradient.
+        /// @param color2 A xtd::drawing::color structure that represents the ending color of the linear gradient.
+        /// @param linear_gradient_mode A xtd::drawing::drawing2d::linear_gradient_mode enumeration element that specifies the orientation of the gradient. The orientation determines the starting and ending points of the gradient. For example, xtd::drawing::drawing2d::linear_gradient_mode::forward_diagonal specifies that the starting point is the upper-left corner of the rectangle and the ending point is the lower-right corner of the rectangle.
+        linear_gradient_brush(const xtd::drawing::rectangle& rect, const std::vector<xtd::drawing::color>& linear_colors, xtd::drawing::drawing2d::linear_gradient_mode linear_gradient_mode);
+        /// @brief Initializes a new instance of the linear_gradient_brush class with the specified rectangle, colors, and orientation angle.
+        /// @param rect A xtd::drawing::rectangle structure that specifies the bounds of the linear gradient.
+        /// @param color1 A xtd::drawing::color structure that represents the starting color of the linear gradient.
+        /// @param color2 A xtd::drawing::color structure that represents the ending color of the linear gradient.
+        /// @param angle The angle, measured in degrees clockwise from the x-axis, of the gradient's orientation line.
+        linear_gradient_brush(const xtd::drawing::rectangle& rect, const std::vector<xtd::drawing::color>& linear_colors, float angle);
+        /// @brief Initializes a new instance of the linear_gradient_brush class with the specified rectangle, colors, and orientation.
+        /// @param rect A xtd::drawing::rectangle_f structure that specifies the bounds of the linear gradient.
+        /// @param color1 A xtd::drawing::color structure that represents the starting color of the linear gradient.
+        /// @param color2 A xtd::drawing::color structure that represents the ending color of the linear gradient.
+        /// @param linear_gradient_mode A xtd::drawing::drawing2d::linear_gradient_mode enumeration element that specifies the orientation of the gradient. The orientation determines the starting and ending points of the gradient. For example, xtd::drawing::drawing2d::linear_gradient_mode::forward_diagonal specifies that the starting point is the upper-left corner of the rectangle and the ending point is the lower-right corner of the rectangle.
+        linear_gradient_brush(const xtd::drawing::rectangle_f& rect, const std::vector<xtd::drawing::color>& linear_colors, xtd::drawing::drawing2d::linear_gradient_mode linear_gradient_mode);
+        /// @brief Initializes a new instance of the linear_gradient_brush class with the specified rectangle, colors, and orientation angle.
+        /// @param rect A xtd::drawing::rectangle_f structure that specifies the bounds of the linear gradient.
+        /// @param color1 A xtd::drawing::color structure that represents the starting color of the linear gradient.
+        /// @param color2 A xtd::drawing::color structure that represents the ending color of the linear gradient.
+        /// @param angle The angle, measured in degrees clockwise from the x-axis, of the gradient's orientation line.
+        linear_gradient_brush(const xtd::drawing::rectangle_f& rect, const std::vector<xtd::drawing::color>& linear_colors, float angle);
+        /// @brief Initializes a new instance of the linear_gradient_brush class with the specified rectangle and colors.
+        /// @param rect A xtd::drawing::rectangle_f structure that specifies the bounds of the linear gradient.
+        /// @param color1 A xtd::drawing::color structure that represents the starting color of the linear gradient.
+        /// @param color2 A xtd::drawing::color structure that represents the ending color of the linear gradient.
+        linear_gradient_brush(const xtd::drawing::rectangle& rect, const std::vector<xtd::drawing::color>& linear_colors);
+        /// @brief Initializes a new instance of the linear_gradient_brush class with the specified rectangle and colors.
+        /// @param rect A xtd::drawing::rectangle_f structure that specifies the bounds of the linear gradient.
+        /// @param color1 A xtd::drawing::color structure that represents the starting color of the linear gradient.
+        /// @param color2 A xtd::drawing::color structure that represents the ending color of the linear gradient.
+        linear_gradient_brush(const xtd::drawing::rectangle_f& rect, const std::vector<xtd::drawing::color>& linear_colors);
         /// @}
         
         /// @cond
         linear_gradient_brush(const linear_gradient_brush& value);
         linear_gradient_brush& operator=(const linear_gradient_brush& value);
-        bool operator==(const linear_gradient_brush& value) const {return data_->rect == value.data_->rect && data_->linear_colors_[0] == value.data_->linear_colors_[0] && data_->linear_colors_[1] == value.data_->linear_colors_[1];}
+        bool operator==(const linear_gradient_brush& value) const {return data_ == value.data_;}
         bool operator!=(const linear_gradient_brush& value) const {return !operator==(value);}
         /// @endcond
         
@@ -91,14 +137,11 @@ namespace xtd {
         /// @{
         /// @brief Gets the starting and ending colors of the gradient.
         /// @return An array of two xtd::drawing::color structures that represents the starting and ending colors of the gradient.
-        const std::array<xtd::drawing::color, 2>& linear_colors() const noexcept;
-        /// @brief Gets the starting and ending colors of the gradient.
-        /// @return An array of two xtd::drawing::color structures that represents the starting and ending colors of the gradient.
-        std::array<xtd::drawing::color, 2>& linear_colors() noexcept;
+        const std::vector<xtd::drawing::color>& linear_colors() const noexcept;
         /// @brief sets the starting and ending colors of the gradient.
         /// @param linear_colors An array of two xtd::drawing::color structures that represents the starting and ending colors of the gradient.
         /// @return Current linear_gradient_brush instance.
-        xtd::drawing::drawing2d::linear_gradient_brush& linear_colors(const std::array<xtd::drawing::color, 2>& linear_colors);
+        xtd::drawing::drawing2d::linear_gradient_brush& linear_colors(const std::vector<xtd::drawing::color>& linear_colors);
         
         /// @brief Gets a rectangular region that defines the starting and ending points of the gradient.
         /// @return A xtd::drawing::rectangle_f structure that specifies the starting and ending points of the gradient.
@@ -112,7 +155,7 @@ namespace xtd {
         struct data {
           xtd::drawing::rectangle_f rect;
           float angle = 0;
-          std::array<xtd::drawing::color, 2> linear_colors_ = {xtd::drawing::color::black, xtd::drawing::color::black};
+          std::vector<xtd::drawing::color> linear_colors;
         };
         std::shared_ptr<data> data_ = std::make_shared<data>();
       };
--- src/xtd.forms/src/xtd/forms/style_sheets/background_image.cpp
+++ src/xtd.forms/src/xtd/forms/style_sheets/background_image.cpp
@@ -75,7 +75,7 @@ void background_image::image_type(style_sheets::image_type value) noexcept {
 
 std::unique_ptr<xtd::drawing::brush> background_image::make_brush(const xtd::forms::style_sheets::background_image& image, const xtd::drawing::rectangle& rect) {
   if (image.image_type() == style_sheets::image_type::linear_gradient)
-    return make_unique<linear_gradient_brush>(rect, image.colors()[1], image.colors()[0], image.angle());
+    return make_unique<linear_gradient_brush>(rect, image.colors(), image.angle());
   if (image.image_type() == style_sheets::image_type::url)
     return make_unique<texture_brush>(image::from_file(image.url()));
   return null;
