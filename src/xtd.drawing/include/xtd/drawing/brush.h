bc90be9d77c1569152f9d801bfb08facf1165d0b
Gammasoft <gammasoft71@gmail.com>
2022-02-22 17:44:20 +0100

    Update brush.h

--- src/xtd.drawing/include/xtd/drawing/brush.h
+++ src/xtd.drawing/include/xtd/drawing/brush.h
@@ -24,8 +24,7 @@ namespace xtd {
     /// @par Library
     /// xtd.drawing
     /// @ingroup xtd_drawing drawing
-    /// @remarks This is an abstract base class and cannot be instantiated. To create a brush object, use classes derived from xtd::drawing::brush, such as xtd::drawing::solid_brush, xtd::drawing::texture_brush, xtd::drawing::drawing2d::hatch_brush, xtd::drawing::drawing2d::conical_gradient_brush, xtd::drawing::drawing2d::linear_gradien_brush, and xtd::drawing::drawing2d::radial_gradient_brush.
-    /// @par Brushes
+    /// @remarks This is an abstract base class and cannot be instantiated. To create a brush object, use classes derived from xtd::drawing::brush, such as xtd::drawing::solid_brush, xtd::drawing::texture_brush, xtd::drawing::drawing2d::hatch_brush, xtd::drawing::drawing2d::conical_gradient_brush, xtd::drawing::drawing2d::linear_gradien_brush, and xtd::drawing::drawing2d::radial_gradient_brush.<br>
     /// <table align="center" border="0" cellpadding="10" cellspacing="0" style="width:100%">
     ///   <tr>
     ///     <th><b>xtd::drawing::solid_brush</b></th>
@@ -36,8 +35,7 @@ namespace xtd {
     ///     <td>@image html solid_brush.png</td>
     ///     <td>@image html texture_brush1.png</td>
     ///     <td>@image html hatch_brush.png</td>
-    ///   </tr>
-    ///   <br>
+    ///   </tr><br>
     ///   <tr>
     ///     <th><b>xtd::drawing::drawing2d::conical_gradient_brush</b></th>
     ///     <th><b>xtd::drawing::drawing2d::linear_gradient_brush</b></th>
