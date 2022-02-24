d01958d8b365578e89940bcee4651d5d7bf3fd97
Gammasoft <gammasoft71@gmail.com>
2022-02-24 14:31:14 +0100

    add documentation

--- src/xtd.drawing/include/xtd/drawing/rectangle_f.h
+++ src/xtd.drawing/include/xtd/drawing/rectangle_f.h
@@ -41,54 +41,46 @@ namespace xtd {
       /// @name Constructors
       
       /// @{
-      rectangle_f() = default;
-      rectangle_f(float x, float y, float width, float height) : x_(x), y_(y), width_(width), height_(height) {}
-      rectangle_f(const point_f& location, const size_f& size) : rectangle_f(location.x(), location.y(), size.width(), size.height()) {}
+      rectangle_f() noexcept = default;
+      rectangle_f(float x, float y, float width, float height) noexcept;
+      rectangle_f(const point_f& location, const size_f& size) noexcept;
       /// @}
       
       /// @cond
-      rectangle_f(const rectangle_f&) = default;
-      rectangle_f& operator=(const rectangle_f&) = default;
+      rectangle_f(const rectangle_f&) noexcept = default;
+      rectangle_f& operator=(const rectangle_f&) noexcept = default;
       /// @endcond
       
       /// @name Properties
       
       /// @{
-      float bottom() const {return y_ + height_;}
+      float bottom() const noexcept;
       
-      float height() const {return height_;}
-      void height(float height) {height_ = height;}
+      float height() const noexcept;
+      void height(float height) noexcept;
       
-      bool is_empty() const {return *this == rectangle_f::empty;}
+      bool is_empty() const noexcept;
       
-      float left() const {return x_;}
-      void left(float left) {x_ = left;}
+      float left() const noexcept;
       
-      point_f location() const {return {x_, y_};}
-      void location(const point_f& location) {
-        x_ = location.x();
-        y_ = location.y();
-      }
+      point_f location() const noexcept;
+      void location(const point_f& location);
       
-      float right() const {return x_ + width_;}
+      float right() const noexcept;
       
-      size_f size() const {return {width_, height_};}
-      void size(const size_f& size) {
-        width_ = size.width();
-        height_ = size.height();
-      }
+      size_f size() const noexcept;
+      void size(const size_f& size) noexcept;
       
-      float top() const {return y_;}
-      void top(float top) {y_ = top;}
+      float top() const noexcept;
       
-      float x() const {return x_;}
-      void x(float x) {x_ = x;}
+      float x() const noexcept;
+      void x(float x) noexcept;
       
-      float y() const {return y_;}
-      void y(float y) {y_ = y;}
+      float y() const noexcept;
+      void y(float y) noexcept;
       
-      float width() const {return width_;}
-      void width(float width) {width_ = width;}
+      float width() const noexcept;
+      void width(float width) noexcept;
       /// @}
       
       /// @name Methods
@@ -117,62 +109,117 @@ namespace xtd {
       /// @remarks This method makes a copy of rect, adds the copy, and then returns the added copy.
       static rectangle_f add(const rectangle_f& rect, const drawing::size_f& sz) noexcept;
 
-      bool contains(float x, float y) const {return x_ <= x && x < x_ + width_ && y_ <= y && y < y_ + height_;}
-      bool contains(const point_f& pt) const {return contains(pt.x(), pt.y());}
-      bool contains(const rectangle_f& rect) const {return x_ <= rect.x_ && (rect.x_ + rect.width_) <= (x_ + width_) && y_ <= rect.y_ && (rect.y_ + rect.height_) <= (y_ + height_);}
+      /// @brief Determines if the specified point is contained within this xtd::drawing::rectangle_f structure.
+      /// @param pt The xtd::drawing::point_f to test.
+      /// @return This method returns true if the point represented by pt is contained within this xtd::drawing::rectangle_f structure; otherwise false.
+      /// @remarks The containing rectangle must be normalized for this method to return accurate results.
+      bool contains(const point_f& pt) const noexcept;
+      /// @brief Determines if the rectangular region represented by rect is entirely contained within this xtd::drawing::rectangle_f structure.
+      /// @param rect The xtd::drawing::rectangle_f to test.
+      /// @return This method returns true if the rectangular region represented by rect is entirely contained within this xtd::drawing::rectangle_f structure; otherwise false.
+      /// @remarks The containing rectangle must be normalized for this method to return accurate results.
+      bool contains(const rectangle_f& rect) const noexcept;
+      /// @brief Determines if the specified point is contained within this xtd::drawing::rectangle_f structure.
+      /// @param x The x-coordinate of the point to test.
+      /// @param y The y-coordinate of the point to test.
+      /// @return This method returns true if the point defined by x and y is contained within this xtd::drawing::rectangle_f structure; otherwise false.
+      /// @remarks The containing rectangle must be normalized for this method to return accurate results.
+      bool contains(float x, float y) const noexcept;
       
-      static rectangle_f from_ltrb(float left, float top, float right, float bottom) {return rectangle_f(left, top, right - left, bottom - top);}
+      /// @brief Creates a xtd::drawing::rectangle_f structure with the specified edge locations.
+      /// @param left The x-coordinate of the upper-left corner of this xtd::drawing::rectangle_f structure.
+      /// @param top The y-coordinate of the upper-left corner of this xtd::drawing::rectangle_f structure.
+      /// @param right The x-coordinate of the lower-right corner of this xtd::drawing::rectangle_f structure.
+      /// @param bottom The y-coordinate of the lower-right corner of this xtd::drawing::rectangle_f structure.
+      /// @return The new xtd::drawing::rectangle_f that this method creates.
+      /// @remarks This method creates a xtd::drawing::rectangle_f with the specified upper-left and lower-right corners.
+      static rectangle_f from_ltrb(float left, float top, float right, float bottom) noexcept;
       
-      void inflate(const drawing::size_f& sz) {inflate(sz.width(), sz.height());}
-      void inflate(float width, float height) {
-        x_ -= width;
-        y_ -= height;
-        width_ += 2 * width;
-        height_ +=  2 * height;
-      }
-      static rectangle_f inflate(const rectangle_f& rect, const drawing::size_f& sz) {return inflate(rect, sz.width(), sz.height());}
-      static rectangle_f inflate(const rectangle_f& rect, float width, float height) {
-        auto result = rect;
-        result.inflate(width, height);
-        return result;
-      }
+      /// @brief Enlarges this xtd::drawing::rectangle_f by the specified amount.
+      /// @param sz The amount to inflate this rectangle.
+      /// @remarks This method enlarges this rectangle, not a copy of it. The rectangle is enlarged in both directions along an axis. For example, if a 50 by 50 rectangle is enlarged by 50 in the x-axis, the resultant rectangle will be 150 units long (the original 50, the 50 in the minus direction, and the 50 in the plus direction) maintaining the rectangle's geometric center.
+      void inflate(const drawing::size_f& sz) noexcept;
+      /// @brief Enlarges this xtd::drawing::rectangle_f by the specified amount.
+      /// @param width The amount to inflate this xtd::drawing::rectangle_f horizontally.
+      /// @param height The amount to inflate this xtd::drawing::rectangle_f verticaly.
+      /// @remarks This method enlarges this rectangle, not a copy of it. The rectangle is enlarged in both directions along an axis. For example, if a 50 by 50 rectangle is enlarged by 50 in the x-axis, the resultant rectangle will be 150 units long (the original 50, the 50 in the minus direction, and the 50 in the plus direction) maintaining the rectangle's geometric center.
+      /// @remarks If either x or y is negative, the xtd::drawing::rectangle_f structure is deflated in the corresponding direction.
+      void inflate(float width, float height) noexcept;
+      /// @brief Creates and returns an enlarged copy of the specified xtd::drawing::rectangle_f structure. The copy is enlarged by the specified amount. The original xtd::drawing::rectangle_f structure remains unmodified.
+      /// @param rect The xtd::drawing::rectangle_f with which to start. This rectangle is not modified.
+      /// @param x The amount to inflate the specified rectangle horizontally.
+      /// @param y The amount to inflate the specified rectangle vertically.
+      /// @return The enlarged xtd::drawing::rectangle_f.
+      /// @remarks This method makes a copy of rect, enlarges the copy, and then returns the enlarged copy. The rectangle is enlarged in both directions along an axis. For example, if a 50 by 50 rectangle is enlarged by 50 in the x-axis, the resultant rectangle will be 150 units long (the original 50, the 50 in the minus direction, and the 50 in the plus direction) maintaining the rectangle's geometric center.
+      static rectangle_f inflate(const rectangle_f& rect, const drawing::size_f& sz) noexcept;
+      /// @brief Creates and returns an enlarged copy of the specified xtd::drawing::rectangle_f structure. The copy is enlarged by the specified amount. The original xtd::drawing::rectangle_f structure remains unmodified.
+      /// @param rect The xtd::drawing::rectangle_f with which to start. This rectangle is not modified.
+      /// @param sz The amount to inflate the specified rectangle.
+      /// @return The enlarged xtd::drawing::rectangle_f.
+      /// @remarks This method makes a copy of rect, enlarges the copy, and then returns the enlarged copy. The rectangle is enlarged in both directions along an axis. For example, if a 50 by 50 rectangle is enlarged by 50 in the x-axis, the resultant rectangle will be 150 units long (the original 50, the 50 in the minus direction, and the 50 in the plus direction) maintaining the rectangle's geometric center.
+      static rectangle_f inflate(const rectangle_f& rect, float width, float height) noexcept;
       
-      bool intersects_with(const rectangle_f& rect) const {return (rect.x_ < x_ + width_) && (x_ < (rect.x_ + rect.width_)) && (rect.y_ < y_ + height_) && (y_ < rect.y_ + rect.height_);}
+      /// @brief Determines if this rectangle intersects with rect.
+      /// @param rect The rectangle to test.
+      /// @return This method returns true if there is any intersection, otherwise false.
+      bool intersects_with(const rectangle_f& rect) const noexcept;
       
-      static rectangle_f make_intersect(const rectangle_f& a, const rectangle_f& b) {
-        auto result = a;
-        result.make_intersect(b);
-        return result;
-      }
-      void make_intersect(const rectangle_f& rect);
+      /// @brief Returns a third xtd::drawing::rectangle_f structure that represents the intersection of two other xtd::drawing::rectangle_f structures. If there is no intersection, an empty xtd::drawing::rectangle_f is returned.
+      /// @param a A rectangle to intersect.
+      /// @param b A rectangle to intersect.
+      /// @return A xtd::drawing::rectangle_f that represents the intersection of a and b.
+      static rectangle_f make_intersect(const rectangle_f& a, const rectangle_f& b) noexcept;
+      /// @brief Replaces this xtd::drawing::rectangle_f with the intersection of itself and the specified xtd::drawing::rectangle_f.
+      /// @param rect The xtd::drawing::rectangle_f with which to intersect.
+      void make_intersect(const rectangle_f& rect) noexcept;
       
-      static rectangle_f make_union(const rectangle_f& a, const rectangle_f& b) {
-        auto result = a;
-        result.make_union(b);
-        return result;
-      }
-      void make_union(const rectangle_f& rect);
+      /// @brief Gets a xtd::drawing::rectangle_f structure that contains the union of two xtd::drawing::rectangle_f structures.
+      /// @param a A rectangle to union.
+      /// @param b A rectangle to union.
+      /// @return A xtd::drawing::rectangle_f structure that bounds the union of the two xtd::drawing::rectangle_f structures.
+      static rectangle_f make_union(const rectangle_f& a, const rectangle_f& b) noexcept;
+      /// @brief Replaces this xtd::drawing::rectangle_f with the union of itself and the specified xtd::drawing::rectangle_f.
+      /// @param rect The xtd::drawing::rectangle_f with which to union.
+      void make_union(const rectangle_f& rect) noexcept;
       
-      void offset(const point_f& pt) {offset(pt.x(), pt.y());}
-      void offset(float dx, float dy) {
-        x_ += dx;
-        y_ += dy;
-      }
-      static rectangle_f offset(const rectangle_f& rect, const point_f& pt) {return offset(rect, pt.x(), pt.y());}
-      static rectangle_f offset(const rectangle_f& rect, float x, float y) {
-        auto result = rect;
-        result.offset(x, y);
-        return result;
-      }
+      /// @brief Adjusts the location of this rectangle by the specified amount.
+      /// @param pos Amount to offset the location.
+      /// @remarks This method adjusts the location of the upper-left corner horizontally by the x-coordinate of the specified point, and vertically by the y-coordinate of the specified point.
+      void offset(const point_f& pos) noexcept;
+      /// @brief Adjusts the location of this rectangle by the specified amount.
+      /// @param x The horizontal offset.
+      /// @param y The vertical offset.
+      void offset(float x, float y) noexcept;
+      /// @brief Creates and returns an offsetted copy of the specified xtd::drawing::rectangle_f structure. The copy is offsetted by the specified amount. The original xtd::drawing::rectangle_f structure remains unmodified.
+      /// @param rect The xtd::drawing::rectangle_f with which to start. This rectangle is not modified.
+      /// @param pos The amount to offset the specified rectangle.
+      /// @return The offsetted xtd::drawing::rectangle_f.
+      /// @remarks This method makes a copy of rect, offsets the copy, and then returns the offsetted copy.
+      static rectangle_f offset(const rectangle_f& rect, const point_f& pos) noexcept;
+      /// @brief Creates and returns an offsetted copy of the specified xtd::drawing::rectangle_f structure. The copy is offsetted by the specified amount. The original xtd::drawing::rectangle_f structure remains unmodified.
+      /// @param rect The xtd::drawing::rectangle_f with which to start. This rectangle is not modified.
+      /// @param x The amount to offset the specified rectangle horizontally.
+      /// @param y The amount to offset the specified rectangle vertically.
+      /// @return The offsetted xtd::drawing::rectangle_f.
+      /// @remarks This method makes a copy of rect, offsets the copy, and then returns the offsetted copy.
+      static rectangle_f offset(const rectangle_f& rect, float x, float y) noexcept;
       
-      xtd::ustring to_string() const noexcept override {return "{x=" + std::to_string(x_) + ", y=" + std::to_string(y_) + ", width=" + std::to_string(width_) + ", height=" + std::to_string(height_) + "}";}
+      /// @brief Converts the attributes of this xtd::drawing::rectangle_f to a human-readable string.
+      /// @return A string that contains the position, width, and height of this Rectangle structure ¾ for example, {X=20, Y=20, Width=100, Height=50}.
+      xtd::ustring to_string() const noexcept override;
       /// @}
 
       /// @name Operators
       
       /// @{
-      bool operator==(const rectangle_f& value) const {return x_ == value.x_ && y_ == value.y_ && width_ == value.width_ && height_ == value.height_;}
-      bool operator!=(const rectangle_f& value) const {return !operator==(value);}
+      /// @brief Tests whether two xtd::drawing::rectangle_f structures have equal location and size.
+      /// @param value The xtd::drawing::rectangle_f structure that is to the right of the equality operator.
+      /// @return This operator returns true if the two xtd::drawing::rectangle_f structures have equal xtd::drawing::rectangle_f::x, xtd::drawing::rectangle_f::y, xtd::drawing::rectangle_f::width, and xtd::drawing::rectangle_f::height properties.
+      bool operator==(const rectangle_f& value) const noexcept;
+      /// @brief Tests whether two xtd::drawing::rectangle_f structures differ in location or size.
+      /// @param value The xtd::drawing::rectangle_f structure that is to the right of the inequality operator.
+      /// @return This operator returns true if any of the xtd::drawing::rectangle_f::x, xtd::drawing::rectangle_f::y, xtd::drawing::rectangle_f::width or xtd::drawing::rectangle_f::height properties of the two xtd::drawing::rectangle_f structures are unequal; otherwise false.
+      bool operator!=(const rectangle_f& value) const noexcept;
       /// @}
       
       /// @cond
