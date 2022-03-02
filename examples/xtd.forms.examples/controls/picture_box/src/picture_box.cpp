87ee2cf7a0d39dc2265b9d68874d0133500b3d4e
Gammasoft <gammasoft71@gmail.com>
2022-03-02 22:47:57 +0100

    fix build

--- examples/xtd.forms.examples/controls/picture_box/src/picture_box.cpp
+++ examples/xtd.forms.examples/controls/picture_box/src/picture_box.cpp
@@ -22,7 +22,7 @@ namespace picture_box {
       };
       
       picture_box1.parent(*this);
-      picture_box1.border_style(border_style::etched);
+      picture_box1.border_style(border_style::groove);
       picture_box1.location({20, 65});
       picture_box1.size({260, 260});
       picture_box1.image(properties::resources::logo());
