cf96dbda63aa6edd78cbb1ab10600b06e4720a1f
Gammasoft <gammasoft71@gmail.com>
2022-03-07 22:51:48 +0100

    add interlocked example

---
+++ examples/xtd.core.examples/threading/interlocked/README.md
@@ -0,0 +1,24 @@
+# interlocked
+
+Shows how to use [xtd::threading::interlocked](../../../../src/xtd.core/include/xtd/threading/interlocked.h) class.
+
+## Sources
+
+[src/interlocked.cpp](src/interlocked.cpp)
+
+[CMakeLists.txt](CMakeLists.txt)
+
+# Build and run
+
+Open "Command Prompt" or "Terminal". Navigate to the folder that contains the project and type the following:
+
+```cmake
+xtdc run
+```
+
+# Output
+
+```
+www.google.com addresses:
+  * 216.58.208.100
+```
---
+++ examples/xtd.core.examples/threading/interlocked/src/interlocked.cpp
@@ -0,0 +1,74 @@
+#include <vector>
+#include <thread>
+#include <xtd/xtd>
+
+using namespace std;
+using namespace std::chrono;
+using namespace xtd;
+using namespace xtd::threading;
+
+namespace examples {
+  struct my_thread {
+    std::thread thread;
+    ustring name;
+  };
+  class my_interlocked_exchange_class : public object {
+  private:
+    //0 for False, 1 for True.
+    inline static int using_resource;
+    
+    static const int num_thread_iterations = 5;
+    static const int num_threads = 10;
+    
+  public:
+    // The main entry point for the application.
+    static void main() {
+      vector<my_thread> my_threads(num_threads);
+      xtd::random rnd;
+      
+      for (int i = 0; i < num_threads; ++i) {
+        my_threads[i].name = ustring::format("Thread{0}", i + 1);
+        
+        //Wait a random amount of time before starting next thread.
+        this_thread::sleep_for(milliseconds(rnd.next(0, 1000)));
+        my_threads[i].thread = thread(my_thread_proc, my_threads[i].name);
+      }
+      
+      for (int i = 0; i < num_threads; ++i)
+        my_threads[i].thread.join();
+    }
+    
+    static void my_thread_proc(const ustring& name) {
+      for (int i = 0; i < num_thread_iterations; i++) {
+        use_resource(name);
+        
+        //Wait 1 second before next attempt.
+        this_thread::sleep_for(milliseconds(1000));
+      }
+    }
+    
+    //A simple method that denies reentrancy.
+    static bool use_resource(const ustring& name) {
+      //0 indicates that the method is not in use.
+      if (0 == interlocked::exchange(using_resource, 1)) {
+        console::write_line("{0} acquired the lock", name);
+        
+        //Code to access a resource that is not thread safe would go here.
+        
+        //Simulate some work
+        this_thread::sleep_for(milliseconds(500));
+        
+        console::write_line("{0} exiting lock", name);
+        
+        //Release the lock
+        interlocked::exchange(using_resource, 0);
+        return true;
+      } else {
+        console::write_line("   {0} was denied the lock", name);
+        return false;
+      }
+    }
+  };
+}
+
+startup_(examples::my_interlocked_exchange_class);
