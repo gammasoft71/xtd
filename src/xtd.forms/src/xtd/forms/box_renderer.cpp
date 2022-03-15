24f434f7c36de3cbb0de72211fa99b9f0f0dc027
Gammasoft <gammasoft71@gmail.com>
2022-03-15 21:45:19 +0100

    review fill_box

--- src/xtd.forms/src/xtd/forms/box_renderer.cpp
+++ src/xtd.forms/src/xtd/forms/box_renderer.cpp
@@ -14,20 +14,15 @@ using namespace xtd::forms::style_sheets;
 
 namespace {
   static void fill_box(xtd::drawing::graphics& graphics, const xtd::drawing::brush& brush, const xtd::drawing::rectangle& fill_rect, int radius_top_left, int radius_top_right, int radius_bottom_right, int radius_bottom_left) {
-    radius_top_left *= 2;
-    radius_top_right *= 2;
-    radius_bottom_right *= 2;
-    radius_bottom_left *= 2;
-    
     /*
      graphics_path path;
-     if (radius_top_left != 0) path.add_arc(fill_rect.left(), fill_rect.top(), radius_top_left, radius_top_left, 180, 90);
+     if (radius_top_left != 0) path.add_arc(fill_rect.left(), fill_rect.top(), radius_top_left * 2, radius_top_left * 2, 180, 90);
      path.add_line(fill_rect.left() + radius_top_left / 2, fill_rect.top(), fill_rect.right() - radius_top_right / 2, fill_rect.top());
-     if (radius_top_right != 0) path.add_arc(fill_rect.right() - radius_top_right, fill_rect.top(), radius_top_right, radius_top_right, 270, 90);
+     if (radius_top_right != 0) path.add_arc(fill_rect.right() - radius_top_right * 2, fill_rect.top(), radius_top_right * 2, radius_top_right * 2, 270, 90);
      path.add_line(fill_rect.right(), fill_rect.top() + radius_top_right / 2, fill_rect.right(), fill_rect.bottom() - radius_bottom_right / 2);
-     if (radius_bottom_right != 0) path.add_arc(fill_rect.right() - radius_bottom_right, fill_rect.bottom() - radius_bottom_right, radius_bottom_right, radius_bottom_right, 0, 90);
+     if (radius_bottom_right != 0) path.add_arc(fill_rect.right() - radius_bottom_right * 2, fill_rect.bottom() - radius_bottom_right * 2, radius_bottom_right * 2, radius_bottom_right * 2, 0, 90);
      path.add_line(fill_rect.right() - radius_bottom_right  /2, fill_rect.bottom(), fill_rect.left() + radius_bottom_left / 2, fill_rect.bottom());
-     if (radius_bottom_left != 0) path.add_arc(fill_rect.left(), fill_rect.bottom() - radius_bottom_left, radius_bottom_left, radius_bottom_left, 90, 90);
+     if (radius_bottom_left != 0) path.add_arc(fill_rect.left(), fill_rect.bottom() - radius_bottom_left * 2, radius_bottom_left * 2, radius_bottom_left * 2, 90, 90);
      path.add_line(fill_rect.left(), fill_rect.bottom() - radius_bottom_left  /2, fill_rect.left(), fill_rect.top() + radius_top_left / 2);
      path.close_all_figures();
      graphics.fill_path(brush, path);
@@ -38,32 +33,32 @@ namespace {
     
     if (radius_top_left != 0) {
       xtd::drawing::drawing2d::graphics_path path;
-      path.add_pie(fill_rect.left(), fill_rect.top(), radius_top_left, radius_top_left, 180, 90);
+      path.add_pie(fill_rect.left(), fill_rect.top(), radius_top_left * 2, radius_top_left * 2, 180, 90);
       if (path.get_bounds().width() > 0 && path.get_bounds().height() > 0) region.make_union(path);
     }
     if (radius_top_right != 0) {
       xtd::drawing::drawing2d::graphics_path path;
-      path.add_pie(fill_rect.right() - radius_top_right, fill_rect.top(), radius_top_right, radius_top_right, 270, 90);
+      path.add_pie(fill_rect.right() - radius_top_right * 2, fill_rect.top(), radius_top_right * 2, radius_top_right * 2, 270, 90);
       if (path.get_bounds().width() > 0 && path.get_bounds().height() > 0) region.make_union(path);
     }
     if (radius_bottom_right != 0) {
       xtd::drawing::drawing2d::graphics_path path;
-      path.add_pie(fill_rect.right() - radius_bottom_right, fill_rect.bottom() - radius_bottom_right, radius_bottom_right, radius_bottom_right, 0, 90);
+      path.add_pie(fill_rect.right() - radius_bottom_right * 2, fill_rect.bottom() - radius_bottom_right * 2, radius_bottom_right * 2, radius_bottom_right * 2, 0, 90);
       if (path.get_bounds().width() > 0 && path.get_bounds().height() > 0) region.make_union(path);
     }
     if (radius_bottom_left != 0) {
       xtd::drawing::drawing2d::graphics_path path;
-      path.add_pie(fill_rect.left(), fill_rect.bottom() - radius_bottom_left, radius_bottom_left, radius_bottom_left, 90, 90);
+      path.add_pie(fill_rect.left(), fill_rect.bottom() - radius_bottom_left * 2, radius_bottom_left * 2, radius_bottom_left * 2, 90, 90);
       if (path.get_bounds().width() > 0 && path.get_bounds().height() > 0) region.make_union(path);
     }
-    region.make_union(rectangle(fill_rect.left() + radius_top_left / 2, fill_rect.top() + xtd::math::max(radius_top_left, radius_top_right) / 2, fill_rect.width() - radius_top_left / 2 - radius_top_right / 2, fill_rect.height() - xtd::math::max(radius_top_left, radius_top_right) / 2 - xtd::math::max(radius_bottom_left, radius_bottom_right) / 2));
+    region.make_union(rectangle(fill_rect.left() + radius_top_left, fill_rect.top() + xtd::math::max(radius_top_left, radius_top_right), fill_rect.width() - radius_top_left - radius_top_right, fill_rect.height() - xtd::math::max(radius_top_left, radius_top_right) - xtd::math::max(radius_bottom_left, radius_bottom_right)));
     
-    region.make_union(rectangle(fill_rect.left() + radius_top_left / 2, fill_rect.top(), fill_rect.width() - radius_top_left / 2 - radius_top_right / 2, xtd::math::max(radius_top_left, radius_top_right) / 2));
-    region.make_union(rectangle(fill_rect.left() + radius_bottom_left / 2, fill_rect.bottom() - 1, fill_rect.width() - radius_bottom_left / 2 - radius_bottom_right / 2, -math::max(radius_bottom_left, radius_bottom_right) / 2 - 1));
+    region.make_union(rectangle(fill_rect.left() + radius_top_left, fill_rect.top(), fill_rect.width() - radius_top_left - radius_top_right, xtd::math::max(radius_top_left, radius_top_right)));
+    region.make_union(rectangle(fill_rect.left() + radius_bottom_left, fill_rect.bottom() - 1, fill_rect.width() - radius_bottom_left - radius_bottom_right, -math::max(radius_bottom_left, radius_bottom_right) - 1));
     
-    region.make_union(rectangle(fill_rect.left(), fill_rect.top() + radius_top_left / 2, xtd::math::max(radius_top_left, radius_bottom_left) / 2, fill_rect.height() - radius_top_left / 2 - radius_bottom_left / 2));
-    region.make_union(rectangle(fill_rect.right() - 1 - xtd::math::max(radius_top_right, radius_bottom_right) / 2, fill_rect.top() + radius_top_right / 2, xtd::math::max(radius_top_right, radius_bottom_right) / 2 + 1, fill_rect.height() - radius_top_right / 2 - radius_bottom_right / 2));
-
+    region.make_union(rectangle(fill_rect.left(), fill_rect.top() + radius_top_left, xtd::math::max(radius_top_left, radius_bottom_left), fill_rect.height() - radius_top_left - radius_bottom_left));
+    region.make_union(rectangle(fill_rect.right() - 1 - xtd::math::max(radius_top_right, radius_bottom_right), fill_rect.top() + radius_top_right, xtd::math::max(radius_top_right, radius_bottom_right) + 1, fill_rect.height() - radius_top_right - radius_bottom_right));
+    
     graphics.fill_region(brush, region);
   }
 }
