5e363dd4c8bcec67fdf28a192f8ad4f2a77649f7
Gammasoft <gammasoft71@gmail.com>
2022-03-29 15:39:32 +0200

    Add focused pseudo state

--- src/xtd.forms/include/xtd/forms/style_sheets/pseudo_state.h
+++ src/xtd.forms/include/xtd/forms/style_sheets/pseudo_state.h
@@ -25,14 +25,16 @@ namespace xtd {
         pressed = 0b10,
         /// @brief The irem is disabled, generally the item is grayed.
         disabled = 0b100,
+        /// @brief The item is focused.
+        focused = 0b1000,
         /// @brief The item is the default (see ibutton_control).
-        default_state = 0b1000,
+        default_state = 0b10000,
         /// @brief The item is unchecked.
         unchecked = standard,
         /// @brief The item is checked.
-        checked = 0b10000,
+        checked = 0b100000,
         /// @brief The item is mixed. For the three-state ittem.
-        mixed = 0b100000,
+        mixed = 0b1000000,
       };
       
       /// @cond
@@ -47,8 +49,8 @@ namespace xtd {
       inline pseudo_state operator &(pseudo_state lhs, pseudo_state rhs) {return static_cast<pseudo_state>(static_cast<long long>(lhs) & static_cast<long long>(rhs));}
       inline pseudo_state operator |(pseudo_state lhs, pseudo_state rhs) {return static_cast<pseudo_state>(static_cast<long long>(lhs) | static_cast<long long>(rhs));}
       inline pseudo_state operator ^(pseudo_state lhs, pseudo_state rhs) {return static_cast<pseudo_state>(static_cast<long long>(lhs) ^ static_cast<long long>(rhs));}
-      inline std::ostream& operator<<(std::ostream& os, const pseudo_state value) {return os << to_string(value, {{pseudo_state::standard, "standard"}, {pseudo_state::hover, "hover"}, {pseudo_state::pressed, "pressed"}, {pseudo_state::disabled, "disabled"}, {pseudo_state::default_state, "default_state"}, {pseudo_state::unchecked, "unchecked"}, {pseudo_state::checked, "checked"}, {pseudo_state::mixed, "mixed"}});}
-      inline std::wostream& operator<<(std::wostream& os, const pseudo_state value) {return os << to_string(value, {{pseudo_state::standard, L"standard"}, {pseudo_state::hover, L"hover"}, {pseudo_state::pressed, L"pressed"}, {pseudo_state::disabled, L"disabled"}, {pseudo_state::default_state, L"default_state"}, {pseudo_state::unchecked, L"unchecked"}, {pseudo_state::checked, L"checked"}, {pseudo_state::mixed, L"mixed"}});}
+      inline std::ostream& operator<<(std::ostream& os, const pseudo_state value) {return os << to_string(value, {{pseudo_state::standard, "standard"}, {pseudo_state::hover, "hover"}, {pseudo_state::pressed, "pressed"}, {pseudo_state::disabled, "disabled"}, {pseudo_state::focused, "focused"}, {pseudo_state::default_state, "default_state"}, {pseudo_state::unchecked, "unchecked"}, {pseudo_state::checked, "checked"}, {pseudo_state::mixed, "mixed"}});}
+      inline std::wostream& operator<<(std::wostream& os, const pseudo_state value) {return os << to_string(value, {{pseudo_state::standard, L"standard"}, {pseudo_state::hover, L"hover"}, {pseudo_state::pressed, L"pressed"}, {pseudo_state::disabled, L"disabled"}, {pseudo_state::focused, L"focused"}, {pseudo_state::default_state, L"default_state"}, {pseudo_state::unchecked, L"unchecked"}, {pseudo_state::checked, L"checked"}, {pseudo_state::mixed, L"mixed"}});}
       /// @endcond
     }
   }
