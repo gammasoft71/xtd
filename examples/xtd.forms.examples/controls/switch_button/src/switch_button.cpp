c5adca82524510a57478252c613bb146fd58e783
Gammasoft <gammasoft71@gmail.com>
2022-04-11 19:31:32 +0200

    Update switch_button.cpp

--- examples/xtd.forms.examples/controls/switch_button/src/switch_button.cpp
+++ examples/xtd.forms.examples/controls/switch_button/src/switch_button.cpp
@@ -19,19 +19,33 @@ public:
     switch_button2.parent(*this);
     switch_button2.location({50, 70});
     switch_button2.checked_changed += [&] {
-      label1.text(ustring::format("switch: {}", switch_button2.checked() ? "On" : "Off"));
+      label1.text(ustring::format("rounded switch: {}", switch_button2.checked() ? "On" : "Off"));
     };
     switch_button2.checked(true);
-    
+
     label1.parent(*this);
-    label1.location({50, 110});
+    label1.location({110, 72});
     label1.auto_size(true);
+    
+    switch_button3.parent(*this);
+    switch_button3.location({50, 110});
+    switch_button3.rounded(false);
+    switch_button3.checked_changed += [&] {
+      label2.text(ustring::format("square switch: {}", switch_button3.checked() ? "On" : "Off"));
+    };
+    switch_button3.checked(true);
+
+    label2.parent(*this);
+    label2.location({110, 112});
+    label2.auto_size(true);
   }
   
 private:
   switch_button switch_button1;
   switch_button switch_button2;
+  switch_button switch_button3;
   label label1;
+  label label2;
 };
 
 int main() {
