2f035fd8d7f7281ee3b178769ccc54b8fd28ea79
Gammasoft <gammasoft71@gmail.com>
2022-04-19 14:17:00 +0200

    Add uninstall xtd.

---
+++ scripts/install/uninstall.cmd
@@ -0,0 +1,31 @@
+@echo off
+
+if "%xtd_version%"== "" (
+  echo ERROR : Use uninstall.cmd from root folder
+  exit /B 1
+)
+
+echo Uninstall xtd libraries version %xtd_version%, copyright Gammasoft, 2022
+echo.
+
+:: check if administrator mode
+whoami /Groups | find "12288" > nul 2>&1
+if %ERRORLEVEL% neq 0 (
+  echo You are not in administrator mode!
+  echo Run cmd as administrator before execute install.
+  exit /B 1
+)
+
+echo WARNING : You will unsinstall xtd.
+echo Press ENTER to continue or CTRL-C to stop...
+pause > nul 2>&1
+
+echo Removing wxwidgets files...
+del /F /S /Q "%ProgramFiles(x86)%\wxwidgets" > nul 2>&1
+echo Removing xtd files...
+del /F /S /Q "%ProgramFiles(x86)%\xtd" > nul 2>&1
+del /F /S /Q "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\xtd" > nul 2>&1
+
+echo.
+echo xtd is uninstalled.
+echo If you want to use xtd again, you will have to reinstall it.
