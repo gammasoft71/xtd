eae4605f52f194727ec5a2a97c9e476c08a8fcdd
Gammasoft <gammasoft71@gmail.com>
2022-04-23 20:37:26 +0200

    Update search_direction.h

--- src/xtd.forms/include/xtd/forms/search_direction.h
+++ src/xtd.forms/include/xtd/forms/search_direction.h
@@ -8,7 +8,7 @@
 namespace xtd {
   /// @brief The xtd::forms namespace contains classes for creating Windows-based applications that take full advantage of the rich user interface features available in the Microsoft Windows operating system, Apple macOS and Linux like Ubuntu operating system.
   namespace forms {
-    /// @brief Specifies the search direction for xtd::forms::find_dialog and xtd::forms::replace_dialog dialogs.
+    /// @brief Specifies the search direction for xtd::forms::find_dialog dialog.
     /// @par Namespace
     /// xtd::forms
     /// @par Library
