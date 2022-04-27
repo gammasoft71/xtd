3e9e9cfe0b67c55102c2673dbd2021a5a7a68fa7
Gammasoft <gammasoft71@gmail.com>
2022-04-27 19:43:09 +0200

    Update colors.cpp

--- examples/xtd.forms.examples/others/colors/src/colors.cpp
+++ examples/xtd.forms.examples/others/colors/src/colors.cpp
@@ -218,6 +218,7 @@ namespace examples {
       panel_color_box.paint += [&](object& sender, paint_event_args& e) {
         e.graphics().fill_rectangle(hatch_brush(hatch_style::wide_checker_board, color::from_argb(0x99, 0x99, 0x99), color::from_argb(0x66, 0x66, 0x66)), e.clip_rectangle());
         e.graphics().fill_rectangle(solid_brush(color_), e.clip_rectangle());
+        control_paint::draw_border(panel_color_box, e.graphics(), panel_color_box.border_style(), panel_color_box.border_sides(), application::style_sheet().system_colors().control_text(), e.clip_rectangle());
       };
     }
     
