c8058934afcaa21429a8373b34e9d16df1518dd3
Gammasoft <gammasoft71@gmail.com>
2022-04-27 15:49:02 +0200

    Update color_picker.cpp

--- examples/xtd.forms.examples/controls/color_picker/src/color_picker.cpp
+++ examples/xtd.forms.examples/controls/color_picker/src/color_picker.cpp
@@ -15,6 +15,7 @@ namespace examples {
       test_zone.location({10, 50});
       test_zone.back_color(color::red);
       test_zone.border_style(border_style::fixed_3d);
+      test_zone.double_buffered(true);
       test_zone.paint += [&](object& sender, paint_event_args& e) {
         e.graphics().fill_rectangle(hatch_brush(hatch_style::wide_checker_board, color::from_argb(0x99, 0x99, 0x99), color::from_argb(0x66, 0x66, 0x66)), e.clip_rectangle());
         e.graphics().fill_rectangle(solid_brush(color_picker.color()), e.clip_rectangle());
