8b8276e11fa8ab0e89d3b6e187f51b3051240e80
Gammasoft <gammasoft71@gmail.com>
2022-04-27 19:43:05 +0200

    Update color_picker.cpp

--- examples/xtd.forms.examples/controls/color_picker/src/color_picker.cpp
+++ examples/xtd.forms.examples/controls/color_picker/src/color_picker.cpp
@@ -13,16 +13,16 @@ namespace examples {
       
       test_zone.parent(*this);
       test_zone.location({10, 50});
-      test_zone.back_color(color::red);
       test_zone.border_style(border_style::fixed_3d);
       test_zone.double_buffered(true);
       test_zone.paint += [&](object& sender, paint_event_args& e) {
         e.graphics().fill_rectangle(hatch_brush(hatch_style::wide_checker_board, color::from_argb(0x99, 0x99, 0x99), color::from_argb(0x66, 0x66, 0x66)), e.clip_rectangle());
         e.graphics().fill_rectangle(solid_brush(color_picker.color()), e.clip_rectangle());
+        control_paint::draw_border(test_zone, e.graphics(), test_zone.border_style(), test_zone.border_sides(), application::style_sheet().system_colors().control_text(), e.clip_rectangle());
       };
 
       color_picker.parent(*this);
-      color_picker.color(test_zone.back_color());
+      color_picker.color(color::red);
       color_picker.location({10, 10});
       color_picker.color_changed += [&] {
         test_zone.invalidate();
