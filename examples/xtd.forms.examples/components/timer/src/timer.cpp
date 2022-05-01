7b50b37be719f22553ad48b3485f58013a328940
Gammasoft <gammasoft71@gmail.com>
2022-05-01 18:51:09 +0200

    Update timer.cpp

--- examples/xtd.forms.examples/components/timer/src/timer.cpp
+++ examples/xtd.forms.examples/components/timer/src/timer.cpp
@@ -14,7 +14,6 @@ namespace examples {
       label1.font(drawing::font("Arial", 48, font_style::italic));
       label1.location({10, 10});
       label1.auto_size(true);
-      label1.flat_style(flat_style::system);
       label1.fore_color(color::dodger_blue);
 
       button1.parent(*this);
