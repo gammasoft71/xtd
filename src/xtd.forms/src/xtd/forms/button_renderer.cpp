f5cc4cee0c1508c0ff5218a0e5c2d8cd339555ec
Gammasoft <gammasoft71@gmail.com>
2022-05-16 11:09:17 +0200

    Add tool_bar_button_renderer class

--- src/xtd.forms/src/xtd/forms/button_renderer.cpp
+++ src/xtd.forms/src/xtd/forms/button_renderer.cpp
@@ -146,40 +146,6 @@ void button_renderer::draw_popup_button(const style_sheets::style_sheet& style_s
   text_renderer::draw_text(graphics, content_rectangle, text, current_style_sheet);
 }
 
-void button_renderer::draw_toolbar_button(const style_sheets::style_sheet& style_sheet, graphics& graphics, const rectangle& bounds, push_button_state button_state, bool default_button, const optional<color>& back_color, const flat_button_appearance& appearance, const ustring& text, const optional<content_alignment>& text_align, const optional<color>& fore_color, const optional<font>& font, const xtd::drawing::image& image, const optional<content_alignment>& image_align) {
-  auto pseudo_state_base = pseudo_state::standard;
-  if (default_button) pseudo_state_base += pseudo_state::default_state;
-  
-  button current_style_sheet;
-  switch (button_state) {
-    case push_button_state::normal: current_style_sheet = style_sheet.flat_button(pseudo_state_base); break;
-    case push_button_state::hot: current_style_sheet = style_sheet.flat_button(pseudo_state_base | pseudo_state::hover); break;
-    case push_button_state::pressed: current_style_sheet = style_sheet.flat_button(pseudo_state_base | pseudo_state::pressed); break;
-    case push_button_state::checked: current_style_sheet = style_sheet.flat_button(pseudo_state_base | pseudo_state::checked); break;
-    case push_button_state::disabled: current_style_sheet = style_sheet.flat_button(pseudo_state_base | pseudo_state::disabled); break;
-    case push_button_state::default_state: current_style_sheet = style_sheet.flat_button(pseudo_state_base | pseudo_state::default_state); break;
-  }
-  if (back_color.has_value() && (button_state == push_button_state::normal || button_state == push_button_state::default_state)) current_style_sheet.background_color(back_color.value());
-  if (text_align.has_value()) current_style_sheet.text_alignment(text_align.value());
-  if (fore_color.has_value() && button_state != push_button_state::disabled) current_style_sheet.color(fore_color.value());
-  if (font.has_value()) current_style_sheet.font(font.value());
-  if (image_align.has_value()) current_style_sheet.image_alignment(image_align.value());
-  
-  if (appearance.border_radius().has_value()) current_style_sheet.border_radius(xtd::forms::style_sheets::border_radius(appearance.border_radius().value()));
-  if (appearance.border_size().has_value()) current_style_sheet.border_width(xtd::forms::style_sheets::border_width(appearance.border_size().value()));
-  if (appearance.border_color().has_value()) current_style_sheet.border_color(xtd::forms::style_sheets::border_color(appearance.border_color().value()));
-  if (appearance.checked_back_color().has_value() && button_state == push_button_state::checked) current_style_sheet.background_color(appearance.checked_back_color().value());
-  if (appearance.mouse_down_back_color().has_value() && button_state == push_button_state::pressed) current_style_sheet.background_color(appearance.mouse_down_back_color().value());
-  if (appearance.mouse_over_back_color().has_value() && button_state == push_button_state::hot) current_style_sheet.background_color(appearance.mouse_over_back_color().value());
-  
-  box_renderer::draw_box(graphics, bounds, current_style_sheet);
-  auto content_rectangle = current_style_sheet.get_content_rectangle(bounds);
-  image_renderer::draw_image(graphics, content_rectangle, image, button_state != push_button_state::disabled, current_style_sheet.get_solid_background_color(), current_style_sheet);
-  if (current_style_sheet.text_alignment() == content_alignment::middle_left && image != drawing::image::empty)
-    content_rectangle = rectangle::offset(content_rectangle, drawing::point(image.size().width() + 4, 0));
-  text_renderer::draw_text(graphics, content_rectangle, text, current_style_sheet);
-}
-
 namespace {
   xtd::ustring get_hotkey_prefix_locations(const xtd::ustring& str, std::vector<size_t>& locations) {
     auto offset = 0U;
