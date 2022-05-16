114cd4508884226b7a9bdfdfc398c0b9284624e3
Gammasoft <gammasoft71@gmail.com>
2022-05-16 20:26:20 +0200

    Update tool_bar.cpp

--- examples/xtd.forms.examples/menus_and_toolbars/tool_bar/src/tool_bar.cpp
+++ examples/xtd.forms.examples/menus_and_toolbars/tool_bar/src/tool_bar.cpp
@@ -18,6 +18,7 @@ namespace examples {
       tool_bar1.image_list().images().push_back_range({tool_bar_images::file_new(), tool_bar_images::file_open(), tool_bar_images::file_save(), tool_bar_images::file_print(), tool_bar_images::edit_cut(), tool_bar_images::edit_copy(), tool_bar_images::edit_paste(), tool_bar_images::help_about()});
       tool_bar1.items().push_back_range({new_tool_bar_button, open_tool_bar_button, save_tool_bar_button, print_tool_bar_button, tool_bar1_separator1, cut_tool_bar_button, copy_tool_bar_button, paste_tool_bar_button, tool_bar1_separator2, about_tool_bar_button});
       
+      tool_bar2.appearnce(xtd::forms::tool_bar_appearance::flat);
       tool_bar2.dock(dock_style::bottom);
       tool_bar2.show_text(true);
       tool_bar2.text_align(xtd::forms::tool_bar_text_align::right);
