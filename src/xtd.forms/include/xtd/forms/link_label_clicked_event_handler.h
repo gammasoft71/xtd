ec5638141eccce0148b289fa6f4cacc3bdf2cecf
Gammasoft <gammasoft71@gmail.com>
2022-05-28 11:20:52 +0200

    Update link_label_clicked_event_handler.h

--- src/xtd.forms/include/xtd/forms/link_label_clicked_event_handler.h
+++ src/xtd.forms/include/xtd/forms/link_label_clicked_event_handler.h
@@ -15,7 +15,7 @@ namespace xtd {
     /// xtd::forms
     /// @par Library
     /// xtd.forms
-    /// @ingroup xtd_forms forms event
+    /// @ingroup xtd_forms events
     /// @remarks When you create a xtd::forms::link_label_link_clicked_event_handler delegate, you identify the method that will handle the event. To associate the event with your event handler, add an instance of the delegate to the event. The event handler is called whenever the event occurs, until you remove the delegate.
     using link_label_clicked_event_handler = delegate<void(object& sender, link_label_clicked_event_args& e)>;
   }
