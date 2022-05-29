397892970b528b125cf081d5bc01626cefbd6cc9
Gammasoft <gammasoft71@gmail.com>
2022-05-29 21:09:32 +0200

    Add status bar na status bar panel style sheets reader

---
+++ src/xtd.forms/include/xtd/forms/style_sheets/status_bar_panel.h
@@ -0,0 +1,24 @@
+/// @file
+/// @brief Contains xtd::forms::style_sheets::status_bar_panel alias.
+/// @copyright Copyright (c) 2022 Gammasoft. All rights reserved.
+#pragma once
+#include "control.h"
+
+/// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
+namespace xtd {
+  /// @brief The xtd::forms namespace contains classes for creating Windows-based applications that take full advantage of the rich user interface features available in the Microsoft Windows operating system, Apple macOS and Linux like Ubuntu operating system.
+  namespace forms {
+    /// @brief The xtd::forms::style_sheets namespace contains various properties, states, and subcontrols that make it possible to customize the look of control.
+    namespace style_sheets {
+      /// @brief The buttton data allows you to specify the box of a status_bar_panel control.
+      /// @par Namespace
+      /// xtd::forms::style_sheets
+      /// @par Library
+      /// xtd.forms
+      /// @ingroup xtd_forms style_sheets
+      /// @remarks You need to know how the box model works.
+      /// @remarks For more information, see <a href="https://github.com/gammasoft71/xtd/blob/master/docs/guide_style_sheets_box_model.md">Box Model</a>.
+      using status_bar_panel = xtd::forms::style_sheets::control;
+    }
+  }
+}
