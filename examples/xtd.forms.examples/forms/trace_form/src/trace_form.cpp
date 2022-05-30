2c42eb1272f1a37acc2d71c2b522cd72c95f6c43
Gammasoft <gammasoft71@gmail.com>
2022-05-30 12:52:32 +0200

    Update trace_form.cpp

--- examples/xtd.forms.examples/forms/trace_form/src/trace_form.cpp
+++ examples/xtd.forms.examples/forms/trace_form/src/trace_form.cpp
@@ -25,5 +25,7 @@ private:
 
 int main() {
   trace_form trace_form;
+  trace_form.back_color(xtd::drawing::color::navy);
+  trace_form.fore_color(xtd::drawing::color::white);
   application::run(form1());
 }
