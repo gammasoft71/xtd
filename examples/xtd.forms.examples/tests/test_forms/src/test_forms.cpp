0f146fd4562a097d44fe3e3cdb8e9f72cb7b5c6d
Gammasoft <gammasoft71@gmail.com>
2022-05-31 22:38:22 +0200

    Update test_forms.cpp

--- examples/xtd.forms.examples/tests/test_forms/src/test_forms.cpp
+++ examples/xtd.forms.examples/tests/test_forms/src/test_forms.cpp
@@ -1,17 +1,10 @@
 #include <xtd/xtd>
 
-using namespace std;
-using namespace xtd;
-using namespace xtd::diagnostics;
-using namespace xtd::drawing;
-using namespace xtd::io;
-using namespace xtd::windows::forms;
-
-class virtual_console : public text_box {
+class virtual_console : public xtd::forms::text_box {
 public:
   virtual_console()  {
-    environment::current_directory(environment::get_folder_path(environment::special_folder::home));
-    prompt = ustring::format("[{}] $ ",  environment::current_directory());
+    xtd::environment::current_directory(xtd::environment::get_folder_path(xtd::environment::special_folder::home));
+    prompt = xtd::ustring::format("[{}] $ ", xtd::environment::current_directory());
     
     multiline(true);
     back_color(default_back_color());
@@ -22,12 +15,12 @@ public:
   }
   
 protected:
-  xtd::drawing::color default_back_color() const override {return color::black;}
-  xtd::drawing::font default_font() const override {return drawing::font(font_family::generic_monospace(), 10);}
-  xtd::drawing::color default_fore_color() const override {return color::lime;}
+  xtd::drawing::color default_back_color() const override {return xtd::drawing::color::black;}
+  xtd::drawing::font default_font() const override {return xtd::drawing::font(xtd::drawing::font_family::generic_monospace(), 10);}
+  xtd::drawing::color default_fore_color() const override {return xtd::drawing::color::lime;}
   xtd::drawing::size default_size() const override {return {300, 200};}
 
-  void on_key_down(key_event_args& e) override {
+  void on_key_down(xtd::forms::key_event_args& e) override {
     switch(e.key_code()) {
       case xtd::forms::keys::down: key_down_pressed(e); break;
       case xtd::forms::keys::up: key_up_pressed(e); break;
@@ -38,70 +31,78 @@ protected:
   }
   
 private:
-  ustring get_command() const {
+  xtd::ustring get_command() const {
     auto pos = text().last_index_of(prompt);
-    if (pos != ustring::npos) {
+    if (pos != xtd::ustring::npos) {
       pos += prompt.size();
       return text().substring(pos);
     }
     return "";
   }
   
-  void key_down_pressed(key_event_args& e) {
+  void key_down_pressed(xtd::forms::key_event_args& e) {
     e.handled(true);
   }
-  void key_up_pressed(key_event_args& e) {
+  void key_up_pressed(xtd::forms::key_event_args& e) {
     e.handled(true);
   }
-  void key_enter_pressed(key_event_args& e) {
+  void key_enter_pressed(xtd::forms::key_event_args& e) {
     auto command_line = get_command();
     if (!command_line.empty()) {
-      process_start_info start_info;
+      xtd::diagnostics::process_start_info start_info;
       try {
         auto args = command_line.split({' '});
         start_info.file_name(args[0]);
         if (args.size() > 1)
-          start_info.arguments(ustring::join(" ", std::vector<ustring>(args.begin() + 1, args.end())));
+          start_info.arguments(xtd::ustring::join(" ", std::vector<xtd::ustring>(args.begin() + 1, args.end())));
         
-        append_text(environment::new_line());
+        append_text(xtd::environment::new_line());
         start_info.use_shell_execute(false);
         start_info.redirect_standard_error(true);
         start_info.redirect_standard_output(true);
 
-        process proc;
-        proc.start_info(start_info);
-        proc.start();
-        istream& standard_error = proc.standard_error();
-        istream& standard_output = proc.standard_output();
-
-        stream_reader error_reader(standard_error);
-        while (!error_reader.end_of_stream()) {
-          append_text(error_reader.read_line());
-          append_text(environment::new_line());
-        }
-
-        stream_reader output_reader(standard_output);
-        while (!output_reader.end_of_stream()) {
-          append_text(output_reader.read_line());
-          append_text(environment::new_line());
-        }
-        
         if (start_info.file_name() == "cd" && !start_info.arguments().empty()) {
-          environment::current_directory(start_info.arguments());
-          prompt = ustring::format("[{}] $ ",  environment::current_directory());
+          if (xtd::io::directory::exists(start_info.arguments())) {
+            xtd::environment::current_directory(start_info.arguments());
+            prompt = xtd::ustring::format("[{}] $ ",  xtd::environment::current_directory());
+          } else {
+            append_text(xtd::ustring::format("cd: no such file or directory: {}", start_info.arguments()));
+            append_text(xtd::environment::new_line());
+          }
+        } else {
+          xtd::diagnostics::process process;
+          process.start_info(start_info);
+          process.start();
+          std::istream& standard_error = process.standard_error();
+          std::istream& standard_output = process.standard_output();
+
+          xtd::io::stream_reader error_reader(standard_error);
+          while (!error_reader.end_of_stream()) {
+            append_text(error_reader.read_line());
+            append_text(xtd::environment::new_line());
+          }
+
+          xtd::io::stream_reader output_reader(standard_output);
+          while (!output_reader.end_of_stream()) {
+            append_text(output_reader.read_line());
+            append_text(xtd::environment::new_line());
+          }
         }
       } catch(...) {
-        append_text(ustring::format("command not found: {}", start_info.file_name()));
-        append_text(environment::new_line());
+        append_text(xtd::ustring::format("command not found: {}", start_info.file_name()));
+        append_text(xtd::environment::new_line());
       }
     }
-    append_text(environment::new_line());
+    append_text(xtd::environment::new_line());
     append_text(prompt);
     e.handled(true);
   }
-  ustring prompt = ustring::format("[{}] $ ",  environment::current_directory());
-  vector<ustring> commands;
+  xtd::ustring prompt = xtd::ustring::format("[{}] $ ",  xtd::environment::current_directory());
+  std::vector<xtd::ustring> commands;
 };
+
+using namespace xtd;
+using namespace xtd::windows::forms;
 
 class form_main : public form {
 public:
