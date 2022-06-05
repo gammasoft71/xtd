7cd467d5371e362c81cc47151c47c273db7d1a3b
Bader <badereddineouaich@gmail.com>
2022-06-05 15:17:48 +0100

    fix icon was not set properly

--- src/xtd.forms.native.wxwidgets/src/xtd/forms/native/wxwidgets/message_notifier.cpp
+++ src/xtd.forms.native.wxwidgets/src/xtd/forms/native/wxwidgets/message_notifier.cpp
@@ -1,16 +1,16 @@
+#include <xtd/convert_string.h>
 #define __XTD_FORMS_NATIVE_LIBRARY__
 #include <xtd/forms/native/message_notifier.h>
 #include <xtd/forms/native/message_notifier_styles.h>
 #include "../../../../../include/xtd/forms/native/wxwidgets/control_handler.h"
 #undef __XTD_FORMS_NATIVE_LIBRARY__
-#include <xtd/convert_string.h>
 #include <wx/app.h>
 #include <wx/notifmsg.h>
 #include <wx/icon.h>
 using namespace xtd;
 using namespace xtd::forms::native;
-#include <xtd/diagnostics/debug.h>
 
+#include <xtd/diagnostics/debug.h>
 
 //int32_t convert_to_buttons(uint32_t style){
 //    switch(style)
@@ -35,30 +35,33 @@ void message_notifier::show(intptr control, const xtd::ustring& title,
                  xtd::delegate<void()> on_notifier_closed)
 {
     if (!wxTheApp) throw argument_exception(csf_);
+
    wxNotificationMessage notifmsg;
    notifmsg.SetParent(control == 0 ? nullptr : reinterpret_cast<control_handler*>(control)->control());
    notifmsg.SetTitle(convert_string::to_wstring(title));
    notifmsg.SetMessage(convert_string::to_wstring(message));
+   if(icon != xtd::drawing::icon::empty)
+      notifmsg.SetIcon(reinterpret_cast<wxIconBundle*>(icon.handle())->GetIcon());
+   else
+      notifmsg.SetFlags(convert_to_icon(style));
 
     // Events
-   notifmsg.Bind(wxEVT_NOTIFICATION_MESSAGE_DISMISSED, [&on_notifier_closed](wxCommandEvent& event) {
+   notifmsg.Bind(wxEVT_NOTIFICATION_MESSAGE_DISMISSED, [on_notifier_closed](wxCommandEvent& event) {
        xtd::diagnostics::debug::write_line("wxEVT_NOTIFICATION_MESSAGE_DISMISSED");
        on_notifier_closed();
        event.Skip();
     });
-//   notifmsg.Bind(wxEVT_NOTIFICATION_MESSAGE_CLICK, [&on_notifier_clicked](wxCommandEvent& event) {
+//   notifmsg.Bind(wxEVT_NOTIFICATION_MESSAGE_CLICK, [on_notifier_clicked](wxCommandEvent& event) {
 //       xtd::diagnostics::debug::write_line("wxEVT_NOTIFICATION_MESSAGE_CLICK");
 //       on_notifier_clicked();
 //       event.Skip();
 //    });
-//   notifmsg.Bind(wxEVT_NOTIFICATION_MESSAGE_ACTION, [&on_notifier_closed](wxCommandEvent& event) {
+//   notifmsg.Bind(wxEVT_NOTIFICATION_MESSAGE_ACTION, [on_notifier_closed](wxCommandEvent& event) {
 //       xtd::diagnostics::debug::write_line("wxEVT_NOTIFICATION_MESSAGE_ACTION");
-//       on_notifier_closed();
+//       on_notifier_action(event.GetId());
 //       event.Skip();
 //    });
-   notifmsg.SetFlags(convert_to_icon(style));
-    if(icon != xtd::drawing::icon::empty)
-       notifmsg.SetIcon(wxIcon(*reinterpret_cast<wxIcon*>(icon.handle())));
+
    notifmsg.Show(close_timeout_enabled_ ? static_cast<int32_t>(std::chrono::duration_cast<std::chrono::seconds>(close_timeout_interval).count()) : static_cast<int32_t>(wxNotificationMessage::Timeout_Never)); // Timeout_Never notification will never timeout
 
    // TODO: Problem -> Since system notifications are detached from app main thread,
