25c919146886cfd4ac48342982ec363b0a2d6e3e
Gammasoft <gammasoft71@gmail.com>
2022-06-06 16:22:14 +0200

    Update block_scope.cpp

--- examples/xtd.core.examples/keywords/block_scope/src/block_scope.cpp
+++ examples/xtd.core.examples/keywords/block_scope/src/block_scope.cpp
@@ -5,7 +5,7 @@ using namespace xtd;
 
 int main() {
   block_scope_(auto values = {1, 2, 3, 4, 5})
-  cout << ustring::join(", ", values) << endl;
+    cout << ustring::join(", ", values) << endl;
   
   // uncomment next line cause build error with undeclared identifier 'values'.
   // cout << ustring::join(", ", values) << endl;
