99cab0fca730808830adc52f1597e84139f61ce4
Gammasoft <gammasoft71@gmail.com>
2022-06-30 13:08:44 +0200

    Update status_bar.cpp

--- examples/xtd.forms.examples/menus_and_toolbars/status_bar/src/status_bar.cpp
+++ examples/xtd.forms.examples/menus_and_toolbars/status_bar/src/status_bar.cpp
@@ -12,9 +12,27 @@ namespace examples {
       client_size({820, 500});
       
       status_bar1.parent(*this);
-      status_bar1.panels().push_back_range({status_bar_panel1, status_bar_panel2, status_bar_panel3});
+      status_bar1.panels().push_back_range({status_bar_panel1, status_bar_panel2, status_bar_panel3, status_bar_panel4});
       status_bar1.show_panels(true);
-      status_bar1.sizing_grip(true);
+      
+      status_bar_panel1.auto_size(xtd::forms::status_bar_panel_auto_size::contents);
+      status_bar_panel1.border_style(xtd::forms::status_bar_panel_border_style::sunken);
+      status_bar_panel1.tool_tip_text("Status one tool tip text");
+
+      status_bar_panel2.auto_size(xtd::forms::status_bar_panel_auto_size::contents);
+      status_bar_panel2.border_style(xtd::forms::status_bar_panel_border_style::sunken);
+
+      status_bar_panel3.auto_size(xtd::forms::status_bar_panel_auto_size::spring);
+      status_bar_panel3.border_style(xtd::forms::status_bar_panel_border_style::sunken);
+
+      status_bar_panel4.auto_size(xtd::forms::status_bar_panel_auto_size::contents);
+      status_bar_panel4.border_style(xtd::forms::status_bar_panel_border_style::sunken);
+      
+      timer1.interval_milliseconds(50);
+      timer1.tick += [&] {
+        progress_bar1.value(progress_bar1.value() < progress_bar1.maximum() ? progress_bar1.value() + 1 : progress_bar1.minimum());
+      };
+      timer1.enabled(true);
     }
     
   private:
@@ -30,6 +48,9 @@ namespace examples {
     status_bar_panel status_bar_panel1 = status_bar_panel::create_panel("Status one", image_from_color(color::light_green));
     status_bar_panel status_bar_panel2 = status_bar_panel::create_panel("Status two");
     status_bar_panel status_bar_panel3 = status_bar_panel::create_panel("Status three");
+    timer timer1;
+    progress_bar progress_bar1;
+    status_bar_panel status_bar_panel4 = status_bar_panel::create_control(progress_bar1);
   };
 }
 
