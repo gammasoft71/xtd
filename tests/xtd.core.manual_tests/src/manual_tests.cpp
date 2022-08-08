2b318f3cee754d4350b47ffefd70db88b04443f0
Gammasoft <gammasoft71@gmail.com>
2022-08-08 22:22:02 +0200

    Fix console::beep on macOS

--- tests/xtd.core.manual_tests/src/manual_tests.cpp
+++ tests/xtd.core.manual_tests/src/manual_tests.cpp
@@ -4,23 +4,11 @@
 using namespace std;
 using namespace xtd;
 
-
-namespace {
-  template<typename type_t>
-  void add_bytes(vector<byte_t>& bytes, type_t value) {
-    auto value_bytes = bit_converter::get_bytes(value);
-    bytes.insert(bytes.end(), value_bytes.begin(), value_bytes.end());
-  }
-}
-
-class xtd_core_manual_test : public object {
+class xtd_core_manual_test {
 public:
   static void main() {
-    console::write_line("Hello, World!");
-    console::write_line("processor_count = {}", environment::processor_count());
-    console::write_line("os = {}", environment::os_version().name());
-    console::write_line("desktop = {}", environment::os_version().desktop_environment());
-    console::write_line("theme = {}", environment::os_version().desktop_theme());
+    console::beep(800, 200);
+    console::write_line("End");
   }
 };
 
