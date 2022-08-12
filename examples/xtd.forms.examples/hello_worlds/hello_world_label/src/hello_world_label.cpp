42045461e8bd5d46b9d09bb6f50234f79e6f70a5
Gammasoft <gammasoft71@gmail.com>
2022-08-13 00:02:21 +0200

    Update hello_world_label.cpp

--- examples/xtd.forms.examples/hello_worlds/hello_world_label/src/hello_world_label.cpp
+++ examples/xtd.forms.examples/hello_worlds/hello_world_label/src/hello_world_label.cpp
@@ -12,7 +12,7 @@ namespace examples {
       controls().push_back(label);
       
       label.dock(dock_style::fill);
-      label.font(drawing::font(label.font(), 32, font_style::bold | font_style::italic));
+      label.font(drawing::font {label.font(), 32, font_style::bold | font_style::italic});
       label.fore_color(color::green);
       label.shadow(true);
       label.text("Hello, World!");
@@ -25,5 +25,5 @@ namespace examples {
 }
 
 int main() {
-  application::run(examples::main_form());
+  application::run(examples::main_form {});
 }
