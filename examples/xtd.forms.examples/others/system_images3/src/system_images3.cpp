d9ffdeb6b22ac45f48acabe6d30242525524b094
Gammasoft <gammasoft71@gmail.com>
2022-08-17 20:29:02 +0200

    Update system_images3.cpp

--- examples/xtd.forms.examples/others/system_images3/src/system_images3.cpp
+++ examples/xtd.forms.examples/others/system_images3/src/system_images3.cpp
@@ -13,33 +13,8 @@ namespace example {
   public:
     form1() {
       text("System images example");
-      client_size({676, 330});
+      client_size({676, 350});
       controls().push_back_range({choice_theme, choice_context, picture_16, picture_32, picture_64, picture_128, picture_256, label_picture_name, button_previous, button_next});
-      
-      picture_16.back_color(system_colors::window());
-      picture_16.border_style(forms::border_style::fixed_3d);
-      picture_16.bounds({10, 40, 30, 30});
-      picture_16.size_mode(picture_box_size_mode::center_image);
-      
-      picture_32.back_color(system_colors::window());
-      picture_32.border_style(forms::border_style::fixed_3d);
-      picture_32.bounds({50, 40, 56, 56});
-      picture_32.size_mode(picture_box_size_mode::center_image);
-      
-      picture_64.back_color(system_colors::window());
-      picture_64.border_style(forms::border_style::fixed_3d);
-      picture_64.bounds({126, 40, 88, 88});
-      picture_64.size_mode(picture_box_size_mode::center_image);
-      
-      picture_128.back_color(system_colors::window());
-      picture_128.border_style(forms::border_style::fixed_3d);
-      picture_128.bounds({224, 40, 152, 152});
-      picture_128.size_mode(picture_box_size_mode::center_image);
-      
-      picture_256.back_color(system_colors::window());
-      picture_256.border_style(forms::border_style::fixed_3d);
-      picture_256.bounds({386, 40, 280, 280});
-      picture_256.size_mode(picture_box_size_mode::center_image);
       
       choice_theme.bounds({10, 10, 280, 25});
       choice_theme.items().push_back("default_theme");
@@ -55,6 +30,31 @@ namespace example {
         update_form();
       };
       
+      picture_16.back_color(system_colors::window());
+      picture_16.border_style(forms::border_style::fixed_3d);
+      picture_16.bounds({10, 60, 30, 30});
+      picture_16.size_mode(picture_box_size_mode::center_image);
+      
+      picture_32.back_color(system_colors::window());
+      picture_32.border_style(forms::border_style::fixed_3d);
+      picture_32.bounds({50, 60, 56, 56});
+      picture_32.size_mode(picture_box_size_mode::center_image);
+      
+      picture_64.back_color(system_colors::window());
+      picture_64.border_style(forms::border_style::fixed_3d);
+      picture_64.bounds({126, 60, 88, 88});
+      picture_64.size_mode(picture_box_size_mode::center_image);
+      
+      picture_128.back_color(system_colors::window());
+      picture_128.border_style(forms::border_style::fixed_3d);
+      picture_128.bounds({224, 60, 152, 152});
+      picture_128.size_mode(picture_box_size_mode::center_image);
+      
+      picture_256.back_color(system_colors::window());
+      picture_256.border_style(forms::border_style::fixed_3d);
+      picture_256.bounds({386, 60, 280, 280});
+      picture_256.size_mode(picture_box_size_mode::center_image);
+      
       label_picture_name.bounds({10, 240, 280, label_picture_name.height()});
       
       button_previous.auto_repeat(true);
@@ -62,7 +62,7 @@ namespace example {
       button_previous.image(button_images::previous());
       button_previous.image_align(content_alignment::middle_left);
       button_previous.text(system_texts::previous());
-      button_previous.bounds({10, 280, 125, 40});
+      button_previous.bounds({10, 300, 125, 40});
       button_previous.click += [&] {
         if (current_image_index > 0) --current_image_index;
         update_form();
@@ -72,7 +72,7 @@ namespace example {
       button_next.image(button_images::next());
       button_next.image_align(content_alignment::middle_right);
       button_next.text(system_texts::next());
-      button_next.bounds({165, 280, 125, 40});
+      button_next.bounds({165, 300, 125, 40});
       button_next.click += [&] {
         if (current_image_index < system_images::names(choice_context.selected_item().value()).size()) ++current_image_index;
         update_form();
