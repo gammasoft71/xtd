04adbdba7ae12308b27e6086689a235fd5de40f3
Gammasoft <gammasoft71@gmail.com>
2022-08-28 18:38:06 +0200

    update documentation

--- src/xtd.forms/include/xtd/forms/dialog_result.h
+++ src/xtd.forms/include/xtd/forms/dialog_result.h
@@ -16,18 +16,18 @@ namespace xtd {
     /// @ingroup xtd_forms
     /// @remarks The xtd::forms::button::dialog_result property and the xtd::forms::form::show_dialog method use this enumeration.
     /// @par Examples
-    /// The following code example demonstrates how to display a MessageBox with the options supported by this overload of Show. After verifying that a string variable, ServerName, is empty, the example displays a MessageBox, offering the user the option to cancel the operation. If the Show method's return value evaluates to Yes, the form that displayed the MessageBox is closed.
+    /// The following code example demonstrates how to display a xtd::forms::message_box with the options supported by this overload of xtd::forms::message_box::show. After verifying that a string variable, ServerName, is empty, the example displays a MessageBox, offering the user the option to cancel the operation. If the Show method's return value evaluates to Yes, the form that displayed the MessageBox is closed.
     /// @code
     /// void validate_user_entry() {
     ///   // Checks the value of the text.
     ///   if (server_name.text().size() == 0) {
-    ///     // Initializes the variables to pass to the MessageBox.Show method.
+    ///     // Initializes the variables to pass to the xtd::forms::message_box::show method.
     ///     string message = "You did not enter a server name. Cancel this operation?";
     ///     string caption = "No Server Name Specified";
     ///     message_box_buttons buttons = message_box_buttons::yes_no;
     ///     dialog_result result;
     ///
-    ///     // Displays the MessageBox.
+    ///     // Displays the xtd::forms::message_box.
     ///     result = message_box::show(*this, message, caption, buttons);
     ///     if (result == dialog_result::yes) {
     ///       // Closes the parent form.
