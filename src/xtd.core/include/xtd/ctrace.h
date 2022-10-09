c6336863fa3c65b780be4d07356702282a3caaf8
Gammasoft <gammasoft71@gmail.com>
2022-10-09 18:08:00 +0200

    add documentation

--- src/xtd.core/include/xtd/ctrace.h
+++ src/xtd.core/include/xtd/ctrace.h
@@ -5,20 +5,19 @@
 #include <iostream>
 #include "tracestreambuf.h"
 
-/// @cond
-inline xtd::tracestreambuf __ctracestreambuf__;
-/// @endcond
-
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
   #if defined(TRACE)
+  /// @brief A xtd::tracestreambuf variable used by xtd::ctrace ostream.
+  inline xtd::tracestreambuf ctracestreambuf;
+
   /// @brief Provides an std::ostream for xtd::diagnostics::trace.
   /// @par Namespace
   /// xtd
   /// @par Library
   /// xtd.core
   /// @ingroup xtd_core
-  inline std::ostream ctrace(&__ctracestreambuf__);
+  inline std::ostream ctrace(&ctracestreambuf);
   #else
   /// @brief Provides an std::ostream for xtd::diagnostics::trace.
   /// @par Namespace
