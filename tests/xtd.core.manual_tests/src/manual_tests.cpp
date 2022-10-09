71c4362b998d452db4a088654c2d8de28b0d1605
Gammasoft <gammasoft71@gmail.com>
2022-10-09 20:10:47 +0200

    Update manual_tests.cpp

--- tests/xtd.core.manual_tests/src/manual_tests.cpp
+++ tests/xtd.core.manual_tests/src/manual_tests.cpp
@@ -8,7 +8,6 @@ class xtd_core_manual_test {
 public:
   static void main() {
     console::write_line("{}", char_object::is_punctuation('.'));
-    console::write_line("{}", wchar_object::is_punctuation(L'.'));
   }
 };
 
