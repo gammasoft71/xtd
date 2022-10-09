fea88f2f977e9538a39bfbc2e9d2d757b7370267
Gammasoft <gammasoft71@gmail.com>
2022-10-09 18:08:00 +0200

    add documentation

--- src/xtd.core/include/xtd/cdebug.h
+++ src/xtd.core/include/xtd/cdebug.h
@@ -5,20 +5,19 @@
 #include <iostream>
 #include "debugstreambuf.h"
 
-/// @cond
-inline xtd::debugstreambuf __cdebugstreambuf__;
-/// @endcond
-
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
   #if !defined(NDEBUG) || defined(DEBUG) || defined(TRACE)
+  /// @brief A xtd::debugstreambuf variable used by xtd::cdebug ostream.
+  inline xtd::debugstreambuf cdebugstreambuf;
+  
   /// @brief Provides an std::ostream for xtd::diagnostics::debug.
   /// @par Namespace
   /// xtd
   /// @par Library
   /// xtd.core
   /// @ingroup xtd_core
-  inline std::ostream cdebug(&__cdebugstreambuf__);
+  inline std::ostream cdebug(&cdebugstreambuf);
   #else
   /// @brief Provides an std::ostream for xtd::diagnostics::debug.
   /// @par Namespace
