7d9163c1f6d5773b32264143774ef6bd9cb1aba8
Gammasoft <gammasoft71@gmail.com>
2022-10-13 19:02:22 +0200

    Update status_bar_without_panels.cpp

--- examples/xtd.forms.examples/others/status_bar_without_panels/src/status_bar_without_panels.cpp
+++ examples/xtd.forms.examples/others/status_bar_without_panels/src/status_bar_without_panels.cpp
@@ -7,8 +7,6 @@ namespace examples {
   class form1 : public form {
   public:
     form1() {
-      back_color(drawing::system_colors::text_box());
-      diagnostics::debug::write_line(ustring::format("color = 0x{:X8}", drawing::system_colors::text_box().to_argb()));
       size({400, 250});
       text("Status bar without panels example");
       
