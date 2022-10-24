202399ecd1e331fbe42307b336973832f39b4c1e
Gammasoft <gammasoft71@gmail.com>
2022-10-24 15:01:46 +0200

    Update form_paint.cpp

--- examples/xtd.forms.examples/events/form_paint/src/form_paint.cpp
+++ examples/xtd.forms.examples/events/form_paint/src/form_paint.cpp
@@ -1,5 +1,6 @@
 #include <xtd/xtd>
 
+using namespace std;
 using namespace xtd;
 using namespace xtd::drawing;
 using namespace xtd::forms;
@@ -9,23 +10,20 @@ namespace examples {
   public:
     form1() {
       text("Paint example");
-      location({100, 100});
       client_size({640, 480});
       
       paint += [](object & sender, paint_event_args & e) {
-        e.graphics().fill_rectangle(brushes::blue(), 0, 0, 640, 480);
-        e.graphics().clear(color::light_yellow);
-        e.graphics().draw_rectangle(pen(color::light_pink, 10), e.clip_rectangle());
-        e.graphics().draw_line(pen(color::light_steel_blue, 5), 20, 60, 260, 60);
-        e.graphics().fill_rectangle(brushes::light_blue(), 50, 300, 400, 50);
-        e.graphics().draw_line(pens::red(), 0, 0, 1, 1);
-        e.graphics().draw_string("Draw string", drawing::font("Arial", 34, font_style::regular), brushes::light_green(), 20.0f, 0.0f);
-        e.graphics().fill_rectangle(brushes::light_sea_green(), 400, 70, 100, 200);
-        e.graphics().draw_arc(pen(color::black, 10), 400, 70, 100, 200, 45, 270);
-        e.graphics().draw_ellipse(pen(color::red, 10), 100, 80, 200, 200);
-        e.graphics().fill_pie(brushes::green(), 120, 100, 160, 160, 45, 270);
-        e.graphics().fill_pie(brushes::light_green(), 120, 100, 160, 160, 270, 180);
-        e.graphics().draw_bezier(pens::black(), 100, 100, 150, 150, 200, 100, 250, 50);
+        e.graphics().clear(color::cyan);
+        e.graphics().draw_string("Drawing with graphics", drawing::font("Arial", 34, font_style::regular), brushes::dark_cyan(), {20.0f, 20.0f, e.clip_rectangle().width() - 40.0f, e.clip_rectangle().height() - 40.0f});
+        e.graphics().fill_ellipse(brushes::yellow(), e.clip_rectangle().width() - 150, 50, 100, 100);
+        e.graphics().draw_ellipse(pen(color::yellow_green, 2), e.clip_rectangle().width() - 150, 50, 100, 100);
+        e.graphics().fill_rectangle(brushes::spring_green(), 0, e.clip_rectangle().height() - 100, e.clip_rectangle().width(), 100);
+        e.graphics().fill_rectangle(brushes::red(), e.clip_rectangle().width() / 2 - 140, e.clip_rectangle().height() - 180, 280, 150);
+        e.graphics().fill_rectangle(brushes::black(), e.clip_rectangle().width() / 2 - 30, e.clip_rectangle().height() - 140, 60, 110);
+        e.graphics().fill_rectangle(brushes::white(), e.clip_rectangle().width() / 2 - 120, e.clip_rectangle().height() - 140, 70, 60);
+        e.graphics().fill_rectangle(brushes::white(), e.clip_rectangle().width() / 2 + 50, e.clip_rectangle().height() - 140, 70, 60);
+        e.graphics().fill_polygon(brushes::brown(), vector<point> {{e.clip_rectangle().width() / 2, e.clip_rectangle().height() - 300}, {e.clip_rectangle().width() / 2 + 160, e.clip_rectangle().height() - 180}, {e.clip_rectangle().width() / 2 - 160, e.clip_rectangle().height() - 180},
+        });
       };
     }
   };
