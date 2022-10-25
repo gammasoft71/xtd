40fc11f842191d3f967011a617e89c55c32f29ab
Gammasoft <gammasoft71@gmail.com>
2022-10-25 22:06:27 +0200

    update message

--- src/xtd.forms/src/xtd/forms/message.cpp
+++ src/xtd.forms/src/xtd/forms/message.cpp
@@ -6,8 +6,47 @@
 using namespace xtd;
 using namespace xtd::forms;
 
-int32_t message::counter_ = 0;
+intptr_t message::hwnd() const {return hwnd_;}
+
+void message::hwnd(intptr_t value) {hwnd_ = value;}
+
+intptr_t message::lparam() const {return lparam_;}
+
+void message::lparam(intptr_t value) {lparam_ = value;}
+
+uint32_t message::msg() const {return msg_;}
+
+void message::msg(uint32_t value) {msg_ = value;}
+
+intptr_t message::result() const {return result_;}
+
+void message::result(intptr_t value) {result_ = value;}
+
+intptr_t message::wparam() const {return wparam_;}
+
+void message::wparam(intptr_t value) {wparam_ = value;}
+
+intptr_t message::handle() const {return handle_;}
+
+void message::handle(intptr_t value) {handle_ = value;}
+
+message message::create(intptr_t hwnd, int32_t msg, intptr_t wparam, intptr_t lparam) {return message(hwnd, msg, wparam, lparam);}
+
+message message::create(intptr_t hwnd, int32_t msg, intptr_t wparam, intptr_t lparam, intptr_t result) {return message(hwnd, msg, wparam, lparam, result);}
+message message::create(intptr_t hwnd, int32_t msg, intptr_t wparam, intptr_t lparam, intptr_t result, intptr_t handle) {return message(hwnd, msg, wparam, lparam, result, handle);}
 
 ustring message::to_string() const noexcept {
   return ustring::format("msg=0x{:X4} ({}), hwnd=0x{:X}, wparam=0x{:X}, lparam=0x{:X}, result=0x{:X}", msg_, native::control::message_to_string(msg_), hwnd_, wparam_, lparam_, result_);
+}
+
+message::message(intptr_t hwnd, int32_t msg, intptr_t wparam, intptr_t lparam) : hwnd_(hwnd), msg_(msg), wparam_(wparam), lparam_(lparam) {
+  id_++;
+}
+
+message::message(intptr_t hwnd, int32_t msg, intptr_t wparam, intptr_t lparam, intptr_t result) : hwnd_(hwnd), msg_(msg), wparam_(wparam), lparam_(lparam), result_(result) {
+  id_++;
+}
+
+message::message(intptr_t hwnd, int32_t msg, intptr_t wparam, intptr_t lparam, intptr_t result, intptr_t handle) : hwnd_(hwnd), msg_(msg), wparam_(wparam), lparam_(lparam), result_(result), handle_(handle) {
+  id_++;
 }
