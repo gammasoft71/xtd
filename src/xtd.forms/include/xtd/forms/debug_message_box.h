fa60395b9b233d5065b49c441299cf2cf2f77a9e
Gammasoft <gammasoft71@gmail.com>
2022-11-02 23:13:13 +0100

    Add documentation

--- src/xtd.forms/include/xtd/forms/debug_message_box.h
+++ src/xtd.forms/include/xtd/forms/debug_message_box.h
@@ -15,6 +15,30 @@ namespace xtd {
     /// @par Library
     /// xtd.forms
     /// @ingroup xtd_forms dialogs debug
+    /// @par Appearance
+    /// <table align="center" border="0" cellpadding="10" cellspacing="0" style="width:100%">
+    ///   <tr>
+    ///     <th style="width:100px"></th>
+    ///     <th><b>Windows</b></th>
+    ///     <th><b>macOS</b></th>
+    ///     <th><b>Gnome</b></th>
+    ///   </tr>
+    ///   <tr>
+    ///     <th>Light</th>
+    ///     <td>@image html dialog_debug_message_box_w.png</td>
+    ///     <td>@image html dialog_debug_message_box_m.png</td>
+    ///     <td>@image html dialog_debug_message_box_g.png</td>
+    ///   </tr>
+    ///   <tr>
+    ///     <th>Dark</th>
+    ///     <td>@image html dialog_debug_message_box_wd.png</td>
+    ///     <td>@image html dialog_debug_message_box_md.png</td>
+    ///     <td>@image html dialog_debug_message_box_gd.png</td>
+    ///   </tr>
+    /// </table>
+    /// @par Examples
+    /// The following code example demonstrates the use of debug_message_box dialog.
+    /// @include debug_message_box.cpp
     class debug_message_box static_ {
     public:
       /// @name Methods
