3816cfcdb38f44d36e3aac9a10bce909460b82a7
Gammasoft <gammasoft71@gmail.com>
2022-11-03 10:44:38 +0100

    Add documentation

--- src/xtd.forms/include/xtd/forms/font_dialog.h
+++ src/xtd.forms/include/xtd/forms/font_dialog.h
@@ -21,6 +21,27 @@ namespace xtd {
     /// @ingroup xtd_forms dialogs
     /// @remarks The inherited member show_dialog must be invoked to create this specific common dialog box. hook_proc can be overridden to implement specific dialog box hook functionality.
     /// @remarks When you create an instance of font_dialog, some of the read/write properties are set to initial values. For a list of these values, see the font_dialog constructor.
+    /// @par Appearance
+    /// <table align="center" border="0" cellpadding="10" cellspacing="0" style="width:100%">
+    ///   <tr>
+    ///     <th style="width:100px"></th>
+    ///     <th><b>Windows</b></th>
+    ///     <th><b>macOS</b></th>
+    ///     <th><b>Gnome</b></th>
+    ///   </tr>
+    ///   <tr>
+    ///     <th>Light</th>
+    ///     <td>@image html dialog_font_dialog_w.png</td>
+    ///     <td>@image html dialog_font_dialog_m.png</td>
+    ///     <td>@image html dialog_font_dialog_g.png</td>
+    ///   </tr>
+    ///   <tr>
+    ///     <th>Dark</th>
+    ///     <td>@image html dialog_font_dialog_wd.png</td>
+    ///     <td>@image html dialog_font_dialog_md.png</td>
+    ///     <td>@image html dialog_font_dialog_gd.png</td>
+    ///   </tr>
+    /// </table>
     /// @par Examples
     /// The following code example demonstrates the use of font_dialog dialog.
     /// @include font_dialog.cpp
