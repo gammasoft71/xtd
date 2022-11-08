ce79569c63b2682ed13be2b969c05e3702363251
Gammasoft <gammasoft71@gmail.com>
2022-11-08 14:28:39 +0100

    remove [[maybe_unused]] attribute

--- src/xtd.tunit/include/xtd/tunit/unit_test.h
+++ src/xtd.tunit/include/xtd/tunit/unit_test.h
@@ -484,14 +484,14 @@ namespace xtd {
       xtd::date_time start_time_point_;
       // The following variables are a hack to ensure that GoogleTestAdapter (Microsoft Visual Studio) will detect the tunit application as a google test application...
       // See https://github.com/csoltenborn/GoogleTestAdapter/blob/master/GoogleTestAdapter/Core/GoogleTestConstants.cs
-      [[maybe_unused]] inline static const char* __test_body_signature__ = "::TestBody";
-      [[maybe_unused]] inline static const char* __parameterized_test_marker__ = "  # GetParam() = ";
-      [[maybe_unused]] inline static const char* __typed_test_marker__ = ".  # TypeParam = ";
-      [[maybe_unused]] inline static const char* __google_test_dll_marker__ = "gtest.dll";
-      [[maybe_unused]] inline static const char* __google_test_dll_marker_debug__ = "gtestd.dll";
-      [[maybe_unused]] inline static const char* __google_test_main_dll_marker__ = "gtest_main.dll";
-      [[maybe_unused]] inline static const char* __google_test_main_dll_marker_debug__ = "gtest_maind.dll";
-      [[maybe_unused]] inline static const char* __google_test_executable_markers__[] = {
+      inline static const char* __test_body_signature__ = "::TestBody";
+      inline static const char* __parameterized_test_marker__ = "  # GetParam() = ";
+      inline static const char* __typed_test_marker__ = ".  # TypeParam = ";
+      inline static const char* __google_test_dll_marker__ = "gtest.dll";
+      inline static const char* __google_test_dll_marker_debug__ = "gtestd.dll";
+      inline static const char* __google_test_main_dll_marker__ = "gtest_main.dll";
+      inline static const char* __google_test_main_dll_marker_debug__ = "gtest_maind.dll";
+      inline static const char* __google_test_executable_markers__[] = {
           "This program contains tests written using Google Test. You can use the",
           "For more information, please read the Google Test documentation at",
           "Run only the tests whose name matches one of the positive patterns but",
