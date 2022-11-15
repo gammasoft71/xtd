c86e739a7a6ec77b48ec679d92c60047b693295c
Gammasoft <gammasoft71@gmail.com>
2022-11-15 16:23:17 +0100

    Fix enum cast

--- src/xtd.core/include/xtd/as.h
+++ src/xtd.core/include/xtd/as.h
@@ -14353,11 +14353,14 @@ namespace xtd {
   template<typename new_type_t, typename current_type_t>
   struct __as_enum__<new_type_t, current_type_t, std::true_type> {
     const new_type_t& convert(const current_type_t& value) {
-      return (const new_type_t&)value;
+      __result__ = static_cast<new_type_t>(value);
+      return __result__;
     }
     new_type_t& convert(current_type_t& value) {
-      return (new_type_t&)value;
+      __result__ = static_cast<new_type_t>(value);
+      return __result__;
     }
+    thread_local inline static new_type_t __result__ {};
   };
   
   template<typename new_type_t, typename current_type_t>
