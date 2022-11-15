0ab7f1f94b5538bae21d58c6075b71f5da5ab152
Gammasoft <gammasoft71@gmail.com>
2022-11-15 22:28:32 +0100

    Add format_class_object example

---
+++ examples/xtd.core.examples/format/format_class_object/src/format_class_object.cpp
@@ -0,0 +1,35 @@
+#include <xtd/xtd>
+
+using namespace std;
+using namespace xtd;
+
+class character : public object {
+public:
+  character(const ustring& name, const ustring& rank) noexcept : name_(name), rank_(rank) {}
+  
+  const ustring& name() const noexcept {return name_;}
+  const ustring& rank() const noexcept {return rank_;}
+  
+  ustring to_string() const noexcept override {return name_ + " (" + rank_ + ")";}
+  
+private:
+  ustring name_;
+  ustring rank_;
+};
+
+using characters = vector<character>;
+
+int main() {
+  for (auto c : characters {{"Jean-Luc Picard", "Captain"}, {"William Riker", "Commander"}, {"Data", "Commander"}, {"Beverly Crusher", "Commander"}, {"Geordi La Forge", "Lieutenant Commander"}, {"Worf", "Lieutenant Commander"}, {"Tasha Yar", "Lieutenant"}})
+  cout << ustring::format("{}", c) << endl;
+}
+
+// This code produces the following output :
+//
+// Jean-Luc Picard (Captain)
+// William Riker (Commander)
+// Data (Commander)
+// Beverly Crusher (Commander)
+// Geordi La Forge (Lieutenant Commander)
+// Worf (Lieutenant Commander)
+// Tasha Yar (Lieutenant)
