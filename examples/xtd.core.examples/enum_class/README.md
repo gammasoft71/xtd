1573ef489c4e6654fe07f3a05f0a4c7ba0175141
Gammasoft <gammasoft71@gmail.com>
2022-11-17 20:33:02 +0100

    Add enum_class_parse example

--- examples/xtd.core.examples/enum_class/README.md
+++ examples/xtd.core.examples/enum_class/README.md
@@ -1,6 +1,7 @@
-# Environment examples
+# Enum class examples
 
 [This folder](.) contains environment examples used by [Reference Guide](https://codedocs.xyz/gammasoft71/xtd/) and more.
 
 * [enum_class](enum_class/README.md) shows how to use [xtd::enum_object](../../../src/xtd.core/include/xtd/xtd::enum_object.h) class and [enum_class_](../../../src/xtd.core/include/xtd/xtd::enum_object.h) helper.
+* [enum_class_parse](enum_class_parse/README.md) shows how to use [xtd::enum_object::parse](../../../src/xtd.core/include/xtd/xtd::enum_object.h) method.
 * [enum_class_without_helper](enum_class_without_helper/README.md) shows how to use [xtd::enum_object](../../../src/xtd.core/include/xtd/xtd::enum_object.h) class and [xtd::enumregister](../../../src/xtd.core/include/xtd/xtd::enum_object.h) classes.
---
+++ examples/xtd.core.examples/enum_class/enum_class_parse/src/enum_class_parse.cpp
@@ -0,0 +1,26 @@
+#include <xtd/xtd>
+
+using namespace xtd;
+
+enum_class_(enum_test, {
+  value_one,
+  value_two,
+  value_three,
+  value_four
+});
+
+int main() {
+  console::write_line("result = {}", enum_object<>::parse<enum_test>("value_two"));
+  console::write_line("result = {}", enum_object<>::parse<enum_test>("VaLuE_fOuR", true));
+  try {
+    console::write_line("result = {}", enum_object<>::parse<enum_test>("value_five"));
+  } catch(const xtd::system_exception& e) {
+    console::write_line("enum_test::value_five does not exists!");
+  }
+
+  enum_test result;
+  if (enum_object<>::try_parse<enum_test>("value_three", result)) console::write_line("result = {}", result);
+  if (enum_object<>::try_parse<enum_test>("vAlUe_OnE", true, result)) console::write_line("result = {}", result);
+  if (enum_object<>::try_parse<enum_test>("value_six", result)) console::write_line("result = {}", result);
+  else console::write_line("enum_test::value_six does not exists!");
+}
