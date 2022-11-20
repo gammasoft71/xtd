ede7c121df98bcb323997662ae3d3a0e63574886
Gammasoft <gammasoft71@gmail.com>
2022-11-20 21:58:30 +0100

    Add green_colors

---
+++ src/xtd.drawing/src/xtd/drawing/green_colors.cpp
@@ -0,0 +1,88 @@
+#include "../../../include/xtd/drawing/green_colors.h"
+
+using namespace std;
+using namespace xtd::drawing;
+
+color green_colors::chartreuse() {
+  return color::from_known_color(known_color::chartreuse);
+}
+
+color green_colors::dark_green() {
+  return color::from_known_color(known_color::dark_green);
+}
+
+color green_colors::dark_olive_green() {
+  return color::from_known_color(known_color::dark_olive_green);
+}
+
+color green_colors::dark_sea_green() {
+  return color::from_known_color(known_color::dark_sea_green);
+}
+
+color green_colors::green() {
+  return color::from_known_color(known_color::green);
+}
+
+color green_colors::green_yellow() {
+  return color::from_known_color(known_color::green_yellow);
+}
+
+color green_colors::forest_green() {
+  return color::from_known_color(known_color::forest_green);
+}
+
+color green_colors::lawn_green() {
+  return color::from_known_color(known_color::lawn_green);
+}
+
+color green_colors::light_green() {
+  return color::from_known_color(known_color::light_green);
+}
+
+color green_colors::lime() {
+  return color::from_known_color(known_color::lime);
+}
+
+color green_colors::lime_green() {
+  return color::from_known_color(known_color::lime_green);
+}
+
+color green_colors::medium_aquamarine() {
+  return color::from_known_color(known_color::medium_aquamarine);
+}
+
+color green_colors::medium_sea_green() {
+  return color::from_known_color(known_color::medium_sea_green);
+}
+
+color green_colors::medium_spring_green() {
+  return color::from_known_color(known_color::medium_spring_green);
+}
+
+color green_colors::olive() {
+  return color::from_known_color(known_color::olive);
+}
+
+color green_colors::olive_drab() {
+  return color::from_known_color(known_color::olive_drab);
+}
+
+color green_colors::pale_green() {
+  return color::from_known_color(known_color::pale_green);
+}
+
+color green_colors::sea_green() {
+  return color::from_known_color(known_color::sea_green);
+}
+
+color green_colors::spring_green() {
+  return color::from_known_color(known_color::spring_green);
+}
+
+color green_colors::yellow_green() {
+  return color::from_known_color(known_color::yellow_green);
+}
+
+vector<color> green_colors::get_colors() {
+  return {green_colors::dark_green(), green_colors::green(), green_colors::dark_olive_green(), green_colors::forest_green(), green_colors::sea_green(), green_colors::olive(), green_colors::olive_drab(), green_colors::medium_sea_green(), green_colors::lime_green(), green_colors::lime(), green_colors::spring_green(), green_colors::medium_spring_green(), green_colors::dark_sea_green(), green_colors::medium_aquamarine(), green_colors::yellow_green(), green_colors::lawn_green(), green_colors::chartreuse(), green_colors::light_green(), green_colors::green_yellow(), green_colors::pale_green()};
+}
