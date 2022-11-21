c4444317bd216ebd1d6793be7bf6a4b379253926
Gammasoft <gammasoft71@gmail.com>
2022-11-21 06:29:07 +0100

    add purle_colors class and magenta_colors and violet_colors aliases

---
+++ src/xtd.drawing/include/xtd/drawing/violet_colors.h
@@ -0,0 +1,19 @@
+/// @file
+/// @brief Contains xtd::drawing::violet_colors alias.
+/// @copyright Copyright (c) 2022 Gammasoft. All rights reserved.
+#pragma once
+#include "purple_colors.h"
+
+/// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
+namespace xtd {
+  /// @brief The xtd::drawing namespace provides access to GDI+ basic graphics functionality. More advanced functionality is provided in the xtd::drawing::drawing2d, xtd::drawing::imaging, and xtd::drawing::text namespaces.
+  namespace drawing {
+    /// @brief Purple, violet and magenta colors for all the hmtl purple, violet and magenta colors.
+    /// @par Namespace
+    /// xtd::drawing
+    /// @par Library
+    /// xtd.drawing
+    /// @ingroup xtd_drawing drawing
+    using violet_colors = xtd::drawing::purple_colors;
+  }
+}
