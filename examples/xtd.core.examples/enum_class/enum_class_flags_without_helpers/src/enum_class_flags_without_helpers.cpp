e6e4efcdcf7e683efebf3f64eeb3f3d363571222
Gammasoft <gammasoft71@gmail.com>
2022-11-21 23:05:14 +0100

    review enum code

--- examples/xtd.core.examples/enum_class/enum_class_flags_without_helpers/src/enum_class_flags_without_helpers.cpp
+++ examples/xtd.core.examples/enum_class/enum_class_flags_without_helpers/src/enum_class_flags_without_helpers.cpp
@@ -24,12 +24,7 @@ int main() {
   console::write_line("name = {}", text_attribute::bold | text_attribute::italic);
   console::write_line("value = {}", enum_object(text_attribute::bold | text_attribute::italic).to_int32());
   console::write_line("as<int> = {}", as<int>(text_attribute::bold | text_attribute::italic));
-  console::write_line("values = {}", enum_object<>::get_values<text_attribute>());
+  console::write_line("values = {}", enum_object<>::get_values_as_int32<text_attribute>());
   console::write_line("names = {}", enum_object<>::get_names<text_attribute>());
-  console::write("entries = [");
-  for (auto entry : enum_object<>::get_entries<text_attribute>()) {
-    static auto index = 0;
-    console::write("{}({}, {})", index++ == 0 ? "" : ", ", as<int>(entry.first), entry.second);
-  }
-  console::write_line("]");
+  console::write_line("entries = {}", enum_object<>::get_entries_as_int32<text_attribute>());
 }
--- examples/xtd.core.examples/enum_class/enum_class_without_helper/src/enum_class_without_helper.cpp
+++ examples/xtd.core.examples/enum_class/enum_class_without_helper/src/enum_class_without_helper.cpp
@@ -17,12 +17,7 @@ int main() {
   console::write_line("name = {}", enum_test::value_four);
   console::write_line("value = {}", enum_object(enum_test::value_four).to_int32());
   console::write_line("as<int> = {}", as<int>(enum_test::value_four));
-  console::write_line("values = {}", enum_object<>::get_values<enum_test>());
+  console::write_line("values = {}", enum_object<>::get_values_as_int32<enum_test>());
   console::write_line("names = {}", enum_object<>::get_names<enum_test>());
-  console::write("entries = [");
-  for (auto entry : enum_object<>::get_entries<enum_test>()) {
-    static auto index = 0;
-    console::write("{}({}, {})", index++ == 0 ? "" : ", ", as<int>(entry.first), entry.second);
-  }
-  console::write_line("]");
+  console::write_line("entries = {}", enum_object<>::get_entries_as_int32<enum_test>());
 }
