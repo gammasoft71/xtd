9d38dad00bbf916f91d345afbc688a41832b6ff0
Gammasoft <gammasoft71@gmail.com>
2022-11-22 14:29:22 +0100

    Fix __enum_register__

--- src/xtd.core/include/xtd/internal/__enum_register.h
+++ src/xtd.core/include/xtd/internal/__enum_register.h
@@ -29,7 +29,9 @@ public:
   
   explicit operator auto() const {
     xtd::enum_collection<enum_t> entries;
+    if (enum_definition_.empty()) return entries;
     int64_t current_value = 0;
+    if (enum_definition_.index_of('{') == enum_definition_.npos || enum_definition_.index_of('}') == enum_definition_.npos) throw xtd::format_exception("Not a valid enum declaration", csf_);
     for (auto entry : enum_definition_.substring(enum_definition_.index_of('{')).replace('{', ' ').replace('}', ' ').trim().split({','})) {
       auto key_value = entry.trim().split({'='});
       if (key_value.size() < 1 || key_value.size() > 2 || (key_value.size() == 2 && xtd::ustring::is_empty(key_value[1])))
