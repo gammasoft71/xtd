8b80dc41502d222e1998e49a39420a3c53abdf19
Gammasoft <gammasoft71@gmail.com>
2022-11-24 02:19:59 +0100

    Update translator.cpp

--- src/xtd.core.native.win32/src/xtd/native/win32/translator.cpp
+++ src/xtd.core.native.win32/src/xtd/native/win32/translator.cpp
@@ -12,7 +12,7 @@ using namespace xtd::native;
 
 std::string translator::get_system_language() {
   if (!environment::get_environment_variable("LANG").empty()) return win32::strings::to_lower(win32::strings::substring(environment::get_environment_variable("LANG"), 0, 2));
-  auto data_size = GetLocaleInfo(LOCALE_USER_DEFAULT, LOCALE_SISO639LANGNAME, nullptr, 0);
+  size_t data_size = GetLocaleInfo(LOCALE_USER_DEFAULT, LOCALE_SISO639LANGNAME, nullptr, 0);
   std::wstring data {data_size, 0};
   GetLocaleInfo(LOCALE_USER_DEFAULT, LOCALE_SISO639LANGNAME, data.data(), data_size);
   return win32::strings::to_string(data);
