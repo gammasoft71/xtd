cbb020c1c13490f2e776eaaa247f22127cc9938b
Gammasoft <gammasoft71@gmail.com>
2022-11-24 01:28:26 +0100

    Update translator.cpp

--- src/xtd.core.native.win32/src/xtd/native/win32/translator.cpp
+++ src/xtd.core.native.win32/src/xtd/native/win32/translator.cpp
@@ -5,15 +5,15 @@
 #include "../../../../include/xtd/native/win32/strings.h"
 #undef __XTD_CORE_NATIVE_LIBRARY__
 #include <Windows.h>
-
-using namespace xtd::native;
-
 #undef max
 #undef min
 
+using namespace xtd::native;
+
 std::string translator::get_system_language() {
   if (!environment::get_environment_variable("LANG").empty()) return win32::strings::to_lower(win32::strings::substring(environment::get_environment_variable("LANG"), 0, 2));
-  wchar_t data[512] = {0};
-  GetLocaleInfo(LOCALE_USER_DEFAULT, LOCALE_SISO639LANGNAME, data, 512);
+  auto data_size = GetLocaleInfo(LOCALE_USER_DEFAULT, LOCALE_SISO639LANGNAME, nullptr, 0);
+  std::wstring data {data_size, 0};
+  GetLocaleInfo(LOCALE_USER_DEFAULT, LOCALE_SISO639LANGNAME, data.data(), data_size);
   return win32::strings::to_string(data);
 }
