7abdf812e635635a5fc61247523ab1624c6186e6
Gammasoft <gammasoft71@gmail.com>
2022-11-26 15:17:11 +0100

    Review code

--- src/xtd.forms/src/xtd/forms/background_worker.cpp
+++ src/xtd.forms/src/xtd/forms/background_worker.cpp
@@ -4,19 +4,55 @@
 using namespace xtd;
 using namespace xtd::forms;
 
-background_worker::background_worker() {
+background_worker::background_worker() noexcept {
 }
 
-background_worker::~background_worker() {
+background_worker::~background_worker() noexcept {
   if (data_->thread.joinable()) {
     if (data_->cancellation_pending) data_->thread.detach();
     else data_->thread.join();
   }
 }
 
+bool background_worker::cancellation_pending() const noexcept {
+  return data_->cancellation_pending;
+}
+
+bool background_worker::is_busy() const noexcept {
+  return data_->is_busy;
+}
+
+bool background_worker::worker_reports_progress() const noexcept {
+  return data_->worker_reports_progress;
+}
+
+void background_worker::worker_reports_progress(bool value) {
+  data_->worker_reports_progress = value;
+}
+
+bool background_worker::worker_supports_cancellation() const noexcept {
+  return data_->worker_supports_cancellation;
+}
+
+void background_worker::worker_supports_cancellation(bool value) {
+  data_->worker_supports_cancellation = value;
+}
+
 void background_worker::cancel_async() {
   if (data_->worker_supports_cancellation)
     data_->cancellation_pending = true;
+}
+
+void background_worker::on_do_work(do_work_event_args& e) {
+  do_work(*this, e);
+}
+
+void background_worker::on_progress_changed(const progress_changed_event_args& e) {
+  progress_changed(*this, e);
+}
+
+void background_worker::on_run_worker_completed(const run_worker_completed_event_args& e) {
+  run_worker_completed(*this, e);
 }
 
 void background_worker::report_progress(int32_t percent_progress) {
