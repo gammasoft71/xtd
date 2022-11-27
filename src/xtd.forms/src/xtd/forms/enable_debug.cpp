e8506d3002f7e4b6621fb73d2c3e9aa728c12bd0
Gammasoft <gammasoft71@gmail.com>
2022-11-27 20:58:33 +0100

    review code

--- src/xtd.forms/src/xtd/forms/enable_debug.cpp
+++ src/xtd.forms/src/xtd/forms/enable_debug.cpp
@@ -12,5 +12,36 @@ const enable_debug enable_debug::layout {0b10000};
 const enable_debug enable_debug::workaround {0b100000};
 const enable_debug enable_debug::all {0b1111111111111111111111111111111111111111111111111111111111111111};
 
+bool enable_debug::operator ==(const enable_debug& value) const noexcept {
+  return value_ == value.value_;
+}
+
+bool enable_debug::operator !=(const enable_debug& value) const noexcept {
+  return !operator ==(value);
+}
+
+enable_debug enable_debug::operator |(const enable_debug& value) const noexcept {
+  return enable_debug(value_ | value.value_);
+}
+
+enable_debug enable_debug::operator +(const enable_debug& value) const  noexcept {
+  return enable_debug(value_ + value.value_);
+}
+
+xtd::diagnostics::trace_switch& enable_debug::trace_switch() noexcept {
+  return trace_switch_;
+}
+
+bool enable_debug::get(const enable_debug& flags) {
+  return (values_.value_ & flags.value_) == flags.value_;
+}
+
+void enable_debug::set(const enable_debug& flags, bool on) {
+  values_.value_ = on ? (values_.value_ | flags.value_) : (values_.value_ & ~flags.value_);
+}
+
+enable_debug::enable_debug(uint64_t value) : value_(value) {
+}
+
 trace_switch enable_debug::trace_switch_ {"trace_switch_xtd_forms", "The trace switch for xtd forms library"};
 enable_debug enable_debug::values_;
