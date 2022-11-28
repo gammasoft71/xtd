df2d713246ba785836c89018e2732e3df75789b2
Gammasoft <gammasoft71@gmail.com>
2022-11-28 13:29:35 +0100

    review code

--- src/xtd.forms/src/xtd/forms/country.cpp
+++ src/xtd.forms/src/xtd/forms/country.cpp
@@ -11,6 +11,14 @@ using namespace xtd::io;
 
 const vector<ustring> country::enclosed_letters {"\U0001F1E6", "\U0001F1E7", "\U0001F1E8", "\U0001F1E9", "\U0001F1EA", "\U0001F1EB", "\U0001F1EC", "\U0001F1ED", "\U0001F1EE", "\U0001F1EF", "\U0001F1F0", "\U0001F1F1", "\U0001F1F2", "\U0001F1F3", "\U0001F1F4", "\U0001F1F5", "\U0001F1F6", "\U0001F1F7", "\U0001F1F8", "\U0001F1F9", "\U0001F1FA", "\U0001F1FB", "\U0001F1FC", "\U0001F1FD", "\U0001F1FE", "\U0001F1FF"};
 
+country::country(const country& other) {
+  *data_ = *other.data_;
+}
+country& country::operator =(const country& other) {
+  *data_ = *other.data_;
+  return *this;
+}
+
 bool country::operator ==(const country& value) const noexcept {
   return data_->name == value.data_->name && data_->alpha_2_code == value.data_->alpha_2_code && data_->alpha_3_code == value.data_->alpha_3_code && data_->numeric_code == value.data_->numeric_code;
 }
