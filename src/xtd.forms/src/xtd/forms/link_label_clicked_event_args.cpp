2aea8db19a709fc64e59c8e70889af2cf6079acd
Gammasoft <gammasoft71@gmail.com>
2022-11-29 13:39:00 +0100

    review code

--- src/xtd.forms/src/xtd/forms/link_label_clicked_event_args.cpp
+++ src/xtd.forms/src/xtd/forms/link_label_clicked_event_args.cpp
@@ -6,15 +6,15 @@ using namespace xtd::forms;
 link_label_clicked_event_args::link_label_clicked_event_args(const xtd::forms::link& link, xtd::forms::mouse_buttons button) : link_(link), button_(button) {
 }
 
-const xtd::forms::link& link_label_clicked_event_args::link() const {
+const xtd::forms::link& link_label_clicked_event_args::link() const noexcept {
   return link_;
 }
 
-xtd::forms::mouse_buttons link_label_clicked_event_args::button() const {
+xtd::forms::mouse_buttons link_label_clicked_event_args::button() const noexcept {
   return button_;
 }
 
-bool link_label_clicked_event_args::visited() const {
+bool link_label_clicked_event_args::visited() const noexcept {
   return visited_;
 }
 void link_label_clicked_event_args::visited(bool value) {
