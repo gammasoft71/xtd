dd2d1094a35fdd26a9b4b20eda8e12f0c3d06c79
Gammasoft <gammasoft71@gmail.com>
2022-11-30 22:40:36 +0100

    Fix documentation

--- src/xtd.core/include/xtd/async_callback.h
+++ src/xtd.core/include/xtd/async_callback.h
@@ -1,5 +1,5 @@
 /// @file
-/// @brief Contains xtd::action delegate.
+/// @brief Contains xtd::async_callback delegate.
 /// @copyright Copyright (c) 2022 Gammasoft. All rights reserved.
 #pragma once
 
