57e10f6214ac3c1c52d1dc219a5e6c5e11af63e1
Gammasoft <gammasoft71@gmail.com>
2022-11-30 17:06:25 +0100

    review code

--- src/xtd.forms/include/xtd/forms/splitter.h
+++ src/xtd.forms/include/xtd/forms/splitter.h
@@ -41,36 +41,23 @@ namespace xtd {
       /// @note The anchor and dock properties are mutually exclusive. Only one can be set at a time, and the last one set takes precedence.
       /// @par Notes to Inheritors
       /// When overriding the dock property in a derived class, use the base class's dock property to extend the base implementation. Otherwise, you must provide all the implementation. You are not required to override both the get and set methods of the dock property; you can override only one if needed.
-      virtual control& dock(dock_style dock) override {
-        if (dock != dock_style::left && dock != dock_style::right && dock != dock_style::top && dock != dock_style::bottom) throw argument_exception("splitter control must be docked left, right, top, or bottom."_t, current_stack_frame_);
-        control::dock(dock);
-        cursor(default_cursor());
-        if (default_width_ && (dock == dock_style::left || dock == dock_style::right)) width(3);
-        if (default_width_ && (dock == dock_style::top || dock == dock_style::bottom)) height(3);
-        return *this;
-      }
+      control& dock(dock_style dock) override;
       
       /// @brief Gets the minimum distance that must remain between the splitter control and the container edge that the control is docked to.
       /// @return The minimum distance, in pixels, between the splitter control and the container edge that the control is docked to. The default is 25.
-      virtual int min_size() const {return min_size_;}
+      virtual int min_size() const noexcept;
       /// @brief Sets the minimum distance that must remain between the splitter control and the container edge that the control is docked to.
       /// @param min_size The minimum distance, in pixels, between the splitter control and the container edge that the control is docked to. The default is 25.
       /// @return Current splitter instance.
-      virtual splitter& min_size(int min_size) {
-        min_size_ = min_size;
-        return *this;
-      }
+      virtual splitter& min_size(int min_size);
       
       /// @brief Gets the style of the splitter.
       /// @return One of xtd::forms::splitter_style values. The default is xtd::forms::splitter_style::update_children
-      virtual xtd::forms::splitter_style splitter_style() const {return splitter_style_;}
+      virtual xtd::forms::splitter_style splitter_style() const noexcept;
       /// @brief Sets the style of the splitter.
       /// @param splitter_style One of xtd::forms::splitter_style values. The default is xtd::forms::splitter_style::update_children
       /// @return Current splitter instance.
-      virtual splitter& splitter_style(xtd::forms::splitter_style splitter_style) {
-        splitter_style_ = splitter_style;
-        return *this;
-      }
+      virtual splitter& splitter_style(xtd::forms::splitter_style splitter_style);
       /// @}
       
     protected:
@@ -84,72 +71,26 @@ namespace xtd {
       /// @name Protected methods
       
       /// @{
-      void on_mouse_down(const mouse_event_args& e) override {
-        control::on_mouse_down(e);
-        mouse_down_location = control::dock() == dock_style::left || control::dock() == dock_style::right ? cursor::position().x() : cursor::position().y();
-        if (parent().has_value()) {
-          for (size_t index = 0; index < parent().value().get().controls().size(); index++) {
-            if (parent().value().get().controls()[index].get() == *this) {
-              if (index > 0) previous_control_cursor_ = (previous_control_ = &parent().value().get().controls()[index - 1].get())->cursor();
-              if (index < parent().value().get().controls().size() - 1) next_control_cursor_ = (next_control_ = &parent().value().get().controls()[index + 1].get())->cursor();
-              break;
-            }
-          }
-        }
-        if (previous_control_) previous_control_->cursor(default_cursor());
-        if (next_control_) next_control_->cursor(default_cursor());
-      }
-      
-      void on_mouse_move(const mouse_event_args& e) override {
-        control::on_mouse_move(e);
-        if (previous_control_) previous_control_->cursor(default_cursor());
-        if (next_control_) next_control_->cursor(default_cursor());
-        if (mouse_down_location != -1 && next_control_) {
-          if (splitter_style_ == splitter_style::draw_line) {
-          
-          } else {
-            int delta_size = control::dock() == dock_style::left || control::dock() == dock_style::right ? (next_control_->width() + cursor::position().x()) : (next_control_->height() + cursor::position().y());
-            int new_size = delta_size - mouse_down_location;
-            if (new_size < min_size_) new_size = min_size_;
-            //if (previous_control_.size()  < min_size_extra_) new_size = min_size_;
-            if (control::dock() == dock_style::left || control::dock() == dock_style::right) next_control_->width(new_size);
-            else next_control_->height(new_size);
-            mouse_down_location = control::dock() == dock_style::left || control::dock() == dock_style::right ? cursor::position().x() : cursor::position().y();
-          }
-        }
-      }
-      
-      void on_mouse_up(const mouse_event_args& e) override {
-        control::on_mouse_up(e);
-        if (previous_control_) previous_control_->cursor(previous_control_cursor_);
-        if (next_control_) next_control_->cursor(previous_control_cursor_);
-        if (mouse_down_location != -1 && parent().has_value() && next_control_) {
-          int new_size = (control::dock() == dock_style::left || control::dock() == dock_style::right ? (next_control_->width() + cursor::position().x()) : (next_control_->height() + cursor::position().y())) - mouse_down_location;
-          if (new_size < min_size_) new_size = min_size_;
-          if (control::dock() == dock_style::left || control::dock() == dock_style::right) next_control_->width(new_size);
-          else next_control_->height(new_size);
-          mouse_down_location = -1;
-        }
-        previous_control_ = nullptr;
-        next_control_ = nullptr;
-      }
-      
-      void on_handle_created(const event_args& e) override {
-        control::on_handle_created(e);
-      }
+      void on_handle_created(const event_args& e) override;
+      void on_mouse_down(const mouse_event_args& e) override;
+      void on_mouse_move(const mouse_event_args& e) override;
+      void on_mouse_up(const mouse_event_args& e) override;
       /// @}
       
     private:
-      bool default_width_ = true;
-      int min_size_ = 25;
-      //int min_size_extra_ = 25;
-      //int split_position_ = -1;
-      int mouse_down_location = -1;
-      xtd::forms::splitter_style splitter_style_ = xtd::forms::splitter_style::update_children;
-      xtd::forms::cursor previous_control_cursor_;
-      xtd::forms::cursor next_control_cursor_;
-      control* previous_control_ = nullptr;
-      control* next_control_ = nullptr;
+      struct data {
+        bool default_width = true;
+        int min_size = 25;
+        //int min_size_extra = 25;
+        //int split_position = -1;
+        int mouse_down_location = -1;
+        xtd::forms::splitter_style splitter_style = xtd::forms::splitter_style::update_children;
+        xtd::forms::cursor previous_control_cursor;
+        xtd::forms::cursor next_control_cursor;
+        control* previous_control = nullptr;
+        control* next_control = nullptr;
+      };
+      std::shared_ptr<data> data_ = std::make_shared<data>();
     };
   }
 }
--- src/xtd.forms/src/xtd/forms/splitter.cpp
+++ src/xtd.forms/src/xtd/forms/splitter.cpp
@@ -8,10 +8,93 @@ splitter::splitter() {
   set_style(control_styles::selectable, false);
 }
 
+control& splitter::dock(dock_style dock) {
+  if (dock != dock_style::left && dock != dock_style::right && dock != dock_style::top && dock != dock_style::bottom) throw argument_exception("splitter control must be docked left, right, top, or bottom."_t, current_stack_frame_);
+  control::dock(dock);
+  cursor(default_cursor());
+  if (data_->default_width && (dock == dock_style::left || dock == dock_style::right)) width(3);
+  if (data_->default_width && (dock == dock_style::top || dock == dock_style::bottom)) height(3);
+  return *this;
+}
+
+int splitter::min_size() const noexcept {
+  return data_->min_size;
+}
+/// @brief Sets the minimum distance that must remain between the splitter control and the container edge that the control is docked to.
+/// @param min_size The minimum distance, in pixels, between the splitter control and the container edge that the control is docked to. The default is 25.
+/// @return Current splitter instance.
+splitter& splitter::min_size(int min_size) {
+  data_->min_size = min_size;
+  return *this;
+}
+
+xtd::forms::splitter_style splitter::splitter_style() const noexcept {
+  return data_->splitter_style;
+}
+
+splitter& splitter::splitter_style(xtd::forms::splitter_style splitter_style) {
+  data_->splitter_style = splitter_style;
+  return *this;
+}
+
 forms::cursor splitter::default_cursor() const noexcept {
   return dock() == dock_style::left || dock() == dock_style::right ? xtd::forms::cursors::vsplit() : xtd::forms::cursors::hsplit();
 }
 
 drawing::size splitter::default_size() const noexcept {
   return {3, 3};
+}
+
+void splitter::on_handle_created(const event_args& e) {
+  control::on_handle_created(e);
+}
+
+void splitter::on_mouse_down(const mouse_event_args& e) {
+  control::on_mouse_down(e);
+  data_->mouse_down_location = control::dock() == dock_style::left || control::dock() == dock_style::right ? cursor::position().x() : cursor::position().y();
+  if (parent().has_value()) {
+    for (size_t index = 0; index < parent().value().get().controls().size(); index++) {
+      if (parent().value().get().controls()[index].get() == *this) {
+        if (index > 0) data_->previous_control_cursor = (data_->previous_control = &parent().value().get().controls()[index - 1].get())->cursor();
+        if (index < parent().value().get().controls().size() - 1) data_->next_control_cursor = (data_->next_control = &parent().value().get().controls()[index + 1].get())->cursor();
+        break;
+      }
+    }
+  }
+  if (data_->previous_control) data_->previous_control->cursor(default_cursor());
+  if (data_->next_control) data_->next_control->cursor(default_cursor());
+}
+
+void splitter::on_mouse_move(const mouse_event_args& e) {
+  control::on_mouse_move(e);
+  if (data_->previous_control) data_->previous_control->cursor(default_cursor());
+  if (data_->next_control) data_->next_control->cursor(default_cursor());
+  if (data_->mouse_down_location != -1 && data_->next_control) {
+    if (data_->splitter_style == splitter_style::draw_line) {
+      
+    } else {
+      int delta_size = control::dock() == dock_style::left || control::dock() == dock_style::right ? (data_->next_control->width() + cursor::position().x()) : (data_->next_control->height() + cursor::position().y());
+      int new_size = delta_size - data_->mouse_down_location;
+      if (new_size < data_->min_size) new_size = data_->min_size;
+      //if (data_->previous_control.size()  < data_->min_size_extra) new_size = data_->min_size;
+      if (control::dock() == dock_style::left || control::dock() == dock_style::right) data_->next_control->width(new_size);
+      else data_->next_control->height(new_size);
+      data_->mouse_down_location = control::dock() == dock_style::left || control::dock() == dock_style::right ? cursor::position().x() : cursor::position().y();
+    }
+  }
+}
+
+void splitter::on_mouse_up(const mouse_event_args& e) {
+  control::on_mouse_up(e);
+  if (data_->previous_control) data_->previous_control->cursor(data_->previous_control_cursor);
+  if (data_->next_control) data_->next_control->cursor(data_->previous_control_cursor);
+  if (data_->mouse_down_location != -1 && parent().has_value() && data_->next_control) {
+    int new_size = (control::dock() == dock_style::left || control::dock() == dock_style::right ? (data_->next_control->width() + cursor::position().x()) : (data_->next_control->height() + cursor::position().y())) - data_->mouse_down_location;
+    if (new_size < data_->min_size) new_size = data_->min_size;
+    if (control::dock() == dock_style::left || control::dock() == dock_style::right) data_->next_control->width(new_size);
+    else data_->next_control->height(new_size);
+    data_->mouse_down_location = -1;
+  }
+  data_->previous_control = nullptr;
+  data_->next_control = nullptr;
 }
