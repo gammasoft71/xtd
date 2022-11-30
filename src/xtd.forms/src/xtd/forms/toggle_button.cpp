8fccb5a65409576b01b58d66c71624c33c7da001
Gammasoft <gammasoft71@gmail.com>
2022-11-30 20:10:01 +0100

    review code

--- src/xtd.forms/src/xtd/forms/toggle_button.cpp
+++ src/xtd.forms/src/xtd/forms/toggle_button.cpp
@@ -17,9 +17,29 @@ toggle_button::toggle_button() {
   set_style(control_styles::standard_click | control_styles::standard_double_click, false);
 }
 
+bool toggle_button::auto_check() const noexcept {
+  return data_->auto_check;
+}
+
 toggle_button& toggle_button::auto_check(bool auto_check) {
-  auto_check_ = auto_check;
+  data_->auto_check = auto_check;
   return *this;
+}
+
+content_alignment toggle_button::check_align() const noexcept {
+  return data_->check_align;
+}
+
+toggle_button& toggle_button::check_align(content_alignment check_align) {
+  if (data_->check_align != check_align) {
+    data_->check_align = check_align;
+    post_recreate_handle();
+  }
+  return *this;
+}
+
+bool toggle_button::checked() const noexcept {
+  return data_->checked;
 }
 
 toggle_button& toggle_button::checked(bool checked) {
@@ -27,30 +47,30 @@ toggle_button& toggle_button::checked(bool checked) {
   return *this;
 }
 
-toggle_button& toggle_button::check_align(content_alignment check_align) {
-  if (check_align_ != check_align) {
-    check_align_ = check_align;
-    post_recreate_handle();
-  }
-  return *this;
+forms::check_state toggle_button::check_state() const noexcept {
+  return data_->check_state;
 }
 
 toggle_button& toggle_button::check_state(forms::check_state check_state) {
-  if (check_state_ != check_state) {
-    check_state_ = check_state;
-    if (checked_ != (check_state_ != forms::check_state::unchecked)) {
-      checked_ = check_state_ != forms::check_state::unchecked;
+  if (data_->check_state != check_state) {
+    data_->check_state = check_state;
+    if (data_->checked != (data_->check_state != forms::check_state::unchecked)) {
+      data_->checked = data_->check_state != forms::check_state::unchecked;
       on_checked_changed(event_args::empty);
     }
-    if (is_handle_created() && flat_style() == flat_style::system) native::toggle_button::check_state(handle(), static_cast<int32_t>(check_state_));
+    if (is_handle_created() && flat_style() == flat_style::system) native::toggle_button::check_state(handle(), static_cast<int32_t>(data_->check_state));
     on_check_state_changed(event_args::empty);
   }
   return *this;
 }
 
+bool toggle_button::three_state() const noexcept {
+  return data_->three_state;
+}
+
 toggle_button& toggle_button::three_state(bool three_state) {
-  if (three_state_ != three_state) {
-    three_state_ = three_state;
+  if (data_->three_state != three_state) {
+    data_->three_state = three_state;
     post_recreate_handle();
   }
   return *this;
@@ -60,12 +80,12 @@ forms::create_params toggle_button::create_params() const noexcept {
   forms::create_params create_params = button_base::create_params();
   
   create_params.class_name("togglebutton");
-  if (three_state_) create_params.style(create_params.style() | (auto_check_ ? BS_AUTO3STATE : BS_3STATE));
-  else if (auto_check_) create_params.style(create_params.style() | BS_AUTOCHECKBOX);
+  if (data_->three_state) create_params.style(create_params.style() | (data_->auto_check ? BS_AUTO3STATE : BS_3STATE));
+  else if (data_->auto_check) create_params.style(create_params.style() | BS_AUTOCHECKBOX);
   else create_params.style(create_params.style() | BS_CHECKBOX);
   create_params.style(create_params.style() | BS_PUSHLIKE);
   
-  switch (check_align_) {
+  switch (data_->check_align) {
     case content_alignment::top_right:
     case content_alignment::middle_right:
     case content_alignment::bottom_right: create_params.style(create_params.style() | BS_RIGHTBUTTON); break;
@@ -79,10 +99,75 @@ drawing::size toggle_button::measure_control() const noexcept {
   return button_base::measure_text() + drawing::size(18, 0);
 }
 
+void toggle_button::on_checked_changed(const event_args& e) {
+  if (flat_style() != xtd::forms::flat_style::system && enabled()) {
+    if (data_->check_state == xtd::forms::check_state::unchecked) data_->state = xtd::forms::visual_styles::toggle_button_state::unchecked_normal;
+    else if (data_->check_state == xtd::forms::check_state::checked) data_->state = xtd::forms::visual_styles::toggle_button_state::checked_normal;
+    else if (data_->check_state == xtd::forms::check_state::indeterminate) data_->state = xtd::forms::visual_styles::toggle_button_state::mixed_normal;
+  }
+  checked_changed(*this, e);
+  if (flat_style() != xtd::forms::flat_style::system) invalidate();
+}
+
+void toggle_button::on_check_state_changed(const event_args& e) {
+  if (flat_style() != xtd::forms::flat_style::system && enabled()) {
+    if (data_->check_state == xtd::forms::check_state::unchecked) data_->state = xtd::forms::visual_styles::toggle_button_state::unchecked_normal;
+    else if (data_->check_state == xtd::forms::check_state::checked) data_->state = xtd::forms::visual_styles::toggle_button_state::checked_normal;
+    else if (data_->check_state == xtd::forms::check_state::indeterminate) data_->state = xtd::forms::visual_styles::toggle_button_state::mixed_normal;
+  }
+  check_state_changed(*this, e);
+  if (flat_style() != xtd::forms::flat_style::system) invalidate();
+}
+
+void toggle_button::on_enabled_changed(const event_args& e) {
+  if (flat_style() != xtd::forms::flat_style::system) {
+    if (data_->check_state == xtd::forms::check_state::unchecked) data_->state = enabled() ? xtd::forms::visual_styles::toggle_button_state::unchecked_normal : xtd::forms::visual_styles::toggle_button_state::unchecked_disabled;
+    else if (data_->check_state == xtd::forms::check_state::checked) data_->state = enabled() ? xtd::forms::visual_styles::toggle_button_state::checked_normal : xtd::forms::visual_styles::toggle_button_state::checked_disabled;
+    else if (data_->check_state == xtd::forms::check_state::indeterminate) data_->state = enabled() ? xtd::forms::visual_styles::toggle_button_state::mixed_normal : xtd::forms::visual_styles::toggle_button_state::mixed_disabled;
+  }
+  button_base::on_enabled_changed(e);
+}
+
 void toggle_button::on_handle_created(const event_args& e) {
   button_base::on_handle_created(e);
-  if (flat_style() == flat_style::system) native::toggle_button::check_state(handle(), static_cast<int32_t>(check_state_));
-  if (flat_style() != xtd::forms::flat_style::system && check_state_ != xtd::forms::check_state::unchecked) invalidate();
+  if (flat_style() == flat_style::system) native::toggle_button::check_state(handle(), static_cast<int32_t>(data_->check_state));
+  if (flat_style() != xtd::forms::flat_style::system && data_->check_state != xtd::forms::check_state::unchecked) invalidate();
+}
+
+void toggle_button::on_mouse_down(const mouse_event_args& e) {
+  if (flat_style() != xtd::forms::flat_style::system && enabled()) {
+    if (data_->check_state == xtd::forms::check_state::unchecked) data_->state = xtd::forms::visual_styles::toggle_button_state::unchecked_pressed;
+    else if (data_->check_state == xtd::forms::check_state::checked) data_->state = xtd::forms::visual_styles::toggle_button_state::checked_pressed;
+    else if (data_->check_state == xtd::forms::check_state::indeterminate) data_->state = xtd::forms::visual_styles::toggle_button_state::mixed_pressed;
+  }
+  button_base::on_mouse_down(e);
+}
+
+void toggle_button::on_mouse_enter(const event_args& e) {
+  if (flat_style() != xtd::forms::flat_style::system && enabled()) {
+    if (data_->check_state == xtd::forms::check_state::unchecked) data_->state = (mouse_buttons() & mouse_buttons::left) == mouse_buttons::left ? xtd::forms::visual_styles::toggle_button_state::unchecked_pressed : xtd::forms::visual_styles::toggle_button_state::unchecked_hot;
+    else if (data_->check_state == xtd::forms::check_state::checked) data_->state = (mouse_buttons() & mouse_buttons::left) == mouse_buttons::left ? xtd::forms::visual_styles::toggle_button_state::checked_pressed : xtd::forms::visual_styles::toggle_button_state::checked_hot;
+    else if (data_->check_state == xtd::forms::check_state::indeterminate) data_->state = (mouse_buttons() & mouse_buttons::left) == mouse_buttons::left ? xtd::forms::visual_styles::toggle_button_state::mixed_pressed : xtd::forms::visual_styles::toggle_button_state::mixed_hot;
+  }
+  button_base::on_mouse_enter(e);
+}
+
+void toggle_button::on_mouse_leave(const event_args& e) {
+  if (flat_style() != xtd::forms::flat_style::system && enabled()) {
+    if (data_->check_state == xtd::forms::check_state::unchecked) data_->state = xtd::forms::visual_styles::toggle_button_state::unchecked_normal;
+    else if (data_->check_state == xtd::forms::check_state::checked) data_->state = xtd::forms::visual_styles::toggle_button_state::checked_normal;
+    else if (data_->check_state == xtd::forms::check_state::indeterminate) data_->state = xtd::forms::visual_styles::toggle_button_state::mixed_normal;
+  }
+  button_base::on_mouse_leave(e);
+}
+
+void toggle_button::on_mouse_up(const mouse_event_args& e) {
+  if (flat_style() != xtd::forms::flat_style::system && enabled() && (data_->state == xtd::forms::visual_styles::toggle_button_state::unchecked_pressed || data_->state == xtd::forms::visual_styles::toggle_button_state::checked_pressed || data_->state == xtd::forms::visual_styles::toggle_button_state::mixed_pressed)) {
+    if (data_->check_state == xtd::forms::check_state::unchecked) data_->state = xtd::forms::visual_styles::toggle_button_state::unchecked_hot;
+    else if (data_->check_state == xtd::forms::check_state::checked) data_->state = xtd::forms::visual_styles::toggle_button_state::checked_hot;
+    else if (data_->check_state == xtd::forms::check_state::indeterminate) data_->state = xtd::forms::visual_styles::toggle_button_state::mixed_hot;
+  }
+  button_base::on_mouse_up(e);
 }
 
 void toggle_button::on_paint(paint_event_args& e) {
@@ -91,6 +176,10 @@ void toggle_button::on_paint(paint_event_args& e) {
   if (flat_style() == xtd::forms::flat_style::flat) toggle_button_renderer::draw_flat_toggle_button(style, e.graphics(), e.clip_rectangle(), state(), back_color() != default_back_color() ? std::optional<drawing::color> {back_color()} : std::nullopt, flat_appearance(), text(), text_align(), fore_color() != default_fore_color() ? std::optional<drawing::color> {fore_color()} : std::nullopt, font() != default_font() ? std::optional<drawing::font> {font()} : std::nullopt, image(), image_align());
   if (flat_style() == xtd::forms::flat_style::popup) toggle_button_renderer::draw_popup_toggle_button(style, e.graphics(), e.clip_rectangle(), state(), back_color() != default_back_color() ? std::optional<drawing::color> {back_color()} : std::nullopt, flat_appearance(), text(), text_align(), fore_color() != default_fore_color() ? std::optional<drawing::color> {fore_color()} : std::nullopt, font() != default_font() ? std::optional<drawing::font> {font()} : std::nullopt, image(), image_align());
   button_base::on_paint(e);
+}
+
+xtd::forms::visual_styles::toggle_button_state toggle_button::state() const noexcept {
+  return data_->state;
 }
 
 void toggle_button::wnd_proc(message& message) {
@@ -114,7 +203,7 @@ void toggle_button::wm_mouse_down(message& message) {
 }
 
 void toggle_button::wm_mouse_up(message& message) {
-  if (auto_check_)
+  if (data_->auto_check)
     switch (check_state()) {
       case forms::check_state::unchecked: check_state(forms::check_state::checked); break;
       case forms::check_state::checked: check_state(three_state() ? forms::check_state::indeterminate : forms::check_state::unchecked); break;
