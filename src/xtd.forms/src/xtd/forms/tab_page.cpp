ddf06dd4bdee4d02628c3ecadbfe94a22fdd6e29
Gammasoft <gammasoft71@gmail.com>
2022-11-30 19:35:47 +0100

    review code

--- src/xtd.forms/src/xtd/forms/tab_page.cpp
+++ src/xtd.forms/src/xtd/forms/tab_page.cpp
@@ -27,10 +27,14 @@ scrollable_control& tab_page::auto_scroll(bool value) {
   return *this;
 }
 
+int32_t tab_page::image_index() const noexcept {
+  return data_->image_index;
+}
+
 tab_page& tab_page::image_index(int32_t value) {
-  if (image_index_ != value) {
-    image_index_ = value;
-    if (is_handle_created() && parent().has_value()) native::tab_page::image_index(handle(), image_index_);
+  if (data_->image_index != value) {
+    data_->image_index = value;
+    if (is_handle_created() && parent().has_value()) native::tab_page::image_index(handle(), data_->image_index);
   }
   return *this;
 }
@@ -52,7 +56,7 @@ void tab_page::on_handle_created(const event_args& e) {
   panel::on_handle_created(e);
   native::tab_control::insert_page(parent().value().get().handle(), parent().value().get().get_child_index(handle()), handle());
   native::tab_page::text(handle(), text());
-  native::tab_page::image_index(handle(), image_index_);
+  native::tab_page::image_index(handle(), data_->image_index);
   if (parent().value().get().get_child_index(handle()) == static_cast<tab_control&>(parent().value().get()).data_->selected_index)
     native::tab_control::selected_index(parent().value().get().handle(), static_cast<tab_control&>(parent().value().get()).data_->selected_index);
 }
