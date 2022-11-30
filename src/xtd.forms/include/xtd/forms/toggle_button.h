57f259acb6eb9b186c4a1792d7f26f70d7c1e83c
Gammasoft <gammasoft71@gmail.com>
2022-11-30 20:10:01 +0100

    review code

--- src/xtd.forms/include/xtd/forms/toggle_button.h
+++ src/xtd.forms/include/xtd/forms/toggle_button.h
@@ -69,7 +69,7 @@ namespace xtd {
       /// @brief Gets a value indicating whether the checked or check_state values and the toggle_button's appearance are automatically changed when the toggle_button is clicked.
       /// @return true if the checked value or check_state value and the appearance of the control are automatically changed on the click event; otherwise, false. The default value is true.
       /// @remarks If auto_check is set to false, you will need to add code to update the checked or check_state values in the click event handler.
-      virtual bool auto_check() const {return auto_check_;}
+      virtual bool auto_check() const noexcept;
       /// @brief Sets a value indicating whether the checked or check_state values and the toggle_button's appearance are automatically changed when the toggle_button is clicked.
       /// @param auto_check true if the checked value or check_state value and the appearance of the control are automatically changed on the click event; otherwise, false. The default value is true.
       /// @remarks If auto_check is set to false, you will need to add code to update the checked or check_state values in the click event handler.
@@ -77,7 +77,7 @@ namespace xtd {
       
       /// @brief Gets the horizontal and vertical alignment of the check mark on a toggle_button control.
       /// @return One of the content_alignment values. The default value is middle_left.
-      virtual content_alignment check_align() const {return check_align_;}
+      virtual content_alignment check_align() const noexcept;
       /// @brief Sets the horizontal and vertical alignment of the check mark on a toggle_button control.
       /// @param check_align One of the content_alignment values. The default value is middle_left.
       virtual toggle_button& check_align(content_alignment check_align);
@@ -85,7 +85,7 @@ namespace xtd {
       /// @brief Gets a value indicating whether the toggle_button is in the checked state.
       /// @return true if the toggle_button is in the checked state; otherwise, false. The default value is false. If the three_state property is set to true, the checked property will return true for either a checked or indeterminate check_state.
       /// @remarks When the value is true, the toggle_button portion of the control displays a check mark. If the appearance property is set to button, the control will appear sunken when checked is true and raised like a standard button when false.
-      virtual bool checked() const {return checked_;}
+      virtual bool checked() const noexcept;
       /// @brief Sets a value indicating whether the toggle_button is in the checked state.
       /// @param checked true if the toggle_button is in the checked state; otherwise, false. The default value is false.
       /// @remarks When the value is true, the toggle_button portion of the control displays a check mark. If the appearance property is set to button, the control will appear sunken when checked is true and raised like a standard button when false.
@@ -100,7 +100,7 @@ namespace xtd {
       /// | checked       | The toggle_button displays a check mark.          | The control appears sunken. |
       /// | unchecked     | The CheckBox is empty.                            | The control appears raised. |
       /// | indeterminate | The CheckBox displays a check mark and is shaded. | The control appears flat.   |
-      virtual forms::check_state check_state() const {return check_state_;}
+      virtual forms::check_state check_state() const noexcept;
       /// @brief Sets the state of the toggle_button.
       /// @param chexk_state One of the check_state enumeration values. The default value is unchecked.
       /// @remarks The following table describes the xtd::forms::appearance of the toggle_button control in its different states for the normal and button style control toggle_button::appearance.
@@ -114,7 +114,7 @@ namespace xtd {
       /// @brief Gets a value indicating whether the toggle_button will allow three check states rather than two.
       /// @return true if the toggle_button is able to display three check states; otherwise, false. The default value is false.
       /// @remarks If the three_state property is set to false, the check_state property value can only be set to the indeterminate value of xtd.forms.check_state in code and not by user interaction.
-      virtual bool three_state() const {return three_state_;}
+      virtual bool three_state() const noexcept;
       /// @brief Gets a value indicating whether the toggle_button will allow three check states rather than two.
       /// @param three_state true if the toggle_button is able to display three check states; otherwise, false. The default value is false.
       /// @remarks If the three_state property is set to false, the check_state property value can only be set to the indeterminate value of xtd.forms.check_state in code and not by user interaction.
@@ -156,84 +156,29 @@ namespace xtd {
       /// @param e An event_args that contains the event data.
       /// @remarks Raising an event invokes the event handler through a delegate.
       /// @remarks The on_checked_changed method also allows derived classes to handle the event without attaching a delegate. This is the preferred technique for handling the event in a derived class.
-      virtual void on_checked_changed(const event_args& e) {
-        if (flat_style() != xtd::forms::flat_style::system && enabled()) {
-          if (check_state_ == xtd::forms::check_state::unchecked) state_ = xtd::forms::visual_styles::toggle_button_state::unchecked_normal;
-          else if (check_state_ == xtd::forms::check_state::checked) state_ = xtd::forms::visual_styles::toggle_button_state::checked_normal;
-          else if (check_state_ == xtd::forms::check_state::indeterminate) state_ = xtd::forms::visual_styles::toggle_button_state::mixed_normal;
-        }
-        checked_changed(*this, e);
-        if (flat_style() != xtd::forms::flat_style::system) invalidate();
-      }
+      virtual void on_checked_changed(const event_args& e);
       
       /// @brief Raises the toggle_button::check_state_changed event.
       /// @param e An event_args that contains the event data.
       /// @remarks Raising an event invokes the event handler through a delegate.
       /// @remarks The on_check_state_changed method also allows derived classes to handle the event without attaching a delegate. This is the preferred technique for handling the event in a derived class.
-      virtual void on_check_state_changed(const event_args& e) {
-        if (flat_style() != xtd::forms::flat_style::system && enabled()) {
-          if (check_state_ == xtd::forms::check_state::unchecked) state_ = xtd::forms::visual_styles::toggle_button_state::unchecked_normal;
-          else if (check_state_ == xtd::forms::check_state::checked) state_ = xtd::forms::visual_styles::toggle_button_state::checked_normal;
-          else if (check_state_ == xtd::forms::check_state::indeterminate) state_ = xtd::forms::visual_styles::toggle_button_state::mixed_normal;
-        }
-        check_state_changed(*this, e);
-        if (flat_style() != xtd::forms::flat_style::system) invalidate();
-      }
+      virtual void on_check_state_changed(const event_args& e);
       
-      void on_enabled_changed(const event_args& e) override {
-        if (flat_style() != xtd::forms::flat_style::system) {
-          if (check_state_ == xtd::forms::check_state::unchecked) state_ = enabled() ? xtd::forms::visual_styles::toggle_button_state::unchecked_normal : xtd::forms::visual_styles::toggle_button_state::unchecked_disabled;
-          else if (check_state_ == xtd::forms::check_state::checked) state_ = enabled() ? xtd::forms::visual_styles::toggle_button_state::checked_normal : xtd::forms::visual_styles::toggle_button_state::checked_disabled;
-          else if (check_state_ == xtd::forms::check_state::indeterminate) state_ = enabled() ? xtd::forms::visual_styles::toggle_button_state::mixed_normal : xtd::forms::visual_styles::toggle_button_state::mixed_disabled;
-        }
-        button_base::on_enabled_changed(e);
-      }
+      void on_enabled_changed(const event_args& e) override;
       
       /// @brief Raises the handle_created event.
       /// @param e An event_args that contains the event data.
       void on_handle_created(const event_args& e) override;
       
-      void on_mouse_down(const mouse_event_args& e) override {
-        if (flat_style() != xtd::forms::flat_style::system && enabled()) {
-          if (check_state_ == xtd::forms::check_state::unchecked) state_ = xtd::forms::visual_styles::toggle_button_state::unchecked_pressed;
-          else if (check_state_ == xtd::forms::check_state::checked) state_ = xtd::forms::visual_styles::toggle_button_state::checked_pressed;
-          else if (check_state_ == xtd::forms::check_state::indeterminate) state_ = xtd::forms::visual_styles::toggle_button_state::mixed_pressed;
-        }
-        button_base::on_mouse_down(e);
-      }
-      
-      void on_mouse_enter(const event_args& e) override {
-        if (flat_style() != xtd::forms::flat_style::system && enabled()) {
-          if (check_state_ == xtd::forms::check_state::unchecked) state_ = (mouse_buttons() & mouse_buttons::left) == mouse_buttons::left ? xtd::forms::visual_styles::toggle_button_state::unchecked_pressed : xtd::forms::visual_styles::toggle_button_state::unchecked_hot;
-          else if (check_state_ == xtd::forms::check_state::checked) state_ = (mouse_buttons() & mouse_buttons::left) == mouse_buttons::left ? xtd::forms::visual_styles::toggle_button_state::checked_pressed : xtd::forms::visual_styles::toggle_button_state::checked_hot;
-          else if (check_state_ == xtd::forms::check_state::indeterminate) state_ = (mouse_buttons() & mouse_buttons::left) == mouse_buttons::left ? xtd::forms::visual_styles::toggle_button_state::mixed_pressed : xtd::forms::visual_styles::toggle_button_state::mixed_hot;
-        }
-        button_base::on_mouse_enter(e);
-      }
-      
-      void on_mouse_leave(const event_args& e) override {
-        if (flat_style() != xtd::forms::flat_style::system && enabled()) {
-          if (check_state_ == xtd::forms::check_state::unchecked) state_ = xtd::forms::visual_styles::toggle_button_state::unchecked_normal;
-          else if (check_state_ == xtd::forms::check_state::checked) state_ = xtd::forms::visual_styles::toggle_button_state::checked_normal;
-          else if (check_state_ == xtd::forms::check_state::indeterminate) state_ = xtd::forms::visual_styles::toggle_button_state::mixed_normal;
-        }
-        button_base::on_mouse_leave(e);
-      }
-      
-      void on_mouse_up(const mouse_event_args& e) override {
-        if (flat_style() != xtd::forms::flat_style::system && enabled() && (state_ == xtd::forms::visual_styles::toggle_button_state::unchecked_pressed || state_ == xtd::forms::visual_styles::toggle_button_state::checked_pressed || state_ == xtd::forms::visual_styles::toggle_button_state::mixed_pressed)) {
-          if (check_state_ == xtd::forms::check_state::unchecked) state_ = xtd::forms::visual_styles::toggle_button_state::unchecked_hot;
-          else if (check_state_ == xtd::forms::check_state::checked) state_ = xtd::forms::visual_styles::toggle_button_state::checked_hot;
-          else if (check_state_ == xtd::forms::check_state::indeterminate) state_ = xtd::forms::visual_styles::toggle_button_state::mixed_hot;
-        }
-        button_base::on_mouse_up(e);
-      }
-      
+      void on_mouse_down(const mouse_event_args& e) override;
+      void on_mouse_enter(const event_args& e) override;
+      void on_mouse_leave(const event_args& e) override;
+      void on_mouse_up(const mouse_event_args& e) override;
       void on_paint(paint_event_args& e) override;
       
       /// @brief Gets state.
       /// @return One of xtd::forms::visual_styles::toggle_button_state values.
-      xtd::forms::visual_styles::toggle_button_state state() const noexcept {return state_;}
+      xtd::forms::visual_styles::toggle_button_state state() const noexcept;
 
       void wnd_proc(message& message) override;
       /// @}
@@ -243,12 +188,15 @@ namespace xtd {
       void wm_mouse_down(message& message);
       void wm_mouse_up(message& message);
 
-      bool auto_check_ = true;
-      bool three_state_ = 0;
-      bool checked_ = false;
-      content_alignment check_align_ = content_alignment::middle_left;
-      forms::check_state check_state_ = forms::check_state::unchecked;
-      xtd::forms::visual_styles::toggle_button_state state_ = xtd::forms::visual_styles::toggle_button_state::unchecked_normal;
+      struct data {
+        bool auto_check = true;
+        bool three_state = 0;
+        bool checked = false;
+        content_alignment check_align = content_alignment::middle_left;
+        forms::check_state check_state = forms::check_state::unchecked;
+        xtd::forms::visual_styles::toggle_button_state state = xtd::forms::visual_styles::toggle_button_state::unchecked_normal;
+      };
+      std::shared_ptr<data> data_ = std::make_shared<data>();
     };
   }
 }
