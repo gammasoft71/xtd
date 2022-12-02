44a8f10468ebf347524c312db3e2425707182bf5
Gammasoft <gammasoft71@gmail.com>
2022-12-02 14:11:57 +0100

    review code

--- src/xtd.core/include/xtd/cpp_language.h
+++ src/xtd.core/include/xtd/cpp_language.h
@@ -27,14 +27,13 @@ namespace xtd {
     
     /// @{
     /// @brief Initialize a new instance of xtd::cpp_language class.
-    explicit cpp_language(uint32_t cpp) noexcept : cpp_(cpp) {}
+    explicit cpp_language(uint32_t cpp) noexcept;
     /// @}
     
     /// @cond
     cpp_language() noexcept = default;
     cpp_language(const cpp_language&) noexcept = default;
     cpp_language& operator =(const cpp_language&) noexcept = default;
-    friend std::ostream& operator <<(std::ostream& os, const cpp_language& cpp_language) noexcept {return os << cpp_language.to_string();}
     /// @endcond
     
     /// @name Properties
@@ -42,69 +41,44 @@ namespace xtd {
     /// @{
     /// @brief Gets if is experimental language
     /// @return true if is experimental language; otherwise false.
-    bool is_experimental_language() const noexcept {return language() != experimental_language();}
+    bool is_experimental_language() const noexcept;
     
     /// @brief Gets if is supported by xtd.
     /// @return true is supported by xtd; otherwise false.
-    bool is_supported() const noexcept {return cpp_ >= 201703L;}
+    bool is_supported() const noexcept;
     
     /// @brief Gets lagnguage id.
     /// @return One of xtd::languade_id values.
-    language_id language() const noexcept {
-      if (cpp_ >= 202002L) return language_id::cpp20;
-      if (cpp_ >= 201703L) return language_id::cpp17;
-      if (cpp_ >= 201402L) return language_id::cpp14;
-      if (cpp_ >= 201103L) return language_id::cpp11;
-      if (cpp_ >= 199711L) return language_id::cpp98;
-      if (cpp_ >= 1L) return language_id::cpp_pre98;
-      return language_id::unknown;
-    }
+    language_id language() const noexcept;
     
     /// @brief Gets experimental language id.
     /// @return One of xtd::language_id values.
     /// @Remarks If is not experimental, the xtd::language_id is the same as xtd::cpp_language::language property.
-    language_id experimental_language() const noexcept {
-      if (cpp_ >= 201707L) return language_id::cpp20;
-      if (cpp_ >= 201411L) return language_id::cpp17;
-      if (cpp_ >= 201210L) return language_id::cpp14;
-      if (cpp_ >= 200410L) return language_id::cpp11;
-      if (cpp_ >= 199711L) return language_id::cpp98;
-      if (cpp_ >= 1L) return language_id::cpp_pre98;
-      return language_id::unknown;
-    }
+    language_id experimental_language() const noexcept;
     
     /// @brief Gets the month when the cpp language was released.
     /// @return The month when the cpp version was released.
-    int32_t month() const noexcept {return cpp_ % 100;}
+    int32_t month() const noexcept;
     
     /// @brief Gets the offical name of the cpp language.
     /// @return The offical name of the cpp version.
-    xtd::ustring name() const noexcept {
-      static std::map<language_id, xtd::ustring> names {{language_id::cpp_pre98, "C++ Pre 98"}, {language_id::cpp98, "C++ 98"}, {language_id::cpp11, "C++ 11"}, {language_id::cpp14, "C++ 14"}, {language_id::cpp17, "C++ 17"}, {language_id::cpp20, "C++ 20"}, {language_id::unknown, "<unknown>"}};
-      if (is_experimental_language()) return ustring::format("Experimental {}", names[experimental_language()]);
-      return names[language()];
-    }
+    xtd::ustring name() const noexcept;
     
     /// @brief Gets the value of cpp language.
     /// @return The value of cpp version.
-    uint32_t value() const noexcept {return cpp_;}
+    uint32_t value() const noexcept;
     
     /// @brief Gets the version string of cpp language.
     /// @return The version string.
-    xtd::ustring version_string() const noexcept {
-      return ustring::format("{} {}", name(), version());
-    }
+    xtd::ustring version_string() const noexcept;
     
     /// @brief Gets The version of cpp language.
     /// @return The version.
-    const xtd::version& version() const noexcept {
-      static xtd::version ver(cpp_ / 100, cpp_ % 100);
-      return ver;
-    }
+    const xtd::version& version() const noexcept;
     
     /// @brief Gets the year when the cpp language was released.
     /// @return The year when the cpp version was released.
-    int32_t year() const noexcept {return cpp_ / 100;}
+    int32_t year() const noexcept;
     /// @}
     
     /// @name Methods
@@ -112,7 +86,7 @@ namespace xtd {
     /// @{
     /// @brief Converts the value of this operating_system object to its equivalent string representation.
     /// @return The string representation of the values returned by the platform, version, and service_pack methods.
-    xtd::ustring to_string() const noexcept override {return version_string();}
+    xtd::ustring to_string() const noexcept override;
     /// @}
     
   private:
