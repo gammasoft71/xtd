184182ba554a88a6de951a9732d6d96641340eb8
Gammasoft <gammasoft71@gmail.com>
2022-12-02 20:46:37 +0100

    review code

--- src/xtd.core/include/xtd/processor.h
+++ src/xtd.core/include/xtd/processor.h
@@ -25,7 +25,7 @@ namespace xtd {
     /// @param architecture One of xtd::architectuure_id valuues.
     /// @param is_64_bit true if 64 bit architecture; otherwise false.
     /// @param core_count THe number of cores.
-    processor(xtd::architecture_id architecture, bool is_64_bit, uint32_t core_count) : architecture_(architecture), is_64_bit_(is_64_bit), core_count_(core_count) {}
+    processor(xtd::architecture_id architecture, bool is_64_bit, uint32_t core_count);
     /// @}
     
     /// @cond
@@ -39,32 +39,23 @@ namespace xtd {
     /// @{
     /// @brief Gets a xtd::platform_id enumeration value that identifies the operating system platform.
     /// @return xtd::platform_id One of the xtd::platform_id values.
-    xtd::architecture_id architecture() const noexcept {return architecture_;}
+    xtd::architecture_id architecture() const noexcept;
     
+    /// @brief Gets architecture string.
+    /// @return The architecture string.
+    xtd::ustring architecture_string() const noexcept;
+
     /// @brief Gets the number of processors on the current machine.
     /// @return The 32-bit unsigned integer that specifies the number of processors on the current machine. There is no default. If the current machine contains multiple processor groups, this property returns the number of logical processors that are available for use.
-    uint32_t core_count() const noexcept {return core_count_;}
+    uint32_t core_count() const noexcept;
     
     /// @brief Determines whether the current operating system is a 64-bit operating system.
     /// @return true if the operating system is 64-bit; otherwise, false.
-    bool is_64_bit() const noexcept {return is_64_bit_;}
+    bool is_64_bit() const noexcept;
     
     /// @brief Gets the concatenated string representation of the platform identifier.
     /// @return The string representation of the values returned by the platform.
-    xtd::ustring name() const noexcept {
-      static std::map<xtd::architecture_id, xtd::ustring> processor_names {{architecture_id::x86, "Intel or AMD"}, {architecture_id::arm, "ARM"}, {architecture_id::unknown, "<Unknown>"}};
-      return processor_names[architecture_];
-    }
-    
-    xtd::ustring architecture_string() const noexcept {
-      if (!architecture_string_.empty()) return architecture_string_;
-      architecture_string_ = ustring::format("{}", architecture_);
-      if (is_64_bit_) {
-        if (architecture_ == architecture_id::x86) architecture_string_ += "_";
-        architecture_string_ += "64";
-      }
-      return architecture_string_;
-    }
+    xtd::ustring name() const noexcept;
     /// @}
     
     /// @name Methods
@@ -72,7 +63,7 @@ namespace xtd {
     /// @{
     /// @brief Converts the value of this processor object to its equivalent string representation.
     /// @return The string representation of the values returned by the platform, version, and service_pack methods.
-    xtd::ustring to_string() const noexcept override {return architecture_string();}
+    xtd::ustring to_string() const noexcept override;
     /// @}
     
   private:
---
+++ src/xtd.core/src/xtd/processor.cpp
@@ -0,0 +1,37 @@
+#include "../../include/xtd/processor.h"
+
+using namespace xtd;
+
+processor::processor(xtd::architecture_id architecture, bool is_64_bit, uint32_t core_count) : architecture_(architecture), is_64_bit_(is_64_bit), core_count_(core_count) {
+}
+
+xtd::architecture_id processor::architecture() const noexcept {
+  return architecture_;
+}
+
+xtd::ustring processor::architecture_string() const noexcept {
+  if (!architecture_string_.empty()) return architecture_string_;
+  architecture_string_ = ustring::format("{}", architecture_);
+  if (is_64_bit_) {
+    if (architecture_ == architecture_id::x86) architecture_string_ += "_";
+    architecture_string_ += "64";
+  }
+  return architecture_string_;
+}
+
+uint32_t processor::core_count() const noexcept {
+  return core_count_;
+}
+
+bool processor::is_64_bit() const noexcept {
+  return is_64_bit_;
+}
+
+xtd::ustring processor::name() const noexcept {
+  static std::map<xtd::architecture_id, xtd::ustring> processor_names {{architecture_id::x86, "Intel or AMD"}, {architecture_id::arm, "ARM"}, {architecture_id::unknown, "<Unknown>"}};
+  return processor_names[architecture_];
+}
+
+xtd::ustring processor::to_string() const noexcept {
+  return architecture_string();
+}
