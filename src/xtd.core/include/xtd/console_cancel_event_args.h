08cd498f54b4280fa54d071d4bed661e2086a31c
Gammasoft <gammasoft71@gmail.com>
2022-12-02 13:56:10 +0100

    review code

--- src/xtd.core/include/xtd/console_cancel_event_args.h
+++ src/xtd.core/include/xtd/console_cancel_event_args.h
@@ -26,7 +26,7 @@ namespace xtd {
     /// @brief Initializes a new instance of the console_cancel_event_args class.
     /// @param cancel true if the current process should resume when the event handler concludes; false if the current process should terminate. The default value is false; the current process terminates when the event handler returns. If true, the current process continues..
     /// @param special_key One of the enumeration values that specifies the key combination that interrupted the current process. There is no default value..
-    console_cancel_event_args(bool cancel, console_special_key special_key) : cancel_(cancel), special_key_(special_key) {}
+    console_cancel_event_args(bool cancel, console_special_key special_key);
     /// @}
     
     /// @cond
@@ -39,15 +39,15 @@ namespace xtd {
     /// @{
     /// @brief Gets a value that indicates whether simultaneously pressing the Control modifier key and the C console key (Ctrl+C) or the Ctrl+Break keys terminates the current process. The default is false, which terminates the current process.
     /// @return true if the current process should resume when the event handler concludes; false if the current process should terminate. The default value is false; the current process terminates when the event handler returns. If true, the current process continues.
-    bool cancel() const {return cancel_;}
+    bool cancel() const noexcept;
     
     /// @brief Sets a value that indicates whether simultaneously pressing the Control modifier key and the C console key (Ctrl+C) or the Ctrl+Break keys terminates the current process. The default is false, which terminates the current process.
     /// @param true if the current process should resume when the event handler concludes; false if the current process should terminate. The default value is false; the current process terminates when the event handler returns. If true, the current process continues.
-    void cancel(bool cancel) {cancel_ = cancel;}
+    void cancel(bool cancel);
     
     /// @brief Gets the combination of modifier and console keys that interrupted the current process.
     /// @return One of the enumeration values that specifies the key combination that interrupted the current process. There is no default value.
-    console_special_key special_key() const {return special_key_;}
+    console_special_key special_key() const noexcept;
     /// @}
     
   private:
