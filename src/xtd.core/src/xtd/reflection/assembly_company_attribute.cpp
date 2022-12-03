bb27cb37439deb1282a83ad1d59f514102d406df
Gammasoft <gammasoft71@gmail.com>
2022-12-03 22:41:45 +0100

    review code

--- src/xtd.core/src/xtd/reflection/assembly_company_attribute.cpp
+++ src/xtd.core/src/xtd/reflection/assembly_company_attribute.cpp
@@ -10,6 +10,10 @@ assembly_company_attribute::assembly_company_attribute(const ustring& company, c
   __assembly_company_attribute__() = make_shared<xtd::reflection::assembly_company_attribute>(company);
 }
 
+const ustring& assembly_company_attribute::company() const noexcept {
+  return company_;
+}
+
 shared_ptr<object> assembly_company_attribute::get_type_id() const noexcept {
   return xtd::guid::new_guid().memberwise_clone<xtd::guid>();
 }
