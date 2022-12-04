731cbedc27f6410429847692719933e51f48ef40
Gammasoft <gammasoft71@gmail.com>
2022-12-04 16:33:06 +0100

    review code

--- src/xtd.drawing/include/xtd/drawing/bitmap.h
+++ src/xtd.drawing/include/xtd/drawing/bitmap.h
@@ -35,32 +35,32 @@ namespace xtd {
       /// @{
       /// @brief Initializes a new instance of the bitmap class from the specified existing image.
       /// @param original The xtd::drawing::image from which to create the new bitmap.
-      explicit bitmap(const image& original) : xtd::drawing::image(original) {}
+      explicit bitmap(const image& original);
       /// @brief Initializes a new instance of the bitmap class from the specified existing image, scaled to the specified size.
       /// @param original The xtd::drawing::image from which to create the new bitmap.
       /// @param new_size The xtd::drawing::size structure that represent the size of the new bitmap.
-      bitmap(const image& original, const xtd::drawing::size& new_size) : xtd::drawing::image(original, new_size.width(), new_size.height()) {}
+      bitmap(const image& original, const xtd::drawing::size& new_size);
       /// @brief Initializes a new instance of the bitmap class from the specified existing image, scaled to the specified rectangle.
       /// @param original The xtd::drawing::image from which to create the new bitmap.
       /// @param new_rect The xtd::drawing::rect structure that represent the location and size of the new bitmap.
-      bitmap(const image& original, const rectangle& new_rect) : xtd::drawing::image(original, new_rect) {}
+      bitmap(const image& original, const rectangle& new_rect);
       /// @brief Initializes a new instance of the bitmap class from the specified file.
       /// @param filename The bitmap file name and path.
       /// @remarks The file name and path can be relative to the application or an absolute path. Use this constructor to open images with the following file formats: BMP, GIF, EXIF, JPG, PNG and TIFF. For more information about supported formats, see Types of Bitmaps. The file remains locked until the Bitmap is disposed.
-      explicit bitmap(const xtd::ustring& filename) : xtd::drawing::image(filename) {}
+      explicit bitmap(const xtd::ustring& filename);
       /// @brief Initializes a new instance of the bitmap class from the specified data stream.
       /// @param stream The data stream used to load the image.
-      explicit bitmap(std::istream& stream) : xtd::drawing::image(stream) {}
+      explicit bitmap(std::istream& stream);
       /// @brief Initializes a new instance of the bitmap class from the specified data xpm.
       /// @param bits The data xpm used to load the image.
-      explicit bitmap(const char* const* bits) : xtd::drawing::image(bits) {}
+      explicit bitmap(const char* const* bits);
       /// @brief Initializes a new instance of the Bitmap class with the specified size.
       /// @param width The width, in pixels, of the new bitmap.
       /// @param height The height, in pixels, of the new bitmap.
-      bitmap(int32_t width, int32_t height) : xtd::drawing::image(width, height) {}
+      bitmap(int32_t width, int32_t height);
       /// @brief Initializes a new instance of the Bitmap class with the specified size.
       /// @param size the size, in pixels, of the new bitmap.
-      explicit bitmap(const xtd::drawing::size& size) : xtd::drawing::image(size.width(), size.height()) {}
+      explicit bitmap(const xtd::drawing::size& size);
       /// @}
       
       /// @name Methods
@@ -68,10 +68,10 @@ namespace xtd {
       /// @{
       /// @brief Creates a copy of the section of this Bitmap defined with a specified rectangle.
       /// @param rect Defines the portion of this bitmap to copy.
-      bitmap clone(const rectangle& rect) {return bitmap(*this, rect);}
+      bitmap clone(const rectangle& rect) const;
       /// @brief Creates a copy of the section of this Bitmap defined with a specified rectangle.
       /// @param rect Defines the portion of this bitmap to copy.
-      bitmap clone(const rectangle_f& rect) {return bitmap(*this, xtd::drawing::rectangle::ceiling(rect));}
+      bitmap clone(const rectangle_f& rect) const;
       
       /// @cond
       bitmap(const bitmap& bitmap) = default;
@@ -82,7 +82,7 @@ namespace xtd {
       /// @param x The x-coordinate of the pixel to retrieve.
       /// @param y The y-coordinate of the pixel to retrieve.
       /// @return A xtd::drawing::color structure that represents the color of the specified pixel.
-      drawing::color get_pixel(int32_t x, int32_t y) const;
+      drawing::color get_pixel(int32_t x, int32_t y)  const;
       
       /// @brief Sets the color of the specified pixel in this bitmap.
       /// @param x The x-coordinate of the pixel to retrieve.
--- src/xtd.drawing/src/xtd/drawing/bitmap.cpp
+++ src/xtd.drawing/src/xtd/drawing/bitmap.cpp
@@ -8,6 +8,38 @@ using namespace xtd::drawing;
 
 bitmap bitmap::empty;
 
+bitmap::bitmap(const image& original) : xtd::drawing::image(original) {
+}
+
+bitmap::bitmap(const image& original, const xtd::drawing::size& new_size) : xtd::drawing::image(original, new_size.width(), new_size.height()) {
+}
+
+bitmap::bitmap(const image& original, const rectangle& new_rect) : xtd::drawing::image(original, new_rect) {
+}
+
+bitmap::bitmap(const xtd::ustring& filename) : xtd::drawing::image(filename) {
+}
+
+bitmap::bitmap(std::istream& stream) : xtd::drawing::image(stream) {
+}
+
+bitmap::bitmap(const char* const* bits) : xtd::drawing::image(bits) {
+}
+
+bitmap::bitmap(int32_t width, int32_t height) : xtd::drawing::image(width, height) {
+}
+
+bitmap::bitmap(const xtd::drawing::size& size) : xtd::drawing::image(size.width(), size.height()) {
+}
+
+bitmap bitmap::clone(const rectangle& rect) const {
+  return bitmap(*this, rect);
+}
+
+bitmap bitmap::clone(const rectangle_f& rect) const {
+  return bitmap(*this, xtd::drawing::rectangle::ceiling(rect));
+}
+
 drawing::color bitmap::get_pixel(int32_t x, int32_t y) const {
   uint8_t a = 0, r = 0, g = 0, b = 0;
   native::image::get_pixel(handle(), x, y, a, r, g, b);
