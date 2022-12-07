1a0576de26a1b9a098ff053cb149f309494213fb
Gammasoft <gammasoft71@gmail.com>
2022-12-07 17:29:50 +0100

    Fix build

--- examples/xtd.core.examples/environment/environment_current_directory/src/environment_current_directory.cpp
+++ examples/xtd.core.examples/environment/environment_current_directory/src/environment_current_directory.cpp
@@ -1,13 +1,11 @@
 #include <xtd/xtd>
 
-using namespace std::filesystem;
 using namespace xtd;
+using namespace xtd::io;
 
 int main() {
   environment::current_directory(environment::get_folder_path(environment::special_folder::desktop));
-  directory_entry info(".");
-  
-  console::write_line("Directory entry:   " + absolute(info.path()).string());
+  console::write_line("Directory entry:   " + path::get_full_path("."));
 }
 
 // This code produces the following output :
