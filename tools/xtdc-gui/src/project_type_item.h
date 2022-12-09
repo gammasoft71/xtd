902fc1a0e050db9c5b0167b1cd2abcf13466d6d9
Gammasoft <gammasoft71@gmail.com>
2022-12-09 14:56:55 +0100

    use iequatable interface

--- tools/xtdc-gui/src/project_type_item.h
+++ tools/xtdc-gui/src/project_type_item.h
@@ -2,6 +2,7 @@
 /// @brief Contains main_form class.
 #pragma once
 #include <string>
+#include <xtd/iequatable.h>
 #include <xtd/drawing/image.h>
 #include "project_language.h"
 #include "project_platform.h"
@@ -10,14 +11,12 @@
 
 namespace xtdc_gui {
   /// @brief Represents the project_type_item class
-  class project_type_item {
+  class project_type_item : public xtd::iequatable<project_type_item> {
   public:
     /// @brief Initializes a new instance of the project_type_item class.
     project_type_item() = default;
     project_type_item(const project_type_item&) = default;
     project_type_item& operator =(const project_type_item&) = default;
-    bool operator ==(const project_type_item& value) const {return icon_ == value.icon_ && name_ == value.name_ && description_ == value.description_ && project_language_ == value.project_language_ && project_platform_ == value.project_platform_ && project_sdk_ == value.project_sdk_ && project_type_ == value.project_type_;}
-    bool operator !=(const project_type_item& value) const {return !operator ==(value);}
     
     /// @brief Initializes a new instance of the project_type_item class.
     project_type_item(const xtd::drawing::image& icon, const xtd::ustring& name, const xtd::ustring& description, xtdc_gui::project_language project_language, xtdc_gui::project_sdk project_sdk, xtdc_gui::project_platform project_platform, xtdc_gui::project_type project_type) : icon_(icon), name_(name), description_(description), project_language_(project_language), project_platform_(project_platform), project_sdk_(project_sdk), project_type_(project_type) {}
@@ -29,7 +28,9 @@ namespace xtdc_gui {
     xtdc_gui::project_platform project_platform() const {return project_platform_;}
     xtdc_gui::project_sdk project_sdk() const {return project_sdk_;}
     xtdc_gui::project_type project_type() const {return project_type_;}
-    
+
+    bool equals(const project_type_item& value) const noexcept override {return icon_ == value.icon_ && name_ == value.name_ && description_ == value.description_ && project_language_ == value.project_language_ && project_platform_ == value.project_platform_ && project_sdk_ == value.project_sdk_ && project_type_ == value.project_type_;}
+
   private:
     xtd::drawing::image icon_ = xtd::drawing::image::empty;
     xtd::ustring name_;
