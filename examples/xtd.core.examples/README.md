1db24fd23a09312f18089f45d57023a1155e83ec
Gammasoft <gammasoft71@gmail.com>
2022-12-09 23:17:20 +0100

    Add iobservable_iobserver example

--- examples/xtd.core.examples/README.md
+++ examples/xtd.core.examples/README.md
@@ -161,6 +161,10 @@
 
 * [guid](guid/guid/README.md) shows how to use [xtd::guid](../../src/xtd.core/include/xtd/guid.h) class.
 
+## [Interfaces](interfaces/README.md)
+
+* [iobservable_iobserver](interfaces/iobservable_iobserver/README.md) shows how to use how to use [xtd::iobservable](https://codedocs.xyz/gammasoft71/xtd/classxtd_1_1iobservable.html) and [xtd::iobserver](https://codedocs.xyz/gammasoft71/xtd/classxtd_1_1iobserver.html) interfaces.
+
 ## [IO](io/README.md)
 
 * [binary_reader](io/binary_reader/README.md) shows how to use [xtd::io::binary_reader](../../src/xtd.core/include/xtd/io/binary_reader.h) class.
---
+++ examples/xtd.core.examples/interfaces/iobservable_iobserver/src/iobservable_iobserver.cpp
@@ -0,0 +1,113 @@
+#include <xtd/xtd>
+
+using namespace std;
+using namespace xtd;
+
+struct location {
+public:
+  location(double latitude, double longitude) : latitutde_(latitude), longitude_(longitude) {}
+  
+  double latitude() const noexcept {return latitutde_;}
+  double longitude() const noexcept {return longitude_;}
+  
+private:
+  double latitutde_ = .0;
+  double longitude_ = .0;
+};
+
+class location_unknown_exception : public system_exception {
+public:
+  location_unknown_exception() {}
+};
+
+class location_tracker : public iobservable<location> {
+public:
+  location_tracker() = default;
+  
+  void subscribe(iobserver<location>& observer) noexcept override {
+    if (find(observers_.begin(), observers_.end(), &observer) == observers_.end())
+      observers_.push_back(&observer);
+  }
+  
+  void unsubscribe(iobserver<location>& observer) noexcept override {
+    auto iterator = find(observers_.begin(), observers_.end(), &observer);
+    if (iterator != observers_.end())
+      observers_.erase(iterator);
+  }
+  
+  void track_location(optional<location> loc) {
+    for (auto observer : observers_) {
+      if (!loc.has_value())
+        observer->on_error(location_unknown_exception());
+      else
+        observer->on_next(loc.value());
+    }
+  }
+  
+  void end_transmission() {
+    for (auto observer : observers_)
+      observer->on_completed();
+    observers_.clear();
+  }
+  
+private:
+  vector<iobserver<location>*> observers_;
+};
+
+class location_reporter : iobserver<location> {
+public:
+  location_reporter(ustring name) : name_(name) {}
+  virtual ~location_reporter() {unsubscribe();}
+  
+  ustring name() const noexcept {return name_;}
+  
+  virtual void subscribe(iobservable<location>& provider) {
+    provider_ = &provider;
+    provider_->subscribe(*this);
+  }
+  
+  void on_completed() noexcept override {
+    console::write_line("The location Tracker has completed transmitting data to {}.", name());
+    unsubscribe();
+  }
+  
+  void on_error(const exception& e) noexcept override {
+    console::write_line("{}: The location cannot be determined.", name());
+  }
+  
+  void on_next(const location& value) noexcept override {
+    console::write_line("{}: The current location is {}, {}", name(), value.latitude(), value.longitude());
+  }
+  
+  virtual void unsubscribe() {
+    if (provider_ != nullptr) provider_->unsubscribe(*this);
+    provider_ = nullptr;
+  }
+  
+private:
+  ustring name_;
+  iobservable<location>* provider_ = nullptr;
+};
+
+int main() {
+  // Define a provider and two observers.
+  location_tracker provider;
+  location_reporter reporter1 {"Fixed GPS"};
+  reporter1.subscribe(provider);
+  location_reporter reporter2 {"Mobile GPS"};
+  reporter2.subscribe(provider);
+  
+  provider.track_location(location {47.6456, -122.1312});
+  reporter1.unsubscribe();
+  provider.track_location(location {47.6677, -122.1199});
+  provider.track_location(nullopt);
+  provider.end_transmission();
+}
+
+// The example displays output similar to the following:
+//
+// Fixed GPS: The current location is 47.6456, -122.1312
+// Mobile GPS: The current location is 47.6456, -122.1312
+// Mobile GPS: The current location is 47.6677, -122.1199
+// Mobile GPS: The location cannot be determined.
+// The location Tracker has completed transmitting data to Mobile GPS.
