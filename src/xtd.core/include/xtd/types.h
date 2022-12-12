e11512a15202d56636c661f58e46df6764a89d8f
Gammasoft <gammasoft71@gmail.com>
2022-12-12 22:40:42 +0100

    Move cha8_t in __char8_t file

--- src/xtd.core/include/xtd/types.h
+++ src/xtd.core/include/xtd/types.h
@@ -6,49 +6,10 @@
 #include <string>
 #include <typeinfo>
 #include "core_export.h"
-
 /// @cond
-#if !defined(__cpp_lib_char8_t)
-class char8 {
-public:
-  char8() {}
-  char8(char value) : value_(value) {}
-  char8(const char8&) = default;
-  char8(char8&&) = default;
-  
-  operator char() const {return value_;}
-  bool operator ==(char8 c) const {return value_ == c.value_;}
-  bool operator !=(char8 c) const {return value_ != c.value_;}
-  bool operator <(char8 c) const {return value_ < c.value_;}
-  bool operator >(char8 c) const {return value_ > c.value_;}
-  bool operator <=(char8 c) const {return value_ <= c.value_;}
-  bool operator >=(char8 c) const {return value_ >= c.value_;}
-  
-  char8 operator +(char8 c) const {return char8(value_ + c.value_);}
-  char8 operator -(char8 c) const {return char8(value_ - c.value_);}
-  char8& operator =(char8 c) {value_ = c.value_; return *this;}
-  char8& operator ++() {value_ += 1; return *this;}
-  char8 operator ++(int) {char8 result(*this); ++(*this); return result;}
-  char8& operator --() {value_ -= 1; return *this;}
-  char8 operator --(int) {char8 result(*this); --(*this); return result;}
-  
-  bool operator ==(char c) const {return value_ == c;}
-  bool operator !=(char c) const {return value_ != c;}
-  bool operator <(char c) const {return value_ < c;}
-  bool operator >(char c) const {return value_ > c;}
-  bool operator <=(char c) const {return value_ <= c;}
-  bool operator >=(char c) const {return value_ >= c;}
-  
-  friend std::ostream& operator <<(std::ostream& os, const char8& c) noexcept {return os << std::to_string(static_cast<int>(c.value_));}
-  
-private:
-  char value_ = 0;
-};
-
-namespace std {
-  using u8string = basic_string<char8>;
-}
-#endif
+#define __XTD_CORE_CHAR8_T_INTERNAL__
+#include "internal/__char8_t.h"
+#undef __XTD_CORE_CHAR8_T_INTERNAL__
 /// @endcond
 
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
