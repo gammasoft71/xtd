9f517665b2ee9e4f3b16e0263be3415376bfa58b
Gammasoft <gammasoft71@gmail.com>
2022-12-13 10:35:03 +0100

    Replace int8_t by xtd::sbyte because int8_t is optional.

--- examples/xtd.tunit.examples/test_classes/generic_test_class/src/generic_test_class.cpp
+++ examples/xtd.tunit.examples/test_classes/generic_test_class/src/generic_test_class.cpp
@@ -14,7 +14,7 @@ template<>
 size_t get_max_value<int>() {return 2147483647;}
 
 template<>
-size_t get_max_value<int8_t>() {return 127;}
+size_t get_max_value<xtd::sbyte>() {return 127;}
 
 template<>
 size_t get_max_value<long int>() {return 9223372036854775807;}
@@ -30,7 +30,7 @@ namespace unit_tests {
   test_class_attribute<test<bool>> test_bool_class_attr {"test<bool>"};
   test_class_attribute<test<int>> test_int_class_attr {"test<int>"};
   test_class_attribute<test<long int>> test_double_class_attr {"test<long_int>"};
-  test_class_attribute<test<int8_t>> test_int8_t_class_attr {"test<int8_t>"};
+  test_class_attribute<test<xtd::sbyte>> test_int8_t_class_attr {"test<sbyte>"};
   test_class_attribute<test<uint8_t>> test_uint8_t_class_attr {"test<uint8_t>"};
   
   // The test class must be inherit from test_class class.
@@ -38,7 +38,7 @@ namespace unit_tests {
   class test : public test_class {
   public:
     void test_method_(test_max_value) {
-      static std::map<std::string, size_t> expected_max_values = {{typeid(bool).name(), std::numeric_limits<bool>::max()}, {typeid(int).name(), std::numeric_limits<int>::max()}, {typeid(long int).name(), std::numeric_limits<long int>::max()}, {typeid(int8_t).name(), std::numeric_limits<int8_t>::max()}};
+      static std::map<std::string, size_t> expected_max_values = {{typeid(bool).name(), std::numeric_limits<bool>::max()}, {typeid(int).name(), std::numeric_limits<int>::max()}, {typeid(long int).name(), std::numeric_limits<long int>::max()}, {typeid(xtd::sbyte).name(), std::numeric_limits<xtd::sbyte>::max()}};
       
       if (expected_max_values.find(typeid(type_t).name()) == expected_max_values.end())
         assert::fail("expect max value for this type is not defined.");
@@ -59,7 +59,7 @@ int main() {
 //   SUCCEED test<bool>.test_max_value (0 ms total)
 //   SUCCEED test<int>.test_max_value (0 ms total)
 //   SUCCEED test<long int>.test_max_value (0 ms total)
-//   SUCCEED test<int8_t>.test_max_value (0 ms total)
+//   SUCCEED test<sbyte>.test_max_value (0 ms total)
 //   FAILED  test<uint8_t>.test_max_value (0 ms total)
 //     expect max value for this type is not defined.
 //     Stack Trace: in |---OMITTED---|/generic_test_class.cpp:39
