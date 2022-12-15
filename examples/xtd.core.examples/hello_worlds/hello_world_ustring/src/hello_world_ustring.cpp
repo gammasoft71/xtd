7e2ac968dc003d30b3667a6150297c09581a2701
Gammasoft <gammasoft71@gmail.com>
2022-12-15 06:24:38 +0100

    Update hello_world_ustring.cpp

--- examples/xtd.core.examples/hello_worlds/hello_world_ustring/src/hello_world_ustring.cpp
+++ examples/xtd.core.examples/hello_worlds/hello_world_ustring/src/hello_world_ustring.cpp
@@ -1,10 +1,9 @@
 #include <xtd/xtd>
 
-using namespace std::string_literals;
 using namespace xtd;
 
 int main() {
-  console::out << ustring::format("{},{,10}!", "Hello", "World"s) << environment::new_line;
+  console::out << ustring::format("{},{,10}!", "Hello", "World"_s) << environment::new_line;
 }
 
 // This code produces the following output:
