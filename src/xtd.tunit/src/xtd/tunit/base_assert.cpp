bb5d73eb738d2af914cbb56cd0279f5d39a80467
Gammasoft <gammasoft71@gmail.com>
2022-12-15 19:52:36 +0100

    Review code

--- src/xtd.tunit/src/xtd/tunit/base_assert.cpp
+++ src/xtd.tunit/src/xtd/tunit/base_assert.cpp
@@ -8,6 +8,18 @@ using namespace xtd::tunit;
 using namespace std;
 using namespace std::string_literals;
 
+void base_assert::abort() {
+  abort("", xtd::diagnostics::stack_frame::empty());
+}
+
+void base_assert::abort(const xtd::diagnostics::stack_frame& stack_frame) {
+  abort("", stack_frame);
+}
+
+void base_assert::abort(const std::string& message) {
+  abort(message, xtd::diagnostics::stack_frame::empty());
+}
+
 void base_assert::abort(const std::string& message, const xtd::diagnostics::stack_frame& stack_frame) {
   if (!xtd::tunit::test::has_current_test()) throw abort_error(!message.empty() ? message : "Test aborted"s);
   else {
@@ -18,6 +30,22 @@ void base_assert::abort(const std::string& message, const xtd::diagnostics::stac
     xtd::tunit::test::current_test().status_ = test::test_status::aborted;
     throw abort_error(xtd::tunit::test::current_test().message_);
   }
+}
+
+void base_assert::fail() {
+  fail("", xtd::diagnostics::stack_frame::empty());
+}
+
+void base_assert::fail(const xtd::diagnostics::stack_frame& stack_frame) {
+  fail("", stack_frame);
+}
+
+void base_assert::fail(const std::string& message) {
+  fail(message, xtd::diagnostics::stack_frame::empty());
+}
+
+void base_assert::fail(const std::string& message, const xtd::diagnostics::stack_frame& stack_frame) {
+  base_assert::fail("", "", message, stack_frame);
 }
 
 void base_assert::error() {
@@ -56,6 +84,18 @@ void base_assert::fail(const std::string& expected, const std::string& actual, c
   throw assert_error(message != ""s ? message : "assertion failed!"s);
 }
 
+void base_assert::ignore() {
+  ignore("", xtd::diagnostics::stack_frame::empty());
+}
+
+void base_assert::ignore(const xtd::diagnostics::stack_frame& stack_frame) {
+  ignore("", stack_frame);
+}
+
+void base_assert::ignore(const std::string& message) {
+  ignore(message, xtd::diagnostics::stack_frame::empty());
+}
+
 void base_assert::ignore(const std::string& message, const xtd::diagnostics::stack_frame& stack_frame) {
   if (!xtd::tunit::test::has_current_test()) throw ignore_error(!message.empty() ? message : "Test ignored"s);
   else {
@@ -64,8 +104,20 @@ void base_assert::ignore(const std::string& message, const xtd::diagnostics::sta
     xtd::tunit::test::current_test().message_ = message != ""s ? message : "Test ignored"s;
     xtd::tunit::test::current_test().status_ = test::test_status::ignored;
     throw ignore_error(xtd::tunit::test::current_test().message_);
-  }}
-  
+  }
+}
+
+void base_assert::succeed() {
+  succeed("", csf_);
+}
+
+void base_assert::succeed(const xtd::diagnostics::stack_frame& stack_frame) {
+  succeed("", stack_frame);
+}
+
+void base_assert::succeed(const std::string& message) {
+  succeed(message, csf_);
+}
 
 void base_assert::succeed(const std::string& message, const xtd::diagnostics::stack_frame& stack_frame) {
   if (xtd::tunit::test::has_current_test()) {
@@ -75,4 +127,8 @@ void base_assert::succeed(const std::string& message, const xtd::diagnostics::st
     if (xtd::tunit::test::current_test().status_ != test::test_status::failed)
       xtd::tunit::test::current_test().status_ = test::test_status::succeed;
   }
+}
+
+std::string base_assert::join_items(const std::string& str) {
+  return __tunit_join__items(str);
 }
