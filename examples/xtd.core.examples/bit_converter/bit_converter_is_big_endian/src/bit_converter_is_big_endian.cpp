16930f3212093c342039968ca297026e8145a96a
Gammasoft <gammasoft71@gmail.com>
2022-12-15 06:27:06 +0100

    Update bit_converter_is_big_endian.cpp

--- examples/xtd.core.examples/bit_converter/bit_converter_is_big_endian/src/bit_converter_is_big_endian.cpp
+++ examples/xtd.core.examples/bit_converter/bit_converter_is_big_endian/src/bit_converter_is_big_endian.cpp
@@ -11,7 +11,7 @@ namespace examples {
         "This example of the bit_converter::is_big_endian field "
         "generates \nthe following output when run on "
         "x86-class computers.\n");
-      console::write_line("is_big_endian:  {0}",
+      console::write_line("is_big_endian:  {}",
         bit_converter::is_big_endian);
     }
   };
