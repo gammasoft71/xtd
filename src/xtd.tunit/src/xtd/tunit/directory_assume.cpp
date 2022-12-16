c4da3660886e392e54edde14d16a5b195d9b8262
Gammasoft <gammasoft71@gmail.com>
2022-12-16 13:58:22 +0100

    Fix namespace

--- src/xtd.tunit/src/xtd/tunit/directory_assume.cpp
+++ src/xtd.tunit/src/xtd/tunit/directory_assume.cpp
@@ -1,10 +1,11 @@
 #include "../../../include/xtd/tunit/directory_assume.h"
 
-using namespace std::filesystem;
 using namespace xtd;
 using namespace xtd::tunit;
 
 #if defined(__cpp_lib_filesystem)
+using namespace std::filesystem;
+
 void directory_assume::are_equal(const directory_entry& expected, const directory_entry& actual) {
   are_equal(expected, actual, "", xtd::diagnostics::stack_frame::empty());
 }
