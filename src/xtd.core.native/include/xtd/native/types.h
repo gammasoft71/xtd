d127880779484c53c599fd1371a1b7e02e572c97
Gammasoft <gammasoft71@gmail.com>
2022-12-16 01:26:12 +0100

    qualify type with signed

--- src/xtd.core.native/include/xtd/native/types.h
+++ src/xtd.core.native/include/xtd/native/types.h
@@ -16,14 +16,14 @@ namespace xtd {
 #if defined(__linux__) && defined(_LP64)
     /// @brief Represents a 32-bit or 64-bit signed integer.
     /// @warning Internal use only
-    using __slong__ = long long int; // Workaround: On linux 64 bits unfortunately 'int_least64_t' is an alias on 'long' and not on 'long long int' as other platforms. In order to avoid conflicts in operator or method overloads, it is necessary to give the type that is not taken into account.
+    using __slong__ = signed long long int; // Workaround: On linux 64 bits unfortunately 'int_least64_t' is an alias on 'long' and not on 'long long int' as other platforms. In order to avoid conflicts in operator or method overloads, it is necessary to give the type that is not taken into account.
     /// @brief Represents a 32-bit or 64-bit unsigned integer.
     /// @warning Internal use only
     using __ulong__ = unsigned long long int; // Workaround: On linux 64 bits unfortunately 'uint_least64_t' is an alias on 'unigned long' and not on 'unsigned long long int' as other platforms. In order to avoid conflicts in operator or method overloads, it is necessary to give the type that is not taken into account.
 #else
     /// @brief Represents a 32-bit or 64-bit signed integer.
     /// @warning Internal use only
-    using __slong__ = long; // Workaround: On linux 64 bits unfortunately 'int_least64_t' is an alias on 'long' and not on 'long long int' as other platforms. In order to avoid conflicts in operator or method overloads, it is necessary to give the type that is not taken into account.
+    using __slong__ = signed long; // Workaround: On linux 64 bits unfortunately 'int_least64_t' is an alias on 'long' and not on 'long long int' as other platforms. In order to avoid conflicts in operator or method overloads, it is necessary to give the type that is not taken into account.
     /// @brief Represents a 32-bit or 64-bit unsigned integer.
     /// @warning Internal use only
     using __ulong__ = unsigned long; // Workaround: On linux 64 bits unfortunately 'uint_least64_t' is an alias on 'unigned long' and not on 'unsigned long long int' as other platforms. In order to avoid conflicts in operator or method overloads, it is necessary to give the type that is not taken into account.
