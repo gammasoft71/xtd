5bad55209888ab0d6e4ad8887fbbcc3204d8ec75
Gammasoft <gammasoft71@gmail.com>
2022-12-16 14:17:40 +0100

    review code

---
+++ src/xtd.tunit/src/xtd/tunit/registered_test_class.cpp
@@ -0,0 +1,13 @@
+#include "../../../include/xtd/tunit/registered_test_class.h"
+
+using namespace std;
+using namespace xtd;
+using namespace xtd::tunit;
+
+registered_test_class::registered_test_class(const string& name, shared_ptr<xtd::tunit::test_class> test_class) noexcept : tc_(test_class) {
+  tc_->name_ = ustring(name).replace(' ', '_');
+}
+
+const shared_ptr<xtd::tunit::test_class>& registered_test_class::test() const noexcept {
+  return tc_;
+}
