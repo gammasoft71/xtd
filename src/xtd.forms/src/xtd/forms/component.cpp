221c1c6e8434bbf25fc29833bcc32b3dda02c65c
Gammasoft <gammasoft71@gmail.com>
2022-12-18 23:53:45 +0100

    move struct data definition in cpp file

--- src/xtd.forms/src/xtd/forms/component.cpp
+++ src/xtd.forms/src/xtd/forms/component.cpp
@@ -3,6 +3,14 @@
 using namespace xtd;
 using namespace xtd::forms;
 
+struct component::data {
+  bool can_raise_event = true;
+  bool design_mode = false;
+};
+
+component::component() : data_(std::make_shared<data>()) {
+}
+
 bool component::can_raise_events() const noexcept {
   return data_->can_raise_event;
 }
