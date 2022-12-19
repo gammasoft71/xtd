13055baf01b7286a48ec4d3022ae9470fa2b46cb
Gammasoft <gammasoft71@gmail.com>
2022-12-19 13:12:44 +0100

    move struct data definition in cpp file

---
+++ src/xtd.forms/src/xtd/forms/find_event_args.cpp
@@ -0,0 +1,46 @@
+#include "../../../include/xtd/forms/find_event_args.h"
+
+using namespace xtd;
+using namespace xtd::forms;
+
+struct find_event_args::data {
+  xtd::ustring find_string;
+  bool match_case = false;
+  xtd::forms::search_direction search_direction = xtd::forms::search_direction::down;
+  bool whole_word = false;
+};
+
+find_event_args::find_event_args() : data_(std::make_shared<data>()) {
+}
+
+find_event_args::find_event_args(const xtd::ustring& find_string, bool match_case, xtd::forms::search_direction search_direction, bool whole_word) : data_(std::make_shared<data>()) {
+  data_->find_string = find_string;
+  data_->match_case = match_case;
+  data_->search_direction = search_direction;
+  data_->whole_word = whole_word;
+}
+
+find_event_args::find_event_args(const find_event_args& other) : data_(std::make_shared<data>()) {
+  *data_ = *other.data_;
+}
+
+find_event_args& find_event_args::operator =(const find_event_args& other) {
+  *data_ = *other.data_;
+  return *this;
+}
+
+const xtd::ustring& find_event_args::find_string() const noexcept {
+  return data_->find_string;
+}
+
+bool find_event_args::match_case() const noexcept {
+  return data_->match_case;
+}
+
+xtd::forms::search_direction find_event_args::search_direction() const noexcept {
+  return data_->search_direction;
+}
+
+bool find_event_args::whole_word() const noexcept {
+  return data_->whole_word;
+}
