54c154144b9f2d7222189fae0265d10bc9b47709
Gammasoft <gammasoft71@gmail.com>
2022-12-19 11:59:29 +0100

    Update open_file_dialog.cpp

--- examples/xtd.forms.examples/dialogs/open_file_dialog/src/open_file_dialog.cpp
+++ examples/xtd.forms.examples/dialogs/open_file_dialog/src/open_file_dialog.cpp
@@ -9,6 +9,7 @@ public:
   form1() {
     text("Open file dialog example");
     controls().push_back(button1);
+    auto_scroll(true);
     
     button1.location({10, 10});
     button1.text("Open...");
