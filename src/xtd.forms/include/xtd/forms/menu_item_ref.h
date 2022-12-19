0cdc528bf1e54d05b3e70bf18e1d85a444e9a080
Gammasoft <gammasoft71@gmail.com>
2022-12-19 22:26:07 +0100

    move menu_item_ref in separate include file

---
+++ src/xtd.forms/include/xtd/forms/menu_item_ref.h
@@ -0,0 +1,30 @@
+/// @file
+/// @brief Contains xtd::forms::menu_item_ref alias.
+/// @copyright Copyright (c) 2022 Gammasoft. All rights reserved.
+#pragma once
+
+/// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
+namespace xtd {
+  /// @brief The xtd::forms namespace contains classes for creating Windows-based applications that take full advantage of the rich user interface features available in the Microsoft Windows operating system, Apple macOS and Linux like Ubuntu operating system.
+  namespace forms {
+    /// @cond
+    class menu_item;
+    /// @endcond
+    
+    /// @brief Represents a menu item reference.
+    /// @par Namespace
+    /// xtd::forms
+    /// @par Library
+    /// xtd.forms
+    /// @ingroup xtd_forms
+    using menu_item_ref = std::reference_wrapper<menu_item>;
+    
+    /// @brief Represents a menu item reference.
+    /// @par Namespace
+    /// xtd::forms
+    /// @par Library
+    /// xtd.forms
+    /// @ingroup xtd_forms
+    using const_menu_item_ref = std::reference_wrapper<const menu_item>;
+  }
+}
