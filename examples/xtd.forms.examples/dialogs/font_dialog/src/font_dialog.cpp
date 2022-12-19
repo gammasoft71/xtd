f32abdd3eafc669afa1497cdbffff2011b7968b0
Gammasoft <gammasoft71@gmail.com>
2022-12-19 13:22:57 +0100

    add auto_scroll and auto_size

--- examples/xtd.forms.examples/dialogs/font_dialog/src/font_dialog.cpp
+++ examples/xtd.forms.examples/dialogs/font_dialog/src/font_dialog.cpp
@@ -9,6 +9,7 @@ public:
     text("Font dialog example");
     client_size({400, 400});
     controls().push_back_range({button1, label1});
+    auto_scroll(true);
     
     button1.location({10, 10});
     button1.text("Font...");
@@ -23,7 +24,7 @@ public:
     };
     
     label1.location({10, 50});
-    label1.size({380, 340});
+    label1.auto_size(true);
     label1.anchor(anchor_styles::left | anchor_styles::top | anchor_styles::right | anchor_styles::bottom);
     label1.text(u8"The quick brown fox jumps over the lazy dog.\n"
       u8"THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.\n"
