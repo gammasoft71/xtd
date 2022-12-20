f153bd864d096a0c264dbd73441e8b7317fd536e
Gammasoft <gammasoft71@gmail.com>
2022-12-20 19:47:36 +0100

    Optimization

--- src/xtd.drawing.native.wxwidgets/include/xtd/drawing/native/wxDrawString.h
+++ src/xtd.drawing.native.wxwidgets/include/xtd/drawing/native/wxDrawString.h
@@ -85,7 +85,9 @@ namespace xtd {
             height += static_cast<float>(lineHeight);
             
             // Workaround : with wxWidgets version <= 3.1.5 width size text is too small on macOS and linux.
-            if (wxPlatformInfo::Get().GetOperatingSystemFamilyName() != "Windows" && font.GetStyle() > wxFontStyle::wxFONTSTYLE_NORMAL) width += std::ceil(dc.GetFontMetrics().averageWidth / 2.3f);
+#if !defined (__WXMSW__)
+            if (font.GetStyle() > wxFontStyle::wxFONTSTYLE_NORMAL) width += std::ceil(dc.GetFontMetrics().averageWidth / 2.3f);
+#endif
           }
         }
         
