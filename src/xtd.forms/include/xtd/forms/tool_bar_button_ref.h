ee877c96f2aaade45e9c8c6dbea7d78cec2892c7
Gammasoft <gammasoft71@gmail.com>
2022-12-20 14:23:26 +0100

    move tool_bar_button_ref in tool_bar_button_ref.h include

---
+++ src/xtd.forms/include/xtd/forms/tool_bar_button_ref.h
@@ -0,0 +1,23 @@
+/// @file
+/// @brief Contains xtd::forms::tool_bar_button_ref alias.
+/// @copyright Copyright (c) 2022 Gammasoft. All rights reserved.
+#pragma once
+#include <functional>
+
+/// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
+namespace xtd {
+  /// @brief The xtd::forms namespace contains classes for creating Windows-based applications that take full advantage of the rich user interface features available in the Microsoft Windows operating system, Apple macOS and Linux like Ubuntu operating system.
+  namespace forms {
+    /// @cond
+    class tool_bar_button;
+    /// @endcond
+    
+    /// @brief Represents a xtd::forms::tool_bar_item reference.
+    /// @par Namespace
+    /// xtd::forms
+    /// @par Library
+    /// xtd.forms
+    /// @ingroup xtd_forms
+    using tool_bar_button_ref = std::reference_wrapper<tool_bar_button>;
+  }
+}
