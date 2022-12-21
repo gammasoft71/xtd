75bad10a42a73abca26127fb460213fe1633f7e3
Gammasoft <gammasoft71@gmail.com>
2022-12-21 09:59:13 +0100

    Fix build on Windows

--- src/xtd.tunit/src/xtd/tunit/__demangle_win32.cpp
+++ src/xtd.tunit/src/xtd/tunit/__demangle_win32.cpp
@@ -2,7 +2,10 @@
 
 #include "../../../include/xtd/tunit/__demangle.h"
 
-std::string __tunit_demangle(const std::string& name) {
+using namespace std;
+using namespace std::literals;
+
+string __tunit_demangle(const string& name) {
   string result = name;
   for (auto& item : {"enum "s, "class "s, "union "s, "struct "s}) {
     size_t index = 0;
