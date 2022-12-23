f5849fdc9412a51e82357d190907587205dd9600
Gammasoft <gammasoft71@gmail.com>
2022-12-23 14:13:39 +0100

    Add example and documentation

---
+++ examples/xtd.core.examples/enumerations/enum_class_flags_introspection/README.md
@@ -0,0 +1,28 @@
+# enum_class_flags_introspection
+
+* Shows how the introspection works with [enum_class_](https://codedocs.xyz/gammasoft71/xtd/group__keywords.html#gade4794e2743558ee1eb5bbad10c7f6d4) helper.
+
+## Sources
+
+[src/enum_class_flags_introspection.cpp](src/enum_class_flags_introspection.cpp)
+
+[CMakeLists.txt](CMakeLists.txt)
+
+# Build and run
+
+Open "Command Prompt" or "Terminal". Navigate to the folder that contains the project and type the following:
+
+```cmake
+xtdc run
+```
+
+# Output
+
+```
+name = bold, italic
+value = 3
+as<int> = 3
+values = [0, 1, 2, 4, 8]
+names = [normal, bold, italic, underline, strikeout]
+entries = [(0, normal), (1, bold), (2, italic), (4, underline), (8, strikeout)]
+```
---
+++ examples/xtd.core.examples/enumerations/enum_class_flags_introspection/src/enum_class_flags_introspection.cpp
@@ -0,0 +1,37 @@
+#include <xtd/xtd>
+
+using namespace xtd;
+
+constexpr int NORMAL = 0b0;
+constexpr int BOLD = 0b1;
+constexpr int ITALIC = 0b10;
+constexpr int UNDERLINE = 0b100;
+constexpr int STRIKEOUT = 0b1000;
+
+enum_class_(, text_attribute,
+  normal = NORMAL,
+  bold = BOLD,
+  italic = ITALIC,
+  underline = UNDERLINE,
+  strikeout = STRIKEOUT
+);
+
+flags_attribute_(, text_attribute);
+
+int main() {
+  console::write_line("name = {}", text_attribute::bold | text_attribute::italic);
+  console::write_line("value = {}", enum_object(text_attribute::bold | text_attribute::italic).to_int32());
+  console::write_line("as<int> = {}", as<int>(text_attribute::bold | text_attribute::italic));
+  console::write_line("values = {}", enum_object<>::get_values_as_int32<text_attribute>());
+  console::write_line("names = {}", enum_object<>::get_names<text_attribute>());
+  console::write_line("entries = {}", enum_object<>::get_entries_as_int32<text_attribute>());
+}
+
+// This code produces the following output :
+//
+// name = bold, italic
+// value = 3
+// as<int> = 3
+// values = [0, 1, 2, 4, 8]
+// names = [normal, bold, italic, underline, strikeout]
+// entries = [(0, normal), (1, bold), (2, italic), (4, underline), (8, strikeout)]
