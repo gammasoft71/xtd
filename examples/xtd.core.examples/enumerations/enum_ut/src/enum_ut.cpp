fe6cd06cfad193f6d74344c7c052e8126744f743
Gammasoft <gammasoft71@gmail.com>
2022-12-23 14:13:39 +0100

    Add example and documentation

--- examples/xtd.core.examples/enumerations/enum_ut/src/enum_ut.cpp
+++ examples/xtd.core.examples/enumerations/enum_ut/src/enum_ut.cpp
@@ -11,8 +11,8 @@ enum_ut_(, enum_test, byte,
 
 int main() {
   console::write_line("name = {}", enum_test::value_four);
-  console::write_line("value = {}", enum_object(enum_test::value_four).to_int32());
-  console::write_line("as<int> = {}", as<int>(enum_test::value_four));
+  console::write_line("value = {}", enum_object(enum_test::value_four).to_byte());
+  console::write_line("as<byte> = {}", as<byte>(enum_test::value_four));
   console::write_line("values = {}", enum_object<>::get_values_as_int32<enum_test>());
   console::write_line("names = {}", enum_object<>::get_names<enum_test>());
   console::write_line("entries = {}", enum_object<>::get_entries_as_int32<enum_test>());
@@ -22,7 +22,7 @@ int main() {
 //
 // name = value_four
 // value = 3
-// as<int> = 3
+// as<byte> = 3
 // values = [0, 1, 2, 3]
 // names = [value_one, value_two, value_three, value_four]
 // entries = [(0, value_one), (1, value_two), (2, value_three), (3, value_four)]
