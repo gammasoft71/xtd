ac343c074ff4bfc61ea2da799c830ff26a8da74a
Gammasoft <gammasoft71@gmail.com>
2022-12-24 16:07:42 +0100

    Add parse_enum example

--- examples/xtd.core.examples/README.md
+++ examples/xtd.core.examples/README.md
@@ -228,6 +228,7 @@
 ## [Parse](parse/README.md)
 
 * [parse_boolean](parse/parse_boolean/README.md) shows how to use [xtd::parse](https://codedocs.xyz/gammasoft71/xtd/group__xtd__core.html#gaf38b51b1a3c788f678bb6e89f7694ee4) method with boolean value.
+* [parse_enum](parse/parse_enum/README.md) shows how to use [xtd::enum_object<>::parse](https://codedocs.xyz/gammasoft71/xtd/classxtd_1_1enum__object_3_01std_1_1nullptr__t_01_4.html#a2ad0cf97016a5826ea31f86175c01991) method.
 * [parse_floating_point](parse/parse_floating_point/README.md) shows how to use [xtd::parse](https://codedocs.xyz/gammasoft71/xtd/group__xtd__core.html#gaf38b51b1a3c788f678bb6e89f7694ee4) method with floating point (float, double and decimal).
 * [parse_numeric](parse/parse_numeric/README.md) shows how to use [xtd::parse](https://codedocs.xyz/gammasoft71/xtd/group__xtd__core.html#gaf38b51b1a3c788f678bb6e89f7694ee4) method with numeric (int8, int16, int32, int64, uint8, uint16, uint32 and uint64).
 
---
+++ examples/xtd.core.examples/parse/parse_enum/src/parse_enum.cpp
@@ -0,0 +1,41 @@
+#include <xtd/xtd>
+
+using namespace std;
+using namespace xtd;
+
+enum week_day {
+  monday,
+  tuesday,
+  wednesday,
+  thursday,
+  friday,
+  saturday,
+  sunday
+};
+
+// Only this operator is needed for week_day enum to be recognized by ustring::format().
+template<> struct xtd::enum_register<week_day> {
+  explicit operator auto() const noexcept {return xtd::enum_collection<week_day> {{week_day::monday, "monday"}, {week_day::tuesday, "tuesday"}, {week_day::wednesday, "wednesday"}, {week_day::thursday, "thursday"}, {week_day::friday, "friday"}, {week_day::saturday, "saturday"}, {week_day::sunday, "sunday"}};}
+};
+
+int main() {
+  week_day wd1 = enum_object<>::parse<week_day>("wednesday");
+  console::write_line(wd1);
+  week_day wd2 = enum_object<>::parse<week_day>("WEDNESDAY", true);
+  console::write_line(wd2);
+  week_day wd3 = enum_object<>::parse<week_day>("2");
+  console::write_line(wd3);
+  try {
+    week_day wd4 = enum_object<>::parse<week_day>("january");
+    console::write_line(wd4);
+  } catch(const exception& e) {
+    console::write_line("Exception : {}", e.what());
+  }
+}
+
+// This code produces the following output :
+//
+// wednesday
+// wednesday
+// wednesday
+// Exception : invalid character found
