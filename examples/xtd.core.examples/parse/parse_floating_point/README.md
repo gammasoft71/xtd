d16321a1f13d7b3798bd30cc17098b5c6d11ae64
Gammasoft <gammasoft71@gmail.com>
2022-12-24 15:16:40 +0100

    add parse_floating_point example

---
+++ examples/xtd.core.examples/parse/parse_floating_point/README.md
@@ -0,0 +1,30 @@
+# parse_floating_point
+
+Shows how to use format [xtd::parse](https://codedocs.xyz/gammasoft71/xtd/group__xtd__core.html#gaf38b51b1a3c788f678bb6e89f7694ee4) method with numeric values (float, double and decimal).
+
+## Sources
+
+[src/parse_floating_point.cpp](src/parse_floating_point.cpp)
+
+[CMakeLists.txt](CMakeLists.txt)
+
+# Build and run
+
+Open "Command Prompt" or "Terminal". Navigate to the folder that contains the project and type the following:
+
+```cmake
+xtdc run
+```
+
+# Output (with colors)
+
+```
+12.345
+12.345
+12.345
+inf
+-inf
+nan
+Exception : invalid character found
+```
+
---
+++ examples/xtd.core.examples/parse/parse_floating_point/src/parse_floating_point.cpp
@@ -0,0 +1,36 @@
+#include <xtd/xtd>
+
+using namespace std;
+using namespace xtd;
+
+int main() {
+  locale::global(locale("en_US.UTF-8")); // change to us for currency
+  double d1 = xtd::parse<double>("12.345");
+  console::write_line(d1);
+  double d2 = xtd::parse<double>("$12.345", number_styles::currency);
+  console::write_line(d2);
+  double d3 = xtd::parse<double>("1.234500e+01", number_styles::number | number_styles::allow_exponent);
+  console::write_line(d3);
+  double d4 = xtd::parse<double>("inf");
+  console::write_line(d4);
+  double d5 = xtd::parse<double>("-inf");
+  console::write_line(d5);
+  double d6 = xtd::parse<double>("nan");
+  console::write_line(d6);
+  try {
+    double d7 = xtd::parse<int>("Not floating point", number_styles::any);
+    console::write_line(d7);
+  } catch(const exception& e) {
+    console::write_line("Exception : {}", e.what());
+  }
+}
+
+// This code produces the following output :
+//
+// 2.345
+// 12.345
+// 12.345
+// inf
+// -inf
+// nan
+// Exception : invalid character found
