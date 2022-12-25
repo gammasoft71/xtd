2971cb1dd7cf6c5878b946ea44679793aa1e383c
Gammasoft <gammasoft71@gmail.com>
2022-12-25 12:13:12 +0100

    add parse_enum_struct example

---
+++ examples/xtd.core.examples/parse/parse_enum_struct/src/parse_enum_struct.cpp
@@ -0,0 +1,36 @@
+#include <xtd/xtd>
+
+using namespace std;
+using namespace xtd;
+
+enum_struct_(, week_day,
+  monday,
+  tuesday,
+  wednesday,
+  thursday,
+  friday,
+  saturday,
+  sunday
+);
+
+int main() {
+  week_day wd1 = parse<week_day>("wednesday");
+  console::write_line(wd1);
+  week_day wd2 = enum_object<>::parse<week_day>("WEDNESDAY", true);
+  console::write_line(wd2);
+  week_day wd3 = parse<week_day>("2");
+  console::write_line(wd3);
+  try {
+    week_day wd4 = parse<week_day>("january");
+    console::write_line(wd4);
+  } catch(const exception& e) {
+    console::write_line("Exception : {}", e.what());
+  }
+}
+
+// This code produces the following output :
+//
+// wednesday
+// wednesday
+// wednesday
+// Exception : invalid character found
