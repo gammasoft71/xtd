2cb5b250b1b3e52cd3d56956e4b0a566814f41cd
Gammasoft <gammasoft71@gmail.com>
2022-12-26 16:20:24 +0100

    add get_color_names

--- src/xtd.drawing/src/xtd/drawing/basic_colors.cpp
+++ src/xtd.drawing/src/xtd/drawing/basic_colors.cpp
@@ -1,6 +1,7 @@
 #include "../../../include/xtd/drawing/basic_colors.h"
 
 using namespace std;
+using namespace xtd;
 using namespace xtd::drawing;
 
 color basic_colors::aqua() {
@@ -67,6 +68,12 @@ color basic_colors::yellow() {
   return color::from_known_color(known_color::yellow);
 }
 
-vector<color> basic_colors::get_colors() {
-  return {basic_colors::white(), basic_colors::silver(), basic_colors::gray(), basic_colors::black(), basic_colors::red(), basic_colors::maroon(), basic_colors::yellow(), basic_colors::olive(), basic_colors::lime(), basic_colors::green(), basic_colors::aqua(), basic_colors::teal(), basic_colors::blue(), basic_colors::navy(), basic_colors::fuchsia(), basic_colors::purple()};
+const vector<color>& basic_colors::get_colors() {
+  static vector<color> colors {basic_colors::white(), basic_colors::silver(), basic_colors::gray(), basic_colors::black(), basic_colors::red(), basic_colors::maroon(), basic_colors::yellow(), basic_colors::olive(), basic_colors::lime(), basic_colors::green(), basic_colors::aqua(), basic_colors::teal(), basic_colors::blue(), basic_colors::navy(), basic_colors::fuchsia(), basic_colors::purple()};
+  return colors;
+}
+
+const vector<ustring>& basic_colors::get_color_names() {
+  static vector<ustring> color_names {basic_colors::white().name(), basic_colors::silver().name(), basic_colors::gray().name(), basic_colors::black().name(), basic_colors::red().name(), basic_colors::maroon().name(), basic_colors::yellow().name(), basic_colors::olive().name(), basic_colors::lime().name(), basic_colors::green().name(), basic_colors::aqua().name(), basic_colors::teal().name(), basic_colors::blue().name(), basic_colors::navy().name(), basic_colors::fuchsia().name(), basic_colors::purple().name()};
+  return color_names;
 }
