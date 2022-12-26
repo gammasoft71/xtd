18940d6887a647dcf4d08ba7a6c9146d69a631ae
Gammasoft <gammasoft71@gmail.com>
2022-12-26 16:43:11 +0100

    add get_color_names method

--- src/xtd.drawing/src/xtd/drawing/orange_colors.cpp
+++ src/xtd.drawing/src/xtd/drawing/orange_colors.cpp
@@ -1,6 +1,7 @@
 #include "../../../include/xtd/drawing/orange_colors.h"
 
 using namespace std;
+using namespace xtd;
 using namespace xtd::drawing;
 
 color orange_colors::coral() {
@@ -23,6 +24,12 @@ color orange_colors::orange() {
   return color::from_known_color(known_color::orange);
 }
 
-vector<color> orange_colors::get_colors() {
-  return {orange_colors::orange_red(), orange_colors::tomato(), orange_colors::dark_orange(), orange_colors::coral(), orange_colors::orange()};
+const vector<color>& orange_colors::get_colors() {
+  static vector colors {orange_colors::orange_red(), orange_colors::tomato(), orange_colors::dark_orange(), orange_colors::coral(), orange_colors::orange()};
+  return colors;
+}
+
+const vector<ustring>& orange_colors::get_color_names() {
+  static vector color_names {orange_colors::orange_red().name(), orange_colors::tomato().name(), orange_colors::dark_orange().name(), orange_colors::coral().name(), orange_colors::orange().name()};
+  return color_names;
 }
