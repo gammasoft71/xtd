e44cd3d17f826783d02bc6b70653edc8596f957b
Gammasoft <gammasoft71@gmail.com>
2022-12-26 16:50:04 +0100

    add get_color_names method

--- src/xtd.drawing/src/xtd/drawing/red_colors.cpp
+++ src/xtd.drawing/src/xtd/drawing/red_colors.cpp
@@ -1,6 +1,7 @@
 #include "../../../include/xtd/drawing/red_colors.h"
 
 using namespace std;
+using namespace xtd;
 using namespace xtd::drawing;
 
 color red_colors::crimson() {
@@ -39,6 +40,12 @@ color red_colors::salmon() {
   return color::from_known_color(known_color::salmon);
 }
 
-vector<color> red_colors::get_colors() {
-  return {red_colors::dark_red(), red_colors::red(), red_colors::firebrick(), red_colors::crimson(), red_colors::indian_red(), red_colors::light_coral(), red_colors::salmon(), red_colors::dark_salmon(), red_colors::light_salmon()};
+const vector<color>& red_colors::get_colors() {
+  static vector colors {red_colors::dark_red(), red_colors::red(), red_colors::firebrick(), red_colors::crimson(), red_colors::indian_red(), red_colors::light_coral(), red_colors::salmon(), red_colors::dark_salmon(), red_colors::light_salmon()};
+  return colors;
+}
+
+const vector<ustring>& red_colors::get_color_names() {
+  static vector color_names {red_colors::dark_red().name(), red_colors::red().name(), red_colors::firebrick().name(), red_colors::crimson().name(), red_colors::indian_red().name(), red_colors::light_coral().name(), red_colors::salmon().name(), red_colors::dark_salmon().name(), red_colors::light_salmon().name()};
+  return color_names;
 }
