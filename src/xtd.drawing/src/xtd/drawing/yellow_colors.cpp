8e73f9db308f754d75c096c4be71aa2927ed034e
Gammasoft <gammasoft71@gmail.com>
2022-12-26 17:04:10 +0100

    add get_color_names method

--- src/xtd.drawing/src/xtd/drawing/yellow_colors.cpp
+++ src/xtd.drawing/src/xtd/drawing/yellow_colors.cpp
@@ -1,6 +1,7 @@
 #include "../../../include/xtd/drawing/yellow_colors.h"
 
 using namespace std;
+using namespace xtd;
 using namespace xtd::drawing;
 
 color yellow_colors::dark_khaki() {
@@ -48,6 +49,11 @@ color yellow_colors::yellow() {
 }
 
 const vector<color>& yellow_colors::get_colors() {
-  static vector<color> colors {yellow_colors::dark_khaki(), yellow_colors::gold(), yellow_colors::khaki(), yellow_colors::peach_puff(), yellow_colors::yellow(), yellow_colors::pale_goldenrod(), yellow_colors::moccasin(), yellow_colors::papaya_whip(), yellow_colors::light_goldenrod_yellow(), yellow_colors::lemon_chiffon(), yellow_colors::light_yellow()};
+  static vector colors {yellow_colors::dark_khaki(), yellow_colors::gold(), yellow_colors::khaki(), yellow_colors::peach_puff(), yellow_colors::yellow(), yellow_colors::pale_goldenrod(), yellow_colors::moccasin(), yellow_colors::papaya_whip(), yellow_colors::light_goldenrod_yellow(), yellow_colors::lemon_chiffon(), yellow_colors::light_yellow()};
   return colors;
+}
+
+const vector<ustring>& yellow_colors::get_color_names() {
+  static vector color_names {yellow_colors::dark_khaki().name(), yellow_colors::gold().name(), yellow_colors::khaki().name(), yellow_colors::peach_puff().name(), yellow_colors::yellow().name(), yellow_colors::pale_goldenrod().name(), yellow_colors::moccasin().name(), yellow_colors::papaya_whip().name(), yellow_colors::light_goldenrod_yellow().name(), yellow_colors::lemon_chiffon().name(), yellow_colors::light_yellow().name()};
+  return color_names;
 }
