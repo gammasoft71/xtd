29d7a8133aef8d4999b51394a6da771b8add0900
Gammasoft <gammasoft71@gmail.com>
2022-12-26 17:02:34 +0100

    Update white_colors.h

--- src/xtd.drawing/include/xtd/drawing/white_colors.h
+++ src/xtd.drawing/include/xtd/drawing/white_colors.h
@@ -102,7 +102,7 @@ namespace xtd {
       /// @brief Gets an array of colors.
       static const std::vector<xtd::drawing::color>& get_colors();
       
-      /// @brief Gets an array of colors.
+      /// @brief Gets an array of color names.
       static const std::vector<xtd::ustring>& get_color_names();
       /// @}
     };
