524a227aca0dff32229250223a7591e2de80f1f4
Gammasoft <gammasoft71@gmail.com>
2022-12-26 12:21:04 +0100

    Add get_colors method

--- src/xtd.drawing/include/xtd/drawing/system_colors.h
+++ src/xtd.drawing/include/xtd/drawing/system_colors.h
@@ -42,15 +42,15 @@ namespace xtd {
       /// @brief Gets a xtd::drawing::color structure that is the color of the background of the active window's title bar.
       /// @return A xtd::drawing::color that is the color of the active window's title bar.
       static xtd::drawing::color active_caption();
-      
-      /// @brief Gets a xtd::drawing::color structure that is the color of a active text.
-      /// @return A xtd::drawing::color that is the color of a active text.
-      /// @remark The active text color is the text color of an html link text when mouse down.
-      static xtd::drawing::color active_text();
 
       /// @brief Gets a xtd::drawing::color structure that is the color of the text in the active window's title bar.
       /// @return A xtd::drawing::color that is the color of the text in the active window's title bar.
       static xtd::drawing::color active_caption_text();
+
+      /// @brief Gets a xtd::drawing::color structure that is the color of a active text.
+      /// @return A xtd::drawing::color that is the color of a active text.
+      /// @remark The active text color is the text color of an html link text when mouse down.
+      static xtd::drawing::color active_text();
       
       /// @brief Gets a xtd::drawing::color structure that is the color of the application workspace.
       /// @return A xtd::drawing::color that is the color of the application workspace.
@@ -200,6 +200,13 @@ namespace xtd {
       /// @brief Gets a xtd::drawing::color structure that is the color of the text in the client area of a window.
       /// @return A xtd::drawing::color that is the color of the text in the client area of a window.
       static xtd::drawing::color window_text();
+      /// @}
+
+      /// @name Public methods
+      
+      /// @{
+      /// @brief Gets an array of system colors.
+      static std::vector<xtd::drawing::color> get_colors();
       /// @}
     };
   }
