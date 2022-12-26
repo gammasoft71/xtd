5798d6564d3dc6bbd81659e78dd2009f9f3f093c
Gammasoft <gammasoft71@gmail.com>
2022-12-26 15:35:40 +0100

    add methods

--- src/xtd.drawing/src/xtd/drawing/system_fonts.cpp
+++ src/xtd.drawing/src/xtd/drawing/system_fonts.cpp
@@ -43,6 +43,12 @@ font system_fonts::tool_font() {
   return font(native::system_fonts::tool_font());
 }
 
-vector<xtd::drawing::font> system_fonts::get_system_fonts() {
-  return {system_fonts::caption_font(), system_fonts::default_font(), system_fonts::dialog_font(), system_fonts::icon_title_font(), system_fonts::menu_font(), system_fonts::message_box_font(), system_fonts::small_caption_font(), system_fonts::status_font(), system_fonts::tool_font(),};
+const vector<font>& system_fonts::get_fonts() {
+  static vector<font> system_fonts {system_fonts::caption_font(), system_fonts::default_font(), system_fonts::dialog_font(), system_fonts::icon_title_font(), system_fonts::menu_font(), system_fonts::message_box_font(), system_fonts::small_caption_font(), system_fonts::status_font(), system_fonts::tool_font(),};
+  return system_fonts;
+}
+
+const vector<ustring>& system_fonts::get_font_names() {
+  static vector<ustring> font_names {system_fonts::caption_font().name(), system_fonts::default_font().name(), system_fonts::dialog_font().name(), system_fonts::icon_title_font().name(), system_fonts::menu_font().name(), system_fonts::message_box_font().name(), system_fonts::small_caption_font().name(), system_fonts::status_font().name(), system_fonts::tool_font().name(),};
+  return font_names;
 }
