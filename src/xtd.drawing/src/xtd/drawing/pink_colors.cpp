721dcf762758169c9ba53ed1347ad1ec86f49881
Gammasoft <gammasoft71@gmail.com>
2022-12-26 16:45:34 +0100

    add get_color_names method

--- src/xtd.drawing/src/xtd/drawing/pink_colors.cpp
+++ src/xtd.drawing/src/xtd/drawing/pink_colors.cpp
@@ -1,6 +1,7 @@
 #include "../../../include/xtd/drawing/pink_colors.h"
 
 using namespace std;
+using namespace xtd;
 using namespace xtd::drawing;
 
 color pink_colors::deep_pink() {
@@ -27,6 +28,12 @@ color pink_colors::pink() {
   return color::from_known_color(known_color::pink);
 }
 
-vector<color> pink_colors::get_colors() {
-  return {pink_colors::medium_violet_red(), pink_colors::deep_pink(), pink_colors::pale_violet_red(), pink_colors::hot_pink(), pink_colors::light_pink(), pink_colors::pink()};
+const vector<color>& pink_colors::get_colors() {
+  static vector colors {pink_colors::medium_violet_red(), pink_colors::deep_pink(), pink_colors::pale_violet_red(), pink_colors::hot_pink(), pink_colors::light_pink(), pink_colors::pink()};
+  return colors;
+}
+
+const vector<ustring>& pink_colors::get_color_names() {
+  static vector color_names {pink_colors::medium_violet_red().name(), pink_colors::deep_pink().name(), pink_colors::pale_violet_red().name(), pink_colors::hot_pink().name(), pink_colors::light_pink().name(), pink_colors::pink().name()};
+  return color_names;
 }
