6c3746a25a3fd534b159d51686e5972d981ac636
Gammasoft <gammasoft71@gmail.com>
2022-12-26 17:28:45 +0100

    review known_color range

--- src/xtd.drawing/include/xtd/drawing/known_color.h
+++ src/xtd.drawing/include/xtd/drawing/known_color.h
@@ -16,60 +16,11 @@ namespace xtd {
     /// @ingroup xtd_drawing
     /// @see xtd::drawing::color
     enum class known_color {
-      /// @brief The system-defined color of the active window's border.
-      active_border = 1,
-      /// @brief The system-defined color of the background of the active window's title bar.
-      active_caption,
-      /// @brief The system-defined color of the text in the active window's title bar.
-      active_caption_text,
-      /// @brief The system-defined color of the application workspace. The application workspace is the area in a multiple-document view that is not being occupied by documents.
-      app_workspace,
-      /// @brief The system-defined face color of a 3-D element.
-      control,
-      /// @brief The system-defined shadow color of a 3-D element. The shadow color is applied to parts of a 3-D element that face away from the light source.
-      control_dark,
-      /// @brief The system-defined color that is the dark shadow color of a 3-D element. The dark shadow color is applied to the parts of a 3-D element that are the darkest color.
-      control_dark_dark,
-      /// @brief The system-defined color that is the light color of a 3-D element. The light color is applied to parts of a 3-D element that face the light source.
-      control_light,
-      /// @brief The system-defined highlight color of a 3-D element. The highlight color is applied to the parts of a 3-D element that are the lightest color.
-      control_light_light,
-      /// @brief The system-defined color of text in a 3-D element.
-      control_text,
-      /// @brief The system-defined color of the desktop.
-      desktop,
-      /// @brief The system-defined color of dimmed text. Items in a list that are disabled are displayed in dimmed text.
-      gray_text,
-      /// @brief The system-defined color of the background of selected items. This includes selected menu items as well as selected text.
-      highlight,
-      /// @brief The system-defined color of the text of selected items.
-      highlight_text,
-      /// @brief The system-defined color used to designate a hot-tracked item. Single-clicking a hot-tracked item executes the item.
-      hot_track,
-      /// @brief The system-defined color of an inactive window's border.
-      inactive_border,
-      /// @brief The system-defined color of the background of an inactive window's title bar.
-      inactive_caption,
-      /// @brief The system-defined color of the text in an inactive window's title bar.
-      inactive_caption_text,
-      /// @brief The system-defined color of the background of a ToolTip.
-      info,
-      /// @brief The system-defined color of the text of a ToolTip.
-      info_text,
-      /// @brief The system-defined color of a menu's background.
-      menu,
-      /// @brief The system-defined color of a menu's text.
-      menu_text,
-      /// @brief The system-defined color of the background of a scroll bar.
-      scroll_bar,
-      /// @brief The system-defined color of the background in the client area of a window.
-      window,
-      /// @brief The system-defined color of a window frame.
-      window_frame,
-      /// @brief The system-defined color of the text in the client area of a window.
-      window_text,
+      // 0 - reserved for "not a known color"
+
+      // "Web" colors.
       /// @brief A system-defined color.
-      transparent,
+      transparent = 1,
       /// @brief A system-defined color.
       alice_blue,
       /// @brief A system-defined color.
@@ -352,34 +303,88 @@ namespace xtd {
       yellow,
       /// @brief A system-defined color.
       yellow_green,
+      
+      // "System" colors.
+      /// @brief The system-defined color of the accent color (macos specific. On other platform is same as menu_highlight).
+      accent = 0xFF01, // see xtd::drawing::color::start_know_system_colors_range.
+      /// @brief The system-defined color of the accent text color (macos specific. On other platform is same as highlight_text).
+      accent_text,
+      /// @brief The system-defined color of the active window's border.
+      active_border,
+      /// @brief The system-defined color of the background of the active window's title bar.
+      active_caption,
+      /// @brief The system-defined color of the text in the active window's title bar.
+      active_caption_text,
+      /// @brief The active text color is the text color of an html link text when mouse down.
+      active_text,
+      /// @brief The system-defined color of the application workspace. The application workspace is the area in a multiple-document view that is not being occupied by documents.
+      app_workspace,
       /// @brief The system-defined face color of a 3-D element.
       button_face,
       /// @brief The system-defined color that is the highlight color of a 3-D element. This color is applied to parts of a 3-D element that face the light source.
       button_highlight,
       /// @brief The system-defined color that is the shadow color of a 3-D element. This color is applied to parts of a 3-D element that face away from the light source.
       button_shadow,
+      /// @brief The system-defined face color of a 3-D element.
+      control,
+      /// @brief The system-defined shadow color of a 3-D element. The shadow color is applied to parts of a 3-D element that face away from the light source.
+      control_dark,
+      /// @brief The system-defined color that is the dark shadow color of a 3-D element. The dark shadow color is applied to the parts of a 3-D element that are the darkest color.
+      control_dark_dark,
+      /// @brief The system-defined color that is the light color of a 3-D element. The light color is applied to parts of a 3-D element that face the light source.
+      control_light,
+      /// @brief The system-defined highlight color of a 3-D element. The highlight color is applied to the parts of a 3-D element that are the lightest color.
+      control_light_light,
+      /// @brief The system-defined color of text in a 3-D element.
+      control_text,
+      /// @brief The system-defined color of the desktop.
+      desktop,
       /// @brief The system-defined color of the lightest color in the color gradient of an active window's title bar.
       gradient_active_caption,
       /// @brief The system-defined color of the lightest color in the color gradient of an inactive window's title bar.
       gradient_inactive_caption,
+      /// @brief The system-defined color of dimmed text. Items in a list that are disabled are displayed in dimmed text.
+      gray_text,
+      /// @brief The system-defined color of the background of selected items. This includes selected menu items as well as selected text.
+      highlight,
+      /// @brief The system-defined color of the text of selected items.
+      highlight_text,
+      /// @brief The system-defined color used to designate a hot-tracked item. Single-clicking a hot-tracked item executes the item.
+      hot_track,
+      /// @brief The system-defined color of an inactive window's border.
+      inactive_border,
+      /// @brief The system-defined color of the background of an inactive window's title bar.
+      inactive_caption,
+      /// @brief The system-defined color of the text in an inactive window's title bar.
+      inactive_caption_text,
+      /// @brief The system-defined color of the background of a ToolTip.
+      info,
+      /// @brief The system-defined color of the text of a ToolTip.
+      info_text,
+      /// @brief The link text color is the text color of an html link text.
+      link_text,
+      /// @brief The system-defined color of a menu's background.
+      menu,
       /// @brief The system-defined color of the background of a menu bar.
       menu_bar,
       /// @brief The system-defined color used to highlight menu items when the menu appears as a flat menu.
       menu_highlight,
-      /// @brief The system-defined color of the accent color (macos specific. On other platform is same as menu_highlight).
-      accent,
-      /// @brief The system-defined color of the accent text color (macos specific. On other platform is same as highlight_text).
-      accent_text,
+      /// @brief The system-defined color of a menu's text.
+      menu_text,
+      /// @brief The system-defined color of the background of a scroll bar.
+      scroll_bar,
       /// @brief The system-defined color of the accent color (macos specific. On other platform is same as window).
       text_box,
       /// @brief The system-defined color of the accent text color (macos specific. On other platform is same as window_text).
       text_box_text,
-      /// @brief The active text color is the text color of an html link text when mouse down.
-      active_text,
-      /// @brief The link text color is the text color of an html link text.
-      link_text,
       /// @brief The visited text color is the text color of an html visited link text.
       visited_text,
+      /// @brief The system-defined color of the background in the client area of a window.
+      window,
+      /// @brief The system-defined color of a window frame.
+      window_frame,
+      /// @brief The system-defined color of the text in the client area of a window.
+      window_text,
     };
   }
 }
