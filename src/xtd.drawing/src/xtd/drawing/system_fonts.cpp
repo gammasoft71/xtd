4d6c46f1777481b3a5b73647821a5a096370ab42
Gammasoft <gammasoft71@gmail.com>
2022-12-26 12:30:39 +0100

    Add get_fonts method

--- src/xtd.drawing/src/xtd/drawing/system_fonts.cpp
+++ src/xtd.drawing/src/xtd/drawing/system_fonts.cpp
@@ -3,6 +3,7 @@
 #include <xtd/drawing/native/system_fonts.h>
 #undef __XTD_DRAWING_NATIVE_LIBRARY__
 
+using namespace std;
 using namespace xtd;
 using namespace xtd::drawing;
 
@@ -40,4 +41,8 @@ font system_fonts::status_font() {
 
 font system_fonts::tool_font() {
   return font(native::system_fonts::tool_font());
+}
+
+vector<xtd::drawing::font> system_fonts::get_fonts() {
+  return {system_fonts::caption_font(), system_fonts::default_font(), system_fonts::dialog_font(), system_fonts::icon_title_font(), system_fonts::menu_font(), system_fonts::message_box_font(), system_fonts::small_caption_font(), system_fonts::status_font(), system_fonts::tool_font(),};
 }
