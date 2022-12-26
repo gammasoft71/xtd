ed1f279d9a3a20355a83e8ee8e501e044b1fc6b5
Gammasoft <gammasoft71@gmail.com>
2022-12-26 15:35:01 +0100

    add properties

--- src/xtd.drawing/include/xtd/drawing/system_brushes.h
+++ src/xtd.drawing/include/xtd/drawing/system_brushes.h
@@ -26,6 +26,12 @@ namespace xtd {
       /// @name Properties
       
       /// @{
+      /// @brief Gets a solid_brush that is the color of the accent.
+      static xtd::drawing::solid_brush accent();
+
+      /// @brief Gets a solid_brush that is the color of the accent text.
+      static xtd::drawing::solid_brush accent_text();
+
       /// @brief Gets a solid_brush that is the color of the active border.
       static xtd::drawing::solid_brush active_border();
       
@@ -35,9 +41,21 @@ namespace xtd {
       /// @brief Gets a solid_brush that is the color of the active caption text.
       static xtd::drawing::solid_brush active_caption_text();
       
+      /// @brief Gets a solid_brush that is the color of the active text.
+      static xtd::drawing::solid_brush active_text();
+
       /// @brief Gets a solid_brush that is the color of the app workspace.
       static xtd::drawing::solid_brush app_workspace();
       
+      /// @brief Gets a solid_brush that is the color of the button face.
+      static xtd::drawing::solid_brush button_face();
+      
+      /// @brief Gets a solid_brush that is the color of the button highlight.
+      static xtd::drawing::solid_brush button_highlight();
+      
+      /// @brief Gets a solid_brush that is the color of the button shadow.
+      static xtd::drawing::solid_brush button_shadow();
+
       /// @brief Gets a solid_brush that is the color of the control.
       static xtd::drawing::solid_brush control();
       
@@ -59,6 +77,12 @@ namespace xtd {
       /// @brief Gets a solid_brush that is the color of the desktop.
       static xtd::drawing::solid_brush desktop();
       
+      /// @brief Gets a solid_brush that is the color of the gradient active caption.
+      static xtd::drawing::solid_brush gradient_active_caption();
+
+      /// @brief Gets a solid_brush that is the color of the gradient inactive caption.
+      static xtd::drawing::solid_brush gradient_inactive_caption();
+
       /// @brief Gets a solid_brush that is the color of the gray text.
       static xtd::drawing::solid_brush gray_text();
       
@@ -86,15 +110,33 @@ namespace xtd {
       /// @brief Gets a solid_brush that is the color of the info text.
       static xtd::drawing::solid_brush info_text();
       
+      /// @brief Gets a solid_brush that is the color of the link text.
+      static xtd::drawing::solid_brush link_text();
+
       /// @brief Gets a solid_brush that is the color of the menu.
       static xtd::drawing::solid_brush menu();
       
+      /// @brief Gets a solid_brush that is the color of the menu bar.
+      static xtd::drawing::solid_brush menu_bar();
+      
+      /// @brief Gets a solid_brush that is the color of the menu highlight.
+      static xtd::drawing::solid_brush menu_highlight();
+
       /// @brief Gets a solid_brush that is the color of the menu text.
       static xtd::drawing::solid_brush menu_text();
       
       /// @brief Gets a solid_brush that is the color of the scroll bar.
       static xtd::drawing::solid_brush scroll_bar();
       
+      /// @brief Gets a solid_brush that is the color of the text box.
+      static xtd::drawing::solid_brush text_box();
+      
+      /// @brief Gets a solid_brush that is the color of the text box text.
+      static xtd::drawing::solid_brush text_box_text();
+      
+      /// @brief Gets a solid_brush that is the color of the visited text.
+      static xtd::drawing::solid_brush visited_text();
+
       /// @brief Gets a solid_brush that is the color of the window.
       static xtd::drawing::solid_brush window();
       
@@ -103,30 +145,13 @@ namespace xtd {
       
       /// @brief Gets a solid_brush that is the color of the window text.
       static xtd::drawing::solid_brush window_text();
+      /// @}
+
+      /// @name Public methods
       
-      /// @brief Gets a solid_brush that is the color of the button face.
-      static xtd::drawing::solid_brush button_face();
-      
-      /// @brief Gets a solid_brush that is the color of the button highlight.
-      static xtd::drawing::solid_brush button_highlight();
-      
-      /// @brief Gets a solid_brush that is the color of the button shadow.
-      static xtd::drawing::solid_brush button_shadow();
-      
-      /// @brief Gets a solid_brush that is the color of the gradient active caption.
-      static xtd::drawing::solid_brush gradient_active_caption();
-      
-      /// @brief Gets a solid_brush that is the color of the gradient inactive caption.
-      static xtd::drawing::solid_brush gradient_inactive_caption();
-      
-      /// @brief Gets a solid_brush that is the color of the menu bar.
-      static xtd::drawing::solid_brush menu_bar();
-      
-      /// @brief Gets a solid_brush that is the color of the menu highlight.
-      static xtd::drawing::solid_brush menu_highlight();
-      
-      /// @brief Gets a solid_brush that is the color of the accent.
-      static xtd::drawing::solid_brush accent();
+      /// @{
+      /// @brief Gets an array of system colors.
+      static const std::vector<xtd::drawing::solid_brush>& get_brushes();
       /// @}
     };
   }
--- src/xtd.drawing/src/xtd/drawing/system_brushes.cpp
+++ src/xtd.drawing/src/xtd/drawing/system_brushes.cpp
@@ -1,6 +1,16 @@
 #include "../../../include/xtd/drawing/system_brushes.h"
 
+using namespace std;
+using namespace xtd;
 using namespace xtd::drawing;
+
+solid_brush system_brushes::accent() {
+  return solid_brush(color::from_known_color(known_color::accent));
+}
+
+solid_brush system_brushes::accent_text() {
+  return solid_brush(color::from_known_color(known_color::accent_text));
+}
 
 solid_brush system_brushes::active_border() {
   return solid_brush(color::from_known_color(known_color::active_border));
@@ -14,8 +24,24 @@ solid_brush system_brushes::active_caption_text() {
   return solid_brush(color::from_known_color(known_color::active_caption_text));
 }
 
+solid_brush system_brushes::active_text() {
+  return solid_brush(color::from_known_color(known_color::active_text));
+}
+
 solid_brush system_brushes::app_workspace() {
   return solid_brush(color::from_known_color(known_color::app_workspace));
+}
+
+solid_brush system_brushes::button_face() {
+  return solid_brush(color::from_known_color(known_color::button_face));
+}
+
+solid_brush system_brushes::button_highlight() {
+  return solid_brush(color::from_known_color(known_color::button_highlight));
+}
+
+solid_brush system_brushes::button_shadow() {
+  return solid_brush(color::from_known_color(known_color::button_shadow));
 }
 
 solid_brush system_brushes::control() {
@@ -44,6 +70,14 @@ solid_brush system_brushes::control_text() {
 
 solid_brush system_brushes::desktop() {
   return solid_brush(color::from_known_color(known_color::desktop));
+}
+
+solid_brush system_brushes::gradient_active_caption() {
+  return solid_brush(color::from_known_color(known_color::gradient_active_caption));
+}
+
+solid_brush system_brushes::gradient_inactive_caption() {
+  return solid_brush(color::from_known_color(known_color::gradient_inactive_caption));
 }
 
 solid_brush system_brushes::gray_text() {
@@ -82,8 +116,20 @@ solid_brush system_brushes::info_text() {
   return solid_brush(color::from_known_color(known_color::info_text));
 }
 
+solid_brush system_brushes::link_text() {
+  return solid_brush(color::from_known_color(known_color::link_text));
+}
+
 solid_brush system_brushes::menu() {
   return solid_brush(color::from_known_color(known_color::menu));
+}
+
+solid_brush system_brushes::menu_bar() {
+  return solid_brush(color::from_known_color(known_color::menu_bar));
+}
+
+solid_brush system_brushes::menu_highlight() {
+  return solid_brush(color::from_known_color(known_color::menu_highlight));
 }
 
 solid_brush system_brushes::menu_text() {
@@ -92,6 +138,18 @@ solid_brush system_brushes::menu_text() {
 
 solid_brush system_brushes::scroll_bar() {
   return solid_brush(color::from_known_color(known_color::scroll_bar));
+}
+
+solid_brush system_brushes::text_box() {
+  return solid_brush(color::from_known_color(known_color::text_box));
+}
+
+solid_brush system_brushes::text_box_text() {
+  return solid_brush(color::from_known_color(known_color::text_box_text));
+}
+
+solid_brush system_brushes::visited_text() {
+  return solid_brush(color::from_known_color(known_color::visited_text));
 }
 
 solid_brush system_brushes::window() {
@@ -106,34 +164,7 @@ solid_brush system_brushes::window_text() {
   return solid_brush(color::from_known_color(known_color::window_text));
 }
 
-solid_brush system_brushes::button_face() {
-  return solid_brush(color::from_known_color(known_color::button_face));
-}
-
-solid_brush system_brushes::button_highlight() {
-  return solid_brush(color::from_known_color(known_color::button_highlight));
-}
-
-solid_brush system_brushes::button_shadow() {
-  return solid_brush(color::from_known_color(known_color::button_shadow));
-}
-
-solid_brush system_brushes::gradient_active_caption() {
-  return solid_brush(color::from_known_color(known_color::gradient_active_caption));
-}
-
-solid_brush system_brushes::gradient_inactive_caption() {
-  return solid_brush(color::from_known_color(known_color::gradient_inactive_caption));
-}
-
-solid_brush system_brushes::menu_bar() {
-  return solid_brush(color::from_known_color(known_color::menu_bar));
-}
-
-solid_brush system_brushes::menu_highlight() {
-  return solid_brush(color::from_known_color(known_color::menu_highlight));
-}
-
-solid_brush system_brushes::accent() {
-  return solid_brush(color::from_known_color(known_color::accent));
+const vector<solid_brush>& system_brushes::get_brushes() {
+  static vector<solid_brush> brushes {system_brushes::accent(), system_brushes::accent_text(), system_brushes::active_border(), system_brushes::active_caption(), system_brushes::active_caption_text(), system_brushes::active_text(), system_brushes::accent_text(), system_brushes::app_workspace(), system_brushes::button_face(), system_brushes::button_highlight(), system_brushes::button_shadow(), system_brushes::control(), system_brushes::control_dark(), system_brushes::control_dark_dark(), system_brushes::control_light(), system_brushes::control_light_light(), system_brushes::control_text(), system_brushes::desktop(), system_brushes::gradient_active_caption(), system_brushes::gradient_inactive_caption(), system_brushes::gray_text(), system_brushes::highlight(), system_brushes::highlight_text(), system_brushes::hot_track(), system_brushes::inactive_border(), system_brushes::inactive_caption(), system_brushes::inactive_caption_text(), system_brushes::info(), system_brushes::info_text(), system_brushes::link_text(), system_brushes::menu(), system_brushes::menu_bar(), system_brushes::menu_highlight(), system_brushes::menu_text(), system_brushes::scroll_bar(), system_brushes::text_box(), system_brushes::text_box_text(), system_brushes::visited_text(), system_brushes::window(), system_brushes::window_frame(), system_brushes::window_text(),};
+  return brushes;
 }
