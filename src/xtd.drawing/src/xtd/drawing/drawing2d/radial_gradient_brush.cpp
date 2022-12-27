9f50f8394acd23219c5cc89bd0d5b89cb0d21b72
Gammasoft <gammasoft71@gmail.com>
2022-12-27 16:04:03 +0100

    Fix useStlAlgorithm

--- src/xtd.drawing/src/xtd/drawing/drawing2d/radial_gradient_brush.cpp
+++ src/xtd.drawing/src/xtd/drawing/drawing2d/radial_gradient_brush.cpp
@@ -127,8 +127,7 @@ bool radial_gradient_brush::equals(const radial_gradient_brush& value) const noe
 void radial_gradient_brush::recreate_handle() {
   vector<tuple<xtd::byte, xtd::byte, xtd::byte, xtd::byte, float>> colors;
   
-  for (auto color : data_->radial_colors)
-    colors.emplace_back(color.first.r(), color.first.g(), color.first.b(), color.first.a(), color.second);
+  for_each (data_->radial_colors.begin(), data_->radial_colors.end(), [&](auto color) {colors.emplace_back(color.first.r(), color.first.g(), color.first.b(), color.first.a(), color.second);});
     
   native::brush::radial_gradient(handle(), as<int32>(data_->center_point.x()), as<int32>(data_->center_point.y()), as<int32>(data_->focal_point.x()), as<int32>(data_->focal_point.y()), colors, data_->radius);
 }
