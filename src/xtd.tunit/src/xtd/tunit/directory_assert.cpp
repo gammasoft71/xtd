086c22f79ced3587a05cc352eb2eab74eba54211
Gammasoft <gammasoft71@gmail.com>
2022-12-29 15:54:08 +0100

    Add XTD_BUILD_WITH_CONTINUOUS_INTEGRATION_SYSTE option

--- src/xtd.tunit/src/xtd/tunit/directory_assert.cpp
+++ src/xtd.tunit/src/xtd/tunit/directory_assert.cpp
@@ -1,6 +1,7 @@
 #include "../../../include/xtd/tunit/directory_assert.h"
 
 using namespace xtd;
+using namespace xtd::io;
 using namespace xtd::tunit;
 
 #if defined(__cpp_lib_filesystem)
@@ -24,7 +25,28 @@ void directory_assert::are_equal(const directory_entry& expected, const director
   else
     assert::succeed(message, stack_frame);
 }
+#endif
 
+void directory_assert::are_equal(const directory_info& expected, const directory_info& actual) {
+  are_equal(expected, actual, "", xtd::diagnostics::stack_frame::empty());
+}
+
+void directory_assert::are_equal(const directory_info& expected, const directory_info& actual, const xtd::diagnostics::stack_frame& stack_frame) {
+  are_equal(expected, actual, "", stack_frame);
+}
+
+void directory_assert::are_equal(const directory_info& expected, const directory_info& actual, const std::string& message) {
+  are_equal(expected, actual, message, xtd::diagnostics::stack_frame::empty());
+}
+
+void directory_assert::are_equal(const directory_info& expected, const directory_info& actual, const std::string& message, const xtd::diagnostics::stack_frame& stack_frame) {
+  if (expected != actual)
+    base_assert::fail(base_assert::to_string(expected.full_name()), base_assert::to_string(actual.full_name()), message, stack_frame);
+  else
+    assert::succeed(message, stack_frame);
+}
+
+#if defined(__cpp_lib_filesystem)
 void directory_assert::are_not_equal(const directory_entry& expected, const directory_entry& actual) {
   are_not_equal(expected, actual, "", xtd::diagnostics::stack_frame::empty());
 }
@@ -44,3 +66,22 @@ void directory_assert::are_not_equal(const directory_entry& expected, const dire
     assert::succeed(message, stack_frame);
 }
 #endif
+
+void directory_assert::are_not_equal(const directory_info& expected, const directory_info& actual) {
+  are_not_equal(expected, actual, "", xtd::diagnostics::stack_frame::empty());
+}
+
+void directory_assert::are_not_equal(const directory_info& expected, const directory_info& actual, const xtd::diagnostics::stack_frame& stack_frame) {
+  are_not_equal(expected, actual, "", stack_frame);
+}
+
+void directory_assert::are_not_equal(const directory_info& expected, const directory_info& actual, const std::string& message) {
+  are_not_equal(expected, actual, message, xtd::diagnostics::stack_frame::empty());
+}
+
+void directory_assert::are_not_equal(const directory_info& expected, const directory_info& actual, const std::string& message, const xtd::diagnostics::stack_frame& stack_frame) {
+  if (expected == actual)
+    base_assert::fail("not " + base_assert::to_string(expected.full_name()), base_assert::to_string(actual.full_name()), message, stack_frame);
+  else
+    assert::succeed(message, stack_frame);
+}
