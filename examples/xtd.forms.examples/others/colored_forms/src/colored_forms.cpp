800f287147b77a8477aee5e296cac6745450d71f
Gammasoft <gammasoft71@gmail.com>
2023-01-02 19:34:13 +0100

    Update control::create<..>

--- examples/xtd.forms.examples/others/colored_forms/src/colored_forms.cpp
+++ examples/xtd.forms.examples/others/colored_forms/src/colored_forms.cpp
@@ -8,14 +8,15 @@ using namespace xtd::forms;
 
 int main() {
   auto form_main = control::create<forms::form>("Main Form", {screen::primary_screen().working_area().width() - 340, 40});
-  form_main->start_position(form_start_position::manual);
+  form_main.start_position(form_start_position::manual);
   
-  auto button = control::create<forms::button>(*form_main, "Create", {10, 10});
-  button->click += [&] {
+  auto button = control::create<forms::button>(form_main, "Create", {10, 10});
+  button.click += [&] {
     static vector<shared_ptr<form>> forms;
     static auto form_color = known_color::alice_blue;
     
-    auto form = control::create<forms::form>(ustring::format("{}", form_color));
+    auto form = make_unique<forms::form>();
+    form->text(ustring::format("{}", form_color));
     form->back_color(color::from_known_color(form_color));
     form->visible(true);
     forms.push_back(std::move(form));
@@ -23,5 +24,5 @@ int main() {
     form_color = form_color != known_color::yellow_green ? known_color(int(form_color) + 1) : known_color::alice_blue;
   };
   
-  application::run(*form_main);
+  application::run(form_main);
 }
