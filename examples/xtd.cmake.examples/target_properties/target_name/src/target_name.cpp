1dc2be6220eec7b4aaecf97d747108dc687b7de8
Gammasoft <gammasoft71@gmail.com>
2023-01-02 19:33:11 +0100

    Update control::create<..>

--- examples/xtd.cmake.examples/target_properties/target_name/src/target_name.cpp
+++ examples/xtd.cmake.examples/target_properties/target_name/src/target_name.cpp
@@ -3,6 +3,5 @@
 using namespace xtd::forms;
 
 int main() {
-  auto main_form = control::create<form>("Application name");
-  application::run(*main_form);
+  application::run(control::create<form>("Application name"));
 }
