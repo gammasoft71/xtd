c42c542a88b7e8a1784461b29edf6673a18937a8
Gammasoft <gammasoft71@gmail.com>
2023-01-03 10:01:04 +0100

    Review example

--- examples/xtd.forms.examples/containers/tab_control/src/tab_control.cpp
+++ examples/xtd.forms.examples/containers/tab_control/src/tab_control.cpp
@@ -12,8 +12,10 @@ public:
     tab_control1.location({10, 10});
     tab_control1.size({370, 250});
     tab_control1.anchor(anchor_styles::left | anchor_styles::top | anchor_styles::right | anchor_styles::bottom);
-    
-    tab_page1.parent(tab_control1);
+    tab_control1.tab_pages().push_back(tab_page1);
+    tab_control1.tab_pages().push_back("Tab page 2");
+    tab_control1.tab_pages().push_back("Tab page 3", "Tab page 3");
+
     tab_page1.text("Tab page 1");
     
     label1.dock(dock_style::fill);
@@ -21,19 +23,13 @@ public:
     label1.text("label1");
     label1.text_align(xtd::forms::content_alignment::top_left);
     
-    tab_page2.parent(tab_control1);
-    tab_page2.text("Tab page 2");
-    
     label2.dock(dock_style::fill);
-    label2.parent(tab_page2);
+    label2.parent(tab_control1.tab_pages()[1].get());
     label2.text_align(xtd::forms::content_alignment::middle_center);
     label2.text("label2");
     
-    tab_page3.parent(tab_control1);
-    tab_page3.text("Tab page 3");
-    
     label3.dock(dock_style::fill);
-    label3.parent(tab_page3);
+    label3.parent(tab_control1.tab_pages()[2].get());
     label3.text_align(xtd::forms::content_alignment::bottom_right);
     label3.text("label3");
   }
@@ -41,8 +37,6 @@ public:
 private:
   tab_control tab_control1;
   tab_page tab_page1;
-  tab_page tab_page2;
-  tab_page tab_page3;
   
   label label1;
   label label2;
