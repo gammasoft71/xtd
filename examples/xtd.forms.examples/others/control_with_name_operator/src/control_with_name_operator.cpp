ac78f02b2ebfaa563cc5ef627fb8adeb9a9939a2
Gammasoft <gammasoft71@gmail.com>
2023-01-03 12:50:14 +0100

    Add control_with_name_operator example

---
+++ examples/xtd.forms.examples/others/control_with_name_operator/src/control_with_name_operator.cpp
@@ -0,0 +1,35 @@
+#include <xtd/xtd>
+
+using namespace xtd;
+using namespace xtd::forms;
+
+namespace examples {
+  class main_form : public form {
+  public:
+    main_form() {
+      controls().push_back_range({button1, label1});
+      button1.name("button1");
+      label1.name("label1");
+      
+      controls()["button1"].value().get().location({10, 10});
+      controls()["button1"].value().get().text("Click me");
+      as<button>(controls()["button1"].value().get()).auto_repeat(true);
+      controls()["button1"].value().get().click += [&] {
+        static int count = 0;
+        controls()["label1"].value().get().text(ustring::format("button clicked {} times", ++count));
+      };
+
+      controls()["label1"].value().get().auto_size(true);
+      controls()["label1"].value().get().location({10, 50});
+      controls()["label1"].value().get().text("button clicked 0 times");
+    }
+    
+  private:
+    button button1;
+    label label1;
+  };
+}
+
+int main() {
+  xtd::forms::application::run(examples::main_form {});
+}
