6bff7b05c1d00a6204190e24230b6bb2dd6c3688
Gammasoft <gammasoft71@gmail.com>
2023-01-04 14:44:56 +0100

    Review message_box2 example

--- examples/xtd.forms.examples/hello_worlds/hello_world_message_box2/src/hello_world_message_box2.cpp
+++ examples/xtd.forms.examples/hello_worlds/hello_world_message_box2/src/hello_world_message_box2.cpp
@@ -2,17 +2,9 @@
 
 using namespace xtd::forms;
 
-int main() {
-  button button;
-  button.text("&Click me");
-  button.location({10, 10});
-  button.click += [] {
-    message_box::show("Hello, World!");
-  };
-  
-  form form;
-  form.text("Hello world (message_box)");
-  form.controls().push_back(button);
-  
-  application::run(form);
+auto main()->int {
+  auto main_form = control::create<form>("Hello world (message_box)");
+  auto message_button = control::create<button>(main_form, "&Click me", {10, 10});
+  message_button.click += [] {message_box::show("Hello, World!");};
+  application::run(main_form);
 }
