52f7d963080d460aeaf4a98cc54867b02c5a8d6f
Gammasoft <gammasoft71@gmail.com>
2023-01-07 14:48:09 +0100

    Add unit tests

--- tests/xtd.core.manual_tests/src/manual_tests.cpp
+++ tests/xtd.core.manual_tests/src/manual_tests.cpp
@@ -1,71 +1,29 @@
 #include <xtd/xtd.core>
 #include <xtd/xtd.drawing>
 
-using namespace std;
 using namespace xtd;
 
-enum class value {
-  one,
-  two,
-  three,
-  four,
-  five
-};
+enum_(, enum_test,
+      value_one,
+      value_two,
+      value_three,
+      value_four
+      );
 
 int main() {
-  vector<pair<value, ustring>> values {{value::one, "one"}, {value::two, "two"}, {value::three, "three"}, {value::four, "four"}, {value::five, "five"}};
-  vector<int> results;
-  for_each(values.begin(), values.end(), [&](auto v) {results.push_back(static_cast<int>(v.first));});
+  console::write_line("name = {}", enum_test::value_four);
+  console::write_line("value = {}", enum_object(enum_test::value_four).to_int32());
+  console::write_line("as<int> = {}", as<int>(enum_test::value_four));
+  console::write_line("values = {}", enum_object<>::get_values_as_int32<enum_test>());
+  console::write_line("names = {}", enum_object<>::get_names<enum_test>());
+  console::write_line("entries = {}", enum_object<>::get_entries_as_int32<enum_test>());
 }
 
-/*
-ustring get_description(const xtd::ustring& file) {
-  auto content = io::file::read_all_lines(file);
-  if (content.size() < 2) return "";
-  auto description = content[2];
-  if (description.empty()) return "";
-  static const std::regex rgx_md_link(R"(\[(.*?)\]\((.*?)\))", std::regex::optimize);
-  xtd::ustring text = description;
-  std::sregex_iterator iterator(description.begin(), description.end(), rgx_md_link), end{};
-  for (auto it = iterator; it != end; ++it) {
-    if (it->size() == 3) { // 3 matches: whole []() + sub [] + sub ()
-      const xtd::ustring whole = it->str(0); // []()
-      const xtd::ustring title = it->str(1); // [] contents
-      text = text.replace(whole, title);
-    }
-  }
-
-  return text;
-}
-
-int main() {
-  auto count = 0;
-  auto examples_dyrectories = vector {"/Users/yves/Projects/xtd/examples/xtd.core.examples", "/Users/yves/Projects/xtd/examples/xtd.drawing.examples", "/Users/yves/Projects/xtd/examples/xtd.forms.examples", "/Users/yves/Projects/xtd/examples/xtd.tunit.examples", "/Users/yves/Projects/xtd/examples/xtd.cmake.examples"};
-  for (auto examples_dyrectory : examples_dyrectories) {
-    auto type_directories = io::directory::get_directories(examples_dyrectory);
-    sort(type_directories.begin(), type_directories.end());
-    for (auto type_directory : type_directories) {
-      auto directories = io::directory::get_directories(type_directory);
-      sort(directories.begin(), directories.end());
-      for (auto directory : directories) {
-        auto files = io::directory::get_files(directory);
-        for (auto file : files) {
-          if (io::path::get_extension(file) == ".md") {
-            auto format = ustring::format("/Users/yves/Projects/xtd//docs/pictures/examples/{}_{{}}.png", io::path::get_file_name(directory));
-            vector images = {xtd::io::file::exists(ustring::format(format, "w")) ? xtd::drawing::bitmap(ustring::format(format, "w")) : drawing::bitmap::empty, xtd::io::file::exists(ustring::format(format, "wd")) ? xtd::drawing::bitmap(ustring::format(format, "wd")) : drawing::bitmap::empty, xtd::io::file::exists(ustring::format(format, "m")) ? xtd::drawing::bitmap(ustring::format(format, "m")) : drawing::bitmap::empty, xtd::io::file::exists(ustring::format(format, "md")) ? xtd::drawing::bitmap(ustring::format(format, "md")) : drawing::bitmap::empty, xtd::io::file::exists(ustring::format(format, "g")) ? xtd::drawing::bitmap(ustring::format(format, "g")) : drawing::bitmap::empty, xtd::io::file::exists(ustring::format(format, "gd")) ? xtd::drawing::bitmap(ustring::format(format, "gd")) : drawing::bitmap::empty};
-
-            console::write_line("{} - {} - {} - {}", ++count, io::path::get_file_name(examples_dyrectory), io::path::get_file_name(directory), get_description(file));
-            continue;
-          }
-        }
-      }
-    }
-  }
-}
-
-
-// This code produces the following output for a consome application:
+// This code produces the following output :
 //
-// target_type="console application"
-// target_type="console application"
-*/
+// name = value_four
+// value = 3
+// as<int> = 3
+// values = [0, 1, 2, 3]
+// names = [value_one, value_two, value_three, value_four]
+// entries = [(0, value_one), (1, value_two), (2, value_three), (3, value_four)]
