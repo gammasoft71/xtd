a81b6d10015a774672f811663f26adc3be233e5d
Gammasoft <gammasoft71@gmail.com>
2023-01-08 18:16:23 +0100

    Add documentations

--- src/xtd.forms/src/xtd/forms/selection_range.cpp
+++ src/xtd.forms/src/xtd/forms/selection_range.cpp
@@ -11,14 +11,16 @@ date_time selection_range::end() const noexcept {
   return end_;
 }
 
-void selection_range::end(date_time value) {
+selection_range& selection_range::end(date_time value) {
   end_ = value;
+  return *this;
 }
 
 date_time selection_range::start() const noexcept {
   return start_;
 }
 
-void selection_range::start(date_time value) {
+selection_range& selection_range::start(date_time value) {
   start_ = value;
+  return *this;
 }
