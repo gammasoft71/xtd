53fcfeace474db7135f62d744b4af4778b3fcca5
Gammasoft <gammasoft71@gmail.com>
2023-01-09 20:23:07 +0100

    Fix documentation

--- src/xtd.core/include/xtd/diagnostics/trace_switch.h
+++ src/xtd.core/include/xtd/diagnostics/trace_switch.h
@@ -15,7 +15,7 @@ namespace xtd {
     /// class core_export_ trace_switch : public xtd::diagnostics::switch_base
     /// @endcode
     /// @par Inheritance
-    /// xtd::diagnostics::switch_base → xtd::diagnostics::trace_switch
+    /// xtd::object → xtd::diagnostics::switch_base → xtd::diagnostics::trace_switch
     /// @par Namespace
     /// xtd::diagnostics
     /// @par Library
