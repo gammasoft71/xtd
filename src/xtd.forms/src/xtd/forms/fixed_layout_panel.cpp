605de0d420d3e8a2f765a2af9f809d277b461baf
Gammasoft <gammasoft71@gmail.com>
2023-01-15 21:26:47 +0100

    Add create methods

--- src/xtd.forms/src/xtd/forms/fixed_layout_panel.cpp
+++ src/xtd.forms/src/xtd/forms/fixed_layout_panel.cpp
@@ -1,8 +1,26 @@
-#include "../../../include/xtd/forms/flow_layout_panel.h"
+#include "../../../include/xtd/forms/fixed_layout_panel.h"
 
 using namespace xtd;
 using namespace xtd::forms;
 
-void flow_layout_panel::on_layout(const event_args& e) {
+
+fixed_layout_panel fixed_layout_panel::create(const drawing::point& location, const drawing::size& size, const xtd::ustring& name) {
+  fixed_layout_panel item;
+  if (location != drawing::point {-1, -1}) item.location(location);
+  if (size != drawing::size {-1, -1}) item.size(size);
+  item.name(name);
+  return item;
+}
+
+fixed_layout_panel fixed_layout_panel::create(const control& parent, const drawing::point& location, const drawing::size& size, const xtd::ustring& name) {
+  fixed_layout_panel item;
+  item.parent(parent);
+  if (location != drawing::point {-1, -1}) item.location(location);
+  if (size != drawing::size {-1, -1}) item.size(size);
+  item.name(name);
+  return item;
+}
+
+void fixed_layout_panel::on_layout(const event_args& e) {
   panel::on_layout(e);
 }
