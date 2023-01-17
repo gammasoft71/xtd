d5ca869cce171f8b271ab79b03111738c74828ca
Gammasoft <gammasoft71@gmail.com>
2023-01-17 17:41:33 +0100

    Add create methods

--- src/xtd.forms/include/xtd/forms/link_label.h
+++ src/xtd.forms/include/xtd/forms/link_label.h
@@ -163,6 +163,26 @@ namespace xtd {
       link_label& visited_link_color(const xtd::drawing::color& color);
       /// @}
       
+      /// @name Methods
+      
+      /// @{
+      /// @brief A factory to create an xtd::forms::link_label with specified text, location, size, and name.
+      /// @param text A string that represent text of the xtd::forms::link_label.
+      /// @param location A xtd::drawing::point that represent location of the xtd::forms::link_label.
+      /// @param size A xtd::drawing::size that represent size of the xtd::forms::link_label.
+      /// @param name The name of the xtd::forms::link_label.
+      /// @return New xtd::forms::link_label created.
+      static link_label create(const xtd::ustring& text = "", const drawing::point& location = {-1, -1}, const drawing::size& size = {-1, -1}, const xtd::ustring& name = xtd::ustring::empty_string);
+      /// @brief A factory to create an xtd::forms::link_label with specified parent, text, location ,size, and name.
+      /// @param parent The parent that contains the new created xtd::forms::link_label.
+      /// @param text A string that represent text of the xtd::forms::link_label.
+      /// @param location A xtd::drawing::point that represent location of the xtd::forms::link_label.
+      /// @param size A xtd::drawing::size that represent size of the xtd::forms::link_label.
+      /// @param name The name of the xtd::forms::link_label.
+      /// @return New xtd::forms::link_label created.
+      static link_label create(const control& parent, const xtd::ustring& text = "", const drawing::point& location = {-1, -1}, const drawing::size& size = {-1, -1}, const xtd::ustring& name = xtd::ustring::empty_string);
+      /// @}
+
       /// @name Events
       
       /// @{
