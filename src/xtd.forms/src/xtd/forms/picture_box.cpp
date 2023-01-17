217c49358b277a682bd7e862bc79062c0297bb74
Gammasoft <gammasoft71@gmail.com>
2023-01-17 21:40:08 +0100

    Add create methods

--- src/xtd.forms/src/xtd/forms/picture_box.cpp
+++ src/xtd.forms/src/xtd/forms/picture_box.cpp
@@ -101,6 +101,42 @@ picture_box& picture_box::size_mode(picture_box_size_mode size_mode) {
   return *this;
 }
 
+picture_box picture_box::create(const drawing::point& location, const drawing::size& size, const xtd::ustring& name) {
+  picture_box item;
+  if (location != drawing::point {-1, -1}) item.location(location);
+  if (size != drawing::size {-1, -1}) item.size(size);
+  item.name(name);
+  return item;
+}
+
+picture_box picture_box::create(const xtd::drawing::image& image, const drawing::point& location, const drawing::size& size, const xtd::ustring& name) {
+  picture_box item;
+  item.image(image);
+  if (location != drawing::point {-1, -1}) item.location(location);
+  if (size != drawing::size {-1, -1}) item.size(size);
+  item.name(name);
+  return item;
+}
+
+picture_box picture_box::create(const control& parent, const drawing::point& location, const drawing::size& size, const xtd::ustring& name) {
+  picture_box item;
+  item.parent(parent);
+  if (location != drawing::point {-1, -1}) item.location(location);
+  if (size != drawing::size {-1, -1}) item.size(size);
+  item.name(name);
+  return item;
+}
+
+picture_box picture_box::create(const control& parent, const xtd::drawing::image& image, const drawing::point& location, const drawing::size& size, const xtd::ustring& name) {
+  picture_box item;
+  item.parent(parent);
+  item.image(image);
+  if (location != drawing::point {-1, -1}) item.location(location);
+  if (size != drawing::size {-1, -1}) item.size(size);
+  item.name(name);
+  return item;
+}
+
 forms::create_params picture_box::create_params() const noexcept {
   forms::create_params create_params = control::create_params();
   
