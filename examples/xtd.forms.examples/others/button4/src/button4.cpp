15a54530b3838199c07c18c8960d759cd36afdb9
Gammasoft <gammasoft71@gmail.com>
2023-01-17 17:34:18 +0100

    Update examples

--- examples/xtd.forms.examples/others/button4/src/button4.cpp
+++ examples/xtd.forms.examples/others/button4/src/button4.cpp
@@ -5,8 +5,8 @@ auto main()->int {
   auto form1 = xtd::forms::form::create("Button 4 example");
   auto button1 = xtd::forms::button::create(form1, "Button 1", {50, 50});
   auto button2 = xtd::forms::button::create(form1, "Button 2", {50, 100}, {200, 75});
-  auto label1 = xtd::forms::control::create<xtd::forms::label>(form1, "Button 1 clicked 0 times", {50, 200}, {200, 23});
-  auto label2 = xtd::forms::control::create<xtd::forms::label>(form1, "Button 2 clicked 0 times", {50, 230}, {200, 23});
+  auto label1 = xtd::forms::label::create(form1, "Button 1 clicked 0 times", {50, 200}, {200, 23});
+  auto label2 = xtd::forms::label::create(form1, "Button 2 clicked 0 times", {50, 230}, {200, 23});
   button1.click += [&] {label1.text(xtd::ustring::format("Button 1 clicked {} times", ++button1_clicked));};
   button2.click += [&] {label2.text(xtd::ustring::format("Button 2 clicked {} times", ++button2_clicked));};
   xtd::forms::application::run(form1);
