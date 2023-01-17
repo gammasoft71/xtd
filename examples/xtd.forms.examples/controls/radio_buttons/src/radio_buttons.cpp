4c3f35c423088a3d737aea30e1378b86973454f6
Gammasoft <gammasoft71@gmail.com>
2023-01-17 14:45:22 +0100

    Update examples

--- examples/xtd.forms.examples/controls/radio_buttons/src/radio_buttons.cpp
+++ examples/xtd.forms.examples/controls/radio_buttons/src/radio_buttons.cpp
@@ -4,7 +4,7 @@ using namespace xtd::forms;
 
 int main() {
   auto form_main = form::create("Radio buttons example");
-  auto group_box_alignment = control::create<group_box>(form_main, system_texts::alignment(), {10, 10}, {150, 180});
+  auto group_box_alignment = group_box::create(form_main, system_texts::alignment(), {10, 10}, {150, 180});
   auto radio_button_align_left = radio_buttons::align_left(group_box_alignment, true, {20, 20});
   auto radio_button_align_right = radio_buttons::align_right(group_box_alignment, false, {20, 50});
   auto radio_button_centered = radio_buttons::centered(group_box_alignment, false, {20, 80});
