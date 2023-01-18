04cf4e3a950120a6d68776ab9335bc35598ca7cf
Gammasoft <gammasoft71@gmail.com>
2023-01-18 20:25:22 +0100

    Add create methods

--- src/xtd.forms/include/xtd/forms/tab_page.h
+++ src/xtd.forms/include/xtd/forms/tab_page.h
@@ -85,9 +85,25 @@ namespace xtd {
       control& text(const xtd::ustring& text) override;
       /// @}
       
-      /// @name Metthods
+      /// @name Methods
       
       /// @{
+      /// @brief A factory to create an xtd::forms::tab_page with specified text, location, size, and name.
+      /// @param text A string that represent text of the xtd::forms::tab_page.
+      /// @param location A xtd::drawing::point that represent location of the xtd::forms::tab_page.
+      /// @param size A xtd::drawing::size that represent size of the xtd::forms::tab_page.
+      /// @param name The name of the xtd::forms::tab_page.
+      /// @return New xtd::forms::tab_page created.
+      static tab_page create(const xtd::ustring& text = "", const drawing::point& location = {-1, -1}, const drawing::size& size = {-1, -1}, const xtd::ustring& name = xtd::ustring::empty_string);
+      /// @brief A factory to create an xtd::forms::tab_page with specified parent, text, location ,size, and name.
+      /// @param parent The parent that contains the new created xtd::forms::tab_page.
+      /// @param text A string that represent text of the xtd::forms::tab_page.
+      /// @param location A xtd::drawing::point that represent location of the xtd::forms::tab_page.
+      /// @param size A xtd::drawing::size that represent size of the xtd::forms::tab_page.
+      /// @param name The name of the xtd::forms::tab_page.
+      /// @return New xtd::forms::tab_page created.
+      static tab_page create(const control& parent, const xtd::ustring& text = "", const drawing::point& location = {-1, -1}, const drawing::size& size = {-1, -1}, const xtd::ustring& name = xtd::ustring::empty_string);
+
       void destroy_handle() override;
       /// @}
       
