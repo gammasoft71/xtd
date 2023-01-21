45ed8b6f7b77ea8460651fa8ef3617d0379306d3
Gammasoft <gammasoft71@gmail.com>
2023-01-21 17:10:39 +0100

    Update animation.cpp

--- examples/xtd.forms.examples/controls/animation/src/animation.cpp
+++ examples/xtd.forms.examples/controls/animation/src/animation.cpp
@@ -11,10 +11,10 @@ namespace examples {
   class form1 : public form {
   public:
     form1() {
-      back_color(color::dark_cyan);
+      back_color(colors::dark_cyan());
       client_size({360, 280});
       controls().push_back(fish_animation);
-      fore_color(color::cyan);
+      fore_color(colors::cyan());
       text("Animation example");
       
       fish_animation.dock(dock_style::fill);
@@ -33,7 +33,7 @@ namespace examples {
     void on_fish_animation_paint(object& sender, paint_event_args& e) {
       e.graphics().clear(fish_animation.back_color());
       static const auto fish_length = 16;
-      vector<point_f> points;
+      auto points = vector<point_f> {};
       for (auto i = 0; i < fish_length; ++i) {
         auto radius = 100 + 10 * sin(as<float>(fish_animation.frame_counter()) * 0.1f + as<float>(i) * 0.5f);
         auto point = point_f(as<float>(e.clip_rectangle().width()) / 2.0f + 1.5f * radius * sin(as<float>(fish_animation.frame_counter()) * 0.02f + as<float>(i) * 0.12f), as<float>(e.clip_rectangle().height()) / 2.0f + 1.0f * radius * cos(as<float>(fish_animation.frame_counter()) * 0.04f + as<float>(i) * 0.12f));
@@ -52,8 +52,8 @@ namespace examples {
 }
 
 auto main()->int {
-  debug_form df;
-  df.back_color(drawing::basic_colors::black());
-  df.fore_color(drawing::basic_colors::lime());
+  auto df = debug_form {};
+  df.back_color(basic_colors::black());
+  df.fore_color(basic_colors::lime());
   application::run(examples::form1 {});
 }
