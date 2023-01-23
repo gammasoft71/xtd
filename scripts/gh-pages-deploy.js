8052103554594701388240afcd1aad7aceebd4a5
Arsero <54241944+Arsero@users.noreply.github.com>
2023-01-23 16:17:10 +0100

    fix: deploy scripts

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -1,34 +1,25 @@
 // source:https://dev.to/rolanddoda/deploy-to-github-pages-like-a-pro-with-github-actions-4hdg
 
-const execa = require('execa');
-const emoji = require('node-emoji');
-const chalk = require('chalk');
-const fs = require('fs');
-
-const e_arrows = emoji.get('fast_forward');
-const e_rocket = emoji.get('rocket');
-const e_x = emoji.get('x');
+const execa = require("execa");
+const fs = require("fs");
 
 (async () => {
   try {
-    await execa('git', ['checkout', '--orphan', 'gh-pages']);
-
-    console.log(`${e_arrows} ${chalk.yellow('Building...')}`);
-    await execa('yarn', ['build']);
+    await execa("git", ["checkout", "--orphan", "gh-pages"]);
+    console.log("Building...");
+    await execa("yarn", ["build"]);
     // Understand if it's dist or build folder
-    const folderName = fs.existsSync('dist') ? 'dist' : 'build';
-    await execa('git', ['--work-tree', folderName, 'add', '--all']);
-    await execa('git', ['--work-tree', folderName, 'commit', '-m', 'gh-pages']);
-
-    console.log(`${e_arrows} ${chalk.yellow('Pushing...')}`);
-    await execa('git', ['push', 'origin', 'HEAD:gh-pages', '--force']);
-
-    await execa('rm', ['-r', folderName]);
-    await execa('git', ['checkout', '-f', 'docs']);
-    await execa('git', ['branch', '-D', 'gh-pages']);
-    console.log(`${e_rocket} ${chalk.green('Successfully deployed')} ${e_rocket}`);
-  } catch (e) {
-    console.log(`${e_x} ${chalk.red('An error occured')} ${e_x}\n${e.message}`);
+    const folderName = fs.existsSync("dist") ? "dist" : "build";
+    await execa("git", ["--work-tree", folderName, "add", "--all"]);
+    await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
+    console.log("Pushing to gh-pages...");
+    await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
+    await execa("rm", ["-r", folderName]);
+    await execa("git", ["checkout", "-f", "docs"]);
+    await execa("git", ["branch", "-D", "gh-pages"]);
+    console.log("Successfully deployed");
+  } catch (e) {-
+    console.log(e.message);
     process.exit(1);
   }
 })();
