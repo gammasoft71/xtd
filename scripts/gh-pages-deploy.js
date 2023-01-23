fc941ffa350771bf7a33a94fd41d5da25d2d5e88
Arsero <54241944+Arsero@users.noreply.github.com>
2023-01-23 17:34:55 +0100

    Update gh-pages-deploy.js

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -1,24 +1,24 @@
-// source:https://dev.to/rolanddoda/deploy-to-github-pages-like-a-pro-with-github-actions-4hdg
-
-const execa = require("execa");
-const fs = require("fs");
-
 (async () => {
   try {
-    await execa("git", ["checkout", "--orphan", "gh-pages"]);
-    console.log("Building...");
-    await execa("yarn", ["build"]);
+    const { execa } = await import('execa');
+    const fs = await import('fs');
+
+    await execa('git', ['checkout', '--orphan', 'gh-pages']);
+    console.log('Building...');
+    await execa('yarn', ['build']);
     // Understand if it's dist or build folder
-    const folderName = fs.existsSync("dist") ? "dist" : "build";
-    await execa("git", ["--work-tree", folderName, "add", "--all"]);
-    await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
-    console.log("Pushing to gh-pages...");
-    await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
-    await execa("rm", ["-r", folderName]);
-    await execa("git", ["checkout", "-f", "docs"]);
-    await execa("git", ["branch", "-D", "gh-pages"]);
-    console.log("Successfully deployed");
-  } catch (e) {-
+    const folderName = fs.existsSync('dist') ? 'dist' : 'build';
+    await execa('git', ['--work-tree', folderName, 'add', '--all']);
+    await execa('git', ['--work-tree', folderName, 'commit', '-m', 'gh-pages']);
+    
+    console.log('Pushing to gh-pages...');
+    await execa('git', ['push', 'origin', 'HEAD:gh-pages', '--force']);
+    fs.rmSync(folderName, { recursive: true, force: true });
+    await execa('git', ['checkout', '-f', 'docs']);
+    await execa('git', ['branch', '-D', 'gh-pages']);
+
+    console.log('Successfully deployed');
+  } catch (e) {
     console.log(e.message);
     process.exit(1);
   }
