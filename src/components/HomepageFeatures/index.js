8ef791cdfc8c907d6bba33c79b30cfe5337a3411
Gammasoft <gammasoft71@gmail.com>
2023-01-26 15:51:44 +0100

    Fix main page

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -8,11 +8,11 @@ const FeatureList = [
     Svg: require('@site/static/img/undraw_in_sync_re_jlqd.svg').default,
     description: (
       <>
-      Free and open-source (MIT License). A collection of native C++ classes libraries, to complete std.
-      <br/>API close to the .net API with modern C++ approach and full integration with the std standard.
-      <br/>xtd is designed to manage GUI controls and dialogs in pure native mode or with CSS styles.
-      <br/>Written in efficient, modern c++17/20 with RAII programming idiom.
-      And highly portable and available on many different platforms.
+      • Free and open-source (MIT License). A collection of native C++ classes libraries, to complete std.
+      <br/>• API close to the .net API with modern C++ approach and full integration with the std standard.
+      <br/>• xtd is designed to manage GUI controls and dialogs in pure native mode or with CSS styles.
+      <br/>• Written in efficient, modern c++17/20 with RAII programming idiom.
+      <br/>• And highly portable and available on many different platforms.
       </>
     ),
   },
@@ -21,9 +21,12 @@ const FeatureList = [
     Svg: require('@site/static/img/undraw_next_tasks_re_5eyy.svg').default,
     description: (
       <>
-      xtd is a powerful, yet easy to use platform to build your applications upon. xtd is modular and scalable from embedded applications.
-      xtd provides consistent, comprehensive and comprehensible programming interfaces. xtd favors simplicity over complexity ("as simple as possible, but not simpler").
-      xtd aims for consistency in design, coding style and documentation. xtd emphasizes source code quality, in terms of readability, comprehensiveness, consistency, style and testability.
+      • xtd is a powerful, yet easy to use platform to build your applications upon. 
+      <br/>• xtd is modular and scalable from embedded applications.
+      <br/>• xtd provides consistent, comprehensive and comprehensible programming interfaces. 
+      <br/>• xtd favors simplicity over complexity ("as simple as possible, but not simpler").
+      <br/>• xtd aims for consistency in design, coding style and documentation. 
+      <br/>• xtd emphasizes source code quality, in terms of readability, comprehensiveness, consistency, style and testability.
       </>
     ),
   },
@@ -32,8 +35,9 @@ const FeatureList = [
     Svg: require('@site/static/img/undraw_programming_re_kg9v.svg').default,
     description: (
       <>
-      Installation provides download and install documentation. Guides provides xtd guides and tutorials.
-      Examples provides over 500 examples to help you use xtd, grouped by libraries and topics.
+      • Installation provides download and install documentation.
+      <br/>• Guides provides xtd guides and tutorials.
+      <br/>• Examples provides over 500 examples to help you use xtd, grouped by libraries and topics.
       </>
     ),
   },
@@ -41,11 +45,10 @@ const FeatureList = [
 
 function Feature({Svg, title, description}) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx('row row--4')}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
       </div>
-      <div className="text--center padding-horiz--md">
+      <div className="text--left padding-horiz--md">
         <h3>{title}</h3>
         <p>{description}</p>
       </div>
