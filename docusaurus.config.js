ad848da250ba6e6e6c3cd6f13dd71c944e08caf9
Gammasoft <gammasoft71@gmail.com>
2023-01-27 14:22:47 +0100

    Remove "edit this page"

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -39,7 +39,7 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/gammasoft71/xtd/tree/docs',
+          //editUrl: 'https://github.com/gammasoft71/xtd/tree/docs',
         },
         blog: false,
         theme: {
