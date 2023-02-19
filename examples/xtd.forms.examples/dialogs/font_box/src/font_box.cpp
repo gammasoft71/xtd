ff196ca0e4659ce9067c531957a7be12100b0244
Gammasoft <gammasoft71@gmail.com>
2023-02-19 17:04:54 +0100

    review font_box code

--- examples/xtd.forms.examples/dialogs/font_box/src/font_box.cpp
+++ examples/xtd.forms.examples/dialogs/font_box/src/font_box.cpp
@@ -7,13 +7,13 @@ class form1 : public form {
 public:
   form1() {
     text("Font box example");
-    client_size({ 220, 160 });
-    controls().push_back_range({ button_font, button_font_and_color });
+    client_size({220, 160});
+    controls().push_back_range({button_font, button_font_and_color});
     
     button_font.parent(*this)
-    .location({ 10, 10 })
+    .location({10, 10})
     .text("Select a font")
-    .size({ 200, 45 })
+    .size({200, 45})
     .click += [this] {
       drawing::font font = xtd::drawing::system_fonts::default_font();
       auto res = font_box::show(font, *this);
@@ -22,9 +22,9 @@ public:
     };
     
     button_font_and_color.parent(*this)
-    .location({ 10, 60 })
+    .location({10, 60})
     .text("Select a font and a font color")
-    .size({ 200, 45 })
+    .size({200, 45})
     .click += [] {
       drawing::font font(drawing::font_family::generic_sans_serif(), 16);
       drawing::color color = drawing::color::blue;
---
+++ src/xtd.forms/src/xtd/forms/font_box.cpp
@@ -0,0 +1,63 @@
+#include "../../../include/xtd/forms/font_box.h"
+#include "../../../include/xtd/forms/font_dialog.h"
+
+using namespace xtd;
+using namespace xtd::drawing;
+using namespace xtd::forms;
+
+dialog_result font_box::show(font& font) {
+  return show_font_box(font);
+}
+
+dialog_result font_box::show(font& font, const font_box_options options) {
+  return show_font_box(font, nullptr, nullptr, options);
+}
+
+dialog_result font_box::show(font& font, const iwin32_window& owner) {
+  return show_font_box(font, nullptr, &owner);
+}
+
+dialog_result font_box::show(font& font, const iwin32_window& owner, const font_box_options options) {
+  return show_font_box(font, nullptr, &owner, options);
+}
+
+dialog_result font_box::show(font& font, color& color) {
+  return show_font_box(font, &color);
+}
+
+dialog_result font_box::show(font& font, color& color, const font_box_options options) {
+  return show_font_box(font, &color, nullptr, options);
+}
+
+dialog_result font_box::show(font& font, color& color, const iwin32_window& owner) {
+  return show_font_box(font, &color, &owner);
+}
+
+dialog_result font_box::show(font& font, color& color, const iwin32_window& owner, const font_box_options options) {
+  return show_font_box(font, &color, &owner, options);
+}
+
+dialog_result font_box::show_font_box(font& font, color* color, const iwin32_window* owner, font_box_options options) {
+  font_dialog dialog{};
+  if (color) {
+    options |= font_box_options::show_color;
+    dialog.color(*color);
+  }
+  dialog.allow_simulation((options & font_box_options::allow_simulations) == font_box_options::allow_simulations)
+    .allow_vector_fonts((options & font_box_options::allow_vector_fonts) == font_box_options::allow_vector_fonts)
+    .allow_vertical_fonts((options & font_box_options::allow_vertical_fonts) == font_box_options::allow_vertical_fonts)
+    .allow_script_change((options & font_box_options::allow_script_change) == font_box_options::allow_script_change)
+    .fixed_pitch_only((options & font_box_options::fixed_pitch_only) == font_box_options::fixed_pitch_only)
+    .font(font)
+    .script_only((options & font_box_options::scripts_only) == font_box_options::scripts_only)
+    .show_apply((options & font_box_options::show_apply) == font_box_options::show_apply)
+    .show_color((options & font_box_options::show_color) == font_box_options::show_color)
+    .show_effect((options & font_box_options::show_effects) == font_box_options::show_effects)
+    .show_help((options & font_box_options::show_help) == font_box_options::show_help);
+  auto res = owner ? dialog.show_sheet_dialog(*owner) : dialog.show_dialog();
+  if (res == dialog_result::ok) {
+    font = dialog.font();
+    if (color) *color = dialog.color();
+  }
+  return res;
+}
