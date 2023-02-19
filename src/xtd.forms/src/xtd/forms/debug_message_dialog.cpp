9d61c21f71335fd546713e7832da9ac807ad43de
Gammasoft <gammasoft71@gmail.com>
2023-02-19 17:40:52 +0100

    review debug_message_dialog code

---
+++ src/xtd.forms/src/xtd/forms/debug_message_dialog.cpp
@@ -0,0 +1,30 @@
+#include "../../../include/xtd/forms/debug_message_dialog.h"
+#include "../../../include/xtd/forms/application.h"
+
+using namespace xtd;
+using namespace xtd::forms;
+
+debug_message_dialog::debug_message_dialog() {
+  dialog_.dialog_closed += [&](object& sender, const dialog_closed_event_args& e) {on_dialog_closed(e);};
+  reset();
+}
+
+dialog_result debug_message_dialog::dialog_result() const noexcept {
+  return dialog_.dialog_result();
+}
+
+ustring debug_message_dialog::message() const noexcept {
+  return dialog_.message();
+}
+
+void debug_message_dialog::reset() {
+  dialog_.reset();
+  dialog_.buttons(xtd::forms::message_dialog_buttons::ok);
+  dialog_.icon(xtd::forms::message_dialog_icon::exclamation);
+  dialog_.text(xtd::forms::application::product_name());
+}
+
+void debug_message_dialog::on_dialog_closed(const dialog_closed_event_args& e) {
+  auto raise_event = dialog_closed;
+  if (!raise_event.is_empty()) raise_event(*this, e);
+}
