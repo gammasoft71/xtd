385927f2e7febc0fdca28c94bfd981b85a02c9fc
Gammasoft <gammasoft71@gmail.com>
2023-02-24 23:06:03 +0100

    Update manual_tests.cpp

--- tests/xtd.core.manual_tests/src/manual_tests.cpp
+++ tests/xtd.core.manual_tests/src/manual_tests.cpp
@@ -3,27 +3,9 @@
 
 using namespace xtd;
 
-enum_(, enum_test,
-  value_one,
-  value_two,
-  value_three,
-  value_four
-);
-
 auto main()->int {
-  console::write_line("name = {}", enum_test::value_four);
-  console::write_line("value = {}", enum_object(enum_test::value_four).to_int32());
-  console::write_line("as<int> = {}", as<int>(enum_test::value_four));
-  console::write_line("values = {}", enum_object<>::get_values_as_int32<enum_test>());
-  console::write_line("names = {}", enum_object<>::get_names<enum_test>());
-  console::write_line("entries = {}", enum_object<>::get_entries_as_int32<enum_test>());
+  int min = as<int>(single_object::min_value);
+  int max = as<int>(single_object::max_value);
+  console::write_line("main = {}; max= {}", min, max);
+  console::write_line("main = {}; max= {}", int32_object::min_value, int32_object::max_value);
 }
-
-// This code produces the following output :
-//
-// name = value_four
-// value = 3
-// as<int> = 3
-// values = [0, 1, 2, 3]
-// names = [value_one, value_two, value_three, value_four]
-// entries = [(0, value_one), (1, value_two), (2, value_three), (3, value_four)]
