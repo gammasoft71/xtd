cfff402cb2b4a7f9aa318579c8e733fa48d4c035
Gammasoft <gammasoft71@gmail.com>
2023-02-28 22:18:04 +0100

    Fix signal_catcher

--- src/xtd.core/src/xtd/signal_catcher.cpp
+++ src/xtd.core/src/xtd/signal_catcher.cpp
@@ -3,7 +3,6 @@
 #include "../../include/xtd/arithmetic_exception.h"
 #include "../../include/xtd/invalid_operation_exception.h"
 #include "../../include/xtd/system_exception.h"
-#include "../../include/xtd/startup.h"
 #include "../../include/xtd/unused.h"
 
 using namespace std;
@@ -41,29 +40,23 @@ namespace {
     void __check__();
     
     static void signal_illegal_instruction_handler(int32 signal) {
-      //__startup_catch_exception__(xtd::invalid_operation_exception(csf_));
-      //exit(EXIT_FAILURE);
       ::signal(signal, signal_catcher::signal_illegal_instruction_handler);
       throw xtd::invalid_operation_exception(csf_);
     }
     
     static void signal_abort_exception_handler(int32 signal) {
-      //__startup_catch_exception__(xtd::threading::thread_abort_exception(csf_));
-      //exit(EXIT_FAILURE);
       ::signal(signal, signal_catcher::signal_abort_exception_handler);
-      throw xtd::threading::thread_abort_exception(csf_);
+      // Do not uncomment the next line, otherwise the exception is triggered forever.
+      //throw xtd::threading::thread_abort_exception(csf_);
+       exit(EXIT_FAILURE);
     }
     
     static void signal_floating_point_exception_handler(int32 signal) {
-      //__startup_catch_exception__(xtd::arithmetic_exception(csf_));
-      //exit(EXIT_FAILURE);
       ::signal(signal, signal_catcher::signal_floating_point_exception_handler);
       throw xtd::arithmetic_exception(csf_);
     }
     
     static void signal_segmentation_violation_handler(int32 signal) {
-      //__startup_catch_exception__(xtd::access_violation_exception(csf_));
-      //exit(EXIT_FAILURE);
       ::signal(signal, signal_catcher::signal_segmentation_violation_handler);
       throw xtd::access_violation_exception(csf_);
     }
