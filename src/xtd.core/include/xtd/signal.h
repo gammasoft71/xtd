2680d1fb0102be8ae7a6cac76381a00146f70b16
Gammasoft <gammasoft71@gmail.com>
2023-03-06 08:38:06 +0100

    Update documentation

--- src/xtd.core/include/xtd/signal.h
+++ src/xtd.core/include/xtd/signal.h
@@ -16,17 +16,17 @@ namespace xtd {
   /// @remarks Each of the above values represents a constant, which represent different signals sent to the program.
   /// @remarks For more information about signal, see <a href="https://en.cppreference.com/w/cpp/utility/program/signal">std::signal</a>.
   enum class signal {
-    /// @brief Abnormal termination triggered by abort call. Is equal to SIGABRT.
+    /// @brief Abnormal termination triggered by abort call. Is equal to <a href="https://en.cppreference.com/w/cpp/utility/program/signal">SIGABRT</a>.
     abnormal_termination = SIGABRT,
-    /// @brief Erroneous arithmetic operation such as divide by zero. Is equal to SIGFPE.
+    /// @brief Erroneous arithmetic operation such as divide by zero. Is equal to <a href="https://en.cppreference.com/w/cpp/utility/program/signal">SIGFPE</a>.
     floating_point_exception = SIGFPE,
-    /// @brief Invalid program image, such as invalid instruction. Is equal to SIGILL.
+    /// @brief Invalid program image, such as invalid instruction. Is equal to <a href="https://en.cppreference.com/w/cpp/utility/program/signal">SIGILL</a>.
     illegal_instruction = SIGILL,
-    /// @brief external interrupt, usually initiated by the user. Is equal to SIGINT.
+    /// @brief external interrupt, usually initiated by the user. Is equal to <a href="https://en.cppreference.com/w/cpp/utility/program/signal">SIGINT</a>.
     interrupt = SIGINT,
-    /// @brief Invalid memory access (segmentation fault). Is equal to SIGSEGV.
+    /// @brief Invalid memory access (segmentation fault). Is equal to <a href="https://en.cppreference.com/w/cpp/utility/program/signal">SIGSEGV</a>.
     segmentation_violation = SIGSEGV,
-    /// @brief Termination request, sent to the program. Is equal to SIGTERM.
+    /// @brief Termination request, sent to the program. Is equal to <a href="https://en.cppreference.com/w/cpp/utility/program/signal">SIGTERM</a>.
     software_termination = SIGTERM,
   };
 }
