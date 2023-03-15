98d92742f669ce878e3005da4bdc1425c178ca78
Gammasoft <gammasoft71@gmail.com>
2023-03-16 00:54:31 +0100

    Fix typo

--- examples/xtd.forms.examples/applications/application_restart/src/application_restart.cpp
+++ examples/xtd.forms.examples/applications/application_restart/src/application_restart.cpp
@@ -5,7 +5,7 @@ using namespace xtd;
 using namespace xtd::forms;
 
 auto main()->int {
-  auto restart_count = environment::get_environment_variable("application_restart_count").empty() ? 0 : parse<int>(environment::get_environment_variable("applicatioon_restart_count"));
+  auto restart_count = environment::get_environment_variable("application_restart_count").empty() ? 0 : parse<int>(environment::get_environment_variable("application_restart_count"));
   auto main_form = form::create(ustring::format("Restart {} times", restart_count));
   auto restart_button = button::create(main_form, "Restart", {10, 10});
   restart_button.click += application::restart;
