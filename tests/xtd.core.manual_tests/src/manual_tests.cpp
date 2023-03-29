ffe6c48e2fd89639a1a61ff9f7aee102c15ef500
Gammasoft <gammasoft71@gmail.com>
2023-03-29 21:28:35 +0200

    Update manual_tests.cpp

--- tests/xtd.core.manual_tests/src/manual_tests.cpp
+++ tests/xtd.core.manual_tests/src/manual_tests.cpp
@@ -8,11 +8,16 @@ class program {
 public:
   static void main() {
     auto os = environment::os_version();
-    console::write_line("os = {}", os);
-    console::write_line("os.distribution.name = {}", os.distribution().name());
-    console::write_line("os.distribution.version = {}", os.distribution().version());
-    console::write_line("os.distribution.code_name = {}", os.distribution().code_name());
-    console::write_line("os.distribution.description = {}", os.distribution().description());
+    console::write_line("os.distribution = {}", os);
+    console::write_line("  bug_repport = {}", os.distribution().bug_repport());
+    console::write_line("  code_name = {}", os.distribution().code_name());
+    console::write_line("  description = {}", os.distribution().description());
+    console::write_line("  home = {}", os.distribution().home());
+    console::write_line("  id = {}", os.distribution().id());
+    console::write_line("  like_ids = {}", os.distribution().like_ids());
+    console::write_line("  name = {}", os.distribution().name());
+    console::write_line("  version = {}", os.distribution().version());
+    console::write_line("  version_string = {}", os.distribution().version_string());
   }
 };
 
