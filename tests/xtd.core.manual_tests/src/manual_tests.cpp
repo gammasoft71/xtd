9a433f7434d54f134fb668ad4432bc4f603c5530
Gammasoft <gammasoft71@gmail.com>
2023-03-30 21:37:09 +0200

    Update manual_tests.cpp

--- tests/xtd.core.manual_tests/src/manual_tests.cpp
+++ tests/xtd.core.manual_tests/src/manual_tests.cpp
@@ -1,6 +1,4 @@
-#include <csignal>
 #include <xtd/xtd.core>
-#include <xtd/xtd.drawing>
 
 using namespace xtd;
 
@@ -8,16 +6,24 @@ class program {
 public:
   static void main() {
     auto os = environment::os_version();
-    console::write_line("os.distribution = {}", os);
-    console::write_line("  bug_repport = {}", os.distribution().bug_repport());
-    console::write_line("  code_name = {}", os.distribution().code_name());
-    console::write_line("  description = {}", os.distribution().description());
-    console::write_line("  home = {}", os.distribution().home());
-    console::write_line("  id = {}", os.distribution().id());
-    console::write_line("  like_ids = {}", os.distribution().like_ids());
-    console::write_line("  name = {}", os.distribution().name());
-    console::write_line("  version = {}", os.distribution().version());
-    console::write_line("  version_string = {}", os.distribution().version_string());
+    console::write_line("os = {}", os);
+    console::write_line("   desktop_environment = {}", os.desktop_environment());
+    console::write_line("   desktop_theme = {}", os.desktop_theme());
+    console::write_line("   distribution = {}", os.distribution());
+    console::write_line("      bug_repport = {}", os.distribution().bug_repport());
+    console::write_line("      code_name = {}", os.distribution().code_name());
+    console::write_line("      description = {}", os.distribution().description());
+    console::write_line("      home = {}", os.distribution().home());
+    console::write_line("      id = {}", os.distribution().id());
+    console::write_line("      like_ids = {}", os.distribution().like_ids());
+    console::write_line("      name = {}", os.distribution().name());
+    console::write_line("      version = {}", os.distribution().version());
+    console::write_line("      version_string = {}", os.distribution().version_string());
+    console::write_line("   name = {}", os.name());
+    console::write_line("   platform = {}", os.platform());
+    console::write_line("   service_pack = {}", os.service_pack());
+    console::write_line("   version = {}", os.version());
+    console::write_line("   version_string = {}", os.version_string());
   }
 };
 
