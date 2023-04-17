588fe27f50b784d436211e089a7f9961999947f8
gammasoft71 <gammasoft71@gmail.com>
2023-04-17 21:00:08 +0200

    update latest reference guide

---
+++ static/reference_guides/latest/html/search/groups_b.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['style_5fsheets_0',['style_sheets',['../group__style__sheets.html',1,'']]],
+  ['synthesis_1',['synthesis',['../group__synthesis.html',1,'']]],
+  ['system_2',['system',['../group__system.html',1,'']]],
+  ['system_20information_3',['system information',['../group__system__information.html',1,'']]]
+];
---
+++ static/reference_guides/latest/html/search/typedefs_13.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['vector_5flist_0',['vector_list',['../group__collections.html#gaf18249e78122f5f0f0f09003cf6abc5c',1,'xtd::collections']]],
+  ['vertical_5fcontrol_5flayout_5fstyle_5fcollection_1',['vertical_control_layout_style_collection',['../group__xtd__forms.html#ga43e6f3603d145d6676820fe8a11584e4',1,'xtd::forms']]],
+  ['violet_5fcolors_2',['violet_colors',['../group__drawing.html#ga00b820083dc10635d0003659f19bea3f',1,'xtd::drawing']]]
+];
