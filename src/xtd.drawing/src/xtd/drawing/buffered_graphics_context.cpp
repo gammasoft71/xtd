0df978e0a6202d5039021bfc95c62dd55b906ebb
Gammasoft <gammasoft71@gmail.com>
2023-04-30 11:32:47 +0200

    buffered_graphics_context implementation

--- src/xtd.drawing/src/xtd/drawing/buffered_graphics_context.cpp
+++ src/xtd.drawing/src/xtd/drawing/buffered_graphics_context.cpp
@@ -1,7 +1,9 @@
 #include "../../../include/xtd/drawing/buffered_graphics_context.h"
+#include <optional>
 #include <xtd/argument_exception.h>
 #include <xtd/threading/interlocked.h>
 
+using namespace std;
 using namespace xtd;
 using namespace xtd::drawing;
 using namespace xtd::threading;
@@ -17,17 +19,81 @@ buffered_graphics_context& buffered_graphics_context::maximum_buffer(const xtd::
   return *this;
 }
 
+
+void buffered_graphics_context::allocate(const xtd::drawing::graphics& target_graphics, const xtd::drawing::rectangle& target_rectangle) {
+  compat_graphics_ = target_graphics;
+  
+}
+
+void buffered_graphics_context::allocate(intptr target_dc, const xtd::drawing::rectangle& target_rectangle) {
+  allocate(graphics::from_hdc(target_dc), target_rectangle);
+}
+
 void buffered_graphics_context::invalidate() {
-  int old_busy = interlocked::compare_exchange(buzy_, BUFFER_BUSY_DESTROYING, BUFFER_FREE);
+  int old_busy = interlocked::compare_exchange(busy_, BUFFER_BUSY_DESTROYING, BUFFER_FREE);
   
   // if we're not busy with our buffer, lets clean it up now
   if (old_busy == BUFFER_FREE) {
     destroy();
-    buzy_ = BUFFER_FREE;
+    busy_ = BUFFER_FREE;
   } else
     // this will indicate to free the buffer as soon as it becomes non-busy
     invalidate_when_free_ = true;
 }
+
+/*
+xtd::drawing::buffered_graphics buffered_graphics_context::alloc_buffer_in_temp_manager(const xtd::drawing::graphics* target_graphics, intptr target_dc, const xtd::drawing::rectangle& target_rectangle) {
+  buffered_graphics_context temp_context;
+  buffered_graphics temp_buffer;
+  
+  try {
+      temp_buffer = temp_context.alloc_buffer(targetGraphics, targetDC, targetRectangle);
+      temp_buffer.free_context(true);
+    }
+  }
+  finally {
+    if (temp_context != null && (temp_buffer == null || (temp_buffer != null && !temp_buffer.DisposeContext))) {
+      temp_context.Dispose();
+    }
+  }
+  
+  return temp_buffer;
+}
+
+buffered_graphics buffered_graphics_context::allocate_buffer(const graphics* target_graphics, intptr target_dc, const rectangle& target_rectangle) {
+  int old_busy = interlocked::compare_exchange(busy_, BUFFER_BUSY_PAINTING, BUFFER_FREE);
+  
+  // In the case were we have contention on the buffer - i.e. two threads trying to use the buffer at the same time, we just create a temp buffermanager and have the buffer dispose of it when it is done.
+  //
+  if (old_busy != BUFFER_FREE) {
+    return alloc_buffer_in_temp_manager(target_graphics, target_dc, target_rectangle);
+  }
+  
+  optional<graphics> surface;
+  target_location_ = point(target_rectangle.x(), target_rectangle.y());
+  
+  try {
+    if (target_graphics != null) {
+      intptr dest_dc = target_graphics->get_hdc();
+      try {
+        surface = create_buffer(dest_dc, -target_location_.x(), -target_location_.y(), target_rectangle.width(), target_rectangle.height());
+      } catch(...) {
+        
+      }
+      target_graphics->release_hdc(dest_dc);
+    }
+    else {
+      surface = create_buffer(target_dc, -target_location_.x(), -target_location_.y(), target_rectangle.width(), target_rectangle.height());
+    }
+    
+    buffer_ = buffered_graphics(surface.value()// *, *this* /, *target_graphics, target_dc, target_location_, virtual_size_);
+  } catch(...) {
+    busy_ = BUFFER_FREE; // free the buffer so it can be disposed.
+    throw;
+  }
+  return buffer_;
+}
+*/
 
 void buffered_graphics_context::destroy() {
   
