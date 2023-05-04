1c41c1b287f4bf1d6942c31b73b968b0a844ba9b
Gammasoft <gammasoft71@gmail.com>
2023-05-04 20:57:29 +0200

    generate option implementation

--- tools/xtdc/src/base_project.h
+++ tools/xtdc/src/base_project.h
@@ -30,6 +30,26 @@ namespace xtdc_command {
     
     const xtd::ustring& current_path() const noexcept {return current_path_;}
     
+  protected:
+    static std::tuple<std::vector<xtd::ustring>, std::vector<xtd::ustring>> get_sources(const xtd::ustring& current_path, const xtd::ustring& path) {
+      std::vector<xtd::ustring> headers;
+      std::vector<xtd::ustring> sources;
+      for (auto file : xtd::io::directory::get_files(path, "*.h"))
+        headers.push_back(file.replace(current_path + xtd::io::path::directory_separator_char(), ""));
+      for (auto file : xtd::io::directory::get_files(path, "*.cpp"))
+        sources.push_back(file.replace(current_path + xtd::io::path::directory_separator_char(), ""));
+      
+      for (auto sub_path : xtd::io::directory::get_directories(path)) {
+        auto [sub_headers, sub_sources] = get_sources(current_path, sub_path);
+        for (auto file : sub_headers)
+          headers.push_back(file);
+        for (auto file : sub_sources)
+          sources.push_back(file);
+      }
+      
+      return std::make_tuple(headers, sources);
+    }
+    
   private:
     xtd::ustring current_path_ = xtd::environment::current_directory();
   };
