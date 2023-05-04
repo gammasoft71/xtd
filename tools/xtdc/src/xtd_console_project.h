68b3a3ebbe7562b144788c05972e3561ef383a6f
Gammasoft <gammasoft71@gmail.com>
2023-05-04 22:34:04 +0200

    generate option implementation

--- tools/xtdc/src/xtd_console_project.h
+++ tools/xtdc/src/xtd_console_project.h
@@ -16,6 +16,10 @@ namespace xtdc_command {
       create_xtd_console_source(name, create_solution ? xtd::io::path::combine(current_path(), name) : current_path());
     }
     
+    void generate(const xtd::ustring& name, bool create_solution) const {
+      generate_xtd_console_cmakelists_txt(name, current_path());
+    }
+
   private:
     void create_cmakelists_txt(const xtd::ustring& name) const {
       std::vector<xtd::ustring> lines {
@@ -101,6 +105,29 @@ namespace xtdc_command {
       };
       
       xtd::io::file::write_all_lines(xtd::io::path::combine(path, "src", "program.cpp"), lines);
+    }
+
+    void generate_xtd_console_cmakelists_txt(const xtd::ustring& name, const xtd::ustring& path) const {
+      std::vector<xtd::ustring> lines {
+        "cmake_minimum_required(VERSION 3.3)",
+        "",
+        "# Project",
+        xtd::ustring::format("project({})", name),
+        "find_package(xtd REQUIRED)",
+        "add_sources(",
+      };
+      auto [headers, sources] = get_sources(path, path);
+      for (auto file : headers)
+        lines.push_back(xtd::ustring::format("  {}", file));
+      for (auto file : sources)
+        lines.push_back(xtd::ustring::format("  {}", file));
+      lines.push_back(")");
+      lines.push_back("target_type(CONSOLE_APPLICATION)");
+      lines.push_back("");
+      lines.push_back("# Install");
+      lines.push_back("install_component()");
+      
+      xtd::io::file::write_all_lines(xtd::io::path::combine(path, "CMakeLists.txt"), lines);
     }
   };
 }
