94036ae55a6f301fffef6618bae47a66d64dbed1
Gammasoft <gammasoft71@gmail.com>
2023-05-06 17:18:27 +0200

    Update architecture_id_tests.cpp

--- tests/xtd.core.unit_tests/src/xtd/tests/architecture_id_tests.cpp
+++ tests/xtd.core.unit_tests/src/xtd/tests/architecture_id_tests.cpp
@@ -10,16 +10,19 @@ namespace xtd::tests {
     void test_method_(unknown) {
       assert::are_equal(-1, enum_object<>::to_int32(architecture_id::unknown), csf_);
       assert::are_equal("unknown", enum_object<>::to_string(architecture_id::unknown), csf_);
+      assert::are_equal(architecture_id::unknown, enum_object<>::parse<architecture_id>("unknown"), csf_);
     }
     
     void test_method_(x86) {
       assert::are_equal(0, enum_object<>::to_int32(architecture_id::x86), csf_);
       assert::are_equal("x86", enum_object<>::to_string(architecture_id::x86), csf_);
+      assert::are_equal(architecture_id::x86, enum_object<>::parse<architecture_id>("x86"), csf_);
     }
     
     void test_method_(arm) {
       assert::are_equal(1, enum_object<>::to_int32(architecture_id::arm), csf_);
       assert::are_equal("arm", enum_object<>::to_string(architecture_id::arm), csf_);
+      assert::are_equal(architecture_id::arm, enum_object<>::parse<architecture_id>("arm"), csf_);
     }
   };
 }
