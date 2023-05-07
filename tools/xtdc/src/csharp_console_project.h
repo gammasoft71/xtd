5d82eb991be82d5eb77b58234473fd273355b9a7
Gammasoft <gammasoft71@gmail.com>
2023-05-07 22:51:04 +0200

    generate option implementation

--- tools/xtdc/src/csharp_console_project.h
+++ tools/xtdc/src/csharp_console_project.h
@@ -13,6 +13,10 @@ namespace xtdc_command {
       create_source(name, create_solution ? xtd::io::path::combine(current_path(), name) : current_path());
     }
     
+    void generate(const xtd::ustring& name, bool create_solution) const {
+      generate_cmakelists_txt(name, create_solution ? xtd::io::path::combine(current_path(), name) : current_path());
+    }
+
   private:
     void create_solution_cmakelists_txt(const xtd::ustring& name) const {
       std::vector<xtd::ustring> lines {
@@ -66,6 +70,29 @@ namespace xtdc_command {
       };
       
       xtd::io::file::write_all_lines(xtd::io::path::combine(path, "src", "Program.cs"), lines);
+    }
+    
+    void generate_cmakelists_txt(const xtd::ustring& name, const xtd::ustring& path) const {
+      std::vector<xtd::ustring> lines {
+        "cmake_minimum_required(VERSION 3.8)",
+        "",
+        "# Project",
+        xtd::ustring::format("project({} VERSION 1.0.0 LANGUAGES CSharp)", name),
+        "include(CSharpUtilities)",
+        "set(SOURCES",
+      };
+      for (auto file : get_csharp_sources(path, path))
+        lines.push_back(xtd::ustring::format("  {}", file));
+      lines.push_back(")");
+      lines.push_back("source_group(src FILES ${SOURCES})");
+      lines.push_back("");
+      lines.push_back("# Options");
+      lines.push_back("set_property(GLOBAL PROPERTY USE_FOLDERS ON)");
+      lines.push_back("");
+      lines.push_back("# Application properties");
+      lines.push_back("add_executable(${PROJECT_NAME} ${SOURCES})");
+
+      xtd::io::file::write_all_lines(xtd::io::path::combine(path, "CMakeLists.txt"), lines);
     }
   };
 }
