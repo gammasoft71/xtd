09a1c73b9ec03f1543043244ce61fca7a441b2ad
Gammasoft <gammasoft71@gmail.com>
2023-05-08 21:33:18 +0200

    generate option implementatioon

--- tools/xtdc/src/cpp_static_library_project.h
+++ tools/xtdc/src/cpp_static_library_project.h
@@ -15,6 +15,10 @@ namespace xtdc_command {
       create_source(name, create_solution ? xtd::io::path::combine(current_path(), name) : current_path());
     }
     
+    void generate(const xtd::ustring& name) const {
+      generate_cmakelists_txt(name, current_path());
+    }
+
   private:
     void create_solution_cmakelists_txt(const xtd::ustring& name) const {
       std::vector<xtd::ustring> lines {
@@ -104,6 +108,54 @@ namespace xtdc_command {
       };
       
       xtd::io::file::write_all_lines(xtd::io::path::combine(path, "src", "class1.cpp"), lines);
+    }
+    
+    void generate_cmakelists_txt(const xtd::ustring& name, const xtd::ustring& path) const {
+      std::vector<xtd::ustring> lines;
+      lines.push_back("cmake_minimum_required(VERSION 3.3)");
+      lines.push_back("");
+      lines.push_back("# Project");
+      lines.push_back(xtd::ustring::format("project({} VERSION 1.0.0)", name));
+      auto [headers, sources] = get_cpp_sources(path, path);
+      lines.push_back("set(INCLUDES");
+      for (auto file : headers)
+        lines.push_back(xtd::ustring::format("  {}", file));
+      lines.push_back(")");
+      lines.push_back("set(SOURCES");
+      for (auto file : sources)
+        lines.push_back(xtd::ustring::format("  {}", file));
+      lines.push_back(")");
+      lines.push_back("source_group(include FILES ${INCLUDES})");
+      lines.push_back("source_group(src FILES ${SOURCES})");
+      lines.push_back("");
+      lines.push_back("# Options");
+      lines.push_back("set(CMAKE_CXX_STANDARD 17)");
+      lines.push_back("set(CMAKE_CXX_STANDARD_REQUIRED ON)");
+      lines.push_back("set_property(GLOBAL PROPERTY USE_FOLDERS ON)");
+      lines.push_back("");
+      lines.push_back("# Application properties");
+      lines.push_back("add_library(${PROJECT_NAME} STATIC ${INCLUDES} ${SOURCES})");
+      lines.push_back("target_include_directories(${PROJECT_NAME} PUBLIC $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include> $<INSTALL_INTERFACE:include>)");
+      lines.push_back("");
+      lines.push_back("# Install");
+      lines.push_back(xtd::ustring::format("install(DIRECTORY include/. DESTINATION include/{})", name));
+      lines.push_back(xtd::ustring::format("file(WRITE ${{CMAKE_CURRENT_BINARY_DIR}}/{}Config.cmake", name));
+      lines.push_back("  \"include(CMakeFindDependencyMacro)\\n\"");
+      lines.push_back("  \"include(\\\"\\${CMAKE_CURRENT_LIST_DIR}/${PROJECT_NAME}.cmake\\\")\\n\"");
+      lines.push_back("  \"\\n\"");
+      lines.push_back(xtd::ustring::format("  \"get_filename_component({}_INCLUDE_DIRS \\\"\\${{CMAKE_CURRENT_LIST_DIR}}/../include\\\" ABSOLUTE)\\n\"", name.to_upper()));
+      lines.push_back(xtd::ustring::format("  \"get_filename_component({}_LIBRARIES_DIRS \\\"\\${{CMAKE_CURRENT_LIST_DIR}}/../lib\\\" ABSOLUTE)\\n\"", name.to_upper()));
+      lines.push_back("  \"\\n\"");
+      lines.push_back(xtd::ustring::format("  \"set({}_LIBRARIES {})\\n\"", name.to_upper(), name));
+      lines.push_back(xtd::ustring::format("  \"set({}_FOUND TRUE)\\n\"", name.to_upper()));
+      lines.push_back(")");
+      lines.push_back(xtd::ustring::format("install(FILES ${{CMAKE_CURRENT_BINARY_DIR}}/{}Config.cmake DESTINATION cmake)", name));
+      lines.push_back("install(FILES $<TARGET_FILE_DIR:${PROJECT_NAME}>/${PROJECT_NAME}${CMAKE_DEBUG_POSTFIX}.pdb DESTINATION lib CONFIGURATIONS Debug OPTIONAL)");
+      lines.push_back("install(FILES $<TARGET_FILE_DIR:${PROJECT_NAME}>/${PROJECT_NAME}.pdb DESTINATION lib CONFIGURATIONS Release OPTIONAL)");
+      lines.push_back("install(TARGETS ${PROJECT_NAME} EXPORT ${PROJECT_NAME} DESTINATION lib)");
+      lines.push_back("install(EXPORT ${PROJECT_NAME} DESTINATION cmake)");
+      
+      xtd::io::file::write_all_lines(xtd::io::path::combine(path, "CMakeLists.txt"), lines);
     }
   };
 }
