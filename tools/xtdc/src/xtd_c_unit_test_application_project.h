8d031c445ff47c2d580c84591c5b88daa72fc800
Gammasoft <gammasoft71@gmail.com>
2023-05-08 21:55:54 +0200

    generate option implementation

--- tools/xtdc/src/xtd_c_unit_test_application_project.h
+++ tools/xtdc/src/xtd_c_unit_test_application_project.h
@@ -7,11 +7,15 @@ namespace xtdc_command {
     explicit xtd_c_unit_test_application_project(const xtd::ustring& path) : base_project(path) {}
     
     void create(const xtd::ustring& name, bool create_solution) const {
-      create_cmakelists_txt(name);
+      create_cmakelists_txt(name, current_path());
+    }
+    
+    void generate(const xtd::ustring& name) const {
+      generate_cmakelists_txt(name, current_path());
     }
     
   private:
-    void create_cmakelists_txt(const xtd::ustring& name) const {
+    void create_cmakelists_txt(const xtd::ustring& name, const xtd::ustring& path) const {
       std::vector<xtd::ustring> lines {
         "cmake_minimum_required(VERSION 3.3)",
         "",
@@ -23,6 +27,30 @@ namespace xtdc_command {
         "",
         "# Projects"
       };
+      xtd::io::file::write_all_lines(xtd::io::path::combine(current_path(), "CMakeLists.txt"), lines);
+    }
+
+    void generate_cmakelists_txt(const xtd::ustring& name, const xtd::ustring& path) const {
+      std::vector<xtd::ustring> lines;
+      lines.push_back("cmake_minimum_required(VERSION 3.3)");
+      lines.push_back("");
+      lines.push_back("# Project");
+      lines.push_back(xtd::ustring::format("project({})", name));
+      lines.push_back("find_package(xtd REQUIRED)");
+      lines.push_back("add_sources(");
+      auto [headers, sources] = get_c_sources(path, path);
+      for (auto file : headers)
+        lines.push_back(xtd::ustring::format("  {}", file));
+      for (auto file : sources)
+        lines.push_back(xtd::ustring::format("  {}", file));
+      lines.push_back(")");
+      lines.push_back("target_type(TEST_APPLICATION)");
+      lines.push_back("");
+      lines.push_back("# Install");
+      lines.push_back("install_package()");
+      lines.push_back("");
+      lines.push_back("# Projects");
+
       xtd::io::file::write_all_lines(xtd::io::path::combine(current_path(), "CMakeLists.txt"), lines);
     }
   };
