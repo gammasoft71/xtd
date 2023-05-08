625d0b779efdb0db36e71b807ac807dc27775bf7
Gammasoft <gammasoft71@gmail.com>
2023-05-08 20:56:19 +0200

    generate option implementation

--- tools/xtdc/src/project_management.h
+++ tools/xtdc/src/project_management.h
@@ -416,7 +416,7 @@ namespace xtdc_command {
         case project_sdk::xtd_c: xtd_c_shared_library_project(path_).generate(name); break;
         default: std::map<project_language, xtd::action<const xtd::ustring&, bool>> {
           {project_language::c, {c_shared_library_project {path_}, &c_shared_library_project::generate}},
-          {project_language::cpp, {cpp_shared_library_project {path_}, &cpp_shared_library_project::create}},
+          {project_language::cpp, {cpp_shared_library_project {path_}, &cpp_shared_library_project::generate}},
           {project_language::csharp, {csharp_shared_library_project {path_}, &csharp_shared_library_project::create}},
           {project_language::objectivec, {objectivec_shared_library_project {path_}, &objectivec_shared_library_project::create}}
         } [language](name, create_solution); break;
