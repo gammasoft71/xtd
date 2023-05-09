2037c4153711670bb32da8a4db2cec276ec59d32
Gammasoft <gammasoft71@gmail.com>
2023-05-09 17:13:18 +0200

    add build_type_tests.cpp file

---
+++ tests/xtd.core.unit_tests/src/xtd/tests/build_type_tests.cpp
@@ -0,0 +1,23 @@
+#include <xtd/build_type.h>
+#include <xtd/xtd.tunit>
+
+using namespace xtd;
+using namespace xtd::tunit;
+
+namespace xtd::tests {
+  class test_class_(build_type_tests) {
+  public:
+    void test_method_(debug) {
+      assert::are_equal(0, enum_object<>::to_int32(build_type::debug), csf_);
+      assert::are_equal("debug", enum_object<>::to_string(build_type::debug), csf_);
+      assert::are_equal(build_type::debug, enum_object<>::parse<build_type>("debug"), csf_);
+    }
+    
+    void test_method_(release) {
+      assert::are_equal(1, enum_object<>::to_int32(build_type::release), csf_);
+      assert::are_equal("release", enum_object<>::to_string(build_type::release), csf_);
+      assert::are_equal(build_type::release, enum_object<>::parse<build_type>("release"), csf_);
+    }
+  };
+}
+
