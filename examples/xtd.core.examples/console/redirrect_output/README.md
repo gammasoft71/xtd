4942bfb65352cf609dcd90337f98c58840ae3f98
Gammasoft <gammasoft71@gmail.com>
2023-05-10 23:18:30 +0200

    Add redirect_output example

---
+++ examples/xtd.core.examples/console/redirrect_output/README.md
@@ -0,0 +1,25 @@
+# redirrect_output
+
+Shows how to use [xtd::console](https://gammasoft71.github.io/xtd/reference_guides/latest/classxtd_1_1console.html) class.
+
+# Sources
+
+[src/redirrect_output.cpp](src/redirrect_output.cpp)
+
+[CMakeLists.txt](CMakeLists.txt)
+
+# Build and run
+
+Open "Command Prompt" or "Terminal". Navigate to the folder that contains the project and type the following:
+
+```cmake
+xtdc run
+```
+
+# Output
+
+```
+Item1
+Item2
+Item3
+```
---
+++ examples/xtd.core.examples/console/redirrect_output/src/redirrect_output.cpp
@@ -0,0 +1,28 @@
+#include <xtd/xtd>
+
+using namespace xtd;
+using namespace xtd::io;
+
+auto main()->int {
+  auto output_file_path = path::combine(path::get_temp_path(), "xtd_example_write.txt");
+  
+  auto output_stream = xtd::io::file::open_write(output_file_path);
+  console::set_out(output_stream);
+ 
+  console::write_line("Item1");
+  console::write_line("Item2");
+  console::write_line("Item3");
+
+  output_stream.close();
+  console::set_out(console::open_standard_output());
+  
+  console::write_line(ustring::join(environment::new_line(), file::read_all_lines(output_file_path)));
+  
+  file::remove(output_file_path);
+}
+
+// This code can produce the following output:
+//
+// Item1
+// Item2
+// Item3
