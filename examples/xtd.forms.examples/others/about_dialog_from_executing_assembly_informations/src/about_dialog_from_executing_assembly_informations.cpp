26ffb26e58f29ac562e8baf7120edfee0524294a
Gammasoft <gammasoft71@gmail.com>
2023-05-14 17:06:11 +0200

    Add about_dialog_from_executing_assembly_informations example

---
+++ examples/xtd.forms.examples/others/about_dialog_from_executing_assembly_informations/src/about_dialog_from_executing_assembly_informations.cpp
@@ -0,0 +1,28 @@
+#include <xtd/xtd>
+#include "../resources/gammasoft_64x64.xpm"
+
+using namespace xtd;
+using namespace xtd::forms;
+
+class form1 : public form {
+public:
+  form1() {
+    text("About dialog from executng assembly information example");
+    controls().push_back(button1);
+    
+    button1.location({10, 10});
+    button1.text("About...");
+    button1.click += [&] {
+      static about_dialog dialog = about_dialog::from_executing_assembly_informations();
+      dialog.icon(xtd::drawing::bitmap(gammasoft_64x64_xpm));
+      dialog.show(*this);
+    };
+  }
+  
+private:
+  button button1;
+};
+
+auto main()->int {
+  application::run(form1 {});
+}
