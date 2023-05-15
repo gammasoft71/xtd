68ccf37f64ef282a5624e19c356958976d116c3d
Gammasoft <gammasoft71@gmail.com>
2023-05-15 23:05:11 +0200

    Fix warnings

--- tests/xtd.core.unit_tests/src/xtd/tests/cpp_language_tests.cpp
+++ tests/xtd.core.unit_tests/src/xtd/tests/cpp_language_tests.cpp
@@ -41,7 +41,7 @@ namespace xtd::tests {
       assert::are_equal(language_id::cpp20, cl.experimental_language(), csf_);
       assert::are_equal(2, cl.month(), csf_);
       assert::are_equal("C++ 20", cl.name(), csf_);
-      assert::are_equal(202002, cl.value(), csf_);
+      assert::are_equal(202002UL, cl.value(), csf_);
       assert::are_equal(version {2020, 2}, cl.version(), csf_);
       assert::are_equal("C++ 20 2020.2", cl.version_string(), csf_);
       assert::are_equal(2020, cl.year(), csf_);
@@ -57,7 +57,7 @@ namespace xtd::tests {
       assert::are_equal(language_id::cpp17, cl.experimental_language(), csf_);
       assert::are_equal(3, cl.month(), csf_);
       assert::are_equal("C++ 17", cl.name(), csf_);
-      assert::are_equal(201703, cl.value(), csf_);
+      assert::are_equal(201703UL, cl.value(), csf_);
       assert::are_equal(version {2017, 3}, cl.version(), csf_);
       assert::are_equal("C++ 17 2017.3", cl.version_string(), csf_);
       assert::are_equal(2017, cl.year(), csf_);
@@ -73,7 +73,7 @@ namespace xtd::tests {
       assert::are_equal(language_id::cpp14, cl.experimental_language(), csf_);
       assert::are_equal(2, cl.month(), csf_);
       assert::are_equal("C++ 14", cl.name(), csf_);
-      assert::are_equal(201402, cl.value(), csf_);
+      assert::are_equal(201402UL, cl.value(), csf_);
       assert::are_equal(version {2014, 2}, cl.version(), csf_);
       assert::are_equal("C++ 14 2014.2", cl.version_string(), csf_);
       assert::are_equal(2014, cl.year(), csf_);
@@ -89,7 +89,7 @@ namespace xtd::tests {
       assert::are_equal(language_id::cpp11, cl.experimental_language(), csf_);
       assert::are_equal(3, cl.month(), csf_);
       assert::are_equal("C++ 11", cl.name(), csf_);
-      assert::are_equal(201103, cl.value(), csf_);
+      assert::are_equal(201103UL, cl.value(), csf_);
       assert::are_equal(version {2011, 3}, cl.version(), csf_);
       assert::are_equal("C++ 11 2011.3", cl.version_string(), csf_);
       assert::are_equal(2011, cl.year(), csf_);
@@ -105,7 +105,7 @@ namespace xtd::tests {
       assert::are_equal(language_id::cpp98, cl.experimental_language(), csf_);
       assert::are_equal(11, cl.month(), csf_);
       assert::are_equal("C++ 98", cl.name(), csf_);
-      assert::are_equal(199711, cl.value(), csf_);
+      assert::are_equal(199711UL, cl.value(), csf_);
       assert::are_equal(version {1997, 11}, cl.version(), csf_);
       assert::are_equal("C++ 98 1997.11", cl.version_string(), csf_);
       assert::are_equal(1997, cl.year(), csf_);
@@ -121,7 +121,7 @@ namespace xtd::tests {
       assert::are_equal(language_id::cpp_pre98, cl.experimental_language(), csf_);
       assert::are_equal(1, cl.month(), csf_);
       assert::are_equal("C++ Pre 98", cl.name(), csf_);
-      assert::are_equal(1, cl.value(), csf_);
+      assert::are_equal(1UL, cl.value(), csf_);
       assert::are_equal(version {0, 1}, cl.version(), csf_);
       assert::are_equal("C++ Pre 98 0.1", cl.version_string(), csf_);
       assert::is_zero(cl.year(), csf_);
@@ -153,7 +153,7 @@ namespace xtd::tests {
       assert::are_equal(language_id::cpp20, cl.experimental_language(), csf_);
       assert::are_equal(7, cl.month(), csf_);
       assert::are_equal("Experimental C++ 20", cl.name(), csf_);
-      assert::are_equal(201707, cl.value(), csf_);
+      assert::are_equal(201707UL, cl.value(), csf_);
       assert::are_equal(version {2017, 7}, cl.version(), csf_);
       assert::are_equal("Experimental C++ 20 2017.7", cl.version_string(), csf_);
       assert::are_equal(2017, cl.year(), csf_);
