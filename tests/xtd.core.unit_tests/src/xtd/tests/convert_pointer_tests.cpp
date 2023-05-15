4788467ecfaa9f2c98e9c450d459706f9f2cba8e
Gammasoft <gammasoft71@gmail.com>
2023-05-15 18:40:01 +0200

    Add convert_pointer unit tests

--- tests/xtd.core.unit_tests/src/xtd/tests/convert_pointer_tests.cpp
+++ tests/xtd.core.unit_tests/src/xtd/tests/convert_pointer_tests.cpp
@@ -11,6 +11,70 @@ using namespace xtd::tunit;
 namespace xtd::tests {
   class test_class_(convert_pointer_tests) {
   public:
+    void test_method_(const_object_to_ptr) {
+      const guid* g1 = new guid();
+      
+      const guid* g2 = convert_pointer::to_ptr(*g1);
+      assert::are_equal(g1, g2, csf_);
+      delete g1;
+    }
+
+    void test_method_(object_to_ptr) {
+      guid* g1 = new guid();
+      
+      guid* g2 = convert_pointer::to_ptr(*g1);
+      assert::are_equal(g1, g2, csf_);
+      delete g1;
+    }
+    
+    void test_method_(const_object_to_other_ptr) {
+      const guid* g = new guid();
+      
+      const object* o = convert_pointer::to_ptr<object>(*g);
+      assert::are_equal(g, o, csf_);
+      delete g;
+    }
+    
+    void test_method_(object_to_other_ptr) {
+      guid* g = new guid();
+      
+      object* o = convert_pointer::to_ptr<object>(*g);
+      assert::are_equal(g, o, csf_);
+      delete g;
+    }
+    
+    void test_method_(const_ptr_to_ptr) {
+      const guid* g1 = new guid();
+      
+      const guid* g2 = convert_pointer::to_ptr(g1);
+      assert::are_equal(g1, g2, csf_);
+      delete g1;
+    }
+    
+    void test_method_(ptr_to_ptr) {
+      guid* g1 = new guid();
+      
+      guid* g2 = convert_pointer::to_ptr(g1);
+      assert::are_equal(g1, g2, csf_);
+      delete g1;
+    }
+    
+    void test_method_(const_ptr_to_other_ptr) {
+      const guid* g = new guid();
+      
+      const object* o = convert_pointer::to_ptr<object>(g);
+      assert::are_equal(g, o, csf_);
+      delete g;
+    }
+    
+    void test_method_(ptr_to_other_ptr) {
+      guid* g = new guid();
+      
+      object* o = convert_pointer::to_ptr<object>(g);
+      assert::are_equal(g, o, csf_);
+      delete g;
+    }
+
     void test_method_(const_object_to_ref) {
       const guid* g1 = new guid();
       
@@ -27,7 +91,7 @@ namespace xtd::tests {
       delete g1;
     }
     
-    void test_method_(const_object_to_ref_object) {
+    void test_method_(const_object_to_other_ref) {
       const guid* g = new guid();
 
       const object& o = convert_pointer::to_ref<object>(*g);
@@ -35,7 +99,7 @@ namespace xtd::tests {
       delete g;
     }
     
-    void test_method_(object_to_ref_object) {
+    void test_method_(object_to_other_ref) {
       guid* g = new guid();
       
       object& o = convert_pointer::to_ref<object>(*g);
@@ -57,14 +121,14 @@ namespace xtd::tests {
       delete d1;
     }
     
-    void test_method_(const_pointer_to_ref_object) {
+    void test_method_(const_pointer_to_other_ref) {
       const date_time* d = new date_time();
       const object& o = convert_pointer::to_ref<object>(d);
       assert::are_equal(d, &o, csf_);
       delete d;
     }
     
-    void test_method_(pointer_to_ref_object) {
+    void test_method_(pointer_to_other_ref) {
       date_time* d = new date_time();
       object& o = convert_pointer::to_ref<object>(d);
       assert::are_equal(d, &o, csf_);
