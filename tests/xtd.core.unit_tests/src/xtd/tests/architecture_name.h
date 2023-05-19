34586dfd248eb39de445693737729e4643f087d8
Gammasoft <gammasoft71@gmail.com>
2023-05-19 16:40:42 +0200

    Create architecture_name.h

---
+++ tests/xtd.core.unit_tests/src/xtd/tests/architecture_name.h
@@ -0,0 +1,14 @@
+#pragma one
+#if defined(__amd64__)
+#  define __architecture_name__ "x86_64"
+#elif defined(__arm__)
+#  define __architecture_name__ "arm"
+#elif defined(__aarch64__)
+#  define __architecture_name__ "arm64"
+#elif defined(__i386__)
+#  define __architecture_name__ "x86"
+#elif defined(__ia64__)
+#  define __architecture_name__ "x86_64"
+#else
+#  define __architecture_name__ "unknown"
+#endif
