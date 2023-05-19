937ccfd9c08945fc198b7d51745cb61d42fdbfa1
Gammasoft <gammasoft71@gmail.com>
2023-05-19 16:40:45 +0200

    Create compiler_name.h

---
+++ tests/xtd.core.unit_tests/src/xtd/tests/compiler_name.h
@@ -0,0 +1,11 @@
+#pragma one
+#if defined(_MSC_VER)
+#  define __compiler_name__ "Microsoft Visual Studio"
+#elif defined(__clang__)
+#  define __compiler_name__ "clang"
+#elif defined(__GNUC__)
+#  define __compiler_name__ "gcc"
+#else
+#  define __compiler_name__ "unknown"
+#endif
+
