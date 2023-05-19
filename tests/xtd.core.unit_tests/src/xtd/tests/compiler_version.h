80978b26e8d081957b14c1d545cbacc2fc7e2005
Gammasoft <gammasoft71@gmail.com>
2023-05-19 16:40:49 +0200

    Create compiler_version.h

---
+++ tests/xtd.core.unit_tests/src/xtd/tests/compiler_version.h
@@ -0,0 +1,10 @@
+#pragma one
+#if defined(_MSC_VER)
+#  define __compiler_version__ xtd::version {_MSC_VER / 100, _MSC_VER % 100, 0}
+#elif defined(__clang__)
+#  define __compiler_version__ xtd::version {__clang_major__, __clang_minor__, __clang_patchlevel__}
+#elif defined(__GNUC__)
+#  define __compiler_version__ xd::version {__GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__}
+#else
+#  define __compiler_version__ xtd::version {}
+#endif
