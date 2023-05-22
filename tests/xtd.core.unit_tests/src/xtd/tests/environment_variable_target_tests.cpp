a25ced08cfd2e3930b10077b7433e9b8f0fc679a
Gammasoft <gammasoft71@gmail.com>
2023-05-22 18:48:07 +0200

    Add environment_variable_target unit tests

---
+++ tests/xtd.core.unit_tests/src/xtd/tests/environment_variable_target_tests.cpp
@@ -0,0 +1,28 @@
+#include <xtd/environment_variable_target.h>
+#include <xtd/xtd.tunit>
+
+using namespace xtd;
+using namespace xtd::tunit;
+
+namespace xtd::tests {
+  class test_class_(environment_variable_target_tests) {
+  public:
+    void test_method_(process) {
+      assert::are_equal(0, enum_object<>::to_int32(environment_variable_target::process), csf_);
+      assert::are_equal("process", enum_object<>::to_string(environment_variable_target::process), csf_);
+      assert::are_equal(environment_variable_target::process, enum_object<>::parse<environment_variable_target>("process"), csf_);
+    }
+    
+    void test_method_(user) {
+      assert::are_equal(1, enum_object<>::to_int32(environment_variable_target::user), csf_);
+      assert::are_equal("user", enum_object<>::to_string(environment_variable_target::user), csf_);
+      assert::are_equal(environment_variable_target::user, enum_object<>::parse<environment_variable_target>("user"), csf_);
+    }
+    
+    void test_method_(machine) {
+      assert::are_equal(2, enum_object<>::to_int32(environment_variable_target::machine), csf_);
+      assert::are_equal("machine", enum_object<>::to_string(environment_variable_target::machine), csf_);
+      assert::are_equal(environment_variable_target::machine, enum_object<>::parse<environment_variable_target>("machine"), csf_);
+    }
+  };
+}
