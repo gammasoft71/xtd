c9139cba89ea8ffa00c912b027001b7f3b3c328a
Gammasoft <gammasoft71@gmail.com>
2023-05-22 23:55:44 +0200

    Add file_access unit tests

---
+++ tests/xtd.core.unit_tests/src/xtd/io/tests/file_access_tests.cpp
@@ -0,0 +1,29 @@
+#include <xtd/io/file_access.h>
+#include <xtd/xtd.tunit>
+
+using namespace xtd::io;
+using namespace xtd::tunit;
+
+namespace xtd::tests {
+  class test_class_(file_access_tests) {
+  public:
+    void test_method_(read) {
+      assert::are_equal(1, enum_object<>::to_int32(file_access::read), csf_);
+      assert::are_equal("read", enum_object<>::to_string(file_access::read), csf_);
+      assert::are_equal(file_access::read, enum_object<>::parse<file_access>("read"), csf_);
+    }
+    
+    void test_method_(write) {
+      assert::are_equal(2, enum_object<>::to_int32(file_access::write), csf_);
+      assert::are_equal("write", enum_object<>::to_string(file_access::write), csf_);
+      assert::are_equal(file_access::write, enum_object<>::parse<file_access>("write"), csf_);
+    }
+    
+    void test_method_(read_write) {
+      assert::are_equal(3, enum_object<>::to_int32(file_access::read_write), csf_);
+      assert::are_equal("read_write", enum_object<>::to_string(file_access::read_write), csf_);
+      assert::are_equal(file_access::read_write, enum_object<>::parse<file_access>("read_write"), csf_);
+    }
+  };
+}
+
