9f446e50552792e0dfb2d9bccad0999f355c1665
Gammasoft <gammasoft71@gmail.com>
2023-05-22 17:42:43 +0200

    Add xtd::enum_attribute unit tests

---
+++ tests/xtd.core.unit_tests/src/xtd/tests/enum_attribute_tests.cpp
@@ -0,0 +1,22 @@
+#include <xtd/enum_attribute.h>
+#include <xtd/xtd.tunit>
+
+using namespace xtd;
+using namespace xtd::tunit;
+
+namespace xtd::tests {
+  class test_class_(enum_attribute_tests) {
+  public:
+    void test_method_(standard) {
+      assert::are_equal(0, enum_object<>::to_int32(enum_attribute::standard), csf_);
+      assert::are_equal("standard", enum_object<>::to_string(enum_attribute::standard), csf_);
+      assert::are_equal(enum_attribute::standard, enum_object<>::parse<enum_attribute>("standard"), csf_);
+    }
+    
+    void test_method_(flags) {
+      assert::are_equal(1, enum_object<>::to_int32(enum_attribute::flags), csf_);
+      assert::are_equal("flags", enum_object<>::to_string(enum_attribute::flags), csf_);
+      assert::are_equal(enum_attribute::flags, enum_object<>::parse<enum_attribute>("flags"), csf_);
+    }
+  };
+}
