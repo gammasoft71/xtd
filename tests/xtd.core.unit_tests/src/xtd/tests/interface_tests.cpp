dbe04e16db17e753a9b36da09684382a4797da52
Gammasoft <gammasoft71@gmail.com>
2023-05-22 15:55:58 +0200

    Add interface uni tests

---
+++ tests/xtd.core.unit_tests/src/xtd/tests/interface_tests.cpp
@@ -0,0 +1,53 @@
+#include <xtd/interface.h>
+#include <xtd/ustring.h>
+#include <xtd/xtd.tunit>
+
+using namespace std;
+using namespace xtd;
+using namespace xtd::tunit;
+
+namespace xtd::tests {
+  class test_class_(interface_tests) {
+    class iinterface_test interface_ {
+    public:
+      virtual int value() const noexcept = 0;
+    };
+  
+    class interface_test : public iinterface_test {
+    public:
+      interface_test() = default;
+      interface_test(interface_test&&) = default;
+      interface_test(const interface_test&) = default;
+      interface_test& operator =(const interface_test&) = default;
+      interface_test(int value) : value_(value) {}
+
+      int value() const noexcept override {return value_;}
+      
+    private:
+      int value_ = 0;
+    };
+      
+  public:
+    void test_method_(interface_test_value) {
+      interface_test it {42};
+      assert::are_equal(42, it.value(), csf_);
+      assert::are_equal(42, as<iinterface_test>(it).value(), csf_);
+    }
+
+    void test_method_(iinterface_test_value) {
+      interface_test it {42};
+      iinterface_test& iit = it;
+      assert::are_equal(42, it.value(), csf_);
+      assert::are_equal(42, iit.value(), csf_);
+      assert::are_equal(42, as<interface_test>(iit).value(), csf_);
+    }
+
+    void test_method_(interface_value) {
+      interface_test it {42};
+      interface& i = it;
+      assert::are_equal(42, it.value(), csf_);
+      assert::are_equal(42, as<iinterface_test>(i).value(), csf_);
+      assert::are_equal(42, as<interface_test>(i).value(), csf_);
+    }
+  };
+}
