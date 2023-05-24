f7c31683b5807faa7a6184aea384c86a7b6b5744
Gammasoft <gammasoft71@gmail.com>
2023-05-24 23:39:29 +0200

    Add string_split_options unit tests

---
+++ tests/xtd.core.unit_tests/src/xtd/tests/string_split_options_tests.cpp
@@ -0,0 +1,22 @@
+#include <xtd/string_split_options.h>
+#include <xtd/xtd.tunit>
+
+using namespace xtd;
+using namespace xtd::tunit;
+
+namespace xtd::tests {
+  class test_class_(string_split_options_tests) {
+  public:
+    void test_method_(none) {
+      assert::are_equal(0, enum_object<>::to_int32(string_split_options::none), csf_);
+      assert::are_equal("none", enum_object<>::to_string(string_split_options::none), csf_);
+      assert::are_equal(string_split_options::none, enum_object<>::parse<string_split_options>("none"), csf_);
+    }
+    
+    void test_method_(remove_empty_entries) {
+      assert::are_equal(1, enum_object<>::to_int32(string_split_options::remove_empty_entries), csf_);
+      assert::are_equal("remove_empty_entries", enum_object<>::to_string(string_split_options::remove_empty_entries), csf_);
+      assert::are_equal(string_split_options::remove_empty_entries, enum_object<>::parse<string_split_options>("remove_empty_entries"), csf_);
+    }
+  };
+}
