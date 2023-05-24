166bba81e0bd66910fa46fa4bdc96451dacd55da
Gammasoft <gammasoft71@gmail.com>
2023-05-24 16:40:29 +0200

    Add select_mode unit tests

---
+++ tests/xtd.core.unit_tests/src/xtd/net/sockets/tests/select_mode_tests.cpp
@@ -0,0 +1,28 @@
+#include <xtd/net/sockets/select_mode.h>
+#include <xtd/xtd.tunit>
+
+using namespace xtd::net::sockets;
+using namespace xtd::tunit;
+
+namespace xtd::tests {
+  class test_class_(select_mode_tests) {
+  public:
+    void test_method_(select_read) {
+      assert::are_equal(0, enum_object<>::to_int32(select_mode::select_read), csf_);
+      assert::are_equal("select_read", enum_object<>::to_string(select_mode::select_read), csf_);
+      assert::are_equal(select_mode::select_read, enum_object<>::parse<select_mode>("select_read"), csf_);
+    }
+    
+    void test_method_(select_write) {
+      assert::are_equal(1, enum_object<>::to_int32(select_mode::select_write), csf_);
+      assert::are_equal("select_write", enum_object<>::to_string(select_mode::select_write), csf_);
+      assert::are_equal(select_mode::select_write, enum_object<>::parse<select_mode>("select_write"), csf_);
+    }
+    
+    void test_method_(select_error) {
+      assert::are_equal(2, enum_object<>::to_int32(select_mode::select_error), csf_);
+      assert::are_equal("select_error", enum_object<>::to_string(select_mode::select_error), csf_);
+      assert::are_equal(select_mode::select_error, enum_object<>::parse<select_mode>("select_error"), csf_);
+    }
+  };
+}
