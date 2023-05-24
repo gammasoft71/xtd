bcbf3b03f82380cf52893deaa6d680bf79f9cba9
Gammasoft <gammasoft71@gmail.com>
2023-05-24 16:38:19 +0200

    Add search_option unit tests

---
+++ tests/xtd.core.unit_tests/src/xtd/io/tests/search_option_tests.cpp
@@ -0,0 +1,22 @@
+#include <xtd/io/search_option.h>
+#include <xtd/xtd.tunit>
+
+using namespace xtd::io;
+using namespace xtd::tunit;
+
+namespace xtd::tests {
+  class test_class_(search_option_tests) {
+  public:
+    void test_method_(top_directory_only) {
+      assert::are_equal(0, enum_object<>::to_int32(search_option::top_directory_only), csf_);
+      assert::are_equal("top_directory_only", enum_object<>::to_string(search_option::top_directory_only), csf_);
+      assert::are_equal(search_option::top_directory_only, enum_object<>::parse<search_option>("top_directory_only"), csf_);
+    }
+
+    void test_method_(all_directories) {
+      assert::are_equal(1, enum_object<>::to_int32(search_option::all_directories), csf_);
+      assert::are_equal("all_directories", enum_object<>::to_string(search_option::all_directories), csf_);
+      assert::are_equal(search_option::all_directories, enum_object<>::parse<search_option>("all_directories"), csf_);
+    }
+  };
+}
