17ccf67192412115ebbca7a349568df968802c9f
Gammasoft <gammasoft71@gmail.com>
2023-05-24 18:57:02 +0200

    Add socket_shutdown unit tests

---
+++ tests/xtd.core.unit_tests/src/xtd/net/sockets/tests/socket_shutdown_tests.cpp
@@ -0,0 +1,28 @@
+#include <xtd/net/sockets/socket_shutdown.h>
+#include <xtd/xtd.tunit>
+
+using namespace xtd::net::sockets;
+using namespace xtd::tunit;
+
+namespace xtd::tests {
+  class test_class_(socket_shutdown_tests) {
+  public:
+    void test_method_(receive) {
+      assert::are_equal(0, enum_object<>::to_int32(socket_shutdown::receive), csf_);
+      assert::are_equal("receive", enum_object<>::to_string(socket_shutdown::receive), csf_);
+      assert::are_equal(socket_shutdown::receive, enum_object<>::parse<socket_shutdown>("receive"), csf_);
+    }
+    
+    void test_method_(send) {
+      assert::are_equal(1, enum_object<>::to_int32(socket_shutdown::send), csf_);
+      assert::are_equal("send", enum_object<>::to_string(socket_shutdown::send), csf_);
+      assert::are_equal(socket_shutdown::send, enum_object<>::parse<socket_shutdown>("send"), csf_);
+    }
+    
+    void test_method_(both) {
+      assert::are_equal(2, enum_object<>::to_int32(socket_shutdown::both), csf_);
+      assert::are_equal("both", enum_object<>::to_string(socket_shutdown::both), csf_);
+      assert::are_equal(socket_shutdown::both, enum_object<>::parse<socket_shutdown>("both"), csf_);
+    }
+  };
+}
