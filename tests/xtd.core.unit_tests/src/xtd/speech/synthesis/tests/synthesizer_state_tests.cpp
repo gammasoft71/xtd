3c71391b1e3fa6854b37a58d3cc045dd288a6aa1
Gammasoft <gammasoft71@gmail.com>
2023-05-25 15:19:17 +0200

    Add synthesizer_state unit tests

---
+++ tests/xtd.core.unit_tests/src/xtd/speech/synthesis/tests/synthesizer_state_tests.cpp
@@ -0,0 +1,28 @@
+#include <xtd/speech/synthesis/synthesizer_state.h>
+#include <xtd/xtd.tunit>
+
+using namespace xtd::speech::synthesis;
+using namespace xtd::tunit;
+
+namespace xtd::tests {
+  class test_class_(synthesizer_state_tests) {
+  public:
+    void test_method_(ready) {
+      assert::are_equal(0, enum_object<>::to_int32(synthesizer_state::ready), csf_);
+      assert::are_equal("ready", enum_object<>::to_string(synthesizer_state::ready), csf_);
+      assert::are_equal(synthesizer_state::ready, enum_object<>::parse<synthesizer_state>("ready"), csf_);
+    }
+    
+    void test_method_(speaking) {
+      assert::are_equal(1, enum_object<>::to_int32(synthesizer_state::speaking), csf_);
+      assert::are_equal("speaking", enum_object<>::to_string(synthesizer_state::speaking), csf_);
+      assert::are_equal(synthesizer_state::speaking, enum_object<>::parse<synthesizer_state>("speaking"), csf_);
+    }
+    
+    void test_method_(pause) {
+      assert::are_equal(2, enum_object<>::to_int32(synthesizer_state::pause), csf_);
+      assert::are_equal("pause", enum_object<>::to_string(synthesizer_state::pause), csf_);
+      assert::are_equal(synthesizer_state::pause, enum_object<>::parse<synthesizer_state>("pause"), csf_);
+    }
+  };
+}
