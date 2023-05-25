7a6f1b62396833b0aef88f910620dac1f8e9e717
Gammasoft <gammasoft71@gmail.com>
2023-05-25 15:56:31 +0200

    Add uri_kind unit tests

---
+++ tests/xtd.core.unit_tests/src/xtd/tests/uri_kind_tests.cpp
@@ -0,0 +1,28 @@
+#include <xtd/uri_kind.h>
+#include <xtd/xtd.tunit>
+
+using namespace xtd;
+using namespace xtd::tunit;
+
+namespace xtd::tests {
+  class test_class_(uri_kind_tests) {
+  public:
+    void test_method_(relative_or_absolute) {
+      assert::are_equal(0, enum_object<>::to_int32(uri_kind::relative_or_absolute), csf_);
+      assert::are_equal("relative_or_absolute", enum_object<>::to_string(uri_kind::relative_or_absolute), csf_);
+      assert::are_equal(uri_kind::relative_or_absolute, enum_object<>::parse<uri_kind>("relative_or_absolute"), csf_);
+    }
+    
+    void test_method_(absolute) {
+      assert::are_equal(1, enum_object<>::to_int32(uri_kind::absolute), csf_);
+      assert::are_equal("absolute", enum_object<>::to_string(uri_kind::absolute), csf_);
+      assert::are_equal(uri_kind::absolute, enum_object<>::parse<uri_kind>("absolute"), csf_);
+    }
+    
+    void test_method_(relative) {
+      assert::are_equal(2, enum_object<>::to_int32(uri_kind::relative), csf_);
+      assert::are_equal("relative", enum_object<>::to_string(uri_kind::relative), csf_);
+      assert::are_equal(uri_kind::relative, enum_object<>::parse<uri_kind>("relative"), csf_);
+    }
+  };
+}
