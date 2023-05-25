7bf137697b61d372ed7ccca76914f1cbd7407327
Gammasoft <gammasoft71@gmail.com>
2023-05-25 15:29:13 +0200

    Add trace_level unit tests

---
+++ tests/xtd.core.unit_tests/src/xtd/diagnostics/tests/trace_level_tests.cpp
@@ -0,0 +1,40 @@
+#include <xtd/diagnostics/trace_level.h>
+#include <xtd/xtd.tunit>
+
+using namespace xtd::diagnostics;
+using namespace xtd::tunit;
+
+namespace xtd::tests {
+  class test_class_(trace_level_tests) {
+  public:
+    void test_method_(off) {
+      assert::are_equal(0, enum_object<>::to_int32(trace_level::off), csf_);
+      assert::are_equal("off", enum_object<>::to_string(trace_level::off), csf_);
+      assert::are_equal(trace_level::off, enum_object<>::parse<trace_level>("off"), csf_);
+    }
+    
+    void test_method_(error) {
+      assert::are_equal(1, enum_object<>::to_int32(trace_level::error), csf_);
+      assert::are_equal("error", enum_object<>::to_string(trace_level::error), csf_);
+      assert::are_equal(trace_level::error, enum_object<>::parse<trace_level>("error"), csf_);
+    }
+    
+    void test_method_(warning) {
+      assert::are_equal(2, enum_object<>::to_int32(trace_level::warning), csf_);
+      assert::are_equal("warning", enum_object<>::to_string(trace_level::warning), csf_);
+      assert::are_equal(trace_level::warning, enum_object<>::parse<trace_level>("warning"), csf_);
+    }
+    
+    void test_method_(info) {
+      assert::are_equal(3, enum_object<>::to_int32(trace_level::info), csf_);
+      assert::are_equal("info", enum_object<>::to_string(trace_level::info), csf_);
+      assert::are_equal(trace_level::info, enum_object<>::parse<trace_level>("info"), csf_);
+    }
+    
+    void test_method_(verbose) {
+      assert::are_equal(4, enum_object<>::to_int32(trace_level::verbose), csf_);
+      assert::are_equal("verbose", enum_object<>::to_string(trace_level::verbose), csf_);
+      assert::are_equal(trace_level::verbose, enum_object<>::parse<trace_level>("verbose"), csf_);
+    }
+  };
+}
