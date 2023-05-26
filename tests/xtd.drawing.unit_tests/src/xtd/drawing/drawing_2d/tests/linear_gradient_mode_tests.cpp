42b4422c2c06b9dc63ce36aa3adc202bdc96d9d6
Gammasoft <gammasoft71@gmail.com>
2023-05-26 21:20:33 +0200

    Add linear_gradient_mode unit tests

---
+++ tests/xtd.drawing.unit_tests/src/xtd/drawing/drawing_2d/tests/linear_gradient_mode_tests.cpp
@@ -0,0 +1,35 @@
+#include <xtd/drawing/drawing_2d/linear_gradient_mode.h>
+#include <xtd/xtd.tunit>
+
+using namespace xtd::drawing::drawing_2d;
+using namespace xtd::tunit;
+
+namespace xtd::tests {
+  class test_class_(linear_gradient_mode_tests) {
+  public:
+    void test_method_(horizontal) {
+      assert::are_equal(0, enum_object<>::to_int32(linear_gradient_mode::horizontal), csf_);
+      assert::are_equal("horizontal", enum_object<>::to_string(linear_gradient_mode::horizontal), csf_);
+      assert::are_equal(linear_gradient_mode::horizontal, enum_object<>::parse<linear_gradient_mode>("horizontal"), csf_);
+    }
+    
+    void test_method_(vertical) {
+      assert::are_equal(1, enum_object<>::to_int32(linear_gradient_mode::vertical), csf_);
+      assert::are_equal("vertical", enum_object<>::to_string(linear_gradient_mode::vertical), csf_);
+      assert::are_equal(linear_gradient_mode::vertical, enum_object<>::parse<linear_gradient_mode>("vertical"), csf_);
+    }
+    
+    void test_method_(forward_diagonal) {
+      assert::are_equal(2, enum_object<>::to_int32(linear_gradient_mode::forward_diagonal), csf_);
+      assert::are_equal("forward_diagonal", enum_object<>::to_string(linear_gradient_mode::forward_diagonal), csf_);
+      assert::are_equal(linear_gradient_mode::forward_diagonal, enum_object<>::parse<linear_gradient_mode>("forward_diagonal"), csf_);
+    }
+    
+    void test_method_(backward_diagonal) {
+      assert::are_equal(3, enum_object<>::to_int32(linear_gradient_mode::backward_diagonal), csf_);
+      assert::are_equal("backward_diagonal", enum_object<>::to_string(linear_gradient_mode::backward_diagonal), csf_);
+      assert::are_equal(linear_gradient_mode::backward_diagonal, enum_object<>::parse<linear_gradient_mode>("backward_diagonal"), csf_);
+    }
+  };
+}
+
