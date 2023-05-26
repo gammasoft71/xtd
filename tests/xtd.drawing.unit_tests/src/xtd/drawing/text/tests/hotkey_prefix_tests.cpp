6052977c1d0adaef8f935e99f66237485d6ee7dc
Gammasoft <gammasoft71@gmail.com>
2023-05-26 13:14:38 +0200

    Add hotkey_prefix unit tests

---
+++ tests/xtd.drawing.unit_tests/src/xtd/drawing/text/tests/hotkey_prefix_tests.cpp
@@ -0,0 +1,28 @@
+#include <xtd/drawing/text/hotkey_prefix.h>
+#include <xtd/xtd.tunit>
+
+using namespace xtd::drawing::text;
+using namespace xtd::tunit;
+
+namespace xtd::tests {
+  class test_class_(hotkey_prefix_tests) {
+  public:
+    void test_method_(none) {
+      assert::are_equal(0, enum_object<>::to_int32(hotkey_prefix::none), csf_);
+      assert::are_equal("none", enum_object<>::to_string(hotkey_prefix::none), csf_);
+      assert::are_equal(hotkey_prefix::none, enum_object<>::parse<hotkey_prefix>("none"), csf_);
+    }
+    
+    void test_method_(show) {
+      assert::are_equal(1, enum_object<>::to_int32(hotkey_prefix::show), csf_);
+      assert::are_equal("show", enum_object<>::to_string(hotkey_prefix::show), csf_);
+      assert::are_equal(hotkey_prefix::show, enum_object<>::parse<hotkey_prefix>("show"), csf_);
+    }
+    
+    void test_method_(hide) {
+      assert::are_equal(2, enum_object<>::to_int32(hotkey_prefix::hide), csf_);
+      assert::are_equal("hide", enum_object<>::to_string(hotkey_prefix::hide), csf_);
+      assert::are_equal(hotkey_prefix::hide, enum_object<>::parse<hotkey_prefix>("hide"), csf_);
+    }
+  };
+}
