c42a0fad6c0dee1d4bf28f292eef0ba424c34d2e
Gammasoft <gammasoft71@gmail.com>
2023-05-26 22:27:21 +0200

    Add anchor_styles unit tests

---
+++ tests/xtd.forms.unit_tests/src/xtd/forms/tests/anchor_styles_tests.cpp
@@ -0,0 +1,40 @@
+#include <xtd/forms/anchor_styles.h>
+#include <xtd/xtd.tunit>
+
+using namespace xtd::forms;
+using namespace xtd::tunit;
+
+namespace xtd::tests {
+  class test_class_(anchor_styles_tests) {
+  public:
+    void test_method_(none) {
+      assert::are_equal(0, enum_object<>::to_int32(anchor_styles::none), csf_);
+      assert::are_equal("none", enum_object<>::to_string(anchor_styles::none), csf_);
+      assert::are_equal(anchor_styles::none, enum_object<>::parse<anchor_styles>("none"), csf_);
+    }
+    
+    void test_method_(top) {
+      assert::are_equal(1, enum_object<>::to_int32(anchor_styles::top), csf_);
+      assert::are_equal("top", enum_object<>::to_string(anchor_styles::top), csf_);
+      assert::are_equal(anchor_styles::top, enum_object<>::parse<anchor_styles>("top"), csf_);
+    }
+    
+    void test_method_(bottom) {
+      assert::are_equal(2, enum_object<>::to_int32(anchor_styles::bottom), csf_);
+      assert::are_equal("bottom", enum_object<>::to_string(anchor_styles::bottom), csf_);
+      assert::are_equal(anchor_styles::bottom, enum_object<>::parse<anchor_styles>("bottom"), csf_);
+    }
+    
+    void test_method_(left) {
+      assert::are_equal(4, enum_object<>::to_int32(anchor_styles::left), csf_);
+      assert::are_equal("left", enum_object<>::to_string(anchor_styles::left), csf_);
+      assert::are_equal(anchor_styles::left, enum_object<>::parse<anchor_styles>("left"), csf_);
+    }
+    
+    void test_method_(right) {
+      assert::are_equal(8, enum_object<>::to_int32(anchor_styles::right), csf_);
+      assert::are_equal("right", enum_object<>::to_string(anchor_styles::right), csf_);
+      assert::are_equal(anchor_styles::right, enum_object<>::parse<anchor_styles>("right"), csf_);
+    }
+  };
+}
