0efa2134660c86b97ebbf79a6edf3247cc39df79
Gammasoft <gammasoft71@gmail.com>
2023-05-26 12:58:33 +0200

    Add font_style unit tests

---
+++ tests/xtd.drawing.unit_tests/src/xtd/drawing/text/tests/generic_font_families_tests.cpp
@@ -0,0 +1,28 @@
+#include <xtd/drawing/text/generic_font_families.h>
+#include <xtd/xtd.tunit>
+
+using namespace xtd::drawing::text;
+using namespace xtd::tunit;
+
+namespace xtd::tests {
+  class test_class_(generic_font_families_tests) {
+  public:
+    void test_method_(serif) {
+      assert::are_equal(0, enum_object<>::to_int32(generic_font_families::serif), csf_);
+      assert::are_equal("serif", enum_object<>::to_string(generic_font_families::serif), csf_);
+      assert::are_equal(generic_font_families::serif, enum_object<>::parse<generic_font_families>("serif"), csf_);
+    }
+    
+    void test_method_(sans_serif) {
+      assert::are_equal(1, enum_object<>::to_int32(generic_font_families::sans_serif), csf_);
+      assert::are_equal("sans_serif", enum_object<>::to_string(generic_font_families::sans_serif), csf_);
+      assert::are_equal(generic_font_families::sans_serif, enum_object<>::parse<generic_font_families>("sans_serif"), csf_);
+    }
+    
+    void test_method_(monospace) {
+      assert::are_equal(2, enum_object<>::to_int32(generic_font_families::monospace), csf_);
+      assert::are_equal("monospace", enum_object<>::to_string(generic_font_families::monospace), csf_);
+      assert::are_equal(generic_font_families::monospace, enum_object<>::parse<generic_font_families>("monospace"), csf_);
+    }
+  };
+}
