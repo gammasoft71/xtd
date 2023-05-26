3a07d70e877982d6d1f7e2c9c2a11b54a347492d
Gammasoft <gammasoft71@gmail.com>
2023-05-26 21:45:38 +0200

    Add string_alignment unit tests

---
+++ tests/xtd.drawing.unit_tests/src/xtd/drawing/tests/string_alignment_tests.cpp
@@ -0,0 +1,29 @@
+#include <xtd/drawing/string_alignment.h>
+#include <xtd/xtd.tunit>
+
+using namespace xtd::drawing;
+using namespace xtd::tunit;
+
+namespace xtd::tests {
+  class test_class_(string_alignment_tests) {
+  public:
+    void test_method_(near) {
+      assert::are_equal(0, enum_object<>::to_int32(string_alignment::near), csf_);
+      assert::are_equal("near", enum_object<>::to_string(string_alignment::near), csf_);
+      assert::are_equal(string_alignment::near, enum_object<>::parse<string_alignment>("near"), csf_);
+    }
+    
+    void test_method_(center) {
+      assert::are_equal(1, enum_object<>::to_int32(string_alignment::center), csf_);
+      assert::are_equal("center", enum_object<>::to_string(string_alignment::center), csf_);
+      assert::are_equal(string_alignment::center, enum_object<>::parse<string_alignment>("center"), csf_);
+    }
+    
+    void test_method_(far) {
+      assert::are_equal(2, enum_object<>::to_int32(string_alignment::far), csf_);
+      assert::are_equal("far", enum_object<>::to_string(string_alignment::far), csf_);
+      assert::are_equal(string_alignment::far, enum_object<>::parse<string_alignment>("far"), csf_);
+    }
+  };
+}
+
