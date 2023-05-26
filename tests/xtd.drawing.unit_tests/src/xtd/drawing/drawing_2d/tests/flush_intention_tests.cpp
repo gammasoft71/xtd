39f32261f3130cda78c5475d206c52dbf1298208
Gammasoft <gammasoft71@gmail.com>
2023-05-26 12:50:39 +0200

    Add flush_intention unit tests

---
+++ tests/xtd.drawing.unit_tests/src/xtd/drawing/drawing_2d/tests/flush_intention_tests.cpp
@@ -0,0 +1,22 @@
+#include <xtd/drawing/drawing_2d/flush_intention.h>
+#include <xtd/xtd.tunit>
+
+using namespace xtd::drawing::drawing_2d;
+using namespace xtd::tunit;
+
+namespace xtd::tests {
+  class test_class_(flush_intention_tests) {
+  public:
+    void test_method_(flush) {
+      assert::are_equal(0, enum_object<>::to_int32(flush_intention::flush), csf_);
+      assert::are_equal("flush", enum_object<>::to_string(flush_intention::flush), csf_);
+      assert::are_equal(flush_intention::flush, enum_object<>::parse<flush_intention>("flush"), csf_);
+    }
+    
+    void test_method_(sync) {
+      assert::are_equal(1, enum_object<>::to_int32(flush_intention::sync), csf_);
+      assert::are_equal("sync", enum_object<>::to_string(flush_intention::sync), csf_);
+      assert::are_equal(flush_intention::sync, enum_object<>::parse<flush_intention>("sync"), csf_);
+    }
+  };
+}
