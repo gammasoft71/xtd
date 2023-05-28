9653edd350397089eff8ff56e7590be7f6174aa6
Gammasoft <gammasoft71@gmail.com>
2023-05-28 12:48:30 +0200

    Add file_assert unit tests

---
+++ tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/file_assert_are_not_equal_string_failed_tests.cpp
@@ -0,0 +1,36 @@
+#include "assert_unit_tests.h"
+#include <xtd/xtd.tunit>
+
+namespace xtd::tunit::tests {
+  class test_class_(file_assert_are_not_equal_string_failed_tests) {
+  public:
+    void test_method_(test_case_failed) {
+      xtd::io::file::write_all_text(xtd::io::path::combine(xtd::io::path::get_temp_path(), "test_tunit_file1.txt"), "Test xtd::tunit::file_assert::are_not_equal.");
+      xtd::io::file::write_all_text(xtd::io::path::combine(xtd::io::path::get_temp_path(), "test_tunit_file2.txt"), "Test xtd::tunit::file_assert::are_not_equal.");
+      
+      file_assert::are_not_equal(xtd::io::path::combine(xtd::io::path::get_temp_path(), "test_tunit_file2.txt"), xtd::io::path::combine(xtd::io::path::get_temp_path(), "test_tunit_file1.txt"), csf_);
+      
+      xtd::io::file::remove(xtd::io::path::combine(xtd::io::path::get_temp_path(), "test_tunit_file1.txt"));
+      xtd::io::file::remove(xtd::io::path::combine(xtd::io::path::get_temp_path(), "test_tunit_file2.txt"));
+    }
+  };
+}
+
+void test_(file_assert_are_not_equal_string_failed_tests, test_output) {
+  xtd::tunit::settings::default_settings().exit_status(0);
+  xtd::tunit::settings::default_settings().filter_tests("file_assert_are_not_equal_string_failed_tests.*");
+  std::stringstream ss;
+  xtd::tunit::unit_test(std::make_unique<assert_unit_tests::unit_tests_event_listener>(ss)).run();
+  assert_value_("Start 1 test from 1 test case\n"
+    "  FAILED  file_assert_are_not_equal_string_failed_tests.test_case_failed\n"
+    "    Expected: not equal <std::__1::basic_ifstream<char, std::__1::char_traits<char> >>\n"
+    "    But was:  <std::__1::basic_ifstream<char, std::__1::char_traits<char> >>\n"
+    "End 1 test from 1 test case ran.\n", ss.str());
+}
+
+void test_(file_assert_are_not_equal_string_failed_tests, test_result) {
+  xtd::tunit::settings::default_settings().exit_status(0);
+  xtd::tunit::settings::default_settings().filter_tests("file_assert_are_not_equal_string_failed_tests.*");
+  std::stringstream ss;
+  assert_value_(1, xtd::tunit::unit_test(std::make_unique<assert_unit_tests::unit_tests_event_listener>(ss)).run());
+}
