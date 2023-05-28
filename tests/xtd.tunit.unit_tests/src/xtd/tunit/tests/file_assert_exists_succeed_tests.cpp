d9596a7a6495af928a75f22b280e377a03587710
Gammasoft <gammasoft71@gmail.com>
2023-05-28 22:01:53 +0200

    Add file_assert unit tests

---
+++ tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/file_assert_exists_succeed_tests.cpp
@@ -0,0 +1,32 @@
+#include "assert_unit_tests.h"
+#include <xtd/xtd.tunit>
+
+namespace xtd::tunit::tests {
+  class test_class_(file_assert_exists_succeed_tests) {
+  public:
+    void test_method_(test_case_succeed) {
+      xtd::io::file::write_all_text(xtd::io::path::combine(xtd::io::path::get_temp_path(), "test_tunit_file.txt"), "Test xtd::tunit::file_assert::are_equal.");
+      
+      file_assert::exists(xtd::io::path::combine(xtd::io::path::get_temp_path(), "test_tunit_file.txt"), csf_);
+      
+      xtd::io::file::remove(xtd::io::path::combine(xtd::io::path::get_temp_path(), "test_tunit_file.txt"));
+    }
+  };
+}
+
+void test_(file_assert_exists_succeed_tests, test_output) {
+  xtd::tunit::settings::default_settings().exit_status(0);
+  xtd::tunit::settings::default_settings().filter_tests("file_assert_exists_succeed_tests.*");
+  std::stringstream ss;
+  xtd::tunit::unit_test(std::make_unique<assert_unit_tests::unit_tests_event_listener>(ss)).run();
+  assert_value_("Start 1 test from 1 test case\n"
+    "  SUCCEED file_assert_exists_succeed_tests.test_case_succeed\n"
+    "End 1 test from 1 test case ran.\n", ss.str());
+}
+
+void test_(file_assert_exists_succeed_tests, test_result) {
+  xtd::tunit::settings::default_settings().exit_status(0);
+  xtd::tunit::settings::default_settings().filter_tests("file_assert_exists_succeed_tests.*");
+  std::stringstream ss;
+  assert_value_(0, xtd::tunit::unit_test(std::make_unique<assert_unit_tests::unit_tests_event_listener>(ss)).run());
+}
