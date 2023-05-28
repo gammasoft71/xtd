7e8925ae4aeab68359bc5ef13fbc9442380bc5ae
Gammasoft <gammasoft71@gmail.com>
2023-05-28 23:07:43 +0200

    Add assume unit tests

---
+++ tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/assume_are_equal_const_char_pointer_failed_tests.cpp
@@ -0,0 +1,30 @@
+#include "assert_unit_tests.h"
+#include <xtd/xtd.tunit>
+
+namespace xtd::tunit::tests {
+  class test_class_(assume_are_equal_const_char_pointer_failed_tests) {
+  public:
+    void test_method_(test_case_failed) {
+      const char* s = "string";
+      xtd::tunit::assume::are_equal("str", s);
+    }
+  };
+}
+
+void test_(assume_are_equal_const_char_pointer_failed_tests, test_output) {
+  xtd::tunit::settings::default_settings().exit_status(0);
+  xtd::tunit::settings::default_settings().filter_tests("assume_are_equal_const_char_pointer_failed_tests.*");
+  std::stringstream ss;
+  xtd::tunit::unit_test(std::make_unique<assert_unit_tests::unit_tests_event_listener>(ss)).run();
+  assert_value_("Start 1 test from 1 test case\n"
+    "  ABORTED assume_are_equal_const_char_pointer_failed_tests.test_case_failed\n"
+    "    Test aborted\n"
+    "End 1 test from 1 test case ran.\n", ss.str());
+}
+
+void test_(assume_are_equal_const_char_pointer_failed_tests, test_result) {
+  xtd::tunit::settings::default_settings().exit_status(0);
+  xtd::tunit::settings::default_settings().filter_tests("assume_are_equal_const_char_pointer_failed_tests.*");
+  std::stringstream ss;
+  assert_value_(0, xtd::tunit::unit_test(std::make_unique<assert_unit_tests::unit_tests_event_listener>(ss)).run());
+}
---
+++ tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/assume_are_equal_const_char_pointer_succeed_tests.cpp
@@ -0,0 +1,29 @@
+#include "assert_unit_tests.h"
+#include <xtd/xtd.tunit>
+
+namespace xtd::tunit::tests {
+  class test_class_(assume_are_equal_const_char_pointer_succeed_tests) {
+  public:
+    void test_method_(test_case_succeed) {
+      const char* s = "string";
+      xtd::tunit::assume::are_equal("string", s);
+    }
+  };
+}
+
+void test_(assume_are_equal_const_char_pointer_succeed_tests, test_output) {
+  xtd::tunit::settings::default_settings().exit_status(0);
+  xtd::tunit::settings::default_settings().filter_tests("assume_are_equal_const_char_pointer_succeed_tests.*");
+  std::stringstream ss;
+  xtd::tunit::unit_test(std::make_unique<assert_unit_tests::unit_tests_event_listener>(ss)).run();
+  assert_value_("Start 1 test from 1 test case\n"
+    "  SUCCEED assume_are_equal_const_char_pointer_succeed_tests.test_case_succeed\n"
+    "End 1 test from 1 test case ran.\n", ss.str());
+}
+
+void test_(assume_are_equal_const_char_pointer_succeed_tests, test_result) {
+  xtd::tunit::settings::default_settings().exit_status(0);
+  xtd::tunit::settings::default_settings().filter_tests("assume_are_equal_const_char_pointer_succeed_tests.*");
+  std::stringstream ss;
+  assert_value_(0, xtd::tunit::unit_test(std::make_unique<assert_unit_tests::unit_tests_event_listener>(ss)).run());
+}
