a2a266e978d320c73c1e5d49fe81b880a907b170
Gammasoft <gammasoft71@gmail.com>
2023-05-29 23:36:52 +0200

    Fix build unit tests

--- tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/valid_is_zero_succeed_tests.cpp
+++ tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/valid_is_zero_succeed_tests.cpp
@@ -2,7 +2,7 @@
 #include <xtd/xtd.tunit>
 
 namespace xtd::tunit::tests {
-  class test_class_(assert_is_zero_succeed_tests) {
+  class test_class_(valid_is_zero_succeed_tests) {
   public:
     void test_method_(test_case_succeed) {
       int i = 0;
@@ -11,19 +11,19 @@ namespace xtd::tunit::tests {
   };
 }
 
-void test_(assert_is_zero_succeed_tests, test_output) {
+void test_(valid_is_zero_succeed_tests, test_output) {
   xtd::tunit::settings::default_settings().exit_status(0);
-  xtd::tunit::settings::default_settings().filter_tests("assert_is_zero_succeed_tests.*");
+  xtd::tunit::settings::default_settings().filter_tests("valid_is_zero_succeed_tests.*");
   std::stringstream ss;
   xtd::tunit::unit_test(std::make_unique<assert_unit_tests::unit_tests_event_listener>(ss)).run();
   assert_value_("Start 1 test from 1 test case\n"
-    "  SUCCEED assert_is_zero_succeed_tests.test_case_succeed\n"
+    "  SUCCEED valid_is_zero_succeed_tests.test_case_succeed\n"
     "End 1 test from 1 test case ran.\n", ss.str());
 }
 
-void test_(assert_is_zero_succeed_tests, test_result) {
+void test_(valid_is_zero_succeed_tests, test_result) {
   xtd::tunit::settings::default_settings().exit_status(0);
-  xtd::tunit::settings::default_settings().filter_tests("assert_is_zero_succeed_tests.*");
+  xtd::tunit::settings::default_settings().filter_tests("valid_is_zero_succeed_tests.*");
   std::stringstream ss;
   assert_value_(0, xtd::tunit::unit_test(std::make_unique<assert_unit_tests::unit_tests_event_listener>(ss)).run());
 }
