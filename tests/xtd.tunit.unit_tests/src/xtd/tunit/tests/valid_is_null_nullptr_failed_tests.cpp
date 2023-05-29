2d4e4d07bf9836b6352b791be8c7da4832f4b32e
Gammasoft <gammasoft71@gmail.com>
2023-05-29 23:11:01 +0200

    Fix unit tests

--- tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/valid_is_null_nullptr_failed_tests.cpp
+++ tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/valid_is_null_nullptr_failed_tests.cpp
@@ -19,8 +19,8 @@ void test_(valid_is_null_nullptr_failed_tests, test_output) {
   xtd::tunit::unit_test(std::make_unique<assert_unit_tests::unit_tests_event_listener>(ss)).run();
   assert_value_("Start 1 test from 1 test case\n"
     "  FAILED  valid_is_null_nullptr_failed_tests.test_case_failed\n"
-    "    Expected: not null\n"
-    "    But was:  null\n"
+    "    Expected: null\n"
+    "    But was:  not null\n"
     "End 1 test from 1 test case ran.\n", ss.str());
 }
 
