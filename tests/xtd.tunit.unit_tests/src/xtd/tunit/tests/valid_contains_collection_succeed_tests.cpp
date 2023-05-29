4ebab14187217d86f23580d95e95f64314be434c
Gammasoft <gammasoft71@gmail.com>
2023-05-29 15:18:32 +0200

    Add tunit unit tests

---
+++ tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/valid_contains_collection_succeed_tests.cpp
@@ -0,0 +1,30 @@
+#include "assert_unit_tests.h"
+#include <xtd/xtd.tunit>
+#include <vector>
+
+namespace xtd::tunit::tests {
+  class test_class_(valid_contains_collection_succeed_testsv) {
+  public:
+    void test_method_(test_case_succeed) {
+      std::vector v = {0, 1, 2, 3};
+      xtd::tunit::valid::contains(2, v);
+    }
+  };
+}
+
+void test_(valid_contains_collection_succeed_testsv, test_output) {
+  xtd::tunit::settings::default_settings().exit_status(0);
+  xtd::tunit::settings::default_settings().filter_tests("valid_contains_collection_succeed_testsv.*");
+  std::stringstream ss;
+  xtd::tunit::unit_test(std::make_unique<assert_unit_tests::unit_tests_event_listener>(ss)).run();
+  assert_value_("Start 1 test from 1 test case\n"
+    "  SUCCEED valid_contains_collection_succeed_testsv.test_case_succeed\n"
+    "End 1 test from 1 test case ran.\n", ss.str());
+}
+
+void test_(valid_contains_collection_succeed_testsv, test_result) {
+  xtd::tunit::settings::default_settings().exit_status(0);
+  xtd::tunit::settings::default_settings().filter_tests("valid_contains_collection_succeed_testsv.*");
+  std::stringstream ss;
+  assert_value_(0, xtd::tunit::unit_test(std::make_unique<assert_unit_tests::unit_tests_event_listener>(ss)).run());
+}
