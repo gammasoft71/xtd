863abf73c8983471e18eca157b60a8f8ae9ab807
Gammasoft <gammasoft71@gmail.com>
2023-05-29 14:45:28 +0200

    Add tuint unit tests

---
+++ tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/valid_are_not_equal_const_char16_t_pointer_succeed_tests.cpp
@@ -0,0 +1,29 @@
+#include "assert_unit_tests.h"
+#include <xtd/xtd.tunit>
+
+namespace xtd::tunit::tests {
+  class test_class_(valid_are_not_equal_const_char16_t_pointer_succeed_tests) {
+  public:
+    void test_method_(test_case_succeed) {
+      const char16* s = u"string";
+      xtd::tunit::valid::are_not_equal(u"str", s);
+    }
+  };
+}
+
+void test_(valid_are_not_equal_const_char16_t_pointer_succeed_tests, test_output) {
+  xtd::tunit::settings::default_settings().exit_status(0);
+  xtd::tunit::settings::default_settings().filter_tests("valid_are_not_equal_const_char16_t_pointer_succeed_tests.*");
+  std::stringstream ss;
+  xtd::tunit::unit_test(std::make_unique<assert_unit_tests::unit_tests_event_listener>(ss)).run();
+  assert_value_("Start 1 test from 1 test case\n"
+    "  SUCCEED valid_are_not_equal_const_char16_t_pointer_succeed_tests.test_case_succeed\n"
+    "End 1 test from 1 test case ran.\n", ss.str());
+}
+
+void test_(valid_are_not_equal_const_char16_t_pointer_succeed_tests, test_result) {
+  xtd::tunit::settings::default_settings().exit_status(0);
+  xtd::tunit::settings::default_settings().filter_tests("valid_are_not_equal_const_char16_t_pointer_succeed_tests.*");
+  std::stringstream ss;
+  assert_value_(0, xtd::tunit::unit_test(std::make_unique<assert_unit_tests::unit_tests_event_listener>(ss)).run());
+}
