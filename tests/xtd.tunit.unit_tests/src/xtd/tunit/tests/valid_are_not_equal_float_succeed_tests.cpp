1a9ea52749653a6f559e5c096db95d8be7f88650
Gammasoft <gammasoft71@gmail.com>
2023-05-29 15:00:04 +0200

    Add tunit unit tests

--- tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/valid_are_not_equal_float_succeed_tests.cpp
+++ tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/valid_are_not_equal_float_succeed_tests.cpp
@@ -2,28 +2,28 @@
 #include <xtd/xtd.tunit>
 
 namespace xtd::tunit::tests {
-  class test_class_(assert_are_not_equal_float_succeed_tests) {
+  class test_class_(valid_are_not_equal_float_succeed_tests) {
   public:
     void test_method_(test_case_succeed) {
       float f = 0.4f;
-      xtd::tunit::assert::are_not_equal(0.2f, f);
+      xtd::tunit::valid::are_not_equal(0.2f, f);
     }
   };
 }
 
-void test_(assert_are_not_equal_float_succeed_tests, test_output) {
+void test_(valid_are_not_equal_float_succeed_tests, test_output) {
   xtd::tunit::settings::default_settings().exit_status(0);
-  xtd::tunit::settings::default_settings().filter_tests("assert_are_not_equal_float_succeed_tests.*");
+  xtd::tunit::settings::default_settings().filter_tests("valid_are_not_equal_float_succeed_tests.*");
   std::stringstream ss;
   xtd::tunit::unit_test(std::make_unique<assert_unit_tests::unit_tests_event_listener>(ss)).run();
   assert_value_("Start 1 test from 1 test case\n"
-    "  SUCCEED assert_are_not_equal_float_succeed_tests.test_case_succeed\n"
+    "  SUCCEED valid_are_not_equal_float_succeed_tests.test_case_succeed\n"
     "End 1 test from 1 test case ran.\n", ss.str());
 }
 
-void test_(assert_are_not_equal_float_succeed_tests, test_result) {
+void test_(valid_are_not_equal_float_succeed_tests, test_result) {
   xtd::tunit::settings::default_settings().exit_status(0);
-  xtd::tunit::settings::default_settings().filter_tests("assert_are_not_equal_float_succeed_tests.*");
+  xtd::tunit::settings::default_settings().filter_tests("valid_are_not_equal_float_succeed_tests.*");
   std::stringstream ss;
   assert_value_(0, xtd::tunit::unit_test(std::make_unique<assert_unit_tests::unit_tests_event_listener>(ss)).run());
 }
