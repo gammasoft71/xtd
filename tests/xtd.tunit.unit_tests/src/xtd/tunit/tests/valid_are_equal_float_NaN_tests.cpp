4778cf2e5039b6a2b875b98bf5336b8ee87e5c76
Gammasoft <gammasoft71@gmail.com>
2023-05-29 11:24:59 +0200

    Add tunit unit tests

---
+++ tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/valid_are_equal_float_NaN_tests.cpp
@@ -0,0 +1,29 @@
+#include "assert_unit_tests.h"
+#include <xtd/xtd.tunit>
+
+namespace xtd::tunit::tests {
+  class test_class_(valid_are_equal_float_NaN_tests) {
+  public:
+    void test_method_(test_case_succeed) {
+      float f = std::numeric_limits<float>::quiet_NaN();
+      xtd::tunit::valid::are_equal(std::numeric_limits<float>::quiet_NaN(), f);
+    }
+  };
+}
+
+void test_(valid_are_equal_float_NaN_tests, test_output) {
+  xtd::tunit::settings::default_settings().exit_status(0);
+  xtd::tunit::settings::default_settings().filter_tests("valid_are_equal_float_NaN_tests.*");
+  std::stringstream ss;
+  xtd::tunit::unit_test(std::make_unique<assert_unit_tests::unit_tests_event_listener>(ss)).run();
+  assert_value_("Start 1 test from 1 test case\n"
+    "  SUCCEED valid_are_equal_float_NaN_tests.test_case_succeed\n"
+    "End 1 test from 1 test case ran.\n", ss.str());
+}
+
+void test_(valid_are_equal_float_NaN_tests, test_result) {
+  xtd::tunit::settings::default_settings().exit_status(0);
+  xtd::tunit::settings::default_settings().filter_tests("valid_are_equal_float_NaN_tests.*");
+  std::stringstream ss;
+  assert_value_(0, xtd::tunit::unit_test(std::make_unique<assert_unit_tests::unit_tests_event_listener>(ss)).run());
+}
