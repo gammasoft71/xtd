7b43fbadf4f2444ea4e054265d3002f1bf05d264
Gammasoft <gammasoft71@gmail.com>
2023-05-30 22:53:58 +0200

    Review code

--- tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/assert_is_not_null_shared_ptr_succeed_tests.cpp
+++ tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/assert_is_not_null_shared_ptr_succeed_tests.cpp
@@ -13,18 +13,13 @@ namespace xtd::tunit::tests {
 }
 
 void test_(assert_is_not_null_shared_ptr_succeed_tests, test_output) {
-  xtd::tunit::settings::default_settings().exit_status(0);
-  xtd::tunit::settings::default_settings().filter_tests("assert_is_not_null_shared_ptr_succeed_tests.*");
-  std::stringstream ss;
-  xtd::tunit::unit_test(std::make_unique<assert_unit_tests::unit_tests_event_listener>(ss)).run();
+  auto [result, output] = run_test_("assert_is_not_null_shared_ptr_succeed_tests.*");
   assert_value_("Start 1 test from 1 test case\n"
     "  SUCCEED assert_is_not_null_shared_ptr_succeed_tests.test_case_succeed\n"
-    "End 1 test from 1 test case ran.\n", ss.str());
+    "End 1 test from 1 test case ran.\n", output);
 }
 
 void test_(assert_is_not_null_shared_ptr_succeed_tests, test_result) {
-  xtd::tunit::settings::default_settings().exit_status(0);
-  xtd::tunit::settings::default_settings().filter_tests("assert_is_not_null_shared_ptr_succeed_tests.*");
-  std::stringstream ss;
-  assert_value_(0, xtd::tunit::unit_test(std::make_unique<assert_unit_tests::unit_tests_event_listener>(ss)).run());
+  auto [result, output] = run_test_("assert_is_not_null_shared_ptr_succeed_tests.*");
+  assert_value_(0, result);
 }
