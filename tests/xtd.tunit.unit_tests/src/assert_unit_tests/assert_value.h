d1cea139c50622e1f2f942dc17837bf1a3a2b34c
Gammasoft <gammasoft71@gmail.com>
2023-05-30 14:55:11 +0200

    review and split code

---
+++ tests/xtd.tunit.unit_tests/src/assert_unit_tests/assert_value.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "unit_test_error.h"
+#include <iostream>
+#include <sstream>
+#include <string>
+
+namespace assert_unit_tests {
+  template <typename expected_t, typename actual_t>
+  void assert_value(const std::string& name, const expected_t& expected, const actual_t& actual, const std::string& file, int line_number) {
+    if (actual == expected)
+      std::cout << "  SUCCEED " << name << std::endl;
+    else {
+      std::cout << "  FAILED  " << name << std::endl;
+      std::cout << "    Expected:" << std::endl;
+      std::stringstream f;
+      f << expected;
+      std::string line;
+      while (std::getline(f, line))
+        std::cout << "      " << line << std::endl;
+      std::cout << "    But was:" << std::endl;
+      f.str("");
+      f.clear();
+      f << actual;
+      while (std::getline(f, line))
+        std::cout << "      " << line << std::endl;
+      std::cout << "    Stack trace:" << std::endl;
+      std::cout << "      " << file << ":" << line_number << std::endl;
+      throw unit_test_error("assertion failed!");
+    }
+  }
+}
+
+#define assert_value_(expected, actual) \
+  assert_unit_tests::assert_value(name, expected, actual, __FILE__, __LINE__)
+
+#define assert_value_(expected, actual) \
+assert_unit_tests::assert_value(name, expected, actual, __FILE__, __LINE__)
---
+++ tests/xtd.tunit.unit_tests/src/assert_unit_tests/unit_test_error.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <exception>
+#include <string>
+
+namespace assert_unit_tests {
+  class unit_test_error : public std::exception {
+  public:
+    explicit unit_test_error(const std::string& message) : message_(message) {}
+    explicit unit_test_error(const char* message) : message_(message) {}
+    unit_test_error(const unit_test_error&) = default;
+    unit_test_error& operator =(const unit_test_error&) = default;
+    const char* what() const noexcept {return message_.c_str();}
+    
+  private:
+    std::string message_;
+  };
+}
