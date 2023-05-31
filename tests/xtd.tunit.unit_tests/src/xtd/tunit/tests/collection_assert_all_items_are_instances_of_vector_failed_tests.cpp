834ce4de9b1e68cc60e30dee23ecfbed127283a7
Gammasoft <gammasoft71@gmail.com>
2023-05-31 20:55:40 +0200

    Review code

--- tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/collection_assert_all_items_are_instances_of_vector_failed_tests.cpp
+++ tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/collection_assert_all_items_are_instances_of_vector_failed_tests.cpp
@@ -4,7 +4,7 @@
 /*
  This test is commented, it can't work because the pointer addresses change at each build...
 
-namespace xtd::tunit::tests {
+ namespace xtd::tunit::tests {
   class test_class_(collection_assert_all_items_are_instances_of_vector_failed_tests) {
   public:
     void test_method_(test_case_failed) {
@@ -15,21 +15,16 @@ namespace xtd::tunit::tests {
 }
 
 void test_(collection_assert_all_items_are_instances_of_vector_failed_tests, test_output) {
-  xtd::tunit::settings::default_settings().exit_status(0);
-  xtd::tunit::settings::default_settings().filter_tests("collection_assert_all_items_are_instances_of_vector_failed_tests.*");
-  std::stringstream ss;
-  xtd::tunit::unit_test(std::make_unique<assert_unit_tests::unit_tests_event_listener>(ss)).run();
+  auto [result, output] = run_test_("collection_assert_all_items_are_instances_of_vector_failed_tests.*");
   assert_value_("Start 1 test from 1 test case\n"
     "  FAILED  collection_assert_all_items_are_instances_of_vector_failed_tests.test_case_failed\n"
     "    Expected: all items instance of <std::__1::basic_ostream<char, std::__1::char_traits<char> >*>\n"
     "    But was:  < 8-byte object <90-9A 44-E9 01-00 00-00>, 8-byte object <D0-9B 44-E9 01-00 00-00>, 8-byte object <48-99 44-E9 01-00 00-00> >\n"
-    "End 1 test from 1 test case ran.\n", ss.str());
+    "End 1 test from 1 test case ran.\n", output);
 }
 
 void test_(collection_assert_all_items_are_instances_of_vector_failed_tests, test_result) {
-  xtd::tunit::settings::default_settings().exit_status(0);
-  xtd::tunit::settings::default_settings().filter_tests("collection_assert_all_items_are_instances_of_vector_failed_tests.*");
-  std::stringstream ss;
-  assert_value_(1, xtd::tunit::unit_test(std::make_unique<assert_unit_tests::unit_tests_event_listener>(ss)).run());
+  auto [result, output] = run_test_("collection_assert_all_items_are_instances_of_vector_failed_tests.*");
+  assert_value_(1, result);
 }
 */
