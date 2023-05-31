04f06d3553b5a467eb1d53d23ad36bc18959d84d
Gammasoft <gammasoft71@gmail.com>
2023-05-31 21:30:49 +0200

    Review code

--- tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/two_tests_five_test_cases_tests.cpp
+++ tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/two_tests_five_test_cases_tests.cpp
@@ -23,22 +23,17 @@ namespace xtd::tunit::tests {
 }
 
 void test_(two_tests_five_test_cases, test_output) {
-  xtd::tunit::settings::default_settings().exit_status(0);
-  xtd::tunit::settings::default_settings().filter_tests("two_tests_five_test_cases_?_tests.*");
-  std::stringstream ss;
-  xtd::tunit::unit_test(std::make_unique<assert_unit_tests::unit_tests_event_listener>(ss)).run();
+  auto [result, output] = run_test_("two_tests_five_test_cases_*");
   assert_value_("Start 5 tests from 2 test cases\n"
     "  SUCCEED two_tests_five_test_cases_1_tests.test_case1\n"
     "  SUCCEED two_tests_five_test_cases_1_tests.test_case2\n"
     "  SUCCEED two_tests_five_test_cases_2_tests.test_case3\n"
     "  SUCCEED two_tests_five_test_cases_2_tests.test_case4\n"
     "  SUCCEED two_tests_five_test_cases_2_tests.test_case5\n"
-    "End 5 tests from 2 test cases ran.\n", ss.str());
+    "End 5 tests from 2 test cases ran.\n", output);
 }
 
 void test_(two_tests_five_test_cases, test_result) {
-  xtd::tunit::settings::default_settings().exit_status(0);
-  xtd::tunit::settings::default_settings().filter_tests("two_tests_two_test_cases_?_tests.*");
-  std::stringstream ss;
-  assert_value_(0, xtd::tunit::unit_test(std::make_unique<assert_unit_tests::unit_tests_event_listener>(ss)).run());
+  auto [result, output] = run_test_("two_tests_five_test_cases_*");
+  assert_value_(0, result);
 }
