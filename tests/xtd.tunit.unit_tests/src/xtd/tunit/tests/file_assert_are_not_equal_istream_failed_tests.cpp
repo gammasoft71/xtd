5c595ce8678d282a68ad42ee05b02f82ae130faf
Gammasoft <gammasoft71@gmail.com>
2023-05-31 21:13:39 +0200

    Review code

--- tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/file_assert_are_not_equal_istream_failed_tests.cpp
+++ tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/file_assert_are_not_equal_istream_failed_tests.cpp
@@ -12,20 +12,15 @@ namespace xtd::tunit::tests {
 }
 
 void test_(file_assert_are_not_equal_istream_failed_tests, test_output) {
-  xtd::tunit::settings::default_settings().exit_status(0);
-  xtd::tunit::settings::default_settings().filter_tests("file_assert_are_not_equal_istream_failed_tests.*");
-  std::stringstream ss;
-  xtd::tunit::unit_test(std::make_unique<assert_unit_tests::unit_tests_event_listener>(ss)).run();
+  auto [result, output] = run_test_("file_assert_are_not_equal_istream_failed_tests.*");
   assert_value_(xtd::ustring::format("Start 1 test from 1 test case\n"
     "  FAILED  file_assert_are_not_equal_istream_failed_tests.test_case_failed\n"
     "    Expected: not equal <{0}>\n"
     "    But was:  <{0}>\n"
-    "End 1 test from 1 test case ran.\n", xtd::ustring::full_class_name<std::istringstream>()), ss.str());
+    "End 1 test from 1 test case ran.\n", xtd::ustring::full_class_name<std::istringstream>()), output);
 }
 
 void test_(file_assert_are_not_equal_istream_failed_tests, test_result) {
-  xtd::tunit::settings::default_settings().exit_status(0);
-  xtd::tunit::settings::default_settings().filter_tests("file_assert_are_not_equal_istream_failed_tests.*");
-  std::stringstream ss;
-  assert_value_(1, xtd::tunit::unit_test(std::make_unique<assert_unit_tests::unit_tests_event_listener>(ss)).run());
+  auto [result, output] = run_test_("file_assert_are_not_equal_istream_failed_tests.*");
+  assert_value_(1, result);
 }
--- tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/file_assert_does_not_exist_failed_tests.cpp
+++ tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/file_assert_does_not_exist_failed_tests.cpp
@@ -13,24 +13,19 @@ namespace xtd::tunit::tests {
 }
 
 void test_(file_assert_does_not_exist_failed_tests, test_output) {
-  xtd::tunit::settings::default_settings().exit_status(0);
-  xtd::tunit::settings::default_settings().filter_tests("file_assert_does_not_exist_failed_tests.*");
-  std::stringstream ss;
-  xtd::tunit::unit_test(std::make_unique<assert_unit_tests::unit_tests_event_listener>(ss)).run();
+  auto [result, output] = run_test_("file_assert_does_not_exist_failed_tests.*");
   assert_value_(xtd::ustring::format("Start 1 test from 1 test case\n"
     "  FAILED  file_assert_does_not_exist_failed_tests.test_case_failed\n"
     "    Expected: not file exists\n"
     "    But was:  {}\n"
-    "End 1 test from 1 test case ran.\n", std::quoted(xtd::io::path::combine(xtd::io::path::get_temp_path(), "test_tunit_file.txt"))), ss.str());
+    "End 1 test from 1 test case ran.\n", std::quoted(xtd::io::path::combine(xtd::io::path::get_temp_path(), "test_tunit_file.txt"))), output);
   
   xtd::io::file::remove(xtd::io::path::combine(xtd::io::path::get_temp_path(), "test_tunit_file.txt"));
 }
 
 void test_(file_assert_does_not_exist_failed_tests, test_result) {
-  xtd::tunit::settings::default_settings().exit_status(0);
-  xtd::tunit::settings::default_settings().filter_tests("file_assert_does_not_exist_failed_tests.*");
-  std::stringstream ss;
-  assert_value_(1, xtd::tunit::unit_test(std::make_unique<assert_unit_tests::unit_tests_event_listener>(ss)).run());
+  auto [result, output] = run_test_("file_assert_does_not_exist_failed_tests.*");
+  assert_value_(1, result);
   
   xtd::io::file::remove(xtd::io::path::combine(xtd::io::path::get_temp_path(), "test_tunit_file.txt"));
 }
