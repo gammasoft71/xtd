01dfbde4bf704426c2e57d8fa1c45e4a8f2b99a2
Gammasoft <gammasoft71@gmail.com>
2023-05-31 22:16:16 +0200

    Review code

--- tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/valid_are_equal_float_with_tolerence_failed_tests.cpp
+++ tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/valid_are_equal_float_with_tolerence_failed_tests.cpp
@@ -12,20 +12,15 @@ namespace xtd::tunit::tests {
 }
 
 void test_(valid_are_equal_float_with_tolerence_failed_tests, test_output) {
-  xtd::tunit::settings::default_settings().exit_status(0);
-  xtd::tunit::settings::default_settings().filter_tests("valid_are_equal_float_with_tolerence_failed_tests.*");
-  std::stringstream ss;
-  xtd::tunit::unit_test(std::make_unique<assert_unit_tests::unit_tests_event_listener>(ss)).run();
+  auto [result, output] = run_test_("valid_are_equal_float_with_tolerence_failed_tests*");
   assert_value_("Start 1 test from 1 test case\n"
     "  FAILED  valid_are_equal_float_with_tolerence_failed_tests.test_case_failed\n"
     "    Expected: 0.6\n"
     "    But was:  0.599\n"
-    "End 1 test from 1 test case ran.\n", ss.str());
+    "End 1 test from 1 test case ran.\n", output);
 }
 
 void test_(valid_are_equal_float_with_tolerence_failed_tests, test_result) {
-  xtd::tunit::settings::default_settings().exit_status(0);
-  xtd::tunit::settings::default_settings().filter_tests("valid_are_equal_float_with_tolerence_failed_tests.*");
-  std::stringstream ss;
-  assert_value_(1, xtd::tunit::unit_test(std::make_unique<assert_unit_tests::unit_tests_event_listener>(ss)).run());
+  auto [result, output] = run_test_("valid_are_equal_float_with_tolerence_failed_tests*");
+  assert_value_(1, result);
 }
