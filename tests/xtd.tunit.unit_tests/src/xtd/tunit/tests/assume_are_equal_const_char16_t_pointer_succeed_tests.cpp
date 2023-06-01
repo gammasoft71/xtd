1bca8ae7a1eff197b1b6d3d564f7e13eae7467f5
Gammasoft <gammasoft71@gmail.com>
2023-06-01 22:29:17 +0200

    Add tunit unit tests

---
+++ tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/assume_are_equal_const_char16_t_pointer_succeed_tests.cpp
@@ -0,0 +1,24 @@
+#include "../../../assert_unit_tests/assert_unit_tests.h"
+#include <xtd/xtd.tunit>
+
+namespace xtd::tunit::tests {
+  class test_class_(assume_are_equal_const_char16_t_pointer_succeed_tests) {
+  public:
+    void test_method_(test_case_succeed) {
+      const char16* s = u"string";
+      xtd::tunit::assume::are_equal(u"string", s);
+    }
+  };
+}
+
+void test_(assume_are_equal_const_char16_t_pointer_succeed_tests, test_output) {
+  auto [output, result] = run_test_("assume_are_equal_const_char16_t_pointer_succeed_tests.*");
+  assert_value_("Start 1 test from 1 test case\n"
+    "  SUCCEED assume_are_equal_const_char16_t_pointer_succeed_tests.test_case_succeed\n"
+    "End 1 test from 1 test case ran.\n", output);
+}
+
+void test_(assume_are_equal_const_char16_t_pointer_succeed_tests, test_result) {
+  auto [output, result] = run_test_("assume_are_equal_const_char16_t_pointer_succeed_tests.*");
+  assert_value_(0, result);
+}
