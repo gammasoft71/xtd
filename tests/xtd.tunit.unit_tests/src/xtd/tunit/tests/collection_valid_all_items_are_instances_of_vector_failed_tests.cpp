3d890f0861314f80d801a734d0996f1c6f36669e
Gammasoft <gammasoft71@gmail.com>
2023-06-01 15:07:07 +0200

    Add tunit unit tests

---
+++ tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/collection_valid_all_items_are_instances_of_vector_failed_tests.cpp
@@ -0,0 +1,30 @@
+#include "../../../assert_unit_tests/assert_unit_tests.h"
+#include <xtd/xtd.tunit>
+
+/*
+ This test is commented, it can't work because the pointer addresses change at each build...
+
+ namespace xtd::tunit::tests {
+  class test_class_(collection_valid_all_items_are_instances_of_vector_failed_tests) {
+  public:
+    void test_method_(test_case_failed) {
+      std::vector<std::ios_base*> a = {&std::cout, &std::cerr, &std::cin};
+      xtd::tunit::collection_valid::all_items_are_instances_of<std::basic_ostream<char>*>(a);
+    }
+  };
+}
+
+void test_(collection_valid_all_items_are_instances_of_vector_failed_tests, test_output) {
+  auto [output, result] = run_test_("collection_valid_all_items_are_instances_of_vector_failed_tests.*");
+  assert_value_("Start 1 test from 1 test case\n"
+    "  FAILED  collection_valid_all_items_are_instances_of_vector_failed_tests.test_case_failed\n"
+    "    Expected: all items instance of <std::__1::basic_ostream<char, std::__1::char_traits<char> >*>\n"
+    "    But was:  < 8-byte object <90-9A 44-E9 01-00 00-00>, 8-byte object <D0-9B 44-E9 01-00 00-00>, 8-byte object <48-99 44-E9 01-00 00-00> >\n"
+    "End 1 test from 1 test case ran.\n", output);
+}
+
+void test_(collection_valid_all_items_are_instances_of_vector_failed_tests, test_result) {
+  auto [output, result] = run_test_("collection_valid_all_items_are_instances_of_vector_failed_tests.*");
+  assert_value_(1, result);
+}
+*/
