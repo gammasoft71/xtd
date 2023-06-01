876e03b9f30e9759cd32bfbb7c37a26d488eab63
Gammasoft <gammasoft71@gmail.com>
2023-06-01 12:19:54 +0200

    Review code

--- tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/valid_contains_collection_succeed_tests.cpp
+++ tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/valid_contains_collection_succeed_tests.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 
 namespace xtd::tunit::tests {
-  class test_class_(valid_contains_collection_succeed_testsv) {
+  class test_class_(valid_contains_collection_succeed_tests) {
   public:
     void test_method_(test_case_succeed) {
       std::vector v = {0, 1, 2, 3};
@@ -12,19 +12,14 @@ namespace xtd::tunit::tests {
   };
 }
 
-void test_(valid_contains_collection_succeed_testsv, test_output) {
-  xtd::tunit::settings::default_settings().exit_status(0);
-  xtd::tunit::settings::default_settings().filter_tests("valid_contains_collection_succeed_testsv.*");
-  std::stringstream ss;
-  xtd::tunit::unit_test(std::make_unique<assert_unit_tests::unit_tests_event_listener>(ss)).run();
+void test_(valid_contains_collection_succeed_tests, test_output) {
+  auto [result, output] = run_test_("valid_contains_collection_succeed_tests*");
   assert_value_("Start 1 test from 1 test case\n"
-    "  SUCCEED valid_contains_collection_succeed_testsv.test_case_succeed\n"
-    "End 1 test from 1 test case ran.\n", ss.str());
+    "  SUCCEED valid_contains_collection_succeed_tests.test_case_succeed\n"
+    "End 1 test from 1 test case ran.\n", output);
 }
 
-void test_(valid_contains_collection_succeed_testsv, test_result) {
-  xtd::tunit::settings::default_settings().exit_status(0);
-  xtd::tunit::settings::default_settings().filter_tests("valid_contains_collection_succeed_testsv.*");
-  std::stringstream ss;
-  assert_value_(0, xtd::tunit::unit_test(std::make_unique<assert_unit_tests::unit_tests_event_listener>(ss)).run());
+void test_(valid_contains_collection_succeed_tests, test_result) {
+  auto [result, output] = run_test_("valid_contains_collection_succeed_tests*");
+  assert_value_(0, result);
 }
