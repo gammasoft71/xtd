2883308cb1888cbabc33626e720c0ed3f6c92603
Gammasoft <gammasoft71@gmail.com>
2023-06-01 13:41:04 +0200

    Review code

--- tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/valid_is_not_null_weak_ptr_succeed_tests.cpp
+++ tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/valid_is_not_null_weak_ptr_succeed_tests.cpp
@@ -14,18 +14,13 @@ namespace xtd::tunit::tests {
 }
 
 void test_(valid_is_not_null_weak_ptr_succeed_tests, test_output) {
-  xtd::tunit::settings::default_settings().exit_status(0);
-  xtd::tunit::settings::default_settings().filter_tests("valid_is_not_null_weak_ptr_succeed_tests.*");
-  std::stringstream ss;
-  xtd::tunit::unit_test(std::make_unique<assert_unit_tests::unit_tests_event_listener>(ss)).run();
+  auto [result, output] = run_test_("valid_is_not_null_weak_ptr_succeed_tests.*");
   assert_value_("Start 1 test from 1 test case\n"
     "  SUCCEED valid_is_not_null_weak_ptr_succeed_tests.test_case_succeed\n"
-    "End 1 test from 1 test case ran.\n", ss.str());
+    "End 1 test from 1 test case ran.\n", output);
 }
 
 void test_(valid_is_not_null_weak_ptr_succeed_tests, test_result) {
-  xtd::tunit::settings::default_settings().exit_status(0);
-  xtd::tunit::settings::default_settings().filter_tests("valid_is_not_null_weak_ptr_succeed_tests.*");
-  std::stringstream ss;
-  assert_value_(0, xtd::tunit::unit_test(std::make_unique<assert_unit_tests::unit_tests_event_listener>(ss)).run());
+  auto [result, output] = run_test_("valid_is_not_null_weak_ptr_succeed_tests.*");
+  assert_value_(0, result);
 }
