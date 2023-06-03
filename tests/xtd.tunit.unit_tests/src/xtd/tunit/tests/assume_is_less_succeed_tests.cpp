c175d4c90e44850bead2688c6d9bf1220390a306
Gammasoft <gammasoft71@gmail.com>
2023-06-03 15:41:58 +0200

    Add tunit unit tests

---
+++ tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/assume_is_less_succeed_tests.cpp
@@ -0,0 +1,24 @@
+#include "../../../assert_unit_tests/assert_unit_tests.h"
+#include <xtd/xtd.tunit>
+
+namespace xtd::tunit::tests {
+  class test_class_(assume_is_less_succeed_tests) {
+  public:
+    void test_method_(test_case_succeed) {
+      int i = 42;
+      xtd::tunit::assume::is_less(i, 84);
+    }
+  };
+}
+
+void test_(assume_is_less_succeed_tests, test_output) {
+  auto [output, result] = run_test_("assume_is_less_succeed_tests.*");
+  assert_value_("Start 1 test from 1 test case\n"
+    "  SUCCEED assume_is_less_succeed_tests.test_case_succeed\n"
+    "End 1 test from 1 test case ran.\n", output);
+}
+
+void test_(assume_is_less_succeed_tests, test_result) {
+  auto [output, result] = run_test_("assume_is_less_succeed_tests.*");
+  assert_value_(0, result);
+}
