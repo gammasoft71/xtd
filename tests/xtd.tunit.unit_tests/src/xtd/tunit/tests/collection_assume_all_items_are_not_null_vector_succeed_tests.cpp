540a9aaa90a1abc23058d3f08fe5bace2fd0f533
Gammasoft <gammasoft71@gmail.com>
2023-06-03 22:20:23 +0200

    Add tunit unit tests

---
+++ tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/collection_assume_all_items_are_not_null_vector_succeed_tests.cpp
@@ -0,0 +1,25 @@
+#include "../../../assert_unit_tests/assert_unit_tests.h"
+#include <xtd/xtd.tunit>
+
+namespace xtd::tunit::tests {
+  class test_class_(collection_assume_all_items_are_not_null_vector_succeed_tests) {
+  public:
+    void test_method_(test_case_succeed) {
+      int i1 = 0, i2 = 3;
+      std::vector<int*> a = {&i1, &i2};
+      xtd::tunit::collection_assume::all_items_are_not_null(a);
+    }
+  };
+}
+
+void test_(collection_assume_all_items_are_not_null_vector_succeed_tests, test_output) {
+  auto [output, result] = run_test_("collection_assume_all_items_are_not_null_vector_succeed_tests.*");
+  assert_value_("Start 1 test from 1 test case\n"
+    "  SUCCEED collection_assume_all_items_are_not_null_vector_succeed_tests.test_case_succeed\n"
+    "End 1 test from 1 test case ran.\n", output);
+}
+
+void test_(collection_assume_all_items_are_not_null_vector_succeed_tests, test_result) {
+  auto [output, result] = run_test_("collection_assume_all_items_are_not_null_vector_succeed_tests.*");
+  assert_value_(0, result);
+}
---
+++ tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/collection_assume_does_not_contain_vector_failed_tests.cpp
@@ -0,0 +1,25 @@
+#include "../../../assert_unit_tests/assert_unit_tests.h"
+#include <xtd/xtd.tunit>
+
+namespace xtd::tunit::tests {
+  class test_class_(collection_assume_does_not_contain_vector_failed_tests) {
+  public:
+    void test_method_(test_case_failed) {
+      std::vector<int> a = {1, 2, 3, 4};
+      xtd::tunit::collection_assume::does_not_contain({1, 2}, a);
+    }
+  };
+}
+
+void test_(collection_assume_does_not_contain_vector_failed_tests, test_output) {
+  auto [output, result] = run_test_("collection_assume_does_not_contain_vector_failed_tests.*");
+  assert_value_("Start 1 test from 1 test case\n"
+    "  ABORTED collection_assume_does_not_contain_vector_failed_tests.test_case_failed\n"
+    "    Test aborted\n"
+    "End 1 test from 1 test case ran.\n", output);
+}
+
+void test_(collection_assume_does_not_contain_vector_failed_tests, test_result) {
+  auto [output, result] = run_test_("collection_assume_does_not_contain_vector_failed_tests.*");
+  assert_value_(0, result);
+}
