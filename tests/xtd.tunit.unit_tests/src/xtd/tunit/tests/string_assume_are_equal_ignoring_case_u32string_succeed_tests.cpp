13f078458fffdd98f51188946c0fb130153e4db5
Gammasoft <gammasoft71@gmail.com>
2023-06-03 22:59:13 +0200

    Add tunit unit tests

---
+++ tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/string_assume_are_equal_ignoring_case_u32string_succeed_tests.cpp
@@ -0,0 +1,24 @@
+#include "../../../assert_unit_tests/assert_unit_tests.h"
+#include <xtd/xtd.tunit>
+
+namespace xtd::tunit::tests {
+  class test_class_(string_assume_are_equal_ignoring_case_u32string_succeed_tests) {
+  public:
+    void test_method_(test_case_succeed) {
+      std::u32string s = U"VALUE";
+      xtd::tunit::string_assume::are_equal_ignoring_case(U"value", s);
+    }
+  };
+}
+
+void test_(string_assume_are_equal_ignoring_case_u32string_succeed_tests, test_output) {
+  auto [output, result] = run_test_("string_assume_are_equal_ignoring_case_u32string_succeed_tests.*");
+  assert_value_("Start 1 test from 1 test case\n"
+    "  SUCCEED string_assume_are_equal_ignoring_case_u32string_succeed_tests.test_case_succeed\n"
+    "End 1 test from 1 test case ran.\n", output);
+}
+
+void test_(string_assume_are_equal_ignoring_case_u32string_succeed_tests, test_result) {
+  auto [output, result] = run_test_("string_assume_are_equal_ignoring_case_u32string_succeed_tests.*");
+  assert_value_(0, result);
+}
