fcfab2200cc4e28c477c9f7e07f4e401e02dd1d3
Gammasoft <gammasoft71@gmail.com>
2023-06-03 21:23:04 +0200

    Add tunit unit tests

---
+++ tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/assume_is_null_pointer_succeed_tests.cpp
@@ -0,0 +1,24 @@
+#include "../../../assert_unit_tests/assert_unit_tests.h"
+#include <xtd/xtd.tunit>
+
+namespace xtd::tunit::tests {
+  class test_class_(assume_is_null_pointer_succeed_tests) {
+  public:
+    void test_method_(test_case_succeed) {
+      int* p = nullptr;
+      xtd::tunit::assume::is_null(p);
+    }
+  };
+}
+
+void test_(assume_is_null_pointer_succeed_tests, test_output) {
+  auto [output, result] = run_test_("assume_is_null_pointer_succeed_tests.*");
+  assert_value_("Start 1 test from 1 test case\n"
+    "  SUCCEED assume_is_null_pointer_succeed_tests.test_case_succeed\n"
+    "End 1 test from 1 test case ran.\n", output);
+}
+
+void test_(assume_is_null_pointer_succeed_tests, test_result) {
+  auto [output, result] = run_test_("assume_is_null_pointer_succeed_tests.*");
+  assert_value_(0, result);
+}
---
+++ tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/assume_is_null_shared_ptr_failed_tests.cpp
@@ -0,0 +1,26 @@
+#include "../../../assert_unit_tests/assert_unit_tests.h"
+#include <xtd/xtd.tunit>
+#include <memory>
+
+namespace xtd::tunit::tests {
+  class test_class_(assume_is_null_shared_ptr_failed_tests) {
+  public:
+    void test_method_(test_case_failed) {
+      std::shared_ptr<int> p = std::make_shared<int>(42);
+      xtd::tunit::assume::is_null(p);
+    }
+  };
+}
+
+void test_(assume_is_null_shared_ptr_failed_tests, test_output) {
+  auto [output, result] = run_test_("assume_is_null_shared_ptr_failed_tests.*");
+  assert_value_("Start 1 test from 1 test case\n"
+    "  ABORTED assume_is_null_shared_ptr_failed_tests.test_case_failed\n"
+    "    Test aborted\n"
+    "End 1 test from 1 test case ran.\n", output);
+}
+
+void test_(assume_is_null_shared_ptr_failed_tests, test_result) {
+  auto [output, result] = run_test_("assume_is_null_shared_ptr_failed_tests.*");
+  assert_value_(0, result);
+}
