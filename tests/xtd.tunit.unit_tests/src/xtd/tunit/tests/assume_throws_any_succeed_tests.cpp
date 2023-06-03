2e99699b6166b8105bdd81bf71fa70e88fc8abb7
Gammasoft <gammasoft71@gmail.com>
2023-06-03 21:53:26 +0200

    Add tunit unit tests

---
+++ tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/assume_throws_any_succeed_tests.cpp
@@ -0,0 +1,25 @@
+#include "../../../assert_unit_tests/assert_unit_tests.h"
+#include <xtd/xtd.tunit>
+#include <vector>
+
+namespace xtd::tunit::tests {
+  class test_class_(assume_throws_any_succeed_tests) {
+  public:
+    void test_method_(test_case_succeed) {
+      std::vector v = {1, 2, 3, 4};
+      xtd::tunit::assume::throws_any([&] {v.at(5);});
+    }
+  };
+}
+
+void test_(assume_throws_any_succeed_tests, test_output) {
+  auto [output, result] = run_test_("assume_throws_any_succeed_tests.*");
+  assert_value_("Start 1 test from 1 test case\n"
+    "  SUCCEED assume_throws_any_succeed_tests.test_case_succeed\n"
+    "End 1 test from 1 test case ran.\n", output);
+}
+
+void test_(assume_throws_any_succeed_tests, test_result) {
+  auto [output, result] = run_test_("assume_throws_any_succeed_tests.*");
+  assert_value_(0, result);
+}
---
+++ tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/assume_throws_failed_tests.cpp
@@ -0,0 +1,26 @@
+#include "../../../assert_unit_tests/assert_unit_tests.h"
+#include <xtd/xtd.tunit>
+#include <vector>
+
+namespace xtd::tunit::tests {
+  class test_class_(assume_throws_failed_tests) {
+  public:
+    void test_method_(test_case_failed) {
+      std::vector v = {1, 2, 3, 4};
+      xtd::tunit::assume::throws<std::out_of_range>([&] {v.at(2);});
+    }
+  };
+}
+
+void test_(assume_throws_failed_tests, test_output) {
+  auto [output, result] = run_test_("assume_throws_failed_tests.*");
+  assert_value_("Start 1 test from 1 test case\n"
+    "  ABORTED assume_throws_failed_tests.test_case_failed\n"
+    "    Test aborted\n"
+    "End 1 test from 1 test case ran.\n", output);
+}
+
+void test_(assume_throws_failed_tests, test_result) {
+  auto [output, result] = run_test_("assume_throws_failed_tests.*");
+  assert_value_(0, result);
+}
