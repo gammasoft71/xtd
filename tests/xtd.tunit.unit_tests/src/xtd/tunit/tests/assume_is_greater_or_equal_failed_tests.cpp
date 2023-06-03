e0327aa8ac78faacd237eb405fc4b167c9933779
Gammasoft <gammasoft71@gmail.com>
2023-06-03 14:46:34 +0200

    Add tunit unit tests

---
+++ tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/assume_is_greater_or_equal_failed_tests.cpp
@@ -0,0 +1,25 @@
+#include "../../../assert_unit_tests/assert_unit_tests.h"
+#include <xtd/xtd.tunit>
+
+namespace xtd::tunit::tests {
+  class test_class_(assume_is_greater_or_equal_failed_tests) {
+  public:
+    void test_method_(test_case_failed) {
+      int i = 42;
+      xtd::tunit::assume::is_greater_or_equal(i, 84);
+    }
+  };
+}
+
+void test_(assume_is_greater_or_equal_failed_tests, test_output) {
+  auto [output, result] = run_test_("assume_is_greater_or_equal_failed_tests.*");
+  assert_value_("Start 1 test from 1 test case\n"
+    "  ABORTED assume_is_greater_or_equal_failed_tests.test_case_failed\n"
+    "    Test aborted\n"
+    "End 1 test from 1 test case ran.\n", output);
+}
+
+void test_(assume_is_greater_or_equal_failed_tests, test_result) {
+  auto [output, result] = run_test_("assume_is_greater_or_equal_failed_tests.*");
+  assert_value_(0, result);
+}
