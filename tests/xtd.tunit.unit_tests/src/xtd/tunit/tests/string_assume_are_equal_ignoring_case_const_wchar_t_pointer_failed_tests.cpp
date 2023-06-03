3c9b1a15e523427969294750d76eb9c1103eab9c
Gammasoft <gammasoft71@gmail.com>
2023-06-03 22:59:13 +0200

    Add tunit unit tests

---
+++ tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/string_assume_are_equal_ignoring_case_const_wchar_t_pointer_failed_tests.cpp
@@ -0,0 +1,25 @@
+#include "../../../assert_unit_tests/assert_unit_tests.h"
+#include <xtd/xtd.tunit>
+
+namespace xtd::tunit::tests {
+  class test_class_(string_assume_are_equal_ignoring_case_const_wchar_t_pointer_failed_tests) {
+  public:
+    void test_method_(test_case_failed) {
+      const wchar* s = L"VALUE";
+      xtd::tunit::string_assume::are_equal_ignoring_case(L"key", s);
+    }
+  };
+}
+
+void test_(string_assume_are_equal_ignoring_case_const_wchar_t_pointer_failed_tests, test_output) {
+  auto [output, result] = run_test_("string_assume_are_equal_ignoring_case_const_wchar_t_pointer_failed_tests.*");
+  assert_value_("Start 1 test from 1 test case\n"
+    "  ABORTED string_assume_are_equal_ignoring_case_const_wchar_t_pointer_failed_tests.test_case_failed\n"
+    "    Test aborted\n"
+    "End 1 test from 1 test case ran.\n", output);
+}
+
+void test_(string_assume_are_equal_ignoring_case_const_wchar_t_pointer_failed_tests, test_result) {
+  auto [output, result] = run_test_("string_assume_are_equal_ignoring_case_const_wchar_t_pointer_failed_tests.*");
+  assert_value_(0, result);
+}
