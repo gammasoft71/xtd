cb438db26f76f0dd7a01bb778996d44ab3ef4ced
Gammasoft <gammasoft71@gmail.com>
2023-06-03 21:06:34 +0200

    Add tunit unit tests

---
+++ tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/assume_is_not_null_unique_ptr_succeed_tests.cpp
@@ -0,0 +1,25 @@
+#include "../../../assert_unit_tests/assert_unit_tests.h"
+#include <xtd/xtd.tunit>
+#include <memory>
+
+namespace xtd::tunit::tests {
+  class test_class_(assume_is_not_null_unique_ptr_succeed_tests) {
+  public:
+    void test_method_(test_case_succeed) {
+      std::unique_ptr<int> p = std::make_unique<int>(42);
+      xtd::tunit::assume::is_not_null(p);
+    }
+  };
+}
+
+void test_(assume_is_not_null_unique_ptr_succeed_tests, test_output) {
+  auto [output, result] = run_test_("assume_is_not_null_unique_ptr_succeed_tests.*");
+  assert_value_("Start 1 test from 1 test case\n"
+    "  SUCCEED assume_is_not_null_unique_ptr_succeed_tests.test_case_succeed\n"
+    "End 1 test from 1 test case ran.\n", output);
+}
+
+void test_(assume_is_not_null_unique_ptr_succeed_tests, test_result) {
+  auto [output, result] = run_test_("assume_is_not_null_unique_ptr_succeed_tests.*");
+  assert_value_(0, result);
+}
