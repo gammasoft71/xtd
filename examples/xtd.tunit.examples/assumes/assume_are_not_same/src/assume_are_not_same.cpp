3228c07750391d6b1f9f3613a9e31a4c0c274b65
Gammasoft <gammasoft71@gmail.com>
2023-06-04 17:39:28 +0200

    Add assume_are_not_same example

---
+++ examples/xtd.tunit.examples/assumes/assume_are_not_same/src/assume_are_not_same.cpp
@@ -0,0 +1,39 @@
+#include <xtd/xtd.tunit>
+
+using namespace xtd::tunit;
+
+namespace unit_tests {
+  class test_class_(test) {
+  public:
+    void test_method_(test_case_succeed) {
+      int a = 24;
+      int b =  24;
+      assume::are_not_same(b, a);
+    }
+    
+    void test_method_(test_case_failed) {
+      int a = 24;
+      int& b = a;
+      assume::are_not_same(b, a);
+    }
+  };
+}
+
+auto main()->int {
+  return console_unit_test().run();
+}
+
+// This code produces the following output:
+//
+// Start 2 tests from 1 test case
+// Run tests:
+//   SUCCEED test.test_case_succeed (0 ms total)
+//   FAILED  test.test_case_failed (0 ms total)
+//     Expected: not same as 24
+//     But was:  24
+//     Stack Trace: in |---OMITTED---|/assume_are_not_same.cpp:14
+//
+// Test results:
+//   SUCCEED 1 test.
+//   FAILED  1 test.
+// End 2 tests from 1 test case ran. (0 ms total)
