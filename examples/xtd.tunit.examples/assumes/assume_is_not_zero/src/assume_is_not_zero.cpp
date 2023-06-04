c0af400914346203694446d8ac9177f92c78fefb
Gammasoft <gammasoft71@gmail.com>
2023-06-04 20:37:14 +0200

    Add assume_is_not_zero example

---
+++ examples/xtd.tunit.examples/assumes/assume_is_not_zero/src/assume_is_not_zero.cpp
@@ -0,0 +1,40 @@
+#include <xtd/xtd.tunit>
+#include <iterator>
+#include <string>
+
+using namespace std;
+using namespace xtd::tunit;
+
+namespace unit_tests {
+  class test_class_(test) {
+  public:
+    void test_method_(test_case_succeed) {
+      int i = 42;
+      assume::is_not_zero(i);
+    }
+    
+    void test_method_(test_case_failed) {
+      int i = 0;
+      assume::is_not_zero(i);
+    }
+  };
+}
+
+auto main()->int {
+  return console_unit_test().run();
+}
+
+// This code produces the following output:
+//
+// Start 2 tests from 1 test case
+// Run tests:
+//   SUCCEED test.test_case_succeed (0 ms total)
+//   ABORTED test.test_case_failed (0 ms total)
+//     Expected: not zero
+//     But was:  0
+//     Stack Trace: in |---OMITTED---|/assume_is_not_zero.cpp:15
+//
+// Test results:
+//   SUCCEED 1 test.
+//   ABORTED 1 test.
+// End 2 tests from 1 test case ran. (0 ms total)
