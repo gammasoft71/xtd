498df77b434923017617fb1dfc79063af9eba5e2
Gammasoft <gammasoft71@gmail.com>
2023-06-04 18:52:18 +0200

    Add assume_is_false example

---
+++ examples/xtd.tunit.examples/assumes/assume_is_false/src/assume_is_false.cpp
@@ -0,0 +1,41 @@
+#include <xtd/xtd.tunit>
+#include <iterator>
+#include <string>
+
+using namespace std;
+using namespace xtd::tunit;
+
+namespace unit_tests {
+  class test_class_(test) {
+  public:
+    void test_method_(test_case_succeed) {
+      string s = "string";
+      assume::is_false(std::empty(s));
+    }
+    
+    void test_method_(test_case_failed) {
+      string s;
+      assume::is_false(std::empty(s));
+    }
+  };
+}
+
+auto main()->int {
+  return console_unit_test().run();
+}
+
+// This code produces the following output:
+//
+// Start 2 tests from 1 test case
+// Run tests:
+//   SUCCEED test.test_case_succeed (0 ms total)
+//   ABORTED test.test_case_failed (0 ms total)
+//     Expected: false
+//     But was:  true
+//     Stack Trace: in |---OMITTED---|/assume_is_false.cpp:16
+//
+// Test results:
+//   SUCCEED 1 test.
+//   ABORTED 1 test.
+// End 2 tests from 1 test case ran. (0 ms total)
+
