c5ccf00377e8a0c0682a1e34ed1da37a4e9c3595
Gammasoft <gammasoft71@gmail.com>
2023-06-04 20:06:59 +0200

    Add assume_is_NaN example

---
+++ examples/xtd.tunit.examples/assumes/assume_is_NaN/README.md
@@ -0,0 +1,34 @@
+# assert_is_NaN
+
+Shows how to use [xtd::tunit::assert::is_NaN](https://gammasoft71.github.io/xtd/reference_guides/latest/classxtd_1_1tunit_1_1assume.html#ae0feb57a4c084d50894849e18d085788) method.
+
+## Sources
+
+[src/assert_is_NaN.cpp](src/assert_is_NaN.cpp)
+
+[CMakeLists.txt](CMakeLists.txt)
+
+# Build and run
+
+Open "Command Prompt" or "Terminal". Navigate to the folder that contains the project and type the following:
+
+```cmake
+xtdc run
+```
+
+# Output
+
+```
+Start 2 tests from 1 test case
+Run tests:
+  SUCCEED test.test_case_succeed (0 ms total)
+  ABORTED test.test_case_failed (0 ms total)
+    Expected: NaN
+    But was:  3.14159
+    Stack Trace: in |---OMITTED---|/assert_is_NaN.cpp:15
+
+// Test results:
+  SUCCEED 1 test.
+  ABORTED 1 test.
+End 2 tests from 1 test case ran. (0 ms total)
+```
---
+++ examples/xtd.tunit.examples/assumes/assume_is_NaN/src/assume_is_NaN.cpp
@@ -0,0 +1,39 @@
+#include <xtd/xtd.tunit>
+#include <limits>
+
+using namespace std;
+using namespace xtd::tunit;
+
+namespace unit_tests {
+  class test_class_(test) {
+  public:
+    void test_method_(test_case_succeed) {
+      double d = numeric_limits<double>::quiet_NaN();
+      assume::is_NaN(d);
+    }
+    
+    void test_method_(test_case_failed) {
+      double d = 3.14159;
+      assume::is_NaN(d);
+    }
+  };
+}
+
+auto main()->int {
+  return console_unit_test().run();
+}
+
+// This code produces the following output:
+//
+// Start 2 tests from 1 test case
+// Run tests:
+//   SUCCEED test.test_case_succeed (0 ms total)
+//   ABORTED test.test_case_failed (0 ms total)
+//     Expected: NaN
+//     But was:  3.14159
+//     Stack Trace: in |---OMITTED---|/assume_is_NaN.cpp:15
+//
+// Test results:
+//   SUCCEED 1 test.
+//   ABORTED 1 test.
+// End 2 tests from 1 test case ran. (0 ms total)
