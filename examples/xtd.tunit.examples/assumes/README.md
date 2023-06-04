a7f4bb34321989728597cb96715852234080a813
Gammasoft <gammasoft71@gmail.com>
2023-06-04 18:17:58 +0200

    Add assume_contains example

--- examples/xtd.tunit.examples/assumes/README.md
+++ examples/xtd.tunit.examples/assumes/README.md
@@ -6,3 +6,4 @@
 * [assume_are_not_equal](assume_are_not_equal/README.md) shows how to use [xtd::tunit::assume::are_not_equal](https://gammasoft71.github.io/xtd/reference_guides/latest/classxtd_1_1tunit_1_1assume.html#a18979db548114b94daa0a137c154acb2) method.
 * [assume_are_not_same](assume_are_not_same/README.md) shows how to use [xtd::tunit::assume::are_not_same](https://gammasoft71.github.io/xtd/reference_guides/latest/classxtd_1_1tunit_1_1assume.html#a27df29a18fb50ab2793991a06d227276) method.
 * [assume_are_same](assume_are_same/README.md) shows how to use [xtd::tunit::assume::are_same](https://gammasoft71.github.io/xtd/reference_guides/latest/classxtd_1_1tunit_1_1assume.html#af93128c52d73fd842286247b428bfd09) method.
+* [assume_contains](assume_contains/README.md) shows how to use [xtd::tunit::assume::contains](https://gammasoft71.github.io/xtd/reference_guides/latest/classxtd_1_1tunit_1_1assume.html#af46db342cef45ea859709a544922585d) method.
---
+++ examples/xtd.tunit.examples/assumes/assume_contains/src/assume_contains.cpp
@@ -0,0 +1,50 @@
+#include <xtd/xtd.tunit>
+#include <vector>
+
+using namespace std;
+using namespace xtd::tunit;
+
+namespace unit_tests {
+  class test_class_(test) {
+  public:
+    void test_method_(test_case_collection_succeed) {
+      assume::contains(2, {0, 1, 2, 3});
+    }
+    
+    void test_method_(test_case_collection_failed) {
+      assume::contains(4, {0, 1, 2, 3});
+    }
+    
+    void test_method_(test_case_string_succeed) {
+      assume::contains('i', "string");
+    }
+    
+    void test_method_(test_case_string_failed) {
+      assume::contains('a', "string");
+    }
+  };
+}
+
+auto main()->int {
+  return console_unit_test().run();
+}
+
+// This code produces the following output:
+//
+// Start 4 tests from 1 test case
+// Run tests:
+//   SUCCEED test.test_case_collection_succeed (0 ms total)
+//   ABORTED test.test_case_collection_failed (0 ms total)
+//     Expected: collection containing 4
+//     But was:  < 0, 1, 2, 3 >
+//     Stack Trace: in |---OMITTED---|/assume_contains.cpp:14
+//   SUCCEED test.test_case_string_succeed (0 ms total)
+//   ABORTED test.test_case_string_failed (0 ms total)
+//     Expected: collection containing a
+//     But was:  < 's', 't', 'r', 'i', 'n', 'g' >
+//     Stack Trace: in |---OMITTED---|/assume_contains.cpp:22
+//
+// Test results:
+//   SUCCEED 2 tests.
+//   ABORTED 2 tests.
+// End 4 tests from 1 test case ran. (0 ms total)
