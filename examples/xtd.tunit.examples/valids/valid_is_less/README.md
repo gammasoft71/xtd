7fa78ec1ff4cb46a82b9d5099f444bdd24be782b
Gammasoft <gammasoft71@gmail.com>
2023-06-04 23:17:14 +0200

    Add valid_is_less example

---
+++ examples/xtd.tunit.examples/valids/valid_is_less/README.md
@@ -0,0 +1,34 @@
+# valid_is_less
+
+Shows how to use [xtd::tunit::valid::is_less](https://gammasoft71.github.io/xtd/reference_guides/latest/classxtd_1_1tunit_1_1valid.html#a919ed90c86a9eb75bc5dd79e13f2fc8d) method.
+
+## Sources
+
+[src/valid_is_less.cpp](src/valid_is_less.cpp)
+
+[CMakeLists.txt](CMakeLists.txt)
+
+# Build and run
+
+Open "Command Prompt" or "Terminal". Navigate to the folder that contains the project and type the following:
+
+```cmake
+xtdc run
+```
+
+# Output
+
+```
+Start 2 tests from 1 test case
+Run tests:
+  SUCCEED test.test_case_succeed (0 ms total)
+  FAILED  test.test_case_failed (0 ms total)
+    Expected: less than 12
+    But was:  24
+    Stack Trace: in |---OMITTED---|/valid_is_less.cpp:13
+
+Test results:
+  SUCCEED 1 test.
+  FAILED  1 test.
+End 2 tests from 1 test case ran. (0 ms total)
+```
---
+++ examples/xtd.tunit.examples/valids/valid_is_less/src/valid_is_less.cpp
@@ -0,0 +1,37 @@
+#include <xtd/xtd.tunit>
+
+using namespace xtd::tunit;
+
+namespace unit_tests {
+  class test_class_(test) {
+  public:
+    void test_method_(test_case_succeed) {
+      int i = 24;
+      valid::is_less(i, 48);
+    }
+    
+    void test_method_(test_case_failed) {
+      int i = 24;
+      valid::is_less(i, 12);
+    }
+  };
+}
+
+auto main()->int {
+  return console_unit_test().run();
+}
+
+// This code produces the following output:
+//
+// Start 2 tests from 1 test case
+// Run tests:
+//   SUCCEED test.test_case_succeed (0 ms total)
+//   FAILED  test.test_case_failed (0 ms total)
+//     Expected: less than 12
+//     But was:  24
+//     Stack Trace: in |---OMITTED---|/valid_is_less.cpp:13
+//
+// Test results:
+//   SUCCEED 1 test.
+//   FAILED  1 test.
+// End 2 tests from 1 test case ran. (0 ms total)
