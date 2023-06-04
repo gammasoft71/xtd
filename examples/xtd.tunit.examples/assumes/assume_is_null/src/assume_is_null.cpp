c7347b2547cfff63e4705f8a67a9ac98b942ecba
Gammasoft <gammasoft71@gmail.com>
2023-06-04 20:41:38 +0200

    Add assume_is_null example

---
+++ examples/xtd.tunit.examples/assumes/assume_is_null/src/assume_is_null.cpp
@@ -0,0 +1,39 @@
+#include <xtd/xtd.tunit>
+#include <memory>
+
+using namespace std;
+using namespace xtd::tunit;
+
+namespace unit_tests {
+  class test_class_(test) {
+  public:
+    void test_method_(test_case_succeed) {
+      shared_ptr<string> s;
+      assume::is_null(s);
+    }
+    
+    void test_method_(test_case_failed) {
+      shared_ptr<string> s = make_shared<string>("string");
+      assume::is_null(s);
+    }
+  };
+}
+
+auto main()->int {
+  return console_unit_test().run();
+}
+
+// This code produces the following output:
+//
+// Start 2 tests from 1 test case
+// Run tests:
+//   SUCCEED test.test_case_succeed (0 ms total)
+//   ABORTED test.test_case_failed (0 ms total)
+//     Expected: null
+//     But was:  not null
+//     Stack Trace: in |---OMITTED---|/assume_is_null.cpp:15
+//
+// Test results:
+//   SUCCEED 1 test.
+//   ABORTED 1 test.
+// End 2 tests from 1 test case ran. (0 ms total)
