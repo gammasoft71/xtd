d147035cbece96dd1deee895ef530cb0420b6cc9
Gammasoft <gammasoft71@gmail.com>
2023-06-04 22:28:06 +0200

    Add valid_does_not_throw example

---
+++ examples/xtd.tunit.examples/valids/valid_does_not_throw/README.md
@@ -0,0 +1,34 @@
+# valid_does_not_throw
+
+Shows how to use [xtd::tunit::valid::does_not_throw](https://gammasoft71.github.io/xtd/reference_guides/latest/classxtd_1_1tunit_1_1valid.html#ad5daf4889e0f44e20ad12e1db802e94f) method.
+
+## Sources
+
+[src/valid_does_not_throw.cpp](src/valid_does_not_throw.cpp)
+
+[CMakeLists.txt](CMakeLists.txt)
+
+# Build and run
+
+Open "Command Prompt" or "Terminal". Navigate to the folder that contains the project and type the following:
+
+```cmake
+xtdc run
+```
+
+# Output
+
+```
+Start 2 tests from 1 test case
+Run tests:
+  SUCCEED test.test_case_succeed (0 ms total)
+  FAILED  test.test_case_failed (0 ms total)
+    Expected: No Exception to be thrown
+    But was:  <std::out_of_range>
+    Stack Trace: in |---OMITTED---|/valid_does_not_throw.cpp:15
+
+Test results:
+  SUCCEED 1 test.
+  FAILED  1 test.
+End 2 tests from 1 test case ran. (0 ms total)
+```
---
+++ examples/xtd.tunit.examples/valids/valid_does_not_throw/src/valid_does_not_throw.cpp
@@ -0,0 +1,39 @@
+#include <xtd/xtd.tunit>
+#include <vector>
+
+using namespace std;
+using namespace xtd::tunit;
+
+namespace unit_tests {
+  class test_class_(test) {
+  public:
+    void test_method_(test_case_succeed) {
+      vector v = {1, 2, 3, 4};
+      valid::does_not_throw([&] {return v.at(2);});
+    }
+    
+    void test_method_(test_case_failed) {
+      vector v = {1, 2, 3, 4};
+      valid::does_not_throw([&] {return v.at(5);});
+    }
+  };
+}
+
+auto main()->int {
+  return console_unit_test().run();
+}
+
+// This code produces the following output:
+//
+// Start 2 tests from 1 test case
+// Run tests:
+//   SUCCEED test.test_case_succeed (0 ms total)
+//   FAILED  test.test_case_failed (0 ms total)
+//     Expected: No Exception to be thrown
+//     But was:  <std::out_of_range>
+//     Stack Trace: in |---OMITTED---|/valid_does_not_throw.cpp:15
+//
+// Test results:
+//   SUCCEED 1 test.
+//   FAILED  1 test.
+// End 2 tests from 1 test case ran. (0 ms total)
