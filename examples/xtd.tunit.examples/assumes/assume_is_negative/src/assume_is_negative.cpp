43bdeadca6e2a03c41e6213d6e279186dfcfda46
Gammasoft <gammasoft71@gmail.com>
2023-06-04 20:12:41 +0200

    Add assume_is_negative example

---
+++ examples/xtd.tunit.examples/assumes/assume_is_negative/src/assume_is_negative.cpp
@@ -0,0 +1,40 @@
+#include <xtd/xtd.tunit>
+#include <iterator>
+#include <string>
+
+using namespace std;
+using namespace xtd::tunit;
+
+namespace unit_tests {
+  class test_class_(test) {
+  public:
+    void test_method_(test_case_succeed) {
+      int i = -1;
+      assume::is_negative(i);
+    }
+    
+    void test_method_(test_case_failed) {
+      int i = 1;
+      assume::is_negative(i);
+    }
+  };
+}
+
+auto main()->int {
+  return console_unit_test().run();
+}
+
+// This code produces the following output:
+//
+// Start 2 tests from 1 test case
+// Run tests:
+//   SUCCEED test.test_case_succeed (0 ms total)
+//   ABORTED test.test_case_failed (0 ms total)
+//     Expected: negative
+//     But was:  1
+//     Stack Trace: in |---OMITTED---|/assume_is_negative.cpp:16
+//
+// Test results:
+//   SUCCEED 1 test.
+//   ABORTED 1 test.
+// End 2 tests from 1 test case ran. (0 ms total)
