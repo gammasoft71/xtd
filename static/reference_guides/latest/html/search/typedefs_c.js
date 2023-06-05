867d8ac9ccd243dad03f260c6f86778cd05aaa0c
gammasoft71 <gammasoft71@gmail.com>
2023-06-05 21:00:08 +0200

    update latest reference guide

---
+++ static/reference_guides/latest/html/search/typedefs_c.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['no_5farguments_5ffunction_5ft_0',['no_arguments_function_t',['../classxtd_1_1delegate_3_01result__t_07arguments__t_8_8_8_08_4.html#a0519c93d63e33630da52254043a7e7ab',1,'xtd::delegate&lt; result_t(arguments_t...)&gt;']]]
+];
