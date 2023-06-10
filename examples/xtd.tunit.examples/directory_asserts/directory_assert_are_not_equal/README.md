72487ea75f92656dc898a3fd640a8db56e671b24
Gammasoft <gammasoft71@gmail.com>
2023-06-10 17:56:20 +0200

    Add directory_assert_are_not_equal example

---
+++ examples/xtd.tunit.examples/directory_asserts/directory_assert_are_not_equal/README.md
@@ -0,0 +1,34 @@
+# directory_assert_are_not_equal
+
+Shows how to use [xtd::tunit::directory_assert::are_not_equal](https://gammasoft71.github.io/xtd/reference_guides/latest/classxtd_1_1tunit_1_1directory__assert.html#adb6a94ab27ef36eaec266d8abc4cc60d) method.
+
+## Sources
+
+[src/directory_assert_are_not_equal.cpp](src/directory_assert_are_not_equal.cpp)
+
+[CMakeLists.txt](CMakeLists.txt)
+
+# Build and run
+
+Open "Command Prompt" or "Terminal". Navigate to the folder that contains the project and type the following:
+
+```cmake
+xtdc run
+```
+
+# Output
+
+```
+Start 2 tests from 1 test case
+  Run tests:
+  SUCCEED test.test_case_succeed (0 ms total)
+  FAILED  test.test_case_failed (0 ms total)
+    Expected: not "/var/folders/xg/2fvdl7v939g9kbp8xn1dpgg00000gn/T"
+    But was:  "/var/folders/xg/2fvdl7v939g9kbp8xn1dpgg00000gn/T"
+    Stack Trace: in |---OMITTED---|/directory_assert_are_not_equal.cpp:15
+
+Test results:
+  SUCCEED 1 test.
+  FAILED  1 test.
+End 2 tests from 1 test case ran. (0 ms total)
+```
---
+++ examples/xtd.tunit.examples/directory_asserts/directory_assert_are_not_equal/src/directory_assert_are_not_equal.cpp
@@ -0,0 +1,39 @@
+#include <xtd/xtd.tunit>
+
+using namespace xtd;
+using namespace xtd::io;
+using namespace xtd::tunit;
+
+namespace unit_tests {
+  class test_class_(test) {
+  public:
+    void test_method_(test_case_succeed) {
+      auto di = directory_info(environment::get_folder_path(environment::special_folder::system));
+      directory_assert::are_not_equal(directory_info {path::get_temp_path()}, di);
+    }
+    
+    void test_method_(test_case_failed) {
+      auto di = directory_info {path::get_temp_path()};
+      directory_assert::are_not_equal(directory_info {path::get_temp_path()}, di);
+    }
+  };
+}
+
+auto main()->int {
+  return console_unit_test().run();
+}
+
+// This code produces the following output:
+//
+// Start 2 tests from 1 test case
+// Run tests:
+//   SUCCEED test.test_case_succeed (0 ms total)
+//   FAILED  test.test_case_failed (0 ms total)
+//     Expected: not "/var/folders/xg/2fvdl7v939g9kbp8xn1dpgg00000gn/T"
+//     But was:  "/var/folders/xg/2fvdl7v939g9kbp8xn1dpgg00000gn/T"
+//     Stack Trace: in |---OMITTED---|/directory_assert_are_not_equal.cpp:15
+//
+// Test results:
+//   SUCCEED 1 test.
+//   FAILED  1 test.
+// End 2 tests from 1 test case ran. (0 ms total)
