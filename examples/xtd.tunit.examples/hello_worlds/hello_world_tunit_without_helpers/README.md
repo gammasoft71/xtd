68c5a28919bfbcc779a642d7dca5d1563155db45
Gammasoft <gammasoft71@gmail.com>
2023-06-10 14:46:12 +0200

    Add hello_world_tunit_without_helpers example

---
+++ examples/xtd.tunit.examples/hello_worlds/hello_world_tunit_without_helpers/README.md
@@ -0,0 +1,30 @@
+# hello_world_tunit_without_helpers
+
+The classic first application "Hello, World!" with [xtd::tunit::console_unit_test](https://gammasoft71.github.io/xtd/reference_guides/latest/classxtd_1_1tunit_1_1console__unit__test.html) class and without helpers.
+
+## Sources
+
+[src/hello_world_tunit_without_helpers.cpp](src/hello_world_tunit_without_helpers.cpp)
+
+[CMakeLists.txt](CMakeLists.txt)
+
+# Build and run
+
+Open "Command Prompt" or "Terminal". Navigate to the folder that contains the project and type the following:
+
+```cmake
+xtdc run
+```
+
+# Output
+
+```
+Start 2 tests from 1 test case
+Run tests:
+  SUCCEED hello_world_tunit_without_helpers.create_string_from_literal (0 ms total)
+  SUCCEED hello_world_tunit_without_helpers.create_string_from_chars (0 ms total)
+
+Test results:
+  SUCCEED 2 tests.
+End 2 tests from 1 test case ran. (0 ms total)
+```
---
+++ examples/xtd.tunit.examples/hello_worlds/hello_world_tunit_without_helpers/src/hello_world_tunit_without_helpers.cpp
@@ -0,0 +1,43 @@
+#include <xtd/xtd.tunit>
+#include <string>
+
+using namespace std;
+using namespace xtd::tunit;
+
+namespace unit_tests {
+  class hello_world_test;
+  
+  test_class_attribute<hello_world_test> hello_world_test_class_attr {"hello_world_test"};
+  class hello_world_test : public test_class {
+  public:
+    test_method_attribute create_string_from_literal_attr {"create_string_from_literal", *this, &hello_world_test::create_string_from_literal};
+    void create_string_from_literal() {
+      string s = "Hello, World!";
+      valid::are_equal(13, s.size());
+      assert::are_equal("Hello, World!", s);
+    }
+    
+    test_method_attribute create_string_from_chars_attr {"create_string_from_chars", *this, &hello_world_test::create_string_from_chars};
+    void create_string_from_chars() {
+      string s = {'H', 'e', 'l', 'l', 'o', ',', ' ', 'W', 'o', 'r', 'l', 'd', '!'};
+      valid::are_equal(13, s.size());
+      string_assert::starts_with("Hello,", s);
+      string_assert::ends_with(" World!", s);
+    }
+  };
+}
+
+auto main()->int {
+  return console_unit_test().run();
+}
+
+// This code produces the following output:
+//
+// Start 2 tests from 1 test case
+// Run tests:
+//   SUCCEED hello_world_test.create_string_from_literal (0 ms total)
+//   SUCCEED hello_world_test.create_string_from_chars (0 ms total)
+//
+// Test results:
+//   SUCCEED 2 tests.
+// End 2 tests from 1 test case ran. (0 ms total)
