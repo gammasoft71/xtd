f4391da3ac371dfee985f2659690127952f0c6fd
Gammasoft <gammasoft71@gmail.com>
2023-06-10 19:53:11 +0200

    Add file_assert_are_not_equal example

--- examples/xtd.tunit.examples/README.md
+++ examples/xtd.tunit.examples/README.md
@@ -71,6 +71,7 @@
 
 * [file_assert](file_asserts/file_assert/README.md) shows how to use [xtd::tunit::file_assert](https://gammasoft71.github.io/xtd/reference_guides/latest/classxtd_1_1tunit_1_1file__assert.html) class.
 * [file_assert_are_equal](file_asserts/file_assert_are_equal/README.md) shows how to use [xtd::tunit::file_assert::are_equal](https://gammasoft71.github.io/xtd/reference_guides/latest/classxtd_1_1tunit_1_1file__assert.html#a96fc1cd7bccd421b4506f2883ec8e63b) method.
+* [file_assert_are_not_equal](file_asserts/file_assert_are_not_equal/README.md) shows how to use [xtd::tunit::file_assert::are_not_equal](https://gammasoft71.github.io/xtd/reference_guides/latest/classxtd_1_1tunit_1_1file__assert.html#a21c91ba00241ff7cfc030b445c1895a4) method.
 
 ## [String asserts](string_asserts/README.md)
 
---
+++ examples/xtd.tunit.examples/file_asserts/file_assert_are_not_equal/src/file_assert_are_not_equal.cpp
@@ -0,0 +1,38 @@
+#include <xtd/xtd.tunit>
+
+using namespace std;
+using namespace xtd::tunit;
+
+namespace unit_tests {
+  class test_class_(test) {
+  public:
+    void test_method_(test_case_succeed) {
+      auto is = istringstream {"xtd::tunit::file_assert::are_not_equal example."};
+      file_assert::are_not_equal(istringstream {"xtd::tunit::file_assert::are_not_equel example."}, is);
+    }
+    
+    void test_method_(test_case_failed) {
+      auto is = istringstream {"xtd::tunit::file_assert::are_not_equal example."};
+      file_assert::are_not_equal(istringstream {"xtd::tunit::file_assert::are_not_equal example."}, is);
+    }
+  };
+}
+
+auto main()->int {
+  return console_unit_test().run();
+}
+
+// This code produces the following output:
+//
+// Start 2 tests from 1 test case
+// Run tests:
+//   SUCCEED test.test_case_succeed (0 ms total)
+//   FAILED  test.test_case_failed (0 ms total)
+//     Expected: not equal <std::basic_istringstream<char, std::char_traits<char>, std::allocator<char>>>
+//     But was:  <std::basic_istringstream<char, std::char_traits<char>, std::allocator<char>>>
+//     Stack Trace: in |---OMITTED---|/file_assert_are_not_equal.cpp:14
+//
+// Test results:
+//   SUCCEED 1 test.
+//   FAILED  1 test.
+// End 2 tests from 1 test case ran. (0 ms total)
