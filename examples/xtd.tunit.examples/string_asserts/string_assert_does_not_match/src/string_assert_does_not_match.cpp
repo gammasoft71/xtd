1380fdb404d7bdfee7a480b4ea83d71a06937fb4
Gammasoft <gammasoft71@gmail.com>
2023-06-11 23:24:44 +0200

    Add string_assert_does_not_match example

---
+++ examples/xtd.tunit.examples/string_asserts/string_assert_does_not_match/src/string_assert_does_not_match.cpp
@@ -0,0 +1,40 @@
+#include <xtd/xtd.tunit>
+#include <string>
+
+using namespace std;
+using namespace std::string_literals;
+using namespace xtd::tunit;
+
+namespace unit_tests {
+  class test_class_(test) {
+  public:
+    void test_method_(test_case_succeed) {
+      auto s = "value"s;
+      string_assert::does_not_match("^item", s);
+    }
+    
+    void test_method_(test_case_failed) {
+      auto s = L"value";
+      string_assert::does_not_match(L"^value", s);
+    }
+  };
+}
+
+auto main()->int {
+  return console_unit_test().run();
+}
+
+// This code produces the following output:
+//
+// Start 2 tests from 1 test case
+// Run tests:
+//   SUCCEED test.test_case_succeed (0 ms total)
+//   FAILED  test.test_case_failed (0 ms total)
+//     Expected: not string matching "^value"
+//     But was:  "value"
+//     Stack Trace: in |---OMITTED---|/string_assert_does_not_match.cpp:16
+//
+// Test results:
+//   SUCCEED 1 test.
+//   FAILED  1 test.
+// End 2 tests from 1 test case ran. (0 ms total)
