9a1fdd7625b598c2260f67b9c268b95e562c6177
Gammasoft <gammasoft71@gmail.com>
2023-06-11 11:49:00 +0200

    Add string_assert example

--- src/xtd.tunit/include/xtd/tunit/string_assert.h
+++ src/xtd.tunit/include/xtd/tunit/string_assert.h
@@ -23,7 +23,7 @@ namespace xtd {
     /// @remarks Each method may be called without a message, with a simple text message or with a message and arguments. In the last case the message is formatted using the provided text and arguments.
     /// @par Examples
     /// This example show how to used some methods :
-    /// @include assert.cpp
+    /// @include string_assert.cpp
     class tunit_export_ string_assert final : private base_assert {
     public:
       /// @cond
