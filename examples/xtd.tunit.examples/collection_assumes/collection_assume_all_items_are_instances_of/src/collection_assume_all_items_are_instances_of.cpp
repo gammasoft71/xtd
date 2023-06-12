ace65834b80f8e7fc71a4085f1579a10cacafc1f
Gammasoft <gammasoft71@gmail.com>
2023-06-12 15:01:22 +0200

    Rename xxx_failed method to xxx_aborted

--- examples/xtd.tunit.examples/collection_assumes/collection_assume_all_items_are_instances_of/src/collection_assume_all_items_are_instances_of.cpp
+++ examples/xtd.tunit.examples/collection_assumes/collection_assume_all_items_are_instances_of/src/collection_assume_all_items_are_instances_of.cpp
@@ -10,7 +10,7 @@ namespace unit_tests {
       collection_assume::all_items_are_instances_of<std::ios_base*>(a);
     }
     
-    void test_method_(test_case_failed) {
+    void test_method_(test_case_aborted) {
       std::vector<std::ios_base*> a = {&std::cout, &std::cerr, &std::cin};
       collection_assume::all_items_are_instances_of<std::basic_ostream<char>*>(a);
     }
@@ -26,7 +26,7 @@ auto main()->int {
 // Start 2 tests from 1 test case
 // Run tests:
 //   SUCCEED test.test_case_succeed (0 ms total)
-//   ABORTED test.test_case_failed (0 ms total)
+//   ABORTED test.test_case_aborted (0 ms total)
 //     Test aborted
 //     Expected: all items instance of <std::basic_ostream<char, std::char_traits<char> >*>
 //     But was:  < 8-byte object <78-37 1D-8C FF-7F 00-00>, 8-byte object <B8-38 1D-8C FF-7F 00-00>, 8-byte object <30-36 1D-8C FF-7F 00-00> >
--- examples/xtd.tunit.examples/collection_assumes/collection_assume_contains/src/collection_assume_contains.cpp
+++ examples/xtd.tunit.examples/collection_assumes/collection_assume_contains/src/collection_assume_contains.cpp
@@ -10,7 +10,7 @@ namespace unit_tests {
       collection_assume::contains({1, 2}, a);
     }
     
-    void test_method_(test_case_failed) {
+    void test_method_(test_case_aborted) {
       std::vector a = {1, 2, 3, 4};
       collection_assume::contains({1, 2, 6}, a);
     }
@@ -26,7 +26,7 @@ auto main()->int {
 // Start 2 tests from 1 test case
 // Run tests:
 //   SUCCEED test.test_case_succeed (0 ms total)
-//   ABORTED test.test_case_failed (0 ms total)
+//   ABORTED test.test_case_aborted (0 ms total)
 //     Test aborted
 //     Expected: contains < 1, 2, 6 >
 //     But was:  < 1, 2, 3, 4 >
