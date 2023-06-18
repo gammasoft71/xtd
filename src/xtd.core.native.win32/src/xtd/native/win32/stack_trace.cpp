91d61863c2d8e549ded253a2ecc8d6952ee64b3b
Gammasoft <gammasoft71@gmail.com>
2023-06-18 21:35:20 +0200

    Review code

--- src/xtd.core.native.win32/src/xtd/native/win32/stack_trace.cpp
+++ src/xtd.core.native.win32/src/xtd/native/win32/stack_trace.cpp
@@ -11,33 +11,34 @@
 using namespace std;
 using namespace xtd::native;
 
-class stack_walker : public StackWalker {
+class stack_walker final : public StackWalker {
 public:
   using frame = std::tuple<std::string, size_t, size_t, std::string, size_t>;
   using frames = std::vector<frame>;
 
   stack_walker() : StackWalker(StackWalker::RetrieveVerbose | StackWalker::SymBuildPath) {}
 
-  const std::vector<frame>& stack() const noexcept {return stack_;}
+  const frames& get_frames() const noexcept {return frames_;}
 
-protected:
+private:
   void OnCallstackEntry(CallstackEntryType type, CallstackEntry& entry) override {
     StackWalker::OnCallstackEntry(type, entry);
-    stack_.push_back(make_tuple(entry.lineFileName, entry.lineNumber, entry.offsetFromLine, entry.name, static_cast<size_t>(entry.offsetFromSmybol)));
+    frames_.push_back(make_tuple(entry.lineFileName, entry.lineNumber, entry.offsetFromLine, entry.name, static_cast<size_t>(entry.offsetFromSmybol)));
   }
 
 private:
-  std::vector<frame> stack_;
+  frames frames_;
 };
 
 stack_trace::frames stack_trace::get_frames(size_t skip_frames) {
-  stack_walker sw;
-  sw.ShowCallstack();
+  auto sw = stack_walker {};
+  if (!sw.ShowCallstack()) return {};
 
-  stack_trace::frames frames;
-  for (size_t index = skip_frames + 2; index < sw.stack().size(); ++index) {
-    frames.push_back(sw.stack()[index]);
-    if (get<3>(sw.stack()[index]) == "main") break;
+  auto result = stack_trace::frames {};
+  auto frames = sw.get_frames();
+  for (auto index = skip_frames + 2; index < frames.size(); ++index) {
+    result.push_back(frames[index]);
+    if (get<3>(frames[index]) == "main") break;
   }
-  return frames;
+  return result;
 }
