648bdcf7d1c1a4f19219078a46e45aba5eeeb9fd
Gammasoft <gammasoft71@gmail.com>
2023-06-25 19:45:33 +0200

    Add exit_status enumeration

---
+++ tests/xtd.core.unit_tests/src/xtd/tests/exit_status_tests.cpp
@@ -0,0 +1,22 @@
+#include <xtd/exit_status.h>
+#include <xtd/xtd.tunit>
+
+using namespace xtd;
+using namespace xtd::tunit;
+
+namespace xtd::tests {
+  class test_class_(exit_status_tests) {
+  public:
+    void test_method_(success) {
+      assert::are_equal(EXIT_SUCCESS, enum_object<>::to_int32(exit_status::success), csf_);
+      assert::are_equal("success", enum_object<>::to_string(exit_status::success), csf_);
+      assert::are_equal(exit_status::success, enum_object<>::parse<exit_status>("success"), csf_);
+    }
+    
+    void test_method_(failure) {
+      assert::are_equal(EXIT_FAILURE, enum_object<>::to_int32(exit_status::failure), csf_);
+      assert::are_equal("failure", enum_object<>::to_string(exit_status::failure), csf_);
+      assert::are_equal(exit_status::failure, enum_object<>::parse<exit_status>("failure"), csf_);
+    }
+  };
+}
