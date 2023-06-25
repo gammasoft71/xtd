bd318c7a880e0377549626c83ddaa1b865e7d1a1
Gammasoft <gammasoft71@gmail.com>
2023-06-25 23:14:03 +0200

    Review example

--- examples/xtd.core.examples/environment/environment_program_stopped/src/environment_program_stopped.cpp
+++ examples/xtd.core.examples/environment/environment_program_stopped/src/environment_program_stopped.cpp
@@ -1,5 +1,7 @@
 #include <xtd/xtd>
 
+using namespace std;
+using namespace std::this_thread;
 using namespace xtd;
 
 namespace environment_cancel_sgnal_example {
@@ -10,6 +12,18 @@ namespace environment_cancel_sgnal_example {
       environment::program_stopped += [] {
         console::write_line("The program is stopped!");
       };
+      
+      console::write_line("Start");
+      // Do something...
+      auto t = thread([] {
+        for (auto step = 0; step < 50; ++step) {
+          console::write('.');
+          sleep_for(100_ms);
+        }
+      });
+      t.join();
+      console::write_line("");
+      console::write_line("End");
     }
   };
 }
@@ -18,4 +32,8 @@ startup_(environment_cancel_sgnal_example::program);
 
 // This code produces the following output :
 //
+// Start
+// ..................................................
+// End
 // The program is stopped!
+
