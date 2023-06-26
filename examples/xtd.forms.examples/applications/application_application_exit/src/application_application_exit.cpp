c6fac7a39560fe38b3960fd6d2f5fbc086a9af29
Gammasoft <gammasoft71@gmail.com>
2023-06-26 17:49:09 +0200

    Add application_application_exit example

---
+++ examples/xtd.forms.examples/applications/application_application_exit/src/application_application_exit.cpp
@@ -0,0 +1,15 @@
+#define TRACE
+#include <xtd/xtd>
+
+using namespace xtd;
+using namespace xtd::diagnostics;
+using namespace xtd::forms;
+
+auto main()->int {
+  environment::program_exit += [] {trace::write_line("The program is stopped");};
+  application::application_exit += [] {trace::write_line("The application is stopped");};
+  trace::write_line("Before application run");
+  application::run(form {});
+  trace::write_line("Afer application exit");
+  trace::write_line("Before program exit");
+}
