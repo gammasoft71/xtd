e465ff6e76bbdbb18e842e43dc5919e02809e9d3
Gammasoft <gammasoft71@gmail.com>
2023-06-26 15:34:34 +0200

    Review code

--- src/xtd.core/include/xtd/startup.h
+++ src/xtd.core/include/xtd/startup.h
@@ -17,12 +17,10 @@
 /// @cond
 struct __startup__ final static_ {
   static int run(void (*main_function)(), int, char* []) {main_function(); return xtd::environment::exit_code();}
-  static int run(int (*main_function)(), int, char* []) {return main_function();}
   static int run(void (*main_function)(int argc, char* argv[]), int argc, char* argv[]) {main_function(argc, argv); return xtd::environment::exit_code();}
-  static int run(void (*main_function)(xtd::collections::specialized::string_vector), int argc, char* argv[]) {main_function({argv + 1, argv + argc}); return xtd::environment::exit_code();}
   static int run(void (*main_function)(const xtd::collections::specialized::string_vector&), int argc, char* argv[]) {main_function({argv + 1, argv + argc}); return xtd::environment::exit_code();}
+  static int run(int (*main_function)(), int, char* []) {return main_function();}
   static int run(int (*main_function)(int argc, char* argv[]), int argc, char* argv[]) {return main_function(argc, argv);}
-  static int run(int (*main_function)(xtd::collections::specialized::string_vector), int argc, char* argv[]) {return main_function({argv + 1, argv + argc});}
   static int run(int (*main_function)(const xtd::collections::specialized::string_vector&), int argc, char* argv[]) {return main_function({argv + 1, argv + argc});}
 };
 
