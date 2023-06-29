0aeb6ce78828ed94df83f7e46c2219ec3f98a6b9
Gammasoft <gammasoft71@gmail.com>
2023-06-29 13:53:23 +0200

    Review include files

--- tools/guidgen/src/guidgen.cpp
+++ tools/guidgen/src/guidgen.cpp
@@ -1,4 +1,7 @@
-#include <xtd/xtd.core>
+#include <xtd/console.h>
+#include <xtd/guid.h>
+#include <xtd/startup.h>
+#include <xtd/ustring.h>
 #include <chrono>
 
 using namespace std;
@@ -8,7 +11,7 @@ namespace guidgen {
   class program final static_ {
   public:
     static auto main(const vector<ustring>& args) {
-      string format = "";
+      ustring format = "";
       int count = 1;
       bool show_help = false;
       bool show_version = false;
@@ -33,12 +36,12 @@ namespace guidgen {
     }
     
   private:
-    static string get_error() {
+    static ustring get_error() {
       return "guidgen : invalid params\n"
         "Try 'guidgen --help' for more information.";
     }
     
-    static string get_usage() {
+    static ustring get_usage() {
       return "Usage\n"
         "  guidgen [--format Format] [--count Count]\n"
         "\n"
@@ -53,13 +56,13 @@ namespace guidgen {
         "-h, --help    : Shows this help page.";
     }
     
-    static string get_version() {
+    static ustring get_version() {
       return ustring::format("guidgen version {}, (c) {:L} by Gammasoft", environment::version(), date_time::now());
     }
     
-    static bool process_arguments(const vector<ustring>& args, string& format, int& count, bool& show_version, bool& show_help) {
+    static bool process_arguments(const vector<ustring>& args, ustring& format, int& count, bool& show_version, bool& show_help) {
       for (size_t index = 0; index < args.size(); index += 1) {
-        vector<string> format_types {"N", "D", "B", "P", "X"};
+        vector<ustring> format_types {"N", "D", "B", "P", "X"};
         if ((args[index] == "-f" || args[index] == "--format") && index + 1 < args.size() && std::count(format_types.begin(), format_types.end(), args[index + 1]))
           format = args[index++ + 1];
         else if ((args[index] == "-c" || args[index] == "--count") && index + 1 < args.size() && ustring::try_parse(args[index++ + 1], count) && count >= 1) {
