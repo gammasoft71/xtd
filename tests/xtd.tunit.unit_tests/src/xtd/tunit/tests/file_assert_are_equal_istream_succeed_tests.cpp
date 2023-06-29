ddd7a5f0cb6cd8b709a3471ba0be3db6cada65cd
Gammasoft <gammasoft71@gmail.com>
2023-06-29 10:42:27 +0200

    Review include files

--- tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/file_assert_are_equal_istream_succeed_tests.cpp
+++ tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/file_assert_are_equal_istream_succeed_tests.cpp
@@ -1,5 +1,7 @@
+#include <xtd/tunit/file_assert.h>
+#include <xtd/tunit/test_class_attribute.h>
+#include <xtd/tunit/test_method_attribute.h>
 #include "../../../assert_unit_tests/assert_unit_tests.h"
-#include <xtd/xtd.tunit>
 
 namespace xtd::tunit::tests {
   class test_class_(file_assert_are_equal_istream_succeed_tests) {
