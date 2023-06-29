607af5efb6ac5f030ee07cfdb6dfb6c7a00adba2
Gammasoft <gammasoft71@gmail.com>
2023-06-29 09:27:19 +0200

    Review include files

--- tests/xtd.core.unit_tests/src/xtd/net/tests/ip_host_entry_tests.cpp
+++ tests/xtd.core.unit_tests/src/xtd/net/tests/ip_host_entry_tests.cpp
@@ -1,5 +1,8 @@
 #include <xtd/net/ip_host_entry.h>
-#include <xtd/xtd.tunit>
+#include <xtd/tunit/assert.h>
+#include <xtd/tunit/collection_assert.h>
+#include <xtd/tunit/test_class_attribute.h>
+#include <xtd/tunit/test_method_attribute.h>
 
 using namespace xtd;
 using namespace xtd::net;
