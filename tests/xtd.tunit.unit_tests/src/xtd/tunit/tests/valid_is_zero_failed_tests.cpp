fabd97386062e3f6ffdb067afe2e87fa96e18c90
Gammasoft <gammasoft71@gmail.com>
2023-06-29 10:22:13 +0200

    Review include files

--- tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/valid_is_zero_failed_tests.cpp
+++ tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/valid_is_zero_failed_tests.cpp
@@ -1,5 +1,7 @@
+#include <xtd/tunit/valid.h>
+#include <xtd/tunit/test_class_attribute.h>
+#include <xtd/tunit/test_method_attribute.h>
 #include "../../../assert_unit_tests/assert_unit_tests.h"
-#include <xtd/xtd.tunit>
 
 namespace xtd::tunit::tests {
   class test_class_(valid_is_zero_failed_tests) {
