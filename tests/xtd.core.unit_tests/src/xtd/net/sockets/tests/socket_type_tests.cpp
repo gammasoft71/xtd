2ee70058b8031728ccc39f0892fa5d1ed9c4d3d2
Gammasoft <gammasoft71@gmail.com>
2023-06-29 09:23:38 +0200

    Review include files

--- tests/xtd.core.unit_tests/src/xtd/net/sockets/tests/socket_type_tests.cpp
+++ tests/xtd.core.unit_tests/src/xtd/net/sockets/tests/socket_type_tests.cpp
@@ -1,5 +1,7 @@
 #include <xtd/net/sockets/socket_type.h>
-#include <xtd/xtd.tunit>
+#include <xtd/tunit/assert.h>
+#include <xtd/tunit/test_class_attribute.h>
+#include <xtd/tunit/test_method_attribute.h>
 
 using namespace xtd::net::sockets;
 using namespace xtd::tunit;
