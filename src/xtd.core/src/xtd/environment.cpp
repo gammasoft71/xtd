28276d46429d6fb4b7f13a88a71f89faef05cd30
Gammasoft <gammasoft71@gmail.com>
2023-06-29 19:58:18 +0200

    Code reorganization

--- src/xtd.core/src/xtd/environment.cpp
+++ src/xtd.core/src/xtd/environment.cpp
@@ -187,50 +187,6 @@ bool environment::has_shutdown_started() {
   return native::environment::has_shutdown_started();
 }
 
-xtd::collections::specialized::string_vector environment::get_command_line_args() {
-  auto args = native::environment::get_command_line_args();
-  return {args.begin(), args.end()};
-}
-
-ustring environment::get_environment_variable(const ustring& variable, environment_variable_target target) {
-  if (!enum_object<>::is_defined<environment_variable_target>(target)) throw xtd::argument_exception("Invalid environment variable target value"_t, csf_);
-  return native::environment::get_environment_variable(variable, as<int32>(target));
-}
-
-map<string, string>& environment::get_environment_variables(environment_variable_target target) {
-  if (!enum_object<>::is_defined<environment_variable_target>(target)) throw xtd::argument_exception("Invalid environment variable target value"_t, csf_);
-  return native::environment::get_environment_variables(as<int32>(target));
-}
-
-ustring environment::get_folder_path(environment::special_folder folder, environment::special_folder_option option) {
-  switch (folder) {
-    case environment::special_folder::xtd_install: return __XTD_INSTALL_PATH__;
-    case environment::special_folder::xtd_locale: return __XTD_LOCALE_PATH__; break;
-    case environment::special_folder::xtd_reference_guide: return __XTD_REFERENCE_GUIDE_PATH__;
-    case environment::special_folder::xtd_themes: return __XTD_THEMES_PATH__;
-    case environment::special_folder::xtd_include: return environment::xtd_include_path;
-    case environment::special_folder::xtd_libraries: return environment::xtd_libraries_path;
-    case environment::special_folder::xtd_resources: return environment::xtd_resources_path;
-    case environment::special_folder::xtd_console_include: return environment::xtd_console_include_path;
-    case environment::special_folder::xtd_console_libraries: return environment::xtd_console_libraries_path;
-    case environment::special_folder::xtd_drawing_include: return environment::xtd_drawing_include_path;
-    case environment::special_folder::xtd_drawing_libraries: return environment::xtd_drawing_libraries_path;
-    case environment::special_folder::xtd_drawing_resources: return environment::xtd_drawing_resources_path;
-    case environment::special_folder::xtd_forms_include: return environment::xtd_forms_include_path;
-    case environment::special_folder::xtd_forms_libraries: return environment::xtd_forms_libraries_path;
-    case environment::special_folder::xtd_forms_resources: return environment::xtd_forms_resources_path;
-    case environment::special_folder::xtd_tunit_include: return environment::xtd_tunit_include_path;
-    case environment::special_folder::xtd_tunit_libraries: return environment::xtd_tunit_libraries_path;
-    default: break;
-  }
-  
-  ustring path = native::environment::get_know_folder_path(static_cast<int32>(folder));
-  if (path.empty()) return path;
-  if (option == environment::special_folder_option::none) return !xtd::io::directory::exists(path) ? "" :  path;
-  if (!xtd::io::directory::exists(path)) xtd::io::directory::create_directory(path);
-  return path;
-}
-
 bool environment::is_64_bit_operating_system() noexcept {
   return os_version().is_64_bit();
 }
@@ -275,24 +231,6 @@ xtd::processor environment::processor_information() {
   if (proc.architecture() == xtd::architecture_id::unknown)
     proc = xtd::processor(native::environment::is_processor_arm() ? architecture_id::arm : architecture_id::x86, native::environment::is_os_64_bit(), native::environment::get_processor_count());
   return proc;
-}
-
-void environment::raise(xtd::signal signal) {
-  std::raise(enum_object<>::to_int32(signal));
-}
-
-void environment::set_environment_variable(const ustring& variable, const ustring& value, environment_variable_target target) {
-  if (ustring::is_empty(variable)) throw xtd::argument_exception("Environment variable name is empty"_t, csf_);
-  
-  if (!enum_object<>::is_defined<environment_variable_target>(target)) throw xtd::argument_exception("Invalid environment variable target value"_t, csf_);
-  
-  if (ustring::is_empty(value)) {
-    native::environment::get_environment_variables(as<int32>(target)).erase(variable);
-    native::environment::unset_environment_variable(variable, as<int32>(target));
-  } else {
-    native::environment::get_environment_variables(as<int32>(target))[variable] = value;
-    native::environment::set_environment_variable(variable, value, as<int32>(target));
-  }
 }
 
 xtd::ustring environment::stack_trace() {
@@ -377,16 +315,60 @@ xtd::ustring environment::expand_environment_variables(const xtd::ustring& name)
   return result;
 }
 
+xtd::collections::specialized::string_vector environment::get_command_line_args() {
+  auto args = native::environment::get_command_line_args();
+  return {args.begin(), args.end()};
+}
+
 xtd::ustring environment::get_environment_variable(const xtd::ustring& variable) {
   return get_environment_variable(variable, environment_variable_target::process);
+}
+
+ustring environment::get_environment_variable(const ustring& variable, environment_variable_target target) {
+  if (!enum_object<>::is_defined<environment_variable_target>(target)) throw xtd::argument_exception("Invalid environment variable target value"_t, csf_);
+  return native::environment::get_environment_variable(variable, as<int32>(target));
 }
 
 std::map<std::string, std::string>& environment::get_environment_variables() {
   return get_environment_variables(environment_variable_target::process);
 }
 
+map<string, string>& environment::get_environment_variables(environment_variable_target target) {
+  if (!enum_object<>::is_defined<environment_variable_target>(target)) throw xtd::argument_exception("Invalid environment variable target value"_t, csf_);
+  return native::environment::get_environment_variables(as<int32>(target));
+}
+
 xtd::ustring environment::get_folder_path(environment::special_folder folder) {
   return get_folder_path(folder, environment::special_folder_option::none);
+}
+
+ustring environment::get_folder_path(environment::special_folder folder, environment::special_folder_option option) {
+  switch (folder) {
+    case environment::special_folder::xtd_install: return __XTD_INSTALL_PATH__;
+    case environment::special_folder::xtd_locale: return __XTD_LOCALE_PATH__; break;
+    case environment::special_folder::xtd_reference_guide: return __XTD_REFERENCE_GUIDE_PATH__;
+    case environment::special_folder::xtd_themes: return __XTD_THEMES_PATH__;
+    case environment::special_folder::xtd_include: return environment::xtd_include_path;
+    case environment::special_folder::xtd_libraries: return environment::xtd_libraries_path;
+    case environment::special_folder::xtd_resources: return environment::xtd_resources_path;
+    case environment::special_folder::xtd_console_include: return environment::xtd_console_include_path;
+    case environment::special_folder::xtd_console_libraries: return environment::xtd_console_libraries_path;
+    case environment::special_folder::xtd_drawing_include: return environment::xtd_drawing_include_path;
+    case environment::special_folder::xtd_drawing_libraries: return environment::xtd_drawing_libraries_path;
+    case environment::special_folder::xtd_drawing_resources: return environment::xtd_drawing_resources_path;
+    case environment::special_folder::xtd_forms_include: return environment::xtd_forms_include_path;
+    case environment::special_folder::xtd_forms_libraries: return environment::xtd_forms_libraries_path;
+    case environment::special_folder::xtd_forms_resources: return environment::xtd_forms_resources_path;
+    case environment::special_folder::xtd_tunit_include: return environment::xtd_tunit_include_path;
+    case environment::special_folder::xtd_tunit_libraries: return environment::xtd_tunit_libraries_path;
+    default: break;
+  }
+  
+  ustring path = native::environment::get_know_folder_path(static_cast<int32>(folder));
+  if (path.empty()) return path;
+  if (option == environment::special_folder_option::none) return !xtd::io::directory::exists(path) ? "" :  path;
+  if (!xtd::io::directory::exists(path)) xtd::io::directory::create_directory(path);
+  return path;
 }
 
 xtd::collections::specialized::string_vector environment::get_logical_drives() {
@@ -407,8 +389,26 @@ void environment::quick_exit(xtd::exit_status exit_status) noexcept {
   quick_exit(enum_object<>::to_int32(exit_status));
 }
 
+void environment::raise(xtd::signal signal) {
+  std::raise(enum_object<>::to_int32(signal));
+}
+
 void environment::set_environment_variable(const xtd::ustring& variable, const xtd::ustring& value) {
   set_environment_variable(variable, value, environment_variable_target::process);
+}
+
+void environment::set_environment_variable(const ustring& variable, const ustring& value, environment_variable_target target) {
+  if (ustring::is_empty(variable)) throw xtd::argument_exception("Environment variable name is empty"_t, csf_);
+  
+  if (!enum_object<>::is_defined<environment_variable_target>(target)) throw xtd::argument_exception("Invalid environment variable target value"_t, csf_);
+  
+  if (ustring::is_empty(value)) {
+    native::environment::get_environment_variables(as<int32>(target)).erase(variable);
+    native::environment::unset_environment_variable(variable, as<int32>(target));
+  } else {
+    native::environment::get_environment_variables(as<int32>(target))[variable] = value;
+    native::environment::set_environment_variable(variable, value, as<int32>(target));
+  }
 }
 
 void environment::on_cancel_signal(signal_cancel_event_args& e) {
