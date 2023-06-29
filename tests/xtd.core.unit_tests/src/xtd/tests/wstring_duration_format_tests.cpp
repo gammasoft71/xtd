7eae22496416484c80cadd5f4d33d2c598f02319
Gammasoft <gammasoft71@gmail.com>
2023-06-29 09:03:22 +0200

    Review include files

--- tests/xtd.core.unit_tests/src/xtd/tests/wstring_duration_format_tests.cpp
+++ tests/xtd.core.unit_tests/src/xtd/tests/wstring_duration_format_tests.cpp
@@ -1,6 +1,8 @@
 #include <xtd/ustring.h>
 #include <xtd/format_exception.h>
-#include <xtd/xtd.tunit>
+#include <xtd/tunit/assert.h>
+#include <xtd/tunit/test_class_attribute.h>
+#include <xtd/tunit/test_method_attribute.h>
 
 using namespace std;
 using namespace std::chrono_literals;
