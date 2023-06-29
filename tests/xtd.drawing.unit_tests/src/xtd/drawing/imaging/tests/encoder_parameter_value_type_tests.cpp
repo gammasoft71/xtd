14cab3ead9882699497f1f7570b0c5cfa22d3c78
Gammasoft <gammasoft71@gmail.com>
2023-06-29 09:33:08 +0200

    Review include files

--- tests/xtd.drawing.unit_tests/src/xtd/drawing/imaging/tests/encoder_parameter_value_type_tests.cpp
+++ tests/xtd.drawing.unit_tests/src/xtd/drawing/imaging/tests/encoder_parameter_value_type_tests.cpp
@@ -1,5 +1,7 @@
 #include <xtd/drawing/imaging/encoder_parameter_value_type.h>
-#include <xtd/xtd.tunit>
+#include <xtd/tunit/assert.h>
+#include <xtd/tunit/test_class_attribute.h>
+#include <xtd/tunit/test_method_attribute.h>
 
 using namespace xtd::drawing::imaging;
 using namespace xtd::tunit;
