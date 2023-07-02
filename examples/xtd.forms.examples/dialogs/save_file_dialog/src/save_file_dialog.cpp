b40c9fab5621b41fb8788c396f6c916cba1a371e
Gammasoft <gammasoft71@gmail.com>
2023-07-02 17:28:10 +0200

    use auto

--- examples/xtd.forms.examples/dialogs/save_file_dialog/src/save_file_dialog.cpp
+++ examples/xtd.forms.examples/dialogs/save_file_dialog/src/save_file_dialog.cpp
@@ -13,7 +13,7 @@ public:
     button1.location({10, 10});
     button1.text("Save...");
     button1.click += [&] {
-      save_file_dialog dialog;
+      auto dialog = save_file_dialog {};
       dialog.initial_directory(environment::get_folder_path(environment::special_folder::desktop));
       dialog.file_name("MyFile.txt");
       dialog.filter("Text Files (*.txt)|*.txt;*.md|All Files (*.*)|*.*");
