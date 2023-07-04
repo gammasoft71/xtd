cc0741da16a13cc66f66d8c626c28526ddb96e96
Gammasoft <gammasoft71@gmail.com>
2023-07-04 15:24:26 +0200

    Add console_key_available example

---
+++ examples/xtd.core.examples/console/console_key_available/README.md
@@ -0,0 +1,36 @@
+# console_key_available
+
+Shows how to use [xtd::console::key_available](https://gammasoft71.github.io/xtd/reference_guides/latest/classxtd_1_1console.html#ade7353994e38d4645a831a12caab9427) and [xtd::console::read_key](https://gammasoft71.github.io/xtd/reference_guides/latest/classxtd_1_1console.html#ac0ebb643e4d3b01faa8380bd90d22aa0) methods.
+
+# Sources
+
+[src/console_key_available.cpp](src/console_key_available.cpp)
+
+[CMakeLists.txt](CMakeLists.txt)
+
+# Build and run
+
+Open "Command Prompt" or "Terminal". Navigate to the folder that contains the project and type the following:
+
+```cmake
+xtdc run
+```
+
+# Output if user stroke 'h', 'e', pagge_up, down_arrow, and 'x' keys
+
+```
+Press a key to display; press the 'x' key to quit.
+You pressed the 'h' key.
+
+Press a key to display; press the 'x' key to quit.
+You pressed the 'e' key.
+
+Press a key to display; press the 'x' key to quit.
+You pressed the 'page_up' key.
+
+Press a key to display; press the 'x' key to quit.
+You pressed the 'down_arrow' key.
+
+Press a key to display; press the 'x' key to quit.
+You pressed the 'x' key.
+```
---
+++ examples/xtd.core.examples/console/console_key_available/src/console_key_available.cpp
@@ -0,0 +1,38 @@
+#include <xtd/xtd>
+
+using namespace std::this_thread;
+using namespace xtd;
+
+auto main()->int {
+  console_key_info cki;
+  
+  do {
+    console::write_line("\nPress a key to display; press the 'x' key to quit.");
+    
+    // Your code could perform some useful task in the following loop. However,
+    // for the sake of this example we'll merely pause for a quarter second.
+    
+    while (console::key_available() == false)
+      sleep_for(250_ms); // Loop until input is entered.
+    
+    cki = console::read_key(true);
+    console::write_line("You pressed the '{0}' key.", cki.key());
+  } while (cki.key() != console_key::x);
+}
+
+// This example produces results if user stroke 'h', 'e', pagge_up, down_arrow, and 'x' keys
+//
+// Press a key to display; press the 'x' key to quit.
+// You pressed the 'h' key.
+//
+// Press a key to display; press the 'x' key to quit.
+// You pressed the 'e' key.
+//
+// Press a key to display; press the 'x' key to quit.
+// You pressed the 'page_up' key.
+//
+// Press a key to display; press the 'x' key to quit.
+// You pressed the 'down_arrow' key.
+//
+// Press a key to display; press the 'x' key to quit.
+// You pressed the 'x' key.
