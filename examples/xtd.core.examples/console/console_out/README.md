3663daca0421567c5044e08471505f1d96591b05
Gammasoft <gammasoft71@gmail.com>
2023-07-04 15:32:51 +0200

    Add console_out example

---
+++ examples/xtd.core.examples/console/console_out/README.md
@@ -0,0 +1,28 @@
+# console_out
+
+Shows how to use [xtd::console::out](https://gammasoft71.github.io/xtd/reference_guides/latest/classxtd_1_1console.html#a923837ac84baf01726703e0474ca4888) stream.
+
+# Sources
+
+[src/console_out.cpp](src/console_out.cpp)
+
+[CMakeLists.txt](CMakeLists.txt)
+
+# Build and run
+
+Open "Command Prompt" or "Terminal". Navigate to the folder that contains the project and type the following:
+
+```cmake
+xtdc run
+```
+
+# Output
+
+```
+Hello, World!
+Value = 42
+Value (Hex) = 0x2A
+Current date = Tue Jul  4 15:30:02 2023
+DayOfWeek = (unregistered)
+Duration = 23:05:24
+```
---
+++ examples/xtd.core.examples/console/console_out/src/console_out.cpp
@@ -0,0 +1,21 @@
+#include <xtd/xtd>
+
+using namespace xtd;
+
+auto main()->int {
+  console::out << "Hello, " << "World!" << environment::new_line();
+  console::out << "Value = " << 42 << environment::new_line();
+  console::out << "Value (Hex) = 0x" << int32_object(42).to_string("X") << environment::new_line();
+  console::out << "Current date = " << date_time::now() << environment::new_line();
+  console::out << "DayOfWeek = " << day_of_week::monday << environment::new_line();
+  console::out << "Duration = " << 23_h + 5_min + 24_s << environment::new_line();
+}
+
+// This code produces the following output:
+//
+// Hello, World!
+// Value = 42
+// Value (Hex) = 0x2A
+// Current date = Tue Jul  4 15:30:02 2023
+// DayOfWeek = (unregistered)
+// Duration = 23:05:24
