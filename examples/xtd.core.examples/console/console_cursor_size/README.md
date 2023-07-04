b0e64c77ccaaac3b7c5eac802e0dd7ee3f6ed046
Gammasoft <gammasoft71@gmail.com>
2023-07-04 03:52:38 +0200

    Add console_cursor_size example

---
+++ examples/xtd.core.examples/console/console_cursor_size/README.md
@@ -0,0 +1,35 @@
+# console_cursor_size
+
+Shows how to use [xtd::console::set_cursor_size](https://gammasoft71.github.io/xtd/reference_guides/latest/classxtd_1_1console.html#a3fa30230b3bb51effe82807627275e9e) property.
+
+# Sources
+
+[src/console_cursor_size.cpp](src/console_cursor_size.cpp)
+
+[CMakeLists.txt](CMakeLists.txt)
+
+# Build and run
+
+Open "Command Prompt" or "Terminal". Navigate to the folder that contains the project and type the following:
+
+```cmake
+xtdc run
+```
+
+# Output
+
+```
+This example increments the cursor size from 1% to 100%:
+
+Cursor size = 1%. (Press any key to continue...)
+Cursor size = 10%. (Press any key to continue...)
+Cursor size = 20%. (Press any key to continue...)
+Cursor size = 30%. (Press any key to continue...)
+Cursor size = 40%. (Press any key to continue...)
+Cursor size = 50%. (Press any key to continue...)
+Cursor size = 60%. (Press any key to continue...)
+Cursor size = 70%. (Press any key to continue...)
+Cursor size = 80%. (Press any key to continue...)
+Cursor size = 90%. (Press any key to continue...)
+Cursor size = 100%. (Press any key to continue...)
+```
---
+++ examples/xtd.core.examples/console/console_cursor_size/src/console_cursor_size.cpp
@@ -0,0 +1,34 @@
+#include <xtd/xtd>
+
+using namespace xtd;
+
+auto main()->int {
+  auto m0 = "This example increments the cursor size from 1% to 100%:\n";
+  auto m1 = "Cursor size = {0}%. (Press any key to continue...)";
+  auto sizes = { 1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+
+  auto save_cursor_size = console::cursor_size();
+  console::write_line(m0);
+  for (auto size : sizes) {
+    console::cursor_size(size);
+    console::write_line(m1, size);
+    console::read_key();
+  }
+  console::cursor_size(save_cursor_size);
+}
+
+// This code produces the following output:
+//
+// This example increments the cursor size from 1% to 100%:
+//
+// Cursor size = 1%. (Press any key to continue...)
+// Cursor size = 10%. (Press any key to continue...)
+// Cursor size = 20%. (Press any key to continue...)
+// Cursor size = 30%. (Press any key to continue...)
+// Cursor size = 40%. (Press any key to continue...)
+// Cursor size = 50%. (Press any key to continue...)
+// Cursor size = 60%. (Press any key to continue...)
+// Cursor size = 70%. (Press any key to continue...)
+// Cursor size = 80%. (Press any key to continue...)
+// Cursor size = 90%. (Press any key to continue...)
+// Cursor size = 100%. (Press any key to continue...)
