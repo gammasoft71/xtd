65ea6657ae317fcc6784fdc83daef2b4a642fd1b
gammasoft71 <gammasoft71@gmail.com>
2023-07-05 21:00:07 +0200

    update latest reference guide

---
+++ static/reference_guides/latest/html/search/enums_2.js
@@ -0,0 +1,21 @@
+var searchData=
+[
+  ['character_5fcasing_0',['character_casing',['../group__xtd__forms.html#gaf4ada098194f7c1c262928001ae7c781',1,'xtd::forms']]],
+  ['check_5fbox_5fstate_1',['check_box_state',['../group__xtd__forms.html#ga5d4c9f71675861b4f904d45ddcc60912',1,'xtd::forms::visual_styles']]],
+  ['check_5fstate_2',['check_state',['../group__xtd__forms.html#ga5ada8078f6297710207025403d7e7866',1,'xtd::forms']]],
+  ['close_5freason_3',['close_reason',['../group__xtd__forms.html#gad306a8c86886e96653e8e813745adb62',1,'xtd::forms']]],
+  ['color_5fbox_5fstyles_4',['color_box_styles',['../group__xtd__forms.html#ga2eb5191f396b67ceb2ccab03798d6ec0',1,'xtd::forms']]],
+  ['combo_5fbox_5fstyle_5',['combo_box_style',['../group__xtd__forms.html#gaaaee367e532741ff94690bc3e78352ac',1,'xtd::forms']]],
+  ['compiler_5fid_6',['compiler_id',['../group__xtd__core.html#gaeb666968a02e2d1ea1aab56ec278ffc9',1,'xtd']]],
+  ['compositing_5fmode_7',['compositing_mode',['../group__xtd__drawing.html#gacf43a9559ec2a8a422bccf24aa0613e5',1,'xtd::drawing::drawing_2d']]],
+  ['compositing_5fquality_8',['compositing_quality',['../group__xtd__drawing.html#gafd60a6a6d05faefa464a894037aa6562',1,'xtd::drawing::drawing_2d']]],
+  ['console_5fcolor_9',['console_color',['../namespacextd.html#ab60a2a64b74b91771b667e4a079681f2',1,'xtd']]],
+  ['console_5fkey_10',['console_key',['../group__xtd__core.html#gad94b2f43c7550180321e0ac4b02a4ff2',1,'xtd']]],
+  ['console_5fmodifiers_11',['console_modifiers',['../group__xtd__core.html#gacc628237bd3e274afe3d5d836e2db786',1,'xtd']]],
+  ['console_5fspecial_5fkey_12',['console_special_key',['../group__xtd__core.html#gaa3d0d07e86f61a82f96e00c5f516622e',1,'xtd']]],
+  ['content_5falignment_13',['content_alignment',['../group__xtd__forms.html#gad8fe8984e9f4652c13654b2c4d7288f0',1,'xtd::forms']]],
+  ['control_5fappearance_14',['control_appearance',['../group__xtd__forms.html#ga5d307293383256b1361069a8e6c57316',1,'xtd::forms']]],
+  ['control_5fstate_15',['control_state',['../group__xtd__forms.html#ga5f838907d816afcb82bb6ddbff6ef125',1,'xtd::forms::visual_styles']]],
+  ['control_5fstyles_16',['control_styles',['../group__xtd__forms.html#ga8b6e8bca23778f49dc78093e0d5fadbf',1,'xtd::forms']]],
+  ['copy_5fpixel_5foperation_17',['copy_pixel_operation',['../group__xtd__drawing.html#ga5a8edc9c35f0925441603048859eef6e',1,'xtd::drawing']]]
+];
