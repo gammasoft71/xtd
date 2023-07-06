085ef5cf009a3a5549748257327011f5374d875c
Gammasoft <gammasoft71@gmail.com>
2023-07-06 20:57:12 +0200

    Add assert_box dialog

---
+++ src/xtd.forms/include/xtd/forms/assert_box.h
@@ -0,0 +1,74 @@
+/// @file
+/// @brief Contains xtd::forms::assert_box dialog.
+/// @copyright Copyright (c) 2023 Gammasoft. All rights reserved.
+#pragma once
+#include <xtd/diagnostics/stack_frame.h>
+#include <xtd/static.h>
+#include <xtd/ustring.h>
+#include "dialog_result.h"
+#include "iwin32_window.h"
+#include "../forms_export.h"
+
+/// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
+namespace xtd {
+  /// @brief The xtd::forms namespace contains classes for creating Windows-based applications that take full advantage of the rich user interface features available in the Microsoft Windows operating system, Apple macOS and Linux like Ubuntu operating system.
+  namespace forms {
+    /// @cond
+    class control;
+    /// @endcond
+    
+    /// @brief Represents a common dialog box that displays assert box.
+    /// @par Namespace
+    /// xtd::forms
+    /// @par Library
+    /// xtd.forms
+    /// @ingroup xtd_forms dialogs
+    /// @par Appearance
+    /// |       | Windows                                   | macOS                                     | Gnome                                     |
+    /// | ----- | ----------------------------------------- | ----------------------------------------- | ----------------------------------------- |
+    /// | Light |  @image html dialog_assert_dialog_w.png   |  @image html dialog_assert_dialog_m.png   |  @image html dialog_assert_dialog_g.png   |
+    /// | Dark  |  @image html dialog_assert_dialog_wd.png  |  @image html dialog_assert_dialog_md.png  |  @image html dialog_assert_dialog_gd.png  |
+    /// @par Examples
+    /// The following code example demonstrates the use of assert_box dialog.
+    /// @include assert_box.cpp
+    class forms_export_ assert_box final static_ {
+    public:
+      /// @name Methods
+      
+      /// @{
+      /// @brief Displays a assert box in front of the specified window. The assert box displays an unknown assert.
+      /// @param owner A iwin32_window that represents the owner window of the busy box.
+      static dialog_result show(const iwin32_window& owner);
+      /// @brief Displays a assert box in front of the specified window. The assert box displays a specified caption and unknown assert.
+      /// @param owner A iwin32_window that represents the owner window of the busy box.
+      /// @param message A string that specifies the message to display.
+      static dialog_result show(const iwin32_window& owner, const xtd::ustring& message);
+      /// @brief Displays a assert box in front of the specified window. The assert box displays a specified assert.
+      /// @param owner A iwin32_window that represents the owner window of the busy box.
+      /// @param stack_frame An stack_frame to display.
+      static dialog_result show(const iwin32_window& owner, const xtd::diagnostics::stack_frame& stack_frame);
+      /// @brief Displays a assert box in front of the specified window. The assert box displays a specified assert and caption.
+      /// @param owner A iwin32_window that represents the owner window of the busy box.
+      /// @param message A string that specifies the message to display.
+      /// @param stack_frame An stack_frame to display.
+      static dialog_result show(const iwin32_window& owner, const xtd::ustring& message, const xtd::diagnostics::stack_frame& stack_frame);
+      
+      /// @brief Displays a assert box. The assert box displays an unknown assert.
+      static dialog_result show();
+      /// @brief Displays a assert box. The assert box displays a specified caption and unknown assert.
+      /// @param message A string that specifies the message to display.
+      static dialog_result show(const xtd::ustring& message);
+      /// @brief Displays a assert box. The assert box displays a specified assert.
+      /// @param stack_frame An stack_frame to display.
+      static dialog_result show(const xtd::diagnostics::stack_frame& stack_frame);
+      /// @brief Displays a assert box. The assert box displays a specified assert and caption.
+      /// @param message A string that specifies the message to display.
+      /// @param stack_frame An stack_frame to display.
+      static dialog_result show(const xtd::ustring& message, const xtd::diagnostics::stack_frame& stack_frame);
+      /// @}
+      
+    private:
+      static dialog_result show_assert_box(const iwin32_window* owner, const xtd::ustring& message = "", const xtd::diagnostics::stack_frame& stack_frame = xtd::diagnostics::stack_frame::empty());
+    };
+  }
+}
---
+++ src/xtd.forms/src/xtd/forms/assert_box.cpp
@@ -0,0 +1,44 @@
+#include "../../../include/xtd/forms/assert_box.h"
+#include "../../../include/xtd/forms/assert_dialog.h"
+
+using namespace xtd;
+using namespace xtd::forms;
+
+dialog_result assert_box::show(const iwin32_window& owner) {
+  return show_assert_box(&owner);
+}
+
+dialog_result assert_box::show(const iwin32_window& owner, const ustring& message) {
+  return show_assert_box(&owner, message);
+}
+
+dialog_result assert_box::show(const iwin32_window& owner, const xtd::diagnostics::stack_frame& stack_frame) {
+  return show_assert_box(&owner, "", stack_frame);
+}
+
+dialog_result assert_box::show(const iwin32_window& owner, const ustring& message, const xtd::diagnostics::stack_frame& stack_frame) {
+  return show_assert_box(&owner, message, stack_frame);
+}
+
+dialog_result assert_box::show() {
+  return show_assert_box(nullptr);
+}
+
+dialog_result assert_box::show(const ustring& message) {
+  return show_assert_box(nullptr, message);
+}
+
+dialog_result assert_box::show(const xtd::diagnostics::stack_frame& stack_frame) {
+  return show_assert_box(nullptr, "", stack_frame);
+}
+
+dialog_result assert_box::show(const ustring& message, const xtd::diagnostics::stack_frame& stack_frame) {
+  return show_assert_box(nullptr, message, stack_frame);
+}
+
+dialog_result assert_box::show_assert_box(const iwin32_window* owner, const ustring& message, const xtd::diagnostics::stack_frame& stack_frame) {
+  assert_dialog dialog;
+  dialog.message(message);
+  dialog.stack_frame(stack_frame);
+  return owner ? dialog.show_sheet_dialog(*owner) : dialog.show_dialog();
+}
