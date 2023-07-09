170ee2e3a0340ae899016aefc0fc9fc9af9d0131
Gammasoft <gammasoft71@gmail.com>
2023-07-09 05:21:48 +0200

    Update include files

--- examples/xtd.forms.examples/applications/application_restart/src/application_restart.cpp
+++ examples/xtd.forms.examples/applications/application_restart/src/application_restart.cpp
@@ -1,4 +1,7 @@
-#include <xtd/xtd>
+#include <xtd/forms/application>
+#include <xtd/forms/button>
+#include <xtd/forms/form>
+#include <xtd/environment>
 
 using namespace std;
 using namespace xtd;
