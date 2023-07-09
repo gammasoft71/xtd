3f5650a51ee8194b4ac77a2d9a0bc2b4f6692ebf
Gammasoft <gammasoft71@gmail.com>
2023-07-09 07:50:55 +0200

    Update include files

--- examples/xtd.forms.examples/controls/radio_button/src/radio_button.cpp
+++ examples/xtd.forms.examples/controls/radio_button/src/radio_button.cpp
@@ -1,4 +1,7 @@
-#include <xtd/xtd>
+#include <xtd/forms/application>
+#include <xtd/forms/form>
+#include <xtd/forms/label>
+#include <xtd/forms/radio_button>
 
 using namespace xtd;
 using namespace xtd::forms;
