1f21fb3aa9f1f2935e595c45c1b631c724be220a
Gammasoft <gammasoft71@gmail.com>
2023-07-09 07:49:27 +0200

    Update include files

--- examples/xtd.forms.examples/controls/domain_up_down/src/domain_up_down.cpp
+++ examples/xtd.forms.examples/controls/domain_up_down/src/domain_up_down.cpp
@@ -1,4 +1,7 @@
-#include <xtd/xtd>
+#include <xtd/forms/application>
+#include <xtd/forms/domain_up_down>
+#include <xtd/forms/form>
+#include <xtd/forms/label>
 
 using namespace xtd;
 using namespace xtd::forms;
