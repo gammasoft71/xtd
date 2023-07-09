1e57c7d504c44cbbecee7b42024b4169a740b2bd
Gammasoft <gammasoft71@gmail.com>
2023-07-09 07:50:50 +0200

    Update include files

--- examples/xtd.forms.examples/controls/progress_bar/src/progress_bar.cpp
+++ examples/xtd.forms.examples/controls/progress_bar/src/progress_bar.cpp
@@ -1,4 +1,7 @@
-#include <xtd/xtd>
+#include <xtd/forms/application>
+#include <xtd/forms/form>
+#include <xtd/forms/progress_bar>
+#include <xtd/forms/timer>
 
 using namespace xtd;
 using namespace xtd::forms;
