7a972f98b076804f7f611fc9737272d2c9de4907
gammasoft71 <gammasoft71@gmail.com>
2023-07-10 22:55:53 +0200

    Update latest reference guides

--- static/reference_guides/latest/search/files_14.js
+++ static/reference_guides/latest/search/files_14.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['web_5fexception_2eh_0',['web_exception.h',['../web__exception_8h.html',1,'']]],
-  ['white_5fcolors_2eh_1',['white_colors.h',['../white__colors_8h.html',1,'']]],
-  ['white_5fspace_2eh_2',['white_space.h',['../white__space_8h.html',1,'']]]
+  ['wchar_5fobject_2eh_0',['wchar_object.h',['../wchar__object_8h.html',1,'']]],
+  ['web_5fexception_2eh_1',['web_exception.h',['../web__exception_8h.html',1,'']]],
+  ['white_5fcolors_2eh_2',['white_colors.h',['../white__colors_8h.html',1,'']]],
+  ['white_5fspace_2eh_3',['white_space.h',['../white__space_8h.html',1,'']]]
 ];
