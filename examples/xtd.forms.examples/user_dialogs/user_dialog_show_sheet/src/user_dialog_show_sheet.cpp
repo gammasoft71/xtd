b537e98be349a1df9a64991bed1331dd3ad0d7fa
Gammasoft <gammasoft71@gmail.com>
2023-07-14 08:09:39 +0200

    Update include files

--- examples/xtd.forms.examples/user_dialogs/user_dialog_show_sheet/src/user_dialog_show_sheet.cpp
+++ examples/xtd.forms.examples/user_dialogs/user_dialog_show_sheet/src/user_dialog_show_sheet.cpp
@@ -1,4 +1,8 @@
-#include <xtd/xtd>
+#include <xtd/forms/application>
+#include <xtd/forms/button>
+#include <xtd/forms/form>
+#include <xtd/forms/label>
+#include <xtd/forms/text_box>
 
 using namespace std;
 using namespace xtd;
