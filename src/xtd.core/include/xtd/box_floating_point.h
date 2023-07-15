af6f9454e8b768abb8c9134c3cc96e53b0515aea
Gammasoft <gammasoft71@gmail.com>
2023-07-15 22:06:23 +0200

    remove infinity field

--- src/xtd.core/include/xtd/box_floating_point.h
+++ src/xtd.core/include/xtd/box_floating_point.h
@@ -49,18 +49,15 @@ namespace xtd {
     /// @brief Represents the smallest positive type_t value greater than zero. This field is constant.
     /// @remarks The value of this field is 4.94066e-324.
     static constexpr type_t epsilon = __get_epsilon(type_t{});
-    /// @brief Represents infinity. This field is constant.
-    /// @remarks The value of this field is std::numeric_limits<type_t>::infinity().
-    static constexpr type_t infinity = std::numeric_limits<type_t>::infinity();
     /// @brief Represents not a number (NaN). This field is constant.
     /// @remarks The value of this field is std::numeric_limits<type_t>::quiet_NaN().
     static constexpr type_t NaN = std::numeric_limits<type_t>::quiet_NaN();
     /// @brief Represents negative infinity. This field is constant.
     /// @remarks The value of this field is -std::numeric_limits<type_t>::infinity().
-    static constexpr type_t negative_infinity = -infinity;
+    static constexpr type_t negative_infinity = -std::numeric_limits<type_t>::infinity();
     /// @brief Represents positive infinity. This field is constant.
     /// @remarks The value of this field is std::numeric_limits<type_t>::infinity().
-    static constexpr type_t positive_infinity = +infinity;
+    static constexpr type_t positive_infinity = +std::numeric_limits<type_t>::infinity();
     /// @}
     
     /// @name Methods
