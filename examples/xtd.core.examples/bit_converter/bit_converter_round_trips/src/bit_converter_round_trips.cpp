3bed6e42624c919d3ce01db3841354619b05cd17
Gammasoft <gammasoft71@gmail.com>
2023-07-16 09:29:24 +0200

    Review examples

--- examples/xtd.core.examples/bit_converter/bit_converter_round_trips/src/bit_converter_round_trips.cpp
+++ examples/xtd.core.examples/bit_converter/bit_converter_round_trips/src/bit_converter_round_trips.cpp
@@ -11,14 +11,14 @@ namespace bit_converter_round_trips_example {
   public:
     // The main entry point for the application.
     static auto main() {
-      int value = -16;
-      vector<unsigned char> bytes = bit_converter::get_bytes(value);
+      auto value = -16;
+      auto bytes = bit_converter::get_bytes(value);
       
       // Convert bytes back to int.
-      int int_value = bit_converter::to_int32(bytes, 0);
+      auto int_value = bit_converter::to_int32(bytes, 0);
       console::write_line("{0} = {1}: {2}", value, int_value, value == int_value ? "Round-trips" : "Does not round-trip");
       // Convert bytes to unsigned int.
-      uint uint_value = bit_converter::to_uint32(bytes, 0);
+      auto uint_value = bit_converter::to_uint32(bytes, 0);
       console::write_line("{0} = {1}: {2}", value, uint_value, static_cast<uint>(value) == uint_value ? "Round-trips" : "Does not round-trip");
     }
   };
