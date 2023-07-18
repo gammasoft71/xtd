a872bb9e18d8062d3f7c2b84238f0b86b8830fd3
Gammasoft <gammasoft71@gmail.com>
2023-07-18 16:07:12 +0200

    Review examples

--- examples/xtd.core.examples/threading/interlocked/src/interlocked.cpp
+++ examples/xtd.core.examples/threading/interlocked/src/interlocked.cpp
@@ -1,9 +1,12 @@
-#include <vector>
+#include <xtd/threading/interlocked>
+#include <xtd/console>
+#include <xtd/random>
+#include <xtd/startup>
 #include <thread>
-#include <xtd/xtd>
 
 using namespace std;
 using namespace std::chrono;
+using namespace std::this_thread;
 using namespace xtd;
 using namespace xtd::threading;
 
@@ -12,59 +15,59 @@ namespace interlocked_example {
     std::thread thread;
     ustring name;
   };
+  
   class my_interlocked_exchange_class {
   private:
     //0 for False, 1 for True.
     inline static int using_resource = 0;
     
-    static const int num_thread_iterations = 5;
-    static const int num_threads = 10;
+    static constexpr int num_thread_iterations = 5;
     
   public:
     // The main entry point for the application.
     static auto main() {
-      vector<my_thread> my_threads(num_threads);
-      xtd::random rnd;
+      auto my_threads = array<my_thread, 10> {};
+      auto rnd = xtd::random {};
       
-      for (int i = 0; i < num_threads; ++i) {
-        my_threads[i].name = ustring::format("Thread{0}", i + 1);
+      for (auto index = 0ul; index < my_threads.size(); ++index) {
+        my_threads[index].name = ustring::format("Thread{}", index + 1);
         
         //Wait a random amount of time before starting next thread.
-        this_thread::sleep_for(milliseconds(rnd.next(0, 1000)));
-        my_threads[i].thread = thread(my_thread_proc, my_threads[i].name);
+        sleep_for(milliseconds(rnd.next(0, 1000)));
+        my_threads[index].thread = thread {my_thread_proc, my_threads[index].name};
       }
       
-      for (int i = 0; i < num_threads; ++i)
-        my_threads[i].thread.join();
+      for (auto index = 0ul; index < my_threads.size(); ++index)
+        my_threads[index].thread.join();
     }
     
     static void my_thread_proc(const ustring& name) {
-      for (int i = 0; i < num_thread_iterations; i++) {
+      for (auto index = 0; index < num_thread_iterations; ++index) {
         use_resource(name);
         
         //Wait 1 second before next attempt.
-        this_thread::sleep_for(milliseconds(1000));
+        sleep_for(milliseconds(1000));
       }
     }
     
     //A simple method that denies reentrancy.
-    static bool use_resource(const ustring& name) {
+    static auto use_resource(auto name)->bool {
       //0 indicates that the method is not in use.
       if (0 == interlocked::exchange(using_resource, 1)) {
-        console::write_line("{0} acquired the lock", name);
+        console::write_line("{} acquired the lock", name);
         
         //Code to access a resource that is not thread safe would go here.
         
         //Simulate some work
-        this_thread::sleep_for(milliseconds(500));
+        sleep_for(milliseconds(500));
         
-        console::write_line("{0} exiting lock", name);
+        console::write_line("{} exiting lock", name);
         
         //Release the lock
         interlocked::exchange(using_resource, 0);
         return true;
       } else {
-        console::write_line("   {0} was denied the lock", name);
+        console::write_line("   {} was denied the lock", name);
         return false;
       }
     }
