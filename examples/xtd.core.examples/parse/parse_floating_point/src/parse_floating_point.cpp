e9c2ffc173c53d9d76c3a26487b105d4d476d2c2
Gammasoft <gammasoft71@gmail.com>
2023-07-18 13:41:12 +0200

    Review examples

--- examples/xtd.core.examples/parse/parse_floating_point/src/parse_floating_point.cpp
+++ examples/xtd.core.examples/parse/parse_floating_point/src/parse_floating_point.cpp
@@ -1,27 +1,28 @@
-#include <xtd/xtd>
+#include <xtd/console>
+#include <xtd/parse>
 
 using namespace std;
 using namespace xtd;
 
 auto main()->int {
   locale::global(locale("en_US.UTF-8")); // change to us for currency
-  double d1 = xtd::parse<double>("12.345");
+  auto d1 = xtd::parse<double>("12.345");
   console::write_line(d1);
-  double d2 = xtd::parse<double>("$12.345", number_styles::currency);
+  auto d2 = xtd::parse<double>("$12.345", number_styles::currency);
   console::write_line(d2);
-  double d3 = xtd::parse<double>("1.234500e+01", number_styles::number | number_styles::allow_exponent);
+  auto d3 = xtd::parse<double>("1.234500e+01", number_styles::number | number_styles::allow_exponent);
   console::write_line(d3);
-  double d4 = xtd::parse<double>("inf");
+  auto d4 = xtd::parse<double>("inf");
   console::write_line(d4);
-  double d5 = xtd::parse<double>("-inf");
+  auto d5 = xtd::parse<double>("-inf");
   console::write_line(d5);
-  double d6 = xtd::parse<double>("nan");
+  auto d6 = xtd::parse<double>("nan");
   console::write_line(d6);
   try {
-    double d7 = xtd::parse<int>("Not floating point", number_styles::any);
+    auto d7 = xtd::parse<double>("Not floating point", number_styles::any);
     console::write_line(d7);
-  } catch (const exception& e) {
-    console::write_line("Exception : {}", e.what());
+  } catch (const system_exception& e) {
+    console::write_line("Exception : {}", e.message());
   }
 }
 
--- examples/xtd.core.examples/parse/parse_numeric/src/parse_numeric.cpp
+++ examples/xtd.core.examples/parse/parse_numeric/src/parse_numeric.cpp
@@ -1,31 +1,32 @@
-#include <xtd/xtd>
+#include <xtd/console>
+#include <xtd/parse>
 
 using namespace std;
 using namespace xtd;
 
 auto main()->int {
   locale::global(locale("en_US.UTF-8")); // change to us for currency
-  int i1 = xtd::parse<int>("42");
+  auto i1 = xtd::parse<int>("42");
   console::write_line(i1);
-  int i2 = xtd::parse<int>("+42.8", number_styles::number);
+  auto i2 = xtd::parse<int>("+42.8", number_styles::number);
   console::write_line(i2);
-  int i3 = xtd::parse<int>("0x2A", number_styles::hex_number);
+  auto i3 = xtd::parse<int>("0x2A", number_styles::hex_number);
   console::write_line(i3);
-  int i4 = xtd::parse<int>("052", number_styles::octal_number);
+  auto i4 = xtd::parse<int>("052", number_styles::octal_number);
   console::write_line(i4);
-  int i5 = xtd::parse<int>("0b00101010", number_styles::binary_number);
+  auto i5 = xtd::parse<int>("0b00101010", number_styles::binary_number);
   console::write_line(i5);
-  int i6 = xtd::parse<int>("$42", number_styles::currency);
+  auto i6 = xtd::parse<int>("$42", number_styles::currency);
   console::write_line(i6);
-  int i7 = xtd::parse<int>("$42", number_styles::currency);
+  auto i7 = xtd::parse<int>("$42", number_styles::currency);
   console::write_line(i7);
-  int i8 = xtd::parse<int>("(42)", number_styles::number | number_styles::allow_parentheses);
+  auto i8 = xtd::parse<int>("(42)", number_styles::number | number_styles::allow_parentheses);
   console::write_line(i8);
   try {
-    int i9 = xtd::parse<int>("Not number", number_styles::any);
+    auto i9 = xtd::parse<int>("Not number", number_styles::any);
     console::write_line(i9);
-  } catch (const exception& e) {
-    console::write_line("Exception : {}", e.what());
+  } catch (const system_exception& e) {
+    console::write_line("Exception : {}", e.message());
   }
 }
 
