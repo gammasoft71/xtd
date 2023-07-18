ca7d3cb772dfa20b92648c124772443dfcc5ade5
Gammasoft <gammasoft71@gmail.com>
2023-07-18 15:49:35 +0200

    Review example

--- examples/xtd.core.examples/random/random3/src/random3.cpp
+++ examples/xtd.core.examples/random/random3/src/random3.cpp
@@ -10,8 +10,8 @@ auto main()->int {
   auto female_pet_names = vector {"Maggie", "Penny", "Saya", "Princess", "Abby", "Laila", "Sadie", "Olivia", "Starlight", "Talla"};
   
   // Generate random indexes for pet names.
-  size_t male_index = rnd.next(male_pet_names.size());
-  size_t female_index = rnd.next(female_pet_names.size());
+  auto male_index = rnd.next(male_pet_names.size());
+  auto female_index = rnd.next(female_pet_names.size());
   
   // Display the result.
   console::write_line("Suggested pet name of the day: ");
