39828b6bc1cff5ea83212b410f61b554db0576bd
gammasoft71 <gammasoft71@gmail.com>
2023-07-18 22:21:15 +0200

    Update latest reference guide

--- static/reference_guides/latest/search/files_9.js
+++ static/reference_guides/latest/search/files_9.js
@@ -6,6 +6,7 @@ var searchData=
   ['key_5fpress_5fevent_5fargs_2eh_3',['key_press_event_args.h',['../key__press__event__args_8h.html',1,'']]],
   ['key_5fpress_5fevent_5fhandler_2eh_4',['key_press_event_handler.h',['../key__press__event__handler_8h.html',1,'']]],
   ['keys_2eh_5',['keys.h',['../keys_8h.html',1,'']]],
-  ['known_5fcolor_2eh_6',['known_color.h',['../known__color_8h.html',1,'']]],
-  ['known_5fthemed_5fcolor_2eh_7',['known_themed_color.h',['../known__themed__color_8h.html',1,'']]]
+  ['keywords_2eh_6',['keywords.h',['../keywords_8h.html',1,'']]],
+  ['known_5fcolor_2eh_7',['known_color.h',['../known__color_8h.html',1,'']]],
+  ['known_5fthemed_5fcolor_2eh_8',['known_themed_color.h',['../known__themed__color_8h.html',1,'']]]
 ];
