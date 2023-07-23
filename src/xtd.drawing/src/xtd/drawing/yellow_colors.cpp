5962e810c5ba40711c33178ea28860c710c7a8a0
Gammasoft <gammasoft71@gmail.com>
2023-07-23 10:14:28 +0200

    Review code

--- src/xtd.drawing/src/xtd/drawing/yellow_colors.cpp
+++ src/xtd.drawing/src/xtd/drawing/yellow_colors.cpp
@@ -4,56 +4,56 @@ using namespace std;
 using namespace xtd;
 using namespace xtd::drawing;
 
-color yellow_colors::dark_khaki() {
+color yellow_colors::dark_khaki() noexcept {
   return color::from_known_color(known_color::dark_khaki);
 }
 
-color yellow_colors::gold() {
+color yellow_colors::gold() noexcept {
   return color::from_known_color(known_color::gold);
 }
 
-color yellow_colors::khaki() {
+color yellow_colors::khaki() noexcept {
   return color::from_known_color(known_color::khaki);
 }
 
-color yellow_colors::lemon_chiffon() {
+color yellow_colors::lemon_chiffon() noexcept {
   return color::from_known_color(known_color::lemon_chiffon);
 }
 
-color yellow_colors::light_goldenrod_yellow() {
+color yellow_colors::light_goldenrod_yellow() noexcept {
   return color::from_known_color(known_color::light_goldenrod_yellow);
 }
 
-color yellow_colors::light_yellow() {
+color yellow_colors::light_yellow() noexcept {
   return color::from_known_color(known_color::light_yellow);
 }
 
-color yellow_colors::moccasin() {
+color yellow_colors::moccasin() noexcept {
   return color::from_known_color(known_color::moccasin);
 }
 
-color yellow_colors::pale_goldenrod() {
+color yellow_colors::pale_goldenrod() noexcept {
   return color::from_known_color(known_color::pale_goldenrod);
 }
 
-color yellow_colors::papaya_whip() {
+color yellow_colors::papaya_whip() noexcept {
   return color::from_known_color(known_color::papaya_whip);
 }
 
-color yellow_colors::peach_puff() {
+color yellow_colors::peach_puff() noexcept {
   return color::from_known_color(known_color::peach_puff);
 }
 
-color yellow_colors::yellow() {
+color yellow_colors::yellow() noexcept {
   return color::from_known_color(known_color::yellow);
 }
 
-const vector<color>& yellow_colors::get_colors() {
+const vector<color>& yellow_colors::get_colors() noexcept {
   static vector colors {yellow_colors::dark_khaki(), yellow_colors::gold(), yellow_colors::khaki(), yellow_colors::peach_puff(), yellow_colors::yellow(), yellow_colors::pale_goldenrod(), yellow_colors::moccasin(), yellow_colors::papaya_whip(), yellow_colors::light_goldenrod_yellow(), yellow_colors::lemon_chiffon(), yellow_colors::light_yellow()};
   return colors;
 }
 
-const vector<ustring>& yellow_colors::get_color_names() {
+const vector<ustring>& yellow_colors::get_color_names() noexcept {
   static vector color_names {yellow_colors::dark_khaki().name(), yellow_colors::gold().name(), yellow_colors::khaki().name(), yellow_colors::peach_puff().name(), yellow_colors::yellow().name(), yellow_colors::pale_goldenrod().name(), yellow_colors::moccasin().name(), yellow_colors::papaya_whip().name(), yellow_colors::light_goldenrod_yellow().name(), yellow_colors::lemon_chiffon().name(), yellow_colors::light_yellow().name()};
   return color_names;
 }
