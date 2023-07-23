26700b4201a5e1df510451c94882ea5f8ab12edb
gammasoft71 <gammasoft71@gmail.com>
2023-07-23 07:13:57 +0200

    Update latest reference guide

--- static/reference_guides/latest/html/search/typedefs_14.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['wchar_0',['wchar',['../group__types.html#ga155357d92cf162b71b396e46accee518',1,'xtd']]],
-  ['wchar_5fobject_1',['wchar_object',['../group__types.html#gaa5ac5641de5f9362829662a60349ecde',1,'xtd']]]
-];
