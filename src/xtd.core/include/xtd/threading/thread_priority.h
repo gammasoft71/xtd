48505e3b657a22042f8657ba170d848e04a19910
Gammasoft <gammasoft71@gmail.com>
2023-07-25 22:16:29 +0200

    Add thread_priority enumeration

---
+++ src/xtd.core/include/xtd/threading/thread_priority.h
@@ -0,0 +1,40 @@
+/// @file
+/// @brief Contains xtd::threading::thread_priority enumeration.
+/// @copyright Copyright (c) 2023 Gammasoft. All rights reserved.
+#pragma once
+
+#include "../enum.h"
+
+/// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
+namespace xtd {
+  /// @brief The xtd::threading namespace provides classes and interfaces that enable multithread_priorityed programming.
+  /// In addition to classes for synchronizing thread_priority activities and access to data ( xtd::threading::mutex, xtd::threading::monitor, xtd::threading::thread_priority, xtd::threading::auto_reset_event, and so on), this namespace includes a xtd::threading::thread_priority_pool class that allows you to use a pool of system-supplied thread_prioritys, and a xtd::threading::timer class that executes callback methods on thread_priority pool thread_prioritys.
+  namespace threading {
+    /// @brief Specifies the scheduling priority of a system::threading::thread.
+    /// @par Header
+    /// @code #include <xtd/threading/thread_priority> @endcode
+    /// @par Namespace
+    /// xtd::threading
+    /// @par Library
+    /// xtd.core threading
+    /// @ingroup xtd_core
+    enum class thread_priority {
+      /// @brief The system::threading::thread can be scheduled after threads with any other priority.
+      lowest = 0,
+      /// @brief The system::threading::thread can be scheduled after threads with thread_priority::normal priority and before those with thread_priority::lowest priority.
+      below_normal = 1,
+      /// @brief The system::threading::thread can be scheduled after threads with thread_priority::above_normal priority and before those with thread_priority::below_normal priority. Threads have thread_priority::normal priority by default.
+      normal = 2,
+      /// @brief The system::threading::thread can be scheduled after threads with thread_priority::highest priority and before those with thread_priority::normal priority.
+      above_normal = 3,
+      /// @brief The system::threading::thread can be scheduled before threads with any other priority.
+      highest = 4,
+    };
+  }
+}
+
+/// @cond
+template<> struct xtd::enum_register<xtd::threading::thread_priority> {
+  explicit operator auto() const noexcept {return xtd::enum_collection<xtd::threading::thread_priority> {{xtd::threading::thread_priority::lowest, "lowest"}, {xtd::threading::thread_priority::below_normal, "below_normal"}, {xtd::threading::thread_priority::normal, "normal"}, {xtd::threading::thread_priority::above_normal, "above_normal"}, {xtd::threading::thread_priority::highest, "highest"}};}
+};
+/// @endcond
