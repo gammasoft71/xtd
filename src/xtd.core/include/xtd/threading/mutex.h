01719bbec1f51d7686b70519fb1c6f863bc14d97
Gammasoft <gammasoft71@gmail.com>
2023-07-27 01:01:52 +0200

    Add constructor

--- src/xtd.core/include/xtd/threading/mutex.h
+++ src/xtd.core/include/xtd/threading/mutex.h
@@ -9,7 +9,7 @@ namespace xtd {
   /// @brief The xtd::threading namespace provides classes and interfaces that enable multithreaded programming.
   /// In addition to classes for synchronizing thread activities and access to data ( xtd::threading::mutex, xtd::threading::monitor, xtd::threading::interlocked, xtd::threading::auto_reset_event, and so on), this namespace includes a xtd::threading::thread_pool class that allows you to use a pool of system-supplied threads, and a xtd::threading::timer class that executes callback methods on thread pool threads.
   namespace threading {
-    /// @brief Encapsulates operating system specific objects that wait for exclusive access to shared resources.
+    /// @brief A synchronization primitive that can also be used for interprocess synchronization.
     /// @par Header
     /// @code #include <xtd/threading/mutex> @endcode
     /// @par Namespace
@@ -26,7 +26,13 @@ namespace xtd {
       
       /// @{
       /// @brief Initializes a new instance of the xtd::threading::mutex class.
+      /// @exception xtd::io::io_xception An Io error occurred.
       mutex();
+      /// @brief Initializes a new instance of the xtd::threading::mutex with a bool value that indicates whether the calling thread should have initial ownership of the mutex.
+      /// @param initially_owned true to give the calling thread initial ownership of the named system mutex if the named system mutex is created as a result of this call; otherwise, false.
+      /// @exception xtd::io::io_xception An Io error occurred.
+      mutex(bool initially_owned);
+
       /// @}
       
       /// @name Properties
@@ -41,6 +47,11 @@ namespace xtd {
       /// @{
       void close() override;
       
+      /// @brief Opens an existing named mutex.
+      /// @param name The name of a system-wide named mutex object.
+      /// @return A xtd::threading::mutex object that represents a named system mutex.
+      /// @exception xtd::argument_exception is a zero-length string <br>-or-<br> name is longer than 128 characters
+      /// @exception xtd::io::io_Exception An Io error occurred.
       mutex open_existing(const ustring& name);
       
     protected:
