33b3207ab2d9828f4577a2c50e7c2724c7108146
gammasoft71 <gammasoft71@gmail.com>
2023-07-28 21:00:07 +0200

    update latest reference guide

---
+++ static/reference_guides/latest/html/search/files_14.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['wait_5fhandle_2eh_0',['wait_handle.h',['../wait__handle_8h.html',1,'']]],
+  ['wchar_2eh_1',['wchar.h',['../wchar_8h.html',1,'']]],
+  ['wchar_5fobject_2eh_2',['wchar_object.h',['../wchar__object_8h.html',1,'']]],
+  ['web_5fexception_2eh_3',['web_exception.h',['../web__exception_8h.html',1,'']]],
+  ['white_5fcolors_2eh_4',['white_colors.h',['../white__colors_8h.html',1,'']]],
+  ['white_5fspace_2eh_5',['white_space.h',['../white__space_8h.html',1,'']]]
+];
