64105090edf6aeda6722222ad018d42eafc953eb
Gammasoft <gammasoft71@gmail.com>
2023-07-31 18:46:24 +0200

    Review named_semaphore on macOS

--- src/xtd.core.native.macos/src/xtd/native/macos/named_semaphore.mm
+++ src/xtd.core.native.macos/src/xtd/native/macos/named_semaphore.mm
@@ -14,16 +14,12 @@
 #import <Cocoa/Cocoa.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <algorithm>
 
 using namespace xtd::native;
 
 intmax_t named_semaphore::create(int_least32_t initial_count, int_least32_t max_count, const std::string& name) {
-  sem_t* semaphore = sem_open(name.c_str(), O_CREAT | O_EXCL, S_IRUSR | S_IWUSR, max_count);
-  if (semaphore != SEM_FAILED && initial_count) {
-    bool io_error = false;
-    for (auto index = 0; !io_error && index < initial_count; ++index)
-      wait(reinterpret_cast<intmax_t>(semaphore), -1, io_error);
-  }
+  sem_t* semaphore = sem_open(name.c_str(), O_CREAT | O_EXCL, S_IRUSR | S_IWUSR, std::min(initial_count, max_count));
   return reinterpret_cast<intmax_t>(semaphore);
 }
 
@@ -33,13 +29,13 @@ void named_semaphore::destroy(intmax_t handle, const std::string& name) {
 }
 
 intmax_t named_semaphore::open(const std::string& name) {
-  sem_t* semaphore = nullptr;
-  semaphore = sem_open(name.c_str(), O_RDWR, S_IRUSR | S_IWUSR);
+  sem_t* semaphore = sem_open(name.c_str(), O_RDWR, S_IRUSR | S_IWUSR);
   return reinterpret_cast<intmax_t>(semaphore);
 }
 
 bool named_semaphore::signal(intmax_t handle, int_least32_t release_count, int_least32_t& previous_count, bool& io_error) {
   if (reinterpret_cast<sem_t*>(handle) == SEM_FAILED) return !(io_error = true);
+  previous_count = -1;
   sem_getvalue(reinterpret_cast<sem_t*>(handle), &previous_count);
   io_error = false;
   for (auto count = 0; !io_error && count < release_count; ++count)
@@ -51,20 +47,17 @@ bool named_semaphore::wait(intmax_t handle, int_least32_t milliseconds_timeout, 
   if (reinterpret_cast<sem_t*>(handle) == SEM_FAILED) return !(io_error = true);
   io_error = false;
   if (milliseconds_timeout == -1) {
-    if (sem_wait(reinterpret_cast<sem_t*>(handle)) == -1) {
-      if (errno == EAGAIN) return true;;
-      if (errno == EINVAL) io_error = true;
-    }
+    if (sem_wait(reinterpret_cast<sem_t*>(handle)) == -1) io_error = true;
     return !io_error;
   }
-  
+
   struct timespec timeout;
   clock_gettime(CLOCK_REALTIME, &timeout);
-  timeout.tv_sec = milliseconds_timeout / 1000;
-  timeout.tv_nsec = (milliseconds_timeout % 1000) * 1000000;
+  timeout.tv_sec += milliseconds_timeout / 1000;
+  timeout.tv_nsec += (milliseconds_timeout % 1000) * 1000000;
   if (sem_timedwait(reinterpret_cast<sem_t*>(handle), &timeout) == -1) {
-    if (errno == EAGAIN) return true;
-    if (errno != ETIMEDOUT) io_error = true;
+    if (errno == ETIMEDOUT) return false;
+    io_error = true;
   }
   return !io_error;
 }
