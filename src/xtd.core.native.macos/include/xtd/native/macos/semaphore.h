1d907f094cde0b73b8f1ee8dc7fdd3ba304bf37c
Gammasoft <gammasoft71@gmail.com>
2023-07-31 18:46:01 +0200

    Fix sem_timedwait on macOS

--- src/xtd.core.native.macos/include/xtd/native/macos/semaphore.h
+++ src/xtd.core.native.macos/include/xtd/native/macos/semaphore.h
@@ -10,11 +10,15 @@
 
 static inline int sem_timedwait(sem_t *sem, const struct timespec *abs_timeout) {
   while (sem_trywait(sem) == -1) {
-    if (errno != EAGAIN) return -1;
-    usleep(20000);
-    struct timespec current_time;
-    clock_gettime(CLOCK_REALTIME, &current_time);
-    if (current_time.tv_sec > abs_timeout->tv_sec || (current_time.tv_sec == abs_timeout->tv_sec && current_time.tv_nsec >= abs_timeout->tv_nsec)) return ETIMEDOUT;
+    if (errno == EAGAIN) {
+      struct timespec current_time;
+      clock_gettime(CLOCK_REALTIME, &current_time);
+      if (current_time.tv_sec > abs_timeout->tv_sec || (current_time.tv_sec == abs_timeout->tv_sec && current_time.tv_nsec >= abs_timeout->tv_nsec)) {
+        errno = ETIMEDOUT;
+        return -1;
+      }
+      usleep(20000);
+    } else return -1;
   }
   return 0;
 }
