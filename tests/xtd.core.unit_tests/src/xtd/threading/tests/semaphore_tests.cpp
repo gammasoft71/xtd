3b9113560c36e9b34eadcaf041a875431ed064cc
Gammasoft <gammasoft71@gmail.com>
2023-08-01 14:43:48 +0200

    Update semaphore_tests.cpp

--- tests/xtd.core.unit_tests/src/xtd/threading/tests/semaphore_tests.cpp
+++ tests/xtd.core.unit_tests/src/xtd/threading/tests/semaphore_tests.cpp
@@ -8,9 +8,14 @@ using namespace xtd::tunit;
 namespace xtd::tests {
   class test_class_(semaphore_tests) {
   public:
+    static void class_initialize_(class_initialize) {
+      auto created_new = false;
+      auto s = semaphore {"xtd_mutex_test", created_new};
+    }
+    
     void test_method_(constructor) {
-      auto m = semaphore {};
-      assert::are_not_equal(semaphore::invalid_handle, m.handle(), csf_);
+      auto s = semaphore {};
+      assert::are_not_equal(semaphore::invalid_handle, s.handle(), csf_);
     }
   };
 }
