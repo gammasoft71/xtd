721db3ef222e774bd0ecfbc5f22a19e8025bd097
Gammasoft <gammasoft71@gmail.com>
2023-08-03 22:08:30 +0200

    Add unit tests

--- tests/xtd.core.unit_tests/src/xtd/threading/tests/semaphore_tests.cpp
+++ tests/xtd.core.unit_tests/src/xtd/threading/tests/semaphore_tests.cpp
@@ -272,5 +272,99 @@ namespace xtd::tests {
       assert::is_true(thread_ran, csf_);
       assert::are_equal(wait_handle::invalid_handle, s.handle(), csf_);
     }
+
+    void test_method_(open_existing_with_same_name) {
+      if (environment::os_version().is_windows() && !environment::is_64_bit_process()) assert::ignore();
+      auto created_new = false;
+      auto s = semaphore {"xtd_semaphore_test", created_new};
+      assert::are_not_equal(wait_handle::invalid_handle, s.handle(), csf_);
+      auto thread_ran = false;
+      auto thread = std::thread {[&] {
+        auto s2 = semaphore::open_existing("xtd_semaphore_test");
+        assert::are_not_equal(wait_handle::invalid_handle, s2.handle(), csf_);
+        thread_ran = true;
+      }};
+      thread.join();
+      assert::is_true(thread_ran, csf_);
+    }
+    
+    void test_method_(open_existing_with_different_name) {
+      if (environment::os_version().is_windows() && !environment::is_64_bit_process()) assert::ignore();
+      auto created_new = false;
+      auto s = semaphore {"xtd_semaphore_test", created_new};
+      assert::are_not_equal(wait_handle::invalid_handle, s.handle(), csf_);
+      auto thread_ran = false;
+      auto thread = std::thread {[&] {
+        assert::throws<io::io_exception>([] {auto s2 = semaphore::open_existing("xtd_semaphore_test_2");}, csf_);
+        thread_ran = true;
+      }};
+      thread.join();
+      assert::is_true(thread_ran, csf_);
+    }
+    
+    void test_method_(open_existing_with_empty_name) {
+      if (environment::os_version().is_windows() && !environment::is_64_bit_process()) assert::ignore();
+      auto created_new = false;
+      auto s = semaphore {"xtd_semaphore_test", created_new};
+      assert::are_not_equal(wait_handle::invalid_handle, s.handle(), csf_);
+      auto thread_ran = false;
+      auto thread = std::thread {[&] {
+        assert::throws<argument_exception>([] {auto s2 = semaphore::open_existing("");}, csf_);
+        thread_ran = true;
+      }};
+      thread.join();
+      assert::is_true(thread_ran, csf_);
+    }
+
+    void test_method_(try_open_existing_with_same_name) {
+      if (environment::os_version().is_windows() && !environment::is_64_bit_process()) assert::ignore();
+      auto created_new = false;
+      auto s = semaphore {"xtd_semaphore_test", created_new};
+      assert::are_not_equal(wait_handle::invalid_handle, s.handle(), csf_);
+      auto thread_ran = false;
+      auto thread = std::thread {[&] {
+        auto s2 = semaphore {};
+        auto result = semaphore::try_open_existing("xtd_semaphore_test", s2);
+        assert::is_true(result, csf_);
+        assert::are_not_equal(wait_handle::invalid_handle, s2.handle(), csf_);
+        thread_ran = true;
+      }};
+      thread.join();
+      assert::is_true(thread_ran, csf_);
+    }
+    
+    void test_method_(try_open_existing_with_different_name) {
+      if (environment::os_version().is_windows() && !environment::is_64_bit_process()) assert::ignore();
+      auto created_new = false;
+      auto s = semaphore {"xtd_semaphore_test", created_new};
+      assert::are_not_equal(wait_handle::invalid_handle, s.handle(), csf_);
+      auto thread_ran = false;
+      auto thread = std::thread {[&] {
+        auto s2 = semaphore {};
+        auto result = semaphore::try_open_existing("xtd_semaphore_test_2", s2);
+        assert::is_false(result, csf_);
+        assert::are_equal(wait_handle::invalid_handle, s2.handle(), csf_);
+        thread_ran = true;
+      }};
+      thread.join();
+      assert::is_true(thread_ran, csf_);
+    }
+    
+    void test_method_(try_open_existing_with_empty_name) {
+      if (environment::os_version().is_windows() && !environment::is_64_bit_process()) assert::ignore();
+      auto created_new = false;
+      auto s = semaphore {"xtd_semaphore_test", created_new};
+      assert::are_not_equal(wait_handle::invalid_handle, s.handle(), csf_);
+      auto thread_ran = false;
+      auto thread = std::thread {[&] {
+        auto s2 = semaphore {};
+        auto result = semaphore::try_open_existing("", s2);
+        assert::is_false(result, csf_);
+        assert::are_equal(wait_handle::invalid_handle, s2.handle(), csf_);
+        thread_ran = true;
+      }};
+      thread.join();
+      assert::is_true(thread_ran, csf_);
+    }
   };
 }
