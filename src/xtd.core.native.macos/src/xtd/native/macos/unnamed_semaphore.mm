0c4828e2e04ace71d3482fd0790455adff1b7b5a
Gammasoft <gammasoft71@gmail.com>
2023-08-04 09:13:07 +0200

    Add xtd::native::unnamed_semaphore API.

---
+++ src/xtd.core.native.macos/src/xtd/native/macos/unnamed_semaphore.mm
@@ -0,0 +1,48 @@
+#define __XTD_CORE_NATIVE_LIBRARY__
+#include <xtd/native/unnamed_semaphore.h>
+#include "../../../../include/xtd/native/macos/semaphore.h"
+#undef __XTD_CORE_NATIVE_LIBRARY__
+#import <CoreFoundation/CoreFoundation.h>
+#include <TargetConditionals.h>
+#import <Cocoa/Cocoa.h>
+#include <mach/clock.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+using namespace xtd::native;
+
+intmax_t unnamed_semaphore::create(int_least32_t initial_count, int_least32_t maximum_count) {
+  semaphore_t semaphore;
+  if (semaphore_create(current_task(), &semaphore, SYNC_POLICY_FIFO, std::min(initial_count, maximum_count)) != err_none)
+    return reinterpret_cast<intmax_t>(SEM_FAILED);
+  return static_cast<intmax_t>(semaphore);
+}
+
+void unnamed_semaphore::destroy(intmax_t handle) {
+  if (reinterpret_cast<sem_t*>(handle) == SEM_FAILED) return;
+  semaphore_destroy(current_task(), static_cast<semaphore_t>(handle));
+}
+
+bool unnamed_semaphore::signal(intmax_t handle, int_least32_t release_count, int_least32_t& previous_count, bool& io_error) {
+  io_error = false;
+  if (reinterpret_cast<sem_t*>(handle) == SEM_FAILED) {
+    io_error = true;
+    return false;
+  }
+  previous_count = -1;
+  
+  for (auto count = 0; !io_error && count < release_count; ++count)
+    if (semaphore_signal(static_cast<semaphore_t>(handle)) != err_none) io_error = true;
+  return !io_error;
+}
+
+uint_least32_t unnamed_semaphore::wait(intmax_t handle, int_least32_t milliseconds_timeout) {
+  if (reinterpret_cast<sem_t*>(handle) == SEM_FAILED) return 0xFFFFFFFF;
+  auto result = milliseconds_timeout == -1 ? sem_wait(reinterpret_cast<sem_t*>(handle)) : sem_milliseconds_timedwait(reinterpret_cast<sem_t*>(handle), milliseconds_timeout);
+  if (result && errno == EAGAIN) return 0xFFFFFFFF;
+  if (result && errno == EDEADLK) return 0xFFFFFFFF;
+  if (result && errno == EINTR) return 0x00000080;
+  if (result && errno == EINVAL) return 0xFFFFFFFF;
+  if (result && errno == ETIMEDOUT) return 0x00000102;
+  return 0x00000000;
+}
---
+++ src/xtd.core.native/include/xtd/native/unnamed_semaphore.h
@@ -0,0 +1,78 @@
+/// @file
+/// @brief Contains unnamed_semaphore API.
+/// @copyright Copyright (c) 2023 Gammasoft. All rights reserved.
+#pragma once
+/// @cond
+#if !defined(__XTD_CORE_NATIVE_LIBRARY__)
+#error "Do not include this file: Internal use only"
+#endif
+/// @endcond
+
+#include <xtd/core_native_export.h>
+#include <cstddef>
+#include <cstdint>
+#include <string>
+
+/// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
+namespace xtd {
+  /// @cond
+  namespace threading {
+    class semaphore;
+  }
+  /// @endcond
+  
+  /// @brief The xtd::native namespace contains internal native API definitions to access underlying operating system components used by xtd.core library.
+  /// @warning Internal use only
+  namespace native {
+    /// @brief Contains unnamed_semaphore native API.
+    /// @par Namespace
+    /// xtd::native
+    /// @par Library
+    /// xtd.core.native
+    /// @ingroup xtd_core_native native
+    /// @remarks xtd::threading::semaphore does not use xtd::native::unnamed_semaphore API as it uses std::counting_semaphore class.
+    /// @warning Internal use only
+    class core_native_export_ unnamed_semaphore final {
+      unnamed_semaphore() = delete;
+      friend xtd::threading::semaphore;
+    protected:
+      /// @name Protected methods
+      
+      /// @{
+      /// @brief Create named semaphore with specified initial count and maxixum count.
+      /// @return The handle of the created semaphore.
+      /// @warning Internal use only
+      static intmax_t create(int_least32_t initial_count, int_least32_t max_count);
+      
+      /// @brief Destroy named semaphore with specified semaphore handle.
+      /// @param name The name of the semaphore.
+      /// @param handle The semaphore handle to destroy.
+      /// @warning Internal use only
+      static void destroy(intmax_t handle);
+      
+      /// @brief Open named semaphore with specified name.
+      /// @param name The name, if the synchronization object is to be shared with other processes.
+      /// @return The handle of the opened semaphore.
+      /// @warning Internal use only
+      static intmax_t open(const std::string& name);
+      
+      /// @brief Signal named semaphore with specified semaphore handle.
+      /// @param handle The semaphore handle to signal.
+      /// @return true if succeed; otherwhise false.
+      /// @warning Internal use only
+      static bool signal(intmax_t handle, int_least32_t release_count, int_least32_t& previous_count, bool& io_error);
+      
+      /// @brief Wait named semaphore with specified semaphore handle and timeout.
+      /// @param handle The semaphore handle to wait.
+      /// @param milliseconds_timeout The timeout in milliseconds (-1 is infinite timeout).
+      /// @return error code :
+      ///  * 0x00000000 : Sucess
+      ///  * 0x00000080 : Abandoned
+      ///  * 0x00000102 : Timeout
+      ///  * 0xFFFFFFFF : Failed
+      /// @warning Internal use only
+      static uint_least32_t wait(intmax_t handle, int_least32_t milliseconds_timeout);
+      /// @}
+    };
+  }
+}
