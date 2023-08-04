ca027732f402a20ed5eb6245705ff8b098017d72
Gammasoft <gammasoft71@gmail.com>
2023-08-04 10:01:41 +0200

    Add xtd::native::unnamed_mutex API

---
+++ src/xtd.core.native.macos/src/xtd/native/macos/unnamed_mutex.mm
@@ -0,0 +1,46 @@
+#define __XTD_CORE_NATIVE_LIBRARY__
+#include <xtd/native/unnamed_mutex.h>
+#include "../../../../include/xtd/native/macos/mutex.h"
+#undef __XTD_CORE_NATIVE_LIBRARY__
+#import <CoreFoundation/CoreFoundation.h>
+#include <TargetConditionals.h>
+#import <Cocoa/Cocoa.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+using namespace xtd::native;
+
+intmax_t unnamed_mutex::create(bool initially_owned) {
+  auto mutex = new pthread_mutex_t {};
+  if (pthread_mutex_init(mutex, nullptr) != 0) {
+    delete mutex;
+    return reinterpret_cast<intmax_t>(MUTEX_FAILED);
+  }
+  return reinterpret_cast<intmax_t>(mutex);
+}
+
+void unnamed_mutex::destroy(intmax_t handle) {
+  pthread_mutex_destroy(reinterpret_cast<pthread_mutex_t*>(handle));
+}
+
+bool unnamed_mutex::signal(intmax_t handle, bool& io_error) {
+  io_error = false;
+  if (reinterpret_cast<pthread_mutex_t*>(handle) == MUTEX_FAILED) {
+    io_error = true;
+    return false;
+  }
+  pthread_mutex_lock(reinterpret_cast<pthread_mutex_t*>(handle));
+  if (pthread_mutex_unlock(reinterpret_cast<pthread_mutex_t*>(handle)) != 0) io_error = true;
+  return !io_error;
+}
+
+uint_least32_t unnamed_mutex::wait(intmax_t handle, int_least32_t milliseconds_timeout) {
+  if (reinterpret_cast<pthread_mutex_t*>(handle) == MUTEX_FAILED) return 0xFFFFFFFF;
+  auto result = milliseconds_timeout == -1 ? pthread_mutex_unlock(reinterpret_cast<pthread_mutex_t*>(handle)) : pthread_mutex_milliseconds_timedlock(reinterpret_cast<pthread_mutex_t*>(handle), milliseconds_timeout);
+  if (result && errno == EAGAIN) return 0xFFFFFFFF;
+  if (result && errno == EDEADLK) return 0xFFFFFFFF;
+  if (result && errno == EINTR) return 0x00000080;
+  if (result && errno == EINVAL) return 0xFFFFFFFF;
+  if (result && errno == ETIMEDOUT) return 0x00000102;
+  return 0x00000000;
+}
---
+++ src/xtd.core.native/include/xtd/native/unnamed_mutex.h
@@ -0,0 +1,74 @@
+/// @file
+/// @brief Contains unnamed_mutex API.
+/// @copyright Copyright (c) 2023 Gammasoft. All rights reserved.
+#pragma once
+/// @cond
+#if !defined(__XTD_CORE_NATIVE_LIBRARY__)
+#error "Do not include this file: Internal use only"
+#endif
+/// @endcond
+
+#include <xtd/core_native_export.h>
+#include <cstddef>
+#include <cstdint>
+#include <string>
+
+/// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
+namespace xtd {
+  /// @cond
+  namespace threading {
+    class mutex;
+  }
+  /// @endcond
+  
+  /// @brief The xtd::native namespace contains internal native API definitions to access underlying operating system components used by xtd.core library.
+  /// @warning Internal use only
+  namespace native {
+    /// @brief Contains unnamed_mutex native API.
+    /// @par Namespace
+    /// xtd::native
+    /// @par Library
+    /// xtd.core.native
+    /// @ingroup xtd_core_native native
+    /// @remarks xtd::threading::semaphore does not use xtd::native::unnamed_mutex API as it uses std::recursive_timed_mutex class.
+    /// @warning Internal use only
+    class core_native_export_ unnamed_mutex final {
+      unnamed_mutex() = delete;
+      friend xtd::threading::mutex;
+    protected:
+      /// @name Protected methods
+      
+      /// @{
+      /// @brief Create named mutex with specified name and a bool value that.
+      /// @param initially_owned true to give the calling thread initial ownership of the named system mutex if the named system mutex is created as a result of this call; otherwise, false.
+      /// @param name The name, if the synchronization object is to be shared with other processes.
+      /// @return The handle of the created mutex.
+      /// @warning Internal use only
+      static intmax_t create(bool initially_owned);
+      
+      /// @brief Destroy named mutex with specified mutex handle.
+      /// @param name The name of the mutex.
+      /// @param handle The mutex handle to destroy.
+      /// @warning Internal use only
+      static void destroy(intmax_t handle);
+            
+      /// @brief Signal named mutex with specified mutex handle.
+      /// @param handle The mutex handle to signal.
+      /// @return true if succeed; otherwhise false.
+      /// @warning Internal use only
+      static bool signal(intmax_t handle, bool& io_error);
+      
+      /// @brief Wait named mutex with specified mutex handle and timeout.
+      /// @param handle The mutex handle to wait.
+      /// @param milliseconds_timeout The timeout in milliseconds (-1 is infinite timeout).
+      /// @return error code :
+      ///  * 0x00000000 : Sucess
+      ///  * 0x00000080 : Abandoned
+      ///  * 0x00000102 : Timeout
+      ///  * 0xFFFFFFFF : Failed
+      /// @warning Internal use only
+      static uint_least32_t wait(intmax_t handle, int_least32_t milliseconds_timeout);
+      /// @}
+    };
+  }
+}
