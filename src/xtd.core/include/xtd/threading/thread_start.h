1d5803be379c68556eb2d76efddc61344eaf2ee6
Gammasoft <gammasoft71@gmail.com>
2023-08-05 15:49:02 +0200

    Add xtd::threading::thread_start delegate

---
+++ src/xtd.core/include/xtd/threading/thread_start.h
@@ -0,0 +1,21 @@
+/// @file
+/// @brief Contains xtd::threading::thread_start exception.
+/// @copyright Copyright (c) 2023 Gammasoft. All rights reserved.
+#pragma once
+#include "../action.h"
+
+/// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
+namespace xtd {
+  /// @brief The xtd::threading namespace provides classes and interfaces that enable multithreaded programming. In addition to classes for synchronizing thread activities and access to data ( xtd::threading::auto_reset_event, xtd::threading::monitor, xtd::threading::interlocked, xtd::threading::auto_reset_event, and so on), this namespace includes a xtd::threading::thread_pool class that allows you to use a pool of system-supplied threads, and a xtd::threading::timer class that executes callback methods on thread pool threads.
+  namespace threading {
+    /// @brief Represents the method that executes on a xtd::threading::thread.
+    /// @par Header
+    /// @code #include <xtd/threading/thread_start> @endcode
+    /// @par Namespace
+    /// xtd::threading
+    /// @par Library
+    /// xtd.core
+    /// @ingroup xtd_core
+    using thread_start = action<>;
+  }
+}
