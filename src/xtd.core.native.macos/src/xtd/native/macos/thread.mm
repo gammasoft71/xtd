bd56bb3dfbfc9c065c19ff3780af34cba93dbfc5
Gammasoft <gammasoft71@gmail.com>
2023-08-05 16:48:54 +0200

    Add xtd::native::thread methods

--- src/xtd.core.native.macos/src/xtd/native/macos/thread.mm
+++ src/xtd.core.native.macos/src/xtd/native/macos/thread.mm
@@ -16,3 +16,28 @@ bool thread::cancel(intmax_t handle) {
 intmax_t thread::get_current_thread_handle() {
   return reinterpret_cast<intmax_t>(pthread_self());
 }
+
+bool thread::resume(intmax_t handle) {
+  // The POSIX standard provides no mechanism by which a thread A can suspend the execution of another thread B, without cooperation from B. The only way to implement a suspend/resume mechanism is to have B check periodically some global variable for a suspend request and then suspend itself on a condition variable, which another thread can signal later to restart B.
+  return false;
+}
+
+bool thread::set_current_thread_name(const std::string& name) {
+  pthread_setname_np(name.c_str());
+  return true;
+}
+
+bool thread::set_priority(intmax_t handle, int_least32_t priority) {
+  int_least32_t policy;
+  sched_param schedParam;
+  if (::pthread_getschedparam(reinterpret_cast<pthread_t>(handle), &policy, &schedParam) != 0)
+    return false;
+  
+  schedParam.sched_priority = static_cast<int_least32_t>(ceil((static_cast<double>(priority) * (sched_get_priority_max(policy) - sched_get_priority_min(policy)) / 4) + sched_get_priority_min(policy)));
+  return pthread_setschedparam(reinterpret_cast<pthread_t>(handle), policy, &schedParam) == 0;
+}
+
+bool thread::suspend(intmax_t handle) {
+  // The POSIX standard provides no mechanism by which a thread A can suspend the execution of another thread B, without cooperation from B. The only way to implement a suspend/resume mechanism is to have B check periodically some global variable for a suspend request and then suspend itself on a condition variable, which another thread can signal later to restart B.
+  return false;
+}
