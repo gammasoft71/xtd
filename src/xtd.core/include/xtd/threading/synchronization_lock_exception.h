be34d5a336fecf8d586a1b4280180d1c1c6d59cb
Gammasoft <gammasoft71@gmail.com>
2023-08-05 07:57:35 +0200

    Add synchronization_lock_exception exception

---
+++ src/xtd.core/include/xtd/threading/synchronization_lock_exception.h
@@ -0,0 +1,105 @@
+/// @file
+/// @brief Contains xtd::synchronization_lock_exception exception.
+/// @copyright Copyright (c) 2023 Gammasoft. All rights reserved.
+#pragma once
+#include "../system_exception.h"
+#include "../typeof.h"
+
+/// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
+namespace xtd {
+  /// @brief The xtd::threading namespace provides classes and interfaces that enable multithreaded programming. In addition to classes for synchronizing thread activities and access to data ( xtd::threading::mutex, xtd::threading::monitor, xtd::threading::interlocked, xtd::threading::auto_reset_event, and so on), this namespace includes a xtd::threading::thread_pool class that allows you to use a pool of system-supplied threads, and a xtd::threading::timer class that executes callback methods on thread pool threads.
+  namespace threading {
+    /// @brief The exception that is thrown when a Thread is in an invalid ThreadState for the method call.
+    /// @par Header
+    /// @code #include <xtd/threading/synchronization_lock_exception> @endcode
+    /// @par Namespace
+    /// xtd::threading
+    /// @par Library
+    /// xtd.core
+    /// @ingroup xtd_core exceptions
+    /// @par Examples
+    /// The following example demonstrates how to throw and catch an synchronization_lock_exception.
+    /// @include synchronization_lock_exception.cpp
+    class synchronization_lock_exception : public system_exception {
+    public:
+      /// @name Constructors
+      
+      /// @{
+      /// @brief Create a new instance of class synchronization_lock_exception
+      /// @param information (optional) Contains current information about member name, file path and  line number in the file where the exception is occurred. Typically #current_stack_frame_.
+      /// @remarks Message is set with the default message associate to the exception.
+      explicit synchronization_lock_exception(const xtd::diagnostics::stack_frame& info = xtd::diagnostics::stack_frame::empty()) : system_exception(default_message(), info) {}
+      /// @brief Create a new instance of class synchronization_lock_exception
+      /// @param message Message string associate to the exception.
+      /// @param information (optional) Contains current information about member name, file path and  line number in the file where the exception is occurred. Typically #current_stack_frame_.
+      explicit synchronization_lock_exception(const xtd::ustring& message, const xtd::diagnostics::stack_frame& info = xtd::diagnostics::stack_frame::empty()) : system_exception(message, info) {}
+      /// @brief Create a new instance of class synchronization_lock_exception
+      /// @param message Message string associate to the exception.
+      /// @param error Error code associate to the exception.
+      /// @param information (optional) Contains current information about member name, file path and  line number in the file where the exception is occurred. Typically #current_stack_frame_.
+      explicit synchronization_lock_exception(const xtd::ustring& message, const std::error_code& error, const xtd::diagnostics::stack_frame& info = xtd::diagnostics::stack_frame::empty()) : system_exception(message, error, info) {}
+      /// @brief Create a new instance of class synchronization_lock_exception
+      /// @param message Message string associate to the exception.
+      /// @param help_link Help link string associate to the exception.
+      /// @param information (optional) Contains current information about member name, file path and  line number in the file where the exception is occurred. Typically #current_stack_frame_.
+      explicit synchronization_lock_exception(const xtd::ustring& message, const xtd::ustring& help_link, const xtd::diagnostics::stack_frame& info = xtd::diagnostics::stack_frame::empty()) : system_exception(message, help_link, info) {}
+      /// @brief Create a new instance of class synchronization_lock_exception
+      /// @param message Message string associate to the exception.
+      /// @param error Error code associate to the exception.
+      /// @param help_link Help link string associate to the exception.
+      explicit synchronization_lock_exception(const xtd::ustring& message, const std::error_code& error, const xtd::ustring& help_link, const xtd::diagnostics::stack_frame& info = xtd::diagnostics::stack_frame::empty()) : system_exception(message, error, help_link, info) {}
+      /// @brief Create a new instance of class synchronization_lock_exception
+      /// @param inner_exception The exception that is the cause of the current exception.
+      /// @param information (optional) Contains current information about member name, file path and  line number in the file where the exception is occurred. Typically #current_stack_frame_.
+      /// @remarks Message is set with the default message associate to the exception.
+      explicit synchronization_lock_exception(const std::exception& inner_exception, const xtd::diagnostics::stack_frame& info = xtd::diagnostics::stack_frame::empty()) : system_exception(default_message(), inner_exception, info) {}
+      /// @brief Create a new instance of class synchronization_lock_exception
+      /// @param message Message string associate to the exception.
+      /// @param inner_exception The exception that is the cause of the current exception.
+      /// @param information (optional) Contains current information about member name, file path and  line number in the file where the exception is occurred. Typically #current_stack_frame_.
+      explicit synchronization_lock_exception(const xtd::ustring& message, const std::exception& inner_exception, const xtd::diagnostics::stack_frame& info = xtd::diagnostics::stack_frame::empty()) : system_exception(message, inner_exception, info) {}
+      /// @brief Create a new instance of class synchronization_lock_exception
+      /// @param message Message string associate to the exception.
+      /// @param inner_exception The exception that is the cause of the current exception.
+      /// @param error Error code associate to the exception.
+      /// @param information (optional) Contains current information about member name, file path and  line number in the file where the exception is occurred. Typically #current_stack_frame_.
+      explicit synchronization_lock_exception(const xtd::ustring& message, const std::exception& inner_exception, const std::error_code& error, const xtd::diagnostics::stack_frame& info = xtd::diagnostics::stack_frame::empty()) : system_exception(message, inner_exception, error, info) {}
+      /// @brief Create a new instance of class synchronization_lock_exception
+      /// @param message Message string associate to the exception.
+      /// @param inner_exception The exception that is the cause of the current exception.
+      /// @param help_link Help link string associate to the exception.
+      /// @param information (optional) Contains current information about member name, file path and  line number in the file where the exception is occurred. Typically #current_stack_frame_.
+      explicit synchronization_lock_exception(const xtd::ustring& message, const std::exception& inner_exception, const xtd::ustring& help_link, const xtd::diagnostics::stack_frame& info = xtd::diagnostics::stack_frame::empty()) : system_exception(message, inner_exception, help_link, info) {}
+      /// @brief Create a new instance of class synchronization_lock_exception
+      /// @param message Message string associate to the exception.
+      /// @param inner_exception The exception that is the cause of the current exception.
+      /// @param error Error code associate to the exception.
+      /// @param help_link Help link string associate to the exception.
+      /// @param information (optional) Contains current information about member name, file path and  line number in the file where the exception is occurred. Typically #current_stack_frame_.
+      explicit synchronization_lock_exception(const xtd::ustring& message, const std::exception& inner_exception, const std::error_code& error, const xtd::ustring& help_link, const xtd::diagnostics::stack_frame& info = xtd::diagnostics::stack_frame::empty()) : system_exception(message, inner_exception, error, help_link, info) {}
+      /// @}
+      
+      /// @cond
+      synchronization_lock_exception(const synchronization_lock_exception&) = default;
+      synchronization_lock_exception& operator =(const synchronization_lock_exception&) = default;
+      /// @endcond
+      
+    private:
+      const char* default_message() const noexcept {return "Object synchronization method was called from an unsynchronized block of code.";}
+    };
+  }
+}
+
+/// @brief Helper on xtd::synchronization_lock_exception to call it with current stack frame information.
+/// @par Library
+/// xtd.core
+/// @ingroup xtd_core exceptions
+/// @remarks Is equivalent to xtd::synchronization_lock_exception({any argument}, csf_)
+/// @code
+/// void my_func() {
+///   if (invalid_info) throw synchronization_lock_exception_(); // same as : throw synchronization_lock_exception(csf_);
+///   if (invalid_value) throw synchronization_lock_exception_("Bad value"); // same as : throw synchronization_lock_exception("Bad value", csf_);
+///   ...
+/// }
+/// @endcode
+#define synchronization_lock_exception_(...) synchronization_lock_exception(add_csf_(__VA_ARGS__))
