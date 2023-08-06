39eb423baee8510c58faba2ccc28e6f3a7726474
Gammasoft <gammasoft71@gmail.com>
2023-08-06 21:06:00 +0200

    Add thread example

---
+++ examples/xtd.core.examples/threading/thread/README.md
@@ -0,0 +1,29 @@
+# mixing_std_and_xtd_threads
+
+Shows how to use and mixing [xtd::threading::thread](https://gammasoft71.github.io/xtd/reference_guides/latest/classxtd_1_1threading_1_1thread.html) and [std::thread](https://en.cppreference.com/w/cpp/thread/thread) classes.
+
+## Sources
+
+[src/mixing_std_and_xtd_threads.cpp](src/mixing_std_and_xtd_threads.cpp)
+
+[CMakeLists.txt](CMakeLists.txt)
+
+# Build and run
+
+Open "Command Prompt" or "Terminal". Navigate to the folder that contains the project and type the following:
+
+```cmake
+xtdc run
+```
+
+# Output
+
+```
+threads {
+  thread={name=main, managed_thread_id=1, priority=normal, thread_id=0x20245e080, state=running}
+  thread={name=xtd_thread_3, managed_thread_id=3, priority=normal, thread_id=0x16ff9f000, state=background}
+  thread={name=std_thread_2, managed_thread_id=0, priority=highest, thread_id=0x16ff13000, state=background}
+  thread={name=std_thread_4, managed_thread_id=0, priority=highest, thread_id=0x16ff13000, state=background}
+  thread={name=xtd_thread_1, managed_thread_id=2, priority=below_normal, thread_id=0x16fe87000, state=running}
+}
+```
---
+++ examples/xtd.core.examples/threading/thread/src/thread.cpp
@@ -0,0 +1,68 @@
+#include <xtd/threading/thread>
+#include <xtd/console>
+#include <xtd/startup>
+
+using namespace xtd;
+using namespace xtd::threading;
+
+// Simple threading scenario:  Start a static method running on a second thread.
+class thread_example {
+public:
+  // The thread_proc method is called when the thread starts.
+  // It loops ten times, writing to the console and yielding
+  // the rest of its time slice each time, and then ends.
+  static void thread_proc() {
+    for (int i = 0; i < 10; i++) {
+      console::write_line("thread_proc: {0}", i);
+      // Yield the rest of the time slice.
+      thread::sleep(0);
+    }
+  }
+  
+  static void main() {
+    console::write_line("Main thread: Start a second thread.");
+    // The constructor for the Thread class requires a ThreadStart
+    // delegate that represents the method to be executed on the
+    // thread.  C# simplifies the creation of this delegate.
+    thread t {thread_start {thread_proc}};
+    
+    // Start thread_proc.  Note that on a uniprocessor, the new
+    // thread does not get any processor time until the main thread
+    // is preempted or yields.  Uncomment the Thread.Sleep that
+    // follows t.Start() to see the difference.
+    t.start();
+    //thread::sleep(0);
+    
+    for (int i = 0; i < 4; i++) {
+      console::write_line("Main thread: Do some work.");
+      thread::sleep(0);
+    }
+    
+    console::write_line("Main thread: Call Join(), to wait until thread_proc ends.");
+    t.join();
+    console::write_line("Main thread: thread_proc.Join has returned.  Press Enter to end program.");
+    console::read_line();
+  }
+};
+
+startup_(thread_example);
+
+// This code can produce the following output:
+//
+// Main thread: Start a second thread.
+// Main thread: Do some work.
+// Main thread: Do some work.
+// Main thread: Do some work.
+// Main thread: Do some work.
+// Main thread: Call Join(), to wait until thread_proc ends.
+// thread_proc: 0
+// thread_proc: 1
+// thread_proc: 2
+// thread_proc: 3
+// thread_proc: 4
+// thread_proc: 5
+// thread_proc: 6
+// thread_proc: 7
+// thread_proc: 8
+// thread_proc: 9
+// Main thread: thread_proc.Join has returned.  Press Enter to end program.
