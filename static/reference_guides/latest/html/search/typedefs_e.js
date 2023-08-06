11a0f453d8cf0d4e7bcaaf96acef9bf78184143c
gammasoft71 <gammasoft71@gmail.com>
2023-08-06 21:00:10 +0200

    update latest reference guide

--- static/reference_guides/latest/html/search/typedefs_e.js
+++ static/reference_guides/latest/html/search/typedefs_e.js
@@ -5,11 +5,12 @@ var searchData=
   ['panel_2',['panel',['../group__style__sheets.html#gaae87e0a9c1c5ae2ac0e089e242234eca',1,'xtd::forms::style_sheets']]],
   ['panel_5fstate_3',['panel_state',['../group__xtd__forms.html#ga2d29bb9d20f84517372b956243b5c810',1,'xtd::forms::visual_styles']]],
   ['panels_5ft_4',['panels_t',['../classxtd_1_1forms_1_1style__sheets_1_1style__sheet.html#ae1ddf69a5f561082156ab04d7615d3e1',1,'xtd::forms::style_sheets::style_sheet']]],
-  ['pointer_5',['pointer',['../classxtd_1_1forms_1_1layout_1_1arranged__element__collection.html#ac583e335292983f536a0f34b45052e59',1,'xtd::forms::layout::arranged_element_collection']]],
-  ['points_5fcollection_6',['points_collection',['../classxtd_1_1forms_1_1dot__matrix__display.html#a8290530d8532857d7e620616c985891b',1,'xtd::forms::dot_matrix_display']]],
-  ['predicate_7',['predicate',['../group__xtd__core.html#gaa083bb9debdfafabdf55431467759471',1,'xtd']]],
-  ['program_5fexit_5fevent_5fhandler_8',['program_exit_event_handler',['../group__events.html#ga05028567f0783e489a1c6e338026b99d',1,'xtd']]],
-  ['progress_5fchanged_5fevent_5fhandler_9',['progress_changed_event_handler',['../group__events.html#gab622f963701c9e772309591048e9f180',1,'xtd::forms']]],
-  ['property_5fmap_10',['property_map',['../property__map_8h.html#a8f98379cbe916d9dcf3e0cfac9eeaae6',1,'xtd::web::css']]],
-  ['ptrdiff_11',['ptrdiff',['../group__types.html#gafc1f246c3bac57da997e1ebeb373d5db',1,'xtd']]]
+  ['parameterized_5fthread_5fstart_5',['parameterized_thread_start',['../group__xtd__core.html#ga75b3f3c4dc90a1776a73a030be9cb437',1,'xtd::threading']]],
+  ['pointer_6',['pointer',['../classxtd_1_1forms_1_1layout_1_1arranged__element__collection.html#ac583e335292983f536a0f34b45052e59',1,'xtd::forms::layout::arranged_element_collection']]],
+  ['points_5fcollection_7',['points_collection',['../classxtd_1_1forms_1_1dot__matrix__display.html#a8290530d8532857d7e620616c985891b',1,'xtd::forms::dot_matrix_display']]],
+  ['predicate_8',['predicate',['../group__xtd__core.html#gaa083bb9debdfafabdf55431467759471',1,'xtd']]],
+  ['program_5fexit_5fevent_5fhandler_9',['program_exit_event_handler',['../group__events.html#ga05028567f0783e489a1c6e338026b99d',1,'xtd']]],
+  ['progress_5fchanged_5fevent_5fhandler_10',['progress_changed_event_handler',['../group__events.html#gab622f963701c9e772309591048e9f180',1,'xtd::forms']]],
+  ['property_5fmap_11',['property_map',['../property__map_8h.html#a8f98379cbe916d9dcf3e0cfac9eeaae6',1,'xtd::web::css']]],
+  ['ptrdiff_12',['ptrdiff',['../group__types.html#gafc1f246c3bac57da997e1ebeb373d5db',1,'xtd']]]
 ];
