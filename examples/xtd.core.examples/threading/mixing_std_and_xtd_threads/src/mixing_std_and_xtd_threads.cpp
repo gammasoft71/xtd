918e099e02a9670aa7048803b571b4d794f4f133
Gammasoft <gammasoft71@gmail.com>
2023-08-06 20:52:31 +0200

    Add mixing_std_and_xtd_threads example

---
+++ examples/xtd.core.examples/threading/mixing_std_and_xtd_threads/src/mixing_std_and_xtd_threads.cpp
@@ -0,0 +1,68 @@
+#include <xtd/threading/thread>
+#include <xtd/console>
+#include <xtd/startup>
+
+using namespace xtd;
+using namespace xtd::threading;
+
+namespace mixing_std_and_xtd_threads_example {
+  class mixing_std_and_xtd_threads_class {
+  private:
+    static ustring to_string(const thread& thread) {
+      return ustring::format("  thread={{name={}, managed_thread_id={}, priority={}, thread_id=0x{:x}, state={}}}", thread.name(), thread.managed_thread_id(), thread.thread_priority(), thread.thread_id(), thread.thread_state());
+    }
+    
+  public:
+    static auto main() {
+      console::write_line("threads {");
+      thread::current_thread().name("main");
+      console::write_line(to_string(thread::current_thread()));
+      
+      thread t1 {thread_start {[&] {
+        thread::sleep(20_ms);
+        console::write_line(to_string(t1));
+      }}};
+      t1.name("xtd_thread_1");
+      t1.thread_priority(thread_priority::below_normal);
+      t1.start();
+      
+      std::thread t2 {[] {
+        thread::current_thread().name("std_thread_2");
+        thread::current_thread().thread_priority(thread_priority::highest);
+        console::write_line(to_string(thread::current_thread()));
+      }};
+      
+      thread t3 {thread_start {[] {
+        console::write_line(to_string(thread::current_thread()));
+      }}};
+      t3.name("xtd_thread_3");
+      t3.is_background(true);
+      t3.start();
+      
+      std::thread t4 {[] {
+        thread::sleep(10_ms);
+        thread::current_thread().name("std_thread_4");
+        thread::current_thread().thread_priority(thread_priority::highest);
+        console::write_line(to_string(thread::current_thread()));
+      }};
+      
+      if (t1.joinable()) t1.join();
+      if (t2.joinable()) t2.join();
+      if (t3.joinable()) t3.join();
+      if (t4.joinable()) t4.join();
+      console::write_line("}");
+    }
+  };
+}
+
+startup_(mixing_std_and_xtd_threads_example::mixing_std_and_xtd_threads_class);
+
+// This code produces the following output:
+//
+// threads {
+//   thread={name=main, managed_thread_id=1, priority=normal, thread_id=0x20245e080, state=running}
+//   thread={name=xtd_thread_3, managed_thread_id=3, priority=normal, thread_id=0x16ff9f000, state=background}
+//   thread={name=std_thread_2, managed_thread_id=0, priority=highest, thread_id=0x16ff13000, state=background}
+//   thread={name=std_thread_4, managed_thread_id=0, priority=highest, thread_id=0x16ff13000, state=background}
+//   thread={name=xtd_thread_1, managed_thread_id=2, priority=below_normal, thread_id=0x16fe87000, state=running}
+// }
