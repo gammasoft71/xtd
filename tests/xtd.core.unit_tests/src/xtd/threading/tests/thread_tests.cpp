84c903e8b6c6205adbaaaed1213a454d69247852
Gammasoft <gammasoft71@gmail.com>
2023-08-08 18:52:48 +0200

    Add unit tests

--- tests/xtd.core.unit_tests/src/xtd/threading/tests/thread_tests.cpp
+++ tests/xtd.core.unit_tests/src/xtd/threading/tests/thread_tests.cpp
@@ -20,8 +20,52 @@ namespace xtd::tests {
       assert::are_equal(thread_priority::normal, t.priority(), csf_);
       assert::are_equal(thread_state::unstarted, t.thread_state(), csf_);
     }
+    
+    void test_method_(constructor_with_empty_thread_start) {
+      assert::throws<argument_exception>([]{thread t {thread_start {}};}, csf_);
+    }
+    
+    void test_method_(constructor_with_thread_start_and_negative_max_stack_size) {
+      assert::throws<argument_exception>([]{thread t {thread_start {[] {}}, -1};}, csf_);
+    }
 
-    void test_method_(maint_thread_default_status) {
+    void test_method_(constructor_with_thread_start) {
+      thread t {thread_start {[] {}}};
+      assert::are_equal(thread::invalid_handle, t.handle(), csf_);
+      assert::is_false(t.is_alive(), csf_);
+      assert::is_false(t.is_background(), csf_);
+      assert::is_false(t.is_thread_pool_thread(), csf_);
+      assert::is_false(t.joinable(), csf_);
+      assert::is_not_zero(t.managed_thread_id(), csf_);
+      assert::is_empty(t.name(), csf_);
+      assert::are_equal(thread::invalid_thread_id, t.thread_id(), csf_);
+      assert::are_equal(thread_priority::normal, t.priority(), csf_);
+      assert::are_equal(thread_state::unstarted, t.thread_state(), csf_);
+    }
+    
+    void test_method_(constructor_with_empty_parameterized_thread_start) {
+      assert::throws<argument_exception>([]{thread t {parameterized_thread_start {}};}, csf_);
+    }
+    
+    void test_method_(constructor_with_parameterized_thread_start_and_negative_max_stack_size) {
+      assert::throws<argument_exception>([]{thread t {parameterized_thread_start {[](std::any) {}}, -1};}, csf_);
+    }
+
+    void test_method_(constructor_with_parameterized_thread_start) {
+      thread t {parameterized_thread_start {[](std::any) {}}};
+      assert::are_equal(thread::invalid_handle, t.handle(), csf_);
+      assert::is_false(t.is_alive(), csf_);
+      assert::is_false(t.is_background(), csf_);
+      assert::is_false(t.is_thread_pool_thread(), csf_);
+      assert::is_false(t.joinable(), csf_);
+      assert::is_not_zero(t.managed_thread_id(), csf_);
+      assert::is_empty(t.name(), csf_);
+      assert::are_equal(thread::invalid_thread_id, t.thread_id(), csf_);
+      assert::are_equal(thread_priority::normal, t.priority(), csf_);
+      assert::are_equal(thread_state::unstarted, t.thread_state(), csf_);
+    }
+
+    void test_method_(main_as_current_thread) {
       thread t = thread::current_thread();
       assert::are_not_equal(thread::invalid_handle, t.handle(), csf_);
       assert::is_true(t.is_alive(), csf_);
@@ -35,7 +79,7 @@ namespace xtd::tests {
       assert::are_equal(thread_state::running, t.thread_state(), csf_);
     }
     
-    void test_method_(std_thread_created_default_status) {
+    void test_method_(std_thread_as_current_thread) {
       bool thread_ran = false;
       std::thread t {[&] {
         thread_ran = true;
@@ -55,7 +99,7 @@ namespace xtd::tests {
       assert::is_true(thread_ran, csf_);
     }
     
-    void test_method_(xtd_threading_thread_created_default_status) {
+    void test_method_(xtd_threading_as_current_thread) {
       bool thread_ran = false;
       thread t {thread_start {[&] {
         thread_ran = true;
