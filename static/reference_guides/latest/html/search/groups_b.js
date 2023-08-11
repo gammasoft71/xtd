82d6daccca9a2991f79403259530d5eef3311c60
gammasoft71 <gammasoft71@gmail.com>
2023-08-11 10:41:54 +0200

    Update latest reference guide

--- static/reference_guides/latest/html/search/groups_b.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['style_5fsheets_0',['style_sheets',['../group__style__sheets.html',1,'']]],
-  ['synthesis_1',['synthesis',['../group__synthesis.html',1,'']]],
-  ['system_2',['system',['../group__system.html',1,'']]],
-  ['system_20information_3',['system information',['../group__system__information.html',1,'']]]
-];
