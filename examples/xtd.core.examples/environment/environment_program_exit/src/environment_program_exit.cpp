bedbfb630ded19b10f6bc83a71e6bb5f15f53d67
Gammasoft <gammasoft71@gmail.com>
2023-08-15 11:48:08 +0200

    Using xtd::threading::thread

--- examples/xtd.core.examples/environment/environment_program_exit/src/environment_program_exit.cpp
+++ examples/xtd.core.examples/environment/environment_program_exit/src/environment_program_exit.cpp
@@ -3,27 +3,28 @@
 #include <xtd/environment>
 #include <xtd/startup>
 
-using namespace std;
 using namespace xtd;
+using namespace xtd::threading;
 
 namespace environment_program_exit_example {
   class program static_ {
   public:
     // The main entry point for the application.
-    static auto main(const vector<ustring>& args) {
+    static auto main(const std::vector<ustring>& args) {
       environment::program_exit += [](auto e) {
         console::write_line("The program is stopped {}ly!", e.exit_mode());
       };
       
       console::write_line("Start");
       // Do something...
-      auto do_something_thread = thread([] {
+      auto do_something_thread = thread{thread_start {[] {
         for (auto step = 0; step < 50; ++step) {
           console::write('.');
           threading::thread::sleep(100_ms);
         }
         console::write_line();
-      });
+      }}};
+      do_something_thread.start();
       do_something_thread.join();
       console::write_line("End");
       
