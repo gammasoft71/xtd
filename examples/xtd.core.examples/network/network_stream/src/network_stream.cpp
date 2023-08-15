0b8544a805f6986bbd26b13554ec6d5016cb8c7f
Gammasoft <gammasoft71@gmail.com>
2023-08-15 11:48:13 +0200

    Using xtd::threading::thread

--- examples/xtd.core.examples/network/network_stream/src/network_stream.cpp
+++ examples/xtd.core.examples/network/network_stream/src/network_stream.cpp
@@ -3,20 +3,19 @@
 #include <xtd/net/sockets/socket>
 #include <xtd/net/sockets/network_stream>
 #include <xtd/net/ip_end_point>
+#include <xtd/threading/thread>
 #include <xtd/console>
-#include <thread>
 
-using namespace std;
-using namespace std::this_thread;
 using namespace xtd;
 using namespace xtd::io;
 using namespace xtd::net;
 using namespace xtd::net::sockets;
+using namespace xtd::threading;
 
 auto main()->int {
   auto terminate_app = false;
   
-  auto server = thread {[&] {
+  auto server = thread {thread_start {[&] {
     auto server_socket = socket {address_family::inter_network, socket_type::stream, protocol_type::tcp};
     server_socket.bind(ip_end_point {ip_address::any, 9400});
     server_socket.listen();
@@ -25,9 +24,10 @@ auto main()->int {
     
     while (!terminate_app)
       if (stream.data_available()) console::write_line(reader.read_line());
-  }};
+  }}};
+  server.start();
   
-  auto client = thread {[&] {
+  auto client = thread {thread_start {[&] {
     auto stream = network_stream {socket {address_family::inter_network, socket_type::stream, protocol_type::tcp}};
     stream.socket().connect(ip_address::loopback, 9400);
     auto writer = stream_writer {stream};
@@ -35,9 +35,10 @@ auto main()->int {
     auto counter = 0;
     while (!terminate_app) {
       writer.write_line(ustring::format("counter={}", ++counter));
-      sleep_for(50_ms);
+      thread::sleep(50_ms);
     }
-  }};
+  }}};
+  client.start();
   
   console::read_key();
   terminate_app = true;
