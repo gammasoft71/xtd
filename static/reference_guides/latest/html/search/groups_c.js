eefa1690e9551c6efc8f390eb2d46251fcfcde2a
gammasoft71 <gammasoft71@gmail.com>
2023-08-21 21:00:07 +0200

    update latest reference guide

---
+++ static/reference_guides/latest/html/search/groups_c.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['threading_0',['threading',['../group__threading.html',1,'']]],
+  ['translate_1',['translate',['../group__translate.html',1,'']]],
+  ['types_2',['types',['../group__types.html',1,'']]]
+];
---
+++ static/reference_guides/latest/html/search/typedefs_6.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['generic_5fevent_5fhandler_0',['generic_event_handler',['../group__events.html#ga531b610b74cb14c6047fb0843ab686b4',1,'xtd']]],
+  ['gradient_5fstop_1',['gradient_stop',['../group__drawing.html#ga64f6a18e05cfdce0be5e7a24c7d6ba65',1,'xtd::drawing::drawing_2d']]],
+  ['gradient_5fstop_5fcollection_2',['gradient_stop_collection',['../group__drawing.html#ga1af9e4c38fd8d7b0b54f5f695e98cb01',1,'xtd::drawing::drawing_2d']]]
+];
