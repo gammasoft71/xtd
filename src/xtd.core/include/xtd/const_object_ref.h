236773f63f8b72cf4093c6148c0435534cbfd611
Gammasoft <gammasoft71@gmail.com>
2023-08-22 19:23:13 +0200

    Add const _object ref alias

---
+++ src/xtd.core/include/xtd/const_object_ref.h
@@ -0,0 +1,19 @@
+/// @file
+/// @brief Contains xtd::const_object_ref alias.
+/// @copyright Copyright (c) 2023 Gammasoft. All rights reserved.
+#pragma once
+#include "object"
+#include <functional>
+
+/// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
+namespace xtd {
+  /// @brief Represents an object reference.
+  /// @par Header
+  /// @code #include <xtd/forms/const_object_ref> @endcode
+  /// @par Namespace
+  /// xtd::forms
+  /// @par Library
+  /// xtd.forms
+  /// @ingroup xtd_core
+  using const_object_ref = std::reference_wrapper<const object>;
+}
