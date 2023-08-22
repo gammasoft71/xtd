2b5c5d4727f6695b8add456de9dfe7b950eb4914
Gammasoft <gammasoft71@gmail.com>
2023-08-22 19:25:09 +0200

    Add xtd::timers::elapsed_event_handler alias

---
+++ src/xtd.core/include/xtd/timers/elapsed_event_handler.h
@@ -0,0 +1,18 @@
+/// @file
+/// @brief Contains xtd::elapsed_event_handler event handler.
+/// @copyright Copyright (c) 2023 Gammasoft. All rights reserved.
+#pragma once
+#include "../event_handler.h"
+#include "elapsed_event_args.h"
+
+/// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
+namespace xtd {
+  /// @brief Represents the method that will handle the signal events of a xtd::environment.
+  /// @param e A xtd::elapsed_event_args object that contains the event data.
+  /// @par Namespace
+  /// xtd
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core events
+  using elapsed_event_handler = xtd::delegate<void(xtd::object& sender, const elapsed_event_args& e)>;
+}
