959c094e23b13461fb8dbfcdcc0f9cc9ceb9ec89
gammasoft71 <gammasoft71@gmail.com>
2023-08-27 14:45:02 +0200

    Update latest reference guide

--- static/reference_guides/latest/html/search/defines_10.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['wait_5fhandle_5fcannot_5fbe_5fopened_5fexception_5f_0',['wait_handle_cannot_be_opened_exception_',['../wait__handle__cannot__be__opened__exception_8h.html#acd7c9ebc0466571b74a21efaf152f4d8',1,'wait_handle_cannot_be_opened_exception.h']]],
-  ['web_5fexception_5f_1',['web_exception_',['../web__exception_8h.html#a5132bd51f17d9968a91b55d9d9b13690',1,'web_exception.h']]]
-];
--- static/reference_guides/latest/html/search/groups_7.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['libraries_0',['libraries',['../group__libraries.html',1,'']]],
-  ['literals_1',['literals',['../group__literals.html',1,'']]]
-];
