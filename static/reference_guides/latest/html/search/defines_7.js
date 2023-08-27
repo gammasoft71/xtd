a10c72cdc18ace68f845fe40d2b64080d26e70a4
gammasoft71 <gammasoft71@gmail.com>
2023-08-27 21:00:14 +0200

    update latest reference guide

---
+++ static/reference_guides/latest/html/search/defines_7.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['key_5fnot_5ffound_5fexception_5f_0',['key_not_found_exception_',['../key__not__found__exception_8h.html#ad174ac69ded64fa78e96416de15f317f',1,'key_not_found_exception.h']]]
+];
