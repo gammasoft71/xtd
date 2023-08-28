6cac8beaa1fc4ddb3c77d4bb30f96f8824148578
Gammasoft <gammasoft71@gmail.com>
2023-08-28 16:53:58 +0200

    xtd::threading::barrier implementation (WIP)

---
+++ src/xtd.core/src/xtd/threading/barrier.cpp
@@ -0,0 +1,118 @@
+#include "../../../include/xtd/threading/manual_reset_event.h"
+#include "../../../include/xtd/threading/barrier.h"
+#include "../../../include/xtd/threading/semaphore.h"
+#include "../../../include/xtd/argument_out_of_range_exception.h"
+#include "../../../include/xtd/as.h"
+#include "../../../include/xtd/int32_object.h"
+#include "../../../include/xtd/invalid_operation_exception.h"
+#include "../../../include/xtd/lock.h"
+
+using namespace xtd;
+using namespace xtd::threading;
+
+struct barrier::data : object {
+  const cancellation_token* cancellation_token = nullptr;
+  int32 current_phase_number = 0;
+  int32 participant_count = 0;
+  int32 participants_remaining = 0;
+  action<barrier&> post_phase_action;
+  bool run_post_phase_action = false;
+  semaphore phase_semaphore;
+};
+
+barrier::barrier() : barrier(0) {
+}
+
+barrier::barrier(int32 participant_count) : barrier(participant_count, {}) {
+}
+
+barrier::barrier(int32 participant_count, action<barrier&> post_phase_action) : data_(std::make_shared<data>()) {
+  if (participant_count < 0) throw argument_out_of_range_exception {csf_};
+  data_->participant_count = participant_count;
+  data_->participants_remaining = participant_count;
+  data_->post_phase_action = post_phase_action;
+}
+
+int32 barrier::current_phase_number() const noexcept {
+  return data_->current_phase_number;
+}
+
+int32 barrier::participant_count() const noexcept {
+  return data_->participant_count;
+}
+
+int32 barrier::participants_remaining() const noexcept {
+  return data_->participants_remaining;
+}
+
+int32 barrier::add_participant() {
+  return add_participants(1);
+}
+
+int32 barrier::add_participants(int32 participant_count) {
+  lock_(*data_) {
+    if (participant_count < 0 || as<int64>(data_->participant_count) + participant_count > as<int64>(int32_object::max_value)) throw argument_out_of_range_exception {csf_};
+    if (data_->run_post_phase_action) throw invalid_operation_exception(csf_);
+    data_->participant_count += participant_count;
+    data_->participants_remaining += participant_count;
+    return data_->current_phase_number;
+  }
+  return data_->current_phase_number;
+}
+
+void barrier::close() {
+  // to do...
+}
+
+int32 barrier::remove_participant() {
+  return remove_participants(1);
+}
+
+int32 barrier::remove_participants(int32 participant_count) {
+  lock_(*data_) {
+    if (participant_count < 0) throw argument_out_of_range_exception {csf_};
+    if (data_->participant_count == 0 || data_->run_post_phase_action || data_->participants_remaining < data_->participant_count - participant_count) throw invalid_operation_exception {csf_};
+    if (data_->participant_count < participant_count) throw argument_out_of_range_exception {csf_};
+    data_->participant_count -= participant_count;
+    data_->participants_remaining -= participant_count;
+    return data_->current_phase_number;
+  }
+  return data_->current_phase_number;
+}
+
+void barrier::signal_and_wait() {
+  signal_and_wait(timeout::infinite);
+}
+
+bool barrier::signal_and_wait(int32 milliseconds_timeout) {
+  lock_(*data_) {
+    data_->participants_remaining--;
+    
+    if (data_->participants_remaining == 0) {
+      if (!data_->post_phase_action.is_empty()) data_->post_phase_action(*this);
+      
+      data_->current_phase_number++;
+      data_->participants_remaining = data_->participant_count;
+      
+      for (int i = 0; i < data_->participant_count; i++)
+        data_->phase_semaphore.release();
+    }
+  }
+  return data_->phase_semaphore.wait_one(milliseconds_timeout);
+}
+
+bool barrier::signal_and_wait(const cancellation_token& cancellation_token) {
+  return signal_and_wait(timeout::infinite, cancellation_token);
+}
+bool barrier::signal_and_wait(const time_span& timeout) {
+  return signal_and_wait(as<int32>(timeout.total_milliseconds()));
+}
+
+bool barrier::signal_and_wait(int32 milliseconds_timeout, const cancellation_token& cancellation_token) {
+  data_->cancellation_token = &cancellation_token;
+  return signal_and_wait(milliseconds_timeout);
+}
+
+bool barrier::signal_and_wait(const time_span& timeout, const cancellation_token& cancellation_token) {
+  return signal_and_wait(as<int32>(timeout.total_milliseconds()), cancellation_token);
+}
