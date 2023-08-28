7d521d562b7569c671be454fe317f5584a8e79fb
Gammasoft <gammasoft71@gmail.com>
2023-08-28 13:47:16 +0200

    Fix example

--- examples/xtd.core.examples/date_time/date_time_max_value/src/date_time_max_value.cpp
+++ examples/xtd.core.examples/date_time/date_time_max_value/src/date_time_max_value.cpp
@@ -1,5 +1,6 @@
 #include <xtd/console>
 #include <xtd/date_time>
+#include <xtd/int64_object>
 #include <xtd/startup>
 
 using namespace xtd;
@@ -8,17 +9,17 @@ class program {
 public:
   static auto main() {
     // Attempt to assign an out-of-range value to a date_time constructor.
-    auto number_of_ticks = ticks::max();
+    auto number_of_ticks = int64_object::max_value;
     auto valid_date = date_time {};
     
     // Validate the value.
     if (number_of_ticks >= date_time::min_value.ticks() && number_of_ticks <= date_time::max_value.ticks()) {
       valid_date = date_time(number_of_ticks);
-      console::write_line("{0} is valid.", valid_date.ticks().count());
+      console::write_line("{0} is valid.", valid_date.ticks());
     } else if (number_of_ticks < date_time::min_value.ticks())
-      console::write_line("{0} is less than {1} ticks.", number_of_ticks.count(), date_time::min_value.ticks().count());
+      console::write_line("{0} is less than {1} ticks.", number_of_ticks, date_time::min_value.ticks());
     else
-      console::write_line("{0} is greater than {1} ticks.", number_of_ticks.count(), date_time::max_value.ticks().count());
+      console::write_line("{0} is greater than {1} ticks.", number_of_ticks, date_time::max_value.ticks());
   }
 };
 
