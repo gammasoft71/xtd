54ff4b64a71c0500d9b2efe7980ba41c212cd209
gammasoft71 <gammasoft71@gmail.com>
2023-08-30 23:23:03 +0200

    Update latest reference guide

--- static/reference_guides/latest/html/search/defines_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['block_5fscope_5f_0',['block_scope_',['../block__scope_8h.html#a9ba81b3bf268ec1eb95752f516f563e9',1,'block_scope.h']]]
-];
--- static/reference_guides/latest/html/search/defines_f.js
+++
@@ -1,9 +0,0 @@
-var searchData=
-[
-  ['unauthorized_5faccess_5fexception_5f_0',['unauthorized_access_exception_',['../unauthorized__access__exception_8h.html#a0b351a01800b7f94c38ddc9609ec9662',1,'unauthorized_access_exception.h']]],
-  ['underflow_5fexception_5f_1',['underflow_exception_',['../underflow__exception_8h.html#a2e42b51ecd30611493b2ad42f9b95589',1,'underflow_exception.h']]],
-  ['unused_5f_2',['unused_',['../unused_8h.html#a0ee680d2ff7ec14c0dc73da3bb2e5c91',1,'unused.h']]],
-  ['uri_5fformat_5fexception_5f_3',['uri_format_exception_',['../uri__format__exception_8h.html#acedd52809de7cdfaab837815d91850df',1,'uri_format_exception.h']]],
-  ['uri_5ftemplate_5fmatch_5fexception_5f_4',['uri_template_match_exception_',['../uri__template__match__exception_8h.html#ade3d9c518884628814b60557e9c828f9',1,'uri_template_match_exception.h']]],
-  ['using_5f_5',['using_',['../using_8h.html#a73249a3d4ad48e36724bccf5f3e48f2f',1,'using.h']]]
-];
