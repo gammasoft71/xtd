4f3df0cd47cae34484bd8abc516e902a86675706
Gammasoft <gammasoft71@gmail.com>
2023-08-30 21:21:06 +0200

    Review startup examples

--- examples/xtd.core.examples/startups/startup1/src/startup1.cpp
+++ examples/xtd.core.examples/startups/startup1/src/startup1.cpp
@@ -38,7 +38,7 @@ auto main()->int {
 
 // This code produces the following output if one two "three four" five are entered on command line:
 //
-// /!---OMITTED---!/main1
+// /!---OMITTED---!/startup1
 // one
 // two
 // three four
