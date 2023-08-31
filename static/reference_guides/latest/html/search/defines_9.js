d5be867aaa6db72b5b3c1435f9fb35990cf2cf82
gammasoft71 <gammasoft71@gmail.com>
2023-08-31 21:00:13 +0200

    update latest reference guide

---
+++ static/reference_guides/latest/html/search/defines_9.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['matches_5f_0',['matches_',['../string__assert_8h.html#a28383a4bd73891a18641bd1fba80a1db',1,'string_assert.h']]]
+];
---
+++ static/reference_guides/latest/html/search/groups_c.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['threading_0',['threading',['../group__threading.html',1,'']]],
+  ['timers_1',['timers',['../group__timers.html',1,'']]],
+  ['translate_2',['translate',['../group__translate.html',1,'']]],
+  ['types_3',['types',['../group__types.html',1,'']]]
+];
