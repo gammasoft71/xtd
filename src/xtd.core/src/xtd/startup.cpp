bdc96579314de8de57106284a429e5d2a8afc465
Gammasoft <gammasoft71@gmail.com>
2023-08-31 15:26:51 +0200

    Add startup::run_safe methods with xtd::delegate param

--- src/xtd.core/src/xtd/startup.cpp
+++ src/xtd.core/src/xtd/startup.cpp
@@ -17,11 +17,59 @@ void startup::end_safe_run() {
   thread::join_all();
 }
 
+int startup::safe_run(xtd::delegate<void()> main_function, int argc, char* argv[]) {
+  return internal_safe_run(main_function, argc, argv);
+}
+
+int startup::safe_run(xtd::delegate<void()> main_function) {
+  return internal_safe_run(main_function, std::nullopt, std::nullopt);
+}
+
+int startup::safe_run(xtd::delegate<void(int, char* [])> main_function, int argc, char* argv[]) {
+  return internal_safe_run(main_function, argc, argv);
+}
+
+int startup::safe_run(xtd::delegate<void(int, char* [])> main_function) {
+  return internal_safe_run(main_function, std::nullopt, std::nullopt);
+}
+
+int startup::safe_run(xtd::delegate<void(const string_vector&)> main_function, int argc, char* argv[]) {
+  return internal_safe_run(main_function, argc, argv);
+}
+
+int startup::safe_run(xtd::delegate<void(const string_vector&)> main_function) {
+  return internal_safe_run(main_function, std::nullopt, std::nullopt);
+}
+
+int startup::safe_run(xtd::delegate<int()> main_function, int argc, char* argv[]) {
+  return internal_safe_run(main_function, argc, argv);
+}
+
+int startup::safe_run(xtd::delegate<int()> main_function) {
+  return internal_safe_run(main_function, std::nullopt, std::nullopt);
+}
+
+int startup::safe_run(xtd::delegate<int(int, char* [])> main_function, int argc, char* argv[]) {
+  return internal_safe_run(main_function, argc, argv);
+}
+
+int startup::safe_run(xtd::delegate<int(int, char* [])> main_function) {
+  return internal_safe_run(main_function, std::nullopt, std::nullopt);
+}
+
+int startup::safe_run(xtd::delegate<int(const string_vector&)> main_function, int argc, char* argv[]) {
+  return internal_safe_run(main_function, argc, argv);
+}
+
+int startup::safe_run(xtd::delegate<int(const string_vector&)> main_function) {
+  return internal_safe_run(main_function, std::nullopt, std::nullopt);
+}
+
 int startup::safe_run(void (*main_function)(int, char* []), int argc, char* argv[]) {
   return internal_safe_run(main_function, argc, argv);
 }
 
-int startup::safe_run(void (*main_function)(const xtd::collections::specialized::string_vector&), int argc, char* argv[]) {
+int startup::safe_run(void (*main_function)(const string_vector&), int argc, char* argv[]) {
   return internal_safe_run(main_function, argc, argv);
 }
 
@@ -29,7 +77,7 @@ int startup::safe_run(int (*main_function)(int, char* []), int argc, char* argv[
   return internal_safe_run(main_function, argc, argv);
 }
 
-int startup::safe_run(int (*main_function)(const xtd::collections::specialized::string_vector&), int argc, char* argv[]) {
+int startup::safe_run(int (*main_function)(const string_vector&), int argc, char* argv[]) {
   return internal_safe_run(main_function, argc, argv);
 }
 
@@ -37,7 +85,7 @@ int startup::safe_run(void (*main_function)(int, char* [])) {
   return internal_safe_run(main_function, std::nullopt, std::nullopt);
 }
 
-int startup::safe_run(void (*main_function)(const xtd::collections::specialized::string_vector&)) {
+int startup::safe_run(void (*main_function)(const string_vector&)) {
   return internal_safe_run(main_function, std::nullopt, std::nullopt);
 }
 
@@ -45,16 +93,42 @@ int startup::safe_run(int (*main_function)(int, char* [])) {
   return internal_safe_run(main_function, std::nullopt, std::nullopt);
 }
 
-int startup::safe_run(int (*main_function)(const xtd::collections::specialized::string_vector&)) {
+int startup::safe_run(int (*main_function)(const string_vector&)) {
   return internal_safe_run(main_function, std::nullopt, std::nullopt);
 }
 
+int startup::run(xtd::delegate<void()> main_function, int, char* []) {
+  main_function();
+  return environment::exit_code();
+}
+
+int startup::run(xtd::delegate<void()> main_function) {
+  main_function();
+  return environment::exit_code();
+}
+
 int startup::run(void (*main_function)(), int, char* []) {
-  main_function(); return environment::exit_code();
+  main_function();
+  return environment::exit_code();
 }
 
 int startup::run(void (*main_function)()) {
   main_function();
+  return environment::exit_code();
+}
+
+int startup::run(xtd::delegate<void(int, char* [])> main_function, int argc, char* argv[]) {
+  main_function(argc, argv);
+  return environment::exit_code();
+}
+
+int startup::run(xtd::delegate<void(int, char* [])> main_function) {
+  auto args = environment::get_command_line_args();
+  char** argv = new char*[args.size()];
+  for (auto index = 0ul; index < args.size(); ++index)
+    argv[index] = args[index].data();
+  main_function(as<int32>(args.size()), argv);
+  delete[] argv;
   return environment::exit_code();
 }
 
@@ -73,6 +147,17 @@ int startup::run(void (*main_function)(int, char* [])) {
   return environment::exit_code();
 }
 
+int startup::run(xtd::delegate<void (const string_vector&)> main_function, int argc, char* argv[]) {
+  main_function({argv + 1, argv + argc});
+  return environment::exit_code();
+}
+
+int startup::run(xtd::delegate<void (const string_vector&)> main_function) {
+  auto args = environment::get_command_line_args();
+  main_function({args.begin() + 1, args.end()});
+  return environment::exit_code();
+}
+
 int startup::run(void (*main_function)(const string_vector&), int argc, char* argv[]) {
   main_function({argv + 1, argv + argc});
   return environment::exit_code();
@@ -84,12 +169,34 @@ int startup::run(void (*main_function)(const string_vector&)) {
   return environment::exit_code();
 }
 
+int startup::run(xtd::delegate<int()> main_function, int argc, char* argv[]) {
+  return main_function();
+}
+
+int startup::run(xtd::delegate<int()> main_function) {
+  return main_function();
+}
+
 int startup::run(int (*main_function)(), int, char* []) {
   return main_function();
 }
 
 int startup::run(int (*main_function)()) {
   return main_function();
+}
+
+int startup::run(xtd::delegate<int(int, char* [])> main_function, int argc, char* argv[]) {
+  return main_function(argc, argv);
+}
+
+int startup::run(xtd::delegate<int(int, char* [])> main_function) {
+  auto args = environment::get_command_line_args();
+  char** argv = new char*[args.size()];
+  for (auto index = 0ul; index < args.size(); ++index)
+    argv[index] = args[index].data();
+  auto exit_code = main_function(as<int32>(args.size()), argv);
+  delete[] argv;
+  return exit_code;
 }
 
 int startup::run(int (*main_function)(int, char* []), int argc, char* argv[]) {
@@ -104,6 +211,15 @@ int startup::run(int (*main_function)(int, char* [])) {
   auto exit_code = main_function(as<int32>(args.size()), argv);
   delete[] argv;
   return exit_code;
+}
+
+int startup::run(xtd::delegate<int(const string_vector&)> main_function, int argc, char* argv[]) {
+  return main_function({argv + 1, argv + argc});
+}
+
+int startup::run(xtd::delegate<int(const string_vector&)> main_function) {
+  auto args = environment::get_command_line_args();
+  return main_function({args.begin() + 1, args.end()});
 }
 
 int startup::run(int (*main_function)(const string_vector&), int argc, char* argv[]) {
