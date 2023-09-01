ef1466db09d0c286c2af1a0196ef629a8f39d07b
Gammasoft <gammasoft71@gmail.com>
2023-09-01 13:48:20 +0200

    Add xtd::out_of_memory_exception exception

---
+++ src/xtd.core/include/xtd/out_of_memory_exception.h
@@ -0,0 +1,104 @@
+/// @file
+/// @brief Contains xtd::out_of_memory_exception exception.
+/// @copyright Copyright (c) 2023 Gammasoft. All rights reserved.
+#pragma once
+#include "system_exception.h"
+
+/// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
+namespace xtd {
+  /// @brief The exception that is thrown when one of the arguments provided to a method is not valid.
+  /// @par Header
+  /// @code #include <xtd/out_of_memory_exception> @endcode
+  /// @par Namespace
+  /// xtd
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core exceptions
+  /// @par Examples
+  /// The following example demonstrates how to throw and catch an out_of_memory_exception.
+  /// @include out_of_memory_exception.cpp
+  /// @todo add param_name property and constructors param (param_name is the parameter that cause the exception).
+  class out_of_memory_exception : public system_exception {
+  public:
+    /// @name Constructors
+    
+    /// @{
+    /// @brief Create a new instance of class out_of_memory_exception
+    /// @param information (optional) Contains current information about member name, file path and  line number in the file where the exception is occurred. Typically #current_stack_frame_.
+    /// @remarks Message is set with the default message associate to the exception.
+    explicit out_of_memory_exception(const xtd::diagnostics::stack_frame& info = xtd::diagnostics::stack_frame::empty()) : system_exception(default_message(), info) {}
+    /// @brief Create a new instance of class out_of_memory_exception
+    /// @param message Message string associate to the exception.
+    /// @param information (optional) Contains current information about member name, file path and  line number in the file where the exception is occurred. Typically #current_stack_frame_.
+    explicit out_of_memory_exception(const xtd::ustring& message, const xtd::diagnostics::stack_frame& info = xtd::diagnostics::stack_frame::empty()) : system_exception(message, info) {}
+    /// @brief Create a new instance of class out_of_memory_exception
+    /// @param message Message string associate to the exception.
+    /// @param error Error code associate to the exception.
+    /// @param information (optional) Contains current information about member name, file path and  line number in the file where the exception is occurred. Typically #current_stack_frame_.
+    explicit out_of_memory_exception(const xtd::ustring& message, const std::error_code& error, const xtd::diagnostics::stack_frame& info = xtd::diagnostics::stack_frame::empty()) : system_exception(message, error, info) {}
+    /// @brief Create a new instance of class out_of_memory_exception
+    /// @param message Message string associate to the exception.
+    /// @param help_link Help link string associate to the exception.
+    /// @param information (optional) Contains current information about member name, file path and  line number in the file where the exception is occurred. Typically #current_stack_frame_.
+    explicit out_of_memory_exception(const xtd::ustring& message, const xtd::ustring& help_link, const xtd::diagnostics::stack_frame& info = xtd::diagnostics::stack_frame::empty()) : system_exception(message, help_link, info) {}
+    /// @brief Create a new instance of class out_of_memory_exception
+    /// @param message Message string associate to the exception.
+    /// @param error Error code associate to the exception.
+    /// @param help_link Help link string associate to the exception.
+    explicit out_of_memory_exception(const xtd::ustring& message, const std::error_code& error, const xtd::ustring& help_link, const xtd::diagnostics::stack_frame& info = xtd::diagnostics::stack_frame::empty()) : system_exception(message, error, help_link, info) {}
+    /// @brief Create a new instance of class out_of_memory_exception
+    /// @param inner_exception The exception that is the cause of the current exception.
+    /// @param information (optional) Contains current information about member name, file path and  line number in the file where the exception is occurred. Typically #current_stack_frame_.
+    /// @remarks Message is set with the default message associate to the exception.
+    explicit out_of_memory_exception(const std::exception& inner_exception, const xtd::diagnostics::stack_frame& info = xtd::diagnostics::stack_frame::empty()) : system_exception(default_message(), inner_exception, info) {}
+    /// @brief Create a new instance of class out_of_memory_exception
+    /// @param message Message string associate to the exception.
+    /// @param inner_exception The exception that is the cause of the current exception.
+    /// @param information (optional) Contains current information about member name, file path and  line number in the file where the exception is occurred. Typically #current_stack_frame_.
+    explicit out_of_memory_exception(const xtd::ustring& message, const std::exception& inner_exception, const xtd::diagnostics::stack_frame& info = xtd::diagnostics::stack_frame::empty()) : system_exception(message, inner_exception, info) {}
+    /// @brief Create a new instance of class out_of_memory_exception
+    /// @param message Message string associate to the exception.
+    /// @param inner_exception The exception that is the cause of the current exception.
+    /// @param error Error code associate to the exception.
+    /// @param information (optional) Contains current information about member name, file path and  line number in the file where the exception is occurred. Typically #current_stack_frame_.
+    explicit out_of_memory_exception(const xtd::ustring& message, const std::exception& inner_exception, const std::error_code& error, const xtd::diagnostics::stack_frame& info = xtd::diagnostics::stack_frame::empty()) : system_exception(message, inner_exception, error, info) {}
+    /// @brief Create a new instance of class out_of_memory_exception
+    /// @param message Message string associate to the exception.
+    /// @param inner_exception The exception that is the cause of the current exception.
+    /// @param help_link Help link string associate to the exception.
+    /// @param information (optional) Contains current information about member name, file path and  line number in the file where the exception is occurred. Typically #current_stack_frame_.
+    explicit out_of_memory_exception(const xtd::ustring& message, const std::exception& inner_exception, const xtd::ustring& help_link, const xtd::diagnostics::stack_frame& info = xtd::diagnostics::stack_frame::empty()) : system_exception(message, inner_exception, help_link, info) {}
+    /// @brief Create a new instance of class out_of_memory_exception
+    /// @param message Message string associate to the exception.
+    /// @param inner_exception The exception that is the cause of the current exception.
+    /// @param error Error code associate to the exception.
+    /// @param help_link Help link string associate to the exception.
+    /// @param information (optional) Contains current information about member name, file path and  line number in the file where the exception is occurred. Typically #current_stack_frame_.
+    explicit out_of_memory_exception(const xtd::ustring& message, const std::exception& inner_exception, const std::error_code& error, const xtd::ustring& help_link, const xtd::diagnostics::stack_frame& info = xtd::diagnostics::stack_frame::empty()) : system_exception(message, inner_exception, error, help_link, info) {}
+    /// @}
+    
+    /// @cond
+    out_of_memory_exception(const out_of_memory_exception&) = default;
+    out_of_memory_exception& operator =(const out_of_memory_exception&) = default;
+    /// @endcond
+    
+  private:
+    const char* default_message() const noexcept {return "Insufficient memory to continue the execution of the program"_t;}
+  };
+}
+
+/// @brief Helper on xtd::out_of_memory_exception to call it with current stack frame information.
+/// @par Header
+/// @code #include <xtd/out_of_memory_exception> @endcode
+/// @par Library
+/// xtd.core
+/// @ingroup xtd_core exceptions
+/// @remarks Is equivalent to xtd::out_of_memory_exception({any argument}, csf_)
+/// @code
+/// void my_func() {
+///   if (invalid_info) throw out_of_memory_exception_(); // same as : throw out_of_memory_exception(csf_);
+///   if (invalid_value) throw out_of_memory_exception_("Bad value"); // same as : throw out_of_memory_exception("Bad value", csf_);
+///   ...
+/// }
+/// @endcode
+#define out_of_memory_exception_(...) out_of_memory_exception(add_csf_(__VA_ARGS__))
