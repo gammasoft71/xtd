caf87dce958127766290000fe67346b0db9fdb35
Gammasoft <gammasoft71@gmail.com>
2023-09-01 13:50:53 +0200

    Add thread_pool unit tests

--- tests/xtd.core.unit_tests/src/xtd/threading/tests/thread_pool_tests.cpp
+++ tests/xtd.core.unit_tests/src/xtd/threading/tests/thread_pool_tests.cpp
@@ -1,5 +1,9 @@
-#include <xtd/threading/thread_pool.h>
-#include <xtd/xtd.tunit>
+#include <xtd/threading/thread_pool>
+#include <xtd/threading/interlocked>
+#include <xtd/tunit/assert>
+#include <xtd/tunit/string_assert>
+#include <xtd/tunit/test_class_attribute>
+#include <xtd/tunit/test_method_attribute>
 
 using namespace xtd::threading;
 using namespace xtd::tunit;
@@ -8,19 +12,83 @@ namespace xtd::tests {
   class test_class_(thread_pool_tests) {
   public:
     void test_method_(get_max_threads) {
-      size_t worker_threads = 0;
-      size_t completion_port_threads = 0;
-      thread_pool::get_max_threads(worker_threads, completion_port_threads);
-      assert::are_equal(800ul, worker_threads, csf_);
-      assert::are_equal(800ul, completion_port_threads, csf_);
+      size_t max_worker_threads = 0;
+      size_t max_completion_port_threads = 0;
+      thread_pool::get_max_threads(max_worker_threads, max_completion_port_threads);
+      assert::are_equal(800ul, max_worker_threads, csf_);
+      assert::are_equal(800ul, max_completion_port_threads, csf_);
     }
     
     void test_method_(get_min_threads) {
-      size_t worker_threads = 0;
-      size_t completion_port_threads = 0;
-      thread_pool::get_min_threads(worker_threads, completion_port_threads);
-      assert::are_equal(environment::processor_count(), worker_threads, csf_);
-      assert::are_equal(environment::processor_count(), completion_port_threads, csf_);
+      size_t min_worker_threads = 0;
+      size_t min_completion_port_threads = 0;
+      thread_pool::get_min_threads(min_worker_threads, min_completion_port_threads);
+      assert::are_equal(environment::processor_count(), min_worker_threads, csf_);
+      assert::are_equal(environment::processor_count(), min_completion_port_threads, csf_);
+    }
+    
+    void test_method_(queue_user_work_item_one) {
+      auto count = 0;
+      auto action = wait_callback {[&] {
+        interlocked::increment(count);
+      }};
+      thread_pool::queue_user_work_item(action);
+      thread_pool::close();
+      assert::are_equal(1, count, csf_);
+    }
+    
+    void test_method_(queue_user_work_item_after_close) {
+      auto count = 0;
+      auto action = wait_callback {[&] {
+        interlocked::increment(count);
+      }};
+      thread_pool::queue_user_work_item(action);
+      thread_pool::close();
+      thread_pool::queue_user_work_item(action);
+      thread_pool::close();
+      assert::are_equal(2, count, csf_);
+    }
+
+    void test_method_(queue_user_work_item_min_thread) {
+      size_t min_worker_threads = 0;
+      size_t min_completion_port_threads = 0;
+      thread_pool::get_min_threads(min_worker_threads, min_completion_port_threads);
+      auto count = 0;
+      auto action = wait_callback {[&] {
+        interlocked::increment(count);
+      }};
+      for (auto index = 0ul; index < min_worker_threads; ++index)
+        thread_pool::queue_user_work_item(action);
+      thread_pool::close();
+      assert::are_equal(min_worker_threads, count, csf_);
+    }
+    
+    void test_method_(queue_user_work_item_max_thread) {
+      size_t max_worker_threads = 0;
+      size_t max_completion_port_threads = 0;
+      thread_pool::get_max_threads(max_worker_threads, max_completion_port_threads);
+      auto count = 0;
+      auto action = wait_callback {[&] {
+        interlocked::increment(count);
+      }};
+      for (auto index = 0ul; index < max_worker_threads; ++index)
+        thread_pool::queue_user_work_item(action);
+      thread_pool::close();
+      assert::are_equal(max_worker_threads, count, csf_);
+    }
+    
+    void test_method_(queue_user_work_item_twice__max_thread) {
+      size_t max_worker_threads = 0;
+      size_t max_completion_port_threads = 0;
+      thread_pool::get_max_threads(max_worker_threads, max_completion_port_threads);
+      auto count = 0;
+      auto action = wait_callback {[&] {
+        interlocked::increment(count);
+      }};
+      for (auto index = 0ul; index < 2 * max_worker_threads; ++index)
+        thread_pool::queue_user_work_item(action);
+      thread_pool::close();
+      assert::are_equal(2 * max_worker_threads, count, csf_);
     }
   };
 }
