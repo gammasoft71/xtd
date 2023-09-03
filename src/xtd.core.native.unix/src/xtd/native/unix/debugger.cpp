bdb93f55882d9df49229d90857a32fe426621dbe
Gammasoft <gammasoft71@gmail.com>
2023-09-03 09:47:20 +0200

    Implement xtd::native::debugger::is_attached method on linux

--- src/xtd.core.native.unix/src/xtd/native/unix/debugger.cpp
+++ src/xtd.core.native.unix/src/xtd/native/unix/debugger.cpp
@@ -5,17 +5,36 @@
 #undef __XTD_CORE_NATIVE_LIBRARY__
 #include <iostream>
 #include <cstdlib>
+#include <fstream>
+#include <sstream>
 #include <syslog.h>
+#include <unistd.h>
 #include <gtk/gtk.h>
 
 using namespace xtd::native;
+
+namespace {
+  std::string get_process_name(pid_t pid) {
+    std::ifstream stat_file("/proc/" + std::to_string(pid) + "/status");
+    if (!stat_file) return "Unknown";
+    
+    std::string line;
+    getline(stat_file, line);
+    size_t start = line.find("Name:\t") + 1;
+    
+    if (start == std::string::npos) return "Unknown";
+    start += std::string {"Name:\t"}.size() - 1;
+    return line.substr(start, line.size() - start);
+  }
+}
 
 void debugger::debug_break() {
   std::abort();
 }
 
 bool debugger::is_attached() {
-  return false;
+  auto parent_process_name = get_process_name(getppid());
+  return parent_process_name == "gdb" || parent_process_name == "lldb";
 }
 
 bool debugger::is_logging() {
