2344c3788e36f0428b1a76a51c5e0fa4c7b54bed
Gammasoft <gammasoft71@gmail.com>
2023-09-05 22:19:08 +0200

    Fix documentation

--- src/xtd.core/include/xtd/threading/timer.h
+++ src/xtd.core/include/xtd/threading/timer.h
@@ -11,7 +11,7 @@
 
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
-  /// @brief The xtd::threading namespace provides classes and interfaces that enable multithreaded programming. In addition to classes for synchronizing timer activities and access to data ( xtd::threading::mutex, xtd::threading::monitor, xtd::threading::interlocked, xtd::threading::auto_reset_event, and so on), this namespace includes a xtd::threading::timer class that allows you to use a pool of system-supplied threads, and a xtd::threading::timer class that executes callback methods on timer pool threads.
+  /// @brief The xtd::threading namespace provides classes and interfaces that enable multithreaded programming. In addition to classes for synchronizing thread activities and access to data ( xtd::threading::mutex, xtd::threading::monitor, xtd::threading::interlocked, xtd::threading::auto_reset_event, and so on), this namespace includes a xtd::threading::thread_pool class that allows you to use a pool of system-supplied threads, and a xtd::threading::timer class that executes callback methods on thread pool threads.
   namespace threading {
     /// @brief Provides a mechanism for executing a method on a thread pool thread at specified intervals. This class cannot be inherited.
     /// @code
