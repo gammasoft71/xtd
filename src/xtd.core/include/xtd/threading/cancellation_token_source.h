a81a1318d9ca4e6b0a0b9146b8873990c1833acb
Gammasoft <gammasoft71@gmail.com>
2023-09-05 22:19:08 +0200

    Fix documentation

--- src/xtd.core/include/xtd/threading/cancellation_token_source.h
+++ src/xtd.core/include/xtd/threading/cancellation_token_source.h
@@ -10,7 +10,7 @@
 
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
-  /// @brief The xtd::threading namespace provides classes and interfaces that enable multithreaded programming. In addition to classes for synchronizing cancellation_token_source activities and access to data ( xtd::threading::mutex, xtd::threading::monitor, xtd::threading::interlocked, xtd::threading::auto_reset_event, and so on), this namespace includes a xtd::threading::cancellation_token_source class that allows you to use a pool of system-supplied threads, and a xtd::threading::cancellation_token_source class that executes callback methods on cancellation_token_source pool threads.
+  /// @brief The xtd::threading namespace provides classes and interfaces that enable multithreaded programming. In addition to classes for synchronizing thread activities and access to data ( xtd::threading::mutex, xtd::threading::monitor, xtd::threading::interlocked, xtd::threading::auto_reset_event, and so on), this namespace includes a xtd::threading::thread_pool class that allows you to use a pool of system-supplied threads, and a xtd::threading::timer class that executes callback methods on thread pool threads.
   namespace threading {
     /// @brief Signals to a xtd::threading::cancellation_token that it should be canceled.
     /// @code
