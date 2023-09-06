f03d2d1e6f2ed66f755d5b433741f100f07ae983
gammasoft71 <gammasoft71@gmail.com>
2023-09-06 21:00:11 +0200

    update latest reference guide

---
+++ static/reference_guides/latest/html/search/classes_15.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['xml_5fsyntax_5fexception_0',['xml_syntax_exception',['../classxtd_1_1xml__syntax__exception.html',1,'xtd']]],
+  ['xtd_5flibrary_1',['xtd_library',['../classxtd_1_1environment_1_1xtd__library.html',1,'xtd::environment']]],
+  ['xtd_5fsystem_5fimages_2',['xtd_system_images',['../classxtd_1_1drawing_1_1xtd__system__images.html',1,'xtd::drawing']]]
+];
---
+++ static/reference_guides/latest/html/search/typedefs_1.js
@@ -0,0 +1,12 @@
+var searchData=
+[
+  ['base_0',['base',['../classxtd_1_1diagnostics_1_1trace__listener__collection.html#a1c61e52e18f676199d2d8281f5100852',1,'xtd::diagnostics::trace_listener_collection::base'],['../classxtd_1_1forms_1_1control_1_1control__collection.html#a3432afd414ed8993b3aa7faac64f1939',1,'xtd::forms::control::control_collection::base'],['../classxtd_1_1forms_1_1link__label_1_1link__collection.html#a4111600c4dbdf4cf335c74d09c9993bd',1,'xtd::forms::link_label::link_collection::base'],['../classxtd_1_1forms_1_1tab__control_1_1tab__page__collection.html#abe519db7f4ca3ac7f6bcc0b6d8678759',1,'xtd::forms::tab_control::tab_page_collection::base']]],
+  ['black_5fcolors_1',['black_colors',['../group__drawing.html#gab80da8035442ffdeab6f6ebe0a2b3a59',1,'xtd::drawing']]],
+  ['bool_5f_2',['bool_',['../group__types.html#ga092071ef1d950e637cea6709a6017fd0',1,'xtd']]],
+  ['boolean_5fobject_3',['boolean_object',['../group__system.html#ga020fbeceaf011505de3ac30168375664',1,'xtd']]],
+  ['border_5fwidth_4',['border_width',['../group__style__sheets.html#gabaac5179e059b488f618f7af1189d3da',1,'xtd::forms::style_sheets']]],
+  ['button_5',['button',['../group__style__sheets.html#ga20f9c51904cab0da365efa6c57b84908',1,'xtd::forms::style_sheets']]],
+  ['buttons_5ft_6',['buttons_t',['../classxtd_1_1forms_1_1style__sheets_1_1style__sheet.html#a67c95b5a9abd7b1f00614353edb36544',1,'xtd::forms::style_sheets::style_sheet']]],
+  ['byte_7',['byte',['../group__types.html#gaf8d0aa5786861bead085592c31c09849',1,'xtd']]],
+  ['byte_5fobject_8',['byte_object',['../group__types.html#ga94831211c5798071ff44bbe214d093ca',1,'xtd']]]
+];
