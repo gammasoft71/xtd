a7fa465c729e13d33116757cc28696eefa688514
Gammasoft <gammasoft71@gmail.com>
2023-09-09 16:25:25 +0200

    xtd::threading::monitor class implementation (WIP)

--- src/xtd.core/src/xtd/threading/monitor.cpp
+++ src/xtd.core/src/xtd/threading/monitor.cpp
@@ -3,6 +3,7 @@
 #include "../../../include/xtd/threading/mutex.h"
 #include "../../../include/xtd/threading/thread.h"
 #include "../../../include/xtd/threading/synchronization_lock_exception.h"
+#include "../../../include/xtd/not_implemented_exception.h"
 #define __XTD_CORE_NATIVE_LIBRARY__
 #include <xtd/native/critical_section.h>
 #undef __XTD_CORE_NATIVE_LIBRARY__
@@ -13,14 +14,14 @@
 using namespace xtd;
 using namespace xtd::threading;
 
-class monitor::critical_sction {
+class monitor::critical_section {
 public:
-  critical_sction() : handle_(std::make_shared<intptr>(native::critical_section::create())) {}
+  critical_section() : handle_(std::make_shared<intptr>(native::critical_section::create())) {}
   
-  critical_sction(const critical_sction&) = default;
-  critical_sction& operator =(const critical_sction&) = default;
-  critical_sction(critical_sction&&) = default;
-  ~critical_sction() {if (handle_.use_count() == 1) native::critical_section::destroy(*handle_);}
+  critical_section(const critical_section&) = default;
+  critical_section& operator =(const critical_section&) = default;
+  critical_section(critical_section&&) = default;
+  ~critical_section() {if (handle_.use_count() == 1) native::critical_section::destroy(*handle_);}
 
   void enter() const noexcept {native::critical_section::enter(*handle_);}
   
@@ -46,16 +47,16 @@ private:
   std::shared_ptr<intptr> handle_;
 };
 
-struct monitor::item {
-  critical_sction event;
+struct monitor::monitor_item {
+  critical_section event;
   int32 used_counter {0};
   std::optional<ustring> name;
   intptr thread_id {thread::invalid_thread_id};
 };
 
 struct monitor::static_data {
-  std::recursive_mutex monitor_mutex;
-  monitor::item_collection items;
+  critical_section monitor_items_sync;
+  monitor::monitor_item_collection monitor_items;
 };
 
 void monitor::enter_ptr(ptr_item ptr) {
@@ -69,82 +70,84 @@ void monitor::enter_ptr(ptr_item ptr, bool& lock_taken) {
 }
 
 void monitor::exit_ptr(ptr_item ptr) {
-  get_static_data().monitor_mutex.lock();
+  get_static_data().monitor_items_sync.enter();
   if (!is_entered_ptr(ptr)) {
-    get_static_data().monitor_mutex.unlock();
+    get_static_data().monitor_items_sync.leave();
     throw synchronization_lock_exception(csf_);
   }
   
-  item saved;
-  item* monitor_data = &get_static_data().items[ptr.first];
+  monitor_item saved;
+  monitor_item* monitor_data = &get_static_data().monitor_items[ptr.first];
   if (interlocked::decrement(monitor_data->used_counter) == 0) {
-    saved = get_static_data().items[ptr.first];
+    saved = get_static_data().monitor_items[ptr.first];
     if (ptr.second) delete reinterpret_cast<const ustring*>(ptr.first);
-    get_static_data().items.erase(ptr.first);
+    get_static_data().monitor_items.erase(ptr.first);
     monitor_data = &saved;
   }
   monitor_data->thread_id = thread::invalid_thread_id;
   monitor_data->event.leave();
-  get_static_data().monitor_mutex.unlock();
+  get_static_data().monitor_items_sync.leave();
 }
 
 intptr monitor::get_ustring_ptr(const ustring& str) {
   if (str.empty()) throw argument_exception {csf_};
-  get_static_data().monitor_mutex.lock();
+  get_static_data().monitor_items_sync.enter();
   intptr ptr = reinterpret_cast<intptr>(&str);
-  for (const auto& item : get_static_data().items)
+  for (const auto& item : get_static_data().monitor_items)
     if (item.second.name.has_value() && item.second.name.value() == str) {
       ptr = item.first;
       delete &str;
       break;
     }
-  get_static_data().monitor_mutex.unlock();
+  get_static_data().monitor_items_sync.leave();
   return ptr;
 }
 
 bool monitor::is_entered_ptr(ptr_item ptr) noexcept {
-  auto found = get_static_data().items.find(ptr.first) != get_static_data().items.end();
+  auto found = get_static_data().monitor_items.find(ptr.first) != get_static_data().monitor_items.end();
   return found;
 }
 
 void monitor::pulse_ptr(ptr_item ptr) {
-  item* monitor_item = nullptr;
-  get_static_data().monitor_mutex.lock();
-  if (is_entered_ptr(ptr)) monitor_item = &get_static_data().items[ptr.first];
-  get_static_data().monitor_mutex.unlock();
+  monitor_item* monitor_item = nullptr;
+  get_static_data().monitor_items_sync.enter();
+  if (is_entered_ptr(ptr)) monitor_item = &get_static_data().monitor_items[ptr.first];
+  get_static_data().monitor_items_sync.leave();
   
   if (monitor_item == nullptr) throw invalid_operation_exception(csf_);
   if (monitor_item->thread_id != thread::current_thread().thread_id()) throw synchronization_lock_exception {csf_};
 
   // make pulse...
+  throw not_implemented_exception {csf_};
 }
 
 void monitor::pulse_all_ptr(ptr_item ptr) {
-  item* monitor_item = nullptr;
-  get_static_data().monitor_mutex.lock();
-  if (is_entered_ptr(ptr)) monitor_item = &get_static_data().items[ptr.first];
-  get_static_data().monitor_mutex.unlock();
+  monitor_item* monitor_item = nullptr;
+  get_static_data().monitor_items_sync.enter();
+  if (is_entered_ptr(ptr)) monitor_item = &get_static_data().monitor_items[ptr.first];
+  get_static_data().monitor_items_sync.leave();
   
   if (monitor_item == nullptr) throw invalid_operation_exception(csf_);
   if (monitor_item->thread_id != thread::current_thread().thread_id()) throw synchronization_lock_exception {csf_};
   
   // make pulse...
+  throw not_implemented_exception {csf_};
 }
 
 bool monitor::try_enter_ptr(ptr_item ptr, int32 milliseconds_timeout, bool& lock_taken) noexcept {
   if (milliseconds_timeout < timeout::infinite) return false;
-  get_static_data().monitor_mutex.lock();
+  get_static_data().monitor_items_sync.enter();
   if (!is_entered_ptr(ptr)) {
-    auto i = item {};
+    auto i = monitor_item {};
     if (ptr.second) {
       auto str = reinterpret_cast<const ustring*>(ptr.first);
       i.name = *str;
     }
-    get_static_data().items.insert({ptr.first, i});
+    get_static_data().monitor_items.insert({ptr.first, i});
   }
-  item* monitor_data = &get_static_data().items[ptr.first];
+  monitor_item* monitor_data = &get_static_data().monitor_items[ptr.first];
   interlocked::increment(monitor_data->used_counter);
-  get_static_data().monitor_mutex.unlock();
+  get_static_data().monitor_items_sync.leave();
   lock_taken = monitor_data->event.try_enter(milliseconds_timeout);
   if (lock_taken) monitor_data->thread_id = thread::current_thread().thread_id();
   return lock_taken;
