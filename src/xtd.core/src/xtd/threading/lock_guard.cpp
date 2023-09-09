eb7a20972e5c6f195e747b5f2dc3c191e8f04585
Gammasoft <gammasoft71@gmail.com>
2023-09-09 22:18:54 +0200

    Add wait methods

--- src/xtd.core/src/xtd/threading/lock_guard.cpp
+++ src/xtd.core/src/xtd/threading/lock_guard.cpp
@@ -14,3 +14,15 @@ void lock_guard::pulse() {
 void lock_guard::pulse_all() {
   monitor::pulse_all_ptr(lock_);
 }
+
+bool lock_guard::wait(int32 milliseconds_timeout) {
+  return monitor::wait_ptr(lock_, milliseconds_timeout);
+}
+
+bool lock_guard::wait(const time_span& timeout) {
+  return monitor::wait_ptr(lock_, as<int32>(timeout.total_milliseconds()));
+}
+
+bool lock_guard::wait() {
+  return monitor::wait_ptr(lock_, timeout::infinite);
+}
