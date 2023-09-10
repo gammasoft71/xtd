b8f9f9aa2986de267fc094e07245fd0b0fb0b191
gammasoft71 <gammasoft71@gmail.com>
2023-09-11 00:11:28 +0200

    Update latest reference guide

--- static/reference_guides/latest/html/search/defines_11.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['xml_5fsyntax_5fexception_5f_0',['xml_syntax_exception_',['../xml__syntax__exception_8h.html#aa3dcab114f653d1058d059727a99a2ce',1,'xml_syntax_exception.h']]]
-];
