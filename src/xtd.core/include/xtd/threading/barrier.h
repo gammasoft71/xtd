7b7c49ab0fb79b8f32a5ff8ca065d185b0bd1d08
Gammasoft <gammasoft71@gmail.com>
2023-09-11 20:28:13 +0200

    Add documentation

--- src/xtd.core/include/xtd/threading/barrier.h
+++ src/xtd.core/include/xtd/threading/barrier.h
@@ -22,6 +22,7 @@ namespace xtd {
     /// @ingroup xtd_core threading
     /// @par Examples
     /// @include barrier.cpp
+    /// @remarks A group of tasks cooperate by moving through a series of phases, where each in the group signals it has arrived at the xtd::threading::barrier in a given phase and implicitly waits for all others to arrive. The same xtd::threading::barrier can be used for multiple phases.
     class core_export_ barrier : public object {
       struct data;
     public:
@@ -34,7 +35,15 @@ namespace xtd {
       /// @name Constructors
       
       /// @{
+      /// @brief Initializes a new instance of the xtd::threading::barrier class.
+      /// @param participant_count The number of participating threads.
+      /// @exception xtd::argument_out_of_range_exception participant_count is less than 0 or greater than 32,767.
       explicit barrier(int32 participant_count);
+      /// @brief Initializes a new instance of the xtd::threading::barrier class.
+      /// @param participant_count The number of participating threads.
+      /// @param post_phase_action The xtd::action< barrier& > to be executed after each phase. Empty may be passed to indicate no action is taken.
+      /// @exception xtd::argument_out_of_range_exception participant_count is less than 0 or greater than 32,767.
+      /// @remarks The post_phase_action delegate will be executed after all participants have arrived at the barrier in one phase. The participants will not be released to the next phase until the post_phase_action delegate has completed execution.
       barrier(int32 participant_count, barrier::post_phase_action post_phase_action);
       /// @}
 
@@ -45,29 +54,61 @@ namespace xtd {
       barrier(barrier&&) = default;
       barrier(const barrier&) = default;
       barrier& operator =(const barrier& other);
+      ~barrier();
       ///@endcond
 
       /// @name Properties
       
       /// @{
-      int32 current_phase_number() const noexcept;
+      /// @brief Gets the number of the barrier's current phase.
+      /// @return Returns the number of the barrier's current phase.
+      /// @exception xtd::object_closed_exception The current instance has already been disposed.
+      int32 current_phase_number() const;
 
-      int32 participant_count() const noexcept;
+      /// @brief Gets the total number of participants in the barrier.
+      /// @return Returns the total number of participants in the barrier.
+      /// @exception xtd::object_closed_exception The current instance has already been disposed.
+      int32 participant_count() const;
 
-      int32 participants_remaining() const noexcept;
+      /// @brief Gets the number of participants in the barrier that haven't yet signaled in the current phase.
+      /// @return Returns the number of participants in the barrier that haven't yet signaled in the current phase.
+      /// @exception xtd::object_closed_exception The current instance has already been disposed.
+      /// @remarks This could be 0 during a post-phase action delegate execution or if the participant_count is 0.
+      int32 participants_remaining() const;
       /// @}
 
       /// @name Methods
       
       /// @{
+      /// @brief Notifies the xtd::threading::barrier that there will be an additional participant.
+      /// @return The phase number of the barrier in which the new participants will first participate.
+      /// @exception xtd::object_closed_exception The current instance has already been disposed.
+      /// @exception xtd::argument_out_of_range_exception Adding a participant would cause the barrier's participant count to exceed 32,767.
+      /// @remarks If the barrier is currently executing a post phase action, this call is blocked until the post phase action completes and the barrier has moved on to the next phase.
       int32 add_participant();
 
+      /// @brief Notifies the xtd::threading::barrier that there will be additional participants.
+      /// @param participant_count The number of additional participants to add to the barrier.
+      /// @return The phase number of the barrier in which the new participants will first participate.
+      /// @exception xtd::object_closed_exception The current instance has already been disposed.
+      /// @exception xtd::argument_out_of_range_exception participant_count is less than 0 <br>-or-<br> Adding participant_count participants would cause the barrier's participant count to exceed 32,767..
+      /// @remarks If the barrier is currently executing a post phase action, this call is blocked until the post phase action completes and the barrier has moved on to the next phase.
       int32 add_participants(int32 participant_count);
       
+      /// @brief Close the current instance of the xtd::threading::barrier  class.
       void close();
 
+      /// @brief Notifies the Barrier that there will be one less participant.
+      /// @return The phase number of the barrier in which the new participants will first participate.
+      /// @exception xtd::object_closed_exception The current instance has already been disposed.
+      /// @exception xtd::argument_out_of_range_exception The barrier already has 0 participants.
       int32 remove_participant();
       
+      /// @brief Notifies the Barrier that there will be fewer participants.
+      /// @param participant_count The number of additional participants to remove from the barrier.
+      /// @return The phase number of the barrier in which the new participants will first participate.
+      /// @exception xtd::object_closed_exception The current instance has already been disposed.
+      /// @exception xtd::argument_out_of_range_exception participant_count is less than 0 <br>-or-<br> The total participant count is less than the specified participant_count
       int32 remove_participants(int32 participant_count);
       
       void signal_and_wait();
--- src/xtd.core/src/xtd/threading/barrier.cpp
+++ src/xtd.core/src/xtd/threading/barrier.cpp
@@ -5,6 +5,7 @@
 #include "../../../include/xtd/as.h"
 #include "../../../include/xtd/int16_object.h"
 #include "../../../include/xtd/invalid_operation_exception.h"
+#include "../../../include/xtd/object_closed_exception.h"
 #include "../../../include/xtd/lock.h"
 
 using namespace xtd;
@@ -28,7 +29,7 @@ barrier::barrier(int32 participant_count) : barrier(participant_count, {}) {
 }
 
 barrier::barrier(int32 participant_count, barrier::post_phase_action post_phase_action) : data_(std::make_shared<data>()) {
-  if (participant_count < 0) throw argument_out_of_range_exception {csf_};
+  if (participant_count < 0 || participant_count > int16_object::max_value) throw argument_out_of_range_exception {csf_};
   data_->participant_count = participant_count;
   data_->participants_remaining = participant_count;
   data_->post_phase_action = post_phase_action;
@@ -40,15 +41,22 @@ barrier& barrier::operator =(const barrier& other) {
   return *this;
 }
 
-int32 barrier::current_phase_number() const noexcept {
+barrier::~barrier() {
+  if (data_.use_count() == 1) close();
+}
+
+int32 barrier::current_phase_number() const {
+  if (!data_) throw object_closed_exception {csf_};
   return data_->current_phase_number;
 }
 
-int32 barrier::participant_count() const noexcept {
+int32 barrier::participant_count() const {
+  if (!data_) throw object_closed_exception {csf_};
   return data_->participant_count;
 }
 
-int32 barrier::participants_remaining() const noexcept {
+int32 barrier::participants_remaining() const {
+  if (!data_) throw object_closed_exception {csf_};
   return data_->participants_remaining;
 }
 
@@ -57,6 +65,7 @@ int32 barrier::add_participant() {
 }
 
 int32 barrier::add_participants(int32 participant_count) {
+  if (!data_) throw object_closed_exception {csf_};
   lock_(*data_) {
     if (participant_count < 0 || data_->participant_count + participant_count > int16_object::max_value) throw argument_out_of_range_exception {csf_};
     if (data_->run_post_phase_action) throw invalid_operation_exception {csf_};
@@ -68,8 +77,7 @@ int32 barrier::add_participants(int32 participant_count) {
 }
 
 void barrier::close() {
-  if (data_.use_count() == 1)
-    data_.reset();
+  data_.reset();
 }
 
 int32 barrier::remove_participant() {
@@ -77,6 +85,7 @@ int32 barrier::remove_participant() {
 }
 
 int32 barrier::remove_participants(int32 participant_count) {
+  if (!data_) throw object_closed_exception {csf_};
   lock_(*data_) {
     if (participant_count < 0) throw argument_out_of_range_exception {csf_};
     if (data_->participant_count == 0 || data_->run_post_phase_action || data_->participants_remaining < data_->participant_count - participant_count) throw invalid_operation_exception {csf_};
@@ -93,6 +102,7 @@ void barrier::signal_and_wait() {
 }
 
 bool barrier::signal_and_wait(int32 milliseconds_timeout) {
+  if (!data_) throw object_closed_exception {csf_};
   lock_(*data_) {
     data_->participants_remaining--;
     
