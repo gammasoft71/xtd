42a4f8c43a36546446ab756f75b49519a2dedd58
gammasoft71 <gammasoft71@gmail.com>
2023-09-11 22:10:43 +0200

    Update latest reference guide

--- static/reference_guides/latest/search/typedefs_e.js
+++ static/reference_guides/latest/search/typedefs_e.js
@@ -8,9 +8,10 @@ var searchData=
   ['parameterized_5fthread_5fstart_5',['parameterized_thread_start',['../group__xtd__core.html#ga05ac25649af478143826babb1a9fae7c',1,'xtd::threading']]],
   ['pointer_6',['pointer',['../classxtd_1_1forms_1_1layout_1_1arranged__element__collection.html#ac583e335292983f536a0f34b45052e59',1,'xtd::forms::layout::arranged_element_collection']]],
   ['points_5fcollection_7',['points_collection',['../classxtd_1_1forms_1_1dot__matrix__display.html#a8290530d8532857d7e620616c985891b',1,'xtd::forms::dot_matrix_display']]],
-  ['predicate_8',['predicate',['../group__xtd__core.html#gaa083bb9debdfafabdf55431467759471',1,'xtd']]],
-  ['program_5fexit_5fevent_5fhandler_9',['program_exit_event_handler',['../group__events.html#ga05028567f0783e489a1c6e338026b99d',1,'xtd']]],
-  ['progress_5fchanged_5fevent_5fhandler_10',['progress_changed_event_handler',['../group__events.html#gab622f963701c9e772309591048e9f180',1,'xtd::forms']]],
-  ['property_5fmap_11',['property_map',['../property__map_8h.html#a8f98379cbe916d9dcf3e0cfac9eeaae6',1,'xtd::web::css']]],
-  ['ptrdiff_12',['ptrdiff',['../group__types.html#gac047942a62f03ba75e1b4f0bde7401ca',1,'xtd']]]
+  ['post_5fphase_5faction_8',['post_phase_action',['../classxtd_1_1threading_1_1barrier.html#a541a2b302cb76a52f869eaded0e0f518',1,'xtd::threading::barrier']]],
+  ['predicate_9',['predicate',['../group__xtd__core.html#gaa083bb9debdfafabdf55431467759471',1,'xtd']]],
+  ['program_5fexit_5fevent_5fhandler_10',['program_exit_event_handler',['../group__events.html#ga05028567f0783e489a1c6e338026b99d',1,'xtd']]],
+  ['progress_5fchanged_5fevent_5fhandler_11',['progress_changed_event_handler',['../group__events.html#gab622f963701c9e772309591048e9f180',1,'xtd::forms']]],
+  ['property_5fmap_12',['property_map',['../property__map_8h.html#a8f98379cbe916d9dcf3e0cfac9eeaae6',1,'xtd::web::css']]],
+  ['ptrdiff_13',['ptrdiff',['../group__types.html#gac047942a62f03ba75e1b4f0bde7401ca',1,'xtd']]]
 ];
