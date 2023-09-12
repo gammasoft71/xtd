4523e84566ca84831fd497c6d27cadd477b4647b
Gammasoft <gammasoft71@gmail.com>
2023-09-12 20:30:50 +0200

    Add documentation

--- src/xtd.core/include/xtd/threading/barrier_post_phase_exception.h
+++ src/xtd.core/include/xtd/threading/barrier_post_phase_exception.h
@@ -9,6 +9,9 @@ namespace xtd {
   /// @brief The xtd::threading namespace provides classes and interfaces that enable multithreaded programming. In addition to classes for synchronizing thread activities and access to data ( xtd::threading::mutex, xtd::threading::monitor, xtd::threading::interlocked, xtd::threading::auto_reset_event, and so on), this namespace includes a xtd::threading::thread_pool class that allows you to use a pool of system-supplied threads, and a xtd::threading::timer class that executes callback methods on thread pool threads.
   namespace threading {
     /// @brief The exception that is thrown when a Thread is in an invalid ThreadState for the method call.
+    /// @code
+    /// class barrier_post_phase_exception : public xtd::system_exception
+    /// @endcode
     /// @par Header
     /// @code #include <xtd/threading/barrier_post_phase_exception> @endcode
     /// @par Namespace
