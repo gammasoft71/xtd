d915334bc5f81fb542cb1cc60059fac050cf7fd1
Gammasoft <gammasoft71@gmail.com>
2023-09-12 23:06:59 +0200

    Add documentation

--- src/xtd.core/include/xtd/threading/countdown_event.h
+++ src/xtd.core/include/xtd/threading/countdown_event.h
@@ -31,6 +31,10 @@ namespace xtd {
       /// @name Constructors
       
       /// @{
+      /// @brief Initializes a new instance of xtd::threading::countdown_event class with the specified count.
+      /// @param initial_count he number of signals initially required to set the xtd::threading::countdown_event.
+      /// @exception xtd::argument_out_of_range_exception initial_count is less than 0.
+      /// @remarks If initial_count is 0, the event is created in a signaled state.
       explicit countdown_event(int32 initial_count);
       /// @}
 
@@ -45,20 +49,44 @@ namespace xtd {
       /// @name Properties
       
       /// @{
+      /// @brief Gets the number of remaining signals required to set the event.
+      /// @return The number of remaining signals required to set the event.
+      /// @exception xtd::object_closed_exception The current instance has already been closed.
       int32 current_count() const;
 
+      /// @brief Gets the numbers of signals initially required to set the event.
+      /// @return The number of signals initially required to set the event.
+      /// @exception xtd::object_closed_exception The current instance has already been closed.
       int32 initial_count() const;
 
+      /// @brief ndicates whether the xtd::threading::countdown_event object's current count has reached zero.
+      /// @return true if the current count is zero; otherwise, false.
+      /// @exception xtd::object_closed_exception The current instance has already been closed.
+      /// @remarks The xtd::threading::countdown_event does not raise an event when the countdown has reached zero. Instead, the xtd::threading::countdown_event::current_count property equals zero, and the xtd::threading::countdown_event::is_set property equals true.
       bool is_set() const;
       
+      /// @brief Gets a xtd::threading::wait_handle that is used to wait for the event to be set.
+      /// @return A xtd::threading::wait_handle that is used to wait for the event to be set.
+      /// @exception xtd::object_closed_exception The current instance has already been closed.
       const xtd::threading::wait_handle& wait_handle() const;
+      /// @brief Gets a xtd::threading::wait_handle that is used to wait for the event to be set.
+      /// @return A xtd::threading::wait_handle that is used to wait for the event to be set.
+      /// @exception xtd::object_closed_exception The current instance has already been closed.
       xtd::threading::wait_handle& wait_handle();
       /// @}
 
       /// @name Methods
       
       /// @{
+      /// @brief Increments the xtd::threading::countdown_event's current count by one.
+      /// @exception xtd::object_closed_exception The current instance has already been closed.
+      /// @exception xtd::invalid_operation_exception The current instance is already set.
       void add_count();
+      /// @brief Increments the xtd::threading::countdown_event's current count by one.
+      /// @param count The value by which to increase xtd::threading::countdown_event::current_count.
+      /// @exception xtd::object_closed_exception The current instance has already been closed.
+      /// @exception xtd::invalid_operation_exception The current instance is already set.
+      /// @exception xtd::argument_out_of_range_exception count is less than 0.
       void add_count(int32 count);
       
       /// @brief Close the current instance of the xtd::threading::countdown_event  class.
