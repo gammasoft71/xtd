4b6ab4ae3c6ee3cc3d0f0e4d75965de480db8c73
Gammasoft <gammasoft71@gmail.com>
2023-09-12 20:30:50 +0200

    Add documentation

--- src/xtd.core/include/xtd/threading/semaphore_full_exception.h
+++ src/xtd.core/include/xtd/threading/semaphore_full_exception.h
@@ -9,6 +9,9 @@ namespace xtd {
   /// @brief The xtd::threading namespace provides classes and interfaces that enable multithreaded programming. In addition to classes for synchronizing thread activities and access to data ( xtd::threading::mutex, xtd::threading::monitor, xtd::threading::interlocked, xtd::threading::auto_reset_event, and so on), this namespace includes a xtd::threading::thread_pool class that allows you to use a pool of system-supplied threads, and a xtd::threading::timer class that executes callback methods on thread pool threads.
   namespace threading {
     /// @brief The exception that is thrown when a method call is invalid for the object's current state.
+    /// @code
+    /// class semaphore_full_exception : public xtd::system_exception
+    /// @endcode
     /// @par Header
     /// @code #include <xtd/threading/semaphore_full_exception> @endcode
     /// @par Namespace
--- src/xtd.core/include/xtd/threading/thread_interrupted_exception.h
+++ src/xtd.core/include/xtd/threading/thread_interrupted_exception.h
@@ -9,6 +9,11 @@ namespace xtd {
   /// @brief The xtd::threading namespace provides classes and interfaces that enable multithreaded programming. In addition to classes for synchronizing thread activities and access to data ( xtd::threading::mutex, xtd::threading::monitor, xtd::threading::interlocked, xtd::threading::auto_reset_event, and so on), this namespace includes a xtd::threading::thread_pool class that allows you to use a pool of system-supplied threads, and a xtd::threading::timer class that executes callback methods on thread pool threads.
   namespace threading {
     /// @brief The exception that is thrown when a Thread is interrupted while it is in a waiting state.
+    /// @code
+    /// class thread_abort_exception : public xtd::system_exception
+    /// @endcode
+    /// @par Header
+    /// @code #include <xtd/threading/thread_interrupted_exception> @endcode
     /// @par Namespace
     /// xtd::threading
     /// @par Library
