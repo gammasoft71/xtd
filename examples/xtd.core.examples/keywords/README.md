5531a0b23d650389b4dea855ae03a6685857bb34
Gammasoft <gammasoft71@gmail.com>
2023-09-13 21:51:10 +0200

    Add lock_guard_keyword example

--- examples/xtd.core.examples/keywords/README.md
+++ examples/xtd.core.examples/keywords/README.md
@@ -2,14 +2,15 @@
 
 [This folder](.) contains keywords examples used by [Reference Guide](https://gammasoft71.github.io/xtd/reference_guides/latest/) and more.
 
-* [block_scope](block_scope/README.md) shows how to use [xtd::block_scope](https://gammasoft71.github.io/xtd/reference_guides/latest/block__scope_8h.html) method.
-* [current_stack_frame](current_stack_frame/README.md) shows how to use [xtd::diagnostics::stack_frame](https://gammasoft71.github.io/xtd/reference_guides/latest/classxtd_1_1diagnostics_1_1stack__frame.html) method.
-* [csf](csf/README.md) shows how to use [xtd::diagnostics::stack_frame](https://gammasoft71.github.io/xtd/reference_guides/latest/classxtd_1_1diagnostics_1_1stack__frame.html) method.
-* [interface](interface/README.md) shows how to use [xtd::interface](https://gammasoft71.github.io/xtd/reference_guides/latest/classxtd_1_1interface.html) method.
+* [block_scope](block_scope/README.md) shows how to use [block_scope_](https://gammasoft71.github.io/xtd/reference_guides/latest/block__scope_8h.html) method.
+* [current_stack_frame](current_stack_frame/README.md) shows how to use [current_stack_frame_](https://gammasoft71.github.io/xtd/reference_guides/latest/classxtd_1_1diagnostics_1_1stack__frame.html) method.
+* [csf](csf/README.md) shows how to use [csf_](https://gammasoft71.github.io/xtd/reference_guides/latest/classxtd_1_1diagnostics_1_1stack__frame.html) method.
+* [interface](interface/README.md) shows how to use [interface_](https://gammasoft71.github.io/xtd/reference_guides/latest/classxtd_1_1interface.html) method.
 * [interface2](interface2/README.md) shows how to use [xtd::interface](https://gammasoft71.github.io/xtd/reference_guides/latest/classxtd_1_1interface.html) method.
-* [lock](lock/README.md) shows how to use [xtd::lock](https://gammasoft71.github.io/xtd/reference_guides/latest/group__keywords.html#gacd9906b29d877eb1eb0a8c7bc4ab774a) method.
-* [startup_keyword](startup_keyword/README.md) shows how to use [xtd::startup](https://gammasoft71.github.io/xtd/reference_guides/latest/startup_8h.html) method.
+* [lock](lock/README.md) shows how to use [lock_](https://gammasoft71.github.io/xtd/reference_guides/latest/group__keywords.html#gacd9906b29d877eb1eb0a8c7bc4ab774a) method.
+* [lock_guard_keyword](lock/README.md) shows how to use [lock_guard_](https://gammasoft71.github.io/xtd/reference_guides/latest/group__keywords.html#gacd9906b29d877eb1eb0a8c7bc4ab774a) method.
+* [startup_keyword](startup_keyword/README.md) shows how to use [startup_](https://gammasoft71.github.io/xtd/reference_guides/latest/startup_8h.html) method.
 * [static](static/README.md) shows how to use [static_](https://gammasoft71.github.io/xtd/reference_guides/latest/group__keywords.html#ga28796443ec37b938df7072c79595e3f6) keyword.
-* [static_object](static_object/README.md) shows how to use [xtd::static_object](https://gammasoft71.github.io/xtd/reference_guides/latest/classxtd_1_1static__object.html) keyword.
+* [static_object](static_object/README.md) shows how to use [static_object_](https://gammasoft71.github.io/xtd/reference_guides/latest/classxtd_1_1static__object.html) keyword.
 * [static_object2](static_object2/README.md) shows how to use [xtd::static_object](https://gammasoft71.github.io/xtd/reference_guides/latest/classxtd_1_1static__object.html) class.
-* [using](using/README.md) shows how to use [xtd::using](https://gammasoft71.github.io/xtd/reference_guides/latest/using_8h.html) method.
+* [using](using/README.md) shows how to use [using_](https://gammasoft71.github.io/xtd/reference_guides/latest/using_8h.html) method.
---
+++ examples/xtd.core.examples/keywords/lock_guard_keyword/src/lock_guard_keyword.cpp
@@ -0,0 +1,128 @@
+#include <xtd/threading/thread>
+#include <xtd/console>
+#include <xtd/lock>
+#include <xtd/random>
+#include <xtd/startup>
+
+using namespace xtd;
+using namespace xtd::threading;
+
+namespace examples {
+  class account : public object {
+  public:
+    explicit account(int initial) : balance(initial) {}
+    
+    int withdraw(int amount) {
+      // This condition never is true unless the lock statement is commented out.
+      if (balance < 0)
+        throw system_exception("Negative Balance", csf_);
+      
+      // Comment out the next line to see the effect of leaving out the lock keyword.
+      lock_guard_(balance) {
+        if (balance >= amount) {
+          console::write_line("Balance before Withdrawal :  {0}", balance);
+          console::write_line("Amount to Withdraw        : -{0}", amount);
+          balance = balance - amount;
+          console::write_line("Balance after Withdrawal  :  {0}", balance);
+          return amount;
+        }
+      }
+      return 0;
+    }
+    
+    void do_transactions() {
+      for (int i = 0; i < 100; i++)
+        withdraw(random.next(1, 100));
+    }
+    
+  private:
+    int balance;
+    xtd::random random;
+  };
+  
+  class program {
+  public:
+    static void main() {
+      std::vector<thread> threads(10);
+      account account(1000);
+      for (int i = 0; i < 10; i++) {
+        threads[i] = thread {thread_start {account, &account::do_transactions}};
+        threads[i].start();
+      }
+
+      for (auto& thread : threads)
+        thread.join();
+    }
+  };
+}
+
+startup_(examples::program::main);
+
+// This code can produce the following output:
+//
+// Balance before Withdrawal :  1000
+// Amount to Withdraw        : -75
+// Balance after Withdrawal  :  925
+// Balance before Withdrawal :  925
+// Amount to Withdraw        : -84
+// Balance after Withdrawal  :  841
+// Balance before Withdrawal :  841
+// Amount to Withdraw        : -69
+// Balance after Withdrawal  :  772
+// Balance before Withdrawal :  772
+// Amount to Withdraw        : -71
+// Balance after Withdrawal  :  701
+// Balance before Withdrawal :  701
+// Amount to Withdraw        : -84
+// Balance after Withdrawal  :  617
+// Balance before Withdrawal :  617
+// Amount to Withdraw        : -81
+// Balance after Withdrawal  :  536
+// Balance before Withdrawal :  536
+// Amount to Withdraw        : -15
+// Balance after Withdrawal  :  521
+// Balance before Withdrawal :  521
+// Amount to Withdraw        : -93
+// Balance after Withdrawal  :  428
+// Balance before Withdrawal :  428
+// Amount to Withdraw        : -6
+// Balance after Withdrawal  :  422
+// Balance before Withdrawal :  422
+// Amount to Withdraw        : -54
+// Balance after Withdrawal  :  368
+// Balance before Withdrawal :  368
+// Amount to Withdraw        : -52
+// Balance after Withdrawal  :  316
+// Balance before Withdrawal :  316
+// Amount to Withdraw        : -67
+// Balance after Withdrawal  :  249
+// Balance before Withdrawal :  249
+// Amount to Withdraw        : -93
+// Balance after Withdrawal  :  156
+// Balance before Withdrawal :  156
+// Amount to Withdraw        : -60
+// Balance after Withdrawal  :  96
+// Balance before Withdrawal :  96
+// Amount to Withdraw        : -7
+// Balance after Withdrawal  :  89
+// Balance before Withdrawal :  89
+// Amount to Withdraw        : -42
+// Balance after Withdrawal  :  47
+// Balance before Withdrawal :  47
+// Amount to Withdraw        : -1
+// Balance after Withdrawal  :  46
+// Balance before Withdrawal :  46
+// Amount to Withdraw        : -22
+// Balance after Withdrawal  :  24
+// Balance before Withdrawal :  24
+// Amount to Withdraw        : -18
+// Balance after Withdrawal  :  6
+// Balance before Withdrawal :  6
+// Amount to Withdraw        : -2
+// Balance after Withdrawal  :  4
+// Balance before Withdrawal :  4
+// Amount to Withdraw        : -2
+// Balance after Withdrawal  :  2
+// Balance before Withdrawal :  2
+// Amount to Withdraw        : -2
+// Balance after Withdrawal  :  0
