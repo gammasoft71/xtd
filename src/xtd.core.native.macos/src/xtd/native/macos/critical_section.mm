f95d2351703e6290a24c30f6e29669eba29e0d4a
Gammasoft <gammasoft71@gmail.com>
2023-09-14 13:19:39 +0200

    Review xtd::native::critical_section on macOS

--- src/xtd.core.native.macos/src/xtd/native/macos/critical_section.mm
+++ src/xtd.core.native.macos/src/xtd/native/macos/critical_section.mm
@@ -2,32 +2,37 @@
 #include <xtd/native/critical_section.h>
 #include "../../../../include/xtd/native/macos/mutex.h"
 #undef __XTD_CORE_NATIVE_LIBRARY__
-#include <mutex>
+#include <pthread.h>
 
 using namespace xtd::native;
 
 intmax_t critical_section::create() {
-  auto handle = new std::recursive_mutex {};
+  auto handle = new pthread_mutex_t;
   if (!handle) return reinterpret_cast<intmax_t>(MUTEX_FAILED);
+  pthread_mutexattr_t mutex_attribute;
+  pthread_mutexattr_init(&mutex_attribute);
+  pthread_mutexattr_settype(&mutex_attribute, PTHREAD_MUTEX_RECURSIVE);
+  pthread_mutex_init(handle, &mutex_attribute);
   return reinterpret_cast<intmax_t>(handle);
 }
 
 void critical_section::destroy(intmax_t handle) {
   if (handle == reinterpret_cast<intmax_t>(MUTEX_FAILED)) return;
-  delete reinterpret_cast<std::recursive_mutex*>(handle);
+  pthread_mutex_destroy(reinterpret_cast<pthread_mutex_t*>(handle));
+  delete reinterpret_cast<pthread_mutex_t*>(handle);
 }
 
 void critical_section::enter(intmax_t handle) {
   if (handle == reinterpret_cast<intmax_t>(MUTEX_FAILED)) return;
-  reinterpret_cast<std::recursive_mutex*>(handle)->lock();
+  pthread_mutex_lock(reinterpret_cast<pthread_mutex_t*>(handle));
 }
 
 bool critical_section::try_enter(intmax_t handle) {
   if (handle == reinterpret_cast<intmax_t>(MUTEX_FAILED)) return false;
-  return reinterpret_cast<std::recursive_mutex*>(handle)->try_lock();
+  return pthread_mutex_trylock(reinterpret_cast<pthread_mutex_t*>(handle)) == 0;
 }
 
 void critical_section::leave(intmax_t handle) {
   if (handle == reinterpret_cast<intmax_t>(MUTEX_FAILED)) return;
-  reinterpret_cast<std::recursive_mutex*>(handle)->unlock();
+  pthread_mutex_unlock(reinterpret_cast<pthread_mutex_t*>(handle));
 }
