edd01ccad1d93d19beb7fb97c5e9e1b8ff0e539c
gammasoft71 <gammasoft71@gmail.com>
2023-09-14 21:13:06 +0200

    Update latest reference guide

--- static/reference_guides/latest/html/search/defines_c.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['path_5ftoo_5flong_5fexception_5f_0',['path_too_long_exception_',['../path__too__long__exception_8h.html#a84da0cb06198965be324f411a01d9aa9',1,'path_too_long_exception.h']]],
-  ['platform_5fnot_5fsupported_5fexception_5f_1',['platform_not_supported_exception_',['../platform__not__supported__exception_8h.html#ac60da3ca311b8d2383ab62f7decb386c',1,'platform_not_supported_exception.h']]],
-  ['protocol_5fviolation_5fexception_5f_2',['protocol_violation_exception_',['../protocol__violation__exception_8h.html#a2af71ba4f10740d77b2128f4e7e92e8c',1,'protocol_violation_exception.h']]]
-];
