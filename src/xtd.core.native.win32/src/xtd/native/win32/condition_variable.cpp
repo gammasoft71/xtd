e7a123b5de75c18254fd9fe694f67742f4025b57
Gammasoft <gammasoft71@gmail.com>
2023-09-14 13:09:36 +0200

    xtd::native::condition_variable implementation (WIP)

--- src/xtd.core.native.win32/src/xtd/native/win32/condition_variable.cpp
+++ src/xtd.core.native.win32/src/xtd/native/win32/condition_variable.cpp
@@ -9,18 +9,29 @@
 using namespace xtd::native;
 
 intmax_t condition_variable::create() {
-  return reinterpret_cast<intmax_t>(nullptr);
+  PCONDITION_VARIABLE handle = new CONDITION_VARIABLE;
+  if (handle == nullptr) return reinterpret_cast<intmax_t>(INVALID_HANDLE_VALUE);
+  InitializeConditionVariable(handle);
+  return reinterpret_cast<intmax_t>(handle);
 }
 
 void condition_variable::destroy(intmax_t handle) {
+  if (handle == reinterpret_cast<intmax_t>(INVALID_HANDLE_VALUE)) return;
+  delete reinterpret_cast<PCONDITION_VARIABLE>(handle);
 }
 
 void condition_variable::pulse(intmax_t handle) {
+  if (handle == reinterpret_cast<intmax_t>(INVALID_HANDLE_VALUE)) return;
+  WakeConditionVariable(reinterpret_cast<PCONDITION_VARIABLE>(handle));
 }
 
 void condition_variable::pulse_all(intmax_t handle) {
+  if (handle == reinterpret_cast<intmax_t>(INVALID_HANDLE_VALUE)) return;
+  WakeAllConditionVariable(reinterpret_cast<PCONDITION_VARIABLE>(handle));
 }
 
 bool condition_variable::wait(intmax_t handle, intmax_t critical_section_handle, int_least32_t milliseconds_timeout) {
-  return false;
+  if (handle == reinterpret_cast<intmax_t>(INVALID_HANDLE_VALUE)) return false;
+  if (critical_section_handle == reinterpret_cast<intmax_t>(INVALID_HANDLE_VALUE)) return false;
+  return SleepConditionVariableCS(reinterpret_cast<PCONDITION_VARIABLE>(handle), reinterpret_cast<PCRITICAL_SECTION>(critical_section_handle), milliseconds_timeout) == TRUE;
 }
