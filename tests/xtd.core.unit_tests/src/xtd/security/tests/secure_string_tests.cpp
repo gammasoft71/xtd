b0e2f5125c2aeffa212a550d41a1beaafd20eecd
Gammasoft <gammasoft71@gmail.com>
2023-09-15 09:29:51 +0200

    Review include files

--- tests/xtd.core.unit_tests/src/xtd/security/tests/secure_string_tests.cpp
+++ tests/xtd.core.unit_tests/src/xtd/security/tests/secure_string_tests.cpp
@@ -1,7 +1,7 @@
-#include <xtd/security/secure_string.h>
-#include <xtd/tunit/assert.h>
-#include <xtd/tunit/test_class_attribute.h>
-#include <xtd/tunit/test_method_attribute.h>
+#include <xtd/security/secure_string>
+#include <xtd/tunit/assert>
+#include <xtd/tunit/test_class_attribute>
+#include <xtd/tunit/test_method_attribute>
 
 using namespace xtd::security;
 using namespace xtd::tunit;
