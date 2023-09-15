f675f3798a2ac7033aa355296af05b99d5dc7bda
Gammasoft <gammasoft71@gmail.com>
2023-09-15 09:30:32 +0200

    Review include files

--- tests/xtd.core.unit_tests/src/xtd/speech/synthesis/tests/synthesizer_state_tests.cpp
+++ tests/xtd.core.unit_tests/src/xtd/speech/synthesis/tests/synthesizer_state_tests.cpp
@@ -1,7 +1,7 @@
-#include <xtd/speech/synthesis/synthesizer_state.h>
-#include <xtd/tunit/assert.h>
-#include <xtd/tunit/test_class_attribute.h>
-#include <xtd/tunit/test_method_attribute.h>
+#include <xtd/speech/synthesis/synthesizer_state>
+#include <xtd/tunit/assert>
+#include <xtd/tunit/test_class_attribute>
+#include <xtd/tunit/test_method_attribute>
 
 using namespace xtd::speech::synthesis;
 using namespace xtd::tunit;
