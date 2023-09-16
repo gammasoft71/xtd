37c8f19956925d6e0ccc0afdafc63a827fbc8aa6
Gammasoft <gammasoft71@gmail.com>
2023-09-16 17:33:44 +0200

    Review code

--- src/xtd.core/src/xtd/net/sockets/network_stream.cpp
+++ src/xtd.core/src/xtd/net/sockets/network_stream.cpp
@@ -13,7 +13,7 @@ network_stream::socket_streambuf::~socket_streambuf() {
 }
 
 int32 network_stream::socket_streambuf::underflow() {
-  std::vector<xtd::byte> buffer(1);
+  auto buffer = std::vector<xtd::byte>(1);
   if (socket_.receive(buffer) == 1) {
     value_ = static_cast<char>(buffer[0]);
     setg(&value_, &value_, &value_ + 1);
@@ -24,7 +24,7 @@ int32 network_stream::socket_streambuf::underflow() {
 
 int32 network_stream::socket_streambuf::overflow(int32 value) {
   value_ = static_cast<char>(value);
-  std::vector<xtd::byte> buffer {static_cast<xtd::byte>(value_)};
+  auto buffer = std::vector<xtd::byte> {static_cast<xtd::byte>(value_)};
   if (socket_.send(buffer) == 1) {
     setp(&value_, &value_);
     return 0;
