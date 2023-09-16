1ceb45d7e9f279bf234269de36f9b30760620603
Gammasoft <gammasoft71@gmail.com>
2023-09-16 13:07:00 +0200

    Review code

--- src/xtd.core/src/xtd/io/directory_info.cpp
+++ src/xtd.core/src/xtd/io/directory_info.cpp
@@ -8,6 +8,7 @@
 #include "../../../include/xtd/io/path_too_long_exception.h"
 #include "../../../include/xtd/security/security_exception.h"
 #include "../../../include/xtd/argument_exception.h"
+#include "../../../include/xtd/as.h"
 #include "../../../include/xtd/not_implemented_exception.h"
 #include "../../../include/xtd/unauthorized_access_exception.h"
 #define __XTD_CORE_NATIVE_LIBRARY__
@@ -46,15 +47,15 @@ directory_info::directory_iterator directory_info::directory_iterator::operator 
 
 directory_info::directory_iterator::value_type directory_info::directory_iterator::operator *() const {
   if (data_ == nullptr) return empty;
-  return directory_info(*data_->iterator_);
+  return directory_info {*data_->iterator_};
 }
 
 directory_info::directory_iterator directory_info::directory_iterator::begin() const {
-  return directory_info::directory_iterator(data_->iterator_.path(), data_->iterator_.pattern());
+  return directory_info::directory_iterator {data_->iterator_.path(), data_->iterator_.pattern()};
 }
 
 directory_info::directory_iterator directory_info::directory_iterator::end() const {
-  return directory_info::directory_iterator();
+  return directory_info::directory_iterator {};
 }
 
 bool directory_info::directory_iterator::equals(const directory_info::directory_iterator& other) const noexcept {
@@ -96,14 +97,14 @@ directory_info::file_iterator directory_info::file_iterator::operator ++(int32) 
 
 directory_info::file_iterator::value_type directory_info::file_iterator::operator *() const {
   if (data_ == nullptr) return file_info::empty;
-  return file_info(*data_->iterator_);
+  return file_info {*data_->iterator_};
 }
 
 directory_info::file_iterator directory_info::file_iterator::begin() const {
   return directory_info::file_iterator(data_->iterator_.path(), data_->iterator_.pattern());
 }
 directory_info::file_iterator directory_info::file_iterator::end() const {
-  return xtd::io::directory_info::file_iterator();
+  return xtd::io::directory_info::file_iterator {};
 }
 
 bool directory_info::file_iterator::equals(const directory_info::file_iterator& other) const noexcept {
@@ -144,11 +145,11 @@ directory_info::file_system_info_iterator directory_info::file_system_info_itera
 }
 
 directory_info::file_system_info_iterator directory_info::file_system_info_iterator::begin() const {
-  return directory_info::file_system_info_iterator(data_->iterator_.path(), data_->iterator_.pattern());
+  return directory_info::file_system_info_iterator {data_->iterator_.path(), data_->iterator_.pattern()};
 }
 
 directory_info::file_system_info_iterator directory_info::file_system_info_iterator::end() const {
-  return xtd::io::directory_info::file_system_info_iterator();
+  return xtd::io::directory_info::file_system_info_iterator {};
 }
 
 bool directory_info::file_system_info_iterator::equals(const directory_info::file_system_info_iterator& other) const noexcept {
@@ -165,9 +166,9 @@ ustring directory_info::file_system_info_iterator::pattern() const {
 
 directory_info::file_system_info_iterator::value_type directory_info::file_system_info_iterator::operator *() const {
   if (data_ == nullptr) return std::make_shared<file_info>("");
-  int32 attributes = -1;
+  auto attributes = -1;
   native::file_system::get_attributes(*data_->iterator_, attributes);
-  if ((static_cast<file_attributes>(attributes) & file_attributes::directory) == file_attributes::directory) return std::make_shared<directory_info>(*data_->iterator_);
+  if ((as<file_attributes>(attributes) & file_attributes::directory) == file_attributes::directory) return std::make_shared<directory_info>(*data_->iterator_);
   return std::make_shared<file_info>(*data_->iterator_);
 }
 
@@ -181,12 +182,12 @@ directory_info::directory_info(const xtd::ustring& path) {
 }
 
 bool directory_info::exists() const {
-  int32 attributes = 0;
+  auto attributes = 0;
   return native::file_system::get_attributes(full_path_, attributes) == 0 && (static_cast<file_attributes>(attributes) & file_attributes::directory) == file_attributes::directory;
 }
 
 ustring directory_info::name() const {
-  vector<ustring> items = full_path_.split({path::directory_separator_char()});
+  auto items = full_path_.split({path::directory_separator_char()});
   if (items.size() == 0)
     return full_path_;
   return items[items.size() - 1];
@@ -211,7 +212,7 @@ directory_info directory_info::create_subdirectory(const ustring& path) const {
   if (path.empty() || path.trim(' ').empty()) throw argument_exception {csf_};
   if (native::file_system::is_path_too_long(path::combine(full_path_, path))) throw path_too_long_exception {csf_};
   
-  directory_info dir_info(path::combine(full_path_, path));
+  auto dir_info = directory_info {path::combine(full_path_, path)};
   if (!dir_info.exists()) dir_info.create();
   return dir_info;
 }
@@ -221,7 +222,7 @@ directory_info::directory_iterator directory_info::enumerate_directories() const
 }
 
 directory_info::directory_iterator directory_info::enumerate_directories(const xtd::ustring& pattern) const {
-  return directory_iterator(full_path_, pattern);
+  return directory_iterator {full_path_, pattern};
 }
 
 directory_info::file_iterator directory_info::enumerate_files() const {
@@ -229,7 +230,7 @@ directory_info::file_iterator directory_info::enumerate_files() const {
 }
 
 directory_info::file_iterator directory_info::enumerate_files(const xtd::ustring& pattern) const {
-  return file_iterator(full_path_, pattern);
+  return file_iterator {full_path_, pattern};
 }
 
 directory_info::file_system_info_iterator directory_info::enumerate_file_system_infos() const {
@@ -237,7 +238,7 @@ directory_info::file_system_info_iterator directory_info::enumerate_file_system_
 }
 
 directory_info::file_system_info_iterator directory_info::enumerate_file_system_infos(const xtd::ustring& pattern) const {
-  return file_system_info_iterator(full_path_, pattern);
+  return file_system_info_iterator {full_path_, pattern};
 }
 
 vector<directory_info> directory_info::get_directories() const {
@@ -265,14 +266,13 @@ vector<shared_ptr<file_system_info>> directory_info::get_file_system_infos(const
 }
 
 void directory_info::move_to(const ustring& dest_dir_name) {
-  directory_info dest_dir_info(dest_dir_name);
-  
+  auto dest_dir_info = directory_info {dest_dir_name};
   if (dest_dir_name == "" || dest_dir_info.exists() || equals(dest_dir_info) || !path::get_path_root(full_name()).equals(path::get_path_root(dest_dir_info.full_name())) || dest_dir_info.full_name().starts_with(full_name())) throw io_exception {csf_};
   
-  ustring target_dir_name = path::combine(dest_dir_name, full_path_.substring(full_path_.last_index_of(path::directory_separator_char()) + 1));
+  auto target_dir_name = path::combine(dest_dir_name, full_path_.substring(full_path_.last_index_of(path::directory_separator_char()) + 1));
   if (!dest_dir_info.exists()) throw directory_not_found_exception {csf_};
-  directory::create_directory(target_dir_name);
   
+  directory::create_directory(target_dir_name);
   for (ustring item : native::directory::enumerate_files(full_path_, "*"))
     file::move(path::combine(full_path_, item), path::combine(target_dir_name, item));
   for (ustring item : native::directory::enumerate_directories(full_path_, "*"))
@@ -311,12 +311,12 @@ void directory_info::remove(bool recursive) const {
    */
   
   if (recursive) {
-    for (ustring item : native::directory::enumerate_files(full_path_, "*")) {
+    for (const auto& item : native::directory::enumerate_files(full_path_, "*")) {
       file_info fi(path::combine(full_path_, item));
       if ((fi.attributes() & file_attributes::read_only) == file_attributes::read_only) throw unauthorized_access_exception {csf_};
       file::remove(path::combine(full_path_, item));
     }
-    for (ustring item : native::directory::enumerate_directories(full_path_, "*"))
+    for (const auto& item : native::directory::enumerate_directories(full_path_, "*"))
       directory_info(path::combine(full_path_, item)).remove(true);
   }
   
