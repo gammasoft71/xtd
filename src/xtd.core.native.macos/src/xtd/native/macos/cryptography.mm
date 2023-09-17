27631a3e77844ebc7808a699a5dea5cba730e6bb
Gammasoft <gammasoft71@gmail.com>
2023-09-17 20:51:38 +0200

    Review code

--- src/xtd.core.native.macos/src/xtd/native/macos/cryptography.mm
+++ src/xtd.core.native.macos/src/xtd/native/macos/cryptography.mm
@@ -10,7 +10,7 @@ using namespace xtd::native;
 namespace {
   static std::string create_process(const std::string& command) {
     auto fs = popen(command.c_str(), "r");
-    std::string result;
+    auto result = std::string {};
     while (!feof(fs)) {
       char buf[513];
       size_t l = fread(buf, 1, 512, fs);
@@ -23,18 +23,18 @@ namespace {
 }
 
 vector<uint_least8_t> cryptography::machine_guid() {
-  static bool first = true;
-  static string guid_str = create_process("ioreg -rd1 -c IOPlatformExpertDevice | grep -E '(UUID)'");
+  static auto first = true;
+  static auto guid_str = create_process("ioreg -rd1 -c IOPlatformExpertDevice | grep -E '(UUID)'");
   if (first) guid_str = guid_str.substr(guid_str.find("=") + 1);
   first = false;
   
-  static const string guid_fallback = "30395f0ed6aa4a5eb4af6f90a608c605";
-  static const string hex_chars = "0123456789ABCDEF";
+  static const auto guid_fallback = string {"30395f0ed6aa4a5eb4af6f90a608c605"};
+  static const auto hex_chars = string {"0123456789ABCDEF"};
   for (auto index = 0u; guid_str[index] != 0; ++index)
     if (hex_chars.find(static_cast<char>(toupper(guid_str[index]))) == hex_chars.npos)  guid_str.erase(index--, 1);
   if (guid_str.size() != 32) guid_str = guid_fallback;
   
-  vector<uint_least8_t> bytes;
+  auto bytes = vector<uint_least8_t> {};
   for (auto index = 0u; index < guid_str.size(); index += 2)
     bytes.push_back(static_cast<uint_least8_t>(stoi(guid_str.substr(index, 2), 0, 16)));
   return bytes;
