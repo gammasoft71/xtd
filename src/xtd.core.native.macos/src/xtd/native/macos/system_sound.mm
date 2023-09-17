c7678ebd51705c96ec3cbab504aea4c4115ed6b4
Gammasoft <gammasoft71@gmail.com>
2023-09-17 22:05:39 +0200

    Review code

--- src/xtd.core.native.macos/src/xtd/native/macos/system_sound.mm
+++ src/xtd.core.native.macos/src/xtd/native/macos/system_sound.mm
@@ -11,12 +11,9 @@ using namespace std;
 using namespace xtd::native;
 
 void system_sound::play(uint_least32_t sound) {
-  if (sound == ST_BEEP || sound == ST_OK)
-    NSBeep();
-  else {
-    static map<uint_least32_t, string> sounds = {{ST_ICONERROR, "Funk"}, {ST_ICONQUESTION, "Purr"}, {ST_ICONWARNING, "Hero"}, {ST_ICONASTERISK, "Glass"}};
-    @autoreleasepool {
-      [[NSSound soundNamed:[NSString stringWithUTF8String:sounds[sound].c_str()]] play];
-    }
+  if (sound == ST_BEEP || sound == ST_OK) NSBeep();
+  else @autoreleasepool {
+    static auto sounds = map<uint_least32_t, string> {{ST_ICONERROR, "Funk"}, {ST_ICONQUESTION, "Purr"}, {ST_ICONWARNING, "Hero"}, {ST_ICONASTERISK, "Glass"}};
+    [[NSSound soundNamed:[NSString stringWithUTF8String:sounds[sound].c_str()]] play];
   }
 }
