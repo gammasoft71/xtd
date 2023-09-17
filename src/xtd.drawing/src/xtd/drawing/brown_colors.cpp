802a682469ee9243a5a0561ac0cf5d501b464be0
Gammasoft <gammasoft71@gmail.com>
2023-09-17 22:52:30 +0200

    Review code

--- src/xtd.drawing/src/xtd/drawing/brown_colors.cpp
+++ src/xtd.drawing/src/xtd/drawing/brown_colors.cpp
@@ -73,11 +73,11 @@ color brown_colors::wheat() noexcept {
 }
 
 const vector<color>& brown_colors::get_colors() noexcept {
-  static vector colors {brown_colors::maroon(), brown_colors::brown(), brown_colors::saddle_brown(), brown_colors::sienna(), brown_colors::chocolate(), brown_colors::dark_goldenrod(), brown_colors::peru(), brown_colors::rosy_brown(), brown_colors::goldenrod(), brown_colors::sandy_brown(), brown_colors::tan(), brown_colors::burly_wood(), brown_colors::wheat(), brown_colors::navajo_white(), brown_colors::bisque(), brown_colors::blanched_almond(), brown_colors::cornsilk()};
+  static auto colors = vector {brown_colors::maroon(), brown_colors::brown(), brown_colors::saddle_brown(), brown_colors::sienna(), brown_colors::chocolate(), brown_colors::dark_goldenrod(), brown_colors::peru(), brown_colors::rosy_brown(), brown_colors::goldenrod(), brown_colors::sandy_brown(), brown_colors::tan(), brown_colors::burly_wood(), brown_colors::wheat(), brown_colors::navajo_white(), brown_colors::bisque(), brown_colors::blanched_almond(), brown_colors::cornsilk()};
   return colors;
 }
 
 const vector<ustring>& brown_colors::get_color_names() noexcept {
-  static vector color_names {brown_colors::maroon().name(), brown_colors::brown().name(), brown_colors::saddle_brown().name(), brown_colors::sienna().name(), brown_colors::chocolate().name(), brown_colors::dark_goldenrod().name(), brown_colors::peru().name(), brown_colors::rosy_brown().name(), brown_colors::goldenrod().name(), brown_colors::sandy_brown().name(), brown_colors::tan().name(), brown_colors::burly_wood().name(), brown_colors::wheat().name(), brown_colors::navajo_white().name(), brown_colors::bisque().name(), brown_colors::blanched_almond().name(), brown_colors::cornsilk().name()};
+  static auto color_names = vector {brown_colors::maroon().name(), brown_colors::brown().name(), brown_colors::saddle_brown().name(), brown_colors::sienna().name(), brown_colors::chocolate().name(), brown_colors::dark_goldenrod().name(), brown_colors::peru().name(), brown_colors::rosy_brown().name(), brown_colors::goldenrod().name(), brown_colors::sandy_brown().name(), brown_colors::tan().name(), brown_colors::burly_wood().name(), brown_colors::wheat().name(), brown_colors::navajo_white().name(), brown_colors::bisque().name(), brown_colors::blanched_almond().name(), brown_colors::cornsilk().name()};
   return color_names;
 }
