b885cb3e3bff8d7181060bf159c0cdc313af3b1b
Gammasoft <gammasoft71@gmail.com>
2023-09-17 20:55:36 +0200

    Review code

--- src/xtd.core.native.macos/src/xtd/native/macos/date_time.mm
+++ src/xtd.core.native.macos/src/xtd/native/macos/date_time.mm
@@ -67,7 +67,7 @@ namespace {
    */
   
   // Generate on Windows 10.0.19044.1415 at 27/12/2021
-  vector<date_time::time_zone_info> system_time_zones = {
+  auto system_time_zones = vector<date_time::time_zone_info> {
     {"Dateline Standard Time", -432000000000, "Dateline Daylight Time", "(UTC-12:00) International Date Line West", "Dateline Standard Time", false, {}},
     {"UTC-11", -396000000000, "UTC-11", "(UTC-11:00) Coordinated Universal Time-11", "UTC-11", false, {}},
     {"Aleutian Standard Time", -360000000000, "Aleutian Daylight Time", "(UTC-10:00) Aleutian Islands", "Aleutian Standard Time", true, {{1, 1, 1, 0, 0, 0, 2006, 12, 31, 0, 0, 0, 36000000000, {1, 0, false, 4, 2, 0, 0, 1}, {1, 0, false, 10, 2, 0, 0, 5}}, {2007, 1, 1, 0, 0, 0, 9999, 12, 31, 0, 0, 0, 36000000000, {1, 0, false, 3, 2, 0, 0, 2}, {1, 0, false, 11, 2, 0, 0, 1}}, }},
@@ -212,9 +212,9 @@ namespace {
   };
   
   string string_printf(const char* fmt, ...) {
-    va_list args;
+    auto args = va_list {};
     va_start(args, fmt);
-    string formatted_string(vsnprintf(nullptr, 0, fmt, args), 0);
+    auto formatted_string = string {static_cast<char>(vsnprintf(nullptr, 0, fmt, args)), 0};
     va_end(args);
     va_start(args, fmt);
     vsnprintf(&formatted_string[0], formatted_string.size() + 1, fmt, args);
@@ -235,7 +235,7 @@ namespace {
 }
 
 date_time::time_zone_info date_time::get_local_time_zone() {
-  time_zone_info local_time_zone;
+  auto local_time_zone = time_zone_info {};
   tzset();
   auto stzis = get_system_time_zones();
   auto iterator = std::find_if(stzis.begin(), stzis.end(), [&](auto tzi) {return tzi.id == reinterpret_cast<const char*>(tzname[0]);});
@@ -256,13 +256,13 @@ vector<date_time::time_zone_info> date_time::get_system_time_zones() {
 }
 
 bool date_time::is_daylight(time_t time) {
-  tm value;
+  auto value = tm {};
   localtime_r(&time, &value);
   return value.tm_isdst != 0;
 }
 
 time_t date_time::utc_offset(time_t time) {
-  tm value;
+  auto value = tm {};
   localtime_r(&time, &value);
   return value.tm_gmtoff;
 }
