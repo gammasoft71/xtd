53fa7f1d621a0c3f737f7809e9c3e70fdc7d5a77
Gammasoft <gammasoft71@gmail.com>
2023-09-19 11:30:01 +0200

    Review code

--- src/xtd.drawing/src/xtd/drawing/point_f.cpp
+++ src/xtd.drawing/src/xtd/drawing/point_f.cpp
@@ -2,7 +2,8 @@
 #include "../../../include/xtd/drawing/point_f.h"
 #include "../../../include/xtd/drawing/size.h"
 #include "../../../include/xtd/drawing/size_f.h"
-#include <xtd/as.h>
+#include <xtd/as>
+#include <xtd/ustring>
 
 using namespace xtd::drawing;
 
@@ -80,7 +81,7 @@ point_f point_f::subtract(const point_f& pt1, const point_f& pt2) noexcept {
 }
 
 xtd::ustring point_f::to_string() const noexcept {
-  return "{x=" + std::to_string(x_) + ", y=" + std::to_string(y_) + "}";
+  return  ustring::format("{{x={}, y={}}}", x_, y_);
 }
 
 point_f point_f::operator +(const size& sz) const noexcept {
