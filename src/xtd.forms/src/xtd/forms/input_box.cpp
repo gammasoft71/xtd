7901e0544b0c29028f5caab29999c8b11293a266
Gammasoft <gammasoft71@gmail.com>
2023-09-20 17:52:20 +0200

    Review code

--- src/xtd.forms/src/xtd/forms/input_box.cpp
+++ src/xtd.forms/src/xtd/forms/input_box.cpp
@@ -49,7 +49,7 @@ dialog_result input_box::show(ustring& value, const ustring& text, const ustring
 }
 
 dialog_result input_box::show_input_box(ustring& value, const iwin32_window* owner, const ustring& text, const ustring& caption, input_box_style style, character_casing casing, bool word_wrap) {
-  input_dialog dialog;
+  auto dialog = input_dialog {};
   dialog.value(value);
   dialog.message(text);
   dialog.text(caption);
@@ -57,7 +57,7 @@ dialog_result input_box::show_input_box(ustring& value, const iwin32_window* own
   dialog.multiline(style == input_box_style::multiline);
   dialog.use_system_password_char(style == input_box_style::password);
   dialog.word_wrap(word_wrap);
-  dialog_result result = owner ? dialog.show_sheet_dialog(*owner) : dialog.show_dialog();
+  auto result = owner ? dialog.show_sheet_dialog(*owner) : dialog.show_dialog();
   if (result == dialog_result::ok) value = dialog.value();
   return result;
 }
