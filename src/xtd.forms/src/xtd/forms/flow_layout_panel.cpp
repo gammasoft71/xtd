ed859dd9ee968eb6fbff4460dd639994b3032770
Gammasoft <gammasoft71@gmail.com>
2023-09-20 17:17:05 +0200

    Review code

--- src/xtd.forms/src/xtd/forms/flow_layout_panel.cpp
+++ src/xtd.forms/src/xtd/forms/flow_layout_panel.cpp
@@ -4,20 +4,20 @@ using namespace xtd;
 using namespace xtd::forms;
 
 flow_layout_panel flow_layout_panel::create(const drawing::point& location, const drawing::size& size, const xtd::ustring& name) {
-  flow_layout_panel item;
-  if (location != drawing::point {-1, -1}) item.location(location);
-  if (size != drawing::size {-1, -1}) item.size(size);
-  item.name(name);
-  return item;
+  auto result = flow_layout_panel {};
+  if (location != drawing::point {-1, -1}) result.location(location);
+  if (size != drawing::size {-1, -1}) result.size(size);
+  result.name(name);
+  return result;
 }
 
 flow_layout_panel flow_layout_panel::create(const control& parent, const drawing::point& location, const drawing::size& size, const xtd::ustring& name) {
-  flow_layout_panel item;
-  item.parent(parent);
-  if (location != drawing::point {-1, -1}) item.location(location);
-  if (size != drawing::size {-1, -1}) item.size(size);
-  item.name(name);
-  return item;
+  auto result = flow_layout_panel {};
+  result.parent(parent);
+  if (location != drawing::point {-1, -1}) result.location(location);
+  if (size != drawing::size {-1, -1}) result.size(size);
+  result.name(name);
+  return result;
 }
 
 void flow_layout_panel::on_layout(const event_args& e) {
