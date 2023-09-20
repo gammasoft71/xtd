e9ef160408cf46e81210a3f4f690d75b36aa924a
Gammasoft <gammasoft71@gmail.com>
2023-09-20 17:15:57 +0200

    Review code

--- src/xtd.forms/src/xtd/forms/fixed_layout_panel.cpp
+++ src/xtd.forms/src/xtd/forms/fixed_layout_panel.cpp
@@ -4,20 +4,20 @@ using namespace xtd;
 using namespace xtd::forms;
 
 fixed_layout_panel fixed_layout_panel::create(const drawing::point& location, const drawing::size& size, const xtd::ustring& name) {
-  fixed_layout_panel item;
-  if (location != drawing::point {-1, -1}) item.location(location);
-  if (size != drawing::size {-1, -1}) item.size(size);
-  item.name(name);
-  return item;
+  auto result = fixed_layout_panel {};
+  if (location != drawing::point {-1, -1}) result.location(location);
+  if (size != drawing::size {-1, -1}) result.size(size);
+  result.name(name);
+  return result;
 }
 
 fixed_layout_panel fixed_layout_panel::create(const control& parent, const drawing::point& location, const drawing::size& size, const xtd::ustring& name) {
-  fixed_layout_panel item;
-  item.parent(parent);
-  if (location != drawing::point {-1, -1}) item.location(location);
-  if (size != drawing::size {-1, -1}) item.size(size);
-  item.name(name);
-  return item;
+  auto result = fixed_layout_panel {};
+  result.parent(parent);
+  if (location != drawing::point {-1, -1}) result.location(location);
+  if (size != drawing::size {-1, -1}) result.size(size);
+  result.name(name);
+  return result;
 }
 
 void fixed_layout_panel::on_layout(const event_args& e) {
