a5cf5dce408849e107c80a134ace0ec15663deec
Gammasoft <gammasoft71@gmail.com>
2023-09-20 22:37:33 +0200

    Review code

--- src/xtd.forms/src/xtd/forms/switch_button.cpp
+++ src/xtd.forms/src/xtd/forms/switch_button.cpp
@@ -32,18 +32,16 @@ xtd::drawing::color switch_button::checked_back_color() const noexcept {
 }
 
 switch_button& switch_button::checked_back_color(const xtd::drawing::color& color) {
-  if (!data_->checked_back_color.has_value() || data_->checked_back_color.value() != color) {
-    data_->checked_back_color = color;
-    invalidate();
-  }
+  if (data_->checked_back_color.has_value() && data_->checked_back_color.value() == color) return *this;
+  data_->checked_back_color = color;
+  invalidate();
   return *this;
 }
 
 switch_button& switch_button::checked_back_color(std::nullptr_t) {
-  if (data_->checked_back_color.has_value()) {
-    data_->checked_back_color.reset();
-    invalidate();
-  }
+  if (!data_->checked_back_color.has_value()) return *this;
+  data_->checked_back_color.reset();
+  invalidate();
   return *this;
 }
 
@@ -52,18 +50,16 @@ xtd::drawing::color switch_button::slider_color() const noexcept {
 }
 
 switch_button& switch_button::slider_color(const xtd::drawing::color& color) {
-  if (!data_->slider_color.has_value() || data_->slider_color.value() != color) {
-    data_->slider_color = color;
-    invalidate();
-  }
+  if (data_->slider_color.has_value() && data_->slider_color.value() == color) return *this;
+  data_->slider_color = color;
+  invalidate();
   return *this;
 }
 
 switch_button& switch_button::slider_color(std::nullptr_t) {
-  if (data_->slider_color.has_value()) {
-    data_->slider_color.reset();
-    invalidate();
-  }
+  if (!data_->slider_color.has_value()) return *this;
+  data_->slider_color.reset();
+  invalidate();
   return *this;
 }
 
@@ -72,10 +68,9 @@ bool switch_button::rounded() const noexcept {
 }
 
 switch_button& switch_button::rounded(bool value) {
-  if (data_->rounded != value) {
-    data_->rounded = value;
-    invalidate();
-  }
+  if (data_->rounded == value) return *this;
+  data_->rounded = value;
+  invalidate();
   return *this;
 }
 
@@ -84,11 +79,10 @@ bool switch_button::checked() const noexcept {
 }
 
 switch_button& switch_button::checked(bool checked) {
-  if (data_->checked != checked) {
-    data_->checked = checked;
-    invalidate();
-    on_checked_changed(event_args::empty);
-  }
+  if (data_->checked == checked) return *this;
+  data_->checked = checked;
+  invalidate();
+  on_checked_changed(event_args::empty);
   return *this;
 }
 
@@ -97,30 +91,29 @@ bool switch_button::show_text() const noexcept {
 }
 
 switch_button& switch_button::show_text(bool value) {
-  if (data_->show_text != value) {
-    data_->show_text = value;
-    invalidate();
-  }
+  if (data_->show_text == value) return *this;
+  data_->show_text = value;
+  invalidate();
   return *this;
 }
 
 switch_button switch_button::create(bool checked, const drawing::point& location, const drawing::size& size, const xtd::ustring& name) {
-  switch_button item;
-  item.checked(checked);
-  if (location != drawing::point {-1, -1}) item.location(location);
-  if (size != drawing::size {-1, -1}) item.size(size);
-  item.name(name);
-  return item;
+  auto result = switch_button {};
+  result.checked(checked);
+  if (location != drawing::point {-1, -1}) result.location(location);
+  if (size != drawing::size {-1, -1}) result.size(size);
+  result.name(name);
+  return result;
 }
 
 switch_button switch_button::create(const control& parent, bool checked, const drawing::point& location, const drawing::size& size, const xtd::ustring& name) {
-  switch_button item;
-  item.parent(parent);
-  item.checked(checked);
-  if (location != drawing::point {-1, -1}) item.location(location);
-  if (size != drawing::size {-1, -1}) item.size(size);
-  item.name(name);
-  return item;
+  auto result = switch_button {};
+  result.parent(parent);
+  result.checked(checked);
+  if (location != drawing::point {-1, -1}) result.location(location);
+  if (size != drawing::size {-1, -1}) result.size(size);
+  result.name(name);
+  return result;
 }
 
 drawing::size switch_button::default_size() const noexcept {
@@ -142,10 +135,10 @@ void switch_button::on_handle_created(const event_args& e) {
 }
 
 void switch_button::on_paint(paint_event_args& e) {
-  drawing::color button_back_color = data_->checked ? checked_back_color() : application::style_sheet().system_colors().gray_text();
-  drawing::color text_color = data_->checked ? fore_color() : drawing::color::average(button_back_color, fore_color(), .33);
-  drawing::color sld_color = this->slider_color();
-  xtd::ustring text = data_->checked ? "ON" : "OFF";
+  auto button_back_color = data_->checked ? checked_back_color() : application::style_sheet().system_colors().gray_text();
+  auto text_color = data_->checked ? fore_color() : drawing::color::average(button_back_color, fore_color(), .33);
+  auto sld_color = this->slider_color();
+  auto text = data_->checked ? "ON" : "OFF";
   
   if (!enabled()) {
     button_back_color = drawing::color::average(back_color(), button_back_color, .33);
@@ -154,18 +147,18 @@ void switch_button::on_paint(paint_event_args& e) {
   }
   
   if (data_->rounded) {
-    float height = static_cast<float>(e.clip_rectangle().height() - e.clip_rectangle().height() % 2);
-    drawing::point_f slider_location(data_->checked ? drawing::point_f(e.clip_rectangle().width() - height + 2.0f, 2.0f) : drawing::point_f(2.0f, 2.0f));
+    auto height = static_cast<float>(e.clip_rectangle().height() - e.clip_rectangle().height() % 2);
+    auto slider_location = data_->checked ? drawing::point_f {e.clip_rectangle().width() - height + 2.0f, 2.0f} : drawing::point_f {2.0f, 2.0f};
     
-    drawing::size_f string_size = e.graphics().measure_string(text, font());
-    drawing::point_f string_location(data_->checked ? drawing::point_f(slider_location.x() - string_size.width() - 2, (height - string_size.height()) / 2) : drawing::point_f(slider_location.x() + static_cast<float>(height - 4) + 2, (height - string_size.height()) / 2));
+    auto string_size = e.graphics().measure_string(text, font());
+    auto string_location = data_->checked ? drawing::point_f {slider_location.x() - string_size.width() - 2, (height - string_size.height()) / 2} : drawing::point_f {slider_location.x() + static_cast<float>(height - 4) + 2, (height - string_size.height()) / 2};
     
-    e.graphics().fill_rounded_rectangle(drawing::solid_brush(button_back_color), .0f, .0f, static_cast<float>(e.clip_rectangle().width()), static_cast<float>(height), static_cast<float>(height) / 2);
-    e.graphics().fill_ellipse(drawing::solid_brush(sld_color), slider_location.x(), slider_location.y(), static_cast<float>(height - 4), static_cast<float>(height - 4));
+    e.graphics().fill_rounded_rectangle(drawing::solid_brush {button_back_color}, .0f, .0f, static_cast<float>(e.clip_rectangle().width()), static_cast<float>(height), static_cast<float>(height) / 2);
+    e.graphics().fill_ellipse(drawing::solid_brush {sld_color}, slider_location.x(), slider_location.y(), static_cast<float>(height - 4), static_cast<float>(height - 4));
     if (data_->show_text) e.graphics().draw_string(text, font(), drawing::solid_brush(text_color), string_location);
   } else {
-    drawing::size_f slider_size(static_cast<float>((e.clip_rectangle().width() - 6) / 2), static_cast<float>(e.clip_rectangle().height() - 6));
-    drawing::point_f slider_location(data_->checked ? drawing::point_f(width() / 2.0f, 3.0f) : drawing::point_f(3.0f, 3.0f));
+    auto slider_size = drawing::size_f {static_cast<float>((e.clip_rectangle().width() - 6) / 2), static_cast<float>(e.clip_rectangle().height() - 6)};
+    auto slider_location = data_->checked ? drawing::point_f {width() / 2.0f, 3.0f} : drawing::point_f {3.0f, 3.0f};
     
     drawing::size_f string_size = e.graphics().measure_string(text, font());
     drawing::point_f string_location(data_->checked ? drawing::point_f((width() / 2 - string_size.width()) / 2, (height() - string_size.height()) / 2) : drawing::point_f(width() / 2 + (width() / 2 - string_size.width()) / 2, (height() - string_size.height()) / 2));
@@ -177,7 +170,7 @@ void switch_button::on_paint(paint_event_args& e) {
     e.graphics().draw_line(drawing::pen(control_paint::light(button_back_color), 2), e.clip_rectangle().left(), e.clip_rectangle().bottom(), e.clip_rectangle().right(), e.clip_rectangle().bottom());
     e.graphics().draw_line(drawing::pen(control_paint::light(button_back_color), 2), e.clip_rectangle().right(), e.clip_rectangle().top() + 2, e.clip_rectangle().right(), e.clip_rectangle().bottom());
     
-    e.graphics().fill_rectangle(drawing::solid_brush(sld_color), {slider_location, slider_size});
+    e.graphics().fill_rectangle(drawing::solid_brush {sld_color}, {slider_location, slider_size});
     e.graphics().draw_line(drawing::pen(control_paint::light(sld_color)), slider_location.x(), slider_location.y(), slider_location.x() + slider_size.width(), slider_location.y());
     e.graphics().draw_line(drawing::pen(control_paint::light(sld_color)), slider_location.x(), slider_location.y(), slider_location.x(), slider_location.y() + slider_size.height() - 2);
     e.graphics().draw_line(drawing::pen(control_paint::dark(sld_color)), slider_location.x(), slider_location.y() + slider_size.height() - 1, slider_location.x() + slider_size.width(), slider_location.y() + slider_size.height() - 1);
