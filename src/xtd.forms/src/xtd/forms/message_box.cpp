2fa251ad1e6f20eb5b2d916d20d6dd3140946ddc
Gammasoft <gammasoft71@gmail.com>
2023-09-20 21:06:40 +0200

    Review code

--- src/xtd.forms/src/xtd/forms/message_box.cpp
+++ src/xtd.forms/src/xtd/forms/message_box.cpp
@@ -69,7 +69,7 @@ dialog_result message_box::show(const ustring& text, const ustring& caption, mes
 }
 
 dialog_result message_box::show_message_dialog(const iwin32_window* owner, const ustring& text, const ustring& caption, message_box_buttons buttons, message_box_icon icon, message_box_default_button default_button, message_box_options options, bool display_help_button) {
-  message_dialog dialog;
+  auto dialog = message_dialog {};
   dialog.buttons(buttons);
   dialog.default_button(default_button);
   dialog.display_help_button(display_help_button);
