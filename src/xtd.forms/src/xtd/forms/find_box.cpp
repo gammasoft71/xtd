dc5e947cbcf2544495fab74036ff92d2d8db91b0
Gammasoft <gammasoft71@gmail.com>
2023-09-20 16:39:51 +0200

    Review code

--- src/xtd.forms/src/xtd/forms/find_box.cpp
+++ src/xtd.forms/src/xtd/forms/find_box.cpp
@@ -1,13 +1,14 @@
 #include "../../../include/xtd/forms/find_box.h"
 
+using namespace std;
 using namespace xtd;
 using namespace xtd::forms;
 
-std::unique_ptr<xtd::forms::find_dialog> find_box::dialog_;
+std::unique_ptr<find_dialog> find_box::dialog_;
 
 void find_box::show_find_box(const iwin32_window& owner, find_event_handler find_next, const ustring& find_string, const ustring& title, bool match_case, search_direction search_direction, bool whole_word) {
   if (dialog_) return;
-  dialog_ = std::make_unique<xtd::forms::find_dialog>();
+  dialog_ = make_unique<find_dialog>();
   dialog_->title(title).find_string(find_string).match_case(match_case).search_direction(search_direction).whole_word(whole_word);
   dialog_->show(owner);
   dialog_->find_next += find_next;
