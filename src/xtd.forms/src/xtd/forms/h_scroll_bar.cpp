be11a9cca49c987abaca7e22d297a2ca6c9fcd6d
Gammasoft <gammasoft71@gmail.com>
2023-09-20 17:47:36 +0200

    Review code

--- src/xtd.forms/src/xtd/forms/h_scroll_bar.cpp
+++ src/xtd.forms/src/xtd/forms/h_scroll_bar.cpp
@@ -14,10 +14,8 @@ h_scroll_bar::h_scroll_bar() : scroll_bar(false) {
 }
 
 forms::create_params h_scroll_bar::create_params() const noexcept {
-  forms::create_params create_params = scroll_bar::create_params();
-  
+  auto create_params = scroll_bar::create_params();
   create_params.style(create_params.style() | SBS_HORZ);
-  
   return create_params;
 }
 
@@ -26,24 +24,24 @@ xtd::drawing::size h_scroll_bar::default_size() const noexcept {
 }
 
 h_scroll_bar h_scroll_bar::create(int32 value, int32 minimum, int32 maximum, const drawing::point& location, const drawing::size& size, const xtd::ustring& name) {
-  h_scroll_bar item;
-  item.minimum(minimum);
-  item.maximum(maximum);
-  item.value(value);
-  if (location != drawing::point {-1, -1}) item.location(location);
-  if (size != drawing::size {-1, -1}) item.size(size);
-  item.name(name);
-  return item;
+  auto result = h_scroll_bar {};
+  result.minimum(minimum);
+  result.maximum(maximum);
+  result.value(value);
+  if (location != drawing::point {-1, -1}) result.location(location);
+  if (size != drawing::size {-1, -1}) result.size(size);
+  result.name(name);
+  return result;
 }
 
 h_scroll_bar h_scroll_bar::create(const control& parent, int32 value, int32 minimum, int32 maximum, const drawing::point& location, const drawing::size& size, const xtd::ustring& name) {
-  h_scroll_bar item;
-  item.parent(parent);
-  item.minimum(minimum);
-  item.maximum(maximum);
-  item.value(value);
-  if (location != drawing::point {-1, -1}) item.location(location);
-  if (size != drawing::size {-1, -1}) item.size(size);
-  item.name(name);
-  return item;
+  auto result = h_scroll_bar {};
+  result.parent(parent);
+  result.minimum(minimum);
+  result.maximum(maximum);
+  result.value(value);
+  if (location != drawing::point {-1, -1}) result.location(location);
+  if (size != drawing::size {-1, -1}) result.size(size);
+  result.name(name);
+  return result;
 }
