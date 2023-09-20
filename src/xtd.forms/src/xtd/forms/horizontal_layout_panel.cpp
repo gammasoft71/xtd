7880a74caad579d33df8116125a666ad2d69ffe3
Gammasoft <gammasoft71@gmail.com>
2023-09-20 17:50:39 +0200

    Review code

--- src/xtd.forms/src/xtd/forms/horizontal_layout_panel.cpp
+++ src/xtd.forms/src/xtd/forms/horizontal_layout_panel.cpp
@@ -26,20 +26,20 @@ horizontal_layout_panel& horizontal_layout_panel::control_layout_style(const con
 }
 
 horizontal_layout_panel horizontal_layout_panel::create(const drawing::point& location, const drawing::size& size, const xtd::ustring& name) {
-  horizontal_layout_panel item;
-  if (location != drawing::point {-1, -1}) item.location(location);
-  if (size != drawing::size {-1, -1}) item.size(size);
-  item.name(name);
-  return item;
+  auto result = horizontal_layout_panel {};
+  if (location != drawing::point {-1, -1}) result.location(location);
+  if (size != drawing::size {-1, -1}) result.size(size);
+  result.name(name);
+  return result;
 }
 
 horizontal_layout_panel horizontal_layout_panel::create(const control& parent, const drawing::point& location, const drawing::size& size, const xtd::ustring& name) {
-  horizontal_layout_panel item;
-  item.parent(parent);
-  if (location != drawing::point {-1, -1}) item.location(location);
-  if (size != drawing::size {-1, -1}) item.size(size);
-  item.name(name);
-  return item;
+  auto result = horizontal_layout_panel {};
+  result.parent(parent);
+  if (location != drawing::point {-1, -1}) result.location(location);
+  if (size != drawing::size {-1, -1}) result.size(size);
+  result.name(name);
+  return result;
 }
 
 void horizontal_layout_panel::on_control_added(const xtd::forms::control_event_args& e) {
@@ -58,23 +58,23 @@ void horizontal_layout_panel::on_layout(const event_args& e) {
   panel::on_layout(e);
   if (!parent() && controls().size() == 0) return;
   
-  size_t auto_size_control_count = count_if(control_layout_styles_.begin(), control_layout_styles_.end(), [](auto layout_style)->bool {return layout_style.second.size_type() == size_type::auto_size;});
-  int32 auto_size_width = client_size().width() - static_cast<int32>(control_layout_styles().size() * padding().left()) - static_cast<int32>(control_layout_styles().size() * padding().right());
+  auto auto_size_control_count = count_if(control_layout_styles_.begin(), control_layout_styles_.end(), [](auto layout_style)->bool {return layout_style.second.size_type() == size_type::auto_size;});
+  auto auto_size_width = client_size().width() - static_cast<int32>(control_layout_styles().size() * padding().left()) - static_cast<int32>(control_layout_styles().size() * padding().right());
   
-  int32 absolute_width = 0;
+  auto absolute_width = 0;
   for (auto& [control, layout_style] : control_layout_styles_)
     if (layout_style.size_type() == size_type::absolute) absolute_width += static_cast<int32>(layout_style.width().value_or(control.get().default_size().width()));
   auto_size_width -= absolute_width;
   
-  float total_percent = 0;
+  auto total_percent = .0f;
   for (auto& [control, layout_style] : control_layout_styles_)
     if (layout_style.size_type() == size_type::percent) total_percent += layout_style.width().value_or(0);
-  int32 percent_width = static_cast<int32>(auto_size_width * total_percent);
+  auto percent_width = static_cast<int32>(auto_size_width * total_percent);
   auto_size_width -= percent_width;
   
-  int32 left = padding().left();
-  int32 top = 0;
-  int32 width = 0;
+  auto left = padding().left();
+  auto top = 0;
+  auto width = 0;
   for (auto& [control, layout_style] : control_layout_styles_) {
     if (layout_style.expanded()) top = padding().top();
     else {
@@ -82,7 +82,7 @@ void horizontal_layout_panel::on_layout(const event_args& e) {
       else if (layout_style.align() == content_alignment::bottom_left || layout_style.align() == content_alignment::bottom_center || layout_style.align() == content_alignment::bottom_right) top = client_size().height() - padding().bottom() - control.get().height();
       else top = client_size().height() / 2 - control.get().height() / 2;
     }
-    int32 height = layout_style.expanded() ? client_size().height() - padding().top() - padding().bottom() : control.get().height();
+    auto height = layout_style.expanded() ? client_size().height() - padding().top() - padding().bottom() : control.get().height();
     if (layout_style.size_type() == size_type::absolute) width = static_cast<int32>(layout_style.width().value_or(control.get().default_size().width()));
     else if (layout_style.size_type() == size_type::percent) width = static_cast<int32>(percent_width * (layout_style.width().value_or(0) / total_percent));
     else if (layout_style.size_type() == size_type::auto_size) width = static_cast<int32>(auto_size_width / auto_size_control_count);
