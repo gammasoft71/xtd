4b16fbd5c31eb3082c0ec79e59ae36fd661f1061
Gammasoft <gammasoft71@gmail.com>
2023-09-21 12:24:19 +0200

    Review code

--- src/xtd.forms/src/xtd/forms/up_down_base.cpp
+++ src/xtd.forms/src/xtd/forms/up_down_base.cpp
@@ -17,10 +17,9 @@ forms::border_sides up_down_base::border_sides() const noexcept {
 }
 
 up_down_base& up_down_base::border_sides(forms::border_sides border_sides) {
-  if (data_->border_sides != border_sides) {
-    data_->border_sides = border_sides;
-    if (control_appearance() == forms::control_appearance::standard) invalidate();
-  }
+  if (data_->border_sides == border_sides) return *this;
+  data_->border_sides = border_sides;
+  if (control_appearance() == forms::control_appearance::standard) invalidate();
   return *this;
 }
 
@@ -29,10 +28,9 @@ forms::border_style up_down_base::border_style() const noexcept {
 }
 
 up_down_base& up_down_base::border_style(forms::border_style value) {
-  if (data_->border_style != value) {
-    data_->border_style = value;
-    post_recreate_handle();
-  }
+  if (data_->border_style == value) return *this;
+  data_->border_style = value;
+  post_recreate_handle();
   return *this;
 }
 
@@ -43,7 +41,7 @@ up_down_base::up_down_base() : data_(std::make_shared<data>()) {
 }
 
 forms::create_params up_down_base::create_params() const noexcept {
-  forms::create_params create_params = container_control::create_params();
+  auto create_params = container_control::create_params();
   
   if (data_->border_style == forms::border_style::fixed_single) create_params.style(create_params.style() | WS_BORDER);
   else if (data_->border_style != forms::border_style::none) create_params.ex_style(create_params.ex_style() | WS_EX_CLIENTEDGE);
