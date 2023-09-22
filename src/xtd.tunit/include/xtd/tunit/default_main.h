adc6eec8a9a771a58c8597e8cde112e995235d5c
Gammasoft <gammasoft71@gmail.com>
2023-09-22 22:17:27 +0200

    Fix TUNIT_CONFIG_MAIN

--- src/xtd.tunit/include/xtd/tunit/default_main.h
+++ src/xtd.tunit/include/xtd/tunit/default_main.h
@@ -10,7 +10,7 @@
 /// This example shows how to use
 /// @include tunit_config_main.cpp TUNIT_CONFIG_MAIN
 #if defined(TUNIT_CONFIG_MAIN)
-auto main(int argc, char* argv[])->int {
-  return __tunit_main::main(argc, argv);
+auto main()->int {
+  return __tunit_main::main();
 }
 #endif
