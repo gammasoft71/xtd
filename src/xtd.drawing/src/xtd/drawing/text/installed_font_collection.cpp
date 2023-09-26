7de248aad45f3606bb437b2a42c652c694db18f4
Gammasoft <gammasoft71@gmail.com>
2023-09-26 13:25:33 +0200

    Review include files

--- src/xtd.drawing/src/xtd/drawing/text/installed_font_collection.cpp
+++ src/xtd.drawing/src/xtd/drawing/text/installed_font_collection.cpp
@@ -1,6 +1,6 @@
-#include "../../../../include/xtd/drawing/text/installed_font_collection.h"
+#include "../../../../include/xtd/drawing/text/installed_font_collection"
 #define __XTD_DRAWING_NATIVE_LIBRARY__
-#include <xtd/drawing/native/font_family.h>
+#include <xtd/drawing/native/font_family>
 #undef __XTD_DRAWING_NATIVE_LIBRARY__
 
 using namespace std;
