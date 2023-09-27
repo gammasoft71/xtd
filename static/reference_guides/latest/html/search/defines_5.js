f1835277e4dd4f32d8153d7114b424a62aebc61c
gammasoft71 <gammasoft71@gmail.com>
2023-09-27 21:00:08 +0200

    update latest reference guide

---
+++ static/reference_guides/latest/html/search/defines_5.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['fail_5f_0',['fail_',['../xtd_8tunit_2include_2xtd_2tunit_2assert_8h.html#aeb105cd1722d2158f0cb8cb252c35df7',1,'assert.h']]],
+  ['file_5fformat_5fexception_5f_1',['file_format_exception_',['../file__format__exception_8h.html#a52ad0d781b52117619f6c2450b48be50',1,'file_format_exception.h']]],
+  ['file_5fload_5fexception_5f_2',['file_load_exception_',['../file__load__exception_8h.html#afe02bccfb9546abb89515c8b3e309e8f',1,'file_load_exception.h']]],
+  ['file_5fnot_5ffound_5fexception_5f_3',['file_not_found_exception_',['../file__not__found__exception_8h.html#a64f4d64421b974a24d53f2fdcd7ddd6a',1,'file_not_found_exception.h']]],
+  ['flags_5fattribute_5f_4',['flags_attribute_',['../flags__attribute_8h.html#aea49fae71107df8769685efb159c181a',1,'flags_attribute.h']]],
+  ['format_5fexception_5f_5',['format_exception_',['../format__exception_8h.html#a417233cde751627180f91199f57fa343',1,'format_exception.h']]],
+  ['forms_5fexport_5f_6',['forms_export_',['../forms__export_8h.html#aa2ab2e666648001a7d445dc452ac1767',1,'forms_export.h']]]
+];
