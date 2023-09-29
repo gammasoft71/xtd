2effafe15ef0d5ce507b0b14f8e1aaca0207bab4
Gammasoft <gammasoft71@gmail.com>
2023-09-29 21:48:07 +0200

    Review example

--- examples/xtd.forms.examples/others/form_and_thread/src/form_and_thread.cpp
+++ examples/xtd.forms.examples/others/form_and_thread/src/form_and_thread.cpp
@@ -13,35 +13,34 @@ public:
     text("Form and thread example");
     form_closed += [&] {
       closed = true;
-      for (auto index = 0_sz; index < threads.size(); ++index)
-        threads[index].join();
+      thread::join_all();
     };
     
     messages.parent(*this);
     messages.dock(dock_style::fill);
     
-    for (auto index = 0_sz; index < threads.size(); ++index) {
-      threads[index] = thread {[&](auto user_thread_id) {
+    for (auto index = 0_sz; index < environment::processor_count() - 1; ++index) {
+      thread::start_new([&] {
         auto counter = 0u;
+        auto thread_name = ustring::format("thread {}", thread::current_thread().managed_thread_id());
+        thread::current_thread().name(thread_name);
         while (!closed) {
           /// simulate work...
           thread::sleep(50_ms);
           ++counter;
           /// call invoke method to update UI in the main thread.
-          messages.begin_invoke([&, counter, user_thread_id] {
-            messages.items().push_back(ustring::format("thread {}: counter: {}", user_thread_id, counter));
+          messages.begin_invoke([&, counter, thread_name] {
+            messages.items().push_back(ustring::format("{}: counter: {}", thread_name, counter));
             messages.selected_index(messages.items().size() - 1);
           });
         }
-      }};
-      threads[index].start(index);
+      });
     }
   }
   
 private:
   list_box messages;
   bool closed = false;
-  std::vector<thread> threads = std::vector<thread>(environment::processor_count() - 1);
 };
 
 auto main()->int {
