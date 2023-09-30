b95d482fc776e3478e3ae795541a1fd85b657bbf
Gammasoft <gammasoft71@gmail.com>
2023-09-30 07:08:42 +0200

    Review include files

--- src/xtd.core/src/xtd/reflection/assembly_title_attribute.cpp
+++ src/xtd.core/src/xtd/reflection/assembly_title_attribute.cpp
@@ -1,4 +1,4 @@
-#include "../../../include/xtd/reflection/assembly_title_attribute"
+#include "../../../include/xtd/reflection/assembly_title_attribute.h"
 
 using namespace std;
 using namespace xtd;
