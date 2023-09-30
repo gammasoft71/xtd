acdacbeef6e969a72236c16a4c5fd02a715f8660
Gammasoft <gammasoft71@gmail.com>
2023-09-30 13:14:26 +0200

    Update build_xtd_and_launch_unit_tests_on_linux.sh

--- scripts/github/build_xtd_and_launch_unit_tests_on_linux.sh
+++ scripts/github/build_xtd_and_launch_unit_tests_on_linux.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-sudo apt install gcc-11 g++-11
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 100 --slave /usr/bin/g++ g++ /usr/bin/g++-11 --slave /usr/bin/gcov gcov /usr/bin/gcov-11
-#sudo update-alternatives --set gcc /usr/bin/gcc-11
-sudo update-alternatives --set g++ /usr/bin/g++-11
-
 # clone, generate and build wxwidgets 3.2.2.1
 mkdir -p build/thirdparty/ && cd build/thirdparty
 git clone https://github.com/wxwidgets/wxwidgets.git -b v3.2.2.1 --depth 1
 cd wxwidgets
 git submodule update --init
 mkdir build_cmake && cd build_cmake
-cmake .. -DCMAKE_C_COMPILER=gcc-11 -DCMAKE_CXX_COMPILER=g++-11 -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_CXX_COMPILER=g++-9 -DwxBUILD_SHARED=OFF -DCMAKE_INSTALL_PREFIX=~/local -DwxUSE_OPENGL=OFF
+cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_CXX_COMPILER=g++-9 -DwxBUILD_SHARED=OFF -DCMAKE_INSTALL_PREFIX=~/local -DwxUSE_OPENGL=OFF
 if [ $? -ne 0 ]; then exit -1; fi
 cmake --build . --target install -- -j $(nproc)
 if [ $? -ne 0 ]; then exit -1; fi
@@ -20,7 +15,7 @@ cd ../../../..
 # generate and build lib
 git submodule update --init
 mkdir -p build && cd build
-cmake .. -DCMAKE_C_COMPILER=gcc-11 -DCMAKE_CXX_COMPILER=g++-11 -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DXTD_BUILD_WITH_CONTINUOUS_INTEGRATION_SYSTEM=ON -DXTD_BUILD_TESTS_XTD_CORE=ON -DXTD_BUILD_TESTS_XTD_DRAWING=OFF -DXTD_BUILD_TESTS_XTD_FORMS=OFF -DXTD_BUILD_TESTS_XTD_TUNIT=ON -DCMAKE_CXX_COMPILER=g++-9 -DCMAKE_INSTALL_PREFIX=~/local
+cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DXTD_BUILD_WITH_CONTINUOUS_INTEGRATION_SYSTEM=ON -DXTD_BUILD_TESTS_XTD_CORE=ON -DXTD_BUILD_TESTS_XTD_DRAWING=OFF -DXTD_BUILD_TESTS_XTD_FORMS=OFF -DXTD_BUILD_TESTS_XTD_TUNIT=ON -DCMAKE_CXX_COMPILER=g++-9 -DCMAKE_INSTALL_PREFIX=~/local
 if [ $? -ne 0 ]; then exit -1; fi
 cmake --build . -- -j $(nproc)
 if [ $? -ne 0 ]; then exit -1; fi
