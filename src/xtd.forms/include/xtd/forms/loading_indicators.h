4e2eff59fe363d3f8d86df3829a95a27a81a7ce7
Gammasoft <gammasoft71@gmail.com>
2023-10-11 22:33:11 +0200

    Add loading_indicators factory

---
+++ src/xtd.forms/include/xtd/forms/loading_indicators.h
@@ -0,0 +1,59 @@
+/// @file
+/// @brief Contains xtd::forms::loading_indicators factory.
+/// @copyright Copyright (c) 2023 Gammasoft. All rights reserved.
+#pragma once
+#include "loading_indicator.h"
+#include <xtd/static>
+
+/// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
+namespace xtd {
+  /// @brief The xtd::forms namespace contains classes for creating Windows-based applications that take full advantage of the rich user interface features available in the Microsoft Windows operating system, Apple macOS and Linux like Ubuntu operating system.
+  namespace forms {
+    /// @brief Provides a collection of loading indicator objects for use by a Windows Forms application.
+    /// @par Header
+    /// @code #include <xtd/forms/loading_indicators> @endcode
+    /// @par Namespace
+    /// xtd::forms
+    /// @par Library
+    /// xtd.forms
+    /// @ingroup xtd_forms controls
+    /// @par Examples
+    /// The following code example demonstrates the use of loading_indicators control factory.
+    /// @include loading_indicators.cpp
+    class forms_export_ loading_indicators static_ {
+      struct data;
+      
+    public:
+      /// @name Properties
+      
+      /// @{
+      /// @brief Create a system-defined loading_indicator that represent circle blinks loading indicator.
+      /// @param location The loading_indicator location in pixels.
+      /// @param size The loading_indicator size in pixels.
+      /// @return The created loading_indicator.
+      static loading_indicator circle_blinks(const xtd::drawing::point& location = {-1, -1}, const xtd::drawing::size& size = {-1, -1}) noexcept;
+      /// @brief Create a system-defined loading_indicator that represent standard loading indicator.
+      /// @param location The loading_indicator location in pixels.
+      /// @param size The loading_indicator size in pixels.
+      /// @return The created loading_indicator.
+      static loading_indicator standard(const xtd::drawing::point& location = {-1, -1}, const xtd::drawing::size& size = {-1, -1}) noexcept;
+      /// @brief Create a system-defined loading_indicator that represent system loading indicator.
+      /// @param location The loading_indicator location in pixels.
+      /// @param size The loading_indicator size in pixels.
+      /// @return The created loading_indicator.
+      static loading_indicator system(const xtd::drawing::point& location = {-1, -1}, const xtd::drawing::size& size = {-1, -1}) noexcept;
+      /// @brief Create a system-defined loading_indicator that represent three_balls loading indicator.
+      /// @param location The loading_indicator location in pixels.
+      /// @param size The loading_indicator size in pixels.
+      /// @return The created loading_indicator.
+      static loading_indicator three_balls(const xtd::drawing::point& location = {-1, -1}, const xtd::drawing::size& size = {-1, -1}) noexcept;
+      /// @brief Create a system-defined loading_indicator that represent three_balls_bouncing loading indicator.
+      /// @param location The loading_indicator location in pixels.
+      /// @param size The loading_indicator size in pixels.
+      /// @return The created loading_indicator.
+      static loading_indicator three_balls_bouncing(const xtd::drawing::point& location = {-1, -1}, const xtd::drawing::size& size = {-1, -1}) noexcept;
+      /// @}
+      
+    };
+  }
+}
