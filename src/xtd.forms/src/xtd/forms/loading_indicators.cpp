2f917e55b95b8a7e64b6ae7d4259159b9423fbed
Gammasoft <gammasoft71@gmail.com>
2023-10-12 13:48:14 +0200

    Add overloading properties

--- src/xtd.forms/src/xtd/forms/loading_indicators.cpp
+++ src/xtd.forms/src/xtd/forms/loading_indicators.cpp
@@ -7,8 +7,18 @@ loading_indicator loading_indicators::circle_blinks(const xtd::drawing::point& l
   auto result = loading_indicator {};
   result.loading_indicator_style(xtd::forms::loading_indicator_style::circle_blinks);
   if (location.x() != -1 && location.y() != -1) result.location(location);
-  if (size.width() != -1 && size.height() != -1) result.size(size);
   result.running(running);
+  if (size.width() != -1 && size.height() != -1) result.size(size);
+  return result;
+}
+
+loading_indicator loading_indicators::circle_blinks(const control& parent, const xtd::drawing::point& location, const xtd::drawing::size& size, bool running) noexcept {
+  auto result = loading_indicator {};
+  result.loading_indicator_style(xtd::forms::loading_indicator_style::circle_blinks);
+  if (location.x() != -1 && location.y() != -1) result.location(location);
+  result.parent(parent);
+  result.running(running);
+  if (size.width() != -1 && size.height() != -1) result.size(size);
   return result;
 }
 
@@ -16,8 +26,18 @@ loading_indicator loading_indicators::standard(const xtd::drawing::point& locati
   auto result = loading_indicator {};
   result.loading_indicator_style(xtd::forms::loading_indicator_style::standard);
   if (location.x() != -1 && location.y() != -1) result.location(location);
-  if (size.width() != -1 && size.height() != -1) result.size(size);
   result.running(running);
+  if (size.width() != -1 && size.height() != -1) result.size(size);
+  return result;
+}
+
+loading_indicator loading_indicators::standard(const control& parent, const xtd::drawing::point& location, const xtd::drawing::size& size, bool running) noexcept {
+  auto result = loading_indicator {};
+  result.loading_indicator_style(xtd::forms::loading_indicator_style::standard);
+  if (location.x() != -1 && location.y() != -1) result.location(location);
+  result.parent(parent);
+  result.running(running);
+  if (size.width() != -1 && size.height() != -1) result.size(size);
   return result;
 }
 
@@ -25,8 +45,18 @@ loading_indicator loading_indicators::system(const xtd::drawing::point& location
   auto result = loading_indicator {};
   result.loading_indicator_style(xtd::forms::loading_indicator_style::system);
   if (location.x() != -1 && location.y() != -1) result.location(location);
-  if (size.width() != -1 && size.height() != -1) result.size(size);
   result.running(running);
+  if (size.width() != -1 && size.height() != -1) result.size(size);
+  return result;
+}
+
+loading_indicator loading_indicators::system(const control& parent, const xtd::drawing::point& location, const xtd::drawing::size& size, bool running) noexcept {
+  auto result = loading_indicator {};
+  result.loading_indicator_style(xtd::forms::loading_indicator_style::system);
+  if (location.x() != -1 && location.y() != -1) result.location(location);
+  result.parent(parent);
+  result.running(running);
+  if (size.width() != -1 && size.height() != -1) result.size(size);
   return result;
 }
 
@@ -34,8 +64,18 @@ loading_indicator loading_indicators::three_balls(const xtd::drawing::point& loc
   auto result = loading_indicator {};
   result.loading_indicator_style(xtd::forms::loading_indicator_style::three_balls);
   if (location.x() != -1 && location.y() != -1) result.location(location);
-  if (size.width() != -1 && size.height() != -1) result.size(size);
   result.running(running);
+  if (size.width() != -1 && size.height() != -1) result.size(size);
+  return result;
+}
+
+loading_indicator loading_indicators::three_balls(const control& parent, const xtd::drawing::point& location, const xtd::drawing::size& size, bool running) noexcept {
+  auto result = loading_indicator {};
+  result.loading_indicator_style(xtd::forms::loading_indicator_style::three_balls);
+  if (location.x() != -1 && location.y() != -1) result.location(location);
+  result.parent(parent);
+  result.running(running);
+  if (size.width() != -1 && size.height() != -1) result.size(size);
   return result;
 }
 
@@ -43,7 +83,17 @@ loading_indicator loading_indicators::three_balls_bouncing(const xtd::drawing::p
   auto result = loading_indicator {};
   result.loading_indicator_style(xtd::forms::loading_indicator_style::three_balls_bouncing);
   if (location.x() != -1 && location.y() != -1) result.location(location);
-  if (size.width() != -1 && size.height() != -1) result.size(size);
   result.running(running);
+  if (size.width() != -1 && size.height() != -1) result.size(size);
+  return result;
+}
+
+loading_indicator loading_indicators::three_balls_bouncing(const control& parent, const xtd::drawing::point& location, const xtd::drawing::size& size, bool running) noexcept {
+  auto result = loading_indicator {};
+  result.loading_indicator_style(xtd::forms::loading_indicator_style::three_balls_bouncing);
+  if (location.x() != -1 && location.y() != -1) result.location(location);
+  result.parent(parent);
+  result.running(running);
+  if (size.width() != -1 && size.height() != -1) result.size(size);
   return result;
 }
