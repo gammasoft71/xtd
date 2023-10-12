98aef788e510a439732aaf0a2e5494e8bf12ecb8
Gammasoft <gammasoft71@gmail.com>
2023-10-12 11:16:41 +0200

    Fix animation and size

--- src/xtd.forms/src/xtd/forms/loading_indicator_animation_three_balls.h
+++ src/xtd.forms/src/xtd/forms/loading_indicator_animation_three_balls.h
@@ -6,42 +6,19 @@ namespace xtd::forms {
   class loading_indicator_animation_three_balls : public loading_indicator_animation {
   public:
     void on_paint(xtd::drawing::graphics& graphics,  const xtd::drawing::rectangle& clip_rectangle, const xtd::drawing::color& color, bool enabled) override {
-      auto dot_height = xtd::math::min(clip_rectangle.height(), clip_rectangle.width()) / 4;
-      auto dot_width = dot_height;
+      auto dot_size = xtd::math::min(clip_rectangle.height(), clip_rectangle.width()) / radius_factor_ * 2;
       auto x1 = clip_rectangle.left() + 2;
-      auto x2 = clip_rectangle.left() + clip_rectangle.width() / 2 - dot_width / 2;
-      auto x3 = clip_rectangle.right() - dot_width - 2;
-      auto y1 = clip_rectangle.top() + clip_rectangle.height() / 2 - dot_height / 2;
-      auto y2 = clip_rectangle.top() + clip_rectangle.height() / 2 - dot_height / 2;
-      auto y3 = clip_rectangle.top() + clip_rectangle.height() / 2 - dot_height / 2;
-      auto height1 = dot_height;
-      auto width1 = dot_width;
-      auto height2 = dot_height;
-      auto width2 = dot_width;
-      auto height3 = dot_height;
-      auto width3 = dot_width;
-      auto opacity = enabled ? 255 : 128;
-      
-      if (frame_ <= 2) {
-        x1 -= 2;
-        y1 -= 2;
-        height1 += 4;
-        width1 += 4;
-      } else if (frame_ <= 5) {
-        x2 -= 2;
-        y2 -= 2;
-        height2 += 4;
-        width2 += 4;
-      } else {
-        x3 -= 2;
-        y3 -= 2;
-        height3 += 4;
-        width3 += 4;
-      }
+      auto x2 = clip_rectangle.left() + clip_rectangle.width() / 2 - dot_size / 2;
+      auto x3 = clip_rectangle.right() - dot_size - 2;
+      auto y = clip_rectangle.top() + clip_rectangle.height() / 2 - dot_size / 2;
+      auto opacity = enabled ? 255 : 96;
+      auto size1 = std::vector {4, 4, 2, 2, 0, 0, 2, 2};
+      auto size2 = std::vector {2, 2, 4, 4, 2, 2, 0, 0};
+      auto size3 = std::vector {0, 0, 2, 2, 4, 4, 2, 2};
 
-      graphics.fill_ellipse(xtd::drawing::solid_brush {xtd::drawing::color::from_argb(static_cast<xtd::byte>(opacity), color)}, x1, y1, width1, height1);
-      graphics.fill_ellipse(xtd::drawing::solid_brush {xtd::drawing::color::from_argb(static_cast<xtd::byte>(opacity), color)}, x2, y2, width2, height2);
-      graphics.fill_ellipse(xtd::drawing::solid_brush {xtd::drawing::color::from_argb(static_cast<xtd::byte>(opacity), color)}, x3, y3, width3, height3);
+      graphics.fill_ellipse(xtd::drawing::solid_brush {xtd::drawing::color::from_argb(static_cast<xtd::byte>(opacity), color)}, x1 - size1[frame_] / 2, y - size1[frame_] / 2, dot_size + size1[frame_], dot_size + size1[frame_]);
+      graphics.fill_ellipse(xtd::drawing::solid_brush {xtd::drawing::color::from_argb(static_cast<xtd::byte>(opacity), color)}, x2 - size2[frame_] / 2, y - size2[frame_] / 2, dot_size + size2[frame_], dot_size + size2[frame_]);
+      graphics.fill_ellipse(xtd::drawing::solid_brush {xtd::drawing::color::from_argb(static_cast<xtd::byte>(opacity), color)}, x3 - size3[frame_] / 2, y - size3[frame_] / 2, dot_size + size3[frame_], dot_size + size3[frame_]);
     }
     
     void on_timer() override {
@@ -49,7 +26,8 @@ namespace xtd::forms {
     }
     
   private:
-    xtd::int32 intervals_ = 9;
+    xtd::int32 intervals_ = 8;
+    xtd::int32 radius_factor_ = 10;
     xtd::int32 frame_ = 0;
   };
 }
