cf2046f64b0b38943f5ade573d29ad4055ee662b
Gammasoft <gammasoft71@gmail.com>
2023-10-16 15:59:02 +0200

    Fix github action ubuntu build in c++20

--- scripts/github/build_xtd_and_launch_unit_tests_on_linux.sh
+++ scripts/github/build_xtd_and_launch_unit_tests_on_linux.sh
@@ -6,7 +6,7 @@ git clone https://github.com/wxwidgets/wxwidgets.git -b v3.2.2.1 --depth 1
 cd wxwidgets
 git submodule update --init
 mkdir build_cmake && cd build_cmake
-cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_CXX_COMPILER=g++-9 -DwxBUILD_SHARED=OFF -DCMAKE_INSTALL_PREFIX=~/local -DwxUSE_OPENGL=OFF
+cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DwxBUILD_SHARED=OFF -DCMAKE_INSTALL_PREFIX=~/local -DwxUSE_OPENGL=OFF
 if [ $? -ne 0 ]; then exit -1; fi
 cmake --build . --target install -- -j $(nproc)
 if [ $? -ne 0 ]; then exit -1; fi
@@ -15,7 +15,7 @@ cd ../../../..
 # generate and build lib
 git submodule update --init
 mkdir -p build && cd build
-cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DXTD_BUILD_WITH_CONTINUOUS_INTEGRATION_SYSTEM=ON -DXTD_BUILD_TESTS_XTD_CORE=ON -DXTD_BUILD_TESTS_XTD_DRAWING=OFF -DXTD_BUILD_TESTS_XTD_FORMS=OFF -DXTD_BUILD_TESTS_XTD_TUNIT=ON -DCMAKE_CXX_COMPILER=g++-9 -DCMAKE_INSTALL_PREFIX=~/local
+cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DXTD_BUILD_CPP_STANDARD=20 -DXTD_BUILD_WITH_CONTINUOUS_INTEGRATION_SYSTEM=ON -DXTD_BUILD_TESTS_XTD_CORE=ON -DXTD_BUILD_TESTS_XTD_DRAWING=OFF -DXTD_BUILD_TESTS_XTD_FORMS=OFF -DXTD_BUILD_TESTS_XTD_TUNIT=ON -DCMAKE_INSTALL_PREFIX=~/local
 if [ $? -ne 0 ]; then exit -1; fi
 cmake --build . -- -j $(nproc)
 if [ $? -ne 0 ]; then exit -1; fi
