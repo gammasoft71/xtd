1e8368fc27ef2e89e0763b80a7760049d16f06c3
Gammasoft <gammasoft71@gmail.com>
2023-10-24 11:58:26 +0200

    Update documentation

--- src/xtd.core.native/include/xtd/native/console.h
+++ src/xtd.core.native/include/xtd/native/console.h
@@ -43,58 +43,74 @@ namespace xtd {
       static int_least32_t background_color();
       /// @brief Sets the background color of the console.
       /// @param color the background console color.
+      /// @return true if successfull; otherwhise false.
       /// @warning Internal use only
       static bool background_color(int_least32_t color);
+      
       /// @brief Plays the sound of a beep of a specified frequency and duration through the console speaker.
       /// @param frequency The frequency of the beep, ranging from 37 to 32767 hertz
       /// @param duration The duration of the beep measured in milliseconds
+      /// @return true if successfull; otherwhise false.
       /// @warning Internal use only
       static bool beep(uint_least32_t frequency, uint_least32_t duration);
+      
       /// @brief Gets the height of the buffer area.
       /// @return The current height, in rows, of the buffer area.
       /// @warning Internal use only
       static int_least32_t buffer_height();
       /// @brief Sets or sets the height of the buffer area.
       /// @param height The current height, in rows, of the buffer area.
+      /// @return true if successfull; otherwhise false.
       /// @warning Internal use only
       static bool buffer_height(int_least32_t height);
       /// @brief Gets the width of the buffer area.
       /// @return The current width, in columns, of the buffer area.
       /// @warning Internal use only
+      
       static int_least32_t buffer_width();
       /// @brief Sets the width of the buffer area.
       /// @param width The current width, in columns, of the buffer area.
+      /// @return true if successfull; otherwhise false.
       /// @warning Internal use only
       static bool buffer_width(int_least32_t width);
+      
       /// @brief Gets a value indicating whether the CAPS LOCK keyboard toggle is turned on or turned off.
       /// @return true if CAPS LOCK is turned on; false if CAPS LOCK is turned off.
       /// @warning Internal use only
       static bool caps_lock();
+      
       /// @brief Clears the console buffer and corresponding console window of display information.
+      /// @return true if successfull; otherwhise false.
       /// @warning Internal use only
       static bool clear();
+      
       /// @brief Gets the column position of the cursor within the buffer area.
       /// @return The current position, in columns, of the cursor.
       /// @warning Internal use only
       static int_least32_t cursor_left();
+      
       /// @brief Gets or sets the height of the cursor within a character cell.
       /// @return The size of the cursor expressed as a percentage of the height of a character cell. The property value ranges from 1 to 100.
       /// @warning Internal use only
       static int_least32_t cursor_size();
       /// @brief Sets the height of the cursor within a character cell.
       /// @param size The size of the cursor expressed as a percentage of the height of a character cell. The property value ranges from 1 to 100.
+      /// @return true if successfull; otherwhise false.
       /// @warning Internal use only
       static bool cursor_size(int_least32_t size);
+      
       /// @brief Gets the row position of the cursor within the buffer area.
       /// @return The current position, in rows, of the cursor.
       /// @warning Internal use only
       static int_least32_t cursor_top();
+      
       /// @brief Gets a value indicating whether the cursor is visible.
       /// @return  true if the cursor is visible; otherwise, false.
       /// @warning Internal use only
       static bool cursor_visible();
       /// @brief Sets a value indicating whether the cursor is visible.
       /// @param visible true if the cursor is visible; otherwise, false.
+      /// @return true if successfull; otherwhise false.
       /// @warning Internal use only
       static bool cursor_visible(bool visible);
       
@@ -109,20 +125,25 @@ namespace xtd {
       static int_least32_t foreground_color();
       /// @brief Sets the foreground color of the console.
       /// @param color the foreground console color.
+      /// @return true if successfull; otherwhise false.
       /// @warning Internal use only
       static bool foreground_color(int_least32_t color);
+      
       /// @brief Gets the code page the console uses to read input.
       /// @return The code page used to read console input.
       /// @warning Internal use only
       static int_least32_t input_code_page();
       /// @brief Sets the code page the console uses to read input.
       /// @param code_page The code page used to read console input.
+      /// @return true if successfull; otherwhise false.
       /// @warning Internal use only
       static bool input_code_page(int_least32_t codePage);
+      
       /// @brief Gets a value indicating whether a key press is available in the input stream.
       /// @param true if a key press is available; otherwise, false
       /// @warning Internal use only
       static bool key_available();
+      
       /// @brief Gets the largest possible number of console window rows, based on the current font and screen resolution.
       /// @param The height of the largest possible console window measured in rows.
       /// @warning Internal use only
@@ -131,18 +152,22 @@ namespace xtd {
       /// @param The width of the largest possible console window measured in columns.
       /// @warning Internal use only
       static int_least32_t largest_window_width();
+      
       /// @brief Gets a value indicating whether the NUM LOCK keyboard toggle is turned on or turned off.
       /// @param true if NUM LOCK is turned on; false if NUM LOCK is turned off.
       /// @warning Internal use only
       static bool number_lock();
+      
       /// @brief Gets the code page the console uses to write output.
       /// @return The code page used to write console output.
       /// @warning Internal use only
       static int_least32_t output_code_page();
       /// @brief Sets the code page the console uses to write output.
       /// @param code_page The code page used to write console output.
+      /// @return true if successfull; otherwhise false.
       /// @warning Internal use only
       static bool output_code_page(int_least32_t codePage);
+      
       /// @brief Obtains the next character or function key pressed by the user.
       /// @param key_char A bitwise combination of the enumeration values. There is no default value.
       /// @param key_code A value that identifies the console key that was pressed.
@@ -152,71 +177,87 @@ namespace xtd {
       /// @remarks The five parameters will contains informations on key pressed.
       /// @warning Internal use only
       static void read_key(char32_t& key_char, char32_t& key_code, bool& alt, bool& shift, bool& ctrl);
+     
       /// @brief Register the user cancel callback.
       /// @param user_cancel_callback The user cancel callback method to register.
       /// @remarks This callback is call when when the Control modifier key (Ctrl) and either the ConsoleKey.C console key (C) or the Break key are pressed simultaneously (Ctrl+C or Ctrl+Break).
       /// @remarks By default, the return of callback is false, which causes program execution to terminate when the event handler exits. Changing its return to true specifies that the application should continue to execute.
       /// @warning Internal use only
       static void register_user_cancel_callback(std::function<bool(int_least32_t)> user_cancel_callback);
+      
       /// @brief Sets the foreground and background console colors to their defaults.
       /// @remarks The foreground and background colors are restored to the colors that existed when the current process began. For more information, see the foreground_color and background_color properties.
+      /// @return true if successfull; otherwhise false.
       /// @warning Internal use only
       static bool reset_color();
+      
       /// @brief Sets the position of the cursor.
       /// @param left The column position of the cursor. Columns are numbered from left to right starting at 0.
       /// @param top The row position of the cursor. Rows are numbered from top to bottom starting at 0.
+      /// @return true if successfull; otherwhise false.
       /// @warning Internal use only
       static bool set_cursor_position(int_least32_t left, int_least32_t top);
+      
       /// @brief Gets the title to display in the console title bar.
       /// @return The string to be displayed in the title bar of the console. The maximum length of the title string is 24500 characters.
       /// @warning Internal use only
       static std::string title();
       /// @brief Sets the title to display in the console title bar.
       /// @param title The string to be displayed in the title bar of the console. The maximum length of the title string is 24500 characters.
+      /// @return true if successfull; otherwhise false.
       /// @warning Internal use only
       static bool title(const std::string& title);
+      
       /// @brief Gets a value indicating whether the combination of the Control modifier key and C console key (Ctrl+C) is treated as ordinary input or as an interruption that is handled by the operating system.
       /// @return true if Ctrl+C is treated as ordinary input; otherwise, false.
       /// @warning Internal use only
       static bool treat_control_c_as_input();
       /// @brief Sets a value indicating whether the combination of the Control modifier key and C console key (Ctrl+C) is treated as ordinary input or as an interruption that is handled by the operating system.
       /// @param treat_control_c_as_input true if Ctrl+C is treated as ordinary input; otherwise, false.
+      /// @return true if successfull; otherwhise false.
       /// @warning Internal use only
       static bool treat_control_c_as_input(bool treat_control_c_as_input);
+      
       /// @brief Gets the height of the console window area.
       /// @return The height of the console window measured in rows.
       /// @warning Internal use only
       static int_least32_t window_height();
       /// @brief Sets the height of the console window area.
       /// @param height The height of the console window measured in rows.
+      /// @return true if successfull; otherwhise false.
       /// @warning Internal use only
       static bool window_height(int_least32_t height);
+     
       /// @brief Gets the left of the console window area.
       /// @return The left of the console window measured in columns.
       /// @warning Internal use only
       static int_least32_t window_left();
       /// @brief Sets the left of the console window area.
       /// @param left The left of the console window measured in columns.
+      /// @return true if successfull; otherwhise false.
       /// @warning Internal use only
       static bool window_left(int_least32_t left);
+      
       /// @brief Gets the top of the console window area.
       /// @return The top of the console window measured in rows.
       /// @warning Internal use only
       static int_least32_t window_top();
       /// @brief Sets the top of the console window area.
       /// @param top The top of the console window measured in rows.
+      /// @return true if successfull; otherwhise false.
       /// @warning Internal use only
       static bool window_top(int_least32_t top);
+      
       /// @brief Gets the width of the console window area.
       /// @return The width of the console window measured in columns.
       /// @warning Internal use only
       static int_least32_t window_width();
       /// @brief Sets the width of the console window area.
       /// @param width The width of the console window measured in columns.
+      /// @return true if successfull; otherwhise false.
       /// @warning Internal use only
       static bool window_width(int_least32_t width);
       /// @}
     };
   }
 }
-
