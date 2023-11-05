91c216d0918c7378154946b5e268501f4094d136
Gammasoft <gammasoft71@gmail.com>
2023-11-05 06:37:26 +0100

    Add loading indicators example

---
+++ examples/xtd.forms.examples/controls/loading_indicators/README.md
@@ -0,0 +1,36 @@
+# loading_indicators
+
+[xtd::forms::loading_indicator](https://gammasoft71.github.io/xtd/reference_guides/latest/classxtd_1_1forms_1_1loading__indicator.html) represents a Windows control to display a loading indicator animation.
+
+# Sources
+
+* [src/loading_indicators.cpp](src/loading_indicators.cpp)
+* [CMakeLists.txt](CMakeLists.txt)
+
+# Build and run
+
+Open "Command Prompt" or "Terminal". Navigate to the folder that contains the project and type the following:
+
+```shell
+xtdc run
+```
+
+# Output
+
+## Windows :
+
+![Screenshot](../../../../docs/pictures/examples/loading_indicators_w.png)
+
+![Screenshot](../../../../docs/pictures/examples/loading_indicators_wd.png)
+
+## macOS :
+
+![Screenshot](../../../../docs/pictures/examples/loading_indicators_m.png)
+
+![Screenshot](../../../../docs/pictures/examples/loading_indicators_md.png)
+
+## Gnome :
+
+![Screenshot](../../../../docs/pictures/examples/loading_indicators_g.png)
+
+![Screenshot](../../../../docs/pictures/examples/loading_indicators_gd.png)
---
+++ examples/xtd.forms.examples/controls/loading_indicators/src/loading_indicators.cpp
@@ -0,0 +1,49 @@
+#include <xtd/forms/application>
+#include <xtd/forms/loading_indicator>
+#include <xtd/forms/form>
+#include <xtd/forms/list_box>
+#include <xtd/forms/panel>
+#include <xtd/drawing/system_colors>
+
+using namespace std;
+using namespace xtd;
+using namespace xtd::forms;
+
+class form1 : public form {
+public:
+  form1() {
+    text("Loading indicators example");
+    start_position(form_start_position::manual);
+    location({200, 200});
+    client_size({360, 240});
+    
+    list_box1.parent(*this);
+    list_box1.bounds({20, 20, 150, 200});
+    list_box1.anchor(anchor_styles::top | anchor_styles::left | anchor_styles::bottom);
+    list_box1.items().push_back_range({{"Standard", loading_indicator_style::standard}, {"System", loading_indicator_style::system}, {"Circle bars", loading_indicator_style::circle_bars}, {"Circle blinks", loading_indicator_style::circle_blinks}, {"Five lines", loading_indicator_style::five_lines}, {"Five lines center", loading_indicator_style::five_lines_center}, {"Five lines chronological", loading_indicator_style::five_lines_chronological}, {"Five lines pulse", loading_indicator_style::five_lines_pulse}, {"Five lines wave", loading_indicator_style::five_lines_wave}, {"Pulse", loading_indicator_style::pulse}, {"Pulse outline", loading_indicator_style::pulse_outline}, {"Three balls", loading_indicator_style::three_balls}, {"Three balls bouncing", loading_indicator_style::three_balls_bouncing}, {"Three balls rotation", loading_indicator_style::three_balls_rotation}});
+    list_box1.selected_index_changed += [&] {
+      loading_indicator1.loading_indicator_style(any_cast<forms::loading_indicator_style>(list_box1.selected_item().tag()));
+    };
+    list_box1.selected_item("Standard");
+    
+    panel1.parent(*this);
+    panel1.border_style(border_style::fixed_3d);
+    panel1.back_color(drawing::system_colors::window());
+    panel1.bounds({190, 20, 150, 200});
+    panel1.anchor(anchor_styles::top | anchor_styles::left | anchor_styles::bottom | anchor_styles::right);
+    
+    loading_indicator1.parent(panel1);
+    loading_indicator1.location({59, 84});
+    loading_indicator1.anchor(anchor_styles::top | anchor_styles::left | anchor_styles::bottom | anchor_styles::right);
+    loading_indicator1.running(true);
+  }
+  
+private:
+  list_box list_box1;
+  panel panel1;
+  loading_indicator loading_indicator1;
+};
+
+auto main()->int {
+  application::run(form1 {});
+}
