3fab42457bd302f604c3ad61b27d556b820bbaf2
Gammasoft <gammasoft71@gmail.com>
2023-11-06 17:44:01 +0100

    Add loading indicator bar

--- src/xtd.forms/include/xtd/forms/loading_indicator_style.h
+++ src/xtd.forms/include/xtd/forms/loading_indicator_style.h
@@ -18,8 +18,10 @@ namespace xtd {
     /// @ingroup xtd_forms
     /// @remarks If the system style is used, the appearance of the control is determined by the user's operating system and the following property values will be ignored: control_background_image, image_align, image, image_index, image_list, and text_align. In addition, the control.back_color property will be ignored for button controls. If supported, users can change the appearance of controls by adjusting the appearance settings of their operating system.
     enum class loading_indicator_style {
+      /// @brief The control appears with bar style.
+      bar = 0,
       /// @brief The control appears with circle bars style.
-      circle_bars = 0,
+      circle_bars,
       /// @brief The control appears with circle blinks style.
       circle_blinks,
       /// @brief The control appears with five lines style.
@@ -52,6 +54,6 @@ namespace xtd {
 
 /// @cond
 template<> struct xtd::enum_register<xtd::forms::loading_indicator_style> {
-  explicit operator auto() const noexcept {return xtd::enum_collection<xtd::forms::loading_indicator_style> {{xtd::forms::loading_indicator_style::circle_bars, "circle_bars"}, {xtd::forms::loading_indicator_style::circle_blinks, "circle_blinks"}, {xtd::forms::loading_indicator_style::five_lines_center, "five_lines_center"}, {xtd::forms::loading_indicator_style::five_lines_chronological, "five_lines_chronological"}, {xtd::forms::loading_indicator_style::five_lines_pulse, "five_lines_pulse"}, {xtd::forms::loading_indicator_style::five_lines_wave, "five_lines_wave"}, {xtd::forms::loading_indicator_style::five_lines, "five_lines"}, {xtd::forms::loading_indicator_style::pulse, "pulse"}, {xtd::forms::loading_indicator_style::pulse_outline, "pulse_outline"}, {xtd::forms::loading_indicator_style::standard, "standard"}, {xtd::forms::loading_indicator_style::system, "system"}, {xtd::forms::loading_indicator_style::three_balls, "three_balls"}, {xtd::forms::loading_indicator_style::three_balls_bouncing, "three_balls_bouncing"}, {xtd::forms::loading_indicator_style::three_balls_rotation, "three_balls_rotation"}};}
+  explicit operator auto() const noexcept {return xtd::enum_collection<xtd::forms::loading_indicator_style> {{xtd::forms::loading_indicator_style::bar, "bar"}, {xtd::forms::loading_indicator_style::circle_bars, "circle_bars"}, {xtd::forms::loading_indicator_style::circle_blinks, "circle_blinks"}, {xtd::forms::loading_indicator_style::five_lines_center, "five_lines_center"}, {xtd::forms::loading_indicator_style::five_lines_chronological, "five_lines_chronological"}, {xtd::forms::loading_indicator_style::five_lines_pulse, "five_lines_pulse"}, {xtd::forms::loading_indicator_style::five_lines_wave, "five_lines_wave"}, {xtd::forms::loading_indicator_style::five_lines, "five_lines"}, {xtd::forms::loading_indicator_style::pulse, "pulse"}, {xtd::forms::loading_indicator_style::pulse_outline, "pulse_outline"}, {xtd::forms::loading_indicator_style::standard, "standard"}, {xtd::forms::loading_indicator_style::system, "system"}, {xtd::forms::loading_indicator_style::three_balls, "three_balls"}, {xtd::forms::loading_indicator_style::three_balls_bouncing, "three_balls_bouncing"}, {xtd::forms::loading_indicator_style::three_balls_rotation, "three_balls_rotation"}};}
 };
 /// @endcond
