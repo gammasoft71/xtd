e12d7aba2b855e8b8648cab03cb2c54141c1d48a
Gammasoft <gammasoft71@gmail.com>
2023-11-11 21:02:41 +0100

    Update hello_world_png.cpp

--- examples/xtd.drawing.examples/hello_worlds/hello_world_png/src/hello_world_png.cpp
+++ examples/xtd.drawing.examples/hello_worlds/hello_world_png/src/hello_world_png.cpp
@@ -1,7 +1,6 @@
 #include <xtd/diagnostics/process>
 #include <xtd/drawing/bitmap>
 #include <xtd/drawing/brushes>
-#include <xtd/drawing/graphics>
 #include <xtd/drawing/system_fonts>
 #include <xtd/io/path>
 
@@ -15,7 +14,7 @@ auto main()->int {
   auto graphics = graphics::from_image(hello_world_bitmap);
   graphics.draw_string("Hello World!", {system_fonts::default_font(), 32, font_style::bold | font_style::italic}, solid_brush {color::dark(color::spring_green, 2.0 / 3)}, rectangle::offset({{0, 0}, hello_world_bitmap.size()}, {2, 2}), string_format {}.alignment(string_alignment::center).line_alignment(string_alignment::center));
   graphics.draw_string("Hello World!", {system_fonts::default_font(), 32, font_style::bold | font_style::italic}, brushes::spring_green(), rectangle::inflate({{0, 0}, hello_world_bitmap.size()}, {-2, -2}), string_format {}.alignment(string_alignment::center).line_alignment(string_alignment::center));
-  auto hello_world_png_path = path::combine(path::get_temp_path(), "hello_world_png.png");
-  hello_world_bitmap.save(hello_world_png_path);
-  process::start(hello_world_png_path);
+  auto hello_world_path = path::combine(path::get_temp_path(), "hello_world.png");
+  hello_world_bitmap.save(hello_world_path);
+  process::start(hello_world_path);
 }
