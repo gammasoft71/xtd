320f508b46a9318efcd81502479baa956553d444
gammasoft71 <gammasoft71@gmail.com>
2023-11-12 21:00:08 +0100

    update latest reference guide

---
+++ static/reference_guides/latest/html/search/pages_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['deprecated_20list_0',['Deprecated List',['../deprecated.html',1,'']]],
+  ['dialogs_20appearances_1',['Dialogs appearances',['../_dialogs_appearances_page.html',1,'']]]
+];
