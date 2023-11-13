43ae48eada76dec6a81b36d91d58e85d70ca8377
gammasoft71 <gammasoft71@gmail.com>
2023-11-13 21:00:11 +0100

    update latest reference guide

---
+++ static/reference_guides/latest/html/search/groups_4.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['format_20and_20parse_0',['format and parse',['../group__format__parse.html',1,'']]],
+  ['forms_1',['forms',['../group__forms.html',1,'(Global Namespace)'],['../group__gui.html',1,'(Global Namespace)']]]
+];
---
+++ static/reference_guides/latest/html/search/pages_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['images_20appearances_0',['Images appearances',['../_images_appearances_page.html',1,'']]]
+];
