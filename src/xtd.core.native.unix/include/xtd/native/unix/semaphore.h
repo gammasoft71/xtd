d00539a2d1fda8ca01846dd5213fb65412539eab
Gammasoft <gammasoft71@gmail.com>
2023-12-06 16:51:06 +0100

    Semaphore implementation

--- src/xtd.core.native.unix/include/xtd/native/unix/semaphore.h
+++ src/xtd.core.native.unix/include/xtd/native/unix/semaphore.h
@@ -5,12 +5,26 @@
 #endif
 /// @endcond
 
-#include <ctime>
-#include <fcntl.h>
-#include <semaphore.h>
+#include <errno.h>
 #include <unistd.h>
+#include <semaphore.h>
 
 inline static constexpr size_t PSEMNAMLEN = 31;
+
+static inline int sem_timedwait(sem_t *sem, const struct timespec *abs_timeout) {
+  while (sem_trywait(sem) == -1) {
+    if (errno == EAGAIN) {
+      struct timespec current_time;
+      clock_gettime(CLOCK_REALTIME, &current_time);
+      if (current_time.tv_sec > abs_timeout->tv_sec || (current_time.tv_sec == abs_timeout->tv_sec && current_time.tv_nsec >= abs_timeout->tv_nsec)) {
+        errno = ETIMEDOUT;
+        return -1;
+      }
+      usleep(20000);
+    } else return -1;
+  }
+  return 0;
+}
 
 static inline int sem_milliseconds_timedwait(sem_t *sem, int_least32_t milliseconds_timeout) {
   struct timespec timeout;
