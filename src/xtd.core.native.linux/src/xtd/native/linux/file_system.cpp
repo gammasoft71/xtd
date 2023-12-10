5b2453adc934315075861db1c30b3e0bb76f2d4f
gammasoft71 <gammasoft71@gmail.com>
2023-12-10 17:26:46 +0100

    Using auto

--- src/xtd.core.native.linux/src/xtd/native/linux/file_system.cpp
+++ src/xtd.core.native.linux/src/xtd/native/linux/file_system.cpp
@@ -15,10 +15,14 @@
 using namespace std;
 using namespace xtd::native;
 
+namespace {
+  using struct_stat = struct stat;
+}
+
 int_least32_t file_system::get_attributes(const std::string& path, int_least32_t& attributes) {
   struct system_attribute_to_file_attribute_converter {
     int_least32_t operator()(int_least32_t attribute) {
-      int_least32_t file_attributes = 0;
+      auto file_attributes = 0;
       if ((attribute & S_IRUSR) == S_IRUSR && (attribute & S_IWUSR) != S_IWUSR) file_attributes |= FILE_ATTRIBUTE_READONLY;
       if ((attribute & S_IFSOCK) == S_IFSOCK || (attribute & S_IFIFO) == S_IFIFO) file_attributes |= FILE_ATTRIBUTE_SYSTEM;
       if ((attribute & S_IFDIR) == S_IFDIR) file_attributes |= FILE_ATTRIBUTE_DIRECTORY;
@@ -28,16 +32,16 @@ int_least32_t file_system::get_attributes(const std::string& path, int_least32_t
       return file_attributes;
     }
   };
-  struct stat status;
+  auto status = struct_stat {};
   if (stat(path.c_str(), &status) != 0) return -1;
   attributes = system_attribute_to_file_attribute_converter()(status.st_mode);
   return 0;
 }
 
 int_least32_t file_system::get_file_times(const std::string& path, time_t& creation_time, time_t& last_access_time, time_t& last_write_time) {
-  struct stat status;
+  auto status = struct_stat {};
   if (stat(path.c_str(), &status) != 0) return -1;
-  
+
   creation_time = static_cast<time_t>(status.st_ctime);
   last_access_time = static_cast<time_t>(status.st_atime);
   last_write_time = static_cast<time_t>(status.st_mtime);
@@ -45,88 +49,88 @@ int_least32_t file_system::get_file_times(const std::string& path, time_t& creat
 }
 
 string file_system::get_full_path(const string& relative_path) {
-  vector<string> directories = native::linux::strings::split(relative_path, {path::directory_separator_char()}, std::numeric_limits<size_t>::max(), true);
-  string full_path;
-  
+  auto directories = native::linux::strings::split(relative_path, {path::directory_separator_char()}, std::numeric_limits<size_t>::max(), true);
+  auto full_path = string {};
+
   if (relative_path[0] != path::directory_separator_char())
     full_path = directory::get_current_directory();
-    
-  for (const string& item : directories) {
+
+  for (auto item : directories) {
     if (item == ".." && native::linux::strings::last_index_of(full_path, path::directory_separator_char()) != full_path.npos)
       full_path = native::linux::strings::remove(full_path, native::linux::strings::last_index_of(full_path, path::directory_separator_char()));
     else if (item != ".")
       full_path += path::directory_separator_char() + item;
   }
-  
+
   if (relative_path[relative_path.size() - 1] == path::directory_separator_char())
     full_path += path::directory_separator_char();
-    
+
   auto index = native::linux::strings::last_index_of(full_path, "/./");
   while (index != full_path.npos) {
     full_path = native::linux::strings::remove(full_path, index, 2);
     index = native::linux::strings::last_index_of(full_path, "/./");
   }
-  
+
   return full_path;
 }
 
 int_least32_t file_system::get_permissions(const std::string& path, int_least32_t& permissions) {
   struct system_permission_to_file_permission_converter {
     int_least32_t operator()(mode_t permission) {
-      int_least32_t file_permissions = 0;
+      auto file_permissions = 0;
       if ((permission & S_IRUSR) == S_IRUSR) file_permissions |= FILE_PERMISSIONS_OWNER_READ;
       if ((permission & S_IWUSR) == S_IWUSR) file_permissions |= FILE_PERMISSIONS_OWNER_WRITE;
       if ((permission & S_IXUSR) == S_IXUSR) file_permissions |= FILE_PERMISSIONS_OWNER_EXECUTE;
-      
+
       if ((permission & S_IRGRP) == S_IRGRP) file_permissions |= FILE_PERMISSIONS_GROUP_READ;
       if ((permission & S_IXGRP) == S_IXGRP) file_permissions |= FILE_PERMISSIONS_GROUP_WRITE;
       if ((permission & S_IXGRP) == S_IXGRP) file_permissions |= FILE_PERMISSIONS_GROUP_EXECUTE;
-      
+
       if ((permission & S_IROTH) == S_IROTH) file_permissions |= FILE_PERMISSIONS_OTHERS_READ;
       if ((permission & S_IWOTH) == S_IWOTH) file_permissions |= FILE_PERMISSIONS_OTHERS_WRITE;
       if ((permission & S_IXOTH) == S_IXOTH) file_permissions |= FILE_PERMISSIONS_OTHERS_EXECUTE;
-      
+
       if ((permission & S_ISUID) == S_ISUID) file_permissions |= FILE_PERMISSIONS_SET_UID;
       if ((permission & S_ISGID) == S_ISGID) file_permissions |= FILE_PERMISSIONS_SET_GID;
       if ((permission & S_ISVTX) == S_ISVTX) file_permissions |= FILE_PERMISSIONS_STICKY_BIT;
-      
+
       if (file_permissions == 0) file_permissions = FILE_PERMISSIONS_UNKNOWN;
-      
+
       return file_permissions;
     }
   };
-  struct stat s;
-  int_least32_t ret_value = stat(path.c_str(), &s);
+  auto s = struct_stat {};
+  auto ret_value = stat(path.c_str(), &s);
   permissions = system_permission_to_file_permission_converter()(s.st_mode);
   return ret_value;
 }
 
 bool file_system::is_path_too_long(const std::string& path) {
   if (path.size() > PATH_MAX) return true;
-  
-  size_t index = path.rfind(native::path::directory_separator_char());
+
+  auto index = path.rfind(native::path::directory_separator_char());
   if (index == static_cast<size_t>(-1)) index = path.rfind(native::path::alt_directory_separator_char());
   auto file_name = (index == static_cast<size_t>(-1)) ? path : &path[index + 1];
   return file_name.size() > NAME_MAX;
 }
 
 int_least32_t file_system::set_attributes(const std::string& path, int_least32_t attributes) {
-  struct stat s;
+  auto s = struct_stat {};
   if (stat(path.c_str(), &s) != 0) return -1;
-  
+
   if ((attributes & FILE_ATTRIBUTE_READONLY) != FILE_ATTRIBUTE_READONLY)
     s.st_mode |= S_IWUSR;
   else if ((attributes & FILE_ATTRIBUTE_READONLY) == FILE_ATTRIBUTE_READONLY)
     s.st_mode &= ~S_IWUSR;
-    
+
   // The other attributes can be modified under linux.
-  
+
   return chmod(path.c_str(), s.st_mode);
 }
 
 int_least32_t file_system::set_creation_time(const std::string& path, time_t creation_time) {
   // There is no creation time on linux so we update the last modification time instead...
-  utimbuf times;
+  auto times = utimbuf {};
   time_t creation_time_old = 0, last_access_time = 0, last_write_time = 0;
   get_file_times(path, creation_time_old, last_access_time, last_write_time);
   times.actime = last_access_time;
@@ -135,7 +139,7 @@ int_least32_t file_system::set_creation_time(const std::string& path, time_t cre
 }
 
 int_least32_t file_system::set_last_access_time(const std::string& path, time_t last_access_time) {
-  utimbuf times;
+  auto times = utimbuf {};
   time_t creation_time = 0, last_access_time_old = 0, last_write_time = 0;
   get_file_times(path, creation_time, last_access_time_old, last_write_time);
   times.actime = last_access_time;
@@ -144,7 +148,7 @@ int_least32_t file_system::set_last_access_time(const std::string& path, time_t 
 }
 
 int_least32_t file_system::set_last_write_time(const std::string& path, time_t last_write_time) {
-  utimbuf times;
+  auto times = utimbuf {};
   time_t creation_time = 0, last_access_time = 0, last_write_time_old = 0;
   get_file_times(path, creation_time, last_access_time, last_write_time_old);
   times.actime = last_access_time;
@@ -155,23 +159,23 @@ int_least32_t file_system::set_last_write_time(const std::string& path, time_t l
 int_least32_t file_system::set_permissions(const std::string& path, int_least32_t permissions) {
   struct file_permission_to_system_permission_converter {
     mode_t operator()(int_least32_t permission) {
-      int_least32_t system_permissions = 0;
+      auto system_permissions = 0;
       if ((permission & FILE_PERMISSIONS_OWNER_READ) == FILE_PERMISSIONS_OWNER_READ) system_permissions |= S_IRUSR;
       if ((permission & FILE_PERMISSIONS_OWNER_WRITE) == FILE_PERMISSIONS_OWNER_WRITE) system_permissions |= S_IWUSR;
       if ((permission & FILE_PERMISSIONS_OWNER_EXECUTE) == FILE_PERMISSIONS_OWNER_EXECUTE) system_permissions |= S_IXUSR;
-      
+
       if ((permission & FILE_PERMISSIONS_GROUP_READ) == FILE_PERMISSIONS_GROUP_READ) system_permissions |= S_IRGRP;
       if ((permission & FILE_PERMISSIONS_GROUP_WRITE) == FILE_PERMISSIONS_GROUP_WRITE) system_permissions |= S_IWGRP;
       if ((permission & FILE_PERMISSIONS_GROUP_EXECUTE) == FILE_PERMISSIONS_GROUP_EXECUTE) system_permissions |= S_IXGRP;
-      
+
       if ((permission & FILE_PERMISSIONS_OTHERS_READ) == FILE_PERMISSIONS_OTHERS_READ) system_permissions |= S_IROTH;
       if ((permission & FILE_PERMISSIONS_OTHERS_WRITE) == FILE_PERMISSIONS_OTHERS_WRITE) system_permissions |= S_IWOTH;
       if ((permission & FILE_PERMISSIONS_OTHERS_EXECUTE) == FILE_PERMISSIONS_OTHERS_EXECUTE) system_permissions |= S_IXOTH;
-      
+
       if ((permission & FILE_PERMISSIONS_SET_UID) == FILE_PERMISSIONS_SET_UID) system_permissions |= S_ISUID;
       if ((permission & FILE_PERMISSIONS_SET_GID) == FILE_PERMISSIONS_SET_GID) system_permissions |= S_ISGID;
       if ((permission & FILE_PERMISSIONS_STICKY_BIT) == FILE_PERMISSIONS_STICKY_BIT) system_permissions |= S_ISVTX;
-      
+
       return system_permissions;
     }
   };
