acfbf5d51b8be7a74d22cbe8dbb3d10b638eba7b
gammasoft71 <gammasoft71@gmail.com>
2023-12-10 17:26:28 +0100

    Using auto

--- src/xtd.core.native.linux/src/xtd/native/linux/dns.cpp
+++ src/xtd.core.native.linux/src/xtd/native/linux/dns.cpp
@@ -27,7 +27,7 @@ void dns::destroy(intmax_t host) {
 }
 
 intmax_t dns::get_host_by_address(const string& host_address, int_least32_t host_address_type) {
-  lock_guard<mutex> lock(dns_mutex);
+  auto lock = lock_guard<mutex> {dns_mutex};
   int_least64_t internet_address = 0;
   inet_pton(host_address_type, host_address.c_str(), &internet_address);
   hostent* host = gethostbyaddr(reinterpret_cast<char*>(&internet_address), host_address_type == ADDRESS_FAMILY_INTER_NETWORK ? 4 : 16, host_address_type);
@@ -36,23 +36,23 @@ intmax_t dns::get_host_by_address(const string& host_address, int_least32_t host
 }
 
 intmax_t dns::get_host_by_name(const string& host_name) {
-  lock_guard<mutex> lock(dns_mutex);
+  auto lock = lock_guard<mutex> {dns_mutex};
   hostent* host = gethostbyname(host_name.c_str());
   if (host == nullptr) return 0;
   return reinterpret_cast<intmax_t>(new hostent(*host));
 }
 
 vector<string> dns::get_aliases(intmax_t host) {
-  vector<string> aliases;
-  size_t index = 0;
+  auto aliases = vector<string> {};
+  auto index = size_t {0};
   while (reinterpret_cast<hostent*>(host)->h_aliases[index] != nullptr)
     aliases.push_back(reinterpret_cast<hostent*>(host)->h_aliases[index++]);
   return aliases;
 }
 
 vector<vector<uint_least8_t>> dns::get_addresses(intmax_t host) {
-  size_t index = 0;
-  vector<vector<uint_least8_t>> addresses;
+  auto addresses = vector<vector<uint_least8_t>> {};
+  auto index = size_t {0};
   while (reinterpret_cast<hostent*>(host)->h_addr_list[index] != nullptr) {
     addresses.emplace_back(reinterpret_cast<const uint_least8_t*>(reinterpret_cast<hostent*>(host)->h_addr_list[index]), reinterpret_cast<const uint_least8_t*>(reinterpret_cast<hostent*>(host)->h_addr_list[index]) + (reinterpret_cast<hostent*>(host)->h_addrtype == ADDRESS_FAMILY_INTER_NETWORK ? 4 : 16));
     index++;
@@ -66,7 +66,7 @@ string dns::get_host_name(intmax_t host) {
 
 int_least32_t dns::get_host_name(string& host_name) {
   char name[1024];
-  int_least32_t result = gethostname(name, 1024);
+  auto result = gethostname(name, 1024);
   host_name = name;
   return result;
 }
