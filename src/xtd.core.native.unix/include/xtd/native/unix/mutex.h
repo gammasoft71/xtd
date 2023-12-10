d1b767070cb4e5969713ee12524d192a7827d762
Gammasoft <gammasoft71@gmail.com>
2023-12-10 11:20:18 +0100

    Fix build on FreeBSD

--- src/xtd.core.native.unix/include/xtd/native/unix/mutex.h
+++ src/xtd.core.native.unix/include/xtd/native/unix/mutex.h
@@ -11,21 +11,6 @@
 
 inline static const pthread_mutex_t* MUTEX_FAILED = reinterpret_cast<pthread_mutex_t *>(-1);
 
-static inline int pthread_mutex_timedlock(pthread_mutex_t* mut, const struct timespec *abs_timeout) {
-  while (pthread_mutex_trylock(mut) != 0) {
-    if (errno == EBUSY) {
-      struct timespec current_time;
-      clock_gettime(CLOCK_REALTIME, &current_time);
-      if (current_time.tv_sec > abs_timeout->tv_sec || (current_time.tv_sec == abs_timeout->tv_sec && current_time.tv_nsec >= abs_timeout->tv_nsec)) {
-        errno = ETIMEDOUT;
-        return -1;
-      }
-      usleep(20000);
-    } else return -1;
-  }
-  return 0;
-}
-
 static inline int pthread_mutex_milliseconds_timedlock(pthread_mutex_t* mut, int_least32_t milliseconds_timeout) {
   struct timespec timeout;
   clock_gettime(CLOCK_REALTIME, &timeout);
