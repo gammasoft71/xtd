6151eeb485dbcba2216fcd1eb148a0645d02e6d4
Gammasoft <gammasoft71@gmail.com>
2023-12-10 10:55:27 +0100

    Update to unix

--- src/xtd.core.native/include/xtd/native/value_types.h
+++ src/xtd.core.native/include/xtd/native/value_types.h
@@ -10,13 +10,13 @@
 
 #include <cstdint>
 
-#if defined(__linux__) && defined(_LP64)
+#if (defined(__linux__) || defined(__unix__ )) && defined(_LP64)
 /// @brief Represents a 32-bit or 64-bit signed integer.
 /// @warning Internal use only
-using __slong__ = signed long long int; // Workaround: On linux 64 bits unfortunately 'int_least64_t' is an alias on 'long' and not on 'long long int' as other platforms. In order to avoid conflicts in operator or method overloads, it is necessary to give the type that is not taken into account.
+using __slong__ = signed long long int; // Workaround: On Linux and Unix 64 bits unfortunately 'int_least64_t' is an alias on 'long' and not on 'long long int' as other platforms. In order to avoid conflicts in operator or method overloads, it is necessary to give the type that is not taken into account.
 /// @brief Represents a 32-bit or 64-bit unsigned integer.
 /// @warning Internal use only
-using __ulong__ = unsigned long long int; // Workaround: On linux 64 bits unfortunately 'uint_least64_t' is an alias on 'unigned long' and not on 'unsigned long long int' as other platforms. In order to avoid conflicts in operator or method overloads, it is necessary to give the type that is not taken into account.
+using __ulong__ = unsigned long long int; // Workaround: On Linux and Unix 64 bits unfortunately 'uint_least64_t' is an alias on 'unigned long' and not on 'unsigned long long int' as other platforms. In order to avoid conflicts in operator or method overloads, it is necessary to give the type that is not taken into account.
 #else
 /// @brief Represents a 32-bit or 64-bit signed integer.
 /// @warning Internal use only
