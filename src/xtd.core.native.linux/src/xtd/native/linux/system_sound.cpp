f11d3669cb26a438dc59ec774641f1aa2515bf6d
gammasoft71 <gammasoft71@gmail.com>
2023-12-10 17:27:29 +0100

    Using auto

--- src/xtd.core.native.linux/src/xtd/native/linux/system_sound.cpp
+++ src/xtd.core.native.linux/src/xtd/native/linux/system_sound.cpp
@@ -11,8 +11,8 @@ using namespace xtd::native;
 #if defined(__XTD_USE_GSOUND__)
 #include <gsound.h>
 void system_sound::play(uint_least32_t sound) {
-  static map<uint_least32_t, string> sounds = {{ST_BEEP, "bell-window-system"}, {ST_ICONERROR, "dialog-error"}, {ST_ICONQUESTION, "dialog-question"}, {ST_ICONWARNING, "dialog-warning"}, {ST_ICONINFORMATION, "dialog-information"}, {ST_OK, "bell-window-system"}};
-  static GSoundContext* gsound_context = gsound_context_new(nullptr, nullptr);
+  static auto sounds = map<uint_least32_t, string> {{ST_BEEP, "bell-window-system"}, {ST_ICONERROR, "dialog-error"}, {ST_ICONQUESTION, "dialog-question"}, {ST_ICONWARNING, "dialog-warning"}, {ST_ICONINFORMATION, "dialog-information"}, {ST_OK, "bell-window-system"}};
+  static auto gsound_context = gsound_context_new(nullptr, nullptr);
   gsound_context_play_simple(gsound_context, nullptr, nullptr, GSOUND_ATTR_EVENT_ID, sounds[sound].c_str(), nullptr);
 }
 #else
