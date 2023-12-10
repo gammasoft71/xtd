3966c3e391d66b3b31c2ddbbde9f88bb361386a1
gammasoft71 <gammasoft71@gmail.com>
2023-12-10 17:26:11 +0100

    Using auto

--- src/xtd.core.native.linux/src/xtd/native/linux/cryptography.cpp
+++ src/xtd.core.native.linux/src/xtd/native/linux/cryptography.cpp
@@ -10,10 +10,10 @@ using namespace xtd::native;
 namespace {
   static std::string create_process(const std::string& command) {
     auto fs = popen(command.c_str(), "r");
-    std::string result;
+    auto result = string {};
     while (!feof(fs)) {
       char buf[513];
-      size_t l = fread(buf, 1, 512, fs);
+      auto l = fread(buf, 1, 512, fs);
       buf[l] = 0;
       result += buf;
     }
@@ -23,16 +23,16 @@ namespace {
 }
 
 vector<uint_least8_t> cryptography::machine_guid() {
-  static string guid_str = create_process("{ uname -n ; cat /proc/meminfo | head -n1 ; cat /proc/cpuinfo ; } | md5sum");
-  
-  static const string guid_fallback = "30395f0ed6aa4a5eb4af6f90a608c605";
-  static const string hex_chars = "0123456789ABCDEF";
-  for (auto index = 0U; guid_str[index] != 0; ++index)
+  static auto guid_str = create_process("{ uname -n ; cat /proc/meminfo | head -n1 ; cat /proc/cpuinfo ; } | md5sum");
+
+  static const auto guid_fallback = string {"30395f0ed6aa4a5eb4af6f90a608c605"};
+  static const auto hex_chars = string {"0123456789ABCDEF"};
+  for (auto index = 0u; guid_str[index] != 0; ++index)
     if (hex_chars.find(static_cast<char>(toupper(guid_str[index]))) == hex_chars.npos)  guid_str.erase(index--, 1);
   if (guid_str.size() != 32) guid_str = guid_fallback;
-  
-  vector<uint_least8_t> bytes;
-  for (auto index = 0U; index < guid_str.size(); index += 2)
+
+  auto bytes = vector<uint_least8_t> {};
+  for (auto index = 0u; index < guid_str.size(); index += 2)
     bytes.push_back(static_cast<uint_least8_t>(stoi(guid_str.substr(index, 2), 0, 16)));
   return bytes;
 }
