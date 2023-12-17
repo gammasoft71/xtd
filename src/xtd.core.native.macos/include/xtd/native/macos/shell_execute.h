58c09eab591d85c468937a0a0a2377bc1053886c
Gammasoft <gammasoft71@gmail.com>
2023-12-17 13:44:21 +0100

    Add shell_execute

---
+++ src/xtd.core.native.macos/include/xtd/native/macos/shell_execute.h
@@ -0,0 +1,35 @@
+#pragma once
+/// @cond
+#if !defined(__XTD_CORE_NATIVE_LIBRARY__)
+#error "Do not include this file: Internal use only"
+#endif
+/// @endcond
+
+#include <cstdio>
+#include <string>
+
+namespace xtd::native::macos {
+  class shell_execute final {
+  public:
+    static std::string run(const std::string& file_name) {return run(file_name, {});}
+    static std::string run(const std::string& file_name, const std::string& arguments) {
+      const auto command = file_name + (arguments.empty() ? "" : (" " + arguments));
+      auto file_stream = popen(command.c_str(), "r");
+      if (file_stream == nullptr) return {};
+
+      auto result = std::string {};
+      while (!feof(file_stream)) {
+        auto buffer = std::string(buffer_size, '\0');
+        fread(buffer.data(), 1, buffer.size(), file_stream);
+        result += buffer;
+      }
+
+      pclose(file_stream);
+      return result;
+    }
+
+  private:
+    shell_execute() = delete;
+    static constexpr size_t buffer_size = 128;
+  };
+}
