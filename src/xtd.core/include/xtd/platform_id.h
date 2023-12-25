09b884f655f39a349a04c662374168c80ec29e9e
Gammasoft <gammasoft71@gmail.com>
2023-12-25 15:36:03 +0100

    Add Posix id

--- src/xtd.core/include/xtd/platform_id.h
+++ src/xtd.core/include/xtd/platform_id.h
@@ -48,6 +48,8 @@ namespace xtd {
     mingw,
     /// @brief The operating system is MSYS.
     msys,
+    /// @brief The operating system is Posix.
+    posix,
     /// @brief The operating system is other.
     other = 32767,
   };
@@ -55,6 +57,6 @@ namespace xtd {
 
 /// @cond
 template<> struct xtd::enum_register<xtd::platform_id> {
-  explicit operator auto() const noexcept {return xtd::enum_collection<xtd::platform_id> {{xtd::platform_id::unknown, "unknown"}, {xtd::platform_id::win32s, "win32s"}, {xtd::platform_id::win32_windows, "win32_windows"}, {xtd::platform_id::win32_nt, "win32_nt"}, {xtd::platform_id::win_ce, "win_ce"}, {xtd::platform_id::unix, "unix"}, {xtd::platform_id::xbox, "xbox"}, {xtd::platform_id::macos, "macos"}, {xtd::platform_id::ios, "ios"}, {xtd::platform_id::android, "android"}, {xtd::platform_id::linux, "linux"}, {xtd::platform_id::tvos, "tvos"}, {xtd::platform_id::watchos, "watchos"}, {xtd::platform_id::free_bsd, "free_bsd"}, {xtd::platform_id::mingw, "mingw"}, {xtd::platform_id::msys, "msys"}, {xtd::platform_id::other, "other"}};}
+  explicit operator auto() const noexcept {return xtd::enum_collection<xtd::platform_id> {{xtd::platform_id::unknown, "unknown"}, {xtd::platform_id::win32s, "win32s"}, {xtd::platform_id::win32_windows, "win32_windows"}, {xtd::platform_id::win32_nt, "win32_nt"}, {xtd::platform_id::win_ce, "win_ce"}, {xtd::platform_id::unix, "unix"}, {xtd::platform_id::xbox, "xbox"}, {xtd::platform_id::macos, "macos"}, {xtd::platform_id::ios, "ios"}, {xtd::platform_id::android, "android"}, {xtd::platform_id::linux, "linux"}, {xtd::platform_id::tvos, "tvos"}, {xtd::platform_id::watchos, "watchos"}, {xtd::platform_id::free_bsd, "free_bsd"}, {xtd::platform_id::mingw, "mingw"}, {xtd::platform_id::msys, "msys"}, {xtd::platform_id::posix, "posix"}, {xtd::platform_id::other, "other"}};}
 };
 /// @endcond
