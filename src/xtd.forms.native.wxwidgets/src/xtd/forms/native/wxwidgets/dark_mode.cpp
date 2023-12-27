401180557bd1f090f6844bf197d0ebdec596a016
Gammasoft <gammasoft71@gmail.com>
2023-12-27 04:22:37 +0100

    remove unused methods

--- src/xtd.forms.native.wxwidgets/src/xtd/forms/native/wxwidgets/dark_mode.cpp
+++ src/xtd.forms.native.wxwidgets/src/xtd/forms/native/wxwidgets/dark_mode.cpp
@@ -29,50 +29,14 @@ namespace {
     auto dataDir = ntHdr->OptionalHeader.DataDirectory;
     return RVA2VA<T>(moduleBase, dataDir[entryID].VirtualAddress);
   }
-  
-  /* Unused method
-  PIMAGE_THUNK_DATA FindAddressByName(void* moduleBase, PIMAGE_THUNK_DATA impName, PIMAGE_THUNK_DATA impAddr, const char* funcName) {
-    for (; impName->u1.Ordinal; ++impName, ++impAddr) {
-      if (IMAGE_SNAP_BY_ORDINAL(impName->u1.Ordinal)) continue;
-      auto import = RVA2VA<PIMAGE_IMPORT_BY_NAME>(moduleBase, impName->u1.AddressOfData);
-      if (strcmp(import->Name, funcName) != 0) continue;
-      return impAddr;
-    }
-    return nullptr;
-  }
-  */
-  
+    
   PIMAGE_THUNK_DATA FindAddressByOrdinal(void* moduleBase, PIMAGE_THUNK_DATA impName, PIMAGE_THUNK_DATA impAddr, uint16 ordinal) {
     for (; impName->u1.Ordinal; ++impName, ++impAddr) {
       if (IMAGE_SNAP_BY_ORDINAL(impName->u1.Ordinal) && IMAGE_ORDINAL(impName->u1.Ordinal) == ordinal) return impAddr;
     }
     return nullptr;
   }
-  
-  /* Unused methods
-  PIMAGE_THUNK_DATA FindIatThunkInModule(void* moduleBase, const char* dllName, const char* funcName) {
-    auto imports = DataDirectoryFromModuleBase<PIMAGE_IMPORT_DESCRIPTOR>(moduleBase, IMAGE_DIRECTORY_ENTRY_IMPORT);
-    for (; imports->Name; ++imports) {
-      if (_stricmp(RVA2VA<LPCSTR>(moduleBase, imports->Name), dllName) != 0) continue;
-      auto origThunk = RVA2VA<PIMAGE_THUNK_DATA>(moduleBase, imports->OriginalFirstThunk);
-      auto thunk = RVA2VA<PIMAGE_THUNK_DATA>(moduleBase, imports->FirstThunk);
-      return FindAddressByName(moduleBase, origThunk, thunk, funcName);
-    }
-    return nullptr;
-  }
-  
-  PIMAGE_THUNK_DATA FindDelayLoadThunkInModule(void* moduleBase, const char* dllName, const char* funcName) {
-    auto imports = DataDirectoryFromModuleBase<PIMAGE_DELAYLOAD_DESCRIPTOR>(moduleBase, IMAGE_DIRECTORY_ENTRY_DELAY_IMPORT);
-    for (; imports->DllNameRVA; ++imports) {
-      if (_stricmp(RVA2VA<LPCSTR>(moduleBase, imports->DllNameRVA), dllName) != 0) continue;
-      auto impName = RVA2VA<PIMAGE_THUNK_DATA>(moduleBase, imports->ImportNameTableRVA);
-      auto impAddr = RVA2VA<PIMAGE_THUNK_DATA>(moduleBase, imports->ImportAddressTableRVA);
-      return FindAddressByName(moduleBase, impName, impAddr, funcName);
-    }
-    return nullptr;
-  }
-  */
-  
+ 
   PIMAGE_THUNK_DATA FindDelayLoadThunkInModule(void* moduleBase, const char* dllName, uint16 ordinal) {
     auto imports = DataDirectoryFromModuleBase<PIMAGE_DELAYLOAD_DESCRIPTOR>(moduleBase, IMAGE_DIRECTORY_ENTRY_DELAY_IMPORT);
     for (; imports->DllNameRVA; ++imports) {
