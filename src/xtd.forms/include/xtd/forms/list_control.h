c00e55bb127f275e3d46dfba6224048fa38818f3
stevexuzeta <stevexuzeta@outlook.com>
2024-01-02 15:08:17 +0800

    Update list_control.h
    add #include <algorithm>

--- src/xtd.forms/include/xtd/forms/list_control.h
+++ src/xtd.forms/include/xtd/forms/list_control.h
@@ -4,6 +4,8 @@
 #pragma once
 #include "control.h"
 
+#include <algorithm>
+
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
   /// @brief The xtd::forms namespace contains classes for creating Windows-based applications that take full advantage of the rich user interface features available in the Microsoft Windows operating system, Apple macOS and Linux like Ubuntu operating system.
