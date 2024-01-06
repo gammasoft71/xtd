e079b2a33ae8bf573b30565aba9deb87ddf79963
Gammasoft <gammasoft71@gmail.com>
2024-01-06 21:35:44 +0100

    Using auto

--- examples/xtd.tunit.examples/valids/valid_are_equal/src/valid_are_equal.cpp
+++ examples/xtd.tunit.examples/valids/valid_are_equal/src/valid_are_equal.cpp
@@ -6,12 +6,12 @@ namespace unit_tests {
   class test_class_(test) {
   public:
     void test_method_(test_case_succeed) {
-      int i = 24;
+      auto i = 24;
       valid::are_equal(24, i);
     }
     
     void test_method_(test_case_failed) {
-      int i = 24;
+      auto i = 24;
       valid::are_equal(23, i);
     }
   };
