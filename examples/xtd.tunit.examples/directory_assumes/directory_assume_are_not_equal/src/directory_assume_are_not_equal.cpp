45ea6f602f7a9e0c43f60c493b2efadd8b409a77
Gammasoft <gammasoft71@gmail.com>
2024-01-07 17:37:47 +0100

    Review code

--- examples/xtd.tunit.examples/directory_assumes/directory_assume_are_not_equal/src/directory_assume_are_not_equal.cpp
+++ examples/xtd.tunit.examples/directory_assumes/directory_assume_are_not_equal/src/directory_assume_are_not_equal.cpp
@@ -8,7 +8,7 @@ namespace unit_tests {
   class test_class_(test) {
   public:
     void test_method_(test_case_succeed) {
-      auto di = directory_info(environment::get_folder_path(environment::special_folder::system));
+      auto di = directory_info {environment::get_folder_path(environment::special_folder::system)};
       directory_assume::are_not_equal(directory_info {path::get_temp_path()}, di);
     }
     
