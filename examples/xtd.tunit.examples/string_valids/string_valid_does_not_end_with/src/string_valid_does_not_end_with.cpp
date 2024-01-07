613f2f31e73400d4f02b1ac943f6d4dc9e4b9bf2
Gammasoft <gammasoft71@gmail.com>
2024-01-07 19:35:23 +0100

    Review code

--- examples/xtd.tunit.examples/string_valids/string_valid_does_not_end_with/src/string_valid_does_not_end_with.cpp
+++ examples/xtd.tunit.examples/string_valids/string_valid_does_not_end_with/src/string_valid_does_not_end_with.cpp
@@ -1,15 +1,13 @@
 #include <xtd/xtd.tunit>
-#include <string>
 
-using namespace std;
-using namespace std::string_literals;
+using namespace xtd;
 using namespace xtd::tunit;
 
 namespace unit_tests {
   class test_class_(test) {
   public:
     void test_method_(test_case_succeed) {
-      auto s = "value"s;
+      auto s = "value"_s;
       string_valid::does_not_end_with("val", s);
     }
     
