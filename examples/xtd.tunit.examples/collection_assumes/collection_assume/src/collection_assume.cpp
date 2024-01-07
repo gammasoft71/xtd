868d7ee859fe7c9239e677fc32d590d782b33cb4
Gammasoft <gammasoft71@gmail.com>
2024-01-07 17:16:12 +0100

    Using auto

--- examples/xtd.tunit.examples/collection_assumes/collection_assume/src/collection_assume.cpp
+++ examples/xtd.tunit.examples/collection_assumes/collection_assume/src/collection_assume.cpp
@@ -12,11 +12,11 @@ namespace unit_tests {
     }
     
     void test_method_(test_case2) {
-      collection_assume::is_empty(vector<int> {1, 2, 3});
+      collection_assume::is_empty(vector {1, 2, 3});
     }
     
     void test_method_(test_case3) {
-      collection_assume::is_ordered(vector<int> {1, 3, 2});
+      collection_assume::is_ordered(vector {1, 3, 2});
     }
   };
 }
