e9a8ccb3944099062004fabbca3fcccfc12d9ec5
Gammasoft <gammasoft71@gmail.com>
2024-01-07 17:16:38 +0100

    Using auto

--- examples/xtd.tunit.examples/collection_assumes/collection_assume_are_not_equal/src/collection_assume_are_not_equal.cpp
+++ examples/xtd.tunit.examples/collection_assumes/collection_assume_are_not_equal/src/collection_assume_are_not_equal.cpp
@@ -1,17 +1,18 @@
 #include <xtd/xtd.tunit>
 
+using namespace std;
 using namespace xtd::tunit;
 
 namespace unit_tests {
   class test_class_(test) {
   public:
     void test_method_(test_case_succeed) {
-      std::vector a = {1, 2, 3, 4};
+      auto a = vector {1, 2, 3, 4};
       collection_assume::are_not_equal({4, 3, 2, 1}, a);
     }
     
     void test_method_(test_case_aborted) {
-      std::vector a = {1, 2, 3, 4};
+      auto a = vector {1, 2, 3, 4};
       collection_assume::are_not_equal({1, 2, 3, 4}, a);
     }
   };
