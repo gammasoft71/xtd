9dd7a4f32a0d8e50df01cee0f9aa53a3ee24b593
Gammasoft <gammasoft71@gmail.com>
2024-01-07 17:25:22 +0100

    Using auto

--- examples/xtd.tunit.examples/collection_valids/collection_valid_all_items_are_unique/src/collection_valid_all_items_are_unique.cpp
+++ examples/xtd.tunit.examples/collection_valids/collection_valid_all_items_are_unique/src/collection_valid_all_items_are_unique.cpp
@@ -1,17 +1,18 @@
 #include <xtd/xtd.tunit>
 
+using namespace std;
 using namespace xtd::tunit;
 
 namespace unit_tests {
   class test_class_(test) {
   public:
     void test_method_(test_case_succeed) {
-      std::vector a = {1, 2, 3, 4};
+      auto a = vector {1, 2, 3, 4};
       collection_valid::all_items_are_unique(a);
     }
     
     void test_method_(test_case_failed) {
-      std::vector a = {1, 2, 3, 4, 1};
+      auto a = vector {1, 2, 3, 4, 1};
       collection_valid::all_items_are_unique(a);
     }
   };
