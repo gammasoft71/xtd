170fd9bf81414411f9fdce5997008a6b0285e405
Gammasoft <gammasoft71@gmail.com>
2024-01-14 14:40:00 +0100

    Update manual_tests.cpp

--- tests/xtd.core.manual_tests/src/manual_tests.cpp
+++ tests/xtd.core.manual_tests/src/manual_tests.cpp
@@ -1,21 +1,25 @@
-#include <xtd/as>
-#include <xtd/is>
 #include <xtd/console>
-#include <xtd/date_time>
-#include <xtd/day_of_week>
-#include <xtd/convert_string>
-#include <xtd/typeof>
-#include <xtd/translator>
+#include <xtd/environment>
 
-using namespace std;
 using namespace xtd;
 
 auto main()->int {
-  console::write_line("locale = {}", translator::locale().name());
-  translator::language("fr_CH");
-  translator::locale("zh_CN");
-  console::write_line("locale = {}", translator::locale().name());
-  
-  auto str = translator::translate("&Open...");
-  console::write_line(str);
+  if (environment::os_version().is_64_bit()) console::write_line("64 bit");
+  if (environment::os_version().is_posix_platform()) console::write_line("Posix platform");
+  if (environment::os_version().is_macos_platform()) console::write_line("macOS platform");
+  if (environment::os_version().is_unix_platform()) console::write_line("Unix platform");
+  if (environment::os_version().is_windows_platform()) console::write_line("Windows platform");
+  if (environment::os_version().is_android()) console::write_line("Android");
+  if (environment::os_version().is_free_bsd()) console::write_line("FreeBSD");
+  if (environment::os_version().is_ios()) console::write_line("iOS");
+  if (environment::os_version().is_linux()) console::write_line("Linux");
+  if (environment::os_version().is_macos()) console::write_line("macOS");
+  if (environment::os_version().is_mingw()) console::write_line("MINGW");
+  if (environment::os_version().is_msys()) console::write_line("MSYS");
+  if (environment::os_version().is_posix()) console::write_line("Posix");
+  if (environment::os_version().is_tvos()) console::write_line("tvOS");
+  if (environment::os_version().is_watchos()) console::write_line("watchOS");
+  if (environment::os_version().is_windows()) console::write_line("Windows");
+  if (environment::os_version().is_windows_ce()) console::write_line("Windows CE");
+  if (environment::os_version().is_xbox()) console::write_line("xbox");
 }
