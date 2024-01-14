fa6c9e7b05aa7d361900a5cec91fca5c2dd82eae
Gammasoft <gammasoft71@gmail.com>
2024-01-14 14:40:05 +0100

    Update manual_tests.cpp

--- tests/xtd.tunit.manual_tests/src/manual_tests.cpp
+++ tests/xtd.tunit.manual_tests/src/manual_tests.cpp
@@ -1,4 +1,41 @@
-#include <xtd/xtd.tunit>
+#include <xtd/tunit/assert>
+#include <xtd/tunit/test_class_attribute>
+#include <xtd/tunit/test_method_attribute>
+#include <xtd/tunit/tunit_main>
+#include <xtd/startup>
+
+namespace xtd {
+  namespace tunit {
+    namespace constraints {
+      class is : public xtd::object {
+      public:
+      
+      };
+    }
+    
+    class assert2 : public base_assert {
+      /// @cond
+      assert2() = delete;
+      /// @endcond
+      
+    public:
+      void that(bool condition) {assert::is_true(condition);}
+      void that(bool condition, const xtd::diagnostics::stack_frame& stack_frame) {assert::is_true(condition, stack_frame);}
+      void that(bool condition, const xtd::ustring& message) {assert::is_true(condition, message);}
+      void that(bool condition, const xtd::ustring& message, const xtd::diagnostics::stack_frame& stack_frame) {assert::is_true(condition, message, stack_frame);}
+      
+      template<typename ...args_t>
+      void that(bool condition, const xtd::ustring& message, args_t... args) {
+        assert::is_true(condition, ustring::format(message, args...));
+      }
+      
+      template <typename actual_t>
+      void that(actual_t condition) {
+      
+      }
+    };
+  }
+}
 
 using namespace xtd::tunit;
 
