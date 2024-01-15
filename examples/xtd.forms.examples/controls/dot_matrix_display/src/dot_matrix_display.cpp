3844bdd564d7367a2d9e912625475096c5de5186
Gammasoft <gammasoft71@gmail.com>
2024-01-15 17:34:42 +0100

    rename variables

--- examples/xtd.forms.examples/controls/dot_matrix_display/src/dot_matrix_display.cpp
+++ examples/xtd.forms.examples/controls/dot_matrix_display/src/dot_matrix_display.cpp
@@ -20,8 +20,8 @@ public:
     timer1.interval(300_ms);
     timer1.enabled(true);
     timer1.tick += [&] {
-      dot_matrix_display1.dot_matrix_style(dot_matrix_styles[(counter / chase.size()) % dot_matrix_styles.size()]);
-      dot_matrix_display1.set_dots(chase[counter++ % chase.size()]);
+      dot_matrix_display1.dot_matrix_style(dot_matrix_styles[(counter / crawler.size()) % dot_matrix_styles.size()]);
+      dot_matrix_display1.set_dots(crawler[counter++ % crawler.size()]);
     };
     
     back_color(color::average(color::black, dot_matrix_display1.fore_color(), 0.20));
@@ -33,7 +33,7 @@ public:
   
 private:
   int counter = 0;
-  vector<dot_matrix_display::points_collection> chase = {
+  vector<dot_matrix_display::points_collection> crawler = {
     {},
     {{3, 3}},
     {{3, 3}, {2, 2}, {2, 3}, {2, 4}, {3, 2}, {3, 4}, {4, 2}, {4, 3}, {4, 4}},
--- examples/xtd.forms.examples/controls/seven_segment_display/src/seven_segment_display.cpp
+++ examples/xtd.forms.examples/controls/seven_segment_display/src/seven_segment_display.cpp
@@ -20,8 +20,8 @@ public:
     timer1.interval(300_ms);
     timer1.enabled(true);
     timer1.tick += [&] {
-      seven_segment_display1.segment_style(segment_styles[(counter / chase.size()) % segment_styles.size()]);
-      seven_segment_display1.value(chase[counter++ % chase.size()]);
+      seven_segment_display1.segment_style(segment_styles[(counter / crawler.size()) % segment_styles.size()]);
+      seven_segment_display1.value(crawler[counter++ % crawler.size()]);
     };
     
     back_color(color::average(color::black, seven_segment_display1.fore_color(), 0.20));
@@ -33,7 +33,7 @@ public:
   
 private:
   int counter = 0;
-  vector<segments> chase = {
+  vector<segments> crawler = {
     segments::none,
     segments::a,
     segments::a | segments::b,
