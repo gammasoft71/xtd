e7d05faa97f3a2fa86194f7bf9260acc57f0aa84
Gammasoft <gammasoft71@gmail.com>
2024-01-15 17:40:19 +0100

    Rename variables

--- examples/xtd.forms.examples/controls/sixteen_segment_display/src/sixteen_segment_display.cpp
+++ examples/xtd.forms.examples/controls/sixteen_segment_display/src/sixteen_segment_display.cpp
@@ -20,8 +20,8 @@ public:
     timer1.interval(300_ms);
     timer1.enabled(true);
     timer1.tick += [&] {
-      sixteen_segment_display1.segment_style(segment_styles[(counter / crawler.size()) % segment_styles.size()]);
-      sixteen_segment_display1.value(crawler[counter++ % crawler.size()]);
+      sixteen_segment_display1.segment_style(segment_styles[(counter / chaser.size()) % segment_styles.size()]);
+      sixteen_segment_display1.value(chaser[counter++ % chaser.size()]);
     };
     
     back_color(color::average(color::black, sixteen_segment_display1.fore_color(), 0.20));
@@ -33,7 +33,7 @@ public:
   
 private:
   int counter = 0;
-  vector<segments> crawler = {
+  vector<segments> chaser = {
     segments::none,
     segments::a1,
     segments::a1 | segments::a2,
