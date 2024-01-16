4500091fd09bf2b944379b349789f2257375c135
Gammasoft <gammasoft71@gmail.com>
2024-01-16 18:18:05 +0100

    Add gtest helper to simulate gtest

---
+++ src/xtd.tunit/include/xtd/gtest.h
@@ -0,0 +1,35 @@
+/// @file
+/// @brief Contains gtest helpers.
+/// @copyright Copyright (c) 2024 Gammasoft. All rights reserved.
+#pragma once
+#include "xtd.tunit.h"
+
+/// @cond
+
+namespace testing {
+  void InitGoogleTest() {xtd::tunit::settings::default_settings().gtest_compatibility(true);}
+  void InitGoogleTest(int argc, char* argv[]) {InitGoogleTest();}
+}
+
+#define ASSERT_FALSE(condition) xtd::tunit::assert::is_false(condition, csf_)
+#define ASSERT_TRUE(condition) xtd::tunit::assert::is_true(condition, csf_)
+
+#define EXPECT_FALSE(condition) xtd::tunit::valid::is_false(condition, csf_)
+#define EXPECT_TRUE(condition) xtd::tunit::valid::is_true(condition, csf_)
+
+#define RUN_ALL_TESTS xtd::tunit::console_unit_test().run
+
+#define TEST(class_name, method_name) \
+  void __##class_name##method_name(); \
+  class __class_##class_name##_##method_name; \
+  xtd::tunit::test_class_attribute<__class_##class_name##_##method_name> __class_##class_name##_##method_name##_attr {typeof_<__class_##class_name##_##method_name>().full_name().replace(typeof_<__class_##class_name##_##method_name>().name(), "") + #class_name}; \
+  class __class_##class_name##_##method_name : public xtd::tunit::test_class { \
+    xtd::tunit::test_method_attribute __class_##class_name##_##method_name##_method {#method_name, *this, &__class_##class_name##_##method_name::__method}; \
+    void __method() { \
+      if (xtd::ustring {#method_name}.starts_with("DISABLED_")) xtd::tunit::assert::ignore();\
+      __##class_name##method_name(); \
+    } \
+  }; \
+  void __##class_name##method_name()
+
+/// @endcond
