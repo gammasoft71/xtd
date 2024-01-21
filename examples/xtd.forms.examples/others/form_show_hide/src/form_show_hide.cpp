5f29d87acb9b3d88fe0215fe4f4e1ac639357368
Gammasoft <gammasoft71@gmail.com>
2024-01-21 13:18:30 +0100

    Review code

--- examples/xtd.forms.examples/others/form_show_hide/src/form_show_hide.cpp
+++ examples/xtd.forms.examples/others/form_show_hide/src/form_show_hide.cpp
@@ -9,52 +9,24 @@ using namespace xtd::forms;
 class form1 : public form {
 public:
   form1() {
-    client_size({320, 325});
-    controls().push_back_range({close_button, show_button, hide_button, cancel_close_check_box});
-    text("Form show and hide example");
+    text("Form show and hide example").client_size({320, 325});
     
-    close_button.location({10, 10});
-    close_button.size({100, 40});
-    close_button.text("Close");
-    close_button.click += [&] {
-      form2.close();
-    };
-    
-    show_button.location({10, 60});
-    show_button.size({100, 40});
-    show_button.text("Show");
-    show_button.click += [&] {
-      form2.show();
-    };
-    
-    hide_button.location({10, 110});
-    hide_button.size({100, 40});
-    hide_button.text("Hide");
-    hide_button.click += [&] {
-      form2.hide();
-    };
-    
-    cancel_close_check_box.location({10, 160});
-    cancel_close_check_box.size({100, 40});
-    cancel_close_check_box.text("cancel close");
+    close_button.click += [&] {form2.close();};
+    show_button.click += [&] {form2.show();};
+    hide_button.click += [&] {form2.hide();};
     
     form2.client_size({300, 100});
-    form2.text("Close count = 0");
-    form2.form_closing += [&](object& seander, form_closing_event_args& e) {
-      e.cancel(cancel_close_check_box.checked());
-    };
-    form2.form_closed += [&] {
-      static auto close_count = 0;
-      form2.text(ustring::format("Close count = {}", ++close_count));
-    };
+    form2.form_closing += [&](auto sender, auto& e) {e.cancel(cancel_close_check_box.checked());};
+    form2.form_closed += [&] {form2.text(ustring::format("Close count = {}", ++close_count));};
   }
   
 private:
-  button close_button;
-  button show_button;
-  button hide_button;
-  check_box cancel_close_check_box;
-  form form2;
+  int close_count = 0;
+  button close_button = button::create(*this, "Close", {10, 10}, {100, 40});
+  button show_button = button::create(*this, "Show", {10, 60}, {100, 40});
+  button hide_button = button::create(*this, "Hide", {10, 110}, {100, 40});
+  check_box cancel_close_check_box = check_box::create(*this, "cancel close", false, check_state::unchecked, {10, 160}, {100, 40});
+  form form2 = form::create("Close count = 0");
 };
 
 auto main()->int {
