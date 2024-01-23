b32e237580b58d78b9ed1ac360af77b10338b9fb
Gammasoft <gammasoft71@gmail.com>
2024-01-23 11:46:21 +0100

    Fix mac version

--- src/xtd.forms.native.wxwidgets/src/xtd/forms/native/wxwidgets/button.mm
+++ src/xtd.forms.native.wxwidgets/src/xtd/forms/native/wxwidgets/button.mm
@@ -6,6 +6,7 @@
 #undef __XTD_FORMS_NATIVE_LIBRARY__
 #include <xtd/argument_exception>
 #include <Quartz/Quartz.h>
+#include <Availability.h>
 
 using namespace xtd;
 using namespace xtd::forms::native;
@@ -13,6 +14,10 @@ using namespace xtd::forms::native;
 void __set_button_bezel_style__(wxAnyButton* control, int32 height) {
   if (!control || !wxTheApp) throw argument_exception {csf_};
   NSButton* button = reinterpret_cast<NSButton*>(control->GetHandle());
+#ifdef __MAC_14_0
   [button setBezelStyle: height <= 25 ? NSBezelStylePush : NSBezelStyleFlexiblePush];
+#else
+  [button setBezelStyle: height <= 25 ? NSBezelStyleRounded : NSBezelStyleRegularSquare];
+#endif
 }
 #endif
