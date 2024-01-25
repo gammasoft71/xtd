c9ab910f378d54e05296a6709f1aeb45db8a4336
Gammasoft <gammasoft71@gmail.com>
2024-01-25 09:17:01 +0100

    Add check_state unit tests

---
+++ tests/xtd.forms.unit_tests/src/xtd/forms/tests/check_state_tests.cpp
@@ -0,0 +1,31 @@
+#include <xtd/forms/check_state.h>
+#include <xtd/tunit/assert>
+#include <xtd/tunit/test_class_attribute>
+#include <xtd/tunit/test_method_attribute>
+
+using namespace xtd::forms;
+using namespace xtd::tunit;
+
+namespace xtd::forms::tests {
+  class test_class_(check_state_tests) {
+  public:
+    void test_method_(unchecked) {
+      assert::are_equal(0, enum_object<>::to_int32(check_state::unchecked), csf_);
+      assert::are_equal("unchecked", enum_object<>::to_string(check_state::unchecked), csf_);
+      assert::are_equal(check_state::unchecked, enum_object<>::parse<check_state>("unchecked"), csf_);
+    }
+    
+    void test_method_(checked) {
+      assert::are_equal(1, enum_object<>::to_int32(check_state::checked), csf_);
+      assert::are_equal("checked", enum_object<>::to_string(check_state::checked), csf_);
+      assert::are_equal(check_state::checked, enum_object<>::parse<check_state>("checked"), csf_);
+    }
+    
+    void test_method_(indeterminate) {
+      assert::are_equal(2, enum_object<>::to_int32(check_state::indeterminate), csf_);
+      assert::are_equal("indeterminate", enum_object<>::to_string(check_state::indeterminate), csf_);
+      assert::are_equal(check_state::indeterminate, enum_object<>::parse<check_state>("indeterminate"), csf_);
+    }
+  };
+}
+
