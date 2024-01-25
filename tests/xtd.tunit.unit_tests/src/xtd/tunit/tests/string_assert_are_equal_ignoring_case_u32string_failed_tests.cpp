ad59a0442d7eb6461e775c28409f3f4b5b8c03b0
Gammasoft <gammasoft71@gmail.com>
2024-01-25 22:04:53 +0100

    Review code

--- tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/string_assert_are_equal_ignoring_case_u32string_failed_tests.cpp
+++ tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/string_assert_are_equal_ignoring_case_u32string_failed_tests.cpp
@@ -11,18 +11,18 @@ namespace xtd::tunit::tests {
       xtd::tunit::string_assert::are_equal_ignoring_case(U"key", s);
     }
   };
-}
-
-void test_(string_assert_are_equal_ignoring_case_u32string_failed_tests, test_output) {
-  auto [output, result] = run_test_("xtd::tunit::tests::string_assert_are_equal_ignoring_case_u32string_failed_tests.*");
-  assert_value_("Start 1 test from 1 test case\n"
-    "  FAILED  xtd::tunit::tests::string_assert_are_equal_ignoring_case_u32string_failed_tests.test_case_failed\n"
-    "    Expected: \"key\", ignoring case\n"
-    "    But was:  \"VALUE\"\n"
-    "End 1 test from 1 test case ran.\n", output);
-}
-
-void test_(string_assert_are_equal_ignoring_case_u32string_failed_tests, test_result) {
-  auto [output, result] = run_test_("xtd::tunit::tests::string_assert_are_equal_ignoring_case_u32string_failed_tests.*");
-  assert_value_(1, result);
+  
+  void test_(string_assert_are_equal_ignoring_case_u32string_failed_tests, test_output) {
+    auto [output, result] = run_test_("xtd::tunit::tests::string_assert_are_equal_ignoring_case_u32string_failed_tests.*");
+    assert_value_("Start 1 test from 1 test case\n"
+                  "  FAILED  xtd::tunit::tests::string_assert_are_equal_ignoring_case_u32string_failed_tests.test_case_failed\n"
+                  "    Expected: \"key\", ignoring case\n"
+                  "    But was:  \"VALUE\"\n"
+                  "End 1 test from 1 test case ran.\n", output);
+  }
+  
+  void test_(string_assert_are_equal_ignoring_case_u32string_failed_tests, test_result) {
+    auto [output, result] = run_test_("xtd::tunit::tests::string_assert_are_equal_ignoring_case_u32string_failed_tests.*");
+    assert_value_(1, result);
+  }
 }
