45f25c9ca6c280db22d7ea761c352428e105b5de
Gammasoft <gammasoft71@gmail.com>
2024-01-25 21:55:08 +0100

    Review code

--- tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/file_valid_are_equal_istream_failed_tests.cpp
+++ tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/file_valid_are_equal_istream_failed_tests.cpp
@@ -16,21 +16,21 @@ namespace xtd::tunit::tests {
       xtd::tunit::file_valid::are_equal(std::istringstream("Test xtd::tunit::file_valid::are_equal."), std::istringstream("Test xtd::tunit::Dile_valid::are_equal."));
     }
   };
-}
-
-void test_(file_valid_are_equal_istream_failed_tests, test_output) {
-  auto [output, result] = run_test_("xtd::tunit::tests::file_valid_are_equal_istream_failed_tests.*");
-  assert_value_("Start 2 tests from 1 test case\n"
-    "  FAILED  xtd::tunit::tests::file_valid_are_equal_istream_failed_tests.test_case_lenght_failed\n"
-    "    Expected: istream length 39\n"
-    "    But was:  38\n"
-    "  FAILED  xtd::tunit::tests::file_valid_are_equal_istream_failed_tests.test_case_letter_failed\n"
-    "    Expected: istream at offset 17 value f\n"
-    "    But was:  D\n"
-    "End 2 tests from 1 test case ran.\n", output);
-}
-
-void test_(file_valid_are_equal_istream_failed_tests, test_result) {
-  auto [output, result] = run_test_("xtd::tunit::tests::file_valid_are_equal_istream_failed_tests.*");
-  assert_value_(1, result);
+  
+  void test_(file_valid_are_equal_istream_failed_tests, test_output) {
+    auto [output, result] = run_test_("xtd::tunit::tests::file_valid_are_equal_istream_failed_tests.*");
+    assert_value_("Start 2 tests from 1 test case\n"
+                  "  FAILED  xtd::tunit::tests::file_valid_are_equal_istream_failed_tests.test_case_lenght_failed\n"
+                  "    Expected: istream length 39\n"
+                  "    But was:  38\n"
+                  "  FAILED  xtd::tunit::tests::file_valid_are_equal_istream_failed_tests.test_case_letter_failed\n"
+                  "    Expected: istream at offset 17 value f\n"
+                  "    But was:  D\n"
+                  "End 2 tests from 1 test case ran.\n", output);
+  }
+  
+  void test_(file_valid_are_equal_istream_failed_tests, test_result) {
+    auto [output, result] = run_test_("xtd::tunit::tests::file_valid_are_equal_istream_failed_tests.*");
+    assert_value_(1, result);
+  }
 }
--- tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/file_valid_exists_failed_tests.cpp
+++ tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/file_valid_exists_failed_tests.cpp
@@ -10,18 +10,18 @@ namespace xtd::tunit::tests {
       file_valid::exists(xtd::io::path::combine(xtd::io::path::get_temp_path(), "test_tunit_file.txt"), csf_);
     }
   };
-}
-
-void test_(file_valid_exists_failed_tests, test_output) {
-  auto [output, result] = run_test_("xtd::tunit::tests::file_valid_exists_failed_tests.*");
-  assert_value_(xtd::ustring::format("Start 1 test from 1 test case\n"
-      "  FAILED  xtd::tunit::tests::file_valid_exists_failed_tests.test_case_failed\n"
-      "    Expected: file exists\n"
-      "    But was:  {}\n"
-      "End 1 test from 1 test case ran.\n", std::quoted(xtd::io::path::combine(xtd::io::path::get_temp_path(), "test_tunit_file.txt"))), output);
-}
-
-void test_(file_valid_exists_failed_tests, test_result) {
-  auto [output, result] = run_test_("xtd::tunit::tests::file_valid_exists_failed_tests.*");
-  assert_value_(1, result);
+  
+  void test_(file_valid_exists_failed_tests, test_output) {
+    auto [output, result] = run_test_("xtd::tunit::tests::file_valid_exists_failed_tests.*");
+    assert_value_(xtd::ustring::format("Start 1 test from 1 test case\n"
+                                       "  FAILED  xtd::tunit::tests::file_valid_exists_failed_tests.test_case_failed\n"
+                                       "    Expected: file exists\n"
+                                       "    But was:  {}\n"
+                                       "End 1 test from 1 test case ran.\n", std::quoted(xtd::io::path::combine(xtd::io::path::get_temp_path(), "test_tunit_file.txt"))), output);
+  }
+  
+  void test_(file_valid_exists_failed_tests, test_result) {
+    auto [output, result] = run_test_("xtd::tunit::tests::file_valid_exists_failed_tests.*");
+    assert_value_(1, result);
+  }
 }
