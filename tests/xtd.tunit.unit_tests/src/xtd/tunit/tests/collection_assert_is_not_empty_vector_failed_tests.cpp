742281e3de5dd3640afd15cc6d2e63cd89719c10
Gammasoft <gammasoft71@gmail.com>
2024-01-25 21:21:04 +0100

    Review code

--- tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/collection_assert_is_not_empty_vector_failed_tests.cpp
+++ tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/collection_assert_is_not_empty_vector_failed_tests.cpp
@@ -11,18 +11,18 @@ namespace xtd::tunit::tests {
       xtd::tunit::collection_assert::is_not_empty(a);
     }
   };
-}
-
-void test_(collection_assert_is_not_empty_vector_failed_tests, test_output) {
-  auto [output, result] = run_test_("xtd::tunit::tests::collection_assert_is_not_empty_vector_failed_tests.*");
-  assert_value_("Start 1 test from 1 test case\n"
-    "  FAILED  xtd::tunit::tests::collection_assert_is_not_empty_vector_failed_tests.test_case_failed\n"
-    "    Expected: not <empty>\n"
-    "    But was:  <empty>\n"
-    "End 1 test from 1 test case ran.\n", output);
-}
-
-void test_(collection_assert_is_not_empty_vector_failed_tests, test_result) {
-  auto [output, result] = run_test_("xtd::tunit::tests::collection_assert_is_not_empty_vector_failed_tests.*");
-  assert_value_(1, result);
+  
+  void test_(collection_assert_is_not_empty_vector_failed_tests, test_output) {
+    auto [output, result] = run_test_("xtd::tunit::tests::collection_assert_is_not_empty_vector_failed_tests.*");
+    assert_value_("Start 1 test from 1 test case\n"
+                  "  FAILED  xtd::tunit::tests::collection_assert_is_not_empty_vector_failed_tests.test_case_failed\n"
+                  "    Expected: not <empty>\n"
+                  "    But was:  <empty>\n"
+                  "End 1 test from 1 test case ran.\n", output);
+  }
+  
+  void test_(collection_assert_is_not_empty_vector_failed_tests, test_result) {
+    auto [output, result] = run_test_("xtd::tunit::tests::collection_assert_is_not_empty_vector_failed_tests.*");
+    assert_value_(1, result);
+  }
 }
