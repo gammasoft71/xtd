d54972f3868aa7b84be1e6d43652dbd9a7f08399
Gammasoft <gammasoft71@gmail.com>
2024-01-25 21:27:32 +0100

    Review code

--- tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/collection_valid_all_items_are_unique_vector_failed_tests.cpp
+++ tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/collection_valid_all_items_are_unique_vector_failed_tests.cpp
@@ -11,18 +11,18 @@ namespace xtd::tunit::tests {
       xtd::tunit::collection_valid::all_items_are_unique(a);
     }
   };
-}
-
-void test_(collection_valid_all_items_are_unique_vector_failed_tests, test_output) {
-  auto [output, result] = run_test_("xtd::tunit::tests::collection_valid_all_items_are_unique_vector_failed_tests.*");
-  assert_value_("Start 1 test from 1 test case\n"
-    "  FAILED  xtd::tunit::tests::collection_valid_all_items_are_unique_vector_failed_tests.test_case_failed\n"
-    "    Expected: all items are unique\n"
-    "    But was:  < 1, 2, 3, 4, 1 >\n"
-    "End 1 test from 1 test case ran.\n", output);
-}
-
-void test_(collection_valid_all_items_are_unique_vector_failed_tests, test_result) {
-  auto [output, result] = run_test_("xtd::tunit::tests::collection_valid_all_items_are_unique_vector_failed_tests.*");
-  assert_value_(1, result);
+  
+  void test_(collection_valid_all_items_are_unique_vector_failed_tests, test_output) {
+    auto [output, result] = run_test_("xtd::tunit::tests::collection_valid_all_items_are_unique_vector_failed_tests.*");
+    assert_value_("Start 1 test from 1 test case\n"
+                  "  FAILED  xtd::tunit::tests::collection_valid_all_items_are_unique_vector_failed_tests.test_case_failed\n"
+                  "    Expected: all items are unique\n"
+                  "    But was:  < 1, 2, 3, 4, 1 >\n"
+                  "End 1 test from 1 test case ran.\n", output);
+  }
+  
+  void test_(collection_valid_all_items_are_unique_vector_failed_tests, test_result) {
+    auto [output, result] = run_test_("xtd::tunit::tests::collection_valid_all_items_are_unique_vector_failed_tests.*");
+    assert_value_(1, result);
+  }
 }
--- tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/collection_valid_is_not_empty_vector_failed_tests.cpp
+++ tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/collection_valid_is_not_empty_vector_failed_tests.cpp
@@ -11,18 +11,18 @@ namespace xtd::tunit::tests {
       xtd::tunit::collection_valid::is_not_empty(a);
     }
   };
-}
-
-void test_(collection_valid_is_not_empty_vector_failed_tests, test_output) {
-  auto [output, result] = run_test_("xtd::tunit::tests::collection_valid_is_not_empty_vector_failed_tests.*");
-  assert_value_("Start 1 test from 1 test case\n"
-    "  FAILED  xtd::tunit::tests::collection_valid_is_not_empty_vector_failed_tests.test_case_failed\n"
-    "    Expected: not <empty>\n"
-    "    But was:  <empty>\n"
-    "End 1 test from 1 test case ran.\n", output);
-}
-
-void test_(collection_valid_is_not_empty_vector_failed_tests, test_result) {
-  auto [output, result] = run_test_("xtd::tunit::tests::collection_valid_is_not_empty_vector_failed_tests.*");
-  assert_value_(1, result);
+  
+  void test_(collection_valid_is_not_empty_vector_failed_tests, test_output) {
+    auto [output, result] = run_test_("xtd::tunit::tests::collection_valid_is_not_empty_vector_failed_tests.*");
+    assert_value_("Start 1 test from 1 test case\n"
+                  "  FAILED  xtd::tunit::tests::collection_valid_is_not_empty_vector_failed_tests.test_case_failed\n"
+                  "    Expected: not <empty>\n"
+                  "    But was:  <empty>\n"
+                  "End 1 test from 1 test case ran.\n", output);
+  }
+  
+  void test_(collection_valid_is_not_empty_vector_failed_tests, test_result) {
+    auto [output, result] = run_test_("xtd::tunit::tests::collection_valid_is_not_empty_vector_failed_tests.*");
+    assert_value_(1, result);
+  }
 }
