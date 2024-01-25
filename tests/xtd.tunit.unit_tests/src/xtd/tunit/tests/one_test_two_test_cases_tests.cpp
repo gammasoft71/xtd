c37fe4ff1e6407c65d80264f12bcb137332fabc7
Gammasoft <gammasoft71@gmail.com>
2024-01-25 21:56:40 +0100

    Review code

--- tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/one_test_two_test_cases_tests.cpp
+++ tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/one_test_two_test_cases_tests.cpp
@@ -10,17 +10,17 @@ namespace xtd::tunit::tests {
     void test_method_(test_case2) {
     }
   };
-}
-
-void test_(one_test_two_test_cases_tests, test_output) {
-  auto [output, result] = run_test_("xtd::tunit::tests::one_test_two_test_cases_tests.*");
-  assert_value_("Start 2 tests from 1 test case\n"
-    "  SUCCEED xtd::tunit::tests::one_test_two_test_cases_tests.test_case1\n"
-    "  SUCCEED xtd::tunit::tests::one_test_two_test_cases_tests.test_case2\n"
-    "End 2 tests from 1 test case ran.\n", output);
-}
-
-void test_(one_test_two_test_cases_tests, test_result) {
-  auto [output, result] = run_test_("xtd::tunit::tests::one_test_two_test_cases_tests.*");
-  assert_value_(0, result);
+  
+  void test_(one_test_two_test_cases_tests, test_output) {
+    auto [output, result] = run_test_("xtd::tunit::tests::one_test_two_test_cases_tests.*");
+    assert_value_("Start 2 tests from 1 test case\n"
+                  "  SUCCEED xtd::tunit::tests::one_test_two_test_cases_tests.test_case1\n"
+                  "  SUCCEED xtd::tunit::tests::one_test_two_test_cases_tests.test_case2\n"
+                  "End 2 tests from 1 test case ran.\n", output);
+  }
+  
+  void test_(one_test_two_test_cases_tests, test_result) {
+    auto [output, result] = run_test_("xtd::tunit::tests::one_test_two_test_cases_tests.*");
+    assert_value_(0, result);
+  }
 }
