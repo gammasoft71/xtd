fc0eb9f9d90a8031f0bb6052d31ed190108f079d
Gammasoft <gammasoft71@gmail.com>
2024-01-25 21:33:05 +0100

    Review code

--- tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/directory_valid_are_not_equal_filesystem_directory_entry_succeed_tests.cpp
+++ tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/directory_valid_are_not_equal_filesystem_directory_entry_succeed_tests.cpp
@@ -13,17 +13,17 @@ namespace xtd::tunit::tests {
       xtd::tunit::directory_valid::are_not_equal(d1, d2);
     }
   };
-}
-
-void test_(directory_valid_are_not_equal_filesystem_directory_entry_succeed_tests, test_output) {
-  auto [output, result] = run_test_("xtd::tunit::tests::directory_valid_are_not_equal_filesystem_directory_entry_succeed_tests.*");
-  assert_value_("Start 1 test from 1 test case\n"
-    "  SUCCEED xtd::tunit::tests::directory_valid_are_not_equal_filesystem_directory_entry_succeed_tests.test_case_succeed\n"
-    "End 1 test from 1 test case ran.\n", output);
-}
-
-void test_(directory_valid_are_not_equal_filesystem_directory_entry_succeed_tests, test_result) {
-  auto [output, result] = run_test_("xtd::tunit::tests::directory_valid_are_not_equal_filesystem_directory_entry_succeed_tests.*");
-  assert_value_(0, result);
+  
+  void test_(directory_valid_are_not_equal_filesystem_directory_entry_succeed_tests, test_output) {
+    auto [output, result] = run_test_("xtd::tunit::tests::directory_valid_are_not_equal_filesystem_directory_entry_succeed_tests.*");
+    assert_value_("Start 1 test from 1 test case\n"
+                  "  SUCCEED xtd::tunit::tests::directory_valid_are_not_equal_filesystem_directory_entry_succeed_tests.test_case_succeed\n"
+                  "End 1 test from 1 test case ran.\n", output);
+  }
+  
+  void test_(directory_valid_are_not_equal_filesystem_directory_entry_succeed_tests, test_result) {
+    auto [output, result] = run_test_("xtd::tunit::tests::directory_valid_are_not_equal_filesystem_directory_entry_succeed_tests.*");
+    assert_value_(0, result);
+  }
 }
 #endif
--- tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/directory_valid_does_not_exist_string_failed_tests.cpp
+++ tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/directory_valid_does_not_exist_string_failed_tests.cpp
@@ -11,26 +11,26 @@ namespace xtd::tunit::tests {
       xtd::tunit::directory_valid::does_not_exist(d);
     }
   };
-}
-
-void test_(directory_valid_does_not_exist_string_failed_tests, test_output) {
-  auto [output, result] = run_test_("xtd::tunit::tests::directory_valid_does_not_exist_string_failed_tests.*");
-  #if defined (_WIN32)
-  assert_value_("Start 1 test from 1 test case\n"
-    "  FAILED  xtd::tunit::tests::directory_valid_does_not_exist_string_failed_tests.test_case_failed\n"
-    "    Expected: not directory exists\n"
-    "    But was:  \"C:\\ProgramData\"\n"
-    "End 1 test from 1 test case ran.\n", output);
-  #else
-  assert_value_("Start 1 test from 1 test case\n"
-    "  FAILED  xtd::tunit::tests::directory_valid_does_not_exist_string_failed_tests.test_case_failed\n"
-    "    Expected: not directory exists\n"
-    "    But was:  \"/usr/share\"\n"
-    "End 1 test from 1 test case ran.\n", output);
-  #endif
-}
-
-void test_(directory_valid_does_not_exist_string_failed_tests, test_result) {
-  auto [output, result] = run_test_("xtd::tunit::tests::directory_valid_does_not_exist_string_failed_tests.*");
-  assert_value_(1, result);
+  
+  void test_(directory_valid_does_not_exist_string_failed_tests, test_output) {
+    auto [output, result] = run_test_("xtd::tunit::tests::directory_valid_does_not_exist_string_failed_tests.*");
+#if defined (_WIN32)
+    assert_value_("Start 1 test from 1 test case\n"
+                  "  FAILED  xtd::tunit::tests::directory_valid_does_not_exist_string_failed_tests.test_case_failed\n"
+                  "    Expected: not directory exists\n"
+                  "    But was:  \"C:\\ProgramData\"\n"
+                  "End 1 test from 1 test case ran.\n", output);
+#else
+    assert_value_("Start 1 test from 1 test case\n"
+                  "  FAILED  xtd::tunit::tests::directory_valid_does_not_exist_string_failed_tests.test_case_failed\n"
+                  "    Expected: not directory exists\n"
+                  "    But was:  \"/usr/share\"\n"
+                  "End 1 test from 1 test case ran.\n", output);
+#endif
+  }
+  
+  void test_(directory_valid_does_not_exist_string_failed_tests, test_result) {
+    auto [output, result] = run_test_("xtd::tunit::tests::directory_valid_does_not_exist_string_failed_tests.*");
+    assert_value_(1, result);
+  }
 }
