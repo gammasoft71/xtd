40dd725be9dd6c407eddf544e0cbced3dadd314c
Gammasoft <gammasoft71@gmail.com>
2024-01-29 19:30:41 +0100

    Add style_to_wx_user_window_style method

--- src/xtd.forms.native.wxwidgets/include/xtd/forms/native/wxwidgets/wx_radio_button.h
+++ src/xtd.forms.native.wxwidgets/include/xtd/forms/native/wxwidgets/wx_radio_button.h
@@ -29,7 +29,7 @@ namespace xtd {
           if (!create_params.parent) throw xtd::argument_exception("control must have a parent"_t, csf_);
           owner_draw_ = (create_params.style & BS_OWNERDRAW) == BS_OWNERDRAW;
           if (owner_draw_) {
-            control_handler::create<wx_user_window>(reinterpret_cast<control_handler*>(create_params.parent)->main_control(), wxID_ANY, wxPoint(create_params.location.x(), create_params.location.y()), wxSize(create_params.size.width(), create_params.size.height()), common_control_style_to_wx_style(create_params.style, create_params.ex_style));
+            control_handler::create<wx_user_window>(reinterpret_cast<control_handler*>(create_params.parent)->main_control(), wxID_ANY, wxPoint(create_params.location.x(), create_params.location.y()), wxSize(create_params.size.width(), create_params.size.height()), style_to_wx_user_window_style(create_params.style, create_params.ex_style));
             #if defined(__WXOSX__)
             reinterpret_cast<wx_user_window*>(control())->set_accepts_focus(false);
             #else
@@ -59,6 +59,14 @@ namespace xtd {
           return wx_style;
         }
         
+        static long style_to_wx_user_window_style(size_t style, size_t ex_style) {
+          long wx_style = common_control_style_to_wx_style(style, ex_style);
+          
+          wx_style |= wxNO_BORDER;
+          
+          return wx_style;
+        }
+
         wxSize GetClientSize() const override {
           return control()->GetSize();
         }
