79a3f176357fb60bd05bdc89625d4d69254ef5ce
Gammasoft <gammasoft71@gmail.com>
2024-02-01 16:43:45 +0100

    Add application_add_message_filter example

---
+++ examples/xtd.forms.examples/events/application_add_message_filter/README.md
@@ -0,0 +1,46 @@
+# application_add_message_filter
+
+Shows how to create an application with  [xtd::forms::application::add_message_filter](https://gammasoft71.github.io/xtd/reference_guides/latest/classxtd_1_1forms_1_1application.html#a4f8eac12a2e3e29b5308846260788d14) method.
+
+## Sources
+
+* [src/application_add_message_filter.cpp](src/application_add_message_filter.cpp)
+* [CMakeLists.txt](CMakeLists.txt)
+
+## Build and run
+
+Open "Command Prompt" or "Terminal". Navigate to the folder that contains the project and type the following:
+
+```shell
+xtdc run
+```
+
+## Output
+
+### Windows :
+
+![Screenshot](../../../../docs/pictures/examples/application_w.png)
+
+![Screenshot](../../../../docs/pictures/examples/application_wd.png)
+
+### macOS :
+
+![Screenshot](../../../../docs/pictures/examples/application_m.png)
+
+![Screenshot](../../../../docs/pictures/examples/application_md.png)
+
+### Gnome :
+
+![Screenshot](../../../../docs/pictures/examples/application_g.png)
+
+![Screenshot](../../../../docs/pictures/examples/application_gd.png)
+
+# Trace
+
+```
+Before application run
+The application is stopped
+After application exit
+Before program exit
+The program is stopped
+```
---
+++ examples/xtd.forms.examples/events/application_add_message_filter/src/application_add_message_filter.cpp
@@ -0,0 +1,40 @@
+#include <xtd/forms/application>
+#include <xtd/forms/button>
+#include <xtd/forms/check_box>
+#include <xtd/forms/debug_form>
+
+using namespace xtd;
+using namespace xtd::diagnostics;
+using namespace xtd::forms;
+
+namespace application_add_message_filter_example {
+  class main_form : public form, public imessage_filter {
+  public:
+    main_form() {
+      xtd::forms::application::add_message_filter(*this);
+      text("Application add message filter example");
+      panel_.border_style(forms::border_style::groove);
+      skip_button_click_check_box_.auto_size(true);
+      click += [&] {debug::write_line(ustring::format("(form.click) x={}, y={}", mouse_position().x(), mouse_position().y()));};
+      button_.click += [&] {debug::write_line(ustring::format("(button.click) x={}, y={}", mouse_position().x(), mouse_position().y()));};
+      skip_button_click_check_box_.click += [&] {debug::write_line(ustring::format("(check_box.click) x={}, y={}", mouse_position().x(), mouse_position().y()));};
+      panel_.click += [&] {debug::write_line(ustring::format("(panel.click) x={}, y={}", mouse_position().x(), mouse_position().y()));};
+    }
+    
+  private:
+    bool pre_filter_message(const message& message) {
+      // Uncomment following line to see all messages.
+      //debug::write_line(ustring::format("message=[{}], control=[{}]", message.to_string(), from_handle(message.hwnd()).has_value() ? from_handle(message.hwnd()).value().get().to_string() : "(null)"));
+      return skip_button_click_check_box_.checked() && message.msg() == WM_LBUTTONDOWN && message.hwnd() == button_.handle();
+    }
+    
+    debug_form debug_form_;
+    panel panel_ = panel::create(*this, {10, 10}, {200, 100});
+    button button_ = button::create(panel_, "Click me", {10, 10});
+    check_box skip_button_click_check_box_ = check_box::create(panel_, "Skip button click event", check_state::unchecked, {10, 40});
+  };
+}
+
+int main() {
+  xtd::forms::application::run(application_add_message_filter_example::main_form {});
+}
