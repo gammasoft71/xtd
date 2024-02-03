fce4292bcd0ff8f2dbb1d671fbabc920b2728564
Gammasoft <gammasoft71@gmail.com>
2024-02-03 21:02:18 +0100

    Review code

--- examples/xtd.forms.examples/controls/combo_box/src/combo_box.cpp
+++ examples/xtd.forms.examples/controls/combo_box/src/combo_box.cpp
@@ -16,23 +16,23 @@ namespace combo_box_example {
       combo_box1.location({10, 10});
       combo_box1.items().push_back_range({"Item1", "Item2", "Item3", "Item4", "Item5", "Item6", "Item7", "Item8", "Item9", "Item10"});
       combo_box1.selected_index(0);
-      combo_box1.selected_index_changed += event_handler(*this, &form1::on_combo_box_click);
+      combo_box1.selected_index_changed += event_handler(*this, &form1::on_combo_box_selected_index_changed);
       
       combo_box2.location({10, 50});
       combo_box2.drop_down_style(combo_box_style::drop_down_list);
       combo_box2.items().push_back_range({"Item1", "Item2", "Item3", "Item4", "Item5", "Item6", "Item7", "Item8", "Item9", "Item10"});
       combo_box2.selected_index(0);
-      combo_box2.selected_index_changed += event_handler(*this, &form1::on_combo_box_click);
+      combo_box2.selected_index_changed += event_handler(*this, &form1::on_combo_box_selected_index_changed);
       
       combo_box3.location({10, 90});
       combo_box3.drop_down_style(combo_box_style::simple);
       combo_box3.items().push_back_range({"Item1", "Item2", "Item3", "Item4", "Item5", "Item6", "Item7", "Item8", "Item9", "Item10"});
       combo_box3.selected_index(0);
-      combo_box3.selected_index_changed += event_handler(*this, &form1::on_combo_box_click);
+      combo_box3.selected_index_changed += event_handler(*this, &form1::on_combo_box_selected_index_changed);
     }
     
   private:
-    void on_combo_box_click(object& sender, const xtd::event_args& e) {
+    void on_combo_box_selected_index_changed(object& sender, const xtd::event_args& e) {
       combo_box1.selected_index(as<combo_box&>(sender).selected_index());
       combo_box2.selected_index(as<combo_box&>(sender).selected_index());
       combo_box3.selected_index(as<combo_box&>(sender).selected_index());
