fbe8a56d112545ffb2ecaa961a12336c144a5f7d
Gammasoft <gammasoft71@gmail.com>
2024-02-07 19:41:16 +0100

    using auto

--- src/xtd.core.native.win32/src/xtd/native/win32/environment.cpp
+++ src/xtd.core.native.win32/src/xtd/native/win32/environment.cpp
@@ -25,19 +25,19 @@ __declspec(dllimport) extern int __argc;
 __declspec(dllimport) extern char** __argv;
 int __environment_argc = __argc;
 char** __environment_argv = __argv;
-map<string, string> __machine_envs__;
-map<string, string> __none_envs__;
-map<string, string> __process_envs__;
-map<string, string> __user_envs__;
+auto __machine_envs__ = map<string, string> {};
+auto __none_envs__ = map<string, string> {};
+auto __process_envs__ = map<string, string> {};
+auto __user_envs__ = map<string, string> {};
 
 namespace {
   void get_windows_version(int_least32_t& major, int_least32_t& minor, int_least32_t& build, int_least32_t& revision) {
     // GetVersionEx (https://docs.microsoft.com/en-us/windows/win32/api/sysinfoapi/nf-sysinfoapi-getversionexa) allows you to get the version for a Windows 8 at most. For Windows 10 and more see :
     // https://stackoverflow.com/questions/32115255/c-how-to-detect-windows-10
     NTSTATUS(WINAPI * RtlGetVersion)(LPOSVERSIONINFOEXW);
-    OSVERSIONINFOEXW os_info;
+    auto os_info = OSVERSIONINFOEXW {};
     
-    *(FARPROC*)&RtlGetVersion = GetProcAddress(GetModuleHandleA("ntdll"), "RtlGetVersion");
+    *reinterpret_cast<FARPROC*>(&RtlGetVersion) = GetProcAddress(GetModuleHandleA("ntdll"), "RtlGetVersion");
     if (RtlGetVersion != nullptr) {
       os_info.dwOSVersionInfoSize = sizeof(os_info);
       RtlGetVersion(&os_info);
@@ -48,7 +48,7 @@ namespace {
     } else {
 #pragma warning(push)
 #pragma warning(disable : 4996)
-      OSVERSIONINFOEX version_info {};
+      auto version_info = OSVERSIONINFOEX {};
       version_info.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEX);
       if (GetVersionEx((LPOSVERSIONINFO)&version_info)) {
         major = version_info.dwMajorVersion;
@@ -120,7 +120,8 @@ string environment::get_desktop_environment() {
 }
 
 string environment::get_desktop_theme() {
-  DWORD value = 0, value_size = sizeof(value);
+  auto value = DWORD {};
+  auto value_size = static_cast<DWORD>(sizeof(value));
   if (RegGetValue(HKEY_CURRENT_USER, L"Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize", L"AppsUseLightTheme", RRF_RT_REG_DWORD, nullptr, &value, &value_size) != ERROR_SUCCESS)
     value = 0;
   return value == 0 ? "windows" : "windows dark";
@@ -175,8 +176,8 @@ string environment::get_distribution_version_string() {
 
 string environment::get_environment_variable(const string& variable, int_least32_t target) {
   if (target == ENVIRONMENT_VARIABLE_TARGET_PROCESS) {
-    DWORD environent_variable_size = 65535;
-    wstring environment_variable(environent_variable_size, 0);
+    auto environent_variable_size = DWORD {65535};
+    auto environment_variable = wstring(environent_variable_size, '\0');
     environent_variable_size = GetEnvironmentVariable(win32::strings::to_wstring(variable).data(), environment_variable.data(), environent_variable_size);
     if (!environent_variable_size) return "";
     return win32::strings::to_string(environment_variable);
@@ -194,15 +195,15 @@ map<string, string>& environment::get_environment_variables(int_least32_t target
   auto& envs = __none_envs__;
   
   if (target == ENVIRONMENT_VARIABLE_TARGET_PROCESS) {
-    std::vector<std::string> environments;
-    for (wchar_t* line = GetEnvironmentStrings(); line[0] != 0; line += wcslen(line) + 1)
+    auto environments = std::vector<std::string> {};
+    for (auto line = GetEnvironmentStrings(); line[0] != 0; line += wcslen(line) + 1)
       environments.push_back(win32::strings::to_string(line));
       
     envs = __process_envs__;
     envs.clear();
     
     for (auto environment : environments) {
-      vector<string> key_value = win32::strings::split(environment, { '=' });
+      auto key_value = win32::strings::split(environment, { '=' });
       if (key_value.size() == 2) envs.insert({ key_value[0], key_value[1] });
     }
     return envs;
@@ -212,18 +213,18 @@ map<string, string>& environment::get_environment_variables(int_least32_t target
     envs = target == ENVIRONMENT_VARIABLE_TARGET_USER ? __user_envs__ : __machine_envs__;
     envs.clear();
     
-    HKEY root_key = target == ENVIRONMENT_VARIABLE_TARGET_USER ? HKEY_CURRENT_USER : HKEY_LOCAL_MACHINE;
-    wstring sub_key = target == ENVIRONMENT_VARIABLE_TARGET_USER ? L"Environment" : L"System\\CurrentControlSet\\Control\\Session Manager\\Environment";
-    HKEY environment_key = 0;
-    LSTATUS result = RegOpenKeyEx(root_key, sub_key.data(), 0, KEY_READ, &environment_key);
+    auto root_key = target == ENVIRONMENT_VARIABLE_TARGET_USER ? HKEY_CURRENT_USER : HKEY_LOCAL_MACHINE;
+    auto sub_key = wstring {target == ENVIRONMENT_VARIABLE_TARGET_USER ? L"Environment" : L"System\\CurrentControlSet\\Control\\Session Manager\\Environment"};
+    auto environment_key = HKEY {};
+    auto result = RegOpenKeyEx(root_key, sub_key.data(), 0, KEY_READ, &environment_key);
     if (result != ERROR_SUCCESS || environment_key == 0) return envs;
     
     for (DWORD index = 0; result != ERROR_NO_MORE_ITEMS; ++index) {
-      DWORD value_size = 32767;
-      wstring value(value_size, 0);
-      DWORD data_type = REG_EXPAND_SZ;
-      DWORD data_size = 65535;
-      wstring data(data_size, 0);
+      auto value_size = DWORD {32767};
+      auto value = wstring(value_size, '\0');
+      auto data_type = DWORD {REG_EXPAND_SZ};
+      auto data_size = DWORD {65535};
+      auto data = wstring(data_size, 0);
       result = RegEnumValue(environment_key, index, value.data(), &value_size, nullptr, &data_type, reinterpret_cast<LPBYTE>(data.data()), &data_size);
       if (value[0] != 0) envs.insert({ win32::strings::to_string(value), win32::strings::to_string(data)});
     }
@@ -237,16 +238,14 @@ map<string, string>& environment::get_environment_variables(int_least32_t target
 string environment::get_know_folder_path(int_least32_t id) {
   if (id == CSIDL_HOME)
     return get_environment_variable("HOMEPATH", ENVIRONMENT_VARIABLE_TARGET_PROCESS);
-  DWORD path_size = 65535;
-  wstring path;
-  path.resize(path_size);
+  auto path = wstring(65535 , '\0');
   return SHGetFolderPath(nullptr, id, nullptr, SHGFP_TYPE_CURRENT, path.data()) == S_OK ? win32::strings::to_string(path) : "";
 }
 
 string environment::get_machine_name() {
-  wchar_t machine_name[MAX_COMPUTERNAME_LENGTH + 1];
-  DWORD size = MAX_COMPUTERNAME_LENGTH + 1;
-  if (!GetComputerName(machine_name, &size))
+  auto machine_name = wstring(MAX_COMPUTERNAME_LENGTH, '\0');
+  auto size = DWORD {MAX_COMPUTERNAME_LENGTH};
+  if (!GetComputerName(machine_name.data(), &size))
     return "";
   return win32::strings::to_string(machine_name);
 }
@@ -262,7 +261,7 @@ void environment::get_os_version(int_least32_t& major, int_least32_t& minor, int
 string environment::get_service_pack() {
 #pragma warning(push)
 #pragma warning(disable : 4996)
-  OSVERSIONINFOEX version_info {};
+  auto version_info = OSVERSIONINFOEX {};
   version_info.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEX);
   if (GetVersionEx((LPOSVERSIONINFO)&version_info))
     return win32::strings::to_string(version_info.szCSDVersion);
@@ -271,13 +270,13 @@ string environment::get_service_pack() {
 }
 
 uint_least32_t environment::get_processor_count() {
-  SYSTEM_INFO system_info {};
+  auto system_info = SYSTEM_INFO {};
   GetNativeSystemInfo(&system_info);
   return system_info.dwNumberOfProcessors;
 }
 
 size_t environment::get_system_page_size() {
-  SYSTEM_INFO system_info {};
+  auto system_info = SYSTEM_INFO {};
   GetNativeSystemInfo(&system_info);
   return system_info.dwPageSize;
 }
@@ -296,9 +295,9 @@ string environment::get_user_domain_name() {
 }
 
 string environment::get_user_name() {
-  wchar_t user_name[UNLEN + 1];
-  DWORD size = UNLEN + 1;
-  if (!GetUserName(user_name, &size))
+  auto user_name = wstring(UNLEN, '\0');
+  auto size = DWORD {UNLEN};
+  if (!GetUserName(user_name.data(), &size))
     return "";
   return win32::strings::to_string(user_name);
 }
@@ -310,7 +309,7 @@ bool environment::has_shutdown_started() {
 
 bool environment::is_processor_arm() {
 
-  SYSTEM_INFO system_info {};
+  auto system_info = SYSTEM_INFO {};
   GetNativeSystemInfo(&system_info);
   if ((system_info.wProcessorArchitecture & PROCESSOR_ARCHITECTURE_ARM) == PROCESSOR_ARCHITECTURE_ARM) return true;
   if ((system_info.wProcessorArchitecture & PROCESSOR_ARCHITECTURE_ARM64) == PROCESSOR_ARCHITECTURE_ARM64) return true;
@@ -318,7 +317,7 @@ bool environment::is_processor_arm() {
 }
 
 bool environment::is_os_64_bit() {
-  SYSTEM_INFO system_info {};
+  auto system_info = SYSTEM_INFO {};
   GetNativeSystemInfo(&system_info);
   if ((system_info.wProcessorArchitecture & PROCESSOR_ARCHITECTURE_AMD64) == PROCESSOR_ARCHITECTURE_AMD64) return true;
   if ((system_info.wProcessorArchitecture & PROCESSOR_ARCHITECTURE_ARM64) == PROCESSOR_ARCHITECTURE_ARM64) return true;
