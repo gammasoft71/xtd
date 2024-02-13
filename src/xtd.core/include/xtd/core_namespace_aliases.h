a9b476a3bb8a6fc415ffe1f8cc15472ff5d66ae9
Gammasoft <gammasoft71@gmail.com>
2024-02-13 11:13:22 +0100

    Add core namespace aliases

---
+++ src/xtd.core/include/xtd/core_namespace_aliases.h
@@ -0,0 +1,11 @@
+/// @file
+/// @brief Contains namespace aliases.
+/// @copyright Copyright (c) 2024 Gammasoft. All rights reserved.
+#pragma once
+#undef linux
+
+/// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
+namespace xtd {}
+
+/// @brief The system_ namespace contains all fundamental classes to access Hardware, Os, System, and more.
+namespace system_ = xtd;
