80aff7250c79e315b1dacb98b2a8b820e326adab
gammasoft71 <gammasoft71@gmail.com>
2024-02-16 17:55:35 +0100

    Update latest reference guide

--- static/reference_guides/latest/search/enums_f.js
+++ static/reference_guides/latest/search/enums_f.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['radio_5fbutton_5fstate_0',['radio_button_state',['../group__xtd__forms.html#ga52e8a72a9cb9e386ed48fa62d74a065d',1,'xtd::forms::visual_styles']]],
-  ['right_5fto_5fleft_1',['right_to_left',['../group__xtd__forms.html#gac06dd62f5da1fa2f0e7c3ff5bfa6c94b',1,'xtd::forms']]],
-  ['rotate_5fflip_5ftype_2',['rotate_flip_type',['../group__xtd__drawing.html#gab56dabbbe7320df1f0de54a00ab869fa',1,'xtd::drawing']]]
+  ['reports_1',['reports',['../classxtd_1_1system__report.html#ad9aa20286ab053ab3e44c75417bc050b',1,'xtd::system_report']]],
+  ['right_5fto_5fleft_2',['right_to_left',['../group__xtd__forms.html#gac06dd62f5da1fa2f0e7c3ff5bfa6c94b',1,'xtd::forms']]],
+  ['rotate_5fflip_5ftype_3',['rotate_flip_type',['../group__xtd__drawing.html#gab56dabbbe7320df1f0de54a00ab869fa',1,'xtd::drawing']]]
 ];
