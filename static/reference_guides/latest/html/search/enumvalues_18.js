44c8f62f069d386b11b02713bff94107999bc4a4
gammasoft71 <gammasoft71@gmail.com>
2024-02-16 21:00:07 +0100

    update latest reference guide

---
+++ static/reference_guides/latest/html/search/enumvalues_18.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['y_0',['y',['../group__xtd__core.html#ggad94b2f43c7550180321e0ac4b02a4ff2a415290769594460e2e485922904f345d',1,'xtd::y'],['../group__xtd__forms.html#gga475781e3c7baa300a81ab16d22b16962a415290769594460e2e485922904f345d',1,'xtd::forms::y'],['../group__xtd__forms.html#gga2f00c1ad108efaddc951106e768970dea415290769594460e2e485922904f345d',1,'xtd::forms::y']]],
+  ['yellow_1',['yellow',['../namespacextd.html#ab60a2a64b74b91771b667e4a079681f2ad487dd0b55dfcacdd920ccbdaeafa351',1,'xtd::yellow'],['../group__xtd__drawing.html#ggabb6249e3fb8a4388778157257e99049bad487dd0b55dfcacdd920ccbdaeafa351',1,'xtd::drawing::yellow']]],
+  ['yellow_5fgreen_2',['yellow_green',['../group__xtd__drawing.html#ggabb6249e3fb8a4388778157257e99049baa889858bd19327fe8b643b9ff36a1782',1,'xtd::drawing']]],
+  ['yes_3',['yes',['../group__xtd__forms.html#gga4f0cffaa8fcb3708782f28d60386c525aa6105c0a611b41b08f1209506350279e',1,'xtd::forms::yes'],['../group__xtd__forms.html#ggac06dd62f5da1fa2f0e7c3ff5bfa6c94baa6105c0a611b41b08f1209506350279e',1,'xtd::forms::yes']]],
+  ['yes_5fno_4',['yes_no',['../group__xtd__forms.html#ggaba92ddccbdea7da5ddb782f27ad0c4e0a20f0596c21ad67a7fda598ffc7d038d1',1,'xtd::forms']]],
+  ['yes_5fno_5fcancel_5',['yes_no_cancel',['../group__xtd__forms.html#ggaba92ddccbdea7da5ddb782f27ad0c4e0a5c5cb337bb234e240912dc139048038c',1,'xtd::forms']]]
+];
