b88ce391371352de5b2def771adf1956e0d258a0
Gammasoft <gammasoft71@gmail.com>
2024-02-18 19:08:08 +0100

    Add noexcept attribute

--- src/xtd.drawing/include/xtd/drawing/system_colors.h
+++ src/xtd.drawing/include/xtd/drawing/system_colors.h
@@ -210,10 +210,10 @@ namespace xtd {
       
       /// @{
       /// @brief Gets an array of system colors.
-      static const std::vector<xtd::drawing::color>& get_colors();
+      static const std::vector<xtd::drawing::color>& get_colors() noexcept;
       
       /// @brief Gets an array of system color names.
-      static const std::vector<xtd::ustring>& get_color_names();
+      static const std::vector<xtd::ustring>& get_color_names() noexcept;
       /// @}
     };
   }
