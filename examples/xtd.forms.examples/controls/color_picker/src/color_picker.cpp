0be737adc7d7938b7bd9540332b7a939305ffa77
Gammasoft <gammasoft71@gmail.com>
2024-02-18 07:11:27 +0100

    Review example

--- examples/xtd.forms.examples/controls/color_picker/src/color_picker.cpp
+++ examples/xtd.forms.examples/controls/color_picker/src/color_picker.cpp
@@ -1,8 +1,10 @@
 #include <xtd/drawing/drawing_2d/hatch_brush>
+#include <xtd/drawing/color_translator>
 #include <xtd/forms/application>
 #include <xtd/forms/color_picker>
 #include <xtd/forms/control_paint>
 #include <xtd/forms/form>
+#include <xtd/forms/label>
 #include <xtd/forms/panel>
 
 using namespace xtd;
@@ -16,6 +18,15 @@ namespace color_picker_example {
     form1() {
       text("Color picker example");
       
+      color_picker.parent(*this);
+      color_picker.alpha_color(true);
+      color_picker.location({10, 10});
+      color_picker.color_picker_changed += [&](object& sender, const color_picker_event_args& e) {
+        test_zone.invalidate();
+        color_label.text(color_translator::to_html(e.color()));
+      };
+      color_picker.color(color::red);
+
       test_zone.parent(*this);
       test_zone.location({10, 50});
       test_zone.border_style(border_style::fixed_3d);
@@ -26,17 +37,15 @@ namespace color_picker_example {
         control_paint::draw_border(test_zone, e.graphics(), test_zone.border_style(), test_zone.border_sides(), application::style_sheet().system_colors().control_text(), e.clip_rectangle());
       };
       
-      color_picker.parent(*this);
-      color_picker.color(color::red);
-      color_picker.location({10, 10});
-      color_picker.color_picker_changed += [&] {
-        test_zone.invalidate();
-      };
+      color_label.parent(*this);
+      color_label.auto_size(true);
+      color_label.location({10, 160});
     }
     
   private:
     forms::color_picker color_picker;
     panel test_zone;
+    label color_label;
   };
 }
 
