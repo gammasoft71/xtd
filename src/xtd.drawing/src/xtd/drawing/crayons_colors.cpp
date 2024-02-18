e8d6f7fb6d49a2ff642f5a0b8b349f8ab4378e3b
Gammasoft <gammasoft71@gmail.com>
2024-02-18 19:09:15 +0100

    Add crayons_colors static class

---
+++ src/xtd.drawing/src/xtd/drawing/crayons_colors.cpp
@@ -0,0 +1,207 @@
+#include "../../../include/xtd/drawing/crayons_colors.h"
+
+using namespace std;
+using namespace xtd;
+using namespace xtd::drawing;
+
+color crayons_colors::aluminium() noexcept {
+  return color::from_known_color(known_color::crayons_aluminium);
+}
+
+color crayons_colors::aqua() noexcept {
+  return color::from_known_color(known_color::crayons_aqua);
+}
+
+color crayons_colors::asparagus() noexcept {
+  return color::from_known_color(known_color::crayons_asparagus);
+}
+
+color crayons_colors::banana() noexcept {
+  return color::from_known_color(known_color::crayons_banana);
+}
+
+color crayons_colors::blueberry() noexcept {
+  return color::from_known_color(known_color::crayons_blueberry);
+}
+
+color crayons_colors::bubblegum() noexcept {
+  return color::from_known_color(known_color::crayons_bubblegum);
+}
+
+color crayons_colors::cantaloupe() noexcept {
+  return color::from_known_color(known_color::crayons_cantaloupe);
+}
+
+color crayons_colors::carnation() noexcept {
+  return color::from_known_color(known_color::crayons_carnation);
+}
+
+color crayons_colors::cayenne() noexcept {
+  return color::from_known_color(known_color::crayons_cayenne);
+}
+
+color crayons_colors::clover() noexcept {
+  return color::from_known_color(known_color::crayons_clover);
+}
+
+color crayons_colors::eggplant() noexcept {
+  return color::from_known_color(known_color::crayons_eggplant);
+}
+
+color crayons_colors::fern() noexcept {
+  return color::from_known_color(known_color::crayons_fern);
+}
+
+color crayons_colors::flora() noexcept {
+  return color::from_known_color(known_color::crayons_flora);
+}
+
+color crayons_colors::grape() noexcept {
+  return color::from_known_color(known_color::crayons_grape);
+}
+
+color crayons_colors::honeydew() noexcept {
+  return color::from_known_color(known_color::crayons_honeydew);
+}
+
+color crayons_colors::ice() noexcept {
+  return color::from_known_color(known_color::crayons_ice);
+}
+
+color crayons_colors::iron() noexcept {
+  return color::from_known_color(known_color::crayons_iron);
+}
+
+color crayons_colors::lavendar() noexcept {
+  return color::from_known_color(known_color::crayons_lavendar);
+}
+
+color crayons_colors::lead() noexcept {
+  return color::from_known_color(known_color::crayons_lead);
+}
+
+color crayons_colors::lemon() noexcept {
+  return color::from_known_color(known_color::crayons_lemon);
+}
+
+color crayons_colors::licorice() noexcept {
+  return color::from_known_color(known_color::crayons_licorice);
+}
+
+color crayons_colors::lime() noexcept {
+  return color::from_known_color(known_color::crayons_lime);
+}
+
+color crayons_colors::magenta() noexcept {
+  return color::from_known_color(known_color::crayons_magenta);
+}
+
+color crayons_colors::magnesium() noexcept {
+  return color::from_known_color(known_color::crayons_magnesium);
+}
+
+color crayons_colors::maraschino() noexcept {
+  return color::from_known_color(known_color::crayons_maraschino);
+}
+
+color crayons_colors::maroon() noexcept {
+  return color::from_known_color(known_color::crayons_maroon);
+}
+
+color crayons_colors::mercury() noexcept {
+  return color::from_known_color(known_color::crayons_mercury);
+}
+
+color crayons_colors::midnight() noexcept {
+  return color::from_known_color(known_color::crayons_midnight);
+}
+
+color crayons_colors::mocha() noexcept {
+  return color::from_known_color(known_color::crayons_mocha);
+}
+
+color crayons_colors::moss() noexcept {
+  return color::from_known_color(known_color::crayons_moss);
+}
+
+color crayons_colors::nickel() noexcept {
+  return color::from_known_color(known_color::crayons_nickel);
+}
+
+color crayons_colors::ocean() noexcept {
+  return color::from_known_color(known_color::crayons_ocean);
+}
+
+color crayons_colors::orchid() noexcept {
+  return color::from_known_color(known_color::crayons_orchid);
+}
+
+color crayons_colors::plum() noexcept {
+  return color::from_known_color(known_color::crayons_plum);
+}
+
+color crayons_colors::salmon() noexcept {
+  return color::from_known_color(known_color::crayons_salmon);
+}
+
+color crayons_colors::sea_foam() noexcept {
+  return color::from_known_color(known_color::crayons_sea_foam);
+}
+
+color crayons_colors::silver() noexcept {
+  return color::from_known_color(known_color::crayons_silver);
+}
+
+color crayons_colors::sky() noexcept {
+  return color::from_known_color(known_color::crayons_sky);
+}
+
+color crayons_colors::snow() noexcept {
+  return color::from_known_color(known_color::crayons_snow);
+}
+
+color crayons_colors::spindrift() noexcept {
+  return color::from_known_color(known_color::crayons_spindrift);
+}
+
+color crayons_colors::spring() noexcept {
+  return color::from_known_color(known_color::crayons_spring);
+}
+
+color crayons_colors::steel() noexcept {
+  return color::from_known_color(known_color::crayons_steel);
+}
+
+color crayons_colors::strawberry() noexcept {
+  return color::from_known_color(known_color::crayons_strawberry);
+}
+
+color crayons_colors::tangerine() noexcept {
+  return color::from_known_color(known_color::crayons_tangerine);
+}
+
+color crayons_colors::teal() noexcept {
+  return color::from_known_color(known_color::crayons_teal);
+}
+
+color crayons_colors::tin() noexcept {
+  return color::from_known_color(known_color::crayons_tin);
+}
+
+color crayons_colors::tungsten() noexcept {
+  return color::from_known_color(known_color::crayons_tungsten);
+}
+
+color crayons_colors::turquoise() noexcept {
+  return color::from_known_color(known_color::crayons_turquoise);
+}
+
+const vector<color>& crayons_colors::get_colors() noexcept {
+  static auto colors = vector {crayons_colors::licorice(), crayons_colors::lead(), crayons_colors::tungsten(), crayons_colors::iron(), crayons_colors::steel(), crayons_colors::tin(), crayons_colors::nickel(), crayons_colors::aluminium(), crayons_colors::magnesium(), crayons_colors::silver(), crayons_colors::mercury(), crayons_colors::snow(), crayons_colors::cayenne(), crayons_colors::mocha(), crayons_colors::asparagus(), crayons_colors::fern(), crayons_colors::clover(), crayons_colors::moss(), crayons_colors::teal(), crayons_colors::ocean(), crayons_colors::midnight(), crayons_colors::eggplant(), crayons_colors::plum(), crayons_colors::maroon(), crayons_colors::maraschino(), crayons_colors::tangerine(), crayons_colors::lemon(), crayons_colors::lime(), crayons_colors::spring(), crayons_colors::sea_foam(), crayons_colors::turquoise(), crayons_colors::aqua(), crayons_colors::blueberry(), crayons_colors::grape(), crayons_colors::magenta(), crayons_colors::strawberry(), crayons_colors::salmon(), crayons_colors::cantaloupe(), crayons_colors::banana(), crayons_colors::honeydew(), crayons_colors::flora(), crayons_colors::spindrift(), crayons_colors::ice(), crayons_colors::sky(), crayons_colors::orchid(), crayons_colors::lavendar(), crayons_colors::bubblegum(), crayons_colors::carnation()};
+  return colors;
+}
+
+const vector<ustring>& crayons_colors::get_color_names() noexcept {
+  static auto color_names = vector {crayons_colors::licorice().name(), crayons_colors::lead().name(), crayons_colors::tungsten().name(), crayons_colors::iron().name(), crayons_colors::steel().name(), crayons_colors::tin().name(), crayons_colors::nickel().name(), crayons_colors::aluminium().name(), crayons_colors::magnesium().name(), crayons_colors::silver().name(), crayons_colors::mercury().name(), crayons_colors::snow().name(), crayons_colors::cayenne().name(), crayons_colors::mocha().name(), crayons_colors::asparagus().name(), crayons_colors::fern().name(), crayons_colors::clover().name(), crayons_colors::moss().name(), crayons_colors::teal().name(), crayons_colors::ocean().name(), crayons_colors::midnight().name(), crayons_colors::eggplant().name(), crayons_colors::plum().name(), crayons_colors::maroon().name(), crayons_colors::maraschino().name(), crayons_colors::tangerine().name(), crayons_colors::lemon().name(), crayons_colors::lime().name(), crayons_colors::spring().name(), crayons_colors::sea_foam().name(), crayons_colors::turquoise().name(), crayons_colors::aqua().name(), crayons_colors::blueberry().name(), crayons_colors::grape().name(), crayons_colors::magenta().name(), crayons_colors::strawberry().name(), crayons_colors::salmon().name(), crayons_colors::cantaloupe().name(), crayons_colors::banana().name(), crayons_colors::honeydew().name(), crayons_colors::flora().name(), crayons_colors::spindrift().name(), crayons_colors::ice().name(), crayons_colors::sky().name(), crayons_colors::orchid().name(), crayons_colors::lavendar().name(), crayons_colors::carnation().name()};
+  return color_names;
+}
