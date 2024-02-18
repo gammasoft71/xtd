f180a7348709923fe3b6ff04988e98dbc4df13f7
Gammasoft <gammasoft71@gmail.com>
2024-02-18 19:10:05 +0100

    Use choice for splitting colors static classes

--- examples/xtd.forms.examples/others/colors/src/color_chooser.h
+++ examples/xtd.forms.examples/others/colors/src/color_chooser.h
@@ -1,6 +1,21 @@
+#include <xtd/drawing/drawing_2d/hatch_brush>
 #include <xtd/drawing/apple_colors>
+#include <xtd/drawing/basic_colors>
+#include <xtd/drawing/blue_colors>
+#include <xtd/drawing/brown_colors>
 #include <xtd/drawing/colors>
+#include <xtd/drawing/crayons_colors>
+#include <xtd/drawing/cyan_colors>
+#include <xtd/drawing/gray_colors>
+#include <xtd/drawing/green_colors>
+#include <xtd/drawing/orange_colors>
+#include <xtd/drawing/pink_colors>
+#include <xtd/drawing/purple_colors>
+#include <xtd/drawing/red_colors>
+#include <xtd/drawing/white_colors>
+#include <xtd/drawing/yellow_colors>
 #include <xtd/drawing/system_colors>
+#include <xtd/forms/choice>
 #include <xtd/forms/user_control>
 #include <xtd/as>
 
@@ -15,9 +30,10 @@ namespace colors_example {
       
       void on_paint(xtd::forms::paint_event_args& e) override {
         xtd::forms::user_control::on_paint(e);
-        e.graphics().fill_rectangle(xtd::drawing::solid_brush {xtd::drawing::system_colors::control()}, 0, 0, 100, e.clip_rectangle().height());
-        e.graphics().fill_rectangle(xtd::drawing::solid_brush(color_), 0, 0, 100, e.clip_rectangle().height());
-        e.graphics().draw_string(color_.name(), font(), xtd::drawing::solid_brush {fore_color()}, 120, (e.clip_rectangle().height() - e.graphics().measure_string(color_.name(), font()).height()) / 2);
+        e.graphics().fill_rectangle(xtd::drawing::drawing_2d::hatch_brush(xtd::drawing::drawing_2d::hatch_style::wide_checker_board, xtd::drawing::color::from_argb(0x54, 0x54, 0x54), xtd::drawing::color::from_argb(0xA8, 0xA8, 0xA8)), 6, 4, 94, e.clip_rectangle().height() - 8);
+        e.graphics().fill_rectangle(xtd::drawing::solid_brush(color_), 6, 4, 94, e.clip_rectangle().height() - 8);
+        auto name = color_.name().replace("apple_", "").replace("crayons_", "");
+        e.graphics().draw_string(name, font(), xtd::drawing::solid_brush {fore_color()}, 110, (e.clip_rectangle().height() - e.graphics().measure_string(name, font()).height()) / 2);
       }
       
       auto color() const {return color_;}
@@ -37,19 +53,47 @@ namespace colors_example {
       border_style(xtd::forms::border_style::fixed_3d);
       back_color(xtd::drawing::system_colors::window());
       fore_color(xtd::drawing::system_colors::window_text());
-      auto_scroll(true);
-      
-      for (auto iterator = xtd::drawing::apple_colors::get_colors().rbegin(); iterator != xtd::drawing::apple_colors::get_colors().rend(); ++iterator)
-        add_color_panel(*iterator);
+      controls().push_back_range({main_panel, colors_choice});
+      padding(4);
 
-      for (auto iterator = xtd::drawing::system_colors::get_colors().rbegin(); iterator != xtd::drawing::system_colors::get_colors().rend(); ++iterator)
-        add_color_panel(*iterator);
-      
-      for (auto iterator = xtd::drawing::colors::get_colors().rbegin(); iterator != xtd::drawing::colors::get_colors().rend(); ++iterator)
-        add_color_panel(*iterator);
+      colors_choice.dock(xtd::forms::dock_style::top);
+      colors_choice.items().push_back_range({
+        {"Basic - xtd::drawing::basic_colors", &xtd::drawing::basic_colors::get_colors},
+        {"Blue - xtd::drawing::blue_colors", &xtd::drawing::blue_colors::get_colors},
+        {"Brown - xtd::drawing::brown_colors", &xtd::drawing::brown_colors::get_colors},
+        {"Cyan - xtd::drawing::cyan_colors", &xtd::drawing::cyan_colors::get_colors},
+        {"Gray - xtd::drawing::gray_colors", &xtd::drawing::gray_colors::get_colors},
+        {"Green - xtd::drawing::green_colors", &xtd::drawing::green_colors::get_colors},
+        {"Orange - xtd::drawing::orange_colors", &xtd::drawing::orange_colors::get_colors},
+        {"Pink - xtd::drawing::pink_colors", &xtd::drawing::pink_colors::get_colors},
+        {"Purple - xtd::drawing::purple_colors", &xtd::drawing::purple_colors::get_colors},
+        {"Red - xtd::drawing::red_colors", &xtd::drawing::red_colors::get_colors},
+        {"White - xtd::drawing::white_colors", &xtd::drawing::white_colors::get_colors},
+        {"Yellow - xtd::drawing::yellow_colors", &xtd::drawing::yellow_colors::get_colors},
+        {"Colors - xtd::drawing::colors", &xtd::drawing::colors::get_colors},
+        {"System - xtd::drawing::system_colors", &xtd::drawing::system_colors::get_colors},
+        {"Apple - xtd::drawing::apple_colors", &xtd::drawing::apple_colors::get_colors},
+        {"Crayons - xtd::drawing::crayons_colors", &xtd::drawing::crayons_colors::get_colors},
+      });
+      colors_choice.selected_index_changed += [&] {
+        auto index = selected_index();
+        selected_index(npos);
+        colors_.clear();
+        auto colors = std::any_cast<const std::vector<xtd::drawing::color>&(*)() noexcept>(colors_choice.selected_item().tag());
+        for (auto iterator = colors().rbegin(); iterator != colors().rend(); ++iterator)
+          add_color_panel(*iterator);
+        if (index == npos || colors_.size() == 0) return;
+        if (index < colors_.size()) selected_index(index);
+        else selected_index(colors_.size() - 1);
+        
+      };
+      colors_choice.selected_index(12);
+
+      main_panel.dock(xtd::forms::dock_style::fill);
+      main_panel.auto_scroll(true);
     }
     
-    auto selected_index() const {return selected_index_;}
+    size_t selected_index() const {return selected_index_;}
     void selected_index(size_t value) {
       if (selected_index_ != value) {
         selected_index_ = value;
@@ -57,7 +101,7 @@ namespace colors_example {
       }
     }
     
-    auto selected_color() const {return selected_color_;}
+    const xtd::drawing::color& selected_color() const {return selected_color_;}
     void selected_color(const xtd::drawing::color& value) {
       if (selected_color_ != value) {
         selected_color_ = value;
@@ -77,7 +121,7 @@ namespace colors_example {
       color_panel->color(color);
       color_panel->tag(colors_.size());
       colors_.push_back(color_panel);
-      controls().push_back(*color_panel);
+      main_panel.controls().push_back(*color_panel);
       color_panel->click += [&](xtd::object & sender, const xtd::event_args & e) {
         selected_index(colors_.size() - 1 - std::any_cast<size_t>(xtd::as<control>(sender).tag()));
       };
@@ -109,6 +153,8 @@ namespace colors_example {
     size_t previous_selected_index_ = npos;
     size_t selected_index_ = npos;
     xtd::drawing::color selected_color_ = xtd::drawing::color::empty;
+    xtd::forms::choice colors_choice;
+    xtd::forms::panel main_panel;
     std::vector<std::shared_ptr<color_panel>> colors_;
   };
 }
