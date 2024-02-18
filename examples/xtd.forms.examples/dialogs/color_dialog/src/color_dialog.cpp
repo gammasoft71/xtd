5d2901da70483be1642c9a9de786da5ceb488f04
Gammasoft <gammasoft71@gmail.com>
2024-02-18 08:13:04 +0100

    Update color_dialog.cpp

--- examples/xtd.forms.examples/dialogs/color_dialog/src/color_dialog.cpp
+++ examples/xtd.forms.examples/dialogs/color_dialog/src/color_dialog.cpp
@@ -18,44 +18,35 @@ class form1 : public form {
 public:
   form1() {
     text("Color dialog example");
-    controls().push_back(button1);
     
-    button1.location({10, 10});
-    button1.text("Color...");
     button1.click += [&] {
       auto dialog = color_dialog {};
       dialog.color(selected_color);
       dialog.custom_colors(custom_colors);
       if (dialog.show_dialog(*this) == forms::dialog_result::ok) {
         selected_color = dialog.color();
-        test_zone.invalidate();
         color_label.text(color_translator::to_html(selected_color));
+        test_zone.invalidate();
       }
       custom_colors = dialog.custom_colors();
     };
 
-    test_zone.parent(*this);
-    test_zone.location({10, 50});
-    test_zone.border_style(border_style::fixed_3d);
-    test_zone.double_buffered(true);
+    test_zone.border_style(border_style::inset).double_buffered(true);
     test_zone.paint += [&](object& sender, paint_event_args& e) {
-      e.graphics().fill_rectangle(hatch_brush(hatch_style::wide_checker_board, color::from_argb(0x54, 0x54, 0x54), color::from_argb(0xA8, 0xA8, 0xA8)), e.clip_rectangle());
-      e.graphics().fill_rectangle(solid_brush(selected_color), e.clip_rectangle());
+      e.graphics().fill_rectangle(hatch_brush {hatch_style::wide_checker_board, color::from_argb(0x54, 0x54, 0x54), color::from_argb(0xA8, 0xA8, 0xA8)}, e.clip_rectangle());
+      e.graphics().fill_rectangle(solid_brush {selected_color}, e.clip_rectangle());
       control_paint::draw_border(test_zone, e.graphics(), test_zone.border_style(), test_zone.border_sides(), application::style_sheet().system_colors().control_text(), e.clip_rectangle());
     };
     
-    color_label.parent(*this);
     color_label.auto_size(true);
-    color_label.text(color_translator::to_html(selected_color));
-    color_label.location({10, 160});
   }
   
 private:
-  button button1;
-  panel test_zone;
-  label color_label;
-  color selected_color = drawing::color::red;
+  color selected_color = system_colors::control();
   color_dialog::colors custom_colors = {color::red, color::green, color::blue, color::yellow, system_colors::control()};
+  button button1 = button::create(*this, "Color...", {10, 10});
+  panel test_zone = panel::create(*this, {10, 50});
+  label color_label = label::create(*this, color_translator::to_html(selected_color), {10, 160});
 };
 
 auto main()->int {
