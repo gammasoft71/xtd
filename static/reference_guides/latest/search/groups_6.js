06915e91b89ebaca688f0c34b9e92858f9f244c6
gammasoft71 <gammasoft71@gmail.com>
2024-02-22 23:30:08 +0100

    Update lates reference guide

--- static/reference_guides/latest/search/groups_6.js
+++ static/reference_guides/latest/search/groups_6.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['interfaces_0',['interfaces',['../group__interfaces.html',1,'']]],
-  ['io_1',['io',['../group__io.html',1,'']]]
+  ['images_0',['images',['../group__images.html',1,'']]],
+  ['interfaces_1',['interfaces',['../group__interfaces.html',1,'']]],
+  ['io_2',['io',['../group__io.html',1,'']]]
 ];
