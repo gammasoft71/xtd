203c091c7527ceed65c16323479854d3f5e3764a
Gammasoft <gammasoft71@gmail.com>
2024-02-25 20:14:04 +0100

    Fix disable state

--- src/xtd.forms/src/xtd/forms/loading_indicator_animation_bar.h
+++ src/xtd.forms/src/xtd/forms/loading_indicator_animation_bar.h
@@ -11,13 +11,15 @@ namespace xtd::forms {
       auto p = clip_rectangle.width() * 2 / intervals_;
       auto x = -clip_rectangle.width();
       auto y = (clip_rectangle.height() - h) / 2;
-      
+      auto opacity = enabled ? 32 : 64;
+      auto opacity_bar = enabled ? 255 : 0;
+
       auto graphics_path = xtd::drawing::drawing_2d::graphics_path {};
       graphics_path.add_rounded_rectangle(0, y, clip_rectangle.width(), h, h / 2.0f);
       auto region = xtd::drawing::region {graphics_path};
       graphics.clip(region);
-      graphics.clear(xtd::drawing::color::from_argb(static_cast<xtd::byte>(32), color));
-      graphics.fill_rounded_rectangle(xtd::drawing::solid_brush {xtd::drawing::color::from_argb(static_cast<xtd::byte>(255), color)}, x + (frame_ * p), y, w, h, h / 2);
+      graphics.clear(xtd::drawing::color::from_argb(static_cast<xtd::byte>(opacity), color));
+      graphics.fill_rounded_rectangle(xtd::drawing::solid_brush {xtd::drawing::color::from_argb(static_cast<xtd::byte>(opacity_bar), color)}, x + (frame_ * p), y, w, h, h / 2);
       graphics.reset_clip();
     }
     
