edcfabb09013c896fb80cc3c6eca6e3a32b36525
Gammasoft <gammasoft71@gmail.com>
2024-02-25 06:38:14 +0100

    Add create factories

--- src/xtd.forms/src/xtd/forms/fixed_layout_panel.cpp
+++ src/xtd.forms/src/xtd/forms/fixed_layout_panel.cpp
@@ -3,19 +3,57 @@
 using namespace xtd;
 using namespace xtd::forms;
 
+fixed_layout_panel fixed_layout_panel::create() {
+  return  fixed_layout_panel {};
+}
+
+fixed_layout_panel fixed_layout_panel::create(const drawing::point& location) {
+  auto result = fixed_layout_panel {};
+  result.location(location);
+  return result;
+}
+
+fixed_layout_panel fixed_layout_panel::create(const drawing::point& location, const drawing::size& size) {
+  auto result = fixed_layout_panel {};
+  result.location(location);
+  result.size(size);
+  return result;
+}
+
 fixed_layout_panel fixed_layout_panel::create(const drawing::point& location, const drawing::size& size, const xtd::ustring& name) {
   auto result = fixed_layout_panel {};
-  if (location != drawing::point {-1, -1}) result.location(location);
-  if (size != drawing::size {-1, -1}) result.size(size);
+  result.location(location);
+  result.size(size);
   result.name(name);
+  return result;
+}
+
+fixed_layout_panel fixed_layout_panel::create(const control& parent) {
+  auto result = fixed_layout_panel {};
+  result.parent(parent);
+  return result;
+}
+
+fixed_layout_panel fixed_layout_panel::create(const control& parent, const drawing::point& location) {
+  auto result = fixed_layout_panel {};
+  result.parent(parent);
+  result.location(location);
+  return result;
+}
+
+fixed_layout_panel fixed_layout_panel::create(const control& parent, const drawing::point& location, const drawing::size& size) {
+  auto result = fixed_layout_panel {};
+  result.parent(parent);
+  result.location(location);
+  result.size(size);
   return result;
 }
 
 fixed_layout_panel fixed_layout_panel::create(const control& parent, const drawing::point& location, const drawing::size& size, const xtd::ustring& name) {
   auto result = fixed_layout_panel {};
   result.parent(parent);
-  if (location != drawing::point {-1, -1}) result.location(location);
-  if (size != drawing::size {-1, -1}) result.size(size);
+  result.location(location);
+  result.size(size);
   result.name(name);
   return result;
 }
