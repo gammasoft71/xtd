00acbfbc7de3a770dcb528c7e7647138dbfc2503
Gammasoft <gammasoft71@gmail.com>
2024-02-25 08:54:22 +0100

    Add create factories

--- src/xtd.forms/src/xtd/forms/splitter_panel.cpp
+++ src/xtd.forms/src/xtd/forms/splitter_panel.cpp
@@ -3,19 +3,57 @@
 using namespace xtd;
 using namespace xtd::forms;
 
+splitter_panel splitter_panel::create() {
+  return splitter_panel {};
+}
+
+splitter_panel splitter_panel::create(const drawing::point& location) {
+  auto result = splitter_panel {};
+  result.location(location);
+  return result;
+}
+
+splitter_panel splitter_panel::create(const drawing::point& location, const drawing::size& size) {
+  auto result = splitter_panel {};
+  result.location(location);
+  result.size(size);
+  return result;
+}
+
 splitter_panel splitter_panel::create(const drawing::point& location, const drawing::size& size, const xtd::ustring& name) {
   auto result = splitter_panel {};
-  if (location != drawing::point {-1, -1}) result.location(location);
-  if (size != drawing::size {-1, -1}) result.size(size);
+  result.location(location);
+  result.size(size);
   result.name(name);
+  return result;
+}
+
+splitter_panel splitter_panel::create(const control& parent) {
+  auto result = splitter_panel {};
+  result.parent(parent);
+  return result;
+}
+
+splitter_panel splitter_panel::create(const control& parent, const drawing::point& location) {
+  auto result = splitter_panel {};
+  result.parent(parent);
+  result.location(location);
+  return result;
+}
+
+splitter_panel splitter_panel::create(const control& parent, const drawing::point& location, const drawing::size& size) {
+  auto result = splitter_panel {};
+  result.parent(parent);
+  result.location(location);
+  result.size(size);
   return result;
 }
 
 splitter_panel splitter_panel::create(const control& parent, const drawing::point& location, const drawing::size& size, const xtd::ustring& name) {
   auto result = splitter_panel {};
   result.parent(parent);
-  if (location != drawing::point {-1, -1}) result.location(location);
-  if (size != drawing::size {-1, -1}) result.size(size);
+  result.location(location);
+  result.size(size);
   result.name(name);
   return result;
 }
