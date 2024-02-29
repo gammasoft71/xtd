a1cee0bd1de20b8dc42c272078c5a284ea056906
Gammasoft <gammasoft71@gmail.com>
2024-02-29 21:50:45 +0100

    Add image_transformer class and move image effect methods to it

--- src/xtd.drawing/include/xtd/drawing/image.h
+++ src/xtd.drawing/include/xtd/drawing/image.h
@@ -31,6 +31,7 @@ namespace xtd {
   namespace drawing {
     /// @cond
     class bitmap;
+    class image_transformer;
     /// @endcond
     
     /// @brief An abstract base class that provides functionality for the bitmap and metafile descended classes.
@@ -163,51 +164,6 @@ namespace xtd {
       
       /// @{
 
-      /// @brief Returns a blur version of the specified image with radius value of 10.
-      /// @param image The image to blured.
-      /// @return the blured image result.
-      static image blur(const image& image);
-      
-      /// @brief Returns a blur version of the specified image with radius.
-      /// @param image The image to blured.
-      /// @param radius The radius of the blur, specified as a <length>.
-      /// @return the blured image result.
-      /// @remarks No blur effect, if the radius is `0`.
-      static image blur(const image& image, int32 radius);
-      
-      /// @brief Returns a brightness version of the specified image with percent factor.
-      /// @param image The image to brightness.
-      /// @param percent The percent factor.
-      /// @return The adjusted image result.
-      /// @remarks 0% will make the image completely black. 100% is default and represents the original image. Values over 100% will provide brighter results.
-      static image brightness(const image& image, double percent);
-      
-      /// @brief Returns a contrasted version of the specified image with percent factor.
-      /// @param image The image to change contrast.
-      /// @param percent The percent factor.
-      /// @return The contrasted image result.
-      /// @remarks Thus, `1.0` return the original image. while other will make a contrasted image.
-      static image contrast(const image& image, double percent);
-
-      /// @brief Returns a disabled version of the specified image and background color.
-      /// @param image The image to brightness.
-      /// @param back_color The reference background color.
-      /// @return The disabled image result.
-      static image disabled(const image& image, const color& back_color);
-      
-      /// @brief Returns a disabled version of the specified image and background color.
-      /// @param image The image to brightness.
-      /// @param brightness The brightness of the reference background color.
-      /// @return The disabled image result.
-      static image disabled(const image& image, float brightness);
-      
-      /// @brief Returns an invert version of the specified image with percent factor.
-      /// @param image The image to invert.
-      /// @param percent The percent factor.
-      /// @return The adjusted image result.
-      /// @remarks 0% will represents the original image. 100% will provide image with inverted colors.
-      static image invert(const image& image, double percent);
-
       /// @brief Creates the xtd::drawing::graphics for the image.
       /// @return A xtd::drawing::graphics for the image.
       graphics create_graphics();
@@ -310,18 +266,6 @@ namespace xtd {
       /// @remarks If the xtd::drawing::image contains an embedded thumbnail image, this method retrieves the embedded thumbnail and scales it to the requested size. If the xtd::drawing::image does not contain an embedded thumbnail image, this method creates a thumbnail image by scaling the main image.
       /// @remarks The xtd::drawing::image::get_thumbnail_image method works well when the requested thumbnail image has a size of about 120 x 120 pixels. If you request a large thumbnail image (for example, 300 x 300) from an xtd::drawing::image that has an embedded thumbnail, there could be a noticeable loss of quality in the thumbnail image. It might be better to scale the main image (instead of scaling the embedded thumbnail) by calling the xtd::drawing::graphics::draw_image method.
       xtd::drawing::image get_thmbnail_image(int32 thumb_width, int32 thunb_height) noexcept;
-      
-      /// @brief Returns a grayscale version of the specified image with percent factor.
-      /// @param image The image to grayscale.
-      /// @param percent The percent factor.
-      /// @return the grayscale image result.
-      /// @remarks Thus, `1.0` will make the color completely gray, while `0.0` return the original image.
-      static image grayscale(const image& image, double percent);
-      
-      /// @brief Returns a grayscale version of the specified image.
-      /// @param image The image to grayscale.
-      /// @return the grayscale image result.
-      static image grayscale(const image& image);
 
       /// @brief Returns a value that indicates whether the pixel format for this xtd::drawing::image contains alpha information.
       /// @param pixfmt The PixelFormat to test.
@@ -337,13 +281,6 @@ namespace xtd {
       /// @param pixfmt The PixelFormat to test.
       /// @return true if pixfmt is extended; otherwise, false.
       static bool is_extended_pixel_format(xtd::drawing::imaging::pixel_format pixfmt) noexcept;
-      
-      /// @brief Returns an opacity level version of the specified image with percent factor.
-      /// @param image The image to change the opacity level.
-      /// @param percent The percent factor.
-      /// @return The adjusted image result.
-      /// @remarks 0% will represents a transparent image. 100% will provide the original image.
-      static image opacity(const image& image, double percent);
 
       /// @brief Rotates, flips, or rotates and flips the xtd::drawing::image.
       /// @param rotate_flip_type A xtd::drawing::rotate_flip_type member that specifies the type of rotation and flip to apply to the image.
@@ -351,24 +288,6 @@ namespace xtd {
       /// @remarks If you wish to draw on an image once it has been rotated, you should always retrieve a new graphics object from the image, otherwise an exception could occur.
       void rotate_flip(xtd::drawing::rotate_flip_type rotate_flip_type);
       
-      /// @brief Rotates, flips, or rotates and flips the xtd::drawing::image.
-      /// @param rotate_flip_type A xtd::drawing::rotate_flip_type member that specifies the type of rotation and flip to apply to the image.
-      /// @remarks The xtd::drawing::image::rotate_flip method rotates the image clockwise.
-      /// @remarks If you wish to draw on an image once it has been rotated, you should always retrieve a new graphics object from the image, otherwise an exception could occur.
-      static image rotate_flip(const image& image, xtd::drawing::rotate_flip_type rotate_flip_type);
-      
-      /// @brief Returns a sepia version of the specified image with percent factor.
-      /// @param image The image to sepia.
-      /// @param percent The percent factor.
-      /// @return the sepia image result.
-      /// @remarks Thus, `1.0` will make the color completely sepia, while `0.0` return the original image.
-      static image sepia(const image& image, double percent);
-      
-      /// @brief Returns a sepia version of the specified image.
-      /// @param image The image to sepia.
-      /// @return the sepia image result.
-      static image sepia(const image& image);
-
       /// @brief Saves this xtd::drawing::image to the specified file or stream.
       /// @param filename A string that contains the name of the file to which to save this xtd::drawing::image.
       /// @remarks If no encoder exists for the file format of the image, the Portable Network Graphics (PNG) encoder is used. When you use the xtd::drawing::image::save method to save a graphic image as a Windows Metafile Format (WMF) or Enhanced Metafile Format (EMF) file, the resulting file is saved as a Portable Network Graphics (PNG) file. This behavior occurs because the GDI+ component of the .NET Framework does not have an encoder that you can use to save files as .wmf or .emf files.
@@ -407,6 +326,8 @@ namespace xtd {
       /// @endcond
       
     private:
+      friend class image_transformer;
+      static image blur(const image& image, int32 radius);
       void update_properties();
       
       std::shared_ptr<data> data_;
---
+++ src/xtd.drawing/include/xtd/drawing/image_transformer.h
@@ -0,0 +1,119 @@
+/// @file
+/// @brief Contains xtd::drawing::image_transformer class.
+/// @copyright Copyright (c) 2024 Gammasoft. All rights reserved.
+#pragma once
+#include "../drawing_export.h"
+#include "image.h"
+#include "rotate_flip_type.h"
+#include <xtd/static>
+
+/// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
+namespace xtd {
+  /// @brief The xtd::drawing namespace provides access to GDI+ basic graphics functionality. More advanced functionality is provided in the xtd::drawing::drawing_2d, xtd::drawing::imaging, and xtd::drawing::text namespaces.
+  namespace drawing {
+    /// @cond
+    class bitmap;
+    /// @endcond
+    
+    /// @brief Transforme images to and from xtd::drawing::image class. This class cannot be inherited.
+    /// @code
+    /// class drawing_export_ image_transformer final static_
+    /// @endcode
+    /// @par Inheritance
+    /// xtd::static_object → xtd::drawing::image_transformer
+    /// @par Namespace
+    /// xtd::drawing
+    /// @par Library
+    /// xtd.drawing
+    /// @ingroup xtd_drawing image
+    class drawing_export_ image_transformer final static_ {
+    public:
+      /// @name Methods
+      
+      /// @{
+      
+      /// @brief Returns a blur version of the specified image with radius value of 10.
+      /// @param image The image to blured.
+      /// @return the blured image result.
+      static xtd::drawing::image blur(const xtd::drawing::image& image);
+      
+      /// @brief Returns a blur version of the specified image with radius.
+      /// @param image The image to blured.
+      /// @param radius The radius of the blur, specified as a <length>.
+      /// @return the blured image result.
+      /// @remarks No blur effect, if the radius is `0`.
+      static xtd::drawing::image blur(const xtd::drawing::image& image, int32 radius);
+      
+      /// @brief Returns a brightness version of the specified image with percent factor.
+      /// @param image The image to brightness.
+      /// @param percent The percent factor.
+      /// @return The adjusted image result.
+      /// @remarks 0% will make the image completely black. 100% is default and represents the original image. Values over 100% will provide brighter results.
+      static xtd::drawing::image brightness(const xtd::drawing::image& image, double percent);
+      
+      /// @brief Returns a contrasted version of the specified image with percent factor.
+      /// @param image The image to change contrast.
+      /// @param percent The percent factor.
+      /// @return The contrasted image result.
+      /// @remarks Thus, `1.0` return the original image. while other will make a contrasted image.
+      static xtd::drawing::image contrast(const xtd::drawing::image& image, double percent);
+      
+      /// @brief Returns a disabled version of the specified image and background color.
+      /// @param image The image to brightness.
+      /// @param back_color The reference background color.
+      /// @return The disabled image result.
+      static xtd::drawing::image disabled(const xtd::drawing::image& image, const color& back_color);
+      
+      /// @brief Returns a disabled version of the specified image and background color.
+      /// @param image The image to brightness.
+      /// @param brightness The brightness of the reference background color.
+      /// @return The disabled image result.
+      static xtd::drawing::image disabled(const xtd::drawing::image& image, float brightness);
+      
+      /// @brief Returns an invert version of the specified image with percent factor.
+      /// @param image The image to invert.
+      /// @param percent The percent factor.
+      /// @return The adjusted image result.
+      /// @remarks 0% will represents the original image. 100% will provide image with inverted colors.
+      static xtd::drawing::image invert(const xtd::drawing::image& image, double percent);
+      
+      /// @brief Returns a grayscale version of the specified image with percent factor.
+      /// @param image The image to grayscale.
+      /// @param percent The percent factor.
+      /// @return the grayscale image result.
+      /// @remarks Thus, `1.0` will make the color completely gray, while `0.0` return the original image.
+      static xtd::drawing::image grayscale(const xtd::drawing::image& image, double percent);
+      
+      /// @brief Returns a grayscale version of the specified image.
+      /// @param image The image to grayscale.
+      /// @return the grayscale image result.
+      static xtd::drawing::image grayscale(const xtd::drawing::image& image);
+      
+      /// @brief Returns an opacity level version of the specified image with percent factor.
+      /// @param image The image to change the opacity level.
+      /// @param percent The percent factor.
+      /// @return The adjusted image result.
+      /// @remarks 0% will represents a transparent image. 100% will provide the original image.
+      static xtd::drawing::image opacity(const xtd::drawing::image& image, double percent);
+      
+      /// @brief Rotates, flips, or rotates and flips the xtd::drawing::image.
+      /// @param rotate_flip_type A xtd::drawing::rotate_flip_type member that specifies the type of rotation and flip to apply to the image.
+      /// @remarks The xtd::drawing::image::rotate_flip method rotates the image clockwise.
+      /// @remarks If you wish to draw on an image once it has been rotated, you should always retrieve a new graphics object from the image, otherwise an exception could occur.
+      static xtd::drawing::image rotate_flip(const xtd::drawing::image& image, xtd::drawing::rotate_flip_type rotate_flip_type);
+      
+      /// @brief Returns a sepia version of the specified image with percent factor.
+      /// @param image The image to sepia.
+      /// @param percent The percent factor.
+      /// @return the sepia image result.
+      /// @remarks Thus, `1.0` will make the color completely sepia, while `0.0` return the original image.
+      static xtd::drawing::image sepia(const xtd::drawing::image& image, double percent);
+      
+      /// @brief Returns a sepia version of the specified image.
+      /// @param image The image to sepia.
+      /// @return the sepia image result.
+      static xtd::drawing::image sepia(const xtd::drawing::image& image);
+      /// @}
+    };
+  }
+}
