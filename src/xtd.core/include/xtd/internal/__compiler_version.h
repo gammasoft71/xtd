7c3f8330501dbe999221de7e2cc9924ca2af2880
Gammasoft <gammasoft71@gmail.com>
2024-03-15 10:59:46 +0100

    Update __compiler_version.h

--- src/xtd.core/include/xtd/internal/__compiler_version.h
+++ src/xtd.core/include/xtd/internal/__compiler_version.h
@@ -15,10 +15,14 @@
 #  define __compiler_version xtd::version {_MSC_FULL_VER / 10000000, _MSC_FULL_VER % 10000000 / 100000, _MSC_FULL_VER % 100000}
 #elif defined(_MSC_VER)
 #  define __compiler_version xtd::version {_MSC_VER / 100, _MSC_VER % 100}
-#elif defined(__clang__)
+#elif defined(__clang__) && __clang_patchlevel__ != 0
 #  define __compiler_version xtd::version {__clang_major__, __clang_minor__, __clang_patchlevel__}
-#elif defined(__GNUC__)
+#elif defined(__clang__)
+#  define __compiler_version xtd::version {__clang_major__, __clang_minor__}
+#elif defined(__GNUC__) && __GNUC_PATCHLEVEL__ != 0
 #  define __compiler_version xtd::version {__GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__}
+#elif defined(__GNUC__)
+#  define __compiler_version xtd::version {__GNUC__, __GNUC_MINOR__}
 #else
 #  define __compiler_version xtd::version {}
 #endif
