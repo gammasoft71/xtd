587d6ff88462d472a30c426f67dab3cf15c20e2c
Gammasoft <gammasoft71@gmail.com>
2024-03-15 16:58:55 +0100

    Revue compiler informations

--- src/xtd.core/include/xtd/compiler_id.h
+++ src/xtd.core/include/xtd/compiler_id.h
@@ -18,20 +18,22 @@ namespace xtd {
   enum class compiler_id {
     /// @brief The compiler is unknown.
     unknown = -1,
-    /// @brief The compiler is Visual Studio.
-    visual_studio = 0,
     /// @brief The compiler is Microsoft Visual Studio.
-    /// @deprecated Replaced by xtd::compiler_id::visual_studio - Will be removed in version 0.4.0
-    microsoft_visual_studio [[deprecated("Replaced by xtd::compiler_id::visual_studio - Will be removed in version 0.4.0")]] = visual_studio,
+    msvc,
     /// @brief The compiler is clang.
-    clang = 1,
+    clang,
     /// @brief The compiler is gcc.
-    gcc = 2,
+    gcc,
+    /// @brief The compiler is Apple clang.
+    apple_clang,
+    /// @brief The compiler is Microsoft Visual Studio.
+    /// @deprecated Replaced by xtd::compiler_id::msvc - Will be removed in version 0.4.0
+    microsoft_visual_studio [[deprecated("Replaced by xtd::compiler_id::msvc - Will be removed in version 0.4.0")]] = msvc,
   };
 }
 
 /// @cond
 template<> struct xtd::enum_register<xtd::compiler_id> {
-  explicit operator auto() const noexcept {return xtd::enum_collection<xtd::compiler_id> {{xtd::compiler_id::unknown, "unknown"}, {xtd::compiler_id::visual_studio, "visual_studio"}, {xtd::compiler_id::clang, "clang"}, {xtd::compiler_id::gcc, "gcc"}};}
+  explicit operator auto() const noexcept {return xtd::enum_collection<xtd::compiler_id> {{xtd::compiler_id::unknown, "unknown"}, {xtd::compiler_id::msvc, "msvc"}, {xtd::compiler_id::clang, "clang"}, {xtd::compiler_id::gcc, "gcc"}, {xtd::compiler_id::apple_clang, "apple_clang"}};}
 };
 /// @endcond
