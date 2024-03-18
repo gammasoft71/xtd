21583df6933c64f865b42b17911ff8f6613b93eb
Gammasoft <gammasoft71@gmail.com>
2024-03-18 09:22:32 +0100

    Fix unit tests

--- tests/xtd.forms.unit_tests/src/xtd/forms/tests/message_dialog_icon_tests.cpp
+++ tests/xtd.forms.unit_tests/src/xtd/forms/tests/message_dialog_icon_tests.cpp
@@ -15,22 +15,10 @@ namespace xtd::forms::tests {
       assert::are_equal(message_dialog_icon::none, enum_object<>::parse<message_dialog_icon>("none"), csf_);
     }
     
-    void test_method_(hand) {
-      assert::are_equal(16, enum_object<>::to_int32(message_dialog_icon::hand), csf_);
-      assert::are_equal("hand", enum_object<>::to_string(message_dialog_icon::hand), csf_);
-      assert::are_equal(message_dialog_icon::hand, enum_object<>::parse<message_dialog_icon>("hand"), csf_);
-    }
-    
-    void test_method_(stop) {
-      assert::are_equal(16, enum_object<>::to_int32(message_dialog_icon::stop), csf_);
-      assert::are_equal("hand", enum_object<>::to_string(message_dialog_icon::stop), csf_);
-      assert::are_equal(message_dialog_icon::stop, enum_object<>::parse<message_dialog_icon>("hand"), csf_);
-    }
-    
     void test_method_(error) {
       assert::are_equal(16, enum_object<>::to_int32(message_dialog_icon::error), csf_);
-      assert::are_equal("hand", enum_object<>::to_string(message_dialog_icon::error), csf_);
-      assert::are_equal(message_dialog_icon::error, enum_object<>::parse<message_dialog_icon>("hand"), csf_);
+      assert::are_equal("error", enum_object<>::to_string(message_dialog_icon::error), csf_);
+      assert::are_equal(message_dialog_icon::error, enum_object<>::parse<message_dialog_icon>("error"), csf_);
     }
     
     void test_method_(question) {
@@ -39,28 +27,40 @@ namespace xtd::forms::tests {
       assert::are_equal(message_dialog_icon::question, enum_object<>::parse<message_dialog_icon>("question"), csf_);
     }
     
-    void test_method_(exclamation) {
-      assert::are_equal(48, enum_object<>::to_int32(message_dialog_icon::exclamation), csf_);
-      assert::are_equal("exclamation", enum_object<>::to_string(message_dialog_icon::exclamation), csf_);
-      assert::are_equal(message_dialog_icon::exclamation, enum_object<>::parse<message_dialog_icon>("exclamation"), csf_);
-    }
-    
     void test_method_(warning) {
       assert::are_equal(48, enum_object<>::to_int32(message_dialog_icon::warning), csf_);
-      assert::are_equal("exclamation", enum_object<>::to_string(message_dialog_icon::warning), csf_);
-      assert::are_equal(message_dialog_icon::warning, enum_object<>::parse<message_dialog_icon>("exclamation"), csf_);
-    }
-    
-    void test_method_(asterisk) {
-      assert::are_equal(64, enum_object<>::to_int32(message_dialog_icon::asterisk), csf_);
-      assert::are_equal("asterisk", enum_object<>::to_string(message_dialog_icon::asterisk), csf_);
-      assert::are_equal(message_dialog_icon::asterisk, enum_object<>::parse<message_dialog_icon>("asterisk"), csf_);
+      assert::are_equal("warning", enum_object<>::to_string(message_dialog_icon::warning), csf_);
+      assert::are_equal(message_dialog_icon::warning, enum_object<>::parse<message_dialog_icon>("warning"), csf_);
     }
     
     void test_method_(information) {
       assert::are_equal(64, enum_object<>::to_int32(message_dialog_icon::information), csf_);
-      assert::are_equal("asterisk", enum_object<>::to_string(message_dialog_icon::information), csf_);
-      assert::are_equal(message_dialog_icon::information, enum_object<>::parse<message_dialog_icon>("asterisk"), csf_);
+      assert::are_equal("information", enum_object<>::to_string(message_dialog_icon::information), csf_);
+      assert::are_equal(message_dialog_icon::information, enum_object<>::parse<message_dialog_icon>("information"), csf_);
+    }
+
+    void test_method_(hand) {
+      assert::are_equal(16, enum_object<>::to_int32(message_dialog_icon::hand), csf_);
+      assert::are_equal("error", enum_object<>::to_string(message_dialog_icon::hand), csf_);
+      assert::are_equal(message_dialog_icon::hand, enum_object<>::parse<message_dialog_icon>("error"), csf_);
+    }
+    
+    void test_method_(stop) {
+      assert::are_equal(16, enum_object<>::to_int32(message_dialog_icon::stop), csf_);
+      assert::are_equal("error", enum_object<>::to_string(message_dialog_icon::stop), csf_);
+      assert::are_equal(message_dialog_icon::stop, enum_object<>::parse<message_dialog_icon>("error"), csf_);
+    }
+    
+    void test_method_(exclamation) {
+      assert::are_equal(48, enum_object<>::to_int32(message_dialog_icon::exclamation), csf_);
+      assert::are_equal("warning", enum_object<>::to_string(message_dialog_icon::exclamation), csf_);
+      assert::are_equal(message_dialog_icon::exclamation, enum_object<>::parse<message_dialog_icon>("warning"), csf_);
+    }
+    
+    void test_method_(asterisk) {
+      assert::are_equal(64, enum_object<>::to_int32(message_dialog_icon::asterisk), csf_);
+      assert::are_equal("information", enum_object<>::to_string(message_dialog_icon::asterisk), csf_);
+      assert::are_equal(message_dialog_icon::asterisk, enum_object<>::parse<message_dialog_icon>("information"), csf_);
     }
   };
 }
