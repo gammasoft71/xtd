e18001cb4a2683211e0bc118cb6ecd305b117655
Gammasoft <gammasoft71@gmail.com>
2024-03-21 14:33:27 +0100

    Add documentation

--- src/xtd.core/include/xtd/speech/synthesis/prompt_event_args.h
+++ src/xtd.core/include/xtd/speech/synthesis/prompt_event_args.h
@@ -40,7 +40,10 @@ namespace xtd {
         prompt_event_args(prompt_event_args&&) = default;
         prompt_event_args& operator =(const prompt_event_args&) = default;
         /// @endcond
-        
+
+        /// @name Public Properties
+
+        /// @{
         /// @brief Gets a value indicating whether an asynchronous operation has been canceled.
         /// @return true if the background operation has been canceled; otherwise false. The default is false.
         bool cancelled() const noexcept {return cancelled_;}
@@ -56,6 +59,7 @@ namespace xtd {
         /// @brief Gets the unique identifier for the asynchronous task.
         /// @return An object reference that uniquely identifies the asynchronous task.
         std::any user_state() const noexcept {return user_state_;}
+        /// @}
         
       private:
         prompt_event_args(bool cancelled, const xtd::system_exception* error, xtd::speech::synthesis::prompt* prompt, std::any user_state) : cancelled_(cancelled), error_(error), prompt_(prompt), user_state_(user_state) {}
