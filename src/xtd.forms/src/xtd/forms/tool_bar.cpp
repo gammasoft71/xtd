66e8fe61d61abe491452f5247ff1c78bbf066917
Gammasoft <gammasoft71@gmail.com>
2024-03-31 19:02:00 +0200

    Move constructor implementation

--- src/xtd.forms/src/xtd/forms/tool_bar.cpp
+++ src/xtd.forms/src/xtd/forms/tool_bar.cpp
@@ -72,7 +72,16 @@ tool_bar::tool_bar() : data_(std::make_shared<data>()) {
   set_style(control_styles::user_paint, false);
   set_style(control_styles::fixed_height, auto_size());
   set_style(control_styles::fixed_width, false);
-  
+}
+
+tool_bar::tool_bar(tool_bar&& rhs) : control(std::move(rhs)) {
+  rhs.data_->buttons.item_added -= {rhs, &tool_bar::on_item_added};
+  rhs.data_->buttons.item_updated -= {rhs, &tool_bar::on_item_updated};
+  rhs.data_->buttons.item_removed -= {rhs, &tool_bar::on_item_removed};
+  data_ = std::move(rhs.data_);
+  data_->buttons.item_added += {*this, &tool_bar::on_item_added};
+  data_->buttons.item_updated += {*this, &tool_bar::on_item_updated};
+  data_->buttons.item_removed += {*this, &tool_bar::on_item_removed};
 }
 
 xtd::forms::tool_bar_appearance tool_bar::appearance() const noexcept {
