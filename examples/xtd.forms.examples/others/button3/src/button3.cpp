44649ba6eb75d801d116036aeb8a6d4178a5c4b5
Gammasoft <gammasoft71@gmail.com>
2024-03-31 13:47:49 +0200

    Add auto_reapeat

--- examples/xtd.forms.examples/others/button3/src/button3.cpp
+++ examples/xtd.forms.examples/others/button3/src/button3.cpp
@@ -10,7 +10,7 @@ auto main()->int {
   xtd::forms::form form1;
   
   button1.text("Button 1").location({50, 50});
-  button2.text("Button 2").location({50, 100}).size({200, 75});
+  button2.auto_repeat(true).text("Button 2").location({50, 100}).size({200, 75});
   label1.text("Button 1 clicked 0 times").location({50, 200}).width(200);
   label2.text("Button 2 clicked 0 times").location({50, 230}).width(200);
   button1.click += [&] {label1.text(xtd::ustring::format("Button 1 clicked {} times", ++button1_clicked));};
--- examples/xtd.forms.examples/others/button4/src/button4.cpp
+++ examples/xtd.forms.examples/others/button4/src/button4.cpp
@@ -11,6 +11,6 @@ auto main()->int {
   auto label1 = xtd::forms::label::create(form1, "Button 1 clicked 0 times", {50, 200}, {200, 23});
   auto label2 = xtd::forms::label::create(form1, "Button 2 clicked 0 times", {50, 230}, {200, 23});
   button1.click += [&] {label1.text(xtd::ustring::format("Button 1 clicked {} times", ++button1_clicked));};
-  button2.click += [&] {label2.text(xtd::ustring::format("Button 2 clicked {} times", ++button2_clicked));};
+  button2.auto_repeat(true).click += [&] {label2.text(xtd::ustring::format("Button 2 clicked {} times", ++button2_clicked));};
   xtd::forms::application::run(form1);
 }
