467c76abd1d24e2ea9515972912b71eac1a33c10
Gammasoft <gammasoft71@gmail.com>
2024-04-02 17:52:03 +0200

    Add clone method

--- src/xtd.forms/include/xtd/forms/date_time_picker.h
+++ src/xtd.forms/include/xtd/forms/date_time_picker.h
@@ -333,6 +333,12 @@ namespace xtd {
       /// @name Protected methods
       
       /// @{
+      /// @brief Creates a new object that is a copy of the current instance.
+      /// @return A new object that is a copy of this instance.
+      /// @par Notes to Implementers
+      /// All controls must be override the clone method.
+      std::unique_ptr<xtd::object> clone() const override;
+      
       /// @brief Raises the date_time_picker::close_up event.
       /// @param e An event_args that contains the event data.
       virtual void on_close_up(const event_args& e);
