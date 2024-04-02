1586c83ae9e14ddc3a76e9caf78180c15dd52f1b
Gammasoft <gammasoft71@gmail.com>
2024-04-02 21:26:11 +0200

    Add clone method

--- src/xtd.forms/include/xtd/forms/nine_segment_display.h
+++ src/xtd.forms/include/xtd/forms/nine_segment_display.h
@@ -199,6 +199,12 @@ namespace xtd {
       /// @name Protected methods
       
       /// @{
+      /// @brief Creates a new object that is a copy of the current instance.
+      /// @return A new object that is a copy of this instance.
+      /// @par Notes to Implementers
+      /// All controls must be override the clone method.
+      std::unique_ptr<xtd::object> clone() const override;
+      
       void draw_back_digit(drawing::graphics& graphics) override;
       
       /// @brief Draw segment h on specified graphics with specified color.
