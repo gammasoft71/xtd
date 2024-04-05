1929412cd30fab001c04d1117ca26a4d62cbdbce
Gammasoft <gammasoft71@gmail.com>
2024-04-05 06:43:04 +0200

    Add clone method

---
+++ src/xtd.forms/src/xtd/forms/debug_form.cpp
@@ -0,0 +1,27 @@
+#include "../../../include/xtd/forms/debug_form.h"
+
+using namespace std;
+using namespace xtd;
+using namespace xtd::diagnostics;
+using namespace xtd::forms;
+
+debug_form::~debug_form() {
+  debug::listeners().erase(std::find(debug::listeners().begin(), debug::listeners().end(), listener_));
+}
+
+const xtd::diagnostics::trace_listener& debug_form::trace_listener() const noexcept {
+  return *listener_;
+}
+
+xtd::diagnostics::trace_listener& debug_form::trace_listener() {
+  return *listener_;
+}
+
+void debug_form::flush() override {
+}
+
+unique_ptr<xtd::object> debug_form::clone() const {
+  auto result = make_unique<debug_form>(*this);
+  if (typeof_(*result) != typeof_(*this)) throw xtd::invalid_cast_exception(xtd::ustring::format("The {} does not implement clone method.", typeof_(*this).full_name()), csf_);
+  return result;
+}
