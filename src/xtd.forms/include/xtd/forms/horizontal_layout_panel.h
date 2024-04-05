12a3667e6456ff62dfaea293754d73fefae4123c
Gammasoft <gammasoft71@gmail.com>
2024-04-05 06:04:27 +0200

    Add clone method

--- src/xtd.forms/include/xtd/forms/horizontal_layout_panel.h
+++ src/xtd.forms/include/xtd/forms/horizontal_layout_panel.h
@@ -102,6 +102,12 @@ namespace xtd {
       /// @name Protected methods
       
       /// @{
+      /// @brief Creates a new object that is a copy of the current instance.
+      /// @return A new object that is a copy of this instance.
+      /// @par Notes to Implementers
+      /// All controls must be override the clone method.
+      std::unique_ptr<xtd::object> clone() const override;
+      
       void on_control_added(const xtd::forms::control_event_args& e) override;
       void on_control_removed(const xtd::forms::control_event_args& e) override;
       void on_layout(const xtd::event_args& e) override;
--- src/xtd.forms/src/xtd/forms/horizontal_layout_panel.cpp
+++ src/xtd.forms/src/xtd/forms/horizontal_layout_panel.cpp
@@ -80,6 +80,12 @@ horizontal_layout_panel horizontal_layout_panel::create(const control& parent, c
   return result;
 }
 
+unique_ptr<xtd::object> horizontal_layout_panel::clone() const {
+  auto result = make_unique<horizontal_layout_panel>(*this);
+  if (typeof_(*result) != typeof_(*this)) throw xtd::invalid_cast_exception(xtd::ustring::format("The {} does not implement clone method.", typeof_(*this).full_name()), csf_);
+  return result;
+}
+
 void horizontal_layout_panel::on_control_added(const xtd::forms::control_event_args& e) {
   panel::on_control_added(e);
   control_layout_styles_.emplace_back(control_ref(const_cast<xtd::forms::control&>(e.control())), horizontal_control_layout_style());
