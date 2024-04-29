d5afd9cb9c202c022115af888bc64fba0ce9adb6
Gammasoft <gammasoft71@gmail.com>
2024-04-29 20:23:21 +0200

    Update screen_informations.cpp

--- examples/xtd.forms.examples/others/screen_informations/src/screen_informations.cpp
+++ examples/xtd.forms.examples/others/screen_informations/src/screen_informations.cpp
@@ -23,10 +23,10 @@ public:
     screen_informations_text_box.word_wrap(false);
     screen_informations_text_box.append_text(ustring::format("Number of screens: {}{}", screen::all_screens().size(), environment::new_line()));
     screen_informations_text_box.append_text(ustring::format("Standard pixels per inch: {}{}", screen::get_standard_pixels_per_inch(), environment::new_line()));
-    auto index = 1;
+    auto index = 0;
     for (auto screen : screen::all_screens()) {
       screen_informations_text_box.append_text(environment::new_line());
-      screen_informations_text_box.append_text(ustring::format("Screen: {}\n", index, environment::new_line()));
+      screen_informations_text_box.append_text(ustring::format("Screen: {}\n", ++index, environment::new_line()));
       screen_informations_text_box.append_text(ustring::format("   Device name: {}{}", screen.device_name(), environment::new_line()));
       screen_informations_text_box.append_text(ustring::format("   Bounds: {}{}", screen.bounds(), environment::new_line()));
       screen_informations_text_box.append_text(ustring::format("   High resolution: {}{}", screen.high_resolution(), environment::new_line()));
@@ -35,7 +35,6 @@ public:
       screen_informations_text_box.append_text(ustring::format("   Bits per pixels (Depth): {}{}", screen.bits_per_pixel(), environment::new_line()));
       screen_informations_text_box.append_text(ustring::format("   Pixels per Inch (PPI): {}{}", screen.pixels_per_inch(), environment::new_line()));
       screen_informations_text_box.append_text(ustring::format("   Scale factor: {}{}", screen.scale_factor()));
-      ++index;
     }
   }
   
