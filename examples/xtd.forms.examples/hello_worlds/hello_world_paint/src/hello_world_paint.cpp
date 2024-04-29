22a380b1afb61e0160c27a8ac8a48a45fff4e0b7
Gammasoft <gammasoft71@gmail.com>
2024-04-29 14:55:18 +0200

    Review example

--- examples/xtd.forms.examples/hello_worlds/hello_world_paint/src/hello_world_paint.cpp
+++ examples/xtd.forms.examples/hello_worlds/hello_world_paint/src/hello_world_paint.cpp
@@ -12,9 +12,9 @@ namespace hello_world_paint_example {
       client_size({300, 300});
       
       paint += [&](object& sender, paint_event_args& e) {
-        e.graphics().clear(color::from_argb(0x0, 0x20, 0x10));
-        e.graphics().draw_string("Hello, World!", {system_fonts::default_font(), 32, font_style::bold | font_style::italic}, solid_brush {color_converter::dark(color::spring_green, 2.0 / 3)}, rectangle::offset(e.clip_rectangle(), {2, 2}), string_format().alignment(string_alignment::center).line_alignment(string_alignment::center));
-        e.graphics().draw_string("Hello, World!", {system_fonts::default_font(), 32, font_style::bold | font_style::italic}, brushes::spring_green(), rectangle::inflate(e.clip_rectangle(), {-2, -2}), string_format().alignment(string_alignment::center).line_alignment(string_alignment::center));
+        e.graphics().clear(color::light_blue);
+        e.graphics().draw_string("Hello, World!", {system_fonts::default_font(), 32, font_style::bold | font_style::italic}, solid_brush {color_converter::dark(color::light_blue)}, rectangle::offset(e.clip_rectangle(), {2, 2}), string_format().alignment(string_alignment::center).line_alignment(string_alignment::center));
+        e.graphics().draw_string("Hello, World!", {system_fonts::default_font(), 32, font_style::bold | font_style::italic}, brushes::dark_blue(), rectangle::inflate(e.clip_rectangle(), {-2, -2}), string_format().alignment(string_alignment::center).line_alignment(string_alignment::center));
       };
     }
   };
