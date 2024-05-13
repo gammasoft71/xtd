1698a136e7b570441a3af67644a30cb5a0863953
Gammasoft <gammasoft71@gmail.com>
2024-05-13 14:09:36 +0200

    Review code

--- src/xtd.forms/src/xtd/forms/picture_box.cpp
+++ src/xtd.forms/src/xtd/forms/picture_box.cpp
@@ -266,9 +266,7 @@ void picture_box::on_handle_created(const event_args& e) {
 
 void picture_box::on_paint(paint_event_args& e) {
   control::on_paint(e);
-  if (control_appearance() == forms::control_appearance::standard) {
-    if (image().has_value())
-      control_paint::draw_image(e.graphics(), image().value(), e.clip_rectangle(), to_image_layout(size_mode()));
-    control_paint::draw_border_from_back_color(*this, e.graphics(), border_style(), border_sides(), back_color(), e.clip_rectangle());
-  }
+  if (control_appearance() != forms::control_appearance::standard) return;
+  if (image().has_value()) control_paint::draw_image(e.graphics(), image().value(), e.clip_rectangle(), to_image_layout(size_mode()));
+  control_paint::draw_border_from_back_color(*this, e.graphics(), border_style(), border_sides(), back_color(), e.clip_rectangle());
 }
