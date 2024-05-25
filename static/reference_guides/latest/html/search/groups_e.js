31dcae9a854fb31a403152c45b764ac51e36258b
gammasoft71 <gammasoft71@gmail.com>
2024-05-25 14:02:55 +0200

    Update latest reference guide

--- static/reference_guides/latest/html/search/groups_e.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['texts_0',['texts',['../group__texts.html',1,'']]],
-  ['threading_1',['threading',['../group__threading.html',1,'']]],
-  ['timers_2',['timers',['../group__timers.html',1,'']]],
-  ['translate_3',['translate',['../group__translate.html',1,'']]],
-  ['types_4',['types',['../group__types.html',1,'']]]
-];
