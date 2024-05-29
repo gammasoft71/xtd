fd8f053efa001b50d1cbce810f94ebacf79db418
gammasoft71 <gammasoft71@gmail.com>
2024-05-29 21:00:06 +0200

    update latest reference guide

---
+++ static/reference_guides/latest/html/search/classes_7.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['h_5fscroll_5fbar_0',['h_scroll_bar',['../classxtd_1_1forms_1_1h__scroll__bar.html',1,'xtd::forms']]],
+  ['hatch_5fbrush_1',['hatch_brush',['../classxtd_1_1drawing_1_1drawing__2d_1_1hatch__brush.html',1,'xtd::drawing::drawing_2d']]],
+  ['help_5fevent_5fargs_2',['help_event_args',['../classxtd_1_1forms_1_1help__event__args.html',1,'xtd::forms']]],
+  ['hit_5ftest_5finfo_3',['hit_test_info',['../classxtd_1_1forms_1_1month__calendar_1_1hit__test__info.html',1,'xtd::forms::month_calendar']]],
+  ['horizontal_5fcontrol_5flayout_5fstyle_4',['horizontal_control_layout_style',['../classxtd_1_1forms_1_1horizontal__control__layout__style.html',1,'xtd::forms']]],
+  ['horizontal_5flayout_5fpanel_5',['horizontal_layout_panel',['../classxtd_1_1forms_1_1horizontal__layout__panel.html',1,'xtd::forms']]]
+];
