91f382788ea79bc15cca66475562ec8ca3ed3b49
gammasoft71 <gammasoft71@gmail.com>
2024-05-29 19:38:36 +0200

    Gtk settings implementation

--- src/xtd.forms.native.wxwidgets/src/xtd/forms/native/wxwidgets/settings.cpp
+++ src/xtd.forms.native.wxwidgets/src/xtd/forms/native/wxwidgets/settings.cpp
@@ -11,13 +11,11 @@ using namespace std;
 using namespace std::filesystem;
 using namespace xtd::native;
 
-#if !defined(__WXOSX__)
+#if defined(__WXMSW__)
 #include <xtd/convert_string>
 #include <wx/string.h>
 #include <wx/config.h>
-#if !defined(__WXMSW__)
 #include <unistd.h>
-#endif
 
 intmax_t settings::create(const string& product_name, const string& company_name) {
   return reinterpret_cast<intmax_t>(new wxConfig(convert_string::to_wstring(product_name), convert_string::to_wstring(company_name)));
@@ -47,10 +45,41 @@ void settings::write(intmax_t config, const string& key, const string& value) {
   if (!config) return;
   reinterpret_cast<wxConfig*>(config)->Write(convert_string::to_wstring(key), wxString(convert_string::to_wstring(value)));
 }
-#else
+#elif defined(__WXOSX__)
 class file_settings {
 public:
   file_settings(const string& product_name, const string& company_name) noexcept : name_ {path {getenv("HOME")} / path {"Library"} / path {"Preferences"} / path {company_name} / path {product_name + " Preferences"}} {
+    create_directory(name_.parent_path());
+    reload();
+  }
+  
+  void reset() noexcept {
+    key_values_.clear();
+    remove(name_.c_str());
+  }
+  
+  string read(const string& key, const string& defeult_value) noexcept {
+    reload();
+    if (key_values_.find(key) == key_values_.end()) key_values_[key] = defeult_value;
+    return key_values_[key];
+  }
+  
+  void save() noexcept {
+    if (!modified_) return;
+    modified_ = false;
+    auto stream = ofstream {name_, ios::trunc|ios::binary};
+    if (!stream) return;
+    for (auto [key, value] : key_values_) {
+      auto line = key + "="s + value + "\n"s;
+      stream.write(line.data(), line.size());
+    }
+    stream.close();
+  }
+  
+#else
+class file_settings {
+public:
+  file_settings(const string& product_name, const string& company_name) noexcept : name_ {path {getenv("HOME")} / path {".config"} / path {company_name} / path {product_name + ".conf"}} {
     create_directory(name_.parent_path());
     reload();
   }
