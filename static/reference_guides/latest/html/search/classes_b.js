9c064fd7b7f2ae0b5a693523757fbe01da005392
gammasoft71 <gammasoft71@gmail.com>
2024-05-31 21:00:07 +0200

    update latest reference guide

---
+++ static/reference_guides/latest/html/search/classes_b.js
@@ -0,0 +1,21 @@
+var searchData=
+[
+  ['main_5fmenu_0',['main_menu',['../classxtd_1_1forms_1_1main__menu.html',1,'xtd::forms']]],
+  ['manual_5freset_5fevent_1',['manual_reset_event',['../classxtd_1_1threading_1_1manual__reset__event.html',1,'xtd::threading']]],
+  ['math_2',['math',['../classxtd_1_1math.html',1,'xtd']]],
+  ['matrix_3',['matrix',['../classxtd_1_1drawing_1_1drawing__2d_1_1matrix.html',1,'xtd::drawing::drawing_2d']]],
+  ['menu_4',['menu',['../classxtd_1_1forms_1_1menu.html',1,'xtd::forms']]],
+  ['menu_5fimages_5',['menu_images',['../classxtd_1_1forms_1_1menu__images.html',1,'xtd::forms']]],
+  ['menu_5fitem_6',['menu_item',['../classxtd_1_1forms_1_1menu__item.html',1,'xtd::forms']]],
+  ['message_7',['message',['../classxtd_1_1forms_1_1message.html',1,'xtd::forms']]],
+  ['message_5fbox_8',['message_box',['../classxtd_1_1forms_1_1message__box.html',1,'xtd::forms']]],
+  ['message_5fdialog_9',['message_dialog',['../classxtd_1_1forms_1_1message__dialog.html',1,'xtd::forms']]],
+  ['message_5fnotifier_10',['message_notifier',['../classxtd_1_1forms_1_1message__notifier.html',1,'xtd::forms']]],
+  ['message_5fnotifier_5fbutton_11',['message_notifier_button',['../classxtd_1_1forms_1_1message__notifier__button.html',1,'xtd::forms']]],
+  ['mimetypes_5fsystem_5fimages_12',['mimetypes_system_images',['../classxtd_1_1drawing_1_1mimetypes__system__images.html',1,'xtd::drawing']]],
+  ['monitor_13',['monitor',['../classxtd_1_1threading_1_1monitor.html',1,'xtd::threading']]],
+  ['month_5fcalendar_14',['month_calendar',['../classxtd_1_1forms_1_1month__calendar.html',1,'xtd::forms']]],
+  ['mouse_5fevent_5fargs_15',['mouse_event_args',['../classxtd_1_1forms_1_1mouse__event__args.html',1,'xtd::forms']]],
+  ['multicast_5foption_16',['multicast_option',['../classxtd_1_1net_1_1sockets_1_1multicast__option.html',1,'xtd::net::sockets']]],
+  ['mutex_17',['mutex',['../classxtd_1_1threading_1_1mutex.html',1,'xtd::threading']]]
+];
---
+++ static/reference_guides/latest/html/search/files_13.js
@@ -0,0 +1,12 @@
+var searchData=
+[
+  ['v_5fscroll_5fbar_2eh_0',['v_scroll_bar.h',['../v__scroll__bar_8h.html',1,'']]],
+  ['va_5fargs_5fcomma_2eh_1',['va_args_comma.h',['../va__args__comma_8h.html',1,'']]],
+  ['valid_2eh_2',['valid.h',['../valid_8h.html',1,'']]],
+  ['vector_5flist_2eh_3',['vector_list.h',['../vector__list_8h.html',1,'']]],
+  ['version_2eh_4',['version.h',['../version_8h.html',1,'']]],
+  ['vertical_5fcontrol_5flayout_5fstyle_2eh_5',['vertical_control_layout_style.h',['../vertical__control__layout__style_8h.html',1,'']]],
+  ['vertical_5fcontrol_5flayout_5fstyle_5fcollection_2eh_6',['vertical_control_layout_style_collection.h',['../vertical__control__layout__style__collection_8h.html',1,'']]],
+  ['vertical_5flayout_5fpanel_2eh_7',['vertical_layout_panel.h',['../vertical__layout__panel_8h.html',1,'']]],
+  ['violet_5fcolors_2eh_8',['violet_colors.h',['../violet__colors_8h.html',1,'']]]
+];
