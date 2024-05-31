262c290c2f083a7ae6c79e876f1973b618fa991b
gammasoft71 <gammasoft71@gmail.com>
2024-05-31 19:40:36 +0200

    Update latest reference guide

--- static/reference_guides/latest/search/groups_2.js
+++ static/reference_guides/latest/search/groups_2.js
@@ -3,7 +3,8 @@ var searchData=
   ['collections_0',['collections',['../group__collections.html',1,'']]],
   ['colors_1',['colors',['../group__colors.html',1,'']]],
   ['components_2',['components',['../group__components.html',1,'']]],
-  ['containers_3',['containers',['../group__containers.html',1,'']]],
-  ['controls_4',['controls',['../group__controls.html',1,'']]],
-  ['coordinates_5',['coordinates',['../group__coordinates.html',1,'']]]
+  ['configuration_3',['configuration',['../group__configuration.html',1,'']]],
+  ['containers_4',['containers',['../group__containers.html',1,'']]],
+  ['controls_5',['controls',['../group__controls.html',1,'']]],
+  ['coordinates_6',['coordinates',['../group__coordinates.html',1,'']]]
 ];
