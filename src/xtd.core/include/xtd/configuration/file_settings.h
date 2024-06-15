a6aea44a5da25c52a2fbfe8feb413ca98d44a81f
Gammasoft <gammasoft71@gmail.com>
2024-06-15 10:38:55 +0200

    Fix typo

--- src/xtd.core/include/xtd/configuration/file_settings.h
+++ src/xtd.core/include/xtd/configuration/file_settings.h
@@ -1,6 +1,5 @@
 /// @file
 /// @brief Contains xtd::configuration::file_settings class
-/// .
 /// @copyright Copyright (c) 2024 Gammasoft. All rights reserved.
 #pragma once
 #include "../core_export.h"
