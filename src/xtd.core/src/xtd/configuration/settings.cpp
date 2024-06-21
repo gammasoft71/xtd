602ac16584943f3063d68d3bebc0230b799f3c80
Gammasoft <gammasoft71@gmail.com>
2024-06-21 22:23:46 +0200

    Review code

--- src/xtd.core/src/xtd/configuration/settings.cpp
+++ src/xtd.core/src/xtd/configuration/settings.cpp
@@ -1,16 +1,11 @@
 #include "../../../include/xtd/environment.h"
 #include "../../../include/xtd/configuration/file_settings.h"
 #include "../../../include/xtd/configuration/settings.h"
-#include "../../../include/xtd/io/directory.h"
-#include "../../../include/xtd/io/file.h"
 #include "../../../include/xtd/io/path.h"
-#include "../../../include/xtd/io/stream_reader.h"
-#include "../../../include/xtd/io/stream_writer.h"
 #include "../../../include/xtd/reflection/assembly.h"
 #define __XTD_CORE_NATIVE_LIBRARY__
 #include <xtd/native/settings>
 #undef __XTD_CORE_NATIVE_LIBRARY__
-#include <map>
 
 using namespace xtd;
 using namespace xtd::configuration;
@@ -24,7 +19,7 @@ struct settings::data {
 settings::settings() : data_(std::make_shared<data>()) {
   auto product_name = [] {
     if (assembly::get_executing_assembly().product() != ustring::empty_string) return assembly::get_executing_assembly().product();
-    if (environment::get_command_line_args().size() != 0) return xtd::io::path::get_file_name_without_extension(environment::get_command_line_args()[0]);
+    if (environment::get_command_line_args().size() != 0) return path::get_file_name_without_extension(environment::get_command_line_args()[0]);
     return "noname"_s;
   };
   
@@ -36,11 +31,11 @@ settings::settings() : data_(std::make_shared<data>()) {
   data_->file_settings = file_settings {native::settings::get_path(company_name(), product_name())};
 }
 
-const xtd::ustring& settings::file_path() const noexcept {
+const ustring& settings::file_path() const noexcept {
   return data_->file_settings.file_path();
 }
 
-xtd::ustring settings::read(const xtd::ustring& key, const xtd::ustring& default_value) {
+ustring settings::read(const ustring& key, const ustring& default_value) {
   return read_string(key, default_value);
 }
 
@@ -52,14 +47,14 @@ void settings::save() {
   data_->file_settings.save();
 }
 
-void settings::write(const xtd::ustring& key, const xtd::ustring& value) {
+void settings::write(const ustring& key, const ustring& value) {
   write_string(key, value);
 }
 
-xtd::ustring settings::read_string(const xtd::ustring& key, const xtd::ustring& default_value) {
+ustring settings::read_string(const ustring& key, const ustring& default_value) {
   return data_->file_settings.read(key, default_value);
 }
 
-void settings::write_string(const xtd::ustring& key, const xtd::ustring& value) {
+void settings::write_string(const ustring& key, const ustring& value) {
   data_->file_settings.write(key, value);
 }
