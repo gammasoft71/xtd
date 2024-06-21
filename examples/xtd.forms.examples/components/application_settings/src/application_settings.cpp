c3b5503203f67943b1e9455f214bc962edd43a47
Gammasoft <gammasoft71@gmail.com>
2024-06-21 20:25:38 +0200

    Update application_settings.cpp

--- examples/xtd.forms.examples/components/application_settings/src/application_settings.cpp
+++ examples/xtd.forms.examples/components/application_settings/src/application_settings.cpp
@@ -4,11 +4,11 @@
 #include <xtd/forms/color_picker>
 #include <xtd/forms/form>
 
-using namespace xtd::drawing;
+using namespace application_settings::properties;
 using namespace xtd::forms;
 
 auto main() -> int {
-  auto main_form = form::create(application_settings::properties::settings::default_settings().text(), form_start_position::manual);
+  auto main_form = form::create(settings::default_settings().text(), form_start_position::manual);
   
   auto back_color_picker = color_picker::create(main_form, main_form.back_color(), {10, 10}, {75, 25});
   back_color_picker.color_picker_changed += [&] {
@@ -17,23 +17,23 @@ auto main() -> int {
   
   auto save_button = button::create(main_form, "&Save", {90, 10});
   save_button.click += [&] {
-    application_settings::properties::settings::default_settings().size(main_form.client_size());
-    application_settings::properties::settings::default_settings().location(main_form.location());
-    application_settings::properties::settings::default_settings().back_color(main_form.back_color());
-    application_settings::properties::settings::default_settings().save();
+    settings::default_settings().size(main_form.client_size());
+    settings::default_settings().location(main_form.location());
+    settings::default_settings().back_color(main_form.back_color());
+    settings::default_settings().save();
   };
   
   auto reload_button = button::create(main_form, "&Reload", {170, 10});
   reload_button.click += [&] {
-    main_form.client_size(application_settings::properties::settings::default_settings().size());
-    main_form.location(application_settings::properties::settings::default_settings().location());
-    main_form.back_color(application_settings::properties::settings::default_settings().back_color());
-    back_color_picker.color(application_settings::properties::settings::default_settings().back_color());
+    main_form.client_size(settings::default_settings().size());
+    main_form.location(settings::default_settings().location());
+    main_form.back_color(settings::default_settings().back_color());
+    back_color_picker.color(settings::default_settings().back_color());
   };
   
   auto reset_button = button::create(main_form, "R&eset", {250, 10});
   reset_button.click += [&] {
-    application_settings::properties::settings::default_settings().reset();
+    settings::default_settings().reset();
     reload_button.perform_click();
   };
   
