d6512ced86e73627d3c9050b2ac330c69248ff68
Gammasoft <gammasoft71@gmail.com>
2024-06-21 13:53:27 +0200

    Add file_settings example

---
+++ examples/xtd.core.examples/configuration/README.md
@@ -0,0 +1,13 @@
+# Configuration examples
+
+[This folder](.) contains console examples used by [Reference Guide](https://gammasoft71.github.io/xtd/reference_guides/latest/) and more.
+
+* [file_settings](file_settings/README.md) shows how to use [xtd::consiguration::file_settings](https://gammasoft71.github.io/xtd/reference_guides/latest/classxtd_1_1configuration_1_1file__settings.html) object.
+
+## Build and run any project
+
+Open "Command Prompt" or "Terminal". Navigate to the folder that contains the project and type the following:
+
+```shell
+xtdc run -t any_project_name
+```
---
+++ examples/xtd.core.examples/configuration/file_settings/src/file_settings.cpp
@@ -0,0 +1,59 @@
+#include <xtd/configuration/file_settings>
+#include <xtd/console>
+
+using namespace xtd;
+using namespace xtd::configuration;
+
+auto write_file_settings() {
+  auto file = file_settings("example.ini");
+  file.top_file_comment("Settings file used by file_settings example.\nCopyright (c) 2024 Gammasoft. All rights reserved.");
+  file.write("auto_close", true);
+  file.write("caption", "file_settings example");
+  file.write("Thread \"Process\"", "timeout_", 100_ms);
+  file.save();
+}
+
+auto reset_file_settings() {
+  auto file = file_settings("example.ini");
+  file.reset();
+}
+
+auto read_file_settings() {
+  auto file = file_settings("example.ini");
+  console::write_line("----------------------------------------");
+  console::write_line("\"exemple.ini\" file content :");
+  console::write_line(file.to_string());
+  console::write_line("----------------------------------------");
+  console::write_line("read keys :");
+  console::write_line("auto_close = {}", file.read("auto_close", false));
+  console::write_line("caption = {}", file.read("caption", "example"));
+  console::write_line("Thread \"Process\"", "time_out = {}", file.read("timeout_", 50_ms));
+  console::write_line("----------------------------------------");
+}
+
+auto main() -> int {
+  write_file_settings();
+  // Uncomment the following line to see what happens if the "example.ini" file doesn't exist.
+  //reset_file_settings();
+  read_file_settings();
+}
+
+// This code produces the following output:
+//
+// ----------------------------------------
+// "exemple.ini" file content :
+// # Settings file used by file_settings example.
+// # Copyright (c) 2024 Gammasoft. All rights reserved.
+//
+// auto_close = true
+// caption = file_settings example
+//
+// [Thread "Process"]
+// timeout_ = 00:00:00.1000000
+//
+// ----------------------------------------
+// read keys :
+// auto_close = true
+// caption = file_settings example
+// Thread "Process"
+// ----------------------------------------
