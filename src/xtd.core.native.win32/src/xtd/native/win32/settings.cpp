7ebd36283cfa59c0eb34e672c00bfc3036c79c45
Gammasoft <gammasoft71@gmail.com>
2024-06-23 14:28:54 +0200

    Simplifying code

--- src/xtd.core.native.win32/src/xtd/native/win32/settings.cpp
+++ src/xtd.core.native.win32/src/xtd/native/win32/settings.cpp
@@ -5,12 +5,11 @@
 
 using namespace std;
 using namespace std::filesystem;
-using namespace std::literals;
 using namespace xtd::native;
 
 __declspec(dllimport) extern int __argc;
 __declspec(dllimport) extern char** __argv;
 
 string settings::get_path(const string& company_name, const string& product_name) {
-  return path {path {__argv[0]}.parent_path() / path {product_name + ".ini"s}}.string();
+  return (path {__argv[0]}.parent_path() / product_name).string() + ".ini";
 }
