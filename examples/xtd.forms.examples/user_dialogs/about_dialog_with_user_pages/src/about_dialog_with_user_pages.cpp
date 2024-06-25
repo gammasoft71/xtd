f8161068c5d80043c70da424c7a07b7fffc08813
Gammasoft <gammasoft71@gmail.com>
2024-06-26 00:29:41 +0200

    review code

--- examples/xtd.forms.examples/user_dialogs/about_dialog_with_user_pages/src/about_dialog_with_user_pages.cpp
+++ examples/xtd.forms.examples/user_dialogs/about_dialog_with_user_pages/src/about_dialog_with_user_pages.cpp
@@ -22,18 +22,19 @@ public:
     user_label1.text_align(content_alignment::middle_center).font({user_label1.font(),32}).dock(dock_style::fill).fore_color(color::black);
     user_label2.text_align(content_alignment::middle_center).font({user_label2.font(), 32}).dock(dock_style::fill).fore_color(color::black);
 
+    dialog.icon(system_images::from_name("symbolic", "contact-new", xtd::drawing::size {64, 64}));
+    dialog.name("User about dialog");
+    dialog.version("1.0");
+    dialog.long_version("1.0.0");
+    dialog.description("About dialog with user pages.");
+    dialog.copyright("Copyright (c) 2024 Gammasoft.\nAll rights reserved.");
+    dialog.website("https://gammasoft71.wixsite.com/gammasoft");
+    dialog.website_label("gammasoft website");
+    dialog.user_tab_pages().push_back_range({user_tab_page1, user_tab_page2});
+
     button1.location({10, 10});
     button1.text("About...");
     button1.click += [&] {
-      dialog.icon(system_images::from_name("symbolic", "contact-new", xtd::drawing::size {64, 64}));
-      dialog.name("User about dialog");
-      dialog.version("1.0");
-      dialog.long_version("1.0.0");
-      dialog.description("About dialog with user pages.");
-      dialog.copyright("Copyright (c) 2024 Gammasoft.\nAll rights reserved.");
-      dialog.website("https://gammasoft71.wixsite.com/gammasoft");
-      dialog.website_label("gammasoft website");
-      dialog.user_tab_pages().push_back_range({user_tab_page1, user_tab_page2});
       dialog.show(*this);
     };
   }
