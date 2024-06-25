006e9ce63b0591350dd8cc2ef065a02066322355
Gammasoft <gammasoft71@gmail.com>
2024-06-26 00:35:42 +0200

    revue code

--- examples/xtd.forms.examples/user_dialogs/about_dialog_with_user_pages/src/about_dialog_with_user_pages.cpp
+++ examples/xtd.forms.examples/user_dialogs/about_dialog_with_user_pages/src/about_dialog_with_user_pages.cpp
@@ -14,12 +14,11 @@ class form1 : public form {
 public:
   form1() {
     text("About dialog with user pages example");
-    controls().push_back(button1);
     
     user_tab_page1.back_color(color::from_name(user_tab_page1.text()));
     user_tab_page2.back_color(color::from_name(user_tab_page2.text()));
     
-    user_label1.text_align(content_alignment::middle_center).font({user_label1.font(),32}).dock(dock_style::fill).fore_color(color::black);
+    user_label1.text_align(content_alignment::middle_center).font({user_label1.font(), 32}).dock(dock_style::fill).fore_color(color::black);
     user_label2.text_align(content_alignment::middle_center).font({user_label2.font(), 32}).dock(dock_style::fill).fore_color(color::black);
 
     dialog.icon(system_images::from_name("symbolic", "contact-new", xtd::drawing::size {64, 64}));
@@ -32,8 +31,6 @@ public:
     dialog.website_label("gammasoft website");
     dialog.user_tab_pages().push_back_range({user_tab_page1, user_tab_page2});
 
-    button1.location({10, 10});
-    button1.text("About...");
     button1.click += [&] {
       dialog.show(*this);
     };
@@ -45,8 +42,7 @@ private:
   tab_page user_tab_page2 = tab_page::create("Spring Green");
   label user_label1 = label::create(user_tab_page1, color_translator::to_rgb(color::from_name(user_tab_page1.text())));
   label user_label2 = label::create(user_tab_page2, color_translator::to_rgb(color::from_name(user_tab_page2.text())));
-
-  button button1;
+  button button1 = button::create(*this, "About...", {10, 10});
 };
 
 auto main() -> int {
