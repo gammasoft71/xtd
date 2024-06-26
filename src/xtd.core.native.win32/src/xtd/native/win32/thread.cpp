78fa3241de08a6905594e2a6ef672fa3a90397d7
Gammasoft <gammasoft71@gmail.com>
2024-06-26 15:00:33 +0200

    Review set_current_thread_name on Win32

--- src/xtd.core.native.win32/src/xtd/native/win32/thread.cpp
+++ src/xtd.core.native.win32/src/xtd/native/win32/thread.cpp
@@ -1,39 +1,12 @@
 #define __XTD_CORE_NATIVE_LIBRARY__
 #include <xtd/native/thread>
+#include "../../../../include/xtd/native/win32/strings.h"
 #undef __XTD_CORE_NATIVE_LIBRARY__
 #include <Windows.h>
 #undef max
 #undef min
 
 using namespace xtd::native;
-
-#if !defined(_MSC_VER)
-#define __try
-#define __except if
-#endif
-
-namespace {
-  // https://msdn.microsoft.com/en-us/library/xcb2z8hs.aspx
-  void SetThreadName(DWORD dwThreadID, const char* threadName) {
-#pragma pack(push, 8)
-    struct THREADNAME_INFO {
-      DWORD dwType;
-      LPCSTR szName;
-      DWORD dwThreadID;
-      DWORD dwFlags;
-    };
-#pragma pack(pop)
-    
-#pragma warning(push)
-#pragma warning(disable: 6320 6322)
-    __try {
-      THREADNAME_INFO info {0x1000, threadName, dwThreadID, 0};
-      RaiseException(0x406D1388, 0, sizeof(info) / sizeof(ULONG_PTR), (ULONG_PTR*)&info);
-    } __except (EXCEPTION_EXECUTE_HANDLER) {
-    }
-#pragma warning(pop)
-  }
-}
 
 bool thread::cancel(intmax_t handle) {
   if (reinterpret_cast<HANDLE>(handle) == INVALID_HANDLE_VALUE) return false;
@@ -83,8 +56,8 @@ bool thread::resume(intmax_t handle) {
 }
 
 bool thread::set_current_thread_name(const std::string& name) {
-  SetThreadName(GetCurrentThreadId(), name.c_str());
-  return true;
+  // https://learn.microsoft.com/visualstudio/debugger/tips-for-debugging-threads
+  return SUCCEEDED(SetThreadDescription(GetCurrentThread(), win32::strings::to_wstring(name).data()));
 }
 
 bool thread::set_priority(intmax_t handle, int_least32_t priority) {
