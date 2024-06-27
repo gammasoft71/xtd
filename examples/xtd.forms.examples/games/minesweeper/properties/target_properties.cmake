aac90ad671b4438b773ac62ef4900b6a9364f2ee
Gammasoft <gammasoft71@gmail.com>
2024-06-27 21:59:18 +0200

    Add comments

--- examples/xtd.forms.examples/games/minesweeper/properties/target_properties.cmake
+++ examples/xtd.forms.examples/games/minesweeper/properties/target_properties.cmake
@@ -1,4 +1,14 @@
+# Target application definitions
+# ==============================
+
+# The default namespace definition is used when the "property/resources.h" and "property/settings.h" files are generated so that they use the "minesweeper" namespace.
 target_default_namespace("minesweeper")
-target_icon(resources/minesweeper)
+
+# The target icon is used for the application and the main form.
+target_icon("resources/minesweeper")
+
+# The target name is used to name the built application.
 target_name("minesweeper")
-target_startup("minesweeper::minesweeper_form::main" src/minesweeper.h)
+
+# The target startuo is the entry point definiton and generates the "property/startup.cpp" file wich contains the main method.
+target_startup("minesweeper::minesweeper_form::main" "src/minesweeper.h")
