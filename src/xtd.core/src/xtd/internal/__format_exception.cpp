b7f4a07b79fb490998f62d573a99b382d2d1d04c
Gammasoft <gammasoft71@gmail.com>
2024-07-02 22:13:29 +0200

    Fix message

--- src/xtd.core/src/xtd/internal/__format_exception.cpp
+++ src/xtd.core/src/xtd/internal/__format_exception.cpp
@@ -5,7 +5,7 @@
 #include "../../../include/xtd/typeof.h"
 
 void __format_exception(const xtd::type& type) {
-  throw xtd::format_exception {"The `" + typeof_(type).full_name() + "` object does not inherit from `xtd::iformat` or the specialisation for the `" + typeof_(type).full_name() + "` object in the `xtd::to_string` specialisation method does not exist."};
+  throw xtd::format_exception {"The `" + typeof_(type).full_name() + "` type does not inherit from `xtd::iformat` or the specialisation for the `" + typeof_(type).full_name() + "` type in the `xtd::to_string` specialisation method does not exist."};
 }
 
 void __format_exception(const std::string& message) {
