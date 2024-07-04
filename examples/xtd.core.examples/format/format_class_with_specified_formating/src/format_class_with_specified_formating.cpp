6725b289585a6192a78b3893de3f7cc2c194e45c
Gammasoft <gammasoft71@gmail.com>
2024-07-04 11:03:47 +0200

    Update format_class_with_specified_formating.cpp

--- examples/xtd.core.examples/format/format_class_with_specified_formating/src/format_class_with_specified_formating.cpp
+++ examples/xtd.core.examples/format/format_class_with_specified_formating/src/format_class_with_specified_formating.cpp
@@ -4,35 +4,30 @@
 using namespace std;
 using namespace xtd;
 
-class character {
+class character : public iformatable {
 public:
   character(const ustring& name, const ustring& rank) noexcept : name_(name), rank_(rank) {}
   
   const ustring& name() const noexcept {return name_;}
   const ustring& rank() const noexcept {return rank_;}
   
-  ustring to_string() const noexcept {return to_string("F");}
-  ustring to_string(const ustring& fmt) const {
+  ustring to_string() const noexcept {return to_string("", locale {});}
+  ustring to_string(const ustring& format, const locale& loc) const override {
+    auto fmt = ustring::is_empty(format) ? "F" : format;
     if (fmt == "F") return name_ + " (" + rank_ + ")";
     if (fmt == "N") return name_;
     if (fmt == "R") return rank_;
     throw format_exception(current_stack_frame_);
   }
   
-  // Only this operator is needed for character class to be recognized by ustring::format() without specified formating.
-  friend ostream& operator <<(ostream& os, const character& value) noexcept {return os << value.to_string();}
-  
 private:
   ustring name_;
   ustring rank_;
 };
 
-// Only this method is needed for character class to be recognized by ustring::format() with specified formating (F, N or R).
-template<>
-string xtd::to_string(const character& value, const string& fmt, const locale& loc) {return value.to_string(fmt);}
-
 auto main() -> int {
   auto c = character {"Jean-Luc Picard", "Captain"};
+ 
   cout << ustring::format("{}", c) << endl;
   cout << ustring::format("{:F}", c) << endl;
   cout << ustring::format("{:N}", c) << endl;
