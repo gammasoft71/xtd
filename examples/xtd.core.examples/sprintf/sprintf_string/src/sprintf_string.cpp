c82cb6011c07c83741eb2c6013b72af5f1db55d9
Gammasoft <gammasoft71@gmail.com>
2024-07-04 13:45:13 +0200

    Update sprintf_string.cpp

--- examples/xtd.core.examples/sprintf/sprintf_string/src/sprintf_string.cpp
+++ examples/xtd.core.examples/sprintf/sprintf_string/src/sprintf_string.cpp
@@ -4,11 +4,17 @@ using namespace std;
 using namespace xtd;
 
 int main() {
-  cout << format("{}", "string") << endl;
-  cout << format("{:anything}", "string") << endl;
+  cout << ustring::sprintf("%s", "string") << endl;
+  cout << ustring::sprintf("%s", u8"u8string") << endl;
+  cout << ustring::sprintf("%s", string {"string"}) << endl;
+  cout << ustring::sprintf("%s", ustring {"ustring"}) << endl;
+  cout << ustring::sprintf("%s", u8string {u8"u8string"}) << endl;
 }
 
 // This code produces the following output :
 //
 // string
+// u8string
 // string
+// ustring
+// u8string
