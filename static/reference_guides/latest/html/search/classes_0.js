d0124d2e16b8ca67f292537597df9cd25aae20f8
gammasoft71 <gammasoft71@gmail.com>
2024-07-05 22:14:15 +0200

    update latest reference guide

--- static/reference_guides/latest/html/search/classes_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['_5f_5fxtd_5ftuple_5fstringer_0',['__xtd_tuple_stringer',['../structxtd_1_1____xtd__tuple__stringer.html',1,'xtd']]],
-  ['_5f_5fxtd_5ftuple_5fstringer_3c_20type_5ft_2c_20n_5ft_2c_20n_5ft_20_3e_1',['__xtd_tuple_stringer&lt; type_t, n_t, n_t &gt;',['../structxtd_1_1____xtd__tuple__stringer_3_01type__t_00_01n__t_00_01n__t_01_4.html',1,'xtd']]]
-];
