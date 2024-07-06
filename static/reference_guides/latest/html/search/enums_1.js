6dc7047649da3290483504fa852f27f2c43731f1
gammasoft71 <gammasoft71@gmail.com>
2024-07-06 21:00:58 +0200

    update latest reference guide

---
+++ static/reference_guides/latest/html/search/enums_1.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['battery_5fcharge_5fstatus_0',['battery_charge_status',['../group__xtd__forms.html#gabfcbdab8d51145bfcc9d621c1828cc63',1,'xtd::forms']]],
+  ['boot_5fmode_1',['boot_mode',['../group__xtd__forms.html#gace143665b025363d4624a06a2caed293',1,'xtd::forms']]],
+  ['border_5fsides_2',['border_sides',['../group__xtd__forms.html#ga56260d6433849c6fa239714c85a1d45d',1,'xtd::forms']]],
+  ['border_5fstyle_3',['border_style',['../group__xtd__forms.html#ga663fa5bbff2ea7ea9ece8563a188a1c6',1,'xtd::forms']]],
+  ['bounds_5fspecified_4',['bounds_specified',['../group__xtd__forms.html#ga475781e3c7baa300a81ab16d22b16962',1,'xtd::forms']]],
+  ['build_5ftype_5',['build_type',['../group__xtd__core.html#ga8672853ea39d932089c60c4970d5bf4a',1,'xtd']]],
+  ['button_5fstate_6',['button_state',['../group__xtd__forms.html#ga13e58aa1f50e91172769e708c7f46945',1,'xtd::forms']]]
+];
