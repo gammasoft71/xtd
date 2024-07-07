526ebe0ed0073ae7fff958e77dda6eb26b257f47
Gammasoft <gammasoft71@gmail.com>
2024-07-07 17:34:13 +0200

    Ads xtd::sptr, xt::uptr and xtd::ptr types

--- src/xtd.core/include/xtd/types.h
+++ src/xtd.core/include/xtd/types.h
@@ -14,20 +14,21 @@
 #undef __XTD_VALUE_TYPE_NATIVE__
 /// @endcond
 #include <cstdint>
+#include <memory>
 #include <string>
 #include <typeinfo>
 
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
   /// @name Native types
-
+  
   /// @{
   /// @brief Represents a boolean.
   /// @par Header
   /// ```cpp
   /// #include <xtd/bool>
   /// ```
-    /// @par Namespace
+  /// @par Namespace
   /// xtd
   /// @par Library
   /// xtd.core
@@ -39,7 +40,7 @@ namespace xtd {
   /// ```cpp
   /// #include <xtd/byte>
   /// ```
-    /// @par Namespace
+  /// @par Namespace
   /// xtd
   /// @par Library
   /// xtd.core
@@ -51,7 +52,7 @@ namespace xtd {
   /// ```cpp
   /// #include <xtd/char>
   /// ```
-    /// @par Namespace
+  /// @par Namespace
   /// xtd
   /// @par Library
   /// xtd.core
@@ -59,14 +60,14 @@ namespace xtd {
   /// @remarks Represents a 32-bit unicode character on Linux and many other non-Windows systems, but a 16-bit unicode character on Windows.
   /// @remarks For more information about types, see [Native types, boxing and unboxing](https://gammasoft71.github.io/xtd/docs/documentation/Guides/xtd.core/Types%20overview/types).
   using char_ = char;
-
+  
 #if defined(__xtd__cpp_lib_char8_t)
   /// @brief Represents a 8-bit unicode character.
   /// @par Header
   /// ```cpp
   /// #include <xtd/char8>
   /// ```
-    /// @par Namespace
+  /// @par Namespace
   /// xtd
   /// @par Library
   /// xtd.core
@@ -80,7 +81,7 @@ namespace xtd {
   /// ```cpp
   /// #include <xtd/char16>
   /// ```
-    /// @par Namespace
+  /// @par Namespace
   /// xtd
   /// @par Library
   /// xtd.core
@@ -93,7 +94,7 @@ namespace xtd {
   /// ```cpp
   /// #include <xtd/char32>
   /// ```
-    /// @par Namespace
+  /// @par Namespace
   /// xtd
   /// @par Library
   /// xtd.core
@@ -106,33 +107,33 @@ namespace xtd {
   /// ```cpp
   /// #include <xtd/decimal>
   /// ```
-    /// @par Namespace
+  /// @par Namespace
   /// xtd
   /// @par Library
   /// xtd.core
   /// @ingroup xtd_core types
   /// @remarks For more information about types, see [Native types, boxing and unboxing](https://gammasoft71.github.io/xtd/docs/documentation/Guides/xtd.core/Types%20overview/types).
   using decimal = long double;
-
+  
   /// @brief Represents a double-precision floating-point number.
   /// @par Header
   /// ```cpp
   /// #include <xtd/double>
   /// ```
-    /// @par Namespace
+  /// @par Namespace
   /// xtd
   /// @par Library
   /// xtd.core
   /// @ingroup xtd_core types
   /// @remarks For more information about types, see [Native types, boxing and unboxing](https://gammasoft71.github.io/xtd/docs/documentation/Guides/xtd.core/Types%20overview/types).
   using double_ = double;
-
+  
   /// @brief Represents a 16-bit signed integer.
   /// @par Header
   /// ```cpp
   /// #include <xtd/int16>
   /// ```
-    /// @par Namespace
+  /// @par Namespace
   /// xtd
   /// @par Library
   /// xtd.core
@@ -145,7 +146,7 @@ namespace xtd {
   /// ```cpp
   /// #include <xtd/int32>
   /// ```
-    /// @par Namespace
+  /// @par Namespace
   /// xtd
   /// @par Library
   /// xtd.core
@@ -158,7 +159,7 @@ namespace xtd {
   /// ```cpp
   /// #include <xtd/int64>
   /// ```
-    /// @par Namespace
+  /// @par Namespace
   /// xtd
   /// @par Library
   /// xtd.core
@@ -171,7 +172,7 @@ namespace xtd {
   /// ```cpp
   /// #include <xtd/intptr>
   /// ```
-    /// @par Namespace
+  /// @par Namespace
   /// xtd
   /// @par Library
   /// xtd.core
@@ -184,7 +185,7 @@ namespace xtd {
   /// ```cpp
   /// #include <xtd/ptrdiff>
   /// ```
-    /// @par Namespace
+  /// @par Namespace
   /// xtd
   /// @par Library
   /// xtd.core
@@ -197,33 +198,33 @@ namespace xtd {
   /// ```cpp
   /// #include <xtd/sbyte>
   /// ```
-    /// @par Namespace
+  /// @par Namespace
   /// xtd
   /// @par Library
   /// xtd.core
   /// @ingroup xtd_core types
   /// @remarks For more information about types, see [Native types, boxing and unboxing](https://gammasoft71.github.io/xtd/docs/documentation/Guides/xtd.core/Types%20overview/types).
   using sbyte = int_least8_t;
-
+  
   /// @brief Represents a single-precision floating-point number.
   /// @par Header
   /// ```cpp
   /// #include <xtd/single>
   /// ```
-    /// @par Namespace
+  /// @par Namespace
   /// xtd
   /// @par Library
   /// xtd.core
   /// @ingroup xtd_core types
   /// @remarks For more information about types, see [Native types, boxing and unboxing](https://gammasoft71.github.io/xtd/docs/documentation/Guides/xtd.core/Types%20overview/types).
   using single = float;
-
+  
   /// @brief Represents a size of any object in bytes
   /// @par Header
   /// ```cpp
   /// #include <xtd/size>
   /// ```
-    /// @par Namespace
+  /// @par Namespace
   /// xtd
   /// @par Library
   /// xtd.core
@@ -236,7 +237,7 @@ namespace xtd {
   /// ```cpp
   /// #include <xtd/slong>
   /// ```
-    /// @par Namespace
+  /// @par Namespace
   /// xtd
   /// @par Library
   /// xtd.core
@@ -249,7 +250,7 @@ namespace xtd {
   /// ```cpp
   /// #include <xtd/type>
   /// ```
-    /// @par Namespace
+  /// @par Namespace
   /// xtd
   /// @par Library
   /// xtd.core
@@ -262,7 +263,7 @@ namespace xtd {
   /// ```cpp
   /// #include <xtd/uint16>
   /// ```
-    /// @par Namespace
+  /// @par Namespace
   /// xtd
   /// @par Library
   /// xtd.core
@@ -275,7 +276,7 @@ namespace xtd {
   /// ```cpp
   /// #include <xtd/uint32>
   /// ```
-    /// @par Namespace
+  /// @par Namespace
   /// xtd
   /// @par Library
   /// xtd.core
@@ -288,7 +289,7 @@ namespace xtd {
   /// ```cpp
   /// #include <xtd/uint64>
   /// ```
-    /// @par Namespace
+  /// @par Namespace
   /// xtd
   /// @par Library
   /// xtd.core
@@ -301,7 +302,7 @@ namespace xtd {
   /// ```cpp
   /// #include <xtd/uintptr>
   /// ```
-    /// @par Namespace
+  /// @par Namespace
   /// xtd
   /// @par Library
   /// xtd.core
@@ -314,7 +315,7 @@ namespace xtd {
   /// ```cpp
   /// #include <xtd/ulong>
   /// ```
-    /// @par Namespace
+  /// @par Namespace
   /// xtd
   /// @par Library
   /// xtd.core
@@ -327,7 +328,7 @@ namespace xtd {
   /// ```cpp
   /// #include <xtd/wchar>
   /// ```
-    /// @par Namespace
+  /// @par Namespace
   /// xtd
   /// @par Library
   /// xtd.core
@@ -342,7 +343,7 @@ namespace xtd {
   /// ```cpp
   /// #include <xtd/null>
   /// ```
-    /// @par Namespace
+  /// @par Namespace
   /// xtd
   /// @par Library
   /// xtd.core
@@ -354,4 +355,155 @@ namespace xtd {
   /// ```
   /// @ingroup xtd_core types
   extern core_export_ std::nullptr_t null;
+  
+  /// @brief The xtd::sptr object is a shared pointer.
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core
+  /// @par Examples
+  /// This example shows to use xtd::sptr with xtd::version class
+  /// @code
+  /// sptr<xtd::version> version = new_sptr<xtd::version>(1, 2, 3);
+  ///
+  /// console::write_line("version = {}", version->to_string());
+  /// delete_ptr(version); // Not mandatory.
+  /// @endcode
+  template<typename type_t>
+  using sptr = std::shared_ptr<type_t>;
+  
+  /// @brief The xtd::uptr object is a unique pointer.
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core
+  /// @par Examples
+  /// This example shows to use xtd::uptr with xtd::version class
+  /// @code
+  /// uptr<xtd::version> version = newu_uptr<xtd::version>(1, 2, 3);
+  ///
+  /// console::write_line("version = {}", version->to_string());
+  /// delete_ptr(version); // Not mandatory.
+  /// @endcode
+  template<typename type_t>
+  using uptr = std::unique_ptr<type_t>;
+  
+  /// @brief The xtd::ptr object is a shared pointer.
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core
+  /// @par Examples
+  /// this is example shows to use xtd::new_ with xtd::version class
+  /// @code
+  /// ptr<xtd::version> version = new_ptr<xtd::version>(1, 2, 3);
+  ///
+  /// console::write_line("version = {}", version->to_string());
+  /// delete_ptr(version); // Not mandatory.
+  /// @endcode
+  template<typename type_t>
+  using ptr = sptr<type_t>;
+  
+  /// @brief The xtd::new_ptr operator creates a xtd::ptr object.
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core
+  /// @par Examples
+  /// this is example shows to use xtd::new_ptr with xtd::version class
+  /// @code
+  /// ptr<xtd::version> version = new_ptr<xtd::version>(1, 2, 3);
+  ///
+  /// console::write_line("version = {}", version->to_string());
+  /// delete_ptr(version); // Not mandatory.
+  /// @endcode
+  /// @remarks The xtd::new_ptr is equivalent to [std::make_shared](https://en.cppreference.com/w/cpp/memory/shared_ptr/make_shared).
+  template<typename type_t, typename ...args_t>
+  ptr<type_t> new_ptr(args_t&& ... args) {return ptr<type_t> {new type_t(args...)};}
+  
+  /// @cond
+  template<typename type_t>
+  ptr<type_t> new_ptr(const type_t& arg) {return ptr<type_t> {new type_t(arg)};}
+  
+  template<typename type_t>
+  ptr<type_t> new_ptr() {return ptr<type_t>(new type_t {});}
+  /// @endcond
+  
+  /// @brief xtd::news_sptr operator creates a xtd::sptr object.
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core
+  /// @par Examples
+  /// this is example shows to use xtd::new_sptr with xtd::version class
+  /// @code
+  /// sptr<xtd::version> version = new_sptr<xtd::version>(1, 2, 3);
+  ///
+  /// console::write_line("version = {}", version->to_string());
+  /// delete_ptr(version); // Not mandatory.
+  /// @endcode
+  /// @remarks The xtd::new_sptr is equivalent to [std::make_shared](https://en.cppreference.com/w/cpp/memory/shared_ptr/make_shared).
+  template<typename type_t, typename ...args_t>
+  sptr<type_t> new_sptr(args_t&& ... args) {return sptr<type_t> {new type_t(args...)};}
+  
+  /// @cond
+  template<typename type_t>
+  sptr<type_t> new_sptr(const type_t& arg) {return sptr<type_t> {new type_t(arg)};}
+  
+  template<typename type_t>
+  sptr<type_t> new_sptr() {return sptr<type_t>(new type_t {});}
+  /// @endcond
+  
+  /// @brief xtd::new_uptr operator. This operator creates a xtd::uptr object.
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core
+  /// @par Examples
+  /// this is example shows to use xtd::new_uptr with xtd::version class
+  /// @code
+  /// uptr<xtd::version> version = new_uptr<xtd::version>(1, 2, 3);
+  ///
+  /// console::write_line("version = {}", version->to_string());
+  /// delete_(version); // Not mandatory.
+  /// @endcode
+  /// @remarks The xtd::new_uptr is equivalent to [std::make_unique](https://en.cppreference.com/w/cpp/memory/unique_ptr/make_unique).
+  template<typename type_t, typename ...args_t>
+  uptr<type_t> new_uptr(args_t&& ... args) {return uptr<type_t> {new type_t(args...)};}
+  
+  /// @cond
+  template<typename type_t>
+  uptr<type_t> new_uptr(const type_t& arg) {return uptr<type_t> {new type_t(arg)};}
+  
+  template<typename type_t>
+  uptr<type_t> new_uptr() {return uptr<type_t>(new type_t {});}
+  /// @endcond
+  
+  /// @brief xtd::delete_ptr operator delete a xtd::sptr or xtd::ptr object.
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core
+  /// @par Examples
+  /// this is example shows to use xtd::delete_ptr with xtd::version class
+  /// @code
+  /// ptr<xtd::version> version = new_ptr<xtd::version>(1, 2, 3);
+  ///
+  /// console::write_line("version = {}", version->to_string());
+  /// delete_ptr(version); // Not mandatory.
+  /// @endcode
+  /// @remarks This operator does not have to be called because when an xtd::sptr object leaves the scope, its value is automatically reset.
+  /// @remarks Use it when you want to reset the xtd::sptr object at a specific time.
+  template<typename type_t>
+  void delete_ptr(sptr<type_t>& ptr) {ptr = null;}
+  
+  /// @brief xtd::delete_ptr operator delete a xtd::uptr object.
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core
+  /// @par Examples
+  /// this is example shows to use xtd::delete_ptr with xtd::version class
+  /// @code
+  /// uptr<xtd::version> version = new_uptr<xtd::version>(1, 2, 3);
+  ///
+  /// console::write_line("version = {}", version->to_string());
+  /// delete_ptr(version); // Not mandatory.
+  /// @endcode
+  /// @remarks This operator does not have to be called because when an xtd::uptr object leaves the scope, its value is automatically reset.
+  /// @remarks Use it when you want to reset the xtd::ptr object at a specific time.
+  template<typename type_t>
+  void delete_ptr(uptr<type_t>& ptr) {ptr = null;}
 }
