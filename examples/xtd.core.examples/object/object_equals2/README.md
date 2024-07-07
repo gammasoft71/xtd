452039160764f589f6f374662ab5ebe033286a26
Gammasoft <gammasoft71@gmail.com>
2024-07-07 13:32:50 +0200

    Add object_equals2 example

---
+++ examples/xtd.core.examples/object/object_equals2/README.md
@@ -0,0 +1,22 @@
+# object_equals2
+
+Shows hows how to use [xtd::object::equals](https://gammasoft71.github.io/xtd/reference_guides/latest/classxtd_1_1object.html#a75cb9ed56172fb088ca6697d5d836b13) static method.
+
+## Sources
+
+[src/object_equals2.cpp](src/object_equals2.cpp)
+
+[CMakeLists.txt](CMakeLists.txt)
+
+## Build and run
+
+Open "Command Prompt" or "Terminal". Navigate to the folder that contains the project and type the following:
+
+```cmake
+xtdc run
+```
+
+## Output
+
+```
+```
---
+++ examples/xtd.core.examples/object/object_equals2/src/object_equals2.cpp
@@ -0,0 +1,33 @@
+#include <xtd/console>
+
+using namespace xtd;
+
+auto main() -> int {
+  ustring s1 = "Tom";
+  ustring s2 = "Carol";
+  console::write_line("object::equals(\"{0}\", \"{1}\") => {2}", s1, s2, object::equals(s1, s2));
+  
+  s1 = "Tom";
+  s2 = "Tom";
+  console::write_line("object::equals(\"{0}\", \"{1}\") => {2}", s1, s2, object::equals(s1, s2));
+  
+  s1 = "";
+  s2 = "Tom";
+  console::write_line("object::equals(\"{0}\", \"{1}\") => {2}", s1, s2, object::equals(s1, s2));
+  
+  s1 = "Carol";
+  s2 = "";
+  console::write_line("object::equals(\"{0}\", \"{1}\") => {2}", s1, s2, object::equals(s1, s2));
+  
+  s1 = "";
+  s2 = "";
+  console::write_line("object::equals(\"{0}\", \"{1}\") => {2}", s1, s2, object::equals(s1, s2));
+}
+
+// This code produces the following output:
+//
+// object::equals("Tom", "Carol") => false
+// object::equals("Tom", "Tom") => true
+// object::equals("", "Tom") => false
+// object::equals("Carol", "") => false
+// object::equals("", "") => true
