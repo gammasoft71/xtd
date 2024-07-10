c0f45737986f286c3f42cdbc57ead9e867c93ea4
gammasoft71 <gammasoft71@gmail.com>
2024-07-10 21:00:37 +0200

    update latest reference guide

---
+++ static/reference_guides/latest/html/search/enumvalues_10.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['q_0',['q',['../group__xtd__core.html#ggad94b2f43c7550180321e0ac4b02a4ff2a7694f4a66316e53c8cdd9d9954bd611d',1,'xtd::q'],['../group__xtd__forms.html#gga2f00c1ad108efaddc951106e768970dea7694f4a66316e53c8cdd9d9954bd611d',1,'xtd::forms::q']]],
+  ['query_1',['query',['../group__xtd__core.html#gga20b51d7977f3ffaaca44c6075ad021dba1b1cc7f086b3f074da452bc3129981eb',1,'xtd::query'],['../group__xtd__core.html#ggae28bc955b84c3c42c759ee1eb4717b6aa1b1cc7f086b3f074da452bc3129981eb',1,'xtd::query']]],
+  ['question_2',['question',['../group__xtd__forms.html#gga3ca2a1f60ab79bdedd6b519ae35d3841a5494af1f14a8c19939968c3e9e2d4f79',1,'xtd::forms']]],
+  ['quick_3',['quick',['../group__xtd__core.html#ggae9ba1696ebc733013c909c04118ae81aa1df3746a4728276afdc24f828186f73a',1,'xtd']]]
+];
