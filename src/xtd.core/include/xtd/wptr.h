c32bafb3cc35de0fb156daf7f8d1d72eb9e306f4
Gammasoft <gammasoft71@gmail.com>
2024-07-10 21:37:10 +0200

    Add xtd::wptr type as weak pointer

---
+++ src/xtd.core/include/xtd/wptr.h
@@ -0,0 +1,19 @@
+/// @file
+/// @brief Contains xtd::uptr type.
+/// @copyright Copyright (c) 2024 Gammasoft. All rights reserved.
+#pragma once
+#include <memory>
+
+/// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
+namespace xtd {
+  /// @name Native types
+  
+  /// @{
+  /// @brief The xtd::uptr object is a weak pointer.
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core types
+  template<typename type_t>
+  using wptr = std::weak_ptr<type_t>;
+  /// @}
+}
