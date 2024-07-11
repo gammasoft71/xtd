63b863867fd7c24948a4b106420989292f844d3b
Gammasoft <gammasoft71@gmail.com>
2024-07-11 23:42:34 +0200

    review example

--- examples/xtd.core.examples/object/object_equals/src/object_equals.cpp
+++ examples/xtd.core.examples/object/object_equals/src/object_equals.cpp
@@ -8,11 +8,15 @@ auto main() -> int {
   
   auto object3 = object2;
   console::write_line(object1->equals(*object3));
+  console::write_line(*object1 == *object3);
   object3 = object1;
   console::write_line(object1->equals(*object3));
+  console::write_line(*object1 == *object3);
 }
 
 // This code produces the following output:
 //
 // false
+// false
+// true
 // true
