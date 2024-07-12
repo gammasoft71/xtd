681252b21bd28f1c2c6b07a59ec02805a12a3503
gammasoft71 <gammasoft71@gmail.com>
2024-07-12 21:00:07 +0200

    update latest reference guide

---
+++ static/reference_guides/latest/html/search/typedefs_13.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['vector_5flist_0',['vector_list',['../group__collections.html#ga3c07efce71e0899524aa31dfd0431817',1,'xtd::collections']]],
+  ['vertical_5fcontrol_5flayout_5fstyle_5fcollection_1',['vertical_control_layout_style_collection',['../group__xtd__forms.html#ga1936ca7e95ef2d3a6e2beabd2fd23030',1,'xtd::forms']]],
+  ['violet_5fcolors_2',['violet_colors',['../group__colors.html#ga00b820083dc10635d0003659f19bea3f',1,'xtd::drawing']]]
+];
