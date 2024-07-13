6eca4c9e07a38077040e3e17003bc29e50972c10
gammasoft71 <gammasoft71@gmail.com>
2024-07-13 21:00:09 +0200

    update latest reference guide

---
+++ static/reference_guides/latest/html/search/classes_15.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['v_5fscroll_5fbar_0',['v_scroll_bar',['../classxtd_1_1forms_1_1v__scroll__bar.html',1,'xtd::forms']]],
+  ['valid_1',['valid',['../classxtd_1_1tunit_1_1valid.html',1,'xtd::tunit']]],
+  ['version_2',['version',['../classxtd_1_1version.html',1,'xtd']]],
+  ['vertical_5fcontrol_5flayout_5fstyle_3',['vertical_control_layout_style',['../classxtd_1_1forms_1_1vertical__control__layout__style.html',1,'xtd::forms']]],
+  ['vertical_5flayout_5fpanel_4',['vertical_layout_panel',['../classxtd_1_1forms_1_1vertical__layout__panel.html',1,'xtd::forms']]]
+];
