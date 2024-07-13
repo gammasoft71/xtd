991051d0c2d02b527ee78d6a456e065ebb0eb931
gammasoft71 <gammasoft71@gmail.com>
2024-07-13 21:00:09 +0200

    update latest reference guide

---
+++ static/reference_guides/latest/html/search/groups_6.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['generic_20collections_0',['generic collections',['../group__generic__collections.html',1,'']]]
+];
---
+++ static/reference_guides/latest/html/search/typedefs_f.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['replace_5fevent_5fhandler_0',['replace_event_handler',['../group__events.html#ga9c8eae32395179ad3de65c36e29d282a',1,'xtd::forms']]],
+  ['run_5fworker_5fcompleted_5fevent_5fhandler_1',['run_worker_completed_event_handler',['../group__events.html#ga415f76a09816da51d4b05fd2717477bc',1,'xtd::forms']]]
+];
