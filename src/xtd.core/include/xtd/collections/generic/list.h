e689e5fcbfd38ae717237ca55279054f6b855da0
Gammasoft <gammasoft71@gmail.com>
2024-07-13 19:59:39 +0200

    xtd::collections::generic::list implementation

--- src/xtd.core/include/xtd/collections/generic/list.h
+++ src/xtd.core/include/xtd/collections/generic/list.h
@@ -13,7 +13,8 @@ namespace xtd {
     namespace generic {
       /// @brief Represents a strongly typed list of objects that can be accessed by index. Provides methods to search, sort, and manipulate lists.
       /// ```cpp
-      /// class list
+      /// template<typename type_t>
+      /// class list : public std::vector<type_t, xtd::collections::allocator<type_t>>
       /// ```
       /// @par Header
       /// ```cpp
@@ -24,10 +25,38 @@ namespace xtd {
       /// @par Library
       /// xtd.core
       /// @ingroup xtd_core generic_collections
-      /// @remarks The xtd::collections::generic::list is alias on [std::vector](https://en.cppreference.com/w/cpp/container/vector).
-      /// @remarks A simple alias for the moment, which leaves open the possibility of implementing a specific list for xtd if necessary in the future.
-      template<typename type_t, typename allocator_t = xtd::collections::allocator<type_t>>
-      using list = std::vector<type_t, allocator_t>;
+      /// @remarks The xtd::collections::generic::list class is same as [std::vector](https://en.cppreference.com/w/cpp/container/vector) with xtd::collections::allocator instead [std::allocator](https://en.cppreference.com/w/cpp/memory/allocator).
+      template<typename type_t>
+      class list : public std::vector<type_t, xtd::collections::allocator<type_t>> {
+      public:
+        /// @brief Represents the allocator type of list.
+        using allocator_type = xtd::collections::allocator<type_t>;
+        /// @brief Represents the base type of list.
+        using base_type = std::vector<type_t, allocator_type>;
+        /// @brief Represents the size type of list.
+        using size_type = std::vector<type_t, allocator_type>::size_type;
+
+        /// @name Constructors
+        
+        /// @{
+        list() noexcept = default;
+        explicit list(const allocator_type& alloc) noexcept : base_type(alloc) {}
+        list(size_type count, const type_t& value, const allocator_type& alloc = allocator_type()) : base_type(count, value, alloc) {}
+        explicit list(size_type count, const allocator_type& alloc = allocator_type()) : base_type(count, alloc) {}
+        template<typename input_iterator_t>
+        list(input_iterator_t first, input_iterator_t last, const allocator_type& alloc = allocator_type()) : base_type(first, last, alloc) {}
+        list(const list& other) : base_type(other) {}
+        list(const base_type& other) : base_type(other) {}
+        list(const list& other, const allocator_type& alloc) : base_type(other, alloc) {}
+        list(const base_type& other, const allocator_type& alloc) : base_type(other, alloc) {}
+        list(list&& other) : base_type(other) {}
+        list(base_type&& other) : base_type(other) {}
+        list(list&& other, const allocator_type& alloc) : base_type(other, alloc) {}
+        list(base_type&& other, const allocator_type& alloc) : base_type(other, alloc) {}
+        list(std::initializer_list<type_t> init, const allocator_type& alloc = allocator_type()) : base_type(init, alloc) {}
+        list& operator =(const list& other) = default;
+       /// @}
+      };
     }
   }
 }
