c45cbd35e644334d1ee3a0824c57eec882bcff0b
gammasoft71 <gammasoft71@gmail.com>
2024-07-14 21:00:17 +0200

    update latest reference guide

---
+++ static/reference_guides/latest/html/search/typedefs_13.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['value_5ftype_0',['value_type',['../classxtd_1_1collections_1_1generic_1_1list.html#a19654ead074894e64f01f9cc0f3c382a',1,'xtd::collections::generic::list']]],
+  ['vector_5flist_1',['vector_list',['../group__collections.html#ga3c07efce71e0899524aa31dfd0431817',1,'xtd::collections']]],
+  ['vertical_5fcontrol_5flayout_5fstyle_5fcollection_2',['vertical_control_layout_style_collection',['../group__xtd__forms.html#ga1936ca7e95ef2d3a6e2beabd2fd23030',1,'xtd::forms']]],
+  ['violet_5fcolors_3',['violet_colors',['../group__colors.html#ga00b820083dc10635d0003659f19bea3f',1,'xtd::drawing']]]
+];
