3dc8a0d39a4c2b066001f080cffea64d57feacd2
gammasoft71 <gammasoft71@gmail.com>
2024-07-17 00:50:49 +0200

    Update latest reference guide

--- static/reference_guides/latest/html/search/typedefs_f.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['reference_0',['reference',['../classxtd_1_1collections_1_1generic_1_1list.html#a5de1351dfee4c745490cdf929d0cdcd3',1,'xtd::collections::generic::list']]],
-  ['replace_5fevent_5fhandler_1',['replace_event_handler',['../group__events.html#ga9c8eae32395179ad3de65c36e29d282a',1,'xtd::forms']]],
-  ['reverse_5fiterator_2',['reverse_iterator',['../classxtd_1_1collections_1_1generic_1_1list.html#aa99b6f04636ae59a117180738a566f44',1,'xtd::collections::generic::list']]],
-  ['run_5fworker_5fcompleted_5fevent_5fhandler_3',['run_worker_completed_event_handler',['../group__events.html#ga415f76a09816da51d4b05fd2717477bc',1,'xtd::forms']]]
-];
