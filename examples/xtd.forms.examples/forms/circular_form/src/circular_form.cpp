12f36c74eee85fd7b9a894e0115439f46d950ae9
Gammasoft <gammasoft71@gmail.com>
2024-07-16 22:20:30 +0200

    Review example

--- examples/xtd.forms.examples/forms/circular_form/src/circular_form.cpp
+++ examples/xtd.forms.examples/forms/circular_form/src/circular_form.cpp
@@ -1,4 +1,6 @@
-#include <xtd/drawing/drawing_2d/linear_gradient_brush>
+#include <xtd/drawing/drawing_2d/radial_gradient_brush>
+#include <xtd/drawing/crayons_colors>
+#include <xtd/drawing/color_converter>
 #include <xtd/forms/application>
 #include <xtd/forms/button>
 #include <xtd/forms/form>
@@ -17,7 +19,11 @@ public:
   
   form_main() {
     form_border_style(forms::form_border_style::none);
-    client_size({300, 300});
+    start_position(form_start_position::manual);
+    client_size({200, 200});
+    double_buffered(true);
+    location({screen::primary_screen().working_area().left() + screen::primary_screen().working_area().width() / 2 - client_size().width() / 2, screen::primary_screen().working_area().bottom() - client_size().height()});
+    top_most(true);
     
     // Create a circular region for the form
     graphics_path path;
@@ -25,33 +31,37 @@ public:
     region(drawing::region(path));
     
     paint += [&](object& sender, paint_event_args& e) {
-      e.graphics().fill_rectangle(linear_gradient_brush(e.clip_rectangle(), {color::black, color::blue, color::black}, 90), e.clip_rectangle());
+      auto gradient_color1 = color::from_argb(0xBC, 0, 0);
+      auto gradient_color2 = color::from_argb(0xFF, 0xAA, 0x7E);
+      e.graphics().fill_rectangle(radial_gradient_brush {point {80, 70}, {gradient_color2, gradient_color1}, 110}, e.clip_rectangle());
     };
     
-    mouse_down += [&](object & sender, const mouse_event_args & e) {
+    mouse_down += [&](object& sender, const mouse_event_args& e) {
       mouse_location = e.location();
-      cursor(cursors::no_move_2d());
+      cursor(cursors::hand());
     };
-    mouse_up += [&](object & sender, const mouse_event_args & e) {
+    
+    mouse_up += [&](object& sender, const mouse_event_args& e) {
       mouse_location = point::empty;
       cursor(cursors::default_cursor());
     };
-    mouse_move += [&](object & sender, const mouse_event_args & e) {
-      if (mouse_location != point::empty) location(location() + e.location() - mouse_location);
-    };
     
-    close_button.parent(*this);
-    close_button.location({client_size().width() / 2 - close_button.width() / 2, client_size().height() / 2 - close_button.height() / 2});
-    close_button.back_color(color::from_argb(32, color::white));
-    close_button.fore_color(color::dark_blue);
-    close_button.text("close");
-    close_button.click += [&] {
-      close();
+    mouse_move += [&](object& sender, const mouse_event_args& e) {
+      if (mouse_location == point::empty) return;
+      suspend_layout();
+      auto working_area = screen::from_control(*this).working_area();
+      auto new_bounds = bounds();
+      new_bounds.location(new_bounds.location() + e.location() - mouse_location);
+      if (new_bounds.left() < working_area.left()) new_bounds.location({working_area.left(), new_bounds.top()});
+      if (new_bounds.top() < working_area.top()) new_bounds.location({new_bounds.left(), working_area.top()});
+      if (new_bounds.left() + new_bounds.width() > working_area.right()) new_bounds.location({working_area.right() - new_bounds.width(), new_bounds.top()});
+      if (new_bounds.top() + new_bounds.height() > working_area.bottom()) new_bounds.location({new_bounds.left(), working_area.bottom() - new_bounds.height()});
+      bounds(new_bounds);
+      resume_layout();
     };
   }
   
 private:
-  button close_button;
   point mouse_location = point::empty;
 };
 
