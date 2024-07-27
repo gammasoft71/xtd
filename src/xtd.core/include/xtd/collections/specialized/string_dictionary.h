44ca93eced29e52eabd26ebbf8f3920b94fec7e3
Gammasoft <gammasoft71@gmail.com>
2024-07-27 21:33:03 +0200

    using xtd::collections::generic::dictionary

--- src/xtd.core/include/xtd/collections/specialized/string_dictionary.h
+++ src/xtd.core/include/xtd/collections/specialized/string_dictionary.h
@@ -3,7 +3,8 @@
 /// @copyright Copyright (c) 2024 Gammasoft. All rights reserved.
 #pragma once
 #include "../../ustring.h"
-#include <map>
+#include "../generic/dictionary.h"
+#include "../../ustring.h"
 
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
@@ -11,9 +12,9 @@ namespace xtd {
   namespace collections {
     /// @brief The xtd::collections::specialized namespace contains specialized and strongly-typed collections; for example, a linked list dictionary, a bit vector, and collections that contain only strings.
     namespace specialized {
-      /// @brief Represents a std::map with the key and the value strongly typed to be strings.
+      /// @brief Represents a xtd::collections::generic::dictionary with the key and the value strongly typed to be strings.
       /// ```cpp
-      /// using string_dictionary = std::map<xtd::ustring, xtd::ustring>
+      /// using string_dictionary = xtd::collections::generic::dictionary<xtd::ustring, xtd::ustring>
       /// ```
       /// @par Header
       /// ```cpp
@@ -24,7 +25,7 @@ namespace xtd {
       /// @par Library
       /// xtd.core
       /// @ingroup xtd_core specialized_collections
-      using string_dictionary = std::map<xtd::ustring, xtd::ustring>;
+      using string_dictionary = xtd::collections::generic::dictionary<xtd::ustring, xtd::ustring>;
     }
   }
 }
