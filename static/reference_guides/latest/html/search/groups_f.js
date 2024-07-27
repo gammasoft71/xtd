2c0bbf371b37d4823e3372c037bf1d4ef39940ca
gammasoft71 <gammasoft71@gmail.com>
2024-07-27 23:42:49 +0200

    update latest reference guide

--- static/reference_guides/latest/html/search/groups_f.js
+++
@@ -1,9 +0,0 @@
-var searchData=
-[
-  ['security_0',['security',['../group__security.html',1,'']]],
-  ['specialized_1',['specialized',['../group__specialized__collections.html',1,'']]],
-  ['style_5fsheets_2',['style_sheets',['../group__style__sheets.html',1,'']]],
-  ['synthesis_3',['synthesis',['../group__synthesis.html',1,'']]],
-  ['system_4',['system',['../group__system.html',1,'']]],
-  ['system_20information_5',['system information',['../group__system__information.html',1,'']]]
-];
