38b20f92adcd32558739d7dbe105a22c133623d4
gammasoft71 <gammasoft71@gmail.com>
2024-07-28 22:18:28 +0200

    Update latest reference guide

--- static/reference_guides/html/search/classes_b.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['key_5fevent_5fargs_0',['key_event_args',['../classxtd_1_1forms_1_1key__event__args.html',1,'xtd::forms']]],
-  ['key_5fnot_5ffound_5fexception_1',['key_not_found_exception',['../classxtd_1_1collections_1_1key__not__found__exception.html',1,'xtd::collections']]],
-  ['key_5fpress_5fevent_5fargs_2',['key_press_event_args',['../classxtd_1_1forms_1_1key__press__event__args.html',1,'xtd::forms']]]
-];
