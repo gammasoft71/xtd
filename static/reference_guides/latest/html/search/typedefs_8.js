1e0ab8d269b22492c96ca6d31713637c77dbd623
gammasoft71 <gammasoft71@gmail.com>
2024-07-29 21:00:12 +0200

    update latest reference guide

---
+++ static/reference_guides/latest/html/search/typedefs_8.js
@@ -0,0 +1,18 @@
+var searchData=
+[
+  ['ibutton_5fcontrol_5fref_0',['ibutton_control_ref',['../classxtd_1_1forms_1_1form.html#a99e318f64e65c5fc5278a7eafff46f6d',1,'xtd::forms::form']]],
+  ['ienumerator_1',['ienumerator',['../group__interfaces.html#ga1871d17c4798e1f712e5c7f09ff36506',1,'xtd::collections']]],
+  ['ifstream_5fref_2',['ifstream_ref',['../classxtd_1_1net_1_1sockets_1_1send__packets__element.html#a946a0bbf547eb1c2f97cc7e4000b15e1',1,'xtd::net::sockets::send_packets_element']]],
+  ['image_5fcollection_3',['image_collection',['../classxtd_1_1forms_1_1image__list.html#ab2980da466b87684eb01dc92804c1d73',1,'xtd::forms::image_list']]],
+  ['init_5ffile_5fsettings_4',['init_file_settings',['../group__configuration.html#ga03474f9ed666f1f3e7f9244b0666c18f',1,'xtd::configuration']]],
+  ['int16_5',['int16',['../group__types.html#ga159b2a409f2b3da8165af7a5e32eccf8',1,'xtd']]],
+  ['int16_5fobject_6',['int16_object',['../group__types.html#ga58c299ba39634dc95bc9f28d8938b2ff',1,'xtd']]],
+  ['int32_7',['int32',['../group__types.html#ga92bf6d527cbcada4a30faa2efd0d6a91',1,'xtd']]],
+  ['int32_5fobject_8',['int32_object',['../group__types.html#ga645e4f1656c3b3a1efe9d70224230090',1,'xtd']]],
+  ['int64_9',['int64',['../group__types.html#ga7ab879ebcae19d6021222f00537cfdce',1,'xtd']]],
+  ['int64_5fobject_10',['int64_object',['../group__types.html#gaba73256a6bc8ec54133bb544c7a40d4a',1,'xtd']]],
+  ['intptr_11',['intptr',['../group__types.html#gaa7283e1dd31c385d954467f7ea324451',1,'xtd']]],
+  ['intptr_5fobject_12',['intptr_object',['../group__types.html#ga80e43cf0f75b3bd8374de9e634c036fe',1,'xtd']]],
+  ['item_5fcheck_5fevent_5fhandler_13',['item_check_event_handler',['../group__events.html#gaf978ac3dcd773547450c4536f6ee40c9',1,'xtd::forms']]],
+  ['iterator_14',['iterator',['../classxtd_1_1collections_1_1generic_1_1iterators.html#a9659b66abc6472f5202aac036caae077',1,'xtd::collections::generic::iterators::iterator'],['../classxtd_1_1collections_1_1generic_1_1list.html#ae39fd00c2f363da57190c4cd2d900b5e',1,'xtd::collections::generic::list::iterator'],['../classxtd_1_1forms_1_1layout_1_1arranged__element__collection.html#a54a7c85a5ebca902f6d7d390089c4894',1,'xtd::forms::layout::arranged_element_collection::iterator']]]
+];
