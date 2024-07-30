da7229ee5b7bb21d3d57c8fe2b3b9f483f241533
Gammasoft <gammasoft71@gmail.com>
2024-07-30 22:48:29 +0200

    Review example

--- examples/xtd.core.examples/generic_collections/generic_icollection/src/generic_icollection.cpp
+++ examples/xtd.core.examples/generic_collections/generic_icollection/src/generic_icollection.cpp
@@ -6,47 +6,51 @@ using namespace xtd::collections::generic;
 class program {
 public:
   static auto main() -> void {
-    auto bx_list = box_collection {};
+    auto boxes = box_collection {};
     
-    bx_list.add(program::box {10, 4, 6});
-    bx_list.add(program::box {4, 6, 10});
-    bx_list.add(program::box {6, 10, 4});
-    bx_list.add(program::box {12, 8, 10});
+    boxes.add(program::box {10, 4, 6});
+    boxes.add(program::box {4, 6, 10});
+    boxes.add(program::box {6, 10, 4});
+    boxes.add(program::box {12, 8, 10});
     
     // Same dimensions. Cannot be added:
-    bx_list.add(program::box {10, 4, 6});
+    boxes.add(program::box {10, 4, 6});
     
     // Test the Remove method.
-    display(bx_list);
+    display(boxes);
     console::write_line("Removing 6x10x4");
-    bx_list.remove(program::box {6, 10, 4});
-    display(bx_list);
+    boxes.remove(program::box {6, 10, 4});
+    display(boxes);
     
     // Test the Contains method.
     auto box_check = program::box {8, 12, 10};
-    console::write_line("Contains {}x{}x{} by dimensions: {}", box_check.height, box_check.length, box_check.width, bx_list.contains(box_check));
+    console::write_line("Contains {}x{}x{} by dimensions: {}", box_check.height, box_check.length, box_check.width, boxes.contains(box_check));
     
     // Test the Contains method overload with a specified equality comparer.
-    console::write_line("Contains {}x{}x{} by volume: {}", box_check.height, box_check.length, box_check.width, bx_list.contains(box_check, box_same_volume {}));
+    console::write_line("Contains {}x{}x{} by volume: {}", box_check.height, box_check.length, box_check.width, boxes.contains(box_check, box_same_volume {}));
   }
 
-  struct box : public object, public iequatable<program::box>, public icomparable<program::box> {
+  struct box : public object, public iequatable<program::box> {
+    // Public Constructors :
     box() = default;
     box(int h, int l, int w) : height {h}, length{l}, width {w} {}
     
+    // Public Properties :
     int height = 0;
     int length = 0;
     int width = 0;
     
-    int32 compare_to(const program::box& o) const noexcept override {return 0;}
+    // Public Methods :
     bool equals(const object& o) const noexcept override {return is<program::box>(o) && equals(as<program::box>(o));}
     bool equals(const program::box& o) const noexcept override {return box_same_dimensions {}.equals(*this, o);}
   };
   
   class box_collection : public icollection<program::box> {
   public:
+    // Public Constructors :
     box_collection(const std::initializer_list<program::box>& boxes) : boxes_(boxes) {}
     
+    // Public Properties :
     xtd::size count() const noexcept override {return boxes_.count();}
     
     bool is_read_only() const noexcept override {return false;}
@@ -55,6 +59,7 @@ public:
     
     const xtd::object& sync_root() const noexcept override {return sync_root_;}
 
+    // Public Methods :
     void add(const program::box& item) override {
       if (!contains(item))
         boxes_.add(item);
@@ -67,8 +72,8 @@ public:
     bool contains(const program::box& item) const noexcept override {return boxes_.contains(item);}
     
     bool contains(const program::box& item, const iequality_comparer<program::box>& comparer) const noexcept {
-      for (auto bx : boxes_)
-        if (comparer.equals(bx, item)) return true;
+      for (auto box : boxes_)
+        if (comparer.equals(box, item)) return true;
       return false;
     }
     
@@ -85,8 +90,13 @@ public:
   
   class box_enumerator : public ienumerator<program::box> {
   public:
+    // Public Constructors :
     explicit box_enumerator(const list<program::box>& items) : items_(items) {}
+
+    // Public Properties :
     const program::box& current() const override {return items_[index_];}
+
+    // Public Methods :
     bool move_next() override {return ++index_ < items_.size();}
     void reset() override {index_ = xtd::box_integer<xtd::size>::max_value;}
     
@@ -98,31 +108,33 @@ public:
   // Defines two boxes as equal if they have the same dimensions.
   class box_same_dimensions : public iequality_comparer<program::box> {
   public:
+    // Public Methods :
     bool equals(const program::box& b1, const program::box& b2) const noexcept override {return b1.height == b2.height && b1.length == b2.length && b1.width == b2.width;}
     
-    size get_hash_code(const program::box& bx) const noexcept override {return hash_code::combine(bx.height, bx.length, bx.width);}
+    size get_hash_code(const program::box& box) const noexcept override {return hash_code::combine(box.height, box.length, box.width);}
   };
   
   // Defines two boxes as equal if they have the same volume.
   class box_same_volume : public iequality_comparer<program::box> {
   public:
+    // Public Methods :
     bool equals(const program::box& b1, const program::box& b2) const noexcept override {return b1.height * b1.length * b1.width == b2.height * b2.length * b2.width;}
     
-    size get_hash_code(const program::box& bx) const noexcept override {
-      auto hash_code = hash_code::combine(bx.height, bx.length, bx.width);
+    size get_hash_code(const program::box& box) const noexcept override {
+      auto hash_code = hash_code::combine(box.height, box.length, box.width);
       console::write_line("HC: {}", hash_code);
       return hash_code;
     }
   };
 
-  static void display(const box_collection& bx_list) {
+  static void display(const box_collection& boxes) {
     console::write_line("\nheight  length  width   hash code");
-    for (auto bx : bx_list)
-      console::write_line("{,-6}  {,-6}  {,-6}  {}", bx.height, bx.length, bx.width, bx.get_hash_code());
+    for (auto box : boxes)
+      console::write_line("{,-6}  {,-6}  {,-6}  {}", box.height, box.length, box.width, box.get_hash_code());
     
     // Results by manipulating the enumerator directly:
     
-    //auto enumerator = bx_list.get_enumerator();
+    //auto enumerator = boxes.get_enumerator();
     //console::write_line("\nheight  length  width   hash code");
     //while (enumerator.move_next()) {
     //  auto b = enumerator.current();
