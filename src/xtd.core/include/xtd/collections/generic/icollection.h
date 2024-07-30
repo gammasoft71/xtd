2e7150f05e1b8ea2793904198129e7ff19b19381
Gammasoft <gammasoft71@gmail.com>
2024-07-30 22:37:48 +0200

    Add documentation

--- src/xtd.core/include/xtd/collections/generic/icollection.h
+++ src/xtd.core/include/xtd/collections/generic/icollection.h
@@ -28,6 +28,13 @@ namespace xtd {
       /// @par Library
       /// xtd.core
       /// @ingroup xtd_core interfaces
+      /// @par Examples
+      /// The following example implements the xtd::collections::generic::icollection <type_t> interface to create a collection of custom `box` objects named `box_collection`. Each `box` has `height`, `length`, and `width` properties, which are used to define equality. Equality can be defined as all dimensions being the same or the volume being the same. The Box class implements the xtd::iequatable <type_t> interface to define the default equality as the dimensions being the same.
+      ///
+      /// The `box_collection` class implements the xtd::collections::generic::icollection::contains method to use the default equality to determine whether a `box` is in the collection. This method is used by the xtd::collections::generic::icollection::add method so that each `box` added to the collection has a unique set of dimensions. The `box_collection` class also provides an overload of the xtd::collections::generic::icollection::contains method that takes a specified xtd::collections::generic::iequality_comparer <type_t> interface, such as `b`ox_same_dimensions` and `box_same_volume` classes in the example.
+      ///
+      /// This example also implements an xtd::collections::generic::ienumerator <type_t> interface for the `box_collection` class so that the collection can be enumerated.
+      /// @include generic_icollection.cpp
       /// @remarks The xtd::collections::generic::icollection <type_t> interface is the base interface for classes in the xtd::collections::generic namespace.
       /// @remarks The xtd::collections::generic::icollection <type_t> interface extends xtd::collections::generic::ienumerable <type_t>; xtd::collections::generic::idictionary <key_t, value_t> and xtd::collections::generic::ilist <type_t> are more specialized interfaces that extend xtd::collections::generic::icollection <type_t>. A xtd::collections::generic::idictionary <key_t, value_t> implementation is a collection of key/value pairs, like the xtd::collections::generic::dictoinary <key_t, value_t> class. A xtd::collections::generic::ilist <type_t> implementation is a collection of values, and its members can be accessed by index, like the xtd::collections::generic::list <type_t> class.
       /// @remarks If neither the xtd::collections::generic::idictionary <key_t, value_t> interface nor the xtd::collections::generic::ilist <type_t> interface meet the requirements of the required collection, derive the new collection class from the xtd::collections::generic::icollection <type_t> interface instead for more flexibility.
