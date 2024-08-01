09ddde66d72569066622213718636e84edd7d67f
Gammasoft <gammasoft71@gmail.com>
2024-08-01 10:04:33 +0200

    ilist implementation

--- src/xtd.core/include/xtd/collections/generic/ilist.h
+++ src/xtd.core/include/xtd/collections/generic/ilist.h
@@ -3,6 +3,7 @@
 /// @copyright Copyright (c) 2024 Gammasoft. All rights reserved.
 #pragma once
 #include "icollection.h"
+#include "../../size_object.h"
 
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
@@ -31,19 +32,50 @@ namespace xtd {
       template<typename type_t>
       class ilist : public icollection<type_t> {
       public:
-        /// @name Public Properties
+        /// @name Public Fields
         
         /// @{
+        /// @brief This is a special value equal to the maximum value representable by the type xtd::size.
+        inline static constexpr xtd::size npos = xtd::size_object::max_value;
         /// @}
 
         /// @name Public Methods
         
         /// @{
+        /// @brief Determines the index of a specific item in the xtd::collections::generic::ilist <type_t>.
+        /// @param item The object to locate in the xtd::collections::generic::ilist <type_t>.
+        /// @return The index of item if found in the list; otherwise, xtd::collections::generic::ilist::npos.
+        /// @remarks If an object occurs multiple times in the list, the xtd::collections::generic::ilist::index_of method always returns the first instance found.
+        virtual xtd::size index_of(const type_t& item) const noexcept = 0;
+        
+        /// @brief Inserts an item to the xtd::collections::generic::ilist <type_t> at the specified index.
+        /// @param index The zero-based index at which item should be inserted.
+        /// @param item The object to insert into the xtd::collections::generic::ilist <type_t>.
+        /// @exception xtd::argument_out_of_range_exception `index` is not a valid index in the xtd::collections::generic::ilist <type_t>.
+        /// @exception xtd::not_supported_exception The xtd::collections::generic::ilist <type_t> is read-only.
+        /// @remarks If `index` equals the number of items in the xtd::collections::generic::ilist <type_t>, then item is appended to the list.
+        /// @remarks In collections of contiguous elements, such as lists, the elements that follow the insertion point move down to accommodate the new element. If the collection is indexed, the indexes of the elements that are moved are also updated. This behavior does not apply to collections where elements are conceptually grouped into buckets, such as a hash table.
+        virtual void insert(xtd::size index, const type_t& item) = 0;
+
+        /// @brief Removes the xtd::collections::generic::ilist <type_t> item at the specified index.
+        /// @param index The zero-based index of the item to remove.
+        /// @exception xtd::argument_out_of_range_exception `index` is not a valid index in the xtd::collections::generic::ilist <type_t>.
+        /// @exception xtd::not_supported_exception The xtd::collections::generic::ilist <type_t> is read-only.
+        /// @remarks In collections of contiguous elements, such as lists, the elements that follow the removed element move up to occupy the vacated spot. If the collection is indexed, the indexes of the elements that are moved are also updated. This behavior does not apply to collections where elements are conceptually grouped into buckets, such as a hash table.
+        virtual void remove_at(xtd::size index) = 0;
         /// @}
 
         /// @name Public Operators
         
         /// @{
+        /// @brief Gets the element at the specified index.
+        /// @param index The zero-based index of the element to get.
+        /// @remarks This operator provides the ability to access a specific element in the collection by using the following syntax: `my_collection[index]`.
+        virtual const type_t& operator [](xtd::size index) const = 0;
+        /// @brief Sets the element at the specified index.
+        /// @param index The zero-based index of the element to set.
+        /// @remarks This operator provides the ability to access a specific element in the collection by using the following syntax: `my_collection[index]`.
+        virtual type_t& operator [](xtd::size index) = 0;
         /// @}
       };
     }
