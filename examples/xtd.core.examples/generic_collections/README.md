21051f45eff7c7e22ec2cb6337299be7c61b294e
Gammasoft <gammasoft71@gmail.com>
2024-08-02 14:19:17 +0200

    Add generic_ilist example

--- examples/xtd.core.examples/generic_collections/README.md
+++ examples/xtd.core.examples/generic_collections/README.md
@@ -5,6 +5,7 @@
 * [generic_icollection](generic_icollection/README.md) shows how to use [xtd::collections::generic::icollection](https://gammasoft71.github.io/xtd/reference_guides/latest/classxtd_1_1collections_1_1generic_1_1icollection.html) interface.
 * [generic_ienumerable](generic_ienumerable/README.md) shows how to use [xtd::collections::generic::ienumerable](https://gammasoft71.github.io/xtd/reference_guides/latest/classxtd_1_1collections_1_1generic_1_1ienumerable.html) interface.
 * [generic_ienumerator](generic_ienumerator/README.md) shows how to use [xtd::collections::generic::ienumerator](https://gammasoft71.github.io/xtd/reference_guides/latest/classxtd_1_1collections_1_1generic_1_1ienumerator.html) interface.
+* [generic_ilist](generic_ilist/README.md) shows how to use [xtd::collections::generic::ilist](https://gammasoft71.github.io/xtd/reference_guides/latest/classxtd_1_1collections_1_1generic_1_1ilist.html) interface.
 
 ## Build and run any project
 
--- examples/xtd.core.examples/generic_collections/generic_ienumerable/src/generic_ienumerable.cpp
+++ examples/xtd.core.examples/generic_collections/generic_ienumerable/src/generic_ienumerable.cpp
@@ -1,61 +1,64 @@
-#include <xtd/collections/generic/ienumerable>
-#include <xtd/collections/generic/list>
-#include <xtd/console>
+#include <xtd/xtd>
 
 using namespace xtd;
 using namespace xtd::collections::generic;
 
-struct box : public iequatable<::box>, public icomparable<::box> {
-  box() = default;
-  box(int l, int w, int h) : length{l}, width {w}, height {h} {}
-  
-  int length = 0;
-  int width = 0;
-  int height = 0;
-  
-  int32 compare_to(const ::box& o) const noexcept override {return 0;}
-  bool equals(const ::box& o) const noexcept override {return length == o.length && width == o.width && height == o.height;}
-  ustring to_string() const noexcept {return ustring::format("box [length={}, width={}, height={}]", length, width, height);}
-};
-
-class box_collection : public ienumerable<::box> {
+class program {
 public:
-  box_collection(const std::initializer_list<::box>& boxes) : boxes_(boxes) {}
-  
-  enumerator<::box> get_enumerator() const noexcept override {
-    class box_enumerator : public ienumerator<::box> {
-    public:
-      explicit box_enumerator(const list<::box>& items) : items_(items) {}
-      const ::box& current() const override {return items_[index_];}
-      bool move_next() override {return ++index_ < items_.size();}
-      void reset() override {index_ = xtd::box_integer<xtd::size>::max_value;}
-      
-    protected:
-      const list<::box>& items_;
-      size index_ = xtd::box_integer<xtd::size>::max_value;
-    };
-    return {new_ptr<box_enumerator>(boxes_)};
+  static auto main() -> void {
+    auto boxes = box_collection {{10, 20, 30}, {20, 5, 10}, {12, 3, 7}};
+    
+    auto enumerator = boxes.get_enumerator();
+    while (enumerator.move_next())
+      console::write_line(enumerator.current().to_string());
+    
+    console::write_line();
+    for (auto box : boxes)
+      console::write_line(box.to_string());
+    
+    console::write_line();
+    for (auto iterator = boxes.begin(); iterator != boxes.end(); ++iterator)
+      console::write_line(iterator->to_string());
   }
+
+  struct box : public iequatable<program::box>, public icomparable<program::box> {
+    box() = default;
+    box(int l, int w, int h) : length{l}, width {w}, height {h} {}
+    
+    int length = 0;
+    int width = 0;
+    int height = 0;
+    
+    int32 compare_to(const program::box& o) const noexcept override {return 0;}
+    bool equals(const program::box& o) const noexcept override {return length == o.length && width == o.width && height == o.height;}
+    ustring to_string() const noexcept {return ustring::format("box [length={}, width={}, height={}]", length, width, height);}
+  };
   
-private:
-  list<::box> boxes_;
+  class box_collection : public ienumerable<program::box> {
+  public:
+    box_collection(const std::initializer_list<program::box>& boxes) : boxes_(boxes) {}
+    
+    enumerator<program::box> get_enumerator() const noexcept override {
+      class box_enumerator : public ienumerator<program::box> {
+      public:
+        explicit box_enumerator(const list<program::box>& items) : items_(items) {}
+        const program::box& current() const override {return items_[index_];}
+        bool move_next() override {return ++index_ < items_.size();}
+        void reset() override {index_ = box_integer<size>::max_value;}
+        
+      protected:
+        const list<program::box>& items_;
+        size index_ = box_integer<size>::max_value;
+      };
+      return {new_ptr<box_enumerator>(boxes_)};
+    }
+    
+  private:
+    list<program::box> boxes_;
+  };
 };
 
-auto main() -> int {
-  auto boxes = box_collection {{10, 20, 30}, {20, 5, 10}, {12, 3, 7}};
-  
-  auto enumerator = boxes.get_enumerator();
-  while (enumerator.move_next())
-    console::write_line(enumerator.current().to_string());
-  
-  console::write_line();
-  for (auto box : boxes)
-    console::write_line(box.to_string());
-  
-  console::write_line();
-  for (auto iterator = boxes.begin(); iterator != boxes.end(); ++iterator)
-    console::write_line(iterator->to_string());
-}
+startup_(program::main);
 
 // This code can produces the following output :
 //
