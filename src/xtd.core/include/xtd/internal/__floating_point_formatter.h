42faa9efa5ef543be329cde42b56c6a9a0f5340c
Gammasoft <gammasoft71@gmail.com>
2024-08-03 19:51:24 +0200

    Add haxfloat formater

--- src/xtd.core/include/xtd/internal/__floating_point_formatter.h
+++ src/xtd.core/include/xtd/internal/__floating_point_formatter.h
@@ -7,9 +7,11 @@
 #endif
 /// @endcond
 
+#include "__binary_formatter.h"
 #include "__character_formatter.h"
 #include "__currency_formatter.h"
 #include "__format_exception.h"
+#include "__hexfloat_formatter.h"
 #include "__natural_formatter.h"
 #include "__sprintf.h"
 #include <algorithm>
@@ -21,7 +23,7 @@ inline std::basic_string<char_t> __floating_point_formatter(const std::basic_str
   auto fmt = format;
   if (fmt.empty()) fmt = {'G'};
   
-  std::vector<char_t> possible_formats {'c', 'C', 'e', 'E', 'f', 'F', 'g', 'G', 'n', 'N', 'p', 'P'};
+  std::vector<char_t> possible_formats {'b', 'B', 'c', 'C', 'e', 'E', 'f', 'F', 'g', 'G', 'n', 'N', 'p', 'P', 'x', 'X'};
   if (fmt.size() > 3 || std::find(possible_formats.begin(), possible_formats.end(), fmt[0]) == possible_formats.end() || (fmt.size() >= 2 && !std::isdigit(fmt[1])) || (fmt.size() == 3 && !std::isdigit(fmt[2])))
     __format_exception("Custom format not yet implemented");
     
@@ -37,6 +39,8 @@ inline std::basic_string<char_t> __floating_point_formatter(const std::basic_str
   
   std::basic_string<char_t> fmt_str({'%', '.', '*', 'L'});
   switch (fmt[0]) {
+    case 'b':
+    case 'B': {long double value_long_double = static_cast<long double>(value); int64_t value_int64 = 0; memcpy(&value_int64, &value_long_double, sizeof(value_long_double)); return __binary_formatter<char_t>(value_int64, precision);}
     case 'c':
     case 'C': return __currency_formatter<char_t>(static_cast<long double>(value), loc);
     case 'e':
@@ -49,6 +53,8 @@ inline std::basic_string<char_t> __floating_point_formatter(const std::basic_str
     case 'N': return __natural_formatter<char_t>(static_cast<long double>(value), precision, loc);
     case 'p': return __sprintf((fmt_str + char_t('f')).c_str(), precision, static_cast<long double>(value * 100)) + std::basic_string<char_t>({char_t(' '), char_t('%')});
     case 'P': return __sprintf((fmt_str + char_t('F')).c_str(), precision, static_cast<long double>(value * 100)) + std::basic_string<char_t>({char_t(' '), char_t('%')});
+    case 'x':
+    case 'X': return __hexfloat_formatter<char_t>(static_cast<long double>(value), precision, loc);
     default: __format_exception("Invalid format expression"); return {};
   }
 }
