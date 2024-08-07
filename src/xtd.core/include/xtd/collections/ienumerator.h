4a8ddc538933df3c1a1109f80ceb5ba4958f241a
Gammasoft <gammasoft71@gmail.com>
2024-08-07 17:42:54 +0200

    Add documentation

--- src/xtd.core/include/xtd/collections/ienumerator.h
+++ src/xtd.core/include/xtd/collections/ienumerator.h
@@ -22,7 +22,17 @@ namespace xtd {
     /// xtd::collections
     /// @par Library
     /// xtd.core
-    /// @ingroup xtd_core interfaces
+    /// @ingroup xtd_core collections interfaces
+    /// @remarks xtd::collections::ienumerator is the base interface for all non-generic enumerators. Its generic equivalent is the xtd::collections::generic::iznumerator <type_t> interface.
+    /// @remarks The for each statement of the C++ language hides the complexity of the enumerators. Therefore, using for each is recommended instead of directly manipulating the enumerator.
+    /// @remarks Enumerators can be used to read the data in the collection, but they cannot be used to modify the underlying collection.
+    /// @remarks The xtd::collections::ienumerator::reset method is provided for COM interoperability and does not need to be fully implemented; instead, the implementer can throw a xtd::not_supported_exception.
+    /// @remarks Initially, the enumerator is positioned before the first element in the collection. You must call the xtd::collections::ienumerator::MMoveN_next method to advance the enumerator to the first element of the collection before reading the value of xtd::collections::ienumerator::current; otherwise, xtd::collections::ienumerator::current is undefined.
+    /// @remarks xtd::collections::ienumerator::current returns the same object until either xtd::collections::ienumerator::move_next or xtd::collections::ienumerator::reset is called. xtd::collections::ienumerator::move_next sets xtd::collections::ienumerator::current to the next element.
+    /// @remarks If xtd::collections::ienumerator::move_next passes the end of the collection, the enumerator is positioned after the last element in the collection and xtd::collections::ienumerator::move_next returns `false`. When the enumerator is at this position, subsequent calls to xtd::collections::ienumerator::move_next also return `false`. If the last call to xtd::collections::ienumerator::move_next returned `false`, xtd::collections::ienumerator::current is undefined.
+    /// @remarks To set xtd::collections::ienumerator::current to the first element of the collection again, you can call Reset, if it's implemented, followed by xtd::collections::ienumerator::move_next. If xtd::collections::ienumerator::reset is not implemented, you must create a new enumerator instance to return to the first element of the collection.
+    /// @remarks If changes are made to the collection, such as adding, modifying, or deleting elements, the behavior of the enumerator is undefined.
+    /// @remarks The enumerator does not have exclusive access to the collection; therefore, enumerating through a collection is intrinsically not a thread-safe procedure. Even when a collection is synchronized, other threads can still modify the collection, which causes the enumerator to throw an exception. To guarantee thread safety during enumeration, you can either lock the collection during the entire enumeration or catch the exceptions resulting from changes made by other threads.
     using ienumerator = generic::ienumerator<xtd::any_object>;
   }
 }
