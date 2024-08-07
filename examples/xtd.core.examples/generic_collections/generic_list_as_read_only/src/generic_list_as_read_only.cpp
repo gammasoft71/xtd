efb3e314e0f799bd67ac19fa1f3badf149f7ee93
Gammasoft <gammasoft71@gmail.com>
2024-08-07 09:56:28 +0200

    Add generic_list_as_read_only example

---
+++ examples/xtd.core.examples/generic_collections/generic_list_as_read_only/src/generic_list_as_read_only.cpp
@@ -0,0 +1,64 @@
+#include <xtd/xtd>
+
+using namespace xtd;
+using namespace xtd::collections::generic;
+
+class example {
+public:
+  static auto main() -> void {
+    auto dinosaurs = list<ustring> {};
+    
+    console::write_line("\ncapacity: {0}", dinosaurs.capacity());
+    
+    dinosaurs.add("Tyrannosaurus");
+    dinosaurs.add("Amargasaurus");
+    dinosaurs.add("Deinonychus");
+    dinosaurs.add("Compsognathus");
+
+    console::write_line();
+    for (auto dinosaur : dinosaurs)
+      console::write_line(dinosaur);
+
+    console::write_line("\nauto ro_dinosaurs = dinosaurs.as_read_only()");
+    auto ro_dinosaurs = dinosaurs.as_read_only();
+    
+    console::write_line("\nElements in the read-only ilist:");
+    for (auto dinosaur : ro_dinosaurs)
+      console::write_line(dinosaur);
+
+    console::write_line("\ndinosaurs[2] = \"Coelophysis\"");
+    dinosaurs[2] = "Coelophysis";
+    
+    console::write_line("\nElements in the read-only ilist:");
+    for (auto dinosaur : ro_dinosaurs)
+      console::write_line(dinosaur);
+  }
+};
+
+startup_(example::main);
+
+// This code can produces the following output :
+//
+//
+// capacity: 0
+//
+// Tyrannosaurus
+// Amargasaurus
+// Deinonychus
+// Compsognathus
+//
+// auto ro_dinosaurs = dinosaurs.as_read_only()
+//
+// Elements in the read-only ilist:
+// Tyrannosaurus
+// Amargasaurus
+// Deinonychus
+// Compsognathus
+//
+// dinosaurs[2] = "Coelophysis"
+//
+// Elements in the read-only ilist:
+// Tyrannosaurus
+// Amargasaurus
+// Coelophysis
+// Compsognathus
