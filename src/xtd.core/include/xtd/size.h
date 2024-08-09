e6f737927fcc3716c460befc015845a3aadb016b
Gammasoft <gammasoft71@gmail.com>
2024-08-09 13:58:25 +0200

    Fix include file

--- src/xtd.core/include/xtd/size.h
+++ src/xtd.core/include/xtd/size.h
@@ -2,7 +2,7 @@
 /// @brief Contains xtd::size type.
 /// @copyright Copyright (c) 2024 Gammasoft. All rights reserved.
 #pragma once
-#include <cstdint>
+#include <cstddef>
 
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
