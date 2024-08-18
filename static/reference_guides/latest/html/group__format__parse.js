7e138f08f58e5ac6f085a0cbacd6998ddfcaa72a
gammasoft71 <gammasoft71@gmail.com>
2024-08-18 21:05:21 +0200

    update latest reference guide

--- static/reference_guides/latest/html/group__format__parse.js
+++ static/reference_guides/latest/html/group__format__parse.js
@@ -1,7 +1,5 @@
 var group__format__parse =
 [
-    [ "xtd::basic_string::format", "group__format__parse_gac697a1e080484c7ad534b6fdee4e5000.html#gac697a1e080484c7ad534b6fdee4e5000", null ],
-    [ "xtd::basic_string::sprintf", "group__format__parse_ga06d4b243f60f84e512a0fb001f40f066.html#ga06d4b243f60f84e512a0fb001f40f066", null ],
     [ "xtd::string::format", "group__format__parse_ga05c71aee49d3651807517e61ec8d0e30.html#ga05c71aee49d3651807517e61ec8d0e30", null ],
     [ "xtd::string::sprintf", "group__format__parse_ga41e4a638cc8fc1d76cad41c972d98723.html#ga41e4a638cc8fc1d76cad41c972d98723", null ],
     [ "xtd::parse< drawing::color >", "group__format__parse_gaa980ebef2b93ecd8c9b9f7018e81bcb6.html#gaa980ebef2b93ecd8c9b9f7018e81bcb6", null ]
