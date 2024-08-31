4d169887977e29e73054bd9d71311e4ac22ecfac
gammasoft71 <gammasoft71@gmail.com>
2024-08-31 21:05:59 +0200

    update latest reference guide

--- static/reference_guides/latest/html/____string__definitions_8h.js
+++ static/reference_guides/latest/html/____string__definitions_8h.js
@@ -1,6 +1,6 @@
 var ____string__definitions_8h =
 [
-    [ "string_", "____string__definitions_8h.html#gad8eca5a16f87ec636f6be2518a914b21", null ],
+    [ "string", "____string__definitions_8h.html#ga9fe47a4b2c6dc7d7eeaf5d2fb218d4d1", null ],
     [ "u16string", "____string__definitions_8h.html#ga87efea41d4bd7beb815934226919d4cf", null ],
     [ "u32string", "____string__definitions_8h.html#ga8e905e9638523f5b5d380d8bcbcc834e", null ],
     [ "u8string", "____string__definitions_8h.html#ga10e56556495f88e0d68bde21820cdddf", null ],
