b9ae38a1f0798374d35e5e0957247671fe77480f
Gammasoft <gammasoft71@gmail.com>
2024-09-09 15:51:43 +0200

    Add xtd::array unit tests

---
+++ tests/xtd.core.unit_tests/src/xtd/tests/array_tests.cpp
@@ -0,0 +1,14 @@
+#include <xtd/array>
+#include <xtd/tunit/assert>
+#include <xtd/tunit/test_class_attribute>
+#include <xtd/tunit/test_method_attribute>
+
+using namespace xtd;
+using namespace xtd::tunit;
+
+namespace xtd::tests {
+  class test_class_(array_tests) {
+    void test_method_(default_constructor) {
+    }
+  };
+}
