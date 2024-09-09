40077417e4ebac6736872909cdf1810594323710
Gammasoft <gammasoft71@gmail.com>
2024-09-09 10:18:19 +0200

    xtd::basic_array implementation (WIP)

--- src/xtd.core/include/xtd/array.hpp
+++ src/xtd.core/include/xtd/array.hpp
@@ -7,3 +7,33 @@
 #error "Do not include this file: Internal use only. Include <basic_string> or <basic_string.h> instead."
 #endif
 
+template<typename type_t, typename allocator_t>
+inline const type_t& xtd::basic_array<type_t, allocator_t>::get_value(const xtd::array<xtd::size>& indexes) const {
+  return operator()(indexes);
+}
+
+template<typename type_t, typename allocator_t>
+inline type_t& xtd::basic_array<type_t, allocator_t>::operator()(const xtd::array<xtd::size>& indexes) {
+  auto position = xtd::size {0};
+  for (auto index1 = xtd::size {0}; index1 < indexes.size(); ++index1) {
+    if (indexes[index1] > get_length(index1)) __throw_index_out_of_range_exception(__FILE__, __LINE__, __func__);
+    auto multiplicand = xtd::size {1};
+    for (auto index2 = index1 + 1; index2 < indexes.size(); ++index2)
+      multiplicand *= get_length(index2);
+    position += indexes[index1] * multiplicand;
+  }
+  return data_->items[position];
+}
+
+template<typename type_t, typename allocator_t>
+inline const type_t& xtd::basic_array<type_t, allocator_t>::operator()(const xtd::array<xtd::size>& indexes) const {
+  auto position = xtd::size {0};
+  for (auto index1 = xtd::size {0}; index1 < indexes.size(); ++index1) {
+    if (indexes[index1] > get_length(index1)) __throw_index_out_of_range_exception(__FILE__, __LINE__, __func__);
+    auto multiplicand = xtd::size {1};
+    for (auto index2 = index1 + 1; index2 < indexes.size(); ++index2)
+      multiplicand *= get_length(index2);
+    position += indexes[index1] * multiplicand;
+  }
+  return data_->items[position];
+}
