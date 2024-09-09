e3f86972f5fa8845099d076c16ee849482e8d188
gammasoft71 <gammasoft71@gmail.com>
2024-09-09 23:31:10 +0200

    Update developper reference guide

--- static/reference_guides/developer/classxtd_1_1collections_1_1generic_1_1ilist.js
+++ static/reference_guides/developer/classxtd_1_1collections_1_1generic_1_1ilist.js
@@ -1,7 +1,7 @@
 var classxtd_1_1collections_1_1generic_1_1ilist =
 [
-    [ "iterator", "classxtd_1_1collections_1_1generic_1_1ilist_a94a2091fc2af107af1976d65dd15051a.html#a94a2091fc2af107af1976d65dd15051a", null ],
-    [ "const_iterator", "classxtd_1_1collections_1_1generic_1_1ilist_a3ad57b0ba72194698f0931c8ba84f79a.html#a3ad57b0ba72194698f0931c8ba84f79a", null ],
+    [ "iterator", "classxtd_1_1collections_1_1generic_1_1ilist_a192132ce093e3a233f34095a389b4236.html#a192132ce093e3a233f34095a389b4236", null ],
+    [ "const_iterator", "classxtd_1_1collections_1_1generic_1_1ilist_a2d0527f45d40666d178eb5dd97067f36.html#a2d0527f45d40666d178eb5dd97067f36", null ],
     [ "is_fixed_size", "classxtd_1_1collections_1_1generic_1_1ilist_a27d1e03ba6f5666f4e84f9bd7e96ce72.html#a27d1e03ba6f5666f4e84f9bd7e96ce72", null ],
     [ "index_of", "classxtd_1_1collections_1_1generic_1_1ilist_af0181344a04570eaab3bbe6cd28712f5.html#af0181344a04570eaab3bbe6cd28712f5", null ],
     [ "insert", "classxtd_1_1collections_1_1generic_1_1ilist_ae33563c47a20a0bbb17e16d86efacce2.html#ae33563c47a20a0bbb17e16d86efacce2", null ],
