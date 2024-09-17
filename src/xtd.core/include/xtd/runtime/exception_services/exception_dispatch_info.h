60ff6157aa95d2bd80923609c474817958d19e32
Gammasoft <gammasoft71@gmail.com>
2024-09-17 21:57:17 +0200

    exception_dispatch_info class implementation

--- src/xtd.core/include/xtd/runtime/exception_services/exception_dispatch_info.h
+++ src/xtd.core/include/xtd/runtime/exception_services/exception_dispatch_info.h
@@ -3,6 +3,8 @@
 /// @copyright Copyright (c) 2024 Gammasoft. All rights reserved.
 #pragma once
 #include "../../exception.h"
+#include "../../ptr.h"
+#include <type_traits>
 
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
@@ -28,13 +30,6 @@ namespace xtd {
       /// @remarks For an example, see [Capture exceptions to rethrow later](https://gammasoft71.github.io/xtd/docs/documentation/Guides/xtd.core/Exceptions/exceptions_best_practices#capture-and-rethrow-exceptions-properly).
       class exception_dispatch_info final : public xtd::object {
       public:
-        /// @name Public Aliases
-        
-        /// @{
-        /// @brief Represents the optional exception value type.
-        using exception_value = std::optional<std::reference_wrapper<const xtd::exception>>;
-        /// @}
-        
         /// @cond
         exception_dispatch_info() = default;
         exception_dispatch_info(exception_dispatch_info&&) = default;
@@ -53,23 +48,22 @@ namespace xtd {
         /// @brief Gets the exception that's represented by the current instance.
         /// @return The exception that's represented by the current instance.
         /// @remarks This property is used by the Task Parallel Library, for example, to combine multiple exceptions in an xtd::aggregate_exception object. It's not intended to be used by application code. Use the xtd::runtime::exception_services::exception_dispatch_info::rethrow method to restore the state of the captured exception and throw it.
-        exception_value source_exception() const noexcept {return source_;}
+        xtd::ptr<xtd::exception> source_exception() const noexcept {return source_;}
         /// @}
         
         /// @name Public Properties
         
         /// @{
-        /// @brief Creates an xtd::runtime::exception_services::exception_dispatch_info object that represents the current exception at the current point in code.
-        /// @return An object that represents the specified exception at the current point in code.
-        /// @remarks You can use the xtd::runtime::exception_services::exception_dispatch_info object that's returned by this method at another time and possibly on another thread to rethrow the specified exception, as if the exception had flowed from the point where it was captured to the point where it's rethrown.
-        /// @remarks If the exception is active when it's captured, the current stack trace information that's contained in the exception is stored. If it's inactive, that is, if it has not been thrown, it doesn't have any stack trace information.
-        static exception_dispatch_info capture() {return exception_dispatch_info {true};}
         /// @brief Creates an xtd::runtime::exception_services::exception_dispatch_info object that represents the specified exception at the current point in code.
         /// @param source The exception whose state is captured, and which is represented by the returned object.
         /// @return An object that represents the specified exception at the current point in code.
         /// @remarks You can use the xtd::runtime::exception_services::exception_dispatch_info object that's returned by this method at another time and possibly on another thread to rethrow the specified exception, as if the exception had flowed from the point where it was captured to the point where it's rethrown.
         /// @remarks If the exception is active when it's captured, the current stack trace information that's contained in the exception is stored. If it's inactive, that is, if it has not been thrown, it doesn't have any stack trace information.
-        static exception_dispatch_info capture(const xtd::exception& source) {return exception_dispatch_info {source};}
+        template <typename exception_t>
+        static exception_dispatch_info capture(const exception_t& source) {
+          static_assert(std::is_base_of<xtd::exception, exception_t>::value);
+          return exception_dispatch_info {source};
+        }
         
         /// @brief Rethrows the exception that's represented by the current xtd::runtime::exception_services::exception_dispatch_info object, after restoring the state that was saved when the exception was captured.
         void rethrow() {if (exception_captured()) std::rethrow_exception(exception_ptr_);}
@@ -77,12 +71,20 @@ namespace xtd {
         static void rethrow(const xtd::exception& source) {exception_dispatch_info {source}.rethrow();}
         /// @}
         
-      private:
-        exception_dispatch_info(bool) : exception_ptr_ {std::current_exception()} {}
-        exception_dispatch_info(const xtd::exception& source) : exception_ptr_ {std::make_exception_ptr(source)}, source_ {source} {}
+        /// @name Public Operators
         
+        /// @{
+        /// @brief Convert to boolean operator.
+        /// @return `true` if exception is captured; otherwise `false`.
+        explicit operator bool() const noexcept {return exception_captured();}
+        /// @}
+        
+      private:
+        template <typename exception_t>
+        exception_dispatch_info(const exception_t& source) : source_ {source.template memberwise_clone<exception_t>().release()}, exception_ptr_ {std::make_exception_ptr(source)} {}
+        
+        xtd::ptr<xtd::exception> source_;
         std::exception_ptr exception_ptr_;
-        exception_value source_;
       };
     }
   }
