526696376e37d13f92151827b36e9c4961f94984
gammasoft71 <gammasoft71@gmail.com>
2024-09-20 22:29:50 +0200

    Fix warning

--- src/xtd.core/include/xtd/internal/__iformatable_formatter.h
+++ src/xtd.core/include/xtd/internal/__iformatable_formatter.h
@@ -21,10 +21,11 @@ std::string __to_string_istringable_to_string(const xtd::istringable* obj);
 
 template<typename value_t>
 static std::string __to_string_polymorphic(const value_t& value, const std::string& fmt, const std::locale& loc, std::true_type) {
-  if (dynamic_cast<const std::exception*>(&value)) return std::string {"exception: "} + dynamic_cast<const std::exception&>(value).what();
-  if (dynamic_cast<const xtd::iformatable*>(&value)) return dynamic_cast<const xtd::iformatable&>(value).__opague_internal_formatable__(reinterpret_cast<intptr_t>(&fmt), reinterpret_cast<intptr_t>(&loc), 0, INTPTR_MAX);
-  if (dynamic_cast<const xtd::istringable*>(&value)) return __to_string_istringable_to_string(dynamic_cast<const xtd::istringable*>(&value));
-  if (dynamic_cast<const xtd::object*>(&value)) return __to_string_object_to_string(dynamic_cast<const xtd::object*>(&value));
+  auto value_ptr = &value;
+  if (dynamic_cast<const std::exception*>(value_ptr)) return std::string {"exception: "} + dynamic_cast<const std::exception&>(value).what();
+  if (dynamic_cast<const xtd::iformatable*>(value_ptr)) return dynamic_cast<const xtd::iformatable&>(value).__opague_internal_formatable__(reinterpret_cast<intptr_t>(&fmt), reinterpret_cast<intptr_t>(&loc), 0, INTPTR_MAX);
+  if (dynamic_cast<const xtd::istringable*>(value_ptr)) return __to_string_istringable_to_string(dynamic_cast<const xtd::istringable*>(&value));
+  if (dynamic_cast<const xtd::object*>(value_ptr)) return __to_string_object_to_string(dynamic_cast<const xtd::object*>(value_ptr));
   __format_exception(typeid(value)); return {};
 }
 
