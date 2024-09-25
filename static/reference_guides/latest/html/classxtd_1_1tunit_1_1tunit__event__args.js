b8e73a2d898e97b5af23583666497283d91611a7
gammasoft71 <gammasoft71@gmail.com>
2024-09-25 21:00:54 +0200

    update latest reference guide

---
+++ static/reference_guides/latest/html/classxtd_1_1tunit_1_1tunit__event__args.js
@@ -0,0 +1,5 @@
+var classxtd_1_1tunit_1_1tunit__event__args =
+[
+    [ "tunit_event_args", "classxtd_1_1tunit_1_1tunit__event__args.html#a0b08926c0b6d0a4160d2b4680bb21725", null ],
+    [ "unit_test", "classxtd_1_1tunit_1_1tunit__event__args.html#a5a1345155859584a871e0ba2d9db91b7", null ]
+];
---
+++ static/reference_guides/latest/html/int32__object_8h.js
@@ -0,0 +1,4 @@
+var int32__object_8h =
+[
+    [ "int32_object", "int32__object_8h.html#ga645e4f1656c3b3a1efe9d70224230090", null ]
+];
