b4a469e92dd32a4f0948be80654ee48e6e2f5020
Gammasoft <gammasoft71@gmail.com>
2024-09-29 20:36:32 +0200

    Rename h_results to h_result

--- src/xtd.core/include/xtd/collections/key_not_found_exception.h
+++ src/xtd.core/include/xtd/collections/key_not_found_exception.h
@@ -44,7 +44,7 @@ namespace xtd {
       /// @param inner_exception The exception that is the cause of the current exception.
       /// @param stack_frame (optional) Contains current information about member name, file path and  line number in the file where the exception is occurred. Typically #current_stack_frame_.
       template<typename exception_t>
-      key_not_found_exception(const std::optional<xtd::string>& message, const exception_t& inner_exception, const xtd::diagnostics::stack_frame& stack_frame = xtd::diagnostics::stack_frame::empty()) : xtd::system_exception(message, inner_exception, stack_frame) {error_code(h_results::make_error_code(h_results::E_POINTER));}
+      key_not_found_exception(const std::optional<xtd::string>& message, const exception_t& inner_exception, const xtd::diagnostics::stack_frame& stack_frame = xtd::diagnostics::stack_frame::empty()) : xtd::system_exception(message, inner_exception, stack_frame) {error_code(h_result::make_error_code(h_result::E_POINTER));}
       
       /// @brief Create a new instance of class key_not_found_exception
       /// @param message Message string associate to the exception.
--- src/xtd.core/include/xtd/h_result_category.h
+++ src/xtd.core/include/xtd/h_result_category.h
@@ -1,24 +1,22 @@
 /// @file
-/// @brief Contains h_results class.
+/// @brief Contains h_result class.
 /// @copyright Copyright (c) 2024 Gammasoft. All rights reserved.
 #pragma once
-#include "int32.h"
-#include "string.h"
-#include "static.h"
+#include <system_error>
 
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
-  /// @brief Obtains a reference to the static error category object for h_result errors. The object is required to override the virtual function error_category::name() to return a pointer to the string "h_results_category". It is used to identify error conditions that correspond to the xtd::h_results error codes.
+  /// @brief Obtains a reference to the static error category object for h_result errors. The object is required to override the virtual function error_category::name() to return a pointer to the string "h_result_category". It is used to identify error conditions that correspond to the xtd::h_result error codes.
   /// @return A reference to the static object of unspecified runtime type, derived from [std::error_category](https://en.cppreference.com/w/cpp/error/error_category).
   /// @par Namespace
   /// xtd
   /// @par Library
   /// xtd.core
   /// @ingroup xtd_core exceptions
-  const std::error_category& h_results_category() noexcept;
+  const std::error_category& h_result_category() noexcept;
   
-  /// @brief Creates error code value for xtd::h_results `h_result`.
-  /// @param h_result xtd::h_results error code to create error code for.
+  /// @brief Creates error code value for xtd::h_result `h_result`.
+  /// @param h_result xtd::h_result error code to create error code for.
   /// @return Error code corresponding to `h_result`.
   /// @par Namespace
   /// xtd
