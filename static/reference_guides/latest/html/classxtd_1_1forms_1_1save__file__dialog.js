0b27dcdaf1b549864431d6110aa6783034332952
gammasoft71 <gammasoft71@gmail.com>
2024-10-01 21:01:07 +0200

    update latest reference guide

---
+++ static/reference_guides/latest/html/classxtd_1_1forms_1_1save__file__dialog.js
@@ -0,0 +1,5 @@
+var classxtd_1_1forms_1_1save__file__dialog =
+[
+    [ "save_file_dialog", "classxtd_1_1forms_1_1save__file__dialog.html#a39bb1a9fea69dca82823d2620c148c8c", null ],
+    [ "open_file", "classxtd_1_1forms_1_1save__file__dialog.html#a3efe0174b4ae89465efd027881b9663e", null ]
+];
