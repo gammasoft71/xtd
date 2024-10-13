984ac692fb6571d5a5bbe951161c62ef6abc3b5d
Gammasoft <gammasoft71@gmail.com>
2024-10-13 15:45:33 +0200

    fix h_result

--- src/xtd.core/include/xtd/collections/key_not_found_exception.h
+++ src/xtd.core/include/xtd/collections/key_not_found_exception.h
@@ -45,7 +45,7 @@ namespace xtd {
       /// @param inner_exception The exception that is the cause of the current exception.
       /// @param stack_frame (optional) Contains current stack frame about member name, file path and  line number in the file where the exception is occurred. Typically #current_stack_frame_.
       template<typename exception_t>
-      key_not_found_exception(const xtd::optional<xtd::string>& message, const exception_t& inner_exception, const xtd::diagnostics::stack_frame& stack_frame = null) : xtd::system_exception(message, inner_exception, stack_frame) {error_code(h_result::make_error_code(h_result::E_POINTER));}
+      key_not_found_exception(const xtd::optional<xtd::string>& message, const exception_t& inner_exception, const xtd::diagnostics::stack_frame& stack_frame = null) : xtd::system_exception(message, inner_exception, stack_frame) {error_code(h_result::make_error_code(h_result::COR_E_KEYNOTFOUND));}
       
       /// @brief Create a new instance of class key_not_found_exception
       /// @param message Message string associate to the exception.
