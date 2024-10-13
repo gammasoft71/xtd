14e036dc2feb5e5c43d159e3ecfe410817fd79f6
Gammasoft <gammasoft71@gmail.com>
2024-10-13 20:00:12 +0200

    Revue io_exception

---
+++ src/xtd.core/src/xtd/io/io_exception.cpp
@@ -0,0 +1,49 @@
+#include "../../../include/xtd/io/io_exception.h"
+
+using namespace xtd;
+using namespace xtd::diagnostics;
+using namespace xtd::io;
+
+io_exception::io_exception(const stack_frame& stack_frame) : system_exception(stack_frame) {
+  error_code(h_result::make_error_code(h_result::COR_E_IO));
+}
+
+io_exception::io_exception(const optional<string>& message, const stack_frame& stack_frame) : system_exception(message, stack_frame) {
+  error_code(h_result::make_error_code(h_result::COR_E_IO));
+}
+
+io_exception::io_exception(const string& message, const std::error_code& error, const stack_frame& stack_frame) : system_exception(message, stack_frame) {
+  error_code(error);
+}
+
+io_exception::io_exception(const string& message, const string& help_link, const stack_frame& stack_frame) : system_exception(message, stack_frame) {
+  error_code(h_result::make_error_code(h_result::COR_E_IO));
+  this->help_link(help_link);
+}
+
+io_exception::io_exception(const string& message, const std::error_code& error, const string& help_link, const stack_frame& stack_frame) : system_exception(message, stack_frame) {
+  error_code(error);
+  this->help_link(help_link);
+}
+
+io_exception::io_exception(const std::exception& inner_exception, const stack_frame& stack_frame) : system_exception(stack_frame) {
+  error_code(h_result::make_error_code(h_result::COR_E_IO));
+}
+
+io_exception::io_exception(const string& message, const std::exception& inner_exception, const stack_frame& stack_frame) : system_exception(message, stack_frame) {
+  error_code(h_result::make_error_code(h_result::COR_E_IO));
+}
+
+io_exception::io_exception(const string& message, const std::exception& inner_exception, const std::error_code& error, const stack_frame& stack_frame) : system_exception(message, stack_frame) {
+  error_code(error);
+}
+
+io_exception::io_exception(const string& message, const std::exception& inner_exception, const string& help_link, const stack_frame& stack_frame) : system_exception(message, stack_frame) {
+  error_code(h_result::make_error_code(h_result::COR_E_IO));
+  this->help_link(help_link);
+}
+
+io_exception::io_exception(const string& message, const std::exception& inner_exception, const std::error_code& error, const string& help_link, const xtd::diagnostics::stack_frame& stack_frame) : system_exception(message, stack_frame) {
+  error_code(error);
+  this->help_link(help_link);
+}
--- tests/xtd.core.unit_tests/src/xtd/io/tests/io_exception_tests.cpp
+++ tests/xtd.core.unit_tests/src/xtd/io/tests/io_exception_tests.cpp
@@ -1,10 +1,15 @@
 #include <xtd/io/io_exception>
+#include <xtd/argument_exception>
+#include <xtd/environment>
+#include <xtd/io/path>
+#include <xtd/reflection/assembly>
 #include <xtd/tunit/assert>
 #include <xtd/tunit/test_class_attribute>
 #include <xtd/tunit/test_method_attribute>
 
 using namespace xtd;
 using namespace xtd::io;
+using namespace xtd::reflection;
 using namespace xtd::tunit;
 
 namespace xtd::io::tests {
@@ -13,13 +18,166 @@ namespace xtd::io::tests {
     inline static std::locale previous_locale;
     static void test_initialize_(test_initialize) {
       previous_locale = std::locale::global(std::locale("en_US.UTF-8"));
-      stack_trace_enabled = system_exception::enable_stack_trace();
-      system_exception::enable_stack_trace(false);
+      stack_trace_enabled = io_exception::enable_stack_trace();
+      io_exception::enable_stack_trace(false);
     }
     
     static void test_cleanup_(test_cleanup) {
       std::locale::global(previous_locale);
-      system_exception::enable_stack_trace(stack_trace_enabled);
+      io_exception::enable_stack_trace(stack_trace_enabled);
+    }
+    
+    void test_method_(default_constructor) {
+      auto e = io_exception {};
+      assert::are_equal("xtd::io::io_exception", e.get_type().full_name(), csf_);
+      assert::is_empty(e.help_link(), csf_);
+      assert::are_equal(h_result::COR_E_IO, e.h_result(), csf_);
+      assert::are_equal(h_result::h_result_category(), e.error_code().category(), csf_);
+      assert::are_equal(e.h_result(), e.error_code().value(), csf_);
+      assert::is_false(e.inner_exception().has_value(), csf_);
+      assert::are_equal("I/O error occurred.", e.message(), csf_);
+      assert::are_equal(path::get_file_name(assembly::get_executing_assembly().location()), e.source(), csf_);
+      assert::is_empty(e.stack_trace(), csf_);
+      assert::is_empty(e.get_last_stack_frame().get_file_name(), csf_);
+      assert::are_equal(0u, e.get_last_stack_frame().get_file_line_number(), csf_);
+      assert::is_empty(e.get_last_stack_frame().get_method(), csf_);
+      assert::are_equal("xtd::io::io_exception : I/O error occurred.", e.to_string(), csf_);
+      assert::are_equal("I/O error occurred.", e.what(), csf_);
+    }
+    
+    void test_method_(default_constructor_with_current_stack_frame) {
+      auto stack_frame = current_stack_frame_;
+      auto e = io_exception {stack_frame};
+      assert::are_equal("xtd::io::io_exception", e.get_type().full_name(), csf_);
+      assert::is_empty(e.help_link(), csf_);
+      assert::are_equal(h_result::COR_E_IO, e.h_result(), csf_);
+      assert::are_equal(h_result::h_result_category(), e.error_code().category(), csf_);
+      assert::are_equal(e.h_result(), e.error_code().value(), csf_);
+      assert::is_false(e.inner_exception().has_value(), csf_);
+      assert::are_equal("I/O error occurred.", e.message(), csf_);
+      assert::are_equal(path::get_file_name(assembly::get_executing_assembly().location()), e.source(), csf_);
+      assert::are_equal(stack_frame.to_string(), e.stack_trace(), csf_);
+      assert::are_equal(stack_frame.get_file_name(), e.get_last_stack_frame().get_file_name(), csf_);
+      assert::are_equal(stack_frame.get_file_line_number(), e.get_last_stack_frame().get_file_line_number(), csf_);
+      assert::are_equal(stack_frame.get_method(), e.get_last_stack_frame().get_method(), csf_);
+      assert::are_equal("xtd::io::io_exception : I/O error occurred." + environment::new_line() + stack_frame.to_string(), e.to_string(), csf_);
+      assert::are_equal("I/O error occurred.", e.what(), csf_);
+    }
+    
+    void test_method_(constructor_with_empty_message) {
+      auto e = io_exception {""};
+      assert::are_equal("xtd::io::io_exception", e.get_type().full_name(), csf_);
+      assert::is_empty(e.help_link(), csf_);
+      assert::are_equal(h_result::COR_E_IO, e.h_result(), csf_);
+      assert::are_equal(h_result::h_result_category(), e.error_code().category(), csf_);
+      assert::are_equal(e.h_result(), e.error_code().value(), csf_);
+      assert::is_false(e.inner_exception().has_value(), csf_);
+      assert::is_empty(e.message(), csf_);
+      assert::are_equal(path::get_file_name(assembly::get_executing_assembly().location()), e.source(), csf_);
+      assert::is_empty(e.stack_trace(), csf_);
+      assert::is_empty(e.get_last_stack_frame().get_file_name(), csf_);
+      assert::are_equal(0u, e.get_last_stack_frame().get_file_line_number(), csf_);
+      assert::is_empty(e.get_last_stack_frame().get_method(), csf_);
+      assert::are_equal("xtd::io::io_exception", e.to_string(), csf_);
+      assert::are_equal("xtd::io::io_exception", e.what(), csf_);
+    }
+    
+    void test_method_(constructor_with_message_empty_and_stack_frame) {
+      auto stack_frame = current_stack_frame_;
+      auto e = io_exception {"", stack_frame};
+      assert::are_equal("xtd::io::io_exception", e.get_type().full_name(), csf_);
+      assert::is_empty(e.help_link(), csf_);
+      assert::are_equal(h_result::COR_E_IO, e.h_result(), csf_);
+      assert::are_equal(h_result::h_result_category(), e.error_code().category(), csf_);
+      assert::are_equal(e.h_result(), e.error_code().value(), csf_);
+      assert::is_false(e.inner_exception().has_value(), csf_);
+      assert::is_empty(e.message(), csf_);
+      assert::are_equal(path::get_file_name(assembly::get_executing_assembly().location()), e.source(), csf_);
+      assert::are_equal(stack_frame.to_string(), e.stack_trace(), csf_);
+      assert::are_equal(stack_frame.get_file_name(), e.get_last_stack_frame().get_file_name(), csf_);
+      assert::are_equal(stack_frame.get_file_line_number(), e.get_last_stack_frame().get_file_line_number(), csf_);
+      assert::are_equal(stack_frame.get_method(), e.get_last_stack_frame().get_method(), csf_);
+      assert::are_equal("xtd::io::io_exception" + environment::new_line() + stack_frame.to_string(), e.to_string(), csf_);
+      assert::are_equal("xtd::io::io_exception", e.what(), csf_);
+    }
+    
+    void test_method_(constructor_with_message) {
+      auto e = io_exception {"Test excpetion message."};
+      assert::are_equal("xtd::io::io_exception", e.get_type().full_name(), csf_);
+      assert::is_empty(e.help_link(), csf_);
+      assert::are_equal(h_result::COR_E_IO, e.h_result(), csf_);
+      assert::are_equal(h_result::h_result_category(), e.error_code().category(), csf_);
+      assert::are_equal(e.h_result(), e.error_code().value(), csf_);
+      assert::is_false(e.inner_exception().has_value(), csf_);
+      assert::are_equal("Test excpetion message.", e.message(), csf_);
+      assert::are_equal(path::get_file_name(assembly::get_executing_assembly().location()), e.source(), csf_);
+      assert::is_empty(e.stack_trace(), csf_);
+      assert::is_empty(e.get_last_stack_frame().get_file_name(), csf_);
+      assert::are_equal(0u, e.get_last_stack_frame().get_file_line_number(), csf_);
+      assert::is_empty(e.get_last_stack_frame().get_method(), csf_);
+      assert::are_equal("xtd::io::io_exception : Test excpetion message.", e.to_string(), csf_);
+      assert::are_equal("Test excpetion message.", e.what(), csf_);
+    }
+    
+    void test_method_(constructor_with_message_and_stack_frame) {
+      auto stack_frame = current_stack_frame_;
+      auto e = io_exception {"Test excpetion message.", stack_frame};
+      assert::are_equal("xtd::io::io_exception", e.get_type().full_name(), csf_);
+      assert::is_empty(e.help_link(), csf_);
+      assert::are_equal(h_result::COR_E_IO, e.h_result(), csf_);
+      assert::are_equal(h_result::h_result_category(), e.error_code().category(), csf_);
+      assert::are_equal(e.h_result(), e.error_code().value(), csf_);
+      assert::is_false(e.inner_exception().has_value(), csf_);
+      assert::are_equal("Test excpetion message.", e.message(), csf_);
+      assert::are_equal(path::get_file_name(assembly::get_executing_assembly().location()), e.source(), csf_);
+      assert::are_equal(stack_frame.to_string(), e.stack_trace(), csf_);
+      assert::are_equal(stack_frame.get_file_name(), e.get_last_stack_frame().get_file_name(), csf_);
+      assert::are_equal(stack_frame.get_file_line_number(), e.get_last_stack_frame().get_file_line_number(), csf_);
+      assert::are_equal(stack_frame.get_method(), e.get_last_stack_frame().get_method(), csf_);
+      assert::are_equal("xtd::io::io_exception : Test excpetion message." + environment::new_line() + stack_frame.to_string(), e.to_string(), csf_);
+      assert::are_equal("Test excpetion message.", e.what(), csf_);
+    }
+    
+    void test_method_(constructor_with_message_and_inner_exception) {
+      auto inner_exception = argument_exception {};
+      auto e = io_exception {"Test excpetion message.", inner_exception};
+      assert::is_empty(e.help_link(), csf_);
+      assert::are_equal(h_result::COR_E_IO, e.h_result(), csf_);
+      assert::are_equal(h_result::h_result_category(), e.error_code().category(), csf_);
+      assert::are_equal(e.h_result(), e.error_code().value(), csf_);
+      assert::is_true(e.inner_exception().has_value(), csf_);
+      assert::is_instance_of<xtd::argument_exception>(e.inner_exception().value().get(), csf_);
+      assert::are_equal(inner_exception.to_string(), e.inner_exception().value().get().to_string(), csf_);
+      assert::are_equal("Test excpetion message.", e.message(), csf_);
+      assert::are_equal(path::get_file_name(assembly::get_executing_assembly().location()), e.source(), csf_);
+      assert::is_empty(e.stack_trace(), csf_);
+      assert::is_empty(e.get_last_stack_frame().get_file_name(), csf_);
+      assert::are_equal(0u, e.get_last_stack_frame().get_file_line_number(), csf_);
+      assert::is_empty(e.get_last_stack_frame().get_method(), csf_);
+      assert::are_equal("xtd::io::io_exception : Test excpetion message.", e.to_string(), csf_);
+      assert::are_equal("Test excpetion message.", e.what(), csf_);
+    }
+    
+    void test_method_(constructor_with_message_inner_exception_and_stack_frame) {
+      auto inner_exception = argument_exception {};
+      auto stack_frame = current_stack_frame_;
+      auto e = io_exception {"Test excpetion message.", inner_exception, stack_frame};
+      assert::are_equal("xtd::io::io_exception", e.get_type().full_name(), csf_);
+      assert::is_empty(e.help_link(), csf_);
+      assert::are_equal(h_result::COR_E_IO, e.h_result(), csf_);
+      assert::are_equal(h_result::h_result_category(), e.error_code().category(), csf_);
+      assert::are_equal(e.h_result(), e.error_code().value(), csf_);
+      assert::is_true(e.inner_exception().has_value(), csf_);
+      assert::is_instance_of<xtd::argument_exception>(e.inner_exception().value().get(), csf_);
+      assert::are_equal(inner_exception.to_string(), e.inner_exception().value().get().to_string(), csf_);
+      assert::are_equal("Test excpetion message.", e.message(), csf_);
+      assert::are_equal(path::get_file_name(assembly::get_executing_assembly().location()), e.source(), csf_);
+      assert::are_equal(stack_frame.to_string(), e.stack_trace(), csf_);
+      assert::are_equal(stack_frame.get_file_name(), e.get_last_stack_frame().get_file_name(), csf_);
+      assert::are_equal(stack_frame.get_file_line_number(), e.get_last_stack_frame().get_file_line_number(), csf_);
+      assert::are_equal(stack_frame.get_method(), e.get_last_stack_frame().get_method(), csf_);
+      assert::are_equal("xtd::io::io_exception : Test excpetion message." + environment::new_line() + stack_frame.to_string(), e.to_string(), csf_);
+      assert::are_equal("Test excpetion message.", e.what(), csf_);
     }
   };
 }
