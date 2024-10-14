cc7915344556a65c6b85098ccf332a71369cf0b9
Gammasoft <gammasoft71@gmail.com>
2024-10-14 20:41:17 +0200

    Add end_of_stream_exception unit tests

--- tests/xtd.core.unit_tests/src/xtd/io/tests/end_of_stream_exception_tests.cpp
+++ tests/xtd.core.unit_tests/src/xtd/io/tests/end_of_stream_exception_tests.cpp
@@ -64,6 +64,43 @@ namespace xtd::io::tests {
       assert::are_equal("Attempted to read past the end of the stream.", e.what(), csf_);
     }
     
+    void test_method_(constructor_with_nullopt_message) {
+      auto e = end_of_stream_exception {nullopt};
+      assert::are_equal("xtd::io::end_of_stream_exception", e.get_type().full_name(), csf_);
+      assert::is_empty(e.help_link(), csf_);
+      assert::are_equal(h_result::COR_E_ENDOFSTREAM, e.h_result(), csf_);
+      assert::are_equal(h_result::h_result_category(), e.error_code().category(), csf_);
+      assert::are_equal(e.h_result(), e.error_code().value(), csf_);
+      assert::is_null(e.inner_exception(), csf_);
+      assert::are_equal("Attempted to read past the end of the stream.", e.message(), csf_);
+      assert::are_equal(path::get_file_name(assembly::get_executing_assembly().location()), e.source(), csf_);
+      assert::is_empty(e.stack_trace(), csf_);
+      assert::is_empty(e.get_last_stack_frame().get_file_name(), csf_);
+      assert::are_equal(0u, e.get_last_stack_frame().get_file_line_number(), csf_);
+      assert::is_empty(e.get_last_stack_frame().get_method(), csf_);
+      assert::are_equal("xtd::io::end_of_stream_exception : Attempted to read past the end of the stream.", e.to_string(), csf_);
+      assert::are_equal("Attempted to read past the end of the stream.", e.what(), csf_);
+    }
+    
+    void test_method_(constructor_with_nullopt_message_and_stack_frame) {
+      auto stack_frame = current_stack_frame_;
+      auto e = end_of_stream_exception {nullopt, stack_frame};
+      assert::are_equal("xtd::io::end_of_stream_exception", e.get_type().full_name(), csf_);
+      assert::is_empty(e.help_link(), csf_);
+      assert::are_equal(h_result::COR_E_ENDOFSTREAM, e.h_result(), csf_);
+      assert::are_equal(h_result::h_result_category(), e.error_code().category(), csf_);
+      assert::are_equal(e.h_result(), e.error_code().value(), csf_);
+      assert::is_null(e.inner_exception(), csf_);
+      assert::are_equal("Attempted to read past the end of the stream.", e.message(), csf_);
+      assert::are_equal(path::get_file_name(assembly::get_executing_assembly().location()), e.source(), csf_);
+      assert::are_equal(stack_frame.to_string(), e.stack_trace(), csf_);
+      assert::are_equal(stack_frame.get_file_name(), e.get_last_stack_frame().get_file_name(), csf_);
+      assert::are_equal(stack_frame.get_file_line_number(), e.get_last_stack_frame().get_file_line_number(), csf_);
+      assert::are_equal(stack_frame.get_method(), e.get_last_stack_frame().get_method(), csf_);
+      assert::are_equal("xtd::io::end_of_stream_exception : Attempted to read past the end of the stream." + environment::new_line() + stack_frame.to_string(), e.to_string(), csf_);
+      assert::are_equal("Attempted to read past the end of the stream.", e.what(), csf_);
+    }
+
     void test_method_(constructor_with_empty_message) {
       auto e = end_of_stream_exception {""};
       assert::are_equal("xtd::io::end_of_stream_exception", e.get_type().full_name(), csf_);
@@ -138,6 +175,47 @@ namespace xtd::io::tests {
       assert::are_equal("Test excpetion message.", e.what(), csf_);
     }
     
+    void test_method_(constructor_with_nullopt_message_and_inner_exception) {
+      auto inner_exception = argument_exception {};
+      auto e = end_of_stream_exception {nullopt, inner_exception};
+      assert::are_equal("xtd::io::end_of_stream_exception", e.get_type().full_name(), csf_);
+      assert::is_empty(e.help_link(), csf_);
+      assert::are_equal(h_result::COR_E_ENDOFSTREAM, e.h_result(), csf_);
+      assert::are_equal(h_result::h_result_category(), e.error_code().category(), csf_);
+      assert::are_equal(e.h_result(), e.error_code().value(), csf_);
+      assert::is_not_null(e.inner_exception(), csf_);
+      assert::is_instance_of<xtd::argument_exception>(e.inner_exception().value().get(), csf_);
+      assert::are_equal("Attempted to read past the end of the stream.", e.message(), csf_);
+      assert::are_equal(path::get_file_name(assembly::get_executing_assembly().location()), e.source(), csf_);
+      assert::is_empty(e.stack_trace(), csf_);
+      assert::is_empty(e.get_last_stack_frame().get_file_name(), csf_);
+      assert::are_equal(0u, e.get_last_stack_frame().get_file_line_number(), csf_);
+      assert::is_empty(e.get_last_stack_frame().get_method(), csf_);
+      assert::are_equal("xtd::io::end_of_stream_exception : Attempted to read past the end of the stream.", e.to_string(), csf_);
+      assert::are_equal("Attempted to read past the end of the stream.", e.what(), csf_);
+    }
+    
+    void test_method_(constructor_with_nullopt_message_and_stack_frame_inner_exception) {
+      auto inner_exception = argument_exception {};
+      auto stack_frame = current_stack_frame_;
+      auto e = end_of_stream_exception {nullopt, inner_exception, stack_frame};
+      assert::are_equal("xtd::io::end_of_stream_exception", e.get_type().full_name(), csf_);
+      assert::is_empty(e.help_link(), csf_);
+      assert::are_equal(h_result::COR_E_ENDOFSTREAM, e.h_result(), csf_);
+      assert::are_equal(h_result::h_result_category(), e.error_code().category(), csf_);
+      assert::are_equal(e.h_result(), e.error_code().value(), csf_);
+      assert::is_not_null(e.inner_exception(), csf_);
+      assert::is_instance_of<xtd::argument_exception>(e.inner_exception().value().get(), csf_);
+      assert::are_equal("Attempted to read past the end of the stream.", e.message(), csf_);
+      assert::are_equal(path::get_file_name(assembly::get_executing_assembly().location()), e.source(), csf_);
+      assert::are_equal(stack_frame.to_string(), e.stack_trace(), csf_);
+      assert::are_equal(stack_frame.get_file_name(), e.get_last_stack_frame().get_file_name(), csf_);
+      assert::are_equal(stack_frame.get_file_line_number(), e.get_last_stack_frame().get_file_line_number(), csf_);
+      assert::are_equal(stack_frame.get_method(), e.get_last_stack_frame().get_method(), csf_);
+      assert::are_equal("xtd::io::end_of_stream_exception : Attempted to read past the end of the stream." + environment::new_line() + stack_frame.to_string(), e.to_string(), csf_);
+      assert::are_equal("Attempted to read past the end of the stream.", e.what(), csf_);
+    }
+
     void test_method_(constructor_with_message_and_inner_exception) {
       auto inner_exception = argument_exception {};
       auto e = end_of_stream_exception {"Test excpetion message.", inner_exception};
