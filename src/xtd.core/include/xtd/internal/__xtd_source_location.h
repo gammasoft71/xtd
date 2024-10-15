71a5e3da2c7644f5eae321a6221926b4ab3b7c42
Gammasoft <gammasoft71@gmail.com>
2024-10-15 15:44:15 +0200

    Add specific macros

--- src/xtd.core/include/xtd/internal/__xtd_source_location.h
+++ src/xtd.core/include/xtd/internal/__xtd_source_location.h
@@ -16,6 +16,13 @@
 #include <source_location>
 using __xtd_source_location__ = std::source_location;
 #else
+#include <cstdint>
+
+#define __XTD_FILE__ __builtin_FILE()
+#define __XTD_LINE__ __builtin_LINE()
+#define __XTD_FUNC__ __builtin_FUNCTION()
+#define __XTD_COLUMN__ __builtin_COLUMN()
+
 class __xtd_source_location__ {
 public:
   const char* file_name() const noexcept {return data_.file;}
@@ -23,7 +30,7 @@ public:
   const char* function_name() const noexcept {return data_.func;}
   uint32_t column() const noexcept {return data_.column;}
   
-  static __xtd_source_location__ current(const char* file = __builtin_FILE(), uint32_t line = __builtin_LINE(), const char* func = __builtin_FUNCTION(), uint32_t column = __builtin_COLUMN()) noexcept {
+  static __xtd_source_location__ current(const char* file = __XTD_FILE__, uint32_t line = __XTD_LINE__, const char* func = __XTD_FUNC__, uint32_t column = __XTD_COLUMN__) noexcept {
     auto csl = __xtd_source_location__ {};
     csl.data_ = {file, line, func, column};
     return csl;
@@ -37,5 +44,10 @@ private:
     uint32_t column;
   } data_;
 };
+
+#undef __XTD_FILE__
+#undef __XTD_LINE__
+#undef __XTD_FUNC__
+#undef __XTD_COLUMN__
 #endif
 /// @endcond
