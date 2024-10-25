b255904f0454888d8e12ddf57b9c19f24acc386f
Gammasoft <gammasoft71@gmail.com>
2024-10-25 21:49:31 +0200

    revue demangle usage

--- src/xtd.core.native.macos/src/xtd/native/macos/types.mm
+++ src/xtd.core.native.macos/src/xtd/native/macos/types.mm
@@ -1,20 +1,24 @@
-#include <cxxabi.h>
 #define __XTD_CORE_NATIVE_LIBRARY__
 #include <xtd/native/types>
 #include "../../../../include/xtd/native/macos/semaphore.h"
 #include "../../../../include/xtd/native/macos/strings.h"
 #undef __XTD_CORE_NATIVE_LIBRARY__
+#include <cxxabi.h>
 
 using namespace abi;
 using namespace std;
 using namespace xtd::native;
 
-string types::demangle(const string& name) {
+string __xtd_abi_demangle(const string& name) {
   auto status = 0;
   auto demangled_name = __cxa_demangle(name.c_str(), nullptr, 0, &status);
   auto result = xtd::native::macos::strings::replace(status == 0 && demangled_name ? demangled_name : name, "std::__1::", "std::");
   free(demangled_name);
   return result;
+}
+
+string types::demangle(const string& name) {
+  return __xtd_abi_demangle(name);
 }
 
 intmax_t types::invalid_handle() noexcept {
