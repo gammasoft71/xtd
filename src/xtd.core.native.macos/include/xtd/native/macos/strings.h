e0a4ee70cbe1225087346fd87640c2ccb68eedac
Gammasoft <gammasoft71@gmail.com>
2024-10-27 20:33:48 +0100

    Review native strings

--- src/xtd.core.native.macos/include/xtd/native/macos/strings.h
+++ src/xtd.core.native.macos/include/xtd/native/macos/strings.h
@@ -31,8 +31,8 @@ namespace xtd::native::macos {
     
     template<typename separator_t, typename collection_t>
     static std::string join(const separator_t& separator, const collection_t& values, size_t index, size_t count) noexcept {
-      size_t i = 0;
-      std::stringstream ss;
+      auto i = size_t {};
+      auto ss = std::stringstream {};
       for (const auto& item : values) {
         if (i >= index) {
           if (i != index) ss << separator;
@@ -60,12 +60,12 @@ namespace xtd::native::macos {
     }
     
     static size_t last_index_of(const std::string& str, char value, size_t start_index, size_t count) noexcept {
-      size_t result = str.rfind(value, start_index + count - 1);
+      auto result = str.rfind(value, start_index + count - 1);
       return result < start_index ? str.npos : result;
     }
     
     static size_t last_index_of(const std::string& str, const std::string& value, size_t start_index, size_t count) noexcept {
-      size_t result = str.rfind(value, start_index + count - value.size());
+      auto result = str.rfind(value, start_index + count - value.size());
       return result < start_index ? str.npos : result;
     }
     
@@ -75,13 +75,13 @@ namespace xtd::native::macos {
     
     static std::string remove(const std::string& str, size_t start_index, size_t count) noexcept {
       if (start_index > str.size()) return str;
-      std::string result(str);
+      auto result = str;
       return result.erase(start_index, count);
     }
     
     static std::string replace(const std::string& str, const std::string& old_string, const std::string& new_string) noexcept {
-      std::string result(str);
-      size_t index = 0;
+      auto result = str;
+      auto index = size_t {};
       while (true) {
         index = result.find(old_string, index);
         if (index == std::string::npos) break;
@@ -96,45 +96,45 @@ namespace xtd::native::macos {
       if (count == 0) return {};
       if (count == 1) return {str};
       
-      std::vector<std::string> list;
-      std::string subString;
-      std::vector<char> split_char_separators = separators.size() == 0 ? std::vector<char> {9, 10, 11, 12, 13, 32} : separators;
-      for (std::string::const_iterator it = str.begin(); it != str.end(); ++it) {
-        bool is_separator = std::find(split_char_separators.begin(), split_char_separators.end(), *it) != split_char_separators.end();
-        if (!is_separator) subString.append(std::string(1, *it));
-        if ((static_cast<size_t>(it - str.begin()) == str.length() - 1 || is_separator) && (subString.length() > 0 || (subString.length() == 0  && !remove_empty_entries))) {
+      auto list = std::vector<std::string> {};
+      auto sub_string = std::string {};
+      auto split_char_separators = separators.size() == 0 ? std::vector<char> {9, 10, 11, 12, 13, 32} : separators;
+      for (auto it = str.begin(); it != str.end(); ++it) {
+        auto is_separator = std::find(split_char_separators.begin(), split_char_separators.end(), *it) != split_char_separators.end();
+        if (!is_separator) sub_string.append(std::string(1, *it));
+        if ((static_cast<size_t>(it - str.begin()) == str.length() - 1 || is_separator) && (sub_string.length() > 0 || (sub_string.length() == 0  && !remove_empty_entries))) {
           if (list.size() == count - 1) {
-            list.push_back(subString + std::string(str.c_str(), it - str.begin() + (is_separator ? 0 : 1), str.length() - (it - str.begin()) + (is_separator ? 0 : 1)));
+            list.push_back(sub_string + std::string(str.c_str(), it - str.begin() + (is_separator ? 0 : 1), str.length() - (it - str.begin()) + (is_separator ? 0 : 1)));
             return list;
           }
-          list.push_back(subString);
-          subString.clear();
+          list.push_back(sub_string);
+          sub_string.clear();
         }
       }
       
       return list;
     }
     
-    static std::string substring(const std::string& str, size_t start_index, size_t length) noexcept {
+    static std::string sub_string(const std::string& str, size_t start_index, size_t length) noexcept {
       if (start_index >= str.size()) return "";
       return str.substr(start_index, length);
     }
     
     static const std::string to_lower(const std::string& str) noexcept {
-      std::string result = str;
+      auto result = str;
       std::transform(result.begin(), result.end(), result.begin(), ::tolower);
       return result;
     }
     
     static const std::string to_upper(const std::string& str) noexcept {
-      std::string result = str;
+      auto result = str;
       std::transform(result.begin(), result.end(), result.begin(), ::toupper);
       return result;
     }
 
     static std::string trim_end(const std::string& str, const std::vector<char>& trim_chars) noexcept {
       if (!str.size()) return str;
-      std::string result(str);
+      auto result = str;
       while (std::find(trim_chars.begin(), trim_chars.end(), result[result.size() - 1]) != trim_chars.end())
         result.erase(result.size() - 1, 1);
       return result;
@@ -148,7 +148,5 @@ namespace xtd::native::macos {
         return false;
       }
     }
-    
   };
 }
-
