6c78b08c48f10c4f0b0b79c8c8586360d52d34e0
Gammasoft <gammasoft71@gmail.com>
2024-10-27 20:33:48 +0100

    Review native strings

--- src/xtd.core.native.linux/include/xtd/native/linux/strings.h
+++ src/xtd.core.native.linux/include/xtd/native/linux/strings.h
@@ -17,22 +17,24 @@ namespace xtd::native::linux {
   class strings final {
   public:
     strings() = delete;
-
+    
+    inline static const std::string empty_string;
+    
     static bool contains(const std::string& str, const std::string& value) noexcept {
       return str.find(value) != str.npos;
     }
-
+    
     static bool ends_with(const std::string& str, const std::string& value) noexcept {
       return str.rfind(value) + value.size() == str.size();
     }
-
+    
     template<typename separator_t, typename collection_t>
     static std::string join(const separator_t& separator, const collection_t& values) noexcept {return join(separator, values, 0, values.size());}
-
+    
     template<typename separator_t, typename collection_t>
     static std::string join(const separator_t& separator, const collection_t& values, size_t index, size_t count) noexcept {
-      size_t i = 0;
-      std::stringstream ss;
+      auto i = size_t {};
+      auto ss = std::stringstream {};
       for (const auto& item : values) {
         if (i >= index) {
           if (i != index) ss << separator;
@@ -42,46 +44,46 @@ namespace xtd::native::linux {
       }
       return ss.str();
     }
-
+    
     static size_t last_index_of(const std::string& str, char value) noexcept {
       return last_index_of(str, value, 0, str.size());
     }
-
+    
     static size_t last_index_of(const std::string& str, const std::string& value) noexcept {
       return last_index_of(str, value, 0, str.size());
     }
-
+    
     static size_t last_index_of(const std::string& str, char value, size_t start_index) noexcept {
       return last_index_of(str, value, start_index, str.size() - start_index);
     }
-
+    
     static size_t last_index_of(const std::string& str, const std::string& value, size_t start_index) noexcept {
       return last_index_of(str, value, start_index, str.size() - start_index);
     }
-
+    
     static size_t last_index_of(const std::string& str, char value, size_t start_index, size_t count) noexcept {
-      size_t result = str.rfind(value, start_index + count - 1);
+      auto result = str.rfind(value, start_index + count - 1);
       return result < start_index ? str.npos : result;
     }
-
+    
     static size_t last_index_of(const std::string& str, const std::string& value, size_t start_index, size_t count) noexcept {
-      size_t result = str.rfind(value, start_index + count - value.size());
+      auto result = str.rfind(value, start_index + count - value.size());
       return result < start_index ? str.npos : result;
     }
-
+    
     static std::string remove(const std::string& str, size_t start_index) noexcept {
       return remove(str, start_index, str.size() - start_index);
     }
-
+    
     static std::string remove(const std::string& str, size_t start_index, size_t count) noexcept {
       if (start_index > str.size()) return str;
-      std::string result(str);
+      auto result = str;
       return result.erase(start_index, count);
     }
-
+    
     static std::string replace(const std::string& str, const std::string& old_string, const std::string& new_string) noexcept {
-      std::string result(str);
-      size_t index = 0;
+      auto result = str;
+      auto index = size_t {};
       while (true) {
         index = result.find(old_string, index);
         if (index == std::string::npos) break;
@@ -91,50 +93,55 @@ namespace xtd::native::linux {
       }
       return result;
     }
-
+    
     static std::vector<std::string> split(const std::string& str, const std::vector<char>& separators, size_t count = std::numeric_limits<size_t>::max(), bool remove_empty_entries = false) noexcept {
       if (count == 0) return {};
       if (count == 1) return {str};
-
-      std::vector<std::string> list;
-      std::string subString;
-      std::vector<char> split_char_separators = separators.size() == 0 ? std::vector<char> {9, 10, 11, 12, 13, 32} : separators;
-      for (std::string::const_iterator it = str.begin(); it != str.end(); ++it) {
-        bool is_separator =  std::find(split_char_separators.begin(), split_char_separators.end(), *it) != split_char_separators.end();
-        if (!is_separator) subString.append(std::string(1, *it));
-        if ((static_cast<size_t>(it - str.begin()) == str.length() - 1 || is_separator) && (subString.length() > 0 || (subString.length() == 0  && !remove_empty_entries))) {
+      
+      auto list = std::vector<std::string> {};
+      auto sub_string = std::string {};
+      auto split_char_separators = separators.size() == 0 ? std::vector<char> {9, 10, 11, 12, 13, 32} : separators;
+      for (auto it = str.begin(); it != str.end(); ++it) {
+        auto is_separator = std::find(split_char_separators.begin(), split_char_separators.end(), *it) != split_char_separators.end();
+        if (!is_separator) sub_string.append(std::string(1, *it));
+        if ((static_cast<size_t>(it - str.begin()) == str.length() - 1 || is_separator) && (sub_string.length() > 0 || (sub_string.length() == 0  && !remove_empty_entries))) {
           if (list.size() == count - 1) {
-            list.push_back(subString + std::string(str.c_str(), it - str.begin() + (is_separator ? 0 : 1), str.length() - (it - str.begin()) + (is_separator ? 0 : 1)));
+            list.push_back(sub_string + std::string(str.c_str(), it - str.begin() + (is_separator ? 0 : 1), str.length() - (it - str.begin()) + (is_separator ? 0 : 1)));
             return list;
           }
-          list.push_back(subString);
-          subString.clear();
+          list.push_back(sub_string);
+          sub_string.clear();
         }
       }
-
+      
       return list;
     }
-
-    static const std::string to_lower(const std::string& str) noexcept {
-      std::string result = str;
-      std::transform(result.begin(), result.end(), result.begin(), ::tolower);
-      return result;
-    }
-
-    static std::string substring(const std::string& str, size_t start_index, size_t length) noexcept {
+    
+    static std::string sub_string(const std::string& str, size_t start_index, size_t length) noexcept {
       if (start_index >= str.size()) return "";
       return str.substr(start_index, length);
     }
-
-
+    
+    static const std::string to_lower(const std::string& str) noexcept {
+      auto result = str;
+      std::transform(result.begin(), result.end(), result.begin(), ::tolower);
+      return result;
+    }
+    
+    static const std::string to_upper(const std::string& str) noexcept {
+      auto result = str;
+      std::transform(result.begin(), result.end(), result.begin(), ::toupper);
+      return result;
+    }
+    
     static std::string trim_end(const std::string& str, const std::vector<char>& trim_chars) noexcept {
       if (!str.size()) return str;
-      std::string result(str);
+      auto result = str;
       while (std::find(trim_chars.begin(), trim_chars.end(), result[result.size() - 1]) != trim_chars.end())
         result.erase(result.size() - 1, 1);
       return result;
     }
-
+    
     static bool try_parse(const std::string& str, int32_t& value) noexcept {
       try {
         value = std::atoi(str.c_str());
@@ -143,7 +150,6 @@ namespace xtd::native::linux {
         return false;
       }
     }
-
   };
 }
 
--- src/xtd.core.native.win32/include/xtd/native/win32/strings.h
+++ src/xtd.core.native.win32/include/xtd/native/win32/strings.h
@@ -10,52 +10,77 @@
 #include <vector>
 
 namespace xtd::native {
-  class console;
-  class cryptography;
-  class debugger;
-  class directory;
-  class drive;
-  class environment;
-  class file;
-  class file_system;
-  class named_event_wait_handle;
-  class named_mutex;
-  class named_semaphore;
-  class path;
-  class process;
-  class thread;
-  class translator;
   namespace win32 {
     class strings final {
-      friend xtd::native::console;
-      friend xtd::native::cryptography;
-      friend xtd::native::debugger;
-      friend xtd::native::directory;
-      friend xtd::native::drive;
-      friend xtd::native::environment;
-      friend xtd::native::file;
-      friend xtd::native::file_system;
-      friend xtd::native::named_event_wait_handle;
-      friend xtd::native::named_mutex;
-      friend xtd::native::named_semaphore;
-      friend xtd::native::path;
-      friend xtd::native::process;
-      friend xtd::native::thread;
-      friend xtd::native::translator;
-    protected:
+    public:
       strings() = delete;
+      
+      inline static const std::string empty_string;
       
       static bool contains(const std::string& str, const std::string& value) noexcept {
         return str.find(value) != str.npos;
       }
       
       static bool ends_with(const std::string& str, const std::string& value) noexcept {
-        return to_lower(str).rfind(to_lower(value)) + to_lower(value).size() == str.size();
+        return str.rfind(value) + value.size() == str.size();
+      }
+      
+      template<typename separator_t, typename collection_t>
+      static std::string join(const separator_t& separator, const collection_t& values) noexcept {return join(separator, values, 0, values.size());}
+      
+      template<typename separator_t, typename collection_t>
+      static std::string join(const separator_t& separator, const collection_t& values, size_t index, size_t count) noexcept {
+        auto i = size_t {};
+        auto ss = std::stringstream {};
+        for (const auto& item : values) {
+          if (i >= index) {
+            if (i != index) ss << separator;
+            ss << item;
+          }
+          if (++i >= index + count) break;
+        }
+        return ss.str();
+      }
+      
+      static size_t last_index_of(const std::string& str, char value) noexcept {
+        return last_index_of(str, value, 0, str.size());
+      }
+      
+      static size_t last_index_of(const std::string& str, const std::string& value) noexcept {
+        return last_index_of(str, value, 0, str.size());
+      }
+      
+      static size_t last_index_of(const std::string& str, char value, size_t start_index) noexcept {
+        return last_index_of(str, value, start_index, str.size() - start_index);
+      }
+      
+      static size_t last_index_of(const std::string& str, const std::string& value, size_t start_index) noexcept {
+        return last_index_of(str, value, start_index, str.size() - start_index);
+      }
+      
+      static size_t last_index_of(const std::string& str, char value, size_t start_index, size_t count) noexcept {
+        auto result = str.rfind(value, start_index + count - 1);
+        return result < start_index ? str.npos : result;
+      }
+      
+      static size_t last_index_of(const std::string& str, const std::string& value, size_t start_index, size_t count) noexcept {
+        auto result = str.rfind(value, start_index + count - value.size());
+        return result < start_index ? str.npos : result;
+      }
+      
+      static std::string remove(const std::string& str, size_t start_index) noexcept {
+        return remove(str, start_index, str.size() - start_index);
+      }
+      
+      static std::string remove(const std::string& str, size_t start_index, size_t count) noexcept {
+        if (start_index > str.size()) return str;
+        auto result = str;
+        return result.erase(start_index, count);
       }
       
       static std::string replace(const std::string& str, const std::string& old_string, const std::string& new_string) noexcept {
-        std::string result(str);
-        size_t index = 0;
+        auto result = str;
+        auto index = size_t {};
         while (true) {
           index = result.find(old_string, index);
           if (index == std::string::npos) break;
@@ -66,107 +91,55 @@ namespace xtd::native {
         return result;
       }
       
-      static std::vector<std::string> split(const std::string& str, const std::vector<char>& separators, bool remove_empty_entries = false) noexcept {
-        std::vector<std::string> list;
-        std::string subString;
-        std::vector<char> split_char_separators = separators.size() == 0 ? std::vector<char> {9, 10, 11, 12, 13, 32} : separators;
-        for (std::string::const_iterator it = str.begin(); it != str.end(); ++it) {
-          bool is_separator =  std::find(split_char_separators.begin(), split_char_separators.end(), *it) != split_char_separators.end();
-          if (!is_separator) subString.append(std::string(1, *it));
-          if ((static_cast<size_t>(it - str.begin()) == str.length() - 1 || is_separator) && (subString.length() > 0 || (subString.length() == 0  && !remove_empty_entries))) {
-            list.push_back(subString);
-            subString.clear();
+      static std::vector<std::string> split(const std::string& str, const std::vector<char>& separators, size_t count = std::numeric_limits<size_t>::max(), bool remove_empty_entries = false) noexcept {
+        if (count == 0) return {};
+        if (count == 1) return {str};
+        
+        auto list = std::vector<std::string> {};
+        auto sub_string = std::string {};
+        auto split_char_separators = separators.size() == 0 ? std::vector<char> {9, 10, 11, 12, 13, 32} : separators;
+        for (auto it = str.begin(); it != str.end(); ++it) {
+          auto is_separator = std::find(split_char_separators.begin(), split_char_separators.end(), *it) != split_char_separators.end();
+          if (!is_separator) sub_string.append(std::string(1, *it));
+          if ((static_cast<size_t>(it - str.begin()) == str.length() - 1 || is_separator) && (sub_string.length() > 0 || (sub_string.length() == 0  && !remove_empty_entries))) {
+            if (list.size() == count - 1) {
+              list.push_back(sub_string + std::string(str.c_str(), it - str.begin() + (is_separator ? 0 : 1), str.length() - (it - str.begin()) + (is_separator ? 0 : 1)));
+              return list;
+            }
+            list.push_back(sub_string);
+            sub_string.clear();
           }
         }
         
         return list;
       }
       
-      static std::string substring(const std::string& str, size_t start_index, size_t length) noexcept {
+      static std::string sub_string(const std::string& str, size_t start_index, size_t length) noexcept {
         if (start_index >= str.size()) return "";
         return str.substr(start_index, length);
       }
       
       static const std::string to_lower(const std::string& str) noexcept {
-        std::string result;
-        std::for_each(str.begin(), str.end(), [&](auto c) {result += static_cast<char>(std::tolower(c)); });
+        auto result = str;
+        std::transform(result.begin(), result.end(), result.begin(), ::tolower);
         return result;
       }
       
-      static std::string to_string(const std::wstring& str) { return to_string(str.c_str()); }
-      static std::string to_string(const wchar_t* str) {
-        std::string out;
-        unsigned int codepoint = 0;
-        while (*str != 0) {
-          wchar_t character = static_cast<wchar_t>(*str);
-          if (character >= 0xd800 && character <= 0xdbff)
-            codepoint = ((character - 0xd800) << 10) + 0x10000;
-          else {
-            if (character >= 0xdc00 && character <= 0xdfff)
-              codepoint |= character - 0xdc00;
-            else
-              codepoint = character;
-              
-            if (codepoint <= 0x7f)
-              out.append(1, static_cast<char>(codepoint));
-            else if (codepoint <= 0x7ff) {
-              out.append(1, static_cast<char>(0xc0 | ((codepoint >> 6) & 0x1f)));
-              out.append(1, static_cast<char>(0x80 | (codepoint & 0x3f)));
-            } else if (codepoint <= 0xffff) {
-              out.append(1, static_cast<char>(0xe0 | ((codepoint >> 12) & 0x0f)));
-              out.append(1, static_cast<char>(0x80 | ((codepoint >> 6) & 0x3f)));
-              out.append(1, static_cast<char>(0x80 | (codepoint & 0x3f)));
-            } else {
-              out.append(1, static_cast<char>(0xf0 | ((codepoint >> 18) & 0x07)));
-              out.append(1, static_cast<char>(0x80 | ((codepoint >> 12) & 0x3f)));
-              out.append(1, static_cast<char>(0x80 | ((codepoint >> 6) & 0x3f)));
-              out.append(1, static_cast<char>(0x80 | (codepoint & 0x3f)));
-            }
-            codepoint = 0;
-          }
-          ++str;
-        }
-        return out;
-      }
-
       static const std::string to_upper(const std::string& str) noexcept {
-        std::string result;
-        std::for_each(str.begin(), str.end(), [&](auto c) {result += static_cast<char>(std::toupper(c)); });
+        auto result = str;
+        std::transform(result.begin(), result.end(), result.begin(), ::toupper);
         return result;
-      }
-
-      static std::wstring to_wstring(const std::string& str) { return to_wstring(str.c_str()); }
-      static std::wstring to_wstring(const char* str) {
-        std::wstring out;
-        char32_t codepoint = 0;
-        while (*str != 0) {
-          unsigned char ch = static_cast<unsigned char>(*str);
-          if (ch <= 0x7f) codepoint = ch;
-          else if (ch <= 0xbf) codepoint = (codepoint << 6) | (ch & 0x3f);
-          else if (ch <= 0xdf) codepoint = ch & 0x1f;
-          else if (ch <= 0xef) codepoint = ch & 0x0f;
-          else codepoint = ch & 0x07;
-          ++str;
-          if (((*str & 0xc0) != 0x80) && (codepoint <= 0x10ffff)) {
-            if (codepoint > 0xffff) {
-              out.append(1, static_cast<wchar_t>(0xd800 + (static_cast<char16_t>(codepoint) >> 10)));
-              out.append(1, static_cast<wchar_t>(0xdc00 + (static_cast<char16_t>(codepoint) & 0x03ff)));
-            } else if (codepoint < 0xd800 || codepoint >= 0xe000)
-              out.append(1, static_cast<wchar_t>(codepoint));
-          }
-        }
-        return out;
       }
       
       static std::string trim_end(const std::string& str, const std::vector<char>& trim_chars) noexcept {
         if (!str.size()) return str;
-        std::string result(str);
+        auto result = str;
         while (std::find(trim_chars.begin(), trim_chars.end(), result[result.size() - 1]) != trim_chars.end())
           result.erase(result.size() - 1, 1);
         return result;
       }
       
-      static bool try_parse(const std::string& str, int32_t& value) {
+      static bool try_parse(const std::string& str, int32_t& value) noexcept {
         try {
           value = std::atoi(str.c_str());
           return true;
