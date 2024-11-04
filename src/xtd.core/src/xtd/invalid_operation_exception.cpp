8cd6c6e2d42489613b7f9114ebc7ad484d2ce07a
Gammasoft <gammasoft71@gmail.com>
2024-11-04 09:49:11 +0100

    Revue invalid_operation_exception exception

---
+++ src/xtd.core/src/xtd/invalid_operation_exception.cpp
@@ -0,0 +1,49 @@
+#include "../../include/xtd/invalid_operation_exception.h"
+#include "../../include/xtd/literals.h"
+
+using namespace xtd;
+using namespace xtd::diagnostics;
+
+invalid_operation_exception::invalid_operation_exception(const stack_frame& stack_frame) : system_exception(std::nullopt, stack_frame) {
+  error_code(h_result::make_error_code(h_result::COR_E_INVALIDOPERATION));
+}
+
+invalid_operation_exception::invalid_operation_exception(const std::optional<xtd::string>& message, const xtd::diagnostics::stack_frame& stack_frame) : system_exception(message, stack_frame) {
+  error_code(h_result::make_error_code(h_result::COR_E_INVALIDOPERATION));
+}
+
+invalid_operation_exception::invalid_operation_exception(const xtd::string& message, const std::error_code& error, const xtd::diagnostics::stack_frame& stack_frame) : system_exception(message, stack_frame) {
+  error_code(error);
+}
+
+invalid_operation_exception::invalid_operation_exception(const xtd::string& message, const xtd::string& help_link, const xtd::diagnostics::stack_frame& stack_frame) : system_exception(message, stack_frame) {
+  error_code(h_result::make_error_code(h_result::COR_E_INVALIDOPERATION));
+  this->help_link(help_link);
+}
+
+invalid_operation_exception::invalid_operation_exception(const xtd::string& message, const std::error_code& error, const xtd::string& help_link, const xtd::diagnostics::stack_frame& stack_frame) : system_exception(message, stack_frame) {
+  error_code(error);
+  this->help_link(help_link);
+}
+
+invalid_operation_exception::invalid_operation_exception(const std::exception& inner_exception, const xtd::diagnostics::stack_frame& stack_frame) : system_exception(stack_frame) {
+  error_code(h_result::make_error_code(h_result::COR_E_INVALIDOPERATION));
+}
+
+invalid_operation_exception::invalid_operation_exception(const xtd::string& message, const std::exception& inner_exception, const xtd::diagnostics::stack_frame& stack_frame) : system_exception(message, stack_frame) {
+  error_code(h_result::make_error_code(h_result::COR_E_INVALIDOPERATION));
+}
+
+invalid_operation_exception::invalid_operation_exception(const xtd::string& message, const std::exception& inner_exception, const std::error_code& error, const xtd::diagnostics::stack_frame& stack_frame) : system_exception(message, stack_frame) {
+  error_code(error);
+}
+
+invalid_operation_exception::invalid_operation_exception(const xtd::string& message, const std::exception& inner_exception, const xtd::string& help_link, const xtd::diagnostics::stack_frame& stack_frame) : system_exception(message, stack_frame) {
+  error_code(h_result::make_error_code(h_result::COR_E_INVALIDOPERATION));
+  this->help_link(help_link);
+}
+
+invalid_operation_exception::invalid_operation_exception(const xtd::string& message, const std::exception& inner_exception, const std::error_code& error, const xtd::string& help_link, const xtd::diagnostics::stack_frame& stack_frame) : system_exception(message, stack_frame) {
+  error_code(error);
+  this->help_link(help_link);
+}
