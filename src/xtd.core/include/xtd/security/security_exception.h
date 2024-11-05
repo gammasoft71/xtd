bd7eb49f89c9f24a3cbe91e6bc667175dacc1c21
Gammasoft <gammasoft71@gmail.com>
2024-11-05 15:31:00 +0100

    Review security_exception exception

--- src/xtd.core/include/xtd/security/security_exception.h
+++ src/xtd.core/include/xtd/security/security_exception.h
@@ -30,64 +30,23 @@ namespace xtd {
       /// @brief Create a new instance of class security_exception
       /// @param stack_frame (optional) Contains current stack frame about member name, file path and  line number in the file where the exception is occurred. Typically  xtd::diagnostics::stack_frame::current().
       /// @remarks Message is set with the default message associate to the exception.
-      explicit security_exception(const xtd::diagnostics::stack_frame& stack_frame = xtd::diagnostics::stack_frame::current()) : system_exception(default_message(), stack_frame) {}
+      explicit security_exception(const xtd::diagnostics::stack_frame& stack_frame = xtd::diagnostics::stack_frame::current());
       /// @brief Create a new instance of class security_exception
       /// @param message Message string associate to the exception.
       /// @param stack_frame (optional) Contains current stack frame about member name, file path and  line number in the file where the exception is occurred. Typically  xtd::diagnostics::stack_frame::current().
-      explicit security_exception(const xtd::string& message, const xtd::diagnostics::stack_frame& stack_frame = xtd::diagnostics::stack_frame::current()) : system_exception(message, stack_frame) {}
-      /// @brief Create a new instance of class security_exception
-      /// @param message Message string associate to the exception.
-      /// @param error Error code associate to the exception.
-      /// @param stack_frame (optional) Contains current stack frame about member name, file path and  line number in the file where the exception is occurred. Typically  xtd::diagnostics::stack_frame::current().
-      explicit security_exception(const xtd::string& message, const std::error_code& error, const xtd::diagnostics::stack_frame& stack_frame = xtd::diagnostics::stack_frame::current()) : system_exception(message, error, stack_frame) {}
-      /// @brief Create a new instance of class security_exception
-      /// @param message Message string associate to the exception.
-      /// @param help_link Help link string associate to the exception.
-      /// @param stack_frame (optional) Contains current stack frame about member name, file path and  line number in the file where the exception is occurred. Typically  xtd::diagnostics::stack_frame::current().
-      explicit security_exception(const xtd::string& message, const xtd::string& help_link, const xtd::diagnostics::stack_frame& stack_frame = xtd::diagnostics::stack_frame::current()) : system_exception(message, help_link, stack_frame) {}
-      /// @brief Create a new instance of class security_exception
-      /// @param message Message string associate to the exception.
-      /// @param error Error code associate to the exception.
-      /// @param help_link Help link string associate to the exception.
-      explicit security_exception(const xtd::string& message, const std::error_code& error, const xtd::string& help_link, const xtd::diagnostics::stack_frame& stack_frame = xtd::diagnostics::stack_frame::current()) : system_exception(message, error, help_link, stack_frame) {}
-      /// @brief Create a new instance of class security_exception
-      /// @param inner_exception The exception that is the cause of the current exception.
-      /// @param stack_frame (optional) Contains current stack frame about member name, file path and  line number in the file where the exception is occurred. Typically  xtd::diagnostics::stack_frame::current().
-      /// @remarks Message is set with the default message associate to the exception.
-      explicit security_exception(const std::exception& inner_exception, const xtd::diagnostics::stack_frame& stack_frame = xtd::diagnostics::stack_frame::current()) : system_exception(default_message(), inner_exception, stack_frame) {}
+      explicit security_exception(const std::optional<xtd::string>& message, const xtd::diagnostics::stack_frame& stack_frame = xtd::diagnostics::stack_frame::current());
       /// @brief Create a new instance of class security_exception
       /// @param message Message string associate to the exception.
       /// @param inner_exception The exception that is the cause of the current exception.
       /// @param stack_frame (optional) Contains current stack frame about member name, file path and  line number in the file where the exception is occurred. Typically  xtd::diagnostics::stack_frame::current().
-      explicit security_exception(const xtd::string& message, const std::exception& inner_exception, const xtd::diagnostics::stack_frame& stack_frame = xtd::diagnostics::stack_frame::current()) : system_exception(message, inner_exception, stack_frame) {}
-      /// @brief Create a new instance of class security_exception
-      /// @param message Message string associate to the exception.
-      /// @param inner_exception The exception that is the cause of the current exception.
-      /// @param error Error code associate to the exception.
-      /// @param stack_frame (optional) Contains current stack frame about member name, file path and  line number in the file where the exception is occurred. Typically  xtd::diagnostics::stack_frame::current().
-      explicit security_exception(const xtd::string& message, const std::exception& inner_exception, const std::error_code& error, const xtd::diagnostics::stack_frame& stack_frame = xtd::diagnostics::stack_frame::current()) : system_exception(message, inner_exception, error, stack_frame) {}
-      /// @brief Create a new instance of class security_exception
-      /// @param message Message string associate to the exception.
-      /// @param inner_exception The exception that is the cause of the current exception.
-      /// @param help_link Help link string associate to the exception.
-      /// @param stack_frame (optional) Contains current stack frame about member name, file path and  line number in the file where the exception is occurred. Typically  xtd::diagnostics::stack_frame::current().
-      explicit security_exception(const xtd::string& message, const std::exception& inner_exception, const xtd::string& help_link, const xtd::diagnostics::stack_frame& stack_frame = xtd::diagnostics::stack_frame::current()) : system_exception(message, inner_exception, help_link, stack_frame) {}
-      /// @brief Create a new instance of class security_exception
-      /// @param message Message string associate to the exception.
-      /// @param inner_exception The exception that is the cause of the current exception.
-      /// @param error Error code associate to the exception.
-      /// @param help_link Help link string associate to the exception.
-      /// @param stack_frame (optional) Contains current stack frame about member name, file path and  line number in the file where the exception is occurred. Typically  xtd::diagnostics::stack_frame::current().
-      explicit security_exception(const xtd::string& message, const std::exception& inner_exception, const std::error_code& error, const xtd::string& help_link, const xtd::diagnostics::stack_frame& stack_frame = xtd::diagnostics::stack_frame::current()) : system_exception(message, inner_exception, error, help_link, stack_frame) {}
+      template<typename exception_t>
+      security_exception(const std::optional<xtd::string>& message, const exception_t& inner_exception, const xtd::diagnostics::stack_frame& stack_frame = xtd::diagnostics::stack_frame::current()) : xtd::system_exception(message, inner_exception, stack_frame) {error_code(h_result::make_error_code(h_result::COR_E_SECURITY));}
       /// @}
       
       /// @cond
       security_exception(const security_exception&) = default;
       security_exception& operator =(const security_exception&) = default;
       /// @endcond
-      
-    private:
-      const char* default_message() const noexcept {return "Security error detected."_t;}
     };
   }
 }
---
+++ src/xtd.core/src/xtd/security/security_exception.cpp
@@ -0,0 +1,15 @@
+#include "../../../include/xtd/security/security_exception.h"
+#include "../../../include/xtd/literals.h"
+
+using namespace xtd;
+using namespace xtd::security;
+using namespace xtd::diagnostics;
+
+security_exception::security_exception(const stack_frame& stack_frame) : system_exception(std::nullopt, stack_frame) {
+  error_code(h_result::make_error_code(h_result::COR_E_SECURITY));
+}
+
+security_exception::security_exception(const std::optional<xtd::string>& message, const xtd::diagnostics::stack_frame& stack_frame) : system_exception(message, stack_frame) {
+  error_code(h_result::make_error_code(h_result::COR_E_SECURITY));
+}
+/// @}
