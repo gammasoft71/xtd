20e237fb5d9a916a6168bc6106cc473d155d89d8
Gammasoft <gammasoft71@gmail.com>
2024-12-04 15:13:02 +0100

    Fix messages

--- src/xtd.forms/include/xtd/forms/power_line_status.h
+++ src/xtd.forms/include/xtd/forms/power_line_status.h
@@ -3,8 +3,8 @@
 /// @copyright Copyright (c) 2024 Gammasoft. All rights reserved.
 #pragma once
 #if defined(_MSC_VER) && __cplusplus < 202302L
-#  pragma message("Deprecated : Replaced by #include </Users/yves/Projects/xtd/src/xtd.forms/include/xtd/forms/power_line_status.hpp> - Will be removed in version 0.4.0.")
+#  pragma message("Deprecated : Replaced by #include <xtd/forms/power_line_status.hpp> - Will be removed in version 0.4.0.")
 #else
-#  warning "Deprecated : Replaced by #include </Users/yves/Projects/xtd/src/xtd.forms/include/xtd/forms/power_line_status.hpp> - Will be removed in version 0.4.0."
+#  warning "Deprecated : Replaced by #include <xtd/forms/power_line_status.hpp> - Will be removed in version 0.4.0."
 #endif
 #include "power_line_status.hpp"
