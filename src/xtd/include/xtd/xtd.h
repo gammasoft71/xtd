acef6832d9cbc1373e67c14bedf08ff46c740268
Gammasoft <gammasoft71@gmail.com>
2024-12-04 20:38:31 +0100

    Create xtd.h

---
+++ src/xtd/include/xtd/xtd.h
@@ -0,0 +1,7 @@
+#pragma once
+#if defined(_MSC_VER) && __cplusplus < 202302L
+#  pragma message("Deprecated : Replaced by #include <xtd/xtd.hpp> - Will be removed in version 0.4.0.")
+#else
+#  warning "Deprecated : Replaced by #include <xtd/xtd.hpp> - Will be removed in version 0.4.0."
+#endif
+#include "xtd.hpp"
