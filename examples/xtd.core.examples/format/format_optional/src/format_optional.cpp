1db397284517ab99cb6909efb62f6c6bd258c3ab
Gammasoft <gammasoft71@gmail.com>
2024-12-19 20:05:05 +0100

    Review example

--- examples/xtd.core.examples/format/format_optional/src/format_optional.cpp
+++ examples/xtd.core.examples/format/format_optional/src/format_optional.cpp
@@ -7,13 +7,17 @@
 using namespace xtd;
 
 auto main() -> int {
-  console::out << format("{}", nullopt) << environment::new_line;
-  console::out << format("{}", optional<string> {}) << environment::new_line;
   console::out << format("{}", optional<string> {"string value"}) << environment::new_line;
+  console::out << format("{}", optional<string> {"string value"}.value()) << environment::new_line;
+  console::out << format("{}", optional<string> {}.value_or("fallback string")) << environment::new_line;
+  console::out << format("{}", optional<string> {}) << environment::new_line;
+  console::out << format("{}", nullopt) << environment::new_line;
 }
 
 // This code produces the following output :
 //
-// (null)
-// (null)
 // (string value)
+// string value
+// fallback string
+// (null)
+// (null)
