16369115dfccbe61c43ae11c3d1ab6d77e035f1e
Gammasoft <gammasoft71@gmail.com>
2024-12-21 12:16:50 +0100

    Add all with iterators

--- tests/xtd.core.unit_tests/src/xtd/linq/tests/enumerable_tests.cpp
+++ tests/xtd.core.unit_tests/src/xtd/linq/tests/enumerable_tests.cpp
@@ -12,7 +12,7 @@ using namespace xtd::tunit;
 namespace xtd::linq::tests {
   class test_class_(enumerable_tests) {
     void test_method_(aggregate_with_source_and_func) {
-      assert::are_equal(15, enumerable::aggregate(array {1, 2, 3, 4, 5}, [](int total, int next) {return total + next;}));
+      assert::are_equal(15, enumerable::aggregate(array {1, 2, 3, 4, 5}, [](int total, int next)->int {return total + next;}));
     }
 
     void test_method_(aggregate_with_source_seed_and_func) {
@@ -24,23 +24,30 @@ namespace xtd::linq::tests {
     }
     
     void test_method_(aggregate_with_iterators_and_func) {
-      auto s = std::vector {1, 2, 3, 4, 5};
+      auto s = array {1, 2, 3, 4, 5};
       assert::are_equal(15, enumerable::aggregate<int>(s.begin(), s.end(), [](int total, int next) {return total + next;}));
     }
     
     void test_method_(aggregate_with_iterators_seed_and_func) {
-      auto s = std::vector {1, 2, 3, 4, 5};
+      auto s = array {1, 2, 3, 4, 5};
       assert::are_equal(25, enumerable::aggregate(s.begin(), s.end(), 10, [](int total, int next) {return total + next;}));
     }
 
     void test_method_(aggregate_with_iterators_seed_func_and_result_selector) {
-      auto s = std::vector {1, 2, 3, 4, 5};
+      auto s = array {1, 2, 3, 4, 5};
       assert::are_equal(-25, enumerable::aggregate(s.begin(), s.end(), 10, [](int total, int next) {return total + next;}, [](int aggregated) {return -aggregated;}));
     }
 
     void test_method_(all_with_source_and_func) {
       assert::is_true(enumerable::all(array {2, 4, 6, 8, 10, 12}, [](int next) {return next % 2 == 0;}));
       assert::is_false(enumerable::all(array {1, 2, 3, 4, 5}, [](int next) {return next % 2 == 0;}));
+    }
+
+    void test_method_(all_with_iterators_and_func) {
+      auto s1 = array {2, 4, 6, 8, 10, 12};
+      assert::is_true(enumerable::all<int>(s1.begin(), s1.end(), [](int next) {return next % 2 == 0;}));
+      auto s2 = array {1, 2, 3, 4, 5};
+      assert::is_false(enumerable::all<int>(s2.begin(), s2.end(), [](int next) {return next % 2 == 0;}));
     }
 
     void test_method_(any_with_source) {
