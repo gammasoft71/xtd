e680db09743e58880451935285e46ae8bbeeea7d
Gammasoft <gammasoft71@gmail.com>
2024-12-23 21:11:29 +0100

    Add enumerable examples

---
+++ examples/xtd.core.examples/linq/enumerable_append2/src/enumerable_append2.cpp
@@ -0,0 +1,33 @@
+#include <xtd/collections/generic/list>
+#include <xtd/linq/enumerable>
+#include <xtd/console>
+
+using namespace xtd;
+using namespace xtd::collections::generic;
+using namespace xtd::linq;
+
+auto main() -> int {
+  // Creating a list of numbers
+  auto numbers = list {1, 2, 3, 4};
+  
+  // Trying to append any value of the same type
+  enumerable::append(numbers.begin(), numbers.end(), 5);
+  
+  // It doesn't work because the original list has not been changed
+  console::write_line(string::join(", ", numbers));
+  
+  // It works now because we are using a changed copy of the original list
+  console::write_line(string::join(", ", enumerable::append(numbers.begin(), numbers.end(), 5)));
+  
+  // If you prefer, you can create a new list explicitly
+  auto new_numbers = enumerable::append(numbers.begin(), numbers.end(), 5); //.to_list();
+  
+  // And then write to the console output
+  console::write_line(string::join(", ", new_numbers));
+}
+
+// This code produces the following output :
+//
+// 1, 2, 3, 4
+// 1, 2, 3, 4, 5
+// 1, 2, 3, 4, 5
