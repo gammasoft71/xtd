fb22cd2a642b6c5376900b4cb21656272b95c8ef
Gammasoft <gammasoft71@gmail.com>
2024-12-27 02:44:36 +0100

    Add xtd::linq::from methods

---
+++ src/xtd.core/include/xtd/linq/from.hpp
@@ -0,0 +1,80 @@
+/// @file
+/// @brief Contains xtd::linq::from methods.
+/// @copyright Copyright (c) 2024 Gammasoft. All rights reserved.
+#pragma once
+#include "enumerable.hpp"
+
+/// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
+namespace xtd {
+  /// @brief Provides classes and interfaces that support queries that use Language-Integrated Query (LINQ).
+  namespace linq {
+    /// @brief Returns the input typed as xtd::collection::generic::ienumerable <type_t>.
+    /// @param source A sequence of values.
+    /// @return The input sequence typed as xtd::collection::generic::ienumerable <type_t>.
+    /// @par Header
+    /// ```cpp
+    /// #include <xtd/linq/from>
+    /// ```
+    /// @par Namespace
+    /// xtd::linq
+    /// @par Library
+    /// xtd.core
+    /// @ingroup xtd_core linq
+    template <typename source_t>
+    const auto& from(const xtd::collections::generic::ienumerable<source_t>& source) {
+      return enumerable::as_enumerable(source);
+    }
+    
+    /// @brief Returns the input typed as xtd::collection::generic::ienumerable <type_t>.
+    /// @param source A sequence of values.
+    /// @return The input sequence typed as xtd::collection::generic::ienumerable <type_t>.
+    /// @par Header
+    /// ```cpp
+    /// #include <xtd/linq/from>
+    /// ```
+    /// @par Namespace
+    /// xtd::linq
+    /// @par Library
+    /// xtd.core
+    /// @ingroup xtd_core linq
+    template <typename source_t>
+    const auto& from(const std::initializer_list<source_t>& source) {
+      return enumerable::as_enumerable(source);
+    }
+    
+    /// @brief Returns the input typed as xtd::collection::generic::ienumerable <type_t>.
+    /// @param source A sequence of values.
+    /// @return The input sequence typed as xtd::collection::generic::ienumerable <type_t>.
+    /// @par Header
+    /// ```cpp
+    /// #include <xtd/linq/from>
+    /// ```
+    /// @par Namespace
+    /// xtd::linq
+    /// @par Library
+    /// xtd.core
+    /// @ingroup xtd_core linq
+    template <typename collection_t>
+    const auto& from(const collection_t& source) {
+      return enumerable::as_enumerable(source);
+    }
+    
+    /// @brief Returns the input typed as xtd::collection::generic::ienumerable <type_t>.
+    /// @param first The first iterator.
+    /// @param last The last iterator.
+    /// @return The input sequence typed as xtd::collection::generic::ienumerable <type_t>.
+    /// @par Header
+    /// ```cpp
+    /// #include <xtd/linq/from>
+    /// ```
+    /// @par Namespace
+    /// xtd::linq
+    /// @par Library
+    /// xtd.core
+    /// @ingroup xtd_core linq
+    template <typename input_iterator_t>
+    const auto& from(input_iterator_t first, input_iterator_t last) {
+      return enumerable::as_enumerable(first, last);
+    }
+  }
+}
