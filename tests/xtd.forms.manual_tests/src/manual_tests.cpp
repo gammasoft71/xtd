a80cf389d7f06311eaec206aa2431e5a38483414
Gammasoft <gammasoft71@gmail.com>
2024-12-30 01:19:31 +0100

    Update manual_tests.cpp

--- tests/xtd.forms.manual_tests/src/manual_tests.cpp
+++ tests/xtd.forms.manual_tests/src/manual_tests.cpp
@@ -1,8 +1,5 @@
 #include <xtd/xtd>
 
-using namespace xtd;
-using namespace xtd::forms;
-
 auto main()->int {
   auto form1 = form::create("form1");
   auto button1 = button::create(form1, "Click me", {10, 10});
