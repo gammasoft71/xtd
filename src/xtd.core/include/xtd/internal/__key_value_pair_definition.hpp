139c517e24cb7d5590960e3b91f24d1e4b6fa430
Gammasoft <gammasoft71@gmail.com>
2025-01-03 20:55:03 +0100

    add __list_definition and __key_value_pair include files

---
+++ src/xtd.core/include/xtd/internal/__key_value_pair_definition.hpp
@@ -0,0 +1,13 @@
+/// @file
+/// @brief Contains xtd::collections::generic::key_value_pair definitions.
+/// @copyright Copyright (c) 2025 Gammasoft. All rights reserved.
+#pragma once
+#include <cstddef>
+
+/// @cond
+namespace xtd::collections::generic {
+  template <typename key_t = std::nullptr_t, typename value_t = std::nullptr_t>
+  class key_value_pair;
+  
+}
+/// @endcond
