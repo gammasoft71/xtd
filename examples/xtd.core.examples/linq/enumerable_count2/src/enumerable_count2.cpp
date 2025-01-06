b0569a6825a0b5d7b672b450441ec8374b132d8f
Gammasoft <gammasoft71@gmail.com>
2025-01-06 20:40:43 +0100

    Review examples

--- examples/xtd.core.examples/linq/enumerable_count2/src/enumerable_count2.cpp
+++ examples/xtd.core.examples/linq/enumerable_count2/src/enumerable_count2.cpp
@@ -6,7 +6,7 @@ struct pet {
   string name;
   bool vaccinated = false;
   
-  friend bool operator==(const pet& lhs, const pet& rhs) noexcept {
+  friend bool operator ==(const pet& lhs, const pet& rhs) noexcept {
     return lhs.name == rhs.name && lhs.vaccinated == rhs.vaccinated;
   }
 };
