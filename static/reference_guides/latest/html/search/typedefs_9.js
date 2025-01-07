ed36ee70802ed4dc8d82fbb5efe683a498a9e95a
gammasoft71 <gammasoft71@gmail.com>
2025-01-07 21:01:37 +0100

    update latest reference guide

--- static/reference_guides/latest/html/search/typedefs_9.js
+++ static/reference_guides/latest/html/search/typedefs_9.js
@@ -1,7 +1,8 @@
 var searchData=
 [
-  ['key_5fevent_5fhandler_0',['key_event_handler',['../group__events.html#ga9b38a7523169feb64cd37ef7de8dac8b',1,'xtd::forms']]],
-  ['key_5fpress_5fevent_5fhandler_1',['key_press_event_handler',['../group__events.html#gaacd8585e8139ce491fe44534f938d475',1,'xtd::forms']]],
-  ['key_5ftype_2',['key_type',['../classxtd_1_1collections_1_1generic_1_1key__value__pair.html#a58d4e276fbece354323932362f2841ed',1,'xtd::collections::generic::key_value_pair']]],
-  ['key_5fvalue_5fpair_3',['key_value_pair',['../classxtd_1_1linq_1_1enumerable.html#acaa2aa9384131fb9d7c0b02830e55050',1,'xtd::linq::enumerable']]]
+  ['key_5fequal_0',['key_equal',['../classxtd_1_1collections_1_1generic_1_1dictionary.html#a3875535b1af051b47b4c4be60ca9cb86',1,'xtd::collections::generic::dictionary']]],
+  ['key_5fevent_5fhandler_1',['key_event_handler',['../group__events.html#ga9b38a7523169feb64cd37ef7de8dac8b',1,'xtd::forms']]],
+  ['key_5fpress_5fevent_5fhandler_2',['key_press_event_handler',['../group__events.html#gaacd8585e8139ce491fe44534f938d475',1,'xtd::forms']]],
+  ['key_5ftype_3',['key_type',['../classxtd_1_1collections_1_1generic_1_1dictionary.html#ad608d204a607fb2863d6170014293e1d',1,'xtd::collections::generic::dictionary::key_type'],['../classxtd_1_1collections_1_1generic_1_1key__value__pair.html#a58d4e276fbece354323932362f2841ed',1,'xtd::collections::generic::key_value_pair::key_type']]],
+  ['key_5fvalue_5fpair_4',['key_value_pair',['../classxtd_1_1linq_1_1enumerable.html#acaa2aa9384131fb9d7c0b02830e55050',1,'xtd::linq::enumerable']]]
 ];
