d88e9824e953886c96517b180c969e78cd9d33c3
Gammasoft <gammasoft71@gmail.com>
2025-01-07 16:30:41 +0100

    Start of xtd::collections::generic::dictionary implementation

--- src/xtd.core/include/xtd/collections/generic/dictionary.hpp
+++ src/xtd.core/include/xtd/collections/generic/dictionary.hpp
@@ -5,7 +5,10 @@
 #include "helpers/allocator.hpp"
 #include "helpers/equator.hpp"
 #include "helpers/hasher.hpp"
-#include <unordered_set>
+#include "idictionary.hpp"
+#include "../../new_ptr.hpp"
+#include "../../ptr.hpp"
+#include <unordered_map>
 
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
@@ -32,8 +35,109 @@ namespace xtd {
       /// @par Examples
       /// The following shows how to use xtd::collections::generic::dictionary.
       /// @include dictionary.cpp
-      template<typename key_t, typename value_t>
-      using dictionary = std::unordered_map<key_t, value_t, helpers::hasher<key_t>, helpers::equator<key_t>, helpers::allocator<std::pair<const key_t, value_t>>>;
+      template<typename key_t, typename value_t, typename hasher_t = helpers::hasher<key_t>, typename equator_t = helpers::equator<key_t>, typename allocator_t = helpers::allocator<std::pair<const key_t, value_t>>>
+      class dictionary : public idictionary<key_t, value_t> {
+      public:
+        /// @name Public Aliases
+        
+        /// @{
+        /// @brief Represents the dictionary key type.
+        using key_type = key_t;
+        /// @brief Represents the dictionary mapped type.
+        using mapped_type = value_t;
+        /// @brief Represents the dictionary value type.
+        using value_type = xtd::collections::generic::key_value_pair<key_t, value_t>;
+        /// @brief Represents the dictionary base value type.
+        using base_value_type = std::pair<key_t, value_t>;
+        /// @brief Represents the dictionary base type.
+        using base_type = std::unordered_map<key_type, mapped_type, hasher_t, equator_t, allocator_t>;
+        /// @brief Represents the dictionary size type.
+        using size_type = xtd::size;
+        /// @brief Represents the dictionary difference type.
+        using difference_type = xtd::ptrdiff;
+        /// @brief Represents the dictionary hasher type.
+        using hasher = hasher_t;
+        /// @brief Represents the dictionary equator type.
+        using equator = equator_t;
+        /// @brief Represents the dictionary equator type.
+        using key_equal = equator_t;
+        /// @brief Represents the dictionary allocator type.
+        using allocator_type = allocator_t;
+        /// @brief Represents the dictionary reference type.
+        using reference = value_type&;
+        /// @brief Represents the dictionary const reference type.
+        using const_reference = const value_type&;
+        /// @brief Represents the dictionary pointer type.
+        using pointer = std::allocator_traits<allocator_t>::pointer;
+        /// @brief Represents the dictionary const pointer type.
+        using const_pointer = std::allocator_traits<allocator_t>::const_pointer;
+        /// @brief Represents the iterator of dictionary value type.
+        using iterator = typename xtd::collections::generic::ilist<value_type>::iterator;
+        /// @brief Represents the const iterator of dictionary value type.
+        using const_iterator = typename xtd::collections::generic::ilist<value_type>::const_iterator;
+        /// @brief Represents the local iterator of dictionary value type.
+        using local_iterator = typename base_type::local_iterator;
+        /// @brief Represents the const local iterator of dictionary value type.
+        using const_local_iterator = typename base_type::const_local_iterator;
+        /// @brief Represents the dictionary node type.
+        using node_type = typename base_type::node_type;
+        /// @brief Represents the dictionary insert return type.
+        using insert_return_type = typename base_type::insert_return_type;
+        /// @}
+
+        /// @name Public Constructors
+        
+        /// @{
+        /// @brief Initializes a new instance of the dictionary <key_t, value_t> class that is empty, has the default initial capacity, and uses the default equality comparer for the key type.
+        /// @remarks Every key in a dictionary <key_t, value_t> must be unique according to the default equality comparer.
+        /// @remarks dictionary <key_t, value_t> requires an equality implementation to determine whether keys are equal. This constructor uses the default generic equality comparer, equality_comparer::default. If type `key_t` implements the xtd::equatable <type_t> generic interface, the default equality comparer uses that implementation. Alternatively, you can specify an implementation of the iequality_comparer <typer_t> generic interface by using a constructor that accepts a comparer parameter.
+        /// @note If you can estimate the size of the collection, using a constructor that specifies the initial capacity eliminates the need to perform a number of resizing operations while adding elements to the dictionary <key_t, value_t>.
+        /// @remarks This constructor is an O(1) operation.
+        /// @par Exemples
+        /// The following code example creates an empty dictionary <key_t, value_t> of strings with string keys and uses the Add method to add some elements. The example demonstrates that the dictionary::add method throws an xtd::argument_exception when attempting to add a duplicate key.
+        ///
+        /// This code example is part of a larger example provided for the dictionary <key_t, value_t> class.
+        /// ```
+        /// // Create a new dictionary of strings, with string keys.
+        /// //
+        /// auto open_with = dictionary<string, string> {};
+        ///
+        /// // Add some elements to the dictionary. There are no
+        /// // duplicate keys, but some of the values are duplicates.
+        /// open_with.add("txt", "notepad.exe");
+        /// open_with.add("bmp", "paint.exe");
+        /// open_with.add("dib", "paint.exe");
+        /// open_with.add("rtf", "wordpad.exe");
+        ///
+        /// // The add method throws an exception if the new key is
+        /// // already in the dictionary.
+        /// try {
+        ///   open_with.add("txt", "winword.exe");
+        /// } catch (const argument_exception&) {
+        ///   console::write_line("An element with Key = \"txt\" already exists.");
+        /// }
+        /// ```
+        dictionary() noexcept = default;
+        /// @}
+
+        /// @name Public Properties
+        
+        /// @{
+        /// @}
+
+        /// @name Public Methods
+        
+        /// @{
+        /// @}
+
+      private:
+        struct data {
+          base_type items;
+          size_type version = 0;
+          xtd::object sync_root;
+        };
+        xtd::ptr<data> data_ = xtd::new_ptr<data>();
+      };
     }
   }
 }
