d2170c35aee262659aa77a2cc36384efb5a1a9d9
Gammasoft <gammasoft71@gmail.com>
2025-01-07 22:10:33 +0100

    Add some xtd::collections::generic::dictionary properties

--- src/xtd.core/include/xtd/collections/generic/dictionary.hpp
+++ src/xtd.core/include/xtd/collections/generic/dictionary.hpp
@@ -36,17 +36,17 @@ namespace xtd {
       /// The following shows how to use xtd::collections::generic::dictionary.
       /// @include dictionary.cpp
       template<typename key_t, typename value_t, typename hasher_t = xtd::collections::generic::helpers::hasher<key_t>, typename equator_t = xtd::collections::generic::helpers::equator<key_t>, typename allocator_t = xtd::collections::generic::helpers::allocator<std::pair<const key_t, value_t>>>
-      class dictionary : public xtd::collections::generic::idictionary<key_t, value_t> {
+      class dictionary : public xtd::object, public xtd::collections::generic::idictionary<key_t, value_t> {
       public:
         /// @name Public Aliases
         
         /// @{
         /// @brief Represents the dictionary key type.
-        using key_type = key_t;
+        using key_type = typename xtd::collections::generic::idictionary<key_t, value_t>::key_type;
         /// @brief Represents the dictionary mapped type.
-        using mapped_type = value_t;
+        using mapped_type = typename xtd::collections::generic::idictionary<key_t, value_t>::mapped_type;
         /// @brief Represents the dictionary value type.
-        using value_type = xtd::collections::generic::key_value_pair<key_t, value_t>;
+        using value_type = typename xtd::collections::generic::idictionary<key_type, mapped_type>::value_type;
         /// @brief Represents the dictionary size type.
         using size_type = xtd::size;
         /// @brief Represents the dictionary difference type.
@@ -68,13 +68,13 @@ namespace xtd {
         /// @brief Represents the dictionary const reference type.
         using const_reference = const value_type&;
         /// @brief Represents the dictionary pointer type.
-        using pointer = std::allocator_traits<allocator_t>::pointer;
+        using pointer = typename std::allocator_traits<allocator_t>::pointer;
         /// @brief Represents the dictionary const pointer type.
-        using const_pointer = std::allocator_traits<allocator_t>::const_pointer;
+        using const_pointer = typename std::allocator_traits<allocator_t>::const_pointer;
         /// @brief Represents the iterator of dictionary value type.
-        using iterator = typename xtd::collections::generic::icollection<value_type>::iterator;
+        using iterator = typename xtd::collections::generic::idictionary<key_type, mapped_type>::iterator;
         /// @brief Represents the const iterator of dictionary value type.
-        using const_iterator = typename xtd::collections::generic::icollection<value_type>::const_iterator;
+        using const_iterator = typename xtd::collections::generic::idictionary<key_type, mapped_type>::const_iterator;
         /// @brief Represents the local iterator of dictionary value type.
         using local_iterator = typename base_type::local_iterator;
         /// @brief Represents the const local iterator of dictionary value type.
@@ -83,20 +83,24 @@ namespace xtd {
         using node_type = typename base_type::node_type;
         /// @brief Represents the dictionary insert return type.
         using insert_return_type = typename base_type::insert_return_type;
+        /// @brief Represents the idictionary key collection type.
+        using key_collection = typename xtd::collections::generic::idictionary<key_type, mapped_type>::key_collection;
+        /// @brief Represents the idictionary value collection type.
+        using value_collection = typename xtd::collections::generic::idictionary<key_type, mapped_type>::value_collection;
         /// @}
-
+        
         /// @name Public Constructors
         
         /// @{
-        /// @brief Initializes a new instance of the dictionary <key_t, value_t> class that is empty, has the default initial capacity, and uses the default equality comparer for the key type.
-        /// @remarks Every key in a dictionary <key_t, value_t> must be unique according to the default equality comparer.
-        /// @remarks dictionary <key_t, value_t> requires an equality implementation to determine whether keys are equal. This constructor uses the default generic equality comparer, equality_comparer::default. If type `key_t` implements the xtd::equatable <type_t> generic interface, the default equality comparer uses that implementation. Alternatively, you can specify an implementation of the iequality_comparer <typer_t> generic interface by using a constructor that accepts a comparer parameter.
-        /// @note If you can estimate the size of the collection, using a constructor that specifies the initial capacity eliminates the need to perform a number of resizing operations while adding elements to the dictionary <key_t, value_t>.
+        /// @brief Initializes a new instance of the xtd::collections::generic::dictionary <key_t, value_t> class that is empty, has the default initial capacity, and uses the default equality comparer for the key type.
+        /// @remarks Every key in a xtd::collections::generic::dictionary <key_t, value_t> must be unique according to the default equality comparer.
+        /// @remarks xtd::collections::generic::dictionary <key_t, value_t> requires an equality implementation to determine whether keys are equal. This constructor uses the default generic equality comparer, equality_comparer::default. If type `key_t` implements the xtd::equatable <type_t> generic interface, the default equality comparer uses that implementation. Alternatively, you can specify an implementation of the iequality_comparer <typer_t> generic interface by using a constructor that accepts a comparer parameter.
+        /// @note If you can estimate the size of the collection, using a constructor that specifies the initial capacity eliminates the need to perform a number of resizing operations while adding elements to the xtd::collections::generic::dictionary <key_t, value_t>.
         /// @remarks This constructor is an O(1) operation.
         /// @par Exemples
-        /// The following code example creates an empty dictionary <key_t, value_t> of strings with string keys and uses the Add method to add some elements. The example demonstrates that the dictionary::add method throws an xtd::argument_exception when attempting to add a duplicate key.
+        /// The following code example creates an empty xtd::collections::generic::dictionary <key_t, value_t> of strings with string keys and uses the Add method to add some elements. The example demonstrates that the dictionary::add method throws an xtd::argument_exception when attempting to add a duplicate key.
         ///
-        /// This code example is part of a larger example provided for the dictionary <key_t, value_t> class.
+        /// This code example is part of a larger example provided for the xtd::collections::generic::dictionary <key_t, value_t> class.
         /// ```
         /// // Create a new dictionary of strings, with string keys.
         /// //
@@ -119,15 +123,65 @@ namespace xtd {
         /// ```
         dictionary() noexcept = default;
         /// @}
-
+        
         /// @name Public Properties
         
         /// @{
+        /// @brief Gets the total numbers of elements the internal data structure can hold without resizing.
+        /// @return The total numbers of elements the internal data structure can hold without resizing.
+        xtd::size capacity() const noexcept {return data_->items.size();}
+        
+        /// @brief Gets the number of key/value pairs contained in the xtd::collections::generic::dictionary <key_t, value_t>.
+        /// @return the number of key/value pairs contained in the xtd::collections::generic::dictionary <key_t, value_t>.
+        /// @remarks The capacity of a xtd::collections::generic::dictionary <key_t, value_t> is the number of elements that the xtd::collections::generic::dictionary <key_t, value_t> can store. The xtd::collections::generic::dictionary::count property is the number of elements that are actually in the xtd::collections::generic::dictionary <key_t, value_t>.
+        /// @remarks The capacity is always greater than or equal to xtd::collections::generic::dictionary::count. If xtd::collections::generic::dictionary::count exceeds the capacity while adding elements, the capacity is increased by automatically reallocating the internal array before copying the old elements and adding the new elements.
+        /// @remarks Getting the value of this property is an O(1) operation.
+        xtd::size count() const noexcept {return data_->items.size();}
+
+        /// @brief Returns the underlying base type items.
+        /// @return The underlying base type items.
+        virtual const base_type& items() const noexcept {return data_->items;}
+        /// @brief Returns the underlying base type items.
+        /// @return The underlying base type items.
+        virtual base_type& items() noexcept {return data_->items;}
+
+        /// @brief Gets a collection containing the keys in the xtd::collections::generic::dictionary <key_t, value_t>.
+        /// @return A xtd::collections::generic::dictionary::key_collection containing the keys in the xtd::collections::generic::dictionary <key_t, value_t>.
+        /// @remarks The order of the keys in the xtd::collections::generic::dictionary::key_collection is unspecified, but it is the same order as the associated values in the xtd::collections::generic::dictionary::value_collection returned by the xtd::collections::generic::dictionary::values property.
+        /// @remarks Getting the value of this property is an O(1) operation.
+        key_collection keys() const noexcept override {
+          auto keys = key_collection {};
+          for (const auto& [key, value] : data_->items)
+            keys.add(key);
+          return keys;
+        }
+        
+        /// @brief Gets a collection containing the values in the xtd::collections::generic::dictionary <key_t, value_t>.
+        /// @return A xtd::collections::generic::dictionary::value_collection containing the values in the xtd::collections::generic::dictionary <key_t, value_t>.
+        /// @remarks The order of the values in the xtd::collections::generic::dictionary::value_collection is unspecified, but it is the same order as the associated keys in the xtd::collections::generic::dictionary::key_collection returned by the xtd::collections::generic::dictionary::keys property.
+        /// @remarks Getting the value of this property is an O(1) operation.
+        value_collection values() const noexcept override {
+          auto values = value_collection {};
+          for (const auto& [key, value] : data_->items)
+            values.add(value);
+          return values;
+        }
         /// @}
 
         /// @name Public Methods
         
         /// @{
+        /// @}
+        
+        /// @name Public Operators
+        
+        /// @{
+        /// @brief Returns a reference to the underlying base type.
+        /// @return Reference to the underlying base type.
+        operator const base_type&() const noexcept {return data_->items;}
+        /// @brief Returns a reference to the underlying base type.
+        /// @return Reference to the underlying base type.
+        operator base_type&() noexcept {return data_->items;}
         /// @}
 
       private:
--- src/xtd.core/include/xtd/collections/generic/idictionary.hpp
+++ src/xtd.core/include/xtd/collections/generic/idictionary.hpp
@@ -4,6 +4,7 @@
 #pragma once
 #include "icollection.hpp"
 #include "key_value_pair.hpp"
+#include "list.hpp"
 
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
@@ -31,8 +32,50 @@ namespace xtd {
       /// @remarks Each pair must have a unique key. Implementations can vary in whether they allow key to be empty. The value can be empty and does not have to be unique. The idictionary <key_t, value_t> interface allows the contained keys and values to be enumerated, but it does not imply any particular sort order.
       /// @remarks The foreach statement of the C# language (For Each in Visual Basic, for each in C++) returns an object of the type of the elements in the collection. Since each element of the IDictionary<TKey,TValue> is a key/value pair, the element type is not the type of the key or the type of the value. Instead, the element type is KeyValuePair<TKey,TValue>. For example:
       template<typename key_t, typename value_t>
-      class idictionary interface_ {
+      class idictionary interface_ /* : public xtd::collections::generic::icollection<xtd::collections::generic::key_value_pair<key_t, value_t>> */ {
       public:
+        /// @name Public Aliases
+        
+        /// @{
+        /// @brief Represents the dictionary key type.
+        using key_type = key_t;
+        /// @brief Represents the dictionary mapped type.
+        using mapped_type = value_t;
+        /// @brief Represents the dictionary value type.
+        using value_type = xtd::collections::generic::key_value_pair<key_type, mapped_type>;
+        /// @brief Represents the iterator of xtd::collections::generic::ienumerable value type.
+        using iterator = typename xtd::collections::generic::icollection<value_type>::iterator;
+        /// @brief Represents the const iterator of xtd::collections::generic::ienumerable value type.
+        using const_iterator = typename xtd::collections::generic::icollection<value_type>::const_iterator;
+        /// @brief Represents the idictionary key collection type.
+        using key_collection = xtd::collections::generic::list<key_type>;
+        /// @brief Represents the idictionary value collection type.
+        using value_collection = xtd::collections::generic::list<mapped_type>;
+        /// @}
+        
+        /// @name Public Properties
+
+        /// @{
+        /// @brief Gets an xtd::collections::generic::icollection <type_t> containing the keys of the xtd::collections::generic::idictionary <key_t, value_t>.
+        /// @return An xtd::collections::generic::icollection <type_t> containing the keys of the object that implements xtd::collections::generic::idictionary <key_t, value_t>.
+        /// @remarks The order of the keys in the returned xtd::collections::generic::icollection <type_t> is unspecified, but it is guaranteed to be the same order as the corresponding values in the xtd::collections::generic::icollection <type_t> returned by the xtd::collections::generic::idictionary::values property.
+        virtual key_collection keys() const noexcept = 0;
+
+        /// @brief Gets an xtd::collections::generic::icollection <type_t> containing the values of the xtd::collections::generic::idictionary <key_t, value_t>.
+        /// @return An xtd::collections::generic::icollection <type_t> containing the values of the object that implements xtd::collections::generic::idictionary <key_t, value_t>.
+        /// @remarks The order of the values in the returned xtd::collections::generic::icollection <type_t> is unspecified, but it is guaranteed to be the same order as the corresponding values in the xtd::collections::generic::icollection <type_t> returned by the xtd::collections::generic::idictionary::keys property.
+        virtual value_collection values() const noexcept = 0;
+        /// @}
+        
+        /// @name Public Methods
+        
+        /// @{
+        /// @}
+
+        /// @name Public Operators
+        
+        /// @{
+        /// @}
       };
     }
   }
