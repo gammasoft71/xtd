4be70e390d032472dc2263668d33cb709bca8ea6
Gammasoft <gammasoft71@gmail.com>
2025-01-08 09:56:10 +0100

    Add enumerator::create method

--- src/xtd.core/include/xtd/collections/generic/enumerator.hpp
+++ src/xtd.core/include/xtd/collections/generic/enumerator.hpp
@@ -3,7 +3,11 @@
 /// @copyright Copyright (c) 2025 Gammasoft. All rights reserved.
 #pragma once
 #include "ienumerator.hpp"
+#define __XTD_CORE_INTERNAL__
+#include "../../internal/__external_exceptions.hpp"
+#undef __XTD_CORE_INTERNAL__
 #include "../../ptr.hpp"
+#include "../../static.hpp"
 
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
@@ -11,6 +15,11 @@ namespace xtd {
   namespace collections {
     /// @brief The xtd::collections::generic namespace contains interfaces and classes that define generic collections, which allow users to create strongly typed collections that provide better type safety and performance than non-generic strongly typed collections.
     namespace generic {
+      /// @cond
+      template <typename type_t = std::nullptr_t>
+      class enumerator;
+      /// @endcond
+      
       /// @brief Supports a simple iteration over a generic collection.
       /// @par Definition
       /// ```cpp
@@ -72,6 +81,76 @@ namespace xtd {
       
       private:
         ptr<ienumerator<type_t>> enumerator_;
+      };
+
+      /// @brief Supports a simple iteration over a generic collection.
+      /// @par Definition
+      /// ```cpp
+      /// template<typename type_t>
+      /// class enumerator interface_
+      /// ```
+      /// @par Header
+      /// ```cpp
+      /// #include <xtd/collections/enumerator
+      /// ```
+      /// @par Namespace
+      /// xtd::collections::generic
+      /// @par Library
+      /// xtd.core
+      /// @ingroup xtd_core generic_collections
+      /// @remarks The xtd::collections::generic::enumerator <type_t> class is used to encapsulate an xtd::collections::generic::ienumerator <type_t>.
+      template <>
+      class enumerator<> static_ {
+      public:
+        /// @name Public Static Methods
+        
+        /// @{
+        /// @brief Create an enumerator from specified collection and version.
+        /// @tparam collection_t The collection type.
+        /// @tparam vertion_t The collection version type.
+        /// @param items The collection to create an enumerator on.
+        /// @param current_version A pointer on the collection current version.
+        /// @return The enumerator created.
+        /// @remarks This method is used for xtd frameork enumerables.
+        template <typename collection_t, typename vertion_t = std::nullptr_t>
+        static auto create(const collection_t& items, const vertion_t* current_version = nullptr) noexcept {
+          using value_type = typename collection_t::value_type;
+          using const_iterator = typename collection_t::const_iterator;
+          class internal_enumerator : public ienumerator<value_type> {
+          public:
+            explicit internal_enumerator(const collection_t& items, const vertion_t* current_version) : items_(items), version_(current_version ? *current_version : 0), current_version_(current_version) {}
+            
+            const value_type& current() const override {
+              if (current_version_ && version_ != *current_version_) __throw_invalid_operation_exception("Collection was modified; enumeration operation may not execute.", __FILE__, __LINE__, __func__);
+              if (iterator_ != items_.cend()) return *iterator_;
+              static auto default_value_type = value_type {};
+              return default_value_type;
+            }
+            
+            bool move_next() override {
+              if (current_version_ && version_ != *current_version_) __throw_invalid_operation_exception("Collection was modified; enumeration operation may not execute.", __FILE__, __LINE__, __func__);
+              if (!reset_) return ++iterator_ != items_.cend();
+              reset_ = false;
+              iterator_ = items_.cbegin();
+              return iterator_ != items_.cend();
+            }
+            
+            void reset() override {
+              reset_ = true;
+              version_ = current_version_ ? *current_version_ : 0;
+              iterator_ = items_.cend();
+            }
+            
+          protected:
+            bool reset_ = true;
+            const collection_t& items_;
+            const_iterator iterator_ = items_.cend();
+            vertion_t version_ = 0;
+            const vertion_t* current_version_ = nullptr;
+          };
+          return enumerator<value_type> {new_ptr<internal_enumerator>(items, current_version)};
+        }
+        /// @}
       };
     }
   }
