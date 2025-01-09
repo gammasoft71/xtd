e6a80773f4eff06cc64a80896fe90091d2bd4a43
Gammasoft <gammasoft71@gmail.com>
2025-01-09 22:18:47 +0100

    rename xtd::linq::enumerable_collection to __opaque_xtd_linq_enumerable_collection__

--- src/xtd.core/include/xtd/array.hpp
+++ src/xtd.core/include/xtd/array.hpp
@@ -116,8 +116,8 @@ namespace xtd {
 template <typename source_t>
 const xtd::linq::enumerable::ienumerable<xtd::array<source_t>>& xtd::linq::enumerable::chunk(const ienumerable<source_t>& source, size_t size) {
   if (size == 0) __throw_argument_out_of_range_exception(__FILE__, __LINE__, __func__);
-  static thread_local auto chunks = enumerable_collection<xtd::array<source_t>> {};
-  chunks = enumerable_collection<xtd::array<source_t>> {};
+  static thread_local auto chunks = __opaque_xtd_linq_enumerable_collection__<xtd::array<source_t>> {};
+  chunks = __opaque_xtd_linq_enumerable_collection__<xtd::array<source_t>> {};
   auto chunk = std::vector<source_t> {};
   for (auto index = size_t {0}; const auto& item : source) {
     chunk.push_back(item);
--- src/xtd.core/include/xtd/as.hpp
+++ src/xtd.core/include/xtd/as.hpp
@@ -55,8 +55,8 @@ new_type_t* __convert_value__(current_type_t* value) {
 
 template <typename result_t, typename source_t>
 inline const xtd::collections::generic::ienumerable<result_t>& xtd::linq::enumerable::cast(const xtd::collections::generic::ienumerable<source_t>& source) noexcept {
-  static thread_local auto result = xtd::linq::enumerable_collection<result_t> {};
-  result = xtd::linq::enumerable_collection<result_t> {};
+  static thread_local auto result = __opaque_xtd_linq_enumerable_collection__<result_t> {};
+  result = __opaque_xtd_linq_enumerable_collection__<result_t> {};
   for (const auto& item : source)
     result.items.push_back(xtd::as<result_t>(item));
   return static_cast<const xtd::collections::generic::ienumerable<result_t>&>(result);
