7dc1632c1eab242b1d028a96e06cc0ca855077c0
Gammasoft <gammasoft71@gmail.com>
2025-01-09 20:35:00 +0100

    Fix documentation

--- src/xtd.core/include/xtd/collections/generic/sorted_set.hpp
+++ src/xtd.core/include/xtd/collections/generic/sorted_set.hpp
@@ -1,5 +1,5 @@
 /// @file
-/// @brief Contains xtd::collections::generic::sorted_set struct.
+/// @brief Contains xtd::collections::generic::sorted_set <value_t> class.
 /// @copyright Copyright (c) 2025 Gammasoft. All rights reserved.
 #pragma once
 #include "helpers/allocator.hpp"
