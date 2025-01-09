4e5c2236a5472b07e25bd00a981ce73118e70f96
Gammasoft <gammasoft71@gmail.com>
2025-01-09 20:33:50 +0100

    Fix documentation

--- src/xtd.core/include/xtd/collections/generic/hash_set.hpp
+++ src/xtd.core/include/xtd/collections/generic/hash_set.hpp
@@ -1,5 +1,5 @@
 /// @file
-/// @brief Contains xtd::collections::generic::hash_set struct.
+/// @brief Contains xtd::collections::generic::hash_set <type_t> class.
 /// @copyright Copyright (c) 2025 Gammasoft. All rights reserved.
 #pragma once
 #include "helpers/allocator.hpp"
