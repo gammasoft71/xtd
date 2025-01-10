9b7c0e5ad3ef382a1aa34e854967f83a7cffc24a
gammasoft71 <gammasoft71@gmail.com>
2025-01-10 21:01:46 +0100

    update latest reference guide

---
+++ static/reference_guides/latest/html/tool__bar__button__style_8h.js
@@ -0,0 +1,11 @@
+var tool__bar__button__style_8h =
+[
+    [ "tool_bar_button_style", "tool__bar__button__style_8h.html#ga601aa3d12e8f50c0ef209bcdf1bf3675", [
+      [ "push_button", "tool__bar__button__style_8h.html#gga601aa3d12e8f50c0ef209bcdf1bf3675a7e60a2ad3cc1f06c5d17a928f2dfb168", null ],
+      [ "toggle_button", "tool__bar__button__style_8h.html#gga601aa3d12e8f50c0ef209bcdf1bf3675af32a6ba3b86973e167ab565ecf20ba7a", null ],
+      [ "separator", "tool__bar__button__style_8h.html#gga601aa3d12e8f50c0ef209bcdf1bf3675aa0f0bc95016c862498bbad29d1f4d9d4", null ],
+      [ "drop_down_button", "tool__bar__button__style_8h.html#gga601aa3d12e8f50c0ef209bcdf1bf3675ad1db10e926dd74260b2f59f294e97ead", null ],
+      [ "stretchable_separator", "tool__bar__button__style_8h.html#gga601aa3d12e8f50c0ef209bcdf1bf3675ac8763a4e50ca0aa6d902b21e2b3a91cc", null ],
+      [ "control", "tool__bar__button__style_8h.html#gga601aa3d12e8f50c0ef209bcdf1bf3675afc5364bf9dbfa34954526becad136d4b", null ]
+    ] ]
+];
