ad7d8277d682b4b06672261bd99f95a634f54030
gammasoft71 <gammasoft71@gmail.com>
2025-01-15 21:00:53 +0100

    update latest reference guide

--- static/reference_guides/latest/html/search/typedefs_7.js
+++ static/reference_guides/latest/html/search/typedefs_7.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['hash_5fset_0',['hash_set',['../group__generic__collections.html#ga64eee1442dc5ab233c858fb9210538f0',1,'xtd::collections::generic']]],
-  ['hashtable_1',['hashtable',['../group__collections.html#ga9869e5a28ce3a47249bdee5783d54dc9',1,'xtd::collections']]],
+  ['hashtable_1',['hashtable',['../group__collections.html#gad3e0111139723e7d2c53bab2257a33f7',1,'xtd::collections']]],
   ['hatch_5fbrush_2',['hatch_brush',['../namespacextd_1_1drawing_1_1drawing2d.html#abd9c313af180cd366442e09c33331296',1,'xtd::drawing::drawing2d']]],
   ['hatch_5fstyle_3',['hatch_style',['../namespacextd_1_1drawing_1_1drawing2d.html#a5474a0f4df1deacfab4aae122d017525',1,'xtd::drawing::drawing2d']]],
   ['help_5fevent_5fhandler_4',['help_event_handler',['../group__events.html#gaae5e1074f328eef5208f78149f5cc29b',1,'xtd::forms']]],
