5320679fc30c154e686d48c7f99355d4637c93c4
Gammasoft <gammasoft71@gmail.com>
2025-01-15 20:10:42 +0100

    Add generic_dictionary_constructor_with_idicationary example

--- examples/xtd.core.examples/generic_collections/generic_dictionary/src/generic_dictionary.cpp
+++ examples/xtd.core.examples/generic_collections/generic_dictionary/src/generic_dictionary.cpp
@@ -8,7 +8,7 @@ public:
   static auto main() -> void {
     // Create a new dictionary of strings, with string keys.
     //
-    auto open_with = dictionary<string, string>();
+    auto open_with = dictionary<string, string> {};
     
     // Add some elements to the dictionary. There are no
     // duplicate keys, but some of the values are duplicates.
@@ -22,15 +22,14 @@ public:
     try {
       open_with.add("txt", "winword.exe");
     } catch (const argument_exception&) {
-      console::write_line("An element with Key = \"txt\" already exists.");
+      console::write_line("An element with key = \"txt\" already exists.");
     }
     
-    // The Item property is another name for the indexer, so you
+    // The operator [] is another name for the indexer, so you
     // can omit its name when accessing elements.
     console::write_line("For key = \"rtf\", value = {0}.", xtd::as_const(open_with)["rtf"]);
     
-    // The indexer can be used to change the value associated
-    // with a key.
+    // The indexer can be used to change the value associated with a key.
     open_with["rtf"] = "winword.exe";
     console::write_line("For key = \"rtf\", value = {0}.", xtd::as_const(open_with)["rtf"]);
     
@@ -42,55 +41,54 @@ public:
     try {
       console::write_line("For key = \"tif\", value = {0}.", xtd::as_const(open_with)["tif"]);
     } catch (const key_not_found_exception&) {
-      console::write_line("Key = \"tif\" is not found.");
+      console::write_line("key = \"tif\" is not found.");
     }
     
     // When a program often has to try keys that turn out not to
-    // be in the dictionary, TryGetValue can be a more efficient
+    // be in the dictionary, try_get_value can be a more efficient
     // way to retrieve values.
     auto value = ""_s;
     if (open_with.try_get_value("tif", value))
       console::write_line("For key = \"tif\", value = {0}.", value);
     else
-      console::write_line("Key = \"tif\" is not found.");
+      console::write_line("key = \"tif\" is not found.");
     
-    // ContainsKey can be used to test keys before inserting
-    // them.
+    // contains_key can be used to test keys before inserting them.
     if (!open_with.contains_key("ht")) {
       open_with.add("ht", "hypertrm.exe");
-      console::write_line("Value added for key = \"ht\": {0}", xtd::as_const(open_with)["ht"]);
+      console::write_line("value added for key = \"ht\": {0}", xtd::as_const(open_with)["ht"]);
     }
     
-    // When you use foreach to enumerate dictionary elements,
-    // the elements are retrieved as KeyValuePair objects.
+    // When you use for each to enumerate dictionary elements,
+    // the elements are retrieved as key_value_pair objects.
     console::write_line();
     for (const key_value_pair<string, string>& kvp : open_with)
-      console::write_line("Key = {0}, Value = {1}", kvp.key(), kvp.value());
+      console::write_line("key = {0}, value = {1}", kvp.key(), kvp.value());
     
-    // To get the values alone, use the Values property.
+    // To get the values alone, use the values property.
     dictionary<string, string>::value_collection values = open_with.values();
     
-    // The elements of the ValueCollection are strongly typed
+    // The elements of the value_collection are strongly typed
     // with the type that was specified for dictionary values.
     console::write_line();
-    for(const string& s : values)
-      console::write_line("Value = {0}", s);
+    for(const auto s : values)
+      console::write_line("value = {0}", s);
     
-    // To get the keys alone, use the Keys property.
+    // To get the keys alone, use the keys property.
     dictionary<string, string>::key_collection keys = open_with.keys();
     
-    // The elements of the KeyCollection are strongly typed
+    // The elements of the key_collection are strongly typed
     // with the type that was specified for dictionary keys.
     console::write_line();
     for(const string& s : keys)
-      console::write_line("Key = {0}", s);
+      console::write_line("key = {0}", s);
     
-    // Use the Remove method to remove a key/value pair.
+    // Use the remove method to remove a key/value pair.
     console::write_line("\nRemove(\"doc\")");
     open_with.remove("doc");
     
     if (!open_with.contains_key("doc"))
-      console::write_line("Key \"doc\" is not found.");
+      console::write_line("key \"doc\" is not found.");
   }
 };
 
@@ -98,33 +96,33 @@ startup_(example::main);
 
 // This code produces the following output :
 //
-// An element with Key = "txt" already exists.
+// An element with key = "txt" already exists.
 // For key = "rtf", value = wordpad.exe.
 // For key = "rtf", value = winword.exe.
-// Key = "tif" is not found.
-// Key = "tif" is not found.
-// Value added for key = "ht": hypertrm.exe
+// key = "tif" is not found.
+// key = "tif" is not found.
+// value added for key = "ht": hypertrm.exe
 //
-// Key = doc, Value = winword.exe
-// Key = rtf, Value = winword.exe
-// Key = bmp, Value = paint.exe
-// Key = ht, Value = hypertrm.exe
-// Key = dib, Value = paint.exe
-// Key = txt, Value = notepad.exe
+// key = doc, value = winword.exe
+// key = rtf, value = winword.exe
+// key = bmp, value = paint.exe
+// key = ht, value = hypertrm.exe
+// key = dib, value = paint.exe
+// key = txt, value = notepad.exe
 //
-// Value = winword.exe
-// Value = winword.exe
-// Value = paint.exe
-// Value = hypertrm.exe
-// Value = paint.exe
-// Value = notepad.exe
+// value = winword.exe
+// value = winword.exe
+// value = paint.exe
+// value = hypertrm.exe
+// value = paint.exe
+// value = notepad.exe
 //
-// Key = doc
-// Key = rtf
-// Key = bmp
-// Key = ht
-// Key = dib
-// Key = txt
+// key = doc
+// key = rtf
+// key = bmp
+// key = ht
+// key = dib
+// key = txt
 //
 // Remove("doc")
-// Key "doc" is not found.
+// key "doc" is not found.
---
+++ examples/xtd.core.examples/generic_collections/generic_dictionary_constructor_with_idicationary/src/generic_dictionary_constructor_with_idicationary.cpp
@@ -0,0 +1,36 @@
+#include <xtd/xtd>
+
+using namespace xtd;
+using namespace xtd::collections::generic;
+
+class example {
+public:
+  static auto main() -> void {
+    // Create a new sorted dictionary of strings, with string keys.
+    auto open_with = ::sorted_dictionary<string, string> {};
+    
+    // Add some elements to the dictionary.
+    open_with.add("txt", "notepad.exe");
+    open_with.add("bmp", "paint.exe");
+    open_with.add("dib", "paint.exe");
+    open_with.add("rtf", "wordpad.exe");
+    
+    // Create a dictionary of strings with string keys, and
+    // initialize it with the contents of the sorted dictionary.
+    auto copy = dictionary<string, string>(open_with);
+    
+    // List the contents of the copy.
+    console::write_line();
+    for(const key_value_pair<string, string>& kvp : copy)
+      console::write_line("key = {0}, value = {1}", kvp.key(), kvp.value());
+  }
+};
+
+startup_(example::main);
+
+// This code produces the following output :
+//
+// key = txt, value = notepad.exe
+// key = bmp, value = paint.exe
+// key = dib, value = paint.exe
+// key = rtf, value = wordpad.exe
