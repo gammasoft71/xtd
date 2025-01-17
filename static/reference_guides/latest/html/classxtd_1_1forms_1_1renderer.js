b8ca6b88c7eead869f46b09d4eb121d04b71764d
gammasoft71 <gammasoft71@gmail.com>
2025-01-17 21:01:43 +0100

    update latest reference guide

---
+++ static/reference_guides/latest/html/classxtd_1_1forms_1_1renderer.js
@@ -0,0 +1,5 @@
+var classxtd_1_1forms_1_1renderer =
+[
+    [ "to_string", "classxtd_1_1forms_1_1renderer.html#ae6787f34cdbeafdd82f626e0a1789b70", null ],
+    [ "equals", "classxtd_1_1forms_1_1renderer.html#a2743b6f75405882c28eebe2456f3837b", null ]
+];
---
+++ static/reference_guides/latest/html/structxtd_1_1const__overload.js
@@ -0,0 +1,4 @@
+var structxtd_1_1const__overload =
+[
+    [ "operator()", "structxtd_1_1const__overload.html#a3a91eff6f4b4eb7be21ef89e80133f72", null ]
+];
