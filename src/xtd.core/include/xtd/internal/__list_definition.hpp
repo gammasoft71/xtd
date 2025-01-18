007144c381cf0184aa3d5c90ca12f4449b6154af
Gammasoft <gammasoft71@gmail.com>
2025-01-18 22:28:08 +0100

    Add xtd::collections::generic::type_to_list_type alias

--- src/xtd.core/include/xtd/internal/__list_definition.hpp
+++ src/xtd.core/include/xtd/internal/__list_definition.hpp
@@ -4,9 +4,29 @@
 #pragma once
 #include <cstddef>
 
-/// @cond
-namespace xtd::collections::generic {
-  template<class type_t, class allocator_t = xtd::collections::generic::helpers::allocator<typename std::conditional<std::is_same<bool, type_t>::value, char, type_t>::type>>
-  class list;
+/// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
+namespace xtd {
+  /// @brief The xtd::collections namespace contains interfaces and classes that define various collections of objects, such as lists, queues, bit arrays, hash tables and dictionaries.
+  namespace collections {
+    /// @brief The xtd::collections::generic namespace contains interfaces and classes that define generic collections, which allow users to create strongly typed collections that provide better type safety and performance than non-generic strongly typed collections.
+    namespace generic {
+      /// @brief Represents the type to list type.
+      /// @par Header
+      /// ```cpp
+      /// #include <xtd/collections/generic/list>
+      /// ```
+      /// @par Namespace
+      /// xtd::collections::generic
+      /// @par Library
+      /// xtd.core
+      /// @ingroup xtd_core generic_collections
+      template<class type_t>
+      using type_to_list_t = typename std::conditional<std::is_same<bool, type_t>::value, char, type_t>::type;
+      
+      /// @cond
+      template<class type_t, class allocator_t = xtd::collections::generic::helpers::allocator<type_to_list_t<type_t>>>
+      class list;
+      /// @endcond
+    }
+  }
 }
-/// @endcond
