04cf9c8a2ad4692584094d68359677289bb42ede
Gammasoft <gammasoft71@gmail.com>
2025-01-20 14:25:57 +0100

    Add documentation

--- src/xtd.core/include/xtd/io/memory_stream.hpp
+++ src/xtd.core/include/xtd/io/memory_stream.hpp
@@ -39,6 +39,12 @@ namespace xtd {
       /// @remarks The capacity of the current stream automatically increases when you use the xtd::io::memory_stream::set_length method to set the length to a value larger than the capacity of the current stream.
       /// @remarks This constructor exposes the underlying stream, which xtd::io::memory_stream::get_buffer returns.
       memory_stream();
+      /// @brief Initializes a new instance of the MemoryStream class with an expandable capacity initialized as specified.
+      /// @param capacity The initial size of the internal array in bytes.
+      /// @remarks The xtd::io::memory_stream::can_read, xtd::io::memory_stream::can_seek, and xtd::io::memory_stream::can_write properties are all set to true.
+      /// @remarks The capacity of the current stream automatically increases when you use the xtd::io::memory_stream::set_length method to set the length to a value larger than the capacity of the current stream.
+      /// @remarks This constructor exposes the underlying stream, which xtd::io::memory_stream::get_buffer returns.
+        This constructor exposes the underlying stream that GetBuffer returns.
       memory_stream(xtd::size capacity);
       /// @}
       
@@ -54,13 +60,29 @@ namespace xtd {
       /// @brief Gets a value indicating whether the current stream supports writing.
       /// @return `true` if the stream supports writing; otherwise, `false`.
       bool can_write() const noexcept;
-      
+
+      /// @brief Gets the number of bytes allocated for this stream.
+      /// @return The length of the usable portion of the buffer for the stream.
+      /// @exception xtd::argument_out_of_range_exception A capacity is less than the current length of the stream.
+      /// @remarks `capacity` is the buffer length for system-provided byte arrays. `capacity` cannot be set to a value less than the current length of the stream.
       xtd::size capacity() const noexcept;
+      /// @brief Sets the number of bytes allocated for this stream.
+      /// @param value The length of the usable portion of the buffer for the stream.
+      /// @return This current instance.
+      /// @exception xtd::argument_out_of_range_exception A capacity is less than the current length of the stream.
+      /// @remarks `capacity` is the buffer length for system-provided byte arrays. `capacity` cannot be set to a value less than the current length of the stream.
       memory_stream& capacity(xtd::size value);
 
+      /// @brief Gets the length of the stream in bytes.
+      /// @return The length of the stream in bytes.
       xtd::size length() const noexcept;
-      
+
+      /// @brief Gets the current position within the stream.
+      /// @return The current position within the stream.
       xtd::size position() const noexcept;
+      /// @brief Sets the current position within the stream.
+      /// @param value The current position within the stream.
+      /// @return This current instance.
       memory_stream& position(xtd::size value);
       /// @}
       
