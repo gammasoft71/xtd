28cf1d55f948e825cde73271122119c880858ce7
Gammasoft <gammasoft71@gmail.com>
2025-01-23 23:20:01 +0100

    xtd::io::stream implementztion

--- src/xtd.core/src/xtd/io/stream.cpp
+++ src/xtd.core/src/xtd/io/stream.cpp
@@ -1,3 +1,4 @@
+#include "../../../include/xtd/io/end_of_stream_exception.hpp"
 #include "../../../include/xtd/io/stream.hpp"
 #include "../../../include/xtd/argument_out_of_range_exception.hpp"
 #include "../../../include/xtd/as.hpp"
@@ -75,6 +76,18 @@ xtd::size stream::read(xtd::array<xtd::byte>& buffer) {
   return read(buffer, 0_z, buffer.length());
 }
 
+xtd::size stream::read_at_least(xtd::array<xtd::byte>& buffer, xtd::size minimum_bytes, bool throw_on_end_of_stream) {
+  if (is_closed()) throw object_closed_exception {};
+  if (!can_read()) throw not_supported_exception {};
+  
+  if (minimum_bytes == 0_z) return 0_z;
+  if (buffer.length() < minimum_bytes) throw argument_out_of_range_exception {};
+  
+  auto read_count = read(buffer, 0_z, minimum_bytes);
+  if (throw_on_end_of_stream && read_count < minimum_bytes) throw end_of_stream_exception {};
+  return read_count;
+}
+
 int32 stream::read_byte() {
   if (is_closed()) throw object_closed_exception {};
   if (!can_read()) throw not_supported_exception {};
@@ -83,6 +96,26 @@ int32 stream::read_byte() {
   if (read(b) == 1_z)
     return as<int32>(b[0]);
   return stream::eof;
+}
+
+void stream::read_exactly(xtd::array<xtd::byte>& buffer) {
+  read_exactly(buffer, 0_z, buffer.length());
+}
+
+void stream::read_exactly(xtd::array<xtd::byte>& buffer, xtd::size offset, xtd::size count) {
+  if (is_closed()) throw object_closed_exception {};
+  if (!can_read()) throw not_supported_exception {};
+
+  if (count == 0_z) return;
+  if (offset >= buffer.size() || offset + count > buffer.size()) throw argument_out_of_range_exception {};
+
+  auto total_read_count = 0_z;
+  
+  while (total_read_count < count) {
+    auto read_count = read(buffer, offset + total_read_count, count - total_read_count);
+    if (read_count == 0_z) throw end_of_stream_exception {};
+    total_read_count += read_count;
+  }
 }
 
 void stream::write(const xtd::array<xtd::byte>& buffer) {
