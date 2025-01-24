54cc08cedd282413048cba6c6943ed316480afe5
Gammasoft <gammasoft71@gmail.com>
2025-01-24 19:11:29 +0100

    Add properties and methods

--- src/xtd.core/include/xtd/span.hpp
+++ src/xtd.core/include/xtd/span.hpp
@@ -12,6 +12,7 @@
 #include "object.hpp"
 #include "ptrdiff.hpp"
 #include "size.hpp"
+#include "typeof.hpp"
 #include <limits>
 #include <type_traits>
 #include <vector>
@@ -57,11 +58,11 @@ namespace xtd {
     /// @{
     
     template <xtd::size count = 0>
-    span() : data_ {xtd::null}, size_ {0} {};
-
+    span() : data_ {xtd::null}, length_ {0} {};
+    
     template<class iterator_t>
-    span(iterator_t first, iterator_t last) : data_ {const_cast<type_t*>(&(*first))}, size_ {extent != dynamic_extent ? extent : static_cast<size_type>(std::distance(first, last))} {}
-
+    span(iterator_t first, iterator_t last) : data_ {const_cast<type_t*>(&(*first))}, length_ {extent != dynamic_extent ? extent : static_cast<size_type>(std::distance(first, last))} {}
+    
     template<class collection_t>
     explicit span(const collection_t& items) noexcept : span {items, size_type {0}, items.size()} {}
     template<class collection_t>
@@ -71,36 +72,36 @@ namespace xtd {
     template<class collection_t>
     span(collection_t& items, xtd::size count) : span {items, size_type {0}, count} {}
     template<class collection_t>
-    span(const collection_t& items, xtd::size offset, xtd::size count) : data_ {const_cast<type_t*>(items.data() + offset)}, size_ {extent != dynamic_extent ? extent : count} {
+    span(const collection_t& items, xtd::size offset, xtd::size count) : data_ {const_cast<type_t*>(items.data() + offset)}, length_ {extent != dynamic_extent ? extent : count} {
       if (offset + count > items.size()) throw argument_out_of_range_exception {};
     }
     template<class collection_t>
-    span(collection_t& items, xtd::size offset, xtd::size count) : data_ {items.data() + offset}, size_ {extent != dynamic_extent ? extent : count} {
+    span(collection_t& items, xtd::size offset, xtd::size count) : data_ {const_cast<type_t*>(items.data()) + offset}, length_ {extent != dynamic_extent ? extent : count} {
       if (offset + count > items.size()) throw argument_out_of_range_exception {};
     }
-
-    explicit span(std::initializer_list<type_t> items) noexcept : data_ {const_cast<type_t*>(&(*items.begin()))}, size_ {extent != dynamic_extent ? extent : items.size()} {}
-
-    span(const type_t* data, size_type size) : data_ {const_cast<type_t*>(data)}, size_ {extent != dynamic_extent ? extent : size} {
+    
+    explicit span(std::initializer_list<type_t> items) noexcept : data_ {const_cast<type_t*>(&(*items.begin()))}, length_ {extent != dynamic_extent ? extent : items.size()} {}
+    
+    span(const type_t* data, size_type size) : data_ {const_cast<type_t*>(data)}, length_ {extent != dynamic_extent ? extent : size} {
       if (!data) throw argument_null_exception {};
     }
-    span(type_t* data, size_type size) : data_ {data}, size_ {extent != dynamic_extent ? extent : size} {
+    span(type_t* data, size_type size) : data_ {data}, length_ {extent != dynamic_extent ? extent : size} {
       if (!data) throw argument_null_exception {};
     }
-
+    
     template<xtd::size len>
-    constexpr span(const type_t (&array)[len]) noexcept : data_ {array}, size_ {extent != dynamic_extent ? extent : len} {}
+    constexpr span(const type_t (&array)[len]) noexcept : data_ {array}, length_ {extent != dynamic_extent ? extent : len} {}
     template<xtd::size len>
-    constexpr span(type_t (&array)[len]) noexcept : data_ {array}, size_ {extent != dynamic_extent ? extent : len} {}
-
+    constexpr span(type_t (&array)[len]) noexcept : data_ {array}, length_ {extent != dynamic_extent ? extent : len} {}
+    
     template<class array_type_t, xtd::size len>
-    constexpr span(const std::array<array_type_t, len>& array) noexcept : data_ {const_cast<type_t*>(array.data())}, size_ {extent != dynamic_extent ? extent : len} {}
+    constexpr span(const std::array<array_type_t, len>& array) noexcept : data_ {const_cast<type_t*>(array.data())}, length_ {extent != dynamic_extent ? extent : len} {}
     template<class array_type_t, xtd::size len>
-    constexpr span(std::array<array_type_t, len>& array) noexcept : data_ {array.data()}, size_ {extent != dynamic_extent ? extent : len} {}
-
+    constexpr span(std::array<array_type_t, len>& array) noexcept : data_ {array.data()}, length_ {extent != dynamic_extent ? extent : len} {}
+    
     span(span&& items) = default;
     span(const span& items) = default;
-
+    
     span& operator =(span&& items) = default;
     span& operator =(const span& items) = default;
     /// @}
@@ -110,53 +111,57 @@ namespace xtd {
     /// @{
     const_reference back() const {
       if (empty()) throw argument_out_of_range_exception {};
-      return *(data_ + size_ - 1);
+      return *(data_ + length_ - 1);
     }
-
+    
     const_iterator begin() const {return cbegin();}
     iterator begin() {return iterator {data_};}
-
+    
     const_iterator cbegin() const {return const_iterator {data_};}
-    const_iterator cend() const {return const_iterator {data_ + size_};}
-
-    const_reverse_iterator crbegin() const {return const_reverse_iterator {data_ + size_};}
+    const_iterator cend() const {return const_iterator {data_ + length_};}
+    
+    const_reverse_iterator crbegin() const {return const_reverse_iterator {data_ + length_};}
     reverse_iterator crend() const {return reverse_iterator {data_};}
     
     constexpr const_pointer data() const noexcept {return data_;}
     
-    constexpr bool empty() const noexcept {return !size_;}
-
+    constexpr bool empty() const noexcept {return is_empty();}
+    
     const_iterator end() const {return cend();}
-    iterator end() {return iterator {data_ + size_};}
-
+    iterator end() {return iterator {data_ + length_};}
+    
     const_reference front() const {
       if (empty()) throw argument_out_of_range_exception {};
       return *data_;
     }
     
+    constexpr bool is_empty() const noexcept {return !length_;}
+
+    constexpr size_type length() const noexcept {return length_;}
+
     const_reverse_iterator rbegin() const {return crbegin();}
-    reverse_iterator rbegin() {return reverse_iterator {data_ + size_};}
+    reverse_iterator rbegin() {return reverse_iterator {data_ + length_};}
     
     const_reverse_iterator rend() const {return crend();}
     reverse_iterator rend() {return reverse_iterator {data_};}
-
-    constexpr size_type size() const noexcept {return size_;}
     
-    constexpr size_type size_bytes() const noexcept {return size_ * sizeof(value_type);}
+    constexpr size_type size() const noexcept {return length();}
+    
+    constexpr size_type size_bytes() const noexcept {return length_ * sizeof(value_type);}
     /// @}
     
     /// @name Public Methods
     
     /// @{
     const_reference at(size_type index) const {
-      if (index >= size_) throw argument_out_of_range_exception {};
+      if (index >= length_) throw argument_out_of_range_exception {};
       return *(data_ + index);
     }
     reference at(size_type index) {
-      if (index >= size_) throw argument_out_of_range_exception {};
+      if (index >= length_) throw argument_out_of_range_exception {};
       return *(data_ + index);
     }
-
+    
     void clear() noexcept {
       for (auto& item : *this)
         item = value_type {};
@@ -172,26 +177,29 @@ namespace xtd {
     
     template<xtd::size count__>
     span<element_type, count__> first() const {
-      if (count__ > size_) throw argument_out_of_range_exception {};
+      if (count__ > length_) throw argument_out_of_range_exception {};
       return span<element_type, count__> {data_, count__};
     }
     
     template<xtd::size count__>
     span<type_t, count__> last() const {
-      if (count__ > size_) throw argument_out_of_range_exception {};
-      return span<type_t, count__> {data_ + size_ - count__, count__};
+      if (count__ > length_) throw argument_out_of_range_exception {};
+      return span<type_t, count__> {data_ + length_ - count__, count__};
     }
     
     span<type_t> slice(size_type offset, size_type count) const {
-      if (offset + count > size_) throw argument_out_of_range_exception {};
+      if (offset + count > length_) throw argument_out_of_range_exception {};
       return span<type_t> {data_ + offset, count};
     }
     
     span<type_t> subspan(size_type offset, size_type count) const {
       return slice(offset, count);
     }
-
-    string to_string() const noexcept override {return xtd::string::format("[{}]", xtd::string::join(", ", *this));}
+    
+    string to_string() const noexcept override {
+      if (typeof_<type_t>() == typeof_<char>()) return xtd::string::join("", *this);
+      return xtd::string::format("[{}]", xtd::string::join(", ", *this));
+    }
     /// @}
     
     /// @name Public Operators
@@ -203,7 +211,7 @@ namespace xtd {
 
   private:
     pointer data_ = null;
-    size_type size_ = size_type {};
+    size_type length_ = size_type {};
   };
   
   /// @cond
