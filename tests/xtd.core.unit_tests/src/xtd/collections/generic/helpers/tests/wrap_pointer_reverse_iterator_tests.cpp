18c62950ff7216392b74569fcbc7cd70c4781e69
Gammasoft <gammasoft71@gmail.com>
2025-01-24 19:13:33 +0100

    Add xtd::collections::generic::helpers::wrap_pointer_iterator and xtd::collections::generic::helpers::wrap_pointer_reverse_iterator  unit tests

---
+++ tests/xtd.core.unit_tests/src/xtd/collections/generic/helpers/tests/wrap_pointer_reverse_iterator_tests.cpp
@@ -0,0 +1,73 @@
+#include <xtd/collections/generic/helpers/wrap_pointer_reverse_iterator>
+#include <xtd/as_const>
+#include <xtd/tunit/assert>
+#include <xtd/tunit/collection_assert>
+#include <xtd/tunit/test_class_attribute>
+#include <xtd/tunit/test_method_attribute>
+
+using namespace xtd;
+using namespace xtd::collections::generic::helpers;
+using namespace xtd::tunit;
+
+namespace xtd::collections::generic::helpers::tests {
+  class test_class_(wrap_pointer_reverse_iterator_tests) {
+    void test_method_(value_type) {
+      assert::are_equal(typeof_<int*>(), typeof_<wrap_pointer_reverse_iterator<int*>::value_type>());
+    }
+    
+    void test_method_(iterator_category) {
+      assert::are_equal(typeof_<std::forward_iterator_tag>(), typeof_<wrap_pointer_reverse_iterator<int*>::iterator_category>());
+    }
+    
+    void test_method_(difference_type) {
+      assert::are_equal(typeof_<xtd::ptrdiff>(), typeof_<wrap_pointer_reverse_iterator<int*>::difference_type>());
+    }
+    
+    void test_method_(pointer) {
+      assert::are_equal(typeof_<int**>(), typeof_<wrap_pointer_reverse_iterator<int*>::pointer>());
+    }
+    
+    void test_method_(const_pointer) {
+      assert::are_equal(typeof_<int* const*>(), typeof_<wrap_pointer_reverse_iterator<int*>::const_pointer>());
+    }
+    
+    void test_method_(reference) {
+      assert::are_equal(typeof_<int*&>(), typeof_<wrap_pointer_reverse_iterator<int*>::reference>());
+    }
+    
+    void test_method_(const_reference) {
+      assert::are_equal(typeof_<int* const&>(), typeof_<wrap_pointer_reverse_iterator<int*>::const_reference>());
+    }
+    
+    void test_method_(constructor_with_pointer) {
+      auto a = array {10, 20, 30, 40, 50};
+      auto i = wrap_pointer_reverse_iterator<int*> {a.data() + a.size()};
+      assert::are_equal(a.data() + a.size() - 1, i.data());
+    }
+    
+    void test_method_(data_const) {
+      auto a = array {10, 20, 30, 40, 50};
+      auto i = wrap_pointer_reverse_iterator<int*> {a.data() + a.size()};
+      assert::are_equal(a.data() + a.size() - 1, as_const(i.data()));
+    }
+
+    void test_method_(data) {
+      auto a = array {10, 20, 30, 40, 50};
+      auto i = wrap_pointer_reverse_iterator<int*> {a.data() + a.size()};
+      i.data() = as_const(i.data()) - 1;
+      assert::are_equal(a.data() + a.size() - 2, as_const(i.data()));
+    }
+
+    void test_method_(const_reference_operator) {
+      auto a = array {10, 20, 30, 40, 50};
+      auto i = wrap_pointer_reverse_iterator<int*> {a.data() + a.size()};
+      assert::are_equal(*(a.data() + a.size() - 1), *as_const(i));
+    }
+
+    void test_method_(reference_operator) {
+      auto a = array {10, 20, 30, 40, 50};
+      auto i = wrap_pointer_reverse_iterator<int*> {a.data() + a.size()};
+      assert::are_equal(*(a.data() + a.size() - 1), *i);
+    }
+  };
+}
