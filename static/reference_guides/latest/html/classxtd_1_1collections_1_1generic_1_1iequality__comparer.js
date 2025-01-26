ee27234b5a6d6997de92ed21764e8386cff7a4d1
gammasoft71 <gammasoft71@gmail.com>
2025-01-26 21:48:14 +0100

    Update latest reference guide

--- static/reference_guides/latest/html/classxtd_1_1collections_1_1generic_1_1iequality__comparer.js
+++
@@ -1,5 +0,0 @@
-var classxtd_1_1collections_1_1generic_1_1iequality__comparer =
-[
-    [ "equals", "classxtd_1_1collections_1_1generic_1_1iequality__comparer.html#a7c29799db6f25ad42078e7281a9828ea", null ],
-    [ "get_hash_code", "classxtd_1_1collections_1_1generic_1_1iequality__comparer.html#a06a1a48a6f69dd2eb1e59904213cb461", null ]
-];
--- static/reference_guides/latest/html/link__label__clicked__event__handler_8h.js
+++
@@ -1,4 +0,0 @@
-var link__label__clicked__event__handler_8h =
-[
-    [ "link_label_clicked_event_handler", "link__label__clicked__event__handler_8h.html#ga93db4aef76d50e8baa3b72de925206ab", null ]
-];
