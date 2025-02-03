933aae9abf21f99d35fcc4a7e3de10acf035b46f
Gammasoft <gammasoft71@gmail.com>
2025-02-03 21:15:12 +0100

    Fix __polymorphic_equator__

--- src/xtd.core/include/xtd/internal/__polymorphic_equator.hpp
+++ src/xtd.core/include/xtd/internal/__polymorphic_equator.hpp
@@ -21,8 +21,8 @@ struct __polymorphic_equator__ {};
 template<class type_t>
 struct __polymorphic_equator__<type_t, std::true_type> {
   bool operator()(const type_t& a, const type_t& b) const {
-    if (dynamic_cast<const xtd::iequatable<type_t>*>(&a)) return static_cast<const xtd::iequatable<type_t>&>(a).equals(b);
-    if (dynamic_cast<const xtd::object*>(&a)) return static_cast<const xtd::object&>(a).equals(static_cast<const xtd::object&>(b));
+    if (dynamic_cast<const xtd::iequatable<type_t>*>(&a)) return dynamic_cast<const xtd::iequatable<type_t>&>(a).equals(b);
+    if (dynamic_cast<const xtd::object*>(&a)) return dynamic_cast<const xtd::object&>(a).equals(dynamic_cast<const xtd::object&>(b));
     return std::equal_to<type_t> {}(a, b);
   }
 };
