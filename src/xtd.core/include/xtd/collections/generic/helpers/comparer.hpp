28e932420adf5db3da5b93295ff986e0428a1ba4
Gammasoft <gammasoft71@gmail.com>
2025-02-04 10:49:21 +0100

    using xtd::collections::generic::helpers::comparer class

--- src/xtd.core/include/xtd/collections/generic/helpers/comparer.hpp
+++ src/xtd.core/include/xtd/collections/generic/helpers/comparer.hpp
@@ -27,13 +27,6 @@ namespace xtd {
         /// @par Library
         /// xtd.core
         /// @ingroup xtd_core helpers_generic_collections
-        /// @remarks The associative containers xtd::collections::generic::sorted_set, xtd::collections::generic::sorted_multi_set, xtd::collections::generic::sorted_dictionary, xtd::collections::generic::sorted_multi_dictionary use specializations of the template xtd::collections::generic::helpers::comparer as the defaultcompare function.
-        /// @par Examples
-        /// The following example show how to use xtd::collections::generic::helpers::comparer with `std::unordered_map`.
-        /// ```cpp
-        /// auto key_values = std::map<xtd::date_time, xtd::string, xtd::collections::generic::helpers::comparer<xtd::date_time>, xtd::collections::generic::helpers::allocator<xtd::collections::generic::key_value_pair<const xtd::date_time, xtd::string>>> {};
-        /// key_values.insert({{1971, 1, 5}, "Birth date"});
-        /// ```
         template<class type_t = void>
         struct comparer {
           /// @name Public Aliases
@@ -44,18 +37,24 @@ namespace xtd {
           /// @brief Represents the second argument type.
           using second_argument_type = type_t;
           /// @brief Represents the result type.
-          using result_type = bool;
+          using result_type = int32;
           /// @}
           
           /// @name Public Operators
           
           /// @{
-          /// @brief Serves as a hash function for a specified key with a particular type (type_t).
-          /// @param key The key to hash.
-          /// @return A hash code for the spesified key.
-          /// @remarks If key_t inherits from xtd::object, the xtd::object::get_hash_code method will be used; otherwise, the [std::hash](https://en.cppreference.com/w/cpp/utility/hash) object function will be used.
-          constexpr result_type operator()(const first_argument_type& lhs, const second_argument_type& rhs) const {
-            return __polymorphic_comparer__<first_argument_type, typename std::is_polymorphic<first_argument_type>::type> {}(lhs, rhs);
+          /// @brief Compares two entities and returns a value indicating whether one is less than, equal to, or greater than the other.
+          /// @param x The first entity to compare.
+          /// @param y The second entity to compare.
+          /// @return A 32-bit signed integer that indicates the relative order of the entities being compared. The return value has these meanings:
+          ///
+          /// | Value             | Condition            |
+          /// |-------------------|----------------------|
+          /// | Less than zero    | x is less than y.    |
+          /// | Zero              | x equals y.          |
+          /// | Greater than zero | x is greater than y. |
+          constexpr result_type operator()(const first_argument_type& x, const second_argument_type& y) const {
+            return __polymorphic_comparer__<first_argument_type, typename std::is_polymorphic<first_argument_type>::type> {}(x, y);
           }
           /// @}
         };
