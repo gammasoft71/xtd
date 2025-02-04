36d854908b43db76a5e07f0d69b22162114d4aff
Gammasoft <gammasoft71@gmail.com>
2025-02-04 10:39:52 +0100

    Revue code

--- src/xtd.core/include/xtd/internal/__polymorphic_hasher.hpp
+++ src/xtd.core/include/xtd/internal/__polymorphic_hasher.hpp
@@ -15,23 +15,42 @@
 #include <type_traits>
 
 /// @cond
-template<class key_t, class bool_t>
-struct __polymorphic_hasher__ {};
+template<typename key_t, typename bool_t>
+struct __object_hasher__ {};
 
-template<class key_t>
-struct __polymorphic_hasher__<key_t, std::true_type> {
-  xtd::size operator()(const key_t& key) const {
-    if (dynamic_cast<const xtd::iequatable<key_t>*>(&key)) return dynamic_cast<const xtd::ihashable&>(key).get_hash_code();
-    if (dynamic_cast<const xtd::object*>(&key)) return dynamic_cast<const xtd::object&>(key).get_hash_code();
-    __throw_argument_exception(__FILE__, __LINE__, __func__);
-    return 0;
-  }
+template<typename key_t>
+struct __object_hasher__<key_t, std::true_type> {
+  xtd::size operator()(const key_t& key) const {return static_cast<const xtd::object&>(key).get_hash_code();}
 };
 
-template<class key_t>
+template<typename key_t>
+struct __object_hasher__<key_t, std::false_type> {
+  xtd::size operator()(const key_t& key) const {return std::hash<key_t> {}(key);}
+};
+
+template<typename key_t, typename bool_t>
+struct __ihashable_hasher__ {};
+
+template<typename key_t>
+struct __ihashable_hasher__<key_t, std::true_type> {
+  xtd::size operator()(const key_t& key) const {return static_cast<const xtd::ihashable&>(key).get_hash_code();}
+};
+
+template<typename key_t>
+struct __ihashable_hasher__<key_t, std::false_type> {
+  xtd::size operator()(const key_t& key) const {return __object_hasher__<key_t, typename std::is_base_of<xtd::object, key_t>::type> {}(key);}
+};
+
+template<typename key_t, typename bool_t>
+struct __polymorphic_hasher__ {};
+
+template<typename key_t>
+struct __polymorphic_hasher__<key_t, std::true_type> {
+  xtd::size operator()(const key_t& key) const {return __ihashable_hasher__<key_t, typename std::is_base_of<xtd::ihashable, key_t>::type> {}(key);}
+};
+
+template<typename key_t>
 struct __polymorphic_hasher__<key_t, std::false_type> {
-  xtd::size operator()(const key_t& key) const {
-    return std::hash<key_t> {}(key);
-  }
+  xtd::size operator()(const key_t& key) const {return std::hash<key_t> {}(key);}
 };
 /// @endcond
