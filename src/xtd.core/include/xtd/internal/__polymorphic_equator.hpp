46d6da8c006a567b8b62ac15fb30d213a10c5411
Gammasoft <gammasoft71@gmail.com>
2025-02-04 10:39:48 +0100

    Revue code

--- src/xtd.core/include/xtd/internal/__polymorphic_equator.hpp
+++ src/xtd.core/include/xtd/internal/__polymorphic_equator.hpp
@@ -15,16 +15,38 @@
 #include <type_traits>
 
 /// @cond
+template<typename type_t, typename bool_t>
+struct __object_equtor__ {};
+
+template<typename type_t>
+struct __object_equtor__<type_t, std::true_type> {
+  bool operator()(const type_t& a, const type_t& b) const {return static_cast<const xtd::object&>(a).equals(b);}
+};
+
+template<typename type_t>
+struct __object_equtor__<type_t, std::false_type> {
+  bool operator()(const type_t& a, const type_t& b) const {return std::equal_to<type_t> {}(a, b);}
+};
+
+template<typename type_t, typename bool_t>
+struct __iequatable_equator__ {};
+
+template<typename type_t>
+struct __iequatable_equator__<type_t, std::true_type> {
+  bool operator()(const type_t& a, const type_t& b) const {return static_cast<const xtd::iequatable<type_t>&>(a).equals(b);}
+};
+
+template<typename type_t>
+struct __iequatable_equator__<type_t, std::false_type> {
+  bool operator()(const type_t& a, const type_t& b) const {return __object_equtor__<type_t, typename std::is_base_of<xtd::object, type_t>::type> {}(a, b);}
+};
+
 template<class type_t, class bool_t>
 struct __polymorphic_equator__ {};
 
 template<class type_t>
 struct __polymorphic_equator__<type_t, std::true_type> {
-  bool operator()(const type_t& a, const type_t& b) const {
-    if (dynamic_cast<const xtd::iequatable<type_t>*>(&a)) return dynamic_cast<const xtd::iequatable<type_t>&>(a).equals(b);
-    if (dynamic_cast<const xtd::object*>(&a)) return dynamic_cast<const xtd::object&>(a).equals(dynamic_cast<const xtd::object&>(b));
-    return std::equal_to<type_t> {}(a, b);
-  }
+  bool operator()(const type_t& a, const type_t& b) const {return __iequatable_equator__<type_t, typename std::is_base_of<xtd::iequatable<type_t>, type_t>::type> {}(a, b);}
 };
 
 template<class type_t>
