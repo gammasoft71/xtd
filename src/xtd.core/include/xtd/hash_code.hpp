444661f12f0319d5a15373b66f4d7f76b25448b8
Gammasoft <gammasoft71@gmail.com>
2025-02-05 22:36:26 +0100

    Add documentation

--- src/xtd.core/include/xtd/hash_code.hpp
+++ src/xtd.core/include/xtd/hash_code.hpp
@@ -45,8 +45,13 @@ namespace xtd {
       return *this;
     }
     
+    /// @brief Determines whether the specified object is equal to the current object.
+    /// @param obj The object to compare with the current object.
+    /// @return `true` if the specified object is equal to the current object. otherwise, `false`.
     bool equals(const xtd::object& other) const noexcept override;
 
+    /// @brief Serves as a hash function for a particular type.
+    /// @return A hash code for the current object.
     xtd::size get_hash_code() const noexcept override;
 
     /// @brief Calculates the final hash code after consecutive xtd::hash_code::add invocations.
