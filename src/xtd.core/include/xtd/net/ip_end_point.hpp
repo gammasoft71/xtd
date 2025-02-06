5f947dbaa136e6b66ff007c6c19c7bc5c061660a
Gammasoft <gammasoft71@gmail.com>
2025-02-06 23:02:30 +0100

    td::net::ip_end_point inherits from iequatable interface

--- src/xtd.core/include/xtd/net/ip_end_point.hpp
+++ src/xtd.core/include/xtd/net/ip_end_point.hpp
@@ -4,13 +4,17 @@
 #pragma once
 #include "end_point.hpp"
 #include "ip_address.hpp"
+#include "../iequatable.hpp"
 
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
   /// @brief The xtd::net namespace provides a simple programming interface for many of the protocols used on networks today. The xtd::net::web_request and xtd::net::web_response classes form the basis of what are called pluggable protocols, an implementation of network services that enables you to develop applications that use Internet resources without worrying about the specific details of the individual protocols.
   namespace net {
     /// @brief Represents a network endpoint as an IP address and a port number.
-    /// @remarks The xtd::net::ip_end_point class contains the host and local or remote port information needed by an application to connect to a service on a host. By combining the host's IP address and port number of a service, the ip_end_point class forms a connection point to a service.
+    /// @par Definition
+    /// ```cpp
+    /// class core_export_ ip_end_point : public xtd::net::end_point, public xtd::iequatable<xtd::net::ip_end_point>;
+    /// ```
     /// @par Header
     /// ```cpp
     /// #include <xtd/net/ip_end_point>
@@ -20,7 +24,8 @@ namespace xtd {
     /// @par Library
     /// xtd.core
     /// @ingroup xtd_core
-    class core_export_ ip_end_point : public xtd::net::end_point {
+    /// @remarks The xtd::net::ip_end_point class contains the host and local or remote port information needed by an application to connect to a service on a host. By combining the host's IP address and port number of a service, the ip_end_point class forms a connection point to a service.
+    class core_export_ ip_end_point : public xtd::net::end_point, public xtd::iequatable<xtd::net::ip_end_point> {
     public:
       /// @name Public Fields
       
@@ -79,6 +84,19 @@ namespace xtd {
       /// @return A new end_point instance that is initialized from the specified xtd::net::socket_address instance.
       /// @exception xtd::not_supported_exception Any attempt is made to access the method when the method is not overridden in a descendant class.
       xtd::uptr<end_point> create(const xtd::net::socket_address& socket_address) const override;
+      
+      /// @brief Determines whether the specified object is equal to the current object.
+      /// @param obj The object to compare with the current object.
+      /// @return `true` if the specified object is equal to the current object. otherwise, `false`.
+      bool equals(const object& obj) const noexcept override;
+      /// @brief Determines whether the specified object is equal to the current object.
+      /// @param other The object to compare with the current object.
+      /// @return `true` if the specified object is equal to the current object. otherwise, `false`.
+      bool equals(const ip_end_point& other) const noexcept override;
+      
+      /// @brief Serves as a hash function for a particular type.
+      /// @return A hash code for the current object.
+      xtd::size get_hash_code() const noexcept override;
       
       /// @brief Serializes endpoint information into a socket_address instance.
       /// @return A new xtd::net::socket_address instance that contains the endpoint information.
