a923123b9b68863a62126bda05f48118130ab54e
Gammasoft <gammasoft71@gmail.com>
2025-02-06 20:36:24 +0100

    convert xtd::net:sockets::linger_option to struct

--- src/xtd.core/src/xtd/net/sockets/linger_option.cpp
+++ src/xtd.core/src/xtd/net/sockets/linger_option.cpp
@@ -4,23 +4,5 @@ using namespace xtd;
 using namespace xtd::net;
 using namespace xtd::net::sockets;
 
-linger_option::linger_option(bool enabled, uint32 linger_time) : enabled_(enabled), linger_time_(linger_time) {
-}
-
-bool linger_option::enabled() const noexcept {
-  return enabled_;
-}
-
-linger_option& linger_option::enabled(bool value) noexcept {
-  enabled_ = value;
-  return *this;
-}
-
-uint32 linger_option::linger_time() const noexcept {
-  return linger_time_;
-}
-
-linger_option& linger_option::linger_time(uint32 value) noexcept {
-  linger_time_ = value;
-  return *this;
+linger_option::linger_option(bool enabled, uint32 linger_time) : enabled {enabled}, linger_time {linger_time} {
 }
--- tests/xtd.core.unit_tests/src/xtd/net/sockets/tests/linger_option_tests.cpp
+++ tests/xtd.core.unit_tests/src/xtd/net/sockets/tests/linger_option_tests.cpp
@@ -12,28 +12,28 @@ namespace xtd::net::sockets::tests {
   class test_class_(linger_option_tests) {
     void test_method_(default_constructor) {
       linger_option linger;
-      assert::is_false(linger.enabled());
-      assert::is_zero(linger.linger_time());
+      assert::is_false(linger.enabled);
+      assert::is_zero(linger.linger_time);
     }
     
     void test_method_(constructor_wtih_enabled_and_linger_time) {
       linger_option linger(true, 5);
-      assert::is_true(linger.enabled());
-      assert::are_equal(5u, linger.linger_time());
+      assert::is_true(linger.enabled);
+      assert::are_equal(5u, linger.linger_time);
     }
     
     void test_method_(set_enabled) {
       linger_option linger;
-      linger.enabled(true);
-      assert::is_true(linger.enabled());
-      assert::is_zero(linger.linger_time());
+      linger.enabled = true;
+      assert::is_true(linger.enabled);
+      assert::is_zero(linger.linger_time);
     }
     
     void test_method_(set_linger_time) {
       linger_option linger;
-      linger.linger_time(5);
-      assert::is_false(linger.enabled());
-      assert::are_equal(5u, linger.linger_time());
+      linger.linger_time = 5;
+      assert::is_false(linger.enabled);
+      assert::are_equal(5u, linger.linger_time);
     }
   };
 }
