de2f57bc54eadebf2f81c18e7a439fee3054ccae
Gammasoft <gammasoft71@gmail.com>
2025-02-07 14:14:09 +0100

    inherits from iequatable interface and adds get_hash_code method

--- src/xtd.core/src/xtd/reflection/assembly_description_attribute.cpp
+++ src/xtd.core/src/xtd/reflection/assembly_description_attribute.cpp
@@ -14,6 +14,18 @@ const string& assembly_description_attribute::description() const noexcept {
   return description_;
 }
 
+bool assembly_description_attribute::equals(const object& obj) const noexcept {
+  return is<assembly_description_attribute>(obj) && equals(static_cast<const assembly_description_attribute&>(obj));
+}
+
+bool assembly_description_attribute::equals(const assembly_description_attribute& other) const noexcept {
+  return description_ == other.description_;
+}
+
+size assembly_description_attribute::get_hash_code() const noexcept {
+  return hash_code::combine(description_);
+}
+
 xtd::sptr<object> assembly_description_attribute::get_type_id() const noexcept {
   return xtd::guid::new_guid().memberwise_clone<xtd::guid>();
 }
