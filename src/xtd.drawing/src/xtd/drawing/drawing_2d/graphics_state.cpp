5a826de2f4386f2e3453748540fd36536e5644c7
Gammasoft <gammasoft71@gmail.com>
2025-02-07 18:39:54 +0100

    Add get_hash_code method

--- src/xtd.drawing/src/xtd/drawing/drawing_2d/graphics_state.cpp
+++ src/xtd.drawing/src/xtd/drawing/drawing_2d/graphics_state.cpp
@@ -1,7 +1,18 @@
 #include "../../../../include/xtd/drawing/drawing_2d/graphics_state.hpp"
+#include <xtd/hash_code>
+#include <xtd/is>
 
+using namespace xtd;
 using namespace xtd::drawing::drawing_2d;
 
-bool graphics_state::equals(const graphics_state& value) const noexcept {
-  return handle_ == value.handle_;
+bool graphics_state::equals(const object& obj) const noexcept {
+  return is<graphics_state>(obj) && equals(static_cast<const graphics_state&>(obj));
+}
+
+bool graphics_state::equals(const graphics_state& other) const noexcept {
+  return handle_ == other.handle_;
+}
+
+size graphics_state::get_hash_code() const noexcept {
+  return hash_code::combine(handle_);
 }
