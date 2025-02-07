3da2182867968335710515c167595790192ba4c1
Gammasoft <gammasoft71@gmail.com>
2025-02-07 14:15:31 +0100

    inherits from iequatable interface and adds get_hash_code method

--- src/xtd.core/src/xtd/reflection/assembly_version_attribute.cpp
+++ src/xtd.core/src/xtd/reflection/assembly_version_attribute.cpp
@@ -14,6 +14,18 @@ const string& assembly_version_attribute::version() const noexcept {
   return version_;
 }
 
+bool assembly_version_attribute::equals(const object& obj) const noexcept {
+  return is<assembly_version_attribute>(obj) && equals(static_cast<const assembly_version_attribute&>(obj));
+}
+
+bool assembly_version_attribute::equals(const assembly_version_attribute& other) const noexcept {
+  return version_ == other.version_;
+}
+
+size assembly_version_attribute::get_hash_code() const noexcept {
+  return hash_code::combine(version_);
+}
+
 xtd::sptr<object> assembly_version_attribute::get_type_id() const noexcept {
   return xtd::guid::new_guid().memberwise_clone<xtd::guid>();
 }
