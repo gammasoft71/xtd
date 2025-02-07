a901ca344eb0323aa48c1fd896456b3163009228
Gammasoft <gammasoft71@gmail.com>
2025-02-07 20:13:26 +0100

    convert xtd::drawing::imaging::bitmap_data to struct

--- src/xtd.drawing/src/xtd/drawing/bitmap.cpp
+++ src/xtd.drawing/src/xtd/drawing/bitmap.cpp
@@ -86,14 +86,21 @@ bitmap_data bitmap::lock_bits(const rectangle& rect, xtd::drawing::imaging::imag
 
 bitmap_data bitmap::lock_bits(const rectangle& rect, xtd::drawing::imaging::image_lock_mode flags, xtd::drawing::imaging::pixel_format format, const bitmap_data& data) {
   if (format == xtd::drawing::imaging::pixel_format::indexed || format == xtd::drawing::imaging::pixel_format::gdi) throw argument_exception {};
-  auto image_data_height = data.height();
-  auto image_data_pixel_format = static_cast<int32>(data.pixel_format());
-  auto image_data_reserved = data.reserved();
-  auto image_data_scan0 = data.scan0();
-  auto image_data_stride = data.stride();
-  auto image_data_width = data.width();
+  auto image_data_height = data.height;
+  auto image_data_pixel_format = static_cast<int32>(data.pixel_format);
+  auto image_data_reserved = data.reserved;
+  auto image_data_scan0 = data.scan0;
+  auto image_data_stride = data.stride;
+  auto image_data_width = data.width;
   native::image::lock_bits(handle(), rect.left(), rect.top(), rect.width, rect.height, static_cast<int32>(flags), static_cast<int32>(format), image_data_height, image_data_pixel_format, image_data_reserved, image_data_scan0, image_data_stride, image_data_width);
-  return bitmap_data().height(image_data_height).pixel_format(static_cast<xtd::drawing::imaging::pixel_format>(image_data_pixel_format)).reserved(image_data_reserved).scan0(image_data_scan0).stride(image_data_stride).width(image_data_width);
+  auto result = bitmap_data {};
+  result.height = image_data_height;
+  result.pixel_format = static_cast<xtd::drawing::imaging::pixel_format>(image_data_pixel_format);
+  result.reserved = image_data_reserved;
+  result.scan0 = image_data_scan0;
+  result.stride = image_data_stride;
+  result.width = image_data_width;
+  return result;
 }
 
 void bitmap::make_transparent() {
@@ -117,11 +124,11 @@ void bitmap::set_resolution(int32 x_dpi, int32 y_dpi) {
 }
 
 void bitmap::unlock_bits(const bitmap_data& data) {
-  auto image_data_height = data.height();
-  auto image_data_pixel_format = static_cast<int32>(data.pixel_format());
-  auto image_data_reserved = data.reserved();
-  auto image_data_scan0 = data.scan0();
-  auto image_data_stride = data.stride();
-  auto image_data_width = data.width();
+  auto image_data_height = data.height;
+  auto image_data_pixel_format = static_cast<int32>(data.pixel_format);
+  auto image_data_reserved = data.reserved;
+  auto image_data_scan0 = data.scan0;
+  auto image_data_stride = data.stride;
+  auto image_data_width = data.width;
   native::image::unlock_bits(handle(), image_data_height, image_data_pixel_format, image_data_reserved, image_data_scan0, image_data_stride, image_data_width);
 }
