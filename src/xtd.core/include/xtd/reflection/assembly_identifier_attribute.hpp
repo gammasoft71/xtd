d1d13fd81adeae7ac26a6dc9db79fc2aec085a86
Gammasoft <gammasoft71@gmail.com>
2025-02-07 14:14:46 +0100

    inherits from iequatable interface and adds get_hash_code method

--- src/xtd.core/include/xtd/reflection/assembly_identifier_attribute.hpp
+++ src/xtd.core/include/xtd/reflection/assembly_identifier_attribute.hpp
@@ -2,6 +2,7 @@
 /// @brief Contains xtd::reflection::assembly_identifier_attribute attribute.
 #pragma once
 #include "../attribute.hpp"
+#include "../iequatable.hpp"
 
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
@@ -9,7 +10,7 @@ namespace xtd {
   namespace reflection {
     /// @brief Specifies the identifier of the assembly being attributed.
     /// ```cpp
-    /// class core_export_ assembly_identifier_attribute final : public xtd::attribute
+    /// class core_export_ assembly_identifier_attribute final : public xtd::attribute, public xtd::iequatable<xtd::reflection::assembly_identifier_attribute>;
     /// ```
     /// @par Inheritance
     /// xtd::object → xtd::attribute → xtd::reflection::assembly_identifier_attribute
@@ -22,7 +23,7 @@ namespace xtd {
     /// @par Library
     /// xtd.core
     /// @ingroup xtd_core
-    class core_export_ assembly_identifier_attribute final : public xtd::attribute {
+    class core_export_ assembly_identifier_attribute final : public xtd::attribute, public xtd::iequatable<xtd::reflection::assembly_identifier_attribute> {
     public:
       /// @name Public Constructors
       
@@ -42,7 +43,24 @@ namespace xtd {
       /// @return A string containing the identifier information.
       const string& identifier() const noexcept;
       /// @}
+ 
+      /// @name Public Methods
       
+      /// @{
+      /// @brief Determines whether the specified object is equal to the current object.
+      /// @param obj The object to compare with the current object.
+      /// @return `true` if the specified object is equal to the current object. otherwise, `false`.
+      bool equals(const object& obj) const noexcept override;
+      /// @brief Determines whether the specified object is equal to the current object.
+      /// @param other The object to compare with the current object.
+      /// @return `true` if the specified object is equal to the current object. otherwise, `false`.
+      bool equals(const assembly_identifier_attribute& other) const noexcept override;
+      
+      /// @brief Serves as a hash function for a particular type.
+      /// @return A hash code for the current object.
+      xtd::size get_hash_code() const noexcept override;
+      /// @}
+
     protected:
       xtd::sptr<object> get_type_id() const noexcept override;
       
