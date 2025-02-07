93be77efec6457300707c39b831eae973a33bd9c
Gammasoft <gammasoft71@gmail.com>
2025-02-07 14:14:37 +0100

    inherits from iequatable interface and adds get_hash_code method

--- src/xtd.core/src/xtd/reflection/assembly_guid_attribute.cpp
+++ src/xtd.core/src/xtd/reflection/assembly_guid_attribute.cpp
@@ -14,6 +14,18 @@ const string& assembly_guid_attribute::guid() const noexcept {
   return guid_;
 }
 
+bool assembly_guid_attribute::equals(const object& obj) const noexcept {
+  return is<assembly_guid_attribute>(obj) && equals(static_cast<const assembly_guid_attribute&>(obj));
+}
+
+bool assembly_guid_attribute::equals(const assembly_guid_attribute& other) const noexcept {
+  return guid_ == other.guid_;
+}
+
+size assembly_guid_attribute::get_hash_code() const noexcept {
+  return hash_code::combine(guid_);
+}
+
 xtd::sptr<object> assembly_guid_attribute::get_type_id() const noexcept {
   return xtd::guid::new_guid().memberwise_clone<xtd::guid>();
 }
