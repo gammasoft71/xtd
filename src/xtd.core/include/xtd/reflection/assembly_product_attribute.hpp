193d7cf57c9a94b3ddca13f6b35f4b772ef95060
Gammasoft <gammasoft71@gmail.com>
2025-02-07 14:15:05 +0100

    inherits from iequatable interface and adds get_hash_code method

--- src/xtd.core/include/xtd/reflection/assembly_product_attribute.hpp
+++ src/xtd.core/include/xtd/reflection/assembly_product_attribute.hpp
@@ -2,6 +2,7 @@
 /// @brief Contains xtd::reflection::assembly_product_attribute attribute.
 #pragma once
 #include "../attribute.hpp"
+#include "../iequatable.hpp"
 
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
@@ -9,7 +10,7 @@ namespace xtd {
   namespace reflection {
     /// @brief Defines a product name custom attribute for an assembly manifest.
     /// ```cpp
-    /// class core_export_ assembly_product_attribute final : public xtd::attribute
+    /// class assembly_product_attribute final : public xtd::attribute, public xtd::iequatable<xtd::reflection::assembly_product_attribute>;
     /// ```
     /// @par Inheritance
     /// xtd::object → xtd::attribute → xtd::reflection::assembly_product_attribute
@@ -22,7 +23,7 @@ namespace xtd {
     /// @par Library
     /// xtd.core
     /// @ingroup xtd_core
-    class assembly_product_attribute final : public xtd::attribute {
+    class assembly_product_attribute final : public xtd::attribute, public xtd::iequatable<xtd::reflection::assembly_product_attribute> {
     public:
       /// @name Public Constructors
       
@@ -42,7 +43,24 @@ namespace xtd {
       /// @return A string containing the product information.
       const string& product() const noexcept;
       /// @}
+
+      /// @name Public Methods
       
+      /// @{
+      /// @brief Determines whether the specified object is equal to the current object.
+      /// @param obj The object to compare with the current object.
+      /// @return `true` if the specified object is equal to the current object. otherwise, `false`.
+      bool equals(const object& obj) const noexcept override;
+      /// @brief Determines whether the specified object is equal to the current object.
+      /// @param other The object to compare with the current object.
+      /// @return `true` if the specified object is equal to the current object. otherwise, `false`.
+      bool equals(const assembly_product_attribute& other) const noexcept override;
+      
+      /// @brief Serves as a hash function for a particular type.
+      /// @return A hash code for the current object.
+      xtd::size get_hash_code() const noexcept override;
+      /// @}
+
     protected:
       xtd::sptr<object> get_type_id() const noexcept override;
       
--- src/xtd.core/src/xtd/reflection/assembly_product_attribute.cpp
+++ src/xtd.core/src/xtd/reflection/assembly_product_attribute.cpp
@@ -14,6 +14,18 @@ const string& assembly_product_attribute::product() const noexcept {
   return product_;
 }
 
+bool assembly_product_attribute::equals(const object& obj) const noexcept {
+  return is<assembly_product_attribute>(obj) && equals(static_cast<const assembly_product_attribute&>(obj));
+}
+
+bool assembly_product_attribute::equals(const assembly_product_attribute& other) const noexcept {
+  return product_ == other.product_;
+}
+
+size assembly_product_attribute::get_hash_code() const noexcept {
+  return hash_code::combine(product_);
+}
+
 xtd::sptr<object> assembly_product_attribute::get_type_id() const noexcept {
   return xtd::guid::new_guid().memberwise_clone<xtd::guid>();
 }
