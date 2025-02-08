b9a2dc2954e87cfcbeccc5288806575ca0a3e249
Gammasoft <gammasoft71@gmail.com>
2025-02-08 12:09:20 +0100

    revue xtd::drawing::imaging::image_format code

--- src/xtd.drawing/include/xtd/drawing/imaging/image_format.hpp
+++ src/xtd.drawing/include/xtd/drawing/imaging/image_format.hpp
@@ -34,7 +34,7 @@ namespace xtd {
         /// @brief Initializes a new instance of the image_format class by using the specified guid structure.
         /// @param guid The guid structure that specifies a particular image format.
         /// @remarks Typically you use the static properties of the image_format class to specify a particular format such as Png or Jpeg, rather than constructing a new image_format.
-        explicit image_format(const guid& guid) : guid_(guid) {}
+        explicit image_format(const guid& guid) noexcept;
         /// @}
         
         /// @cond
@@ -47,7 +47,7 @@ namespace xtd {
         /// @{
         /// @brief Gets a guid structure that represents this image_format object.
         /// @return A guid structure that represents this image_format object.
-        const xtd::guid guid() const noexcept {return guid_;}
+        const xtd::guid guid() const noexcept;
         /// @}
 
         /// @name Public Static Properties
@@ -55,151 +55,172 @@ namespace xtd {
         /// @{
         /// @brief Gets the bitmap (BMP) image format.
         /// @return An image_format object that indicates the bitmap image format.
-        static image_format bmp() noexcept {return image_format(xtd::guid("b96b3cab-0728-11d3-9d7b-0000f81ef32e"));}
+        static image_format bmp() noexcept;
         
         /// @brief Gets the enhanced metafile (EMF) image format.
         /// @return An image_format object that indicates the enhanced metafile image format.
-        static image_format emf() noexcept {return image_format(xtd::guid("b96b3cac-0728-11d3-9d7b-0000f81ef32e"));}
+        static image_format emf() noexcept;
         
         /// @brief Gets the Exchangeable Image File (Exif) format.
         /// @return An image_format object that indicates the Exif format.
-        static image_format exif() noexcept {return image_format(xtd::guid("b96b3cb2-0728-11d3-9d7b-0000f81ef32e"));}
+        static image_format exif() noexcept;
         
         /// @brief Gets the Graphics Interchange Format (GIF) image format.
         /// @return An image_format object that indicates the GIF image format.
-        static image_format gif() noexcept {return image_format(xtd::guid("b96b3cb0-0728-11d3-9d7b-0000f81ef32e"));}
+        static image_format gif() noexcept;
         
         /// @brief Gets the windows icon (ico) image format.
         /// @return An image_format object that indicates the icon image format.
-        static image_format ico() noexcept {return image_format(xtd::guid("b96b3cb5-0728-11d3-9d7b-0000f81ef32e"));}
+        static image_format ico() noexcept;
         
         /// @brief Gets the Joint Photographic Experts Group (JPEG) image format.
         /// @return An image_format object that indicates the JOEG image format.
-        static image_format jpeg() noexcept {return image_format(xtd::guid("b96b3cae-0728-11d3-9d7b-0000f81ef32e"));}
+        static image_format jpeg() noexcept;
         
         /// @brief Gets the format of a bitmap in memory.
         /// @return An image_format object that indicates the bitmap memory image format.
-        static image_format memory_bmp() noexcept {return image_format(xtd::guid("b96b3caa-0728-11d3-9d7b-0000f81ef32e"));}
+        static image_format memory_bmp() noexcept;
         
         /// @brief Gets the W3C Portable Network Graphics (PNG) image format.
         /// @return An image_format object that indicates the PNG image format.
-        static image_format png() noexcept {return image_format(xtd::guid("b96b3caf-0728-11d3-9d7b-0000f81ef32e"));}
+        static image_format png() noexcept;
         
         /// @brief Gets the Tagged Image File Format (TIFF) image format.
         /// @return An image_format object that indicates the TIFF image format.
-        static image_format tiff() noexcept {return image_format(xtd::guid("b96b3cb1-0728-11d3-9d7b-0000f81ef32e"));}
+        static image_format tiff() noexcept;
         
         /// @brief Gets the Windows metafile (WMF) image format.
         /// @return An image_format object that indicates the WMF image format.
-        static image_format wmf() noexcept {return image_format(xtd::guid("b96b3cad-0728-11d3-9d7b-0000f81ef32e"));}
+        static image_format wmf() noexcept;
         
         /// @brief Gets the GIF memory format.
         /// @return An image_format object that indicates the GIF mempry image format.
-        static image_format memory_gif() noexcept {return image_format(xtd::guid("b96b3cb6-0728-11d3-9d7b-0000f81ef32e"));}
+        static image_format memory_gif() noexcept;
         
         /// @brief Gets the ICO memory format.
         /// @return An image_format object that indicates the icon memory image format.
-        static image_format memory_ico() noexcept {return image_format(xtd::guid("b96b3cb7-0728-11d3-9d7b-0000f81ef32e"));}
+        static image_format memory_ico() noexcept;
         
         /// @brief Gets the JPEG memory format.
         /// @return An image_format object that indicates the JPEG memory image format.
-        static image_format memory_jpeg() noexcept {return image_format(xtd::guid("b96b3cb8-0728-11d3-9d7b-0000f81ef32e"));}
+        static image_format memory_jpeg() noexcept;
         
         /// @brief Gets the PNG memory format.
         /// @return An image_format object that indicates the PNG memory image format.
-        static image_format memory_png() noexcept {return image_format(xtd::guid("b96b3cb9-0728-11d3-9d7b-0000f81ef32e"));}
+        static image_format memory_png() noexcept;
         
         /// @brief Gets the TIFF memory.
         /// @return An image_format object that indicates the TIFF memory image format.
-        static image_format memory_tiff() noexcept {return image_format(xtd::guid("b96b3cba-0728-11d3-9d7b-0000f81ef32e"));}
+        static image_format memory_tiff() noexcept;
         
         /// @brief Gets the cursor (CUR) image format.
         /// @return An image_format object that indicates the cursor image format.
-        static image_format cur() noexcept {return image_format(xtd::guid("b96b3cbb-0728-11d3-9d7b-0000f81ef32e"));}
+        static image_format cur() noexcept;
         
         /// @brief Gets the CUR memory.
         /// @return An image_format object that indicates the CUR memory image format.
-        static image_format memory_cur() noexcept {return image_format(xtd::guid("b96b3cbc-0728-11d3-9d7b-0000f81ef32e"));}
+        static image_format memory_cur() noexcept;
         
         /// @brief Gets the x bitmap (XBM) image format.
         /// @return An image_format object that indicates the XBM image format.
-        static image_format xbm() noexcept {return image_format(xtd::guid("b96b3cbd-0728-11d3-9d7b-0000f81ef32e"));}
+        static image_format xbm() noexcept;
         
         /// @brief Gets the XMB memory.
         /// @return An image_format object that indicates the XBM memory image format.
-        static image_format memory_xbm() noexcept {return image_format(xtd::guid("b96b3cbe-0728-11d3-9d7b-0000f81ef32e"));}
+        static image_format memory_xbm() noexcept;
         
         /// @brief Gets the x pixmap (XPM) image format.
         /// @return An image_format object that indicates the XPM image format.
-        static image_format xpm() noexcept {return image_format(xtd::guid("b96b3cbf-0728-11d3-9d7b-0000f81ef32e"));}
+        static image_format xpm() noexcept;
         
         /// @brief Gets the XPM memory.
         /// @return An image_format object that indicates the XPM memory image format.
-        static image_format memory_xpm() noexcept {return image_format(xtd::guid("b96b3cc0-0728-11d3-9d7b-0000f81ef32e"));}
+        static image_format memory_xpm() noexcept;
         
         /// @brief Gets the portable pixmap (PNM) image format.
         /// @return An image_format object that indicates the PNM image format.
-        static image_format pnm() noexcept {return image_format(xtd::guid("b96b3cc1-0728-11d3-9d7b-0000f81ef32e"));}
+        static image_format pnm() noexcept;
         
         /// @brief Gets the PNM memory.
         /// @return An image_format object that indicates the PNM memory image format.
-        static image_format memory_pnm() noexcept {return image_format(xtd::guid("b96b3cc2-0728-11d3-9d7b-0000f81ef32e"));}
+        static image_format memory_pnm() noexcept;
         
         /// @brief Gets the pcx (PCX) image format.
         /// @return An image_format object that indicates the PCX image format.
-        static image_format pcx() noexcept {return image_format(xtd::guid("b96b3cc3-0728-11d3-9d7b-0000f81ef32e"));}
+        static image_format pcx() noexcept;
         
         /// @brief Gets the PCX memory.
         /// @return An image_format object that indicates the PCX memory image format.
-        static image_format memory_pcx() noexcept {return image_format(xtd::guid("b96b3cc4-0728-11d3-9d7b-0000f81ef32e"));}
+        static image_format memory_pcx() noexcept;
         
         /// @brief Gets the apple pict (PICT) image format.
         /// @return An image_format object that indicates the PICT image format.
-        static image_format pict() noexcept {return image_format(xtd::guid("b96b3cc5-0728-11d3-9d7b-0000f81ef32e"));}
+        static image_format pict() noexcept;
         
         /// @brief Gets the PICT memory.
         /// @return An image_format object that indicates the PICT memory image format.
-        static image_format memory_pict() noexcept {return image_format(xtd::guid("b96b3cc6-0728-11d3-9d7b-0000f81ef32e"));}
+        static image_format memory_pict() noexcept;
         
         /// @brief Gets the apple icon (ICON) image format.
         /// @return An image_format object that indicates the ICON image format.
-        static image_format icon() noexcept {return image_format(xtd::guid("b96b3cc7-0728-11d3-9d7b-0000f81ef32e"));}
+        static image_format icon() noexcept;
         
         /// @brief Gets the ICON memory.
         /// @return An image_format object that indicates the ICOM memory image format.
-        static image_format memory_icon() noexcept {return image_format(xtd::guid("b96b3cc8-0728-11d3-9d7b-0000f81ef32e"));}
+        static image_format memory_icon() noexcept;
         
         /// @brief Gets the apple cursor (CURSOR) image format.
         /// @return An image_format object that indicates the CURSOR format.
-        static image_format cursor() noexcept {return image_format(xtd::guid("b96b3cc9-0728-11d3-9d7b-0000f81ef32e"));}
+        static image_format cursor() noexcept;
         
         /// @brief Gets the CURSOR memory.
         /// @return An image_format object that indicates the CURSOR memory image format.
-        static image_format memory_cursor() noexcept {return image_format(xtd::guid("b96b3cca-0728-11d3-9d7b-0000f81ef32e"));}
+        static image_format memory_cursor() noexcept;
         
         /// @brief Gets the animated file cursor (ANI) image format.
         /// @return An image_format object that indicates the ANI image format.
-        static image_format ani() noexcept {return image_format(xtd::guid("b96b3ccb-0728-11d3-9d7b-0000f81ef32e"));}
+        static image_format ani() noexcept;
         
         /// @brief Gets the iif (IIF) image format.
         /// @return An image_format object that indicates the IIF image format.
-        static image_format iif() noexcept {return image_format(xtd::guid("b96b3ccc-0728-11d3-9d7b-0000f81ef32e"));}
+        static image_format iif() noexcept;
         
         /// @brief Gets the truevision targa (TGA) image format.
         /// @return An image_format object that indicates the TGA image format.
-        static image_format tga() noexcept {return image_format(xtd::guid("b96b3ccd-0728-11d3-9d7b-0000f81ef32e"));}
+        static image_format tga() noexcept;
         /// @}
         
         /// @name Public Methods
         
         /// @{
-        int32 compare_to(const image_format& value) const noexcept override {return guid_ < value.guid_ ? -1 : guid_ > value.guid_ ? 1 : 0;}
-        using object::equals;
-        bool equals(const image_format& value) const noexcept override {return guid_ == value.guid_;}
+        /// @brief Compares the current instance with another object of the same type.
+        /// @param obj An object to compare with this instance.
+        /// @return A 32-bit signed integer that indicates the relative order of the objects being compared.
+        /// The return value has these meanings:
+        ///
+        /// | Value             | Condition                          |
+        /// | ----------------- | ---------------------------------- |
+        /// | Less than zero    | This instance is less than obj.    |
+        /// | Zero              | This instance is equal to obj.     |
+        /// | Greater than zero | This instance is greater than obj. |
+        int32 compare_to(const image_format& value) const noexcept override;
         
+        /// @brief Determines whether the specified object is equal to the current object.
+        /// @param obj The object to compare with the current object.
+        /// @return `true` if the specified object is equal to the current object. otherwise, `false`.
+        bool equals(const object& obj) const noexcept override;
+        /// @brief Determines whether the specified object is equal to the current object.
+        /// @param other The object to compare with the current object.
+        /// @return `true` if the specified object is equal to the current object. otherwise, `false`.
+        bool equals(const image_format& other) const noexcept override;
+        
+        /// @brief Serves as a hash function for a particular type.
+        /// @return A hash code for the current object.
+        xtd::size get_hash_code() const noexcept override;
+
         /// @brief Converts this image_format object to a human-readable string.
         /// @return A string that represents this image_format object.
-        xtd::string to_string() const noexcept override {return string::format("[image_format: {}]", guid_);}
+        xtd::string to_string() const noexcept override;
         /// @}
         
       private:
---
+++ src/xtd.drawing/src/xtd/drawing/imaging/image_format.cpp
@@ -0,0 +1,168 @@
+#include "../../../../include/xtd/drawing/imaging/image_format.hpp"
+
+using namespace xtd;
+using namespace xtd::drawing;
+using namespace xtd::drawing::imaging;
+
+image_format::image_format(const struct guid& guid) noexcept : guid_(guid) {
+}
+
+const struct guid image_format::guid() const noexcept {
+  return guid_;
+}
+
+image_format image_format::bmp() noexcept {
+  return image_format {xtd::guid {"b96b3cab-0728-11d3-9d7b-0000f81ef32e"}};
+}
+
+image_format image_format::emf() noexcept {
+  return image_format {xtd::guid {"b96b3cac-0728-11d3-9d7b-0000f81ef32e"}};
+}
+
+image_format image_format::exif() noexcept {
+  return image_format {xtd::guid {"b96b3cb2-0728-11d3-9d7b-0000f81ef32e"}};
+}
+
+image_format image_format::gif() noexcept {
+  return image_format {xtd::guid {"b96b3cb0-0728-11d3-9d7b-0000f81ef32e"}};
+}
+
+image_format image_format::ico() noexcept {
+  return image_format {xtd::guid {"b96b3cb5-0728-11d3-9d7b-0000f81ef32e"}};
+}
+
+image_format image_format::jpeg() noexcept {
+  return image_format {xtd::guid {"b96b3cae-0728-11d3-9d7b-0000f81ef32e"}};
+}
+
+image_format image_format::memory_bmp() noexcept {
+  return image_format {xtd::guid {"b96b3caa-0728-11d3-9d7b-0000f81ef32e"}};
+}
+
+image_format image_format::png() noexcept {
+  return image_format {xtd::guid {"b96b3caf-0728-11d3-9d7b-0000f81ef32e"}};
+}
+
+image_format image_format::tiff() noexcept {
+  return image_format {xtd::guid {"b96b3cb1-0728-11d3-9d7b-0000f81ef32e"}};
+}
+
+image_format image_format::wmf() noexcept {
+  return image_format {xtd::guid {"b96b3cad-0728-11d3-9d7b-0000f81ef32e"}};
+}
+
+image_format image_format::memory_gif() noexcept {
+  return image_format {xtd::guid {"b96b3cb6-0728-11d3-9d7b-0000f81ef32e"}};
+}
+
+image_format image_format::memory_ico() noexcept {
+  return image_format {xtd::guid {"b96b3cb7-0728-11d3-9d7b-0000f81ef32e"}};
+}
+
+image_format image_format::memory_jpeg() noexcept {
+  return image_format {xtd::guid {"b96b3cb8-0728-11d3-9d7b-0000f81ef32e"}};
+}
+
+image_format image_format::memory_png() noexcept {
+  return image_format {xtd::guid {"b96b3cb9-0728-11d3-9d7b-0000f81ef32e"}};
+}
+
+image_format image_format::memory_tiff() noexcept {
+  return image_format {xtd::guid {"b96b3cba-0728-11d3-9d7b-0000f81ef32e"}};
+}
+
+image_format image_format::cur() noexcept {
+  return image_format {xtd::guid {"b96b3cbb-0728-11d3-9d7b-0000f81ef32e"}};
+}
+
+image_format image_format::memory_cur() noexcept {
+  return image_format {xtd::guid {"b96b3cbc-0728-11d3-9d7b-0000f81ef32e"}};
+}
+
+image_format image_format::xbm() noexcept {
+  return image_format {xtd::guid {"b96b3cbd-0728-11d3-9d7b-0000f81ef32e"}};
+}
+
+image_format image_format::memory_xbm() noexcept {
+  return image_format {xtd::guid {"b96b3cbe-0728-11d3-9d7b-0000f81ef32e"}};
+}
+
+image_format image_format::xpm() noexcept {
+  return image_format {xtd::guid {"b96b3cbf-0728-11d3-9d7b-0000f81ef32e"}};
+}
+
+image_format image_format::memory_xpm() noexcept {
+  return image_format {xtd::guid {"b96b3cc0-0728-11d3-9d7b-0000f81ef32e"}};
+}
+
+image_format image_format::pnm() noexcept {
+  return image_format {xtd::guid {"b96b3cc1-0728-11d3-9d7b-0000f81ef32e"}};
+}
+
+image_format image_format::memory_pnm() noexcept {
+  return image_format {xtd::guid {"b96b3cc2-0728-11d3-9d7b-0000f81ef32e"}};
+}
+
+image_format image_format::pcx() noexcept {
+  return image_format {xtd::guid {"b96b3cc3-0728-11d3-9d7b-0000f81ef32e"}};
+}
+
+image_format image_format::memory_pcx() noexcept {
+  return image_format {xtd::guid {"b96b3cc4-0728-11d3-9d7b-0000f81ef32e"}};
+}
+
+image_format image_format::pict() noexcept {
+  return image_format {xtd::guid {"b96b3cc5-0728-11d3-9d7b-0000f81ef32e"}};
+}
+
+image_format image_format::memory_pict() noexcept {
+  return image_format {xtd::guid {"b96b3cc6-0728-11d3-9d7b-0000f81ef32e"}};
+}
+
+image_format image_format::icon() noexcept {
+  return image_format {xtd::guid {"b96b3cc7-0728-11d3-9d7b-0000f81ef32e"}};
+}
+
+image_format image_format::memory_icon() noexcept {
+  return image_format {xtd::guid {"b96b3cc8-0728-11d3-9d7b-0000f81ef32e"}};
+}
+
+image_format image_format::cursor() noexcept {
+  return image_format {xtd::guid {"b96b3cc9-0728-11d3-9d7b-0000f81ef32e"}};
+}
+
+image_format image_format::memory_cursor() noexcept {
+  return image_format {xtd::guid {"b96b3cca-0728-11d3-9d7b-0000f81ef32e"}};
+}
+
+image_format image_format::ani() noexcept {
+  return image_format {xtd::guid {"b96b3ccb-0728-11d3-9d7b-0000f81ef32e"}};
+}
+
+image_format image_format::iif() noexcept {
+  return image_format {xtd::guid {"b96b3ccc-0728-11d3-9d7b-0000f81ef32e"}};
+}
+
+image_format image_format::tga() noexcept {
+  return image_format {xtd::guid {"b96b3ccd-0728-11d3-9d7b-0000f81ef32e"}};
+}
+
+int32 image_format::compare_to(const image_format& value) const noexcept {
+  return guid_ < value.guid_ ? -1 : guid_ > value.guid_ ? 1 : 0;
+}
+
+bool image_format::equals(const object& obj) const noexcept {
+  return is<image_format>(obj) && equals(static_cast<const image_format&>(obj));
+}
+
+bool image_format::equals(const image_format& other) const noexcept {
+  return guid_ == other.guid_;
+}
+
+size image_format::get_hash_code() const noexcept {
+  return hash_code::combine(guid_);
+}
+
+string image_format::to_string() const noexcept {
+  return string::format("[image_format: {}]", guid_);
+}
