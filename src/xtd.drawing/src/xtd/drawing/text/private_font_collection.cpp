c4cfb8a1d2de2770961c616e67a9096f3ae6fd38
Gammasoft <gammasoft71@gmail.com>
2025-02-08 14:26:59 +0100

    using xtd::size instead size_t

--- src/xtd.drawing/src/xtd/drawing/text/private_font_collection.cpp
+++ src/xtd.drawing/src/xtd/drawing/text/private_font_collection.cpp
@@ -6,16 +6,15 @@
 #undef __XTD_DRAWING_NATIVE_LIBRARY__
 
 using namespace xtd;
-using namespace xtd::drawing;
 using namespace xtd::drawing::text;
 
 private_font_collection::private_font_collection() {
 }
 
-void private_font_collection::add_font_file(const xtd::string& filename) {
-  throw xtd::not_implemented_exception {};
+void private_font_collection::add_font_file(const string& filename) {
+  throw not_implemented_exception {};
 }
 
-void private_font_collection::add_memory_font(intptr memory, size_t length) {
-  throw xtd::not_implemented_exception {};
+void private_font_collection::add_memory_font(intptr memory, size length) {
+  throw not_implemented_exception {};
 }
