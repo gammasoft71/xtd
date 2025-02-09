28ea0e1e2deee9fb5c31381c348a789cecf9f96a
Gammasoft <gammasoft71@gmail.com>
2025-02-09 19:38:21 +0100

    Add get_hash_code method

--- src/xtd.drawing/include/xtd/drawing/pen.hpp
+++ src/xtd.drawing/include/xtd/drawing/pen.hpp
@@ -205,9 +205,19 @@ namespace xtd {
       /// @name Public Methods
       
       /// @{
-      using object::equals;
-      bool equals(const xtd::drawing::pen& value) const noexcept override;
+      /// @brief Determines whether the specified object is equal to the current object.
+      /// @param obj The object to compare with the current object.
+      /// @return `true` if the specified object is equal to the current object. otherwise, `false`.
+      bool equals(const object& obj) const noexcept override;
+      /// @brief Indicates whether the current object is equal to another object of the same type.
+      /// @param other An object to compare with this object.
+      /// @return `true` if the current object is equal to the other parameter; otherwise, `false`.
+      bool equals(const xtd::drawing::pen& other) const noexcept override;
       
+      /// @brief Serves as a hash function for a particular type.
+      /// @return A hash code for the current object.
+      xtd::size get_hash_code() const noexcept override;
+
       xtd::string to_string() const noexcept override;
       /// @}
       
