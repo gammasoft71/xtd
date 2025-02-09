ce8aa70a6585684ce6614316ba29a3c31131a8ca
Gammasoft <gammasoft71@gmail.com>
2025-02-09 19:37:59 +0100

    Add get_hash_code method

--- src/xtd.drawing/src/xtd/drawing/font_family.cpp
+++ src/xtd.drawing/src/xtd/drawing/font_family.cpp
@@ -8,15 +8,15 @@ using namespace xtd;
 using namespace xtd::drawing;
 
 struct font_family::data {
-  intptr handle_ = 0;
-  xtd::string name_;
+  intptr handle = 0;
+  xtd::string name;
 };
 
 font_family::font_family(const string& name) : data_(xtd::new_sptr<data>()) {
-  data_->name_ = name;
-  if (data_->name_.empty()) throw xtd::argument_exception("name is an empty string"_t);
-  data_->handle_ = native::font_family::create(name);
-  if (data_->handle_ == 0) throw xtd::argument_exception("name specifies a font that is not installed on the computer running the application."_t);
+  data_->name = name;
+  if (data_->name.empty()) throw xtd::argument_exception("name is an empty string"_t);
+  data_->handle = native::font_family::create(name);
+  if (data_->handle == 0) throw xtd::argument_exception("name specifies a font that is not installed on the computer running the application."_t);
 }
 
 font_family::font_family(text::generic_font_families generic_font_families) : data_(xtd::new_sptr<data>()) {
@@ -36,7 +36,7 @@ font_family::font_family(const string& name, const text::font_collection& font_c
 }
 
 font_family::font_family(const font_family& value) : data_(xtd::new_sptr<data>()) {
-  if (data_.use_count() == 1 && data_->handle_ != 0) native::font_family::destroy(data_->handle_);
+  if (data_.use_count() == 1 && data_->handle != 0) native::font_family::destroy(data_->handle);
   data_ = value.data_;
 }
 
@@ -45,13 +45,13 @@ font_family::font_family() : data_(xtd::new_sptr<data>()) {
 }
 
 font_family& font_family::operator =(const font_family& value) {
-  if (data_.use_count() == 1 && data_->handle_ != 0) native::font_family::destroy(data_->handle_);
+  if (data_.use_count() == 1 && data_->handle != 0) native::font_family::destroy(data_->handle);
   data_ = value.data_;
   return *this;
 }
 
 font_family::~font_family() {
-  if (data_.use_count() == 1 && data_->handle_ != 0) native::font_family::destroy(data_->handle_);
+  if (data_.use_count() == 1 && data_->handle != 0) native::font_family::destroy(data_->handle);
 }
 
 std::vector<font_family> font_family::families() noexcept {
@@ -71,37 +71,45 @@ font_family font_family::generic_serif() noexcept {
 }
 
 const xtd::string& font_family::name() const noexcept {
-  return data_->name_;
+  return data_->name;
 }
 
-bool font_family::equals(const font_family& value) const noexcept {
-  return data_->name_ == value.data_->name_;
+bool font_family::equals(const object& obj) const noexcept {
+  return is<font_family>(obj) && equals(static_cast<const font_family&>(obj));
+}
+
+bool font_family::equals(const font_family& other) const noexcept {
+  return data_->name == other.data_->name;
 }
 
 int32 font_family::get_cell_ascent(font_style style) const {
-  return native::font_family::get_cell_ascent(data_->handle_, get_em_height(style), (style & font_style::bold) == font_style::bold, (style & font_style::italic) == font_style::italic, (style & font_style::underline) == font_style::underline, (style & font_style::strikeout) == font_style::strikeout);
+  return native::font_family::get_cell_ascent(data_->handle, get_em_height(style), (style & font_style::bold) == font_style::bold, (style & font_style::italic) == font_style::italic, (style & font_style::underline) == font_style::underline, (style & font_style::strikeout) == font_style::strikeout);
 }
 
 int32 font_family::get_cell_descent(font_style style) const {
-  return native::font_family::get_cell_descent(data_->handle_, get_em_height(style), (style & font_style::bold) == font_style::bold, (style & font_style::italic) == font_style::italic, (style & font_style::underline) == font_style::underline, (style & font_style::strikeout) == font_style::strikeout);
+  return native::font_family::get_cell_descent(data_->handle, get_em_height(style), (style & font_style::bold) == font_style::bold, (style & font_style::italic) == font_style::italic, (style & font_style::underline) == font_style::underline, (style & font_style::strikeout) == font_style::strikeout);
 }
 
 int32 font_family::get_em_height(font_style style) const noexcept {
   return 2048;
 }
 
+size font_family::get_hash_code() const noexcept {
+  return hash_code::combine(data_->name);
+}
+
 int32 font_family::get_line_spacing(font_style style) const {
-  return native::font_family::get_line_spacing(data_->handle_, get_em_height(style), (style & font_style::bold) == font_style::bold, (style & font_style::italic) == font_style::italic, (style & font_style::underline) == font_style::underline, (style & font_style::strikeout) == font_style::strikeout);
+  return native::font_family::get_line_spacing(data_->handle, get_em_height(style), (style & font_style::bold) == font_style::bold, (style & font_style::italic) == font_style::italic, (style & font_style::underline) == font_style::underline, (style & font_style::strikeout) == font_style::strikeout);
 }
 
 string font_family::get_name(int32 language) const {
-  return native::font_family::get_name(data_->handle_, language);
+  return native::font_family::get_name(data_->handle, language);
 }
 
 bool font_family::is_style_available(font_style style) const {
-  return native::font_family::is_style_available(data_->handle_, (style & font_style::bold) == font_style::bold, (style & font_style::italic) == font_style::italic, (style & font_style::underline) == font_style::underline, (style & font_style::strikeout) == font_style::strikeout);
+  return native::font_family::is_style_available(data_->handle, (style & font_style::bold) == font_style::bold, (style & font_style::italic) == font_style::italic, (style & font_style::underline) == font_style::underline, (style & font_style::strikeout) == font_style::strikeout);
 }
 
 xtd::string font_family::to_string() const noexcept {
-  return string::format("[{}: name={}]", get_type().name(), data_->name_);
+  return string::format("[{}: name={}]", get_type().name(), data_->name);
 }
