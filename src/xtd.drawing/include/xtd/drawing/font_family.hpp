62130abca1135d56219657797043927c71970eda
Gammasoft <gammasoft71@gmail.com>
2025-02-09 19:37:59 +0100

    Add get_hash_code method

--- src/xtd.drawing/include/xtd/drawing/font_family.hpp
+++ src/xtd.drawing/include/xtd/drawing/font_family.hpp
@@ -96,8 +96,14 @@ namespace xtd {
       /// @name Public Methods
       
       /// @{
-      using object::equals;
-      bool equals(const font_family& value) const noexcept override;
+      /// @brief Determines whether the specified object is equal to the current object.
+      /// @param obj The object to compare with the current object.
+      /// @return `true` if the specified object is equal to the current object. otherwise, `false`.
+      bool equals(const xtd::object& obj) const noexcept override;
+      /// @brief Determines whether the specified object is equal to the current object.
+      /// @param other The object to compare with the current object.
+      /// @return `true` if the specified object is equal to the current object. otherwise, `false`.
+      bool equals(const font_family& other) const noexcept override;
       
       /// @brief Returns the cell ascent, in design units, of the font_family of the specified style.
       /// @param style A font_style that contains style information for the font.
@@ -113,7 +119,11 @@ namespace xtd {
       /// @param style A font_style that contains style information for the font.
       /// @return The height of the em square.
       int32 get_em_height(font_style style) const noexcept;
-      
+
+      /// @brief Serves as a hash function for a particular type.
+      /// @return A hash code for the current object.
+      xtd::size get_hash_code() const noexcept override;
+
       /// @brief Returns the line spacing, in design units, of the font_family of the specified style. The line spacing is the vertical distance between the base lines of two consecutive lines of text.
       /// @param style A font_style that contains style information for the font.
       /// @return The distance between two consecutive lines of text.
