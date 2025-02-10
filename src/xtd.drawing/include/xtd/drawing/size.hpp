455ac1604f585775e892cbd9589ed3c2f1ddab81
Gammasoft <gammasoft71@gmail.com>
2025-02-10 19:54:00 +0100

    Add get_hash_code method

--- src/xtd.drawing/include/xtd/drawing/size.hpp
+++ src/xtd.drawing/include/xtd/drawing/size.hpp
@@ -77,9 +77,19 @@ namespace xtd {
       /// @name Public Methods
       
       /// @{
-      using object::equals;
-      bool equals(const xtd::drawing::size& value) const noexcept override;
+      /// @brief Determines whether the specified object is equal to the current object.
+      /// @param obj The object to compare with the current object.
+      /// @return `true` if the specified object is equal to the current object. otherwise, `false`.
+      bool equals(const xtd::object& obj) const noexcept override;
+      /// @brief Determines whether the specified object is equal to the current object.
+      /// @param other The object to compare with the current object.
+      /// @return `true` if the specified object is equal to the current object. otherwise, `false`.
+      bool equals(const xtd::drawing::size& other) const noexcept override;
       
+      /// @brief Serves as a hash function for a particular type.
+      /// @return A hash code for the current object.
+      xtd::size get_hash_code() const noexcept override;
+
       /// @brief Creates a human-readable string that represents this xtd::drawing::size class.
       /// @return A xtd::string that represents this xtd::drawing::size.
       xtd::string to_string() const noexcept override;
