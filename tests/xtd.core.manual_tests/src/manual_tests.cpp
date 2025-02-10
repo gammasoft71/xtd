e6a387ad1ed5a585bb27d111b90293fd6d9ec884
Gammasoft <gammasoft71@gmail.com>
2025-02-10 20:30:13 +0100

    Add get_hash_code method

--- tests/xtd.core.manual_tests/src/manual_tests.cpp
+++ tests/xtd.core.manual_tests/src/manual_tests.cpp
@@ -1,13 +1,7 @@
 #include <xtd/xtd>
 
-using namespace xtd::collections;
-
 auto main() -> int {
-  auto items = dictionary<string, int> {};
-  items << key_value_pair<>::create("one"_s, 1) << key_value_pair<>::create("two"_s, 2);
-  println("std::pair = {}", std::make_pair("one", 1));
-  println("key_value_pair = {}", key_value_pair<>::create("one", 1));
-  println("items = {{{}}}", xtd::string::join(", ", items));
+  println("Hello, world!");
 }
 
 // This code can produces the following output:
