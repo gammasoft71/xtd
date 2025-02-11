1e2979d13a77e4c152f760f8e1d79c67ddceafd5
Gammasoft <gammasoft71@gmail.com>
2025-02-11 09:58:28 +0100

    Add get_hash_code method

--- src/xtd.forms/include/xtd/forms/style_sheets/background_image.hpp
+++ src/xtd.forms/include/xtd/forms/style_sheets/background_image.hpp
@@ -149,9 +149,19 @@ namespace xtd {
         /// @name Public Methods
         
         /// @{
-        using object::equals;
-        bool equals(const background_image& other) const noexcept override {return image_type_ == other.image_type_ && colors_ == other.colors_ && angle_ == other.angle_;}
-                
+        /// @brief Determines whether the specified object is equal to the current object.
+        /// @param obj The object to compare with the current object.
+        /// @return `true` if the specified object is equal to the current object. otherwise, `false`.
+        bool equals(const xtd::object& obj) const noexcept override;
+        /// @brief Determines whether the specified object is equal to the current object.
+        /// @param other The object to compare with the current object.
+        /// @return `true` if the specified object is equal to the current object. otherwise, `false`.
+        bool equals(const background_image& other) const noexcept override;
+        
+        /// @brief Serves as a hash function for a particular type.
+        /// @return A hash code for the current object.
+        xtd::size get_hash_code() const noexcept override;
+
         xtd::string to_string() const noexcept override;
         /// @}
 
