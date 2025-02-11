4f18af965a8aac620cc5bf9f863bb9035ea96950
Gammasoft <gammasoft71@gmail.com>
2025-02-11 10:31:26 +0100

    Add get_hash_code method

--- src/xtd.forms/include/xtd/forms/style_sheets/length.hpp
+++ src/xtd.forms/include/xtd/forms/style_sheets/length.hpp
@@ -125,9 +125,19 @@ namespace xtd {
         /// @name Public Methods
         
         /// @{
-        using object::equals;
+        /// @brief Determines whether the specified object is equal to the current object.
+        /// @param obj The object to compare with the current object.
+        /// @return `true` if the specified object is equal to the current object. otherwise, `false`.
+        bool equals(const xtd::object& obj) const noexcept override;
+        /// @brief Determines whether the specified object is equal to the current object.
+        /// @param other The object to compare with the current object.
+        /// @return `true` if the specified object is equal to the current object. otherwise, `false`.
         bool equals(const length& other) const noexcept override;
         
+        /// @brief Serves as a hash function for a particular type.
+        /// @return A hash code for the current object.
+        xtd::size get_hash_code() const noexcept override;
+
         /// @brief Gets the legnth in pixels.
         /// @param rect The xtd::drawing::rectangle is used to compute the length for relative length units.
         /// @return The legnth in pixels.
