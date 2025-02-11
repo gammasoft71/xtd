17ed37e263835c7cfb987d65c8e3aedd66ed7dcf
Gammasoft <gammasoft71@gmail.com>
2025-02-11 12:59:45 +0100

    Add get_hash_code method

--- src/xtd.forms/include/xtd/forms/domain_up_down.hpp
+++ src/xtd.forms/include/xtd/forms/domain_up_down.hpp
@@ -72,9 +72,31 @@ namespace xtd {
         /// @name Public Methods
         
         /// @{
+        /// @brief Compares the current instance with another object of the same type.
+        /// @param obj An object to compare with this instance.
+        /// @return A 32-bit signed integer that indicates the relative order of the objects being compared.
+        /// The return value has these meanings:
+        ///
+        /// | Value             | Condition                          |
+        /// | ----------------- | ---------------------------------- |
+        /// | Less than zero    | This instance is less than obj.    |
+        /// | Zero              | This instance is equal to obj.     |
+        /// | Greater than zero | This instance is greater than obj. |
         int32 compare_to(const item& value) const noexcept override;
-        using object::equals;
-        bool equals(const item& value) const noexcept override;
+
+        /// @brief Determines whether the specified object is equal to the current object.
+        /// @param obj The object to compare with the current object.
+        /// @return `true` if the specified object is equal to the current object. otherwise, `false`.
+        bool equals(const xtd::object& obj) const noexcept override;
+        /// @brief Determines whether the specified object is equal to the current object.
+        /// @param other The object to compare with the current object.
+        /// @return `true` if the specified object is equal to the current object. otherwise, `false`.
+        bool equals(const item& other) const noexcept override;
+        
+        /// @brief Serves as a hash function for a particular type.
+        /// @return A hash code for the current object.
+        xtd::size get_hash_code() const noexcept override;
+
         /// @brief Returns a string containing the vague of the item.
         /// @return A string containing the value of the item.
         xtd::string to_string() const noexcept override;
