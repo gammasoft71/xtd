f5793072663cbf0c5863683f901f275e28bc4ec9
Gammasoft <gammasoft71@gmail.com>
2025-02-12 11:24:18 +0100

    Add foreach_ keyword

--- src/xtd.core/include/xtd/collections/generic/extensions/enumerable.hpp
+++ src/xtd.core/include/xtd/collections/generic/extensions/enumerable.hpp
@@ -3,6 +3,7 @@
 /// @copyright Copyright (c) 2025 Gammasoft. All rights reserved.
 #pragma once
 #include "../../../linq/enumerable.hpp"
+#include "../../../linq/foreach.hpp"
 
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
