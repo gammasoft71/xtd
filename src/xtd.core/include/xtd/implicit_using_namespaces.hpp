eec51925ed4f623bc0e2e7d16ed29818fe2e5048
Gammasoft <gammasoft71@gmail.com>
2025-02-16 10:39:02 +0100

    Update implicit_using_namespaces.hpp

--- src/xtd.core/include/xtd/implicit_using_namespaces.hpp
+++ src/xtd.core/include/xtd/implicit_using_namespaces.hpp
@@ -6,7 +6,9 @@
 
 /// @cond
 #if defined(__XTD_ENABLE_IMPLICIT_USING_NAMESPACES__)
-#define __using_namespace__(name_space) namespace name_space {} using namespace name_space
+#define __using_namespace__(name_space) \
+  namespace name_space {} \
+  using namespace name_space
 
 #if __XTD_CURRENT_TARGET_ID__ == __CMAKE_TARGET_ID_TEST_APPLICATION__ || __XTD_CURRENT_TARGET_ID__ == __XTD_TARGET_ID_CONSOLE_APPLICATION__ || __XTD_CURRENT_TARGET_ID__ == __XTD_TARGET_ID_GUI_APPLICATION__ || __XTD_CURRENT_TARGET_ID__ == __XTD_TARGET_ID_UNKNOWN__
 __using_namespace__(xtd);
