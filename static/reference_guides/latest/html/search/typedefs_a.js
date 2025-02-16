9052175bce916b5a206182feb6ce15e4475bf682
gammasoft71 <gammasoft71@gmail.com>
2025-02-16 21:00:22 +0100

    update latest reference guide

---
+++ static/reference_guides/latest/html/search/typedefs_a.js
@@ -0,0 +1,18 @@
+var searchData=
+[
+  ['label_0',['label',['../group__style__sheets.html#ga0095c55e57882e69ada99760a885e216',1,'xtd::forms::style_sheets']]],
+  ['label_5fstate_1',['label_state',['../group__xtd__forms.html#gafed99743dcad627755f7bbee0d3cf6f1',1,'xtd::forms::visual_styles']]],
+  ['labels_5ft_2',['labels_t',['../classxtd_1_1forms_1_1style__sheets_1_1style__sheet.html#a9fad2fb991b71077b5613cba2f7d18e6',1,'xtd::forms::style_sheets::style_sheet']]],
+  ['linear_5fgradient_5fbrush_3',['linear_gradient_brush',['../namespacextd_1_1drawing_1_1drawing2d.html#ac914892584a317dca5c48791232bc3f6',1,'xtd::drawing::drawing2d']]],
+  ['linear_5fgradient_5fmode_4',['linear_gradient_mode',['../namespacextd_1_1drawing_1_1drawing2d.html#af2908fcb248439bbc3141c3c96ddf6e2',1,'xtd::drawing::drawing2d']]],
+  ['link_5',['link',['../classxtd_1_1forms_1_1link__label.html#a0ec39d1624a179c1aa60b3a5ddcab4cb',1,'xtd::forms::link_label']]],
+  ['link_5flabel_6',['link_label',['../group__style__sheets.html#ga6e21de97748b7cb3eb1f0069de0b1500',1,'xtd::forms::style_sheets']]],
+  ['link_5flabel_5fclicked_5fevent_5fhandler_7',['link_label_clicked_event_handler',['../group__events.html#ga9568aa2f74ef05b1758f1dba2c769c8d',1,'xtd::forms']]],
+  ['link_5flabel_5fstate_8',['link_label_state',['../group__xtd__forms.html#gab8d7ac770488755bdbec24d02bc84983',1,'xtd::forms::visual_styles']]],
+  ['link_5flabels_5ft_9',['link_labels_t',['../classxtd_1_1forms_1_1style__sheets_1_1style__sheet.html#a85e8986defa85032d79151580f46504d',1,'xtd::forms::style_sheets::style_sheet']]],
+  ['linked_5flist_10',['linked_list',['../group__generic__collections.html#ga52295a51ccfe37b2a4192b073ce237b9',1,'xtd::collections::generic']]],
+  ['list_11',['list',['../classxtd_1_1collections_1_1generic_1_1extensions_1_1enumerable.html#a417e4adb1792949e9a54516bee2ca3d2',1,'xtd::collections::generic::extensions::enumerable::list'],['../classxtd_1_1linq_1_1enumerable.html#a4ff59be55b7be66d275f28741e2f1f0a',1,'xtd::linq::enumerable::list']]],
+  ['listener_5fcollection_12',['listener_collection',['../classxtd_1_1diagnostics_1_1debug.html#a5f2023248b2a37b78e2b4f364c29909a',1,'xtd::diagnostics::debug::listener_collection'],['../classxtd_1_1diagnostics_1_1trace.html#a951774bf53b93b1c0a428bb3773953e2',1,'xtd::diagnostics::trace::listener_collection']]],
+  ['local_5fiterator_13',['local_iterator',['../classxtd_1_1collections_1_1generic_1_1dictionary.html#adc966a9b4269ce145604c61eace7f57c',1,'xtd::collections::generic::dictionary']]],
+  ['lock_14',['lock',['../group__threading.html#ga90b16e4ed2e4bde4276f0c807e5a7112',1,'xtd']]]
+];
