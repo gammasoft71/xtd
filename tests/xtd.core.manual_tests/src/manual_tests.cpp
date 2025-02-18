ecf43ff0321ea2047a945f4f2ec63babe724295e
Gammasoft <gammasoft71@gmail.com>
2025-02-18 09:32:27 +0100

    Update manual_tests.cpp

--- tests/xtd.core.manual_tests/src/manual_tests.cpp
+++ tests/xtd.core.manual_tests/src/manual_tests.cpp
@@ -1,13 +1,15 @@
 #include <xtd/xtd>
 
-class manual_tests static_ {
-public:
-  static auto main(const auto& args) {
-    console::write_line("Hello, world!");
-  }
-};
+namespace manual_tests {
+  class program static_ {
+  public:
+    static auto main(const auto& args) {
+      console::write_line("Hello, world!");
+    }
+  };
+}
 
-startup_(manual_tests::main);
+startup_(manual_tests::program::main);
 
 // This code can produces the following output:
 //
