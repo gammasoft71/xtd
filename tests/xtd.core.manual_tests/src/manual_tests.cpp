85022f189a39e9c3efff095194fd7ed25b03b6f2
Gammasoft <gammasoft71@gmail.com>
2025-02-20 22:03:45 +0100

    Update .gitignore

--- tests/xtd.core.manual_tests/src/manual_tests.cpp
+++ tests/xtd.core.manual_tests/src/manual_tests.cpp
@@ -1,10 +1,40 @@
 #include <xtd/xtd>
 
+using namespace xtd::drawing;
+
 namespace manual_tests {
   class program static_ {
   public:
     static auto main(const auto& args) {
-      console::write_line("Hello, world!");
+      //console::write_line("Hello, world!");
+      foreach_(auto color, colors::get_colors()) {
+        println("  void test_method_({}) {{", color.name());
+        println("    auto c = colors::{}();", color.name());
+        println("    ");
+        println("    assert::are_equal(colors::{}(), c);", color.name());
+        println("    assert::are_equal(color::from_known_color(known_color::{}), c);", color.name());
+        println("    assert::are_not_equal(color(), c);");
+        println("    assert::are_not_equal(color::empty, c);");
+        println("    ");
+        println("    assert::are_equal(0x{:X2}, c.a());", color.a());
+        println("    assert::are_equal(0x{:X2}, c.r());", color.r());
+        println("    assert::are_equal(0x{:X2}, c.g());", color.g());
+        println("    assert::are_equal(0x{:X2}, c.b());", color.b());
+        println("    assert::are_equal(0, c.handle());");
+        println("    assert::are_equal(\"{}\", c.name());", color.name());
+        println("    ");
+        println("    assert::are_equal(\"color [{}]\", c.to_string());", color.name());
+        println("    ");
+        println("    assert::is_false(c.is_empty());");
+        println("    assert::is_true(c.is_known_color());");
+        println("    assert::is_false(c.is_system_color());");
+        println("    assert::is_true(c.is_named_color());");
+        println("    ");
+        println("    assert::are_equal(0x{:X8}, c.to_argb());", color.to_argb());
+        println("    assert::are_equal(known_color::{}, c.to_known_color());", color.name());
+        println("  }}");
+        println("  ");
+      }
     }
   };
 }
