83f816975963689b4dda0b476547aa0fc1ef7409
Gammasoft <gammasoft71@gmail.com>
2025-02-24 21:00:47 +0100

    Update target_properties.cmake

--- tools/xtdc-gui/properties/target_properties.cmake
+++ tools/xtdc-gui/properties/target_properties.cmake
@@ -1,5 +1,5 @@
 target_categories(Development)
 target_default_namespace("xtdc_gui")
-target_icon(resources/xtdc-gui)
+target_icon("resources/xtdc-gui")
 target_display(ON)
-target_startup("xtdc_gui::main_form::main" src/main_form.hpp)
+target_startup(xtdc_gui::main_form::main "src/main_form.hpp")
