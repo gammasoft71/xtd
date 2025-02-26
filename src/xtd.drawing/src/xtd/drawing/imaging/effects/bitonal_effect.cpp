e70042b9b9443bdc23bddac71fcf1942bda2689e
Gammasoft <gammasoft71@gmail.com>
2025-02-26 21:04:17 +0100

    Add xtd::drawing::imaging::effects::bitonal_effect struct

---
+++ src/xtd.drawing/src/xtd/drawing/imaging/effects/bitonal_effect.cpp
@@ -0,0 +1,30 @@
+#include "../../../../../include/xtd/drawing/helpers/alpha.hpp"
+#include "../../../../../include/xtd/drawing/helpers/rgb.hpp"
+#include "../../../../../include/xtd/drawing/imaging/effects/bitonal_effect.hpp"
+#include "../../../../../include/xtd/drawing/bitmap.hpp"
+
+using namespace xtd;
+using namespace xtd::drawing;
+using namespace xtd::drawing::imaging;
+using namespace xtd::drawing::imaging::effects;
+
+bitonal_effect::bitonal_effect(int32 threshold, color upper_color, color lower_color) : threshold {threshold}, upper_color {upper_color}, lower_color {lower_color} {
+}
+
+void bitonal_effect::apply(xtd::drawing::graphics& graphics, const xtd::drawing::rectangle& rectangle) const {
+  auto threshold = std::clamp(this->threshold, 0, 3 * byte_object::max_value);
+  auto image = bitmap {rectangle.size()};
+  auto img_graphics = image.create_graphics();
+  img_graphics.copy_from_graphics(graphics, rectangle.location(), {0, 0}, rectangle.size());
+  
+  auto alpha = reinterpret_cast<helpers::alpha*>(image.alpha());
+  auto rgb = reinterpret_cast<helpers::rgb*>(image.rgb());
+  for (auto y = 0; y < image.height(); ++y)
+    for (auto x = 0; x < image.width(); ++x) {
+      auto pixel = y * image.width() + x;
+      if (!alpha[pixel].a) continue;
+      auto lower = rgb[pixel].r + rgb[pixel].g + rgb[pixel].b <= threshold;
+      rgb[pixel] = lower ? helpers::rgb {lower_color.r(), lower_color.g(), lower_color.b()} : helpers::rgb {upper_color.r(), upper_color.g(), upper_color.b()};
+    }
+  graphics.draw_image(image, rectangle);
+}
