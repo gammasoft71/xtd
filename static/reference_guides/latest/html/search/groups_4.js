b8d2a0d195419be5ad053b5513cf4dcb49d5426c
gammasoft71 <gammasoft71@gmail.com>
2025-02-26 21:00:31 +0100

    update latest reference guide

--- static/reference_guides/latest/html/search/groups_4.js
+++ static/reference_guides/latest/html/search/groups_4.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['events_0',['events',['../group__events.html',1,'']]],
-  ['exceptions_1',['exceptions',['../group__exceptions.html',1,'']]],
-  ['extensions_2',['extensions',['../group__extensions.html',1,'(Global Namespace)'],['../group__extensions__generic__collections.html',1,'(Global Namespace)']]]
+  ['effects_0',['effects',['../group__effects.html',1,'']]],
+  ['events_1',['events',['../group__events.html',1,'']]],
+  ['exceptions_2',['exceptions',['../group__exceptions.html',1,'']]],
+  ['extensions_3',['extensions',['../group__extensions.html',1,'(Global Namespace)'],['../group__extensions__generic__collections.html',1,'(Global Namespace)']]]
 ];
