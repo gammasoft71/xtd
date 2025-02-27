6961971e72c05b1af772cca508358f9fb052d587
Gammasoft <gammasoft71@gmail.com>
2025-02-27 20:48:11 +0100

    Fix exception

--- src/xtd.forms/src/xtd/forms/numeric_up_down.cpp
+++ src/xtd.forms/src/xtd/forms/numeric_up_down.cpp
@@ -416,7 +416,7 @@ void numeric_up_down::on_value_changed(const event_args& e) {
 void numeric_up_down::on_lost_focus(const event_args& e) {
   up_down_base::on_lost_focus(e);
   auto text_value = .0;
-  if (try_parse<double>(text().chars(), text_value, number_styles::number))
+  if (!text().empty() && try_parse<double>(text().chars(), text_value, number_styles::number))
     value(text_value);
 }
 
