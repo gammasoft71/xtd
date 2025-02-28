5f5ec8d3bd58acf616da935348e4b7689fa03750
Gammasoft <gammasoft71@gmail.com>
2025-02-28 20:22:46 +0100

    Update color_effect.cpp

--- src/xtd.drawing/src/xtd/drawing/imaging/effects/color_effect.cpp
+++ src/xtd.drawing/src/xtd/drawing/imaging/effects/color_effect.cpp
@@ -20,6 +20,7 @@ void color_effect::apply(xtd::drawing::image& image) const {
   for (auto y = 0; y < image.height(); ++y)
     for (auto x = 0; x < image.width(); ++x) {
       auto pixel = y * image.width() + x;
+      if (!alpha[pixel].a) continue;
       auto color_r = helpers::rgb::alpha_blend(rgb[pixel].r, static_cast<byte>(std::clamp(static_cast<int32>(rgb[pixel].r) + color.r(), 0, 255)), percent);
       auto color_g = helpers::rgb::alpha_blend(rgb[pixel].g, static_cast<byte>(std::clamp(static_cast<int32>(rgb[pixel].g) + color.g(), 0, 255)), percent);
       auto color_b = helpers::rgb::alpha_blend(rgb[pixel].b, static_cast<byte>(std::clamp(static_cast<int32>(rgb[pixel].b) + color.b(), 0, 255)), percent);
