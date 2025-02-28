1921410b5e8d9b4f09e8518f21605e5e4c1ca227
Gammasoft <gammasoft71@gmail.com>
2025-02-28 20:13:14 +0100

    Update documentation

--- src/xtd.drawing/include/xtd/drawing/imaging/effects/brightness_effect.hpp
+++ src/xtd.drawing/include/xtd/drawing/imaging/effects/brightness_effect.hpp
@@ -28,7 +28,7 @@ namespace xtd {
         /// The following example code shows hot tu use xtd::drawing::imaging::effects::brightness_effect with image.
         /// ```cpp
         /// const auto img = image::from_file("ball.png");
-        /// auto result = image_effector::set_effect(img, brightness_effect {10});
+        /// auto result = image_effector::set_effect(img, brightness_effect {125});
         /// result.save("ball_blur.png");
         /// ```
         struct brightness_effect : effect {
