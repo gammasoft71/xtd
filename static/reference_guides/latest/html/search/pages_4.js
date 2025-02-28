894668a241f3405fc107fcf1e969bb8033182265
gammasoft71 <gammasoft71@gmail.com>
2025-02-28 21:00:25 +0100

    update latest reference guide

--- static/reference_guides/latest/html/search/pages_4.js
+++ static/reference_guides/latest/html/search/pages_4.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['image_20conversions_0',['Image conversions',['../image_conversions_page.html',1,'']]],
+  ['image_20effects_0',['Image effects',['../image_effects_page.html',1,'']]],
   ['images_20appearances_1',['Images appearances',['../_images_appearances_page.html',1,'']]]
 ];
