db758db56c3d64866271ad67decd1be5348e7573
Gammasoft <gammasoft71@gmail.com>
2025-02-28 12:28:28 +0100

    Update posterize_effect.hpp

--- src/xtd.drawing/include/xtd/drawing/imaging/effects/posterize_effect.hpp
+++ src/xtd.drawing/include/xtd/drawing/imaging/effects/posterize_effect.hpp
@@ -14,7 +14,7 @@ namespace xtd {
     namespace imaging {
       /// @brief The xtd::drawing::imaging::effects namespace provides functionality for adding effects to images.
       namespace effects {
-        /// @brief Represents the base class for all effects.
+        /// @brief The xtd::drawing::imaging::effects::posterize_effect converts image into posterized colors.
         /// @par Namespace
         /// xtd::drawing::imaging::effects
         /// @par Library
