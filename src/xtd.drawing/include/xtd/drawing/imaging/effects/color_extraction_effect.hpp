f904c7de7ee8d6b0bcbd1277a29a057230968ce1
Gammasoft <gammasoft71@gmail.com>
2025-03-01 10:40:41 +0100

    Add xtd::drawing::imaging::effects::color_extraction_effect effect

---
+++ src/xtd.drawing/include/xtd/drawing/imaging/effects/color_extraction_effect.hpp
@@ -0,0 +1,77 @@
+/// @file
+/// @brief Contains xtd::drawing::imaging::effects::color_extraction_effect struct.
+/// @copyright Copyright (c) 2025 Gammasoft. All rights reserved.
+#pragma once
+#include "effect.hpp"
+#include "../../color.hpp"
+
+/// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
+namespace xtd {
+  /// @brief The xtd::drawing namespace provides access to GDI+ basic graphics functionality. More advanced functionality is provided in the xtd::drawing::drawing_2d, xtd::drawing::imaging, and xtd::drawing::text namespaces.
+  namespace drawing {
+    /// @brief The xtd::drawing::imaging namespace provides advanced GDI+ imaging functionality. Basic graphics functionality is provided by the xtd.drawing namespace.
+    /// @remarks The metafile class provides methods for recording and saving metafiles. The encoder class enables users to extend GDI+ to support any image format. The property_item class provides methods for storing and retrieving metadata in image files.
+    namespace imaging {
+      /// @brief The xtd::drawing::imaging::effects namespace provides functionality for adding effects to images.
+      namespace effects {
+        /// @brief The xtd::drawing::imaging::effects::color_extraction_effect filters image with extraction color and replaces all other colors with other pixels color.
+        /// @par Namespace
+        /// xtd::drawing::imaging::effects
+        /// @par Library
+        /// xtd.drawing
+        /// @ingroup xtd_drawing effects
+        /// @par Appearance
+        /// | Original             | Converted                          |
+        /// | -------------------- | ---------------------------------- |
+        /// | @image html ball.png | @image html ball_color_extract.png |
+        /// @par Examples
+        /// The following example code shows hot tu use xtd::drawing::imaging::effects::color_extraction_effect with image.
+        /// ```cpp
+        /// const auto img = image::from_file("ball.png");
+        /// auto result = image_effector::set_effect(img, color_extraction_effect {260, color::green, color::white});
+        /// result.save("ball_color_extraction.png");
+        /// ```
+        struct color_extraction_effect : effect {
+          /// @name Public Constructors
+          
+          /// @{
+          /// @brief Initialise the new instance of xtd::drawing::imaging::color_extraction_effect object.
+          color_extraction_effect() = default;
+          /// @brief Initialise the new instance of xtd::drawing::imaging::color_extraction_effect object with specified threshold, upper color, and lower color.
+          /// @param threshold The threshold value (from 0 to 765).
+          /// @param extraction_color The color used when RGB color is upper the threshold value.
+          /// @param other_pixels_color The color used when RGB color is under or equal the threshold value.
+          color_extraction_effect(int32 threshold, color extraction_color, color other_pixels_color);
+          /// @}
+
+          /// @name Public Properties
+          
+          /// @{
+          /// @brief Gets or sets the threshold value (from 0 to 765).
+          /// @param threshold The threshold value (from 0 to 765).
+          /// @remarks The threshold is the sum of the RGB components.
+          int32 threshold = 0;
+
+          /// @brief Gets or sets the extraction color.
+          /// @param extraction_color The extraction color.
+          color extraction_color;
+
+          /// @brief Gets or sets the other pixels color.
+          /// @param other_pixels_color The other pixels color.
+          color other_pixels_color;
+          /// @}
+
+        protected:
+          /// @name Protected Methods
+          
+          /// @{
+          /// @brief Apply the effect.
+          /// @param graphics The graphics to apply effect.
+          /// @param image The image where the effect is applied.
+          void apply(xtd::drawing::image& image) const override;
+          /// @}
+        };
+      }
+    }
+  }
+}
---
+++ src/xtd.drawing/src/xtd/drawing/imaging/effects/color_extraction_effect.cpp
@@ -0,0 +1,28 @@
+#include "../../../../../include/xtd/drawing/helpers/alpha.hpp"
+#include "../../../../../include/xtd/drawing/helpers/rgb.hpp"
+#include "../../../../../include/xtd/drawing/imaging/effects/color_extraction_effect.hpp"
+#include "../../../../../include/xtd/drawing/bitmap.hpp"
+#include <xtd/math>
+
+using namespace xtd;
+using namespace xtd::drawing;
+using namespace xtd::drawing::imaging;
+using namespace xtd::drawing::imaging::effects;
+
+color_extraction_effect::color_extraction_effect(int32 threshold, color extraction_color, color other_pixels_color) : threshold {threshold}, extraction_color {extraction_color}, other_pixels_color {other_pixels_color} {
+}
+
+void color_extraction_effect::apply(xtd::drawing::image& image) const {
+  auto threshold = math::clamp(this->threshold, 0, 3 * byte_object::max_value);
+  auto alpha = reinterpret_cast<helpers::alpha*>(image.alpha());
+  auto rgb = reinterpret_cast<helpers::rgb*>(image.rgb());
+
+  for (auto y = 0; y < image.height(); ++y)
+    for (auto x = 0; x < image.width(); ++x) {
+      auto pixel = y * image.width() + x;
+      if (!alpha[pixel].a) continue;
+      int extraction_total_rgb = extraction_color.r() + extraction_color.g() + extraction_color.b();
+      int total_rgb = rgb[pixel].r + rgb[pixel].g + rgb[pixel].b;
+      if (math::abs(total_rgb - extraction_total_rgb) >= threshold) rgb[pixel] = {other_pixels_color.r(), other_pixels_color.g(), other_pixels_color.b()};
+    }
+}
