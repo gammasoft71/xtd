8c19b7d1acd01e080b4519d1eea7c6e3c3b7a346
Gammasoft <gammasoft71@gmail.com>
2025-03-02 21:01:28 +0100

    Revue resize effect

--- src/xtd.drawing/src/xtd/drawing/image_converter.cpp
+++ src/xtd.drawing/src/xtd/drawing/image_converter.cpp
@@ -394,13 +394,13 @@ xtd::drawing::image image_converter::rescale(const xtd::drawing::image& image, c
   return result;
 }
 
-void image_converter::resize(xtd::drawing::image& image, const xtd::drawing::size& size) {
-  image.resize(size.width, size.height);
+void image_converter::resize(xtd::drawing::image& image, const xtd::drawing::rectangle& rectangle) {
+  image.resize(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
 }
 
-xtd::drawing::image image_converter::resize(const xtd::drawing::image& image, const xtd::drawing::size& size) {
+xtd::drawing::image image_converter::resize(const xtd::drawing::image& image, const xtd::drawing::rectangle& rectangle) {
   auto result = image.clone();
-  resize(result, size);
+  resize(result, rectangle);
   return result;
 }
 
