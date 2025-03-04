51e073e1574dfbee807a677488a2a32c14a5a85d
Gammasoft <gammasoft71@gmail.com>
2025-03-04 23:04:32 +0100

    Add argb_from_uint32 method

--- src/xtd.drawing/include/xtd/drawing/helpers/argb.hpp
+++ src/xtd.drawing/include/xtd/drawing/helpers/argb.hpp
@@ -20,7 +20,36 @@ namespace xtd {
       /// @par Library
       /// xtd.drawing
       /// @ingroup xtd_drawing
-      struct argb : alpha, rgb {
+      struct argb {
+        /// @name Public Properties
+        
+        /// @{
+        /// @brief Gets or sets the alpha component value of this xtd::drawing::helpers::argb struct.
+        /// @param a byte The alpha component value of this xtd::drawing::helpers::argb.
+        xtd::byte a = 255;
+        /// @brief Gets or sets the red component value of this xtd::drawing::helpers::argb struct.
+        /// @param r byte The red component value of this xtd::drawing::helpers::argb.
+        xtd::byte r = 0;
+        /// @brief Gets or sets the green component value of this xtd::drawing::helpers::argb struct.
+        /// @param r byte The green component value of this xtd::drawing::helpers::argb.
+        xtd::byte g = 0;
+        /// @brief Gets or sets the blue component value of this xtd::drawing::helpers::argb struct.
+        /// @param r byte The blue component value of this xtd::drawing::helpers::argb.
+        xtd::byte b = 0;
+        /// @}
+
+        /// @name Public Static Methods
+        
+        /// @{
+        static argb from_uint32(uint32 value) {
+          return argb {
+            .a = static_cast<xtd::byte>((value & 0xFF000000) >> 24),
+            .r = static_cast<xtd::byte>((value & 0x00FF0000) >> 16),
+            .g = static_cast<xtd::byte>((value & 0x0000FF00) >> 8),
+            .b = static_cast<xtd::byte>(value & 0x000000FF)
+          };
+        }
+        /// @}
       };
     }
   }
