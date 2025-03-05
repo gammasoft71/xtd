2f79cf2874396097f71a81fe8cd8ada90ef1e5e7
Gammasoft <gammasoft71@gmail.com>
2025-03-05 19:26:42 +0100

    check radius

--- src/xtd.drawing/src/xtd/drawing/imaging/effects/blur_effect.cpp
+++ src/xtd.drawing/src/xtd/drawing/imaging/effects/blur_effect.cpp
@@ -1,9 +1,6 @@
-#include "../../../../../include/xtd/drawing/helpers/alpha.hpp"
-#include "../../../../../include/xtd/drawing/helpers/rgb.hpp"
 #include "../../../../../include/xtd/drawing/imaging/effects/blur_effect.hpp"
 #include "../../../../../include/xtd/drawing/imaging/effects/resize_effect.hpp"
 #include "../../../../../include/xtd/drawing/imaging/image_effector.hpp"
-#include "../../../../../include/xtd/drawing/bitmap.hpp"
 #include <xtd/math>
 
 using namespace xtd;
@@ -24,6 +21,8 @@ blur_effect::blur_effect(int32 horizontal_radius, int32 vertical_radius, bool ex
 }
 
 void blur_effect::apply(xtd::drawing::image& image) const {
+  auto horizontal_radius = this->horizontal_radius < 0 ? 0 : this->horizontal_radius;
+  auto vertical_radius = this->vertical_radius < 0 ? 0 : this->vertical_radius;
   if (expand_edge) image_effector::set_effect(image, resize_effect {rectangle {horizontal_radius, vertical_radius, image.width() + horizontal_radius * 2, image.height() + vertical_radius * 2}});
   image.blur(horizontal_radius, vertical_radius);
 }
