97208baf37b00451b7a5be6c06009cb928270d9f
Gammasoft <gammasoft71@gmail.com>
2025-03-05 12:19:49 +0100

    Fix include file

--- src/xtd.drawing/include/xtd/drawing/helpers/argb.hpp
+++ src/xtd.drawing/include/xtd/drawing/helpers/argb.hpp
@@ -3,6 +3,7 @@
 /// @copyright Copyright (c) 2025 Gammasoft. All rights reserved.
 #pragma once
 #include <xtd/byte>
+#include <xtd/uint32>
 
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
