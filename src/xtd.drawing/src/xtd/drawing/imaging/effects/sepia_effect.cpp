0af386abff91c1bdd0e3a4e457fe5fba6b6303f6
Gammasoft <gammasoft71@gmail.com>
2025-03-06 18:10:05 +0100

    using xtd::math class

--- src/xtd.drawing/src/xtd/drawing/imaging/effects/sepia_effect.cpp
+++ src/xtd.drawing/src/xtd/drawing/imaging/effects/sepia_effect.cpp
@@ -13,7 +13,7 @@ sepia_effect::sepia_effect(double percent) : percent {percent} {
 }
 
 void sepia_effect::apply(xtd::drawing::image& image) const {
-  auto percent = std::clamp(this->percent, 0.0, 1.0);
+  auto percent = math::clamp(this->percent, 0.0, 1.0);
   auto alpha = reinterpret_cast<helpers::alpha*>(image.alpha());
   auto rgb = reinterpret_cast<helpers::rgb*>(image.rgb());
 
@@ -22,8 +22,8 @@ void sepia_effect::apply(xtd::drawing::image& image) const {
       auto pixel = y * image.width() + x;
       if (!alpha[pixel].a) continue;
       auto value = rgb[pixel];
-      rgb[pixel].r = helpers::rgb::alpha_blend(rgb[pixel].r, static_cast<byte>(std::clamp(0.393 * value.r + 0.769 * value.g + 0.189 * value.b, .0, 255.0)), percent);
-      rgb[pixel].g = helpers::rgb::alpha_blend(value.g, static_cast<byte>(std::clamp(0.349 * value.r + 0.686 * value.g + 0.168 * value.b, .0, 255.0)), percent);
-      rgb[pixel].b = helpers::rgb::alpha_blend(value.b, static_cast<byte>(std::clamp(0.272 * value.r + 0.534 * value.g + 0.131 * value.b, .0, 255.0)), percent);
+      rgb[pixel].r = helpers::rgb::alpha_blend(rgb[pixel].r, static_cast<byte>(math::clamp(0.393 * value.r + 0.769 * value.g + 0.189 * value.b, .0, 255.0)), percent);
+      rgb[pixel].g = helpers::rgb::alpha_blend(value.g, static_cast<byte>(math::clamp(0.349 * value.r + 0.686 * value.g + 0.168 * value.b, .0, 255.0)), percent);
+      rgb[pixel].b = helpers::rgb::alpha_blend(value.b, static_cast<byte>(math::clamp(0.272 * value.r + 0.534 * value.g + 0.131 * value.b, .0, 255.0)), percent);
     }
 }
