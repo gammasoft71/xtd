5e58c782feb8480326c867971b5d7826989da93e
Gammasoft <gammasoft71@gmail.com>
2025-03-06 21:26:25 +0100

    percent max value

--- src/xtd.drawing/src/xtd/drawing/imaging/effects/invert_effect.cpp
+++ src/xtd.drawing/src/xtd/drawing/imaging/effects/invert_effect.cpp
@@ -13,7 +13,7 @@ invert_effect::invert_effect(double percent) : percent {percent} {
 }
 
 void invert_effect::apply(xtd::drawing::image& image) const {
-  auto percent = std::clamp(this->percent, 0.0, 2.0);
+  auto percent = math::clamp(this->percent, 0.0, 1.0);
   auto alpha = reinterpret_cast<helpers::alpha*>(image.alpha());
   auto rgb = reinterpret_cast<helpers::rgb*>(image.rgb());
 
