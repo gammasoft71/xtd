20a86af4a4f0c8d761ee4b4cfa49b2ea670dcfbb
Gammasoft <gammasoft71@gmail.com>
2025-03-06 17:24:50 +0100

    Fix posterise_effect

--- src/xtd.drawing/src/xtd/drawing/imaging/effects/posterize_effect.cpp
+++ src/xtd.drawing/src/xtd/drawing/imaging/effects/posterize_effect.cpp
@@ -13,17 +13,17 @@ posterize_effect::posterize_effect(int32 levels) : levels {levels} {
 }
 
 void posterize_effect::apply(xtd::drawing::image& image) const {
-  auto levels = math::clamp(this->levels, 1, 256);
+  auto levels = math::clamp(this->levels, 2, 256);
   auto alpha = reinterpret_cast<helpers::alpha*>(image.alpha());
   auto rgb = reinterpret_cast<helpers::rgb*>(image.rgb());
-  auto step = 256 / levels;
+  auto step = 255.0f / (levels-1);
   
   for (auto y = 0; y < image.height(); ++y)
     for (auto x = 0; x < image.width(); ++x) {
       auto pixel = y * image.width() + x;
       if (!alpha[pixel].a) continue;
-      rgb[pixel].r = std::round(rgb[pixel].r / step) * step;
-      rgb[pixel].g = std::round(rgb[pixel].g / step) * step;
-      rgb[pixel].b = std::round(rgb[pixel].b / step) * step;
+      rgb[pixel].r = static_cast<byte>(std::round(rgb[pixel].r / step) * step);
+      rgb[pixel].g = static_cast<byte>(std::round(rgb[pixel].g / step) * step);
+      rgb[pixel].b = static_cast<byte>(std::round(rgb[pixel].b / step) * step);
     }
 }
