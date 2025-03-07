4fe0d1391456f6073b313067c8887b5da36da4f4
Gammasoft <gammasoft71@gmail.com>
2025-03-07 10:33:37 +0100

    fix namespace ambiguity

--- examples/xtd.forms.examples/others/docked_panels/src/docked_panels.cpp
+++ examples/xtd.forms.examples/others/docked_panels/src/docked_panels.cpp
@@ -9,27 +9,27 @@ auto main() -> int {
   auto form_main = form::create("Docked panels example");
   form_main.client_size({300, 300});
   
-  auto panel1 = panel::create(form_main, point {10, 10}, size {100, 100});
+  auto panel1 = panel::create(form_main, point {10, 10}, drawing::size {100, 100});
   panel1.back_color(color::light_green);
   panel1.border_style(border_style::fixed_single);
   panel1.dock(dock_style::fill);
   
-  auto panel2 = panel::create(form_main, point {10, 10}, size {100, 100});
+  auto panel2 = panel::create(form_main, point {10, 10}, drawing::size {100, 100});
   panel2.back_color(color::light_yellow);
   panel2.border_style(border_style::fixed_single);
   panel2.dock(dock_style::right);
   
-  auto panel3 = panel::create(form_main, point {10, 10}, size {100, 100});
+  auto panel3 = panel::create(form_main, point {10, 10}, drawing::size {100, 100});
   panel3.back_color(color::light_blue);
   panel3.border_style(border_style::fixed_single);
   panel3.dock(dock_style::top);
   
-  auto panel4 = panel::create(form_main, point {10, 10}, size {100, 100});
+  auto panel4 = panel::create(form_main, point {10, 10}, drawing::size {100, 100});
   panel4.back_color(color::light_pink);
   panel4.border_style(border_style::fixed_single);
   panel4.dock(dock_style::left);
   
-  auto panel5 = panel::create(form_main, point {10, 10}, size {100, 100});
+  auto panel5 = panel::create(form_main, point {10, 10}, drawing::size {100, 100});
   panel5.back_color(color::light_salmon);
   panel5.border_style(border_style::fixed_single);
   panel5.dock(dock_style::bottom);
