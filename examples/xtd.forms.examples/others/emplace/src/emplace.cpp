50c9b825e2587f2d21da0a1594ddfc02c40d47d5
Gammasoft <gammasoft71@gmail.com>
2025-03-07 10:32:36 +0100

    Fix namespace ambiguity

--- examples/xtd.forms.examples/others/emplace/src/emplace.cpp
+++ examples/xtd.forms.examples/others/emplace/src/emplace.cpp
@@ -12,10 +12,10 @@ auto main() -> int {
   form1.controls().emplace<xtd::forms::button>(form1.controls().begin(), "Button 1", point{50, 50}).click += [&] {
     form1.controls()["label1"].value().get().text(xtd::string::format("Button 1 clicked {} times", ++button1_clicked));
   };
-  form1.controls().emplace_at<xtd::forms::button>(1, "Button 2", point {50, 100}, size {200, 75}).auto_repeat(true).click += [&] {
+  form1.controls().emplace_at<xtd::forms::button>(1, "Button 2", point {50, 100}, drawing::size {200, 75}).auto_repeat(true).click += [&] {
     form1.controls()["label2"].value().get().text(xtd::string::format("Button 2 clicked {} times", ++button2_clicked));
   };
-  form1.controls().emplace<xtd::forms::label>(form1.controls().end(), "Button 1 clicked 0 times", point {50, 200}, size {200, 23}, "label1");
-  form1.controls().emplace_back<xtd::forms::label>("Button 2 clicked 0 times", point {50, 230}, size {200, 23}, "label2");
+  form1.controls().emplace<xtd::forms::label>(form1.controls().end(), "Button 1 clicked 0 times", point {50, 200}, drawing::size {200, 23}, "label1");
+  form1.controls().emplace_back<xtd::forms::label>("Button 2 clicked 0 times", point {50, 230}, drawing::size {200, 23}, "label2");
   xtd::forms::application::run(form1);
 }
