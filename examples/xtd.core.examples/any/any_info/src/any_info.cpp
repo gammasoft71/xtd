951f2428dd023eb25743bd32739054d627161873
Gammasoft <gammasoft71@gmail.com>
2025-03-07 11:16:14 +0100

    Fix any examples

--- examples/xtd.core.examples/any/any_info/src/any_info.cpp
+++ examples/xtd.core.examples/any/any_info/src/any_info.cpp
@@ -4,12 +4,14 @@
 
 using namespace xtd;
 
-struct foo1 : public object {
+struct foo1 : public object, public icomparable<foo1>, public iequatable<foo1> {
   explicit foo1(int v) : value(v) {}
   int value = 0;
   
   string to_string() const noexcept override {return string::format("{}", value);}
-  bool operator <(const foo1& f) const noexcept {return value < f.value;}
+  bool equals(const object& o) const noexcept override {return is<foo1>(o) && equals(static_cast<const foo1&>(o));}
+  bool equals(const foo1& f) const noexcept override {return value == f.value;}
+  int32 compare_to(const foo1& f) const noexcept override {return value < f.value ? -1 : value > f.value ? 1 : 0;}
 };
 
 struct foo2 {
@@ -17,10 +19,16 @@ struct foo2 {
   
   bool operator ==(const foo2& f) const noexcept {return value == f.value;}
   bool operator <(const foo2& f) const noexcept {return value < f.value;}
+  bool operator >(const foo2& f) const noexcept {return value > f.value;}
 };
 
 template<>
 std::string xtd::to_string(const foo2& value, const std::string& fmt, const std::locale& loc) {return xtd::int32_object {value.value}.to_string(fmt, loc);}
+
+template<>
+struct std::hash<foo2> {
+  xtd::size operator()(const foo2& value) const noexcept {return xtd::hash_code::combine(value.value);}
+};
 
 template<typename type_t>
 string get_any_info(const type_t& value) {
