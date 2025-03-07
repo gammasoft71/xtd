24eed14503ca9a88e2888de2f00c0bf8df7a38f1
Gammasoft <gammasoft71@gmail.com>
2025-03-07 22:22:22 +0100

    Add from_base64_string and to_base64_string methods

--- src/xtd.core/src/xtd/convert.cpp
+++ src/xtd.core/src/xtd/convert.cpp
@@ -4,6 +4,55 @@
 using namespace xtd;
 using namespace xtd::helpers;
 
+namespace {
+  static const string base64_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+  
+  static bool is_base64(byte c) {return (std::isalnum(c) || (c == '+') || (c == '/'));}
+}
+
+// https://stackoverflow.com/questions/180947/base64-decode-snippet-in-c
+array<byte> convert::from_base64_string(const string& s) {
+  //if (s.size() % 4 != 0) throw_helper::throws(xtd::helpers::exception_case::format);
+  auto in_len = s.size();
+  auto i = 0_z;
+  auto j = 0_z;
+  auto in_ = 0_z;
+  byte char_array_4[4], char_array_3[3];
+  std::vector<byte> ret;
+  
+  while (in_len-- && ( s[in_] != '=') && is_base64(s[in_])) {
+    char_array_4[i++] = s[in_]; in_++;
+    if (i == 4) {
+      for (i = 0; i < 4; i++)
+        char_array_4[i] = base64_chars.find(char_array_4[i]);
+      
+      char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
+      char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+      char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
+      
+      for (i = 0; (i < 3); i++)
+        ret.push_back(char_array_3[i]);
+      i = 0;
+    }
+  }
+  
+  if (i) {
+    for (j = i; j <4; j++)
+      char_array_4[j] = 0;
+    
+    for (j = 0; j <4; j++)
+      char_array_4[j] = base64_chars.find(char_array_4[j]);
+    
+    char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
+    char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+    char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
+    
+    for (j = 0; (j < i - 1); j++) ret.push_back(char_array_3[j]);
+  }
+  
+  return ret;
+}
+
 std::any convert::to_any(std::any value) noexcept {
   return value;
 }
@@ -155,6 +204,54 @@ std::any convert::to_any(const wchar* value) noexcept {
 
 std::any convert::to_any(wchar* value) noexcept {
   return value;
+}
+
+// https://stackoverflow.com/questions/180947/base64-decode-snippet-in-c
+string convert::to_base64_string(const array<byte>& in_array) {
+  return to_base64_string(read_only_span<byte>(in_array));
+}
+
+string convert::to_base64_string(const xtd::read_only_span<xtd::byte>& bytes) {
+  auto ret = string {};
+  auto i = 0_z;
+  auto j = 0_z;
+  byte char_array_3[3];
+  byte char_array_4[4];
+  auto buf = bytes.data();
+  auto bufLen = bytes.size();
+  
+  while (bufLen--) {
+    char_array_3[i++] = *(buf++);
+    if (i == 3) {
+      char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+      char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+      char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+      char_array_4[3] = char_array_3[2] & 0x3f;
+      
+      for(i = 0; (i <4) ; i++)
+        ret += base64_chars[char_array_4[i]];
+      i = 0;
+    }
+  }
+  
+  if (i)
+  {
+    for(j = i; j < 3; j++)
+      char_array_3[j] = '\0';
+    
+    char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+    char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+    char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+    char_array_4[3] = char_array_3[2] & 0x3f;
+    
+    for (j = 0; (j < i + 1); j++)
+      ret += base64_chars[char_array_4[j]];
+    
+    while((i++ < 3))
+      ret += '=';
+  }
+  
+  return ret;
 }
 
 bool convert::to_boolean(std::any value) {
