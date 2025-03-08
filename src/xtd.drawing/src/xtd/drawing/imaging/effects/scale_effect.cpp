2e997acac35e5fbbc9c508c9a40623b7ce6d7afb
Gammasoft <gammasoft71@gmail.com>
2025-03-08 21:14:19 +0100

    Add and implement default_scaling method

--- src/xtd.drawing/src/xtd/drawing/imaging/effects/scale_effect.cpp
+++ src/xtd.drawing/src/xtd/drawing/imaging/effects/scale_effect.cpp
@@ -1,14 +1,9 @@
 #include "bicubic_scaling.hpp"
 #include "bilinear_scaling.hpp"
 #include "box_average_scaling.hpp"
+#include "default_scaling.hpp"
 #include "nearest_scaling.hpp"
 #include "../../../../../include/xtd/drawing/imaging/effects/scale_effect.hpp"
-
-namespace {
-  xtd::drawing::image default_scaling(const xtd::drawing::image& source_image, const xtd::drawing::size& size) {
-    return size.width < source_image.width() && size.height < source_image.height() ? box_average_scaling(source_image, size) : bicubic_scaling(source_image, size);
-  }
-}
 
 using namespace xtd::drawing::imaging::effects;
 
