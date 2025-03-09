b78015ab3d41fc976d62be3bb18184a185831864
Gammasoft <gammasoft71@gmail.com>
2025-03-09 12:00:45 +0100

    review code

--- src/xtd.drawing/src/xtd/drawing/imaging/effects/box_average_scaling.hpp
+++ src/xtd.drawing/src/xtd/drawing/imaging/effects/box_average_scaling.hpp
@@ -9,9 +9,9 @@ namespace {
     xtd::int32 box_end;
   };
   
-  void resample_box_precalculates(std::vector<box_precalculate>& precalculates, xtd::int32 old_size) {
+  inline void resample_box_precalculates(std::vector<box_precalculate>& precalculates, xtd::int32 old_size) {
     const auto new_size = static_cast<xtd::int32>(precalculates.size());
-    if (old_size <= 0 || new_size <= 0) xtd::helpers::throw_helper::throws(xtd::helpers::exception_case::argument);
+    if (old_size <= 0 || new_size == 0) xtd::helpers::throw_helper::throws(xtd::helpers::exception_case::argument);
     
     auto value = 0;
     for (auto distance = 0; distance < new_size; ++distance) {
@@ -23,13 +23,13 @@ namespace {
   }
 
   xtd::drawing::image box_average_scaling(const xtd::drawing::image& source_image, const xtd::drawing::size& size) {
-    auto source_width = source_image.width();
-    auto source_height = source_image.height();
-    auto source_alpha = reinterpret_cast<const xtd::drawing::helpers::alpha*>(source_image.alpha());
-    auto source_rgb = reinterpret_cast<const xtd::drawing::helpers::rgb*>(source_image.rgb());
+    const auto source_width = source_image.width();
+    const auto source_height = source_image.height();
+    const auto source_alpha = reinterpret_cast<const xtd::drawing::helpers::alpha*>(source_image.alpha());
+    const auto source_rgb = reinterpret_cast<const xtd::drawing::helpers::rgb*>(source_image.rgb());
     
-    auto result_width = size.width;
-    auto result_height = size.height;
+    const auto result_width = size.width;
+    const auto result_height = size.height;
     auto result_image = xtd::drawing::bitmap {result_width, result_height};
     auto result_alpha = reinterpret_cast<xtd::drawing::helpers::alpha*>(result_image.alpha());
     auto result_rgb = reinterpret_cast<xtd::drawing::helpers::rgb*>(result_image.rgb());
@@ -44,13 +44,13 @@ namespace {
       const auto& vertical_precalculate = vertical_precalculates[y];
       for (auto x = 0; x < result_width; ++x) {
         const auto& horizontal_precalculate = horizontal_precalculates[x];
-        auto averaged_pixels = (vertical_precalculate.box_end - vertical_precalculate.box_start + 1)  * (horizontal_precalculate.box_end - horizontal_precalculate.box_start + 1);
+        const auto averaged_pixels = (vertical_precalculate.box_end - vertical_precalculate.box_start + 1)  * (horizontal_precalculate.box_end - horizontal_precalculate.box_start + 1);
         const auto result_pixel = y * result_width + x;
         auto sum_a = 0.0, sum_r = 0.0, sum_g = 0.0, sum_b = 0.0;
         
         for (auto j = vertical_precalculate.box_start; j <= vertical_precalculate.box_end; ++j) {
           for (auto i = horizontal_precalculate.box_start; i <= horizontal_precalculate.box_end; ++i) {
-            auto source_pixel = j * source_width + i;
+            const auto source_pixel = j * source_width + i;
             sum_a += source_alpha[source_pixel].a;
             sum_r += source_rgb[source_pixel].r * source_alpha[source_pixel].a;
             sum_g += source_rgb[source_pixel].g * source_alpha[source_pixel].a;
