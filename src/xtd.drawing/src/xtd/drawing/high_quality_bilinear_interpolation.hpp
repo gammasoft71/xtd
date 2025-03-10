ea42d6794de86b992b67a8fbb98f773613a5355c
Gammasoft <gammasoft71@gmail.com>
2025-03-10 18:15:17 +0100

    Review code

--- src/xtd.drawing/src/xtd/drawing/high_quality_bilinear_interpolation.hpp
+++ src/xtd.drawing/src/xtd/drawing/high_quality_bilinear_interpolation.hpp
@@ -11,14 +11,14 @@ namespace {
     if (source_image.width() % size.width == 0 && source_image.width() >= size.width && source_image.height() % size.height == 0 && source_image.height() >= size.height)
       return shrink_interpolation(source_image, size);
     
-    auto scale_x = static_cast<double>(source_image.width()) / size.width;
-    auto scale_y = static_cast<double>(source_image.height()) / size.height;
-    auto passes = static_cast<xtd::int32>(xtd::math::ceiling(xtd::math::max(xtd::math::log(scale_x, 2), xtd::math::log(scale_y, 2))));
+    const auto scale_x = static_cast<double>(source_image.width()) / size.width;
+    const auto scale_y = static_cast<double>(source_image.height()) / size.height;
+    const auto passes = static_cast<xtd::int32>(xtd::math::ceiling(xtd::math::max(xtd::math::log(scale_x, 2), xtd::math::log(scale_y, 2))));
     
-    xtd::drawing::image result_image = source_image;
-    for (int i = 0; i < passes; ++i) {
-      double intermediate_width = xtd::math::max(static_cast<double>(size.width), result_image.width() / 2.0);
-      double intermediate_height = xtd::math::max(static_cast<double>(size.height), result_image.height() / 2.0);
+    auto result_image = source_image;
+    for (auto passe = 0; passe < passes; ++passe) {
+      const auto intermediate_width = xtd::math::max(static_cast<double>(size.width), result_image.width() / 2.0);
+      const auto intermediate_height = xtd::math::max(static_cast<double>(size.height), result_image.height() / 2.0);
       result_image = bilinear_interpolation(result_image, {static_cast<int>(intermediate_width), static_cast<int>(intermediate_height)});
     }
     
