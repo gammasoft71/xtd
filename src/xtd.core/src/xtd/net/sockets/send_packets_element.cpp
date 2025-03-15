d9bb860c9506876da808caf2743f2bdc20b8cb50
Gammasoft <gammasoft71@gmail.com>
2025-03-15 09:57:24 +0100

    using xtd::array

--- src/xtd.core/src/xtd/net/sockets/send_packets_element.cpp
+++ src/xtd.core/src/xtd/net/sockets/send_packets_element.cpp
@@ -3,13 +3,13 @@
 using namespace xtd;
 using namespace xtd::net::sockets;
 
-send_packets_element::send_packets_element(const std::vector<xtd::byte>& buffer) : buffer_(buffer) {
+send_packets_element::send_packets_element(const xtd::array<xtd::byte>& buffer) : buffer_(buffer) {
 }
 
 send_packets_element::send_packets_element(const xtd::string& file_path, size_t offset, size_t count, bool end_of_packet) : count_(count), end_of_packet_(end_of_packet), file_path_(file_path), offset_(offset) {
 }
 
-send_packets_element::send_packets_element(const std::vector<xtd::byte>& buffer, size_t offset, size_t count, bool end_of_packet) : buffer_(buffer), count_(count), end_of_packet_(end_of_packet), offset_(offset) {
+send_packets_element::send_packets_element(const xtd::array<xtd::byte>& buffer, size_t offset, size_t count, bool end_of_packet) : buffer_(buffer), count_(count), end_of_packet_(end_of_packet), offset_(offset) {
 }
 
 send_packets_element::send_packets_element(const xtd::string& file_path, size_t offset, size_t count) : count_(count), file_path_(file_path), offset_(offset) {
@@ -18,7 +18,7 @@ send_packets_element::send_packets_element(const xtd::string& file_path, size_t 
 send_packets_element::send_packets_element(std::ifstream& file_stream, size_t offset, size_t count, bool end_of_packet) : count_(count), end_of_packet_(end_of_packet), file_stream_(&file_stream), offset_(offset), delete_file_stream_when_destroy_(true) {
 }
 
-send_packets_element::send_packets_element(const std::vector<xtd::byte>& buffer, size_t offset, size_t count) : buffer_(buffer), count_(count), offset_(offset) {
+send_packets_element::send_packets_element(const xtd::array<xtd::byte>& buffer, size_t offset, size_t count) : buffer_(buffer), count_(count), offset_(offset) {
 }
 
 send_packets_element::send_packets_element(const xtd::string& file_path) : file_path_(file_path) {
@@ -31,7 +31,7 @@ send_packets_element::~send_packets_element() {
   if (delete_file_stream_when_destroy_) delete file_stream_;
 }
 
-std::vector<xtd::byte> send_packets_element::buffer() const noexcept {
+xtd::array<xtd::byte> send_packets_element::buffer() const noexcept {
   return buffer_;
 }
 
