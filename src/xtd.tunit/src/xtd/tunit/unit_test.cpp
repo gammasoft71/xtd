fe5d069743579843f5e9c62c6632e5e38422403f
Gammasoft <gammasoft71@gmail.com>
2025-03-16 19:20:26 +0100

    Add std::

--- src/xtd.tunit/src/xtd/tunit/unit_test.cpp
+++ src/xtd.tunit/src/xtd/tunit/unit_test.cpp
@@ -160,7 +160,7 @@ int32 unit_test::run() noexcept {
     
     for (repeat_iteration_ = 1; repeat_iteration_ <= settings::default_settings().repeat_test() || settings::default_settings().repeat_test() < 0; ++repeat_iteration_) {
       if (settings::default_settings().shuffle_test())
-        shuffle(test_classes().begin(), test_classes().end(), random.generator());
+        std::shuffle(test_classes().begin(), test_classes().end(), random.generator());
       
       try {
         if (!settings::default_settings().brief()) event_listener_->on_unit_test_start(tunit_event_args(*this));
