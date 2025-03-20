7a60f0129d3320aecbe33106d7f674478682d9c0
gammasoft71 <gammasoft71@gmail.com>
2025-03-20 20:19:42 +0100

    update latest reference guide

--- static/reference_guides/latest/search/typedefs_4.js
+++ static/reference_guides/latest/search/typedefs_4.js
@@ -5,7 +5,7 @@ var searchData=
   ['enum_5fcollection_2',['enum_collection',['../group__xtd__core.html#ga2be4d2f270243268ff197700fa3ae42f',1,'xtd']]],
   ['enum_5ftype_3',['enum_type',['../structxtd_1_1enum__object.html#a96fcf375790c9a544567f9a532b37c6c',1,'xtd::enum_object']]],
   ['enumerable_5ftype_4',['enumerable_type',['../classxtd_1_1collections_1_1generic_1_1extensions_1_1enumerable.html#afa0428a5d439bb7fd6a75f0ca54751c5',1,'xtd::collections::generic::extensions::enumerable']]],
-  ['enumerator_5',['enumerator',['../group__collections.html#ga46ca5358d7207b976828dddad86adcf8',1,'xtd::collections']]],
+  ['enumerator_5',['enumerator',['../classxtd_1_1linq_1_1enumerable.html#a2ddf87538e12a66991705171183f8d5b',1,'xtd::linq::enumerable::enumerator'],['../group__collections.html#ga46ca5358d7207b976828dddad86adcf8',1,'xtd::collections::enumerator']]],
   ['enumerator_5ftype_6',['enumerator_type',['../classxtd_1_1basic__string.html#a2b5493bd9034616dded9fb810d65fd6c',1,'xtd::basic_string']]],
   ['environment_5fvariable_5fcollection_7',['environment_variable_collection',['../classxtd_1_1system__report.html#a32f008785a4a0c04f79a510ccacebb86',1,'xtd::system_report']]],
   ['event_5fhandler_8',['event_handler',['../group__events.html#ga226db3bbc8ae6fdf2afaa94a5c947c54',1,'xtd']]],
