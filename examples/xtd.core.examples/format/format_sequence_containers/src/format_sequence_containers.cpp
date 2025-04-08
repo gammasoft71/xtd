883c6800c770c8f61d58a5d93ca97796d727f7c0
Gammasoft <gammasoft71@gmail.com>
2025-04-08 21:47:05 +0200

    Add array and fixed_array

--- examples/xtd.core.examples/format/format_sequence_containers/src/format_sequence_containers.cpp
+++ examples/xtd.core.examples/format/format_sequence_containers/src/format_sequence_containers.cpp
@@ -1,12 +1,15 @@
 #include <xtd/collections/array_list>
 #include <xtd/console>
 #include <xtd/environment>
+#include <xtd/fixed_array>
 #include <xtd/string>
 
 using namespace xtd;
 using namespace xtd::collections;
 
 auto main() -> int {
+  console::out << string::format("{}", array<int> {1, 2, 3, 4}) << environment::new_line;
+  console::out << string::format("{}", fixed_array<int, 4> {1, 2, 3, 4}) << environment::new_line;
   console::out << string::format("{}", generic::list<int> {1, 2, 3, 4}) << environment::new_line;
   console::out << environment::new_line;
   console::out << string::format("{}", std::initializer_list<int> {1, 2, 3, 4}) << environment::new_line;
@@ -19,6 +22,8 @@ auto main() -> int {
 
 // This code produces the following output :
 //
+// [1, 2, 3, 4]
+// [1, 2, 3, 4]
 // [1, 2, 3, 4]
 //
 // [1, 2, 3, 4]
