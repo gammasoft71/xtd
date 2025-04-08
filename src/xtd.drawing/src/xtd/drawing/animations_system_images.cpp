5c958d2153fda1f47ce58273f977e276301c7abc
Gammasoft <gammasoft71@gmail.com>
2025-04-08 11:45:19 +0200

    using xtd::array

--- src/xtd.drawing/src/xtd/drawing/animations_system_images.cpp
+++ src/xtd.drawing/src/xtd/drawing/animations_system_images.cpp
@@ -15,16 +15,16 @@ image animations_system_images::process_working(const xtd::drawing::size& size) 
   return system_images_base::from_name("process-working", size);
 }
 
-const std::vector<image>& animations_system_images::get_images() noexcept {
+const array<image>& animations_system_images::get_images() noexcept {
   static auto images = get_images(default_size());
   return images;
 }
 
-std::vector<image> animations_system_images::get_images(const xtd::drawing::size& size) noexcept {
-  return std::vector {process_working(size), };
+array<image> animations_system_images::get_images(const xtd::drawing::size& size) noexcept {
+  return array {process_working(size), };
 }
 
-const std::vector<string>& animations_system_images::get_image_names() noexcept {
-  static auto names = std::vector {"process-working"_s, };
+const array<string>& animations_system_images::get_image_names() noexcept {
+  static auto names = array {"process-working"_s, };
   return names;
 }
