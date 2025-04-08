82a85ebbebe0acaa9ccef2b64eb8e469a977a6b1
Gammasoft <gammasoft71@gmail.com>
2025-04-08 16:33:26 +0200

    using xtd::array

--- src/xtd.drawing/include/xtd/drawing/purple_colors.hpp
+++ src/xtd.drawing/include/xtd/drawing/purple_colors.hpp
@@ -5,7 +5,6 @@
 #include "../drawing_export.hpp"
 #include "color.hpp"
 #include <xtd/static>
-#include <vector>
 
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
