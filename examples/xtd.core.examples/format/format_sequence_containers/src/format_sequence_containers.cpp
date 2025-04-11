e0f2ee99b243a17abb24375f137aa78df03b5339
Gammasoft <gammasoft71@gmail.com>
2025-04-11 20:24:07 +0200

    add array_list

--- examples/xtd.core.examples/format/format_sequence_containers/src/format_sequence_containers.cpp
+++ examples/xtd.core.examples/format/format_sequence_containers/src/format_sequence_containers.cpp
@@ -6,11 +6,13 @@
 
 using namespace xtd;
 using namespace xtd::collections;
+using namespace xtd::collections::generic;
 
 auto main() -> int {
   console::out << string::format("{}", array<int> {1, 2, 3, 4}) << environment::new_line;
   console::out << string::format("{}", fixed_array<int, 4> {1, 2, 3, 4}) << environment::new_line;
-  console::out << string::format("{}", generic::list<int> {1, 2, 3, 4}) << environment::new_line;
+  console::out << string::format("{}", list<int> {1, 2, 3, 4}) << environment::new_line;
+  console::out << string::format("{}", array_list {1, 2, 3, 4}) << environment::new_line;
   console::out << environment::new_line;
   console::out << string::format("{}", std::initializer_list<int> {1, 2, 3, 4}) << environment::new_line;
   console::out << string::format("{}", std::array<int, 4> {1, 2, 3, 4}) << environment::new_line;
@@ -22,6 +24,7 @@ auto main() -> int {
 
 // This code produces the following output :
 //
+// [1, 2, 3, 4]
 // [1, 2, 3, 4]
 // [1, 2, 3, 4]
 // [1, 2, 3, 4]
