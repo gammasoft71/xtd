317f1e50d84e10f620f9fab463bdaaada971edba
Gammasoft <gammasoft71@gmail.com>
2025-04-12 11:11:02 +0200

    using xtd::array and xtd::collections::generic::list

--- tools/xtdc/src/cpp_console_project.hpp
+++ tools/xtdc/src/cpp_console_project.hpp
@@ -19,7 +19,7 @@ namespace xtdc_command {
     
   private:
     void create_cmakelists_txt(const xtd::string& name) const {
-      std::vector<xtd::string> lines {
+      auto lines = xtd::array<xtd::string> {
         "cmake_minimum_required(VERSION 3.20)",
         "",
         "# Solution",
@@ -30,7 +30,7 @@ namespace xtdc_command {
     }
     
     void create_cpp_console_cmakelists_txt(const xtd::string& name, const xtd::string& path) const {
-      std::vector<xtd::string> lines {
+      auto lines = xtd::array<xtd::string> {
         "cmake_minimum_required(VERSION 3.20)",
         "",
         "# Project",
@@ -53,7 +53,7 @@ namespace xtdc_command {
     }
     
     void create_cpp_console_source(const xtd::string& name, const xtd::string& path) const {
-      std::vector<xtd::string> lines {
+      auto lines = xtd::array<xtd::string> {
         "#include <iostream>",
         "",
         "// The main entry point for the application.",
@@ -66,7 +66,7 @@ namespace xtdc_command {
     }
     
     void generate_cpp_console_cmakelists_txt(const xtd::string& name, const xtd::string& path) const {
-      std::vector<xtd::string> lines;
+      auto lines = xtd::collections::generic::list<xtd::string> {};
       lines.push_back("cmake_minimum_required(VERSION 3.20)");
       lines.push_back("");
       lines.push_back("# Project");
