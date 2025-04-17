4ab673e96a97aa851f7ce94ad62c0e16611d764b
gammasoft71 <gammasoft71@gmail.com>
2025-04-17 21:00:26 +0200

    update latest reference guide

---
+++ static/reference_guides/latest/html/search/classes_10.js
@@ -0,0 +1,27 @@
+var searchData=
+[
+  ['radial_5fgradient_5fbrush_0',['radial_gradient_brush',['../classxtd_1_1drawing_1_1drawing__2d_1_1radial__gradient__brush.html',1,'xtd::drawing::drawing_2d']]],
+  ['radio_5fbutton_1',['radio_button',['../classxtd_1_1forms_1_1radio__button.html',1,'xtd::forms']]],
+  ['radio_5fbutton_5frenderer_2',['radio_button_renderer',['../classxtd_1_1forms_1_1radio__button__renderer.html',1,'xtd::forms']]],
+  ['radio_5fbuttons_3',['radio_buttons',['../classxtd_1_1forms_1_1radio__buttons.html',1,'xtd::forms']]],
+  ['random_4',['random',['../classxtd_1_1random.html',1,'xtd']]],
+  ['rank_5fexception_5',['rank_exception',['../classxtd_1_1rank__exception.html',1,'xtd']]],
+  ['read_5fonly_5fcollection_6',['read_only_collection',['../classxtd_1_1collections_1_1object__model_1_1read__only__collection.html',1,'xtd::collections::object_model']]],
+  ['read_5fonly_5fspan_7',['read_only_span',['../classxtd_1_1read__only__span.html',1,'xtd']]],
+  ['rectangle_8',['rectangle',['../structxtd_1_1drawing_1_1rectangle.html',1,'xtd::drawing']]],
+  ['rectangle_5ff_9',['rectangle_f',['../structxtd_1_1drawing_1_1rectangle__f.html',1,'xtd::drawing']]],
+  ['red_5fcolors_10',['red_colors',['../classxtd_1_1drawing_1_1red__colors.html',1,'xtd::drawing']]],
+  ['region_11',['region',['../classxtd_1_1drawing_1_1region.html',1,'xtd::drawing']]],
+  ['registered_5ftest_5fclass_12',['registered_test_class',['../classxtd_1_1tunit_1_1registered__test__class.html',1,'xtd::tunit']]],
+  ['registered_5fwait_5fhandle_13',['registered_wait_handle',['../classxtd_1_1threading_1_1registered__wait__handle.html',1,'xtd::threading']]],
+  ['renderer_14',['renderer',['../classxtd_1_1forms_1_1renderer.html',1,'xtd::forms']]],
+  ['replace_5fbox_15',['replace_box',['../classxtd_1_1forms_1_1replace__box.html',1,'xtd::forms']]],
+  ['replace_5fdialog_16',['replace_dialog',['../classxtd_1_1forms_1_1replace__dialog.html',1,'xtd::forms']]],
+  ['replace_5fevent_5fargs_17',['replace_event_args',['../classxtd_1_1forms_1_1replace__event__args.html',1,'xtd::forms']]],
+  ['reset_5fcolor_18',['reset_color',['../classxtd_1_1reset__color.html',1,'xtd']]],
+  ['resize_5feffect_19',['resize_effect',['../structxtd_1_1drawing_1_1imaging_1_1effects_1_1resize__effect.html',1,'xtd::drawing::imaging::effects']]],
+  ['resources_20',['resources',['../classxtd_1_1forms_1_1properties_1_1resources.html',1,'xtd::forms::properties']]],
+  ['rgb_21',['rgb',['../structxtd_1_1drawing_1_1helpers_1_1rgb.html',1,'xtd::drawing::helpers']]],
+  ['rotate_5fflip_5feffect_22',['rotate_flip_effect',['../structxtd_1_1drawing_1_1imaging_1_1effects_1_1rotate__flip__effect.html',1,'xtd::drawing::imaging::effects']]],
+  ['run_5fworker_5fcompleted_5fevent_5fargs_23',['run_worker_completed_event_args',['../classxtd_1_1forms_1_1run__worker__completed__event__args.html',1,'xtd::forms']]]
+];
