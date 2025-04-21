0b2697aab571d7e35cd4627ee8d53a644ec86749
Gammasoft <gammasoft71@gmail.com>
2025-04-21 20:35:50 +0200

    xtd::unique_ptr_object implementation

--- src/xtd.core/include/xtd/internal/__unique_ptr_object_definition.hpp
+++ src/xtd.core/include/xtd/internal/__unique_ptr_object_definition.hpp
@@ -2,6 +2,11 @@
 /// @brief Contains xtd::unique_ptr_object definition.
 /// @copyright Copyright (c) 2025 Gammasoft. All rights reserved.
 #pragma once
+/// @cond
+#if !defined(__XTD_CORE_INTERNAL__)
+#error "Do not include this file: Internal use only"
+#endif
+/// @endcond
 #include <memory>
 
 /// @cond
