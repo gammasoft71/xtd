b3a796e7ae8cadaf1a98313f8c3beac034eecc78
Gammasoft <gammasoft71@gmail.com>
2025-04-25 16:51:32 +0200

    Update example

--- examples/xtd.core.examples/convert/as/src/as.cpp
+++ examples/xtd.core.examples/convert/as/src/as.cpp
@@ -1,6 +1,7 @@
 #include <xtd/as>
 #include <xtd/console>
 #include <xtd/convert>
+#include <xtd/convert_pointer>
 #include <xtd/random>
 
 using namespace xtd;
@@ -21,9 +22,10 @@ auto main() -> int {
   s2 = as<string>(a);
   console::write_line("a = {0}, convert::to_string(a) = {1}, as<string>(a) = {2}", a, s1, s2);
   
-  ptr<object> value = new_ptr<string>("a");
-  ptr<string> value2 = as<string>(value);
-  console::write_line("value = {0}, as<string>(value) = {1}", *value, *value2);
+  ptr<object> p = new_ptr<string>("a");
+  ptr<string> p1 = convert_pointer::to_ptr<string>(p);
+  ptr<string> p2 = as<string>(p);
+  console::write_line("p = {0}, convert_pointer::to_ptr<string>(p) = {1}, as<string>(p) = {2}", *p, *p1, *p2);
   
   xtd::random rand;
   try {
@@ -33,9 +35,11 @@ auto main() -> int {
   }
   
   ptr<object> ver = new_ptr<version>(1, 1, 5);
-  ptr<string> str = as<string>(ver);
-  if (str == nullptr)
-    console::write_line("Bad cast!");
+  try {
+    ptr<string> str = as<string>(ver);
+  } catch (const invalid_cast_exception& e) {
+    console::write_line("Bad cast : {0} !", e.message());
+  }
 }
 
 // This code produces the following output :
@@ -43,6 +47,6 @@ auto main() -> int {
 // d = 41.6, convert::to_int32(d) = 42, as<int>(d) = 42
 // i = 42, convert::to_string(i) = 42, as<string>(i) = 42
 // a = string, convert::to_string(a) = string, as<string>(a) = string
-// value = a, as<string>(value) = a
+// p = a, convert_pointer::to_ptr<string>(p) = a, as<string>(p) = a
 // Bad cast : std::bad_cast !
-// Bad cast!
+// Bad cast : std::bad_cast !
