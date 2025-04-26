3bfe1bdfc016070e23504edec134330d15c7e026
Gammasoft <gammasoft71@gmail.com>
2025-04-26 20:22:24 +0200

    using xtd::ref

--- src/xtd.forms/include/xtd/forms/const_form_ref.hpp
+++ src/xtd.forms/include/xtd/forms/const_form_ref.hpp
@@ -2,7 +2,7 @@
 /// @brief Contains xtd::forms::form_ref alias.
 /// @copyright Copyright (c) 2025 Gammasoft. All rights reserved.
 #pragma once
-#include <functional>
+#include <xtd/ref>
 
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
@@ -22,6 +22,6 @@ namespace xtd {
     /// @par Library
     /// xtd.forms
     /// @ingroup xtd_forms
-    using const_form_ref = std::reference_wrapper<const form>;
+    using const_form_ref = ref<const form>;
   }
 }
