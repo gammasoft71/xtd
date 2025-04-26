23873522912870bc36f3ab128af488973b485bac
Gammasoft <gammasoft71@gmail.com>
2025-04-26 20:22:18 +0200

    using xtd::ref

--- src/xtd.forms/include/xtd/forms/const_control_ref.hpp
+++ src/xtd.forms/include/xtd/forms/const_control_ref.hpp
@@ -2,7 +2,7 @@
 /// @brief Contains xtd::forms::const_control_ref typedef.
 /// @copyright Copyright (c) 2025 Gammasoft. All rights reserved.
 #pragma once
-#include <functional>
+#include <xtd/ref>
 
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
@@ -22,6 +22,6 @@ namespace xtd {
     /// @par Library
     /// xtd.forms
     /// @ingroup xtd_forms
-    using const_control_ref = std::reference_wrapper<const control>;
+    using const_control_ref = ref<const control>;
   }
 }
