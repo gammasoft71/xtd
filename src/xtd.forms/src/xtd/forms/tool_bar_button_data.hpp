0969b19c764bc47122454a0aad7e432ad4ef078e
Gammasoft <gammasoft71@gmail.com>
2025-04-28 00:11:16 +0200

    using xtd::ref

--- src/xtd.forms/src/xtd/forms/tool_bar_button_data.hpp
+++ src/xtd.forms/src/xtd/forms/tool_bar_button_data.hpp
@@ -2,7 +2,7 @@
 
 struct xtd::forms::tool_bar_button::data {
   xtd::forms::control* control = nullptr;
-  std::optional<std::reference_wrapper<xtd::forms::context_menu>> drop_down_menu;
+  std::optional<xtd::ref<xtd::forms::context_menu>> drop_down_menu;
   bool enabled = true;
   size_t image_index = xtd::forms::image_list::image_collection::npos;
   xtd::drawing::image image_key;
