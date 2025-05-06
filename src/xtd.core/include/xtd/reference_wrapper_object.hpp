8b5286c5f4dd4682a2a05d8916cc4d3960ca5263
Gammasoft <gammasoft71@gmail.com>
2025-05-06 10:55:12 +0200

    Add assignment operators

--- src/xtd.core/include/xtd/reference_wrapper_object.hpp
+++ src/xtd.core/include/xtd/reference_wrapper_object.hpp
@@ -265,6 +265,12 @@ namespace xtd {
     }
     /// @brief Assignment operator with specified xtd::reference_wrapper_object value.
     /// @param value The value to assign.
+    reference_wrapper_object& operator =(reference_wrapper_object&& value) noexcept {
+      ref_ = std::move(value.ref_);
+      return *this;
+    }
+    /// @brief Assignment operator with specified xtd::reference_wrapper_object value.
+    /// @param value The value to assign.
     template<class value_t>
     reference_wrapper_object& operator =(const reference_wrapper_object<value_t>& value) noexcept {
       ref_ = value.ref_;
@@ -275,6 +281,13 @@ namespace xtd {
     template<class value_t>
     reference_wrapper_object& operator =(reference_wrapper_object<value_t>& value) noexcept {
       ref_ = value.ref_;
+      return *this;
+    }
+    /// @brief Assignment operator with specified xtd::reference_wrapper_object value.
+    /// @param value The value to assign.
+    template<class value_t>
+    reference_wrapper_object& operator =(reference_wrapper_object<value_t>&& value) noexcept {
+      ref_ = std::move(value.ref_);
       return *this;
     }
     /// @brief Assignment operator with specified xtd::reference_wrapper_object::base_type value.
