4a513e347173fd390383db6193d3ad1110aecfa7
Gammasoft <gammasoft71@gmail.com>
2025-05-08 22:13:53 +0200

    Add check and message

--- scripts/install/uninstall.cmd
+++ scripts/install/uninstall.cmd
@@ -1,38 +1,70 @@
 @echo off
+setlocal enabledelayedexpansion
 
+::______________________________________________________________________________
+::                                                                 Check context
 if "%xtd_version%"== "" (
-  echo ERROR : Use uninstall.cmd from root folder
+  echo ---------------------------------------------------------------
+  echo ERROR : Use uninstall.cmd from the root folder!
+  echo ---------------------------------------------------------------
   exit /B 1
 )
 
-echo Uninstall xtd libraries version %xtd_version%, copyright © 2024 Gammasoft
+echo Uninstalling xtd libraries version %xtd_version%, copyright © 2025 Gammasoft
+echo.
 
 ::______________________________________________________________________________
 ::                                                   Check if administrator mode
 whoami /Groups | find "12288" > nul 2>&1
 if %ERRORLEVEL% neq 0 (
-  echo You are not in administrator mode!
-  echo Run cmd as administrator before execute install.
+  echo ---------------------------------------------------------------
+  echo ERROR : You are not in administrator mode!
+  echo Please run cmd as administrator before executing uninstall.
+  echo ---------------------------------------------------------------
   exit /B 1
 )
 
-echo.
-echo ---------------------------------------------------------------
-echo.
-echo WARNING : You will unsinstall xtd!
-echo.
-echo ---------------------------------------------------------------
-echo.
-echo Press ENTER to continue or CTRL-C to stop...
-pause > nul 2>&1
+::______________________________________________________________________________
+::                                                   Optional parameter --silent
+set silent=
+if /I "%1"=="--silent" (
+  set silent=1
+  shift
+)
+
+::______________________________________________________________________________
+::                                                                  User warning
+if not defined silent (
+  echo ---------------------------------------------------------------
+  echo WARNING : This will completely uninstall xtd.
+  echo ---------------------------------------------------------------
+  echo Press ENTER to continue or CTRL-C to cancel...
+  pause > nul
+)
 
 ::______________________________________________________________________________
 ::                                                                Removing files
 echo Removing files...
-del /F /S /Q "%ProgramFiles(x86)%\wxwidgets" > nul 2>&1
-del /F /S /Q "%ProgramFiles(x86)%\xtd" > nul 2>&1
-del /F /S /Q "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\xtd" > nul 2>&1
+rd /S /Q "%ProgramFiles(x86)%\wxwidgets" > nul 2>&1
+rd /S /Q "%ProgramFiles(x86)%\xtd" > nul 2>&1
+rd /S /Q "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\xtd" > nul 2>&1
 
+::______________________________________________________________________________
+::                                                            Cleaning variables
+echo Cleaning environment variables...
+reg delete "HKCU\Environment" /v XTD_ROOT_PATH /f > nul 2>&1
+reg delete "HKCU\Environment" /v XTD_TOOLKIT_PATH /f > nul 2>&1
+
+:: WARNING : Do not attempt to modify PATH directly via script to avoid 
+::           incorrect deletions.
+
+
+::______________________________________________________________________________
+::                                                                 Final message
 echo.
-echo xtd is uninstalled.
-echo If you want to use xtd again, you will have to reinstall it.
+echo ---------------------------------------------------------------
+echo xtd has been successfully uninstalled.
+echo If you want to reinstall it, just run install.cmd again.
+echo ---------------------------------------------------------------
+
+endlocal
