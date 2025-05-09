76347c1e5976ea1a53f2885d2a0fc9ecee1ed587
Gammasoft <gammasoft71@gmail.com>
2025-05-09 22:06:02 +0200

    Review scripts

--- scripts/github/build_xtd_and_launch_unit_tests_on_macos.sh
+++ scripts/github/build_xtd_and_launch_unit_tests_on_macos.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 
-# clone, generate and build wxwidgets 3.2.2.1
-mkdir -p build/thirdparty/ && cd build/thirdparty
-git clone https://github.com/wxwidgets/wxwidgets.git -b v3.2.2.1 --depth 1
-cd wxwidgets
-git submodule update --init
-mkdir build_cmake && cd build_cmake
-cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DwxBUILD_SHARED=OFF -DCMAKE_INSTALL_PREFIX=~/local
-if [ $? -ne 0 ]; then exit -1; fi
-cmake --build . --target install -- -j 8
-if [ $? -ne 0 ]; then exit -1; fi
-cd ..
-rm -rf build_cmake
-cd ../../..
-
 # generate and build lib
-git submodule update --init
 mkdir -p build && cd build
 cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DXTD_BUILD_WITH_CONTINUOUS_INTEGRATION_SYSTEM=ON -DXTD_BUILD_TESTS_XTD_CORE=ON -DXTD_BUILD_TESTS_XTD_DRAWING=ON -DXTD_BUILD_TESTS_XTD_FORMS=ON -DXTD_BUILD_TESTS_XTD_TUNIT=ON -DCMAKE_INSTALL_PREFIX=~/local
 if [ $? -ne 0 ]; then exit -1; fi
