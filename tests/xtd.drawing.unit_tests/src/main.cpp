bf88218ac04a2823ae08ef6111cec8e86cca73ac
Gammasoft <gammasoft71@gmail.com>
2025-05-10 23:11:50 +0200

    Set param brief to true if build for CI

--- tests/xtd.drawing.unit_tests/src/main.cpp
+++ tests/xtd.drawing.unit_tests/src/main.cpp
@@ -5,6 +5,9 @@ namespace xtd::drawing {
   class unit_tests {
   public:
     static auto main() -> int {
+#if defined(__XTD_BUILD_WITH_CONTINUOUS_INTEGRATION_SYSTEM__)
+      xtd::tunit::settings::default_settings().brief(true);
+#endif
       return xtd::tunit::console_unit_test {}.run();
     }
   };
