830626d9bb0b8a4acc968062b280cb3d8646fd1d
Gammasoft <gammasoft71@gmail.com>
2025-05-12 11:23:57 +0200

    check CI

--- scripts/github/build_xtd_and_launch_unit_tests_on_macos.sh
+++ scripts/github/build_xtd_and_launch_unit_tests_on_macos.sh
@@ -2,7 +2,7 @@
 
 # generate and build lib
 mkdir -p build && cd build
-cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DXTD_BUILD_CPP_STANDARD=23 -DXTD_BUILD_WITH_CONTINUOUS_INTEGRATION_SYSTEM=ON -DXTD_BUILD_TESTS_XTD_CORE=OFF -DXTD_BUILD_TESTS_XTD_DRAWING=OFF -DXTD_BUILD_TESTS_XTD_FORMS=OFF -DXTD_BUILD_TESTS_XTD_TUNIT=OFF -DCMAKE_INSTALL_PREFIX=~/local
+cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DXTD_BUILD_CPP_STANDARD=23 -DXTD_BUILD_WITH_CONTINUOUS_INTEGRATION_SYSTEM=ON -DXTD_BUILD_TESTS_XTD_CORE=ON -DXTD_BUILD_TESTS_XTD_DRAWING=OFF -DXTD_BUILD_TESTS_XTD_FORMS=OFF -DXTD_BUILD_TESTS_XTD_TUNIT=OFF -DCMAKE_INSTALL_PREFIX=~/local
 if [ $? -ne 0 ]; then exit -1; fi
 cmake --build . -- -j 8
 if [ $? -ne 0 ]; then exit -1; fi
@@ -11,10 +11,10 @@ if [ $? -ne 0 ]; then exit -1; fi
 cd ..
 
 # run registered unit tests
-cd build
-ctest -j 8 --output-on-failure --build-config $BUILD_TYPE
-if [ $? -ne 0 ]; then exit -1; fi
-cd ..
+#cd build
+#ctest -j 8 --output-on-failure --build-config $BUILD_TYPE
+#if [ $? -ne 0 ]; then exit -1; fi
+#cd ..
 
 cd build/tests/xtd.core.manual_tests/$BUILD_TYPE
 ./xtd.core.manual_tests
