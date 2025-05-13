822b206f6dcf3e6318f33a97137dcb2fa708e1d3
Gammasoft <gammasoft71@gmail.com>
2025-05-13 23:19:22 +0200

    Update manual_tests.cpp

--- tests/xtd.core.manual_tests/src/manual_tests.cpp
+++ tests/xtd.core.manual_tests/src/manual_tests.cpp
@@ -2,7 +2,7 @@
 
 auto main() -> int {
   auto sl = diagnostics::source_location::current();
-  console::write_line("source_location :");
+  console::write_line("source_location:");
   console::write_line("  file_name     = {}", sl.file_name());
   console::write_line("  function_name = {}", sl.function_name());
   console::write_line("  line          = {}", sl.line());
