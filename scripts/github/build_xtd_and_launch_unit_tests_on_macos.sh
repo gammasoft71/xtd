7f7ab180023574f55695dc3c23807d8ae3b076b2
Gammasoft <gammasoft71@gmail.com>
2025-05-14 15:51:14 +0200

    Review script

--- scripts/github/build_xtd_and_launch_unit_tests_on_macos.sh
+++ scripts/github/build_xtd_and_launch_unit_tests_on_macos.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
+set -e
+set -o pipefail
 
-# generate and build lib
+echo "🔧 Configuring CMake..."
 mkdir -p build && cd build
-cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DXTD_BUILD_CPP_STANDARD=23 -DXTD_BUILD_WITH_CONTINUOUS_INTEGRATION_SYSTEM=ON -DXTD_BUILD_TESTS_XTD_CORE=ON -DXTD_BUILD_TESTS_XTD_DRAWING=OFF -DXTD_BUILD_TESTS_XTD_FORMS=OFF -DXTD_BUILD_TESTS_XTD_TUNIT=OFF -DCMAKE_INSTALL_PREFIX=~/local
-if [ $? -ne 0 ]; then exit -1; fi
-cmake --build . -- -j 8
-if [ $? -ne 0 ]; then exit -1; fi
-cmake --build . --target install
-if [ $? -ne 0 ]; then exit -1; fi
-cd ..
+cmake .. \
+  -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+  -DXTD_BUILD_CPP_STANDARD=23 \
+  -DXTD_BUILD_WITH_CONTINUOUS_INTEGRATION_SYSTEM=ON \
+  -DXTD_BUILD_TESTS_XTD_CORE=ON \
+  -DXTD_BUILD_TESTS_XTD_DRAWING=OFF \
+  -DXTD_BUILD_TESTS_XTD_FORMS=OFF \
+  -DXTD_BUILD_TESTS_XTD_TUNIT=OFF \
+  -DCMAKE_INSTALL_PREFIX=~/local
 
-# run registered unit tests
-cd build
-ctest -j 8 --output-on-failure --build-config $BUILD_TYPE
-if [ $? -ne 0 ]; then exit -1; fi
-cd ..
+echo "🛠 Building..."
+cmake --build . -- -j8
+
+echo "📦 Installing..."
+cmake --build . --target install
+
+echo "🧪 Running tests..."
+ctest -j8 --output-on-failure --build-config $BUILD_TYPE
