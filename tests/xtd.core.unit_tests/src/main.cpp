b00c746d7ddb5a4d905c9a9b51b6b2d07cbf6bd2
Gammasoft <gammasoft71@gmail.com>
2025-05-16 14:13:41 +0200

    Comment temporary for coverage

--- tests/xtd.core.unit_tests/src/main.cpp
+++ tests/xtd.core.unit_tests/src/main.cpp
@@ -6,7 +6,7 @@ namespace xtd::core {
   public:
     static auto main() -> int {
 #if defined(__XTD_BUILD_WITH_CONTINUOUS_INTEGRATION_SYSTEM__)
-      xtd::tunit::settings::default_settings().brief(true);
+      //xtd::tunit::settings::default_settings().brief(true);
 #endif
       return xtd::tunit::console_unit_test {}.run();
     }
