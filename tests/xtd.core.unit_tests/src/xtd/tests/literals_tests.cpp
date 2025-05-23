65615e8827ef27a3deeac66deaafadb17169e102
Gammasoft <gammasoft71@gmail.com>
2025-05-23 21:04:34 +0200

    Ad literals unit tests

--- tests/xtd.core.unit_tests/src/xtd/tests/literals_tests.cpp
+++ tests/xtd.core.unit_tests/src/xtd/tests/literals_tests.cpp
@@ -10,85 +10,129 @@ using namespace xtd::tunit;
 namespace xtd::tests {
   class test_class_(lietarls_tests) {
     void test_method_(_b_operator_on_char_ptr) {
-      auto b = "42"_b;
-      assert::is_true(is<byte>(b));
-      assert::are_equal(42_b, b);
+      auto v = "42"_b;
+      assert::is_true(is<byte>(v));
+      assert::are_equal(42_b, v);
     }
     
 #if defined(__xtd__cpp_lib_char8_t)
     void test_method_(_b_operator_on_char8_ptr) {
-      auto b = u8"42"_b;
-      assert::is_true(is<byte>(b));
-      assert::are_equal(42_b, b);
+      auto v = u8"42"_b;
+      assert::is_true(is<byte>(v));
+      assert::are_equal(42_b, v);
     }
 #endif
     
     void test_method_(_b_operator_on_char16_ptr) {
-      auto b = u"42"_b;
-      assert::is_true(is<byte>(b));
-      assert::are_equal(42_b, b);
+      auto v = u"42"_b;
+      assert::is_true(is<byte>(v));
+      assert::are_equal(42_b, v);
     }
     
     void test_method_(_b_operator_on_char32_ptr) {
-      auto b = U"42"_b;
-      assert::is_true(is<byte>(b));
-      assert::are_equal(42_b, b);
+      auto v = U"42"_b;
+      assert::is_true(is<byte>(v));
+      assert::are_equal(42_b, v);
     }
     
     void test_method_(_b_operator_on_wchar_ptr) {
-      auto b = L"42"_b;
-      assert::is_true(is<byte>(b));
-      assert::are_equal(42_b, b);
+      auto v = L"42"_b;
+      assert::is_true(is<byte>(v));
+      assert::are_equal(42_b, v);
     }
 
     void test_method_(_b_operator_on_long_double) {
-      auto b = 42.0_b;
-      assert::is_true(is<byte>(b));
-      assert::are_equal(42_b, b);
+      auto v = 42.0_b;
+      assert::is_true(is<byte>(v));
+      assert::are_equal(42_b, v);
     }
 
     void test_method_(_b_operator_on_unsigned_long_long) {
-      auto b = 42_b;
-      assert::is_true(is<byte>(b));
-      assert::are_equal(42_b, b);
+      auto v = 42_b;
+      assert::is_true(is<byte>(v));
+      assert::are_equal(42_b, v);
     }
 
+    void test_method_(_d_operator_on_char_ptr) {
+      auto v = "42.0"_d;
+      assert::is_true(is<double>(v));
+      assert::are_equal(42.0_d, v);
+    }
+    
+#if defined(__xtd__cpp_lib_char8_t)
+    void test_method_(_d_operator_on_char8_ptr) {
+      auto v = u8"42.0"_d;
+      assert::is_true(is<double>(v));
+      assert::are_equal(42.0_d, v);
+    }
+#endif
+    
+    void test_method_(_d_operator_on_char16_ptr) {
+      auto v = u"42.0"_d;
+      assert::is_true(is<double>(v));
+      assert::are_equal(42.0_d, v);
+    }
+    
+    void test_method_(_d_operator_on_char32_ptr) {
+      auto v = U"42.0"_d;
+      assert::is_true(is<double>(v));
+      assert::are_equal(42.0_d, v);
+    }
+    
+    void test_method_(_d_operator_on_wchar_ptr) {
+      auto v = L"42.0"_d;
+      assert::is_true(is<double>(v));
+      assert::are_equal(42.0_d, v);
+    }
+    
+    void test_method_(_d_operator_on_long_double) {
+      auto v = 42.0_d;
+      assert::is_true(is<double>(v));
+      assert::are_equal(42.0_b, v);
+    }
+    
+    void test_method_(_d_operator_on_unsigned_long_long) {
+      auto v = 42_d;
+      assert::is_true(is<double>(v));
+      assert::are_equal(42.0_d, v);
+    }
+    
     void test_method_(_s_operator_on_char_ptr) {
-      auto s = "string"_s;
-      assert::is_instance_of<string>(s);
-      assert::are_equal("string", s);
+      auto v = "string"_s;
+      assert::is_instance_of<string>(v);
+      assert::are_equal("string", v);
     }
     
 #if defined(__xtd__cpp_lib_char8_t)
     void test_method_(_s_operator_on_char8_ptr) {
-      auto s = u8"string"_s;
-      assert::is_instance_of<string>(s);
-      assert::are_equal("string", s);
+      auto v = u8"string"_s;
+      assert::is_instance_of<string>(v);
+      assert::are_equal("string", v);
     }
 #endif
     
     void test_method_(_s_operator_on_char16_ptr) {
-      auto s = u"string"_s;
-      assert::is_instance_of<string>(s);
-      assert::are_equal("string", s);
+      auto v = u"string"_s;
+      assert::is_instance_of<string>(v);
+      assert::are_equal("string", v);
     }
     
     void test_method_(_s_operator_on_char32_ptr) {
-      auto s = U"string"_s;
-      assert::is_instance_of<string>(s);
-      assert::are_equal("string", s);
+      auto v = U"string"_s;
+      assert::is_instance_of<string>(v);
+      assert::are_equal("string", v);
     }
 
     void test_method_(_sd_operator_on_long_double) {
-      auto s = 42.0_sd;
-      assert::is_instance_of<string>(s);
-      assert::are_equal("42", s);
+      auto v = 42.0_sd;
+      assert::is_instance_of<string>(v);
+      assert::are_equal("42", v);
     }
 
     void test_method_(_sd_operator_on_unsigned_long_long) {
-      auto s = 42_sd;
-      assert::is_instance_of<string>(s);
-      assert::are_equal("42", s);
+      auto v = 42_sd;
+      assert::is_instance_of<string>(v);
+      assert::are_equal("42", v);
     }
   };
 }
