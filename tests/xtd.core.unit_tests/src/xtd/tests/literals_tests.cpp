58a4dddab74ddbbf80b0bd46c5f62f11c216b305
Gammasoft <gammasoft71@gmail.com>
2025-05-24 14:28:34 +0200

    Add literals unit tests

--- tests/xtd.core.unit_tests/src/xtd/tests/literals_tests.cpp
+++ tests/xtd.core.unit_tests/src/xtd/tests/literals_tests.cpp
@@ -946,6 +946,70 @@ namespace xtd::tests {
     void test_method_(_u32_operator_on_invalid_wchar_ptr) {
       assert::throws<xtd::format_exception>([] {L"42str"_u32;});
     }
+    
+    void test_method_(_u64_operator_on_long_double) {
+      auto v = 42.0_u64;
+      assert::is_true(is<uint64>(v));
+      assert::are_equal(42_u64, v);
+    }
+    
+    void test_method_(_u64_operator_on_unsigned_long_long) {
+      auto v = 42_u64;
+      assert::is_true(is<uint64>(v));
+      assert::are_equal(42_u64, v);
+    }
+    
+    void test_method_(_u64_operator_on_char_ptr) {
+      auto v = "42"_u64;
+      assert::is_true(is<uint64>(v));
+      assert::are_equal(42_u64, v);
+    }
+    
+    void test_method_(_u64_operator_on_invalid_char_ptr) {
+      assert::throws<xtd::format_exception>([] {"42str"_u64;});
+    }
+    
+#if defined(__xtd__cpp_lib_char8_t)
+    void test_method_(_u64_operator_on_char8_ptr) {
+      auto v = u8"42"_u64;
+      assert::is_true(is<uint64>(v));
+      assert::are_equal(42_u64, v);
+    }
+    
+    void test_method_(_u64_operator_on_invalid_char8_ptr) {
+      assert::throws<xtd::format_exception>([] {u8"42str"_u64;});
+    }
+#endif
+    
+    void test_method_(_u64_operator_on_char16_ptr) {
+      auto v = u"42"_u64;
+      assert::is_true(is<uint64>(v));
+      assert::are_equal(42_u64, v);
+    }
+    
+    void test_method_(_u64_operator_on_invalid_char16_ptr) {
+      assert::throws<xtd::format_exception>([] {u"42str"_u64;});
+    }
+    
+    void test_method_(_u64_operator_on_char32_ptr) {
+      auto v = U"42"_u64;
+      assert::is_true(is<uint64>(v));
+      assert::are_equal(42_u64, v);
+    }
+    
+    void test_method_(_u64_operator_on_invalid_char32_ptr) {
+      assert::throws<xtd::format_exception>([] {U"42str"_u64;});
+    }
+    
+    void test_method_(_u64_operator_on_wchar_ptr) {
+      auto v = L"42"_u64;
+      assert::is_true(is<uint64>(v));
+      assert::are_equal(42_u64, v);
+    }
+    
+    void test_method_(_u64_operator_on_invalid_wchar_ptr) {
+      assert::throws<xtd::format_exception>([] {L"42str"_u64;});
+    }
 
     //_________________________________________________________________________
     //                                                          String suffixes
