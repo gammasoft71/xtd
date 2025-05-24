4c71cba0160c236a2c830d2b0b3e94fe837f0ab8
Gammasoft <gammasoft71@gmail.com>
2025-05-24 14:08:20 +0200

    Add literals unit tests

--- tests/xtd.core.unit_tests/src/xtd/tests/literals_tests.cpp
+++ tests/xtd.core.unit_tests/src/xtd/tests/literals_tests.cpp
@@ -563,6 +563,70 @@ namespace xtd::tests {
       assert::throws<xtd::format_exception>([] {L"42str"_s8;});
     }
 
+    void test_method_(_s16_operator_on_long_double) {
+      auto v = 42.0_s16;
+      assert::is_true(is<int16>(v));
+      assert::are_equal(42_s16, v);
+    }
+    
+    void test_method_(_s16_operator_on_unsigned_long_long) {
+      auto v = 42_s16;
+      assert::is_true(is<int16>(v));
+      assert::are_equal(42_s16, v);
+    }
+    
+    void test_method_(_s16_operator_on_char_ptr) {
+      auto v = "42"_s16;
+      assert::is_true(is<int16>(v));
+      assert::are_equal(42_s16, v);
+    }
+    
+    void test_method_(_s16_operator_on_invalid_char_ptr) {
+      assert::throws<xtd::format_exception>([] {"42str"_s16;});
+    }
+    
+#if defined(__xtd__cpp_lib_char8_t)
+    void test_method_(_s16_operator_on_char8_ptr) {
+      auto v = u8"42"_s16;
+      assert::is_true(is<int16>(v));
+      assert::are_equal(42_s16, v);
+    }
+    
+    void test_method_(_s16_operator_on_invalid_char8_ptr) {
+      assert::throws<xtd::format_exception>([] {u8"42str"_s16;});
+    }
+#endif
+    
+    void test_method_(_s16_operator_on_char16_ptr) {
+      auto v = u"42"_s16;
+      assert::is_true(is<int16>(v));
+      assert::are_equal(42_s16, v);
+    }
+    
+    void test_method_(_s16_operator_on_invalid_char16_ptr) {
+      assert::throws<xtd::format_exception>([] {u"42str"_s16;});
+    }
+    
+    void test_method_(_s16_operator_on_char32_ptr) {
+      auto v = U"42"_s16;
+      assert::is_true(is<int16>(v));
+      assert::are_equal(42_s16, v);
+    }
+    
+    void test_method_(_s16_operator_on_invalid_char32_ptr) {
+      assert::throws<xtd::format_exception>([] {U"42str"_s16;});
+    }
+    
+    void test_method_(_s16_operator_on_wchar_ptr) {
+      auto v = L"42"_s16;
+      assert::is_true(is<int16>(v));
+      assert::are_equal(42_s16, v);
+    }
+    
+    void test_method_(_s16_operator_on_invalid_wchar_ptr) {
+      assert::throws<xtd::format_exception>([] {L"42str"_s16;});
+    }
+
     //_________________________________________________________________________
     //                                                          String suffixes
     void test_method_(_s_operator_on_char_ptr) {
