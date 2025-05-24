70423855d35f99579f064836d9103d3c9a68a2ee
Gammasoft <gammasoft71@gmail.com>
2025-05-24 14:30:29 +0200

    Add literals unit tests

--- tests/xtd.core.unit_tests/src/xtd/tests/literals_tests.cpp
+++ tests/xtd.core.unit_tests/src/xtd/tests/literals_tests.cpp
@@ -1010,6 +1010,70 @@ namespace xtd::tests {
     void test_method_(_u64_operator_on_invalid_wchar_ptr) {
       assert::throws<xtd::format_exception>([] {L"42str"_u64;});
     }
+    
+    void test_method_(_uz_operator_on_long_double) {
+      auto v = 42.0_uz;
+      assert::is_true(is<size>(v));
+      assert::are_equal(42_uz, v);
+    }
+    
+    void test_method_(_uz_operator_on_unsigned_long_long) {
+      auto v = 42_uz;
+      assert::is_true(is<size>(v));
+      assert::are_equal(42_uz, v);
+    }
+    
+    void test_method_(_uz_operator_on_char_ptr) {
+      auto v = "42"_uz;
+      assert::is_true(is<size>(v));
+      assert::are_equal(42_uz, v);
+    }
+    
+    void test_method_(_uz_operator_on_invalid_char_ptr) {
+      assert::throws<xtd::format_exception>([] {"42str"_uz;});
+    }
+    
+#if defined(__xtd__cpp_lib_char8_t)
+    void test_method_(_uz_operator_on_char8_ptr) {
+      auto v = u8"42"_uz;
+      assert::is_true(is<size>(v));
+      assert::are_equal(42_uz, v);
+    }
+    
+    void test_method_(_uz_operator_on_invalid_char8_ptr) {
+      assert::throws<xtd::format_exception>([] {u8"42str"_uz;});
+    }
+#endif
+    
+    void test_method_(_uz_operator_on_char16_ptr) {
+      auto v = u"42"_uz;
+      assert::is_true(is<size>(v));
+      assert::are_equal(42_uz, v);
+    }
+    
+    void test_method_(_uz_operator_on_invalid_char16_ptr) {
+      assert::throws<xtd::format_exception>([] {u"42str"_uz;});
+    }
+    
+    void test_method_(_uz_operator_on_char32_ptr) {
+      auto v = U"42"_uz;
+      assert::is_true(is<size>(v));
+      assert::are_equal(42_uz, v);
+    }
+    
+    void test_method_(_uz_operator_on_invalid_char32_ptr) {
+      assert::throws<xtd::format_exception>([] {U"42str"_uz;});
+    }
+    
+    void test_method_(_uz_operator_on_wchar_ptr) {
+      auto v = L"42"_uz;
+      assert::is_true(is<size>(v));
+      assert::are_equal(42_uz, v);
+    }
+    
+    void test_method_(_uz_operator_on_invalid_wchar_ptr) {
+      assert::throws<xtd::format_exception>([] {L"42str"_uz;});
+    }
 
     //_________________________________________________________________________
     //                                                          String suffixes
