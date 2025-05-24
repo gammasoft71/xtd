630b125667b854128e7c2e388edb02576a429f88
Gammasoft <gammasoft71@gmail.com>
2025-05-24 14:09:46 +0200

    Add literals unit tests

--- tests/xtd.core.unit_tests/src/xtd/tests/literals_tests.cpp
+++ tests/xtd.core.unit_tests/src/xtd/tests/literals_tests.cpp
@@ -627,6 +627,70 @@ namespace xtd::tests {
       assert::throws<xtd::format_exception>([] {L"42str"_s16;});
     }
 
+    void test_method_(_s32_operator_on_long_double) {
+      auto v = 42.0_s32;
+      assert::is_true(is<int32>(v));
+      assert::are_equal(42_s32, v);
+    }
+    
+    void test_method_(_s32_operator_on_unsigned_long_long) {
+      auto v = 42_s32;
+      assert::is_true(is<int32>(v));
+      assert::are_equal(42_s32, v);
+    }
+    
+    void test_method_(_s32_operator_on_char_ptr) {
+      auto v = "42"_s32;
+      assert::is_true(is<int32>(v));
+      assert::are_equal(42_s32, v);
+    }
+    
+    void test_method_(_s32_operator_on_invalid_char_ptr) {
+      assert::throws<xtd::format_exception>([] {"42str"_s32;});
+    }
+    
+#if defined(__xtd__cpp_lib_char8_t)
+    void test_method_(_s32_operator_on_char8_ptr) {
+      auto v = u8"42"_s32;
+      assert::is_true(is<int32>(v));
+      assert::are_equal(42_s32, v);
+    }
+    
+    void test_method_(_s32_operator_on_invalid_char8_ptr) {
+      assert::throws<xtd::format_exception>([] {u8"42str"_s32;});
+    }
+#endif
+    
+    void test_method_(_s32_operator_on_char16_ptr) {
+      auto v = u"42"_s32;
+      assert::is_true(is<int32>(v));
+      assert::are_equal(42_s32, v);
+    }
+    
+    void test_method_(_s32_operator_on_invalid_char16_ptr) {
+      assert::throws<xtd::format_exception>([] {u"42str"_s32;});
+    }
+    
+    void test_method_(_s32_operator_on_char32_ptr) {
+      auto v = U"42"_s32;
+      assert::is_true(is<int32>(v));
+      assert::are_equal(42_s32, v);
+    }
+    
+    void test_method_(_s32_operator_on_invalid_char32_ptr) {
+      assert::throws<xtd::format_exception>([] {U"42str"_s32;});
+    }
+    
+    void test_method_(_s32_operator_on_wchar_ptr) {
+      auto v = L"42"_s32;
+      assert::is_true(is<int32>(v));
+      assert::are_equal(42_s32, v);
+    }
+    
+    void test_method_(_s32_operator_on_invalid_wchar_ptr) {
+      assert::throws<xtd::format_exception>([] {L"42str"_s32;});
+    }
+
     //_________________________________________________________________________
     //                                                          String suffixes
     void test_method_(_s_operator_on_char_ptr) {
