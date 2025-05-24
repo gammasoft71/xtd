8cbb09cac27f9dcc2ac17fe9014122610770412b
Gammasoft <gammasoft71@gmail.com>
2025-05-24 13:43:32 +0200

    Add literals unit tests

--- tests/xtd.core.unit_tests/src/xtd/tests/literals_tests.cpp
+++ tests/xtd.core.unit_tests/src/xtd/tests/literals_tests.cpp
@@ -69,11 +69,19 @@ namespace xtd::tests {
       assert::are_equal(42_b, v);
     }
     
+    void test_method_(_b_operator_on_invalid_char_ptr) {
+      assert::throws<xtd::format_exception>([] {"str"_b;});
+    }
+    
 #if defined(__xtd__cpp_lib_char8_t)
     void test_method_(_b_operator_on_char8_ptr) {
       auto v = u8"42"_b;
       assert::is_true(is<byte>(v));
       assert::are_equal(42_b, v);
+    }
+    
+    void test_method_(_b_operator_on_invalid_char8_ptr) {
+      assert::throws<xtd::format_exception>([] {u8"str"_b;});
     }
 #endif
     
@@ -83,16 +91,28 @@ namespace xtd::tests {
       assert::are_equal(42_b, v);
     }
     
+    void test_method_(_b_operator_on_invalid_char16_ptr) {
+      assert::throws<xtd::format_exception>([] {u"str"_b;});
+    }
+
     void test_method_(_b_operator_on_char32_ptr) {
       auto v = U"42"_b;
       assert::is_true(is<byte>(v));
       assert::are_equal(42_b, v);
     }
     
+    void test_method_(_b_operator_on_invalid_char32_ptr) {
+      assert::throws<xtd::format_exception>([] {U"str"_b;});
+    }
+
     void test_method_(_b_operator_on_wchar_ptr) {
       auto v = L"42"_b;
       assert::is_true(is<byte>(v));
       assert::are_equal(42_b, v);
+    }
+    
+    void test_method_(_b_operator_on_invalid_wchar_ptr) {
+      assert::throws<xtd::format_exception>([] {L"str"_b;});
     }
 
     void test_method_(_d_operator_on_long_double) {
