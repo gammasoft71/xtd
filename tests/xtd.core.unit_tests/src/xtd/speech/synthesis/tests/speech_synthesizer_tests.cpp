8855dd6528e48f81cfa2efe952bd023683f93223
Gammasoft <gammasoft71@gmail.com>
2025-05-24 20:49:07 +0200

    Add unit tests

--- tests/xtd.core.unit_tests/src/xtd/speech/synthesis/tests/speech_synthesizer_tests.cpp
+++ tests/xtd.core.unit_tests/src/xtd/speech/synthesis/tests/speech_synthesizer_tests.cpp
@@ -14,16 +14,86 @@ namespace xtd::speech::synthesis::tests {
     }
 
     void test_method_(state) {
-      assert::are_equal(synthesizer_state::ready, speech_synthesizer {}.state());
+      auto s = speech_synthesizer {};
+      assert::are_equal(synthesizer_state::ready, s.state());
+      s.speak("");
+      assert::are_equal(synthesizer_state::ready, s.state());
     }
 
     void test_method_(speak_with_prompt) {
       auto p = prompt("");
-      assert::does_not_throw([&p] {speech_synthesizer {}.speak(p);});
+      assert::is_false(p.is_completed());
+      speech_synthesizer {}.speak(p);
+      assert::is_true(p.is_completed());
     }
-
+    
     void test_method_(speak_with_string) {
       assert::does_not_throw([] {speech_synthesizer {}.speak("");});
+    }
+
+    void test_method_(speak_async_with_prompt) {
+      auto p = prompt("");
+      speech_synthesizer {}.speak_async(p);
+      assert::is_true(p.is_completed());
+    }
+    
+    void test_method_(speak_async_with_string) {
+      auto p = speech_synthesizer {}.speak_async("");
+      assert::is_true(p.is_completed());
+    }
+
+    void test_method_(speak_completed) {
+      auto has_completed_event = false;
+      auto p = prompt("");
+      auto s = speech_synthesizer {};
+      
+      s.speak_completed += [&](object& sender, const speak_completed_event_args& e) {
+        assert::is_false(e.cancelled());
+        assert::is_false(e.error().has_value());
+        assert::are_same(p, e.prompt());
+        assert::is_true(e.prompt().is_completed());
+        assert::is_false(e.user_state().has_value());
+        has_completed_event = true;
+      };
+
+      s.speak(p);
+      assert::is_true(has_completed_event);
+    }
+
+    void test_method_(speak_started) {
+      auto has_started_event = false;
+      auto p = prompt("");
+      auto s = speech_synthesizer {};
+      
+      s.speak_started += [&](object& sender, const speak_started_event_args& e) {
+        assert::is_false(e.cancelled());
+        assert::is_false(e.error().has_value());
+        assert::are_same(p, e.prompt());
+        assert::is_false(e.prompt().is_completed());
+        assert::is_false(e.user_state().has_value());
+        has_started_event = true;
+      };
+      s.speak(p);
+      assert::is_true(has_started_event);
+    }
+
+    void test_method_(state_changed) {
+      auto state_changed_event_value = 0;
+      auto p = prompt("");
+      auto s = speech_synthesizer {};
+      
+      s.state_changed += [&](object& sender, const state_changed_event_args& e) {
+        if (state_changed_event_value == 0) {
+          assert::are_equal(synthesizer_state::ready, e.previous_state());
+          assert::are_equal(synthesizer_state::speaking, e.state());
+        } else if (state_changed_event_value == 1) {
+          assert::are_equal(synthesizer_state::speaking, e.previous_state());
+          assert::are_equal(synthesizer_state::ready, e.state());
+        }
+        ++state_changed_event_value;
+      };
+      s.speak(p);
+      assert::are_equal(2, state_changed_event_value);
     }
   };
 }
