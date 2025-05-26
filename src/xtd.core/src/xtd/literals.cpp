1630811dfc715c35a80014701f1b24cfd8304fcd
Gammasoft <gammasoft71@gmail.com>
2025-05-26 16:18:50 +0200

    Add ip_address literals

--- src/xtd.core/src/xtd/literals.cpp
+++ src/xtd.core/src/xtd/literals.cpp
@@ -1,7 +1,10 @@
+#include "../../include/xtd/literals.hpp"
 #include "../../include/xtd/as.hpp"
 #include "../../include/xtd/translator.hpp"
+#include "../../include/xtd/net/ip_address.hpp"
 
 using namespace xtd;
+using namespace xtd::net;
 
 char32 xtd::literals::operator""_c(unsigned long long c) {
   return as<char32>(c);
@@ -49,6 +52,28 @@ guid xtd::literals::operator""_guid(const char32* s, xtd::size n) {
 
 guid xtd::literals::operator""_guid(const wchar* s, xtd::size n) {
   return guid(string {s});
+}
+
+ip_address xtd::literals::operator""_ip(const char* s, xtd::size n) {
+  return ip_address::parse(string {s});
+}
+
+#if defined(__xtd__cpp_lib_char8_t)
+ip_address xtd::literals::operator""_ip(const char8* s, xtd::size n) {
+  return ip_address::parse(string {s});
+}
+#endif
+
+ip_address xtd::literals::operator""_ip(const char16* s, xtd::size n) {
+  return ip_address::parse(string {s});
+}
+
+ip_address xtd::literals::operator""_ip(const char32* s, xtd::size n) {
+  return ip_address::parse(string {s});
+}
+
+ip_address xtd::literals::operator""_ip(const wchar* s, xtd::size n) {
+  return ip_address::parse(string {s});
 }
 
 xtd::byte xtd::literals::operator""_b(long double n) {
