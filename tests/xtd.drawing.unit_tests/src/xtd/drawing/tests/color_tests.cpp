5d1e78a13434369efc0e5a9f198c40b807c89977
Gammasoft <gammasoft71@gmail.com>
2025-05-26 19:42:24 +0200

    Add unit tests

--- tests/xtd.drawing.unit_tests/src/xtd/drawing/tests/color_tests.cpp
+++ tests/xtd.drawing.unit_tests/src/xtd/drawing/tests/color_tests.cpp
@@ -7271,3 +7271,39 @@ namespace xtd::drawing::tests {
     }
   };
 }
+
+namespace xtd::tests {
+  class test_class_(color_suffixes_tests) {
+    void test_method_(_color_operator_on_char_ptr) {
+      auto v = "dodger blue"_color;
+      assert::is_instance_of<color>(v);
+      assert::are_equal("color [dodger_blue]", v.to_string());
+    }
+    
+#if defined(__xtd__cpp_lib_char8_t)
+    void test_method_(_color_operator_on_char8_ptr) {
+      auto v = u8"dodger blue"_color;
+      assert::is_instance_of<color>(v);
+      assert::are_equal("color [dodger_blue]", v.to_string());
+    }
+#endif
+    
+    void test_method_(_color_operator_on_char16_ptr) {
+      auto v = u"dodger blue"_color;
+      assert::is_instance_of<color>(v);
+      assert::are_equal("color [dodger_blue]", v.to_string());
+    }
+    
+    void test_method_(_color_operator_on_char32_ptr) {
+      auto v = U"dodger blue"_color;
+      assert::is_instance_of<color>(v);
+      assert::are_equal("color [dodger_blue]", v.to_string());
+    }
+    
+    void test_method_(_color_operator_on_wchar_ptr) {
+      auto v = L"dodger blue"_color;
+      assert::is_instance_of<color>(v);
+      assert::are_equal("color [dodger_blue]", v.to_string());
+    }
+  };
+}
