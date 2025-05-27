0905b3d30184eeb41c7d2c4d7d09d64da0e3a373
Gammasoft <gammasoft71@gmail.com>
2025-05-27 22:46:42 +0200

    moving literal operators code

--- src/xtd.core/include/xtd/double.hpp
+++ src/xtd.core/include/xtd/double.hpp
@@ -2,11 +2,7 @@
 /// @brief Contains xtd::double_ type.
 /// @copyright Copyright (c) 2025 Gammasoft. All rights reserved.
 #pragma once
-#include <cstddef>
 #include <cstdint>
-#define __XTD_STD_INTERNAL__
-#include "internal/__xtd_std_version.hpp"
-#undef __XTD_STD_INTERNAL__
 
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
@@ -26,97 +22,6 @@ namespace xtd {
   /// @remarks For more information about types, see [Native types, boxing and unboxing](https://gammasoft71.github.io/xtd/docs/documentation/guides/xtd.core/Types%20overview/types).
   using double_ = double;
   /// @}
-
-  /// @brief The xtd::literals namespace contains all xtd's litterals operators.
-  inline namespace literals {
-    /// @name Double suffixes
-    
-    /// @{
-    /// @brief Used to convert specified value into byte.
-    /// @par Namespace
-    /// xtd
-    /// @par Library
-    /// xtd.core
-    /// @ingroup xtd_core literals
-    /// @par Examples
-    /// ```cpp
-    /// xtd::console::out << 1.6_d << xtd::environment::new_line;
-    /// ```
-    double operator""_d(long double n);
-    
-    /// @brief Used to convert specified value into byte.
-    /// @par Namespace
-    /// xtd
-    /// @par Library
-    /// xtd.core
-    /// @ingroup xtd_core literals
-    /// @par Examples
-    /// ```cpp
-    /// xtd::console::out << 54_d << xtd::environment::new_line;
-    /// ```
-    double operator""_d(unsigned long long n);
-    
-    /// @brief Used to convert specified value into byte.
-    /// @par Namespace
-    /// xtd
-    /// @par Library
-    /// xtd.core
-    /// @ingroup xtd_core literals
-    /// @par Examples
-    /// ```cpp
-    /// xtd::console::out << "125"_d << xtd::environment::new_line;
-    /// ```
-    double operator""_d(const char* s, std::size_t n);
-    
-#if defined(__xtd__cpp_lib_char8_t)
-    /// @brief Used to convert specified value into byte.
-    /// @par Namespace
-    /// xtd
-    /// @par Library
-    /// xtd.core
-    /// @ingroup xtd_core literals
-    /// @par Examples
-    /// ```cpp
-    /// xtd::console::out << u8"125"_d << xtd::environment::new_line;
-    /// ```
-    double operator""_d(const char8_t* s, std::size_t n);
-#endif
-    
-    /// @brief Used to convert specified value into byte.
-    /// @par Namespace
-    /// xtd
-    /// @par Library
-    /// xtd.core
-    /// @ingroup xtd_core literals
-    /// @par Examples
-    /// ```cpp
-    /// xtd::console::out << u"125"_d << xtd::environment::new_line;
-    /// ```
-    double operator""_d(const char16_t* s, std::size_t n);
-    
-    /// @brief Used to convert specified value into byte.
-    /// @par Namespace
-    /// xtd
-    /// @par Library
-    /// xtd.core
-    /// @ingroup xtd_core literals
-    /// @par Examples
-    /// ```cpp
-    /// xtd::console::out << U"125"_d << xtd::environment::new_line;
-    /// ```
-    double operator""_d(const char32_t* s, std::size_t n);
-    
-    /// @brief Used to convert specified value into byte.
-    /// @par Namespace
-    /// xtd
-    /// @par Library
-    /// xtd.core
-    /// @ingroup xtd_core literals
-    /// @par Examples
-    /// ```cpp
-    /// xtd::console::out << L"125"_d << xtd::environment::new_line;
-    /// ```
-    double operator""_d(const wchar_t* s, std::size_t n);
-    /// @}
-  }
 }
+
+#include "literals/double.hpp"
