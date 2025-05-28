3d8c2aab169a43cdc2b1bd279f17ce9f034aa4e1
Gammasoft <gammasoft71@gmail.com>
2025-05-28 03:22:23 +0200

    moving literal operators code

--- src/xtd.core/include/xtd/literals/translator.hpp
+++ src/xtd.core/include/xtd/literals/translator.hpp
@@ -2,8 +2,8 @@
 /// @brief Contains xtd::translator suffixes.
 /// @copyright Copyright (c) 2025 Gammasoft. All rights reserved.
 #pragma once
-#include <cstddef>
 #include "../string.hpp"
+#include "../types.hpp"
 #define __XTD_STD_INTERNAL__
 #include "../internal/__xtd_std_version.hpp"
 #undef __XTD_STD_INTERNAL__
@@ -26,7 +26,7 @@ namespace xtd {
     /// xtd::string s = "Belgium"_t;
     /// xtd::console::out << s << xtd::environment::new_line;
     /// ```
-    const char* operator""_t(const char* s, std::size_t n) noexcept;
+    const char* operator""_t(const char* s, xtd::size n) noexcept;
     
 #if defined(__xtd__cpp_lib_char8_t)
     /// @brief Used to translate specified string into locale.
@@ -40,7 +40,7 @@ namespace xtd {
     /// xtd::string s = u8"Belgium"_t;
     /// xtd::console::out << s << xtd::environment::new_line;
     /// ```
-    xtd::string operator""_t(const char8_t* s, std::size_t n) noexcept;
+    xtd::string operator""_t(const char8* s, xtd::size n) noexcept;
 #endif
     
     /// @brief Used to translate specified string into locale.
@@ -54,7 +54,7 @@ namespace xtd {
     /// xtd::string s = u"Belgium"_t;
     /// xtd::console::out << s << xtd::environment::new_line;
     /// ```
-    xtd::string operator""_t(const char16_t* s, std::size_t n) noexcept;
+    xtd::string operator""_t(const char16* s, xtd::size n) noexcept;
     
     /// @brief Used to translate specified string into locale.
     /// @par Namespace
@@ -67,7 +67,7 @@ namespace xtd {
     /// xtd::string s = U"Belgium"_t;
     /// xtd::console::out << s << xtd::environment::new_line;
     /// ```
-    xtd::string operator""_t(const char32_t* s, std::size_t n) noexcept;
+    xtd::string operator""_t(const char32* s, xtd::size n) noexcept;
     
     /// @brief Used to translate specified string into locale.
     /// @par Namespace
@@ -80,7 +80,7 @@ namespace xtd {
     /// xtd::string s = L"Belgium"_t;
     /// xtd::console::out << s << xtd::environment::new_line;
     /// ```
-    xtd::string operator""_t(const wchar_t* s, std::size_t n) noexcept;
+    xtd::string operator""_t(const wchar* s, xtd::size n) noexcept;
     /// @}
   }
 }
