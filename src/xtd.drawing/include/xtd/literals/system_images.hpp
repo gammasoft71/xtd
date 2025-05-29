2ed462a9cece71108762770b73b13cddc5af10fc
Gammasoft <gammasoft71@gmail.com>
2025-05-29 12:35:39 +0200

    Add documentations

--- src/xtd.drawing/include/xtd/literals/system_images.hpp
+++ src/xtd.drawing/include/xtd/literals/system_images.hpp
@@ -22,6 +22,9 @@ namespace xtd {
     /// xtd::drawing::image i = "system-file-manager"_image;
     /// i.save("system_file_manager.png");
     /// ```
+    /// @remarks The xtd::drawing::system_images::names contains the names of the images that can be used.
+    /// @remarks The xtd::drawing::size of the image is equal to xtd::drawing::system_images::default_size property.
+    /// @note If you want load image with other size use xtd::drawing::system_images::from_name (const xtd::string& name, const xtd::drawing::size& size) method.
     xtd::drawing::image operator""_image(const char* s, xtd::size n);
     
 #if defined(__xtd__cpp_lib_char8_t)
@@ -36,6 +39,9 @@ namespace xtd {
     /// xtd::drawing::image i = u8"system-file-manager"_image;
     /// i.save("system_file_manager.png");
     /// ```
+    /// @remarks The xtd::drawing::system_images::names contains the names of the images that can be used.
+    /// @remarks The xtd::drawing::size of the image is equal to xtd::drawing::system_images::default_size property.
+    /// @note If you want load image with other size use xtd::drawing::system_images::from_name (const xtd::string& name, const xtd::drawing::size& size) method.
     xtd::drawing::image operator""_image(const char8* s, xtd::size n);
 #endif
     
@@ -50,6 +56,9 @@ namespace xtd {
     /// xtd::drawing::image i = u"system-file-manager"_image;
     /// i.save("system_file_manager.png");
     /// ```
+    /// @remarks The xtd::drawing::system_images::names contains the names of the images that can be used.
+    /// @remarks The xtd::drawing::size of the image is equal to xtd::drawing::system_images::default_size property.
+    /// @note If you want load image with other size use xtd::drawing::system_images::from_name (const xtd::string& name, const xtd::drawing::size& size) method.
     xtd::drawing::image operator""_image(const char16* s, xtd::size n);
     
     /// @brief Used to convert specified value into xtd::drawing::image.
@@ -63,6 +72,9 @@ namespace xtd {
     /// xtd::drawing::image i = U"system-file-manager"_image;
     /// i.save("system_file_manager.png");
     /// ```
+    /// @remarks The xtd::drawing::system_images::names contains the names of the images that can be used.
+    /// @remarks The xtd::drawing::size of the image is equal to xtd::drawing::system_images::default_size property.
+    /// @note If you want load image with other size use xtd::drawing::system_images::from_name (const xtd::string& name, const xtd::drawing::size& size) method.
     xtd::drawing::image operator""_image(const char32* s, xtd::size n);
     
     /// @brief Used to convert specified value into xtd::drawing::image.
@@ -76,6 +88,9 @@ namespace xtd {
     /// xtd::drawing::image i = L"system-file-manager"_image;
     /// i.save("system_file_manager.png");
     /// ```
+    /// @remarks The xtd::drawing::system_images::names contains the names of the images that can be used.
+    /// @remarks The xtd::drawing::size of the image is equal to xtd::drawing::system_images::default_size property.
+    /// @note If you want load image with other size use xtd::drawing::system_images::from_name (const xtd::string& name, const xtd::drawing::size& size) method.
     xtd::drawing::image operator""_image(const wchar* s, xtd::size n);
     /// @}
   }
