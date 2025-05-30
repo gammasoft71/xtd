06b0fa10248598c3319ceeb5247e249e44e7dfb3
Gammasoft <gammasoft71@gmail.com>
2025-05-30 12:24:26 +0200

    Add icons

---
+++ examples/xtd.forms.examples/forms/circular_form/properties/resources.cmake
@@ -0,0 +1,9 @@
+# Resources file
+# ==============
+
+# Remarks
+# This file generates the "properties/resources.hpp" file, which is used to access the resources.
+
+# Icons
+resource(ball_ico "resources/ball.ico")
+resource(ball_png "resources/ball.png")
