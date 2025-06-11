c8495841a1b9cb86af2daf78c69b9ddc91fa818e
Gammasoft <gammasoft71@gmail.com>
2025-06-11 23:27:52 +0200

    Create availability_versions.h

---
+++ src/xtd.core/include/xtd/availability_versions.h
@@ -0,0 +1,10 @@
+/// @file
+/// @brief Contains availability versions.
+/// @copyright Copyright (c) 2025 Gammasoft. All rights reserved.
+#pragma once
+#if defined(_MSC_VER) && __cplusplus < 202302L
+#  pragma message("Deprecated : Replaced by #include <xtd/availability_versions.hpp> - Will be removed in version 0.4.0.")
+#else
+#  warning "Deprecated : Replaced by #include <xtd/availability_versions.hpp> - Will be removed in version 0.4.0."
+#endif
+#include "availability_versions.hpp"
