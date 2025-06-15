f936fabfe6852fcc2c9062639df07407f5c4485f
Gammasoft <gammasoft71@gmail.com>
2025-06-15 19:27:41 +0200

    Add opacity setting

--- examples/xtd.forms.examples/user_forms/ball/src/settings_form.cpp
+++ examples/xtd.forms.examples/user_forms/ball/src/settings_form.cpp
@@ -14,10 +14,10 @@ settings_form::settings_form() {
   minimize_box(false);
   form_border_style(forms::form_border_style::fixed_3d);
   if (environment::os_version().is_macos()) back_color(application::style_sheet().system_colors().app_workspace());
-  client_size({500, 200});
+  client_size({500, 250});
   start_position(form_start_position::manual);
   location(properties::settings::default_settings().settings_form_location());
-  controls().push_back_range({color_label_, light_point_color_label_, size_label_, default_settings_label_, color_color_picker_, light_point_color_color_picker_, size_numeric_up_down_, reset_button_});
+  controls().push_back_range({color_label_, light_point_color_label_, size_label_, default_settings_label_, color_color_picker_, light_point_color_color_picker_, opcacity_label_, opacity_numeric_up_down_, size_numeric_up_down_, reset_button_});
   
   auto middle = width() / 2;
   color_label_.text("Color");
@@ -30,15 +30,20 @@ settings_form::settings_form() {
   light_point_color_label_.size({200, 25});
   light_point_color_label_.location({middle - light_point_color_label_.width() - 10, 60});
   
+  opcacity_label_.text("Opacity");
+  opcacity_label_.text_align(xtd::forms::content_alignment::middle_right);
+  opcacity_label_.size({200, 25});
+  opcacity_label_.location({middle - opcacity_label_.width() - 10, 90});
+  
   size_label_.text("Size");
   size_label_.text_align(xtd::forms::content_alignment::middle_right);
   size_label_.size({200, 25});
-  size_label_.location({middle - size_label_.width() - 10, 90});
-  
+  size_label_.location({middle - size_label_.width() - 10, 120});
+
   default_settings_label_.text("Default settings");
   default_settings_label_.text_align(xtd::forms::content_alignment::middle_right);
   default_settings_label_.size({200, 25});
-  default_settings_label_.location({middle - default_settings_label_.width() - 10, 150});
+  default_settings_label_.location({middle - default_settings_label_.width() - 10, 180});
 
   color_color_picker_.location({middle + 10, 30});
   color_color_picker_.color(properties::settings::default_settings().color());
@@ -57,7 +62,16 @@ settings_form::settings_form() {
     on_settings_changed(event_args::empty);
   };
 
-  size_numeric_up_down_.location({middle + 10, 90});
+  opacity_numeric_up_down_.location({middle + 10, 90});
+  opacity_numeric_up_down_.set_range(1, 100);
+  opacity_numeric_up_down_.value(properties::settings::default_settings().opacity() * 100);
+  opacity_numeric_up_down_.value_changed += [&] {
+    if (properties::settings::default_settings().opacity() == opacity_numeric_up_down_.value() / 100) return;
+    properties::settings::default_settings().opacity(opacity_numeric_up_down_.value() / 100);
+    on_settings_changed(event_args::empty);
+  };
+
+  size_numeric_up_down_.location({middle + 10, 120});
   size_numeric_up_down_.set_range(10, 600);
   size_numeric_up_down_.value(properties::settings::default_settings().size().width);
   size_numeric_up_down_.value_changed += [&] {
@@ -65,14 +79,15 @@ settings_form::settings_form() {
     properties::settings::default_settings().size({as<int>(size_numeric_up_down_.value()), as<int>(size_numeric_up_down_.value())});
     on_settings_changed(event_args::empty);
   };
-  
-  reset_button_.location({middle + 10, 150});
+
+  reset_button_.location({middle + 10, 180});
   reset_button_.text("Reset");
   reset_button_.click += [&] {
     properties::settings::default_settings().reset();
     auto default_settings = properties::settings::default_settings();
     color_color_picker_.color(default_settings.color());
     light_point_color_color_picker_.color(default_settings.light_point_color());
+    opacity_numeric_up_down_.value(default_settings.opacity() * 100);
     size_numeric_up_down_.value(default_settings.size().width);
     settings(default_settings);
   };
