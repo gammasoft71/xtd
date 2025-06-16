f74aa46287fa82b8a74d5907cf2f7c8fcd51c710
Gammasoft <gammasoft71@gmail.com>
2025-06-16 20:48:58 +0200

    Update check_cpp_version.hpp

--- src/xtd.core/include/xtd/check_cpp_version.hpp
+++ src/xtd.core/include/xtd/check_cpp_version.hpp
@@ -3,13 +3,13 @@
 #pragma once
 
 /// @cond
-#if defined(_MSC_VER) && _MSVC_LANG < 202002L
+#if defined(_MSC_VER) && _MSVC_LANG < 202002l
 #  error "xtd requires C++20 or later. Use /std:c++20 with MSVC."
-#elif defined(__clang__) && __cplusplus < 202002L
+#elif defined(__clang__) && __cplusplus < 202002l
 #  error "xtd requires C++20 or later. Use -std=c++20 with Clang."
-#elif defined(__GNUC__) && __cplusplus < 202002L
+#elif defined(__GNUC__) && __cplusplus < 202002l
 #  error "xtd requires C++20 or later. Use -std=c++20 with GCC."
-#elif __cplusplus < 202002L
+#elif __cplusplus < 202002l
 #  error "xtd requires C++20 or later. Use set std to c++20 with your compiler option."
 #endif
 /// @endcond
