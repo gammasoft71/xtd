6276cf1e3127f47e53321087d5e8b9f0a636fde4
Gammasoft <gammasoft71@gmail.com>
2025-06-22 12:07:48 +0200

    Add array_list3 example

---
+++ examples/xtd.core.examples/collections/array_list3/README.md
@@ -0,0 +1,32 @@
+# array_list3
+
+Shows how to use [xtd::collections::array_list](https://gammasoft71.github.io/xtd/reference_guides/latest/group__collections.html#gaf18249e78122f5f0f0f09003cf6abc5c) alias.
+
+## Sources
+
+[src/array_list3.cpp](src/array_list3.cpp)
+
+[CMakeLists.txt](CMakeLists.txt)
+
+## Build and run
+
+Open "Command Prompt" or "Terminal". Navigate to the folder that contains the project and type the following:
+
+```cmake
+xtdc run
+```
+
+## Output
+
+```
+items = [true, 42, This is a string, win32s, Another string, 4.2, 02:25:43]
+items = [true, 42, This is a string, win32s, Another string, 4.2, 02:25:43]
+
+true
+42
+"THIS IS A STRING"
+platform_id::win32s
+"ANOTHER STRING"
+4.2
+02:25:43
+```
---
+++ examples/xtd.core.examples/collections/array_list3/src/array_list3.cpp
@@ -0,0 +1,34 @@
+#include <xtd/collections/array_list>
+#include <xtd/console>
+#include <xtd/platform_id>
+#include <xtd/time_span>
+
+using namespace xtd;
+using namespace xtd::collections;
+
+auto main() -> int {
+  auto items = array_list {true, 42, "This is a string", platform_id::win32s, u8"Another string", 4.2f, 2_h + 25_min + 43_s};
+  
+  console::write_line("items = [{}]", string::join(", ", items));
+  console::write_line("items = {}", items);
+  console::write_line();
+  
+  for (auto item : items) {
+    if (is<string>(item)) console::write_line("{}", as<string>(item).to_upper().quoted());
+    else if (is<platform_id>(item)) console::write_line("{}::{}", typeof_(as<platform_id>(item)).name(), item);
+    else console::write_line("{}", item);
+  }
+}
+
+// This code produces the following output :
+//
+// items = [true, 42, This is a string, win32s, Another string, 4.2, 02:25:43]
+// items = [true, 42, This is a string, win32s, Another string, 4.2, 02:25:43]
+//
+// true
+// 42
+// "THIS IS A STRING"
+// platform_id::win32s
+// "ANOTHER STRING"
+// 4.2
+// 02:25:43
