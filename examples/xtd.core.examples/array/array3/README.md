75c4a0fd76fe68362790ee0e8405e126729baff4
Gammasoft <gammasoft71@gmail.com>
2025-06-24 15:17:36 +0200

    Add array examples

---
+++ examples/xtd.core.examples/array/array3/README.md
@@ -0,0 +1,34 @@
+# array3
+
+Shows how to use [xtd::array](https://gammasoft71.github.io/xtd/reference_guides/latest/classxtd_1_1array.html) class.
+
+## Sources
+
+[src/array3.cpp](src/array3.cpp)
+
+[CMakeLists.txt](CMakeLists.txt)
+
+## Build and run
+
+Open "Command Prompt" or "Terminal". Navigate to the folder that contains the project and type the following:
+
+```cmake
+xtdc run
+```
+
+## Output
+
+```
+The array has 3 dimension(s) and a total of 24 elements.
+        length  lower   upper
+0:      2       0       1
+1:      3       0       2
+2:      4       0       3
+The array contains the following values:
+        0       1       2       3
+        10      11      12      13
+        20      21      22      23
+        100     101     102     103
+        110     111     112     113
+        120     121     122     123
+```
---
+++ examples/xtd.core.examples/array/array_as_read_only/src/array_as_read_only.cpp
@@ -0,0 +1,83 @@
+#include <xtd/array>
+#include <xtd/as>
+#include <xtd/console>
+#include <xtd/not_supported_exception>
+
+using namespace xtd;
+using namespace xtd::collections::generic;
+
+auto print_index_and_values(const array<string>& my_arr)  {
+  for (auto i = 0_z; i < my_arr.length(); ++i)
+    console::write_line("   [{0}] : {1}", i, my_arr[i]);
+  console::write_line();
+}
+
+auto print_index_and_values(const ilist<string>& my_list)  {
+  for (auto i = 0_z; i < my_list.count(); ++i)
+    console::write_line( "   [{0}] : {1}", i, my_list[i]);
+  console::write_line();
+}
+
+auto main() -> int {
+  // Create and initialize a new string array.
+  auto my_arr = array<string> {"The", "quick", "brown", "fox"};
+  
+  // Display the values of the array.
+  console::write_line("The string array initially contains the following values:");
+  print_index_and_values(my_arr);
+  
+  // Create a read-only ilist wrapper around the array.
+  auto my_list = array<>::as_read_only(my_arr);
+  
+  // Display the values of the read-only ilist.
+  console::write_line("The read-only ilist contains the following values:");
+  print_index_and_values(my_list);
+  
+  // Attempt to change a value through the wrapper.
+  try {
+    my_list[3] = "CAT";
+  } catch (const not_supported_exception& e) {
+    console::write_line("{} - {}", e.get_type(), e.message());
+    console::write_line();
+  }
+  
+  // Change a value in the original array.
+  my_arr[2] = "RED";
+  
+  // Display the values of the array.
+  console::write_line("After changing the third element, the string array contains the following values:");
+  print_index_and_values( my_arr );
+  
+  // Display the values of the read-only ilist.
+  console::write_line("After changing the third element, the read-only ilist contains the following values:");
+  print_index_and_values(my_list);
+}
+
+// This code produces the following output :
+//
+// The string array initially contains the following values:
+//    [0] : The
+//    [1] : quick
+//    [2] : brown
+//    [3] : fox
+//
+// The read-only ilist contains the following values:
+//    [0] : The
+//    [1] : quick
+//    [2] : brown
+//    [3] : fox
+//
+// xtd::not_supported_exception - Collection is read-only.
+//
+// After changing the third element, the string array contains the following values:
+//    [0] : The
+//    [1] : quick
+//    [2] : RED
+//    [3] : fox
+//
+// After changing the third element, the read-only ilist contains the following values:
+//    [0] : The
+//    [1] : quick
+//    [2] : RED
+//    [3] : fox
+//
