09d3dc6d7a908409674f6b8a3355e977955f6706
Gammasoft <gammasoft71@gmail.com>
2025-06-24 15:02:25 +0200

    Update array2.cpp

--- examples/xtd.core.examples/array/array2/src/array2.cpp
+++ examples/xtd.core.examples/array/array2/src/array2.cpp
@@ -1,58 +1,65 @@
+#include <xtd/any_object>
 #include <xtd/array>
 #include <xtd/console>
 
 using namespace xtd;
 
-template<size rank>
-auto print_values(const array<int, rank>& my_arr) {
-  auto my_enumerator = my_arr.get_enumerator();
-  auto i = 0_z;
-  auto cols = my_arr.get_length(my_arr.rank() - 1);
-  while (my_enumerator.move_next()) {
-    if (i < cols) ++i;
-    else {
-      console::write_line();
-      i = 1;
-    }
-    console::write("  {,8}", my_enumerator.current());
-  }
+auto print_values(const array<int>& my_arr) {
+  for (auto i : my_arr)
+    console::write_line("\t{}", i);
+  console::write_line();
+}
+
+auto print_values(const array<any_object>& my_arr) {
+  for (auto o : my_arr)
+    console::write_line("\t{}", o);
   console::write_line();
 }
 
 auto main() -> int {
-  // Creates and initializes a new three-dimensional array of type int.
-  auto my_arr = array<int, 3>(2, 3, 4);
-  for (auto i = my_arr.get_lower_bound(0); i <= my_arr.get_upper_bound(0); ++i) {
-    for (auto j = my_arr.get_lower_bound(1); j <= my_arr.get_upper_bound(1); ++j) {
-      for (auto k = my_arr.get_lower_bound(2); k <= my_arr.get_upper_bound(2); ++k)
-        my_arr.set_value(as<int>((i * 100) + (j * 10) + k), {i, j, k});
-    }
-  }
+  // Creates and initializes a new integer array and a new Object array.
+  auto my_int_array = array<int> {1, 2, 3, 4, 5};
+  auto my_obj_array = array<any_object> {26, 27, 28, 29, 30};
   
-  // Displays the properties of the array.
-  console::write_line("The array has {} dimension(s) and a total of {} elements.", my_arr.rank(), my_arr.length());
-  console::write_line("    length     lower     upper");
-  for (auto i = 0_z; i < my_arr.rank(); ++i) {
-    console::write("{}:{,8}", i, my_arr.get_length(i));
-    console::write_line("  {,8}  {,8}", my_arr.get_lower_bound(i), my_arr.get_upper_bound(i));
-  }
+  // Prints the initial values of both arrays.
+  console::write_line("Initially,");
+  console::write("integer array:");
+  print_values(my_int_array);
+  console::write("Object array: ");
+  print_values(my_obj_array);
   
-  // Displays the contents of the Array.
-  console::write_line("The array contains the following values:");
-  print_values(my_arr);
+  // Copies the first two elements from the integer array to the Object array.
+  array<>::copy(my_int_array, my_obj_array, 2);
+  
+  // Prints the values of the modified arrays.
+  console::write_line("\nAfter copying the first two elements of the integer array to the Object array,");
+  console::write("integer array:");
+  print_values(my_int_array);
+  console::write("Object array: ");
+  print_values(my_obj_array);
+  
+  // Copies the last two elements from the Object array to the integer array.
+  System.Array.Copy(my_obj_array, my_obj_array.GetUpperBound(0) - 1, my_int_array, my_int_array.GetUpperBound(0) - 1, 2);
+  
+  // Prints the values of the modified arrays.
+  console::write_line("\nAfter copying the last two elements of the Object array to the integer array,");
+  console::write("integer array:");
+  print_values(my_int_array);
+  console::write("Object array: ");
+  print_values(my_obj_array);
 }
 
 // This code produces the following output :
 //
 // The array has 3 dimension(s) and a total of 24 elements.
-//     length     lower     upper
-// 0:       2         0         1
-// 1:       3         0         2
-// 2:       4         0         3
+//         length  lower   upper
+// 0:      2       0       1
+// 1:      3       0       2
+// 2:      4       0       3
 // The array contains the following values:
-//          0         1         2         3
-//         10        11        12        13
-//         20        21        22        23
-//        100       101       102       103
-//        110       111       112       113
-//        120       121       122       123
+//         0       1       2       3
+//         10      11      12      13
+//         20      21      22      23
+//         100     101     102     103
+//         110     111     112     113
+//         120     121     122     123
