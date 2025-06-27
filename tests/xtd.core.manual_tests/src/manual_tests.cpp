bf271b13d94c8e10b8ec9865980e6e4696d25017
Gammasoft <gammasoft71@gmail.com>
2025-06-27 11:39:08 +0200

    Update manual_tests.cpp

--- tests/xtd.core.manual_tests/src/manual_tests.cpp
+++ tests/xtd.core.manual_tests/src/manual_tests.cpp
@@ -1,12 +1,7 @@
 #include <xtd/xtd>
 
 auto main() -> int {
-  auto sl = diagnostics::source_location::current();
-  console::write_line("source_location :");
-  console::write_line("  file_name     = {}", sl.file_name());
-  console::write_line("  function_name = {}", sl.function_name());
-  console::write_line("  line          = {}", sl.line());
-  console::write_line("  column        = {}", sl.column());
+  console::write_line("Hello, World!");
 }
 
 // This code can produces the following output:
