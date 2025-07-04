d74de7578da46a99b2595bc957b525ae543b4e15
gammasoft71 <gammasoft71@gmail.com>
2025-07-04 21:00:13 +0200

    update latest reference guide

---
+++ static/reference_guides/latest/html/search/groups_10.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['security_0',['security',['../group__security.html',1,'']]],
+  ['specialized_1',['specialized',['../group__specialized__collections.html',1,'']]],
+  ['strings_2',['strings',['../group__strings.html',1,'']]],
+  ['style_20sheets_3',['style sheets',['../group__style__sheets.html',1,'']]],
+  ['synthesis_4',['synthesis',['../group__synthesis.html',1,'']]],
+  ['system_5',['system',['../group__system.html',1,'']]],
+  ['system_20information_6',['system information',['../group__system__information.html',1,'']]]
+];
