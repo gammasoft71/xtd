0b3b9c13d493b8fb47c85687de0aab8d8687ff89
Gammasoft <gammasoft71@gmail.com>
2025-07-06 12:24:14 +0200

    Fix build on windows

--- src/xtd.core/include/xtd/diagnostics/process.hpp
+++ src/xtd.core/include/xtd/diagnostics/process.hpp
@@ -6,6 +6,7 @@
 #include "../date_time.hpp"
 #include "../event.hpp"
 #include "../event_handler.hpp"
+#include "../icomparable.hpp"
 #include "../object.hpp"
 #include "../string.hpp"
 #include "../optional.hpp"
@@ -46,7 +47,7 @@ namespace xtd {
     /// @remarks The xtd::diagnostics::process component obtains information about a group of properties all at once. After the xtd::diagnostics::process component has obtained information about one member of any group, it will cache the values for the other properties in that group and not obtain new information about the other members of the group until you call the xtd::diagnostics::process::refresh method. Therefore, a property value is not guaranteed to be any newer than the last call to the xtd::diagnostics::process::refresh method. The group breakdowns are operating-system dependent.
     /// @remarks If you have a path variable declared in your system using quotes, you must fully qualify that path when starting any process found in that location. Otherwise, the system will not find the path. For example, if c:\mypath is not in your path, and you add it using quotation marks: path = %path%;"c:\mypath", you must fully qualify any process in c:\mypath when starting it.
     /// @remarks A system process is uniquely identified on the system by its process identifier. Like many Windows resources, a process is also identified by its handle, which might not be unique on the computer. A handle is the generic term for an identifier of a resource. The operating system persists the process handle, which is accessed through the Handle property of the Process component, even when the process has exited. Thus, you can get the process's administrative information, such as the xtd::diagnostics::process::exit_code (usually either zero for success or a nonzero error code) and the xtd::diagnostics::process::exit_time. xtd::diagnostics::process::handles are an extremely valuable resource, so leaking handles is more virulent than leaking memory.
-    class core_export_ process final : public xtd::object {
+    class core_export_ process final : public xtd::object, public xtd::icomparable<process> {
       struct data;
       
     public:
@@ -834,6 +835,10 @@ namespace xtd {
       output_data_received_event output_data_received;
       /// @}
       
+      /// @cond
+      int32 compare_to(const process& o) const noexcept override {return this < &o ? -1 : this > &o ? 1 : 0;}
+      /// @endcond
+  
     protected:
       /// @name Protected Methods
       
