a61ff477cda0e2c8ca2216dc7f9485e10b8d9fe2
Gammasoft <gammasoft71@gmail.com>
2025-07-10 23:29:42 +0200

    Update enumerable_where2.cpp

--- examples/xtd.core.examples/linq/enumerable_where2/src/enumerable_where2.cpp
+++ examples/xtd.core.examples/linq/enumerable_where2/src/enumerable_where2.cpp
@@ -1,13 +1,12 @@
 #include <xtd/xtd>
 
 using namespace xtd;
-using namespace xtd::collections::generic;
 
 auto main() -> int {
-  array<int> numbers = {0, 30, 20, 15, 90, 85, 40, 75};
+  auto numbers = array {0, 30, 20, 15, 90, 85, 40, 75};
   
-  const ienumerable<int>& query = numbers.where([](int number, size_t index) {
-    return number <= int(index * 10);
+  auto query = numbers.where([](int number, size index) {
+    return number <= as<int>(index * 10);
   });
   
   for (const auto& number : query)
