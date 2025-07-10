a9f7ce6cc2d3b7578998f2fde35406b275eb0d84
Gammasoft <gammasoft71@gmail.com>
2025-07-11 00:25:13 +0200

    Update array.cpp

--- examples/xtd.core.examples/array/array/src/array.cpp
+++ examples/xtd.core.examples/array/array/src/array.cpp
@@ -1,26 +1,23 @@
-#include <xtd/array>
-#include <xtd/console>
-
-using namespace xtd;
+#include <xtd/xtd>
 
 auto main() -> int {
   // make a single dimension array
-  array<int> my_array1(5);
+  auto my_array1 = array<int>(5);
   
   // make a 3 dimensional array
-  array<int, 3> my_array2(5, 3, 2);
+  auto my_array2 = array<int, 3>(5, 3, 2);
   
   // make an array from native array
   int my_native_array[] = {10, 20, 30, 40, 50, 60, 70};
-  array<int> my_array3(my_native_array);
+  auto my_array3 = array<int>(my_native_array);
   
   // make an array with initialiser list
-  array my_array4 = {100, 200, 300, 400, 500, 600, 700};
+  auto my_array4 = array {100, 200, 300, 400, 500, 600, 700};
   
-  int index = 0;
+  auto index = 0;
   // show the array values
   console::write_line("The array contains the following values:");
-  for (int value : my_array3)
+  for (auto value : my_array3)
     console::write_line("[{0}] {1}", index++,  value);
 }
 
