385d8a1ba99f25b07f452541a6a2a222f36ef1fd
Gammasoft <gammasoft71@gmail.com>
2025-07-10 23:29:39 +0200

    Update enumerable_where.cpp

--- examples/xtd.core.examples/linq/enumerable_where/src/enumerable_where.cpp
+++ examples/xtd.core.examples/linq/enumerable_where/src/enumerable_where.cpp
@@ -1,12 +1,11 @@
 #include <xtd/xtd>
 
 using namespace xtd;
-using namespace xtd::collections::generic;
 
 auto main() -> int {
-  array<string> fruits = {"apple", "passionfruit", "banana", "mango", "orange", "blueberry", "grape", "strawberry"};
+  auto fruits = array {"apple", "passionfruit", "banana", "mango", "orange", "blueberry", "grape", "strawberry"};
   
-  const ienumerable<string>& query = fruits.where([](const string& fruit) {return fruit.length() < 6;});
+  auto query = fruits.where([](const string& fruit) {return fruit.length() < 6;});
   
   for (const auto& fruit : query)
     console::write_line(fruit);
