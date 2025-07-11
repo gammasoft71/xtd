8baf7fc6bd782d3c31be1a69b7a394c88247d718
Gammasoft <gammasoft71@gmail.com>
2025-07-11 20:11:23 +0200

    code simplification

--- examples/xtd.forms.examples/controls/progress_bar/src/progress_bar.cpp
+++ examples/xtd.forms.examples/controls/progress_bar/src/progress_bar.cpp
@@ -1,10 +1,4 @@
-#include <xtd/forms/application>
-#include <xtd/forms/form>
-#include <xtd/forms/progress_bar>
-#include <xtd/forms/timer>
-
-using namespace xtd;
-using namespace xtd::forms;
+#include <xtd/xtd>
 
 namespace progress_bar_example {
   class form1 : public form {
@@ -12,28 +6,24 @@ namespace progress_bar_example {
     form1() {
       text("Progress bar example");
       client_size({300, 300});
+      controls().push_back_range({progress_bar1, progress_bar2, progress_bar3, progress_bar4, progress_bar5});
       
-      progress_bar1.parent(*this);
       progress_bar1.location({50, 50});
       progress_bar1.width(200);
       
-      progress_bar2.parent(*this);
       progress_bar2.location({50, 80});
       progress_bar2.value(50);
       progress_bar2.width(200);
       
-      progress_bar3.parent(*this);
       progress_bar3.location({50, 110});
       progress_bar3.maximum(300);
       progress_bar3.increment(300);
       progress_bar3.width(200);
       
-      progress_bar4.parent(*this);
       progress_bar4.location({50, 140});
       progress_bar4.step(1);
       progress_bar4.width(200);
       
-      progress_bar5.parent(*this);
       progress_bar5.location({50, 170});
       progress_bar5.maximum(200);
       progress_bar5.minimum(100);
@@ -48,7 +38,7 @@ namespace progress_bar_example {
     }
     
   private:
-    timer timer1;
+    forms::timer timer1;
     progress_bar progress_bar1;
     progress_bar progress_bar2;
     progress_bar progress_bar3;
