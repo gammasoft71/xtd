e52d2f3db89bc49d6233aef1f1fda1fadbad8b30
Gammasoft <gammasoft71@gmail.com>
2025-07-16 16:56:10 +0200

    Update hello_world_paint.cpp

--- examples/xtd.forms.examples/hello_worlds/hello_world_paint/src/hello_world_paint.cpp
+++ examples/xtd.forms.examples/hello_worlds/hello_world_paint/src/hello_world_paint.cpp
@@ -1,9 +1,5 @@
 #include <xtd/xtd>
 
-using namespace xtd;
-using namespace xtd::drawing;
-using namespace xtd::forms;
-
 namespace hello_world_paint_example {
   class main_form : public form {
   public:
