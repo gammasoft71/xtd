867259f6d4b570e2eb1a6c5506fb6d109da082b8
Gammasoft <gammasoft71@gmail.com>
2025-07-27 03:23:45 +0200

    Update background_worker.cpp

--- examples/xtd.forms.examples/components/background_worker/src/background_worker.cpp
+++ examples/xtd.forms.examples/components/background_worker/src/background_worker.cpp
@@ -1,17 +1,4 @@
-#include <xtd/forms/application>
-#include <xtd/forms/background_worker>
-#include <xtd/forms/button>
-#include <xtd/forms/form>
-#include <xtd/forms/label>
-#include <xtd/forms/panel>
-#include <xtd/forms/progress_bar>
-#include <xtd/forms/text_box>
-#include <xtd/threading/thread>
-#include <xtd/startup>
-
-using namespace xtd;
-using namespace xtd::forms;
-using namespace xtd::threading;
+#include <xtd/xtd>
 
 namespace background_worker_example {
   class form1 : public form {
