67e3cde2e36880bbcd01118fb249f02c3ed4f06f
Gammasoft <gammasoft71@gmail.com>
2025-08-03 05:46:04 +0200

    Update input_box.cpp

--- examples/xtd.forms.examples/dialogs/input_box/src/input_box.cpp
+++ examples/xtd.forms.examples/dialogs/input_box/src/input_box.cpp
@@ -1,10 +1,4 @@
-#include <xtd/forms/application>
-#include <xtd/forms/button>
-#include <xtd/forms/form>
-#include <xtd/forms/input_box>
-#include <xtd/forms/label>
-
-using namespace xtd::forms;
+#include <xtd/xtd>
 
 namespace input_box_example {
   class form1 : public form {
