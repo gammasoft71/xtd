356867e2103d2716ad4b3ff2ca0a353d80ad3d86
Gammasoft <gammasoft71@gmail.com>
2025-08-03 05:45:57 +0200

    Update font_box.cpp

--- examples/xtd.forms.examples/dialogs/font_box/src/font_box.cpp
+++ examples/xtd.forms.examples/dialogs/font_box/src/font_box.cpp
@@ -1,13 +1,4 @@
-#include <xtd/forms/application>
-#include <xtd/forms/button>
-#include <xtd/forms/font_box>
-#include <xtd/forms/form>
-#include <xtd/forms/message_box>
-#include <xtd/forms/label>
-
-using namespace xtd;
-using namespace xtd::drawing;
-using namespace xtd::forms;
+#include <xtd/xtd>
 
 class form1 : public form {
 public:
