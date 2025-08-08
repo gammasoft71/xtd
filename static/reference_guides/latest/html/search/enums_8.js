64e3e2545e4b83f7e08560e6ed76a942e7257357
gammasoft71 <gammasoft71@gmail.com>
2025-08-08 21:00:13 +0200

    update latest reference guide

---
+++ static/reference_guides/latest/html/search/enums_8.js
@@ -0,0 +1,11 @@
+var searchData=
+[
+  ['image_5fflags_0',['image_flags',['../group__xtd__drawing.html#ga51f9e2e6360dcb4f44307de5a884f968',1,'xtd::drawing::imaging']]],
+  ['image_5flayout_1',['image_layout',['../group__xtd__forms.html#ga4c8a7d9a6bcf0a2485c3d68bb4d51524',1,'xtd::forms']]],
+  ['image_5flock_5fmode_2',['image_lock_mode',['../group__xtd__drawing.html#gaab4cc0f15617caca1bed28c92505fd3b',1,'xtd::drawing::imaging']]],
+  ['image_5ftype_3',['image_type',['../group__style__sheets.html#ga8adb60dd0863dfed5c946132a4d5a0e4',1,'xtd::forms::style_sheets']]],
+  ['input_5fbox_5fstyle_4',['input_box_style',['../group__xtd__forms.html#ga2a5cbd2f66509fef864644b81cb0b39e',1,'xtd::forms']]],
+  ['interpolation_5fmode_5',['interpolation_mode',['../group__xtd__drawing.html#ga597cca8c636b502a1dbf11b65c47b017',1,'xtd::drawing::drawing_2d']]],
+  ['io_5fcontrol_5fcode_6',['io_control_code',['../group__xtd__core.html#gafcb65e427e8409a46ea4ddaee074a5b2',1,'xtd::net::sockets']]],
+  ['ip_5fprotection_5flevel_7',['ip_protection_level',['../group__xtd__core.html#gab2b5fea204d6ae76bca9bb203bc20397',1,'xtd::net::sockets']]]
+];
