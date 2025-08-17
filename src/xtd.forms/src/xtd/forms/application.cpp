0b0694519f42cfbb82375d4167e17d44c02ef43c
Gammasoft <gammasoft71@gmail.com>
2025-08-17 23:11:08 +0200

    Fix on_thread_exception

--- src/xtd.forms/src/xtd/forms/application.cpp
+++ src/xtd.forms/src/xtd/forms/application.cpp
@@ -475,7 +475,7 @@ bool application::on_app_thread_exception() {
 bool application::on_thread_exception(const threading::thread_exception_event_args& e) {
   if (thread_exception.is_empty()) return (main_form().has_value() ? exception_box::show(main_form().value().get(), e.exception(), product_name()) : exception_box::show(e.exception(), product_name())) == dialog_result::ok;
   thread_exception(e);
-  return false;
+  return true;
 }
 
 void application::raise_enter_thread_modal(const event_args& e) {
