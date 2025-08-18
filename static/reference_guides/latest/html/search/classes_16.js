5ebce8ebd1076fea3cb502243be328a6a763fa75
gammasoft71 <gammasoft71@gmail.com>
2025-08-18 21:00:32 +0200

    update latest reference guide

---
+++ static/reference_guides/latest/html/search/classes_16.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['xml_5fsyntax_5fexception_0',['xml_syntax_exception',['../classxtd_1_1xml__syntax__exception.html',1,'xtd']]],
+  ['xtd_5fabout_5fbox_1',['xtd_about_box',['../classxtd_1_1forms_1_1xtd__about__box.html',1,'xtd::forms']]],
+  ['xtd_5fabout_5fdialog_2',['xtd_about_dialog',['../classxtd_1_1forms_1_1xtd__about__dialog.html',1,'xtd::forms']]],
+  ['xtd_5flibrary_3',['xtd_library',['../classxtd_1_1environment_1_1xtd__library.html',1,'xtd::environment']]],
+  ['xtd_5fsystem_5fimages_4',['xtd_system_images',['../classxtd_1_1drawing_1_1xtd__system__images.html',1,'xtd::drawing']]]
+];
