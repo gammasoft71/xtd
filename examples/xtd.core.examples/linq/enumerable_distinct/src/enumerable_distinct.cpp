dc0809773bd216a04f4159c23dd0625d4449c620
Gammasoft <gammasoft71@gmail.com>
2025-08-28 17:07:19 +0200

    Update enumerable_distinct example

--- examples/xtd.core.examples/linq/enumerable_distinct/src/enumerable_distinct.cpp
+++ examples/xtd.core.examples/linq/enumerable_distinct/src/enumerable_distinct.cpp
@@ -1,10 +1,10 @@
 #include <xtd/xtd>
 
 auto main() -> int {
-  auto numbers = array {1, 1, 2, 3, 3, 3, 3, 4, 5, 6, 6, 2, 7, 8, 9, 10, 8, 9};
+  auto numbers = array {2, 7, 3, 1, 1, 4, 10, 5, 3, 3, 3, 6, 6, 2, 8, 9, 8, 9};
   console::write_line("numbers = {}", numbers.distinct());
 }
 
 // This code produces the following output :
 //
-// numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// numbers = [2, 7, 3, 1, 4, 10, 5, 6, 8, 9]
