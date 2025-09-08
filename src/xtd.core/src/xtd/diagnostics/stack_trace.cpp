e1d6abcf6eaa9c25892acddf160db6580ed4a4bb
Gammasoft <gammasoft71@gmail.com>
2025-09-08 12:41:42 +0200

    Fix need_file_info with stack_frame::current

--- src/xtd.core/src/xtd/diagnostics/stack_trace.cpp
+++ src/xtd.core/src/xtd/diagnostics/stack_trace.cpp
@@ -9,6 +9,7 @@ using namespace xtd::diagnostics;
 
 struct stack_trace::data {
   stack_frame_collection frames;
+  bool need_file_info = false;
 };
 
 stack_trace::stack_trace() : data_{new_ptr<data>()} {
@@ -17,6 +18,7 @@ stack_trace::stack_trace() : data_{new_ptr<data>()} {
 
 stack_trace::stack_trace(bool need_file_info) : data_{new_ptr<data>()} {
   data_->frames = stack_frame::get_stack_frames(string::empty_string, METHODS_TO_SKIP + 1, need_file_info);
+  data_->need_file_info = need_file_info;
 }
 
 stack_trace::stack_trace(const stack_frame& frame) : data_{new_ptr<data>()} {
@@ -25,6 +27,7 @@ stack_trace::stack_trace(const stack_frame& frame) : data_{new_ptr<data>()} {
 
 stack_trace::stack_trace(const string& str, size_t skip_frames, bool need_file_info) : data_{new_ptr<data>()} {
   data_->frames = stack_frame::get_stack_frames(str, skip_frames + METHODS_TO_SKIP + 1, need_file_info);
+  data_->need_file_info = need_file_info;
 }
 
 stack_trace::stack_trace(size_t skip_frames) : data_{new_ptr<data>()} {
@@ -33,6 +36,7 @@ stack_trace::stack_trace(size_t skip_frames) : data_{new_ptr<data>()} {
 
 stack_trace::stack_trace(size_t skip_frames, bool need_file_info) : data_{new_ptr<data>()} {
   data_->frames = stack_frame::get_stack_frames(string::empty_string, skip_frames + METHODS_TO_SKIP + 1, need_file_info);
+  data_->need_file_info = need_file_info;
 }
 
 stack_trace::stack_trace(const std::exception& exception) : data_{new_ptr<data>()} {
@@ -41,6 +45,7 @@ stack_trace::stack_trace(const std::exception& exception) : data_{new_ptr<data>(
 
 stack_trace::stack_trace(const std::exception& exception, bool need_file_info) : data_{new_ptr<data>()} {
   data_->frames = stack_frame::get_stack_frames(typeof_(exception).full_name(), METHODS_TO_SKIP + 1, need_file_info);
+  data_->need_file_info = need_file_info;
 }
 
 stack_trace::stack_trace(const std::exception& exception, size_t skip_frames) : data_{new_ptr<data>()} {
@@ -49,6 +54,7 @@ stack_trace::stack_trace(const std::exception& exception, size_t skip_frames) : 
 
 stack_trace::stack_trace(const std::exception& exception, size_t skip_frames, bool need_file_info) {
   data_->frames = stack_frame::get_stack_frames(typeof_(exception).full_name(), skip_frames + METHODS_TO_SKIP + 1, need_file_info);
+  data_->need_file_info = need_file_info;
 }
 
 size_t stack_trace::frame_count() const noexcept {
@@ -75,7 +81,7 @@ string stack_trace::to_string(size_t skip_frames, const stack_frame& stack_frame
   for (auto index = skip_frames; index < data_->frames.size(); ++index) {
     if (index > skip_frames) str += xtd::environment::new_line();
     str += "   at " + data_->frames[index].get_method();
-    if (index == skip_frames && stack_frame != stack_frame::empty()) str += string::format(" in {}:line {}", stack_frame.get_file_name(), stack_frame.get_file_line_number());
+    if (index == skip_frames && data_->need_file_info && stack_frame != stack_frame::empty()) str += string::format(" in {}:line {}", stack_frame.get_file_name(), stack_frame.get_file_line_number());
     else if (!data_->frames[index].get_file_name().empty()) str += string::format(" in {}:line {}", data_->frames[index].get_file_name(), data_->frames[index].get_file_line_number());
   }
   str += environment::new_line();
