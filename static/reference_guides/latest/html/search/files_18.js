5ae08fa105584a9813c6a4b18f8190f93719bef7
gammasoft71 <gammasoft71@gmail.com>
2025-09-08 21:00:33 +0200

    update latest reference guide

---
+++ static/reference_guides/latest/html/search/files_18.js
@@ -0,0 +1,18 @@
+var searchData=
+[
+  ['xml_5fsyntax_5fexception_2ehpp_0',['xml_syntax_exception.hpp',['../xml__syntax__exception_8hpp.html',1,'']]],
+  ['xtd_2ecore_2ehpp_1',['xtd.core.hpp',['../xtd_8core_8hpp.html',1,'']]],
+  ['xtd_2edrawing_2ehpp_2',['xtd.drawing.hpp',['../xtd_8drawing_8hpp.html',1,'']]],
+  ['xtd_2eforms_2ehpp_3',['xtd.forms.hpp',['../xtd_8forms_8hpp.html',1,'']]],
+  ['xtd_2ehpp_4',['xtd.hpp',['../xtd_8hpp.html',1,'']]],
+  ['xtd_2etunit_2ehpp_5',['xtd.tunit.hpp',['../xtd_8tunit_8hpp.html',1,'']]],
+  ['xtd_5fabout_5fbox_2ehpp_6',['xtd_about_box.hpp',['../xtd__about__box_8hpp.html',1,'']]],
+  ['xtd_5fabout_5fdialog_2ehpp_7',['xtd_about_dialog.hpp',['../xtd__about__dialog_8hpp.html',1,'']]],
+  ['xtd_5fcmake_5fexamples_2edox_8',['xtd_cmake_examples.dox',['../xtd__cmake__examples_8dox.html',1,'']]],
+  ['xtd_5fcore_5fexamples_2edox_9',['xtd_core_examples.dox',['../xtd__core__examples_8dox.html',1,'']]],
+  ['xtd_5fdrawing_5fexamples_2edox_10',['xtd_drawing_examples.dox',['../xtd__drawing__examples_8dox.html',1,'']]],
+  ['xtd_5fforms_5fexamples_2edox_11',['xtd_forms_examples.dox',['../xtd__forms__examples_8dox.html',1,'']]],
+  ['xtd_5fnamespace_5faliases_2ehpp_12',['xtd_namespace_aliases.hpp',['../xtd__namespace__aliases_8hpp.html',1,'']]],
+  ['xtd_5fsystem_5fimages_2ehpp_13',['xtd_system_images.hpp',['../xtd__system__images_8hpp.html',1,'']]],
+  ['xtd_5ftunit_5fexamples_2edox_14',['xtd_tunit_examples.dox',['../xtd__tunit__examples_8dox.html',1,'']]]
+];
