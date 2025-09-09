0e412decabf97475fe9c3dc1b04ebabeac76b26f
Gammasoft <gammasoft71@gmail.com>
2025-09-09 11:37:07 +0200

    Add std::stacktrace

--- src/xtd.core.native.linux/src/xtd/native/linux/stack_trace.cpp
+++ src/xtd.core.native.linux/src/xtd/native/linux/stack_trace.cpp
@@ -1,3 +1,28 @@
+#if __cpp_lib_stacktrace >= 202011l
+
+#define __XTD_CORE_NATIVE_LIBRARY__
+#include <xtd/native/stack_trace>
+#undef __XTD_CORE_NATIVE_LIBRARY__
+#include <stacktrace>
+
+using namespace std;
+using namespace xtd::native;
+
+size_t stack_trace::get_native_offset() {
+  return 3;
+}
+
+stack_trace::frame_collection stack_trace::get_frames(size_t skip_frames, bool need_file_info) {
+  auto frames = frame_collection {};
+  for (auto& entry : std::stacktrace::current(skip_frames + get_native_offset())) {
+    frames.push_back({need_file_info ? entry.source_file() : "", need_file_info ? static_cast<size_t>(entry.source_line()) : size_t {}, size_t {}, entry.description(), size_t {}});
+    if (std::get<3>(frames.back()) == "main") break;
+  }
+  return frames;
+}
+
+#else
+
 #define __XTD_CORE_NATIVE_LIBRARY__
 #include <xtd/native/stack_trace>
 #include "../../../../include/xtd/native/linux/shell_execute.hpp"
@@ -112,9 +137,10 @@ stack_trace::frame_collection stack_trace::get_frames(size_t skip_frames, bool n
   
   auto frames = frame_collection {};
   for (const auto& frame : ::get_frames(get_commands(addresses), addresses.size(), need_file_info)) {
-    if (get<3>(frame) == "__libc_start_call_main") break;
     frames.push_back(frame);
+    if (get<3>(frame) == "main") break;
   }
   return frames;
 }
+#endif
 #endif
