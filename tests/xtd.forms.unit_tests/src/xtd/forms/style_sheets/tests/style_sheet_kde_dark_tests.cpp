426e418ee12a321367e75a4f2e2782f1bf4d40a9
Gammasoft <gammasoft71@gmail.com>
2025-09-10 19:29:35 +0200

    Fix unit tests

--- tests/xtd.forms.unit_tests/src/xtd/forms/style_sheets/tests/style_sheet_kde_dark_tests.cpp
+++ tests/xtd.forms.unit_tests/src/xtd/forms/style_sheets/tests/style_sheet_kde_dark_tests.cpp
@@ -17,7 +17,7 @@ namespace xtd::forms::style_sheets::tests {
       assert::are_equal("KDE (dark)", style.theme().name());
       assert::are_equal("Contains KDE (dark) theme.", style.theme().description());
       assert::are_equal("Gammasoft", style.theme().authors());
-      assert::are_equal("https://gammasoft71.github.io", style.theme().website().to_string());
+      assert::are_equal("https://gammasoft71.github.io/", style.theme().website().to_string());
     }
   };
 }
--- tests/xtd.forms.unit_tests/src/xtd/forms/style_sheets/tests/style_sheet_windows_light_tests.cpp
+++ tests/xtd.forms.unit_tests/src/xtd/forms/style_sheets/tests/style_sheet_windows_light_tests.cpp
@@ -17,7 +17,7 @@ namespace xtd::forms::style_sheets::tests {
       assert::are_equal("Windows (light)", style.theme().name());
       assert::are_equal("Contains Windows (light) theme.", style.theme().description());
       assert::are_equal("Gammasoft", style.theme().authors());
-      assert::are_equal("https://gammasoft71.github.io", style.theme().website().to_string());
+      assert::are_equal("https://gammasoft71.github.io/", style.theme().website().to_string());
     }
   };
 }
