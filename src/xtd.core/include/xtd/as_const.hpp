77687b118db8b677bf0a5cf283611859dfc420c1
Gammasoft <gammasoft71@gmail.com>
2025-09-19 23:02:30 +0200

    Add xtd::as_const on pointer

--- src/xtd.core/include/xtd/as_const.hpp
+++ src/xtd.core/include/xtd/as_const.hpp
@@ -13,6 +13,14 @@ namespace xtd {
   constexpr std::add_const_t<type_t>& as_const(type_t& type) noexcept {
     return type;
   }
+  /// @brief Obtains a reference to `const` to its argument
+  /// @param type The referecne of `type`.
+  /// @return The const reference of `type`.
+  /// @remarks Forms lvalue reference to const type of `type`.
+  template<class type_t>
+  constexpr std::add_const_t<type_t>* as_const(type_t* type) noexcept {
+    return type;
+  }
   /// @cond
   template<class type_t>
   void as_const(const type_t&&) = delete;
