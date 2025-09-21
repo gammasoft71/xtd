b851822954102a5e67c215b1034c63dac2418158
Gammasoft <gammasoft71@gmail.com>
2025-09-21 11:18:11 +0200

    Update ball_form.cpp

--- examples/xtd.forms.examples/user_forms/ball/src/ball_form.cpp
+++ examples/xtd.forms.examples/user_forms/ball/src/ball_form.cpp
@@ -48,11 +48,23 @@ control& ball_form::size(const drawing::size& value) {
 
 void ball_form::on_paint(paint_event_args& e) {
   form::on_paint(e);
-
-  // Radial gradient
+  
+  // Main gradient (sphere)
   auto left = e.clip_rectangle().width * 2 / 5;
-  auto top = e.clip_rectangle().height * 35 /100;
+  auto top = e.clip_rectangle().height * 35 / 100;
   auto radius = e.clip_rectangle().width * 55_f / 100;
   auto brush = drawing_2d::radial_gradient_brush {point {left, top}, light_point_color_, color_, radius};
-  e.graphics().fill_rectangle(brush, e.clip_rectangle());
+  e.graphics().fill_ellipse(brush, e.clip_rectangle());
+  
+  // Dark contour (shading)
+  auto pen = drawing::pen {drawing::color::from_argb(80, 0, 0, 0), 4};
+  e.graphics().draw_ellipse(pen, e.clip_rectangle());
+  
+  // Secondary highlight (light reflection)
+  auto highlight = drawing_2d::radial_gradient_brush(point {client_size().width / 3, client_size().height / 3}, drawing::color::from_argb(150, 255, 255, 255), drawing::color::from_argb(0, 255, 255, 255), client_size().width / 6);
+  e.graphics().fill_ellipse(highlight, rectangle {client_size().width / 4, client_size().height / 4, client_size().width / 2, client_size().height / 2});
+  
+  // Glossy reflection (upper dome)
+  auto glossy = drawing_2d::linear_gradient_brush {point {0, 0}, point {0, client_size().height / 2}, drawing::color::from_argb(120, 255, 255, 255), drawing::color::from_argb(0, 255, 255, 255)};
+  e.graphics().fill_pie(glossy, rectangle {0, 0, client_size().width, client_size().height}, 180, 180);
 }
