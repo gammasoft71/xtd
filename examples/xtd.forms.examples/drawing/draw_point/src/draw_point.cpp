80814a4f8ea46f0592976147a7252605c0f675eb
Gammasoft <gammasoft71@gmail.com>
2025-09-22 12:35:34 +0200

    Review example

--- examples/xtd.forms.examples/drawing/draw_point/src/draw_point.cpp
+++ examples/xtd.forms.examples/drawing/draw_point/src/draw_point.cpp
@@ -1,47 +1,57 @@
 #include <xtd/xtd>
 
-namespace draw_point_example {
+namespace particle_system_example {
   class form1 : public form {
-    struct colored_point {
-      point location;
-      xtd::drawing::color color;
-      int width = 0;
+    struct particle {
+      point position;
+      point velocity;
+      color color;
+      int size;
+
+      void update(int width, int height) {
+        position += velocity;
+        if (position.x < 0 || position.x > width) velocity.x = -velocity.x;
+        if (position.y < 0 || position.y > height) velocity.y = -velocity.y;
+      }
       
-      bool operator ==(const colored_point& rhs) const noexcept {return location == rhs.location && color == rhs.color && width == rhs.width;}
+      bool operator ==(const particle& other) const noexcept {return position == other.position && velocity == other.velocity && color == other.color && size == other.size;}
     };
     
   public:
     form1() {
       text("Draw point example");
+      client_size({600, 400});
       
-      generate_colored_points_timer.interval(200_ms);
-      generate_colored_points_timer.tick += event_handler {*this, &form1::generate_colored_points};
-      generate_colored_points_timer.start();
+      generate_particles_timer.interval(50_ms);
+      generate_particles_timer.tick += event_handler {*this, &form1::generate_particles};
+      generate_particles_timer.start();
     }
     
   protected:
     void on_paint(paint_event_args& e) override {
       form::on_paint(e);
       e.graphics().clear(color::black);
-      for (auto colored_point : colored_points)
-        e.graphics().draw_point(pen(colored_point.color, as<float>(colored_point.width)), colored_point.location);
+      
+      for (auto& particule : particles) {
+        e.graphics().draw_point(pen(particule.color, particule.size), particule.position.x, particule.position.y);
+        particule.update(client_size().width, client_size().height);
+      }
     }
-    
+
   private:
-    void generate_colored_points() {
-      static auto random = xtd::random {};
+    void generate_particles() {
+      static auto rnd = xtd::random {};
       static auto colors = basic_colors::get_colors();
-      colored_points.resize(random.next(height() * width() / 800, height() * width() / 400));
-      for (auto& colored_point : colored_points)
-        colored_point = {{random.next(client_size().width), random.next(client_size().height)}, colors[random.next(colors.count())], random.next(1, 10)};
+      if (particles.size() < 200)
+        particles.add({{rnd.next(client_size().width), rnd.next(client_size().height)}, {rnd.next(-5, 6), rnd.next(-5, 6)}, colors[rnd.next(colors.count())], rnd.next(2, 8)});
       invalidate();
     }
     
-    list<colored_point> colored_points;
-    forms::timer generate_colored_points_timer;
+    list<particle> particles;
+    forms::timer generate_particles_timer;
   };
 }
 
 auto main() -> int {
-  application::run(draw_point_example::form1 {});
+  application::run(particle_system_example::form1{});
 }
