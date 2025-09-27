590d52e0811d54c188e8c916307b7136d1a0ed92
Gammasoft <gammasoft71@gmail.com>
2025-09-27 18:34:07 +0200

    Update source_switch.cpp

--- src/xtd.core/src/xtd/diagnostics/source_switch.cpp
+++ src/xtd.core/src/xtd/diagnostics/source_switch.cpp
@@ -1,4 +1,5 @@
 #include "../../../include/xtd/diagnostics/source_switch.hpp"
+#include "../../../include/xtd/as.hpp"
 
 using namespace xtd;
 using namespace xtd::diagnostics;
@@ -6,23 +7,26 @@ using namespace xtd::diagnostics;
 source_switch::source_switch(const string& name) : switch_object(name, string::empty_string) {
 }
 
-source_switch::source_switch(const string& display_name, const string& default_switch_value) : switch_object(display_name, string::empty_string, default_switch_value) {
+source_switch::source_switch(const string& display_name, const string& default_switch_value) : switch_object(display_name, string::empty_string) {
+  value(default_switch_value);
 }
 
 
 source_levels source_switch::level() const noexcept {
-  return static_cast<source_levels>(switch_setting());
+  return as<source_levels>(switch_setting());
 }
 
 void source_switch::level(xtd::diagnostics::source_levels level) {
-  switch_setting(static_cast<int32>(level));
+  if (self_.level() == level) return;
+  value(enum_object<>::to_string(level));
 }
-
 
 bool source_switch::should_trace(trace_event_type event_type) noexcept {
   return (switch_setting() & static_cast<int32>(event_type)) != 0;
 }
 
 void source_switch::on_value_changed() {
-  switch_setting(static_cast<int32>(parse<source_levels>(value())));
+  auto level_value = source_levels {};
+  if (string::try_parse(value(), level_value)) switch_setting(as<int32>(level_value));
+  else switch_object::on_value_changed();
 }
