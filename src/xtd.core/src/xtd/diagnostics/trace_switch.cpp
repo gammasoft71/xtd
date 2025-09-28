19a25d8086088228341d43776337e17aa3263bfc
Gammasoft <gammasoft71@gmail.com>
2025-09-28 10:50:06 +0200

    Update trace_switch.cpp

--- src/xtd.core/src/xtd/diagnostics/trace_switch.cpp
+++ src/xtd.core/src/xtd/diagnostics/trace_switch.cpp
@@ -1,4 +1,5 @@
 #include "../../../include/xtd/diagnostics/trace_switch.hpp"
+#include "../../../include/xtd/as.hpp"
 
 using namespace xtd;
 using namespace xtd::diagnostics;
@@ -9,12 +10,13 @@ trace_switch::trace_switch(const string& display_name, const string& description
 trace_switch::trace_switch(const string& display_name, const string& description, const string& default_switch_value) : switch_object(display_name, description, default_switch_value) {
 }
 
-xtd::diagnostics::trace_level trace_switch::level() const noexcept {
-  return static_cast<xtd::diagnostics::trace_level>(switch_setting());
+trace_level trace_switch::level() const noexcept {
+  return as<trace_level>(switch_setting());
 }
 
 void trace_switch::level(xtd::diagnostics::trace_level level) noexcept {
-  switch_setting(static_cast<int32>(level));
+  if (self_.level() == level) return;
+  value(enum_object<>::to_string(level));
 }
 
 bool trace_switch::trace_switch::trace_error() const noexcept {
@@ -36,16 +38,17 @@ bool trace_switch::trace_warning() const noexcept {
 void trace_switch::on_switch_setting_changed() {
   switch_object::on_switch_setting_changed();
   auto level = switch_setting();
-  if (level < static_cast<int32>(xtd::diagnostics::trace_level::off)) {
+  if (level < as<int32>(xtd::diagnostics::trace_level::off)) {
     //xtd::diagnostics::debug::write_line<string>(xtd::string("trace_switch level too low"), display_name());
-    switch_setting(static_cast<int32>(xtd::diagnostics::trace_level::off));
-  } else if (level > static_cast<int32>(xtd::diagnostics::trace_level::verbose)) {
+    switch_setting(as<int32>(xtd::diagnostics::trace_level::off));
+  } else if (level > as<int32>(xtd::diagnostics::trace_level::verbose)) {
     //xtd::diagnostics::debug::write_line<string>(xtd::string("trace_switch level too high"), display_name());
-    switch_setting(static_cast<int32>(xtd::diagnostics::trace_level::verbose));
+    switch_setting(as<int32>(xtd::diagnostics::trace_level::verbose));
   }
 }
 
 void trace_switch::on_value_changed() {
-  switch_object::on_value_changed();
-  switch_setting(static_cast<int32>(xtd::parse<xtd::diagnostics::trace_level>(value())));
+  auto level_value = trace_level {};
+  if (string::try_parse(value(), level_value)) switch_setting(as<int32>(level_value));
+  else switch_object::on_value_changed();
 }
