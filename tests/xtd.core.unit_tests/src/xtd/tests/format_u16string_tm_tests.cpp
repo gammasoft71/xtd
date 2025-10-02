1695024b719c0409130e8fde6151f189d56d6659
Gammasoft <gammasoft71@gmail.com>
2025-10-02 16:33:04 +0200

    Fix exception

--- tests/xtd.core.unit_tests/src/xtd/tests/format_u16string_tm_tests.cpp
+++ tests/xtd.core.unit_tests/src/xtd/tests/format_u16string_tm_tests.cpp
@@ -20,7 +20,8 @@ namespace {
   static std::tm to_local_time(time_t time) noexcept {return *std::localtime(&time);}
   //static std::tm to_local_time(const std::chrono::system_clock::time_point& time) noexcept {return to_local_time(std::chrono::system_clock::to_time_t(time));}
   static std::tm to_local_time(std::tm time) noexcept {
-    if (xtd::to_string(time, "Z") != "" && xtd::to_string(time, "Z") == "UTC") return to_local_time(__make_utc_date_time(&time));
+    auto dt = date_time::from_tm(time);
+    if (xtd::date_time::sprintf("%Z", dt) != "" && xtd::date_time::sprintf("%Z", dt)== "UTC") return to_local_time(__make_utc_date_time(&time));
     return to_local_time(mktime(&time));
   }
   
@@ -29,7 +30,8 @@ namespace {
   static std::tm to_universal_time(const std::chrono::system_clock::time_point& time) noexcept {return to_universal_time(std::chrono::system_clock::to_time_t(time));}
   #endif
   static std::tm to_universal_time(std::tm time) noexcept {
-    if (xtd::to_string(time, "Z") != "" && xtd::to_string(time, "Z") == "UTC") return to_universal_time(mktime(&time));
+    auto dt = date_time::from_tm(time);
+    if (xtd::date_time::sprintf("%Z", dt) != "" && xtd::date_time::sprintf("%Z", dt) == "UTC") return to_universal_time(mktime(&time));
     return to_universal_time(__make_utc_date_time(&time));
   }
   
