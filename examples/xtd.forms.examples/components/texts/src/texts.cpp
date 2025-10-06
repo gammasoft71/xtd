d1467574e6eb5ecf6f171cb85a0eedc7a2ceec64
Gammasoft <gammasoft71@gmail.com>
2025-10-07 01:47:02 +0200

    Update texts.cpp

--- examples/xtd.forms.examples/components/texts/src/texts.cpp
+++ examples/xtd.forms.examples/components/texts/src/texts.cpp
@@ -7,7 +7,7 @@ public:
   form1() {
     text("Texts example");
     client_size({200, 400});
-    *this << list_box_texts;
+    self_ << list_box_texts;
     
     list_box_texts.dock(dock_style::fill);
     list_box_texts.items().add_range({texts::abort(), texts::about(), texts::actual_size(), texts::add(), texts::align_left(), texts::align_right(), texts::alignment(), texts::apply(), texts::ascending(), texts::back(), texts::bold(), texts::bottom(), texts::cancel(), texts::cdrom(), texts::centered(), texts::clear(), texts::close(), texts::color(), texts::contents(), texts::convert(), texts::copy(), texts::customize(), texts::cut(), texts::del(), texts::descending(), texts::down(), texts::edit(), texts::execute(), texts::file(), texts::find(), texts::first(), texts::floppy(), texts::font(), texts::forward(), texts::harddisk(), texts::help(), texts::home(), texts::ignore(), texts::indent(), texts::index(), texts::info(), texts::italic(), texts::jump_to(), texts::justified(), texts::last(), texts::network(), texts::new_(), texts::no(), texts::ok(), texts::open(), texts::options(), texts::paste(), texts::preferences(), texts::print(), texts::print_preview(), texts::properties(), texts::quit(), texts::redo(), texts::refresh(), texts::remove(), texts::replace(), texts::retry(), texts::revert_to_saved(), texts::save(), texts::save_as(), texts::search(), texts::select_all(), texts::spell_check(), texts::stop(), texts::strikeout(), texts::tools(), texts::top(), texts::undelete(), texts::underline(), texts::undo(), texts::unindent(), texts::up(), texts::view(), texts::yes(), texts::zoom_in(), texts::zoom_out(), texts::zoom_to_fit()});
@@ -19,8 +19,8 @@ private:
 
 auto main() -> int {
   try {
-    //culture_info::current_culture(culture_info {"fr_FR.utf-8"});
-    //culture_info::current_culture(culture_info {"zh_CN.utf-8"});
+    //culture_info::current_culture(culture_info {"fr-FR"});
+    //culture_info::current_culture(culture_info {"zh-Hans-CN"});
   } catch (const std::exception& e) {
     message_box::show(string::format("Make sure specified locale is installed on your system :\n\n{}", e.what()), "Exception");
     return -1;
