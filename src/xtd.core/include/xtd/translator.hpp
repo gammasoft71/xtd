57af02bfdd0d1a71eb7a9b68267ea4f34e3528e5
Gammasoft <gammasoft71@gmail.com>
2025-10-10 12:10:59 +0200

    Review translator to load dynamically languages

--- src/xtd.core/include/xtd/translator.hpp
+++ src/xtd.core/include/xtd/translator.hpp
@@ -3,12 +3,12 @@
 /// @copyright Copyright (c) 2025 Gammasoft. All rights reserved.
 #pragma once
 #include "collections/specialized/string_dictionary.hpp"
+#include "io/path.hpp"
+#include "environment.hpp"
+#include "optional.hpp"
 #include "static.hpp"
 #include "string.hpp"
 #include <string>
-#include <map>
-#include <set>
-#include <vector>
 
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
@@ -38,19 +38,11 @@ namespace xtd {
     /// @remarks Normally you should use the std::locale() method. This function only exists if you have a good reason to bypass std::locale().
     /// @warning If you set the language with this method, and you use the std::locale method afterwards, translator will use the language initialized with this method and not with std::locale().
     static void language(const xtd::string& language);
-    
-    /// @brief Gets the global locale.
-    /// @return The global locale.
-    /// @remarks is same a call `std::locale {};`
-    static std::locale locale();
-    /// @brief Sets the global locale.
-    /// @param value The global locale.
-    /// @remarks is same a call `std::locale::global(std::locale {value});`
-    static void locale(const xtd::string& value);
-    /// @brief Sets the global locale.
-    /// @param value The global locale.
-    /// @remarks is same a call `std::locale::global(value);`
-    static void locale(const std::locale& value);
+    /// @brief Sets the current application language.
+    /// @param language The current application language.
+    /// @remarks Normally you should use the std::locale() method. This function only exists if you have a good reason to bypass std::locale().
+    /// @warning If you set the language with this method, and you use the std::locale method afterwards, translator will use the language initialized with this method and not with std::locale().
+    static void language(xtd::null_ptr);
     
     /// @brief Gets an array of languages supported by the application.
     /// @return An array of languages supported by the application.
@@ -69,38 +61,67 @@ namespace xtd {
     /// @param key The key to be translated.
     /// @parama value The translation of the key for the specified language.
     /// @warning This method should not be used. The translation is normally done automatically. This method is present only if you want to make an instant translation on a very small number of keys. It would not be wise to translate an entire application into different languages with this method.
-    static void add_value(const xtd::string& language, const xtd::string& key, const xtd::string& value);
+    static void add_value(const xtd::string& language, const xtd::string& key, const xtd::string& value) {
+      auto language_name  = to_language_name(language);
+      if (!xtd::io::directory::exists(xtd::io::path::combine(application_locale_path(), language_name))) language_name = to_fallback_name(language_name);
+      if (load_language(language_name)) languages_[language_name] = language_name;
+      language_values_[languages_[language_name]][key] = value;
+    }
     
     /// @brief Parses a specified path that contains translations.
     /// @param locale_path Path that contains translations.
-    static void parse_locale(const xtd::string& locale_path);
+    /// @return `true` if parse succeed; otherwhise `false`.
+    /// @remarks parse path with xtd::translator::language() language.
+    static bool parse_locale(const xtd::string& locale_path);
+    
+    /// @brief Parses a specified path that contains translations.
+    /// @param locale_path Path that contains translations.
+    /// @return `true` if parse succeed; otherwhise `false`.
+    static bool parse_locale(const xtd::string& locale_path, const xtd::string& language);
+    
+    /// @brief Parses a specified file that contains translations for a specified language.
+    /// @param file The file that contains translations.
+    /// @return `true` if parse succeed; otherwhise `false`.
+    /// @remarks parse file with xtd::translator::language() language.
+    static bool parse_file(const xtd::string& file);
     
     /// @brief Parses a specified file that contains translations for a specified language.
     /// @param file The file that contains translations.
     /// @param language The language of the translation file.
-    static void parse_file(const xtd::string& file, const xtd::string& language);
+    /// @return `true` if parse succeed; otherwhise `false`.
+    static bool parse_file(const xtd::string& file, const xtd::string& language);
     
     /// @brief Translates a string into the lganguage specified by the xtd::translate::language property.
     /// @return The translated string.
     /// @remarks If no translation is found, the result is the same as the value.
-    static xtd::string translate(const xtd::string& value) noexcept;
+    static xtd::string translate(const xtd::string& value) noexcept {return translate(language(), value);}
     /// @brief Translates a string into the specified lganguage.
     /// @return The translated string.
     /// @param language The language used to translate the value
     /// @remarks If no translation is found, the result is the same as the value.
-    static xtd::string translate(const xtd::string& language, const xtd::string& value) noexcept;
+    static xtd::string translate(const xtd::string& language, const xtd::string& value) noexcept {return translate(language, value.chars().c_str());}
     /// @}
     
     /// @cond
-    static const char* translate(const char* value) noexcept;
-    static const char* translate(const xtd::string& language, const char* value) noexcept;
+    static const char* translate(const char* value) noexcept {return translate(language(), value);}
+    static const char* translate(const xtd::string& language, const char* value) noexcept {
+      auto language_name  = to_language_name(language);
+      if (!xtd::io::directory::exists(xtd::io::path::combine(application_locale_path(), language_name))) language_name = to_fallback_name(language_name);
+      if (!load_language(language_name)) return value;
+      if (!language_values_[languages_[language_name]].contains_key(value)) return value;
+      return language_values_[languages_[language_name]][value].c_str();
+    }
     /// @endcond
     
   private:
-    static void initialize();
-    static xtd::string locale_to_language(xtd::string locale);
-    static std::map<xtd::string, std::map<xtd::string, xtd::string>> language_values_;
-    static xtd::string language_;
-    static std::set<string> translated_languages_;
+    static string application_locale_path() {return xtd::io::path::combine(xtd::environment::get_folder_path(environment::special_folder::application_resources), "locale");}
+    static string xtd_locale_path() {return environment::get_folder_path(xtd::environment::special_folder::xtd_locale);}
+    static bool load_language(const xtd::string& language) {return load_language(language, xtd_locale_path(), application_locale_path());}
+    static bool load_language(const xtd::string& language, const xtd::string& xtd_locale_path, const xtd::string& application_locale_path);
+    static string to_language_name(const xtd::string& language);
+    static string to_fallback_name(const xtd::string& language);
+    static xtd::collections::generic::dictionary<xtd::string, xtd::collections::specialized::string_dictionary> language_values_;
+    static xtd::collections::specialized::string_dictionary languages_;
+    static xtd::optional<xtd::string> language_;
   };
 }
