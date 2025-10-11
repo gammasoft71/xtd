e2fef1bfcc8cdf5f173ba3bf580ceb45487b408b
Gammasoft <gammasoft71@gmail.com>
2025-10-11 22:08:48 +0200

    using xtd::globalization::culture_info instead std::locale

--- tests/xtd.core.unit_tests/src/xtd/io/tests/end_of_stream_exception_tests.cpp
+++ tests/xtd.core.unit_tests/src/xtd/io/tests/end_of_stream_exception_tests.cpp
@@ -1,4 +1,5 @@
 #include <xtd/io/end_of_stream_exception>
+#include <xtd/globalization/culture_info>
 #include <xtd/argument_exception>
 #include <xtd/environment>
 #include <xtd/io/path>
@@ -16,15 +17,17 @@ using namespace xtd::tunit;
 namespace xtd::io::tests {
   class test_class_(end_of_stream_exception_tests) {
     inline static bool stack_trace_enabled = false;
-    inline static std::locale previous_locale;
+    inline static xtd::globalization::culture_info previous_culture;
+    
     static void test_initialize_(test_initialize) {
-      previous_locale = std::locale::global(std::locale("en_US.UTF-8"));
+      previous_culture = xtd::globalization::culture_info::current_culture();
+      xtd::globalization::culture_info::current_culture(xtd::globalization::culture_info {"en-US"});
       stack_trace_enabled = end_of_stream_exception::enable_stack_trace();
       end_of_stream_exception::enable_stack_trace(false);
     }
     
     static void test_cleanup_(test_cleanup) {
-      std::locale::global(previous_locale);
+      xtd::globalization::culture_info::current_culture(previous_culture);
       end_of_stream_exception::enable_stack_trace(stack_trace_enabled);
     }
     
