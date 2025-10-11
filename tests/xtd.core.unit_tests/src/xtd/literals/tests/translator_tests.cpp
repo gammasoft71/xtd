e288555903c1faffadd7aabf6fb0389e9d612b91
Gammasoft <gammasoft71@gmail.com>
2025-10-11 17:37:28 +0200

    Add xtd::literals::translator unit tests

--- tests/xtd.core.unit_tests/src/xtd/literals/tests/translator_tests.cpp
+++ tests/xtd.core.unit_tests/src/xtd/literals/tests/translator_tests.cpp
@@ -1,39 +1,44 @@
-#include <xtd/globalization/translator>
+#include <xtd/literals/translator.hpp>
 #include <xtd/environment>
 #include <xtd/tunit/assert>
 #include <xtd/tunit/test_class_attribute>
 #include <xtd/tunit/test_method_attribute>
 
 using namespace xtd;
+using namespace xtd::globalization;
 using namespace xtd::tunit;
 
 namespace xtd {
   inline namespace literals {
     namespace tests {
       class test_class_(translator_tests) {
+        static void class_initialize_(class_initialize) {
+          translator::add_value("fr", "literals_translator_key1", "literals_translator_key1_fr");
+        }
+        
         void test_method_(_t_operator_on_char_ptr) {
-          auto v = "Belgium"_t;
-          assert::are_equal("Belgium"_t, string {v});
+          translator::language("fr");
+          assert::are_equal("literals_translator_key1_fr", "literals_translator_key1"_t);
         }
         
         void test_method_(_t_operator_on_char8_ptr) {
-          auto v = u8"Belgium"_t;
-          assert::are_equal(u8"Belgium"_t, string {v});
+          translator::language("fr");
+          assert::are_equal("literals_translator_key1_fr", u8"literals_translator_key1"_t);
         }
         
         void test_method_(_t_operator_on_char16_ptr) {
-          auto v = u"Belgium"_t;
-          assert::are_equal("Belgium"_t, string {v});
+          translator::language("fr");
+          assert::are_equal("literals_translator_key1_fr", u"literals_translator_key1"_t);
         }
         
         void test_method_(_t_operator_on_char32_ptr) {
-          auto v = U"Belgium"_t;
-          assert::are_equal("Belgium"_t, string {v});
+          translator::language("fr");
+          assert::are_equal("literals_translator_key1_fr", U"literals_translator_key1"_t);
         }
         
         void test_method_(_t_operator_on_wchar_ptr) {
-          auto v = L"Belgium"_t;
-          assert::are_equal("Belgium"_t, string {v});
+          translator::language("fr");
+          assert::are_equal("literals_translator_key1_fr", L"literals_translator_key1"_t);
         }
       };
     }
