bed0c0a1c464b13846676e8b152d58da49ab468c
Gammasoft <gammasoft71@gmail.com>
2025-10-11 22:08:28 +0200

    using xtd::globalization::culture_info instead std::locale

--- tests/xtd.core.unit_tests/src/xtd/globalization/tests/culture_not_found_exception_tests.cpp
+++ tests/xtd.core.unit_tests/src/xtd/globalization/tests/culture_not_found_exception_tests.cpp
@@ -1,4 +1,5 @@
 #include <xtd/globalization/culture_not_found_exception>
+#include <xtd/globalization/culture_info>
 #include <xtd/argument_exception>
 #include <xtd/environment>
 #include <xtd/nullopt>
@@ -18,15 +19,17 @@ using namespace xtd::tunit;
 namespace xtd::globalization::tests {
   class test_class_(culture_not_found_exception_tests) {
     inline static bool stack_trace_enabled = false;
-    inline static std::locale previous_locale;
+    inline static xtd::globalization::culture_info previous_culture;
+
     static void test_initialize_(test_initialize) {
-      previous_locale = std::locale::global(std::locale("en_US.UTF-8"));
+      previous_culture = xtd::globalization::culture_info::current_culture();
+      xtd::globalization::culture_info::current_culture(xtd::globalization::culture_info {"en-US"});
       stack_trace_enabled = culture_not_found_exception::enable_stack_trace();
       culture_not_found_exception::enable_stack_trace(false);
     }
     
     static void test_cleanup_(test_cleanup) {
-      std::locale::global(previous_locale);
+      xtd::globalization::culture_info::current_culture(previous_culture);
       culture_not_found_exception::enable_stack_trace(stack_trace_enabled);
     }
     
