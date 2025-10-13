c31ec16318c515ebfff1a041e3c6c98cee836501
Gammasoft <gammasoft71@gmail.com>
2025-10-13 23:27:19 +0200

    Fix error message

--- src/xtd.core/include/xtd/collections/specialized/string_map.hpp
+++ src/xtd.core/include/xtd/collections/specialized/string_map.hpp
@@ -3,9 +3,9 @@
 /// @copyright Copyright (c) 2025 Gammasoft. All rights reserved.
 #pragma once
 #if defined(_MSC_VER) && __cplusplus < 202302L
-#  pragma message("Deprecated : Replaced by #include <xtd/collection/specialized/string_dictionary.h> - Will be removed in version 0.4.0.")
+#  pragma message("Deprecated : Replaced by #include <xtd/collection/specialized/string_dictionary.hpp> - Will be removed in version 0.4.0.")
 #else
-#  warning "Deprecated : Replaced by #include <xtd/collection/specialized/string_dictionary.h> - Will be removed in version 0.4.0."
+#  warning "Deprecated : Replaced by #include <xtd/collection/specialized/string_dictionary.hpp> - Will be removed in version 0.4.0."
 #endif
 #include "string_dictionary.hpp"
 
