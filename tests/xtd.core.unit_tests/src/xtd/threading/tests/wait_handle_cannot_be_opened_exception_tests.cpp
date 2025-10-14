bb6ca3cf8c4da46de0d0af0245b0c4b56f0e0bc9
Gammasoft <gammasoft71@gmail.com>
2025-10-14 14:20:39 +0200

    using xtd::globalization::culture_info

--- tests/xtd.core.unit_tests/src/xtd/threading/tests/wait_handle_cannot_be_opened_exception_tests.cpp
+++ tests/xtd.core.unit_tests/src/xtd/threading/tests/wait_handle_cannot_be_opened_exception_tests.cpp
@@ -1,4 +1,5 @@
 #include <xtd/threading/wait_handle_cannot_be_opened_exception>
+#include <xtd/globalization/culture_info>
 #include <xtd/tunit/assert>
 #include <xtd/tunit/test_class_attribute>
 #include <xtd/tunit/test_method_attribute>
@@ -9,15 +10,17 @@ using namespace xtd::tunit;
 namespace xtd::threading::tests {
   class test_class_(wait_handle_cannot_be_opened_exception_tests) {
     inline static bool stack_trace_enabled = false;
-    inline static std::locale previous_locale;
+    inline static xtd::globalization::culture_info previous_culture;
+    
     static void test_initialize_(test_initialize) {
-      previous_locale = std::locale::global(std::locale("en_US.UTF-8"));
+      previous_culture = xtd::globalization::culture_info::current_culture();
+      xtd::globalization::culture_info::current_culture(xtd::globalization::culture_info {"en-US"});
       stack_trace_enabled = system_exception::enable_stack_trace();
       system_exception::enable_stack_trace(false);
     }
     
     static void test_cleanup_(test_cleanup) {
-      std::locale::global(previous_locale);
+      xtd::globalization::culture_info::current_culture(previous_culture);
       system_exception::enable_stack_trace(stack_trace_enabled);
     }
   };
