b806c3673da7caefc3aa9f539441478d39f0142f
Gammasoft <gammasoft71@gmail.com>
2025-10-15 21:14:44 +0200

    update latest reference guide

---
+++ static/reference_guides/latest/html/search/typedefs_16.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['xtd_5flibrary_5fcollection_0',['xtd_library_collection',['../classxtd_1_1environment.html#a94fd7b792593363e4d3b4456708cc29e',1,'xtd::environment']]]
+];
---
+++ static/reference_guides/latest/html/search/typedefs_f.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['queue_0',['queue',['../group__generic__collections.html#gafe25926b152d27ca34bd3c297b77beb6',1,'xtd::collections::generic::queue'],['../group__collections.html#ga0ec3432a782c2041cead8cba6f3499e8',1,'xtd::collections::queue']]]
+];
