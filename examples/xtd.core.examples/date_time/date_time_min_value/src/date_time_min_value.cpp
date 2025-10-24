5906f401300bf63a029ae3fce4b3ca2032197974
Gammasoft <gammasoft71@gmail.com>
2025-10-24 22:53:32 +0200

    Update date_time_min_value.cpp

--- examples/xtd.core.examples/date_time/date_time_min_value/src/date_time_min_value.cpp
+++ examples/xtd.core.examples/date_time/date_time_min_value/src/date_time_min_value.cpp
@@ -5,9 +5,9 @@ public:
   static auto main() {
     // Define an uninitialized date.
     auto date1 = date_time {};
-    console::write("{:u}", date1);
+    console::write(date1);
     if (date1.equals(date_time::min_value))
-      console::write_line("  (Equals date_time.min_value)");
+      console::write_line("  (equals date_time::min_value)");
   }
 };
 
@@ -15,4 +15,4 @@ startup_(program::main);
 
 // This code produces the following output :
 //
-// 01/01/0001 00:00:00  (Equals date_time.min_value)
+// 1/1/0001 0:00:00 AM  (equals date_time::min_value)
