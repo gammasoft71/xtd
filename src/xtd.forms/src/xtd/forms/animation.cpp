203af9df32cd6597ac7f13582ebce7d926e82dce
Gammasoft <gammasoft71@gmail.com>
2025-10-25 22:22:56 +0200

    using safe event

--- src/xtd.forms/src/xtd/forms/animation.cpp
+++ src/xtd.forms/src/xtd/forms/animation.cpp
@@ -206,7 +206,8 @@ xtd::uptr<xtd::object> animation::clone() const {
 }
 
 void animation::on_updated(const animation_updated_event_args& e) {
-  updated(*this, e);
+  auto safe_updated = updated;
+  if (!safe_updated.is_empty()) safe_updated(*this, e);
 }
 
 void animation::on_frames_timer_tick(object& timer, const event_args& e) {
