dea70ce9a5a509f4694690e876d6b61970527ded
Gammasoft <gammasoft71@gmail.com>
2025-10-31 15:17:54 +0100

    Update xtd_data_generator.cs

--- tools/xtd_data_generator/xtd_data_generator.cs
+++ tools/xtd_data_generator/xtd_data_generator.cs
@@ -19,6 +19,8 @@ class xtd_data_generator {
     Console.WriteLine(GenerateDateTimeFormats(Path.Combine(new[] {xtdDataPath, "date_time_formats.bin"}), version) ? "[SUCCEED]" : "[FAILED ]");
     Console.Write("  * Generate NumberFormats...   ");
     Console.WriteLine(GenerateNumberFormats(Path.Combine(new[] {xtdDataPath, "number_formats.bin"}), version) ? "[SUCCEED]" : "[FAILED ]");
+    Console.Write("  * Generate Regions...         ");
+    Console.WriteLine(GenerateRegions(Path.Combine(new[] {xtdDataPath, "regions.bin"}), version) ? "[SUCCEED]" : "[FAILED ]");
     Console.Write("  * Generate TimeZones...       ");
     Console.WriteLine(GenerateTimeZones(Path.Combine(new[] {xtdDataPath, "time_zones.bin"}), version) ? "[SUCCEED]" : "[FAILED ]");
     Console.WriteLine("Generation ended");
@@ -27,26 +29,26 @@ class xtd_data_generator {
 
   static bool GenerateCultures(string file_path, int version) {
     try {
-      var bw = new System.IO.BinaryWriter(File.Open(file_path, FileMode.Create), Encoding.UTF8);
-      bw.Write(new byte[] {(byte)'X', (byte)'T', (byte)'D', 0, (byte)'C', (byte)'U', (byte)'L', (byte)'T'});
-      bw.Write(version);
-      var cultures = CultureInfo.GetCultures(CultureTypes.AllCultures);
-      bw.Write(cultures.Length);
-      foreach (var cultureInfo in cultures.OrderBy(culture => culture.Name)) {
-        bw.Write(cultureInfo.Name.ToLower());
-        bw.Write((int)(cultureInfo.CultureTypes & CultureTypes.AllCultures));
-        bw.Write(cultureInfo.DisplayName);
-        bw.Write(cultureInfo.EnglishName);
-        bw.Write(cultureInfo.KeyboardLayoutId);
-        bw.Write(cultureInfo.LCID);
-        bw.Write(cultureInfo.Name);
-        bw.Write(cultureInfo.NativeName);
-        bw.Write(cultureInfo.Parent.Name);
-        bw.Write(cultureInfo.ThreeLetterISOLanguageName);
-        bw.Write(cultureInfo.ThreeLetterWindowsLanguageName);
-        bw.Write(cultureInfo.TwoLetterISOLanguageName);
+      using (var bw = new System.IO.BinaryWriter(File.Open(file_path, FileMode.Create), Encoding.UTF8)) {
+        bw.Write(new byte[] { (byte)'X', (byte)'T', (byte)'D', 0, (byte)'C', (byte)'U', (byte)'L', (byte)'T' });
+        bw.Write(version);
+        var cultures = CultureInfo.GetCultures(CultureTypes.AllCultures);
+        bw.Write(cultures.Length);
+        foreach (var cultureInfo in cultures.OrderBy(culture => culture.Name)) {
+          bw.Write(cultureInfo.Name.ToLower());
+          bw.Write((int)(cultureInfo.CultureTypes & CultureTypes.AllCultures));
+          bw.Write(cultureInfo.DisplayName);
+          bw.Write(cultureInfo.EnglishName);
+          bw.Write(cultureInfo.KeyboardLayoutId);
+          bw.Write(cultureInfo.LCID);
+          bw.Write(cultureInfo.Name);
+          bw.Write(cultureInfo.NativeName);
+          bw.Write(cultureInfo.Parent.Name);
+          bw.Write(cultureInfo.ThreeLetterISOLanguageName);
+          bw.Write(cultureInfo.ThreeLetterWindowsLanguageName);
+          bw.Write(cultureInfo.TwoLetterISOLanguageName);
+        }
       }
-      bw.Close();
       return true;
     } catch (Exception e) {
       Trace.WriteLine("ERROR: " + e.Message);
@@ -57,38 +59,38 @@ class xtd_data_generator {
 
   static bool GenerateDateTimeFormats(string file_path, int version) {
     try {
-      var bw = new System.IO.BinaryWriter(File.Open(file_path, FileMode.Create), Encoding.UTF8);
-      bw.Write(new byte[] {(byte)'X', (byte)'T', (byte)'D', 0, (byte)'D', (byte)'T', (byte)'F', (byte)'M'});
-      bw.Write(version);
-      var cultures = CultureInfo.GetCultures(CultureTypes.AllCultures);
-      bw.Write(cultures.Length);
-      foreach (var cultureInfo in cultures.OrderBy(culture => culture.Name)) {
-        bw.Write(cultureInfo.Name.ToLower());
-        Write(bw, cultureInfo.DateTimeFormat.AbbreviatedDayNames);
-        Write(bw, cultureInfo.DateTimeFormat.AbbreviatedMonthGenitiveNames);
-        Write(bw, cultureInfo.DateTimeFormat.AbbreviatedMonthNames);
-        bw.Write(cultureInfo.DateTimeFormat.AMDesignator);
-        bw.Write(cultureInfo.DateTimeFormat.DateSeparator);
-        Write(bw, cultureInfo.DateTimeFormat.DayNames);
-        bw.Write((int)cultureInfo.DateTimeFormat.FirstDayOfWeek);
-        bw.Write(cultureInfo.DateTimeFormat.FullDateTimePattern);
-        bw.Write(cultureInfo.DateTimeFormat.LongDatePattern);
-        bw.Write(cultureInfo.DateTimeFormat.LongTimePattern);
-        bw.Write(cultureInfo.DateTimeFormat.MonthDayPattern);
-        Write(bw, cultureInfo.DateTimeFormat.MonthGenitiveNames);
-        Write(bw, cultureInfo.DateTimeFormat.MonthNames);
-        bw.Write(cultureInfo.DateTimeFormat.NativeCalendarName);
-        bw.Write(cultureInfo.DateTimeFormat.PMDesignator);
-        bw.Write(cultureInfo.DateTimeFormat.RFC1123Pattern);
-        bw.Write(cultureInfo.DateTimeFormat.ShortDatePattern);
-        bw.Write(cultureInfo.DateTimeFormat.ShortTimePattern);
-        Write(bw, cultureInfo.DateTimeFormat.ShortestDayNames);
-        bw.Write(cultureInfo.DateTimeFormat.SortableDateTimePattern);
-        bw.Write(cultureInfo.DateTimeFormat.TimeSeparator);
-        bw.Write(cultureInfo.DateTimeFormat.UniversalSortableDateTimePattern);
-        bw.Write(cultureInfo.DateTimeFormat.YearMonthPattern);
+      using (var bw = new System.IO.BinaryWriter(File.Open(file_path, FileMode.Create), Encoding.UTF8)) {
+        bw.Write(new byte[] { (byte)'X', (byte)'T', (byte)'D', 0, (byte)'D', (byte)'T', (byte)'F', (byte)'M' });
+        bw.Write(version);
+        var cultures = CultureInfo.GetCultures(CultureTypes.AllCultures);
+        bw.Write(cultures.Length);
+        foreach (var cultureInfo in cultures.OrderBy(culture => culture.Name)) {
+          bw.Write(cultureInfo.Name.ToLower());
+          Write(bw, cultureInfo.DateTimeFormat.AbbreviatedDayNames);
+          Write(bw, cultureInfo.DateTimeFormat.AbbreviatedMonthGenitiveNames);
+          Write(bw, cultureInfo.DateTimeFormat.AbbreviatedMonthNames);
+          bw.Write(cultureInfo.DateTimeFormat.AMDesignator);
+          bw.Write(cultureInfo.DateTimeFormat.DateSeparator);
+          Write(bw, cultureInfo.DateTimeFormat.DayNames);
+          bw.Write((int)cultureInfo.DateTimeFormat.FirstDayOfWeek);
+          bw.Write(cultureInfo.DateTimeFormat.FullDateTimePattern);
+          bw.Write(cultureInfo.DateTimeFormat.LongDatePattern);
+          bw.Write(cultureInfo.DateTimeFormat.LongTimePattern);
+          bw.Write(cultureInfo.DateTimeFormat.MonthDayPattern);
+          Write(bw, cultureInfo.DateTimeFormat.MonthGenitiveNames);
+          Write(bw, cultureInfo.DateTimeFormat.MonthNames);
+          bw.Write(cultureInfo.DateTimeFormat.NativeCalendarName);
+          bw.Write(cultureInfo.DateTimeFormat.PMDesignator);
+          bw.Write(cultureInfo.DateTimeFormat.RFC1123Pattern);
+          bw.Write(cultureInfo.DateTimeFormat.ShortDatePattern);
+          bw.Write(cultureInfo.DateTimeFormat.ShortTimePattern);
+          Write(bw, cultureInfo.DateTimeFormat.ShortestDayNames);
+          bw.Write(cultureInfo.DateTimeFormat.SortableDateTimePattern);
+          bw.Write(cultureInfo.DateTimeFormat.TimeSeparator);
+          bw.Write(cultureInfo.DateTimeFormat.UniversalSortableDateTimePattern);
+          bw.Write(cultureInfo.DateTimeFormat.YearMonthPattern);
+        }
       }
-      bw.Close();
       return true;
     } catch (Exception e) {
       Trace.WriteLine("ERROR: " + e.Message);
@@ -99,41 +101,41 @@ class xtd_data_generator {
 
   static bool GenerateNumberFormats(string file_path, int version) {
     try {
-      var bw = new System.IO.BinaryWriter(File.Open(file_path, FileMode.Create), Encoding.UTF8);
-      bw.Write(new byte[] {(byte)'X', (byte)'T', (byte)'D', 0, (byte)'N', (byte)'U', (byte)'M', (byte)'F'});
-      bw.Write(version);
-      var cultures = CultureInfo.GetCultures(CultureTypes.AllCultures);
-      bw.Write(cultures.Length);
-      foreach (var cultureInfo in cultures.OrderBy(culture => culture.Name)) {
-        bw.Write(cultureInfo.Name.ToLower());
-        bw.Write(cultureInfo.NumberFormat.CurrencyDecimalDigits);
-        bw.Write(cultureInfo.NumberFormat.CurrencyDecimalSeparator);
-        bw.Write(cultureInfo.NumberFormat.CurrencyGroupSeparator);
-        Write(bw, cultureInfo.NumberFormat.CurrencyGroupSizes);
-        bw.Write(cultureInfo.NumberFormat.CurrencyNegativePattern);
-        bw.Write(cultureInfo.NumberFormat.CurrencyPositivePattern);
-        bw.Write(cultureInfo.NumberFormat.CurrencySymbol);
-        bw.Write((int)cultureInfo.NumberFormat.DigitSubstitution);
-        bw.Write(cultureInfo.NumberFormat.NaNSymbol);
-        bw.Write(cultureInfo.NumberFormat.NegativeInfinitySymbol);
-        bw.Write(cultureInfo.NumberFormat.NegativeSign);
-        bw.Write(cultureInfo.NumberFormat.NumberDecimalDigits);
-        bw.Write(cultureInfo.NumberFormat.NumberDecimalSeparator);
-        bw.Write(cultureInfo.NumberFormat.NumberGroupSeparator);
-        Write(bw, cultureInfo.NumberFormat.NumberGroupSizes);
-        bw.Write(cultureInfo.NumberFormat.NumberNegativePattern);
-        bw.Write(cultureInfo.NumberFormat.PercentDecimalDigits);
-        bw.Write(cultureInfo.NumberFormat.PercentDecimalSeparator);
-        bw.Write(cultureInfo.NumberFormat.PercentGroupSeparator);
-        Write(bw, cultureInfo.NumberFormat.PercentGroupSizes);
-        bw.Write(cultureInfo.NumberFormat.PercentNegativePattern);
-        bw.Write(cultureInfo.NumberFormat.PercentPositivePattern);
-        bw.Write(cultureInfo.NumberFormat.PercentSymbol);
-        bw.Write(cultureInfo.NumberFormat.PerMilleSymbol);
-        bw.Write(cultureInfo.NumberFormat.PositiveInfinitySymbol);
-        bw.Write(cultureInfo.NumberFormat.PositiveSign);
+      using (var bw = new System.IO.BinaryWriter(File.Open(file_path, FileMode.Create), Encoding.UTF8)) {
+        bw.Write(new byte[] { (byte)'X', (byte)'T', (byte)'D', 0, (byte)'N', (byte)'U', (byte)'M', (byte)'F' });
+        bw.Write(version);
+        var cultures = CultureInfo.GetCultures(CultureTypes.AllCultures);
+        bw.Write(cultures.Length);
+        foreach (var cultureInfo in cultures.OrderBy(culture => culture.Name)) {
+          bw.Write(cultureInfo.Name.ToLower());
+          bw.Write(cultureInfo.NumberFormat.CurrencyDecimalDigits);
+          bw.Write(cultureInfo.NumberFormat.CurrencyDecimalSeparator);
+          bw.Write(cultureInfo.NumberFormat.CurrencyGroupSeparator);
+          Write(bw, cultureInfo.NumberFormat.CurrencyGroupSizes);
+          bw.Write(cultureInfo.NumberFormat.CurrencyNegativePattern);
+          bw.Write(cultureInfo.NumberFormat.CurrencyPositivePattern);
+          bw.Write(cultureInfo.NumberFormat.CurrencySymbol);
+          bw.Write((int)cultureInfo.NumberFormat.DigitSubstitution);
+          bw.Write(cultureInfo.NumberFormat.NaNSymbol);
+          bw.Write(cultureInfo.NumberFormat.NegativeInfinitySymbol);
+          bw.Write(cultureInfo.NumberFormat.NegativeSign);
+          bw.Write(cultureInfo.NumberFormat.NumberDecimalDigits);
+          bw.Write(cultureInfo.NumberFormat.NumberDecimalSeparator);
+          bw.Write(cultureInfo.NumberFormat.NumberGroupSeparator);
+          Write(bw, cultureInfo.NumberFormat.NumberGroupSizes);
+          bw.Write(cultureInfo.NumberFormat.NumberNegativePattern);
+          bw.Write(cultureInfo.NumberFormat.PercentDecimalDigits);
+          bw.Write(cultureInfo.NumberFormat.PercentDecimalSeparator);
+          bw.Write(cultureInfo.NumberFormat.PercentGroupSeparator);
+          Write(bw, cultureInfo.NumberFormat.PercentGroupSizes);
+          bw.Write(cultureInfo.NumberFormat.PercentNegativePattern);
+          bw.Write(cultureInfo.NumberFormat.PercentPositivePattern);
+          bw.Write(cultureInfo.NumberFormat.PercentSymbol);
+          bw.Write(cultureInfo.NumberFormat.PerMilleSymbol);
+          bw.Write(cultureInfo.NumberFormat.PositiveInfinitySymbol);
+          bw.Write(cultureInfo.NumberFormat.PositiveSign);
+        }
       }
-      bw.Close();
       return true;
     } catch (Exception e) {
       Trace.WriteLine("ERROR: " + e.Message);
@@ -142,43 +144,32 @@ class xtd_data_generator {
     }
   }
 
-  static bool GenerateTimeZones(string file_path, int version) {
+  static bool GenerateRegions(string file_path, int version) {
     try {
-      var bw = new System.IO.BinaryWriter(File.Open(file_path, FileMode.Create), Encoding.UTF8);
-      bw.Write(new byte[] {(byte)'X', (byte)'T', (byte)'D', 0, (byte)'T', (byte)'Z', (byte)'O', (byte)'N'});
-      bw.Write(version);
-      var timeZones = TimeZoneInfo.GetSystemTimeZones();
-      bw.Write(timeZones.Count);
-      foreach (var tz in timeZones.OrderBy(tz => tz.Id)) {
-        bw.Write(tz.Id);
-        bw.Write(tz.DisplayName);
-        bw.Write(tz.StandardName);
-        bw.Write(tz.DaylightName);
-        bw.Write(tz.BaseUtcOffset.TotalMinutes);
-
-        var adjustmentRules = tz.GetAdjustmentRules();
-        bw.Write(adjustmentRules.Length);
-        foreach (var rule in adjustmentRules) {
-          bw.Write(rule.DateStart.ToBinary());
-          bw.Write(rule.DateEnd.ToBinary());
-          bw.Write(rule.DaylightDelta.TotalMinutes);
-
-          bw.Write(rule.DaylightTransitionStart.IsFixedDateRule);
-          bw.Write(rule.DaylightTransitionStart.Month);
-          bw.Write(rule.DaylightTransitionStart.Week);
-          bw.Write(rule.DaylightTransitionStart.Day);
-          bw.Write((int)rule.DaylightTransitionStart.DayOfWeek);
-          bw.Write(rule.DaylightTransitionStart.TimeOfDay.Ticks);
-
-          bw.Write(rule.DaylightTransitionEnd.IsFixedDateRule);
-          bw.Write(rule.DaylightTransitionEnd.Month);
-          bw.Write(rule.DaylightTransitionEnd.Week);
-          bw.Write(rule.DaylightTransitionEnd.Day);
-          bw.Write((int)rule.DaylightTransitionEnd.DayOfWeek);
-          bw.Write(rule.DaylightTransitionEnd.TimeOfDay.Ticks);
+      using (var bw = new System.IO.BinaryWriter(File.Open(file_path, FileMode.Create), Encoding.UTF8)) {
+        bw.Write(new byte[] { (byte)'X', (byte)'T', (byte)'D', 0, (byte)'N', (byte)'U', (byte)'M', (byte)'F' });
+        bw.Write(version);
+        var cultures = CultureInfo.GetCultures(CultureTypes.AllCultures);
+        bw.Write(cultures.Count(c => TryCreateRegion(c.Name, out _)));
+        foreach (var cultureInfo in cultures.OrderBy(culture => culture.Name)) {
+          if (TryCreateRegion(cultureInfo.Name, out var region)) {
+            bw.Write(cultureInfo.Name.ToLower());
+            bw.Write(region.CurrencyEnglishName);
+            bw.Write(region.CurrencyNativeName);
+            bw.Write(region.CurrencySymbol);
+            bw.Write(region.DisplayName);
+            bw.Write(region.EnglishName);
+            bw.Write(region.GeoId);
+            bw.Write(region.IsMetric);
+            bw.Write(region.ISOCurrencySymbol);
+            bw.Write(region.Name);
+            bw.Write(region.NativeName);
+            bw.Write(region.ThreeLetterISORegionName);
+            bw.Write(region.ThreeLetterWindowsRegionName);
+            bw.Write(region.TwoLetterISORegionName);
+          }
         }
       }
-      bw.Close();
       return true;
     } catch (Exception e) {
       Trace.WriteLine("ERROR: " + e.Message);
@@ -187,11 +178,65 @@ class xtd_data_generator {
     }
   }
 
-  static bool IsUsingNls(){
+  static bool GenerateTimeZones(string file_path, int version) {
+    try {
+      using (var bw = new System.IO.BinaryWriter(File.Open(file_path, FileMode.Create), Encoding.UTF8)) {
+        bw.Write(new byte[] { (byte)'X', (byte)'T', (byte)'D', 0, (byte)'T', (byte)'Z', (byte)'O', (byte)'N' });
+        bw.Write(version);
+        var timeZones = TimeZoneInfo.GetSystemTimeZones();
+        bw.Write(timeZones.Count);
+        foreach (var tz in timeZones.OrderBy(tz => tz.Id)) {
+          bw.Write(tz.Id);
+          bw.Write(tz.DisplayName);
+          bw.Write(tz.StandardName);
+          bw.Write(tz.DaylightName);
+          bw.Write(tz.BaseUtcOffset.TotalMinutes);
+
+          var adjustmentRules = tz.GetAdjustmentRules();
+          bw.Write(adjustmentRules.Length);
+          foreach (var rule in adjustmentRules) {
+            bw.Write(rule.DateStart.ToBinary());
+            bw.Write(rule.DateEnd.ToBinary());
+            bw.Write(rule.DaylightDelta.TotalMinutes);
+
+            bw.Write(rule.DaylightTransitionStart.IsFixedDateRule);
+            bw.Write(rule.DaylightTransitionStart.Month);
+            bw.Write(rule.DaylightTransitionStart.Week);
+            bw.Write(rule.DaylightTransitionStart.Day);
+            bw.Write((int)rule.DaylightTransitionStart.DayOfWeek);
+            bw.Write(rule.DaylightTransitionStart.TimeOfDay.Ticks);
+
+            bw.Write(rule.DaylightTransitionEnd.IsFixedDateRule);
+            bw.Write(rule.DaylightTransitionEnd.Month);
+            bw.Write(rule.DaylightTransitionEnd.Week);
+            bw.Write(rule.DaylightTransitionEnd.Day);
+            bw.Write((int)rule.DaylightTransitionEnd.DayOfWeek);
+            bw.Write(rule.DaylightTransitionEnd.TimeOfDay.Ticks);
+          }
+        }
+      }
+      return true;
+    } catch (Exception e) {
+      Trace.WriteLine("ERROR: " + e.Message);
+      Environment.ExitCode = 5;
+      return false;
+    }
+  }
+
+  static bool IsUsingNls() {
     var property = typeof(CultureInfo).Assembly.GetType("System.Globalization.GlobalizationMode")?.GetProperty("UseNls", BindingFlags.Static | BindingFlags.NonPublic);
     return property != null && (bool)property.GetValue(null)!;
   }
   
+  static bool TryCreateRegion(string name, out RegionInfo region) {
+  try {
+    region = new RegionInfo(name);
+    return true;
+  } catch {
+    region = null!;
+    return false;
+  }
+}
   static void Write(BinaryWriter bw, int[] values) {
     bw.Write(values.Length);
     foreach (var value in values)
