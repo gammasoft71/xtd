09bf148e183b3fd4c8203107c1d191f15459cded
Gammasoft <gammasoft71@gmail.com>
2025-11-01 23:50:09 +0100

    Add culture_info_number_formats_tests

---
+++ tests/xtd.core.unit_tests/src/xtd/globalization/tests/culture_info_number_formats_tests.cpp
@@ -0,0 +1,13 @@
+#include <xtd/globalization/culture_info>
+#include <xtd/tunit/assert>
+#include <xtd/tunit/collection_assert>
+#include <xtd/tunit/test_class_attribute>
+#include <xtd/tunit/test_method_attribute>
+
+using namespace xtd::globalization;
+using namespace xtd::tunit;
+
+namespace xtd::globalization::tests {
+  class test_class_(culture_info_number_formats_tests) {
+  };
+}
