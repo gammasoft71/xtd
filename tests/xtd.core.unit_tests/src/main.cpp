ff53850d371ea440123c3f465620bae5173abf9a
Gammasoft <gammasoft71@gmail.com>
2025-11-01 23:50:33 +0100

    Update main.cpp

--- tests/xtd.core.unit_tests/src/main.cpp
+++ tests/xtd.core.unit_tests/src/main.cpp
@@ -1,13 +1,15 @@
 #include <xtd/tunit/console_unit_test>
 #include <xtd/startup>
 
+struct __xtd_initialize_all_cultures__ {
+  __xtd_initialize_all_cultures__() {culture_info::initialize_all_cultures();}
+} __initialize_all_cultures__;
+
 namespace xtd::core {
   class unit_tests {
   public:
-    static auto main() -> int {
-      #if defined(__XTD_BUILD_WITH_CONTINUOUS_INTEGRATION_SYSTEM__)
-      xtd::tunit::settings::default_settings().brief(true);
-      #endif
+    static auto main() {
+      xtd::tunit::settings::default_settings().brief(environment::get_environment_variable("CI") == "true");
       return xtd::tunit::console_unit_test {}.run();
     }
   };
