081b8106a162ef30b05f27c6d11a121b65a3c1e0
Gammasoft <gammasoft71@gmail.com>
2025-11-02 00:55:59 +0100

    Update main.cpp

--- tests/xtd.forms.unit_tests/src/main.cpp
+++ tests/xtd.forms.unit_tests/src/main.cpp
@@ -5,10 +5,10 @@ namespace xtd::forms {
   class unit_tests {
   public:
     static auto main() {
-      xtd::tunit::settings::default_settings().brief(environment::get_environment_variable("CI") == "true");
-      return xtd::tunit::console_unit_test {}.run();
+      settings::default_settings().brief(environment::get_environment_variable("CI") == "true");
+      return tunit::console_unit_test {}.run();
     }
   };
 }
 
-startup_(xtd::forms::unit_tests::main);
+startup_(unit_tests::main);
