e15bac23cca3d212d6200f832cc286284ed9bf1d
Gammasoft <gammasoft71@gmail.com>
2025-11-06 10:14:00 +0100

    review example

--- examples/xtd.core.examples/date_time/date_time_today/src/date_time_today.cpp
+++ examples/xtd.core.examples/date_time/date_time_today/src/date_time_today.cpp
@@ -3,11 +3,15 @@
 class program {
 public:
   static auto main() {
-    auto dates = array {date_time::now(), date_time {2013, 9, 14, 9, 28, 0}, date_time {2011, 5, 28, 10, 35, 0}, date_time {1979, 12, 25, 14, 30, 0}};
-    for (auto date : dates) {
-      console::write_line("Day: {0:d} Time: {1:g}", date.date(), date.time_of_day());
-      console::write_line("Day: {0:d} Time: {0:t}\n", date);
-    }
+    // Get the current date.
+    auto this_day = date_time::today();
+    // Display the date in the default (general) format.
+    console::write_line(this_day.to_string());
+    console::write_line();
+    // Display the date in a variety of formats.
+    console::write_line(this_day.to_string("d"));
+    console::write_line(this_day.to_string("D"));
+    console::write_line(this_day.to_string("g"));
   }
 };
 
@@ -15,14 +19,8 @@ startup_(program::main);
 
 // This code can produce the following output :
 //
-// Day: 01/12/2022 Time: 12:51:49
-// Day: 01/12/2022 Time: 12:51:49
+// 11/5/2025 0:00:00 AM
 //
-// Day: 09/14/2013 Time: 9:28:00
-// Day: 09/14/2013 Time: 09:28:00
-//
-// Day: 05/28/2011 Time: 10:35:00
-// Day: 05/28/2011 Time: 10:35:00
-//
-// Day: 12/25/1979 Time: 14:30:00
-// Day: 12/25/1979 Time: 14:30:00
+// 11/5/2025
+// Wednesday, November 5, 2025
+// 11/5/2025 0:00 AM
