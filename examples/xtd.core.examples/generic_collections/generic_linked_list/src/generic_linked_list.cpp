04c91ee5a560586d9a565a7d043c749173111346
Gammasoft <gammasoft71@gmail.com>
2025-11-09 20:09:12 +0100

    Review exemple

--- examples/xtd.core.examples/generic_collections/generic_linked_list/src/generic_linked_list.cpp
+++ examples/xtd.core.examples/generic_collections/generic_linked_list/src/generic_linked_list.cpp
@@ -6,8 +6,7 @@ class example {
 public:
   static auto main() -> void {
     // Create the link list.
-    auto words = array<string> { "the", "fox", "jumps", "over", "the", "dog" };
-    auto sentence = linked_list<string>(words);
+    auto sentence = linked_list<string> { "the", "fox", "jumps", "over", "the", "dog" };
     display(sentence, "The linked list values:");
     
     // Add the word 'today' to the beginning of the linked list.
@@ -15,9 +14,9 @@ public:
     display(sentence, "Test 1: Add 'today' to beginning of the list:");
     
     // Move the first node to be the last node.
-    auto mark1 = sentence.first()->value();
-    sentence.remove_first();
-    sentence.add_last(mark1);
+    auto node = *sentence.first();
+    sentence.remove(node);
+    sentence.add_last(node);
     display(sentence, "Test 2: Move first node to be last node:");
     
     // Change the last node to 'yesterday'.
@@ -26,83 +25,79 @@ public:
     display(sentence, "Test 3: Change the last node to 'yesterday':");
     
     // Move the last node to be the first node.
-    mark1 = sentence.last()->value();
-    sentence.remove_last();
-    sentence.add_first(mark1);
+    node = *sentence.last();
+    sentence.remove(node);
+    sentence.add_first(node);
     display(sentence, "Test 4: Move last node to be first node:");
     
     // Indicate the last occurence of 'the'.
     sentence.remove_first();
-    auto current = sentence.find_last("the");
-    indicate_node(*current, "Test 5: Indicate last occurence of 'the':");
+    node = *sentence.find_last("the");
+    indicate_node(node, "Test 5: Indicate last occurence of 'the':");
     
-    // Add 'lazy' and 'old' after 'the' (the linked_list_node named current).
-    sentence.add_after(*current, "old");
-    sentence.add_after(*current, "lazy");
-    indicate_node(*current, "Test 6: Add 'lazy' and 'old' after 'the':");
+    // Add 'lazy' and 'old' after 'the' (the linked_list_node named node).
+    sentence.add_after(node, "old");
+    sentence.add_after(*sentence.find_last("the"), "lazy");
+    indicate_node(*sentence.find_last("the"), "Test 6: Add 'lazy' and 'old' after 'the':");
     
     // Indicate 'fox' node.
-    current = sentence.find("fox");
-    indicate_node(*current, "Test 7: Indicate the 'fox' node:");
+    node = *sentence.find_last("fox");
+    indicate_node(node, "Test 7: Indicate the 'fox' node:");
     
     // Add 'quick' and 'brown' before 'fox':
-    sentence.add_before(*current, "quick");
-    sentence.add_before(*current, "brown");
-    indicate_node(*current, "Test 8: Add 'quick' and 'brown' before 'fox':");
+    sentence.add_before(*sentence.find("fox"), "quick");
+    sentence.add_before(*sentence.find("fox"), "brown");
+    indicate_node(*sentence.find("fox"), "Test 8: Add 'quick' and 'brown' before 'fox':");
     
-    // Keep a reference to the current node, 'fox',
-    // and to the previous node in the list. Indicate the 'dog' node.
-    mark1 = current->value();
-    auto mark2 = current->previous();
-    current = sentence.find("dog");
-    indicate_node(*current, "Test 9: Indicate the 'dog' node:");
+    // Keep a reference to the current node, 'fox', and to the previous node in the list. Indicate the 'dog' node.
+    node = *sentence.find_last("dog")->previous();
+    indicate_node(*sentence.find("dog"), "Test 9: Indicate the 'dog' node:");
     
-    // The AddBefore method throws an InvalidOperationException
-    // if you try to add a node that already belongs to a list.
+    // The add_before method throws an invalid_operation_exception if you try to add a node that already belongs to a list.
     console::write_line("Test 10: Throw exception by adding node (fox) already in the list:");
     try {
-      sentence.add_before(*current, mark1);
+      sentence.add_before(*sentence.find("fox"), node);
     } catch (const invalid_operation_exception& ex) {
-      console::write_line("Exception message: {0}", ex.message());
+      console::write_line("Exception message: {}", ex.message());
     }
     console::write_line();
     
-    // Remove the node referred to by mark1, and then add it
-    // before the node referred to by current.
+    // Remove the 'fox' node, and then add it before the 'dog' node.
     // Indicate the node referred to by current.
-    sentence.remove(mark1);
-    sentence.add_before(*current, mark1);
-    indicate_node(*current, "Test 11: Move a referenced node (fox) before the current node (dog):");
+    node = *sentence.find("fox");
+    sentence.remove(node);
+    sentence.add_before(*sentence.find("dog"), node);
+    node = *sentence.find("dog");
+    indicate_node(node, "Test 11: Move a referenced node (fox) before the current node (dog):");
     
-    // Remove the node referred to by current.
-    sentence.remove(*current);
-    indicate_node(*current, "Test 12: Remove current node (dog) and attempt to indicate it:");
+    // Remove the node referred to by node.
+    sentence.remove(node);
+    node = sentence.find("dog") ? *sentence.find("dog") : linked_list_node<string>("dog");
+    indicate_node(node, "Test 12: Remove current node (dog) and attempt to indicate it:");
     
-    // Add the node after the node referred to by mark2.
-    sentence.add_after(*mark2, current->value());
-    indicate_node(*current, "Test 13: Add node removed in test 12 after a referenced node (brown):");
+    // Add the node after the 'brown' node.
+    sentence.add_after(*sentence.find("brown"), node);
+    indicate_node(node, "Test 13: Add node removed in test 12 after a referenced node (brown):");
     
-    // The Remove method finds and removes the
-    // first node that that has the specified value.
+    // The Remove method finds and removes the first node that that has the specified value.
     sentence.remove("old");
     display(sentence, "Test 14: Remove node that has the value 'old':");
     
-    // When the linked list is cast to ICollection(Of String),
-    // the Add method adds a node to the end of the list.
+    // When the linked list is cast to icollection(Of string), the add method adds a node to the end of the list.
     sentence.remove_last();
     auto& icoll = as<icollection<string>>(sentence);
     icoll.add("rhinoceros");
-    display(sentence, "Test 15: Remove last node, cast to ICollection, and add 'rhinoceros':");
+    display(sentence, "Test 15: Remove last node, cast to icollection, and add 'rhinoceros':");
     
     console::write_line("Test 16: Copy the list to an array:");
-    // Create an array with the same number of
-    // elements as the linked list.
+    // Create an array with the same number of elements as the linked list.
     auto sarray = array<string>(sentence.count());
     sentence.copy_to(sarray, 0);
     
     for (auto s : sarray)
       console::write_line(s);
     
+    console::write_line();
     console::write_line("Test 17: linked list Contains 'jumps' = {0}", sentence.contains("jumps"));
     
     // Release all the nodes.
@@ -138,7 +133,7 @@ public:
     
     auto node_next = node.next();
     while (node_next) {
-      result.append(" " + node.value());
+      result.append(" " + node_next->value());
       node_next = node_next->next();
     }
     
@@ -151,3 +146,66 @@ startup_(example::main);
 
 // This code produces the following output :
 //
+// The linked list values:
+// the fox jumps over the dog
+//
+// Test 1: Add 'today' to beginning of the list:
+// today the fox jumps over the dog
+//
+// Test 2: Move first node to be last node:
+// the fox jumps over the dog today
+//
+// Test 3: Change the last node to 'yesterday':
+// the fox jumps over the dog yesterday
+//
+// Test 4: Move last node to be first node:
+// yesterday the fox jumps over the dog
+//
+// Test 5: Indicate last occurence of 'the':
+// the fox jumps over (the) dog
+//
+// Test 6: Add 'lazy' and 'old' after 'the':
+// the fox jumps over (the) lazy old dog
+//
+// Test 7: Indicate the 'fox' node:
+// the (fox) jumps over the lazy old dog
+//
+// Test 8: Add 'quick' and 'brown' before 'fox':
+// the quick brown (fox) jumps over the lazy old dog
+//
+// Test 9: Indicate the 'dog' node:
+// the quick brown fox jumps over the lazy old (dog)
+//
+// Test 10: Throw exception by adding node (fox) already in the list:
+// Exception message: The linked_list node belongs a linked_list.
+//
+// Test 11: Move a referenced node (fox) before the current node (dog):
+// the quick brown jumps over the lazy old fox (dog)
+//
+// Test 12: Remove current node (dog) and attempt to indicate it:
+// Node 'dog' is not in the list.
+//
+// Test 13: Add node removed in test 12 after a referenced node (brown):
+// the quick brown (dog) jumps over the lazy old fox
+//
+// Test 14: Remove node that has the value 'old':
+// the quick brown dog jumps over the lazy fox
+//
+// Test 15: Remove last node, cast to icollection, and add 'rhinoceros':
+// the quick brown dog jumps over the lazy rhinoceros
+//
+// Test 16: Copy the list to an array:
+// the
+// quick
+// brown
+// dog
+// jumps
+// over
+// the
+// lazy
+// rhinoceros
+//
+// Test 17: linked list Contains 'jumps' = true
+//
+// Test 18: Cleared linked list Contains 'jumps' = false
+
