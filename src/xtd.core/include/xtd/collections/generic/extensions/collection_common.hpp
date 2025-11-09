110308f31ce27c3dbc8dab69f14ab5282d5d4e08
Gammasoft <gammasoft71@gmail.com>
2025-11-09 11:33:24 +0100

    add self() properties

--- src/xtd.core/include/xtd/collections/generic/extensions/collection_common.hpp
+++ src/xtd.core/include/xtd/collections/generic/extensions/collection_common.hpp
@@ -36,11 +36,11 @@ namespace xtd {
           /// @{
           /// @brief Checks whether the container is empty.
           /// @return `true` if container is empty; otherwise `false`.
-          virtual bool empty() const noexcept {return static_cast<const collection_t&>(self_).count() == 0;}
+          virtual bool empty() const noexcept {return self().count() == 0;}
           
           /// @brief Gets the number of elements contained in the xtd::collections::generic::icollection <type_t>.
           /// @return The number of elements contained in the xtd::collections::generic::icollection <type_t>.
-          virtual xtd::size size() const noexcept {return static_cast<const collection_t&>(self_).count();}
+          virtual xtd::size size() const noexcept {return self().count();}
           /// @}
           
           /// @name Public Operators
@@ -50,18 +50,16 @@ namespace xtd {
           /// @param item The object to add to the xtd::collections::generic::icollection <type_t>.
           /// @exception xtd::not_supported_exception The xtd::collections::generic::icollection <type_t> is read-only.
           virtual collection_t& operator <<(const type_t& item) {
-            auto& self = static_cast<collection_t&>(self_);
-            self.add(item);
-            return self;
+            self().add(item);
+            return self();
           }
           
           /// @brief The shift left operator adds an item to the xtd::collections::generic::icollection <type_t>.
           /// @param item The object to add to the xtd::collections::generic::icollection <type_t>.
           /// @exception xtd::not_supported_exception The xtd::collections::generic::icollection <type_t> is read-only.
           virtual collection_t& operator <<(type_t&& item) {
-            auto& self = static_cast<collection_t&>(self_);
-            self.add(std::move(item));
-            return self;
+            self().add(std::move(item));
+            return self();
           }
           
           /// @brief The shift right operator removes the first occurrence of a specific object from the xtd::collections::generic::icollection <type_t>.
@@ -70,13 +68,15 @@ namespace xtd {
           /// @remarks Implementations can vary in how they determine equality of objects; for example, xtd::collections::generic::list <type_t> uses xtd::collections::generic::compoarer <type_t>::default_comparer, whereas, xtd::collections::generic::dictionary <key_t, value_t> allows the user to specify the xtd::collections::generic::icompoarer <type_t> implementation to use for comparing keys.
           /// @remarks In collections of contiguous elements, such as lists, the elements that follow the removed element move up to occupy the vacated spot. If the collection is indexed, the indexes of the elements that are moved are also updated. This behavior does not apply to collections where elements are conceptually grouped into buckets, such as a hash table.
           virtual collection_t& operator >>(const type_t& item) {
-            auto& self = static_cast<collection_t&>(self_);
-            self.remove(item);
-            return self;
+            self().remove(item);
+            return self();
           }
           /// @}
           
         private:
+          const collection_t& self() const noexcept {return static_cast<const collection_t&>(self_);}
+          collection_t& self() noexcept {return static_cast<collection_t&>(self_);}
+          
           friend collection_t;
           collection_common() = default;
         };
