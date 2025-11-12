b0f558157a971e513df6544fcd8e16634786812e
Gammasoft <gammasoft71@gmail.com>
2025-11-12 21:19:33 +0100

    using indexer operator

--- src/xtd.drawing/src/xtd/drawing/color.cpp
+++ src/xtd.drawing/src/xtd/drawing/color.cpp
@@ -657,7 +657,7 @@ color color::parse(const string& color) noexcept {
   try {
     auto argb = color.replace("color [a=", string::empty_string).replace(" r=", string::empty_string).replace(" g=", string::empty_string).replace("b=", string::empty_string).replace("]", string::empty_string).split(',');
     if (argb.size() == 1) return color::from_argb(xtd::parse<uint32>(argb.at(0), xtd::number_styles::hex_number));
-    return color::from_argb(string::parse<xtd::byte>(argb.at(0)), string::parse<xtd::byte>(argb.at(1)), string::parse<xtd::byte>(argb.at(2)), string::parse<xtd::byte>(argb.at(3)));
+    return color::from_argb(string::parse<xtd::byte>(argb[0]), string::parse<xtd::byte>(argb[1]), string::parse<xtd::byte>(argb[2]), string::parse<xtd::byte>(argb[3]));
   } catch (...) {
     return color::from_name(color.replace("]", string::empty_string).replace("color [", string::empty_string));
   }
