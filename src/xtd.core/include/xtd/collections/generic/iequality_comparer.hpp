a8f2b849a8f77dfe44a376551d5423db68d8b6ab
Gammasoft <gammasoft71@gmail.com>
2025-11-20 21:56:24 +0100

    using methods with auto

--- src/xtd.core/include/xtd/collections/generic/iequality_comparer.hpp
+++ src/xtd.core/include/xtd/collections/generic/iequality_comparer.hpp
@@ -44,7 +44,7 @@ namespace xtd {
         /// @par Notes to Implementers
         /// Implementations are required to ensure that if the xtd::collections::generic::iequality_comparer::equals method returns `true` for two objects `x` and `y`, then the value returned by the xtd::collections::generic::iequality_comparer::get_hash_code method for `x` must equal the value returned for `y`.
         /// The xtd::collections::generic::iequality_comparer::equals method is reflexive, symmetric, and transitive. That is, it returns `true` if used to compare an object with itself; `true` for two objects `x` and `y` if it is `true` for `y` and `x;` and `true` for two objects `x` and `z` if it is `true` for `x` and `y` and also `true` for `y` and `z`.
-        virtual bool equals(const type_t& x, const type_t& y) const noexcept = 0;
+        virtual auto equals(const type_t& x, const type_t& y) const noexcept -> bool = 0;
         
         /// @brief Returns a hash code for the specified object.
         /// @param obj The xtd::object for which a hash code is to be returned.
@@ -52,7 +52,7 @@ namespace xtd {
         /// @remarks Implement this method to provide a customized hash code for type `type_t`, corresponding to the customized equality comparison provided by the xtd::collections::generic::iequality_comparer::equals method.
         /// @par Notes to Implementers
         /// Implementations are required to ensure that if the xtd::collections::generic::iequality_comparer::equals method returns `true` for two objects `x` and `y`, then the value returned by the xtd::collections::generic::iequality_comparer::get_hash_code method for `x` must equal the value returned for `y`.
-        virtual xtd::size get_hash_code(const type_t& obj) const noexcept = 0;
+        virtual auto get_hash_code(const type_t& obj) const noexcept -> xtd::size = 0;
         /// @}
       };
     }
