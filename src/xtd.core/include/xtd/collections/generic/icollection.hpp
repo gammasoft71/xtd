3f3d61b4ff4e2cb65553270ab9459d8e078a68be
Gammasoft <gammasoft71@gmail.com>
2025-11-20 21:31:18 +0100

    using methods with auto

--- src/xtd.core/include/xtd/collections/generic/icollection.hpp
+++ src/xtd.core/include/xtd/collections/generic/icollection.hpp
@@ -56,12 +56,12 @@ namespace xtd {
         /// @{
         /// @brief Gets the number of elements contained in the xtd::collections::generic::icollection <type_t>.
         /// @return The number of elements contained in the xtd::collections::generic::icollection <type_t>.
-        virtual xtd::size count() const noexcept = 0;
+        virtual auto count() const noexcept -> xtd::size = 0;
         
         /// @brief Gets a value indicating whether the xtd::collections::generic::icollection <type_t> is read-only.
         /// @return `true` if the xtd::collections::generic::icollection <type_t> is read-only; otherwise, `false`.
         /// @remarks A collection that is read-only does not allow the addition or removal of elements after the collection is created. Note that read-only in this context does not indicate whether individual elements of the collection can be modified, since the xtd::collections::generic::icollection <type_t> interface only supports addition and removal operations. For example, the xtd::collections::generic::icollection::is_read_only property of an array that is cast or converted to an xtd::collections::generic::icollection <type_t> object returns `true`, even though individual array elements can be modified.
-        virtual bool is_read_only() const noexcept = 0;
+        virtual auto is_read_only() const noexcept -> bool = 0;
         
         /// @brief Gets a value indicating whether access to the xtd::collections::generic::icollection <type_t> is synchronized (thread safe).
         /// @return `true` if access to the xtd::collections::generic::icollection <type_t> is synchronized (thread safe); otherwise, `false`.
@@ -77,7 +77,7 @@ namespace xtd {
         ///   }
         /// }
         /// @endcode
-        virtual bool is_synchronized() const noexcept = 0;
+        virtual auto is_synchronized() const noexcept -> bool = 0;
         
         /// @brief Gets an object that can be used to synchronize access to the the xtd::collections::generic::icollection <type_t>.
         /// @return An object that can be used to synchronize access to the the xtd::collections::generic::icollection <type_t>.
@@ -100,7 +100,7 @@ namespace xtd {
         ///   }
         /// }
         /// @endcode
-        virtual const xtd::object& sync_root() const noexcept = 0;
+        virtual auto sync_root() const noexcept -> const xtd::object& = 0;
         /// @}
         
         /// @name Public Methods
@@ -109,24 +109,24 @@ namespace xtd {
         /// @brief Adds an item to the xtd::collections::generic::icollection <type_t>.
         /// @param item The object to add to the xtd::collections::generic::icollection <type_t>.
         /// @exception xtd::not_supported_exception The xtd::collections::generic::icollection <type_t> is read-only.
-        virtual void add(const type_t& item) = 0;
+        virtual auto add(const type_t& item) -> void = 0;
         
         /// @brief Removes all items from the xtd::collections::generic::icollection <type_t>.
         /// @exception xtd::not_supported_exception The xtd::collections::generic::icollection <type_t> is read-only.
         /// @remarks xtd::collections::generic::icollection::count must be set to 0, and references to other objects from elements of the collection must be released.
-        virtual void clear() = 0;
+        virtual auto clear() -> void = 0;
         
         /// @brief Determines whether the xtd::collections::generic::icollection <type_t> contains a specific value.
         /// @param item The object to locate in the xtd::collections::generic::icollection <type_t>.
         /// @return `true` if item is found in the xtd::collections::generic::icollection <type_t>; otherwise, `false`.
         /// @remarks Implementations can vary in how they determine equality of objects; for example, xtd::collections::generic::list <type_t> uses xtd::collections::generic::compoarer <type_t>::default_comparer, whereas xtd::collections::generic::dictionary <key_t, value_t> allows the user to specify the xtd::collections::generic::icompoarer <type_t> implementation to use for comparing keys.
-        virtual bool contains(const type_t& item) const noexcept = 0;
+        virtual auto contains(const type_t& item) const noexcept -> bool = 0;
         
         /// @brief Copies the elements of the xtd::collections::generic::icollection <type_t> to an xtd::array, starting at a particular xtd::array index.
         /// @param array The one-dimensional xtd::array that is the destination of the elements copied from xtd::collections::generic::icollection <type_t>. The xtd::array must have zero-based indexing.
         /// @param array_index The zero-based index in `array` at which copying begins.
         /// @exception xtd::argument_exception The number of elements in the source xtd::collections::generic::icollection <type_t> is greater than the available space from `array_index` to the end of the destination `array`.
-        virtual void copy_to(xtd::array<type_t>& array, xtd::size array_index) const = 0;
+        virtual auto copy_to(xtd::array<type_t>& array, xtd::size array_index) const -> void = 0;
         
         /// @brief Removes the first occurrence of a specific object from the xtd::collections::generic::icollection <type_t>.
         /// @param item The object to remove from the xtd::collections::generic::icollection <type_t>.
@@ -134,7 +134,7 @@ namespace xtd {
         /// @exception xtd::not_supported_exception The xtd::collections::generic::icollection <type_t> is read-only.
         /// @remarks Implementations can vary in how they determine equality of objects; for example, xtd::collections::generic::list <type_t> uses xtd::collections::generic::compoarer <type_t>::default_comparer, whereas, xtd::collections::generic::dictionary <key_t, value_t> allows the user to specify the xtd::collections::generic::icompoarer <type_t> implementation to use for comparing keys.
         /// @remarks In collections of contiguous elements, such as lists, the elements that follow the removed element move up to occupy the vacated spot. If the collection is indexed, the indexes of the elements that are moved are also updated. This behavior does not apply to collections where elements are conceptually grouped into buckets, such as a hash table.
-        virtual bool remove(const type_t& item) = 0;
+        virtual auto remove(const type_t& item) -> bool = 0;
         /// @}
       };
     }
