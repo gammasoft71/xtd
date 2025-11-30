0c511b989c648db34b68e3901fb8a390c335865c
Gammasoft <gammasoft71@gmail.com>
2025-11-30 22:37:44 +0100

    using length

--- src/xtd.core/src/xtd/io/memory_stream.cpp
+++ src/xtd.core/src/xtd/io/memory_stream.cpp
@@ -62,7 +62,7 @@ size memory_stream::read(array<byte>& buffer, size offset, size count) {
   if (!can_read()) throw_helper::throws(exception_case::not_supported);
   
   if (count == 0_z) return 0_z;
-  if (offset >= buffer.size() || offset + count > buffer.size()) throw_helper::throws(exception_case::argument_out_of_range);
+  if (offset >= buffer.length() || offset + count > buffer.length()) throw_helper::throws(exception_case::argument_out_of_range);
   if (data_->static_buffer && (data_->position + count > capacity())) throw_helper::throws(exception_case::not_supported);
   
   auto read_count = math::min(length() - data_->position, count);
@@ -90,7 +90,7 @@ void memory_stream::write(const array<byte>& buffer, size offset, size count) {
   if (!can_write()) throw_helper::throws(exception_case::not_supported);
   
   if (count == 0_z) return;
-  if (offset >= buffer.size() || offset + count > buffer.size()) throw_helper::throws(exception_case::argument_out_of_range);
+  if (offset >= buffer.length() || offset + count > buffer.length()) throw_helper::throws(exception_case::argument_out_of_range);
   if (data_->static_buffer && data_->position + count > capacity()) throw_helper::throws(exception_case::not_supported);
   
   if (length() < position()) {
