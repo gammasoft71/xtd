e51ec2d70b5573648a2e65cfefcde6e137ef7152
Gammasoft <gammasoft71@gmail.com>
2025-12-02 16:04:03 +0100

    Update hello_world_png.cpp

--- examples/xtd.drawing.examples/hello_worlds/hello_world_png/src/hello_world_png.cpp
+++ examples/xtd.drawing.examples/hello_worlds/hello_world_png/src/hello_world_png.cpp
@@ -9,5 +9,5 @@ auto main() -> int {
   graphics.draw_string("Hello World!", {system_fonts::default_font(), 32, font_style::bold | font_style::italic}, brushes::spring_green(), rectangle::inflate({{0, 0}, hello_world_bitmap.size()}, {-2, -2}), string_format {}.alignment(string_alignment::center).line_alignment(string_alignment::center));
   auto hello_world_path = path::combine(path::get_temp_path(), "hello_world.png");
   hello_world_bitmap.save(hello_world_path);
-  diagnostics::process::start(diagnostics::process_start_info {hello_world_path}.use_shell_execute(true));
+  diagnostics::process::start(diagnostics::process_start_info {hello_world_path}.use_shell_execute(true)).wait_for_exit();
 }
