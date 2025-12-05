4c0e6894c07e53e2d4f95f671f7aed5925215a45
Gammasoft <gammasoft71@gmail.com>
2025-12-05 10:46:48 +0100

    internal refactoring base_type move from xtd::collections::genric::linked_list to xtd::collections::generic::helpers::raw_queue.

--- src/xtd.core/include/xtd/collections/generic/queue.hpp
+++ src/xtd.core/include/xtd/collections/generic/queue.hpp
@@ -3,7 +3,9 @@
 /// @copyright Copyright (c) 2025 Gammasoft. All rights reserved.
 #pragma once
 #include "helpers/raw_queue.hpp"
-#include "linked_list.hpp"
+#include "icollection.hpp"
+#include "../../size.hpp"
+#include "../../string.hpp"
 
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
@@ -39,7 +41,7 @@ namespace xtd {
       /// An array twice the size of the queue is created, and the xtd::collections::generic::queue::copy_to method is used to copy the array elements beginning at the middle of the array. The xtd::collections::generic::queue <type_t> constructor is used again to create a second copy of the queue containing three null elements at the beginning.
       /// The xtd::collections::generic::queue::contains method is used to show that the string "four" is in the first copy of the queue, after which the Clear method clears the copy and the xtd::collections::generic::queue::count property shows that the queue is empty.
       /// @include generic_queue.cpp
-      template<class type_t, class allocator_t = xtd::collections::generic::helpers::allocator<type_t>>
+      template<class type_t>
       class queue : public xtd::object, public xtd::collections::generic::icollection<type_t> {
       public:
         /// @name Public Aliases
@@ -48,7 +50,7 @@ namespace xtd {
         /// @brief Represents the list value type.
         using value_type = typename icollection<type_t>::value_type;
         /// @brief Represents the list base type.
-        using base_type = xtd::collections::generic::linked_list<value_type, allocator_t>;
+        using base_type = xtd::collections::generic::helpers::raw_queue<value_type>;
         /// @brief Represents the list size type (usually xtd::size).
         using size_type = xtd::size;
         /// @brief Represents the reference of list value type.
@@ -75,7 +77,7 @@ namespace xtd {
         /// @param queue The std::queue <type_t> which elements will be moved from.
         queue(std::queue<type_t>&& queue) {
           while (queue.size()) {
-            data_->items.add_last(queue.front());
+            data_->items.push(queue.front());
             queue.pop();
           }
           ensure_capacity(count());
@@ -96,13 +98,14 @@ namespace xtd {
         /// @brief Initializes a new instance of the xtd::collections::generic::queue <type_t> class that contains elements copied from the specified collection and has sufficient capacity to accommodate the number of elements copied.
         /// @param collection The collection whose elements are copied to the new xtd::collections::generic::queue <type_t>.
         queue(const ienumerable<value_type>& collection) {
-          data_->items = base_type {collection};
+          for (auto item : collection)
+            data_->items.push(item);
           ensure_capacity(count());
         }
         /// @brief Initializes a new instance of the xtd::collections::generic::queue <type_t> class that is empty and has the specified initial capacity.
         /// @param capacity The initial number of elements that the xtd::collections::generic::queue <type_t> can contain.
         queue(size_type capacity) {
-          data_->capacity = capacity;
+          data_->items.reserve(capacity);
         }
         /// @brief Constructs the container with the contents of the specified initializer list, and allocator.
         /// @param items The initializer list to initialize the elements of the container with.
@@ -126,11 +129,11 @@ namespace xtd {
         /// @{
         /// @brief Gets the total numbers of elements the internal data structure can hold without resizing.
         /// @return The total numbers of elements the internal data structure can hold without resizing.
-        auto capacity() const noexcept -> size_type {return data_->capacity;}
+        auto capacity() const noexcept -> size_type {return data_->items.capacity();}
         /// @brief Gets the number of nodes actually contained in the xtd::collections::generic::queue <type_t>.
         /// @return The number of nodes actually contained in the xtd::collections::generic::queue <type_t>.
         /// @remarks Retrieving the value of this property is an O(1) operation.
-        [[nodiscard]] auto count() const noexcept -> size_type override {return data_->items.count();}
+        [[nodiscard]] auto count() const noexcept -> size_type override {return data_->items.size();}
         
         /// @brief Gets a std::queue<type_t>.
         /// @return A std::queue<type_t>.
@@ -146,13 +149,16 @@ namespace xtd {
         /// @remarks This method is an O(n) operation, where n is Count.
         auto clear() -> void override {
           data_->items.clear();
+          ++data_->version;
         }
         
         /// @brief Determines whether an element is in the xtd::collections::generic::queue <type_t>.
         /// @param item The object to locate in the xtd::collections::generic::queue <type_t>.
         /// @return `true` if `item` is found in the xtd::collections::generic::queue <type_t>; otherwise, `false`.
         auto contains(const_reference value) const noexcept -> bool override {
-          return data_->items.contains(value);
+          for (const auto& item : data_->items)
+            if (xtd::collections::generic::helpers::equator<type_t> {}(item, value)) return true;
+          return false;
         }
         
         /// @brief Copies the entire xtd::colllections::generic::linked_list <type_t> to a compatible one-dimensional array, starting at the specified index of the target array.
@@ -163,7 +169,9 @@ namespace xtd {
         /// @remarks The elements are copied to the xtd::array in the same order in which the enumerator iterates through the xtd::colllections::generic::linked_list <type_t>.
         /// @remarks This method is an O(n) operation, where n is xtd::colllections::generic::linked_list::count.
         auto copy_to(xtd::array<type_t>& array, size_type array_index) const -> void override {
-          data_->items.copy_to(array, array_index);
+          if (array_index + count() > array.length()) xtd::helpers::throw_helper::throws(xtd::helpers::exception_case::argument_out_of_range);
+          for (const auto& item : data_->items)
+            array[array_index++] = item;
         }
         
         /// @brief Removes and returns the object at the beginning of the xtd::collections::generic::queue <type_t>.
@@ -178,22 +186,59 @@ namespace xtd {
         /// @brief Adds an object to the end of the xtd::collections::generic::queue <type_t>.
         /// @param item The object to add to the xtd::collections::generic::queue <type_t>. The value can be null for reference types.
         auto enqueue(const_reference value) -> void {
-          data_->items.add_last(value);
+          data_->items.push(value);
           ensure_capacity(count());
+          ++data_->version;
+        }
+        
+        /// @brief Adds an object to the end of the xtd::collections::generic::queue <type_t>.
+        /// @param item The object to add to the xtd::collections::generic::queue <type_t>. The value can be null for reference types.
+        auto enqueue(value_type&& value) -> void {
+          data_->items.push(std::move(value));
+          ensure_capacity(count());
+          ++data_->version;
         }
         
         /// @brief Ensures that the capacity of this queue is at least the specified `capacity`. If the current capacity is less than `capacity`, it is increased to at least the specified `capacity`.
         /// @param capacity The minimum capacity to ensure.
         /// @return The new capacity of this queue.
         auto ensure_capacity(size_type capacity) -> size_type {
-          if (data_->capacity < capacity) data_->capacity = capacity;
-          return data_->capacity;
+          if (data_->items.capacity() < capacity) data_->items.reserve(capacity);
+          return data_->items.capacity();
         }
         
         /// @brief Returns an enumerator that iterates through the xtd::collections::generic::queue <type_t>.
         /// @return A xtd::collections::generic::.enumerator for the xtd::collections::generic::queue <type_t>.
         enumerator<value_type> get_enumerator() const noexcept override {
-          return data_->items.get_enumerator();
+          struct queue_enumerator : public ienumerator < value_type > {
+            explicit queue_enumerator(const queue & items, xtd::size version) : items_(items), version_(version) {}
+            
+            const value_type& current() const override {
+              if (iterator_ == items_.data_->items.cend()) xtd::helpers::throw_helper::throws(xtd::helpers::exception_case::invalid_operation);
+              if (version_ != items_.data_->version) xtd::helpers::throw_helper::throws(xtd::helpers::exception_case::invalid_operation, "Collection was modified; enumeration operation may not execute.");
+              return *iterator_;
+            }
+            
+            bool move_next() override {
+              if (version_ != items_.data_->version) xtd::helpers::throw_helper::throws(xtd::helpers::exception_case::invalid_operation, "Collection was modified; enumeration operation may not execute.");
+              if (index_++ && iterator_ != items_.data_->items.cend()) ++iterator_;
+              else iterator_ = items_.data_->items.cbegin();
+              return iterator_ != items_.data_->items.cend();
+            }
+            
+            void reset() override {
+              index_ = 0;
+              version_ = items_.data_->version;
+              iterator_ = items_.data_->items.cend();
+            }
+            
+          private:
+            size_type index_ = 0;
+            const queue& items_;
+            typename base_type::const_iterator iterator_ = items_.data_->items.cend();
+            size_type version_ = 0;
+          };
+          return {new_ptr < queue_enumerator > (self_, data_->version)};
         }
         
         /// @brief Returns the object at the beginning of the xtd::collections::generic::queue <type_t>
@@ -216,7 +261,7 @@ namespace xtd {
         
         /// @brief Returns a xtd::string that represents the current object.
         /// @return A string that represents the current object.
-        auto to_string() const noexcept -> string override {return data_->items.to_string();}
+        auto to_string() const noexcept -> string override {return xtd::string::format("[{}]", xtd::string::join(", ", self_));}
         
         /// @brief Sets the capacity to the actual number of elements in the xtd::collections::generic::queue <type_t>, if that number is less than 90 percent of current capacity.
         /// @remarks This method can be used to minimize a collection's memory overhead if no new elements will be added to the collection. The cost of reallocating and copying a large xtd::collections::generic::queue <type_t> can be considerable, however, so the xtd::collections::generic::queue::trim_excess method does nothing if the list is at more than 90 percent of capacity. This avoids incurring a large reallocation cost for a relatively small gain.
@@ -231,7 +276,8 @@ namespace xtd {
         /// @exception xtd::argument_out_of_range_exception Passed capacity is lower than entries count.
         auto trim_excess(size_type capacity) -> void {
           if (capacity < count()) xtd::helpers::throw_helper::throws(xtd::helpers::exception_case::argument_out_of_range);
-          data_->capacity = capacity;
+          if (capacity < data_->items.capacity()) data_->items.shrink_to_fit();
+          data_->items.reserve(capacity);
         }
         
         /// @brief Removes the object at the beginning of the xtd::collections::generic::queue <type_t>, and copies it to the result parameter.
@@ -239,7 +285,8 @@ namespace xtd {
         /// @return `true` if the object is successfully removed; `false` if the xtd::collections::generic::queue <type_t> is empty.
         [[nodiscard]] auto try_dequeue(value_type& result) noexcept -> bool {
           if (!try_peek(result)) return false;
-          data_->items.remove_first();
+          data_->items.pop();
+          ++data_->version;
           return true;
         }
         
@@ -247,7 +294,7 @@ namespace xtd {
         /// @param If present, the object at the beginning of the xtd::collections::generic::queue <type_t>; otherwise, the default value of `type_t`.
         /// @return `true` if there is an object at the beginning of the xtd::collections::generic::queue <type_t>; `false` if the xtd::collections::generic::queue <type_t> is empty.
         [[nodiscard]] auto try_peek(value_type& result) const noexcept -> bool {
-          result = count() ? data_->items.first()->value() : type_t {};
+          result = count() ? data_->items.front() : type_t {};
           return count();
         }
         /// @}
@@ -264,6 +311,7 @@ namespace xtd {
         /// @return This current instance.
         auto operator =(queue&& other) noexcept -> queue& {
           data_->items = std::move(other.data_->items);
+          ++data_->version;
           return self_;
         }
         /// @brief Replaces the contents with those identified by initializer list ilist.
@@ -271,6 +319,7 @@ namespace xtd {
         /// @return This current instance.
         auto operator =(const std::initializer_list<type_t>& items) -> queue& {
           data_->items = items;
+          ++data_->version;
           return self_;
         }
         
@@ -288,8 +337,8 @@ namespace xtd {
         
         struct queue_data {
           base_type items;
-          xtd::size capacity = 0;
           xtd::object sync_root;
+          xtd::size version = 0;
         };
         
         xtd::ptr<queue_data> data_ = xtd::new_ptr<queue_data>();
@@ -298,11 +347,11 @@ namespace xtd {
       /// @cond
       // Deduction guides for xtd::collections::generic::queue
       // {
-      template < class type_t, class allocator_t = xtd::collections::generic::helpers::allocator < type_t>>
-      queue(queue < type_t, allocator_t >&&) -> queue < type_t, allocator_t >;
+      template < class type_t>
+      queue(queue < type_t>&&) -> queue < type_t>;
       
-      template < class type_t, class allocator_t = xtd::collections::generic::helpers::allocator < type_t>>
-      queue(const queue < type_t, allocator_t >&) -> queue < type_t, allocator_t >;
+      template < class type_t>
+      queue(const queue < type_t>&) -> queue < type_t>;
       
       template < class type_t>
       queue(const ienumerable < type_t >&) -> queue < type_t >;
