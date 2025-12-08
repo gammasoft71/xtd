e173a17b793f0e315480e945e9799e4f4a683e6f
Gammasoft <gammasoft71@gmail.com>
2025-12-08 18:37:53 +0100

    using auto

--- examples/xtd.core.examples/any/any/src/any.cpp
+++ examples/xtd.core.examples/any/any/src/any.cpp
@@ -4,26 +4,26 @@ struct foo1 : public object, public icomparable<foo1>, public iequatable<foo1> {
   explicit foo1(int v) : value(v) {}
   int value = 0;
   
-  string to_string() const noexcept override {return string::format("{}", value);}
-  bool equals(const object& o) const noexcept override {return is<foo1>(o) && equals(static_cast<const foo1&>(o));}
-  bool equals(const foo1& f) const noexcept override {return value == f.value;}
-  int32 compare_to(const foo1& f) const noexcept override {return value < f.value ? -1 : value > f.value ? 1 : 0;}
+  auto to_string() const noexcept -> string override {return string::format("{}", value);}
+  auto equals(const object& o) const noexcept -> bool override {return is<foo1>(o) && equals(static_cast<const foo1&>(o));}
+  auto equals(const foo1& f) const noexcept -> bool override {return value == f.value;}
+  auto compare_to(const foo1& f) const noexcept -> int32 override {return value < f.value ? -1 : value > f.value ? 1 : 0;}
 };
 
 struct foo2 {
   int value = 0;
   
-  bool operator ==(const foo2& f) const noexcept {return value == f.value;}
-  bool operator <(const foo2& f) const noexcept {return value < f.value;}
-  bool operator >(const foo2& f) const noexcept {return value > f.value;}
+  auto operator ==(const foo2& f) const noexcept -> bool {return value == f.value;}
+  auto operator <(const foo2& f) const noexcept -> bool {return value < f.value;}
+  auto operator >(const foo2& f) const noexcept -> bool {return value > f.value;}
 };
 
 template<>
-string xtd::to_string(const foo2& value, const string& fmt, const std::locale& loc) {return xtd::int32_object {value.value}.to_string(fmt, loc);}
+auto xtd::to_string(const foo2& value, const string& fmt, const std::locale& loc) -> string {return xtd::int32_object {value.value}.to_string(fmt, loc);}
 
 template<>
 struct std::hash<foo2> {
-  xtd::size operator()(const foo2& value) const noexcept {return xtd::hash_code::combine(value.value);}
+  auto operator()(const foo2& value) const noexcept -> xtd::size {return xtd::hash_code::combine(value.value);}
 };
 
 auto main() -> int {
