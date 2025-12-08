f27d2332d78c4be9d437bf71b88f4ef121a27148
Gammasoft <gammasoft71@gmail.com>
2025-12-08 22:49:10 +0100

    Add 64 bits constnt and add final_mix method  to increase dissemination

--- src/xtd.core/src/xtd/hash_code.cpp
+++ src/xtd.core/src/xtd/hash_code.cpp
@@ -1,10 +1,20 @@
 #include "../../include/xtd/as.hpp"
 #include "../../include/xtd/is.hpp"
+#include "../../include/xtd/environment.hpp"
 #include "../../include/xtd/hash_code.hpp"
 #include "../../include/xtd/random.hpp"
 
 using namespace xtd;
 using namespace xtd::collections::generic::helpers;
+
+namespace {
+  static size final_mix(size x) noexcept {
+    x ^= x >> 23;
+    x *= static_cast<size>(0x2127599bf4325c37ull);
+    x ^= x >> 47;
+    return x;
+  }
+}
 
 bool hash_code::equals(const object& other) const noexcept {
   return is<hash_code>(other) ? hash_code_ == as<hash_code>(other).hash_code_ : object::equals(other);
@@ -15,7 +25,7 @@ size hash_code::get_hash_code() const noexcept {
 }
 
 size hash_code::to_hash_code() const noexcept {
-  return hash_code_;
+  return final_mix(hash_code_);
 }
 
 size hash_code::combine_iterator(size seed) noexcept {
@@ -24,7 +34,7 @@ size hash_code::combine_iterator(size seed) noexcept {
 
 size hash_code::hash_combine(size seed, size value) noexcept {
   // https://stackoverflow.com/questions/35985960/c-why-is-boosthash-combine-the-best-way-to-combine-hash-values
-  return (seed ^ value) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+  return (seed ^ value) + (environment::is_64_bit_process() ? static_cast<size>(0x9e3779b97f4a7c15ull) : 0x9e3779b9) + (seed << 6) + (seed >> 2);
 }
 
 size hash_code::generate_uniqueness_seed() noexcept {
