b6c89c11cd40b741331069ad1efa822ff849a720
Gammasoft <gammasoft71@gmail.com>
2025-12-09 20:07:37 +0100

    using auto

--- examples/xtd.core.examples/boxing/boxed_info/src/boxed_info.cpp
+++ examples/xtd.core.examples/boxing/boxed_info/src/boxed_info.cpp
@@ -1,22 +1,25 @@
 #include <xtd/xtd>
 
-// To box a structure or class, it must inherit the xtd::icomparable, xtd::iequatable and xtd::ihashable interfaces.
-struct foo : icomparable<foo>, iequatable<foo>, ihashable, istringable {
-  foo() = default;
-  foo(int value) : value {value} {}
+// To box a structure or class, it must inherit the xtd::icomparable, and xtd::iequatable interfaces, or implements `<`, `>` and `==` operators.
+
+struct foo1 : icomparable<foo1>, iequatable<foo1>, istringable {
+  foo1() = default;
+  explicit foo1(int value) : value {value} {}
   
   int value = 0;
   
-  auto compare_to(const foo& other) const noexcept -> int32 override {return value - other.value;}
-  auto get_hash_code() const noexcept -> size override {return hash_code::combine(value);}
-  auto equals(const foo& other) const noexcept -> bool override {return value == other.value;}
+  auto compare_to(const foo1& other) const noexcept -> int32 override {return value - other.value;}
+  auto equals(const foo1& other) const noexcept -> bool override {return value == other.value;}
   auto to_string() const noexcept -> string override {return string::format("{}", value);}
 };
 
-
-template<>
-struct std::hash<foo> {
-  auto operator()(const foo&) -> size_t {return 0;}
+struct foo2 {
+  int value = 0;
+  
+  auto operator <(const foo2& other) const noexcept -> bool {return value < other.value;}
+  auto operator >(const foo2& other) const noexcept -> bool {return value > other.value;}
+  auto operator ==(const foo2& other) const noexcept -> bool {return value == other.value;}
+  friend auto operator <<(std::ostream& os, const foo2& value) -> std::ostream& {return os << string::format("{}", value.value);}
 };
 
 template<typename type_t>
@@ -33,7 +36,8 @@ auto main() -> int {
   console::write_line(get_boxed_info("A string"));
   console::write_line(get_boxed_info(U"A u32 string"));
   console::write_line(get_boxed_info(date_time::now()));
-  console::write_line(get_boxed_info(foo(42)));
+  console::write_line(get_boxed_info(foo1 {42}));
+  console::write_line(get_boxed_info(foo2 {.value = 42}));
 }
 
 // This code produces the following output :
@@ -42,8 +46,8 @@ auto main() -> int {
 // [type = double, boxed type = xtd::box_floating_point<double>, value = 42.84]
 // [type = bool, boxed type = xtd::box<bool>, value = true]
 // [type = xtd::day_of_week, boxed type = xtd::enum_object<xtd::day_of_week>, value = saturday]
-// [type = char [9], boxed type = xtd::string, value = A string]
-// [type = char32_t [13], boxed type = xtd::string, value = A u32 string]
+// [type = char [9], boxed type = xtd::basic_string<char, std::char_traits<char>, std::allocator<char>>, value = A string]
+// [type = char32_t [13], boxed type = xtd::basic_string<char, std::char_traits<char>, std::allocator<char>>, value = A u32 string]
 // [type = xtd::date_time, boxed type = xtd::date_time, value = Thu Dec  1 20:00:57 2022]
-// [type = foo, boxed type = xtd::box<foo>, value = 42]
-
+// [type = foo1, boxed type = xtd::box<foo1>, value = 42]
+// [type = foo2, boxed type = xtd::box<foo2>, value = 42]
