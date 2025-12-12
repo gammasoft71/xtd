58e11f20531a2f29b24e2f8014c452308d7949f9
Gammasoft <gammasoft71@gmail.com>
2025-12-12 12:49:09 +0100

    Set protected constructors

--- src/xtd.tunit/include/xtd/tunit/constraints/operator_value.hpp
+++ src/xtd.tunit/include/xtd/tunit/constraints/operator_value.hpp
@@ -8,16 +8,38 @@
 namespace xtd {
   /// @brief The tunit namespace contains a unit test library.
   namespace tunit {
+    /// @cond
+    template<class actual_t> class does_not_value;
+    template<class actual_t> class does_value;
+    template<class actual_t> class is_not_value;
+    template<class actual_t> class is_value;
+    /// @endcond
+    
     /// @brief The contraints namespace contains the constraint-based assert model.
     namespace constraints {
       template<class actual_t>
       class operator_value : public actual_value<actual_t> {
       public:
+        /// @name Public Methods
+        
+        /// @{
+        inline auto and_();
+        /// @}
+        
+      protected:
+        /// @name Protected Construtors
+        
+        /// @{
         operator_value() = default;
         operator_value(actual_value<actual_t>&& v) : actual_value<actual_t> {std::move(v)} {}
         operator_value(const actual_value<actual_t>& v) : actual_value<actual_t> {v} {}
-        
-        inline auto and_();
+        /// @}
+
+      private:
+        template<class value_t> friend class does_not_value;
+        template<class value_t> friend class does_value;
+        template<class value_t> friend class is_not_value;
+        template<class value_t> friend class is_value;
       };
     }
   }
