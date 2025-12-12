b710b2db17998bc3d3f7853f1668a54736740d88
Gammasoft <gammasoft71@gmail.com>
2025-12-12 20:43:38 +0100

    using auto

--- tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/assume_contains_string_succeed_tests.cpp
+++ tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/assume_contains_string_succeed_tests.cpp
@@ -8,7 +8,7 @@ namespace xtd::tunit::tests {
   class test_class_(assume_contains_string_succeed_tests) {
   public:
     auto test_method_(test_case_succeed) {
-      std::string s = "string";
+      auto s = "string";
       xtd::tunit::assume::contains('i', s);
     }
   };
