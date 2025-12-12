9833f3b679ee9325cd1b69872906e25ee8e55842
Gammasoft <gammasoft71@gmail.com>
2025-12-12 21:16:38 +0100

    using auto

--- tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/assume_is_not_empty_string_failed_tests.cpp
+++ tests/xtd.tunit.unit_tests/src/xtd/tunit/tests/assume_is_not_empty_string_failed_tests.cpp
@@ -8,7 +8,7 @@ namespace xtd::tunit::tests {
   class test_class_(assume_is_not_empty_string_failed_tests) {
   public:
     auto test_method_(test_case_failed) {
-      std::string s;
+      auto s;
       xtd::tunit::assume::is_not_empty(s);
     }
   };
