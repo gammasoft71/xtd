d638b2b0d424be6145a9b81bad9df6c9acac4e83
Gammasoft <gammasoft71@gmail.com>
2025-12-23 19:54:53 +0100

    Refactor event handler syntax for radio buttons

--- examples/xtd.forms.examples/events/enable_changed_event/src/enable_changed_event.cpp
+++ examples/xtd.forms.examples/events/enable_changed_event/src/enable_changed_event.cpp
@@ -8,13 +8,13 @@ namespace enable_changed_event_example {
       radio_button1.name("radio_button1");
       radio_button1.size({100, 17});
       radio_button1.text("button1");
-      radio_button1.enabled_changed += event_handler(*this, &form1::radio_button1_enabled_changed);
+      radio_button1.enabled_changed += event_handler {self_, &form1::radio_button1_enabled_changed};
       
       radio_button2.location({0, 39});
       radio_button2.name("radio_button2");
       radio_button2.size({150, 17});
       radio_button2.text("Disable button1");
-      radio_button2.checked_changed += event_handler(*this, &form1::radio_button2_checked_changed);
+      radio_button2.checked_changed += event_handler {self_, &form1::radio_button2_checked_changed};
       
       client_size({292, 273});
       controls().add_range({radio_button1, radio_button2});
@@ -22,11 +22,11 @@ namespace enable_changed_event_example {
     }
     
   private:
-    void radio_button1_enabled_changed(object& sender, const event_args& e) {
+    auto radio_button1_enabled_changed(object& sender, const event_args& e) -> void {
       message_box::show("This button has been disabled.");
     }
     
-    void radio_button2_checked_changed(object& sender, const event_args& e) {
+    auto radio_button2_checked_changed(object& sender, const event_args& e) -> void {
       radio_button1.enabled(false);
     }
     
