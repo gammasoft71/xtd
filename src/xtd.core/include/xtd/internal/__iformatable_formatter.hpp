0f3472091bc019b2c79621f554d2fe454fbc0463
Gammasoft <gammasoft71@gmail.com>
2026-01-06 23:21:23 +0100

    review __to_string_polymorphic method

--- src/xtd.core/include/xtd/internal/__iformatable_formatter.hpp
+++ src/xtd.core/include/xtd/internal/__iformatable_formatter.hpp
@@ -10,6 +10,7 @@
 #include "../helpers/throw_helper.hpp"
 #include "../iformatable.hpp"
 #include "../istringable.hpp"
+#include <type_traits>
 
 /// @cond
 namespace xtd {
@@ -22,11 +23,10 @@ std::string __to_string_object_to_string(const xtd::object* obj);
 
 template<class value_t>
 inline static std::string __to_string_polymorphic(const value_t& value, const std::string& fmt, const std::locale& loc) {
-  auto value_ptr = &value;
-  if (dynamic_cast<const xtd::iformatable*>(value_ptr)) return __to_string_iformatable_to_string(dynamic_cast<const xtd::iformatable*>(&value), fmt, loc);
-  if (dynamic_cast<const xtd::istringable*>(value_ptr)) return __to_string_istringable_to_string(dynamic_cast<const xtd::istringable*>(&value));
-  if (dynamic_cast<const xtd::object*>(value_ptr)) return __to_string_object_to_string(dynamic_cast<const xtd::object*>(value_ptr));
-  if (dynamic_cast<const std::exception*>(value_ptr)) return std::string {"exception: "} + dynamic_cast<const std::exception&>(value).what();
-  xtd::helpers::throw_helper::throws(xtd::helpers::exception_case::format_not_iformatable, typeid(value));
+  if constexpr (std::is_base_of<xtd::iformatable, value_t>()) return __to_string_iformatable_to_string(static_cast<const xtd::iformatable*>(&value), fmt, loc);
+  else if constexpr (std::is_base_of<xtd::istringable, value_t>()) return __to_string_istringable_to_string(static_cast<const xtd::istringable*>(&value));
+  else if constexpr (std::is_base_of<xtd::object, value_t>()) return __to_string_object_to_string(static_cast<const xtd::object*>(&value));
+  else if constexpr (std::is_base_of<std::exception, value_t>()) return std::string {"exception: "} + static_cast<const std::exception&>(value).what();
+  else xtd::helpers::throw_helper::throws(xtd::helpers::exception_case::format_not_iformatable, typeid(value));
 }
 /// @endcond
