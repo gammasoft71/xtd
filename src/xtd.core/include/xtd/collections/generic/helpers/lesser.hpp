39bd86fda4e45d40717929a038bab6cd31d1b639
Gammasoft <gammasoft71@gmail.com>
2026-01-10 14:45:07 +0100

    Review xtd::collections::generic::helpers::lesser : using concepts instead SFINAE

--- src/xtd.core/include/xtd/collections/generic/helpers/lesser.hpp
+++ src/xtd.core/include/xtd/collections/generic/helpers/lesser.hpp
@@ -2,10 +2,10 @@
 /// @brief Contains xtd::collections::generic::helpers::lesser struct.
 /// @copyright Copyright (c) 2025 Gammasoft. All rights reserved.
 #pragma once
-#define __XTD_CORE_INTERNAL__
-#include "../../../internal/__polymorphic_lesser.hpp"
-#undef __XTD_CORE_INTERNAL__
 #include "../icomparer.hpp"
+#include "../../../icomparable.hpp"
+#include "../../../int32.hpp"
+#include "../../../helpers/less_than_comparable.hpp"
 
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
@@ -30,20 +30,20 @@ namespace xtd {
         /// @ingroup xtd_core helpers_generic_collections
         /// @remarks The associative containers xtd::collections::generic::sorted_set, xtd::collections::generic::sorted_multi_set, xtd::collections::generic::sorted_dictionary, xtd::collections::generic::sorted_multi_dictionary use specializations of the template xtd::collections::generic::helpers::lesser as the defaultcompare function.
         /// @par Examples
-        /// The following example show how to use xtd::collections::generic::helpers::lesser with `std::unordered_map`.
+        /// The following example show how to use xtd::collections::generic::helpers::lesser with `std::map`.
         /// ```cpp
         /// auto key_values = std::map<xtd::date_time, xtd::string, xtd::collections::generic::helpers::lesser<xtd::date_time>, xtd::collections::generic::helpers::allocator<xtd::collections::generic::key_value_pair<const xtd::date_time, xtd::string>>> {};
         /// key_values.insert({{1971, 1, 5}, "Birth date"});
         /// ```
-        template<class type_t = void>
+        template<class value_t = void>
         struct lesser {
           /// @name Public Aliases
           
           /// @{
           /// @brief Represents the first argument type.
-          using first_argument_type = type_t;
+          using first_argument_type = value_t;
           /// @brief Represents the second argument type.
-          using second_argument_type = type_t;
+          using second_argument_type = value_t;
           /// @brief Represents the result type.
           using result_type = bool;
           /// @}
@@ -55,24 +55,27 @@ namespace xtd {
           lesser() = default;
           /// @brief Initializes a new instance of the comparer with specified comparer.
           /// @param comparer A comparer used to hash the key.
-          explicit lesser(const xtd::collections::generic::icomparer<type_t>& comparer) : comparer_ {&comparer} {}
+          explicit lesser(const xtd::collections::generic::icomparer<value_t>& comparer) : comparer_ {&comparer} {}
           /// @}
           
           /// @name Public Operators
           
           /// @{
-          /// @brief Serves as a hash function for a specified key with a particular type (type_t).
-          /// @param key The key to hash.
-          /// @return A hash code for the spesified key.
-          /// @remarks If key_t inherits from xtd::object, the xtd::object::get_hash_code method will be used; otherwise, the [std::hash](https://en.cppreference.com/w/cpp/utility/hash) object function will be used.
-          constexpr auto operator()(const first_argument_type& lhs, const second_argument_type& rhs) const -> result_type {
-            if (comparer_) return comparer_->compare(lhs, rhs) < 0;
-            return __polymorphic_lesser__<first_argument_type, typename std::is_polymorphic<first_argument_type>::type> {}(lhs, rhs);
+          /// @brief Compares two entities and returns a value indicating whether one is less than the other.
+          /// @param x The first entity to compare.
+          /// @param y The second entity to compare.
+          /// @return true is x less than y; otherwise false.
+          constexpr auto operator()(const first_argument_type& x, const second_argument_type& y) const -> result_type {
+            if (&x == &y) return false;
+            if (comparer_) return comparer_->compare(x, y) < 0;
+            if constexpr(std::is_polymorphic_v<first_argument_type> && std::is_base_of_v<xtd::icomparable<first_argument_type>, first_argument_type>) return static_cast<const xtd::icomparable<first_argument_type>&>(x).compare_to(y) < 0;
+            else if constexpr(xtd::helpers::less_than_comparable<first_argument_type>) return std::less<first_argument_type> {}(x, y);
+            else return std::less<const void*> {}(static_cast<const void*>(&x), static_cast<const void*>(&y));
           }
           /// @}
           
         private:
-          const xtd::collections::generic::icomparer<type_t>* comparer_ = nullptr;
+          const xtd::collections::generic::icomparer<value_t>* comparer_ = nullptr;
         };
       }
     }
