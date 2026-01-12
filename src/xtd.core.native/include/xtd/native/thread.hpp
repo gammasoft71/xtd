bdab638bb15fea739945058eb6c1e61a50ef2c26
Gammasoft <gammasoft71@gmail.com>
2026-01-12 12:31:09 +0100

    Using trailing return type

--- src/xtd.core.native/include/xtd/native/thread.hpp
+++ src/xtd.core.native/include/xtd/native/thread.hpp
@@ -43,7 +43,7 @@ namespace xtd {
       /// @param handle The thread handle.
       /// @return `true` if succeed; otherwise `false`.
       /// @warning Internal use only
-      static bool cancel(intmax_t handle);
+      static auto cancel(std::intmax_t handle) -> bool;
       
       /// @brief Cancel thread with specified handle.
       /// @param start The function to run in the new thread created.
@@ -51,42 +51,42 @@ namespace xtd {
       /// @param max_stack_size The maximum stack size, in bytes, to be used by the thread, or 0 to use the default maximum stack size specified in the header for the executable.
       /// @return The thread handle.
       /// @warning Internal use only
-      static intmax_t create(std::function<void(intmax_t)> start, intmax_t obj, std::int32_t max_stack_size, bool suspended, intmax_t& id);
+      static auto create(std::function<void(std::intmax_t)> start, std::intmax_t obj, std::int32_t max_stack_size, bool suspended, std::intmax_t& id) -> std::intmax_t;
       
       /// @brief Detach thread with specified handle.
       /// @param handle The thread handle.
       /// @return `true` if succeed; otherwise `false`.
       /// @warning Internal use only
-      static bool detach(intmax_t handle);
+      static auto detach(std::intmax_t handle) -> bool;
       
       /// @brief Gets the current thread handle.
       /// @return The current thread handle.
       /// @warning Internal use only
-      static intmax_t get_current_thread_handle();
+      static auto get_current_thread_handle() -> std::intmax_t;
       
       /// @brief Gets the thread id.
       /// @param handle The thread handle.
       /// @return The current thread id.
       /// @warning Internal use only
-      static intmax_t get_thread_id(intmax_t handle);
+      static auto get_thread_id(std::intmax_t handle) -> std::intmax_t;
       
       /// @brief Join thread with specified handle.
       /// @param handle The thread handle.
       /// @return `true` if succeed; otherwise `false`.
       /// @warning Internal use only
-      static bool join(intmax_t handle);
+      static auto join(std::intmax_t handle) -> bool;
       
       /// @brief Resume thread with specified handle.
       /// @param handle The thread handle.
       /// @return `true` if succeed; otherwise `false`.
       /// @warning Internal use only
-      static bool resume(intmax_t handle);
+      static auto resume(std::intmax_t handle) -> bool;
       
       /// @brief Sets the current thread name.
       /// @param name The current thread name.
       /// @return `true` if succeed; otherwise `false`.
       /// @warning Internal use only
-      static bool set_current_thread_name(const std::string& name);
+      static auto set_current_thread_name(const std::string& name) -> bool;
       
       /// @brief Sets the thread priority.
       /// @param handle The thread handle.
@@ -99,30 +99,30 @@ namespace xtd {
       /// * 2 : normal
       /// * 3 : above_normal
       /// * 4 : highest
-      static bool set_priority(intmax_t handle, std::int32_t priority);
+      static auto set_priority(std::intmax_t handle, std::int32_t priority) -> bool;
       
       /// @brief Sets the processors on which the associated thread can run.
       /// @param value An std::vector<size_> that represents an array of processor numbers on which the thread can run.
       /// @return `true` if succeed; otherwise `false`.
       /// @warning Internal use only
-      static bool set_processor_affinity(intmax_t handle, const std::vector<size_t>& processor_affinity);
+      static auto set_processor_affinity(std::intmax_t handle, const std::vector<std::size_t>& processor_affinity) -> bool;
       
       /// @brief Suspends the current thread for a specified time.
       /// @param milliseconds_timeout The number of milliseconds for which the thread is blocked. Specify zero (0) to indicate that this thread should be suspended to allow other waiting threads to execute. Specify -1 to block the thread indefinitely.
       /// @return `true` if succeed; otherwise `false`.
       /// @warning Internal use only
-      static void sleep(int32_t milliseconds_timeout);
+      static auto sleep(std::int32_t milliseconds_timeout) -> void;
       
       /// @brief Suspend thread with specified handle.
       /// @param handle The thread handle.
       /// @return `true` if succeed; otherwise `false`.
       /// @warning Internal use only
-      static bool suspend(intmax_t handle);
+      static auto suspend(std::intmax_t handle) -> bool;
       
       /// @brief Causes the calling thread to yield execution to another thread that is ready to run on the current processor.
       /// @return `true` if succeed; otherwise `false`.
       /// @warning Internal use only
-      static bool yield();
+      static auto yield() -> bool;
       /// @}
     };
   }
