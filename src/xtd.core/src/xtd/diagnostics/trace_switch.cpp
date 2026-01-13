3258a92372a4febd21cb14681a8b68b6a7795239
Gammasoft <gammasoft71@gmail.com>
2026-01-13 15:28:13 +0100

    Using trailing return type

--- src/xtd.core/src/xtd/diagnostics/trace_switch.cpp
+++ src/xtd.core/src/xtd/diagnostics/trace_switch.cpp
@@ -12,32 +12,32 @@ trace_switch::trace_switch(const string& display_name, const string& description
   value(default_switch_value);
 }
 
-trace_level trace_switch::level() const noexcept {
+auto trace_switch::level() const noexcept -> trace_level {
   return as<trace_level>(switch_setting());
 }
 
-void trace_switch::level(xtd::diagnostics::trace_level level) noexcept {
+auto trace_switch::level(xtd::diagnostics::trace_level level) noexcept -> void {
   if (self_.level() == level) return;
   value(enum_object<>::to_string(level));
 }
 
-bool trace_switch::trace_switch::trace_error() const noexcept {
+auto trace_switch::trace_switch::trace_error() const noexcept -> bool {
   return static_cast<int32>(level()) >= static_cast<int32>(xtd::diagnostics::trace_level::error);
 }
 
-bool trace_switch::trace_info() const noexcept {
+auto trace_switch::trace_info() const noexcept -> bool {
   return static_cast<int32>(level()) >= static_cast<int32>(xtd::diagnostics::trace_level::info);
 }
 
-bool trace_switch::trace_verbose() const noexcept {
+auto trace_switch::trace_verbose() const noexcept -> bool {
   return static_cast<int32>(level()) == static_cast<int32>(xtd::diagnostics::trace_level::verbose);
 }
 
-bool trace_switch::trace_warning() const noexcept {
+auto trace_switch::trace_warning() const noexcept -> bool {
   return static_cast<int32>(level()) >= static_cast<int32>(xtd::diagnostics::trace_level::warning);
 }
 
-void trace_switch::on_switch_setting_changed() {
+auto trace_switch::on_switch_setting_changed() -> void {
   switch_object::on_switch_setting_changed();
   auto level = switch_setting();
   if (level < as<int32>(xtd::diagnostics::trace_level::off)) {
@@ -49,7 +49,7 @@ void trace_switch::on_switch_setting_changed() {
   }
 }
 
-void trace_switch::on_value_changed() {
+auto trace_switch::on_value_changed() -> void {
   auto level_value = trace_level {};
   if (string::try_parse(value(), level_value)) switch_setting(as<int32>(level_value));
   else switch_object::on_value_changed();
