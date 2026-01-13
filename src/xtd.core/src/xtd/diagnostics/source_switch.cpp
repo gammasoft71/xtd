87595d389c8350b00b6ad61745f0024c3ce18321
Gammasoft <gammasoft71@gmail.com>
2026-01-13 15:28:40 +0100

    Using trailing return type

--- src/xtd.core/src/xtd/diagnostics/source_switch.cpp
+++ src/xtd.core/src/xtd/diagnostics/source_switch.cpp
@@ -12,20 +12,20 @@ source_switch::source_switch(const string& display_name, const string& default_s
   value(default_switch_value);
 }
 
-source_levels source_switch::level() const noexcept {
+auto source_switch::level() const noexcept -> source_levels {
   return as<source_levels>(switch_setting());
 }
 
-void source_switch::level(xtd::diagnostics::source_levels level) {
+auto source_switch::level(xtd::diagnostics::source_levels level) -> void {
   if (self_.level() == level) return;
   value(enum_object<>::to_string(level));
 }
 
-bool source_switch::should_trace(trace_event_type event_type) noexcept {
+auto source_switch::should_trace(trace_event_type event_type) noexcept -> bool {
   return (switch_setting() & static_cast<int32>(event_type)) != 0;
 }
 
-void source_switch::on_value_changed() {
+auto source_switch::on_value_changed() -> void {
   auto level_value = source_levels {};
   if (string::try_parse(value(), level_value)) switch_setting(as<int32>(level_value));
   else switch_object::on_value_changed();
