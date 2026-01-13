a18a35087306049f27d12c9c4a4bb22b221520c3
Gammasoft <gammasoft71@gmail.com>
2026-01-13 15:28:23 +0100

    Using trailing return type

--- src/xtd.core/src/xtd/diagnostics/source_filter.cpp
+++ src/xtd.core/src/xtd/diagnostics/source_filter.cpp
@@ -7,14 +7,14 @@ using namespace xtd::diagnostics;
 source_filter::source_filter(const string& source) : source_(source) {
 }
 
-const string& source_filter::source() const noexcept {
+auto source_filter::source() const noexcept -> const string& {
   return source_;
 }
 
-void source_filter::source(const string& source) noexcept {
+auto source_filter::source(const string& source) noexcept -> void {
   source_ = source;
 }
 
-bool source_filter::should_trace(const trace_event_cache& cache, const string& source, trace_event_type event_type, int32 id, const string& message, any_object data1, const list<any_object>& data_array) noexcept {
+auto source_filter::should_trace(const trace_event_cache& cache, const string& source, trace_event_type event_type, int32 id, const string& message, any_object data1, const list<any_object>& data_array) noexcept -> bool {
   return source == source_;
 }
