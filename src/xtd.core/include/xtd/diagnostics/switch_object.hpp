b158f916df169fd6063955339388d90d5403292a
Gammasoft <gammasoft71@gmail.com>
2026-01-13 15:26:12 +0100

    Using trailing return type

--- src/xtd.core/include/xtd/diagnostics/switch_object.hpp
+++ src/xtd.core/include/xtd/diagnostics/switch_object.hpp
@@ -33,11 +33,11 @@ namespace xtd {
     /// @remarks This class is the base class for the boolean_switch, source_switch and the trace_switch classes. These switches meet most debugging and tracing needs.
     /// @par Notes to implementers
     /// If you need trace levels, or mechanisms for setting switch levels different from those provided by boolean_switch, source_switch and trace_switch, you can inherit from switch_object. When inheriting from this class, you must implement the switch_setting method.
-    class core_export_ switch_object : public xtd::object, public xtd::iequatable<switch_object> {
+    class core_export_ switch_object : public xtd::object, public xtd::iequatable<xtd::diagnostics::switch_object> {
     public:
       /// @cond
-      switch_object(const switch_object& value) = default;
-      switch_object& operator =(const switch_object& value) = default;
+      switch_object(const xtd::diagnostics::switch_object& value) = default;
+      switch_object& operator =(const xtd::diagnostics::switch_object& value) = default;
       /// @endcond
       
       /// @name Public Aliases
@@ -53,36 +53,36 @@ namespace xtd {
       /// @{
       /// @brief Gets the custom switch attributes
       /// @return A xtd::collections::specialized::string_dictionary containing the case-insensitive custom attributes for the trace switch.
-      const attribute_collection& attributes() const noexcept;
+      auto attributes() const noexcept -> const attribute_collection&;
       /// @brief Gets the custom switch attributes
       /// @return A xtd::collections::specialized::string_dictionary containing the case-insensitive custom attributes for the trace switch.
-      attribute_collection& attributes() noexcept;
+      auto attributes() noexcept -> attribute_collection&;
       /// @brief Sets the custom switch attributes
       /// @param attributes A xtd::collections::specialized::string_dictionary containing the case-insensitive custom attributes for the trace switch.
-      void attributes(const attribute_collection& attributes) noexcept;
+      auto attributes(const attribute_collection& attributes) noexcept -> void;
       
       /// @brief Gets a description of the switch
       /// @return The description of the switch. The default value is an empty string ("").
       /// @remarks This property should indicate the function of the switch; for example, "Enables tracing for a directory watcher component." The value is set by the descriptor parameter in the switch_object constructor.
-      const xtd::string& description() const noexcept;
+      auto description() const noexcept -> const xtd::string&;
       
       /// @brief Gets a name used to identify the switch.
       /// @return The name used to identify the switch. The default value is an empty string ("").
       /// @remarks When you create a new switch_object object, the display_name finds initial switch settings. For more information, see the switch_object constructor.
-      const xtd::string& display_name() const noexcept;
+      auto display_name() const noexcept -> const xtd::string&;
       
       /// @brief Gets the value of the switch.
       /// @return A string representing the value of the switch.
       /// @remarks The on_value_changed method is called when the value of the value property is changed. The on_value_changed method parses the value of this property and converts it to an integer value, which is then used to set the switch_setting property.
       /// @par Notes to inheritors
       /// You should override the on_value_changed() method and provide a conversion operation that sets the appropriate switch_setting value for your switch.
-      const xtd::string& value() const noexcept;
+      auto value() const noexcept -> const xtd::string&;
       /// @brief Sets the value of the switch.
       /// @param value A string representing the value of the switch.
       /// @remarks The on_value_changed method is called when the value of the value property is changed. The on_value_changed method parses the value of this property and converts it to an integer value, which is then used to set the switch_setting property.
       /// @par Notes to inheritors
       /// You should override the on_value_changed() method and provide a conversion operation that sets the appropriate switch_setting value for your switch.
-      void value(const xtd::string& value);
+      auto value(const xtd::string& value) -> void;
       /// @}
       
       /// @name Public Methods
@@ -91,11 +91,11 @@ namespace xtd {
       /// @brief Determines whether the specified object is equal to the current object.
       /// @param obj The object to compare with the current object.
       /// @return `true` if the specified object is equal to the current object. otherwise, `false`.
-      bool equals(const xtd::object& obj) const noexcept override;
+      auto equals(const xtd::object& obj) const noexcept -> bool override;
       /// @brief Determines whether the specified object is equal to the current object.
       /// @param other The object to compare with the current object.
       /// @return `true` if the specified object is equal to the current object. otherwise, `false`.
-      bool equals(const switch_object& other) const noexcept override;
+      auto equals(const switch_object& other) const noexcept -> bool override;
       /// @}
       
     protected:
@@ -116,10 +116,10 @@ namespace xtd {
       /// @{
       /// @brief Gets the current setting for this switch.
       /// @return The current setting for this switch. The default is zero.
-      int32 switch_setting() const noexcept;
+      auto switch_setting() const noexcept -> xtd::int32;
       /// @brief Sets the current setting for this switch.
       /// @param switch_setting The current setting for this switch. The default is zero.
-      void switch_setting(int32 switch_setting);
+      auto switch_setting(xtd::int32 switch_setting) -> void;
       /// @}
       
       /// @name Protected Methods
@@ -130,23 +130,23 @@ namespace xtd {
       /// @remarks The default implementation for the get_supported_attributes method returns {} (empty array). If a switch is added in a configuration file and custom attributes are specified that are not included in the string array returned by get_supported_attributes, a configuration_exception is thrown when the switch is loaded.
       /// @par Notes to Inheritors
       /// When inheriting from the switch_object class or a derived class, you can override the get_supported_attributes() method to provide custom attributes for your class.
-      virtual xtd::array<xtd::string> get_supported_attributes() const noexcept;
+      virtual auto get_supported_attributes() const noexcept -> xtd::array<xtd::string>;
       
       /// @brief Invoked when the switch_setting property is changed.
-      virtual void on_switch_setting_changed();
+      virtual auto on_switch_setting_changed() -> void;
       
       /// @brief Invoked when the value property is changed.
       /// @remarks The default implementation parses the new value of the value property to an integer value which it uses to set the switch_setting property.
       /// @par Not to Inheritors
       /// You should override this method and use a conversion that sets the appropriate switch_setting value for your switch.
-      virtual void on_value_changed();
+      virtual auto on_value_changed() -> void;
       /// @}
       
     private:
       xtd::string display_name_;
       xtd::string description_;
       attribute_collection attributes_;
-      int32 switch_setting_ = 0;
+      xtd::int32 switch_setting_ = 0;
       xtd::string value_ = "0";
       inline static xtd::collections::specialized::string_dictionary switches_;
     };
