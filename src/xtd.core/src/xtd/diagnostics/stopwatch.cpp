958038f8950b950ee41c19156fbf743b0d0ddcae
Gammasoft <gammasoft71@gmail.com>
2026-01-13 15:28:54 +0100

    Using trailing return type

--- src/xtd.core/src/xtd/diagnostics/stopwatch.cpp
+++ src/xtd.core/src/xtd/diagnostics/stopwatch.cpp
@@ -5,72 +5,72 @@ using namespace xtd::diagnostics;
 
 using stopwatch_clock_t = std::chrono::steady_clock;
 
-int64 stopwatch::frequency() noexcept {
+auto stopwatch::frequency() noexcept -> int64 {
   return std::nano::den;
 }
 
-bool stopwatch::is_high_resolution() noexcept {
+auto stopwatch::is_high_resolution() noexcept -> bool {
   return true;
 }
 
-time_span stopwatch::elapsed() const noexcept {
+auto stopwatch::elapsed() const noexcept -> time_span {
   return time_span::from_nanoseconds(std::chrono::nanoseconds {elapsed_nanoseconds()});
 }
 
-int64 stopwatch::elapsed_milliseconds() const noexcept {
+auto stopwatch::elapsed_milliseconds() const noexcept -> int64 {
   return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::nanoseconds {elapsed_nanoseconds()}).count();
 }
 
-int64 stopwatch::elapsed_nanoseconds() const noexcept {
+auto stopwatch::elapsed_nanoseconds() const noexcept -> int64 {
   if (running_) return get_timestamp_nanoseconds() - start_;
   return stop_ - start_;
 }
 
-int64 stopwatch::elapsed_ticks() const noexcept {
+auto stopwatch::elapsed_ticks() const noexcept -> int64 {
   return elapsed().ticks();
 }
 
-bool stopwatch::is_running() const noexcept {
+auto stopwatch::is_running() const noexcept -> bool {
   return running_;
 }
 
-std::chrono::nanoseconds stopwatch::get_timestamp() noexcept {
+auto stopwatch::get_timestamp() noexcept -> std::chrono::nanoseconds {
   return std::chrono::nanoseconds(stopwatch_clock_t::now().time_since_epoch());
 }
 
-int64 stopwatch::get_timestamp_milliseconds() noexcept {
+auto stopwatch::get_timestamp_milliseconds() noexcept -> int64 {
   return std::chrono::duration_cast<std::chrono::milliseconds>(get_timestamp()).count();
 }
 
-int64 stopwatch::get_timestamp_nanoseconds() noexcept {
+auto stopwatch::get_timestamp_nanoseconds() noexcept -> int64 {
   return get_timestamp().count();
 }
 
-int64 stopwatch::get_timestamp_ticks() noexcept {
+auto stopwatch::get_timestamp_ticks() noexcept -> int64 {
   return std::chrono::duration_cast<xtd::ticks>(get_timestamp()).count();
 }
 
-void stopwatch::reset() noexcept {
+auto stopwatch::reset() noexcept -> void {
   start_ = stop_ = 0;
   running_ = false;
 }
 
-void stopwatch::restart() noexcept {
+auto stopwatch::restart() noexcept -> void {
   reset();
   start();
 }
 
-void stopwatch::start() noexcept {
+auto stopwatch::start() noexcept -> void {
   if (running_) return;
   start_ = get_timestamp_nanoseconds() - (stop_ - start_);
   running_ = true;
 }
 
-stopwatch stopwatch::start_new() noexcept {
+auto stopwatch::start_new() noexcept -> stopwatch {
   return stopwatch(true);
 }
 
-void stopwatch::stop() noexcept {
+auto stopwatch::stop() noexcept -> void {
   if (!running_) return;
   stop_ = get_timestamp_nanoseconds();
   running_ = false;
