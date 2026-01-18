8aa597f6ec4a44c68ef5361c4aeb24f67ef0220b
Gammasoft <gammasoft71@gmail.com>
2026-01-18 20:51:54 +0100

    Using trailing return type

--- src/xtd.core/src/xtd/io/file.cpp
+++ src/xtd.core/src/xtd/io/file.cpp
@@ -17,11 +17,11 @@ using namespace xtd::collections::generic;
 using namespace xtd::helpers;
 using namespace io;
 
-std::ofstream file::append_text(const string& path) {
+auto file::append_text(const string& path) -> std::ofstream {
   if (path.index_of_any(xtd::io::path::get_invalid_path_chars()) != string::npos) throw_helper::throws(exception_case::argument);
   if (xtd::string::is_empty(path) || xtd::string::is_empty(path.trim(' '))) throw_helper::throws(exception_case::argument);
   if (native::file_system::is_path_too_long(path)) throw_helper::throws(exception_case::path_too_long);
-  if (!exists(path)) create_text(path);
+  if (!exists(path)) [[maybe_unused]] auto stream = create_text(path);
   if ((get_attributes(path) & file_attributes::read_only) == file_attributes::read_only) throw_helper::throws(exception_case::unauthorized_access);
   
   auto stream = std::ofstream {path, std::ios::out | std::ios::app};
@@ -29,11 +29,11 @@ std::ofstream file::append_text(const string& path) {
   return stream;
 }
 
-void file::copy(const string& src, const string& dest) {
+auto file::copy(const string& src, const string& dest) -> void {
   return copy(src, dest, true);
 }
 
-void file::copy(const string& src, const string& dest, bool overwrite) {
+auto file::copy(const string& src, const string& dest, bool overwrite) -> void {
   if (src.index_of_any(io::path::get_invalid_path_chars()) != src.npos) throw_helper::throws(exception_case::argument);
   if (xtd::string::is_empty(src) || xtd::string::is_empty(src.trim(' '))) throw_helper::throws(exception_case::argument);
   if (native::file_system::is_path_too_long(src)) throw_helper::throws(exception_case::path_too_long);
@@ -52,7 +52,7 @@ void file::copy(const string& src, const string& dest, bool overwrite) {
   if (native::file::copy(src, dest) != 0) throw_helper::throws(exception_case::io);
 }
 
-std::ofstream file::create(const string& path) {
+auto file::create(const string& path) -> std::ofstream {
   if (path.index_of_any(xtd::io::path::get_invalid_path_chars()) != string::npos) throw_helper::throws(exception_case::argument);
   if (xtd::string::is_empty(path) || xtd::string::is_empty(path.trim(' '))) throw_helper::throws(exception_case::argument);
   if (native::file_system::is_path_too_long(path)) throw_helper::throws(exception_case::path_too_long);
@@ -63,22 +63,20 @@ std::ofstream file::create(const string& path) {
   return stream;
 }
 
-std::ofstream file::create_text(const string& path) {
+auto file::create_text(const string& path) -> stream_writer {
   if (path.index_of_any(xtd::io::path::get_invalid_path_chars()) != string::npos) throw_helper::throws(exception_case::argument);
   if (xtd::string::is_empty(path) || xtd::string::is_empty(path.trim(' '))) throw_helper::throws(exception_case::argument);
   if (native::file_system::is_path_too_long(path)) throw_helper::throws(exception_case::path_too_long);
   if (exists(path) && (get_attributes(path) & file_attributes::read_only) == file_attributes::read_only) throw_helper::throws(exception_case::unauthorized_access);
-  
-  auto stream = std::ofstream {path};
-  if (!stream.good()) throw_helper::throws(exception_case::io);
-  return stream;
+
+  return stream_writer {path};
 }
 
-bool file::exists(const string& path) noexcept {
+auto file::exists(const string& path) noexcept -> bool {
   return native::file::exists(path);
 }
 
-file_attributes file::get_attributes(const string& path) {
+auto file::get_attributes(const string& path) -> file_attributes {
   if (path.index_of_any(io::path::get_invalid_path_chars()) != path.npos) throw_helper::throws(exception_case::argument);
   if (xtd::string::is_empty(path) || xtd::string::is_empty(path.trim(' '))) throw_helper::throws(exception_case::argument);
   if (native::file_system::is_path_too_long(path)) throw_helper::throws(exception_case::path_too_long);
@@ -89,7 +87,7 @@ file_attributes file::get_attributes(const string& path) {
   return static_cast<file_attributes>(attributes);
 }
 
-date_time file::get_creation_time(const xtd::string& path) {
+auto file::get_creation_time(const xtd::string& path) -> date_time {
   if (path.index_of_any(io::path::get_invalid_path_chars()) != path.npos) throw_helper::throws(exception_case::argument);
   if (xtd::string::is_empty(path) || xtd::string::is_empty(path.trim(' '))) throw_helper::throws(exception_case::argument);
   if (native::file_system::is_path_too_long(path)) throw_helper::throws(exception_case::path_too_long);
@@ -99,7 +97,7 @@ date_time file::get_creation_time(const xtd::string& path) {
   return date_time::from_time_t(creation_time, date_time_kind::local);
 }
 
-date_time file::get_last_access_time(const xtd::string& path) {
+auto file::get_last_access_time(const xtd::string& path) -> date_time {
   if (path.index_of_any(io::path::get_invalid_path_chars()) != path.npos) throw_helper::throws(exception_case::argument);
   if (xtd::string::is_empty(path) || xtd::string::is_empty(path.trim(' '))) throw_helper::throws(exception_case::argument);
   if (native::file_system::is_path_too_long(path)) throw_helper::throws(exception_case::path_too_long);
@@ -109,7 +107,7 @@ date_time file::get_last_access_time(const xtd::string& path) {
   return date_time::from_time_t(last_access_time, date_time_kind::local);
 }
 
-date_time file::get_last_write_time(const xtd::string& path) {
+auto file::get_last_write_time(const xtd::string& path) -> date_time {
   if (path.index_of_any(io::path::get_invalid_path_chars()) != path.npos) throw_helper::throws(exception_case::argument);
   if (xtd::string::is_empty(path) || xtd::string::is_empty(path.trim(' '))) throw_helper::throws(exception_case::argument);
   if (native::file_system::is_path_too_long(path)) throw_helper::throws(exception_case::path_too_long);
@@ -119,7 +117,7 @@ date_time file::get_last_write_time(const xtd::string& path) {
   return date_time::from_time_t(last_write_time, date_time_kind::local);
 }
 
-file_permissions file::get_permissions(const string& path) {
+auto file::get_permissions(const string& path) -> file_permissions {
   if (path.index_of_any(io::path::get_invalid_path_chars()) != path.npos) throw_helper::throws(exception_case::argument);
   if (xtd::string::is_empty(path) || xtd::string::is_empty(path.trim(' '))) throw_helper::throws(exception_case::argument);
   if (native::file_system::is_path_too_long(path)) throw_helper::throws(exception_case::path_too_long);
@@ -130,11 +128,11 @@ file_permissions file::get_permissions(const string& path) {
   return static_cast<file_permissions>(permission);
 }
 
-void file::move(const string& src, const string& dest) {
+auto file::move(const string& src, const string& dest) -> void {
   move(src, dest, false);
 }
 
-void file::move(const string& src, const string& dest, bool overwrite) {
+auto file::move(const string& src, const string& dest, bool overwrite) -> void {
   if (src.index_of_any(io::path::get_invalid_path_chars()) != src.npos) throw_helper::throws(exception_case::argument);
   if (xtd::string::is_empty(src) || xtd::string::is_empty(src.trim(' '))) throw_helper::throws(exception_case::argument);
   if (native::file_system::is_path_too_long(src)) throw_helper::throws(exception_case::path_too_long);
@@ -153,7 +151,7 @@ void file::move(const string& src, const string& dest, bool overwrite) {
   if (native::file::move(src, dest) != 0) throw_helper::throws(exception_case::io);
 }
 
-std::fstream file::open(const string& path, std::ios::openmode mode) {
+auto file::open(const string& path, std::ios::openmode mode) -> std::fstream {
   if (path.index_of_any(xtd::io::path::get_invalid_path_chars()) != string::npos) throw_helper::throws(exception_case::argument);
   if (xtd::string::is_empty(path) || xtd::string::is_empty(path.trim(' '))) throw_helper::throws(exception_case::argument);
   if (native::file_system::is_path_too_long(path)) throw_helper::throws(exception_case::path_too_long);
@@ -165,7 +163,7 @@ std::fstream file::open(const string& path, std::ios::openmode mode) {
   return stream;
 }
 
-std::ifstream file::open_read(const string& path) {
+auto file::open_read(const string& path) -> std::ifstream {
   if (path.index_of_any(xtd::io::path::get_invalid_path_chars()) != string::npos) throw_helper::throws(exception_case::argument);
   if (xtd::string::is_empty(path) || xtd::string::is_empty(path.trim(' '))) throw_helper::throws(exception_case::argument);
   if (native::file_system::is_path_too_long(path)) throw_helper::throws(exception_case::path_too_long);
@@ -176,18 +174,16 @@ std::ifstream file::open_read(const string& path) {
   return stream;
 }
 
-std::ifstream file::open_text(const string& path) {
+auto file::open_text(const string& path) -> stream_reader {
   if (path.index_of_any(xtd::io::path::get_invalid_path_chars()) != string::npos) throw_helper::throws(exception_case::argument);
   if (xtd::string::is_empty(path) || xtd::string::is_empty(path.trim(' '))) throw_helper::throws(exception_case::argument);
   if (native::file_system::is_path_too_long(path)) throw_helper::throws(exception_case::path_too_long);
   if (!exists(path)) throw_helper::throws(exception_case::file_not_found);
-  
-  auto stream = std::ifstream {path};
-  if (!stream.good()) throw_helper::throws(exception_case::io);
-  return stream;
+
+  return stream_reader {path};
 }
 
-std::ofstream file::open_write(const string& path) {
+auto file::open_write(const string& path) -> std::ofstream {
   if (path.index_of_any(xtd::io::path::get_invalid_path_chars()) != string::npos) throw_helper::throws(exception_case::argument);
   if (xtd::string::is_empty(path) || xtd::string::is_empty(path.trim(' '))) throw_helper::throws(exception_case::argument);
   if (native::file_system::is_path_too_long(path)) throw_helper::throws(exception_case::path_too_long);
@@ -197,7 +193,7 @@ std::ofstream file::open_write(const string& path) {
   return stream;
 }
 
-array<xtd::byte> file::read_all_bytes(const string& path) {
+auto file::read_all_bytes(const string& path) -> array<xtd::byte> {
   if (path.index_of_any(xtd::io::path::get_invalid_path_chars()) != string::npos) throw_helper::throws(exception_case::argument);
   if (xtd::string::is_empty(path) || xtd::string::is_empty(path.trim(' '))) throw_helper::throws(exception_case::argument);
   if (native::file_system::is_path_too_long(path)) throw_helper::throws(exception_case::path_too_long);
@@ -208,7 +204,7 @@ array<xtd::byte> file::read_all_bytes(const string& path) {
   return array<xtd::byte> {std::istreambuf_iterator<char>(stream), std::istreambuf_iterator<char>()};
 }
 
-array<string> file::read_all_lines(const string& path) {
+auto file::read_all_lines(const string& path) -> array<string> {
   if (path.index_of_any(xtd::io::path::get_invalid_path_chars()) != string::npos) throw_helper::throws(exception_case::argument);
   if (xtd::string::is_empty(path) || xtd::string::is_empty(path.trim(' '))) throw_helper::throws(exception_case::argument);
   if (native::file_system::is_path_too_long(path)) throw_helper::throws(exception_case::path_too_long);
@@ -221,7 +217,7 @@ array<string> file::read_all_lines(const string& path) {
   return contents.to_array();
 }
 
-string file::read_all_text(const string& path) {
+auto file::read_all_text(const string& path) -> string {
   if (path.index_of_any(xtd::io::path::get_invalid_path_chars()) != string::npos) throw_helper::throws(exception_case::argument);
   if (xtd::string::is_empty(path) || xtd::string::is_empty(path.trim(' '))) throw_helper::throws(exception_case::argument);
   if (native::file_system::is_path_too_long(path)) throw_helper::throws(exception_case::path_too_long);
@@ -231,7 +227,7 @@ string file::read_all_text(const string& path) {
   return sr.read_to_end();
 }
 
-void file::remove(const string& path) {
+auto file::remove(const string& path) -> void {
   if (path.index_of_any(xtd::io::path::get_invalid_path_chars()) != string::npos) throw_helper::throws(exception_case::argument);
   if (xtd::string::is_empty(path) || xtd::string::is_empty(path.trim(' '))) throw_helper::throws(exception_case::argument);
   if (native::file_system::is_path_too_long(path)) throw_helper::throws(exception_case::path_too_long);
@@ -240,13 +236,13 @@ void file::remove(const string& path) {
   if (native::file::remove(path) != 0) throw_helper::throws(exception_case::io);
 }
 
-void file::replace(const string& source_file_name, const string& destination_file_name, const string& destination_backup_file_name) {
+auto file::replace(const string& source_file_name, const string& destination_file_name, const string& destination_backup_file_name) -> void {
   if (!exists(source_file_name) || !exists(destination_file_name)) throw_helper::throws(exception_case::file_not_found);
   if (destination_backup_file_name != "") copy(destination_file_name, destination_backup_file_name);
   move(source_file_name, destination_file_name, true);
 }
 
-void file::set_attributes(const xtd::string& path, xtd::io::file_attributes attributes) {
+auto file::set_attributes(const xtd::string& path, xtd::io::file_attributes attributes) -> void {
   if (path.index_of_any(io::path::get_invalid_path_chars()) != path.npos) throw_helper::throws(exception_case::argument);
   if (xtd::string::is_empty(path) || xtd::string::is_empty(path.trim(' '))) throw_helper::throws(exception_case::argument);
   if (native::file_system::is_path_too_long(path)) throw_helper::throws(exception_case::path_too_long);
@@ -255,7 +251,7 @@ void file::set_attributes(const xtd::string& path, xtd::io::file_attributes attr
   if (native::file_system::set_attributes(path, static_cast<int32>(attributes)) != 0) throw_helper::throws(exception_case::io);
 }
 
-void file::set_creation_time(const xtd::string& path, const xtd::date_time& creation_time) {
+auto file::set_creation_time(const xtd::string& path, const xtd::date_time& creation_time) -> void {
   if (path.index_of_any(io::path::get_invalid_path_chars()) != path.npos) throw_helper::throws(exception_case::argument);
   if (xtd::string::is_empty(path) || xtd::string::is_empty(path.trim(' '))) throw_helper::throws(exception_case::argument);
   if (native::file_system::is_path_too_long(path)) throw_helper::throws(exception_case::path_too_long);
@@ -264,7 +260,7 @@ void file::set_creation_time(const xtd::string& path, const xtd::date_time& crea
   if (native::file_system::set_creation_time(path, creation_time.to_time_t()) != 0) throw_helper::throws(exception_case::io);
 }
 
-void file::set_last_access_time(const xtd::string& path, const xtd::date_time& last_access_time) {
+auto file::set_last_access_time(const xtd::string& path, const xtd::date_time& last_access_time) -> void {
   if (path.index_of_any(io::path::get_invalid_path_chars()) != path.npos) throw_helper::throws(exception_case::argument);
   if (xtd::string::is_empty(path) || xtd::string::is_empty(path.trim(' '))) throw_helper::throws(exception_case::argument);
   if (native::file_system::is_path_too_long(path)) throw_helper::throws(exception_case::path_too_long);
@@ -273,7 +269,7 @@ void file::set_last_access_time(const xtd::string& path, const xtd::date_time& l
   if (native::file_system::set_last_access_time(path, last_access_time.to_time_t()) != 0) throw_helper::throws(exception_case::io);
 }
 
-void file::set_last_write_time(const xtd::string& path, const xtd::date_time& last_write_time) {
+auto file::set_last_write_time(const xtd::string& path, const xtd::date_time& last_write_time) -> void {
   if (path.index_of_any(io::path::get_invalid_path_chars()) != path.npos) throw_helper::throws(exception_case::argument);
   if (xtd::string::is_empty(path) || xtd::string::is_empty(path.trim(' '))) throw_helper::throws(exception_case::argument);
   if (native::file_system::is_path_too_long(path)) throw_helper::throws(exception_case::path_too_long);
@@ -282,7 +278,7 @@ void file::set_last_write_time(const xtd::string& path, const xtd::date_time& la
   if (native::file_system::set_last_write_time(path, last_write_time.to_time_t()) != 0) throw_helper::throws(exception_case::io);
 }
 
-void file::set_permissions(const xtd::string& path, xtd::io::file_permissions permissions) {
+auto file::set_permissions(const xtd::string& path, xtd::io::file_permissions permissions) -> void {
   if (path.index_of_any(io::path::get_invalid_path_chars()) != path.npos) throw_helper::throws(exception_case::argument);
   if (xtd::string::is_empty(path) || xtd::string::is_empty(path.trim(' '))) throw_helper::throws(exception_case::argument);
   if (native::file_system::is_path_too_long(path)) throw_helper::throws(exception_case::path_too_long);
@@ -291,17 +287,15 @@ void file::set_permissions(const xtd::string& path, xtd::io::file_permissions pe
   if (native::file_system::set_permissions(path, static_cast<int32>(permissions)) != 0) throw_helper::throws(exception_case::io);
 }
 
-std::ofstream file::write_text(const string& path) {
+auto file::write_text(const string& path) -> stream_writer {
   if (path.index_of_any(xtd::io::path::get_invalid_path_chars()) != string::npos) throw_helper::throws(exception_case::argument);
   if (xtd::string::is_empty(path) || xtd::string::is_empty(path.trim(' '))) throw_helper::throws(exception_case::argument);
   if (native::file_system::is_path_too_long(path)) throw_helper::throws(exception_case::path_too_long);
   
-  auto stream = std::ofstream {path};
-  if (!stream.good()) throw_helper::throws(exception_case::io);
-  return stream;
+  return stream_writer(path);
 }
 
-std::tuple<std::time_t, std::time_t, std::time_t> file::get_file_times(const string& path) {
+auto file::get_file_times(const string& path) -> std::tuple<std::time_t, std::time_t, std::time_t> {
   auto creation_time = std::time_t {}, last_access_time = std::time_t {}, last_write_time = std::time_t {};
   if (native::file_system::get_file_times(path, creation_time, last_access_time, last_write_time) != 0) throw_helper::throws(exception_case::io);
   return std::make_tuple(creation_time, last_access_time, last_write_time);
