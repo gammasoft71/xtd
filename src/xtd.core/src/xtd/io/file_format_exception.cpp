80b892ac373e52099689fabe85491c913764f368
Gammasoft <gammasoft71@gmail.com>
2026-01-18 20:51:59 +0100

    Using trailing return type

--- src/xtd.core/src/xtd/io/file_format_exception.cpp
+++ src/xtd.core/src/xtd/io/file_format_exception.cpp
@@ -8,49 +8,49 @@ file_format_exception::file_format_exception(const stack_frame& stack_frame) : f
   error_code(h_result::make_error_code(h_result::COR_E_FORMAT));
 }
 
-file_format_exception::file_format_exception(const std::optional<xtd::string>& message, const xtd::diagnostics::stack_frame& stack_frame) : format_exception(message.value_or(default_message()), stack_frame) {
+file_format_exception::file_format_exception(const std::optional<string>& message, const stack_frame& stack_frame) : format_exception(message.value_or(default_message()), stack_frame) {
   error_code(h_result::make_error_code(h_result::COR_E_FORMAT));
 }
 
 /// @todo remove deprecated in version 0.4.0
 /// @{
-file_format_exception::file_format_exception(const xtd::string& message, const std::error_code& error, const xtd::diagnostics::stack_frame& stack_frame) : format_exception(message, stack_frame) {
+file_format_exception::file_format_exception(const string& message, const std::error_code& error, const stack_frame& stack_frame) : format_exception(message, stack_frame) {
   error_code(error);
 }
 
-file_format_exception::file_format_exception(const xtd::string& message, const xtd::string& help_link, const xtd::diagnostics::stack_frame& stack_frame) : format_exception(message, stack_frame) {
+file_format_exception::file_format_exception(const string& message, const string& help_link, const stack_frame& stack_frame) : format_exception(message, stack_frame) {
   error_code(h_result::make_error_code(h_result::COR_E_FORMAT));
   this->help_link(help_link);
 }
 
-file_format_exception::file_format_exception(const xtd::string& message, const std::error_code& error, const xtd::string& help_link, const xtd::diagnostics::stack_frame& stack_frame) : format_exception(message, stack_frame) {
+file_format_exception::file_format_exception(const string& message, const std::error_code& error, const string& help_link, const stack_frame& stack_frame) : format_exception(message, stack_frame) {
   error_code(error);
   this->help_link(help_link);
 }
 
-file_format_exception::file_format_exception(const std::exception& inner_exception, const xtd::diagnostics::stack_frame& stack_frame) : format_exception(stack_frame) {
+file_format_exception::file_format_exception(const std::exception& inner_exception, const stack_frame& stack_frame) : format_exception(stack_frame) {
   error_code(h_result::make_error_code(h_result::COR_E_FORMAT));
 }
 
-file_format_exception::file_format_exception(const xtd::string& message, const std::exception& inner_exception, const xtd::diagnostics::stack_frame& stack_frame) : format_exception(message, stack_frame) {
+file_format_exception::file_format_exception(const string& message, const std::exception& inner_exception, const stack_frame& stack_frame) : format_exception(message, stack_frame) {
   error_code(h_result::make_error_code(h_result::COR_E_FORMAT));
 }
 
-file_format_exception::file_format_exception(const xtd::string& message, const std::exception& inner_exception, const std::error_code& error, const xtd::diagnostics::stack_frame& stack_frame) : format_exception(message, stack_frame) {
+file_format_exception::file_format_exception(const string& message, const std::exception& inner_exception, const std::error_code& error, const stack_frame& stack_frame) : format_exception(message, stack_frame) {
   error_code(error);
 }
 
-file_format_exception::file_format_exception(const xtd::string& message, const std::exception& inner_exception, const xtd::string& help_link, const xtd::diagnostics::stack_frame& stack_frame) : format_exception(message, stack_frame) {
+file_format_exception::file_format_exception(const string& message, const std::exception& inner_exception, const string& help_link, const stack_frame& stack_frame) : format_exception(message, stack_frame) {
   error_code(h_result::make_error_code(h_result::COR_E_FORMAT));
   this->help_link(help_link);
 }
 
-file_format_exception::file_format_exception(const xtd::string& message, const std::exception& inner_exception, const std::error_code& error, const xtd::string& help_link, const xtd::diagnostics::stack_frame& stack_frame) : format_exception(message, stack_frame) {
+file_format_exception::file_format_exception(const string& message, const std::exception& inner_exception, const std::error_code& error, const string& help_link, const stack_frame& stack_frame) : format_exception(message, stack_frame) {
   error_code(error);
   this->help_link(help_link);
 }
 
-xtd::string file_format_exception::default_message() noexcept {
+auto file_format_exception::default_message() noexcept -> string {
   return "Input file or data stream does not conform to the expected file format specification.";
 }
 /// @}
