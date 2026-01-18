40e037c8f2d1eac6cd97d1ca53a1d01f9329f302
Gammasoft <gammasoft71@gmail.com>
2026-01-18 20:52:27 +0100

    Using trailing return type

--- src/xtd.core/src/xtd/io/file_not_found_exception.cpp
+++ src/xtd.core/src/xtd/io/file_not_found_exception.cpp
@@ -12,15 +12,15 @@ file_not_found_exception::file_not_found_exception(const optional<string>& messa
   error_code(h_result::make_error_code(h_result::COR_E_FILENOTFOUND));
 }
 
-file_not_found_exception::file_not_found_exception(const optional<string>& message, const xtd::optional<xtd::string>& file_name, const xtd::diagnostics::stack_frame& stack_frame) : io_exception {message, stack_frame}, file_name_{file_name}, defined_message_ {message.has_value()} {
+file_not_found_exception::file_not_found_exception(const optional<string>& message, const optional<string>& file_name, const stack_frame& stack_frame) : io_exception {message, stack_frame}, file_name_{file_name}, defined_message_ {message.has_value()} {
   error_code(h_result::make_error_code(h_result::COR_E_FILENOTFOUND));
 }
 
-const optional<string>& file_not_found_exception::file_name() const noexcept {
+auto file_not_found_exception::file_name() const noexcept -> const optional<string>& {
   return file_name_;
 }
 
-const xtd::string& file_not_found_exception::message() const noexcept {
+auto file_not_found_exception::message() const noexcept -> const string& {
   if (defined_message_ || !file_name_.has_value()) return exception::message();
   message_ = string::format("Could not load file '{}'. The system cannot find the file specified.", file_name_.value());
   return message_;
@@ -54,7 +54,7 @@ file_not_found_exception::file_not_found_exception(const string& message, const 
   this->help_link(help_link);
 }
 
-file_not_found_exception::file_not_found_exception(const string& message, const std::exception& inner_exception, const std::error_code& error, const string& help_link, const xtd::diagnostics::stack_frame& stack_frame) : io_exception(message, stack_frame), defined_message_ {true} {
+file_not_found_exception::file_not_found_exception(const string& message, const std::exception& inner_exception, const std::error_code& error, const string& help_link, const stack_frame& stack_frame) : io_exception(message, stack_frame), defined_message_ {true} {
   error_code(error);
   this->help_link(help_link);
 }
