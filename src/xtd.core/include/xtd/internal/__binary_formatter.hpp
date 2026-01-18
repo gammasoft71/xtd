79cc0c48c59a9a894de9da1f92209239ab5066d5
Gammasoft <gammasoft71@gmail.com>
2026-01-18 15:28:36 +0100

    Using trailing return type

--- src/xtd.core/include/xtd/internal/__binary_formatter.hpp
+++ src/xtd.core/include/xtd/internal/__binary_formatter.hpp
@@ -12,7 +12,7 @@
 
 /// @cond
 template<class char_t, class value_t>
-inline std::basic_string<char_t> __binary_formatter(value_t value, int precision) {
+[[nodiscard]] inline auto __binary_formatter(value_t value, int precision) -> std::basic_string<char_t> {
   std::basic_string<char_t> result = std::bitset<sizeof(value) * 8>(value).to_string(char_t('0'), char_t('1'));
   while (result[0] != 0 && result[0] == char_t('0'))
     result.erase(0, 1);
