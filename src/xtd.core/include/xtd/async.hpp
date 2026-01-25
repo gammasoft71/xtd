2358f620c157134eb845c9fbc6d4e0df01c6a2aa
Gammasoft <gammasoft71@gmail.com>
2026-01-25 21:35:53 +0100

    rename xtd::async to xtd::async_task

--- src/xtd.core/include/xtd/async.hpp
+++ src/xtd.core/include/xtd/async.hpp
@@ -1,49 +1,64 @@
 /// @file
-/// @brief Contains xtd::async struct.
+/// @brief Contains xtd::async_task struct.
 /// @copyright Copyright (c) 2025 Gammasoft. All rights reserved.
 #pragma once
-#include "xtd/threading/manual_reset_event.hpp"
+#include "runtime/exception_services/exception_dispatch_info.hpp"
+#include "threading/manual_reset_event.hpp"
 #include <coroutine>
 
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
-  /// @brief Represents the async struct.
+  /// @brief Represents the async_task struct.
   /// @par Header
   /// ```cpp
-  /// #include <xtd/async>
+  /// #include <xtd/async_task>
   /// ```
   /// @par Namespace
   /// xtd
   /// @par Library
   /// xtd.core
   /// @ingroup xtd_core types
-  struct async {
+  struct [[nodiscard]] async_task {
     struct promise_type {
       xtd::threading::manual_reset_event completed;
+      xtd::runtime::exception_services::exception_dispatch_info exception;
       
-      async get_return_object() { return async{this}; }
+      async_task get_return_object() {
+        //return async_task{this};
+        return async_task{std::coroutine_handle<promise_type>::from_promise(*this)};
+      }
       
       std::suspend_never initial_suspend() { return {}; }
       
       auto final_suspend() noexcept {
         struct final_awaiter {
           bool await_ready() noexcept { return false; }
-          void await_suspend(std::coroutine_handle<promise_type> h) noexcept {
-            h.promise().completed.set();
-          }
+          void await_suspend(std::coroutine_handle<promise_type> h) noexcept {h.promise().completed.set();}
           void await_resume() noexcept {}
         };
         return final_awaiter{};
       }
       
       void return_void() {}
-      void unhandled_exception() { std::terminate(); }
+      void unhandled_exception() {
+        exception = exception.capture();
+      }
     };
     
-    promise_type* promise;
-    
-    void wait() const {
-      promise->completed.wait_one();
+    std::coroutine_handle<promise_type> handle;
+
+    ~async_task() {
+      if (handle) handle.destroy();
     }
+    
+    auto wait() const -> void {wait(xtd::threading::timeout::infinite);}
+    auto wait(xtd::int32 milliseconds_timeout) const -> bool {
+      auto result = handle.promise().completed.wait_one(milliseconds_timeout);
+      if (handle.promise().exception) handle.promise().exception.rethrow();
+      return result;
+    }
+    auto wait(xtd::time_span timeout) const -> bool {return wait(xtd::as<xtd::int32>(timeout.total_milliseconds_duration().count()));}
   };
 }
+
+#define async_ -> xtd::async_task
