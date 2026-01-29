75268fd1e89171ea9d0099afaa79573884b01d11
Gammasoft <gammasoft71@gmail.com>
2026-01-29 23:56:04 +0100

    Update manual_tests.cpp

--- tests/xtd.tunit.manual_tests/src/manual_tests.cpp
+++ tests/xtd.tunit.manual_tests/src/manual_tests.cpp
@@ -1,23 +1,16 @@
 #include <xtd/tunit/constraints/assert_that>
 #include <xtd/tunit/test_class_attribute>
 #include <xtd/tunit/test_method_attribute>
-//#include <xtd/tunit/tunit_main>
-#include <xtd/tunit/console_unit_test>
+#include <xtd/tunit/tunit_main>
 #include <xtd/startup>
 
 namespace xtd::tunit::tests {
   class test_class_(manual_test_tunit) {
   public:
     void test_method_(test_method1) {
-      environment::exit(42);
-      assert_that(false).is().true_();
-    }
-    
-    static auto main() {
-      console_unit_test {}.run();
+      assert_that(true).is().true_();
     }
   };
 }
 
-//startup_(tunit_main_);
-startup_(xtd::tunit::tests::manual_test_tunit::main);
+startup_(tunit_main_);
