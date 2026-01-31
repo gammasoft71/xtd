e584a4f0b7353ac1705614e4165b3ed6e7895cf0
Gammasoft <gammasoft71@gmail.com>
2026-01-31 09:41:26 +0100

    Refactor result method to call basic_task wait

--- src/xtd.core/include/xtd/threading/tasks/task_result.hpp
+++ src/xtd.core/include/xtd/threading/tasks/task_result.hpp
@@ -90,7 +90,7 @@ namespace xtd {
         
         /// @{
         [[nodiscard]] auto result() const noexcept -> const result_t& {
-          wait();
+          basic_task<result_t>::wait();
           return basic_task<result_t>::data_->result;
         }
         /// @}
