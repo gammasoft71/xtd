f0ce8af72d1fa6c0a63980496580f2afe75cc083
Gammasoft <gammasoft71@gmail.com>
2026-02-05 10:21:15 +0100

    Add generienumerable_generator example

---
+++ examples/xtd.core.examples/generic_collections/generic_enumerable_generator/README.md
@@ -0,0 +1,34 @@
+# generic_enumerable_generator
+
+Shows how to use [xtd::collections::generic::enumerable_generator](https://gammasoft71.github.io/xtd/reference_guides/latest/classxtd_1_1collections_1_1generic_1_1enumerable__generator.html) generator.
+
+## Sources
+
+* [src/generic_enumerable_generator.cpp](src/generic_enumerable_generator.cpp)
+* [CMakeLists.txt](CMakeLists.txt)
+
+## Build and run
+
+Open "Command Prompt" or "Terminal". Navigate to the folder that contains the project and type the following:
+
+```cmake
+xtdc run
+```
+
+## Output
+
+```
+Odd numbers :
+  31
+  29
+  15
+  35
+  15
+   7
+  43
+  49
+   1
+  27
+   5
+Done!
+```
---
+++ examples/xtd.core.examples/generic_collections/generic_enumerable_generator/src/generic_enumerable_generator.cpp
@@ -0,0 +1,31 @@
+#include <xtd/xtd>
+
+auto odd_numbers_generator(int count = 100, int min_value = 0, int max_value = 100) -> enumerable_generator<int> {
+  auto rnd = xtd::random();
+  for (auto index = 0; index <= count; ++index)
+    co_yield rnd.next(min_value, max_value) | 1;
+}
+
+auto main() -> int {
+  println("Odd numbers : ");
+  auto odd_numbers = odd_numbers_generator()
+    .where([](auto n) {return n % 5 == 0;})
+    .distinct();
+  for (auto number : odd_numbers)
+    println("{,4}", number);
+  println("Done!");
+}
+
+// This code can produce the following output :
+//
+// Odd numbers :
+//   55
+//   75
+//   65
+//   25
+//    5
+//   15
+//   85
+//   45
+//   95
+// Done!
