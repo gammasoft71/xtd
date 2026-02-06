ff99b2ad6d34af0414609599a7d0d915cd118b62
Gammasoft <gammasoft71@gmail.com>
2026-02-06 11:38:43 +0100

    refactor exemple

--- examples/xtd.core.examples/coroutines/coroutine_lambda/src/coroutine_lambda.cpp
+++ examples/xtd.core.examples/coroutines/coroutine_lambda/src/coroutine_lambda.cpp
@@ -6,7 +6,7 @@ public:
     println("[main], thread {}] -> start", thread::current_thread().managed_thread_id());
     
     auto message = co_await [] -> task<string> {co_return "Hello, World!";}();
-    auto length = co_await [](auto message) -> task<size> {co_return message.length();}(message);
+    auto length = co_await [](auto message) -> task<size> {co_return message.size();}(message);
     
     println("message : {}", message);
     println("length  : {}", length);
