d7051045d574a6f8c9ff98a17a0249cc6be8e416
Gammasoft <gammasoft71@gmail.com>
2026-02-07 23:56:13 +0100

    Update itask.hpp

--- src/xtd.core/include/xtd/threading/tasks/itask.hpp
+++ src/xtd.core/include/xtd/threading/tasks/itask.hpp
@@ -12,7 +12,6 @@ namespace xtd {
   namespace threading {
     /// @brief The xtd::threading::tasks namespace provides types that simplify the work of writing concurrent and asynchronous code. The main types are xtd::threading::tasks::task which represents an asynchronous operation that can be waited on and cancelled, and xtd::threading::tasks::task <result_t>, which is a task that can return a value. The xtd::threading::tasks::task_factory class provides static methods for creating and starting tasks, and the xtd::threading::tasks::task_scheduler class provides the default thread scheduling infrastructure.
     namespace tasks {
-      /// @interface ITask
       /// @brief Represents an interface for an asynchronous operation.
       /// @par Library
       /// xtd.core
