662d24fa8ae7f463c7f782f48ab28288698eb68f
Gammasoft <gammasoft71@gmail.com>
2026-02-23 12:14:37 +0100

    Refactor culture_info

--- src/xtd.core.native.haiku/src/xtd/native/haiku/culture_info.cpp
+++ src/xtd.core.native.haiku/src/xtd/native/haiku/culture_info.cpp
@@ -1,17 +1,40 @@
 #define __XTD_CORE_NATIVE_LIBRARY__
 #include <xtd/native/culture_info>
-#include "../../../../include/xtd/native/haiku/shell_execute.hpp"
-#include "../../../../include/xtd/native/haiku/strings.hpp"
 #undef __XTD_CORE_NATIVE_LIBRARY__
-#include <algorithm>
-#include <cctype>
-#include <cstdlib>
+#include <AppFileInfo.h>
+#include <Entry.h>
+#include <File.h>
+#include <LocaleRoster.h>
+#include <Message.h>
+#include <Roster.h>
+#include <String.h>
+#include <image.h>
 #include <string>
+#include <vector>
 
 using namespace xtd::native;
 
 namespace {
-  std::string to_locale_name(const std::string& name) {
+  auto get_signature() -> std::string {
+    auto info = image_info {};
+    auto cookie = 0;
+    while (get_next_image_info(B_CURRENT_TEAM, &cookie, &info) == B_OK) {
+      if (info.type == B_APP_IMAGE) {
+        auto entry = BEntry {info.name};
+        auto ref = entry_ref {};
+        if (entry.GetRef(&ref) == B_OK) {
+          auto file = BFile {&ref, B_READ_ONLY};
+          auto appInfo = BAppFileInfo {&file};
+          char sig[B_MIME_TYPE_LENGTH];
+          if (appInfo.GetSignature(sig) == B_OK) return sig;
+        }
+        break;
+      }
+    }
+    return "application/x-vnd.console";
+  }
+  
+  auto to_locale_name(const std::string& name) -> std::string {
     if (name.empty() || name == "C" || name == "POSIX") return name;
     if (name == "C.UTF-8") return "C";
     if (name == "POSIX.UTF-8") return "POSIX";
@@ -22,19 +45,22 @@ namespace {
   }
 }
 
-std::string culture_info::current_locale_name() {
-  const char* locale_env = std::getenv("LC_ALL");
-  if (!locale_env) locale_env = std::getenv("LANG");
-  if (!locale_env) return "C";
-  return to_locale_name(std::string {locale_env});
+auto culture_info::current_locale_name() -> std::string {
+  auto language = BString {};
+  auto preferred = BMessage {};
+  if (BLocaleRoster::Default()->GetPreferredLanguages(&preferred) == B_OK && preferred.FindString("language", 0, &language) == B_OK)
+    return to_locale_name(language.String());
+  return "C";
 }
 
-std::vector<std::string> culture_info::system_locale_names() {
-  auto locales = std::vector<std::string> {"", "C", "POSIX"};
-  locales.reserve(800);
-  for (auto name : haiku::strings::split(haiku::shell_execute::run("locale", "-a"), {'\n'}))
-    locales.push_back(to_locale_name(name));
-  std::sort(locales.begin(), locales.end());
-  locales.erase(std::unique(locales.begin(), locales.end()), locales.end());
-  return locales;
+auto culture_info::system_locale_names() -> std::vector<std::string> {
+  auto roster = BLocaleRoster::Default();
+  auto languages = BMessage{};
+  if (roster->GetAvailableCatalogs(&languages, get_signature().c_str()) != B_OK) return {};
+  
+  auto language = BString{};
+  auto locale_names = std::vector<std::string>{};
+  for (auto i = 0; languages.FindString("language", i, &language) == B_OK; i++)
+    locale_names.push_back(to_locale_name(language.String()));
+  return locale_names;
 }
