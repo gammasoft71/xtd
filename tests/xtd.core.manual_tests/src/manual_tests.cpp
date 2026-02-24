687c45aa7ba145a70d07af1510d67a85ff6088a3
Gammasoft <gammasoft71@gmail.com>
2026-02-25 00:46:02 +0100

    Update manual_tests.cpp

--- tests/xtd.core.manual_tests/src/manual_tests.cpp
+++ tests/xtd.core.manual_tests/src/manual_tests.cpp
@@ -1,9 +1,15 @@
 #include <xtd/xtd>
 
 auto main() -> int {
-  println("Hello, World!");
-};
+  println("Command line arguments = {}", environment::get_command_line_args());
+  println("Location path = {}", reflection::assembly::get_executing_assembly().location_path());
+  println("Location = {}", reflection::assembly::get_executing_assembly().location());
+  println("File name = {}", reflection::assembly::get_executing_assembly().file_name());
+}
 
 // This code produces the following output :
 //
-// Hello, World!
+// Command line arguments = [./xtd.core.manual_tests, One, Two, Three Four, Five]
+// Location path = /Users/gammasoft71/Projects/xtd/build/tests/xtd.core.manual_tests/Debug
+// Localtion = /Users/gammasoft71/Projects/xtd/build/tests/xtd.core.manual_tests/Debug/xtd.core.manual_tests
+// File name = xtd.core.manual_tests
