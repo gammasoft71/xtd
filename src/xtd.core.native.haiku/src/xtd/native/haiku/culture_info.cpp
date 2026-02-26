22b59cc4da3af5aa67f9731beebdb98e7e5375a5
Gammasoft <gammasoft71@gmail.com>
2026-02-26 14:58:35 +0100

    Refactor culture_info on haiku

--- src/xtd.core.native.haiku/src/xtd/native/haiku/culture_info.cpp
+++ src/xtd.core.native.haiku/src/xtd/native/haiku/culture_info.cpp
@@ -1,5 +1,7 @@
 #define __XTD_CORE_NATIVE_LIBRARY__
 #include <xtd/native/culture_info>
+#include "../../../../include/xtd/native/haiku/shell_execute.hpp"
+#include "../../../../include/xtd/native/haiku/strings.hpp"
 #undef __XTD_CORE_NATIVE_LIBRARY__
 #include <AppFileInfo.h>
 #include <Entry.h>
@@ -16,25 +18,6 @@
 using namespace xtd::native;
 
 namespace {
-  auto get_signature() -> std::string {
-    auto info = image_info {};
-    auto cookie = 0;
-    while (get_next_image_info(B_CURRENT_TEAM, &cookie, &info) == B_OK) {
-      if (info.type == B_APP_IMAGE) {
-        auto entry = BEntry {info.name};
-        auto ref = entry_ref {};
-        if (entry.GetRef(&ref) == B_OK) {
-          auto file = BFile {&ref, B_READ_ONLY};
-          auto appInfo = BAppFileInfo {&file};
-          char sig[B_MIME_TYPE_LENGTH];
-          if (appInfo.GetSignature(sig) == B_OK) return sig;
-        }
-        break;
-      }
-    }
-    return "application/x-vnd.console";
-  }
-  
   auto to_locale_name(const std::string& name) -> std::string {
     if (name.empty() || name == "C" || name == "POSIX") return name;
     if (name == "C.UTF-8") return "C";
@@ -55,16 +38,14 @@ auto culture_info::current_locale_name() -> std::string {
 }
 
 auto culture_info::system_locale_names() -> std::vector<std::string> {
-  auto roster = BLocaleRoster::Default();
-  auto languages = BMessage{};
-  if (roster->GetAvailableCatalogs(&languages, get_signature().c_str()) != B_OK) return {};
-  
-  auto language = BString{};
-  auto locale_names = std::vector<std::string>{};
-  for (auto i = 0; languages.FindString("language", i, &language) == B_OK; i++)
+  auto locales = std::vector<std::string> {"C", "POSIX"};
+  locales.reserve(800);
+  for (auto name : haiku::strings::split(haiku::shell_execute::run("locale", "-a"), {'\n'}))
     try {
-      locale_names.push_back(std::locale {to_locale_name(language.String())}.name());
+      locales.push_back(std::locale {to_locale_name(name)}.name());
     } catch (...) {
     }
-  return locale_names;
+  std::sort(locales.begin(), locales.end());
+  locales.erase(std::unique(locales.begin(), locales.end()), locales.end());
+  return locales;
 }
