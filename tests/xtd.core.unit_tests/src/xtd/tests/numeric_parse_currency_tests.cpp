cad684a75b38028475806a1e67051c48c640c9b4
Gammasoft <gammasoft71@gmail.com>
2026-03-03 21:22:24 +0100

    Using xtd::tunit::register_test_class_types

--- tests/xtd.core.unit_tests/src/xtd/tests/numeric_parse_currency_tests.cpp
+++ tests/xtd.core.unit_tests/src/xtd/tests/numeric_parse_currency_tests.cpp
@@ -2,7 +2,7 @@
 #include <xtd/format_exception>
 #include <xtd/globalization/culture_info>
 #include <xtd/tunit/assert>
-#include <xtd/tunit/test_class_attribute>
+#include <xtd/tunit/register_test_class_types>
 #include <xtd/tunit/test_method_attribute>
 
 using namespace xtd;
@@ -12,28 +12,7 @@ namespace xtd::tests {
   template<class value_t>
   class numeric_parse_string_currency_tests;
   
-  test_class_attribute<numeric_parse_string_currency_tests<char>> numeric_parse_string_currency_tests_class_char_attr {"numeric_parse_string_currency_tests<char>"};
-  test_class_attribute<numeric_parse_string_currency_tests<unsigned char>> numeric_parse_string_currency_tests_class_unsigned_char_attr {"numeric_parse_string_currency_tests<unsigned_char>"};
-  test_class_attribute<numeric_parse_string_currency_tests<short>> numeric_parse_string_currency_tests_class_short_attr {"numeric_parse_string_currency_tests<short>"};
-  test_class_attribute<numeric_parse_string_currency_tests<unsigned short>> numeric_parse_string_currency_tests_class_unsigned_short_attr {"numeric_parse_string_currency_tests<unsigned_short>"};
-  test_class_attribute<numeric_parse_string_currency_tests<int>> numeric_parse_string_currency_tests_class_int_attr {"numeric_parse_string_currency_tests<int>"};
-  test_class_attribute<numeric_parse_string_currency_tests<unsigned int>> numeric_parse_string_currency_tests_class_unsigned_int_attr {"numeric_parse_string_currency_tests<unsigned_int>"};
-  test_class_attribute<numeric_parse_string_currency_tests<long>> numeric_parse_string_currency_tests_class_long_attr {"numeric_parse_string_currency_tests<long>"};
-  test_class_attribute<numeric_parse_string_currency_tests<unsigned long>> numeric_parse_string_currency_tests_class_unsigned_long_attr {"numeric_parse_string_currency_tests<unsigned_long>"};
-  test_class_attribute<numeric_parse_string_currency_tests<long long>> numeric_parse_string_currency_tests_class_long_long_attr {"numeric_parse_string_currency_tests<long_long>"};
-  test_class_attribute<numeric_parse_string_currency_tests<unsigned long long>> numeric_parse_string_currency_tests_class_unsigned_long_long_attr {"numeric_parse_string_currency_tests<unsigned_long_long>"};
-  test_class_attribute<numeric_parse_string_currency_tests<sbyte>> numeric_parse_string_currency_tests_class_sbyte_attr {"numeric_parse_string_currency_tests<sbyte>"};
-  test_class_attribute<numeric_parse_string_currency_tests<int16>> numeric_parse_string_currency_tests_class_int16_attr {"numeric_parse_string_currency_tests<int16>"};
-  test_class_attribute<numeric_parse_string_currency_tests<int32>> numeric_parse_string_currency_tests_class_int32_attr {"numeric_parse_string_currency_tests<int32>"};
-  test_class_attribute<numeric_parse_string_currency_tests<int64>> numeric_parse_string_currency_tests_class_int64_attr {"numeric_parse_string_currency_tests<int64>"};
-  test_class_attribute<numeric_parse_string_currency_tests<intptr>> numeric_parse_string_currency_tests_class_intptr_attr {"numeric_parse_string_currency_tests<intptr>"};
-  test_class_attribute<numeric_parse_string_currency_tests<ptrdiff_t>> numeric_parse_string_currency_tests_class_ptrdiff_t_attr {"numeric_parse_string_currency_tests<ptrdiff_t>"};
-  test_class_attribute<numeric_parse_string_currency_tests<size_t>> numeric_parse_string_currency_tests_class_size_t_attr {"numeric_parse_string_currency_tests<size_t>"};
-  test_class_attribute<numeric_parse_string_currency_tests<xtd::byte>> numeric_parse_string_currency_tests_class_bute_attr {"numeric_parse_string_currency_tests<xtd::byte>"};
-  test_class_attribute<numeric_parse_string_currency_tests<uint16>> numeric_parse_string_currency_tests_class_uint16_attr {"numeric_parse_string_currency_tests<uint16>"};
-  test_class_attribute<numeric_parse_string_currency_tests<uint32>> numeric_parse_string_currency_tests_class_uint32_attr {"numeric_parse_string_currency_tests<uint32>"};
-  test_class_attribute<numeric_parse_string_currency_tests<uint64>> numeric_parse_string_currency_tests_class_uint64_attr {"numeric_parse_string_currency_tests<uint64>"};
-  test_class_attribute<numeric_parse_string_currency_tests<uintptr>> numeric_parse_string_currency_tests_class_uintptr_attr {"numeric_parse_string_currency_tests<uintptr>"};
+  auto register_numeric_parse_string_currency_tests = register_test_class_types<numeric_parse_string_currency_tests, char, unsigned char, short, unsigned short, int, unsigned int, long, unsigned long, long long, unsigned long long, sbyte, int16, int32, int64, intptr, ptrdiff, size, byte, uint16, uint32, uint64, uintptr> {};
   
   template<class value_t>
   class numeric_parse_string_currency_tests : public test_class {
