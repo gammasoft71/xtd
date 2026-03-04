147cee898619f542a09a98c7ede08e0314cd3f8e
Gammasoft <gammasoft71@gmail.com>
2026-03-04 21:52:01 +0100

    Using generic_test_class_ helper

--- tests/xtd.core.unit_tests/src/xtd/tests/numeric_parse_number_tests.cpp
+++ tests/xtd.core.unit_tests/src/xtd/tests/numeric_parse_number_tests.cpp
@@ -2,20 +2,11 @@
 #include <xtd/format_exception>
 #include <xtd/globalization/culture_info>
 #include <xtd/tunit/assert>
-#include <xtd/tunit/register_test_class_types>
+#include <xtd/tunit/generic_test_class>
 #include <xtd/tunit/test_method_attribute>
 
-using namespace xtd;
-using namespace xtd::tunit;
-
 namespace xtd::tests {
-  template<class value_t>
-  class numeric_parse_string_number_tests;
-  
-  auto register_numeric_parse_string_number_tests = register_test_class_types<numeric_parse_string_number_tests, char, unsigned char, short, unsigned short, int, unsigned int, long, unsigned long, long long, unsigned long long, sbyte, int16, int32, int64, intptr, ptrdiff, size, byte, uint16, uint32, uint64, uintptr> {};
-
-  template<class value_t>
-  class numeric_parse_string_number_tests : public test_class {
+  generic_test_class_(numeric_parse_string_number_tests, char, unsigned char, short, unsigned short, int, unsigned int, long, unsigned long, long long, unsigned long long, sbyte, int16, int32, int64, intptr, ptrdiff, size, byte, uint16, uint32, uint64, uintptr) {
     inline static xtd::globalization::culture_info previous_culture;
     
     static void class_initialize_(class_initialize) {
@@ -28,114 +19,114 @@ namespace xtd::tests {
     }
     
     auto test_method_(parse_styles_number) {
-      assert::are_equal(static_cast<value_t>(42), xtd::parse<value_t>("42", number_styles::number));
+      assert::are_equal(static_cast<type_t>(42), xtd::parse<type_t>("42", number_styles::number));
     }
     
     auto test_method_(parse_styles_number_with_leading_white_spaces) {
-      assert::are_equal(static_cast<value_t>(42), xtd::parse<value_t>(" \f\n\r\t\v42", number_styles::number));
+      assert::are_equal(static_cast<type_t>(42), xtd::parse<type_t>(" \f\n\r\t\v42", number_styles::number));
     }
     
     auto test_method_(parse_styles_number_with_trailing_white_spaces) {
-      assert::are_equal(static_cast<value_t>(42), xtd::parse<value_t>("42 \f\n\r\t\v", number_styles::number));
+      assert::are_equal(static_cast<type_t>(42), xtd::parse<type_t>("42 \f\n\r\t\v", number_styles::number));
     }
     
     auto test_method_(parse_styles_number_with_leading_plus_sign) {
-      assert::are_equal(static_cast<value_t>(42), xtd::parse<value_t>("+42", number_styles::number));
+      assert::are_equal(static_cast<type_t>(42), xtd::parse<type_t>("+42", number_styles::number));
     }
     
     auto test_method_(parse_styles_number_with_two_leading_plus_sign) {
-      assert::throws<format_exception>([] {xtd::parse<value_t>("++42", number_styles::number);});
+      assert::throws<format_exception>([] {xtd::parse<type_t>("++42", number_styles::number);});
     }
     
     auto test_method_(parse_styles_number_with_leading_minus_sign) {
-      if (std::is_signed<value_t>::value)
-        assert::are_equal(static_cast<value_t>(-42), xtd::parse<value_t>("-42", number_styles::number));
+      if (std::is_signed<type_t>::value)
+        assert::are_equal(static_cast<type_t>(-42), xtd::parse<type_t>("-42", number_styles::number));
       #if !defined(__linux__)
       else
-        assert::throws<format_exception>([] {xtd::parse<value_t>("-42", number_styles::number);});
+        assert::throws<format_exception>([] {xtd::parse<type_t>("-42", number_styles::number);});
       #endif
     }
     
     auto test_method_(parse_styles_number_with_two_leading_minus_sign) {
-      assert::throws<format_exception>([] {xtd::parse<value_t>("--42", number_styles::number);});
+      assert::throws<format_exception>([] {xtd::parse<type_t>("--42", number_styles::number);});
     }
     
     auto test_method_(parse_styles_number_with_trailing_plus_sign) {
-      assert::are_equal(static_cast<value_t>(42), xtd::parse<value_t>("42+", number_styles::number));
+      assert::are_equal(static_cast<type_t>(42), xtd::parse<type_t>("42+", number_styles::number));
     }
     
     auto test_method_(parse_styles_number_with_two_trailing_plus_sign) {
-      assert::throws<format_exception>([] {xtd::parse<value_t>("42++", number_styles::number);});
+      assert::throws<format_exception>([] {xtd::parse<type_t>("42++", number_styles::number);});
     }
     
     auto test_method_(parse_styles_number_with_trailing_minus_sign) {
-      if (std::is_signed<value_t>::value)
-        assert::are_equal(static_cast<value_t>(-42), xtd::parse<value_t>("42-", number_styles::number));
+      if (std::is_signed<type_t>::value)
+        assert::are_equal(static_cast<type_t>(-42), xtd::parse<type_t>("42-", number_styles::number));
       #if !defined(__linux__)
       else
-        assert::throws<format_exception>([] {xtd::parse<value_t>("42-", number_styles::number);});
+        assert::throws<format_exception>([] {xtd::parse<type_t>("42-", number_styles::number);});
       #endif
     }
     
     auto test_method_(parse_styles_number_with_two_trailing_minus_sign) {
-      assert::throws<format_exception>([] {xtd::parse<value_t>("42--", number_styles::number);});
+      assert::throws<format_exception>([] {xtd::parse<type_t>("42--", number_styles::number);});
     }
     
     auto test_method_(parse_styles_number_with_parentheses) {
-      if (std::is_signed<value_t>::value)
-        assert::throws<format_exception>([] {xtd::parse<value_t>("(42)", number_styles::number);});
+      if (std::is_signed<type_t>::value)
+        assert::throws<format_exception>([] {xtd::parse<type_t>("(42)", number_styles::number);});
       else
-        assert::throws<format_exception>([] {xtd::parse<value_t>("(42)", number_styles::number);});
+        assert::throws<format_exception>([] {xtd::parse<type_t>("(42)", number_styles::number);});
     }
     
     auto test_method_(parse_styles_number_with_two_parentheses) {
-      assert::throws<format_exception>([] {xtd::parse<value_t>("((42))", number_styles::number);});
+      assert::throws<format_exception>([] {xtd::parse<type_t>("((42))", number_styles::number);});
     }
     
     auto test_method_(parse_styles_number_with_decimal_point) {
-      assert::are_equal(static_cast<value_t>(42), xtd::parse<value_t>("42.0", number_styles::number));
+      assert::are_equal(static_cast<type_t>(42), xtd::parse<type_t>("42.0", number_styles::number));
     }
     
     auto test_method_(parse_styles_number_with_two_decimal_point) {
-      assert::throws<format_exception>([] {xtd::parse<value_t>("4.2.0", number_styles::number);});
+      assert::throws<format_exception>([] {xtd::parse<type_t>("4.2.0", number_styles::number);});
     }
     
     auto test_method_(parse_styles_number_with_thousands) {
-      if (!std::is_same<value_t, sbyte>::value && !std::is_same<value_t, char>::value && !std::is_same<value_t, unsigned char>::value)
-        assert::are_equal(static_cast<value_t>(1234), xtd::parse<value_t>("1,234", number_styles::number));
+      if (!std::is_same<type_t, sbyte>::value && !std::is_same<type_t, char>::value && !std::is_same<type_t, unsigned char>::value)
+        assert::are_equal(static_cast<type_t>(1234), xtd::parse<type_t>("1,234", number_styles::number));
     }
     
     auto test_method_(parse_styles_number_with_two_thousands) {
-      if (!std::is_same<value_t, sbyte>::value && !std::is_same<value_t, char>::value && !std::is_same<value_t, unsigned char>::value)
-        assert::throws<format_exception>([] {xtd::parse<value_t>("1,,234", number_styles::number);});
+      if (!std::is_same<type_t, sbyte>::value && !std::is_same<type_t, char>::value && !std::is_same<type_t, unsigned char>::value)
+        assert::throws<format_exception>([] {xtd::parse<type_t>("1,,234", number_styles::number);});
     }
     
     auto test_method_(parse_styles_number_with_exponent) {
-      assert::throws<format_exception>([] {xtd::parse<value_t>("4E+01", number_styles::number);});
+      assert::throws<format_exception>([] {xtd::parse<type_t>("4E+01", number_styles::number);});
     }
     
     auto test_method_(parse_styles_number_with_leading_currency_symbol) {
-      assert::throws<format_exception>([] {xtd::parse<value_t>("$42", number_styles::number);});
+      assert::throws<format_exception>([] {xtd::parse<type_t>("$42", number_styles::number);});
     }
     
     auto test_method_(parse_styles_number_with_trailing_currency_symbol) {
-      assert::throws<format_exception>([] {xtd::parse<value_t>("42$", number_styles::number);});
+      assert::throws<format_exception>([] {xtd::parse<type_t>("42$", number_styles::number);});
     }
     
     auto test_method_(parse_styles_number_with_binary_specifier) {
-      assert::throws<format_exception>([] {xtd::parse<value_t>("0b101010", number_styles::number);});
+      assert::throws<format_exception>([] {xtd::parse<type_t>("0b101010", number_styles::number);});
     }
     
     auto test_method_(parse_styles_number_with_octal_specifier) {
-      assert::are_equal(static_cast<value_t>(52), xtd::parse<value_t>("052", number_styles::number));
+      assert::are_equal(static_cast<type_t>(52), xtd::parse<type_t>("052", number_styles::number));
     }
     
     auto test_method_(parse_styles_number_with_hexa_specifier) {
-      assert::throws<format_exception>([] {xtd::parse<value_t>("0x2A", number_styles::number);});
+      assert::throws<format_exception>([] {xtd::parse<type_t>("0x2A", number_styles::number);});
     }
     
     auto test_method_(parse_styles_number_invalid) {
-      assert::throws<format_exception>([] {xtd::parse<value_t>("z42", number_styles::number);});
+      assert::throws<format_exception>([] {xtd::parse<type_t>("z42", number_styles::number);});
     }
   };
 }
