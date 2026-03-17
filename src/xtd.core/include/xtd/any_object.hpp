cfaacf28d10384883623a97823fb5f02a5d64bc4
Gammasoft <gammasoft71@gmail.com>
2026-03-17 20:21:12 +0100

    using trailing return type

--- src/xtd.core/include/xtd/any_object.hpp
+++ src/xtd.core/include/xtd/any_object.hpp
@@ -59,12 +59,12 @@ namespace xtd {
     /// @brief Gets a value indicating whether the current xtd::any_object object has a valid value of its underlying type.
     /// @return `true` if the current xtd::any_object object has a value; `false` if the current xtd::any_object object has no value.
     /// @remarks If the xtd::any_object::has_value property is `true`, the value of the current xtd::any_object object can be accessed with the xtd::any_object::value property. Otherwise, attempting to access its value throws an xtd::invalid_operation_exception exception.
-    bool has_value() const noexcept;
+    [[nodiscard]] auto has_value() const noexcept -> bool;
     
     /// @brief Gets the value of the current xtd::any_object object if it has been assigned a valid underlying value.
     /// @return The value of the current xtd::any_object object if the xtd::any_object::has_value property is `true`. An exception is thrown if the xtd::any_object::has_value property is `false`.
     /// @exception xtd::invalid_operation_exception The xtd::any_object::has_value property is `false`.
-    const object& value() const;
+    [[nodiscard]] auto value() const -> const object&;
     /// @}
     
     /// @name Public Methods
@@ -80,28 +80,28 @@ namespace xtd {
     /// | Less than zero    | This instance is less than obj.    |
     /// | Zero              | This instance is equal to obj.     |
     /// | Greater than zero | This instance is greater than obj. |
-    int32 compare_to(const any_object& other) const noexcept override;
+    [[nodiscard]] auto compare_to(const any_object& other) const noexcept -> int32 override;
     
     /// @brief Determines whether the specified object is equal to the current object.
     /// @param obj The object to compare with the current object.
     /// @return `true` if the specified object is equal to the current object. otherwise, `false`.
-    bool equals(const object& other) const noexcept override;
+    [[nodiscard]] auto equals(const object& other) const noexcept -> bool override;
     /// @brief Indicates whether the current object is equal to another object of the same type.
     /// @param obj An object to compare with this object.
     /// @return `true` if the current object is equal to the other parameter; otherwise, `false`.
-    bool equals(const any_object& other) const noexcept override;
+    [[nodiscard]] auto equals(const any_object& other) const noexcept -> bool override;
     
     /// @brief Serves as a hash function for a particular type.
     /// @return A hash code for the current object.
-    xtd::size get_hash_code() const noexcept override;
+    [[nodiscard]] auto get_hash_code() const noexcept -> xtd::size override;
     
     /// @brief Returns a xtd::string that represents the current object.
     /// @return A string that represents the current object.
-    string to_string() const noexcept override;
+    [[nodiscard]] auto to_string() const noexcept -> xtd::string override;
     
     /// @brief Reset the current object. Set the current object to null.
     /// @remarks If xtd::any_object::has_values is `true`, destroys the contained object; otherwise does nothing.
-    void reset() noexcept;
+    auto reset() noexcept -> void;
     /// @}
     
     /// @name Public Operators
@@ -110,61 +110,61 @@ namespace xtd {
     /// @brief Copy assignment operator. Replaces the contents with a copy of the contents of other.
     /// @param other Another polymorphic wrapper.
     /// @return This current instance.
-    any_object& operator =(const any_object& other) noexcept = default;
+    auto operator =(const any_object& other) noexcept -> any_object& = default;
     /// @brief Move assignment operator. Replaces the contents with a copy of the contents of other.
     /// @param other Another polymorphic wrapper.
     /// @return This current instance.
-    any_object& operator =(any_object&& other) noexcept;
+    auto operator =(any_object&& other) noexcept -> any_object&;
     /// @}
     
   private:
     template<class type_t>
-    ptr<object> boxing_ptr(const type_t& value) noexcept {return new_ptr<typename __box_enum_or_object__<type_t, typename std::is_enum<type_t>::type>::type>(value);}
+    [[nodiscard]] auto boxing_ptr(const type_t& value) noexcept -> ptr<object> {return new_ptr<typename __box_enum_or_object__<type_t, typename std::is_enum<type_t>::type>::type>(value);}
     template<class type_t, class ...args_t>
-    ptr<object> boxing_ptr(args_t&& ...args) noexcept {return new box<type_t>(args...);}
-    //ptr<object> boxing_ptr(const object& value) noexcept;
-    ptr<object> boxing_ptr(const char* value) noexcept;
-    ptr<object> boxing_ptr(const char8* value) noexcept;
-    ptr<object> boxing_ptr(const char16* value) noexcept;
-    ptr<object> boxing_ptr(const char32* value) noexcept;
-    ptr<object> boxing_ptr(const wchar* value) noexcept;
-    ptr<object> boxing_ptr(const char& value) noexcept;
-    ptr<object> boxing_ptr(const char8& value) noexcept;
-    ptr<object> boxing_ptr(const char16& value) noexcept;
-    ptr<object> boxing_ptr(const char32& value) noexcept;
-    ptr<object> boxing_ptr(const wchar& value) noexcept;
-    ptr<object> boxing_ptr(char& value) noexcept;
-    ptr<object> boxing_ptr(char8& value) noexcept;
-    ptr<object> boxing_ptr(char16& value) noexcept;
-    ptr<object> boxing_ptr(char32& value) noexcept;
-    ptr<object> boxing_ptr(wchar& value) noexcept;
-    ptr<object> boxing_ptr(const xtd::byte& value) noexcept;
-    ptr<object> boxing_ptr(const int16& value) noexcept;
-    ptr<object> boxing_ptr(const int32& value) noexcept;
-    ptr<object> boxing_ptr(const int64& value) noexcept;
-    ptr<object> boxing_ptr(const slong& value) noexcept;
-    ptr<object> boxing_ptr(const sbyte& value) noexcept;
-    ptr<object> boxing_ptr(const uint16& value) noexcept;
-    ptr<object> boxing_ptr(const uint32& value) noexcept;
-    ptr<object> boxing_ptr(const uint64& value) noexcept;
-    ptr<object> boxing_ptr(const xtd::ulong& value) noexcept;
-    ptr<object> boxing_ptr(xtd::byte& value) noexcept;
-    ptr<object> boxing_ptr(int16& value) noexcept;
-    ptr<object> boxing_ptr(int32& value) noexcept;
-    ptr<object> boxing_ptr(int64& value) noexcept;
-    ptr<object> boxing_ptr(slong& value) noexcept;
-    ptr<object> boxing_ptr(sbyte& value) noexcept;
-    ptr<object> boxing_ptr(uint16& value) noexcept;
-    ptr<object> boxing_ptr(uint32& value) noexcept;
-    ptr<object> boxing_ptr(uint64& value) noexcept;
-    ptr<object> boxing_ptr(xtd::ulong& value) noexcept;
-    ptr<object> boxing_ptr(const float& value) noexcept;
-    ptr<object> boxing_ptr(const double& value) noexcept;
-    ptr<object> boxing_ptr(const decimal& value) noexcept;
-    ptr<object> boxing_ptr(float& value) noexcept;
-    ptr<object> boxing_ptr(double& value) noexcept;
-    ptr<object> boxing_ptr(decimal& value) noexcept;
-    ptr<object> boxing_ptr(std::nullptr_t value) noexcept;
+    [[nodiscard]] auto boxing_ptr(args_t&& ...args) noexcept -> ptr<object> {return new box<type_t>(args...);}
+    //[[nodiscard]] auto boxing_ptr(const object& value) noexcept -> ptr<object>;
+    [[nodiscard]] auto boxing_ptr(const char* value) noexcept -> ptr<object>;
+    [[nodiscard]] auto boxing_ptr(const char8* value) noexcept -> ptr<object>;
+    [[nodiscard]] auto boxing_ptr(const char16* value) noexcept -> ptr<object>;
+    [[nodiscard]] auto boxing_ptr(const char32* value) noexcept -> ptr<object>;
+    [[nodiscard]] auto boxing_ptr(const wchar* value) noexcept -> ptr<object>;
+    [[nodiscard]] auto boxing_ptr(const char& value) noexcept -> ptr<object>;
+    [[nodiscard]] auto boxing_ptr(const char8& value) noexcept -> ptr<object>;
+    [[nodiscard]] auto boxing_ptr(const char16& value) noexcept -> ptr<object>;
+    [[nodiscard]] auto boxing_ptr(const char32& value) noexcept -> ptr<object>;
+    [[nodiscard]] auto boxing_ptr(const wchar& value) noexcept -> ptr<object>;
+    [[nodiscard]] auto boxing_ptr(char& value) noexcept -> ptr<object>;
+    [[nodiscard]] auto boxing_ptr(char8& value) noexcept -> ptr<object>;
+    [[nodiscard]] auto boxing_ptr(char16& value) noexcept -> ptr<object>;
+    [[nodiscard]] auto boxing_ptr(char32& value) noexcept -> ptr<object>;
+    [[nodiscard]] auto boxing_ptr(wchar& value) noexcept -> ptr<object>;
+    [[nodiscard]] auto boxing_ptr(const xtd::byte& value) noexcept -> ptr<object>;
+    [[nodiscard]] auto boxing_ptr(const int16& value) noexcept -> ptr<object>;
+    [[nodiscard]] auto boxing_ptr(const int32& value) noexcept -> ptr<object>;
+    [[nodiscard]] auto boxing_ptr(const int64& value) noexcept -> ptr<object>;
+    [[nodiscard]] auto boxing_ptr(const slong& value) noexcept -> ptr<object>;
+    [[nodiscard]] auto boxing_ptr(const sbyte& value) noexcept -> ptr<object>;
+    [[nodiscard]] auto boxing_ptr(const uint16& value) noexcept -> ptr<object>;
+    [[nodiscard]] auto boxing_ptr(const uint32& value) noexcept -> ptr<object>;
+    [[nodiscard]] auto boxing_ptr(const uint64& value) noexcept -> ptr<object>;
+    [[nodiscard]] auto boxing_ptr(const xtd::ulong& value) noexcept -> ptr<object>;
+    [[nodiscard]] auto boxing_ptr(xtd::byte& value) noexcept -> ptr<object>;
+    [[nodiscard]] auto boxing_ptr(int16& value) noexcept -> ptr<object>;
+    [[nodiscard]] auto boxing_ptr(int32& value) noexcept -> ptr<object>;
+    [[nodiscard]] auto boxing_ptr(int64& value) noexcept -> ptr<object>;
+    [[nodiscard]] auto boxing_ptr(slong& value) noexcept -> ptr<object>;
+    [[nodiscard]] auto boxing_ptr(sbyte& value) noexcept -> ptr<object>;
+    [[nodiscard]] auto boxing_ptr(uint16& value) noexcept -> ptr<object>;
+    [[nodiscard]] auto boxing_ptr(uint32& value) noexcept -> ptr<object>;
+    [[nodiscard]] auto boxing_ptr(uint64& value) noexcept -> ptr<object>;
+    [[nodiscard]] auto boxing_ptr(xtd::ulong& value) noexcept -> ptr<object>;
+    [[nodiscard]] auto boxing_ptr(const float& value) noexcept -> ptr<object>;
+    [[nodiscard]] auto boxing_ptr(const double& value) noexcept -> ptr<object>;
+    [[nodiscard]] auto boxing_ptr(const decimal& value) noexcept -> ptr<object>;
+    [[nodiscard]] auto boxing_ptr(float& value) noexcept -> ptr<object>;
+    [[nodiscard]] auto boxing_ptr(double& value) noexcept -> ptr<object>;
+    [[nodiscard]] auto boxing_ptr(decimal& value) noexcept -> ptr<object>;
+    [[nodiscard]] auto boxing_ptr(std::nullptr_t value) noexcept -> ptr<object>;
     
     ptr<object> value_;
   };
@@ -175,12 +175,12 @@ namespace xtd {
   
   template<class type_t>
   struct __is_enum_any_object__<type_t, std::true_type> {
-    bool operator()(const any_object& o) const {return is<enum_object<type_t>>(o.value());}
+    [[nodiscard]] auto operator()(const any_object& o) const -> bool {return is<enum_object<type_t>>(o.value());}
   };
   
   template<class type_t>
   struct __is_enum_any_object__<type_t, std::false_type> {
-    bool operator()(const any_object& o) const {return false;}
+    [[nodiscard]] auto operator()(const any_object& o) const -> bool {return false;}
   };
   
   template<class type_t, class bool_t>
@@ -188,17 +188,17 @@ namespace xtd {
   
   template<class type_t>
   struct __is_polymorphic_any_object__<type_t, std::true_type> {
-    bool operator()(const any_object& o) const {return is<type_t>(o.value());}
+    [[nodiscard]] auto operator()(const any_object& o) const -> bool {return is<type_t>(o.value());}
   };
   
   template<class type_t>
   struct __is_polymorphic_any_object__<type_t, std::false_type> {
-    bool operator()(const any_object& o) const {return __is_enum_any_object__<type_t, typename std::is_enum<type_t>::type> {}(o);}
+    [[nodiscard]] auto operator()(const any_object& o) const -> bool {return __is_enum_any_object__<type_t, typename std::is_enum<type_t>::type> {}(o);}
   };
   
   template<class type_t>
   requires (!std::is_null_pointer_v<type_t> && (!std::integral<type_t> || std::same_as<type_t, bool>))
-  bool is(any_object& o) {
+  [[nodiscard]] auto is(any_object& o) -> bool {
     if (!o.has_value()) return false;
     if (is<box<type_t>>(o.value())) return true;
     return __is_polymorphic_any_object__<type_t, typename std::is_polymorphic<type_t>::type > {}(o);
@@ -206,7 +206,7 @@ namespace xtd {
   
   template<class type_t>
   requires (!std::is_null_pointer_v<type_t> && (!std::integral<type_t> || std::same_as<type_t, bool>))
-  bool is(const any_object& o) {
+  [[nodiscard]] auto is(const any_object& o) -> bool {
     if (!o.has_value()) return false;
     if (is<box<type_t>>(o.value())) return true;
     return __is_polymorphic_any_object__<type_t, typename std::is_polymorphic<type_t>::type > {}(o);
@@ -214,7 +214,7 @@ namespace xtd {
   
   template<class type_t>
   requires (!std::is_null_pointer_v<type_t> && std::integral<type_t> && !std::same_as<type_t, bool>)
-  bool is(any_object& o) {
+  [[nodiscard]] auto is(any_object& o) -> bool {
     if (!o.has_value()) return false;
     if (xtd::is<xtd::box_integer<xtd::byte>>(o.value())) return xtd::box_integer<type_t>::is_valid(static_cast<const xtd::box_integer<xtd::byte>&>(o.value()).value);
     if (xtd::is<xtd::box_integer<xtd::int16>>(o.value())) return xtd::box_integer<type_t>::is_valid(static_cast<const xtd::box_integer<xtd::int16>&>(o.value()).value);
@@ -231,7 +231,7 @@ namespace xtd {
   
   template<class type_t>
   requires (!std::is_null_pointer_v<type_t> && std::integral<type_t> && !std::same_as<type_t, bool>)
-  bool is(const any_object& o) {
+  [[nodiscard]] auto is(const any_object& o) -> bool {
     if (!o.has_value()) return false;
     if (xtd::is<xtd::box_integer<xtd::byte>>(o.value())) return xtd::box_integer<type_t>::is_valid(static_cast<const xtd::box_integer<xtd::byte>&>(o.value()).value);
     if (xtd::is<xtd::box_integer<xtd::int16>>(o.value())) return xtd::box_integer<type_t>::is_valid(static_cast<const xtd::box_integer<xtd::int16>&>(o.value()).value);
@@ -248,24 +248,24 @@ namespace xtd {
 
   template<class type_t>
   requires std::is_null_pointer_v<type_t>
-  bool is(any_object& o) {
+  [[nodiscard]] auto is(any_object& o) -> bool {
     return !o.has_value();
   }
   
   template<class type_t>
   requires std::is_null_pointer_v<type_t>
-  bool is(const any_object& o) {
+  [[nodiscard]] auto is(const any_object& o) -> bool {
     return !o.has_value();
   }
 
   template<class type_t>
-  bool is(any_object* o) {
+  [[nodiscard]] auto is(any_object* o) -> bool {
     if (is<box<type_t>>(o->value())) return true;
     return __is_polymorphic_any_object__<type_t, typename std::is_polymorphic<type_t>::type > {}(*o);
   }
   
   template<class type_t>
-  bool is(const any_object* o) {
+  [[nodiscard]] auto is(const any_object* o) -> bool {
     if (is<box<type_t>>(o->value())) return true;
     return __is_polymorphic_any_object__<type_t, typename std::is_polymorphic<type_t>::type > {}(*o);
   }
