9dfa061533647550ece8247178aa361413cf5bf2
Gammasoft <gammasoft71@gmail.com>
2026-03-17 20:22:08 +0100

    using trailing return type

--- src/xtd.core/include/xtd/async.hpp
+++ src/xtd.core/include/xtd/async.hpp
@@ -1,11 +1,8 @@
 /// @file
-/// @brief Contains async_ keyword.
+/// @brief Contains #async_ keyword.
 /// @copyright Copyright (c) 2026 Gammasoft. All rights reserved.
 #pragma once
 #include "threading/tasks/task.hpp"
 
-/// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
-namespace xtd {
-}
-
-#define async_ -> xtd::threading::tasks::task<>
+#define async_ \
+  -> xtd::threading::tasks::task<>
