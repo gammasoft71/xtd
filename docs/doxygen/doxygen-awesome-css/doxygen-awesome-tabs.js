2d1cb1e3c9ddf36b83c98fbbd1d2027221ab8f75
Gammasoft <gammasoft71@gmail.com>
2026-03-20 12:09:27 +0100

    update doxygen-awesome-css to version 2.4.1

--- docs/doxygen/doxygen-awesome-css/doxygen-awesome-tabs.js
+++ docs/doxygen/doxygen-awesome-css/doxygen-awesome-tabs.js
@@ -1,29 +1,10 @@
+// SPDX-License-Identifier: MIT
 /**
 
 Doxygen Awesome
 https://github.com/jothepro/doxygen-awesome-css
 
-MIT License
-
-Copyright (c) 2024 jothepro
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+Copyright (c) 2023 - 2025 jothepro
 
 */
 
@@ -33,7 +14,7 @@ class DoxygenAwesomeTabs {
         window.addEventListener("load", () => {
             document.querySelectorAll(".tabbed:not(:empty)").forEach((tabbed, tabbedIndex) => {
                 let tabLinkList = []           
-                tabbed.querySelectorAll("li").forEach((tab, tabIndex) => {
+                tabbed.querySelectorAll(":scope > ul > li").forEach((tab, tabIndex) => {
                     tab.id = "tab_" + tabbedIndex + "_" + tabIndex
                     let header = tab.querySelector(".tab-title")
                     let tabLink = document.createElement("button")
@@ -41,7 +22,7 @@ class DoxygenAwesomeTabs {
                     tabLink.appendChild(header)
                     header.title = header.textContent
                     tabLink.addEventListener("click", () => {
-                        tabbed.querySelectorAll("li").forEach((tab) => {
+                        tabbed.querySelectorAll(":scope > ul > li").forEach((tab) => {
                             tab.classList.remove("selected")
                         })
                         tabLinkList.forEach((tabLink) => {
@@ -68,7 +49,7 @@ class DoxygenAwesomeTabs {
 
                 function resize() {
                     let maxTabHeight = 0
-                    tabbed.querySelectorAll("li").forEach((tab, tabIndex) => {
+                    tabbed.querySelectorAll(":scope > ul > li").forEach((tab, tabIndex) => {
                         let visibility = tab.style.display
                         tab.style.display = "block"
                         maxTabHeight = Math.max(tab.offsetHeight, maxTabHeight)
