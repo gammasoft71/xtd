93e5c8cd813d4934744df8269538d96cd99caa1a
Gammasoft <gammasoft71@gmail.com>
2026-03-22 20:22:46 +0100

    Update documentation

--- src/xtd.core/include/xtd/subtractable.hpp
+++ src/xtd.core/include/xtd/subtractable.hpp
@@ -7,42 +7,7 @@
 
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
-  /// @brief Concept subtractable.
-  /// @par Definition
-  /// ```cpp
-  /// template<typename type_t>
-  /// concept xtd::subtractable = requires(const xtd::raw_type<type_t>& a, const xtd::raw_type<type_t>& b) {{a - b} -> std::same_as<xtd::raw_type<type_t>>;};
-  /// ```
-  /// @par Header
-  /// ```cpp
-  /// #include <xtd/subtractable>
-  /// ```
-  /// @par Namespace
-  /// xtd
-  /// @par Library
-  /// xtd.core
-  /// @ingroup xtd_core concepts
-  /// @par Examples
-  /// The following code shows how to use xtd::subtractable concept
-  /// ```cpp
-  /// #include <xtd/xtd>
-  ///
-  /// auto print_is_subtractable(auto value) noexcept {
-  ///   println("{} ({}) {} subtractable", value, type_of(value), subtractable<decltype(value)> ? "is" : "is not");
-  /// }
-  ///
-  /// auto main() -> int {
-  ///   print_is_subtractable(42);
-  ///   print_is_subtractable(time_span {1, 2, 3});
-  ///   print_is_subtractable(version {1, 2, 3});
-  /// }
-  ///
-  /// // This code produces the following output :
-  /// //
-  /// // 42 (int) is subtractable
-  /// // 01:02:03 (xtd::time_span) is subtractable
-  /// // 1.2.3 (xtd::version) is not subtractable
-  /// ```
+  // Since Doxygen does not work properly with the C++20 concept, the documentation for this concept is declared in xtd/subtractable_.hpp
   template<typename type_t>
   concept subtractable = requires(const xtd::raw_type<type_t>& a, const xtd::raw_type<type_t>& b) {
     {a - b} -> std::same_as<xtd::raw_type<type_t>>;
---
+++ src/xtd.core/include/xtd/subtractable_.hpp
@@ -0,0 +1,46 @@
+/// @file
+/// @brief Contains xtd::subtractable concept documentation.
+/// @copyright Copyright (c) 2026 Gammasoft. All rights reserved.
+#error "Do not include this file: Internal use only. Include <xtd/subtractable> or <xtd/subtractable.hpp> instead."
+
+/// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
+namespace xtd {
+  /// @brief Concept subtractable.
+  /// @par Definition
+  /// ```cpp
+  /// template<typename type_t>
+  /// concept xtd::subtractable = requires(const xtd::raw_type<type_t>& a, const xtd::raw_type<type_t>& b) {{a - b} -> std::same_as<xtd::raw_type<type_t>>;};
+  /// ```
+  /// @par Header
+  /// ```cpp
+  /// #include <xtd/subtractable>
+  /// ```
+  /// @par Namespace
+  /// xtd
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core concepts
+  /// @par Examples
+  /// The following code shows how to use xtd::subtractable concept
+  /// ```cpp
+  /// #include <xtd/xtd>
+  ///
+  /// auto print_is_subtractable(auto value) noexcept {
+  ///   println("{} ({}) {} subtractable", value, type_of(value), subtractable<decltype(value)> ? "is" : "is not");
+  /// }
+  ///
+  /// auto main() -> int {
+  ///   print_is_subtractable(42);
+  ///   print_is_subtractable(time_span {1, 2, 3});
+  ///   print_is_subtractable(version {1, 2, 3});
+  /// }
+  ///
+  /// // This code produces the following output :
+  /// //
+  /// // 42 (int) is subtractable
+  /// // 01:02:03 (xtd::time_span) is subtractable
+  /// // 1.2.3 (xtd::version) is not subtractable
+  /// ```
+  template<typename type_t>
+  struct subtractable;
+}
