a6980f46c140f0d3b5f39c48877c2e6b12101370
Gammasoft <gammasoft71@gmail.com>
2026-03-23 21:04:53 +0100

    Add xtd::unsfe::as operators

---
+++ src/xtd.core/include/xtd/unsafe.hpp
@@ -0,0 +1,38 @@
+/// @file
+/// @brief Contains xtd::unsafe static class.
+/// @copyright Copyright (c) 2026 Gammasoft. All rights reserved.
+#pragma once
+#include "is_value_type.hpp"
+
+/// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
+namespace xtd {
+  /// @brief Contains unsafe cast as operators.
+  /// @par Header
+  /// ```cpp
+  /// #include <xtd/unsafe>
+  /// ```
+  /// @par Namespace
+  /// xtd
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core
+  class unsafe static_ {
+  public:
+    template<class to_type, class from_type>
+    requires (!xtd::is_value_type_v<to_type>)
+    [[nodiscard]] static constexpr auto as(from_type&& value) -> to_type {return static_cast<to_type>(std::forward<from_type>(value));}
+    
+    template<class to_type, class from_type>
+    requires xtd::is_value_type_v<to_type>
+    [[nodiscard]] static constexpr auto as(from_type&& value) -> to_type {
+      using underlying = typename to_type::native_type;
+      return to_type{static_cast<underlying>(value)};
+    }
+    
+    template<class to_type, class from_type>
+    [[nodiscard]] static constexpr auto as(const from_type* value) -> to_type* {return reinterpret_cast<to_type*>(value);}
+    
+    template<class to_type, class from_type>
+    [[nodiscard]] static constexpr auto as(from_type* value) -> const to_type* {return reinterpret_cast<const to_type*>(value);}
+  };
+}
