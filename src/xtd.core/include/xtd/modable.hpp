b73740d9a3504c4c119718206b65ecb0ad8a7847
Gammasoft <gammasoft71@gmail.com>
2026-03-24 13:55:55 +0100

    refactor documentation

--- src/xtd.core/include/xtd/modable.hpp
+++ src/xtd.core/include/xtd/modable.hpp
@@ -7,42 +7,7 @@
 
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
-  /// @brief Concept modable.
-  /// @par Definition
-  /// ```cpp
-  /// template<typename type_t>
-  /// concept xtd::modable = requires(const xtd::raw_type<type_t>& a, const xtd::raw_type<type_t>& b) {{a % b} -> std::same_as<xtd::raw_type<type_t>>;};
-  /// ```
-  /// @par Header
-  /// ```cpp
-  /// #include <xtd/modable>
-  /// ```
-  /// @par Namespace
-  /// xtd
-  /// @par Library
-  /// xtd.core
-  /// @ingroup xtd_core concepts
-  /// @par Examples
-  /// The following code shows how to use xtd::modable concept
-  /// ```cpp
-  /// #include <xtd/xtd>
-  ///
-  /// auto print_is_modable(auto value) noexcept {
-  ///   println("{} ({}) {} modable", value, type_of(value), modable<decltype(value)> ? "is" : "is not");
-  /// }
-  ///
-  /// auto main() -> int {
-  ///   print_is_modable(42);
-  ///   print_is_modable(time_span {1, 2, 3});
-  ///   print_is_modable(version {1, 2, 3});
-  /// }
-  ///
-  /// // This code produces the following output :
-  /// //
-  /// // 42 (int) is modable
-  /// // 01:02:03 (xtd::time_span) is not modable
-  /// // 1.2.3 (xtd::version) is not modable
-  /// ```
+  // Since Doxygen does not work properly with the C++20 concept, the documentation for this concept is declared in xtd/modable_.hpp
   template<typename type_t>
   concept modable = requires(const xtd::raw_type<type_t>& a, const xtd::raw_type<type_t>& b) {
     {a % b} -> std::same_as<xtd::raw_type<type_t>>;
