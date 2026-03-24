76643958d4c62a2eb5945e13c77082d72a4c9212
Gammasoft <gammasoft71@gmail.com>
2026-03-24 16:10:54 +0100

    Refactor concept documentation

---
+++ src/xtd.core/include/xtd/integer_16_.hpp
@@ -0,0 +1,76 @@
+/// @file
+/// @brief Contains xtd::integer_16 concept docuemtation.
+/// @copyright Copyright (c) 2026 Gammasoft. All rights reserved.
+#pragma once
+#error "Do not include this file: Internal use only. Include <xtd/integer_16> or <xtd/integer_16.hpp> instead."
+
+/// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
+namespace xtd {
+  /// @brief Represents the integer 16 bits concept.
+  /// @par Definition
+  /// ```cpp
+  /// template<typename type_t>
+  /// concept xtd::integer_16 = xtd::signed_integer_16<type_t> || xtd::unsigned_integer_16<type_t>;
+  /// ```
+  /// @par Header
+  /// ```cpp
+  /// #include <xtd/integer_16>
+  /// ```
+  /// @par Namespace
+  /// xtd
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core concepts
+  /// @par Examples
+  /// The following code shows how to use xtd::integer_16 concept
+  /// ```cpp
+  /// #include <xtd/xtd>
+  ///
+  /// auto print_is_integer_16(auto value) noexcept {
+  ///   println("{} ({}) {} integer_16", value, type_of(value), integer_16<decltype(value)> ? "is" : "is not");
+  /// }
+  ///
+  /// auto main() -> int {
+  ///   print_is_integer_16(42_s8);
+  ///   print_is_integer_16(42_s16);
+  ///   print_is_integer_16(42_s32);
+  ///   print_is_integer_16(42_s64);
+  ///   print_is_integer_16(42_u8);
+  ///   print_is_integer_16(42_u16);
+  ///   print_is_integer_16(42_u32);
+  ///   print_is_integer_16(42_u64);
+  ///   print_is_integer_16(42_z);
+  ///   print_is_integer_16(42.84f);
+  ///   print_is_integer_16(42.84);
+  ///   print_is_integer_16(42.84l);
+  ///   print_is_integer_16('a');
+  ///   print_is_integer_16(true);
+  ///   print_is_integer_16("string"_s);
+  ///   print_is_integer_16("string");
+  ///   print_is_integer_16(1_h + 12_min + 24_s + 48_ms);
+  ///   print_is_integer_16(day_of_week::wednesday);
+  /// }
+  ///
+  /// // This code produces the following output :
+  /// //
+  /// // 42 (signed char) is not integer_16
+  /// // 42 (short) is integer_16
+  /// // 42 (int) is not integer_16
+  /// // 42 (long long) is not integer_16
+  /// // 42 (unsigned char) is not integer_16
+  /// // 42 (unsigned short) is integer_16
+  /// // 42 (unsigned int) is not integer_16
+  /// // 42 (unsigned long long) is not integer_16
+  /// // 42 (unsigned long) is not integer_16
+  /// // 42.84 (float) is not integer_16
+  /// // 42.84 (double) is not integer_16
+  /// // 42.84 (long double) is not integer_16
+  /// // true (bool) is not integer_16
+  /// // string (xtd::basic_string<char, std::char_traits<char>, std::allocator<char>>) is not integer_16
+  /// // string (char const*) is not integer_16
+  /// // 01:12:24.0480000 (xtd::time_span) is not integer_16
+  /// // wednesday (xtd::day_of_week) is not integer_16
+  /// ```
+  template<typename type_t>
+  struct integer_16;
+}
