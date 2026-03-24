ee6e90d2e35101e9b1d8f38daf5b5432051b8fc0
Gammasoft <gammasoft71@gmail.com>
2026-03-24 13:55:51 +0100

    refactor documentation

--- src/xtd.core/include/xtd/divisible.hpp
+++ src/xtd.core/include/xtd/divisible.hpp
@@ -7,42 +7,7 @@
 
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
-  /// @brief Concept divisible.
-  /// @par Definition
-  /// ```cpp
-  /// template<typename type_t>
-  /// concept xtd::divisible = requires(const xtd::raw_type<type_t>& a, const xtd::raw_type<type_t>& b) {{a / b} -> std::same_as<xtd::raw_type<type_t>>;};
-  /// ```
-  /// @par Header
-  /// ```cpp
-  /// #include <xtd/divisible>
-  /// ```
-  /// @par Namespace
-  /// xtd
-  /// @par Library
-  /// xtd.core
-  /// @ingroup xtd_core concepts
-  /// @par Examples
-  /// The following code shows how to use xtd::divisible concept
-  /// ```cpp
-  /// #include <xtd/xtd>
-  ///
-  /// auto print_is_divisible(auto value) noexcept {
-  ///   println("{} ({}) {} divisible", value, type_of(value), divisible<decltype(value)> ? "is" : "is not");
-  /// }
-  ///
-  /// auto main() -> int {
-  ///   print_is_divisible(42);
-  ///   print_is_divisible(time_span {1, 2, 3});
-  ///   print_is_divisible(version {1, 2, 3});
-  /// }
-  ///
-  /// // This code produces the following output :
-  /// //
-  /// // 42 (int) is divisible
-  /// // 01:02:03 (xtd::time_span) is divisible
-  /// // 1.2.3 (xtd::version) is not divisible
-  /// ```
+  // Since Doxygen does not work properly with the C++20 concept, the documentation for this concept is declared in xtd/divisible_.hpp
   template<typename type_t>
   concept divisible = requires(const xtd::raw_type<type_t>& a, const xtd::raw_type<type_t>& b) {
     {a / b} -> std::same_as<xtd::raw_type<type_t>>;
