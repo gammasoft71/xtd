0caa26811a19693fbfd1b2e4f861502e1b648428
Gammasoft <gammasoft71@gmail.com>
2026-03-24 13:55:34 +0100

    refactor documentation

---
+++ src/xtd.core/include/xtd/arithmeticable_.hpp
@@ -0,0 +1,48 @@
+/// @file
+/// @brief Contains xtd::arithmeticable concept documentation.
+/// @copyright Copyright (c) 2026 Gammasoft. All rights reserved.
+#pragma once
+#error "Do not include this file: Internal use only. Include <xtd/arithmeticable> or <xtd/arithmeticable.hpp> instead."
+
+
+/// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
+namespace xtd {
+  /// @brief Represents the arithmeticable concept.
+  /// @par Definition
+  /// ```cpp
+  /// template<typename type_t>
+  /// concept xtd::arithmeticable = xtd::addable<type_t> && xtd::divisible<type_t> && xtd::multipliable<type_t> && xtd::subtractable<type_t>;
+  /// ```
+  /// @par Header
+  /// ```cpp
+  /// #include <xtd/arithmeticable>
+  /// ```
+  /// @par Namespace
+  /// xtd
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core concepts
+  /// @par Examples
+  /// The following code shows how to use xtd::arithmeticable concept
+  /// ```cpp
+  /// #include <xtd/xtd>
+  ///
+  /// auto print_is_arithmeticable(auto value) noexcept {
+  ///   println("{} ({}) {} arithmeticable", value, type_of(value), arithmeticable<decltype(value)> ? "is" : "is not");
+  /// }
+  ///
+  /// auto main() -> int {
+  ///   print_is_arithmeticable(42);
+  ///   print_is_arithmeticable(time_span {1, 2, 3});
+  ///   print_is_arithmeticable(version {1, 2, 3});
+  /// }
+  ///
+  /// // This code produces the following output :
+  /// //
+  /// // 42 (int) is arithmeticable
+  /// // 01:02:03 (xtd::time_span) is not arithmeticable
+  /// // 1.2.3 (xtd::version) is not arithmeticable
+  /// ```
+  template<typename type_t>
+  struct arithmeticable;
+}
