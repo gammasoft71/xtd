5cae13d223f0f069690d8119eee8631643d36a19
Gammasoft <gammasoft71@gmail.com>
2026-03-24 13:55:26 +0100

    refactor documentation

---
+++ src/xtd.core/include/xtd/addable_.hpp
@@ -0,0 +1,47 @@
+/// @file
+/// @brief Contains xtd::addable concept documentation.
+/// @copyright Copyright (c) 2026 Gammasoft. All rights reserved.
+#pragma once
+#error "Do not include this file: Internal use only. Include <xtd/addable> or <xtd/addable.hpp> instead."
+
+/// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
+namespace xtd {
+  /// @brief Represents the addable concept.
+  /// @par Definition
+  /// ```cpp
+  /// template<typename type_t>
+  /// concept xtd::addable = requires(const xtd::raw_type<type_t>& a, const xtd::raw_type<type_t>& b) {{a + b} -> std::same_as<xtd::raw_type<type_t>>;};
+  /// ```
+  /// @par Header
+  /// ```cpp
+  /// #include <xtd/addable>
+  /// ```
+  /// @par Namespace
+  /// xtd
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core concepts
+  /// @par Examples
+  /// The following code shows how to use xtd::addable concept
+  /// ```cpp
+  /// #include <xtd/xtd>
+  ///
+  /// auto print_is_addable(auto value) noexcept {
+  ///   println("{} ({}) {} addable", value, type_of(value), addable<decltype(value)> ? "is" : "is not");
+  /// }
+  ///
+  /// auto main() -> int {
+  ///   print_is_addable(42);
+  ///   print_is_addable(time_span {1, 2, 3});
+  ///   print_is_addable(version {1, 2, 3});
+  /// }
+  ///
+  /// // This code produces the following output :
+  /// //
+  /// // 42 (int) is addable
+  /// // 01:02:03 (xtd::time_span) is addable
+  /// // 1.2.3 (xtd::version) is not addable
+  /// ```
+  template<typename type_t>
+  struct addable;
+}
