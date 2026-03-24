e5912435823aba5c8a24b3d66ac5b4afcb558abe
Gammasoft <gammasoft71@gmail.com>
2026-03-24 16:11:20 +0100

    Refactor concept documentation

--- src/xtd.core/include/xtd/integer_arithmeticable.hpp
+++ src/xtd.core/include/xtd/integer_arithmeticable.hpp
@@ -7,42 +7,7 @@
 
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
-  /// @brief Concept integer_arithmeticable.
-  /// @par Definition
-  /// ```cpp
-  /// template<typename type_t>
-  /// concept xtd::integer_arithmeticable = xtd::arithmeticable<type_t> && xtd::modable<type_t>;
-  /// ```
-  /// @par Header
-  /// ```cpp
-  /// #include <xtd/integer_arithmeticable>
-  /// ```
-  /// @par Namespace
-  /// xtd
-  /// @par Library
-  /// xtd.core
-  /// @ingroup xtd_core concepts
-  /// @par Examples
-  /// The following code shows how to use xtd::integer_arithmeticable concept
-  /// ```cpp
-  /// #include <xtd/xtd>
-  ///
-  /// auto print_is_integer_arithmeticable(auto value) noexcept {
-  ///   println("{} ({}) {} integer_arithmeticable", value, type_of(value), integer_arithmeticable<decltype(value)> ? "is" : "is not");
-  /// }
-  ///
-  /// auto main() -> int {
-  ///   print_is_integer_arithmeticable(42);
-  ///   print_is_integer_arithmeticable(time_span {1, 2, 3});
-  ///   print_is_integer_arithmeticable(version {1, 2, 3});
-  /// }
-  ///
-  /// // This code produces the following output :
-  /// //
-  /// // 42 (int) is integer_arithmeticable
-  /// // 01:02:03 (xtd::time_span) is not integer_arithmeticable
-  /// // 1.2.3 (xtd::version) is not integer_arithmeticable
-  /// ```
+  // Since Doxygen does not work properly with the C++20 concept, the documentation for this concept is declared in xtd/integer_arithmeticable_.hpp
   template<typename type_t>
   concept integer_arithmeticable =
   xtd::arithmeticable<type_t> &&
---
+++ src/xtd.core/include/xtd/integer_arithmeticable_.hpp
@@ -0,0 +1,47 @@
+/// @file
+/// @brief Contains xtd::integer_arithmeticable concept documentation.
+/// @copyright Copyright (c) 2026 Gammasoft. All rights reserved.
+#pragma once
+#error "Do not include this file: Internal use only. Include <xtd/integer_arithmeticable> or <xtd/integer_arithmeticable.hpp> instead."
+
+/// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
+namespace xtd {
+  /// @brief Represents the integer arithmeticable concept.
+  /// @par Definition
+  /// ```cpp
+  /// template<typename type_t>
+  /// concept xtd::integer_arithmeticable = xtd::arithmeticable<type_t> && xtd::modable<type_t>;
+  /// ```
+  /// @par Header
+  /// ```cpp
+  /// #include <xtd/integer_arithmeticable>
+  /// ```
+  /// @par Namespace
+  /// xtd
+  /// @par Library
+  /// xtd.core
+  /// @ingroup xtd_core concepts
+  /// @par Examples
+  /// The following code shows how to use xtd::integer_arithmeticable concept
+  /// ```cpp
+  /// #include <xtd/xtd>
+  ///
+  /// auto print_is_integer_arithmeticable(auto value) noexcept {
+  ///   println("{} ({}) {} integer_arithmeticable", value, type_of(value), integer_arithmeticable<decltype(value)> ? "is" : "is not");
+  /// }
+  ///
+  /// auto main() -> int {
+  ///   print_is_integer_arithmeticable(42);
+  ///   print_is_integer_arithmeticable(time_span {1, 2, 3});
+  ///   print_is_integer_arithmeticable(version {1, 2, 3});
+  /// }
+  ///
+  /// // This code produces the following output :
+  /// //
+  /// // 42 (int) is integer_arithmeticable
+  /// // 01:02:03 (xtd::time_span) is not integer_arithmeticable
+  /// // 1.2.3 (xtd::version) is not integer_arithmeticable
+  /// ```
+  template<typename type_t>
+  struct integer_arithmeticable;
+}
