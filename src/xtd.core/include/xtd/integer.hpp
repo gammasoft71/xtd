5a529bb6360b3d5c55c6ed613f5c95d78ffa600e
Gammasoft <gammasoft71@gmail.com>
2026-03-24 16:10:49 +0100

    Refactor concept documentation

--- src/xtd.core/include/xtd/integer.hpp
+++ src/xtd.core/include/xtd/integer.hpp
@@ -7,71 +7,7 @@
 
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
-  /// @brief Concept integer.
-  /// @par Definition
-  /// ```cpp
-  /// template<typename type_t>
-  /// concept xtd::integer = xtd::signed_integer<type_t> || xtd::unsigned_integer<type_t>;
-  /// ```
-  /// @par Header
-  /// ```cpp
-  /// #include <xtd/integer>
-  /// ```
-  /// @par Namespace
-  /// xtd
-  /// @par Library
-  /// xtd.core
-  /// @ingroup xtd_core concepts
-  /// @par Examples
-  /// The following code shows how to use xtd::integer conceps
-  /// ```cpp
-  /// #include <xtd/xtd>
-  ///
-  /// auto print_is_integer(auto value) noexcept {
-  ///   println("{} ({}) {} integer", value, type_of(value), integer<decltype(value)> ? "is" : "is not");
-  /// }
-  ///
-  /// auto main() -> int {
-  ///   print_is_integer(42_s8);
-  ///   print_is_integer(42_s16);
-  ///   print_is_integer(42_s32);
-  ///   print_is_integer(42_s64);
-  ///   print_is_integer(42_u8);
-  ///   print_is_integer(42_u16);
-  ///   print_is_integer(42_u32);
-  ///   print_is_integer(42_u64);
-  ///   print_is_integer(42_z);
-  ///   print_is_integer(42.84f);
-  ///   print_is_integer(42.84);
-  ///   print_is_integer(42.84l);
-  ///   print_is_integer('a');
-  ///   print_is_integer(true);
-  ///   print_is_integer("string"_s);
-  ///   print_is_integer("string");
-  ///   print_is_integer(1_h + 12_min + 24_s + 48_ms);
-  ///   print_is_integer(day_of_week::wednesday);
-  /// }
-  ///
-  /// // This code produces the following output :
-  /// //
-  /// // 42 (signed char) is integer
-  /// // 42 (short) is integer
-  /// // 42 (int) is integer
-  /// // 42 (long long) is integer
-  /// // 42 (unsigned char) is integer
-  /// // 42 (unsigned short) is integer
-  /// // 42 (unsigned int) is integer
-  /// // 42 (unsigned long long) is integer
-  /// // 42 (unsigned long) is integer
-  /// // 42.84 (float) is not integer
-  /// // 42.84 (double) is not integer
-  /// // 42.84 (long double) is not integer
-  /// // true (bool) is not integer
-  /// // string (xtd::basic_string<char, std::char_traits<char>, std::allocator<char>>) is not integer
-  /// // string (char const*) is not integer
-  /// // 01:12:24.0480000 (xtd::time_span) is not integer
-  /// // wednesday (xtd::day_of_week) is not integer
-  /// ```
+  // Since Doxygen does not work properly with the C++20 concept, the documentation for this concept is declared in xtd/integer_.hpp
   template<typename type_t>
   concept integer =
   xtd::signed_integer<type_t> ||
