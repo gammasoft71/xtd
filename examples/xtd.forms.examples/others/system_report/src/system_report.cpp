22b7ce999306d2540e8b89714c50e8ba75787641
Gammasoft <gammasoft71@gmail.com>
2026-03-25 18:27:14 +0100

    Refactor example

--- examples/xtd.forms.examples/others/system_report/src/system_report.cpp
+++ examples/xtd.forms.examples/others/system_report/src/system_report.cpp
@@ -6,7 +6,7 @@ public:
     text("System reports example");
     start_position(form_start_position::center_screen);
     size({500, 700});
-    
+
     report_tab_control.dock(dock_style::fill);
     report_tab_control.parent(*this);
     report_tab_control.tab_pages().add_range({text_report_tab_page, json_report_tab_page, xml_report_tab_page});
@@ -16,11 +16,9 @@ public:
     xml_report_tab_page.text("xml");
 
     text_reports_text_box.dock(dock_style::fill);
-    text_reports_text_box.font({drawing::font_family::generic_monospace(), text_reports_text_box.font().size()});
     text_reports_text_box.multiline(true);
     text_reports_text_box.parent(text_report_tab_page);
     text_reports_text_box.read_only(true);
-    text_reports_text_box.text(system_report::to_string());
     text_reports_text_box.word_wrap(false);
     
     json_reports_text_box.dock(dock_style::fill);
@@ -28,7 +26,6 @@ public:
     json_reports_text_box.multiline(true);
     json_reports_text_box.parent(json_report_tab_page);
     json_reports_text_box.read_only(true);
-    json_reports_text_box.text(system_report::to_json());
     json_reports_text_box.word_wrap(false);
     
     xml_reports_text_box.dock(dock_style::fill);
@@ -36,11 +33,41 @@ public:
     xml_reports_text_box.multiline(true);
     xml_reports_text_box.parent(xml_report_tab_page);
     xml_reports_text_box.read_only(true);
-    xml_reports_text_box.text(system_report::to_xml());
     xml_reports_text_box.word_wrap(false);
+    
+    reports_panel.parent(*this);
+    reports_panel.padding(10);
+    reports_panel.height(reports_choice.height() + 20);
+    reports_panel.dock(dock_style::top);
+
+    reports_choice.dock(dock_style::fill);
+    reports_choice.parent(reports_panel);
+    reports_choice.items().add_range({
+      {"minimal", system_report::reports::minimal},
+      {"standard", system_report::reports::standard},
+      {"diagnostic", system_report::reports::diagnostic},
+      {"diagnostic | sensitive", system_report::reports::diagnostic | system_report::reports::sensitive},
+      {"safe", system_report::reports::safe},
+      {"full", system_report::reports::full}
+    });
+    reports_choice.selected_value_changed += [&] {
+      text_reports_text_box.text(system_report::to_string(as<system_report::reports>(reports_choice.selected_item().tag())));
+      json_reports_text_box.text(system_report::to_json(as<system_report::reports>(reports_choice.selected_item().tag())));
+      xml_reports_text_box.text(system_report::to_xml(as<system_report::reports>(reports_choice.selected_item().tag())));
+    };
+    reports_choice.selected_index(0);
+
+    reports_label.auto_size(true);
+    reports_label.parent(reports_panel);
+    reports_label.dock(dock_style::left);
+    reports_label.text("Reports :");
+    reports_label.text_align(xtd::forms::content_alignment::middle_left);
   }
   
 private:
+  panel reports_panel;
+  label reports_label;
+  choice reports_choice;
   tab_control report_tab_control;
   tab_page text_report_tab_page;
   tab_page json_report_tab_page;
