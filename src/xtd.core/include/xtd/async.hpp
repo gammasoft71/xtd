64224db7948b787539755a05ae7f53824c6687ec
Gammasoft <gammasoft71@gmail.com>
2026-04-01 12:35:36 +0200

    Add documentation

--- src/xtd.core/include/xtd/async.hpp
+++ src/xtd.core/include/xtd/async.hpp
@@ -4,5 +4,36 @@
 #pragma once
 #include "threading/tasks/task.hpp"
 
+/// @brief Use the #async_ modifier to specify that a method, lambda expression, or anonymous method is asynchronous. If you use this modifier on a method or expression, you create an async method. The following example defines an async method named `example_method_async`:
+/// ```cpp
+/// * Method :
+/// auto example_method_async() async_ {
+///  // ...
+/// }
+/// ```
+/// The previous example corresponds to the following example, without the #async_ keyword:
+/// ```cpp
+/// auto example_method_async() -> xtd::threading::tasks::task<> {
+///  // ...
+/// }
+/// ```
+///
+/// * Lambda :
+/// auto example_method_async = [] async_ {
+///  // ...
+/// }
+/// ```
+/// The previous example corresponds to the following example, without the #async_ keyword:
+/// ```cpp
+/// auto example_method_async = [] -> xtd::threading::tasks::task<> {
+///  // ...
+/// }
+/// ```
+///
+/// @par Namespace
+/// xtd
+/// @par Library
+/// xtd.core
+/// @ingroup xtd_core keywords
 #define async_ \
   -> xtd::threading::tasks::task<>
