37e96463ccd0eb4b3109bba4ab030feeae92f0d1
Gammasoft <gammasoft71@gmail.com>
2026-04-02 10:44:23 +0200

    Refactor stream_insertable concept

--- src/xtd.core/include/xtd/stream_insertable.hpp
+++ src/xtd.core/include/xtd/stream_insertable.hpp
@@ -2,12 +2,13 @@
 /// @brief Contains xtd::stream_insertable concept.
 /// @copyright Copyright (c) 2026 Gammasoft. All rights reserved.
 #pragma once
-#include "is_stream_insertable.hpp"
+#include "raw_type.hpp"
 #include <concepts>
+#include <ostream>
 
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
   // Since Doxygen does not work properly with the C++ concept, the documentation for this concept is declared in xtd/stream_insertable_.hpp
   template<typename value_t>
-  concept stream_insertable = xtd::is_stream_insertable_v<value_t>;
+  concept stream_insertable = requires(std::ostream& os, value_t&& v) {os << std::forward<value_t>(v);};
 }
