9b18517be7217b1fac74748284c38de403dd468d
Gammasoft <gammasoft71@gmail.com>
2026-04-08 17:43:50 +0200

    Refactor xtd::box_floating_point class

--- src/xtd.core/include/xtd/box_floating_point.hpp
+++ src/xtd.core/include/xtd/box_floating_point.hpp
@@ -2,11 +2,10 @@
 /// @brief Contains xtd::box_floating_point class.
 /// @copyright Copyright (c) 2026 Gammasoft. All rights reserved.
 #pragma once
-#include "box_integer.hpp"
+#include "box.hpp"
 #include "decimal.hpp"
-#define __XTD_CORE_INTERNAL__
-#include "internal/__box_floating_point.hpp"
-#undef __XTD_CORE_INTERNAL__
+#include "real.hpp"
+#include <cmath>
 
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
@@ -15,10 +14,10 @@ namespace xtd {
   /// class box_floating_point : public xtd::box<type_t>
   /// ```
   /// @par Inheritance
-  /// xtd::object → xtd::box <type_t> → xtd::box_integer <type_t> → box_floating_point <type_t>
+  /// xtd::object → xtd::box <type_t> → xtd::box <type_t> → box_floating_point <type_t>
   /// @par Header
   /// ```cpp
-  /// #include <xtd/box°floating_point>
+  /// #include <xtd/box_floating_point>
   /// ```
   /// @par Namespace
   /// xtd
@@ -39,11 +38,12 @@ namespace xtd {
   /// console::write_line("result = {}", result); // Display: result = 3.14;
   /// ```
   template<typename type_t>
-  class box_floating_point : public xtd::box_integer<type_t> {
+  requires xtd::real<type_t>
+  class box_floating_point : public xtd::box<type_t> {
   public:
     /// @cond
     box_floating_point() = default;
-    box_floating_point(const type_t& value) : xtd::box_integer<type_t>(value) {}
+    box_floating_point(const type_t& value) : xtd::box<type_t>(value) {}
     box_floating_point(const box_floating_point&) = default;
     box_floating_point(box_floating_point&&) = default;
     box_floating_point& operator =(const box_floating_point&) = default;
@@ -53,8 +53,14 @@ namespace xtd {
     
     /// @{
     /// @brief Represents the smallest positive type_t value greater than zero. This field is constant.
-    /// @remarks The value of this field is 4.94066e-324.
-    static constexpr type_t epsilon = __get_epsilon(type_t{});
+    /// @remarks The value of this field is std::numeric_limits<type_t>::denorm_min().
+    static constexpr type_t epsilon = std::numeric_limits<type_t>::denorm_min();
+    /// @brief Represents the largest possible value of type_t. This field is constant.
+    /// @remarks The value of this field is std::numeric_limits<type_t>::max().
+    static constexpr type_t max_value = std::numeric_limits<type_t>::max();
+    /// @brief Represents the smallest possible value of type_t. This field is constant.
+    /// @remarks The value of this field is std::numeric_limits<type_t>::lowest().
+    static constexpr type_t min_value = std::numeric_limits<type_t>::lowest();
     /// @brief Represents not a number (NaN). This field is constant.
     /// @remarks The value of this field is std::numeric_limits<type_t>::quiet_NaN().
     static constexpr type_t NaN = std::numeric_limits<type_t>::quiet_NaN();
@@ -72,32 +78,59 @@ namespace xtd {
     /// @brief Determines whether the specified value is finite (zero, subnormal, or normal).
     /// @param value A floating-point number.
     /// @return `true` if the value is finite (zero, subnormal or normal); `false` otherwise.
-    static bool is_finite(type_t value) noexcept {return !is_infinity(value);}
+    [[nodiscard]] static auto is_finite(type_t value) noexcept -> bool {return !is_infinity(value) && !is_NaN(value);}
     /// @brief Returns a value indicating whether the specified number evaluates to negative or positive infinity.
     /// @param value A floating-point number.
     /// @return `true` if value evaluates to xtd::math::positive_infinity or xtd::math::negative_infinity; otherwise, `false`.
-    static bool is_infinity(type_t value) noexcept {return is_negative_infinity(value) || is_positive_infinity(value);}
-    
-    /// @brief Returns a value indicating whether the specified number evaluates to negative infinity.
-    /// @param value A floating-point number.
-    /// @return `true` if value evaluates to xtd::math::negative_infinity; otherwise, `false`.
-    static bool is_negative_infinity(type_t value) noexcept {return value <= negative_infinity;}
-    
-    /// @brief Returns a value indicating whether the specified number evaluates to positive infinity.
-    /// @param value A floating-point number.
-    /// @return `true` if value evaluates to xtd::math::positive_infinity; otherwise, `false`.
-    static bool is_positive_infinity(type_t value) noexcept {return value >= positive_infinity;}
+    [[nodiscard]] static auto is_infinity(type_t value) noexcept -> bool {return is_negative_infinity(value) || is_positive_infinity(value);}
     
     /// @brief Returns a value indicating whether the specified number evaluates to not a number.
     /// @param value A floating-point number.
     /// @return `true` if value evaluates to not a number; otherwise, `false`.
-    static bool is_NaN(type_t value) noexcept {return value != value;}
-    
-    /// @brief Determines whether the specified floating point value is within the range of type_t.
+    [[nodiscard]] static auto is_NaN(type_t value) noexcept -> bool {return value != value;}
+
+    /// @brief Returns a value indicating whether the specified number evaluates to negative infinity.
+    /// @param value A floating-point number.
+    /// @return `true` if value evaluates to xtd::math::negative_infinity; otherwise, `false`.
+    [[nodiscard]] static auto is_negative_infinity(type_t value) noexcept -> bool {return value == negative_infinity;}
+
+    /// @brief Returns a value indicating whether the specified number evaluates to normal, i.e. not an infinity, subnormal, not-a-number or zero.
+    /// @param value A floating-point number.
+    /// @return `true` if value evaluates to normal; otherwise, `false`.
+    [[nodiscard]] static auto is_normal(type_t value) noexcept -> bool {return std::fpclassify(value) == FP_NORMAL;}
+
+    /// @brief Returns a value indicating whether the specified number evaluates to positive infinity.
+    /// @param value A floating-point number.
+    /// @return `true` if value evaluates to xtd::math::positive_infinity; otherwise, `false`.
+    [[nodiscard]] static auto is_positive_infinity(type_t value) noexcept -> bool {return value == positive_infinity;}
+
+    /// @brief Returns a value indicating whether the specified number evaluates to subnormal.
+    /// @param value A floating-point number.
+    /// @return `true` if value evaluates to subnormal; otherwise, `false`.
+    [[nodiscard]] static auto is_subnormal(type_t value) noexcept -> bool {return std::fpclassify(value) == FP_SUBNORMAL;}
+
+    /// @brief Determines whether the specified value can be safely converted to type_t without overflow.
     /// @param value The floating point value to validate.
     /// @return true if value is greater than or equal to min_value and less than or equal to max_value; otherwise, false.
     /// @remarks If the value is outside the valid range defined by min_value and max_value, the method returns false.
-    static bool is_valid(std::floating_point auto value) noexcept {return !is_NaN(value) && !is_infinity(value) && value >= static_cast<xtd::decimal>(xtd::box_integer<type_t>::min_value) && value <= static_cast<xtd::decimal>(xtd::box_integer<type_t>::max_value);}
+    [[nodiscard]] static auto is_valid(xtd::real auto value) noexcept -> bool {return !is_NaN(value) && !is_infinity(value) && value >= static_cast<xtd::decimal>(min_value) && value <= static_cast<xtd::decimal>(max_value);}
+
+    /// @brief Returns a value indicating whether the specified number evaluates to zero.
+    /// @param value A floating-point number.
+    /// @return `true` if value evaluates to zero; otherwise, `false`.
+    [[nodiscard]] static auto is_zero(type_t value) noexcept -> bool {return std::fpclassify(value) == FP_ZERO;}
+
+    using box<type_t>::parse;
+    /// @brief Converts the string to its type_t equivalent.
+    /// @param value A string containing a type_t to convert.
+    /// @return A type_t equivalent to the number contained in value.
+    [[nodiscard]] static auto parse(const xtd::string& value, xtd::number_styles styles) -> type_t {return xtd::parse<type_t>(value, styles);}
+    
+    using box<type_t>::try_parse;
+    /// @brief Converts the string to its type_t equivalent.
+    /// @param value A string containing a type_t to convert.
+    /// @return A type_t equivalent to the number contained in value.
+    [[nodiscard]] static auto try_parse(const xtd::string& value, type_t& result, xtd::number_styles styles) noexcept -> bool {return xtd::try_parse<type_t>(value, result, styles);}
     /// @}
   };
 }
