c468e24271c1b39c0144d9e85be669633ef91355
Gammasoft <gammasoft71@gmail.com>
2026-04-09 21:36:41 +0200

    Refactor error message

--- src/xtd.core/include/xtd/check_cpp_exceptions.hpp
+++ src/xtd.core/include/xtd/check_cpp_exceptions.hpp
@@ -4,6 +4,10 @@
 
 /// @cond
 #if !defined(__cpp_exceptions)
-#  error "xtd requires C++ exceptions support. Please enable exceptions in your compiler (do not use -fno-exceptions)."
+#  if defined(_MSC_VER)
+#    error "xtd requires C++ exceptions support. Please enable exceptions in your compiler (do not use /EHs-c-)."
+#  else
+#    error "xtd requires C++ exceptions support. Please enable exceptions in your compiler (do not use -fno-exceptions)."
+#  endif
 #endif
 /// @endcond
