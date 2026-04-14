815ab35b941f007658e0c37a3d5343a402301ec5
Gammasoft <gammasoft71@gmail.com>
2026-04-14 16:45:46 +0200

    Fix documentation

--- src/xtd.core/include/xtd/collections/generic/sorted_set.hpp
+++ src/xtd.core/include/xtd/collections/generic/sorted_set.hpp
@@ -29,7 +29,7 @@ namespace xtd {
       /// xtd.core
       /// @ingroup xtd_core generic_collections
       /// @remarks The xtd::collections::generic::sorted_set class is same as [std::set](https://en.cppreference.com/w/cpp/container/set).
-      /// @remarks A xtd::collections::generic::sorted_set object maintains a sorted order without affecting performance as elements are inserted and deleted. Duplicate elements are ! allowed. Changing the sort values of existing items is ! supported and may lead to unexpected behavior.
+      /// @remarks A xtd::collections::generic::sorted_set object maintains a sorted order without affecting performance as elements are inserted and deleted. Duplicate elements are not allowed. Changing the sort values of existing items is not supported and may lead to unexpected behavior.
       /// @par Examples
       /// The following example demonstrates how to merge two disparate sets. This example creates two xtd::collections::generic::sorted_set objects, and populates them with even and odd numbers, respectively. A third xtd::collections::generic::sorted_set object is created from the set that contains the even numbers. The example then calls the UnionWith method, which adds the odd number set to the third set.
       /// @include sorted_set.cpp
