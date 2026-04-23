222167c08154264d380ba43fc6d1e6b237ac343d
Gammasoft <gammasoft71@gmail.com>
2026-04-23 10:47:26 +0200

    split overload file in const_overlad, non_const_overload abd overload files

--- src/xtd.core/include/xtd/const_overload.hpp
+++ src/xtd.core/include/xtd/const_overload.hpp
@@ -1,33 +1,25 @@
 /// @file
-/// @brief Contains xtd::overload.
+/// @brief Contains xtd::const_overload.
 /// @copyright Copyright (c) 2026 Gammasoft. All rights reserved.
 #pragma once
-#include "const_overload.hpp"
-#include "non_const_overload.hpp"
+#include <functional>
 
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
-  /// @brief Represents class that use to determine one of const and non const overloaded methods.
+  /// @brief Represents class that use to determine one of const overloaded methods.
   /// @par Library
   /// xtd.core
   /// @ingroup xtd_core
   template<typename ...args_t>
-  struct overload_type : const_overload_type<args_t...>, non_const_overload_type<args_t...> {
-    /// @cond
-    using const_overload_type<args_t...>::of;
-    using const_overload_type<args_t...>::operator();
-    using non_const_overload_type<args_t...>::of;
-    using non_const_overload_type<args_t...>::operator();
-    /// @endcond
-    
+  struct const_overload_type {
     /// @name Operators
     
     /// @{
     /// @brief Returns a pointer to an overloaded method. The template parameter is the list of the argument types of the method.
     /// @param method is the pointer to the (member) method
     /// @return Pointer to an overloaded method.
-    template<typename result_t>
-    constexpr auto operator()(result_t (*method)(args_t...)) const noexcept -> decltype(method) {return method;}
+    template<typename result_t, typename type_t>
+    constexpr auto operator()(result_t (type_t::*method)(args_t...) const) const noexcept -> decltype(method) {return method;}
     /// @}
     
     /// @name Public Static Methods
@@ -36,23 +28,23 @@ namespace xtd {
     /// @brief Returns a pointer to an overloaded method. The template parameter is the list of the argument types of the method.
     /// @param method is the pointer to the (member) method
     /// @return Pointer to an overloaded method.
-    template<typename result_t>
-    static constexpr auto of(result_t (*method)(args_t...)) noexcept -> decltype(method) {return method;}
+    template<typename result_t, typename type_t>
+    static constexpr auto of(result_t (type_t::*method)(args_t...) const) noexcept -> decltype(method) {return method;}
     /// @}
   };
-  
-  /// @name Public name
-  
+
+  /// @name Public Variables
+
   /// @{
   template<typename ...args_t>
-  inline constexpr overload_type<args_t...> overload;
+  inline constexpr const_overload_type<args_t...> const_overload;
   /// @}
 }
 
-/// @brief Helper keyword that use to determine one of const and non const overloaded methods.
+/// @brief Helper keyword that use to determine one of const overloaded methods.
 /// @par Namespace
 /// xtd
 /// @par Library
 /// xtd.core
 /// @ingroup xtd_core
-#define overload_ xtd::overload
+#define const_overload_ xtd::const_overload
