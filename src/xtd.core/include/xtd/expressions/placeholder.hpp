5b265ff79544da97d72e61341545797d7ccfe9e2
Gammasoft <gammasoft71@gmail.com>
2026-04-25 01:21:37 +0200

    Add xtd::expressions::arg<N> variable

--- src/xtd.core/include/xtd/expressions/placeholder.hpp
+++ src/xtd.core/include/xtd/expressions/placeholder.hpp
@@ -41,163 +41,186 @@ namespace xtd {
     /// @name Public Variables
     
     /// @{
+    /// @brief The xtd::expressions::arg instance is the `index` argument used by expression.
+    /// @par Library
+    /// xtd.core
+    /// @ingroup xtd_core expressions
+    /// @remarks The index start from 1 to N.
+    /// @par Examples
+    /// ```cpp
+    /// auto expr = arg<1> + arg<2>;
+    /// auto println("expr result => {}", expr(12 + 23)); // expr result => 35
+    /// ```
+    template <size_t index>
+    inline constexpr xtd::expressions::placeholder<index - 1> arg {};
+    
     /// @brief The xtd::expressions::_1 placeholder instance is first argument used by expression.
     /// @par Library
     /// xtd.core
     /// @ingroup xtd_core expressions
+    /// @remarks The xtd::expressions::_1 is an alias on xtd::expressions::arg<1>
     /// @remarks xtd::expressions contains the first ten entries from xtd::expressions::placeholder. If you need an eleventh entry or more, you can define them yourself as follows:
     /// ```cpp
-    /// inline constexpr xtd::expressions::placeholder<10> _11{};
-    /// inline constexpr xtd::expressions::placeholder<11> _12{};
+    /// inline constexpr auto _11 = xtd::expressions::arg<11>;
+    /// inline constexpr auto _12 = xtd::expressions::arg<12>;
     /// //...
-    /// inline constexpr xtd::expressions::placeholder<99> _100{};
+    /// inline constexpr auto _100 = xtd::expressions::arg<100>;
     /// ```
     /// @note Extremely deep or exponentially growing expressions may lead to very large compile-time types and excessive compilation times.
     /// @note This typically occurs with recursive expression composition patterns such as: `expr = expr * expr + constant;`
     /// @note In such cases, prefer standard C++ lambda expressions or break the expression into smaller parts.
-    inline constexpr placeholder<0> _1{};
+    inline constexpr auto _1 = arg<1>;
     /// @brief The xtd::expressions::_2 placeholder instance is second argument used by expression.
     /// @par Library
     /// xtd.core
     /// @ingroup xtd_core expressions
+    /// @remarks The xtd::expressions::_2 is an alias on xtd::expressions::arg<1>
     /// @remarks xtd::expressions contains the first ten entries from xtd::expressions::placeholder. If you need an eleventh entry or more, you can define them yourself as follows:
     /// ```cpp
-    /// inline constexpr xtd::expressions::placeholder<10> _11{};
-    /// inline constexpr xtd::expressions::placeholder<11> _12{};
+    /// inline constexpr auto _11 = xtd::expressions::arg<11>;
+    /// inline constexpr auto _12 = xtd::expressions::arg<12>;
     /// //...
-    /// inline constexpr xtd::expressions::placeholder<99> _100{};
+    /// inline constexpr auto _100 = xtd::expressions::arg<100>;
     /// ```
     /// @note Extremely deep or exponentially growing expressions may lead to very large compile-time types and excessive compilation times.
     /// @note This typically occurs with recursive expression composition patterns such as: `expr = expr * expr + constant;`
     /// @note In such cases, prefer standard C++ lambda expressions or break the expression into smaller parts.
-    inline constexpr placeholder<1> _2{};
+    inline constexpr auto _2 = arg<2>;
     /// @brief The xtd::expressions::_3 placeholder instance is third argument used by expression.
     /// @par Library
     /// xtd.core
     /// @ingroup xtd_core expressions
+    /// @remarks The xtd::expressions::_3 is an alias on xtd::expressions::arg<3>
     /// @remarks xtd::expressions contains the first ten entries from xtd::expressions::placeholder. If you need an eleventh entry or more, you can define them yourself as follows:
     /// ```cpp
-    /// inline constexpr xtd::expressions::placeholder<10> _11{};
-    /// inline constexpr xtd::expressions::placeholder<11> _12{};
+    /// inline constexpr auto _11 = xtd::expressions::arg<11>;
+    /// inline constexpr auto _12 = xtd::expressions::arg<12>;
     /// //...
-    /// inline constexpr xtd::expressions::placeholder<99> _100{};
+    /// inline constexpr auto _100 = xtd::expressions::arg<100>;
     /// ```
     /// @note Extremely deep or exponentially growing expressions may lead to very large compile-time types and excessive compilation times.
     /// @note This typically occurs with recursive expression composition patterns such as: `expr = expr * expr + constant;`
     /// @note In such cases, prefer standard C++ lambda expressions or break the expression into smaller parts.
-    inline constexpr placeholder<2> _3{};
+    inline constexpr auto _3 = arg<3>;
     /// @brief The xtd::expressions::_4 placeholder instance is fourth argument used by expression.
     /// @par Library
     /// xtd.core
     /// @ingroup xtd_core expressions
+    /// @remarks The xtd::expressions::_4 is an alias on xtd::expressions::arg<4>
     /// @remarks xtd::expressions contains the first ten entries from xtd::expressions::placeholder. If you need an eleventh entry or more, you can define them yourself as follows:
     /// ```cpp
-    /// inline constexpr xtd::expressions::placeholder<10> _11{};
-    /// inline constexpr xtd::expressions::placeholder<11> _12{};
+    /// inline constexpr auto _11 = xtd::expressions::arg<11>;
+    /// inline constexpr auto _12 = xtd::expressions::arg<12>;
     /// //...
-    /// inline constexpr xtd::expressions::placeholder<99> _100{};
+    /// inline constexpr auto _100 = xtd::expressions::arg<100>;
     /// ```
     /// @note Extremely deep or exponentially growing expressions may lead to very large compile-time types and excessive compilation times.
     /// @note This typically occurs with recursive expression composition patterns such as: `expr = expr * expr + constant;`
     /// @note In such cases, prefer standard C++ lambda expressions or break the expression into smaller parts.
-    inline constexpr placeholder<3> _4{};
+    inline constexpr auto _4 = arg<4>;
     /// @brief The xtd::expressions::_5 placeholder instance is fifth argument used by expression.
     /// @par Library
     /// xtd.core
     /// @ingroup xtd_core expressions
+    /// @remarks The xtd::expressions::_5 is an alias on xtd::expressions::arg<5>
     /// @remarks xtd::expressions contains the first ten entries from xtd::expressions::placeholder. If you need an eleventh entry or more, you can define them yourself as follows:
     /// ```cpp
-    /// inline constexpr xtd::expressions::placeholder<10> _11{};
-    /// inline constexpr xtd::expressions::placeholder<11> _12{};
+    /// inline constexpr auto _11 = xtd::expressions::arg<11>;
+    /// inline constexpr auto _12 = xtd::expressions::arg<12>;
     /// //...
-    /// inline constexpr xtd::expressions::placeholder<99> _100{};
+    /// inline constexpr auto _100 = xtd::expressions::arg<100>;
     /// ```
     /// @note Extremely deep or exponentially growing expressions may lead to very large compile-time types and excessive compilation times.
     /// @note This typically occurs with recursive expression composition patterns such as: `expr = expr * expr + constant;`
     /// @note In such cases, prefer standard C++ lambda expressions or break the expression into smaller parts.
-    inline constexpr placeholder<4> _5{};
+    inline constexpr auto _5 = arg<5>;
     /// @brief The xtd::expressions::_6 placeholder instance is sixth argument used by expression.
     /// @par Library
     /// xtd.core
     /// @ingroup xtd_core expressions
+    /// @remarks The xtd::expressions::_6 is an alias on xtd::expressions::arg<6>
     /// @remarks xtd::expressions contains the first ten entries from xtd::expressions::placeholder. If you need an eleventh entry or more, you can define them yourself as follows:
     /// ```cpp
-    /// inline constexpr xtd::expressions::placeholder<10> _11{};
-    /// inline constexpr xtd::expressions::placeholder<11> _12{};
+    /// inline constexpr auto _11 = xtd::expressions::arg<11>;
+    /// inline constexpr auto _12 = xtd::expressions::arg<12>;
     /// //...
-    /// inline constexpr xtd::expressions::placeholder<99> _100{};
+    /// inline constexpr auto _100 = xtd::expressions::arg<100>;
     /// ```
     /// @note Extremely deep or exponentially growing expressions may lead to very large compile-time types and excessive compilation times.
     /// @note This typically occurs with recursive expression composition patterns such as: `expr = expr * expr + constant;`
     /// @note In such cases, prefer standard C++ lambda expressions or break the expression into smaller parts.
-    inline constexpr placeholder<5> _6{};
+    inline constexpr auto _6 = arg<6>;
     /// @brief The xtd::expressions::_7 placeholder instance is seventh argument used by expression.
     /// @par Library
     /// xtd.core
     /// @ingroup xtd_core expressions
+    /// @remarks The xtd::expressions::_7 is an alias on xtd::expressions::arg<7>
     /// @remarks xtd::expressions contains the first ten entries from xtd::expressions::placeholder. If you need an eleventh entry or more, you can define them yourself as follows:
     /// ```cpp
-    /// inline constexpr xtd::expressions::placeholder<10> _11{};
-    /// inline constexpr xtd::expressions::placeholder<11> _12{};
+    /// inline constexpr auto _11 = xtd::expressions::arg<11>;
+    /// inline constexpr auto _12 = xtd::expressions::arg<12>;
     /// //...
-    /// inline constexpr xtd::expressions::placeholder<99> _100{};
+    /// inline constexpr auto _100 = xtd::expressions::arg<100>;
     /// ```
     /// @note Extremely deep or exponentially growing expressions may lead to very large compile-time types and excessive compilation times.
     /// @note This typically occurs with recursive expression composition patterns such as: `expr = expr * expr + constant;`
     /// @note In such cases, prefer standard C++ lambda expressions or break the expression into smaller parts.
-    inline constexpr placeholder<6> _7{};
+    inline constexpr auto _7 = arg<7>;
     /// @brief The xtd::expressions::_8 placeholder instance is eighth argument used by expression.
     /// @par Library
     /// xtd.core
     /// @ingroup xtd_core expressions
+    /// @remarks The xtd::expressions::_8 is an alias on xtd::expressions::arg<8>
     /// @remarks xtd::expressions contains the first ten entries from xtd::expressions::placeholder. If you need an eleventh entry or more, you can define them yourself as follows:
     /// ```cpp
-    /// inline constexpr xtd::expressions::placeholder<10> _11{};
-    /// inline constexpr xtd::expressions::placeholder<11> _12{};
+    /// inline constexpr auto _11 = xtd::expressions::arg<11>;
+    /// inline constexpr auto _12 = xtd::expressions::arg<12>;
     /// //...
-    /// inline constexpr xtd::expressions::placeholder<99> _100{};
+    /// inline constexpr auto _100 = xtd::expressions::arg<100>;
     /// ```
     /// @note Extremely deep or exponentially growing expressions may lead to very large compile-time types and excessive compilation times.
     /// @note This typically occurs with recursive expression composition patterns such as: `expr = expr * expr + constant;`
     /// @note In such cases, prefer standard C++ lambda expressions or break the expression into smaller parts.
-    inline constexpr placeholder<7> _8{};
+    inline constexpr auto _8 = arg<8>;
     /// @brief The xtd::expressions::_9 placeholder instance is ninth argument used by expression.
     /// @par Library
     /// xtd.core
     /// @ingroup xtd_core expressions
+    /// @remarks The xtd::expressions::_9 is an alias on xtd::expressions::arg<9>
     /// @remarks xtd::expressions contains the first ten entries from xtd::expressions::placeholder. If you need an eleventh entry or more, you can define them yourself as follows:
     /// ```cpp
-    /// inline constexpr xtd::expressions::placeholder<10> _11{};
-    /// inline constexpr xtd::expressions::placeholder<11> _12{};
+    /// inline constexpr auto _11 = xtd::expressions::arg<11>;
+    /// inline constexpr auto _12 = xtd::expressions::arg<12>;
     /// //...
-    /// inline constexpr xtd::expressions::placeholder<99> _100{};
+    /// inline constexpr auto _100 = xtd::expressions::arg<100>;
     /// ```
     /// @note Extremely deep or exponentially growing expressions may lead to very large compile-time types and excessive compilation times.
     /// @note This typically occurs with recursive expression composition patterns such as: `expr = expr * expr + constant;`
     /// @note In such cases, prefer standard C++ lambda expressions or break the expression into smaller parts.
-    inline constexpr placeholder<8> _9{};
+    inline constexpr auto _9 = arg<9>;
     /// @brief The xtd::expressions::_10 placeholder instance is tenth argument used by expression.
     /// @par Library
     /// xtd.core
     /// @ingroup xtd_core expressions
+    /// @remarks The xtd::expressions::_10 is an alias on xtd::expressions::arg<10>
     /// @remarks xtd::expressions contains the first ten entries from xtd::expressions::placeholder. If you need an eleventh entry or more, you can define them yourself as follows:
     /// ```cpp
-    /// inline constexpr xtd::expressions::placeholder<10> _11{};
-    /// inline constexpr xtd::expressions::placeholder<11> _12{};
+    /// inline constexpr auto _11 = xtd::expressions::arg<11>;
+    /// inline constexpr auto _12 = xtd::expressions::arg<12>;
     /// //...
-    /// inline constexpr xtd::expressions::placeholder<99> _100{};
+    /// inline constexpr auto _100 = xtd::expressions::arg<100>;
     /// ```
     /// @note Extremely deep or exponentially growing expressions may lead to very large compile-time types and excessive compilation times.
     /// @note This typically occurs with recursive expression composition patterns such as: `expr = expr * expr + constant;`
     /// @note In such cases, prefer standard C++ lambda expressions or break the expression into smaller parts.
-    inline constexpr placeholder<9> _10{};
+    inline constexpr auto _10 = arg<10>;
 
     /// @brief The xtd::expressions::_ placeholder instance is used by expression that have only one argument.
     /// @par Library
     /// xtd.core
     /// @ingroup xtd_core expressions
-    /// @remarks The xtd::expressions::_ is an alias on xtd::expressions::_1
-    inline constexpr auto& _ = _1;
+    /// @remarks The xtd::expressions::_ is an alias on xtd::expressions::arg<1>
+    inline constexpr auto _ = arg<1>;
     /// @}
   }
 }
