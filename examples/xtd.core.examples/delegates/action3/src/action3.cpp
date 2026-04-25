dc40aebbc7f703385b6feee7c6c1346fc402a474
Gammasoft <gammasoft71@gmail.com>
2026-04-25 23:18:19 +0200

    Refactor examples

--- examples/xtd.core.examples/delegates/action3/src/action3.cpp
+++ examples/xtd.core.examples/delegates/action3/src/action3.cpp
@@ -4,9 +4,9 @@ auto main() -> int {
   auto message_target = action<const string&> {};
   
   if (environment::get_command_line_args().length() > 1)
-    message_target = [](const string & message) {forms::message_box::show(message);};
+    message_target = [](auto&& message) {forms::message_box::show(message);};
   else
-    message_target = [](const string & message) {console::write_line(message);};
+    message_target = [](auto&& message) {console::write_line(message);};
     
   message_target("Hello, World!");
 }
