bbcf403c707b8a9c374b9c6d2af7836676713d2b
Gammasoft <gammasoft71@gmail.com>
2026-04-26 01:21:28 +0200

    Fix documentation

--- src/xtd.core/include/xtd/expressions/less_expression.hpp
+++ src/xtd.core/include/xtd/expressions/less_expression.hpp
@@ -33,7 +33,7 @@ namespace xtd {
       /// @{
       /// @brief Equal to the specified arguments.
       /// @param args the arguments to less.
-      /// @return The result of addition.
+      /// @return The result of less.
       template <typename... args_t>
       constexpr auto operator()(args_t&&... args) const {return left(std::forward<args_t>(args)...) < right(std::forward<args_t>(args)...);}
       /// @}
