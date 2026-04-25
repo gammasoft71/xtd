f6f4d295f1ed31222bd8a5f67a1db11b2a0b5323
Gammasoft <gammasoft71@gmail.com>
2026-04-25 23:22:01 +0200

    Refactor examples

--- examples/xtd.core.examples/ranges/ranges_views_order_by/src/ranges_views_order_by.cpp
+++ examples/xtd.core.examples/ranges/ranges_views_order_by/src/ranges_views_order_by.cpp
@@ -2,7 +2,7 @@
 
 auto main() -> int {
   auto numbers = array {2, 7, 3, 1, 4, 10, 5, 6, 8, 9};
-  console::write_line("numbers = {}", numbers | order_by([](auto n) {return n;}));
+  console::write_line("numbers = {}", numbers | order_by(_));
 }
 
 // This code produces the following output :
