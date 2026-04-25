1cc40201884ee219ce512aedf2924136a16481cb
Gammasoft <gammasoft71@gmail.com>
2026-04-25 23:19:47 +0200

    Refactor examples

--- examples/xtd.core.examples/linq/enumerable_aggregate2/src/enumerable_aggregate2.cpp
+++ examples/xtd.core.examples/linq/enumerable_aggregate2/src/enumerable_aggregate2.cpp
@@ -4,10 +4,10 @@ auto main() -> int {
   auto ints = array {4, 8, 8, 3, 9, 0, 7, 8, 2};
   
   // Count the even numbers in the array, using a seed value of 0.
-  auto num_even = ints.aggregate(0, [](int total, int next) {
+  auto num_even = ints.aggregate(0, [](auto&& total, auto&& next) {
     return next % 2 == 0 ? total + 1 : total;
   });
-  
+
   console::write_line("The number of even integers is: {}", num_even);
 }
 
