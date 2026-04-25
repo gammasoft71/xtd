8f05a2c504e5f8cce614e42a2678e08ead85a302
Gammasoft <gammasoft71@gmail.com>
2026-04-25 23:18:57 +0200

    Refactor examples

--- examples/xtd.core.examples/expressions/division_expression/src/division_expression.cpp
+++ examples/xtd.core.examples/expressions/division_expression/src/division_expression.cpp
@@ -8,20 +8,20 @@ struct foo {
 };
 
 auto main() -> int {
-  // auto div1 = [](auto a) {return a / 2;}
+  // auto div1 = [](auto&& a) {return a / 2;}
   auto div1 = _ / 2;
 
   println("div1 resul => {}", div1(84));
   println();
 
-  // auto div2 = [](auto a, auto b) {return a / b;}
+  // auto div2 = [](auto&& a, auto&& b) {return a / b;}
   auto div2 = _1 / _2;
 
   println("div2 resul => {}", div2(84, 2));
   println("div2 resul => {}", div2(foo {84}, foo {2}));
   println();
 
-  // auto div3 = [](auto a, auto b, auto, c) {return a / 2 / b / c;}
+  // auto div3 = [](auto&& a, auto&& b, auto&& c) {return a / 2 / b / c;}
   auto div3 = _1 / 2 / _2  / _3;
 
   println("div3 resul => {}", div3(84, 7, 3));
