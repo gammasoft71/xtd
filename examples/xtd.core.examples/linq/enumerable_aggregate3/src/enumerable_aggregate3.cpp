c9c80da7568b9a114018d2c9a5a83417a10a2384
Gammasoft <gammasoft71@gmail.com>
2026-04-25 23:19:53 +0200

    Refactor examples

--- examples/xtd.core.examples/linq/enumerable_aggregate3/src/enumerable_aggregate3.cpp
+++ examples/xtd.core.examples/linq/enumerable_aggregate3/src/enumerable_aggregate3.cpp
@@ -5,9 +5,9 @@ auto main() -> int {
   
   // Determine whether any string in the array is longer than "banana".
   auto longest_name = fruits.aggregate("bananas",
-     [](const string& longest, const string& next) {return next.length() > longest.length() ? next : longest;},
+     [](auto&& longest, auto&& next) {return next.length() > longest.length() ? next : longest;},
      // Return the final result as an upper case string.
-     [](const string& fruit) {return fruit.to_upper();});
+     [](auto&& fruit) {return fruit.to_upper();});
 
   console::write_line("The fruit with the longest name is {}", longest_name);
 }
