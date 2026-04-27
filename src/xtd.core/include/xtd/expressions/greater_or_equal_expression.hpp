cf17d25b5c1bf1ef9adbd2f8c975affd509dde93
Gammasoft <gammasoft71@gmail.com>
2026-04-27 21:22:37 +0200

    Add documentation

--- src/xtd.core/include/xtd/expressions/greater_or_equal_expression.hpp
+++ src/xtd.core/include/xtd/expressions/greater_or_equal_expression.hpp
@@ -17,7 +17,12 @@ namespace xtd {
     /// @remarks The xtd::expressions::greater_or_equal_expression struct is used by xtd::expressions::operator >=().
     template <typename left_t, typename right_t>
     struct greater_or_equal_expression : expression_base {
+      /// @name Public Fields
+      
+      /// @{
+      /// @brief The operator precedence. That contains one of xtd::expressions::operator_precedence values.
       static constexpr operator_precedence precedence = operator_precedence::greater_or_equal;
+      /// @}
       
       /// @name Public Constructors
       
