4c3ec4a0ebf594d3d96eaa949348a3c33c72b8fe
Gammasoft <gammasoft71@gmail.com>
2026-04-27 21:21:30 +0200

    Add documentation

--- src/xtd.core/include/xtd/expressions/and_expression.hpp
+++ src/xtd.core/include/xtd/expressions/and_expression.hpp
@@ -17,7 +17,12 @@ namespace xtd {
     /// @remarks The xtd::expressions::and_expression struct is used by xtd::expressions::operator &&().
     template <typename left_t, typename right_t>
     struct and_expression : expression_base {
+      /// @name Public Fields
+      
+      /// @{
+      /// @brief The operator precedence. That contains one of xtd::expressions::operator_precedence values.
       static constexpr operator_precedence precedence = operator_precedence::logical_and;
+      /// @}
       
       /// @name Public Constructors
       
