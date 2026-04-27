04044c3693aee067773eb4ed57c6858f0bd44de3
Gammasoft <gammasoft71@gmail.com>
2026-04-27 21:23:02 +0200

    Add documentation

--- src/xtd.core/include/xtd/expressions/not_equal_expression.hpp
+++ src/xtd.core/include/xtd/expressions/not_equal_expression.hpp
@@ -17,7 +17,12 @@ namespace xtd {
     /// @remarks The xtd::expressions::not_equal_expression struct is used by xtd::expressions::operator !=().
     template <typename left_t, typename right_t>
     struct not_equal_expression : expression_base {
+      /// @name Public Fields
+      
+      /// @{
+      /// @brief The operator precedence. That contains one of xtd::expressions::operator_precedence values.
       static constexpr operator_precedence precedence = operator_precedence::not_equal;
+      /// @}
       
       /// @name Public Constructors
       
