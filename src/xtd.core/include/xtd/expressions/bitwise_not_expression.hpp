b0460b4a61f51b6a5070d3b30278a28358984c59
Gammasoft <gammasoft71@gmail.com>
2026-04-27 21:21:48 +0200

    Add documentation

--- src/xtd.core/include/xtd/expressions/bitwise_not_expression.hpp
+++ src/xtd.core/include/xtd/expressions/bitwise_not_expression.hpp
@@ -33,7 +33,12 @@ namespace xtd {
     /// ```
     template <typename value_t>
     struct bitwise_not_expression : expression_base {
+      /// @name Public Fields
+      
+      /// @{
+      /// @brief The operator precedence. That contains one of xtd::expressions::operator_precedence values.
       static constexpr operator_precedence precedence = operator_precedence::bitwise_not;
+      /// @}
       
       /// @name Public Constructors
       
