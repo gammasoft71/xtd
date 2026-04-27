044c0bfa4401331faa16ac3e5d0392aa8f75fcdf
Gammasoft <gammasoft71@gmail.com>
2026-04-27 21:21:36 +0200

    Add documentation

--- src/xtd.core/include/xtd/expressions/bitwise_and_expression.hpp
+++ src/xtd.core/include/xtd/expressions/bitwise_and_expression.hpp
@@ -37,7 +37,12 @@ namespace xtd {
     /// ```
     template <typename left_t, typename right_t>
     struct bitwise_and_expression : expression_base {
+      /// @name Public Fields
+      
+      /// @{
+      /// @brief The operator precedence. That contains one of xtd::expressions::operator_precedence values.
       static constexpr operator_precedence precedence = operator_precedence::bitwise_and;
+      /// @}
       
       /// @name Public Constructors
       
