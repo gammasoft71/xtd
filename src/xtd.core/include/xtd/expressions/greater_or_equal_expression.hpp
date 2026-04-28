12ed9b32d52016157dedc3d3da350ea603964889
Gammasoft <gammasoft71@gmail.com>
2026-04-28 23:11:00 +0200

    Refactor documentation

--- src/xtd.core/include/xtd/expressions/greater_or_equal_expression.hpp
+++ src/xtd.core/include/xtd/expressions/greater_or_equal_expression.hpp
@@ -12,6 +12,12 @@ namespace xtd {
   /// @brief The xtd::expressions namespace provides a lightweight, composable expression template framework for building and evaluating lazy, strongly-typed functional expressions from arbitrary callables
   namespace expressions {
     /// @brief The xtd::expressions::greater_or_equal_expression is the greater or equal expression.
+    /// @par Namespace
+    /// xtd::expressions
+    /// @par Header
+    /// ```cpp
+    /// #include <xtd/expressions/greater_or_equal_expression>
+    /// ```
     /// @par Library
     /// xtd.core
     /// @ingroup xtd_core expressions
@@ -60,6 +66,12 @@ namespace xtd {
     /// @param left The left operand.
     /// @param right The right operand.
     /// @return The result of greater or equal.
+    /// @par Namespace
+    /// xtd::expressions
+    /// @par Header
+    /// ```cpp
+    /// #include <xtd/expressions/greater_or_equal_expression>
+    /// ```
     /// @par Library
     /// xtd.core
     /// @ingroup xtd_core expressions
