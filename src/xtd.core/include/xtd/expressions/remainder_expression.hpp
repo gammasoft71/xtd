8865468bc7ea39f42349398b7b703ed2a88cc854
Gammasoft <gammasoft71@gmail.com>
2026-04-28 23:11:59 +0200

    Refactor documentation

--- src/xtd.core/include/xtd/expressions/remainder_expression.hpp
+++ src/xtd.core/include/xtd/expressions/remainder_expression.hpp
@@ -12,6 +12,12 @@ namespace xtd {
   /// @brief The xtd::expressions namespace provides a lightweight, composable expression template framework for building and evaluating lazy, strongly-typed functional expressions from arbitrary callables
   namespace expressions {
     /// @brief The xtd::expressions::remainder_expression is the remainder expression.
+    /// @par Namespace
+    /// xtd::expressions
+    /// @par Header
+    /// ```cpp
+    /// #include <xtd/expressions/remainder_expression>
+    /// ```
     /// @par Library
     /// xtd.core
     /// @ingroup xtd_core expressions
@@ -60,6 +66,12 @@ namespace xtd {
     /// @param left The left operand.
     /// @param right The right operand.
     /// @return The result of division.
+    /// @par Namespace
+    /// xtd::expressions
+    /// @par Header
+    /// ```cpp
+    /// #include <xtd/expressions/remainder_expression>
+    /// ```
     /// @par Library
     /// xtd.core
     /// @ingroup xtd_core expressions
