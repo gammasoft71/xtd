9d63c2a947ed8f6ac9a0bcd977ececc66b92127a
Gammasoft <gammasoft71@gmail.com>
2026-04-28 23:12:13 +0200

    Refactor documentation

--- src/xtd.core/include/xtd/expressions/unary_minus_expression.hpp
+++ src/xtd.core/include/xtd/expressions/unary_minus_expression.hpp
@@ -12,6 +12,12 @@ namespace xtd {
   /// @brief The xtd::expressions namespace provides a lightweight, composable expression template framework for building and evaluating lazy, strongly-typed functional expressions from arbitrary callables
   namespace expressions {
     /// @brief The xtd::expressions::unary_minus_expression is the unary minus expression.
+    /// @par Namespace
+    /// xtd::expressions
+    /// @par Header
+    /// ```cpp
+    /// #include <xtd/expressions/unari_minus_expression>
+    /// ```
     /// @par Library
     /// xtd.core
     /// @ingroup xtd_core expressions
@@ -58,6 +64,12 @@ namespace xtd {
     /// @param left The left operand.
     /// @param right The right operand.
     /// @return The result of unary minus.
+    /// @par Namespace
+    /// xtd::expressions
+    /// @par Header
+    /// ```cpp
+    /// #include <xtd/expressions/unari_minus_expression>
+    /// ```
     /// @par Library
     /// xtd.core
     /// @ingroup xtd_core expressions
