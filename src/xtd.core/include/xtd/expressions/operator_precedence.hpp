0dbd50e6c51d7e1485e7224c229b9009533d80e9
Gammasoft <gammasoft71@gmail.com>
2026-04-28 22:33:26 +0200

    update documentation

--- src/xtd.core/include/xtd/expressions/operator_precedence.hpp
+++ src/xtd.core/include/xtd/expressions/operator_precedence.hpp
@@ -8,71 +8,145 @@
 namespace xtd {
   /// @brief The xtd::expressions namespace provides a lightweight, composable expression template framework for building and evaluating lazy, strongly-typed functional expressions from arbitrary callables
   namespace expressions {
+    /// @brief Specifies the operator precedence.
+    /// @par Namespace
+    /// xtd
+    /// @par Header
+    /// ```cpp
+    /// #include <xtd/expressions/operator_precedence>
+    /// ```
+    /// @par Library
+    /// xtd.core
+    /// @ingroup xtd_core expressions
+    /// @remarks See [operator precedence](https://en.cppreference.com/cpp/language/operator_precedence) for more information.
     enum class operator_precedence {
-      // https://en.cppreference.com/cpp/language/operator_precedence
+      /// @brief Represnets the highest operator precedence.
       highest = 0,
-      placeholder = 0,                        // `_1`
-      constant = 0,                           // 42
-      scope_resolution = 1,                   // `a::b`
-      postfix_increment = 1,                  // `a++`
-      postfix_decrement = 1,                  // `a--`
-      functional_cast = 1,                    // `type(a)` `type {a}`
-      function_call = 2,                      // `a()`
-      subscript = 2,                          // `a[]`
-      member_access = 2,                      // `a.b` `a->b`
-      method_access = 2,                      // `a.b` `a->b`
-      prefix_increment = 2,                   // `++a`
-      prefix_decrement = 2,                   // `--a`
-      unary_plus = 2,                         // `+a`
-      unary_minus = 2,                        // `-a`
-      logical_not = 2,                        // `!a`
-      bitwise_not = 2,                        // `~a`
-      c_ctyle_cast = 2,                       // `(type)a`
-      indirection = 2,                        // `*a`
-      address_of = 3,                         // `&a`
-      reflection = 3,                         // `^^a`
-      size_of = 3,                            // `sizeof`
-      await = 3,                              // `co_await`
-      new_allocation = 3,                     // `new`  `new[]`
-      delete_dallocation = 3,                 // `delete`  `delete[]`
-      pointer_to_member = 4,                  // `a.*.b` `a->*b`
-      multiplication = 5,                     // `a * b`
-      division = 5,                           // `a / b`
-      remainder = 5,                          // `a % b`
-      addition = 6,                           // `a + b`
-      subtraction = 6,                        // `a - b`
-      bitwise_left = 7,                       // `a << b`
-      bitwise_right = 7,                      // `a >> b`
-      three_way_comparison = 8,               // `a <=> b`
-      relational = 9,                         // `a < b`,  `a <= b` `a > b` `a >= b`
-      less = 9,                               // `a < b`
-      less_or_equal = 9,                      // `a <= b`
-      greater = 9 ,                           // `a > b`
-      greater_or_equal = 9,                   // `a >= b`
-      equality = 10,                          // `a == b` // `a != b`
-      equal = 10,                             // `a == b`
-      not_equal = 10,                         // `a != b`
-      bitwise_and = 11,                       // `a & b`
-      bitwise_xor = 12,                       // `a ^ b`
-      bitwise_or = 13,                        // `a | b`
-      logical_and = 14,                       // `a && b`
-      logical_or = 15,                        // `a || b`
-      ternary = 15,                           // `a ? a : c`
-      throws = 15,                            // `throw`
-      yield = 15,                             // `co_yield`
-      assignment = 15,                        // `a = b`
-      compound_assignment = 16,               // `a += b` `a += b` `a -= b` `a *= b` `a /= b` `a %= b` `a <<= b` `a >>= b` `a &= b` `a ^= b` `a |= b`
-      compound_sum_assignment = 16,           // `a += b`
-      compound_difference_assignment = 16,    // `a -= b`
-      compound_product_assignment = 16,       // `a *= b`
-      compound_quotient_assignment = 16,      // `a /= b`
-      compound_remainder_assignment = 16,     // `a %= b`
-      compound_bitwise_left_assignment = 16,  // `a <<= b`
-      compound_bitwise_right_assignment = 16, // `a >>= b`
-      compound_bitwise_and_assignment = 16,   // `a &= b`
-      compound_bitwise_xor_assignment = 16,   // `a ^= b`
-      compound_bitwise_or_assignment = 16,    // `a |= b`
-      comma = 17,                             // `a, b`
+      /// @brief Represnets the placeholder operator precedence (`_1`).
+      placeholder = 0,
+      /// @brief Represnets the constant operator precedence (`42`).
+      constant = 0,
+      /// @brief Represnets the scope resolution operator precedence (`a::b`).
+      scope_resolution = 1,
+      /// @brief Represnets the postfix increment operator precedence (`a++`).
+      postfix_increment = 1,
+      /// @brief Represnets the postfix decrement operator precedence (`a--`).
+      postfix_decrement = 1,
+      /// @brief Represnets the functional cast operator precedence (`type(a)` `type {a}`).
+      functional_cast = 1,
+      /// @brief Represnets the function call operator precedence (`a()`).
+      function_call = 2,
+      /// @brief Represnets the subscript operator precedence (`a[]`).
+      subscript = 2,
+      /// @brief Represnets the member access operator precedence (`a.b` `a->b`).
+      member_access = 2,
+      /// @brief Represnets the method access operator precedence (`a.b` `a->b`).
+      method_access = 2,
+      /// @brief Represnets the prefix increment operator precedence (`++a`).
+      prefix_increment = 2,
+      /// @brief Represnets the prefix decrement operator precedence (`--a`).
+      prefix_decrement = 2,
+      /// @brief Represnets the unary plus operator precedence (`+a`).
+      unary_plus = 2,
+      /// @brief Represnets the unary minus operator precedence (`-a`).
+      unary_minus = 2,
+      /// @brief Represnets the logical not operator precedence (`!a`).
+      logical_not = 2,
+      /// @brief Represnets the bitwise not operator precedence (`~a`).
+      bitwise_not = 2,
+      /// @brief Represnets the c ctyle cast operator precedence (`(type)a`).
+      c_ctyle_cast = 2,
+      /// @brief Represnets the indirection operator precedence (`*a`).
+      indirection = 2,
+      /// @brief Represnets the address of operator precedence (`&a`).
+      address_of = 3,
+      /// @brief Represnets the reflection operator precedence (`^^a`).
+      reflection = 3,
+      /// @brief Represnets the size of operator precedence (`sizeof`).
+      size_of = 3,
+      /// @brief Represnets the await operator precedence (`co_await`).
+      await = 3,
+      /// @brief Represnets the new allocation operator precedence (`new`  `new[]`).
+      new_allocation = 3,
+      /// @brief Represnets the delete dallocation operator precedence (`delete`  `delete[]`).
+      delete_dallocation = 3,
+      /// @brief Represnets the pointer to member operator precedence (`a.*.b` `a->*b`).
+      pointer_to_member = 4,
+      /// @brief Represnets the multiplication operator precedence (`a * b`).
+      multiplication = 5,
+      /// @brief Represnets the division operator precedence (`a / b`).
+      division = 5,
+      /// @brief Represnets the remainder operator precedence (`a % b`).
+      remainder = 5,
+      /// @brief Represnets the addition operator precedence (`a + b`).
+      addition = 6,
+      /// @brief Represnets the subtraction operator precedence (`a - b`).
+      subtraction = 6,
+      /// @brief Represnets the bitwise left operator precedence (`a << b`).
+      bitwise_left = 7,
+      /// @brief Represnets the bitwise right operator precedence (`a >> b`).
+      bitwise_right = 7,
+      /// @brief Represnets the three way comparison operator precedence (`a <=> b`).
+      three_way_comparison = 8,
+      /// @brief Represnets the relational operator precedence (`a < b`,  `a <= b` `a > b` `a >= b`).
+      relational = 9,
+      /// @brief Represnets the less operator precedence (`a < b`).
+      less = 9,
+      /// @brief Represnets the less or equal operator precedence (`a <= b`).
+      less_or_equal = 9,
+      /// @brief Represnets the greater operator precedence (`a > b`).
+      greater = 9 ,
+      /// @brief Represnets the greater or equal operator precedence (`a >= b`).
+      greater_or_equal = 9,
+      /// @brief Represnets the equality operator precedence (`a == b` // `a != b`).
+      equality = 10,
+      /// @brief Represnets the equal operator precedence (`a == b`).
+      equal = 10,
+      /// @brief Represnets the not equal operator precedence (`a != b`).
+      not_equal = 10,
+      /// @brief Represnets the bitwise and operator precedence (`a & b`).
+      bitwise_and = 11,
+      /// @brief Represnets the bitwise xor operator precedence (`a ^ b`).
+      bitwise_xor = 12,
+      /// @brief Represnets the bitwise or operator precedence (`a | b`).
+      bitwise_or = 13,
+      /// @brief Represnets the logical and operator precedence (`a && b`).
+      logical_and = 14,
+      /// @brief Represnets the logical or operator precedence (`a || b`).
+      logical_or = 15,
+      /// @brief Represnets the ternary operator precedence (`a ? a : c`).
+      ternary = 15,
+      /// @brief Represnets the throws operator precedence (`throw`).
+      throws = 15,
+      /// @brief Represnets the yield operator precedence (`co_yield`).
+      yield = 15,
+      /// @brief Represnets the assignment operator precedence (`a = b`).
+      assignment = 15,
+      /// @brief Represnets the compound assignment operator precedence (`a += b` `a += b` `a -= b` `a *= b` `a /= b` `a %= b` `a <<= b` `a >>= b` `a &= b` `a ^= b` `a |= b`).
+      compound_assignment = 16,
+      /// @brief Represnets the compound sum assignment operator precedence (`a += b`).
+      compound_sum_assignment = 16,
+      /// @brief Represnets the compound difference assignment operator precedence (`a -= b`).
+      compound_difference_assignment = 16,
+      /// @brief Represnets the compound product assignment operator precedence (`a *= b`).
+      compound_product_assignment = 16,
+      /// @brief Represnets the compound quotient assignment operator precedence (`a /= b`).
+      compound_quotient_assignment = 16,
+      /// @brief Represnets the compound remainder assignment operator precedence (`a %= b`).
+      compound_remainder_assignment = 16,
+      /// @brief Represnets the compound bitwise left assignment operator precedence (`a <<= b`).
+      compound_bitwise_left_assignment = 16,
+      /// @brief Represnets the compound bitwise right assignment operator precedence (`a >>= b`).
+      compound_bitwise_right_assignment = 16,
+      /// @brief Represnets the compound bitwise and assignment operator precedence (`a &= b`).
+      compound_bitwise_and_assignment = 16,
+      /// @brief Represnets the compound bitwise xor assignment operator precedence (`a ^= b`).
+      compound_bitwise_xor_assignment = 16,
+      /// @brief Represnets the compound bitwise or assignment (`a |= b`) operator precedence ().
+      compound_bitwise_or_assignment = 16,
+      /// @brief Represnets the coma operator precedence (`a, b`).
+      comma = 17,
+      /// @brief Represnets the lowest operator precedence.
       lowest = 18,
     };
   }
