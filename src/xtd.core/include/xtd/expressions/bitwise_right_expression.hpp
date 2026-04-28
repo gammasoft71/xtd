8948de63dc7a990e13986cbdea9917bfebd7bbe0
Gammasoft <gammasoft71@gmail.com>
2026-04-28 23:10:19 +0200

    Refactor documentation

--- src/xtd.core/include/xtd/expressions/bitwise_right_expression.hpp
+++ src/xtd.core/include/xtd/expressions/bitwise_right_expression.hpp
@@ -13,6 +13,12 @@ namespace xtd {
   /// @brief The xtd::expressions namespace provides a lightweight, composable expression template framework for building and evaluating lazy, strongly-typed functional expressions from arbitrary callables
   namespace expressions {
     /// @brief The xtd::expressions::bitwise_right_expression is the bitwise right expression.
+    /// @par Namespace
+    /// xtd::expressions
+    /// @par Header
+    /// ```cpp
+    /// #include <xtd/expressions/bitwise_right_expression>
+    /// ```
     /// @par Library
     /// xtd.core
     /// @ingroup xtd_core expressions
@@ -85,6 +91,12 @@ namespace xtd {
     /// @param left The left operand.
     /// @param right The right operand.
     /// @return The result of bitwise right.
+    /// @par Namespace
+    /// xtd::expressions
+    /// @par Header
+    /// ```cpp
+    /// #include <xtd/expressions/bitwise_right_expression>
+    /// ```
     /// @par Library
     /// xtd.core
     /// @ingroup xtd_core expressions
