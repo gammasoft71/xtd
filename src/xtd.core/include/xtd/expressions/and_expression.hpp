eb7e7f76147f509b0895572281998bbb6098f156
Gammasoft <gammasoft71@gmail.com>
2026-04-29 19:05:05 +0200

    rename bitwise_and_expression in and_expression

--- src/xtd.core/include/xtd/expressions/and_expression.hpp
+++ src/xtd.core/include/xtd/expressions/and_expression.hpp
@@ -1,5 +1,5 @@
 /// @file
-/// @brief Contains xtd::expressions::bitwise_and_also_expression operator.
+/// @brief Contains xtd::expressions::and_expression operator.
 /// @copyright Copyright (c) 2026 Gammasoft. All rights reserved.
 #pragma once
 #include "as_expression.hpp"
@@ -12,19 +12,19 @@
 namespace xtd {
   /// @brief The xtd::expressions namespace provides a lightweight, composable expression template framework for building and evaluating lazy, strongly-typed functional expressions from arbitrary callables
   namespace expressions {
-    /// @brief The xtd::expressions::bitwise_and_also_expression is the bitwise and expression.
+    /// @brief The xtd::expressions::and_expression is the bitwise and expression.
     /// @par Namespace
     /// xtd::expressions
     /// @par Header
     /// ```cpp
-    /// #include <xtd/expressions/bitwise_and_also_expression>
+    /// #include <xtd/expressions/and_expression>
     /// ```
     /// @par Library
     /// xtd.core
     /// @ingroup xtd_core expressions
-    /// @remarks The xtd::expressions::bitwise_and_also_expression struct is used by xtd::expressions::operator |().
+    /// @remarks The xtd::expressions::and_expression struct is used by xtd::expressions::operator |().
     /// @par Examples
-    /// The following example shows how to use xtd::expressions::bitwise_and_also_expression.
+    /// The following example shows how to use xtd::expressions::and_expression.
     /// ```cpp
     /// #include <xtd/xtd>
     ///
@@ -43,7 +43,7 @@ namespace xtd {
     /// // bit_and2 result => 8
     /// ```
     template <typename left_t, typename right_t>
-    struct bitwise_and_also_expression : expression {
+    struct and_expression : expression {
       /// @name Public Fields
       
       /// @{
@@ -54,10 +54,10 @@ namespace xtd {
       /// @name Public Constructors
       
       /// @{
-      /// @brief Initialize a new xtd::expressions::bitwise_and_also_expression object with specified left and right operands.
+      /// @brief Initialize a new xtd::expressions::and_expression object with specified left and right operands.
       /// @param left The left operand.
       /// @param right The right operand.
-      constexpr bitwise_and_also_expression(left_t left, right_t right) : left {std::move(left)}, right {std::move(right)} {}
+      constexpr and_expression(left_t left, right_t right) : left {std::move(left)}, right {std::move(right)} {}
       /// @}
       
       /// @name Public Operators
@@ -77,7 +77,7 @@ namespace xtd {
       /// @}
       
       /// @cond
-      friend inline auto operator <<(std::ostream& os, const bitwise_and_also_expression& e) -> std::ostream& {return os << expression_stream {e.left, e.precedence} << " & " << expression_stream {e.right, e.precedence};}
+      friend inline auto operator <<(std::ostream& os, const and_expression& e) -> std::ostream& {return os << expression_stream {e.left, e.precedence} << " & " << expression_stream {e.right, e.precedence};}
       /// @endcond
 
     private:
@@ -96,13 +96,13 @@ namespace xtd {
     /// xtd::expressions
     /// @par Header
     /// ```cpp
-    /// #include <xtd/expressions/bitwise_and_also_expression>
+    /// #include <xtd/expressions/and_expression>
     /// ```
     /// @par Library
     /// xtd.core
     /// @ingroup xtd_core expressions
     /// @par Examples
-    /// The following example shows how to use xtd::expressions::bitwise_and_also_expression.
+    /// The following example shows how to use xtd::expressions::and_expression.
     /// ```cpp
     /// #include <xtd/xtd>
     ///
@@ -125,7 +125,7 @@ namespace xtd {
     constexpr auto operator &(left_t left, right_t right) {
       auto left_expression = as_expression(left);
       auto right_expression = as_expression(right);
-      return bitwise_and_also_expression<std::decay_t<decltype(left_expression)>, std::decay_t<decltype(right_expression)>> {std::move(left_expression), std::move(right_expression)};
+      return and_expression<std::decay_t<decltype(left_expression)>, std::decay_t<decltype(right_expression)>> {std::move(left_expression), std::move(right_expression)};
     }
     /// @}
   }
