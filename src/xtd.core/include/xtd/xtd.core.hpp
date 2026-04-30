1e9180a30ca5680c6e2831ec4478fd1afd2eed19
Gammasoft <gammasoft71@gmail.com>
2026-04-30 10:55:22 +0200

    Update xtd.core.hpp

--- src/xtd.core/include/xtd/xtd.core.hpp
+++ src/xtd.core/include/xtd/xtd.core.hpp
@@ -113,6 +113,7 @@
 #include "expressions/arg.hpp"
 #include "expressions/args.hpp"
 #include "expressions/as_expression.hpp"
+#include "expressions/binary_expression.hpp"
 #include "expressions/xor_expression.hpp"
 #include "expressions/constant.hpp"
 #include "expressions/division_expression.hpp"
