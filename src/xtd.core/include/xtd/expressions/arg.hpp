8c357d23d6613b5c2c25391ecb95e7f7b10644ba
Gammasoft <gammasoft71@gmail.com>
2026-05-01 22:46:02 +0200

    Add example

--- src/xtd.core/include/xtd/expressions/arg.hpp
+++ src/xtd.core/include/xtd/expressions/arg.hpp
@@ -28,9 +28,34 @@ namespace xtd {
     /// @ingroup xtd_core expressions
     /// @remarks The index start from 1 to N.
     /// @par Examples
+    /// The following example shows how to use xtd::expressions::expression::arg.
     /// ```cpp
-    /// auto expr = arg<1> + arg<2> + arg<3> + arg<4> + arg<5>;
-    /// println("expr result => {}", expr(10, 20, 30, 40, 50)); // expr result => 150
+    /// #include <xtd/xtd>
+    ///
+    /// auto main() -> int {
+    ///   // auto placeholder1 = [](auto&& _) {return _;};
+    ///   auto arg1 = _;
+    ///   println("arg1 result => {}", arg1(10));
+    ///   auto arg2 = _1;
+    ///   println("arg2 result => {}", arg2(10));
+    ///   auto arg3 = arg<1> {};
+    ///   println("arg3 result => {}", arg3(10));
+    ///   auto arg4 = expression::arg<1>();
+    ///   println("arg4 result => {}", arg4(10));
+    ///   println();
+    ///   // auto expr1 = [](auto&& _1, auto&& _2, auto&& _3) {return _1 + _2 + _3;};
+    ///   auto expr1 = _1 + arg<2> + expression::arg<3>();
+    ///   println("expr1 result => {}", expr1(10, 20, 30));
+    /// }
+    ///
+    /// // This code produces the following output :
+    /// //
+    /// // arg1 result => 10
+    /// // arg2 result => 10
+    /// // arg3 result => 10
+    /// // arg4 result => 10
+    /// //
+    /// // expr1 result => 60
     /// ```
     template <size_t index>
     inline constexpr xtd::expressions::placeholder<index - 1> arg {};
