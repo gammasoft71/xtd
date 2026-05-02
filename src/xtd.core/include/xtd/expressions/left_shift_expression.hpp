538c7b5ec88e0d26fc18285812ad203802021d1a
Gammasoft <gammasoft71@gmail.com>
2026-05-02 10:28:46 +0200

    Add xtd::expressions::expression::left_shift expression

--- src/xtd.core/include/xtd/expressions/left_shift_expression.hpp
+++ src/xtd.core/include/xtd/expressions/left_shift_expression.hpp
@@ -23,26 +23,7 @@ namespace xtd {
     /// @par Library
     /// xtd.core
     /// @ingroup xtd_core
-    /// @remarks The xtd::expressions::left_shift_expression struct is used by xtd::expressions::operator <<().
-    /// @par Examples
-    /// The following example shows how to use xtd::expressions::left_shift_expression.
-    /// ```cpp
-    /// #include <xtd/xtd>
-    ///
-    /// auto main() -> int {
-    ///   //auto bit_left1 = [](auto&& _) {return _ << 2;};
-    ///   auto bit_left1 = _ << 2;
-    ///   println("bit_left1 result => {}", bit_left1(42));
-    ///   //auto bit_left2 = [](auto&& _1, auto&& _2) {return _1 << _2;};
-    ///   auto bit_left2 = _1 << _2;
-    ///   println("bit_left2 result => {}", bit_left2(42, 4));
-    /// }
-    ///
-    /// // This code produces the following output :
-    /// //
-    /// // bit_left1 result => 168
-    /// // bit_left2 result => 672
-    /// ```
+    /// @remarks The xtd::expressions::left_shift_expression struct is used by xtd::expressions::expression::left_shift method.
     template <typename left_t, typename right_t>
     struct left_shift_expression : binary_expression {
       /// @name Public Fields
@@ -84,6 +65,16 @@ namespace xtd {
       [[no_unique_address]] left_t left;
       [[no_unique_address]] right_t right;
     };
+    
+    /// @cond
+    template <typename left_t, typename right_t>
+    requires std::is_base_of_v<expression, std::decay_t<left_t>> || std::is_base_of_v<expression, std::decay_t<right_t>>
+    constexpr auto expression::left_shift(left_t left, right_t right) {
+      auto left_expression = as_expression(left);
+      auto right_expression = as_expression(right);
+      return left_shift_expression<std::decay_t<decltype(left_expression)>, std::decay_t<decltype(right_expression)>> {std::move(left_expression), std::move(right_expression)};
+    }
+    /// @endcond
 
     /// @name Public Operators
     
@@ -102,31 +93,35 @@ namespace xtd {
     /// xtd.core
     /// @ingroup xtd_core expressions
     /// @par Examples
-    /// The following example shows how to use xtd::expressions::left_shift_expression.
+    /// The following example shows how to use xtd::expressions::expression::left_shift.
     /// ```cpp
     /// #include <xtd/xtd>
     ///
     /// auto main() -> int {
-    ///   //auto bit_left1 = [](auto&& _) {return _ << 2;};
-    ///   auto bit_left1 = _ << 2;
-    ///   println("bit_left1 result => {}", bit_left1(42));
-    ///   //auto bit_left2 = [](auto&& _1, auto&& _2) {return _1 << _2;};
-    ///   auto bit_left2 = _1 << _2;
-    ///   println("bit_left2 result => {}", bit_left2(42, 4));
+    ///   // auto left_shift1 = [](auto&& _) {return _ << 2;};
+    ///   auto left_shift1 = _ << 2;
+    ///   println("left_shift1 result => {}", left_shift1(42));
+    ///   auto left_shift2 = expression::left_shift(_, 2);
+    ///   println("left_shift2 result => {}", left_shift2(42));
+    ///   println();
+    ///   // auto left_shift3 = [](auto&& _1, auto&& _2) {return _1 << _2;};
+    ///   auto left_shift3 = _1 << _2;
+    ///   println("left_shift3 result => {}", left_shift3(42, 4));
+    ///   auto left_shift4 = expression::left_shift(_1, _2);
+    ///   println("left_shift4 result => {}", left_shift4(42, 4));
     /// }
     ///
     /// // This code produces the following output :
     /// //
-    /// // bit_left1 result => 168
-    /// // bit_left2 result => 672
+    /// // left_shift1 result => 168
+    /// // left_shift2 result => 168
+    /// //
+    /// // left_shift3 result => 672
+    /// // left_shift4 result => 672
     /// ```
     template <typename left_t, typename right_t>
     requires expression_operand<left_t> || expression_operand<right_t> && (!std::is_base_of_v<std::ostream, std::remove_reference_t<left_t>>)
-    constexpr auto operator <<(left_t left, right_t right) {
-      auto left_expression = as_expression(left);
-      auto right_expression = as_expression(right);
-      return left_shift_expression<std::decay_t<decltype(left_expression)>, std::decay_t<decltype(right_expression)>> {std::move(left_expression), std::move(right_expression)};
-    }
+    constexpr auto operator <<(left_t left, right_t right) {return expression::left_shift(std::move(left), std::move(right));}
     /// @}
   }
 }
