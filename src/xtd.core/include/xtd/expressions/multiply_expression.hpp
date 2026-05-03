f7307289adc74f7bcb9c5caafa494fce8d8d9c3b
Gammasoft <gammasoft71@gmail.com>
2026-05-03 17:35:34 +0200

    Add default constructor

--- src/xtd.core/include/xtd/expressions/multiply_expression.hpp
+++ src/xtd.core/include/xtd/expressions/multiply_expression.hpp
@@ -35,6 +35,9 @@ namespace xtd {
       /// @name Public Constructors
       
       /// @{
+      /// @brief Initialize a new xtd::expressions::multiply_expression object.
+      constexpr multiply_expression() = default;
+      
       /// @brief Initialize a new xtd::expressions::multiply_expression object with specified left and right operands.
       /// @param left The left operand.
       /// @param right The right operand.
