1b21837b2a6b172f544e1e0fbf5e484c42084cfc
Gammasoft <gammasoft71@gmail.com>
2026-05-03 17:35:14 +0200

    Add default constructor

--- src/xtd.core/include/xtd/expressions/logical_or_expression.hpp
+++ src/xtd.core/include/xtd/expressions/logical_or_expression.hpp
@@ -35,6 +35,9 @@ namespace xtd {
       /// @name Public Constructors
       
       /// @{
+      /// @brief Initialize a new xtd::expressions::logical_or_expression object.
+      constexpr logical_or_expression() = default;
+      
       /// @brief Initialize a new xtd::expressions::logical_or_expression object with specified left and right operands.
       /// @param left The left operand.
       /// @param right The right operand.
