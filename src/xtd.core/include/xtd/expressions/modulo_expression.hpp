8b92c0d20c7de432c4fd2a320f9a369b444b4386
Gammasoft <gammasoft71@gmail.com>
2026-05-03 17:35:30 +0200

    Add default constructor

--- src/xtd.core/include/xtd/expressions/modulo_expression.hpp
+++ src/xtd.core/include/xtd/expressions/modulo_expression.hpp
@@ -35,6 +35,9 @@ namespace xtd {
       /// @name Public Constructors
       
       /// @{
+      /// @brief Initialize a new xtd::expressions::modulo_expression object.
+      constexpr modulo_expression() = default;
+      
       /// @brief Initialize a new xtd::expressions::modulo_expression object with specified left and right operands.
       /// @param left The left operand.
       /// @param right The right operand.
