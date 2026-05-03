c5b4170afcd0b0fd1f0cd3e564b986bf7f131963
Gammasoft <gammasoft71@gmail.com>
2026-05-03 17:34:22 +0200

    Add default constructor

--- src/xtd.core/include/xtd/expressions/equal_expression.hpp
+++ src/xtd.core/include/xtd/expressions/equal_expression.hpp
@@ -35,6 +35,9 @@ namespace xtd {
       /// @name Public Constructors
       
       /// @{
+      /// @brief Initialize a new xtd::expressions::equal_expression object.
+      constexpr equal_expression() = default;
+      
       /// @brief Initialize a new xtd::expressions::equal_expression object with specified left and right operands.
       /// @param left The left operand.
       /// @param right The right operand.
