c6a727df329a1f23fa3a73df08092b5666c7bd08
Gammasoft <gammasoft71@gmail.com>
2026-05-04 01:11:20 +0200

    Fix documentation

--- src/xtd.core/include/xtd/expressions/multiply_expression.hpp
+++ src/xtd.core/include/xtd/expressions/multiply_expression.hpp
@@ -22,7 +22,7 @@ namespace xtd {
     /// @par Library
     /// xtd.core
     /// @ingroup xtd_core
-    /// @remarks The xtd::expressions::multiply_expression struct is used by xtd::expressions::expression::multiply method.
+    /// @remarks The xtd::expressions::multiply_expression struct is used by xtd::expressions::expression::multiply expression.
     template <typename left_t, typename right_t>
     struct multiply_expression : binary_expression {
       /// @name Public Fields
