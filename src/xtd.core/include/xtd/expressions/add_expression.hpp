fe1afb9f69cfb9b0775d264e97ec219f8488526d
Gammasoft <gammasoft71@gmail.com>
2026-05-03 17:33:53 +0200

    Add default constructor

--- src/xtd.core/include/xtd/expressions/add_expression.hpp
+++ src/xtd.core/include/xtd/expressions/add_expression.hpp
@@ -35,6 +35,9 @@ namespace xtd {
       /// @name Public Constructors
       
       /// @{
+      /// @brief Initialize a new xtd::expressions::add_expression object.
+      constexpr add_expression() = default;
+
       /// @brief Initialize a new xtd::expressions::add_expression object with specified left and right operands.
       /// @param left The left operand.
       /// @param right The right operand.
