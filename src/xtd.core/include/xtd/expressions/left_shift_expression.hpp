cbb6ef055617c88ea53299306ea8d11db8010faf
Gammasoft <gammasoft71@gmail.com>
2026-05-04 01:10:07 +0200

    Fix documentation

--- src/xtd.core/include/xtd/expressions/left_shift_expression.hpp
+++ src/xtd.core/include/xtd/expressions/left_shift_expression.hpp
@@ -23,7 +23,7 @@ namespace xtd {
     /// @par Library
     /// xtd.core
     /// @ingroup xtd_core
-    /// @remarks The xtd::expressions::left_shift_expression struct is used by xtd::expressions::expression::left_shift method.
+    /// @remarks The xtd::expressions::left_shift_expression struct is used by xtd::expressions::expression::left_shift expression.
     template <typename left_t, typename right_t>
     struct left_shift_expression : binary_expression {
       /// @name Public Fields
