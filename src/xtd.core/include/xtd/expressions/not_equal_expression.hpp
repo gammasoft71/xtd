0e62f77519ab7da1b4565aee5fac67acc3949a80
Gammasoft <gammasoft71@gmail.com>
2026-05-03 17:35:38 +0200

    Add default constructor

--- src/xtd.core/include/xtd/expressions/not_equal_expression.hpp
+++ src/xtd.core/include/xtd/expressions/not_equal_expression.hpp
@@ -35,6 +35,9 @@ namespace xtd {
       /// @name Public Constructors
       
       /// @{
+      /// @brief Initialize a new xtd::expressions::not_equal_expression object.
+      constexpr not_equal_expression() = default;
+      
       /// @brief Initialize a new xtd::expressions::not_equal_expression object with specified left and right operands.
       /// @param left The left operand.
       /// @param right The right operand.
