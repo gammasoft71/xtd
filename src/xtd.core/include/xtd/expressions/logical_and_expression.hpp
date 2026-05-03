a6562d67acb821225bd572d4291e1ab127345eeb
Gammasoft <gammasoft71@gmail.com>
2026-05-04 01:11:08 +0200

    Fix documentation

--- src/xtd.core/include/xtd/expressions/logical_and_expression.hpp
+++ src/xtd.core/include/xtd/expressions/logical_and_expression.hpp
@@ -22,7 +22,7 @@ namespace xtd {
     /// @par Library
     /// xtd.core
     /// @ingroup xtd_core
-    /// @remarks The xtd::expressions::logical_and_expression struct is used by xtd::expressions::expression::and_also method.
+    /// @remarks The xtd::expressions::logical_and_expression struct is used by xtd::expressions::expression::logical_and expression.
     template <typename left_t, typename right_t>
     struct logical_and_expression : binary_expression {
       /// @name Public Fields
