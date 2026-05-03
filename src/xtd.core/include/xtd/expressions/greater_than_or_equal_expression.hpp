497f40b99df5df8cd457ae89fe34ded0aea1d146
Gammasoft <gammasoft71@gmail.com>
2026-05-03 17:34:43 +0200

    Add default constructor

--- src/xtd.core/include/xtd/expressions/greater_than_or_equal_expression.hpp
+++ src/xtd.core/include/xtd/expressions/greater_than_or_equal_expression.hpp
@@ -35,6 +35,9 @@ namespace xtd {
       /// @name Public Constructors
       
       /// @{
+      /// @brief Initialize a new xtd::expressions::greater_than_or_equal_expression object.
+      constexpr greater_than_or_equal_expression() = default;
+      
       /// @brief Initialize a new xtd::expressions::greater_than_or_equal_expression object with specified left and right operands.
       /// @param left The left operand.
       /// @param right The right operand.
