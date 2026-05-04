9235e82a5bc765073ba7f563ab18926ca3f22a43
Gammasoft <gammasoft71@gmail.com>
2026-05-04 23:18:20 +0200

    refactor example

--- examples/xtd.forms.examples/others/button2/src/button2.cpp
+++ examples/xtd.forms.examples/others/button2/src/button2.cpp
@@ -1,22 +1,19 @@
-#include <xtd/forms/application>
-#include <xtd/forms/button>
-#include <xtd/forms/form>
-#include <xtd/forms/label>
+#include <xtd/xtd>
 
 auto main() -> int {
   auto button1_clicked = 0, button2_clicked = 0;
-  xtd::forms::button button1, button2;
-  xtd::forms::label label1, label2;
-  xtd::forms::form form1;
+  button button1, button2;
+  label label1, label2;
+  form form1;
   
   button1.text("Button 1").location({50, 50});
   button2.auto_repeat(true).text("Button 2").location({50, 100}).size({200, 75});
   label1.text("Button 1 clicked 0 times").location({50, 200}).width(200);
   label2.text("Button 2 clicked 0 times").location({50, 230}).width(200);
-  button1.click += delegate_ {label1.text(xtd::string::format("Button 1 clicked {} times", ++button1_clicked));};
-  button2.click += delegate_ {label2.text(xtd::string::format("Button 2 clicked {} times", ++button2_clicked));};
+  button1.click += delegate_ {label1.text(string::format("Button 1 clicked {} times", ++button1_clicked));};
+  button2.click += delegate_ {label2.text(string::format("Button 2 clicked {} times", ++button2_clicked));};
   form1.text("Button 2 example");
   form1.controls().add_range({button1, button2, label1, label2});
   
-  xtd::forms::application::run(form1);
+  application::run(form1);
 }
