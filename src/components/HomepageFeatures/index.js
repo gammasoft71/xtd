cf5e553e4618d9c703f5b48c79eb3edb9329c7b0
Gammasoft <gammasoft71@gmail.com>
2026-05-06 21:24:20 +0200

    update xtd versions

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -40,9 +40,9 @@ const FeatureList = [
     title: 'Latest news',
     description: (
       <>
-      • <a href="docs/news/20250615">xtd 0.2.0 will require C++20 as the minimum standard</a> - Posted on June 15, 2025
+      • <a href="docs/news/20250615">xtd 1.0.0 will require C++20 as the minimum standard</a> - Posted on June 15, 2025
       <br/>• <a href="docs/news/20240312">Release xtd 0.1.2 (beta)</a> - Posted on March 12, 2024
-      <br/>• <a href="docs/news/20231112">The xtd 0.2.0 (beta) is growing</a> - Posted on November 12, 2023
+      <br/>• <a href="docs/news/20231112">The xtd 1.0.0 (beta) is growing</a> - Posted on November 12, 2023
       <br/>• <a href="docs/news/20230717">Release xtd 0.1.1 (beta)</a> - Posted on Jully 17, 2023
       <br/>• <a href="docs/news">All news</a>
       </>
