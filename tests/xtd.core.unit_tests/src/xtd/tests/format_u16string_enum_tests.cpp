728b0840cf8d44c6fd81d42793bb2dab0cc0f4e6
Gammasoft <gammasoft71@gmail.com>
2026-05-07 20:51:56 +0200

    refactor code

--- tests/xtd.core.unit_tests/src/xtd/tests/format_u16string_enum_tests.cpp
+++ tests/xtd.core.unit_tests/src/xtd/tests/format_u16string_enum_tests.cpp
@@ -56,7 +56,7 @@ namespace xtd::tests {
     }
     
     auto test_method_(format_tuesday_with_invalid_argument) {
-      assert::throws<xtd::format_exception>([] {format(u"{0:e}", tuesday);});
+      assert::throws<xtd::format_exception>([] {[[maybe_unused]] auto s = format(u"{0:e}", tuesday);});
     }
   };
 }
