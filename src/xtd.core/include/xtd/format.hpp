038d641c89d19bdf38643c970ad2f1b128f2cd67
Gammasoft <gammasoft71@gmail.com>
2026-05-07 19:52:19 +0200

    refactor code

--- src/xtd.core/include/xtd/format.hpp
+++ src/xtd.core/include/xtd/format.hpp
@@ -18,7 +18,7 @@ namespace xtd {
   /// @ingroup format_parse
   /// @remarks for more information about format see @ref FormatPage "Format".
   template<typename ...args_t>
-  xtd::string format(const xtd::string& fmt, args_t&& ... args) {return xtd::string::format(fmt, std::forward<args_t>(args)...);}
+  [[nodiscard]] auto format(const xtd::string& fmt, args_t&& ... args) -> xtd::string {return xtd::string::format(fmt, std::forward<args_t>(args)...);}
   /// @brief Writes the text representation of the specified arguments list, to string using the specified format information.
   /// @param fmt A composite format string.
   /// @param args arguments list to write using format.
@@ -26,7 +26,7 @@ namespace xtd {
   /// @ingroup format_parse
   /// @remarks for more information about format see @ref FormatPage "Format".
   template<typename ...args_t>
-  xtd::u8string format(const xtd::u8string& fmt, args_t&& ... args) {return xtd::string::format(fmt.to_string(), std::forward<args_t>(args)...);}
+  [[nodiscard]] auto format(const xtd::u8string& fmt, args_t&& ... args) -> xtd::u8string {return xtd::string::format(fmt.to_string(), std::forward<args_t>(args)...);}
   /// @brief Writes the text representation of the specified arguments list, to string using the specified format information.
   /// @param fmt A composite format string.
   /// @param args arguments list to write using format.
@@ -34,7 +34,7 @@ namespace xtd {
   /// @ingroup format_parse
   /// @remarks for more information about format see @ref FormatPage "Format".
   template<typename ...args_t>
-  xtd::u16string format(const xtd::u16string& fmt, args_t&& ... args) {return xtd::string::format(fmt.to_string(), std::forward<args_t>(args)...);}
+  [[nodiscard]] auto format(const xtd::u16string& fmt, args_t&& ... args) -> xtd::u16string {return xtd::string::format(fmt.to_string(), std::forward<args_t>(args)...);}
   /// @brief Writes the text representation of the specified arguments list, to string using the specified format information.
   /// @param fmt A composite format string.
   /// @param args arguments list to write using format.
@@ -42,7 +42,7 @@ namespace xtd {
   /// @ingroup format_parse
   /// @remarks for more information about format see @ref FormatPage "Format".
   template<typename ...args_t>
-  xtd::u32string format(const xtd::u32string& fmt, args_t&& ... args) {return xtd::string::format(fmt.to_string(), std::forward<args_t>(args)...);}
+  [[nodiscard]] auto format(const xtd::u32string& fmt, args_t&& ... args) -> xtd::u32string {return xtd::string::format(fmt.to_string(), std::forward<args_t>(args)...);}
   /// @brief Writes the text representation of the specified arguments list, to string using the specified format information.
   /// @param fmt A composite format string.
   /// @param args arguments list to write using format.
@@ -50,19 +50,19 @@ namespace xtd {
   /// @ingroup format_parse
   /// @remarks for more information about format see @ref FormatPage "Format".
   template<typename ...args_t>
-  xtd::wstring format(const xtd::wstring& fmt, args_t&& ... args) {return xtd::string::format(fmt.to_string(), std::forward<args_t>(args)...);}
+  [[nodiscard]] auto format(const xtd::wstring& fmt, args_t&& ... args) -> xtd::wstring {return xtd::string::format(fmt.to_string(), std::forward<args_t>(args)...);}
   
   /// @cond
   template<typename ...args_t>
-  xtd::string format(const char* fmt, args_t&& ... args) {return xtd::string::format(std::string {fmt}, std::forward<args_t>(args)...);}
+  [[nodiscard]] auto format(const char* fmt, args_t&& ... args) -> xtd::string {return xtd::string::format(std::string {fmt}, std::forward<args_t>(args)...);}
   template<typename ...args_t>
-  xtd::u8string format(const char8_t* fmt, args_t&& ... args) {return xtd::string::format(std::u8string {fmt}, std::forward<args_t>(args)...);}
+  [[nodiscard]] auto format(const char8_t* fmt, args_t&& ... args) -> xtd::u8string {return xtd::string::format(std::u8string {fmt}, std::forward<args_t>(args)...);}
   template<typename ...args_t>
-  xtd::u16string format(const char16_t* fmt, args_t&& ... args) {return xtd::string::format(std::u16string {fmt}, std::forward<args_t>(args)...);}
+  [[nodiscard]] auto format(const char16_t* fmt, args_t&& ... args) -> xtd::u16string {return xtd::string::format(std::u16string {fmt}, std::forward<args_t>(args)...);}
   template<typename ...args_t>
-  xtd::u32string format(const char32_t* fmt, args_t&& ... args) {return xtd::string::format(std::u32string {fmt}, std::forward<args_t>(args)...);}
+  [[nodiscard]] auto format(const char32_t* fmt, args_t&& ... args) -> xtd::u32string {return xtd::string::format(std::u32string {fmt}, std::forward<args_t>(args)...);}
   template<typename ...args_t>
-  xtd::wstring format(const wchar_t* fmt, args_t&& ... args) {return xtd::string::format(std::wstring {fmt}, std::forward<args_t>(args)...);}
+  [[nodiscard]] auto format(const wchar_t* fmt, args_t&& ... args) -> xtd::wstring {return xtd::string::format(std::wstring {fmt}, std::forward<args_t>(args)...);}
   /// @endcond
   
   /// @brief Writes the text representation of the specified arguments list, to string using the specified format information.
@@ -72,7 +72,7 @@ namespace xtd {
   /// @ingroup format_parse
   /// @remarks for more information about format see @ref FormatPage "Format".
   template<typename ...args_t>
-  xtd::string format(const xtd::globalization::culture_info& culture, const xtd::string& fmt, args_t&& ... args) {return xtd::string::format(culture, fmt, std::forward<args_t>(args)...);}
+  [[nodiscard]] auto format(const xtd::globalization::culture_info& culture, const xtd::string& fmt, args_t&& ... args) -> xtd::string {return xtd::string::format(culture, fmt, std::forward<args_t>(args)...);}
   /// @brief Writes the text representation of the specified arguments list, to string using the specified format information.
   /// @param fmt A composite format string.
   /// @param args arguments list to write using format.
@@ -80,7 +80,7 @@ namespace xtd {
   /// @ingroup format_parse
   /// @remarks for more information about format see @ref FormatPage "Format".
   template<typename ...args_t>
-  xtd::u8string format(const xtd::globalization::culture_info& culture, const xtd::u8string& fmt, args_t&& ... args) {return xtd::string::format(culture, fmt.to_string(), std::forward<args_t>(args)...);}
+  [[nodiscard]] auto format(const xtd::globalization::culture_info& culture, const xtd::u8string& fmt, args_t&& ... args) -> xtd::u8string {return xtd::string::format(culture, fmt.to_string(), std::forward<args_t>(args)...);}
   /// @brief Writes the text representation of the specified arguments list, to string using the specified format information.
   /// @param fmt A composite format string.
   /// @param args arguments list to write using format.
@@ -88,7 +88,7 @@ namespace xtd {
   /// @ingroup format_parse
   /// @remarks for more information about format see @ref FormatPage "Format".
   template<typename ...args_t>
-  xtd::u16string format(const xtd::globalization::culture_info& culture, const xtd::u16string& fmt, args_t&& ... args) {return xtd::string::format(culture, fmt.to_string(), std::forward<args_t>(args)...);}
+  [[nodiscard]] auto format(const xtd::globalization::culture_info& culture, const xtd::u16string& fmt, args_t&& ... args) -> xtd::u16string {return xtd::string::format(culture, fmt.to_string(), std::forward<args_t>(args)...);}
   /// @brief Writes the text representation of the specified arguments list, to string using the specified format information.
   /// @param fmt A composite format string.
   /// @param args arguments list to write using format.
@@ -96,7 +96,7 @@ namespace xtd {
   /// @ingroup format_parse
   /// @remarks for more information about format see @ref FormatPage "Format".
   template<typename ...args_t>
-  xtd::u32string format(const xtd::globalization::culture_info& culture, const xtd::u32string& fmt, args_t&& ... args) {return xtd::string::format(culture, fmt.to_string(), std::forward<args_t>(args)...);}
+  [[nodiscard]] auto format(const xtd::globalization::culture_info& culture, const xtd::u32string& fmt, args_t&& ... args) -> xtd::u32string {return xtd::string::format(culture, fmt.to_string(), std::forward<args_t>(args)...);}
   /// @brief Writes the text representation of the specified arguments list, to string using the specified format information.
   /// @param fmt A composite format string.
   /// @param args arguments list to write using format.
@@ -104,18 +104,18 @@ namespace xtd {
   /// @ingroup format_parse
   /// @remarks for more information about format see @ref FormatPage "Format".
   template<typename ...args_t>
-  xtd::wstring format(const xtd::globalization::culture_info& culture, const xtd::wstring& fmt, args_t&& ... args) {return xtd::string::format(culture, fmt.to_string(), std::forward<args_t>(args)...);}
+  [[nodiscard]] auto format(const xtd::globalization::culture_info& culture, const xtd::wstring& fmt, args_t&& ... args) -> xtd::wstring {return xtd::string::format(culture, fmt.to_string(), std::forward<args_t>(args)...);}
   
   /// @cond
   template<typename ...args_t>
-  xtd::string format(const xtd::globalization::culture_info& culture, const char* fmt, args_t&& ... args) {return xtd::string::format(culture, std::string {fmt}, std::forward<args_t>(args)...);}
+  [[nodiscard]] auto format(const xtd::globalization::culture_info& culture, const char* fmt, args_t&& ... args) -> xtd::string {return xtd::string::format(culture, std::string {fmt}, std::forward<args_t>(args)...);}
   template<typename ...args_t>
-  xtd::u8string format(const xtd::globalization::culture_info& culture, const char8_t* fmt, args_t&& ... args) {return xtd::string::format(culture, std::u8string {fmt}, std::forward<args_t>(args)...);}
+  [[nodiscard]] auto format(const xtd::globalization::culture_info& culture, const char8_t* fmt, args_t&& ... args) -> xtd::u8string {return xtd::string::format(culture, std::u8string {fmt}, std::forward<args_t>(args)...);}
   template<typename ...args_t>
-  xtd::u16string format(const xtd::globalization::culture_info& culture, const char16_t* fmt, args_t&& ... args) {return xtd::string::format(culture, std::u16string {fmt}, std::forward<args_t>(args)...);}
+  [[nodiscard]] auto format(const xtd::globalization::culture_info& culture, const char16_t* fmt, args_t&& ... args) -> xtd::u16string {return xtd::string::format(culture, std::u16string {fmt}, std::forward<args_t>(args)...);}
   template<typename ...args_t>
-  xtd::u32string format(const xtd::globalization::culture_info& culture, const char32_t* fmt, args_t&& ... args) {return xtd::string::format(culture, std::u32string {fmt}, std::forward<args_t>(args)...);}
+  [[nodiscard]] auto format(const xtd::globalization::culture_info& culture, const char32_t* fmt, args_t&& ... args) -> xtd::u32string {return xtd::string::format(culture, std::u32string {fmt}, std::forward<args_t>(args)...);}
   template<typename ...args_t>
-  xtd::wstring format(const xtd::globalization::culture_info& culture, const wchar_t* fmt, args_t&& ... args) {return xtd::string::format(culture, std::wstring {fmt}, std::forward<args_t>(args)...);}
+  [[nodiscard]] auto format(const xtd::globalization::culture_info& culture, const wchar_t* fmt, args_t&& ... args) -> xtd::wstring {return xtd::string::format(culture, std::wstring {fmt}, std::forward<args_t>(args)...);}
   /// @endcond
 }
