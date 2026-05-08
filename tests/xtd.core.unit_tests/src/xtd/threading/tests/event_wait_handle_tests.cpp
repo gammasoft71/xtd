1ba6aadbb835844f470a039798310b1ecdf990b8
Gammasoft <gammasoft71@gmail.com>
2026-05-08 21:52:33 +0200

    refactor code

--- tests/xtd.core.unit_tests/src/xtd/threading/tests/event_wait_handle_tests.cpp
+++ tests/xtd.core.unit_tests/src/xtd/threading/tests/event_wait_handle_tests.cpp
@@ -1,5 +1,6 @@
 #include <xtd/threading/event_wait_handle>
 #include <xtd/threading/thread>
+#include <xtd/argument_exception>
 #include <xtd/environment>
 #include <xtd/tunit/assert>
 #include <xtd/tunit/test_class_attribute>
