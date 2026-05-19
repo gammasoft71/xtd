d4f4ec387690de2c78bd41a6aeb620518b5e6e20
Gammasoft <gammasoft71@gmail.com>
2026-05-20 00:18:45 +0200

    Add concurrent_bag unit tests

--- tests/xtd.core.unit_tests/src/xtd/collections/concurrent/tests/concurrent_bag_tests.cpp
+++ tests/xtd.core.unit_tests/src/xtd/collections/concurrent/tests/concurrent_bag_tests.cpp
@@ -136,7 +136,7 @@ namespace xtd::collections::tests {
     auto test_method_(concurrent_add) {
       auto start_event = manual_reset_event {};
       auto tasks = list<task<>> {};
-      auto items = concurrent::concurrent_bag<int> {};
+      auto items = concurrent_bag<int> {};
       for (auto task = 0; task < 100; ++task) {
         tasks.add(task_factory {}.start_new([&] {
           start_event.wait_one();
@@ -153,6 +153,9 @@ namespace xtd::collections::tests {
     
     auto test_method_(copy_to) {
       auto a1 = array<int>(5_z);
+      concurrent_bag<int> {}.copy_to(a1, 0);
+      collection_assert::are_equal({0, 0, 0, 0, 0}, a1);
+
       concurrent_bag<int> {1, 2, 3, 4, 5}.copy_to(a1, 0);
       collection_assert::are_equal({1, 2, 3, 4, 5}, a1);
 
@@ -170,7 +173,92 @@ namespace xtd::collections::tests {
       auto enumeator = items.get_enumerator();
       assert::throws<invalid_operation_exception>([&] {[[maybe_unused]] auto v = enumeator.current();});
       assert::is_true(enumeator.move_next());
-      assert::are_equal(1, enumeator.current());        
+      assert::are_equal(1, enumeator.current());
+      assert::is_true(enumeator.move_next());
+      assert::are_equal(2, enumeator.current());
+      assert::is_true(enumeator.move_next());
+      assert::are_equal(3, enumeator.current());
+      assert::is_true(enumeator.move_next());
+      assert::are_equal(4, enumeator.current());
+      assert::is_true(enumeator.move_next());
+      assert::are_equal(5, enumeator.current());
+      assert::is_false(enumeator.move_next());
+      assert::throws<invalid_operation_exception>([&] {[[maybe_unused]] auto v = enumeator.current();});
+    }
+    
+    auto test_method_(to_array) {
+      collection_assert::is_empty(concurrent_bag<int> {}.to_array());
+      collection_assert::are_equal({1, 2, 3, 4, 5}, concurrent_bag {1, 2, 3, 4, 5}.to_array());
+    }
+    
+    auto test_method_(to_string) {
+      assert::are_equal("[]", concurrent_bag<int> {}.to_string());
+      assert::are_equal("[1, 2, 3, 4, 5]", concurrent_bag {1, 2, 3, 4, 5}.to_string());
+    }
+    
+    auto test_method_(try_add) {
+      auto items = concurrent_bag<int> {};
+      assert::is_true(items.try_add(1));
+      collection_assert::are_equivalent({1}, items);
+      assert::is_true(items.try_add(2));
+      collection_assert::are_equivalent({1, 2}, items);
+      assert::is_true(items.try_add(3));
+      collection_assert::are_equivalent({1, 2, 3}, items);
+      assert::is_true(items.try_add(4));
+      collection_assert::are_equivalent({1, 2, 3, 4}, items);
+      assert::is_true(items.try_add(5));
+      collection_assert::are_equivalent({1, 2, 3, 4, 5}, items);
+    }
+    
+    auto test_method_(concurrent_try_add) {
+      auto start_event = manual_reset_event {};
+      auto tasks = list<task<>> {};
+      auto items = concurrent_bag<int> {};
+      auto result_try = std::atomic<bool> {true};
+      for (auto task = 0; task < 100; ++task) {
+        tasks.add(task_factory {}.start_new([&] {
+          start_event.wait_one();
+          for (auto count = 0; count < 100; ++count) {
+            result_try = result_try && items.try_add(task * 10 + count);
+            thread::yield();
+          }
+        }));
+      }
+      start_event.set();
+      task<>::wait_all(tasks);
+      assert::is_true(result_try);
+      assert::are_equal(10000_z, items.count());
+    }
+    
+    auto test_method_(try_peek) {
+      auto result = 0;
+      assert::is_false(concurrent_bag<int> {}.try_peek(result));
+      auto items = concurrent_bag {1, 2, 3, 4, 5};
+      assert::is_true(items.try_peek(result));
+      assert::are_equal(1, result);
+      collection_assert::are_equivalent({1, 2, 3, 4, 5}, items);
+    }
+  
+    auto test_method_(try_take) {
+      auto result = 0;
+      assert::is_false(concurrent_bag<int> {}.try_take(result));
+      auto items = concurrent_bag {1, 2, 3, 4, 5};
+      assert::is_true(items.try_take(result));
+      assert::are_equal(1, result);
+      collection_assert::are_equivalent({2, 3, 4, 5}, items);
+      assert::is_true(items.try_take(result));
+      assert::are_equal(2, result);
+      collection_assert::are_equivalent({3, 4, 5}, items);
+      assert::is_true(items.try_take(result));
+      assert::are_equal(3, result);
+      collection_assert::are_equivalent({4, 5}, items);
+      assert::is_true(items.try_take(result));
+      assert::are_equal(4, result);
+      collection_assert::are_equivalent({5}, items);
+      assert::is_true(items.try_take(result));
+      assert::are_equal(5, result);
+      collection_assert::is_empty(items);
+      assert::is_false(items.try_take(result));
     }
   };
 }
