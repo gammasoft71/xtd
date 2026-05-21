2b77b4fd696735823e68482ef1810b66ef69d705
Gammasoft <gammasoft71@gmail.com>
2026-05-22 00:13:25 +0200

    refactor code : using trailing return type

--- src/xtd.forms.native/include/xtd/forms/native/window_definitions.hpp
+++ src/xtd.forms.native/include/xtd/forms/native/window_definitions.hpp
@@ -34,33 +34,27 @@ struct HELPINFO {
   POINT MousePos;
 };
 
-template < class type1_t, typename type2_t >
-xtd::uint16 MAKEWORD(type1_t a, type2_t b) {
-  return static_cast < xtd::uint16 > ((static_cast < xtd::uint16>((static_cast<xtd::uint32>(a)) & 0xff)) | (static_cast<xtd::uint16 > (static_cast < xtd::uint16 > ((static_cast < xtd::uint32 > (b)) & 0xff))) << 8);
+[[nodiscard]] auto MAKEWORD(auto a, auto b) -> xtd::uint16 {
+  return static_cast<xtd::uint16>((static_cast<xtd::uint16>((static_cast<xtd::uint32>(a)) & 0xff)) | (static_cast<xtd::uint16>(static_cast <xtd::uint16>((static_cast<xtd::uint32>(b)) & 0xff))) << 8);
 }
 
-template < class type1_t, typename type2_t >
-xtd::uint32 MAKELONG(type1_t a, type2_t b) {
-  return static_cast < xtd::uint32 > ((static_cast < xtd::uint16>((static_cast<xtd::uint32>(a)) & 0xffff)) | (static_cast<xtd::uint32 > (static_cast < xtd::uint16 > ((static_cast < xtd::uint32 > (b)) & 0xffff))) << 16);
+[[nodiscard]] auto MAKELONG(auto a, auto b) -> xtd::uint16 {
+  return static_cast<xtd::uint32>((static_cast<xtd::uint16>((static_cast<xtd::uint32>(a)) & 0xffff)) | (static_cast<xtd::uint32>(static_cast<xtd::uint16>((static_cast<xtd::uint32>(b)) & 0xffff))) << 16);
 }
 
-template < class type_t >
-inline xtd::uint16 LOWORD(type_t value) {
-  return static_cast < xtd::uint16 > (static_cast < xtd::uint32 > (value) & 0xFFFF);
+[[nodiscard]] inline auto LOWORD(auto value) -> xtd::uint16 {
+  return static_cast<xtd::uint16>(static_cast<xtd::uint32>(value) & 0xFFFF);
 }
 
-template < class type_t >
-inline xtd::uint16 HIWORD(type_t value) {
-  return static_cast < xtd::uint16 > ((static_cast < xtd::uint32>(value) >> 16) & 0xFFFF);
+[[nodiscard]] inline auto HIWORD(auto value) -> xtd::uint16 {
+  return static_cast<xtd::uint16>((static_cast<xtd::uint32>(value) >> 16) & 0xFFFF);
 }
 
-template < class type_t >
-inline xtd::uint16 LOBYTE(type_t value) {
-  return static_cast < xtd::byte > (static_cast < xtd::uint32 > (value) & 0xFF);
+[[nodiscard]] inline auto LOBYTE(auto value) -> xtd::uint16 {
+  return static_cast<xtd::byte>(static_cast<xtd::uint32>(value) & 0xFF);
 }
 
-template < class type_t >
-inline xtd::byte HIBYTE(type_t value) {
-  return static_cast < xtd::byte > ((static_cast < xtd::uint32>(value) >> 8) & 0xFF);
+[[nodiscard]] inline auto HIBYTE(auto value) -> xtd::byte {
+  return static_cast<xtd::byte>((static_cast<xtd::uint32>(value) >> 8) & 0xFF);
 }
 #endif
