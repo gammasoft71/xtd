58e0576bb5f52ede061b446f78a087104cec906a
Gammasoft <gammasoft71@gmail.com>
2026-05-21 23:27:17 +0200

    refactor code : using trailing return type

--- src/xtd.forms.native/include/xtd/forms/native/color_picker.hpp
+++ src/xtd.forms.native/include/xtd/forms/native/color_picker.hpp
@@ -41,12 +41,12 @@ namespace xtd {
         /// @param control Control window handle.
         /// @return A xtd::drawing::color from picker.
         /// @warning Internal use only
-        static drawing::color color(intptr control);
+        [[nodiscard]] static auto color(xtd::intptr control) -> xtd::drawing::color;
         /// @brief Sets picker color.
         /// @param control Control window handle.
         /// @param color A xtd::drawing::color to picker.
         /// @warning Internal use only
-        static void color(intptr control, const drawing::color& color);
+        static auto color(xtd::intptr control, const xtd::drawing::color& color) -> void;
         /// @}
       };
     }
