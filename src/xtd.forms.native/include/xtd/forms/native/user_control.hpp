7997fca8364a25caa75fd53caf7fad9b4b056783
Gammasoft <gammasoft71@gmail.com>
2026-05-22 00:13:20 +0200

    refactor code : using trailing return type

--- src/xtd.forms.native/include/xtd/forms/native/user_control.hpp
+++ src/xtd.forms.native/include/xtd/forms/native/user_control.hpp
@@ -40,7 +40,7 @@ namespace xtd {
         /// @param control User control window handle.
         /// @param size A virtual xtd::drawing::size.
         /// @warning Internal use only
-        static void virtual_size(intptr control, const drawing::size& size);
+        static auto virtual_size(xtd::intptr control, const xtd::drawing::size& size) -> void;
         /// @}
       };
     }
