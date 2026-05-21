af7c015a2f9842c27795e5cca87a2013f4e61352
Gammasoft <gammasoft71@gmail.com>
2026-05-21 23:28:58 +0200

    refactor code : using trailing return type

--- src/xtd.forms.native/include/xtd/forms/native/input_dialog.hpp
+++ src/xtd.forms.native/include/xtd/forms/native/input_dialog.hpp
@@ -50,7 +50,7 @@ namespace xtd {
         /// @return `true` if the user clicks OK in the dialog box; otherwise, `false`.
         /// @remarks If result is `true`, the font parameter contains the new font, and the color parameter contains the now color.
         /// @warning Internal use only
-        static bool run_dialog(intptr hwnd, const xtd::string& text, const xtd::string& message, xtd::string& value, int32 character_casting, bool multiline, bool use_system_password_char, bool word_wrap);
+        [[nodiscard]] static auto run_dialog(xtd::intptr hwnd, const xtd::string& text, const xtd::string& message, xtd::string& value, xtd::int32 character_casting, bool multiline, bool use_system_password_char, bool word_wrap) -> bool;
         /// @brief Shows async dialog as sheet.
         /// @param on_dialog_closed a dialog close delegate to call when the dialog is closed.
         /// @param hwnd Parent window handle.
@@ -64,7 +64,7 @@ namespace xtd {
         /// @return `true` if the user clicks OK in the dialog box; otherwise, `false`.
         /// @remarks When dialog closed and if result is `true`, the value parameter contains the new string value.
         /// @warning Internal use only
-        static void run_sheet(xtd::delegate<void(bool)> on_dialog_closed, intptr hwnd, const xtd::string& text, const xtd::string& message, xtd::string& value, int32 character_casting, bool multiline, bool use_system_password_char, bool word_wrap);
+        static auto run_sheet(xtd::delegate<void(bool)> on_dialog_closed, xtd::intptr hwnd, const xtd::string& text, const xtd::string& message, xtd::string& value, xtd::int32 character_casting, bool multiline, bool use_system_password_char, bool word_wrap) -> void;
         /// @}
       };
     }
