2839f0a758d24ec01502d8f2c9be0585a5130408
Gammasoft <gammasoft71@gmail.com>
2026-05-23 23:32:10 +0200

    Update documentation

--- src/xtd.drawing/include/xtd/drawing/argb.hpp
+++ src/xtd.drawing/include/xtd/drawing/argb.hpp
@@ -18,7 +18,7 @@ namespace xtd {
     /// xtd::drawing
     /// @par Library
     /// xtd.drawing
-    /// @ingroup xtd_drawing drawing
+    /// @ingroup xtd_drawing colors
     struct argb {
       /// @name Public Properties
       
--- src/xtd.drawing/include/xtd/drawing/hsl.hpp
+++ src/xtd.drawing/include/xtd/drawing/hsl.hpp
@@ -18,7 +18,7 @@ namespace xtd {
     /// xtd::drawing
     /// @par Library
     /// xtd.drawing
-    /// @ingroup xtd_drawing drawing
+    /// @ingroup xtd_drawing colors
     struct hsl {
       /// @name Public Properties
       
