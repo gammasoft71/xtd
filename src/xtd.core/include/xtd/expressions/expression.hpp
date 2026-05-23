5865eb1d06fe22de19fbe25511be90537db821c7
Gammasoft <gammasoft71@gmail.com>
2026-05-23 17:36:43 +0200

    Fix typo

--- src/xtd.core/include/xtd/expressions/expression.hpp
+++ src/xtd.core/include/xtd/expressions/expression.hpp
@@ -75,7 +75,7 @@ namespace xtd {
     ///   | xtd::expressions::expression::arg         |          |
     ///   | xtd::expressions::expression::placeholder |          |
     /// @par Examples
-    /// The following example shos how to use xtd::expressions::expression class.
+    /// The following example shows how to use xtd::expressions::expression class.
     /// ```cpp
     /// #include <xtd/xtd>
     ///
@@ -83,24 +83,24 @@ namespace xtd {
     ///   auto value = 30;
     ///   // auto expr1 = [](auto&& _1, auto&& _2) {return (_1 + _2) * value;};
     ///   auto expr1 = (_1 + _2) * value;
-    ///   println("expr1 resutl = {}", expr1(10, 20));
+    ///   println("expr1 result = {}", expr1(10, 20));
     ///   println("expr1 = {}", expr1);
     ///   println("type_of(expr1) = {}", type_of(expr1));
     ///
     ///   println();
     ///   auto expr2 = expression::multiply(expression::add(_1, _2), value);
-    ///   println("expr2 resutl = {}", expr2(10, 20));
+    ///   println("expr2 result = {}", expr2(10, 20));
     ///   println("expr2 = {}", expr2);
     ///   println("type_of(expr2) = {}", type_of(expr2));
     /// }
     ///
     /// // This code produces the following output :
     /// //
-    /// // expr1 resutl = 900
+    /// // expr1 result = 900
     /// // expr1 = (_1 + _2) * 30
     /// // type_of(expr1) = xtd::expressions::multiply_expression<xtd::expressions::add_expression<xtd::expressions::placeholder<1ul>, xtd::expressions::placeholder<2ul>>, xtd::expressions::constant<int>>
     /// //
-    /// // expr2 resutl = 900
+    /// // expr2 result = 900
     /// // expr2 = (_1 + _2) * 30
     /// // type_of(expr2) = xtd::expressions::multiply_expression<xtd::expressions::add_expression<xtd::expressions::placeholder<1ul>, xtd::expressions::placeholder<2ul>>, xtd::expressions::constant<int>>
     /// ```
