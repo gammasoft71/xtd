f8f6a3a89a04b0b9bce56bc00a171afe58f180e0
Gammasoft <gammasoft71@gmail.com>
2026-05-24 19:44:38 +0200

    refactor code : using trriling return type

--- src/xtd.drawing/include/xtd/drawing/pen.hpp
+++ src/xtd.drawing/include/xtd/drawing/pen.hpp
@@ -74,7 +74,7 @@ namespace xtd {
       
       /// @cond
       pen(const xtd::drawing::pen& value);
-      pen& operator =(const xtd::drawing::pen& value);
+      auto operator =(const xtd::drawing::pen& value) -> xtd::drawing::pen&;
       ~pen();
       /// @endcond
       
@@ -84,22 +84,21 @@ namespace xtd {
       /// @brief Gets the alignment for this xtd::drawing::pen.
       /// @return A xtd::drawing::drawing_2d::pen_alignment that represents the alignment for this xtd::drawing::pen.
       /// @remarks This property determines how the xtd::drawing::pen draws closed curves and polygons. The xtd::drawing_2d::pen_alignment enumeration specifies five values; however, only two values - xtd::drawing_2d::pen_alignment::center and xtd::drawing_2d::pen_alignment::inset - will change the appearance of a drawn line. xtd::drawing_2d::pen_alignment::center is the default value for this property and specifies that the width of the pen is centered on the outline of the curve or polygon. A value of xtd::drawing_2d::pen_alignment::inset for this property specifies that the width of the pen is inside the outline of the curve or polygon. The other three values, xtd::drawing_2d::pen_alignment::right, xtd::drawing_2d::pen_alignment::left, and xtd::drawing_2d::pen_alignment::outset, will result in a pen that is centered.
-      xtd::drawing::drawing_2d::pen_alignment alignment() const noexcept;
-      
+      [[nodiscard]] auto alignment() const noexcept -> xtd::drawing::drawing_2d::pen_alignment;
       /// @brief Sets the alignment for this xtd::drawing::pen.
       /// @param value A xtd::drawing::drawing_2d::pen_alignment that represents the alignment for this xtd::drawing::pen.
       /// @remarks This property determines how the xtd::drawing::pen draws closed curves and polygons. The xtd::drawing::drawing_2d::pen_alignment enumeration specifies five values; however, only two values - xtd::drawing::drawing_2d::pen_alignment::center and xtd::drawing::drawing_2d::pen_alignment::inset - will change the appearance of a drawn line. xtd::drawing::drawing_2d::pen_alignment::center is the default value for this property and specifies that the width of the pen is centered on the outline of the curve or polygon. A value of xtd::drawing::drawing_2d::pen_alignment::inset for this property specifies that the width of the pen is inside the outline of the curve or polygon. The other three values, xtd::drawing::drawing_2d::pen_alignment::right, xtd::drawing::drawing_2d::pen_alignment::left, and xtd::drawing::drawing_2d::pen_alignment::outset, will result in a pen that is centered.
-      xtd::drawing::pen& alignment(xtd::drawing::drawing_2d::pen_alignment value);
+      auto alignment(xtd::drawing::drawing_2d::pen_alignment value) -> xtd::drawing::pen&;
       
       /// @brief Gets the xtd::drawing::brush that determines attributes of this xtd::drawing::pen.
       /// @return A Brush that determines attributes of this xtd::drawing::pen.
       /// @remarks Assigning this property causes the pen to draw filled lines and curves. It overrides thextd::drawing::pen::color property of the xtd::drawing::pen.
-      const xtd::uptr<xtd::drawing::brush>& brush() const noexcept;
+      [[nodiscard]] auto brush() const noexcept -> const xtd::uptr<xtd::drawing::brush>&;
       /// @brief Sets the xtd::drawing::brush that determines attributes of this xtd::drawing::pen.
       /// @param value A xtd::drawing::brush that determines attributes of this xtd::drawing::pen.
       /// @remarks Assigning this property causes the pen to draw filled lines and curves. It overrides thextd::drawing::pen::color property of the xtd::drawing::pen.
       template<typename brush_t>
-      xtd::drawing::pen& brush(const brush_t& value) {
+      auto brush(const brush_t& value) -> xtd::drawing::pen& {
         brush_(value.template memberwise_clone<brush_t>());
         color_(xtd::drawing::color::empty);
         recreate_handle();
@@ -108,97 +107,97 @@ namespace xtd {
       
       /// @brief Gets the color of this xtd::drawing::pen.
       /// @return A xtd::drawing::color structure that represents the color of this xtd::drawing::pen.
-      const xtd::drawing::color& color() const noexcept;
+      [[nodiscard]] auto color() const noexcept -> const xtd::drawing::color&;
       /// @brief Sets the color of this xtd::drawing::pen.
       /// @param value A xtd::drawing::color structure that represents the color of this xtd::drawing::pen.
       /// @return This current instance.
-      xtd::drawing::pen& color(const xtd::drawing::color& value);
+      auto color(const xtd::drawing::color& value) -> xtd::drawing::pen&;
       
       /// @brief Gets an array of custom dashes and spaces.
       /// @return An array of real numbers that specifies the lengths of alternating dashes and spaces in dashed lines.
       /// @remarks Assigning a value other than empty to this property will set the xtd::drawing::pen::dash_style property for this xtd::drawing::pen to xtd::drawing:drawing_2d::dash_style::custom.
       /// @remarks The elements in the dash_pattern array set the length of each dash and space in the dash pattern. The first element sets the length of a dash, the second element sets the length of a space, the third element sets the length of a dash, and so on. Consequently, each element should be a non-zero positive number.
       /// @remarks The length of each dash and space in the dash pattern is the product of the element value in the array and the width of the xtd::drawing::pen.
-      xtd::array<float> dash_pattern() const noexcept;
+      [[nodiscard]] auto dash_pattern() const noexcept -> xtd::array<float>;
       /// @brief Sets an array of custom dashes and spaces.
       /// @param value An array of real numbers that specifies the lengths of alternating dashes and spaces in dashed lines.
       /// @remarks Assigning a value other than empty to this property will set the xtd::drawing::pen::dash_style property for this xtd::drawing::pen to xtd::drawing:drawing_2d::dash_style::custom.
       /// @remarks The elements in the dash_pattern array set the length of each dash and space in the dash pattern. The first element sets the length of a dash, the second element sets the length of a space, the third element sets the length of a dash, and so on. Consequently, each element should be a non-zero positive number.
       /// @remarks The length of each dash and space in the dash pattern is the product of the element value in the array and the width of the xtd::drawing::pen.
-      xtd::drawing::pen& dash_pattern(const xtd::array<float>& value);
+      auto dash_pattern(const xtd::array<float>& value) -> xtd::drawing::pen&;
       
       /// @brief Gets the style used for dashed lines drawn with this xtd::drawing::pen.
       /// @return A xtd::drawing::drawing_2d::dash_style that represents the style used for dashed lines drawn with this xtd::drawing::pen.
       /// @remarks A value of xtd::drawing::drawing_2d::dash_style::custom for this property specifies that a custom pattern of dashes and spaces, defined by the xtd::drawing::pendash_pattern property, makes up lines drawn with this xtd::drawing::pen. If the value of this property is xtd::drawing::drawing_2d::dash_style::custom and the value of the xtd::drawing::pen::dash_pattern property is empty, the pen draws solid lines.
-      xtd::drawing::drawing_2d::dash_style dash_style() const noexcept;
+      [[nodiscard]] auto dash_style() const noexcept -> xtd::drawing::drawing_2d::dash_style;
       /// @brief Sets the style used for dashed lines drawn with this xtd::drawing::pen.
       /// @param value A xtd::drawing::drawing_2d::dash_style that represents the style used for dashed lines drawn with this xtd::drawing::pen.
       /// @return This current instance.
       /// @remarks A value of xtd::drawing::drawing_2d::dash_style::custom for this property specifies that a custom pattern of dashes and spaces, defined by the xtd::drawing::pendash_pattern property, makes up lines drawn with this xtd::drawing::pen. If the value of this property is xtd::drawing::drawing_2d::dash_style::custom and the value of the xtd::drawing::pen::dash_pattern property is empty, the pen draws solid lines.
-      xtd::drawing::pen& dash_style(drawing::drawing_2d::dash_style value);
+      auto dash_style(drawing::drawing_2d::dash_style value) -> xtd::drawing::pen&;
       
       /// @brief Gets the cap style used at the end of lines drawn with this xtd::drawing::pen.
       /// @return One of the xtd::drawing::drawing_2d::line_cap values that represents the cap style used at the end of lines drawn with this xtd::drawing::pen.
-      xtd::drawing::drawing_2d::line_cap end_cap() const noexcept;
+      [[nodiscard]] auto end_cap() const noexcept -> xtd::drawing::drawing_2d::line_cap;
       /// @brief Sets the cap style used at the end of lines drawn with this xtd::drawing::pen.
       /// @param value One of the xtd::drawing::drawing_2d::line_cap values that represents the cap style used at the end of lines drawn with this xtd::drawing::pen.
       /// @return This current instance.
-      xtd::drawing::pen& end_cap(xtd::drawing::drawing_2d::line_cap value);
+      auto end_cap(xtd::drawing::drawing_2d::line_cap value) -> xtd::drawing::pen&;
       
       /// @brief Gets the handle of the pen.
       /// @return An intptr that contains the handle of the pen.
-      intptr handle() const noexcept;
+      [[nodiscard]] auto handle() const noexcept -> xtd::intptr;
       
       /// @brief Gets the join style for the ends of two consecutive lines drawn with this xtd::drawing::pen.
       /// @return A xtd::drawing::drawing_2d::line_join that represents the join style for the ends of two consecutive lines drawn with this xtd::drawing::pen.
       /// @remarks A line join is the common area that is formed by two lines whose ends meet or overlap. There are three line join styles: miter, bevel, and round. When you specify a line join style for a xtd::drawing::pen object, that join style will be applied to all the connected lines in any xtd::drawing::drawing_2d::graphics_path object drawn using that pen. The following illustration shows the results of the beveled line join example.
       /// @image html line_join_bevel.png
-      xtd::drawing::drawing_2d::line_join line_join() const noexcept;
+      [[nodiscard]] auto line_join() const noexcept -> xtd::drawing::drawing_2d::line_join;
       /// @brief Sets the join style for the ends of two consecutive lines drawn with this xtd::drawing::pen.
       /// @param value A xtd::drawing::drawing_2d::line_join that represents the join style for the ends of two consecutive lines drawn with this xtd::drawing::pen.
       /// @return This current instance.
       /// @remarks A line join is the common area that is formed by two lines whose ends meet or overlap. There are three line join styles: miter, bevel, and round. When you specify a line join style for a xtd::drawing::pen object, that join style will be applied to all the connected lines in any xtd::drawing::drawing_2d::graphics_path object drawn using that pen. The following illustration shows the results of the beveled line join example.
       /// @image html line_join_bevel.png
-      xtd::drawing::pen& line_join(xtd::drawing::drawing_2d::line_join value);
+      auto line_join(xtd::drawing::drawing_2d::line_join value) -> xtd::drawing::pen&;
       
       /// @brief Gets the limit of the thickness of the join on a mitered corner.
       /// @return The limit of the thickness of the join on a mitered corner.
       /// @remarks The miter length is the distance from the intersection of the line walls on the inside of the join to the intersection of the line walls outside of the join. The miter length can be large when the angle between two lines is small. The miter limit is the maximum allowed ratio of miter length to stroke width. The default value is 10.0f.
       /// @remarks If the miter length of the join of the intersection exceeds the limit of the join, then the join will be beveled to keep it within the limit of the join of the intersection.
-      float miter_limit() const noexcept;
+      [[nodiscard]] float miter_limit() const noexcept;
       /// @brief Sets the limit of the thickness of the join on a mitered corner.
       /// @param value The limit of the thickness of the join on a mitered corner.
       /// @return This current instance.
       /// @remarks The miter length is the distance from the intersection of the line walls on the inside of the join to the intersection of the line walls outside of the join. The miter length can be large when the angle between two lines is small. The miter limit is the maximum allowed ratio of miter length to stroke width. The default value is 10.0f.
       /// @remarks If the miter length of the join of the intersection exceeds the limit of the join, then the join will be beveled to keep it within the limit of the join of the intersection.
-      xtd::drawing::pen& miter_limit(float value);
+      auto miter_limit(float value) -> xtd::drawing::pen&;
       
       /// @brief Gets the cap style used at the beginning of lines drawn with this xtd::drawing::pen.
       /// @return One of the xtd::drawing::drawing_2d::line_cap values that represents the cap style used at the beginning of lines drawn with this xtd::drawing::pen.
-      xtd::drawing::drawing_2d::line_cap start_cap() const noexcept;
+      [[nodiscard]] auto start_cap() const noexcept -> xtd::drawing::drawing_2d::line_cap;
       /// @brief Sets the cap style used at the beginning of lines drawn with this xtd::drawing::pen.
       /// @param value One of the xtd::drawing::drawing_2d::line_cap values that represents the cap style used at the beginning of lines drawn with this xtd::drawing::pen.
       /// @return This current instance.
-      xtd::drawing::pen& start_cap(xtd::drawing::drawing_2d::line_cap value);
+      auto start_cap(xtd::drawing::drawing_2d::line_cap value) -> xtd::drawing::pen&;
       
       /// @brief Gets the style of lines drawn with this xtd::drawing::pen.
       /// @return A xtd::drawing::drawing_2d::pen_type enumeration that specifies the style of lines drawn with this xtd::drawing::pen.
       /// @remarks A xtd::drawing::pen can draw solid lines, filled lines, or textured lines, depending on the style specified by a member of the PenType enumeration.
-      xtd::drawing::drawing_2d::pen_type type() const noexcept;
+      [[nodiscard]] auto type() const noexcept -> xtd::drawing::drawing_2d::pen_type;
       
       /// @brief Gets the width of this xtd::drawing::pen, in units of the xtd::drawing::graphics object used for drawing.
       /// @return The width of this xtd::drawing::pen.
       /// @remarks You can access the unit of measure of the xtd::drawing::graphics object using its xtd::drawing::graphics::page_unit property. The unit of measure is typically pixels. A Width of 0 will result in the xtd::drawing::pen drawing as if the xtd::drawing::pen::width were 1.
-      float width() const noexcept;
+      [[nodiscard]] auto width() const noexcept -> float;
       /// @brief Sets the width of this xtd::drawing::pen, in units of the xtd::drawing::graphics object used for drawing.
       /// @param value The width of this xtd::drawing::pen.
       /// @return This current instance.
       /// @remarks You can access the unit of measure of the xtd::drawing::graphics object using its xtd::drawing::graphics::page_unit property. The unit of measure is typically pixels. A Width of 0 will result in the xtd::drawing::pen drawing as if the xtd::drawing::pen::width were 1.
-      xtd::drawing::pen& width(float value);
+      auto width(float value) -> xtd::drawing::pen&;
       /// @}
       
       /// @cond
-      xtd::drawing::pen& dash_pattern(const std::initializer_list<float>& value);
+      auto dash_pattern(const std::initializer_list<float>& value) -> xtd::drawing::pen&;
       /// @endcond
       
       /// @name Public Methods
@@ -207,24 +206,24 @@ namespace xtd {
       /// @brief Determines whether the specified object is equal to the current object.
       /// @param obj The object to compare with the current object.
       /// @return `true` if the specified object is equal to the current object. otherwise, `false`.
-      bool equals(const object& obj) const noexcept override;
+      [[nodiscard]] auto equals(const object& obj) const noexcept -> bool override;
       /// @brief Indicates whether the current object is equal to another object of the same type.
       /// @param other An object to compare with this object.
       /// @return `true` if the current object is equal to the other parameter; otherwise, `false`.
-      bool equals(const xtd::drawing::pen& other) const noexcept override;
+      [[nodiscard]] auto equals(const xtd::drawing::pen& other) const noexcept -> bool override;
       
       /// @brief Serves as a hash function for a particular type.
       /// @return A hash code for the current object.
-      xtd::usize get_hash_code() const noexcept override;
+      [[nodiscard]] auto get_hash_code() const noexcept -> xtd::usize override;
       
-      xtd::string to_string() const noexcept override;
+      [[nodiscard]] auto to_string() const noexcept -> xtd::string override;
       /// @}
       
     private:
-      void brush_(xtd::uptr<xtd::drawing::brush>&& brush);
-      void color_(const xtd::drawing::color& color);
-      void create_data();
-      void recreate_handle();
+      auto brush_(xtd::uptr<xtd::drawing::brush>&& brush) -> void;
+      auto color_(const xtd::drawing::color& color) -> void;
+      auto create_data() -> void;
+      auto recreate_handle() -> void;
       
       xtd::sptr<data> data_;
     };
