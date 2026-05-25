369901bf3b6a03987b0d00f2e4e9ede9fcb90fde
Gammasoft <gammasoft71@gmail.com>
2026-05-25 22:32:27 +0200

    using xtd::system as alias on xtd

--- src/xtd.core/include/xtd/core_namespace_aliases.hpp
+++ src/xtd.core/include/xtd/core_namespace_aliases.hpp
@@ -5,7 +5,7 @@
 #undef linux
 
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
-namespace xtd {}
-
-/// @brief The system_ namespace contains all fundamental classes to access Hardware, Os, System, and more.
-namespace system_ = xtd;
+namespace xtd {
+  /// @brief The system namespace contains fundamental classes and base classes that define commonly-used value and reference data types, events and event handlers, interfaces, attributes, and processing exceptions.
+  namespace system = xtd;
+}
