142c0e5901a894226ab91db1f015f394cc27d711
Gammasoft <gammasoft71@gmail.com>
2026-05-30 23:24:19 +0200

    Fix typo

--- src/xtd.core/include/xtd/globalization/culture_types.hpp
+++ src/xtd.core/include/xtd/globalization/culture_types.hpp
@@ -1,5 +1,5 @@
 /// @file
-/// @brief Contains xtd::globalization::culture_type enum class.
+/// @brief Contains xtd::globalization::culture_types enum class.
 /// @copyright Copyright (c) 2026 Gammasoft. All rights reserved.
 #pragma once
 #include "../enum.hpp"
