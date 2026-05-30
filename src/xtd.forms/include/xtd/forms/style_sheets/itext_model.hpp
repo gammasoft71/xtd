a6c773cbabe0fc4e99a4f703a9926922f6708c62
Gammasoft <gammasoft71@gmail.com>
2026-05-30 23:16:15 +0200

    refactor code : using trailing return type

--- src/xtd.forms/include/xtd/forms/style_sheets/itext_model.hpp
+++ src/xtd.forms/include/xtd/forms/style_sheets/itext_model.hpp
@@ -44,35 +44,35 @@ namespace xtd {
         /// @{
         /// @brief Gets a value indicating whether the ellipsis character (...) appears at the right edge of the control, denoting that the control text extends beyond the specified length of the control.
         /// @return `true` if the additional control text is to be indicated by an ellipsis; otherwise, `false`.
-        virtual bool auto_ellipsis() const noexcept = 0;
+        [[nodiscard]] virtual auto auto_ellipsis() const noexcept -> bool = 0;
         
         /// @brief Gets the alignment of the text.
         /// @return A combination of xtd::forms::content_alignment values.
-        virtual xtd::forms::content_alignment text_alignment() const noexcept = 0;
+        [[nodiscard]] virtual auto text_alignment() const noexcept -> xtd::forms::content_alignment = 0;
         
         /// @brief Gets the color is used to add foreground effects to the text.
         /// @return The color is used to add foreground effects to the text.
-        virtual const xtd::drawing::color& color() const noexcept = 0;
+        [[nodiscard]] virtual auto color() const noexcept -> const xtd::drawing::color& = 0;
         
         /// @brief Gets the decoration of the text.
         /// @return One of xtd::forms::style_sheets::text_decoration values.
-        virtual xtd::forms::style_sheets::text_decoration decoration() const noexcept = 0;
+        [[nodiscard]] virtual auto decoration() const noexcept -> xtd::forms::style_sheets::text_decoration = 0;
         
         /// @brief Gets the font of the text.
         /// @return The font of the text.
-        virtual const xtd::drawing::font& font() const noexcept = 0;
+        [[nodiscard]] virtual auto font() const noexcept -> const xtd::drawing::font& = 0;
         
         /// @brief Gets an xtd::forms::style_sheets::shadow array used to draw shadows under the text.
         /// @return The xtd::forms::style_sheets::shadow array used to draw shadows under the text.
-        virtual xtd::forms::style_sheets::shadows shadows() const noexcept = 0;
+        [[nodiscard]] virtual auto shadows() const noexcept -> xtd::forms::style_sheets::shadows = 0;
         
         /// @brief Gets the transformation of the text.
         /// @return One of xtd::forms::style_sheets::text_transformation values.
-        virtual xtd::forms::style_sheets::text_transformation transformation() const noexcept = 0;
+        [[nodiscard]] virtual auto transformation() const noexcept -> xtd::forms::style_sheets::text_transformation = 0;
         
         /// @brief Gets how white-space inside an element is handled.
         /// @return One of xtd::forms::style_sheets::white_space values.
-        virtual xtd::forms::style_sheets::white_space white_space() const noexcept = 0;
+        [[nodiscard]] virtual auto white_space() const noexcept -> xtd::forms::style_sheets::white_space = 0;
         /// @}
         
         /// @name Public Methods
@@ -80,7 +80,7 @@ namespace xtd {
         /// @{
         /// @brief Gets the string format of the text.
         /// @return The string format used by xtd::drawing::graphics::draw_text method to draw a text.
-        virtual xtd::drawing::string_format make_string_format() const noexcept = 0;
+        [[nodiscard]] virtual auto make_string_format() const noexcept -> xtd::drawing::string_format = 0;
         /// @}
       };
     }
