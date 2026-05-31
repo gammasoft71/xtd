f023328937eee4e4e961b16be00043e7f04de868
Gammasoft <gammasoft71@gmail.com>
2026-05-31 22:19:42 +0200

    refactor code : using trailing return type

--- src/xtd.forms/include/xtd/forms/common_dialog.hpp
+++ src/xtd.forms/include/xtd/forms/common_dialog.hpp
@@ -47,16 +47,16 @@ namespace xtd {
       /// @{
       /// @brief Gets async dialog_result result after dialog box is closing.
       /// @return ok if the user clicks OK in the dialog box; otherwise, cancel.
-      xtd::forms::dialog_result dialog_result() const noexcept;
+      [[nodiscard]] auto dialog_result() const noexcept -> xtd::forms::dialog_result;
       
       /// @brief Gets an object that contains data about the control.
       /// @return The object that contains data about the common_dialog.
       /// @remarks Use this property to store arbitrary information about the control.
-      const xtd::any_object& tag() const noexcept;
+      [[nodiscard]] auto tag() const noexcept -> const xtd::any_object&;
       /// @brief Sets an object that contains data about the control.
       /// @param tag The object that contains data about the common_dialog.
       /// @remarks Use this property to store arbitrary information about the control.
-      common_dialog& tag(const xtd::any_object& tag);
+      auto tag(const xtd::any_object& tag) -> common_dialog&;
       /// @}
       
       /// @name Public Methods
@@ -65,28 +65,28 @@ namespace xtd {
       /// @brief When overridden in a derived class, resets the properties of a common dialog box to their default values.
       /// @par Notes to Inheritors
       /// Inheriting classes can override this method to reset their properties.
-      virtual void reset() noexcept = 0;
+      virtual auto reset() noexcept -> void = 0;
       
       /// @brief Runs a common dialog box with a default owner.
       /// @return ok if the user clicks OK in the dialog box; otherwise, cancel.
       /// @remarks This method implements run_dialog.
-      xtd::forms::dialog_result show_dialog();
+      [[nodiscard]] auto show_dialog() -> xtd::forms::dialog_result;
       
       /// @brief Runs a common dialog box with the specified owner.
       /// @param owner Any object that implements iwn32_window that represents the top-level window that will own the modal dialog box.
       /// @return ok the user clicks OK in the dialog box; otherwise, cancel.
       /// @remarks This version of the show_dialog method allows you to specify a specific form or control that will own the dialog box that is shown. If you use the version of this method that has no parameters, the dialog box being shown would be owned automatically by the currently active window of your application.
-      xtd::forms::dialog_result show_dialog(const iwin32_window& owner);
+      [[nodiscard]] auto show_dialog(const iwin32_window& owner) -> xtd::forms::dialog_result;
       
       /// @brief Runs a common dialog box with the specified owner.
       /// @param owner Any object that implements iwn32_window that represents the top-level window that will own the modal dialog box.
       /// @remarks This version of the show_dialog method allows you to specify a specific form or control that will own the dialog box that is shown. If you use the version of this method that has no parameters, the dialog box being shown would be owned automatically by the currently active window of your application.
-      void show_sheet(const iwin32_window& owner);
+      auto show_sheet(const iwin32_window& owner) -> void;
       
       /// @brief Runs a common dialog box with the specified owner.
       /// @param owner Any object that implements iwn32_window that represents the top-level window that will own the modal dialog box.
       /// @remarks This version of the show_dialog method allows you to specify a specific form or control that will own the dialog box that is shown. If you use the version of this method that has no parameters, the dialog box being shown would be owned automatically by the currently active window of your application.
-      xtd::forms::dialog_result show_sheet_dialog(const iwin32_window& owner);
+      [[nodiscard]] auto show_sheet_dialog(const iwin32_window& owner) -> xtd::forms::dialog_result;
       /// @}
       
       /// @name Public Events
@@ -94,11 +94,11 @@ namespace xtd {
       /// @{
       /// @brief Occurs when the user clicks the Help button on a common dialog box.
       /// @remarks For more information about handling events, see [Handling and Raising Events](https://gammasoft71.github.io/xtd/docs/documentation/guides/xtd.core/Events/overview).
-      event<common_dialog, help_event_handler> help_request;
+      xtd::event<common_dialog, xtd::forms::help_event_handler> help_request;
       
       /// @brief Occurs when the user close a common dialog box with dialog close button or other dialog buttons.
       /// @remarks For more information about handling events, see [Handling and Raising Events](https://gammasoft71.github.io/xtd/docs/documentation/guides/xtd.core/Events/overview).
-      event<common_dialog, dialog_closed_event_handler> dialog_closed;
+      xtd::event<common_dialog, xtd::forms::dialog_closed_event_handler> dialog_closed;
       /// @}
       
     protected:
@@ -110,27 +110,27 @@ namespace xtd {
       /// @brief Raises the common_dialog::dialog_close event.
       /// @param e An common_dialog_close_event_args that provides the event data.
       /// @remarks This method is invoked when the common dialog box is closed.
-      virtual void on_dialog_closed(const dialog_closed_event_args& e);
+      virtual auto on_dialog_closed(const xtd::forms::dialog_closed_event_args& e) -> void;
       
       /// @brief Raises the common_dialog::help_request event.
       /// @param e An xtd::forms::help_event_args that provides the event data.
       /// @remarks This method is invoked when the Help button is clicked. Inheriting classes can override this method to handle the event.
-      virtual void on_help_request(help_event_args& e);
+      virtual auto on_help_request(xtd::forms::help_event_args& e) -> void;
       
       /// @brief When overridden in a derived class, specifies a common dialog box.
       /// @param hwnd_owner A value that represents the window handle of the owner window for the common dialog box.
       /// @return `true` if the dialog box was successfully run; otherwise, `false`.
-      virtual bool run_dialog(intptr hwnd_owner) = 0;
+      [[nodiscard]] virtual auto run_dialog(intptr hwnd_owner) -> bool = 0;
       
       /// @brief When overridden in a derived class, specifies a common dialog box.
       /// @param hwnd_owner A value that represents the window handle of the owner window for the common dialog box.
       /// @return `true` if the dialog box was successfully run; otherwise, `false`.
       /// @remarks The result will done in async. Check result_dialog property after dialog box closed to obtain the result.
-      virtual void run_sheet(intptr hwnd_owner) = 0;
+      virtual auto run_sheet(intptr hwnd_owner) -> void = 0;
       
       /// @brief Sets async dialog_result result after dialog box is closing.
       /// @param value ok if the user clicks OK in the dialog box; otherwise, cancel.
-      void set_dialog_result(xtd::forms::dialog_result value);
+      auto set_dialog_result(xtd::forms::dialog_result value) -> void;
       /// @}
       
     private:
