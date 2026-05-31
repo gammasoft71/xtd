bdce23c27f6b03778794286f9dd2ccee8e5d955f
Gammasoft <gammasoft71@gmail.com>
2026-05-31 15:56:04 +0200

    refactor code : using trailing return type

--- src/xtd.forms/include/xtd/forms/about_dialog.hpp
+++ src/xtd.forms/include/xtd/forms/about_dialog.hpp
@@ -73,157 +73,157 @@ namespace xtd {
       /// @{
       /// @brief Gets the artists array.
       /// @return The artists array.
-      const artist_collection& artists() const noexcept;
+      [[nodiscard]] auto artists() const noexcept -> const artist_collection&;
       /// @brief Gets the artists array.
       /// @return The artists array.
-      artist_collection& artists() noexcept;
+      [[nodiscard]] auto artists() noexcept -> artist_collection&;
       /// @brief Sets the artists array.
       /// @param artists The artists array.
       /// @return Current about_dialog instance.
-      about_dialog& artists(const artist_collection& artists);
+      auto artists(const artist_collection& artists) -> about_dialog&;
       
       /// @brief Gets the authors array.
       /// @return The author array.
-      const author_collection& authors() const noexcept;
+      [[nodiscard]] auto authors() const noexcept -> const author_collection&;
       /// @brief Gets the authors array.
       /// @return The authors array.
-      author_collection& authors() noexcept;
+      [[nodiscard]] auto authors() noexcept -> author_collection&;
       /// @brief Sets the authors array.
       /// @param authors The authors array.
       /// @return Current about_dialog instance.
-      about_dialog& authors(const author_collection& authors);
+      auto authors(const author_collection& authors) -> about_dialog&;
       
       /// @brief Gets the product copyright.
       /// @return The product copyright.
-      xtd::string copyright() const noexcept;
+      [[nodiscard]] auto copyright() const noexcept -> xtd::string;
       /// @brief Sets the product description.
       /// @param copyright The description copyright.
       /// @return Current about_dialog instance.
-      about_dialog& copyright(const xtd::string& copyright);
+      auto copyright(const xtd::string& copyright) -> about_dialog&;
       
       /// @brief Gets the product description.
       /// @return The product description.
-      xtd::string description() const noexcept;
+      [[nodiscard]] auto description() const noexcept -> xtd::string;
       /// @brief Sets the product description.
       /// @param description The product description.
       /// @return Current about_dialog instance.
-      about_dialog& description(const xtd::string& description);
+      auto description(const xtd::string& description) -> about_dialog&;
       
       /// @brief Gets the documentation writers array.
       /// @return The documentation writers array.
-      const documenter_collection& documenters() const noexcept;
+      [[nodiscard]] auto documenters() const noexcept -> const documenter_collection&;
       /// @brief Gets the documentation writers array.
       /// @return The documentation writers array.
-      documenter_collection& documenters() noexcept;
+      [[nodiscard]] auto documenters() noexcept -> documenter_collection&;
       /// @brief Sets the documentation writers array.
       /// @param documenters The documentation writers array.
       /// @return Current about_dialog instance.
-      about_dialog& documenters(const documenter_collection& documenters);
+      auto documenters(const documenter_collection& documenters) -> about_dialog&;
       
       /// @brief Gets the dialog appearance.
       /// @return One of the xtd::forms::dialog_appearance values. The default value is xtd::forms::dialog_appearance::standard.
-      xtd::forms::dialog_appearance dialog_appearance() const noexcept;
+      [[nodiscard]] auto dialog_appearance() const noexcept -> xtd::forms::dialog_appearance;
       /// @brief Sets the dialog appearance.
       /// @param dialog_appearance One of the xtd::forms::dialog_appearance values. The default value is xtd::forms::dialog_appearance::standard.
       /// @return Current about_dialog instance.
-      about_dialog& dialog_appearance(xtd::forms::dialog_appearance dialog_appearance);
+      auto dialog_appearance(xtd::forms::dialog_appearance dialog_appearance) -> about_dialog&;
       
       /// @brief Gets the product icon.
       /// @return The product icon.
-      xtd::drawing::icon icon() const noexcept;
+      [[nodiscard]] auto icon() const noexcept -> xtd::drawing::icon;
       /// @brief Sets the product icon.
       /// @param icon The product icon.
       /// @return Current about_dialog instance.
-      about_dialog& icon(const xtd::drawing::icon& icon);
+      auto icon(const xtd::drawing::icon& icon) -> about_dialog&;
       /// @brief Sets the product icon.
       /// @param image The product icon.
       /// @return Current about_dialog instance.
-      about_dialog& icon(const xtd::drawing::image& image);
+      auto icon(const xtd::drawing::image& image) -> about_dialog&;
       /// @brief Sets the product icon.
       /// @param bitmap The product icon.
       /// @return Current about_dialog instance.
-      about_dialog& icon(const xtd::drawing::bitmap& bitmap);
+      auto icon(const xtd::drawing::bitmap& bitmap) -> about_dialog&;
       
       /// @brief Gets the product license.
       /// @return The product license.
-      xtd::string license() const noexcept;
+      [[nodiscard]] auto license() const noexcept -> xtd::string;
       /// @brief Sets the product license.
       /// @param name The product license.
       /// @return Current about_dialog instance.
-      about_dialog& license(const xtd::string& license);
+      auto license(const xtd::string& license) -> about_dialog&;
       
       /// @brief Gets the product long version.
       /// @return The product long version.
-      xtd::string long_version() const noexcept;
+      [[nodiscard]] auto long_version() const noexcept -> xtd::string;
       /// @brief Sets the product long version.
       /// @param version The version version.
       /// @return Current about_dialog instance.
-      about_dialog& long_version(const xtd::string& long_version);
+      auto long_version(const xtd::string& long_version) -> about_dialog&;
       
       /// @brief Gets the product name.
       /// @return The product name.
-      xtd::string name() const noexcept;
+      [[nodiscard]] auto name() const noexcept -> xtd::string;
       /// @brief Sets the product name.
       /// @param name The product name.
       /// @return Current about_dialog instance.
-      about_dialog& name(const xtd::string& name);
+      auto name(const xtd::string& name) -> about_dialog&;
       
       /// @brief Gets the translators array.
       /// @return The translators array.
-      const translator_collection& translators() const noexcept;
+      [[nodiscard]] auto translators() const noexcept -> const translator_collection&;
       /// @brief Gets the translators array.
       /// @return The translators array.
-      translator_collection& translators() noexcept;
+      [[nodiscard]] auto translators() noexcept -> translator_collection&;
       /// @brief Sets the translators array.
       /// @param translators The translators array.
       /// @return Current about_dialog instance.
-      about_dialog& translators(const translator_collection& translators);
+      auto translators(const translator_collection& translators) -> about_dialog&;
       
       /// @brief Gets the collection of user tab pages in this about box.
       /// @return A xtd::forms::about_dialog::user_tab_page_collection that contains the control objects in this about box.
       /// @remarks This property is used to add user tab pages in this about dialog.
-      user_tab_page_collection& user_tab_pages() noexcept;
+      [[nodiscard]] auto user_tab_pages() noexcept -> user_tab_page_collection&;
       /// @brief Gets the collection of user tab pages in this about box.
       /// @return A xtd::forms::about_dialog::user_tab_page_collection that contains the control objects in this about box.
       /// @remarks This property is used to add user tab pages in this about dialog.
-      const user_tab_page_collection& user_tab_pages() const noexcept;
+      [[nodiscard]] auto user_tab_pages() const noexcept -> const user_tab_page_collection&;
       
       /// @brief Gets the product version.
       /// @return The product version.
-      xtd::string version() const noexcept;
+      [[nodiscard]] auto version() const noexcept -> xtd::string;
       /// @brief Sets the product version.
       /// @param version The version version.
       /// @return Current about_dialog instance.
-      about_dialog& version(const xtd::string& version);
+      auto version(const xtd::string& version) -> about_dialog&;
       
       /// @brief Gets the product website.
       /// @return The website.
-      xtd::string website() const noexcept;
+      [[nodiscard]] auto website() const noexcept -> xtd::string;
       /// @brief Sets the product website.
       /// @param website The  product website.
       /// @return Current about_dialog instance.
-      about_dialog& website(const xtd::string& website);
+      auto website(const xtd::string& website) -> about_dialog&;
       
       /// @brief Gets the product website label.
       /// @return The product website label.
-      xtd::string website_label() const noexcept;
+      [[nodiscard]] auto website_label() const noexcept -> xtd::string;
       /// @brief Sets the product website label.
       /// @param website_label The product website label.
       /// @return Current about_dialog instance.
-      about_dialog& website_label(const xtd::string& website_label);
+      auto website_label(const xtd::string& website_label) -> about_dialog&;
       /// @}
       
       /// @name Public Methods
       
       /// @{
       /// @brief Resets all properties to empty string.
-      void reset() noexcept;
+      auto reset() noexcept -> void;
       
       /// @brief Runs about dialog box.
-      void show();
+      auto show() -> void;
       /// @brief Runs about dialog box with specified owner.
       /// @param owner The owner of the about box dialog.
-      void show(const iwin32_window& owner);
+      auto show(const iwin32_window& owner) -> void;
       /// @}
       
       /// @name Public Static Methods
@@ -232,10 +232,10 @@ namespace xtd {
       /// @brief Create a new xtd::forms::about_dialog from specified assembly.
       /// @param assembly The xtd::reflection::assembly object containing the information for filling in the dialog.
       /// @return New xtd::forms::about_dialog with xtd::reflection::assembly informations.
-      static about_dialog from_assembly(const xtd::reflection::assembly& assembly);
+      [[nodiscard]] static auto from_assembly(const xtd::reflection::assembly& assembly) -> about_dialog;
       /// @brief Create a new xtd::forms::about_dialog from executing xtd::reflection::assembly informations.
       /// @return New xtd::forms::about_dialog with executing xtd::reflection::assembly informations.
-      static about_dialog from_executing_assembly_informations();
+      [[nodiscard]] static auto from_executing_assembly_informations() -> about_dialog;
       /// @}
       
     private:
