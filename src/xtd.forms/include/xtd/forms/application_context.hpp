dff6a37621437daebfe64f22ddd48a5aebe60f64
Gammasoft <gammasoft71@gmail.com>
2026-05-31 15:56:23 +0200

    refactor code : using trailing return type

--- src/xtd.forms/include/xtd/forms/application_context.hpp
+++ src/xtd.forms/include/xtd/forms/application_context.hpp
@@ -42,6 +42,8 @@ namespace xtd {
       /// @cond
       application_context(application_context&&) = default;
       application_context(const application_context&) = delete;
+      application_context& operator =(application_context&&) = default;
+      application_context& operator =(const application_context&) = delete;
       ~application_context();
       /// @endcond
       
@@ -51,30 +53,30 @@ namespace xtd {
       /// @brief Gets the form to use as context.
       /// @return The form to use as context.
       /// @remarks This property determines the main form for this context. This property can change at any time. If on_main_form_closed is not overridden, the message loop of the thread terminates when the main_form parameter closes.
-      std::optional<const_form_ref> main_form() const noexcept;
+      [[nodiscard]] auto main_form() const noexcept -> std::optional<const_form_ref>;
       /// @brief Gets or sets the Form to use as context.
       /// @return The form to use as context.
       /// @remarks This property determines the main form for this context. This property can change at any time. If on_main_form_closed is not overridden, the message loop of the thread terminates when the main_form parameter closes.
-      std::optional<form_ref> main_form() noexcept;
+      [[nodiscard]] auto main_form() noexcept -> std::optional<form_ref>;
       /// @brief Sets the Form to use as context.
       /// @param main_form The form to use as context.
       /// @remarks This property determines the main form for this context. This property can change at any time. If on_main_form_closed is not overridden, the message loop of the thread terminates when the main_form parameter closes.
-      void main_form(const form& main_form);
+      auto main_form(const form& main_form) -> void;
       /// @brief Resets the Form to use as context.
       /// @param nullptr
       /// @remarks This property determines the main form for this context. This property can change at any time. If on_main_form_closed is not overridden, the message loop of the thread terminates when the main_form parameter closes.
-      void main_form(std::nullptr_t);
+      auto main_form(std::nullptr_t) -> void;
       
       /// @brief Gets an object that contains data about the control.
       /// @return A xtd::any_object that contains data about the control. The default is empty.
       /// @remarks Any type of class can be assigned to this property.
       /// @remarks A common use for the tag property is to store data that is closely associated with the control. For example, if you have a control that displays information about a customer, you might store a data_set that contains the customer's order history in that control's tag property so the data can be accessed quickly.
-      xtd::any_object tag() const noexcept;
+      [[nodiscard]] auto tag() const noexcept -> xtd::any_object;
       /// @brief Sets an object that contains data about the control.
       /// @param tag A xtd::any_object that contains data about the control. The default is empty.
       /// @remarks Any type of class can be assigned to this property.
       /// @remarks A common use for the tag property is to store data that is closely associated with the control. For example, if you have a control that displays information about a customer, you might store a data_set that contains the customer's order history in that control's tag property so the data can be accessed quickly.
-      void tag(const xtd::any_object& tag);
+      auto tag(const xtd::any_object& tag) -> void;
       /// @}
       
       /// @name Public Methods
@@ -83,7 +85,7 @@ namespace xtd {
       /// @brief Terminates the message loop of the thread.
       /// @remarks This method calls exit_thread_core.
       /// @note exit_thread and exit_thread_core do not actually cause the thread to terminate. These methods raise the thread_exit event to which the Application object listens. The Application object then terminates the thread.
-      void exit_thread();
+      auto exit_thread() -> void;
       /// @}
       
       /// @name Public Events
@@ -98,13 +100,13 @@ namespace xtd {
       /// @brief Terminates the message loop of the thread.
       /// @remarks This method is called from exit_thread.
       /// @note exit_thread and exit_thread_core do not actually cause the thread to terminate. These methods raise the thread_exit event to which the Application object listens. The Application object then terminates the thread.
-      virtual void exit_thread_core();
+      virtual auto exit_thread_core() -> void;
       
       /// @brief Calls ExitThreadCore(), which raises the ThreadExit event.
       /// @param sender The object that raised the event.
       /// @param e The event_args that contains the event data.
       /// @remarks The default implementation of this method calls exit_thread_core.
-      virtual void on_main_form_closed(object& sender, const event_args& e);
+      virtual auto on_main_form_closed(object& sender, const event_args& e) -> void;
       
     private:
       xtd::sptr<data> data_;
