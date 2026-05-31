41de6bbd780638d0cf29342e224c586bc5a5d2c9
Gammasoft <gammasoft71@gmail.com>
2026-05-31 22:19:47 +0200

    refactor code : using trailing return type

--- src/xtd.forms/include/xtd/forms/component.hpp
+++ src/xtd.forms/include/xtd/forms/component.hpp
@@ -40,11 +40,11 @@ namespace xtd {
       /// @brief Gets a value indicating whether the component can raise an event.
       /// @return `true` if the component can raise events; otherwise, `false`. The default is `true`.
       /// @remarks The default implementation of this property always returns `true`. Classes that inherit from the Component class should override this property to provide the ability to disable the raising of events.
-      virtual bool can_raise_events() const noexcept;
+      [[nodiscard]] virtual auto can_raise_events() const noexcept -> bool;
       
       /// @brief Gets a value that indicates whether the component is currently in design mode.
       /// @return `true` if the component is in design mode; otherwise, `false`.
-      bool design_mode() const noexcept;
+      [[nodiscard]] auto design_mode() const noexcept -> bool;
       /// @}
       
     private:
