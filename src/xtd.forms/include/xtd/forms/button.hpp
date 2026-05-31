c5357f4fd9c65ed07aba482e7ddfd9d213e80466
Gammasoft <gammasoft71@gmail.com>
2026-05-31 15:57:03 +0200

    refactor code : using trailing return type

--- src/xtd.forms/include/xtd/forms/button.hpp
+++ src/xtd.forms/include/xtd/forms/button.hpp
@@ -69,45 +69,61 @@ namespace xtd {
       /// @{
       /// @brief Gets if this button instance is auto repeat.
       /// @return `true`, if is auto repeat; otherwise `false`. Default is `false`.
-      virtual bool auto_repeat() const noexcept;
+      [[nodiscard]] virtual auto auto_repeat() const noexcept -> bool;
       /// @brief Sets if this button instance is auto repeat.
       /// @param auto_repeat `true`, if is auto repeat; otherwise `false`. Default is `false`.
       /// @return This button.
-      virtual button& auto_repeat(bool auto_repeat);
+      virtual auto auto_repeat(bool auto_repeat) -> button&;
       
       /// @brief Gets the auto repeat delay.
-      /// @return Auto repeat delay in milliseconds. Default is 300 ms.
-      virtual int32 auto_repeat_delay() const noexcept;
+      /// @return Auto repeat delay. Default is 300 ms.
+      [[nodiscard]] virtual auto auto_repeat_delay() const noexcept -> xtd::time_span;
+      /// @brief Gets the auto repeat delay.
+      /// @param auto_repeat_delay Auto repeat delay. Default is 300 ms.
+      /// @return This button.
+      virtual auto auto_repeat_delay(const xtd::time_span& auto_repeat_delay) -> button&;
       /// @brief Gets the auto repeat delay.
       /// @param auto_repeat_delay Auto repeat delay in milliseconds. Default is 300 ms.
       /// @return This button.
-      virtual button& auto_repeat_delay(int32 auto_repeat_delay);
-      
+      virtual auto auto_repeat_delay(xtd::int32 auto_repeat_delay) -> button&;
+
+      /// @brief Gets the auto repeat delay.
+      /// @return Auto repeat delay in milliseconds. Default is 300 ms.
+      [[nodiscard]] virtual auto auto_repeat_delay_milliseconds() const noexcept -> xtd::int32;
+
+      /// @brief Gets the auto repeat interval.
+      /// @return Auto repeat interval. Default is 100 ms.
+      [[nodiscard]] virtual auto auto_repeat_interval() const noexcept -> xtd::time_span;
+      /// @brief Gets the auto repeat interval.
+      /// @param auto_repeat_interval Auto repeat interval. Default is 100 ms.
+      /// @return This button.
+      virtual auto auto_repeat_interval(const xtd::time_span& auto_repeat_interval) -> button&;
+      /// @brief Gets the auto repeat interval.
+      /// @param auto_repeat_interval Auto repeat interval in milliseconds. Default is 100 ms.
+      /// @return This button.
+      virtual auto auto_repeat_interval(xtd::int32 auto_repeat_interval) -> button&;
+
       /// @brief Gets the auto repeat interval.
       /// @return Auto repeat interval in milliseconds. Default is 100 ms.
-      virtual int32 auto_repeat_interval() const noexcept;
-      /// @brief Gets the auto repeat interval.
-      /// @param auto_repeat_delay Auto repeat interval in milliseconds. Default is 100 ms.
-      /// @return This button.
-      virtual button& auto_repeat_interval(int32 auto_repeat_interval);
-      
+      [[nodiscard]] virtual auto auto_repeat_interval_milliseconds() const noexcept -> xtd::int32;
+
       /// @brief Gets the mode by which the button automatically resizes itself.
       /// @return One of the AutoSizeMode values. The default value is grow_only.
-      virtual forms::auto_size_mode auto_size_mode() const noexcept;
+      [[nodiscard]] virtual auto auto_size_mode() const noexcept -> xtd::forms::auto_size_mode;
       /// @brief Sets the mode by which the button automatically resizes itself.
       /// @param value One of the AutoSizeMode values. The default value is grow only.
-      virtual button& auto_size_mode(forms::auto_size_mode value);
+      virtual auto auto_size_mode(xtd::forms::auto_size_mode value) -> button&;
       
-      forms::dialog_result dialog_result() const noexcept override;
-      control& dialog_result(forms::dialog_result dialog_result) override;
+      [[nodiscard]] auto dialog_result() const noexcept -> xtd::forms::dialog_result override;
+      auto dialog_result(xtd::forms::dialog_result dialog_result) -> xtd::forms::control& override;
       /// @}
       
       /// @name Public Methods
       
       /// @{
-      void notify_default(bool value) override;
+      auto notify_default(bool value) -> void override;
       
-      void perform_click() override;
+      auto perform_click() -> void override;
       /// @}
       
       /// @name Public Static Methods
@@ -115,84 +131,84 @@ namespace xtd {
       /// @{
       /// @brief A factory to create an xtd::forms::button.
       /// @return New xtd::forms::button created.
-      static button create();
+      [[nodiscard]] static auto create() -> button;
       /// @brief A factory to create an xtd::forms::button with specified location.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::button.
       /// @return New xtd::forms::button created.
-      static button create(const drawing::point& location);
+      [[nodiscard]] static auto create(const xtd::drawing::point& location) -> button;
       /// @brief A factory to create an xtd::forms::button with specified location, and size.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::button.
       /// @param size A xtd::drawing::size that represent size of the xtd::forms::button.
       /// @return New xtd::forms::button created.
-      static button create(const drawing::point& location, const drawing::size& size);
+      [[nodiscard]] static auto create(const xtd::drawing::point& location, const xtd::drawing::size& size) -> button;
       /// @brief A factory to create an xtd::forms::button with specified location, size, and name.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::button.
       /// @param size A xtd::drawing::size that represent size of the xtd::forms::button.
       /// @param name The name of the xtd::forms::button.
       /// @return New xtd::forms::button created.
-      static button create(const drawing::point& location, const drawing::size& size, const xtd::string& name);
+      [[nodiscard]] static auto create(const xtd::drawing::point& location, const xtd::drawing::size& size, const xtd::string& name) -> button;
       /// @brief A factory to create an xtd::forms::button with specified text.
       /// @param text A string that represent text of the xtd::forms::button.
       /// @return New xtd::forms::button created.
-      static button create(const xtd::string& text);
+      [[nodiscard]] static auto create(const xtd::string& text) -> button;
       /// @brief A factory to create an xtd::forms::button with specified text, and location.
       /// @param text A string that represent text of the xtd::forms::button.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::button.
       /// @return New xtd::forms::button created.
-      static button create(const xtd::string& text, const drawing::point& location);
+      [[nodiscard]] static auto create(const xtd::string& text, const xtd::drawing::point& location) -> button;
       /// @brief A factory to create an xtd::forms::button with specified text, location, and size.
       /// @param text A string that represent text of the xtd::forms::button.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::button.
       /// @param size A xtd::drawing::size that represent size of the xtd::forms::button.
       /// @return New xtd::forms::button created.
-      static button create(const xtd::string& text, const drawing::point& location, const drawing::size& size);
+      [[nodiscard]] static auto create(const xtd::string& text, const xtd::drawing::point& location, const xtd::drawing::size& size) -> button;
       /// @brief A factory to create an xtd::forms::button with specified text, location, size, and name.
       /// @param text A string that represent text of the xtd::forms::button.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::button.
       /// @param size A xtd::drawing::size that represent size of the xtd::forms::button.
       /// @param name The name of the xtd::forms::button.
       /// @return New xtd::forms::button created.
-      static button create(const xtd::string& text, const drawing::point& location, const drawing::size& size, const xtd::string& name);
+      [[nodiscard]] static auto create(const xtd::string& text, const xtd::drawing::point& location, const xtd::drawing::size& size, const xtd::string& name) -> button;
       /// @brief A factory to create an xtd::forms::button with specified parent,.
       /// @param parent The parent that contains the new created xtd::forms::button.
       /// @return New xtd::forms::button created.
-      static button create(const control& parent);
+      [[nodiscard]] static auto create(const xtd::forms::control& parent) -> button;
       /// @brief A factory to create an xtd::forms::button with specified parent, location.
       /// @param parent The parent that contains the new created xtd::forms::button.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::button.
       /// @return New xtd::forms::button created.
-      static button create(const control& parent, const drawing::point& location);
+      [[nodiscard]] static auto create(const xtd::forms::control& parent, const xtd::drawing::point& location) -> button;
       /// @brief A factory to create an xtd::forms::button with specified parent, location, and size.
       /// @param parent The parent that contains the new created xtd::forms::button.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::button.
       /// @param size A xtd::drawing::size that represent size of the xtd::forms::button.
       /// @return New xtd::forms::button created.
-      static button create(const control& parent, const drawing::point& location, const drawing::size& size);
+      [[nodiscard]] static auto create(const xtd::forms::control& parent, const xtd::drawing::point& location, const xtd::drawing::size& size) -> button;
       /// @brief A factory to create an xtd::forms::button with specified parent, location, size, and name.
       /// @param parent The parent that contains the new created xtd::forms::button.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::button.
       /// @param size A xtd::drawing::size that represent size of the xtd::forms::button.
       /// @param name The name of the xtd::forms::button.
       /// @return New xtd::forms::button created.
-      static button create(const control& parent, const drawing::point& location, const drawing::size& size, const xtd::string& name);
+      [[nodiscard]] static auto create(const xtd::forms::control& parent, const xtd::drawing::point& location, const xtd::drawing::size& size, const xtd::string& name) -> button;
       /// @brief A factory to create an xtd::forms::button with specified parent, and text.
       /// @param parent The parent that contains the new created xtd::forms::button.
       /// @param text A string that represent text of the xtd::forms::button.
       /// @return New xtd::forms::button created.
-      static button create(const control& parent, const xtd::string& text);
+      [[nodiscard]] static auto create(const xtd::forms::control& parent, const xtd::string& text) -> button;
       /// @brief A factory to create an xtd::forms::button with specified parent, text, and location.
       /// @param parent The parent that contains the new created xtd::forms::button.
       /// @param text A string that represent text of the xtd::forms::button.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::button.
       /// @return New xtd::forms::button created.
-      static button create(const control& parent, const xtd::string& text, const drawing::point& location);
+      [[nodiscard]] static auto create(const xtd::forms::control& parent, const xtd::string& text, const xtd::drawing::point& location) -> button;
       /// @brief A factory to create an xtd::forms::button with specified parent, text, location, and size.
       /// @param parent The parent that contains the new created xtd::forms::button.
       /// @param text A string that represent text of the xtd::forms::button.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::button.
       /// @param size A xtd::drawing::size that represent size of the xtd::forms::button.
       /// @return New xtd::forms::button created.
-      static button create(const control& parent, const xtd::string& text, const drawing::point& location, const drawing::size& size);
+      [[nodiscard]] static auto create(const xtd::forms::control& parent, const xtd::string& text, const xtd::drawing::point& location, const xtd::drawing::size& size) -> button;
       /// @brief A factory to create an xtd::forms::button with specified parent, text, location, size, and name.
       /// @param parent The parent that contains the new created xtd::forms::button.
       /// @param text A string that represent text of the xtd::forms::button.
@@ -200,18 +216,18 @@ namespace xtd {
       /// @param size A xtd::drawing::size that represent size of the xtd::forms::button.
       /// @param name The name of the xtd::forms::button.
       /// @return New xtd::forms::button created.
-      static button create(const control& parent, const xtd::string& text, const drawing::point& location, const drawing::size& size, const xtd::string& name);
+      [[nodiscard]] static auto create(const xtd::forms::control& parent, const xtd::string& text, const xtd::drawing::point& location, const xtd::drawing::size& size, const xtd::string& name) -> button;
       /// @}
       
     protected:
       /// @name Protected Properties
       
       /// @{
-      forms::create_params create_params() const noexcept override;
+      [[nodiscard]] auto create_params() const noexcept -> xtd::forms::create_params override;
       
       /// @brief Gets state.
       /// @return One of xtd::forms::visual_styles::push_button_state values.
-      xtd::forms::visual_styles::push_button_state state() const noexcept;
+      [[nodiscard]] auto state() const noexcept -> xtd::forms::visual_styles::push_button_state;
       /// @}
       
       /// @name Protected Methods
@@ -221,24 +237,24 @@ namespace xtd {
       /// @return A new object that is a copy of this instance.
       /// @par Notes to Implementers
       /// All controls must be override the clone method.
-      xtd::uptr<xtd::object> clone() const override;
+      [[nodiscard]] auto clone() const -> xtd::uptr<xtd::object> override;
       
-      drawing::size measure_control() const noexcept override;
+      [[nodiscard]] auto measure_control() const noexcept -> xtd::drawing::size override;
       
-      void on_click(const event_args& e) override;
-      void on_enabled_changed(const event_args& e) override;
-      void on_handle_created(const event_args& e) override;
-      void on_image_changed(const xtd::event_args& e) override;
-      void on_mouse_down(const mouse_event_args& e) override;
-      void on_mouse_enter(const event_args& e) override;
-      void on_mouse_leave(const event_args& e) override;
-      void on_mouse_up(const mouse_event_args& e) override;
-      void on_paint(paint_event_args& e) override;
+      auto on_click(const event_args& e) -> void override;
+      auto on_enabled_changed(const event_args& e) -> void override;
+      auto on_handle_created(const event_args& e) -> void override;
+      auto on_image_changed(const xtd::event_args& e) -> void override;
+      auto on_mouse_down(const mouse_event_args& e) -> void override;
+      auto on_mouse_enter(const event_args& e) -> void override;
+      auto on_mouse_leave(const event_args& e) -> void override;
+      auto on_mouse_up(const mouse_event_args& e) -> void override;
+      auto on_paint(paint_event_args& e) -> void override;
       /// @}
       
     private:
-      void auto_repeat_perform_click();
-      void on_auto_repeat_timer_tick(object& sender, const event_args& e);
+      auto auto_repeat_perform_click() -> void;
+      auto on_auto_repeat_timer_tick(object& sender, const event_args& e) -> void;
       
       xtd::sptr<data> data_;
     };
