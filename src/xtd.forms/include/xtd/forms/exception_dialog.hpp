912d519cf6c91dcd31bda051d6ef83a0447bc202
Gammasoft <gammasoft71@gmail.com>
2026-06-06 10:33:15 +0200

    refactor code : using trailing return type

--- src/xtd.forms/include/xtd/forms/exception_dialog.hpp
+++ src/xtd.forms/include/xtd/forms/exception_dialog.hpp
@@ -35,7 +35,7 @@ namespace xtd {
     /// @par Examples
     /// The following code example demonstrates the use of exception_dialog dialog.
     /// @include exception_dialog.cpp
-    class forms_export_ exception_dialog final : public component {
+    class forms_export_ exception_dialog final : public xtd::forms::component {
       struct data;
       
     public:
@@ -51,49 +51,49 @@ namespace xtd {
       /// @{
       /// @brief Gets async dialog_result result after dialog box is closing.
       /// @return ok if the user clicks OK in the dialog box; otherwise, cancel.
-      xtd::forms::dialog_result dialog_result() const noexcept;
+      [[nodiscard]] auto dialog_result() const noexcept -> xtd::forms::dialog_result;
       
       /// @brief Gets the dialog appearance.
       /// @return One of the xtd::forms::dialog_appearance values. The default value is xtd::forms::dialog_appearance::standard.
-      xtd::forms::dialog_appearance dialog_appearance() const noexcept;
+      [[nodiscard]] auto dialog_appearance() const noexcept -> xtd::forms::dialog_appearance;
       /// @brief Sets the dialog appearance.
       /// @param dialog_appearance One of the xtd::forms::dialog_appearance values. The default value is xtd::forms::dialog_appearance::standard.
       /// @return Current exception_dialog instance.
-      exception_dialog& dialog_appearance(xtd::forms::dialog_appearance dialog_appearance);
+      auto dialog_appearance(xtd::forms::dialog_appearance dialog_appearance) -> exception_dialog&;
       
       /// @brief Gets exception reference.
       /// @return A exception reference.
-      xtd::ref<const std::exception> exception() const noexcept;
+      [[nodiscard]] auto exception() const noexcept -> xtd::ref<const std::exception>;
       /// @brief Sets exception.
       /// @param exception The exception.
       /// @return Current exception_dialog instance.
       /// @warning Contains only a reference on the parameter exception. Do not delete exception before closing dialog.
-      exception_dialog& exception(const std::exception& exception);
+      auto exception(const std::exception& exception) -> exception_dialog&;
       
       /// @brief Gets the dialog caption text.
       /// @return The current dialog caption text.
-      xtd::string text() const noexcept;
+      [[nodiscard]] auto text() const noexcept -> xtd::string;
       /// @brief Sets the dialog caption text.
       /// @param text The new dialog caption text.
       /// @return Current input_dialog instance.
-      exception_dialog& text(const xtd::string& text);
+      auto text(const xtd::string& text) -> exception_dialog&;
       /// @}
       
       /// @name Public Methods
       
       /// @{
       /// @brief Resets all properties to empty string.
-      void reset() noexcept;
+      auto reset() noexcept -> void;
       
       /// @brief Runs exception dialog box.
-      xtd::forms::dialog_result show_dialog();
+      auto show_dialog() -> xtd::forms::dialog_result;
       /// @brief Runs exception dialog box.
-      xtd::forms::dialog_result show_dialog(const iwin32_window& owner);
+      auto show_dialog(const xtd::forms::iwin32_window& owner) -> xtd::forms::dialog_result;
       /// @brief Runs exception dialog box.
       /// @remarks The result will done in async. Check result_dialog property after dialog box closed to obtain the result.
-      void show_sheet(const iwin32_window& owner);
+      auto show_sheet(const xtd::forms::iwin32_window& owner) -> void;
       /// @brief Runs exception dialog box.
-      xtd::forms::dialog_result show_sheet_dialog(const iwin32_window& owner);
+      auto show_sheet_dialog(const xtd::forms::iwin32_window& owner) -> xtd::forms::dialog_result;
       /// @}
       
       /// @name Public Events
@@ -101,7 +101,7 @@ namespace xtd {
       /// @{
       /// @brief Occurs when the user close an exception dialog box with dialog close button or other dialog buttons.
       /// @remarks For more information about handling events, see [Handling and Raising Events](https://gammasoft71.github.io/xtd/docs/documentation/guides/xtd.core/Events/overview).
-      event<exception_dialog, dialog_closed_event_handler> dialog_closed;
+      xtd::event<exception_dialog, xtd::forms::dialog_closed_event_handler> dialog_closed;
       /// @}
       
     protected:
@@ -111,7 +111,7 @@ namespace xtd {
       /// @brief Raises the close event.
       /// @param e An exception_dialog_closed_event_args that provides the event data.
       /// @remarks This method is invoked when the exception dialog box is closed.
-      void on_dialog_closed(const dialog_closed_event_args& e);
+      auto on_dialog_closed(const xtd::forms::dialog_closed_event_args& e) -> void;
       /// @}
       
     private:
