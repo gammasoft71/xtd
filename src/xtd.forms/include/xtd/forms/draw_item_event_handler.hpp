a6ef7ae4952275eb65259bb3e6a43ffff6037b0c
Gammasoft <gammasoft71@gmail.com>
2026-06-06 10:32:22 +0200

    refactor code : using trailing return type

--- src/xtd.forms/include/xtd/forms/draw_item_event_handler.hpp
+++ src/xtd.forms/include/xtd/forms/draw_item_event_handler.hpp
@@ -26,6 +26,6 @@ namespace xtd {
     /// @ingroup xtd_forms events
     /// @remarks When you create a xtd::forms::draw_item_event_args delegate, you identify the method that will handle the event. To associate the event with your event handler, add an instance of the delegate to the event. The event handler is called whenever the event occurs, unless you remove the delegate.
     /// @remarks For more information about handling events, see [Handling and Raising Events](https://gammasoft71.github.io/xtd/docs/documentation/guides/xtd.core/Events/overview).
-    using draw_item_event_handler = xtd::delegate<void(object& sender, const draw_item_event_args& e)>;
+    using draw_item_event_handler = xtd::delegate<void(xtd::object& sender, const xtd::forms::draw_item_event_args& e)>;
   }
 }
