63bccbe9dd210e92cbdb9f7948afb6e253c9d7f3
Gammasoft <gammasoft71@gmail.com>
2026-06-06 10:33:59 +0200

    refactor code : using trailing return type

--- src/xtd.forms/include/xtd/forms/folder_browser_dialog.hpp
+++ src/xtd.forms/include/xtd/forms/folder_browser_dialog.hpp
@@ -28,7 +28,7 @@ namespace xtd {
     /// @par Examples
     /// The following code example demonstrates the use of folder_browser_dialog dialog.
     /// @include folder_browser_dialog.cpp
-    class forms_export_ folder_browser_dialog final : public common_dialog {
+    class forms_export_ folder_browser_dialog final : public xtd::forms::common_dialog {
       struct data;
       
     public:
@@ -48,58 +48,58 @@ namespace xtd {
       /// @{
       /// @brief Gets a value that indicates whether the dialog will be automatically upgraded to enable new features.
       /// @return `true` if the the dialog will be automatically upgraded to enable new features; otherwise, `false`. The default value is `true`.
-      bool auto_upgrade_enable() const noexcept;
+      [[nodiscard]] auto auto_upgrade_enable() const noexcept -> bool;
       /// @brief Sets a value that indicates whether the dialog will be automatically upgraded to enable new features.
       /// @param value `true` if the the dialog will be automatically upgraded to enable new features; otherwise, `false`. The default value is `true`.
       /// @return Current folder_browser_dialog.
-      folder_browser_dialog& auto_upgrade_enable(bool value);
+      auto auto_upgrade_enable(bool value) -> folder_browser_dialog&;
       
       /// @brief Gets the descriptive text displayed above the tree view control in the dialog box.
       /// @return The description to display. The default is an empty string ("").
       /// @remarks The description property can be used to specify additional information to the user, like instructions.
-      const xtd::string& description() const noexcept;
+      [[nodiscard]] auto description() const noexcept -> const xtd::string&;
       /// @brief Sets the descriptive text displayed above the tree view control in the dialog box.
       /// @param value The description to display. The default is an empty string ("").
       /// @return Current folder_browser_dialog.
       /// @remarks The description property can be used to specify additional information to the user, like instructions.
-      folder_browser_dialog& description(const xtd::string& value);
+      auto description(const xtd::string& value) -> folder_browser_dialog&;
       
       /// @brief Gets the root folder where the browsing starts from.
       /// @return One of the environment::special_folder values. The default is environment::special_folder::desktop.
       /// @remarks Only the specified folder and any subfolders that are beneath it will appear in the dialog box and be selectable. The selected_path property, along with root_folder, determines what the selected folder will be when the dialog box is displayed, as long as SelectedPath is an absolute path that is a subfolder of root_folder (or more accurately, points to a subfolder of the shell namespace represented by root_folder).
-      environment::special_folder root_folder() const noexcept;
+      [[nodiscard]] auto root_folder() const noexcept -> xtd::environment::special_folder;
       /// @brief Sets the root folder where the browsing starts from.
       /// @param value One of the environment::special_folder values. The default is environment::special_folder::desktop.
       /// @return Current folder_browser_dialog.
       /// @remarks Only the specified folder and any subfolders that are beneath it will appear in the dialog box and be selectable. The selected_path property, along with root_folder, determines what the selected folder will be when the dialog box is displayed, as long as SelectedPath is an absolute path that is a subfolder of root_folder (or more accurately, points to a subfolder of the shell namespace represented by root_folder).
-      folder_browser_dialog& root_folder(environment::special_folder value);
+      auto root_folder(xtd::environment::special_folder value) -> folder_browser_dialog&;
       /// @brief Gets the path selected by the user.
       /// @return The path of the folder first selected in the dialog box or the last folder selected by the user. The default is an empty string ("").
       /// @remarks If the selected_ath property is set before showing the dialog box, the folder with this path will be the selected folder, as long as selected_path is set to an absolute path that is a subfolder of root_folder (or more accurately, points to a subfolder of the shell namespace represented by root_folder).
       /// @remarks If the show_dialog returns OK, meaning the user clicked the OK button, the selected_path property will return a string containing the path to the selected folder. If show_dialog returns Cancel, meaning the user canceled out of the dialog box, this property will have the same value that it had prior to displaying the dialog box. If the user selects a folder that does not have a physical path (for example, My Computer), the OK button on the dialog box will be disabled.
-      const xtd::string& selected_path() const noexcept;
+      [[nodiscard]] auto selected_path() const noexcept -> const xtd::string&;
       /// @brief Sets the path selected by the user.
       /// @param value The path of the folder first selected in the dialog box or the last folder selected by the user. The default is an empty string ("").
       /// @return Current folder_browser_dialog.
       /// @remarks If the selected_ath property is set before showing the dialog box, the folder with this path will be the selected folder, as long as selected_path is set to an absolute path that is a subfolder of root_folder (or more accurately, points to a subfolder of the shell namespace represented by root_folder).
       /// @remarks If the show_dialog returns OK, meaning the user clicked the OK button, the selected_path property will return a string containing the path to the selected folder. If show_dialog returns Cancel, meaning the user canceled out of the dialog box, this property will have the same value that it had prior to displaying the dialog box. If the user selects a folder that does not have a physical path (for example, My Computer), the OK button on the dialog box will be disabled.
-      folder_browser_dialog& selected_path(const xtd::string& value);
+      auto selected_path(const xtd::string& value) -> folder_browser_dialog&;
       
       /// @brief Gets a value indicating whether the New Folder button appears in the folder browser dialog box.
       /// @return `true` if the New Folder button is shown in the dialog box; otherwise, `false`. The default is `true`.
       /// @remarks When show_new_folder_button is `true`, the New Folder button is visible, giving the user a chance to create a folder. When the user clicks the New Folder button, a new folder is created and the user is prompted to specify the folder name. The selected node in the tree becomes the parent of the new folder. The actual caption of the New Folder button can vary depending upon the operating system
-      bool show_new_folder_button() const noexcept;
+      [[nodiscard]] bool show_new_folder_button() const noexcept;
       /// @brief Sets a value indicating whether the New Folder button appears in the folder browser dialog box.
       /// @param value `true` if the New Folder button is shown in the dialog box; otherwise, `false`. The default is `true`.
       /// @remarks When show_new_folder_button is `true`, the New Folder button is visible, giving the user a chance to create a folder. When the user clicks the New Folder button, a new folder is created and the user is prompted to specify the folder name. The selected node in the tree becomes the parent of the new folder. The actual caption of the New Folder button can vary depending upon the operating system
-      folder_browser_dialog& show_new_folder_button(bool value);
+      auto show_new_folder_button(bool value) -> folder_browser_dialog&;
       /// @}
       
       /// @name Public Methods
       
       /// @{
       /// @brief Resets all options to their default values, the last selected color to black, and the custom colors to their default values.
-      void reset() noexcept override;
+      auto reset() noexcept -> void override;
       /// @}
       
     protected:
@@ -109,16 +109,16 @@ namespace xtd {
       /// @brief When overridden in a derived class, specifies a common dialog box.
       /// @param owner A value that represents the window handle of the owner window for the common dialog box.
       /// @return `true` if the dialog box was successfully run; otherwise, `false`.
-      bool run_dialog(intptr owner) override;
+      auto run_dialog(intptr owner) -> bool override;
       
       /// @brief When overridden in a derived class, specifies a common dialog box.
       /// @param owner A value that represents the window handle of the owner window for the common dialog box.
-      void run_sheet(intptr owner) override;
+       auto run_sheet(intptr owner) -> void override;
       /// @}
       
     private:
-      bool get_option(xtd::usize flag) const noexcept;
-      void set_option(xtd::usize flag, bool value);
+      [[nodiscard]] auto get_option(xtd::usize flag) const noexcept -> bool;
+      auto set_option(xtd::usize flag, bool value) -> void;
       
       xtd::sptr<data> data_;
     };
