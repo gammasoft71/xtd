af902033f615a941c80c746e9b53cbf7049ba5fe
Gammasoft <gammasoft71@gmail.com>
2026-06-07 23:08:22 +0200

    refactor code : using trailing return type

--- src/xtd.forms/include/xtd/forms/link.hpp
+++ src/xtd.forms/include/xtd/forms/link.hpp
@@ -30,7 +30,7 @@ namespace xtd {
     /// @remarks The xtd::forms::link_label::link class defines the properties of a link within a xtd::forms::link_label control. You can use these properties to provide data to the xtd::forms::link_label::link_clicked event of the xtd::forms::link_label control to perform tasks when the link is clicked in the control. The xtd::forms::link_label::link_data property enables you to define information that the xtd::forms::link_label::link_clicked event can use to display a URL within your web browser or to open a file.
     /// @remarks In addition to information related to the link, the properties of the xtd::forms::link_label::link class also help define the text of the xtd::forms::link_label::lLink and its display state. The xtd::forms::link_label::start and xtd::forms::link_label::length properties define the location and length of text from the text of the xtd::forms::link_label control to display as a link. The xtd::forms::link_label::enabled property allows you to display the link as a disabled link, and the xtd::forms::link_label::visited property can alert the user that they already visited the specified link in the current instance of the xtd::forms::link_label.
     /// @remarks You can display multiple links in a single xtd::forms::link_label control. Each xtd::forms::link_label::link is added into the xtd::forms::link_label::link_collection associated with the xtd::forms::link_label control. To obtain the collection of links defined in a xtd::forms::link_label control, use the xtd::forms::link_label::links property.
-    class forms_export_ link : public object, public xtd::iequatable<link> {
+    class forms_export_ link : public xtd::object, public xtd::iequatable<link> {
       struct data;
       
     public:
@@ -52,7 +52,6 @@ namespace xtd {
       
       /// @cond
       link(const link&);
-      link(link&&) = default;
       link& operator =(const link&);
       /// @endcond
       
@@ -61,79 +60,79 @@ namespace xtd {
       /// @{
       /// @brief Gets a text description of the link.
       /// @return A xtd::string representing a text description of the link.
-      const xtd::string& description() const noexcept;
+      [[nodiscard]] auto description() const noexcept -> const xtd::string&;
       /// @brief Sets a text description of the link.
       /// @param value A xtd::string representing a text description of the link.
       /// @return The current instance of xtd::forms::link_label::link.
-      link& description(const xtd::string& value);
+      auto description(const xtd::string& value) -> link&;
       
       /// @brief Gets a value indicating whether the link is enabled.
       /// @return `true` if the link is enabled; otherwise, `false`.
       /// @remarks You can use this property to display a link in a disabled state within the xtd::forms::link_label control. When a link is disabled, clicking on the link does not cause the xtd::forms::link_label control to raise the xtd::forms::link_label::link_clicked event.
-      bool enabled() const noexcept;
+      [[nodiscard]] auto enabled() const noexcept -> bool;
       /// @brief Sets a value indicating whether the link is enabled.
       /// @param value `true` if the link is enabled; otherwise, `false`.
       /// @return The current instance of xtd::forms::link_label::link.
       /// @remarks You can use this property to display a link in a disabled state within the xtd::forms::link_label control. When a link is disabled, clicking on the link does not cause the xtd::forms::link_label control to raise the xtd::forms::link_label::link_clicked event.
-      link& enabled(bool value);
+      auto enabled(bool value) -> link&;
       
       /// @brief Gets the number of characters in the link text.
       /// @return The number of characters, including spaces, in the link text.
       /// @remarks To specify text from the xtd::forms::link_label to display as a link, set the xtd::forms::link_label::start property to the location in the text to start creating the link. After the xtd::forms::link_label::start property is set, set the value of the xtd::forms::link_label::length property to the number of characters, including the character position specified in the xtd::forms::link_label::start property, that you want to make the link text. For example, if you want to make the first word of the text "The quick brown fox" a link, you set the xtd::forms::link_label::start property to zero (0) and the xtd::forms::link_label::length property to three (3).
-      xtd::usize length() const noexcept;
+      [[nodiscard]] auto length() const noexcept -> xtd::usize;
       /// @brief Sets the number of characters in the link text.
       /// @param value The number of characters, including spaces, in the link text.
       /// @return The current instance of xtd::forms::link_label::link.
       /// @remarks To specify text from the xtd::forms::link_label to display as a link, set the xtd::forms::link_label::start property to the location in the text to start creating the link. After the xtd::forms::link_label::start property is set, set the value of the xtd::forms::link_label::length property to the number of characters, including the character position specified in the xtd::forms::link_label::start property, that you want to make the link text. For example, if you want to make the first word of the text "The quick brown fox" a link, you set the xtd::forms::link_label::start property to zero (0) and the xtd::forms::link_label::length property to three (3).
-      link& length(xtd::usize value);
+      auto length(xtd::usize value) -> link&;
       
       /// @brief Gets the data associated with the link.
       /// @return A xtd::any_object object representing the data associated with the link.
       /// @remarks You can use this property to supply information related to the link. The information provided by this property can be used within the xtd::forms::link_label::link_clicked event of the xtd::forms::link_label to provide information about the link that can be used to process the link being clicked. For example, you can specify the URL to display in your web browser when the link is clicked as the value of the xtd::forms::link_label::link_data property. You can also use the xtd::forms::link_label::link_data property to identify a dialog to display when the user clicks on the link.
-      const xtd::any_object& link_data() const noexcept;
+      [[nodiscard]] auto link_data() const noexcept -> const xtd::any_object&;
       /// @brief Sets the data associated with the link.
       /// @param value A xtd::any_object object representing the data associated with the link.
       /// @return The current instance of xtd::forms::link_label::link.
       /// @remarks You can use this property to supply information related to the link. The information provided by this property can be used within the xtd::forms::link_label::link_clicked event of the xtd::forms::link_label to provide information about the link that can be used to process the link being clicked. For example, you can specify the URL to display in your web browser when the link is clicked as the value of the xtd::forms::link_label::link_data property. You can also use the xtd::forms::link_label::link_data property to identify a dialog to display when the user clicks on the link.
-      link& link_data(const xtd::any_object& value);
+      auto link_data(const xtd::any_object& value) -> link&;
       
       /// @brief Gets the name of the xtd::forms::link_label::link.
       /// @return A xtd::string representing the name of the xtd::forms::link_label::link. The default value is the empty string ("").
-      const xtd::string& name() const noexcept;
+      [[nodiscard]] auto name() const noexcept -> const xtd::string&;
       /// @brief Sets the name of the xtd::forms::link_label::link.
       /// @param value A xtd::string representing the name of the xtd::forms::link_label::link. The default value is the empty string ("").
       /// @return The current instance of xtd::forms::link_label::link.
-      link& name(const xtd::string& value);
+      auto name(const xtd::string& value) -> link&;
       
       /// @brief Gets the starting location of the link within the text of the xtd::forms::link_label::link.
       /// @return The location within the text of the the xtd::forms::link_label control where the link starts.
       /// @remarks To specify text from the xtd::forms::link_label to display as a link, set the xtd::forms::link_label::start property to the location in the text to start creating the link. After the xtd::forms::link_label::start property is set, set the value of the xtd::forms::link_label::length property to the number of characters, including the character position specified in the xtd::forms::link_label::start property, that you want to make the link text. For example, if you want to make the first word of the text "The quick brown fox" a link, you set the xtd::forms::link_label::start property to zero (0) and the xtd::forms::link_label::length property to three (3).
-      xtd::usize start() const noexcept;
+      [[nodiscard]] auto start() const noexcept -> xtd::usize;
       /// @brief Sets the starting location of the link within the text of the xtd::forms::link_label::link.
       /// @param value The location within the text of the the xtd::forms::link_label control where the link starts.
       /// @return The current instance of xtd::forms::link_label::link.
       /// @remarks To specify text from the xtd::forms::link_label to display as a link, set the xtd::forms::link_label::start property to the location in the text to start creating the link. After the xtd::forms::link_label::start property is set, set the value of the xtd::forms::link_label::length property to the number of characters, including the character position specified in the xtd::forms::link_label::start property, that you want to make the link text. For example, if you want to make the first word of the text "The quick brown fox" a link, you set the xtd::forms::link_label::start property to zero (0) and the xtd::forms::link_label::length property to three (3).
-      link& start(xtd::usize value);
+      auto start(xtd::usize value) -> link&;
       
       /// @brief Gets the object that contains data about the xtd::forms::link_label::link.
       /// @return An xtd::any_object object that contains data about the control. The default has no value.
-      const xtd::any_object& tag() const noexcept;
+      [[nodiscard]] auto tag() const noexcept -> const xtd::any_object&;
       /// @brief Sets the object that contains data about the xtd::forms::link_label::link.
       /// @param value An xtd::any_object object that contains data about the control. The default has no value.
       /// @return The current instance of xtd::forms::link_label::link.
-      link& tag(const xtd::any_object& value);
+      auto tag(const xtd::any_object& value) -> link&;
       
       /// @brief Gets a value indicating whether the user has visited the link.
       /// @return `true` if the link has been visited; otherwise, `false`.
       /// @remarks A xtd::forms::link_label control does not automatically denote that a link is a visited link. To display the link as a visited link, you can set the value of this property to `true` in an event handler for the xtd::forms::link_label::link_clicked event of a xtd::forms::link_label. A visited link is displayed using the color specified in the xtd::forms::link_label::visited_link_color property of the xtd::forms::link_label control. Once the form containing the xtd::forms::link_label control is closed, the all display state associated with the link is deleted. In order to retain the display state of the link, you need to store the display state of the link in a registry setting associated with your application.
       /// @note If you have only one link specified in a xtd::forms::link_label control, you can use the xtd::forms::link_label::link_visited property of the xtd::forms::link_label to specify the link as visited. You can also use the xtd::forms::link_label::link_visited property if you have multiple links defined, but the setting only applies to the first link defined in the control.
-      bool visited() const noexcept;
+      [[nodiscard]] auto visited() const noexcept -> bool;
       /// @brief Sets a value indicating whether the user has visited the link.
       /// @param value `true` if the link has been visited; otherwise, `false`.
       /// @return The current instance of xtd::forms::link_label::link.
       /// @remarks A xtd::forms::link_label control does not automatically denote that a link is a visited link. To display the link as a visited link, you can set the value of this property to `true` in an event handler for the xtd::forms::link_label::link_clicked event of a xtd::forms::link_label. A visited link is displayed using the color specified in the xtd::forms::link_label::visited_link_color property of the xtd::forms::link_label control. Once the form containing the xtd::forms::link_label control is closed, the all display state associated with the link is deleted. In order to retain the display state of the link, you need to store the display state of the link in a registry setting associated with your application.
       /// @note If you have only one link specified in a xtd::forms::link_label control, you can use the xtd::forms::link_label::link_visited property of the xtd::forms::link_label to specify the link as visited. You can also use the xtd::forms::link_label::link_visited property if you have multiple links defined, but the setting only applies to the first link defined in the control.
-      link& visited(bool value);
+      auto visited(bool value) -> link&;
       /// @}
       
       /// @name Public Methods
@@ -142,21 +141,21 @@ namespace xtd {
       /// @brief Determines whether the specified object is equal to the current object.
       /// @param obj The object to compare with the current object.
       /// @return `true` if the specified object is equal to the current object. otherwise, `false`.
-      bool equals(const xtd::object& obj) const noexcept override;
+      [[nodiscard]] auto equals(const xtd::object& obj) const noexcept -> bool override;
       /// @brief Determines whether the specified object is equal to the current object.
       /// @param other The object to compare with the current object.
       /// @return `true` if the specified object is equal to the current object. otherwise, `false`.
-      bool equals(const link& other) const noexcept override;
+      [[nodiscard]] auto equals(const link& other) const noexcept -> bool override;
       
       /// @brief Serves as a hash function for a particular type.
       /// @return A hash code for the current object.
-      xtd::usize get_hash_code() const noexcept override;
+      [[nodiscard]] auto get_hash_code() const noexcept -> xtd::usize override;
       /// @}
       
     private:
       friend link_label;
-      bool active_() const noexcept;
-      void active_(bool active) noexcept;
+      [[nodiscard]] bool active_() const noexcept;
+      auto active_(bool active) noexcept -> void;
       
       xtd::sptr<data> data_;
     };
