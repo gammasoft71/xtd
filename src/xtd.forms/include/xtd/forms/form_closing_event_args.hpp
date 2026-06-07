d4add264d7c9cfc138fe773d97038c7e30fd4098
Gammasoft <gammasoft71@gmail.com>
2026-06-07 15:44:09 +0200

    refactor code : using trailing return code

--- src/xtd.forms/include/xtd/forms/form_closing_event_args.hpp
+++ src/xtd.forms/include/xtd/forms/form_closing_event_args.hpp
@@ -19,7 +19,7 @@ namespace xtd {
     /// @par Library
     /// xtd.forms
     /// @ingroup xtd_forms events
-    class form_closing_event_args : public cancel_event_args {
+    class form_closing_event_args : public xtd::forms::cancel_event_args {
     public:
       /// @name Public Constructors
       
@@ -27,26 +27,21 @@ namespace xtd {
       /// @brief Initializes a new instance of the form_closing_event_args class.
       form_closing_event_args() = default;
       /// @brief Initializes a new instance of the form_closing_event_args class.
-      /// @param close_reason A xtd::forms::close_reason value that represents the reason why the form is being closed.
+      /// @param value A xtd::forms::close_reason value that represents the reason why the form is being closed.
       /// @remarks The close_reason and cancel parameters are used to initialize the close_reason and cancel properties, respectively.
-      explicit form_closing_event_args(xtd::forms::close_reason close_reason) : close_reason_(close_reason) {};
+      explicit form_closing_event_args(xtd::forms::close_reason value) : close_reason_(value) {};
       /// @}
-      
-      /// @cond
-      form_closing_event_args(const form_closing_event_args& form_closing_event_args) = default;
-      form_closing_event_args& operator =(const form_closing_event_args& form_closing_event_args) = default;
-      /// @endcond
       
       /// @name Public Properties
       
       /// @{
       /// @brief Gets a value that indicates why the form is being closed.
       /// @return One of the xtd::forms::close_reason enumerated values.
-      forms::close_reason close_reason() const noexcept {return close_reason_;}
+      [[nodiscard]] auto close_reason() const noexcept -> xtd::forms::close_reason {return close_reason_;}
       /// @}
       
     private:
-      forms::close_reason close_reason_ = forms::close_reason::none;
+      xtd::forms::close_reason close_reason_ = xtd::forms::close_reason::none;
     };
   }
 }
