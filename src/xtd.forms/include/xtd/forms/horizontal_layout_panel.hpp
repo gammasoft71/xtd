5d670f3a0d402c9cb75253621a556255a752f33e
Gammasoft <gammasoft71@gmail.com>
2026-06-07 15:49:56 +0200

    refactor code : using trailing return code

--- src/xtd.forms/include/xtd/forms/horizontal_layout_panel.hpp
+++ src/xtd.forms/include/xtd/forms/horizontal_layout_panel.hpp
@@ -28,7 +28,7 @@ namespace xtd {
     /// @par Examples
     /// The following code example demonstrates the use of horizontal_layout_panel container.
     /// @include horizontal_layout_panel.cpp
-    class horizontal_layout_panel : public panel {
+    class horizontal_layout_panel : public xtd::forms::panel {
     public:
       /// @name Public Constructors
       
@@ -42,18 +42,18 @@ namespace xtd {
       /// @{
       /// @brief Gets horizontal control layout style for all child controls.
       /// @return The horizontal control layout style collection.
-      const horizontal_control_layout_style_collection& control_layout_styles() const noexcept;
+      [[nodiscard]] auto control_layout_styles() const noexcept -> const xtd::forms::horizontal_control_layout_style_collection&;
       
       /// @brief Gets horizontal control layout style for specified child controls.
       /// @param control The control to get horizontal control layout style.
       /// @return The horizontal control layout style for the specified control.
       /// @exception xtd::argument_exception container does not contains control.
-      const horizontal_control_layout_style& control_layout_style(const control_ref& control) const;
+      [[nodiscard]] auto control_layout_style(const xtd::forms::control_ref& control) const -> const xtd::forms::horizontal_control_layout_style&;
       /// @brief Sets horizontal control layout style for specified child controls.
       /// @param control The control to get horizontal control layout style.
       /// @param value The horizontal control layout style for the specified control.
       /// @exception xtd::argument_exception container does not contains control.
-      horizontal_layout_panel& control_layout_style(const control_ref& control, const horizontal_control_layout_style& value);
+      auto control_layout_style(const xtd::forms::control_ref& control, const xtd::forms::horizontal_control_layout_style& value) -> horizontal_layout_panel&;
       /// @}
       
       /// @name Public Static Methods
@@ -61,57 +61,57 @@ namespace xtd {
       /// @{
       /// @brief A factory to create an xtd::forms::horizontal_layout_panel.
       /// @return New xtd::forms::horizontal_layout_panel created.
-      static horizontal_layout_panel create();
+      [[nodiscard]] static auto create() -> horizontal_layout_panel;
       /// @brief A factory to create an xtd::forms::horizontal_layout_panel with specified location.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::horizontal_layout_panel.
       /// @return New xtd::forms::horizontal_layout_panel created.
-      static horizontal_layout_panel create(const drawing::point& location);
+      [[nodiscard]] static auto create(const xtd::drawing::point& location) -> horizontal_layout_panel;
       /// @brief A factory to create an xtd::forms::horizontal_layout_panel with specified location, and size.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::horizontal_layout_panel.
       /// @param size A xtd::drawing::size that represent size of the xtd::forms::horizontal_layout_panel.
       /// @return New xtd::forms::horizontal_layout_panel created.
-      static horizontal_layout_panel create(const drawing::point& location, const drawing::size& size);
+      [[nodiscard]] static auto create(const xtd::drawing::point& location, const xtd::drawing::size& size) -> horizontal_layout_panel;
       /// @brief A factory to create an xtd::forms::horizontal_layout_panel with specified location, size, and name.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::horizontal_layout_panel.
       /// @param size A xtd::drawing::size that represent size of the xtd::forms::horizontal_layout_panel.
       /// @param name The name of the xtd::forms::horizontal_layout_panel.
       /// @return New xtd::forms::horizontal_layout_panel created.
-      static horizontal_layout_panel create(const drawing::point& location, const drawing::size& size, const xtd::string& name);
+      [[nodiscard]] static auto create(const xtd::drawing::point& location, const xtd::drawing::size& size, const xtd::string& name) -> horizontal_layout_panel;
       /// @brief A factory to create an xtd::forms::horizontal_layout_panel with specified parent.
       /// @param parent The parent that contains the new created xtd::forms::horizontal_layout_panel.
       /// @return New xtd::forms::horizontal_layout_panel created.
-      static horizontal_layout_panel create(const control& parent);
+      [[nodiscard]] static auto create(const xtd::forms::control& parent) -> horizontal_layout_panel;
       /// @brief A factory to create an xtd::forms::horizontal_layout_panel with specified parent, and location.
       /// @param parent The parent that contains the new created xtd::forms::horizontal_layout_panel.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::horizontal_layout_panel.
       /// @return New xtd::forms::horizontal_layout_panel created.
-      static horizontal_layout_panel create(const control& parent, const drawing::point& location);
+      [[nodiscard]] static auto create(const xtd::forms::control& parent, const xtd::drawing::point& location) -> horizontal_layout_panel;
       /// @brief A factory to create an xtd::forms::horizontal_layout_panel with specified parent, location, and size.
       /// @param parent The parent that contains the new created xtd::forms::horizontal_layout_panel.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::horizontal_layout_panel.
       /// @param size A xtd::drawing::size that represent size of the xtd::forms::horizontal_layout_panel.
       /// @return New xtd::forms::horizontal_layout_panel created.
-      static horizontal_layout_panel create(const control& parent, const drawing::point& location, const drawing::size& size);
+      [[nodiscard]] static auto create(const xtd::forms::control& parent, const xtd::drawing::point& location, const xtd::drawing::size& size) -> horizontal_layout_panel;
       /// @brief A factory to create an xtd::forms::horizontal_layout_panel with specified parent, location, size, and name.
       /// @param parent The parent that contains the new created xtd::forms::horizontal_layout_panel.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::horizontal_layout_panel.
       /// @param size A xtd::drawing::size that represent size of the xtd::forms::horizontal_layout_panel.
       /// @param name The name of the xtd::forms::horizontal_layout_panel.
       /// @return New xtd::forms::horizontal_layout_panel created.
-      static horizontal_layout_panel create(const control& parent, const drawing::point& location, const drawing::size& size, const xtd::string& name);
+      [[nodiscard]] static auto create(const xtd::forms::control& parent, const xtd::drawing::point& location, const xtd::drawing::size& size, const xtd::string& name) -> horizontal_layout_panel;
       /// @}
       
     protected:
       /// @name Protected Methods
       
       /// @{
-      void on_control_added(const xtd::forms::control_event_args& e) override;
-      void on_control_removed(const xtd::forms::control_event_args& e) override;
-      void on_layout(const xtd::event_args& e) override;
+      auto on_control_added(const xtd::forms::control_event_args& e) -> void override;
+      auto on_control_removed(const xtd::forms::control_event_args& e) -> void override;
+      auto on_layout(const xtd::event_args& e) -> void override;
       /// @}
       
     private:
-      horizontal_control_layout_style_collection control_layout_styles_;
+      xtd::forms::horizontal_control_layout_style_collection control_layout_styles_;
     };
   }
 }
