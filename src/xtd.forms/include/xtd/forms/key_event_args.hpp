385d4cf45ff002bd967ec128baf7ccda8a6fe2c7
Gammasoft <gammasoft71@gmail.com>
2026-06-07 15:51:22 +0200

    refactor code : using trailing return code

--- src/xtd.forms/include/xtd/forms/key_event_args.hpp
+++ src/xtd.forms/include/xtd/forms/key_event_args.hpp
@@ -33,69 +33,69 @@ namespace xtd {
       /// @param key_data A xtd::forms::keys representing the key that was pressed, combined with any modifier flags that indicate which CMD, CTRL, SHIFT, and ALT keys were pressed at the same time. Possible values are obtained by applying the bitwise OR (|) operator to constants from the Keys enumeration.
       explicit key_event_args(xtd::forms::keys key_data) : key_data_(key_data) {}
       /// @}
-      
-      /// @cond
-      key_event_args(const xtd::forms::key_event_args& key_event_args) = default;
-      key_event_args& operator =(const xtd::forms::key_event_args& key_event_args) = default;
-      /// @endcond
+
       
       /// @name Public Properties
       
       /// @{
       /// @brief Gets a value indicating whether the ALT key was pressed.
       /// @return `true` if the ALT key was pressed; otherwise, `false`.
-      bool alt() const noexcept {return (key_data_ & xtd::forms::keys::alt) == keys::alt;}
+      [[nodiscard]] auto alt() const noexcept -> bool {return (key_data_ & xtd::forms::keys::alt) == keys::alt;}
       
       /// @brief Gets a value indicating whether the CMD key was pressed.
       /// @return `true` if the CMD key was pressed; otherwise, `false`.
-      bool command() const noexcept {return (key_data_ & xtd::forms::keys::command) == keys::command;}
+      [[nodiscard]] auto command() const noexcept -> bool {return (key_data_ & xtd::forms::keys::command) == keys::command;}
       
       /// @brief Gets a value indicating whether the CTRL key was pressed.
       /// @return `true` if the CTRL key was pressed; otherwise, `false`.
-      bool control() const noexcept {return (key_data_ & xtd::forms::keys::control) == keys::control;}
+      [[nodiscard]] auto control() const noexcept -> bool {return (key_data_ & xtd::forms::keys::control) == keys::control;}
       
       /// @brief Gets a value indicating whether the event was handled.
       /// @return `true` to bypass the control's default handling; otherwise, `false` to also pass the event along to the default control handler.
       /// @remarks handled is implemented differently by different controls within xtd.forms. For controls like text_box which subclass native Win32 controls, it is interpreted to mean that the key message should not be passed to the underlying native control. If you set handled to `true` on a text_box, that control will not pass the key press events to the underlying Win32 text box control, but it will still display the characters that the user typed.
-      bool handled() const noexcept {return handled_;}
+      [[nodiscard]] auto handled() const noexcept -> bool {return handled_;}
       /// @brief Sets a value indicating whether the event was handled.
-      /// @param handled `true` to bypass the control's default handling; otherwise, `false` to also pass the event along to the default control handler.
+      /// @param value `true` to bypass the control's default handling; otherwise, `false` to also pass the event along to the default control handler.
       /// @remarks handled is implemented differently by different controls within xtd.forms. For controls like text_box which subclass native Win32 controls, it is interpreted to mean that the key message should not be passed to the underlying native control. If you set handled to `true` on a text_box, that control will not pass the key press events to the underlying Win32 text box control, but it will still display the characters that the user typed.
-      void handled(bool handled) {handled_ = handled;}
+      auto handled(bool value) -> key_event_args& {
+        handled_ = value;
+        return *this;
+      }
       
       /// @brief Gets the keyboard code for a xtd::forms::control::key_down or xtd::forms::control::key_up event.
       /// @return A xtd::forms::keys value that is the key code for the event.
-      keys key_code() const noexcept {return key_data_ & xtd::forms::keys::key_code;}
+      [[nodiscard]] auto key_code() const noexcept -> xtd::forms::keys {return key_data_ & xtd::forms::keys::key_code;}
       
       /// @brief Gets the key data for a xtd::forms::control::key_down or xtd::forms::control::key_up event.
       /// @return A xtd::forms::keys representing the key code for the key that was pressed, combined with modifier flags that indicate which combination of CMD, CTRL, SHIFT, and ALT keys was pressed at the same time.
       /// @remarks You can use constants from Keys to extract information from the key_data property. Use the bitwise AND operator to compare data returned by key_data with constants in Keys to obtain information about which keys the user pressed. To determine whether a specific modifier key was pressed, use the command, control, shift, and alt properties.
-      keys key_data() const noexcept {return key_data_;}
+      [[nodiscard]] auto key_data() const noexcept -> xtd::forms::keys {return key_data_;}
       
       /// @brief Gets the keyboard value for a xtd::forms::control::key_down or xtd::forms::control::key_up event.
       /// @return The integer representation of the key_code property.
-      int32 key_value() const noexcept {return static_cast<int32>(key_data_) & 0xFFFF;}
+      [[nodiscard]] auto key_value() const noexcept -> xtd::int32 {return static_cast<xtd::int32>(key_data_) & 0xFFFF;}
       
       /// @brief Gets the modifier flags for a xtd::forms::control::key_down or xtd::forms::control::key_up event. The flags indicate which combination of CMD, CTRL, SHIFT, and ALT keys was pressed.
       /// @return A xtd::forms::keys value representing one or more modifier flags.
-      keys modifiers() const noexcept {return key_data_ & xtd::forms::keys::modifiers;}
+      [[nodiscard]] auto modifiers() const noexcept -> xtd::forms::keys {return key_data_ & xtd::forms::keys::modifiers;}
       
       /// @brief Gets a value indicating whether the SHIFT key was pressed.
       /// @return `true` if the SHIFT key was pressed; otherwise, `false`.
-      bool shift() const noexcept {return (key_data_ & xtd::forms::keys::shift) == keys::shift;}
+      [[nodiscard]] auto shift() const noexcept -> bool {return (key_data_ & xtd::forms::keys::shift) == keys::shift;}
       
       /// @brief Gets a value indicating whether the key event should be passed on to the underlying control.
       /// @return `true` if the key event should not be sent to the control; otherwise, `false`.
       /// @remarks You can assign `true` to this property in an event handler such as xtd::forms::control::key_down in order to prevent user input.
       /// @remarks Setting suppress_key_press to `true` also sets handled to `true`.
-      bool suppress_key_press() const noexcept {return suppress_key_press_;}
+      [[nodiscard]] auto suppress_key_press() const noexcept -> bool {return suppress_key_press_;}
       /// @brief Sets a value indicating whether the key event should be passed on to the underlying control.
-      /// @param suppress_key_press `true` if the key event should not be sent to the control; otherwise, `false`.
+      /// @param value `true` if the key event should not be sent to the control; otherwise, `false`.
       /// @remarks You can assign `true` to this property in an event handler such as xtd::forms::control::key_down in order to prevent user input.
       /// @remarks Setting suppress_key_press to `true` also sets handled to `true`.
-      void suppress_key_press(bool suppress_key_press) {
-        suppress_key_press_ = suppress_key_press;
-        if (suppress_key_press) handled(true);
+      auto suppress_key_press(bool value) -> key_event_args& {
+        suppress_key_press_ = value;
+        if (value) handled(true);
+        return *this;
       }
       /// @}
       
