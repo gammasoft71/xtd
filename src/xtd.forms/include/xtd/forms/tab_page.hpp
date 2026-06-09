ad866106b5f684c257565e12c8067eefef84809e
Gammasoft <gammasoft71@gmail.com>
2026-06-09 23:29:11 +0200

    refactor code : using trailing return type

--- src/xtd.forms/include/xtd/forms/tab_page.hpp
+++ src/xtd.forms/include/xtd/forms/tab_page.hpp
@@ -30,7 +30,7 @@ namespace xtd {
     /// @par Examples
     /// The following code example demonstrates the use of tab_control and colored tab_page containers.
     /// @include colored_tab_pages.cpp
-    class forms_export_ tab_page : public panel {
+    class forms_export_ tab_page : public xtd::forms::panel {
       struct data;
       
     public:
@@ -42,40 +42,40 @@ namespace xtd {
       /// @}
       
       /// @cond
-      //~tab_page();
+      //~tab_page() -> tab_page;
       /// @endcond
       
       /// @name Public Properties
       
       /// @{
-      using panel::auto_scroll;
+      using xtd::forms::panel::auto_scroll;
       /// @brief Sets a value indicating whether the container enables the user to scroll to any controls placed outside of its visible boundaries.
       /// @param value `true` if the container enables auto-scrolling; otherwise, `false`. The default value is `false`.
       /// @return Current scrollable_control instance.
-      scrollable_control& auto_scroll(bool value) override;
+      auto auto_scroll(bool value) -> xtd::forms::scrollable_control& override;
       
       /// @brief Gets the image list index value of the image displayed on the tab_page control.
       /// @return A zero-based index, which represents the image position in an image_list. The default is -1.
       /// @remarks When the image_index or image_list properties are set, the Image property is set to its default value, xtd::drawing::image::empty.
       /// @remarks image_key and image_index are mutually exclusive, meaning if one is set, the other is set to an invalid value and ignored. If you set the image_key property, the image_index property is automatically set to -1. Alternatively, if you set the image_index property, the image_key is automatically set to an empty string ("").
       /// @remarks If the image_list property value is changed to xtd::forms::image_list::empty, the image_index property returns its default value, -1. However, the assigned image_index value is retained internally and used when another image_list object is assigned to the image_list property. If the new image_list assigned to the image_list property has an imageList::_image_collection::size property value that is less than or equal to the value assigned to the image_index property minus one (to account for the collection being a zero-based index), the image_index property value is adjusted to one less than the size property value. For example, consider a tab_page control whose image_list has three images and whose image_index property is set to 2. If a new image_list that has only two images is assigned to the tab_page, the image_index value changes to 1.
-      virtual xtd::usize image_index() const noexcept;
+      [[nodiscard]] virtual auto image_index() const noexcept -> xtd::usize;
       /// @brief Sets the image list index value of the image displayed on the tab_page control.
       /// @return A zero-based index, which represents the image position in an image_list. The default is -1.
       /// @remarks When the image_index or image_list properties are set, the Image property is set to its default value, xtd::drawing::image::empty.
       /// @remarks image_key and image_index are mutually exclusive, meaning if one is set, the other is set to an invalid value and ignored. If you set the image_key property, the image_index property is automatically set to -1. Alternatively, if you set the image_index property, the image_key is automatically set to an empty string ("").
       /// @remarks If the image_list property value is changed to xtd::forms::image_list::empty, the image_index property returns its default value, -1. However, the assigned image_index value is retained internally and used when another image_list object is assigned to the image_list property. If the new image_list assigned to the image_list property has an imageList::_image_collection::size property value that is less than or equal to the value assigned to the image_index property minus one (to account for the collection being a zero-based index), the image_index property value is adjusted to one less than the size property value. For example, consider a tab_page control whose image_list has three images and whose image_index property is set to 2. If a new image_list that has only two images is assigned to the tab_page, the image_index value changes to 1.
-      virtual tab_page& image_index(xtd::usize value);
+      virtual auto image_index(xtd::usize value) -> tab_page&;
       
-      using panel::parent;
-      control& parent(const control& parent) override;
-      control& parent(std::nullptr_t) override;
+      using xtd::forms::panel::parent;
+      auto parent(const xtd::forms::control& parent) -> xtd::forms::control& override;
+      auto parent(xtd::null_ptr) -> xtd::forms::control& override;
       
-      using panel::text;
+      using xtd::forms::panel::text;
       /// @brief Sets the text associated with this control.
       /// @param text The text associated with this control.
       /// @return Current control instance.
-      control& text(const xtd::string& text) override;
+      auto text(const xtd::string& text) -> xtd::forms::control& override;
       /// @}
       
       /// @name Public Methods
@@ -83,90 +83,90 @@ namespace xtd {
       /// @{
       /// @brief A factory to create an xtd::forms::tab_page.
       /// @return New xtd::forms::tab_page created.
-      void destroy_handle() override;
+      auto destroy_handle() -> void override;
       /// @}
       
       /// @name Public Static Methods
       
       /// @{
-      static tab_page create();
+      [[nodiscard]] static auto create() -> tab_page;
       /// @brief A factory to create an xtd::forms::tab_page with specified location.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::tab_page.
       /// @return New xtd::forms::tab_page created.
-      static tab_page create(const drawing::point& location);
+      [[nodiscard]] static auto create(const xtd::drawing::point& location) -> tab_page;
       /// @brief A factory to create an xtd::forms::tab_page with specified location, and size.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::tab_page.
       /// @param size A xtd::drawing::size that represent size of the xtd::forms::tab_page.
       /// @return New xtd::forms::tab_page created.
-      static tab_page create(const drawing::point& location, const drawing::size& size);
+      [[nodiscard]] static auto create(const xtd::drawing::point& location, const xtd::drawing::size& size) -> tab_page;
       /// @brief A factory to create an xtd::forms::tab_page with specified location, size, and name.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::tab_page.
       /// @param size A xtd::drawing::size that represent size of the xtd::forms::tab_page.
       /// @param name The name of the xtd::forms::tab_page.
       /// @return New xtd::forms::tab_page created.
-      static tab_page create(const drawing::point& location, const drawing::size& size, const xtd::string& name);
+      [[nodiscard]] static auto create(const xtd::drawing::point& location, const xtd::drawing::size& size, const xtd::string& name) -> tab_page;
       /// @brief A factory to create an xtd::forms::tab_page with specified text.
       /// @param text A string that represent text of the xtd::forms::tab_page.
       /// @return New xtd::forms::tab_page created.
-      static tab_page create(const xtd::string& text);
+      [[nodiscard]] static auto create(const xtd::string& text) -> tab_page;
       /// @brief A factory to create an xtd::forms::tab_page with specified text, and location.
       /// @param text A string that represent text of the xtd::forms::tab_page.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::tab_page.
       /// @return New xtd::forms::tab_page created.
-      static tab_page create(const xtd::string& text, const drawing::point& location);
+      [[nodiscard]] static auto create(const xtd::string& text, const xtd::drawing::point& location) -> tab_page;
       /// @brief A factory to create an xtd::forms::tab_page with specified text, location, and size.
       /// @param text A string that represent text of the xtd::forms::tab_page.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::tab_page.
       /// @param size A xtd::drawing::size that represent size of the xtd::forms::tab_page.
       /// @return New xtd::forms::tab_page created.
-      static tab_page create(const xtd::string& text, const drawing::point& location, const drawing::size& size);
+      [[nodiscard]] static auto create(const xtd::string& text, const xtd::drawing::point& location, const xtd::drawing::size& size) -> tab_page;
       /// @brief A factory to create an xtd::forms::tab_page with specified text, location, size, and name.
       /// @param text A string that represent text of the xtd::forms::tab_page.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::tab_page.
       /// @param size A xtd::drawing::size that represent size of the xtd::forms::tab_page.
       /// @param name The name of the xtd::forms::tab_page.
       /// @return New xtd::forms::tab_page created.
-      static tab_page create(const xtd::string& text, const drawing::point& location, const drawing::size& size, const xtd::string& name);
+      [[nodiscard]] static auto create(const xtd::string& text, const xtd::drawing::point& location, const xtd::drawing::size& size, const xtd::string& name) -> tab_page;
       /// @brief A factory to create an xtd::forms::tab_page with specified parent,.
       /// @param parent The parent that contains the new created xtd::forms::tab_page.
       /// @return New xtd::forms::tab_page created.
-      static tab_page create(const control& parent);
+      [[nodiscard]] static auto create(const xtd::forms::control& parent) -> tab_page;
       /// @brief A factory to create an xtd::forms::tab_page with specified parent, location.
       /// @param parent The parent that contains the new created xtd::forms::tab_page.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::tab_page.
       /// @return New xtd::forms::tab_page created.
-      static tab_page create(const control& parent, const drawing::point& location);
+      [[nodiscard]] static auto create(const xtd::forms::control& parent, const xtd::drawing::point& location) -> tab_page;
       /// @brief A factory to create an xtd::forms::tab_page with specified parent, location, and size.
       /// @param parent The parent that contains the new created xtd::forms::tab_page.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::tab_page.
       /// @param size A xtd::drawing::size that represent size of the xtd::forms::tab_page.
       /// @return New xtd::forms::tab_page created.
-      static tab_page create(const control& parent, const drawing::point& location, const drawing::size& size);
+      [[nodiscard]] static auto create(const xtd::forms::control& parent, const xtd::drawing::point& location, const xtd::drawing::size& size) -> tab_page;
       /// @brief A factory to create an xtd::forms::tab_page with specified parent, location, size, and name.
       /// @param parent The parent that contains the new created xtd::forms::tab_page.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::tab_page.
       /// @param size A xtd::drawing::size that represent size of the xtd::forms::tab_page.
       /// @param name The name of the xtd::forms::tab_page.
       /// @return New xtd::forms::tab_page created.
-      static tab_page create(const control& parent, const drawing::point& location, const drawing::size& size, const xtd::string& name);
+      [[nodiscard]] static auto create(const xtd::forms::control& parent, const xtd::drawing::point& location, const xtd::drawing::size& size, const xtd::string& name) -> tab_page;
       /// @brief A factory to create an xtd::forms::tab_page with specified parent, and text.
       /// @param parent The parent that contains the new created xtd::forms::tab_page.
       /// @param text A string that represent text of the xtd::forms::tab_page.
       /// @return New xtd::forms::tab_page created.
-      static tab_page create(const control& parent, const xtd::string& text);
+      [[nodiscard]] static auto create(const xtd::forms::control& parent, const xtd::string& text) -> tab_page;
       /// @brief A factory to create an xtd::forms::tab_page with specified parent, text, and location.
       /// @param parent The parent that contains the new created xtd::forms::tab_page.
       /// @param text A string that represent text of the xtd::forms::tab_page.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::tab_page.
       /// @return New xtd::forms::tab_page created.
-      static tab_page create(const control& parent, const xtd::string& text, const drawing::point& location);
+      [[nodiscard]] static auto create(const xtd::forms::control& parent, const xtd::string& text, const xtd::drawing::point& location) -> tab_page;
       /// @brief A factory to create an xtd::forms::tab_page with specified parent, text, location, and size.
       /// @param parent The parent that contains the new created xtd::forms::tab_page.
       /// @param text A string that represent text of the xtd::forms::tab_page.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::tab_page.
       /// @param size A xtd::drawing::size that represent size of the xtd::forms::tab_page.
       /// @return New xtd::forms::tab_page created.
-      static tab_page create(const control& parent, const xtd::string& text, const drawing::point& location, const drawing::size& size);
+      [[nodiscard]] static auto create(const xtd::forms::control& parent, const xtd::string& text, const xtd::drawing::point& location, const xtd::drawing::size& size) -> tab_page;
       /// @brief A factory to create an xtd::forms::tab_page with specified parent, text, location, size, and name.
       /// @param parent The parent that contains the new created xtd::forms::tab_page.
       /// @param text A string that represent text of the xtd::forms::tab_page.
@@ -174,20 +174,20 @@ namespace xtd {
       /// @param size A xtd::drawing::size that represent size of the xtd::forms::tab_page.
       /// @param name The name of the xtd::forms::tab_page.
       /// @return New xtd::forms::tab_page created.
-      static tab_page create(const control& parent, const xtd::string& text, const drawing::point& location, const drawing::size& size, const xtd::string& name);
+      [[nodiscard]] static auto create(const xtd::forms::control& parent, const xtd::string& text, const xtd::drawing::point& location, const xtd::drawing::size& size, const xtd::string& name) -> tab_page;
       /// @}
       
     protected:
       /// @name Protected Properties
       
       /// @{
-      forms::create_params create_params() const noexcept override;
+      [[nodiscard]] auto create_params() const noexcept -> xtd::forms::create_params override;
       /// @}
       
       /// @name Protected Methods
       
       /// @{
-      void on_handle_created(const event_args& e) override;
+      auto on_handle_created(const event_args& e) -> void override;
       /// @}
       
     private:
