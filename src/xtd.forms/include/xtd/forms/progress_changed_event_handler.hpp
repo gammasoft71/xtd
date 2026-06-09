c3f633dcfe1adef7f4a6ee94b07f52890e0f26f1
Gammasoft <gammasoft71@gmail.com>
2026-06-09 11:18:38 +0200

    using namespace

--- src/xtd.forms/include/xtd/forms/progress_changed_event_handler.hpp
+++ src/xtd.forms/include/xtd/forms/progress_changed_event_handler.hpp
@@ -25,6 +25,6 @@ namespace xtd {
     /// xtd.forms
     /// @ingroup xtd_forms events
     /// @remarks When you create a progress_changed_event_handler delegate, you identify the method that will handle the event. To associate the event with your event handler, add an instance of the delegate to the event. The event-handler method is called whenever the event occurs, unless you remove the delegate
-    using progress_changed_event_handler = xtd::delegate<void(object& sender, const progress_changed_event_args& e)>;
+    using progress_changed_event_handler = xtd::delegate<void(xtd::object& sender, const xtd::forms::progress_changed_event_args& e)>;
   }
 }
