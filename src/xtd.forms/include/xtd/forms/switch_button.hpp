7f7b1e490b20aa891a5e0ef570557b2fce1986f8
Gammasoft <gammasoft71@gmail.com>
2026-06-09 23:00:09 +0200

    refactor code : using trailing return type

--- src/xtd.forms/include/xtd/forms/switch_button.hpp
+++ src/xtd.forms/include/xtd/forms/switch_button.hpp
@@ -46,56 +46,56 @@ namespace xtd {
       /// @brief Gets a value indicating whether the checked and the switch_button's appearance are automatically changed when the switch_button is clicked.
       /// @return `true` if the checked value and the appearance of the control are automatically changed on the click event; otherwise, `false`. The default value is `true`.
       /// @remarks If auto_check is set to `false`, you will need to add code to update the checked in the click event handler.
-      virtual bool auto_check() const noexcept;
+      [[nodiscard]] virtual auto auto_check() const noexcept -> bool;
       /// @brief Sets a value indicating whether the checked and the switch_button's appearance are automatically changed when the switch_button is clicked.
-      /// @param auto_check `true` if the checked value and the appearance of the control are automatically changed on the click event; otherwise, `false`. The default value is `true`.
+      /// @param value `true` if the checked value and the appearance of the control are automatically changed on the click event; otherwise, `false`. The default value is `true`.
       /// @return Current switch_button instance.
       /// @remarks If auto_check is set to `false`, you will need to add code to update the checked in the click event handler.
-      virtual switch_button& auto_check(bool auto_check);
+      virtual auto auto_check(bool value) -> switch_button&;
       
       /// @brief Gets the checked background color for the control.
       /// @return A xtd::drawing::color that represents the checked background color of the control. The default is the value of the xtd::forms::theme_colors::current_theme::accent color.
-      virtual xtd::drawing::color checked_back_color() const noexcept;
+      [[nodiscard]] virtual auto checked_back_color() const noexcept -> xtd::drawing::color;
       /// @brief Sets the checked background color for the control.
-      /// @param color A xtd::drawing::color that represents the checked background color of the control. The default is the value of the xtd::forms::theme_colors::current_theme::accent color.
-      virtual switch_button& checked_back_color(const xtd::drawing::color& color);
-      /// @cond
-      virtual switch_button& checked_back_color(std::nullptr_t);
-      /// @endcond
+      /// @param value A xtd::drawing::color that represents the checked background color of the control. The default is the value of the xtd::forms::theme_colors::current_theme::accent color.
+      virtual auto checked_back_color(const xtd::drawing::color& value) -> switch_button&;
+      /// @brief Resets the checked background color for the control.
+      /// @param xtd::null.
+      virtual auto checked_back_color(xtd::null_ptr) -> switch_button&;
       
       /// @brief Gets the slider color for the control.
       /// @return A xtd::drawing::color that represents the slider color of the control. The default is the value of the xtd::switch_button::back_color property.
-      virtual xtd::drawing::color slider_color() const noexcept;
+      [[nodiscard]] virtual auto slider_color() const noexcept -> xtd::drawing::color;
       /// @brief Sets the slider color for the control.
-      /// @param color A xtd::drawing::color that represents the slider color of the control. The default is the value of the xtd::switch_button::back_color property.
-      virtual switch_button& slider_color(const xtd::drawing::color& color);
-      /// @cond
-      virtual switch_button& slider_color(std::nullptr_t);
-      /// @endcond
+      /// @param value A xtd::drawing::color that represents the slider color of the control. The default is the value of the xtd::switch_button::back_color property.
+      virtual auto slider_color(const xtd::drawing::color& value) -> switch_button&;
+      /// @brief Resets the slider color for the control.
+      /// @param xtd::null.
+      virtual auto slider_color(xtd::null_ptr) -> switch_button&;
       
       /// @brief Gets a value indicating whether the switch_button is rounded appearance.
       /// @return `true` if the switch_button is in the rounded appearance; otherwise, `false`. The default value is `true`.
-      virtual bool rounded() const noexcept;
+      [[nodiscard]] virtual auto rounded() const noexcept -> bool;
       /// @brief Sets a value indicating whether the switch_button is rounded appearance.
       /// @param value `true` if the switch_button is in the rounded appearance; otherwise, `false`. The default value is `true`.
       /// @return Current switch_button instance.
-      virtual switch_button& rounded(bool value);
+      virtual auto rounded(bool value) -> switch_button&;
       
       /// @brief Gets a value indicating whether the switch_button is in the checked state.
       /// @return `true` if the switch_button is in the checked state; otherwise, `false`. The default value is `false`.
-      virtual bool checked() const noexcept;
+      [[nodiscard]] virtual auto checked() const noexcept -> bool;
       /// @brief Sets a value indicating whether the switch_button is in the checked state.
-      /// @param checked `true` if the switch_button is in the checked state; otherwise, `false`. The default value is `false`.
+      /// @param value `true` if the switch_button is in the checked state; otherwise, `false`. The default value is `false`.
       /// @return Current switch_button instance.
-      virtual switch_button& checked(bool checked);
+      virtual auto checked(bool value) -> switch_button&;
       
       /// @brief Gets a value indicating whether the switch_button text is shown.
       /// @return `true` if the switch_button text is shown; otherwise, `false`. The default value is `false`.
-      virtual bool show_text() const noexcept;
+      [[nodiscard]] virtual auto show_text() const noexcept -> bool;
       /// @brief Sets a value indicating whether the switch_button text is shown.
       /// @param value `true` if the switch_button text is shown; otherwise, `false`. The default value is `false`.
       /// @return Current switch_button instance.
-      virtual switch_button& show_text(bool value);
+      virtual auto show_text(bool value) -> switch_button&;
       /// @}
       
       /// @name Public Static Methods
@@ -103,40 +103,40 @@ namespace xtd {
       /// @{
       /// @brief A factory to create an xtd::forms::switch_button.
       /// @return New xtd::forms::switch_button created.
-      static switch_button create();
+      [[nodiscard]] static auto create() -> switch_button;
       /// @brief A factory to create an xtd::forms::switch_button with specified location.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::switch_button.
       /// @return New xtd::forms::switch_button created.
-      static switch_button create(const drawing::point& location);
+      [[nodiscard]] static auto create(const xtd::drawing::point& location) -> switch_button;
       /// @brief A factory to create an xtd::forms::switch_button with specified location, and size.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::switch_button.
       /// @param size A xtd::drawing::size that represent size of the xtd::forms::switch_button.
       /// @return New xtd::forms::switch_button created.
-      static switch_button create(const drawing::point& location, const drawing::size& size);
+      [[nodiscard]] static auto create(const xtd::drawing::point& location, const xtd::drawing::size& size) -> switch_button;
       /// @brief A factory to create an xtd::forms::switch_button with specified location, size, and name.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::switch_button.
       /// @param size A xtd::drawing::size that represent size of the xtd::forms::switch_button.
       /// @param name The name of the xtd::forms::switch_button.
       /// @return New xtd::forms::switch_button created.
-      static switch_button create(const drawing::point& location, const drawing::size& size, const xtd::string& name);
+      [[nodiscard]] static auto create(const xtd::drawing::point& location, const xtd::drawing::size& size, const xtd::string& name) -> switch_button;
       /// @brief A factory to create an xtd::forms::switch_button with specified text, and checked.
       /// @param text A string that represent text of the xtd::forms::switch_button.
       /// @param checked `true` if the check box is checked; otherwise, `false`.
       /// @return New xtd::forms::switch_button created.
-      static switch_button create(bool checked);
+      [[nodiscard]] static auto create(bool checked) -> switch_button;
       /// @brief A factory to create an xtd::forms::switch_button with specified text, checked, and location.
       /// @param text A string that represent text of the xtd::forms::switch_button.
       /// @param checked `true` if the check box is checked; otherwise, `false`.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::switch_button.
       /// @return New xtd::forms::switch_button created.
-      static switch_button create(bool checked, const drawing::point& location);
+      [[nodiscard]] static auto create(bool checked, const xtd::drawing::point& location) -> switch_button;
       /// @brief A factory to create an xtd::forms::switch_button with specified text, checked, location, and size.
       /// @param text A string that represent text of the xtd::forms::switch_button.
       /// @param checked `true` if the check box is checked; otherwise, `false`.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::switch_button.
       /// @param size A xtd::drawing::size that represent size of the xtd::forms::switch_button.
       /// @return New xtd::forms::switch_button created.
-      static switch_button create(bool checked, const drawing::point& location, const drawing::size& size);
+      [[nodiscard]] static auto create(bool checked, const xtd::drawing::point& location, const xtd::drawing::size& size) -> switch_button;
       /// @brief A factory to create an xtd::forms::switch_button with specified text, checked, location, size, and name.
       /// @param text A string that represent text of the xtd::forms::switch_button.
       /// @param checked `true` if the check box is checked; otherwise, `false`.
@@ -144,42 +144,42 @@ namespace xtd {
       /// @param size A xtd::drawing::size that represent size of the xtd::forms::switch_button.
       /// @param name The name of the xtd::forms::switch_button.
       /// @return New xtd::forms::switch_button created.
-      static switch_button create(bool checked, const drawing::point& location, const drawing::size& size, const xtd::string& name);
+      [[nodiscard]] static auto create(bool checked, const xtd::drawing::point& location, const xtd::drawing::size& size, const xtd::string& name) -> switch_button;
       /// @brief A factory to create a xtd::forms::switch_button with specified parent.
       /// @param parent The parent that contains the new created xtd::forms::switch_button.
       /// @return New xtd::forms::switch_button created.
-      static switch_button create(const control& parent);
+      [[nodiscard]] static auto create(const xtd::forms::control& parent) -> switch_button;
       /// @brief A factory to create a xtd::forms::switch_button with specified parent, and location.
       /// @param parent The parent that contains the new created xtd::forms::switch_button.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::switch_button.
       /// @return New xtd::forms::switch_button created.
-      static switch_button create(const control& parent, const drawing::point& location);
+      [[nodiscard]] static auto create(const xtd::forms::control& parent, const xtd::drawing::point& location) -> switch_button;
       /// @brief A factory to create a xtd::forms::switch_button with specified parent, location, and size.
       /// @param parent The parent that contains the new created xtd::forms::switch_button.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::switch_button.
       /// @param size A xtd::drawing::size that represent size of the xtd::forms::switch_button.
       /// @return New xtd::forms::switch_button created.
-      static switch_button create(const control& parent, const drawing::point& location, const drawing::size& size);
+      [[nodiscard]] static auto create(const xtd::forms::control& parent, const xtd::drawing::point& location, const xtd::drawing::size& size) -> switch_button;
       /// @brief A factory to create a xtd::forms::switch_button with specified parent, location, size, and name.
       /// @param parent The parent that contains the new created xtd::forms::switch_button.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::switch_button.
       /// @param size A xtd::drawing::size that represent size of the xtd::forms::switch_button.
       /// @param name The name of the xtd::forms::switch_button.
       /// @return New xtd::forms::switch_button created.
-      static switch_button create(const control& parent, const drawing::point& location, const drawing::size& size, const xtd::string& name);
+      [[nodiscard]] static auto create(const xtd::forms::control& parent, const xtd::drawing::point& location, const xtd::drawing::size& size, const xtd::string& name) -> switch_button;
       /// @brief A factory to create a xtd::forms::switch_button with specified parent, text, and checked.
       /// @param parent The parent that contains the new created xtd::forms::switch_button.
       /// @param text A string that represent text of the xtd::forms::switch_button.
       /// @param checked `true` if the check box is checked; otherwise, `false`.
       /// @return New xtd::forms::switch_button created.
-      static switch_button create(const control& parent, bool checked);
+      [[nodiscard]] static auto create(const xtd::forms::control& parent, bool checked) -> switch_button;
       /// @brief A factory to create a xtd::forms::switch_button with specified parent, text, checked, and location.
       /// @param parent The parent that contains the new created xtd::forms::switch_button.
       /// @param text A string that represent text of the xtd::forms::switch_button.
       /// @param checked `true` if the check box is checked; otherwise, `false`.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::switch_button.
       /// @return New xtd::forms::switch_button created.
-      static switch_button create(const control& parent, bool checked, const drawing::point& location);
+      [[nodiscard]] static auto create(const xtd::forms::control& parent, bool checked, const xtd::drawing::point& location) -> switch_button;
       /// @brief A factory to create a xtd::forms::switch_button with specified parent, text, checked, location, and size.
       /// @param parent The parent that contains the new created xtd::forms::switch_button.
       /// @param text A string that represent text of the xtd::forms::switch_button.
@@ -187,7 +187,7 @@ namespace xtd {
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::switch_button.
       /// @param size A xtd::drawing::size that represent size of the xtd::forms::switch_button.
       /// @return New xtd::forms::switch_button created.
-      static switch_button create(const control& parent, bool checked, const drawing::point& location, const drawing::size& size);
+      [[nodiscard]] static auto create(const xtd::forms::control& parent, bool checked, const xtd::drawing::point& location, const xtd::drawing::size& size) -> switch_button;
       /// @brief A factory to create a xtd::forms::switch_button with specified parent, text, checked, location, size, and name.
       /// @param parent The parent that contains the new created xtd::forms::switch_button.
       /// @param text A string that represent text of the xtd::forms::switch_button.
@@ -196,7 +196,7 @@ namespace xtd {
       /// @param size A xtd::drawing::size that represent size of the xtd::forms::switch_button.
       /// @param name The name of the xtd::forms::switch_button.
       /// @return New xtd::forms::switch_button created.
-      static switch_button create(const control& parent, bool checked, const drawing::point& location, const drawing::size& size, const xtd::string& name);
+      [[nodiscard]] static auto create(const xtd::forms::control& parent, bool checked, const xtd::drawing::point& location, const xtd::drawing::size& size, const xtd::string& name) -> switch_button;
       /// @}
       
       /// @name Public Events
@@ -204,7 +204,7 @@ namespace xtd {
       /// @{
       /// @brief Occurs when the value of the checked property changes.
       /// @remarks For more information about handling events, see [Handling and Raising Events](https://gammasoft71.github.io/xtd/docs/documentation/guides/xtd.core/Events/overview).
-      event<switch_button, event_handler> checked_changed;
+      xtd::event<switch_button, xtd::event_handler> checked_changed;
       /// @}
       
     protected:
@@ -213,7 +213,7 @@ namespace xtd {
       /// @{
       /// @brief Gets the default size of the control.
       /// @return The default size.
-      drawing::size default_size() const noexcept override;
+      [[nodiscard]] auto default_size() const noexcept -> xtd::drawing::size override;
       /// @}
       
       /// @name Protected Methods
@@ -223,12 +223,12 @@ namespace xtd {
       /// @param e An event_args that contains the event data.
       /// @remarks Raising an event invokes the event handler through a delegate.
       /// @remarks The on_checked_changed method also allows derived classes to handle the event without attaching a delegate. This is the preferred technique for handling the event in a derived class.
-      virtual void on_checked_changed(const event_args& e);
+      virtual auto on_checked_changed(const xtd::event_args& e) -> void;
       
-      void on_mouse_down(const mouse_event_args& e) override;
-      void on_handle_created(const event_args& e) override;
-      void on_paint(paint_event_args& e) override;
-      void on_resize(const event_args& e) override;
+      auto on_mouse_down(const xtd::forms::mouse_event_args& e) -> void override;
+      auto on_handle_created(const xtd::event_args& e) -> void override;
+      auto on_paint(xtd::forms::paint_event_args& e) -> void override;
+      auto on_resize(const xtd::event_args& e) -> void override;
       /// @}
       
     private:
