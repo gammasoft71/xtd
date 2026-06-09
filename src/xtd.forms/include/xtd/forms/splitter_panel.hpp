367460d95c76b6220bc444d4beef0059ce91ee9f
Gammasoft <gammasoft71@gmail.com>
2026-06-09 16:51:56 +0200

    refactor code : using trailing return type

--- src/xtd.forms/include/xtd/forms/splitter_panel.hpp
+++ src/xtd.forms/include/xtd/forms/splitter_panel.hpp
@@ -24,8 +24,8 @@ namespace xtd {
     /// @ingroup xtd_forms containers
     /// @remarks The constructor for this object is not public, so you cannot explicitly create a splitter_panel object.
     /// @remarks The splitter_panel is a member of its associated split_container rather than being a member of the underlying form. At design time, the splitter_panel is accessible through the panel1 or panel2 properties of split_container.
-    class forms_export_ splitter_panel : public panel {
-      friend class split_container;
+    class forms_export_ splitter_panel : public xtd::forms::panel {
+      friend class xtd::forms::split_container;
     protected:
       /// @name Public Constructors
       
@@ -39,44 +39,44 @@ namespace xtd {
       /// @{
       /// @brief A factory to create an xtd::forms::splitter_panel.
       /// @return New xtd::forms::splitter_panel created.
-      static splitter_panel create();
+      [[nodiscard]] static auto create() -> splitter_panel;
       /// @brief A factory to create an xtd::forms::splitter_panel with specified location.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::splitter_panel.
       /// @return New xtd::forms::splitter_panel created.
-      static splitter_panel create(const drawing::point& location);
+      [[nodiscard]] static auto create(const drawing::point& location) -> splitter_panel;
       /// @brief A factory to create an xtd::forms::splitter_panel with specified location, and size.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::splitter_panel.
       /// @param size A xtd::drawing::size that represent size of the xtd::forms::splitter_panel.
       /// @return New xtd::forms::splitter_panel created.
-      static splitter_panel create(const drawing::point& location, const drawing::size& size);
+      [[nodiscard]] static auto create(const drawing::point& location, const drawing::size& size) -> splitter_panel;
       /// @brief A factory to create an xtd::forms::splitter_panel with specified location, size, and name.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::splitter_panel.
       /// @param size A xtd::drawing::size that represent size of the xtd::forms::splitter_panel.
       /// @param name The name of the xtd::forms::splitter_panel.
       /// @return New xtd::forms::splitter_panel created.
-      static splitter_panel create(const drawing::point& location, const drawing::size& size, const xtd::string& name);
+      [[nodiscard]] static auto create(const drawing::point& location, const drawing::size& size, const xtd::string& name) -> splitter_panel;
       /// @brief A factory to create an xtd::forms::splitter_panel with specified parent.
       /// @param parent The parent that contains the new created xtd::forms::splitter_panel.
       /// @return New xtd::forms::splitter_panel created.
-      static splitter_panel create(const control& parent);
+      [[nodiscard]] static auto create(const control& parent) -> splitter_panel;
       /// @brief A factory to create an xtd::forms::splitter_panel with specified parent, and location.
       /// @param parent The parent that contains the new created xtd::forms::splitter_panel.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::splitter_panel.
       /// @return New xtd::forms::splitter_panel created.
-      static splitter_panel create(const control& parent, const drawing::point& location);
+      [[nodiscard]] static auto create(const control& parent, const drawing::point& location) -> splitter_panel;
       /// @brief A factory to create an xtd::forms::splitter_panel with specified parent, location, and size.
       /// @param parent The parent that contains the new created xtd::forms::splitter_panel.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::splitter_panel.
       /// @param size A xtd::drawing::size that represent size of the xtd::forms::splitter_panel.
       /// @return New xtd::forms::splitter_panel created.
-      static splitter_panel create(const control& parent, const drawing::point& location, const drawing::size& size);
+      [[nodiscard]] static auto create(const control& parent, const drawing::point& location, const drawing::size& size) -> splitter_panel;
       /// @brief A factory to create an xtd::forms::splitter_panel with specified parent, location, size, and name.
       /// @param parent The parent that contains the new created xtd::forms::splitter_panel.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::splitter_panel.
       /// @param size A xtd::drawing::size that represent size of the xtd::forms::splitter_panel.
       /// @param name The name of the xtd::forms::splitter_panel.
       /// @return New xtd::forms::splitter_panel created.
-      static splitter_panel create(const control& parent, const drawing::point& location, const drawing::size& size, const xtd::string& name);
+      [[nodiscard]] static auto create(const control& parent, const drawing::point& location, const drawing::size& size, const xtd::string& name) -> splitter_panel;
       /// @}
     };
   }
