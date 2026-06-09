b9f242cf1bb57de2462a4e646ad5ac6c5dbdcb33
Gammasoft <gammasoft71@gmail.com>
2026-06-09 16:51:47 +0200

    refactor code : using trailing return type

--- src/xtd.forms/include/xtd/forms/splitter.hpp
+++ src/xtd.forms/include/xtd/forms/splitter.hpp
@@ -39,7 +39,7 @@ namespace xtd {
       /// @{
       using xtd::forms::control::dock;
       /// @brief Sets or sets which control borders are docked to its parent control and determines how a control is resized with its parent.
-      /// @param dock One of the dock_style values. The default is none.
+      /// @param value One of the dock_style values. The default is none.
       /// @return Current control.
       /// @remarks Use the dock property to define how a control is automatically resized as its parent control is resized. For example, setting dock to dock_style::left causes the control to align itself with the left edges of its parent control and to resize as the parent control is resized. Controls are docked in their Z-order, which is the visual layering of controls on a form along the form's Z-axis (depth).
       /// @remarks A control can be docked to one edge of its parent container or can be docked to all edges and fill the parent container.
@@ -47,23 +47,23 @@ namespace xtd {
       /// @note The anchor and dock properties are mutually exclusive. Only one can be set at a time, and the last one set takes precedence.
       /// @par Notes to Inheritors
       /// When overriding the dock property in a derived class, use the base class's dock property to extend the base implementation. Otherwise, you must provide all the implementation. You are not required to override both the get and set methods of the dock property; you can override only one if needed.
-      control& dock(dock_style dock) override;
+      auto dock(xtd::forms::dock_style value) -> xtd::forms::control& override;
       
       /// @brief Gets the minimum distance that must remain between the splitter control and the container edge that the control is docked to.
       /// @return The minimum distance, in pixels, between the splitter control and the container edge that the control is docked to. The default is 25.
-      virtual int32 min_size() const noexcept;
+      [[nodiscard]] virtual auto min_size() const noexcept -> xtd::int32;
       /// @brief Sets the minimum distance that must remain between the splitter control and the container edge that the control is docked to.
-      /// @param min_size The minimum distance, in pixels, between the splitter control and the container edge that the control is docked to. The default is 25.
+      /// @param value The minimum distance, in pixels, between the splitter control and the container edge that the control is docked to. The default is 25.
       /// @return Current splitter instance.
-      virtual splitter& min_size(int32 min_size);
+      virtual auto min_size(xtd::int32 value) -> splitter&;
       
       /// @brief Gets the style of the splitter.
       /// @return One of xtd::forms::splitter_style values. The default is xtd::forms::splitter_style::update_children
-      virtual xtd::forms::splitter_style splitter_style() const noexcept;
+      [[nodiscard]] virtual auto splitter_style() const noexcept -> xtd::forms::splitter_style;
       /// @brief Sets the style of the splitter.
-      /// @param splitter_style One of xtd::forms::splitter_style values. The default is xtd::forms::splitter_style::update_children
+      /// @param value One of xtd::forms::splitter_style values. The default is xtd::forms::splitter_style::update_children
       /// @return Current splitter instance.
-      virtual splitter& splitter_style(xtd::forms::splitter_style splitter_style);
+      virtual auto splitter_style(xtd::forms::splitter_style value) -> splitter&;
       /// @}
       
       /// @name Public Static Methods
@@ -71,61 +71,61 @@ namespace xtd {
       /// @{
       /// @brief A factory to create an xtd::forms::splitter.
       /// @return New xtd::forms::splitter created.
-      static splitter create();
+      [[nodiscard]] static auto create() -> splitter;
       /// @brief A factory to create an xtd::forms::splitter with specified location.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::splitter.
       /// @return New xtd::forms::splitter created.
-      static splitter create(const drawing::point& location);
+      [[nodiscard]] static auto create(const xtd::drawing::point& location) -> splitter;
       /// @brief A factory to create an xtd::forms::splitter with specified location, and size.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::splitter.
       /// @param size A xtd::drawing::size that represent size of the xtd::forms::splitter.
       /// @return New xtd::forms::splitter created.
-      static splitter create(const drawing::point& location, const drawing::size& size);
+      [[nodiscard]] static auto create(const xtd::drawing::point& location, const xtd::drawing::size& size) -> splitter;
       /// @brief A factory to create an xtd::forms::splitter with specified location, size, and name.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::splitter.
       /// @param size A xtd::drawing::size that represent size of the xtd::forms::splitter.
       /// @param name The name of the xtd::forms::splitter.
       /// @return New xtd::forms::splitter created.
-      static splitter create(const drawing::point& location, const drawing::size& size, const xtd::string& name);
+      [[nodiscard]] static auto create(const xtd::drawing::point& location, const xtd::drawing::size& size, const xtd::string& name) -> splitter;
       /// @brief A factory to create an xtd::forms::splitter with specified parent.
       /// @param parent The parent that contains the new created xtd::forms::splitter.
       /// @return New xtd::forms::splitter created.
-      static splitter create(const control& parent);
+      [[nodiscard]] static auto create(const xtd::forms::control& parent) -> splitter;
       /// @brief A factory to create an xtd::forms::splitter with specified parent, and location.
       /// @param parent The parent that contains the new created xtd::forms::splitter.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::splitter.
       /// @return New xtd::forms::splitter created.
-      static splitter create(const control& parent, const drawing::point& location);
+      [[nodiscard]] static auto create(const xtd::forms::control& parent, const xtd::drawing::point& location) -> splitter;
       /// @brief A factory to create an xtd::forms::splitter with specified parent, location, and size.
       /// @param parent The parent that contains the new created xtd::forms::splitter.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::splitter.
       /// @param size A xtd::drawing::size that represent size of the xtd::forms::splitter.
       /// @return New xtd::forms::splitter created.
-      static splitter create(const control& parent, const drawing::point& location, const drawing::size& size);
+      [[nodiscard]] static auto create(const xtd::forms::control& parent, const xtd::drawing::point& location, const xtd::drawing::size& size) -> splitter;
       /// @brief A factory to create an xtd::forms::splitter with specified parent, location, size, and name.
       /// @param parent The parent that contains the new created xtd::forms::splitter.
       /// @param location A xtd::drawing::point that represent location of the xtd::forms::splitter.
       /// @param size A xtd::drawing::size that represent size of the xtd::forms::splitter.
       /// @param name The name of the xtd::forms::splitter.
       /// @return New xtd::forms::splitter created.
-      static splitter create(const control& parent, const drawing::point& location, const drawing::size& size, const xtd::string& name);
+      [[nodiscard]] static auto create(const xtd::forms::control& parent, const xtd::drawing::point& location, const xtd::drawing::size& size, const xtd::string& name) -> splitter;
       /// @}
       
     protected:
       /// @name Protected Properties
       
       /// @{
-      forms::cursor default_cursor() const noexcept override;
-      drawing::size default_size() const noexcept override;
+      [[nodiscard]] auto default_cursor() const noexcept -> xtd::forms::cursor override;
+      [[nodiscard]] auto default_size() const noexcept -> xtd::drawing::size override;
       /// @}
       
       /// @name Protected Methods
       
       /// @{
-      void on_handle_created(const event_args& e) override;
-      void on_mouse_down(const mouse_event_args& e) override;
-      void on_mouse_move(const mouse_event_args& e) override;
-      void on_mouse_up(const mouse_event_args& e) override;
+      auto on_handle_created(const xtd::event_args& e) -> void override;
+      auto on_mouse_down(const xtd::forms::mouse_event_args& e) -> void override;
+      auto on_mouse_move(const xtd::forms::mouse_event_args& e) -> void override;
+      auto on_mouse_up(const xtd::forms::mouse_event_args& e) -> void override;
       /// @}
       
     private:
