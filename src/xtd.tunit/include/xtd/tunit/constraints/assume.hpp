55b785e23874eb37baba79727ea9a2fc791e155b
Gammasoft <gammasoft71@gmail.com>
2026-06-10 21:55:01 +0200

    refactor code : using trailing return code

--- src/xtd.tunit/include/xtd/tunit/constraints/assume.hpp
+++ src/xtd.tunit/include/xtd/tunit/constraints/assume.hpp
@@ -20,7 +20,7 @@ namespace xtd {
   namespace tunit {
     /// @brief The constraints namespace contains the constraint-based assert model.
     namespace constraints {
-      inline auto assume() {return constraints::assert_value(constraints::assert_type::assume);}
+      [[nodiscard]] inline auto assume() {return constraints::assert_value(constraints::assert_type::assume);}
     }
   }
 }
