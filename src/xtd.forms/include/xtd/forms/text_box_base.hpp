79e7cc1aeb8d91df4dc4ce547af29577c1ca0cdf
Gammasoft <gammasoft71@gmail.com>
2026-06-10 20:22:16 +0200

    refactor code : using trailing return type

--- src/xtd.forms/include/xtd/forms/text_box_base.hpp
+++ src/xtd.forms/include/xtd/forms/text_box_base.hpp
@@ -23,7 +23,7 @@ namespace xtd {
     /// xtd.forms
     /// @ingroup xtd_forms
     /// @remarks This class implements the core features of text manipulation controls, such as xtd::forms::text_box and xtd::forms::rich_text_box. These include text selection, xtd::forms::clipboard functionality, multiline text control support, and many events.
-    class text_box_base : public control {
+    class text_box_base : public xtd::forms::control {
       struct data;
       
     public:
@@ -32,111 +32,111 @@ namespace xtd {
       /// @{
       /// @brief Gets a value indicating whether pressing the TAB key in a multiline text box control types a TAB character in the control instead of moving the focus to the next control in the tab order.
       /// @return `true` if users can enter tabs in a multiline text box using the TAB key; `false` if pressing the TAB key moves the focus. The default is `false`.
-      virtual bool accepts_tab() const noexcept;
+      [[nodiscard]] virtual auto accepts_tab() const noexcept -> bool;
       /// @brief Sets a value indicating whether pressing the TAB key in a multiline text box control types a TAB character in the control instead of moving the focus to the next control in the tab order.
       /// @param value `true` if users can enter tabs in a multiline text box using the TAB key; `false` if pressing the TAB key moves the focus. The default is `false`.
       /// @return Current text_box_base instance.
-      virtual text_box_base& accepts_tab(bool value);
+      virtual auto accepts_tab(bool value) -> text_box_base&;
       
       /// @brief Gets the border sides for the control.
       /// @return A bitwise combination of the A bitwise combination values. The default is border_style::all.
-      virtual forms::border_sides border_sides() const noexcept;
+      [[nodiscard]] virtual auto border_sides() const noexcept -> xtd::forms::border_sides;
       /// @brief Sets the border sides for the control.
-      /// @param border_style A bitwise combination of the border_sides values. The default is border_style::all.
-      virtual text_box_base& border_sides(forms::border_sides border_sides);
+      /// @param value A bitwise combination of the border_sides values. The default is border_style::all.
+      virtual auto border_sides(xtd::forms::border_sides value) -> text_box_base&;
       
       /// @brief Gets the border type of the text box control.
       /// @return A xtd::forms::border_style that represents the border type of the text box control. The default is fixed_single.
-      virtual forms::border_style border_style() const noexcept;
+      [[nodiscard]] virtual auto border_style() const noexcept -> xtd::forms::border_style;
       /// @brief Sets the border type of the text box control.
-      /// @param border_style A xtd::forms::border_style that represents the border type of the text box control. The default is fixed_single.
+      /// @param value A xtd::forms::border_style that represents the border type of the text box control. The default is fixed_single.
       /// @return Current text_box_base instance.
-      virtual text_box_base& border_style(forms::border_style border_style);
+      virtual auto border_style(xtd::forms::border_style value) -> text_box_base&;
       /// @brief Resets the border style for the control.
-      /// @param border_style nullptr.
+      /// @param xtd::null.
       /// @return Current label instance.
       /// @remarks You can use this property to add a border to the control. This property is typically used to differentiate a label that labels another control from a label that displays the status of a process in an application.
-      virtual text_box_base& border_style(std::nullptr_t);
+      virtual auto border_style(xtd::null_ptr) -> text_box_base&;
       
       /// @brief Gets the lines of text in a text box control.
       /// @return An array of strings that contains the text in a text box control.
       /// @remarks Each element in the array becomes a line of text in the text box control. If the multiline property of the text box control is set to `true` and a newline character appears in the text, the text following the newline character is added to a new element in the array and displayed on a separate line.
-      xtd::array<xtd::string> lines() const noexcept;
+      [[nodiscard]] virtual auto lines() const noexcept -> xtd::array<xtd::string>;
       /// @brief Sets the lines of text in a text box control.
-      /// @param lines An array of strings that contains the text in a text box control.
+      /// @param value An array of strings that contains the text in a text box control.
       /// @return Current text_box_base instance.
       /// @remarks Each element in the array becomes a line of text in the text box control. If the multiline property of the text box control is set to `true` and a newline character appears in the text, the text following the newline character is added to a new element in the array and displayed on a separate line.
-      text_box_base& lines(const xtd::array<xtd::string>& lines);
+      virtual auto lines(const xtd::array<xtd::string>& value) -> text_box_base&;
       
       /// @brief Gets a value indicating whether this is a multiline text box control.
       /// @return `true` if the control is a multiline text box control; otherwise, `false`. The default is `false`.
       /// @remarks A multiline text box allows you to display more than one line of text in the control. If the word_wrap property is set to `true`, text entered into the multiline text box is wrapped to the next line in the control. If the word_wrap property is set to `false`, text entered into the multiline text box control will be displayed on the same line until a newline character is entered.
-      virtual bool multiline() const noexcept;
+      [[nodiscard]] virtual auto multiline() const noexcept -> bool;
       /// @brief Sets a value indicating whether this is a multiline text box control.
       /// @param value `true` if the control is a multiline text box control; otherwise, `false`. The default is `false`.
       /// @return Current text_box_base instance.
       /// @remarks A multiline text box allows you to display more than one line of text in the control. If the word_wrap property is set to `true`, text entered into the multiline text box is wrapped to the next line in the control. If the word_wrap property is set to `false`, text entered into the multiline text box control will be displayed on the same line until a newline character is entered.
-      virtual text_box_base& multiline(bool value);
+      virtual auto multiline(bool value) -> text_box_base&;
       
       /// @brief Gets a value indicating whether text in the text box is read-only.
       /// @return `true` if the text box is read-only; otherwise, `false`. The default is `false`.
       /// @remarks When this property is set to `true`, the contents of the control cannot be changed by the user at runtime. With this property set to `true`, you can still set the value of the Text property in code. You can use this feature instead of disabling the control with the Enabled property to allow the contents to be copied and ToolTips to be shown.
-      virtual bool read_only() const noexcept;
+      [[nodiscard]] virtual auto read_only() const noexcept -> bool;
       /// @brief Sets a value indicating whether text in the text box is read-only.
       /// @param value `true` if the text box is read-only; otherwise, `false`. The default is `false`.
       /// @return Current text_box_base instance.
       /// @remarks When this property is set to `true`, the contents of the control cannot be changed by the user at runtime. With this property set to `true`, you can still set the value of the Text property in code. You can use this feature instead of disabling the control with the Enabled property to allow the contents to be copied and ToolTips to be shown.
-      virtual text_box_base& read_only(bool value);
+      virtual auto read_only(bool value) -> text_box_base&;
       
       /// @brief Gets the number of characters selected in the text box.
       /// @return The number of characters selected in the text box.
       /// @remarks You can use this property to determine if any characters are currently selected in the text box control before performing operations on the selected text. When the value of the selection_length property is set to a value that is larger than the number of characters within the text of the control, the value of the selection_length property is set to the entire length of text within the control minus the value of the selection_start property (if any value is specified for the selection_start property).
-      virtual xtd::usize selection_length() const noexcept;
+      [[nodiscard]] virtual auto selection_length() const noexcept -> xtd::usize;
       /// @brief Sets the number of characters selected in the text box.
       /// @param value The number of characters selected in the text box.
       /// @return Current text_box_base instance.
       /// @remarks You can use this property to determine if any characters are currently selected in the text box control before performing operations on the selected text. When the value of the selection_length property is set to a value that is larger than the number of characters within the text of the control, the value of the selection_length property is set to the entire length of text within the control minus the value of the selection_start property (if any value is specified for the selection_start property).
-      virtual text_box_base& selection_length(xtd::usize value);
+      virtual auto selection_length(xtd::usize value) -> text_box_base&;
       
       /// @brief Gets o the starting point of text selected in the text box.
       /// @return The starting position of text selected in the text box.
       /// @remarks If no text is selected in the control, this property indicates the insertion point, or caret, for new text. If you set this property to a location beyond the length of the text in the control, the selection start position will be placed after the last character. When text is selected in the text box control, changing this property might decrease the value of the SelectionLength property. If the remaining text in the control after the position indicated by the selection_start property is less than the value of the selection_length property, the value of the selection_length property is automatically decreased. The value of the selection_start property never causes an increase in the selection_length property.
-      virtual xtd::usize selection_start() const noexcept;
+      [[nodiscard]] virtual auto selection_start() const noexcept -> xtd::usize;
       /// @brief Gets o the starting point of text selected in the text box.
       /// @param value The starting position of text selected in the text box.
       /// @return Current text_box_base instance.
       /// @remarks If no text is selected in the control, this property indicates the insertion point, or caret, for new text. If you set this property to a location beyond the length of the text in the control, the selection start position will be placed after the last character. When text is selected in the text box control, changing this property might decrease the value of the SelectionLength property. If the remaining text in the control after the position indicated by the selection_start property is less than the value of the selection_length property, the value of the selection_length property is automatically decreased. The value of the selection_start property never causes an increase in the selection_length property.
-      virtual text_box_base& selection_start(xtd::usize value);
+      virtual auto selection_start(xtd::usize value) -> text_box_base&;
       
       /// @brief Indicates whether a multiline text box control automatically wraps words to the beginning of the next line when necessary.
       /// @return `true` if the multiline text box control wraps words; `false` if the text box control automatically scrolls horizontally when the user types past the right edge of the control. The default is `true`.
-      virtual bool word_wrap() const noexcept;
+      [[nodiscard]] virtual auto word_wrap() const noexcept -> bool;
       /// @brief Indicates whether a multiline text box control automatically wraps words to the beginning of the next line when necessary.
       /// @param value `true` if the multiline text box control wraps words; `false` if the text box control automatically scrolls horizontally when the user types past the right edge of the control. The default is `true`.
       /// @return Current text_box_base instance.
-      virtual text_box_base& word_wrap(bool value);
-      
-      /// @brief Appends text to the current text of a text box.
-      /// @param value The text to append to the current contents of the text box.
-      virtual void append_text(const xtd::string& value);
+      virtual auto word_wrap(bool value) -> text_box_base&;
       /// @}
       
       /// @name Public Methods
       
       /// @{
+      /// @brief Appends text to the current text of a text box.
+      /// @param text The text to append to the current contents of the text box.
+      virtual auto append_text(const xtd::string& text) -> void;
+
       /// @brief Clears all text from the text box control.
       /// @remarks You can use this method to clear the contents of the control instead of assigning the text property an empty string.
-      void clear();
+      auto clear() -> void;
       
       /// @brief Selects a range of text in the text box.
       /// @param start The position of the first character in the current text selection within the text box.
       /// @param length The number of characters to select.
       /// @exception argument_out_of_range_exception The value of the start parameter is less than zero.
-      virtual void select(xtd::usize start, xtd::usize length);
+      virtual auto select(xtd::usize start, xtd::usize length) -> void;
       
       /// @brief Selects all text in the text box.
       /// @remarks This method enables you to select all text within the control. You can use this method in conjunction with the cut method, which requires text to be selected in the control, to cut the entire contents of the control and paste them into the clipboard.
-      void select_all();
+      auto select_all() -> void;
       /// @}
       
       /// @name Public Events
@@ -144,19 +144,19 @@ namespace xtd {
       /// @{
       /// @brief Occurs when the value of the accepts_tab property has changed.
       /// @remarks For more information about handling events, see [Handling and Raising Events](https://gammasoft71.github.io/xtd/docs/documentation/guides/xtd.core/Events/overview).
-      event<text_box_base, event_handler> accepts_tab_changed;
+      xtd::event<text_box_base, xtd::event_handler> accepts_tab_changed;
       
       /// @brief Occurs when the value of the accepts_tab border_style has changed.
       /// @remarks For more information about handling events, see [Handling and Raising Events](https://gammasoft71.github.io/xtd/docs/documentation/guides/xtd.core/Events/overview).
-      event<text_box_base, event_handler> border_style_changed;
+      xtd::event<text_box_base, xtd::event_handler> border_style_changed;
       
       /// @brief Occurs when the value of the accepts_tab border_style has changed.
       /// @remarks For more information about handling events, see [Handling and Raising Events](https://gammasoft71.github.io/xtd/docs/documentation/guides/xtd.core/Events/overview).
-      event<text_box_base, event_handler> multiline_changed;
+      xtd::event<text_box_base, xtd::event_handler> multiline_changed;
       
       /// @brief Occurs when the value of the read_only property has changed.
       /// @remarks For more information about handling events, see [Handling and Raising Events](https://gammasoft71.github.io/xtd/docs/documentation/guides/xtd.core/Events/overview).
-      event<text_box_base, event_handler> read_only_changed;
+      xtd::event<text_box_base, xtd::event_handler> read_only_changed;
       /// @}
       
     protected:
@@ -170,8 +170,8 @@ namespace xtd {
       /// @name Protected Properties
       
       /// @{
-      forms::create_params create_params() const noexcept override;
-      forms::cursor default_cursor() const noexcept override;
+      [[nodiscard]] auto create_params() const noexcept -> xtd::forms::create_params override;
+      [[nodiscard]] auto default_cursor() const noexcept -> xtd::forms::cursor override;
       /// @}
       
       /// @name Protected Methods
@@ -179,19 +179,19 @@ namespace xtd {
       /// @{
       /// @brief Raises the text_box_base::accepts_tab_changed event.
       /// @param e An xtd::event_args that contains the event data.
-      virtual void on_accepts_tab_changed(const event_args& e);
+      virtual auto on_accepts_tab_changed(const xtd::event_args& e) -> void;
       
       /// @brief Raises the text_box_base::border_style_changed event.
       /// @param e An xtd::event_args that contains the event data.
-      virtual void on_border_style_changed(const event_args& e);
+      virtual auto on_border_style_changed(const xtd::event_args& e) -> void;
       
       /// @brief Raises the text_box_base::text_box_base::multiline_changed event.
       /// @param e An xtd::event_args that contains the event data.
-      virtual void on_multiline_changed(const event_args& e);
+      virtual auto on_multiline_changed(const xtd::event_args& e) -> void;
       
       /// @brief Raises the text_box_base::read_only_changed event.
       /// @param e An xtd::event_args that contains the event data.
-      virtual void on_read_only_changed(const event_args& e);
+      virtual auto on_read_only_changed(const xtd::event_args& e) -> void;
       /// @}
       
     private:
