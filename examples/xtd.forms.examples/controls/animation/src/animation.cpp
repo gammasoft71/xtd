10d4dcbf6e15c100c6d903c646d8faa9af2cdcac
Gammasoft <gammasoft71@gmail.com>
2026-06-12 18:21:43 +0200

    refactor example

--- examples/xtd.forms.examples/controls/animation/src/animation.cpp
+++ examples/xtd.forms.examples/controls/animation/src/animation.cpp
@@ -8,20 +8,20 @@ namespace animation_example {
       
       fish_animation.frames_per_second(60).dock(dock_style::fill);
       fish_animation.start();
-      fish_animation.click += [&] {fish_animation.running(!fish_animation.running());};
+      fish_animation.click += [](object& sender, const event_args& e) {as<animation>(sender).running(!as<animation>(sender).running());};
       fish_animation.updated += [](object& sender, const animation_updated_event_args& e){diagnostics::debug::write_line("frame_counter = {}, elapsed = {}", e.frame_counter(), e.elapsed());};
       fish_animation.paint += [](object& sender, paint_event_args& e) {
         e.graphics().clear(basic_colors::teal());
         static const auto fish_length = 15;
         auto points = list<point_f> {};
-        for (auto i = 0; i < fish_length; ++i) {
-          auto radius = e.clip_rectangle().width / 4.0 + 10.0 * math::sin(as<animation>(sender).frame_counter() * 0.1 + i * 0.5);
-          auto x = as<float>(e.clip_rectangle().width / 2.0 + 1.5 * radius * sin(as<animation>(sender).frame_counter() * 0.02 + i * 0.12));
-          auto y = as<float>(e.clip_rectangle().height / 2.0 + 1.0 * radius * math::cos(as<animation>(sender).frame_counter() * 0.04 + i * 0.12f));
-          e.graphics().fill_ellipse(brushes::aqua(), x - i, y - i, 2.0f + 2.0f * i, 2.0f + 2.0f * i);
-          points.add({x, y + 1});
+        for (auto index = 0; index < fish_length; ++index) {
+          auto radius = e.clip_rectangle().width / 4.0 + 10.0 * math::sin(as<animation>(sender).frame_counter() * 0.1 + index * 0.5);
+          auto x = as<float>(e.clip_rectangle().width / 2.0 + 1.5 * radius * math::sin(as<animation>(sender).frame_counter() * 0.02 + index * 0.12));
+          auto y = as<float>(e.clip_rectangle().height / 2.0 + 1.0 * radius * math::cos(as<animation>(sender).frame_counter() * 0.04 + index * 0.12f));
+          e.graphics().fill_ellipse(brushes::aqua(), x - index, y - index, 2.0f + 2.0f * index, 2.0f + 2.0f * index);
+          points.add({x, y});
         }
-        e.graphics().draw_lines(pens::aqua(4), points.to_array());
+        e.graphics().draw_beziers(pens::aqua(4), points.to_array());
       };
     }
     
