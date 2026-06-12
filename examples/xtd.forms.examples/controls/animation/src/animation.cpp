678b525097aea97be11e0a0e9dd324b540e882f4
Gammasoft <gammasoft71@gmail.com>
2026-06-12 10:30:13 +0200

    refactor exemple

--- examples/xtd.forms.examples/controls/animation/src/animation.cpp
+++ examples/xtd.forms.examples/controls/animation/src/animation.cpp
@@ -13,40 +13,31 @@ namespace animation_example {
       fish_animation.dock(dock_style::fill);
       fish_animation.frames_per_second(60);
       fish_animation.start();
-      fish_animation.click += event_handler {*this, &form1::on_fish_animation_click};
-      fish_animation.paint += paint_event_handler {*this, &form1::on_fish_animation_paint};
-      fish_animation.updated += animation_updated_event_handler {*this, &form1::on_fish_animation_updated};
+      fish_animation.click += [&] {fish_animation.running(!fish_animation.running());};
+      fish_animation.updated += [](object& sender, const animation_updated_event_args& e){diagnostics::debug::write_line(string::format("frame_counter={}, elapsed={}", e.frame_counter(), e.elapsed()));};
+      fish_animation.paint += [](object& sender, paint_event_args& e) {
+        e.graphics().clear(as<animation>(sender).back_color());
+        static const auto fish_length = 16;
+        auto points = list<point_f> {};
+        for (auto i = 0; i < fish_length; ++i) {
+          auto radius = 100.0 + 10.0 * math::sin(as<animation>(sender).frame_counter() * 0.1 + i * 0.5);
+          auto x = as<float>(e.clip_rectangle().width / 2.0 + 1.5 * radius * sin(as<animation>(sender).frame_counter() * 0.02 + i * 0.12));
+          auto y = as<float>(e.clip_rectangle().height / 2.0 + 1.0 * radius * math::cos(as<animation>(sender).frame_counter() * 0.04 + i * 0.12f));
+          e.graphics().fill_ellipse(solid_brush(as<animation>(sender).fore_color()), x - i, y - i, 2.0f + 2.0f * i, 2.0f + 2.0f * i);
+          points.add({x, y});
+        }
+        e.graphics().draw_lines(pen(as<animation>(sender).fore_color(), 4), points.to_array());
+      };
     }
     
   private:
-    auto on_fish_animation_click(object& sender, const event_args& e) -> void {
-      fish_animation.running(!fish_animation.running());
-    }
-    
-    auto on_fish_animation_paint(object& sender, paint_event_args& e) -> void {
-      e.graphics().clear(fish_animation.back_color());
-      static const auto fish_length = 16;
-      auto points = list<point_f> {};
-      for (auto i = 0; i < fish_length; ++i) {
-        auto radius = 100.0f + 10.0f * as<float>(math::sin(as<float>(fish_animation.frame_counter()) * 0.1f + as<float>(i) * 0.5f));
-        auto point = point_f {as<float>(e.clip_rectangle().width) / 2.0f + 1.5f * radius * sin(as<float>(fish_animation.frame_counter()) * 0.02f + as<float>(i) * 0.12f), as<float>(e.clip_rectangle().height) / 2.0f + 1.0f * radius * as<float>(math::cos(as<float>(fish_animation.frame_counter()) * 0.04f + as<float>(i) * 0.12f))};
-        e.graphics().fill_ellipse(solid_brush(fish_animation.fore_color()), point.x - as<float>(i), point.y - as<float>(i), 2.0f + 2.0f * as<float>(i), 2.0f + 2.0f * as<float>(i));
-        points.add(point);
-      }
-      e.graphics().draw_lines(pen(fish_animation.fore_color(), 4), points.to_array());
-    }
-    
-    auto on_fish_animation_updated(object& sender, const animation_updated_event_args& e) -> void {
-      diagnostics::debug::write_line(string::format("frame_counter={}, elapsed={}", e.frame_counter(), e.elapsed()));
-    }
-    
     animation fish_animation;
   };
 }
 
 auto main() -> int {
-  auto df = debug_form {};
-  df.back_color(basic_colors::black());
-  df.fore_color(basic_colors::lime());
+  //auto df = debug_form {};
+  //df.back_color(basic_colors::black());
+  //df.fore_color(basic_colors::lime());
   application::run(animation_example::form1 {});
 }
