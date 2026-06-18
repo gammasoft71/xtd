b07b9bbdb9dc91c1addf9daffad64b897fb20c1f
Gammasoft <gammasoft71@gmail.com>
2026-06-18 23:14:24 +0200

    Fix warning

--- examples/xtd.forms.examples/user_controls/numeric_text_box/src/numeric_text_box.cpp
+++ examples/xtd.forms.examples/user_controls/numeric_text_box/src/numeric_text_box.cpp
@@ -8,7 +8,7 @@ namespace numeric_tex_box_example {
     
     double value() const {
       auto result = .0;
-      try_parse(text(), result);
+      [[maybe_unused]] auto _ = try_parse(text(), result);
       return result;
     }
     
