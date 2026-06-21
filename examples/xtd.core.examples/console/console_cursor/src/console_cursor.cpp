a6f1e7f469def8418f3a771a5f966bb2fae9e36c
Gammasoft <gammasoft71@gmail.com>
2026-06-22 00:31:52 +0200

    Refactor example

--- examples/xtd.core.examples/console/console_cursor/src/console_cursor.cpp
+++ examples/xtd.core.examples/console/console_cursor/src/console_cursor.cpp
@@ -1,46 +1,56 @@
 #include <xtd/xtd>
 
-auto orig_row = 0;
-auto orig_col = 0;
+namespace console_curosr_example {
+  class program {
+  public:
+    // The main entry point for the application.
+    static auto main() {
+      // Clear the screen, then save the top and left coordinates.
+      console::clear();
+      orig_row = console::cursor_top();
+      orig_col = console::cursor_left();
+      
+      // Draw the left side of a 5x5 rectangle, from top to bottom.
+      write_at("+", 0, 0);
+      write_at("|", 0, 1);
+      write_at("|", 0, 2);
+      write_at("|", 0, 3);
+      write_at("+", 0, 4);
+      
+      // Draw the bottom side, from left to right.
+      write_at("-", 1, 4); // shortcut: write_at("---", 1, 4)
+      write_at("-", 2, 4); // ...
+      write_at("-", 3, 4); // ...
+      write_at("+", 4, 4);
+      
+      // Draw the right side, from bottom to top.
+      write_at("|", 4, 3);
+      write_at("|", 4, 2);
+      write_at("|", 4, 1);
+      write_at("+", 4, 0);
+      
+      // Draw the top side, from right to left.
+      write_at("-", 3, 0); // shortcut: write_at("---", 1, 0)
+      write_at("-", 2, 0); // ...
+      write_at("-", 1, 0); // ...
+      //
+      write_at("All done!", 0, 6);
+      console::write_line();
+    }
 
-auto write_at(const string& s, int x, int y) {
-  console::set_cursor_position(orig_col + x, orig_row + y);
-  console::write(s);
+  private:
+    static auto write_at(const string& s, int x, int y) noexcept -> void {
+      console::set_cursor_position(orig_col + x, orig_row + y);
+      console::write(s);
+    }
+    
+    static inline auto orig_row = 0;
+    static inline auto orig_col = 0;
+  };
 }
 
-auto main() -> int {
-  // Clear the screen, then save the top and left coordinates.
-  console::clear();
-  orig_row = console::cursor_top();
-  orig_col = console::cursor_left();
-  
-  // Draw the left side of a 5x5 rectangle, from top to bottom.
-  write_at("+", 0, 0);
-  write_at("|", 0, 1);
-  write_at("|", 0, 2);
-  write_at("|", 0, 3);
-  write_at("+", 0, 4);
-  
-  // Draw the bottom side, from left to right.
-  write_at("-", 1, 4); // shortcut: write_at("---", 1, 4)
-  write_at("-", 2, 4); // ...
-  write_at("-", 3, 4); // ...
-  write_at("+", 4, 4);
-  
-  // Draw the right side, from bottom to top.
-  write_at("|", 4, 3);
-  write_at("|", 4, 2);
-  write_at("|", 4, 1);
-  write_at("+", 4, 0);
-  
-  // Draw the top side, from right to left.
-  write_at("-", 3, 0); // shortcut: write_at("---", 1, 0)
-  write_at("-", 2, 0); // ...
-  write_at("-", 1, 0); // ...
-  //
-  write_at("All done!", 0, 6);
-  console::write_line();
-}
+
+startup_(console_curosr_example::program::main);
 
 // This code produces the following output :
 //
