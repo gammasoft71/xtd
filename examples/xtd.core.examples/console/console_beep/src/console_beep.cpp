797a5fcaf2edb8cd43fd6818cc6d6a9c9cf84ef3
Gammasoft <gammasoft71@gmail.com>
2026-06-22 00:31:36 +0200

    Refactor example

--- examples/xtd.core.examples/console/console_beep/src/console_beep.cpp
+++ examples/xtd.core.examples/console/console_beep/src/console_beep.cpp
@@ -1,16 +1,17 @@
 #include <xtd/xtd>
 
-auto main(int argc, char* argv[]) -> int {
-  auto x = 0;
-  //
-  if ((argc == 2) && (int32_object::try_parse(argv[1], x) == true) && ((x >= 1) && (x <= 9))) {
-    for (auto i = 1; i <= x; ++i) {
-      console::write_line("Beep number {}.", i);
-      console::beep();
-      thread::sleep(300_ms);
-    }
-  } else
+auto main() -> int {
+  auto beep_count = 0;
+  if (environment::get_command_line_args().length() != 2 || !int32_object::try_parse(environment::get_command_line_args()[1], beep_count) || beep_count < 1 || beep_count > 9) {
     console::write_line("Usage: Enter the number of times (between 1 and 9) to beep.");
+    return 1;
+  }
+  
+  for (auto i = 1; i <= beep_count; ++i) {
+    console::write_line("Beep number {}.", i);
+    console::beep();
+    thread::sleep(300_ms);
+  }
 }
 
 // This code produces the following output if 3 is entered on command line:
@@ -18,4 +19,3 @@ auto main(int argc, char* argv[]) -> int {
 // Beep number 1.
 // Beep number 2.
 // Beep number 3.
-
