c778dfeaa3c3368729c1e9d7ee74035b45c6b5cf
Gammasoft <gammasoft71@gmail.com>
2026-06-22 23:49:24 +0200

    Refactor example

--- examples/xtd.core.examples/console/console_out/src/console_out.cpp
+++ examples/xtd.core.examples/console/console_out/src/console_out.cpp
@@ -5,7 +5,7 @@ auto main() -> int {
   console::out << "Value = " << 42 << environment::new_line;
   console::out << "Value (Hex) = 0x" << int32_object(42).to_string("X") << environment::new_line;
   console::out << "Current date = " << date_time::now() << environment::new_line;
-  console::out << "DayOfWeek = " << enum_object(day_of_week::monday) << environment::new_line;
+  console::out << "Day of week = " << enum_object {day_of_week::monday} << environment::new_line;
   console::out << "Duration = " << 23_h + 5_min + 24_s << environment::new_line;
 }
 
@@ -14,6 +14,6 @@ auto main() -> int {
 // Hello, World!
 // Value = 42
 // Value (Hex) = 0x2A
-// Current date = Tue Jul  4 15:30:02 2023
-// DayOfWeek = monday
+// Current date = 6/22/2026 10:47:09 AM
+// Day of week = monday
 // Duration = 23:05:24
