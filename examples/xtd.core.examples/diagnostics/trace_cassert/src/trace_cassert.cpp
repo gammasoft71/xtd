1e027202484b0901f2d9335dbf7c09be595d24df
Gammasoft <gammasoft71@gmail.com>
2026-06-23 20:39:22 +0200

    Refactor example

--- examples/xtd.core.examples/diagnostics/trace_cassert/src/trace_cassert.cpp
+++ examples/xtd.core.examples/diagnostics/trace_cassert/src/trace_cassert.cpp
@@ -2,10 +2,8 @@
 #include <xtd/xtd>
 
 auto main() -> int {
-  // Uncomment following lines to remove assert dialog
-  //for (auto listener : diagnostics::trace::listeners())
-  //  if (is<diagnostics::default_trace_listener>(listener))
-  //    as<diagnostics::default_trace_listener>(listener)->assert_ui_enabled(false);
+  // Uncomment following line to remove assert dialog
+  //diagnostics::debug::listeners().for_each([](auto listener) {if (is<diagnostics::default_trace_listener>(listener)) as<diagnostics::default_trace_listener>(listener)->assert_ui_enabled(false);});
 
   auto index = 0;
   console::write_line("Start application");
@@ -20,7 +18,7 @@ auto main() -> int {
 //
 // > If user clicks assert dialog 'Retry' button:
 // Start application
-// > Break the debugger on file tace_cassert.cpp line 13.
+// > Break the debugger on file tace_cassert.cpp line 10.
 // > If user choose continue running after break.
 // End application
 //
