50a776ffb963f6d08f94865e8b3875436b66f452
Gammasoft <gammasoft71@gmail.com>
2026-07-03 00:44:57 +0200

    Refactor example

--- examples/xtd.core.examples/format/format_pointer/src/format_pointer.cpp
+++ examples/xtd.core.examples/format/format_pointer/src/format_pointer.cpp
@@ -1,7 +1,7 @@
 #include <xtd/xtd>
 
 auto main() -> int {
-  auto ptr = static_cast<int*>(nullptr);
+  auto ptr = static_cast<int*>(null);
   console::out << string::format("{}", ptr) << environment::new_line;
   ptr = new int(42);
   console::out << string::format("0x{:x}", ptr) << environment::new_line;
@@ -9,15 +9,15 @@ auto main() -> int {
   delete ptr;
   
   auto sptr = xtd::sptr<int> {};
-  console::out << string::format("{}", sptr) << environment::new_line;
+  console::out << string::format("{}", sptr.get()) << environment::new_line;
   sptr = new_sptr<int>(42);
-  console::out << string::format("0x{:x}", sptr) << environment::new_line;
+  console::out << string::format("0x{:x}", sptr.get()) << environment::new_line;
   console::out << string::format("{}", *sptr) << environment::new_line;
   
   auto uptr = xtd::uptr<int> {};
-  console::out << string::format("{}", uptr) << environment::new_line;
+  console::out << string::format("{}", uptr.get()) << environment::new_line;
   uptr = new_uptr<int>(42);
-  console::out << string::format("0x{:x}", uptr) << environment::new_line;
+  console::out << string::format("0x{:x}", uptr.get()) << environment::new_line;
   console::out << string::format("{}", *uptr) << environment::new_line;
 }
 
