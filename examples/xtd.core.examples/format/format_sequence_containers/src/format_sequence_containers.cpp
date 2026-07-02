cac9ba35d79613b253f05e6f0901b984883a1273
Gammasoft <gammasoft71@gmail.com>
2026-07-03 00:45:02 +0200

    Refactor example

--- examples/xtd.core.examples/format/format_sequence_containers/src/format_sequence_containers.cpp
+++ examples/xtd.core.examples/format/format_sequence_containers/src/format_sequence_containers.cpp
@@ -2,24 +2,34 @@
 
 auto main() -> int {
   console::out << string::format("{}", array<int> {1, 2, 3, 4}) << environment::new_line;
+  console::out << string::format("{}", array<int, 2> {{1, 2, 3, 4}, {5, 6, 7, 8}}) << environment::new_line;
   console::out << string::format("{}", fixed_array<int, 4> {1, 2, 3, 4}) << environment::new_line;
+  console::out << string::format("{}", linked_list<int> {1, 2, 3, 4}) << environment::new_line;
   console::out << string::format("{}", list<int> {1, 2, 3, 4}) << environment::new_line;
   console::out << string::format("{}", collections::array_list {1, 2, 3, 4}) << environment::new_line;
+  console::out << string::format("{}", queue<int> {1, 2, 3, 4}) << environment::new_line;
+  console::out << string::format("{}", stack<int> {1, 2, 3, 4}) << environment::new_line;
   console::out << environment::new_line;
   console::out << string::format("{}", std::initializer_list<int> {1, 2, 3, 4}) << environment::new_line;
   console::out << string::format("{}", std::array<int, 4> {1, 2, 3, 4}) << environment::new_line;
   console::out << string::format("{}", std::forward_list<int> {1, 2, 3, 4}) << environment::new_line;
   console::out << string::format("{}", std::list<int> {1, 2, 3, 4}) << environment::new_line;
-  console::out << string::format("{}", std::deque<int> {1, 2, 3, 4}) << environment::new_line;
   console::out << string::format("{}", std::vector<int> {1, 2, 3, 4}) << environment::new_line;
+  console::out << string::format("{}", std::deque<int> {1, 2, 3, 4}) << environment::new_line;
+  console::out << string::format("{}", std::queue<int> {std::deque<int> {1, 2, 3, 4}}) << environment::new_line;
+  console::out << string::format("{}", std::stack<int> {std::deque<int> {1, 2, 3, 4}}) << environment::new_line;
 }
 
 // This code produces the following output :
 //
 // [1, 2, 3, 4]
+// [[1, 2, 3, 4], [5, 6, 7, 8]]
 // [1, 2, 3, 4]
 // [1, 2, 3, 4]
 // [1, 2, 3, 4]
+// [1, 2, 3, 4]
+// [1, 2, 3, 4]
+// [4, 3, 2, 1]
 //
 // [1, 2, 3, 4]
 // [1, 2, 3, 4]
@@ -27,3 +37,5 @@ auto main() -> int {
 // [1, 2, 3, 4]
 // [1, 2, 3, 4]
 // [1, 2, 3, 4]
+// [1, 2, 3, 4]
+// [4, 3, 2, 1]
