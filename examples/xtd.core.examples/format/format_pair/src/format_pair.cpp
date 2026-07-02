206a36821b5314a8ece00ddb0058b59ec386302b
Gammasoft <gammasoft71@gmail.com>
2026-07-03 00:44:52 +0200

    Refactor example

--- examples/xtd.core.examples/format/format_pair/src/format_pair.cpp
+++ examples/xtd.core.examples/format/format_pair/src/format_pair.cpp
@@ -2,9 +2,13 @@
 #include <utility>
 
 auto main() -> int {
+  console::out << string::format("{}", key_value_pair(1, "one")) << environment::new_line;
+  console::out << environment::new_line;
   console::out << string::format("{}", std::make_pair(1, "one")) << environment::new_line;
 }
 
 // This code produces the following output :
+//
+// (1, one)
 //
 // (1, one)
