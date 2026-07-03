43d9c2a70735f95aa6846891975d097d32affa7a
Gammasoft <gammasoft71@gmail.com>
2026-07-03 22:30:09 +0200

    Refactor example

--- examples/xtd.core.examples/generic_collections/generic_list_as_read_only/src/generic_list_as_read_only.cpp
+++ examples/xtd.core.examples/generic_collections/generic_list_as_read_only/src/generic_list_as_read_only.cpp
@@ -2,7 +2,7 @@
 
 class example {
 public:
-  static auto main() -> void {
+  static auto main() {
     auto dinosaurs = list<string> {};
     
     console::write_line("\ncapacity: {0}", dinosaurs.capacity());
