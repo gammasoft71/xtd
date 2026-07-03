9f9f87c26f0c4e2f25a1a2243c7f4306b74f2243
Gammasoft <gammasoft71@gmail.com>
2026-07-03 22:28:20 +0200

    Refactor example

--- examples/xtd.core.examples/generic_collections/generic_ienumerable3/src/generic_ienumerable3.cpp
+++ examples/xtd.core.examples/generic_collections/generic_ienumerable3/src/generic_ienumerable3.cpp
@@ -2,7 +2,7 @@
 
 class program {
 public:
-  static auto main() -> void {
+  static auto main() {
     auto boxes = box_collection {{10, 20, 30}, {20, 5, 10}, {12, 3, 7}};
     
     auto enumerator = boxes.get_enumerator();
@@ -26,15 +26,15 @@ public:
     int width = 0;
     int height = 0;
     
-    bool equals(const program::box& o) const noexcept override {return length == o.length && width == o.width && height == o.height;}
-    string to_string() const noexcept {return string::format("box [length={}, width={}, height={}]", length, width, height);}
+    auto equals(const program::box& o) const noexcept -> bool override {return length == o.length && width == o.width && height == o.height;}
+    auto to_string() const noexcept -> string {return string::format("box [length={}, width={}, height={}]", length, width, height);}
   };
   
   class box_collection : public ienumerable<program::box> {
   public:
     box_collection(const std::initializer_list<program::box>& boxes) : boxes_(boxes) {}
     
-    enumerator<program::box> get_enumerator() const override {
+    auto get_enumerator() const -> enumerator<program::box> override {
       return enumerator<>::create(boxes_);
     }
     
