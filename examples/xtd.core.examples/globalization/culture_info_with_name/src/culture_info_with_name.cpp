e22b87000910c81db716b83419c5519cfa8fa08a
Gammasoft <gammasoft71@gmail.com>
2026-07-04 18:15:47 +0200

    Refactor example

--- examples/xtd.core.examples/globalization/culture_info_with_name/src/culture_info_with_name.cpp
+++ examples/xtd.core.examples/globalization/culture_info_with_name/src/culture_info_with_name.cpp
@@ -5,11 +5,7 @@ using namespace xtd::globalization;
 auto main() -> int {
   auto current = culture_info::current_culture();
   console::write_line("The current culture is {}", current.name());
-  auto new_culture = culture_info {};
-  if (current.name().equals("fr-FR"))
-    new_culture = culture_info {"fr-LU"};
-  else
-    new_culture = culture_info {"fr-FR"};
+  auto new_culture = culture_info {current.name().equals("fr-FR") ? "fr-LU" : "fr-FR"};
   
   culture_info::current_culture(new_culture);
   console::write_line("The current culture is now {}", culture_info::current_culture().name());
