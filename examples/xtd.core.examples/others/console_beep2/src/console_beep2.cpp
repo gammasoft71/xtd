370ee6742d465d538b44bdddb452556708e0ba85
Gammasoft <gammasoft71@gmail.com>
2026-07-04 18:16:48 +0200

    Refactor example

--- examples/xtd.core.examples/others/console_beep2/src/console_beep2.cpp
+++ examples/xtd.core.examples/others/console_beep2/src/console_beep2.cpp
@@ -1,13 +1,14 @@
 #include <xtd/xtd>
 
-auto main(int argc, char* argv[]) -> int {
-  int x = 0;
-  //
-  if ((argc == 2) && (int32_object::try_parse(argv[1], x) == true) && ((x >= 1) && (x <= 9))) {
-    for (auto i = 1; i <= x; i++) {
-      console::out << string::format("Beep number {}.", i) << beep() << environment::new_line;
-      threading::thread::sleep(100_ms);
-    }
-  } else
-    console::out << "Usage: Enter the number of times (between 1 and 9) to beep." << environment::new_line;
+auto main() -> int {
+  auto beep_count = 0;
+  if (environment::get_command_line_args().length() != 2 || !int32_object::try_parse(environment::get_command_line_args()[1], beep_count) || beep_count < 1 || beep_count > 9) {
+    console::write_line("Usage: Enter the number of times (between 1 and 9) to beep.");
+    return 1;
+  }
+
+  for (auto i = 1; i <= beep_count; ++i) {
+    console::out << string::format("Beep number {}.", i) << beep() << environment::new_line;
+    threading::thread::sleep(300_ms);
+  }
 }
