a56903616911ddde7a796b175ba99949c3dce5ac
Gammasoft <gammasoft71@gmail.com>
2026-07-05 22:46:57 +0200

    Refactor code and add documentation

--- src/xtd.core/include/xtd/unused.hpp
+++ src/xtd.core/include/xtd/unused.hpp
@@ -2,11 +2,11 @@
 /// @brief Contains #__ and #unused_ keywords.
 /// @copyright Copyright (c) 2024 Gammasoft. All rights reserved.
 #pragma once
+#include <version>
 
-#if __cplusplus > 202302L
-#  define __ = _
-#else
-struct [[maybe_unused]] __xtd_place_holder_variable_with_no_name_struct___ {};
+/// @cond
+//#if !defined(__cpp_placeholder_variables)
+#if __cplusplus <= 202302L
 #  ifdef __COUNTER__
 #    define xtd_unique_id_ __COUNTER__
 #  else
@@ -14,8 +14,59 @@ struct [[maybe_unused]] __xtd_place_holder_variable_with_no_name_struct___ {};
 #  endif
 #  define __xtd_unused_concat_hidden__(a, b) a ## b
 #  define __xtd_unused_concat_(a, b) __xtd_unused_concat_hidden__(a, b)
-#  define unused_ \
-__xtd_unused_concat_(__xtd_one_no_name__, xtd_unique_id_) = __xtd_place_holder_variable_with_no_name_struct___ {};\
-[[maybe_unused]] auto __xtd_unused_concat_(__xtd_no_name__, xtd_unique_id_)
-#  define __ unused_
+struct [[maybe_unused]] __xtd_place_holder_variable_with_no_name_struct___ {};
 #endif
+/// @endcond
+
+//#if defined(__cpp_placeholder_variables)
+#if __cplusplus > 202302L
+/// @brief Represents placeholder variables with no name.
+/// @par Header
+/// ```cpp
+/// #include <xtd/unused>
+/// ```
+/// @par Namespace
+/// xtd
+/// @par Library
+/// xtd.core
+/// @ingroup xtd_core keywords
+/// @remarks Same as C++26 feature [_](https://cppreference.com/cpp/language/conflicting_declarations#Potentially-conflict_declarations) but for C++20 and later.
+/// @par Examples
+/// The following code shows how to use #unsued_ keyword.
+/// @include keyword_
+#  define unused_ = _
+#else
+/// @brief Represents placeholder variables with no name.
+/// @par Header
+/// ```cpp
+/// #include <xtd/unused>
+/// ```
+/// @par Namespace
+/// xtd
+/// @par Library
+/// xtd.core
+/// @ingroup xtd_core keywords
+/// @remarks Same as C++26 feature [_](https://cppreference.com/cpp/language/conflicting_declarations#Potentially-conflict_declarations) but for C++20 and later.
+/// @par Examples
+/// The following code shows how to use #unsued_ keyword.
+/// @include keyword_
+#  define unused_ \
+     __xtd_unused_concat_(__xtd_one_no_name__, xtd_unique_id_) = __xtd_place_holder_variable_with_no_name_struct___ {};\
+    [[maybe_unused]] auto __xtd_unused_concat_(__xtd_no_name__, xtd_unique_id_)
+#endif
+
+/// @brief Represents placeholder variables with no name.
+/// @par Header
+/// ```cpp
+/// #include <xtd/unused>
+/// ```
+/// @par Namespace
+/// xtd
+/// @par Library
+/// xtd.core
+/// @ingroup xtd_core keywords
+/// @remarks Same as C++26 feature [_](https://cppreference.com/cpp/language/conflicting_declarations#Potentially-conflict_declarations) but for C++20 and later.
+/// @par Examples
+/// The following code shows how to use #__ keyword.
+/// @include keyword_
+#define __ unused_
