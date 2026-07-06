8f313f21e783cdacc5d4c6f71016d6a582e4959c
Gammasoft <gammasoft71@gmail.com>
2026-07-06 11:29:42 +0200

    Update manual_tests.cpp

--- tests/xtd.core.manual_tests/src/manual_tests.cpp
+++ tests/xtd.core.manual_tests/src/manual_tests.cpp
@@ -1,17 +1,7 @@
 #include <xtd/xtd>
 
-#undef unused_
-#  define unused_ \
-  __xtd_unused_concat_(__xtd_no_name_, xtd_unique_id_)
-
-
 auto main() -> int {
-  [[maybe_unused]] auto __ = 42;
-  [[maybe_unused]] auto __ = "value string";
-  [[maybe_unused]] auto __ = diagnostics::stopwatch::start_new();
-  [[maybe_unused]] auto __ = date_time::now();
-  
-  auto [a, __, b, __] = std::make_tuple(42, "value string", diagnostics::stopwatch::start_new(), date_time::now());
+  println("Hello, World!");
 }
 
 // This code produces the following output :
