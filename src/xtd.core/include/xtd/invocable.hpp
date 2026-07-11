8dc57d1ccb629c42db0979c8e64c24ed0cb8e4db
Gammasoft <gammasoft71@gmail.com>
2026-07-11 23:52:24 +0200

    refactor invocable concept

--- src/xtd.core/include/xtd/invocable.hpp
+++ src/xtd.core/include/xtd/invocable.hpp
@@ -8,5 +8,5 @@
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
   template<class function_t, class result_t = void, class... args_t>
-  concept invocable =std::invocable<function_t, args_t...> && (std::is_void_v<result_t> || std::convertible_to<std::invoke_result_t<function_t, args_t...>, result_t>);
+  concept invocable = std::invocable<function_t, args_t...> && std::same_as<std::invoke_result_t<function_t, args_t...>, result_t>;
 }
