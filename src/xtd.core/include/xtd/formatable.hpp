16109807067fc027e1ec516e76da0ae43c102c6d
Gammasoft <gammasoft71@gmail.com>
2026-07-13 22:13:17 +0200

    Add culture_info

--- src/xtd.core/include/xtd/formatable.hpp
+++ src/xtd.core/include/xtd/formatable.hpp
@@ -2,6 +2,7 @@
 /// @brief Contains xtd::formatable concept.
 /// @copyright Copyright (c) 2026 Gammasoft. All rights reserved.
 #pragma once
+#include "globalization/culture_info.hpp"
 #include "stream_insertable.hpp"
 #include "iformatable.hpp"
 #include "raw_type.hpp"
@@ -15,6 +16,6 @@ namespace xtd {
   template<typename value_t>
   concept formatable =
   std::derived_from<xtd::raw_type<value_t>, xtd::iformatable> ||
-  requires(const xtd::raw_type<value_t>& value, xtd::string_view format) {{value.to_string(format)} -> xtd::textual;} ||
+  requires(const xtd::raw_type<value_t>& value, xtd::string_view format, xtd::globalization::culture_info culture) {{value.to_string(format, culture)} -> xtd::textual;} ||
   xtd::stream_insertable<xtd::raw_type<value_t>>;
 }
