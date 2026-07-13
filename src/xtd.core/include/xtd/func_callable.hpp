3ef7757b731c5a71a7df7c1b7a7b883768f544ed
Gammasoft <gammasoft71@gmail.com>
2026-07-13 21:10:41 +0200

    Add comment

--- src/xtd.core/include/xtd/func_callable.hpp
+++ src/xtd.core/include/xtd/func_callable.hpp
@@ -6,6 +6,7 @@
 
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
+  // Since Doxygen does not work properly with the C++ concept, the documentation for this concept is declared in xtd/func_callable_.hpp
   template<class function_t, class result_t = void, class... args_t>
   concept func_callable = xtd::invocable<function_t, result_t, args_t...>;
 }
