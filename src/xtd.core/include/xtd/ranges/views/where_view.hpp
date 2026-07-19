cd7ee4e805d8813fde4cc56e3f5c3fcf99c5b14a
Gammasoft <gammasoft71@gmail.com>
2026-07-19 22:11:27 +0200

    using xtd::iterable concept

--- src/xtd.core/include/xtd/ranges/views/where_view.hpp
+++ src/xtd.core/include/xtd/ranges/views/where_view.hpp
@@ -3,18 +3,11 @@
 /// @copyright Copyright (c) 2026 Gammasoft. All rights reserved.
 #pragma once
 
-#define __XTD_STD_INTERNAL__
-#include "../../internal/__xtd_std_version.hpp"
-#undef __XTD_STD_INTERNAL__
 #include "../../linq/enumerable.hpp"
 #include "../../istringable.hpp"
+#include "../../iterable.hpp"
 #include "../../string.hpp"
 #include "../../views/views.hpp"
-
-/// @cond
-#if defined(__xtd__cpp_lib_ranges)
-/// @endcond
-#include <ranges>
 
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
@@ -46,11 +39,11 @@ namespace xtd {
         struct with_predicate {
           const predicate_t& predicate;
           
-          template <std::ranges::range range_t>
-          auto operator()(range_t&& source) const {return xtd::linq::enumerable::as_enumerable(std::forward<range_t>(source)).where(predicate);}
+          template <xtd::iterable iterable_t>
+          auto operator()(iterable_t&& source) const {return xtd::linq::enumerable::as_enumerable(std::forward<iterable_t>(source)).where(predicate);}
           
-          template <std::ranges::range range_t>
-          friend auto operator |(range_t&& source, const with_predicate& self) {return self(std::forward<range_t>(source));}
+          template <xtd::iterable iterable_t>
+          friend auto operator |(iterable_t&& source, const with_predicate& self) {return self(std::forward<iterable_t>(source));}
         };
         
       public:
@@ -61,8 +54,8 @@ namespace xtd {
         /// @tparam range_t The type of the elements of source.
         /// @param source A sequence of values to filter.
         /// @param predicate A function to test each element for a condition.
-        template <std::ranges::range range_t, typename predicate_t>
-        auto operator()(range_t&& source, const predicate_t& predicate) const {return xtd::linq::enumerable::from(std::forward<range_t>(source)).where(predicate);}
+        template <xtd::iterable iterable_t, typename predicate_t>
+        auto operator()(iterable_t&& source, const predicate_t& predicate) const {return xtd::linq::enumerable::from(std::forward<iterable_t>(source)).where(predicate);}
         /// @brief Filters a sequence of values based on a predicate.
         /// @tparam range_t The type of the elements of source.
         /// @param predicate A function to test each element for a condition.
@@ -80,13 +73,10 @@ namespace xtd {
         /// auto result = numbers | where_view([](int x) { return x % 2 == 0; });
         /// // result = {2, 4}
         /// @endcode
-        template <std::ranges::range range_type>
-        friend auto operator |(range_type&& source, const where_view& view) {return view(std::forward<range_type>(source));}
+        template <xtd::iterable iterable_t>
+        friend auto operator |(iterable_t&& source, const where_view& view) {return view(std::forward<iterable_t>(source));}
         /// @}
       };
     }
   }
 }
-/// @cond
-#endif
-/// @endcond
