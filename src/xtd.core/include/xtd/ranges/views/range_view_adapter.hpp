bc31f1c3b0bd2347d15b4387cfd7ff71ec6c38b6
Gammasoft <gammasoft71@gmail.com>
2026-07-19 22:11:05 +0200

    using xtd::iterable concept

--- src/xtd.core/include/xtd/ranges/views/range_view_adapter.hpp
+++ src/xtd.core/include/xtd/ranges/views/range_view_adapter.hpp
@@ -3,14 +3,6 @@
 /// @copyright Copyright (c) 2026 Gammasoft. All rights reserved.
 #pragma once
 
-#define __XTD_STD_INTERNAL__
-#include "../../internal/__xtd_std_version.hpp"
-#undef __XTD_STD_INTERNAL__
-
-/// @cond
-#if defined(__xtd__cpp_lib_ranges)
-/// @endcond
-
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
 namespace xtd {
   /// @brief The ranges namespace is an extension and generalization of the xtd::linq that makes them more powerful by making them composable and less error-prone.
@@ -42,6 +34,3 @@ namespace xtd {
     }
   }
 }
-/// @cond
-#endif
-/// @endcond
