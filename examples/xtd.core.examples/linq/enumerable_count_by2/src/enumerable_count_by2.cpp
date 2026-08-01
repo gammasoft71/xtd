64e6a16a5e1b6303f10b9b623ddccf3aee40af31
Gammasoft <gammasoft71@gmail.com>
2026-08-01 20:10:24 +0200

    Add example

---
+++ examples/xtd.core.examples/linq/enumerable_count_by2/src/enumerable_count_by2.cpp
@@ -0,0 +1,27 @@
+#include <xtd/xtd>
+
+struct student {
+  std::string name;
+  std::string score;
+};
+
+auto main() -> int {
+  auto students = std::vector<student> {
+    {"Alice", "A"},
+    {"Bob", "B"},
+    {"Charlie", "C"},
+    {"David", "B"},
+    {"Eve", "A"}
+  };
+  
+  const auto& query = from(students).count_by<string>(_*member(&student::score));
+
+  for (const auto& [score, count] : query)
+    std::println("Students with a {}-score: {}", score, count);
+}
+
+// This code produces the following output :
+//
+// Students with a A-score: 2
+// Students with a B-score: 2
+// Students with a C-score: 1
