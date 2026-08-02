8253d08e738b0231c27eab9e88b05d20ce13e71f
Gammasoft <gammasoft71@gmail.com>
2026-08-02 19:36:41 +0200

    Refactor example

--- examples/xtd.core.examples/linq/enumerable_count_by/src/enumerable_count_by.cpp
+++ examples/xtd.core.examples/linq/enumerable_count_by/src/enumerable_count_by.cpp
@@ -14,7 +14,7 @@ auto main() -> int {
     {"Eve", "A"}
   };
   
-  const auto& query = students.count_by<string>(_*member(&student::score));
+  auto query = students.count_by<string>(_*member(&student::score));
 
   for (const auto& [score, count] : query)
     console::write_line("Students with a {}-score: {}", score, count);
