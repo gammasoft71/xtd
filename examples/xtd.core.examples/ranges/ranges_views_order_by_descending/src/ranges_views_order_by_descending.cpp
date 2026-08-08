4850b3af7a1dc845dc92a59d78c46c17a063a745
Gammasoft <gammasoft71@gmail.com>
2026-08-09 00:02:25 +0200

    using xtd::views

--- examples/xtd.core.examples/ranges/ranges_views_order_by_descending/src/ranges_views_order_by_descending.cpp
+++ examples/xtd.core.examples/ranges/ranges_views_order_by_descending/src/ranges_views_order_by_descending.cpp
@@ -1,5 +1,7 @@
 #include <xtd/xtd>
 
+using namespace views;
+
 auto main() -> int {
   auto numbers = array {2, 7, 3, 1, 4, 10, 5, 6, 8, 9};
   console::write_line("numbers = {}", numbers | order_by_descending(_));
