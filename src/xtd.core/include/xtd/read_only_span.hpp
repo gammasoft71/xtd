6dd65d7ff935060d70da310a6f8ee78a04e363df
Gammasoft <gammasoft71@gmail.com>
2026-08-09 01:30:10 +0200

    Add constructor with specified collection, and range

--- src/xtd.core/include/xtd/read_only_span.hpp
+++ src/xtd.core/include/xtd/read_only_span.hpp
@@ -12,6 +12,7 @@
 #include "null.hpp"
 #include "object.hpp"
 #include "ptrdiff.hpp"
+#include "range.hpp"
 #include "views/views.hpp"
 #include "span.hpp"
 #include "typeof.hpp"
@@ -158,6 +159,16 @@ namespace xtd {
     /// @param length The number of elements to constuct.
     constexpr read_only_span(const type_t* data, size_type length) : data_ {data}, length_ {extent != dynamic_extent ? extent : length} {
       if (!data) xtd::helpers::throw_helper::throws(xtd::helpers::exception_case::argument_null);
+    }
+    /// @brief Creates an xtd::read_only_span with specified collection, and range.
+    /// @param items The collection to construct a view for.
+    /// @param range The range of elements in the collection.
+    /// @exception xtd::argument_out_of_range_exception if range.start or range.start + range.end - range.start are greater than items size.
+    template<typename collection_t>
+    constexpr read_only_span(collection_t& items, const xtd::range& range) : data_ {items.data() + range.start()} {
+      auto length = (range.end() > std::numeric_limits<size_type>::max() / 2 ? (items.size() - ~range.end()) : range.end()) - range.start();
+      if (range.start() + length > items.size()) xtd::helpers::throw_helper::throws(xtd::helpers::exception_case::argument_out_of_range);
+      length_ = extent != dynamic_extent ? extent : length;
     }
     /// @}
     
@@ -401,6 +412,9 @@ namespace xtd {
   template<typename collection_t>
   read_only_span(const collection_t& items, xtd::usize, xtd::usize) -> read_only_span<typename collection_t::value_type>;
   
+  template<typename collection_t>
+  read_only_span(const collection_t&, const xtd::range&) -> read_only_span<const typename collection_t::value_type>;
+
   template<typename type_t>
   read_only_span(const type_t* data, xtd::usize size) -> read_only_span<type_t>;
   // }
