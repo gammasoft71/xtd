f43106c7fa9fe17c97160024db0b78a4181b40bf
Gammasoft <gammasoft71@gmail.com>
2026-08-13 00:42:37 +0200

    Fix xtd::index unit tests

--- tests/xtd.core.unit_tests/src/xtd/tests/index_tests.cpp
+++ tests/xtd.core.unit_tests/src/xtd/tests/index_tests.cpp
@@ -5,9 +5,6 @@
 #include <xtd/tunit/test_class_attribute>
 #include <xtd/tunit/test_method_attribute>
 
-using namespace xtd;
-using namespace xtd::tunit;
-
 namespace xtd::tests {
   class test_class_(index_tests) {
     auto test_method_(default_ctor) {
@@ -52,29 +49,43 @@ namespace xtd::tests {
       assert::is_true(xtd::index {~2_z}.is_from_end());
     }
     
-    auto test_method_(bpos) {
-      assert::is_zero(xtd::index::bpos);
-      assert::is_zero(xtd::index::bpos.value());
-      assert::is_false(xtd::index::bpos.is_from_end());
+    auto test_method_(end) {
+      assert::are_equal(18446744073709551615_z, xtd::index::end);
+      assert::is_zero(xtd::index::end.value());
+      assert::is_true(xtd::index::end.is_from_end());
+    }
+
+    auto test_method_(last) {
+      assert::are_equal(18446744073709551614_z, xtd::index::last);
+      assert::are_equal(1_z, xtd::index::last.value());
+      assert::is_true(xtd::index::last.is_from_end());
     }
     
-    auto test_method_(epos) {
-      assert::are_equal(18446744073709551614_z, xtd::index::epos);
-      assert::are_equal(1_z, xtd::index::epos.value());
-      assert::is_true(xtd::index::epos.is_from_end());
+    auto test_method_(start) {
+      assert::is_zero(xtd::index::start);
+      assert::is_zero(xtd::index::start.value());
+      assert::is_false(xtd::index::start.is_from_end());
     }
-    
-    auto test_method_(npos) {
-      assert::are_equal(18446744073709551615_z, xtd::index::npos);
-      assert::is_zero(xtd::index::npos.value());
-      assert::is_true(xtd::index::npos.is_from_end());
-    }
-    
-    auto test_method_(access_array_item_with_bpos) {
+
+    auto test_method_(access_array_item_with_last) {
       auto items = array {1, 2, 3, 4};
-      assert::are_equal(1, items[xtd::index::bpos]);
-      assert::are_equal(2, items[xtd::index::bpos + 1]);
-      assert::throws<index_out_of_range_exception>([&items] {auto _ = items[xtd::index::bpos - 1];});
+      assert::are_equal(4, items[xtd::index::last]);
+      assert::are_equal(3, items[xtd::index::last - 1]);
+      assert::throws<index_out_of_range_exception>([&items] {auto _ = items[xtd::index::last + 1];});
+    }
+    
+    auto test_method_(access_array_item_with_end) {
+      auto items = array {1, 2, 3, 4};
+      assert::throws<index_out_of_range_exception>([&items] {auto _ = items[xtd::index::end];});
+      assert::are_equal(1, items[xtd::index::end + 1]);
+      assert::are_equal(4, items[xtd::index::end - 1]);
+    }
+
+    auto test_method_(access_array_item_with_start) {
+      auto items = array {1, 2, 3, 4};
+      assert::are_equal(1, items[xtd::index::start]);
+      assert::are_equal(2, items[xtd::index::start + 1]);
+      assert::throws<index_out_of_range_exception>([&items] {auto _ = items[xtd::index::start - 1];});
     }
   };
 }
