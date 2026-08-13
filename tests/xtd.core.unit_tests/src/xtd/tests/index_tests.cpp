8b5e21f3fb48bf3eed6b3ff7b4057c16d1e5a02e
Gammasoft <gammasoft71@gmail.com>
2026-08-13 15:34:32 +0200

    fix xtd::index unit tests

--- tests/xtd.core.unit_tests/src/xtd/tests/index_tests.cpp
+++ tests/xtd.core.unit_tests/src/xtd/tests/index_tests.cpp
@@ -13,58 +13,67 @@ namespace xtd::tests {
       assert::is_false(xtd::index {}.is_from_end());
     }
     
-    auto test_method_(ctor_with_0_z) {
-      assert::is_zero(xtd::index {0_z});
-      assert::is_zero(xtd::index {0_z}.value());
-      assert::is_false(xtd::index {0_z}.is_from_end());
+    auto test_method_(ctor_with_0) {
+      assert::is_zero(xtd::index {0});
+      assert::is_zero(xtd::index {0}.value());
+      assert::is_false(xtd::index {0}.is_from_end());
+      assert::is_zero(xtd::index {0}.get_offset(10));
     }
     
-    auto test_method_(ctor_with_1_z) {
-      assert::are_equal(1_z, xtd::index {1_z});
-      assert::are_equal(1_z, xtd::index {1_z}.value());
-      assert::is_false(xtd::index {1_z}.is_from_end());
+    auto test_method_(ctor_with_1) {
+      assert::are_equal(1_z, xtd::index {1});
+      assert::are_equal(1_z, xtd::index {1}.value());
+      assert::is_false(xtd::index {1}.is_from_end());
+      assert::are_equal(1_z, xtd::index {1}.get_offset(10));
     }
     
-    auto test_method_(ctor_with_2_z) {
-      assert::are_equal(2_z, xtd::index {2_z});
-      assert::are_equal(2_z, xtd::index {2_z}.value());
-      assert::is_false(xtd::index {2_z}.is_from_end());
+    auto test_method_(ctor_with_2) {
+      assert::are_equal(2_z, xtd::index {2});
+      assert::are_equal(2_z, xtd::index {2}.value());
+      assert::is_false(xtd::index {2}.is_from_end());
+      assert::are_equal(2_z, xtd::index {2}.get_offset(10));
     }
     
-    auto test_method_(ctor_with_not_0_z) {
-      assert::are_equal(18446744073709551615_z, xtd::index {~0_z});
-      assert::is_zero(xtd::index {~0_z}.value());
-      assert::is_true(xtd::index {~0_z}.is_from_end());
+    auto test_method_(ctor_with_not_0) {
+      assert::are_equal(18446744073709551615_z, xtd::index {~0});
+      assert::is_zero(xtd::index {~0}.value());
+      assert::is_true(xtd::index {~0}.is_from_end());
+      assert::are_equal(10_z, xtd::index {~0}.get_offset(10));
     }
     
-    auto test_method_(ctor_with_not_1_z) {
-      assert::are_equal(18446744073709551614_z, xtd::index {~1_z});
-      assert::are_equal(1_z, xtd::index {~1_z}.value());
-      assert::is_true(xtd::index {~1_z}.is_from_end());
+    auto test_method_(ctor_with_not_1) {
+      assert::are_equal(18446744073709551614_z, xtd::index {~1});
+      assert::are_equal(1_z, xtd::index {~1}.value());
+      assert::is_true(xtd::index {~1}.is_from_end());
+      assert::are_equal(9_z, xtd::index {~1}.get_offset(10));
     }
     
-    auto test_method_(ctor_with_not_2_z) {
-      assert::are_equal(18446744073709551613_z, xtd::index {~2_z});
-      assert::are_equal(2_z, xtd::index {~2_z}.value());
-      assert::is_true(xtd::index {~2_z}.is_from_end());
+    auto test_method_(ctor_with_not_2) {
+      assert::are_equal(18446744073709551613_z, xtd::index {~2});
+      assert::are_equal(2_z, xtd::index {~2}.value());
+      assert::is_true(xtd::index {~2}.is_from_end());
+      assert::are_equal(8_z, xtd::index {~2}.get_offset(10));
     }
     
     auto test_method_(end) {
       assert::are_equal(18446744073709551615_z, xtd::index::end);
       assert::is_zero(xtd::index::end.value());
       assert::is_true(xtd::index::end.is_from_end());
+      assert::are_equal(10_z, xtd::index::end.get_offset(10));
     }
 
     auto test_method_(last) {
       assert::are_equal(18446744073709551614_z, xtd::index::last);
       assert::are_equal(1_z, xtd::index::last.value());
       assert::is_true(xtd::index::last.is_from_end());
+      assert::are_equal(9_z, xtd::index::last.get_offset(10));
     }
     
     auto test_method_(start) {
       assert::is_zero(xtd::index::start);
       assert::is_zero(xtd::index::start.value());
       assert::is_false(xtd::index::start.is_from_end());
+      assert::is_zero(xtd::index::start.get_offset(10));
     }
 
     auto test_method_(access_array_item_with_last) {
