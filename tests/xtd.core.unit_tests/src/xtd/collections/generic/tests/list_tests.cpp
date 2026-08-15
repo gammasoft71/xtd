e49f4ed9d69e2d168ac7f5c463ae60b184e5aad5
Gammasoft <gammasoft71@gmail.com>
2026-08-15 13:54:22 +0200

    Replace epos by xtd::index::last

--- tests/xtd.core.unit_tests/src/xtd/collections/generic/tests/list_tests.cpp
+++ tests/xtd.core.unit_tests/src/xtd/collections/generic/tests/list_tests.cpp
@@ -56,10 +56,6 @@ namespace xtd::collections::generic::tests {
       assert::are_equal(typeof_<const int*>(), typeof_<list<int>::const_pointer>());
     }
     
-    auto test_method_(epos) {
-      assert::are_equal(usize_object::max_value - 1, list<int>::epos);
-    }
-    
     auto test_method_(npos) {
       assert::are_equal(usize_object::max_value, list<int>::npos);
     }
@@ -873,22 +869,19 @@ namespace xtd::collections::generic::tests {
       collection_assert::are_equal({63, 31, 10}, items);
     }
     
-    auto test_method_(index_operators_with_epos) {
+    auto test_method_(index_operators_with_index_last) {
       auto items = list {1, 2, 3, 4, 5};
       
       assert::are_equal(5, items[items.count() - 1]);
-      assert::are_equal(5, items[items.epos]);
-      assert::are_equal(5, items[xtd::epos]);
+      assert::are_equal(5, items[xtd::index::last]);
       
-      items[items.epos] = 6;
+      items[xtd::index::last] = 6;
       assert::are_equal(6, items[items.count() - 1]);
-      assert::are_equal(6, items[items.epos]);
-      assert::are_equal(6, items[xtd::epos]);
+      assert::are_equal(6, items[xtd::index::last]);
       
-      items[xtd::epos] = 7;
+      items[xtd::index::last] = 7;
       assert::are_equal(7, items[items.count() - 1]);
-      assert::are_equal(7, items[items.epos]);
-      assert::are_equal(7, items[xtd::epos]);
+      assert::are_equal(7, items[xtd::index::last]);
     }
     
     auto test_method_(cast_operator_to_std_vector) {
