8b4e4631927ba9e3d3c104f49261e02101d4202e
Gammasoft <gammasoft71@gmail.com>
2026-08-15 13:51:21 +0200

    Remove epos

--- src/xtd.core/include/xtd/collections/generic/helpers/raw_array.hpp
+++ src/xtd.core/include/xtd/collections/generic/helpers/raw_array.hpp
@@ -144,7 +144,6 @@ namespace xtd {
           
           /// @{
           inline static constexpr size_type npos = std::numeric_limits<size_type>::max(); ///< Represents an invalid index.
-          static inline constexpr size_type epos = npos - 1; ///< End position.
           /// @}
           
           /// @name Public Constructors
