6b9b313fa4a032528a8bf88a6556138775963984
Gammasoft <gammasoft71@gmail.com>
2026-08-20 19:56:36 +0200

    using xtd::basic_string_view

--- src/xtd.core/include/xtd/wstring_view.hpp
+++ src/xtd.core/include/xtd/wstring_view.hpp
@@ -2,7 +2,7 @@
 /// @brief Contains xtd::wstring_view alias.
 /// @copyright Copyright (c) 2026 Gammasoft. All rights reserved.
 #pragma once
-#include <string_view>
+#include "basic_string_view.hpp"
 #include "wchar.hpp"
 
 /// @brief The xtd namespace contains all fundamental classes to access Hardware, Os, System, and more.
@@ -13,5 +13,5 @@ namespace xtd {
   /// @par Library
   /// xtd.core
   /// @ingroup xtd_core system strings
-  using wstring_view = std::basic_string_view<xtd::wchar>;
+  using wstring_view = xtd::basic_string_view<xtd::wchar>;
 }
