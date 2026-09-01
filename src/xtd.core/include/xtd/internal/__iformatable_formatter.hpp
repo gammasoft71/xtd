df0270ba68bac634da487d6558f74deb90385d81
Gammasoft <gammasoft71@gmail.com>
2026-09-01 23:32:14 +0200

    Fix xtd::string::format

--- src/xtd.core/include/xtd/internal/__iformatable_formatter.hpp
+++ src/xtd.core/include/xtd/internal/__iformatable_formatter.hpp
@@ -10,6 +10,7 @@
 #include "../helpers/throw_helper.hpp"
 #include "../iformatable.hpp"
 #include "../istringable.hpp"
+#include "../textual.hpp"
 #include "../raw_type.hpp"
 #include <type_traits>
 
@@ -29,6 +30,8 @@ template<typename value_t>
   else if constexpr(std::derived_from<xtd::raw_type<value_t>, xtd::istringable<xtd::raw_type<value_t>>>) return __to_string_istringable_to_string(static_cast<const xtd::istringable<value_t>*>(&value));
   else if constexpr(std::derived_from<xtd::raw_type<value_t>, xtd::object>) return __to_string_object_to_string(static_cast<const xtd::object*>(&value));
   else if constexpr(std::derived_from<xtd::raw_type<value_t>, std::exception>) return std::string {"exception: "} + static_cast<const std::exception&>(value).what();
+  else if constexpr(requires(const xtd::raw_type<value_t>& value, const xtd::string& _fmt) {{value.to_string(_fmt)} -> xtd::textual;}) return value.to_string(fmt);
+  else if constexpr(requires(const xtd::raw_type<value_t>& value) {{value.to_string()} -> xtd::textual;}) return value.to_string();
   else xtd::helpers::throw_helper::throws(xtd::helpers::exception_case::format_not_iformatable, typeid(value));
 }
 /// @endcond
