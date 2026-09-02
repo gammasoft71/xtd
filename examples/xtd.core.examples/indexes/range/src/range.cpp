d94f238cf2ef7a0b0a34615327047fbfa9d049fc
Gammasoft <gammasoft71@gmail.com>
2026-09-02 12:32:51 +0200

    refactor example

--- examples/xtd.core.examples/indexes/range/src/range.cpp
+++ examples/xtd.core.examples/indexes/range/src/range.cpp
@@ -8,12 +8,12 @@ auto main() -> int {
   println("items[range::start_at(index::from_end(8))] = {}", items[range::start_at(index::from_end(8))]);
   println("items(range::start_at(~8_i)) = {}", items(range::start_at(~8_i)));
   println("items[range::end_at(6)] = {}", items[range::end_at(6)]);
-  println("items[range::end_at(index::from_start(6))] = {}", items[range::end_at(index::from_start(6))]);
+  println("items(range::end_at(index::from_start(6))) = {}", items(range::end_at(index::from_start(6))));
   println("items(range::end_at(~4_i)) = {}", items(range::end_at(~4_i)));
   println("items(range {{2, 6}}) = {}", items[range {2, 6}]);
-  println("items(range {{3_i, ~2_i}}) = {}", items[range {3_i, ~2_i}]);
-  println("items({{0, 8}}) = {}", items[{0, 8}]);
-  println("items({{1_i, ~4_i}}) = {}", items[{1_i, ~4_i}]);
+  println("items[range {{3_i, ~2_i}}) = {}", items[range {3_i, ~2_i}]);
+  println("items[{{0, 8}}] = {}", items[{0, 8}]);
+  println("items[{{1_i, ~4_i}}][{{1_i, ~1_i}}] = {}", items[{1_i, ~4_i}][{1_i, ~1_i}]);
 }
 
 // This code produces the following output :
@@ -21,12 +21,11 @@ auto main() -> int {
 // items[range::all()] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 // items(range::start_at(2)) = [3, 4, 5, 6, 7, 8, 9, 10]
 // items[range::start_at(index::from_end(8))] = [3, 4, 5, 6, 7, 8, 9, 10]
-// items[range::start_at(~8_i)] = [3, 4, 5, 6, 7, 8, 9, 10]
+// items(range::start_at(~8_i)) = [3, 4, 5, 6, 7, 8, 9, 10]
 // items[range::end_at(6)] = [1, 2, 3, 4, 5, 6]
-// items[range::end_at(index::from_start(6))] = [1, 2, 3, 4, 5, 6]
+// items(range::end_at(index::from_start(6))) = [1, 2, 3, 4, 5, 6]
 // items(range::end_at(~4_i)) = [1, 2, 3, 4, 5, 6]
 // items(range {2, 6}) = [3, 4, 5, 6]
-// items(range {3_i, ~2_i}) = [4, 5, 6, 7, 8]
-// items({0, 8}) = [1, 2, 3, 4, 5, 6, 7, 8]
-// items({1_i, ~4_i}) = [2, 3, 4, 5, 6]
-
+// items[range {3_i, ~2_i}) = [4, 5, 6, 7, 8]
+// items[{0, 8}] = [1, 2, 3, 4, 5, 6, 7, 8]
+// items[{1_i, ~4_i}][{1_i, ~1_i}] = [3, 4, 5]
