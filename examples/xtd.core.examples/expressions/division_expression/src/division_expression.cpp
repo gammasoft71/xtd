e15249c3b7f2c9e51f093c25aa0df415e1cc4175
Gammasoft <gammasoft71@gmail.com>
2026-09-03 20:37:49 +0200

    refactor example

--- examples/xtd.core.examples/expressions/division_expression/src/division_expression.cpp
+++ examples/xtd.core.examples/expressions/division_expression/src/division_expression.cpp
@@ -8,32 +8,30 @@ struct foo {
 };
 
 auto main() -> int {
-  // auto div1 = [](auto&& a) {return a / 2;}
-  auto div1 = _ / 2;
-
-  println("div1 resul => {}", div1(84));
+  // auto expr1 = [](auto&& a) {return a / 2;}
+  auto expr1 = _ / 2;
+  println("expr1 = {}", expr1(84));
   println();
 
-  // auto div2 = [](auto&& a, auto&& b) {return a / b;}
-  auto div2 = _1 / _2;
-
-  println("div2 resul => {}", div2(84, 2));
-  println("div2 resul => {}", div2(foo {84}, foo {2}));
+  // auto expr2 = [](auto&& a, auto&& b) {return a / b;}
+  auto expr2 = _1 / _2;
+  println("expr2 = {}", expr2(84, 2));
+  println("expr2 = {}", expr2(foo {84}, foo {2}));
   println();
 
-  // auto div3 = [](auto&& a, auto&& b, auto&& c) {return a / 2 / b / c;}
-  auto div3 = _1 / 2 / _2  / _3;
+  // auto expr3 = [](auto&& a, auto&& b, auto&& c) {return a / 2 / b / c;}
+  auto expr3 = _1 / 2 / _2  / _3;
 
-  println("div3 resul => {}", div3(84, 7, 3));
+  println("expr3 = {}", expr3(84, 7, 3));
   println();
 }
 
 // This code produces the following output :
 //
-// div1 resul => 42
+// expr1 = 42
 //
-// div2 resul => 42
-// div2 resul => 42
+// expr2 = 42
+// expr2 = 42
 //
-// div3 resul => 2
+// expr3 = 2
 //
