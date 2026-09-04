73f8745123f0b9364b17220b7bebed8ce9829d1f
Gammasoft <gammasoft71@gmail.com>
2026-09-04 11:52:27 +0200

    rename less_than_expression to less_than

--- src/xtd.core/include/xtd/xtd.core.hpp
+++ src/xtd.core/include/xtd/xtd.core.hpp
@@ -129,7 +129,7 @@
 #include "expressions/greater_than_or_equal.hpp"
 #include "expressions/invocation_expression.hpp"
 #include "expressions/left_shift.hpp"
-#include "expressions/less_than_expression.hpp"
+#include "expressions/less_than.hpp"
 #include "expressions/less_than_or_equal_expression.hpp"
 #include "expressions/logical_and_expression.hpp"
 #include "expressions/logical_not_expression.hpp"
