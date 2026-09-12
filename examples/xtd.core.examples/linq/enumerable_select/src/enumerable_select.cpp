a6203df47e83abf3cc9d2eaa8c8b384ee0e8ef5c
Gammasoft <gammasoft71@gmail.com>
2026-09-12 19:56:32 +0200

    Refactor example

--- examples/xtd.core.examples/linq/enumerable_select/src/enumerable_select.cpp
+++ examples/xtd.core.examples/linq/enumerable_select/src/enumerable_select.cpp
@@ -3,8 +3,9 @@
 auto main() -> int {
   auto fruits = array<string> {"apple", "banana", "mango", "orange", "passionfruit", "grape"};
   
-  const auto& query = fruits.select<std::tuple<usize, string>>([](auto&& fruit, auto index) {
-    return std::make_tuple(index, fruit.substring(0, index));
+  const auto& query = fruits.select([](auto&& fruit) {
+    static auto index = 0_z;
+    return std::make_tuple(index, fruit.substring(0, index++));
   });
 
   for (const auto& [index, str] : query)
