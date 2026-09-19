63abf022b2f99d591c88ddbac20ddfc536c06a08
Gammasoft <gammasoft71@gmail.com>
2026-09-19 18:06:30 +0200

    Add concurrent_bag example

---
+++ examples/xtd.core.examples/concurrent_collections/concurrent_bag/README.md
@@ -0,0 +1,22 @@
+# concurrent_bag
+
+Shows how to use [xtd::collections::concurrent::concurrent_bag](https://gammasoft71.github.io/xtd/reference_guides/latest/classxtd_1_1collections_1_1concurrent_1_1concurrent__bag.html) class.
+
+## Sources
+
+* [src/concurrent_bag.cpp](src/concurrent_bag.cpp)
+* [CMakeLists.txt](CMakeLists.txt)
+
+## Build and run
+
+Open "Command Prompt" or "Terminal". Navigate to the folder that contains the project and type the following:
+
+```cmake
+xtdc run
+```
+
+## Output
+
+```
+outer_sum = 49995000, should be 49995000
+```
---
+++ examples/xtd.core.examples/concurrent_collections/concurrent_bag/src/concurrent_bag.cpp
@@ -0,0 +1,57 @@
+#include <xtd/xtd>
+
+using namespace xtd::collections::concurrent;
+
+class example {
+public:
+  // Demonstrates:
+  //      concurrent_bag<type_t>::add()
+  //      concurrent_bag<type_t>::is_empty()
+  //      concurrent_bag<type_t>::try_take()
+  //      concurrent_bag<type_t>::try_peek()
+  static auto main() {
+    // Add to concurrent_bag concurrently
+    auto cb = concurrent_bag<int>();
+    auto bag_add_tasks = list<task<>>();
+    for (auto i = 0; i < 500; i++) {
+      auto number_to_add = i;
+      bag_add_tasks.add(task<>::run([&cb, number_to_add] {cb.add(number_to_add);}));
+    }
+    
+    // Wait for all tasks to complete
+    task<>::wait_all(bag_add_tasks);
+    
+    // Consume the items in the bag
+    auto bag_consume_tasks = list<task<>>();
+    auto items_in_bag = 0;
+    while (!cb.is_empty()) {
+      bag_consume_tasks.add(task<>::run([&cb, &items_in_bag] {
+        auto item = 0;
+        if (cb.try_take(item)) {
+          console::write_line(item);
+          interlocked::increment(items_in_bag);
+        }
+      }));
+    }
+    task<>::wait_all(bag_consume_tasks);
+    
+    console::write_line("There were {} items in the bag", items_in_bag);
+    
+    // Checks the bag for an item
+    // The bag should be empty and this should not print anything
+    auto unexpected_item = 0;
+    if (cb.try_peek(unexpected_item))
+      console::write_line("Found an item in the bag when it should be empty");
+  }
+};
+
+startup_(example::main);
+
+// This code produces the following output :
+//
+// 276
+// 265
+// ...
+// 420
+// 423
+// There were 500 items in the bag
